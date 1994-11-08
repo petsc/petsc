7807a1fad4139fba6164672f0e8ae3a049325cc9
Barry Smith <bsmith@mcs.anl.gov>
1994-11-07 22:20:44 -0600

    Initial revision

---
+++ src/mat/utils/gcreate.c
@@ -0,0 +1,27 @@
+
+#include "sys.h"
+#include "options.h"
+#include "sysio.h"
+#include "mat.h"
+#include "comm.h"
+
+/*@C
+      MatCreateInitialMatrix - Reads from command line to determine 
+           what type of matrix to create.
+
+  Input Parameters:
+.   m,n - matrix dimensions
+ 
+  Output Parameter:
+.   V - location to stash resulting matrix.
+@*/
+int MatCreateInitialMatrix(int m,int n,Mat *V)
+{
+  if (OptionsHasName(0,"-dense")) {
+    fprintf(stdout,"Using BLAS+LAPACK sequential dense matrices\n");
+    return MatCreateSequentialDense(m,n,V);
+  }
+  fprintf(stdout,"Using standard sequential AIJ vectors\n");
+  return MatCreateSequentialAIJ(m,n,10,V);
+}
+ 
