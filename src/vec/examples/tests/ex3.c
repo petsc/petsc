c1e9a06e7a458229d0fc5482fa3d1489e8ac5a9c
Barry Smith <bsmith@mcs.anl.gov>
1995-01-04 22:11:52 -0600

    AutoCheckin

--- src/vec/examples/tests/ex3.c
+++ src/vec/examples/tests/ex3.c
@@ -18,32 +18,30 @@ int main(int argc,char **argv)
   Scalar       one = 1.0, two = 2.0, three = 3.0, dots[3],dot;
   double       norm;
   Vec          x,y,w,*z;
-  IS           is1,is2;
-  ISScatterCtx ctx = 0;
+  int          idx;
 
   MPI_Init(&argc,&argv);
   OptionsCreate(argc,argv,(char*)0,(char*)0);
-  OptionsGetInt(0,"-n",&n);
+  OptionsGetInt(0,"-n",&n); if (n < 5) n = 5;
   MPI_Comm_size(MPI_COMM_WORLD,&numtids);
-  if (numtids != 2) SETERR(1,"Must be run with 2 processors\n");
   MPI_Comm_rank(MPI_COMM_WORLD,&mytid); 
 
   /* create two vector */
   ierr = VecCreateSequential(n,&x); CHKERR(ierr);
   ierr = VecCreateMPI(MPI_COMM_WORLD,n,-1,&y); CHKERR(ierr);
-
-  /* create two index sets */
-  ierr = ISCreateSequential(2,idx1,&is1); CHKERR(ierr);
-  ierr = ISCreateParallel(1,2,&mytid,MPI_COMM_WORLD,&is2); CHKERR(ierr);
-
-ISView(is2,0);
-
   ierr = VecSet(&one,x);CHKERR(ierr);
   ierr = VecSet(&two,y);CHKERR(ierr);
-  ierr = VecScatterBegin(x,is1,y,is2,&ctx); CHKERR(ierr);
-  ierr = VecScatterEnd(x,is1,y,is2,&ctx); CHKERR(ierr);
-  
-  VecView(x,0);
+
+  if (mytid == 1) {
+    idx = 2; ierr = VecInsertValues(y,1,&idx,&three); CHKERR(ierr);  
+    idx = 0; ierr = VecInsertValues(y,1,&idx,&one); CHKERR(ierr); 
+  }
+  else {
+    idx = 7; ierr = VecInsertValues(y,1,&idx,&three); CHKERR(ierr); 
+  } 
+  ierr = VecBeginAssembly(y); CHKERR(ierr);
+  ierr = VecEndAssembly(y); CHKERR(ierr);
+
   VecView(y,0);
 
   ierr = VecDestroy(x);CHKERR(ierr);
