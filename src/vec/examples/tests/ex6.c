571df359d2d35c098eec98ddf7d5629ee4430835
Barry Smith <bsmith@mcs.anl.gov>
1995-01-08 21:23:51 -0600

    Initial revision

---
+++ src/vec/examples/tests/ex6.c
@@ -0,0 +1,48 @@
+
+
+/*
+    Demonstrates a scatter with a stride and general index set
+*/
+#include "petsc.h"
+#include "comm.h"
+#include "is.h"
+#include "vec.h"
+#include "sys.h"
+#include "options.h"
+#include "sysio.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int           n = 6, ierr, idx1[3] = {0,1,2}, loc[6] = {0,1,2,3,4,5};
+  Scalar        one = 1.0, two = 2.0, vals[6] = {10,11,12,13,14,15};
+  double        norm;
+  Vec           x,y,w,*z;
+  IS            is1,is2;
+  VecScatterCtx ctx = 0;
+
+  OptionsCreate(argc,argv,(char*)0,(char*)0);
+
+  /* create two vector */
+  ierr = VecCreateSequential(n,&x); CHKERR(ierr);
+  ierr = VecCreate(x,&y); CHKERR(ierr);
+
+  /* create two index sets */
+  ierr = ISCreateSequential(3,idx1,&is1); CHKERR(ierr);
+  ierr = ISCreateStrideSequential(3,0,2,&is2); CHKERR(ierr);
+
+  ierr = VecInsertValues(x,6,loc,vals); CHKERR(ierr);
+  VecView(x,0); printf("----\n");
+  ierr = VecSet(&two,y);CHKERR(ierr);
+  ierr = VecScatterBegin(x,is1,y,is2,&ctx); CHKERR(ierr);
+  ierr = VecScatterEnd(x,is1,y,is2,&ctx); CHKERR(ierr);
+  ierr = VecScatterCtxDestroy(ctx); CHKERR(ierr);
+  
+  VecView(y,0);
+
+  ierr = VecDestroy(x);CHKERR(ierr);
+  ierr = VecDestroy(y);CHKERR(ierr);
+
+  return 0;
+}
+ 
---
+++ src/vec/examples/tutorials/ex2.c
@@ -0,0 +1,45 @@
+
+
+/*
+      Builds a parallel vector with 1 component on the first processor
+   2 on the second,... Then each processor adds one to all elements 
+   except the last mytid.
+
+*/
+#include "petsc.h"
+#include "comm.h"
+#include "is.h"
+#include "vec.h"
+#include "sys.h"
+#include "options.h"
+#include "sysio.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int          i,N,ierr, numtids,mytid;
+  Scalar       one = 1.0;
+  Vec          x;
+
+  PetscInitialize(&argc,&argv,0,0);
+  MPI_Comm_size(MPI_COMM_WORLD,&numtids);
+  MPI_Comm_rank(MPI_COMM_WORLD,&mytid); 
+
+  ierr = VecCreateMPI(MPI_COMM_WORLD,mytid+1,-1,&x); CHKERR(ierr);
+  ierr = VecGetSize(x,&N);
+  ierr = VecSet(&one,x);CHKERR(ierr);
+
+  for ( i=0; i<N-numtids; i++ ) {
+    ierr = VecAddValues(x,1,&i,&one); CHKERR(ierr);  
+  }
+  ierr = VecBeginAssembly(x); CHKERR(ierr);
+  ierr = VecEndAssembly(x); CHKERR(ierr);
+
+  VecView(x,0);
+
+  ierr = VecDestroy(x);CHKERR(ierr);
+
+  PetscFinalize();
+  return 0;
+}
+ 
