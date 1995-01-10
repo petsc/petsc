cf4797565dbc35c777a761b58a853c83b1b4dab2
Barry Smith <bsmith@mcs.anl.gov>
1995-01-10 17:16:17 -0600

    Initial revision

---
+++ src/vec/examples/tests/ex13.c
@@ -0,0 +1,65 @@
+
+
+/*
+    Scatters from  a sequential vector to a parallel vector.
+   Does case when we are merely selecting the local part of the 
+   parallel vector.
+*/
+#include "petsc.h"
+#include "comm.h"
+#include "is.h"
+#include "vec.h"
+#include "sys.h"
+#include "options.h"
+#include "sysio.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int           n = 5, ierr;
+  int           numtids,mytid,i;
+  Scalar        one = 1.0, two = 2.0, three = 3.0, value;
+  double        norm;
+  Vec           x,y;
+  IS            is1,is2;
+  VecScatterCtx ctx = 0;
+
+  PetscInitialize(&argc,&argv,(char*)0,(char*)0);
+
+  MPI_Comm_size(MPI_COMM_WORLD,&numtids);
+  MPI_Comm_rank(MPI_COMM_WORLD,&mytid);
+
+  /* create two vectors */
+  ierr = VecCreateMPI(MPI_COMM_WORLD,-1,numtids*n,&x); CHKERR(ierr);
+  ierr = VecCreateSequential(n,&y); CHKERR(ierr);
+
+  /* create two index sets */
+  ierr = ISCreateStrideSequential(n,n*mytid,1,&is1); CHKERR(ierr);
+  ierr = ISCreateStrideSequential(n,0,1,&is2); CHKERR(ierr);
+
+  /* each processor inserts the entire vector */
+  /* this is redundant but tests assembly */
+  for ( i=0; i<n; i++ ) {
+    value = (Scalar) (i + 10*mytid);
+    ierr = VecInsertValues(y,1,&i,&value); CHKERR(ierr);
+  }
+  ierr = VecBeginAssembly(y); CHKERR(ierr);
+  ierr = VecEndAssembly(y); CHKERR(ierr);
+
+  VecView(y,0); printf("----\n");
+
+  ierr = VecScatterBegin(y,is2,x,is1,&ctx); CHKERR(ierr);
+  ierr = VecScatterEnd(y,is2,x,is1,&ctx); CHKERR(ierr);
+  VecScatterCtxDestroy(ctx);
+  
+  VecView(x,0);
+
+  ierr = VecDestroy(x);CHKERR(ierr);
+  ierr = VecDestroy(y);CHKERR(ierr);
+  ierr = ISDestroy(is1);CHKERR(ierr);
+  ierr = ISDestroy(is2);CHKERR(ierr);
+
+  PetscFinalize(); 
+  return 0;
+}
+ 
