97bd0e124a061cd4a884c334822efe51ef791ff1
Barry Smith <bsmith@mcs.anl.gov>
1995-02-01 14:30:57 -0600

    Initial revision

---
+++ src/vec/examples/tutorials/ex3.c
@@ -0,0 +1,57 @@
+
+static char help[] = "Tests vector scatter gathers\n";
+
+#include "petsc.h"
+#include "comm.h"
+#include "is.h"
+#include "vec.h"
+#include "sys.h"
+#include "options.h"
+#include "sysio.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int           n = 5, ierr, idx1[2] = {0,3}, idx2[2] = {1,4};
+  Scalar        one = 1.0, two = 2.0, three = 3.0, dots[3],dot;
+  double        norm;
+  Vec           x,y,w,*z;
+  IS            is1,is2;
+  VecScatterCtx ctx = 0;
+
+  OptionsCreate(&argc,&argv,(char*)0,(char*)0);
+  if (OptionsHasName(0,0,"-help")) fprintf(stderr,"%s",help);
+  OptionsGetInt(0,0,"-n",&n);
+
+  /* create two vector */
+  ierr = VecCreateSequential(n,&x); CHKERR(ierr);
+  ierr = VecCreate(x,&y); CHKERR(ierr);
+
+  /* create two index sets */
+  ierr = ISCreateSequential(2,idx1,&is1); CHKERR(ierr);
+  ierr = ISCreateSequential(2,idx2,&is2); CHKERR(ierr);
+
+
+  ierr = VecSet(&one,x);CHKERR(ierr);
+  ierr = VecSet(&two,y);CHKERR(ierr);
+  ierr = VecScatterBegin(x,is1,y,is2,InsertValues,&ctx); CHKERR(ierr);
+  ierr = VecScatterEnd(x,is1,y,is2,InsertValues,&ctx); CHKERR(ierr);
+  
+  VecView(y,0);
+
+  ierr = VecScatterBegin(y,is1,x,is2,InsertValues,&ctx); CHKERR(ierr);
+  ierr = VecScatterEnd(y,is1,x,is2,InsertValues,&ctx); CHKERR(ierr);
+  ierr = VecScatterCtxDestroy(ctx); CHKERR(ierr);
+
+  printf("-------\n");VecView(x,0);
+
+  ierr = ISDestroy(is1); CHKERR(ierr);
+  ierr = ISDestroy(is2); CHKERR(ierr);
+
+  ierr = VecDestroy(x);CHKERR(ierr);
+  ierr = VecDestroy(y);CHKERR(ierr);
+
+  PetscFinalize();
+  return 0;
+}
+ 
