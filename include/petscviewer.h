a073374d4a4242346544f8dfa022530342cac393
Barry Smith <bsmith@mcs.anl.gov>
1995-02-24 22:32:14 -0600

    Initial revision

---
+++ include/petscviewer.h
@@ -0,0 +1,7 @@
+send.c:  Viewer viewer = (Viewer) obj; 
+send.c:     ViewerMatlabOpen - Opens a connection to a Matlab server.
+send.c:int ViewerMatlabOpen(char *machine,int port,Viewer *lab)
+send.c:  Viewer v;
+send.c:  CREATEHEADER(v,_Viewer);
+senddense.c:int ViewerMatlabPutArray(Viewer viewer,int m,int n,double *matrix)
+sendsparse.c:int ViewMatlabPutSparse(Viewer viewer,int m,int n,int nnz,double *v,int *c,
