2abeb5033ce2205e350eb1f8599f1c91b93727c5
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-03-04 10:58:40 -0600

    Added vector and matrix creation.

--- src/sles/pc/examples/tests/ex1.c
+++ src/sles/pc/examples/tests/ex1.c
@@ -2,6 +2,8 @@
 static char help[] = "Tests creation of PC context.\n";
 
 #include "pc.h"
+#include "vec.h"
+#include "mat.h"
 #include <stdio.h>
 #include "options.h"
 
@@ -9,15 +11,26 @@ static char help[] = "Tests creation of PC context.\n";
 int main(int argc,char **args)
 {
   PC  pc;
-  int ierr;
+  int ierr, n = 5;
+  Vec u;
+  Mat mat;
 
   PetscInitialize(&argc,&args,0,0);
   if (OptionsHasName(0,0,"-help")) fprintf(stderr,"%s",help);
-  ierr = PCCreate(&pc); CHKERR(ierr);
+  ierr = PCCreate(&pc);				CHKERR(ierr);
+  ierr = PCSetMethod(pc,PCNONE);		CHKERR(ierr);
 
-  ierr = PCSetMethod(pc,PCNONE); CHKERR(ierr);
-  ierr = PCSetUp(pc); CHKERR(ierr);
-  ierr = PCDestroy(pc); CHKERR(ierr);
+  /* Vector and matrix must be set before PCSetUp */
+  ierr = VecCreateSequential(n,&u);		CHKERR(ierr);
+  ierr = PCSetVector(pc,u);			CHKERR(ierr);
+  ierr = MatCreateSequentialAIJ(n,n,3,0,&mat);	CHKERR(ierr);
+  ierr = PCSetMat(pc,mat);			CHKERR(ierr);
+
+  ierr = PCSetUp(pc);				CHKERR(ierr);
+
+  ierr = VecDestroy(u);				CHKERR(ierr);
+  ierr = MatDestroy(mat);			CHKERR(ierr);
+  ierr = PCDestroy(pc);				CHKERR(ierr);
   PetscFinalize();
   return 0;
 }
