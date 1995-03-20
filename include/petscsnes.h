f03417d36c71e2997ebf2d81042931cd8fa697a7
Barry Smith <bsmith@mcs.anl.gov>
1995-03-19 18:23:58 -0600

    *** empty log message ***

--- include/petscsnes.h
+++ include/petscsnes.h
@@ -1,38 +1,54 @@
-/* $Id: nlsles.h,v 1.1 1995/03/20 00:11:33 bsmith Exp $ */
+/* $Id: snes.h,v 1.1 1995/03/20 00:12:47 bsmith Exp bsmith $ */
 
-#if !defined(__NL_SLES)
-#define __NL_SLES
+#if !defined(__SNES_PACKAGE)
+#define __SNES_PACKAGE
 
-/* Include file for use of SLES solvers within SNES routines */
+typedef enum { NLE_NLS1,
+               NLE_NTR1,
+               NLE_NTR2_DOG,
+               NLE_NTR2_LIN,
+               NLE_NBASIC,
+               NLM_NLS1,
+               NLM_NTR1 }
+  NLMETHOD;
 
-#include "nonlin/nlspmat.h"
+typedef enum { NLINITIAL_GUESS,
+               NLRESIDUAL,
+               NLFUNCTION,
+               NLGRADIENT,
+               NLMATRIX,
+               NLCORE,
+               NLSTEP_SETUP,
+               NLSTEP_COMPUTE,
+               NLSTEP_DESTROY,
+               NLTOTAL }
+   NLPHASE;
 
-/* ---- Routines to set information within the NLSles context ---- */
-extern void     NLSlesSetMethod		ANSI_ARGS((NLCtx*, SVMETHOD ));
-extern void     NLSlesSetKSPMethod	ANSI_ARGS((NLCtx*, ITMETHOD ));
-extern void     NLSlesSetDefaults	ANSI_ARGS((NLCtx*, SVMETHOD, 
-					ITMETHOD ));
+typedef enum { NLE, NLM } NLTYPE;
+
+extern void NLSetResidual               ANSI_ARGS((NLCtx*, void *));
+extern void NLSetResidualRoutine        ANSI_ARGS((NLCtx*, 
+                                        void (*)(NLCtx*, void*, void*), int ));
+extern void NLSetMaxResidualEvaluations ANSI_ARGS((NLCtx*, int ));
+extern void *NLGetResidual              ANSI_ARGS((NLCtx *));
+extern void NLiResid                    ANSI_ARGS((NLCtx*, void *, void *));
+
+extern void NLENewtonStatisticsMonitor  ANSI_ARGS((NLCtx*, void*, void*, 
+                                        double *));
+extern void NLENewtonDefaultMonitor     ANSI_ARGS((NLCtx*, void*, void*, 
+                                        double *));
+extern int  NLENewtonDefaultConverged   ANSI_ARGS((NLCtx*, double*, double*, 
+                                        double *));
+extern char *NLENewtonDefaultConvergedType ANSI_ARGS((NLCtx *));
+
+extern int SNESGetSLES(SNES,SLES*);
+
 extern void     NLSlesSetDampingFlag	ANSI_ARGS((NLCtx*, int));
 extern void     NLSlesSetSaveSolverCtx	ANSI_ARGS((NLCtx *));
 
 /* ---- Routines to extract information from the NLSles context ---- */
-extern SVMETHOD NLSlesGetMethod		ANSI_ARGS((NLCtx *));
-extern ITMETHOD NLSlesGetKSPMethod	ANSI_ARGS((NLCtx *));
-extern ITCntx   *NLSlesGetKSPCtx        ANSI_ARGS((NLCtx *));
 extern int      NLSlesGetDampingFlag	ANSI_ARGS((NLCtx *));
 extern int      NLSlesSaveSolverCtx	ANSI_ARGS((NLCtx *));
-
-/* --- Analogues of SLES routines --- */
-extern SVctx    *NLSlesSVCreate 	ANSI_ARGS((NLCtx*, SpMat*, SVMETHOD ));
-extern void     NLSlesSVSetUp 		ANSI_ARGS((NLCtx *));
-extern void     NLSlesSVSetOperators 	ANSI_ARGS((NLCtx*, SpMat*, SpMat*, 
-					int, int ));
-extern int      NLSlesSVSolve 		ANSI_ARGS((NLCtx*, void*, void *));
-extern void     NLSlesSVDestroy 	ANSI_ARGS((NLCtx *));
-extern void     NLSlesSVSetMonitor	ANSI_ARGS((NLCtx*, 
-					void (*)(ITCntx*, int, double), void*));
-extern void     NLSlesSVSetConvergenceTest ANSI_ARGS((NLCtx*, 
-					int (*)(ITCntx*, int, double), void*));
 
 /* --- Miscellaneous routines --- */
 extern void     NLSlesSetUp 		ANSI_ARGS((NLCtx*, void *));
@@ -47,17 +63,3 @@ extern int      NLSlesSolveScale	ANSI_ARGS((NLCtx*, void*, void*,
 					void*, double*, double*, double*, 
 					double*, double*, void *));
 
-/*  
-   NLSlesCheck - Checks that a SLES linear solver is set within the 
-   nonlinear solver context.  The usage is:
-             NLSlesCheck( nlP, name ) {code if context exists;}
- */
-
-#define NLSlesCheck( nlP, name ) \
- if (!(nlP)->sc.svctx || !(nlP)->sc.is_sles) {char buf[200];\
-   sprintf(buf,"Must set SLES linear solver context before calling %s\n",name);\
-   sprintf(buf,"Use NLSlesSVCreate or NLSetLinearSolverCtx.");\
-   SETERRC(1,buf);}\
- else
-
-#endif
