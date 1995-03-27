5b1163686d7a1b72e4b99ea99f36fd7624e278e1
Barry Smith <bsmith@mcs.anl.gov>
1995-03-27 15:17:21 -0600

    *** empty log message ***

--- include/petscpc.h
+++ include/petscpc.h
@@ -31,6 +31,10 @@ extern int    PCGetMethodFromOptions(PC pc,PCMETHOD *);
 extern int    PCPrintMethods(char*,char *);
 extern int    PCGetMethodFromContext(PC,PCMETHOD*);
 extern int    PCGetMethodName(PCMETHOD,char **);
+
+/* Flags for PCSetOperators */
+#define MAT_SAME_NONZERO_PATTERN 1
+
 extern int    PCSetOperators(PC,Mat,Mat,int);
 extern int    PCBJacobiSetUseTrueLocal(PC);
 extern int    PCSetVector(PC,Vec);
@@ -39,7 +43,7 @@ extern int    PCSetOptionsPrefix(PC,char*);
 
 extern int PCSORSetSymmetric(PC, int);
 extern int PCSORSetOmega(PC, double);
-extern int PCESORSetOmega(PC, double);
+extern int PCEisenstatSetOmega(PC, double);
 extern int PCSORSetIterations(PC, int);
 
 extern int PCBJacobiSetBlocks(PC, int);
