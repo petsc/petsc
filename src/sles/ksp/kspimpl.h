bdd82bf196e100a64f785d5f5840cac2f8199ed3
Barry Smith <bsmith@mcs.anl.gov>
1995-03-30 15:04:14 -0600

    *** empty log message ***

--- src/sles/ksp/kspimpl.h
+++ src/sles/ksp/kspimpl.h
@@ -5,8 +5,6 @@
 #include "ptscimpl.h"
 #include "ksp.h"
 
-#define KSP_COOKIE         0x202020
-
 /*
    Iterative method context.
 */
@@ -37,30 +35,23 @@ struct _KSP {
 				      */
 
   /* --------User (or default) routines (most return -1 on error) --------*/
-  int  (*usr_monitor)();            /* returns control to user after residual
-                                      calculation, allows user to, for 
+  int  (*usr_monitor)(KSP,int,double,void*); /* returns control to user after
+                                      residual calculation, allows user to, for 
                                       instance, print residual norm, etc. */
-  int (*converged)();
-  int (*BuildSolution)();        /* Returns a pointer to the solution, or
+  int (*converged)(KSP,int,double,void*);
+  int (*BuildSolution)(KSP,Vec,Vec*);  /* Returns a pointer to the solution, or
 				      calculates the solution in a 
 				      user-provided area. */
-  int (*BuildResidual)();        /* Returns a pointer to the residual, or
+  int (*BuildResidual)(KSP,Vec,Vec,Vec*); /* Returns a pointer to the residual, or
 				      calculates the residual in a 
 				      user-provided area.  */
-  int (*adjust_work_vectors)();    /* should pre-allocate the vectors*/
-  int  (*amult)(),                 /* application of matrix multiply A*/
-       (*binv)(),                  /* application of preconditioner B*/
-       (*matop)();                 /* application of AB or BA; if not set,
-			                         amult and binv will be used */
-  int  (*tamult)(),                /* transposes of the same         */
-       (*tbinv)(),                  
-       (*tmatop)();                
+  int (*adjust_work_vectors)(KSP,Vec*,int); /* should pre-allocate the vectors*/
+  PC  B;    /* fit this framework just fine */
 
   /*------------ Major routines which act on KSPCtx-----------------*/
-  KSPMETHOD  method;            /* type of solver */
-  int  (*solver)();            /* actual solver */
-  int  (*setup)();
-  int  (*adjustwork)();
+  int  (*solver)(KSP,int*);      /* actual solver */
+  int  (*setup)(KSP);
+  int  (*adjustwork)(KSP);
   void *MethodPrivate;          /* holder for misc stuff associated 
                                    with a particular iterative solver */
 
@@ -72,15 +63,11 @@ struct _KSP {
   int  nmatop, namult, nbinv, nvectors, nscalar;
 
   /* ------------Contexts for the user-defined functions-------------- */
-  void *amultP,     /* Amult operations */
-       *binvP,      /* Binv operations */
-       *monP,       /* User Monitor */
+  void *monP,       /* User Monitor */
        *cnvP;       /* Convergence tester */
   int setupcalled;
-  /* ------------Names of setable parameters--------------------------*/
-  char      *namemethod, *namemax_it, *nameright_pre, *nameuse_pres,
-            *namertol, *nameatol, *namedivtol, *namecalc_eigs,
-            *namecalc_res;
+  /* ------Prefix for Names of setable parameters----------------------*/
+  char  *prefix;
 };
 
 /*  
@@ -89,54 +76,36 @@ struct _KSP {
 
 #define CONVERGED(itP,rn,it) (cerr=(*itP->converged)(itP,it,rn,itP->cnvP))
 #define RCONV(itP,it)           ((cerr>0)?it:-it)
-#define PRE(itP,x,y)    {if (itP->binv) { (*itP->binv)(itP->binvP,x,y);} \
-                        else { VecCopy(x,y); }}
-#define TPRE(itP,x,y)   {if (itP->tbinv) { (*itP->tbinv)(itP->binvP,x,y);}\
-                        else {VecCopy(x,y); }}
-#define MATOP(itP,x,y,temp) {if (itP->matop) {\
-                            (*itP->matop)(itP->amultP,itP->binvP,x,y);}\
-                            else {\
-                              if (itP->right_pre) {  \
-				PRE(itP,x,temp);MM(itP,temp,y);} \
-			      else { \
-                                MM(itP,x,temp);PRE(itP,temp,y);}\
-                              }}
-#define TMATOP(itP,x,y,temp)  {  if (itP->tmatop) {\
-                              (*itP->tmatop)(itP->amultP,itP->binvP,x,y);}\
-                            } \
-                            else {                      \
-			      if (!itP->right_pre) {  \
-                                TPRE(itP,x,temp);TMM(itP,temp,y);} \
-			      else {            \
-			        TMM(itP,x,temp);TPRE(itP,temp,y);}\
-                              }}
+
 #define MONITOR(itP,rnorm,it) if (itP->usr_monitor) { \
                                 (*itP->usr_monitor)(itP,it,rnorm,itP->monP);\
                               }
 
 
-#define MM(itP,x,y)            (*itP->amult)(itP->amultP,x,y) 
-#define TMM(itP,x,y)           (*itP->tamult)(itP->amultP,x,y) 
-
 #if !defined(MAX)
 #define  MAX(a,b)           ((a) > (b) ? (a) : (b))
 #endif
 
-int KSPiRichardsonCreate();
-int KSPiChebychevCreate();
-int KSPiCGCreate();
-int KSPiGMRESCreate();
-int KSPiTCQMRCreate();
-int KSPiBCGSCreate();
-int KSPiCGSCreate();
-int KSPiTFQMRCreate();
-int KSPiLSQRCreate();
-int KSPiPREONLYCreate();
-int KSPiCRCreate();
+int KSPCreate_Richardson(KSP);
+int KSPCreate_Chebychev(KSP);
+int KSPCreate_CG(KSP);
+int KSPCreate_GMRES(KSP);
+int KSPCreate_TCQMR(KSP);
+int KSPCreate_BCGS(KSP);
+int KSPCreate_CGS(KSP);
+int KSPCreate_TFQMR(KSP);
+int KSPCreate_LSQR(KSP);
+int KSPCreate_PREONLY(KSP);
+int KSPCreate_CR(KSP);
 
-int   KSPiDefaultAdjustWork();
-int  KSPDefaultBuildSolution();
-int KSPDefaultBuildResidual();
-int     KSPiDefaultDestroy();
+int KSPiDefaultAdjustWork(KSP);
+int KSPDefaultBuildSolution(KSP,Vec,Vec*);
+int KSPDefaultBuildResidual(KSP,Vec,Vec,Vec *);
+int KSPiDefaultDestroy(PetscObject);
+int KSPCheckDef(KSP);
+int KSPiDefaultGetWork(KSP,int);
+int KSPiDefaultFreeWork(KSP);
+int KSPResidual(KSP,Vec,Vec,Vec,Vec,Vec,Vec);
+int KSPUnwindPre(KSP,Vec,Vec);
 
 #endif
