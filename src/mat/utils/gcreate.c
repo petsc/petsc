df998da42b8e01a9f6f165cb1f4d27b92d24edce
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-04-01 15:29:16 -0600

    Added Row format.

--- src/mat/utils/gcreate.c
+++ src/mat/utils/gcreate.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: gcreate.c,v 1.6 1995/03/06 04:00:47 bsmith Exp bsmith $";
+static char vcid[] = "$Id: gcreate.c,v 1.7 1995/03/21 23:19:28 bsmith Exp curfman $";
 #endif
 
 #include "sys.h"
@@ -9,14 +9,14 @@ static char vcid[] = "$Id: gcreate.c,v 1.6 1995/03/06 04:00:47 bsmith Exp bsmith
 
 /*@C
       MatCreateInitialMatrix - Reads from command line to determine 
-           what type of matrix to create. Also uses MPI matrices if 
-           number processors in MPI_COMM_WORLD greater then one.
+           what type of matrix to create.  Also uses MPI matrices if 
+           number processors in MPI_COMM_WORLD is greater then one.
 
   Input Parameters:
-.   m,n - matrix dimensions
+.   m,n - global matrix dimensions
  
   Output Parameter:
-.   V - location to stash resulting matrix.
+.   V - location to stash resulting matrix
 @*/
 int MatCreateInitialMatrix(int m,int n,Mat *V)
 {
@@ -25,7 +25,13 @@ int MatCreateInitialMatrix(int m,int n,Mat *V)
   if (OptionsHasName(0,0,"-dense_mat")) {
     return MatCreateSequentialDense(m,n,V);
   }
+  if (OptionsHasName(0,0,"-row_mat")) {
+    return MatCreateSequentialRow(m,n,10,0,V);
+  }
   if (numtid > 1 || OptionsHasName(0,0,"-mpi_objects")) {
+    if (OptionsHasName(0,0,"-row_mat")) {
+      return MatCreateMPIRow(MPI_COMM_WORLD,-1,-1,m,n,5,0,0,0,V);
+    }
     return MatCreateMPIAIJ(MPI_COMM_WORLD,-1,-1,m,n,5,0,0,0,V);
   }
   return MatCreateSequentialAIJ(m,n,10,0,V);
