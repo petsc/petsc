18ea4bf07f8bc27cc93f17edada14dafb4fbfb89
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-04-13 14:14:19 -0600

    added keywords.

--- src/sles/interface/sles.c
+++ src/sles/interface/sles.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: sles.c,v 1.14 1995/04/05 20:33:20 bsmith Exp curfman $";
+static char vcid[] = "$Id: sles.c,v 1.16 1995/04/13 19:55:21 curfman Exp $";
 #endif
 
 #include "slesimpl.h"
@@ -8,8 +8,11 @@ static char vcid[] = "$Id: sles.c,v 1.14 1995/04/05 20:33:20 bsmith Exp curfman 
 /*@
    SLESPrintHelp - Prints SLES options.
 
-  Input Parameter:
-.  sles - the solver context
+   Input Parameter:
+.  sles - the SLES context
+
+   Notes:
+   Keywords:  SLES, help
 @*/
 int SLESPrintHelp(SLES sles)
 {
@@ -21,13 +24,15 @@ int SLESPrintHelp(SLES sles)
 }
 
 /*@
-    SLESSetOptionsPrefix - Sets the prefix to use on all options setable from 
-                           SLES.
+   SLESSetOptionsPrefix - Sets the prefix used for searching for all 
+   SLES options in the database.
 
-  Input Parameter:
-.   sles - the linear equation solver context
-.   prefix - the prefix to prepend to all option names
+   Input Parameter:
+.  sles - the SLES context
+.  prefix - the prefix to prepend to all option names
 
+   Notes:
+   Keywords:  SLES, options, prefix, database
 @*/
 int SLESSetOptionsPrefix(SLES sles,char *prefix)
 {
@@ -38,12 +43,14 @@ int SLESSetOptionsPrefix(SLES sles,char *prefix)
 }
 
 /*@
-    SLESSetFromOptions - Sets various SLES parameters from user options.
+   SLESSetFromOptions - Sets various SLES parameters from user options.
+   Also takes all KSP and PC options.
 
-  Input Parameter:
-.   sles - the linear equation solver context
+   Input Parameter:
+.  sles - the SLES context
 
-  Also takes all KSP and PC options.
+   Notes:
+   Keywords:  SLES, options, database
 @*/
 int SLESSetFromOptions(SLES sles)
 {
@@ -53,10 +60,13 @@ int SLESSetFromOptions(SLES sles)
   return 0;
 }
 /*@
-    SLESCreate - Creates a linear equation solver context.
+   SLESCreate - Creates a linear equation solver context.
 
-  Output Parameter:
-.   sles - the create context
+   Output Parameter:
+.  sles - the newly created SLES context
+
+   Notes:
+   Keywords:  SLES, create, context
 @*/
 int SLESCreate(SLES *outsles)
 {
@@ -74,14 +84,14 @@ int SLESCreate(SLES *outsles)
   return 0;
 }
 
-
 /*@
    SLESDestroy - Destroys the SLES context.
 
-  Input Parameters:
-.   sles - the SLES context
+   Input Parameters:
+.  sles - the SLES context
 
-  Keywords: sles, destroy
+   Notes:
+   Keywords:  SLES, destroy, context
 @*/
 int SLESDestroy(SLES sles)
 {
@@ -97,13 +107,16 @@ extern int PCPreSolve(PC,KSP),PCPostSolve(PC,KSP);
 /*@
    SLESSolve - Solves a linear system.
 
-  Input Parameters:
-.   sles - the solver context
-.   b - the right hand side
+   Input Parameters:
+.  sles - the SLES context
+.  b - the right hand side
 
-  Output Parameters:
-.   x - the approximate solution
-.   its - the number of iterations used.
+   Output Parameters:
+.  x - the approximate solution
+.  its - the number of iterations used
+
+   Notes:
+   Keywords:  SLES, solve, linear system
 @*/
 int SLESSolve(SLES sles,Vec b,Vec x,int *its)
 {
@@ -128,13 +141,19 @@ int SLESSolve(SLES sles,Vec b,Vec x,int *its)
 }
 
 /*@
-    SLESGetKSP - Returns the Krylov Space context for a sles solver.
+   SLESGetKSP - Returns the KSP context for a SLES solver.
 
-  Input Parameter:
-.   sles - the solver context
+   Input Parameter:
+.  sles - the SLES context
 
-  Output Paramter:
-.   ksp - the Krylov space context
+   Output Parameter:
+.  ksp - the Krylov space context
+
+   Notes:  
+   The user can then directly manipulate the KSP context to set various 
+   options, etc.
+   
+   Keywords:  SLES, KSP, context
 @*/
 int SLESGetKSP(SLES sles,KSP *ksp)
 {
@@ -143,13 +162,19 @@ int SLESGetKSP(SLES sles,KSP *ksp)
   return 0;
 }
 /*@
-    SLESGetPC - Returns the preconditioner context for a sles solver.
+   SLESGetPC - Returns the preconditioner (PC) context for a SLES solver.
 
-  Input Parameter:
-.   sles - the solver context
+   Input Parameter:
+.  sles - the SLES context
 
-  Output Paramter:
+   Output Parameter:
 .  pc - the preconditioner context
+
+   Notes:  
+   The user can then directly manipulate the PC context to set various 
+   options, etc.
+
+   Keywords:  SLES, PC, context
 @*/
 int SLESGetPC(SLES sles,PC *pc)
 {
@@ -160,23 +185,24 @@ int SLESGetPC(SLES sles,PC *pc)
 
 #include "mat/matimpl.h"
 /*@
-    SLESSetOperators - Sets the matrix associated with the linear system
-       and a (possibly) different one associated with the preconditioner. 
+   SLESSetOperators - Sets the matrix associated with the linear system
+   and a (possibly) different one associated with the preconditioner. 
 
-  Input Parameters:
-.   sles - the sles context
-.   mat - the matrix to use
-.   pmat - matrix to be used in constructing the preconditioner, usually
-           the same as mat.  If pmat is 0, the old preconditioner is
-           reused.  
-.   flag - use 0 or MAT_SAME_NONZERO_PATTERN
+   Input Parameters:
+.  sles - the sles context
+.  mat - the matrix to use
+.  pmat - matrix to be used in constructing the preconditioner, usually
+          the same as mat.  If pmat is 0, the old preconditioner is reused.  
+.  flag - use 0 or MAT_SAME_NONZERO_PATTERN
 
-  Notes:
-  The flag can be used to eliminate unnecessary repeated work in the 
-  repeated solution of linear systems of the same size using the same 
-  preconditioner.  The user can set flag = MAT_SAME_NONZERO_PATTERN to 
-  indicate that the preconditioning matrix has the same nonzero pattern 
-  during successive linear solves.
+   Notes:
+   The flag can be used to eliminate unnecessary repeated work in the 
+   repeated solution of linear systems of the same size using the same 
+   preconditioner.  The user can set flag = MAT_SAME_NONZERO_PATTERN to 
+   indicate that the preconditioning matrix has the same nonzero pattern 
+   during successive linear solves.
+
+   Keywords:  SLES, operators, matrix, preconditioner
 @*/
 int SLESSetOperators(SLES sles,Mat mat,Mat pmat,int flag)
 {
