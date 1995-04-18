7ee4b05b674ce5da33405e6125322f85a345c774
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-04-17 22:21:18 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex5.c
+++ src/sles/examples/tutorials/ex5.c
@@ -1,8 +1,8 @@
 
-static char help[] = "Tests MPI parallel AIJ solve with SLES. \n\
-  This examples intentionally lays the matrix out across processors\n\
-  differently then the way it is assembled, this is to test parallel\n\
-  matrix assembly.\n";
+static char help[] = 
+"This example tests MPI parallel linear solves with SLES.  The code\n\
+illustrates repeated solution of linear systems with the same preconditioner\n\
+method but different matrices (having the same nonzero structure).\n";
 
 #include "vec.h"
 #include "mat.h"
@@ -12,13 +12,13 @@ static char help[] = "Tests MPI parallel AIJ solve with SLES. \n\
 
 int main(int argc,char **args)
 {
-  Mat         C; 
-  int         i,j, m = 3, n = 2, mytid,numtids,its;
-  Scalar      v, zero = 0.0, one = 1.0, none = -1.0;
-  int         I, J, ierr;
-  Vec         x,u,b;
-  SLES        sles;
-  double      norm;
+  Mat    C; 
+  Scalar v, *xa, *ua, zero = 0.0, one = 1.0, none = -1.0;
+  int    I, J, ldim, ierr, low, high, iglobal;
+  int    i, j, m = 3, n = 2, mytid, numtids, its;
+  Vec    x, u, b;
+  SLES   sles;
+  double norm;
 
   PetscInitialize(&argc,&args,0,0);
   if (OptionsHasName(0,0,"-help")) fprintf(stderr,"%s",help);
@@ -27,10 +27,19 @@ int main(int argc,char **args)
   MPI_Comm_size(MPI_COMM_WORLD,&numtids);
   n = 2*numtids;
 
-  /* create the matrix for the five point stencil, YET AGAIN*/
-  ierr = MatCreateInitialMatrix(m*n,m*n,&C); 
+  if (OptionsHasName(0,0,"-row_mat"))
+    ierr = MatCreateMPIRow(MPI_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,
+                           m*n,m*n,5,0,5,0,&C); 
+#if defined(HAVE_BLOCKSOLVE) && !defined(PETSC_COMPLEX)
+  else if (OptionsHasName(0,0,"-rowbs_mat"))
+    ierr = MatCreateMPIRowbs(MPI_COMM_WORLD,PETSC_DECIDE,m*n,5,0,0,&C); 
+#endif
+  else
+    ierr = MatCreateMPIAIJ(MPI_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,
+                           m*n,m*n,5,0,5,0,&C); 
   CHKERRA(ierr);
 
+  /* Generate matrix */
   for ( i=0; i<m; i++ ) { 
     for ( j=2*mytid; j<2*mytid+2; j++ ) {
       v = -1.0;  I = j + n*i;
@@ -38,30 +47,71 @@ int main(int argc,char **args)
       if ( i<m-1 ) {J = I + n; MatSetValues(C,1,&I,1,&J,&v,InsertValues);}
       if ( j>0 )   {J = I - 1; MatSetValues(C,1,&I,1,&J,&v,InsertValues);}
       if ( j<n-1 ) {J = I + 1; MatSetValues(C,1,&I,1,&J,&v,InsertValues);}
-      v = 4.0; MatSetValues(C,1,&I,1,&I,&v,InsertValues);
+      v = 6.0; MatSetValues(C,1,&I,1,&I,&v,InsertValues);
     }
   }
-  ierr = MatBeginAssembly(C); CHKERRA(ierr);
-  ierr = MatEndAssembly(C); CHKERRA(ierr);
+  /* Modify matrix slightly to make things more interesting */
+  I = n; J = n-2; v = 2.0;
+  MatSetValues(C,1,&I,1,&J,&v,InsertValues);
+  I = n-2; J = n; v = 2.0;
+  MatSetValues(C,1,&I,1,&J,&v,InsertValues);
+  I = 3; J = 4; v = 3.0;
+  MatSetValues(C,1,&I,1,&J,&v,InsertValues);
+  I = 4; J = 3; v = 3.0;
+  MatSetValues(C,1,&I,1,&J,&v,InsertValues);
+  ierr = MatBeginAssembly(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatEndAssembly(C,FINAL_ASSEMBLY); CHKERRA(ierr);
 
-  ierr = VecCreateInitialVector(m*n,&u); CHKERRA(ierr);
+  /* Generate vectors */
+  ierr = VecCreateMPI(MPI_COMM_WORLD,PETSC_DECIDE,m*n,&u); CHKERRA(ierr);
   ierr = VecCreate(u,&b); CHKERRA(ierr);
   ierr = VecCreate(b,&x); CHKERRA(ierr);
-  VecSet(&one,u); VecSet(&zero,x);
-
-  /* compute right hand side */
+  ierr = VecGetLocalSize(x,&ldim); CHKERR(ierr);
+  ierr = VecGetOwnershipRange(x,&low,&high); CHKERR(ierr);
+  for (i=0; i<ldim; i++) {
+    iglobal = i + low;
+    v = one*i + 100*mytid;
+    ierr = VecSetValues(u,1,&iglobal,&v,InsertValues); CHKERR(ierr);
+  }
+  ierr = VecBeginAssembly(u); CHKERR(ierr);
+  ierr = VecEndAssembly(u); CHKERR(ierr);
+  
+  /* Compute right-hand-side */
   ierr = MatMult(C,u,b); CHKERRA(ierr);
-
-  if ((ierr = SLESCreate(&sles))) SETERRA(ierr,0);
-  if ((ierr = SLESSetOperators(sles,C,C,0))) SETERRA(ierr,0);
+  
+  /* Solve linear system */
+  if ((ierr = SLESCreate(MPI_COMM_WORLD,&sles))) SETERRA(ierr,0);
+  if ((ierr = SLESSetOperators(sles,C,C,MAT_SAME_NONZERO_PATTERN)))
+    SETERRA(ierr,0);
   if ((ierr = SLESSetFromOptions(sles))) SETERRA(ierr,0);
   if ((ierr = SLESSolve(sles,b,x,&its))) SETERRA(ierr,0);
-
-  /* check error */
+ 
+  /* Check error */
   if ((ierr = VecAXPY(&none,u,x))) SETERRA(ierr,0);
   if ((ierr = VecNorm(x,&norm))) SETERRA(ierr,0);
   MPE_printf(MPI_COMM_WORLD,"Norm of error %g Number of iterations %d\n",norm,its);
 
+  /* Change matrix (keeping same nonzero structure) and solve again */
+  for ( i=0; i<m; i++ ) { 
+    for ( j=2*mytid; j<2*mytid+2; j++ ) {
+      I = j + n*i; v = 4.0; MatSetValues(C,1,&I,1,&I,&v,InsertValues);
+    }
+  } 
+  ierr = MatBeginAssembly(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatEndAssembly(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+
+  /* Compute another right-hand-side; then solve */
+  ierr = MatMult(C,u,b); CHKERRA(ierr);
+  if ((ierr = SLESSetOperators(sles,C,C,MAT_SAME_NONZERO_PATTERN)))
+    SETERRA(ierr,0);
+  if ((ierr = SLESSolve(sles,b,x,&its))) SETERRA(ierr,0);
+
+  /* Check error */
+  if ((ierr = VecAXPY(&none,u,x))) SETERRA(ierr,0);
+  if ((ierr = VecNorm(x,&norm))) SETERRA(ierr,0);
+  MPE_printf(MPI_COMM_WORLD,"Norm of error %g Number of iterations %d\n",norm,its);
+
+  /* Free work space */
   ierr = SLESDestroy(sles); CHKERRA(ierr);
   ierr = VecDestroy(u); CHKERRA(ierr);
   ierr = VecDestroy(x); CHKERRA(ierr);
@@ -70,3 +120,5 @@ int main(int argc,char **args)
   PetscFinalize();
   return 0;
 }
+
+
