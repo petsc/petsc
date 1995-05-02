f0bd0ba739920d02fa1442fbc87fd9d77077732e
Barry Smith <bsmith@mcs.anl.gov>
1995-05-02 16:48:29 -0600

    *** empty log message ***

--- src/vec/is/isimpl.h
+++ src/vec/is/isimpl.h
@@ -12,21 +12,24 @@ for now we define only the shell and what we absolutely need.
 #include "ptscimpl.h"
 #include "is.h"
 
-#define IS_COOKIE 0x13131313
-
 struct _ISOps {
-  int  (*size)(),(*localsize)(),(*position)();
-  int  (*indices)();
-  int  (*restoreindices)();
+  int  (*getsize)(IS,int*),(*getlocalsize)(IS,int*);
+  int  (*getindices)(IS,int**);
+  int  (*restoreindices)(IS,int**);
+  int  (*invertpermutation)(IS,IS*);
 };
 
 struct _IS {
   PETSCHEADER
-  struct _ISOps *ops;
+  struct        _ISOps *ops;
+  int           isperm; /* if is a permutation */
+  int           max,min; /* range of possible values */
   void          *data;
 };
 
 
-#define GENERALSEQUENTIAL 1
 
+#define ISGENERALSEQUENTIAL 0
+#define ISSTRIDESEQUENTIAL  2
+#define ISGENERALPARALLEL   1
 #endif
