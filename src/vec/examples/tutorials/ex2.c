9bc15e4d1ec0893eb730df276a9ad83437187d0f
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-05-09 09:34:34 -0600

    *** empty log message ***

--- src/vec/examples/tutorials/ex2.c
+++ src/vec/examples/tutorials/ex2.c
@@ -1,8 +1,8 @@
 
-
-static char help[] = "Builds a parallel vector with 1 component on the first\n\
-   processor 2 on the second,... Then each processor adds one to all elements\n\
-   except the last mytid.\n";
+static char help[] = 
+"This example builds a parallel vector with 1 component on the first\n\
+processor, 2 on the second, etc.  Then each processor adds one to all\n\
+elements except the last mytid.\n\n";
 
 #include "petsc.h"
 #include "is.h"
@@ -24,8 +24,8 @@ int main(int argc,char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD,&mytid); 
 
   ierr = VecCreateMPI(MPI_COMM_WORLD,mytid+1,PETSC_DECIDE,&x); CHKERRA(ierr);
-  ierr = VecGetSize(x,&N);
-  ierr = VecSet(&one,x);CHKERRA(ierr);
+  ierr = VecGetSize(x,&N); CHKERRA(ierr);
+  ierr = VecSet(&one,x); CHKERRA(ierr);
 
   for ( i=0; i<N-mytid; i++ ) {
     ierr = VecSetValues(x,1,&i,&one,ADDVALUES); CHKERRA(ierr);  
@@ -33,9 +33,9 @@ int main(int argc,char **argv)
   ierr = VecAssemblyBegin(x); CHKERRA(ierr);
   ierr = VecAssemblyEnd(x); CHKERRA(ierr);
 
-  VecView(x,SYNC_STDOUT_VIEWER);
+  ierr = VecView(x,SYNC_STDOUT_VIEWER); CHKERRA(ierr);
 
-  ierr = VecDestroy(x);CHKERRA(ierr);
+  ierr = VecDestroy(x); CHKERRA(ierr);
 
   PetscFinalize();
   return 0;
