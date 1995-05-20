ca7faf4f04204f10c1a57355ef3e14e9148a568d
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-05-20 09:24:37 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex1.c
+++ src/sles/examples/tutorials/ex1.c
@@ -1,33 +1,27 @@
-
-static char help[] = 
-"This example solves a tridiagonal linear system with SLES.  Input\n\
-arguments are:\n\
-  -n <size> : problem size\n\n";
+static char help[] = "This example solves a tridiagonal linear system with SLES.\n\n";
 
 #include "sles.h"
-#include <stdio.h>
 #include "petsc.h"
+#include <stdio.h>
 
 int main(int argc,char **args)
 {
-  int    ierr,i,n = 10, col[3], its;
-  Scalar none = -1.0, one = 1.0, value[3];
-  Vec    x,b,u;
-  Mat    A;
-  SLES   sles;
-  double norm;
+  int     ierr, i, n = 10, col[3], its;
+  Scalar  none = -1.0, one = 1.0, value[3];
+  Vec     x, b, u;
+  Mat     A;
+  SLES    sles;
+  double  norm;
 
   PetscInitialize(&argc,&args,0,0);
   if (OptionsHasName(0,"-help")) fprintf(stderr,"%s",help);
   OptionsGetInt(0,"-n",&n);
 
-  /* Create vectors */
   ierr = VecCreate(MPI_COMM_WORLD,n,&x); CHKERRA(ierr);
   ierr = VecDuplicate(x,&b); CHKERRA(ierr);
   ierr = VecDuplicate(x,&u); CHKERRA(ierr);
   ierr = VecSet(&one,u); CHKERRA(ierr);
 
-  /* Assemble matrix */
   ierr = MatCreate(MPI_COMM_WORLD,n,n,&A); CHKERRA(ierr);
   value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
   for (i=1; i<n-1; i++ ) {
@@ -40,22 +34,17 @@ int main(int argc,char **args)
   ierr = MatSetValues(A,1,&i,2,col,value,INSERTVALUES); CHKERRA(ierr);
   ierr = MatAssemblyBegin(A,FINAL_ASSEMBLY); CHKERRA(ierr);
   ierr = MatAssemblyEnd(A,FINAL_ASSEMBLY); CHKERRA(ierr);
-
-  /* Compute the right-hand-side */
   ierr = MatMult(A,u,b); CHKERRA(ierr);
 
-  /* Solve linear system, Ax = b */
   ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
   ierr = SLESSetOperators(sles,A,A,0); CHKERRA(ierr);
   ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
   ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
 
-  /* Check error */
-  ierr = VecAXPY(&none,u,x); CHKERRA(ierr);
+  ierr = VecAXPY(&none,u,x); CHKERRA(ierr);   /* check error */
   ierr  = VecNorm(x,&norm); CHKERRA(ierr);
   MPE_printf(MPI_COMM_WORLD,"Norm of error %g Iterations %d\n",norm,its);
  
-  /* Destroy data structures */
   ierr = VecDestroy(x); CHKERRA(ierr);
   ierr = VecDestroy(u); CHKERRA(ierr);
   ierr = VecDestroy(b); CHKERRA(ierr);
@@ -64,6 +53,3 @@ int main(int argc,char **args)
   PetscFinalize();
   return 0;
 }
-    
-
-
--- src/sles/examples/tutorials/ex2.c
+++ src/sles/examples/tutorials/ex2.c
@@ -1,34 +1,27 @@
-
 static char help[] = 
 "This example solves a linear system in parallel with SLES.  To test the\n\
-parallel matrix assembly, the matrix is intentionally laid out across the\n\
-processors differently from the way it is assembled.  Input arguments are:\n\
-  -m <size> : problem size\n\n";
+parallel matrix assembly, the matrix is intentionally distributed across the\n\
+processors differently from the way it is assembled.\n\n";
 
-#include  <stdio.h>
 #include "sles.h"
 #include "petsc.h"
+#include <stdio.h>
 
 int main(int argc,char **args)
 {
-  Mat         A; 
-  int         i,j, m = 3, n = 2, mytid,numtids,its;
-  Scalar      v, zero = 0.0, one = 1.0, none = -1.0;
-  int         I, J, ierr;
-  Vec         x,u,b;
-  SLES        sles;
-  double      norm;
+  int       i, j, I, J, ierr, m = 3, n = 2, mytid, numtids, its;
+  Scalar    v, zero = 0.0, one = 1.0, none = -1.0;
+  Vec       x, u, b;                       Mat       A; 
+  SLES      sles;                          double    norm;
 
   PetscInitialize(&argc,&args,0,0);
   if (OptionsHasName(0,"-help")) fprintf(stderr,"%s",help);
   OptionsGetInt(0,"-m",&m);
   MPI_Comm_rank(MPI_COMM_WORLD,&mytid);
-  MPI_Comm_size(MPI_COMM_WORLD,&numtids);
-  n = 2*numtids;
+  MPI_Comm_size(MPI_COMM_WORLD,&numtids);  n = 2*numtids;
 
-  /* Assemble the matrix for the five point stencil */
   ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&A); CHKERRA(ierr);
-  for ( i=0; i<m; i++ ) { 
+  for ( i=0; i<m; i++ ) {   /* assemble matrix for the five point stencil */
     for ( j=2*mytid; j<2*mytid+2; j++ ) {
       v = -1.0;  I = j + n*i;
       if ( i>0 )   {J = I - n; MatSetValues(A,1,&I,1,&J,&v,INSERTVALUES);}
@@ -41,33 +34,25 @@ int main(int argc,char **args)
   ierr = MatAssemblyBegin(A,FINAL_ASSEMBLY); CHKERRA(ierr);
   ierr = MatAssemblyEnd(A,FINAL_ASSEMBLY); CHKERRA(ierr);
 
-  /* Create the vectors */
   ierr = VecCreate(MPI_COMM_WORLD,m*n,&u); CHKERRA(ierr);
-  ierr = VecDuplicate(u,&b); CHKERRA(ierr);
+  ierr = VecDuplicate(u,&b); CHKERRA(ierr); 
   ierr = VecDuplicate(b,&x); CHKERRA(ierr);
-  ierr = VecSet(&one,u); CHKERRA(ierr);
+  ierr = VecSet(&one,u); CHKERRA(ierr);     
   ierr = VecSet(&zero,x); CHKERRA(ierr);
-
-  /* Compute the right-hand-side */
   ierr = MatMult(A,u,b); CHKERRA(ierr);
 
-  /* Solve the linear system, Ax = b */
   ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
   ierr = SLESSetOperators(sles,A,A,0); CHKERRA(ierr);
   ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
   ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
 
-  /* Check the error */
-  ierr = VecAXPY(&none,u,x); CHKERRA(ierr);
+  ierr = VecAXPY(&none,u,x); CHKERRA(ierr);   /* check the error */
   ierr = VecNorm(x,&norm); CHKERRA(ierr);
   MPE_printf(MPI_COMM_WORLD,"Norm of error %g Number of iterations %d\n",norm,its);
 
-  /* Destroy the data structures */
   ierr = SLESDestroy(sles); CHKERRA(ierr);
-  ierr = VecDestroy(u); CHKERRA(ierr);
-  ierr = VecDestroy(x); CHKERRA(ierr);
-  ierr = VecDestroy(b); CHKERRA(ierr);
-  ierr = MatDestroy(A); CHKERRA(ierr);
+  ierr = VecDestroy(u); CHKERRA(ierr);  ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = VecDestroy(b); CHKERRA(ierr);  ierr = MatDestroy(A); CHKERRA(ierr);
   PetscFinalize();
   return 0;
 }
