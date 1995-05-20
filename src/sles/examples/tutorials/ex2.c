7881fbf5584ad14d394138beda065361ae23f285
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-05-20 08:42:59 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex2.c
+++ src/sles/examples/tutorials/ex2.c
@@ -11,7 +11,7 @@ processors differently from the way it is assembled.  Input arguments are:\n\
 
 int main(int argc,char **args)
 {
-  Mat         C; 
+  Mat         A; 
   int         i,j, m = 3, n = 2, mytid,numtids,its;
   Scalar      v, zero = 0.0, one = 1.0, none = -1.0;
   int         I, J, ierr;
@@ -26,46 +26,48 @@ int main(int argc,char **args)
   MPI_Comm_size(MPI_COMM_WORLD,&numtids);
   n = 2*numtids;
 
-  /* create the matrix for the five point stencil, YET AGAIN*/
-  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&C); CHKERRA(ierr);
-
+  /* Assemble the matrix for the five point stencil */
+  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&A); CHKERRA(ierr);
   for ( i=0; i<m; i++ ) { 
     for ( j=2*mytid; j<2*mytid+2; j++ ) {
       v = -1.0;  I = j + n*i;
-      if ( i>0 )   {J = I - n; MatSetValues(C,1,&I,1,&J,&v,INSERTVALUES);}
-      if ( i<m-1 ) {J = I + n; MatSetValues(C,1,&I,1,&J,&v,INSERTVALUES);}
-      if ( j>0 )   {J = I - 1; MatSetValues(C,1,&I,1,&J,&v,INSERTVALUES);}
-      if ( j<n-1 ) {J = I + 1; MatSetValues(C,1,&I,1,&J,&v,INSERTVALUES);}
-      v = 4.0; ierr = MatSetValues(C,1,&I,1,&I,&v,INSERTVALUES); CHKERRA(ierr);
+      if ( i>0 )   {J = I - n; MatSetValues(A,1,&I,1,&J,&v,INSERTVALUES);}
+      if ( i<m-1 ) {J = I + n; MatSetValues(A,1,&I,1,&J,&v,INSERTVALUES);}
+      if ( j>0 )   {J = I - 1; MatSetValues(A,1,&I,1,&J,&v,INSERTVALUES);}
+      if ( j<n-1 ) {J = I + 1; MatSetValues(A,1,&I,1,&J,&v,INSERTVALUES);}
+      v = 4.0; ierr = MatSetValues(A,1,&I,1,&I,&v,INSERTVALUES); CHKERRA(ierr);
     }
   }
-  ierr = MatAssemblyBegin(C,FINAL_ASSEMBLY); CHKERRA(ierr);
-  ierr = MatAssemblyEnd(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatAssemblyBegin(A,FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatAssemblyEnd(A,FINAL_ASSEMBLY); CHKERRA(ierr);
 
+  /* Create the vectors */
   ierr = VecCreate(MPI_COMM_WORLD,m*n,&u); CHKERRA(ierr);
   ierr = VecDuplicate(u,&b); CHKERRA(ierr);
   ierr = VecDuplicate(b,&x); CHKERRA(ierr);
   ierr = VecSet(&one,u); CHKERRA(ierr);
   ierr = VecSet(&zero,x); CHKERRA(ierr);
 
-  /* compute right hand side */
-  ierr = MatMult(C,u,b); CHKERRA(ierr);
+  /* Compute the right-hand-side */
+  ierr = MatMult(A,u,b); CHKERRA(ierr);
 
+  /* Solve the linear system, Ax = b */
   ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
-  ierr = SLESSetOperators(sles,C,C,0); CHKERRA(ierr);
+  ierr = SLESSetOperators(sles,A,A,0); CHKERRA(ierr);
   ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
   ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
 
-  /* check error */
+  /* Check the error */
   ierr = VecAXPY(&none,u,x); CHKERRA(ierr);
   ierr = VecNorm(x,&norm); CHKERRA(ierr);
   MPE_printf(MPI_COMM_WORLD,"Norm of error %g Number of iterations %d\n",norm,its);
 
+  /* Destroy the data structures */
   ierr = SLESDestroy(sles); CHKERRA(ierr);
   ierr = VecDestroy(u); CHKERRA(ierr);
   ierr = VecDestroy(x); CHKERRA(ierr);
   ierr = VecDestroy(b); CHKERRA(ierr);
-  ierr = MatDestroy(C); CHKERRA(ierr);
+  ierr = MatDestroy(A); CHKERRA(ierr);
   PetscFinalize();
   return 0;
 }
