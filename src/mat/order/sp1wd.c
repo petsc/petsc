3099e6c308b6c08ae1fbb5887c9d6c9425ad3bdc
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-06-08 07:37:43 -0600

    *** empty log message ***

--- src/mat/order/sp1wd.c
+++ src/mat/order/sp1wd.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: sp1wd.c,v 1.6 1995/04/27 20:15:53 bsmith Exp bsmith $";
+static char vcid[] = "$Id: sp1wd.c,v 1.7 1995/05/29 20:29:21 bsmith Exp curfman $";
 #endif
 
 #include "petsc.h"
@@ -12,12 +12,12 @@ int SpOrder1WD(int nrow, int *ia, int *ja, int * perm )
 {
 int i,   *mask, *xls, nblks, *xblk, *ls;
 
-mask = (int *)MALLOC( nrow * sizeof(int) );     CHKPTR(mask);
-xls  = (int *)MALLOC( (nrow+1) * sizeof(int) ); CHKPTR(xls);
-ls   = (int *)MALLOC( nrow * sizeof(int) );     CHKPTR(ls);
-xblk = (int *)MALLOC( nrow * sizeof(int) );     CHKPTR(xblk);
+mask = (int *)PETSCMALLOC( nrow * sizeof(int) );     CHKPTRQ(mask);
+xls  = (int *)PETSCMALLOC( (nrow+1) * sizeof(int) ); CHKPTRQ(xls);
+ls   = (int *)PETSCMALLOC( nrow * sizeof(int) );     CHKPTRQ(ls);
+xblk = (int *)PETSCMALLOC( nrow * sizeof(int) );     CHKPTRQ(xblk);
 gen1wd( &nrow, ia, ja, mask, &nblks, xblk, perm, xls, ls );
-FREE( mask ); FREE( xls ); FREE( ls ); FREE( xblk );
+PETSCFREE( mask ); PETSCFREE( xls ); PETSCFREE( ls ); PETSCFREE( xblk );
 for (i=0; i<nrow; i++) perm[i]--;
 return 0;
 }
