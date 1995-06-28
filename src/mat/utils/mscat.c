23f4394f9aac2c657fbc389c941e7b678197e001
Barry Smith <bsmith@mcs.anl.gov>
1995-06-28 12:59:12 -0600

    Initial revision

---
+++ src/mat/utils/mscat.c
@@ -0,0 +1,8 @@
+
+
+/*
+     Code to scatter between matrices, at the moment this is very limited
+  one can only scatter from MPIAIJ matrices to AIJ matrices
+*/
+
+#include "matimpl.h"    /*I "mat.h"  I*/
