0fd6775b7978c310bd373a3b14783aa84e934449
Barry Smith <bsmith@mcs.anl.gov>
1995-07-03 12:09:05 -0600

    Initial revision

---
+++ src/dm/da/examples/tests/ex2.c
@@ -0,0 +1,74 @@
+
+/* This file was created by Peter Mell  6/30/95 */
+ 
+static char help[] = "This example tests various DA1 routines.\n\n";
+
+#include "petsc.h"
+#include "da.h"
+#include "sys.h"
+#include "draw.h"
+#include <math.h>
+#include <sysio.h>
+
+
+int main(int argc,char **argv)
+{
+  int      mytid, M = 13, ierr, w=1, s=1, wrap=1;
+  DA       da;
+  DrawCtx  win1,win2;
+  Vec      local,global;
+  Scalar   value;
+
+  PetscInitialize(&argc,&argv,(char*)0,(char*)0);
+  if (OptionsHasName(0,"-help")) fprintf(stderr,"%s",help);
+  ierr = DrawOpenX(MPI_COMM_WORLD,0,"",280,480,600,200,&win1); CHKERRA(ierr);
+  ierr = DrawOpenX(MPI_COMM_WORLD,0,"",280,258,600,200,&win2); CHKERRA(ierr);
+  ierr = DrawSetDoubleBuffer(win1); CHKERRA(ierr);
+
+  OptionsGetInt(0,"-M",&M);
+  OptionsGetInt(0,"-w",&w);  /* degrees of freedom */ 
+  OptionsGetInt(0,"-s",&s);  /* stencil width */
+  OptionsGetInt(0,"-wrap",&wrap);  /* wrap or not */
+
+  ierr = DACreate1d(MPI_COMM_WORLD,M,w,s,wrap,&da); CHKERRA(ierr);
+  ierr = DAGetDistributedVector(da,&global); CHKERRA(ierr);
+  ierr = DAGetLocalVector(da,&local); CHKERRA(ierr);
+
+  MPI_Comm_rank(MPI_COMM_WORLD,&mytid);
+  value = 1;
+  ierr = VecSet(&value,global); CHKERRA(ierr);
+
+  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); CHKERRA(ierr);
+  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); CHKERRA(ierr);
+
+  value = mytid+1;
+  ierr = VecScale(&value,local); CHKERRA(ierr);
+  ierr = DALocalToGlobal(da,local,INSERTVALUES,global); CHKERRA(ierr);
+
+  ierr = VecView(global,(Viewer) win1); CHKERRA(ierr); 
+
+  if (mytid == 0) printf ("\nGlobal Vectors:\n");
+  ierr = VecView(global,SYNC_STDOUT_VIEWER); CHKERRA(ierr); 
+  if (mytid == 0) printf ("\n\n");
+
+  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); CHKERRA(ierr);
+  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); CHKERRA(ierr);
+
+  printf ("\nView Local Array - Processor [%d]\n",mytid);
+  ierr = VecView(local,SYNC_STDOUT_VIEWER); CHKERRA(ierr); 
+
+  DAView1d(da,(Viewer) win2);
+  DADestroy(da);
+  PetscFinalize();
+  return 0;
+}
+ 
+
+
+
+
+
+
+
+
+
---
+++ src/dm/da/examples/tests/ex3.c
@@ -0,0 +1,113 @@
+
+/* This file created by Peter Mell   6/30/95 */ 
+
+static char help[] = "This example creates a 1d wave.\n\n";
+
+#include "petsc.h"
+#include "da.h"
+#include "sys.h"
+#include "draw.h"
+#include <math.h>
+#include <sysio.h>
+
+#define NO_WRAP 0
+#define WRAP 1
+#define PI 3.14159265
+
+
+int main(int argc,char **argv)
+{
+  int       mytid, numtid, M = 14, ierr;
+  int       w=1, s=1, wrap=1;
+  DA        da;
+  DrawCtx   win;
+  Vec       local,global,copy;
+  Scalar    value, *localptr, *copyptr;
+  int       time_steps, mysize;
+  double    a,h,k;
+  int       localsize, j, i, mybase,myend;
+  DrawLGCtx ctx;
+  double    tempx, tempy;
+ 
+  PetscInitialize(&argc,&argv,(char*)0,(char*)0);
+  if (OptionsHasName(0,"-help")) fprintf(stderr,"%s",help);
+
+  OptionsGetInt(0,"-M",&M);
+  OptionsGetInt(0,"-wrap",&wrap);
+    
+  /* Set up the array */ 
+  ierr = DACreate1d(MPI_COMM_WORLD,M,w,s,wrap,&da); CHKERRA(ierr);
+  ierr = DAGetDistributedVector(da,&global); CHKERRQ(ierr);
+  ierr = DAGetLocalVector(da,&local); CHKERRQ(ierr);
+  MPI_Comm_rank(MPI_COMM_WORLD,&mytid);
+  MPI_Comm_size(MPI_COMM_WORLD,&numtid); 
+
+  /* Set Up Display to Show Wave Graph */
+  ierr = DrawOpenX(MPI_COMM_WORLD,0,"",80,480,500,160,&win); CHKERRA(ierr);
+  ierr = DrawSetDoubleBuffer(win); CHKERRQ(ierr);
+
+  /* determine starting point of each processor */
+  VecGetOwnershipRange(global,&mybase,&myend);
+
+  /* Initialize the Array */
+  VecGetLocalSize (local,&localsize);
+  VecGetArray (local,&localptr); 
+  localptr[0] = 0.0;
+  localptr[localsize-1] = 0.0;
+  for (i=1; i<localsize-1; i++) {
+    j=(i-1)+mybase; 
+    localptr[i] = sin( (PI*j*6)/((double)M) 
+                        + 1.2 * sin( (PI*j*2)/((double)M) ) ) * 2;
+  }
+
+  VecRestoreArray(local,&localptr);
+  ierr = DALocalToGlobal(da,local,INSERTVALUES,global); CHKERRQ(ierr);
+  printf ("\nInitial Local Array [%d]\n",mytid);
+  for (i=0; i< localsize; i++) { printf (" %f",localptr[i]); }
+  printf ("\n\n");
+
+  /* Make copy of local array for doing updates */
+  ierr = VecDuplicate(local,&copy); CHKERRA(ierr);
+  ierr = VecGetArray (copy,&copyptr); CHKERRA(ierr);
+
+  /* Assign Parameters */
+  a= 1.0;
+  h= 1.0/M;
+  k= h;
+  time_steps = 100000;
+
+  for (j=0; j<time_steps; j++) {  
+
+    /* Global to Local */
+    ierr = DAGlobalToLocalBegin (da,global,INSERTVALUES,local); CHKERRQ(ierr);
+    ierr = DAGlobalToLocalEnd   (da,global,INSERTVALUES,local); CHKERRQ(ierr);
+
+    /*Extract local array */ 
+    VecGetArray (local,&localptr); 
+
+    /* Update Locally - Make array of new values */
+    /* Note: I don't do anything for the first and last entry */
+    for (i=1; i< localsize-1; i++) {
+      copyptr[i] = .5*(localptr[i+1]+localptr[i-1]) - 
+                    (k / (2.0*a*h)) * (localptr[i+1] - localptr[i-1]);
+    }
+  
+    VecRestoreArray(copy,&copyptr);
+
+    /* Local to Global */
+    ierr = DALocalToGlobal(da,copy,INSERTVALUES,global); CHKERRQ(ierr);
+  
+    /* View Wave */ 
+    VecView (global,(Viewer) win); 
+
+  }
+
+  DADestroy(da);
+  PetscFinalize();
+  return 0;
+}
+ 
+
+
+
+
