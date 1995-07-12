a1c144aa831bc632e4e1939127abfcff8154476b
Barry Smith <bsmith@mcs.anl.gov>
1995-07-12 11:37:16 -0600

    *** empty log message ***

--- src/sys/examples/tutorials/ex3.c
+++ src/sys/examples/tutorials/ex3.c
@@ -1,27 +1,24 @@
 
 static char help[] = 
-"This example tests catching of floating point exceptions.\n\n";
+"Demonstrates how users may insert their own event logging.\n\n";
 
 #include "petsc.h"
+#include "pinclude/plog.h"
 #include <stdio.h>
 
-int CreateError(double x)
-{
-  x = 1.0/x;
-  printf("x = %g\n",x);
-  return 0;
-}
+#define USER_EVENT 75
 
 int main(int argc,char **argv)
 {
   int ierr;
   PetscInitialize(&argc,&argv,0,0);
   if (OptionsHasName(0,"-help")) fprintf(stderr,help);
-  fprintf(stderr,"This is a contrived example to test floating pointing\n");
-  fprintf(stderr,"It is not a true error.\n");
-  fprintf(stderr,"Run with -fp_trap to catch the floating point error\n");
-  fflush(stderr);
-  ierr = CreateError(0.0); CHKERRA(ierr);
+
+  PLogEventRegister(USER_EVENT,"User event");
+  PLogEventBegin(USER_EVENT,0,0,0,0);
+  sleep(1);
+  PLogEventEnd(USER_EVENT,0,0,0,0);
+  PetscFinalize();
   return 0;
 }
  
