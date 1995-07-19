7bc6369ac05300d54eb945467331a27d4d6a33dd
Barry Smith <bsmith@mcs.anl.gov>
1995-07-19 15:08:31 -0600

    *** empty log message ***

--- src/dm/da/examples/tests/ex4.c
+++ src/dm/da/examples/tests/ex4.c
@@ -10,16 +10,18 @@ static char help[] = "This example tests various 2d DA routines.\n\n";
 
 int main(int argc,char **argv)
 {
-  int      mytid,M = 10, N = 8, m = PETSC_DECIDE, n = PETSC_DECIDE, ierr;
-  int      s=2, w=7, xwrap=1, ywrap=1;
-  DA       da;
-  DrawCtx  win;
-  Vec      local,global;
-  Scalar   value;
+  int            mytid,M = 10, N = 8, m = PETSC_DECIDE,ierr;
+  int            s=2, w=2,n = PETSC_DECIDE ;
+  DAPeriodicType wrap = DA_NONPERIODIC;
+  DA             da;
+  DrawCtx        win;
+  Vec            local,global;
+  Scalar         value;
  
   PetscInitialize(&argc,&argv,(char*)0,(char*)0);
   if (OptionsHasName(0,"-help")) fprintf(stderr,"%s",help);
-  ierr = DrawOpenX(MPI_COMM_WORLD,0,"",300,0,600,600,&win); CHKERRA(ierr);
+  ierr = DrawOpenX(MPI_COMM_WORLD,0,"",300,0,400,400,&win);
+  CHKERRA(ierr);
  
   OptionsGetInt(0,"-M",&M);
   OptionsGetInt(0,"-N",&N);
@@ -27,10 +29,12 @@ int main(int argc,char **argv)
   OptionsGetInt(0,"-n",&n);
   OptionsGetInt(0,"-s",&s);
   OptionsGetInt(0,"-w",&w);
-  OptionsGetInt(0,"-xwrap",&xwrap);
-  OptionsGetInt(0,"-ywrap",&ywrap);
+  if (OptionsHasName(0,"-xwrap")) wrap = DA_XPERIODIC;
+  if (OptionsHasName(0,"-ywrap")) wrap = DA_YPERIODIC;
+  if (OptionsHasName(0,"-xywrap")) wrap = DA_XYPERIODIC;
 
-  ierr = DACreate2dn(MPI_COMM_WORLD,M,N,m,n,w,s,xwrap,ywrap,&da); CHKERRA(ierr);
+  ierr = DACreate2d(MPI_COMM_WORLD,wrap,DA_STENCIL_BOX,M,N,m,n,w,s,&da); 
+  CHKERRA(ierr);
   ierr = DAGetDistributedVector(da,&global); CHKERRA(ierr);
   ierr = DAGetLocalVector(da,&local); CHKERRA(ierr);
 
@@ -38,19 +42,23 @@ int main(int argc,char **argv)
   value = 1;
   ierr = VecSet(&value,global); CHKERRA(ierr);
 
-  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); CHKERRA(ierr);
-  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); CHKERRA(ierr);
+  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); 
+  CHKERRA(ierr);
+  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); 
+  CHKERRA(ierr);
 
   value = mytid;
   ierr = VecScale(&value,local); CHKERRA(ierr);
   ierr = DALocalToGlobal(da,local,INSERTVALUES,global); CHKERRA(ierr);
 
-  if (mytid == 0) printf ("\nGlobal Vectors:\n");
+  MPIU_printf (MPI_COMM_WORLD,"\nGlobal Vectors:\n");
   ierr = VecView(global,SYNC_STDOUT_VIEWER); CHKERRA(ierr); 
-  if (mytid == 0) printf ("\n\n");
+  MPIU_printf (MPI_COMM_WORLD,"\n\n");
 
-  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); CHKERRA(ierr);
-  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); CHKERRA(ierr);
+  ierr = DAGlobalToLocalBegin(da,global,INSERTVALUES,local); 
+  CHKERRA(ierr);
+  ierr = DAGlobalToLocalEnd(da,global,INSERTVALUES,local); 
+  CHKERRA(ierr);
 
   printf ("\nView Local Array - Processor [%d]\n",mytid);
   ierr = VecView(local,SYNC_STDOUT_VIEWER); CHKERRA(ierr); 
