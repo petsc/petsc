32f414fbe43e26f9547f28edf99d4658fb9721bd
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-07-24 21:17:14 -0600

    *** empty log message ***

--- src/sles/pc/impls/bjacobi/bjacobi.h
+++ src/sles/pc/impls/bjacobi/bjacobi.h
@@ -1,11 +1,14 @@
+if !defined(__BJACOBI)
+#define __BJACOBI
 
 #include "sles.h"
 /* Base include file for block Jacobi */
 
 typedef struct {
-  int  n,n_local;                   /* number of blocks */
-  int  usetruelocal;                /* use true local matrix, not precond */
-  SLES *sles;
-  void *data;
+  int  n, n_local;           /* number of blocks (global, local) */
+  int  usetruelocal;         /* use true local matrix, not precond matrix */
+  SLES *sles;                /* SLES contexts for blocks */
+  void *data;                /* implementation-specific data */
 } PC_BJacobi;
 
+#endif
