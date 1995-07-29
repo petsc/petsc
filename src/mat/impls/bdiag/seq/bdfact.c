8b44e9ca7835f6dc1d53cd3de52a1f9e0db0814a
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-07-28 20:41:51 -0600

    *** empty log message ***

--- src/mat/impls/bdiag/seq/bdfact.c
+++ src/mat/impls/bdiag/seq/bdfact.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: bdiag.c,v 1.32 1995/07/28 04:22:17 bsmith Exp $";
+static char vcid[] = "$Id: bdfact.c,v 1.1 1995/07/28 21:21:21 curfman Exp curfman $";
 #endif
 
 /* Block diagonal matrix format */
@@ -13,16 +13,35 @@ static char vcid[] = "$Id: bdiag.c,v 1.32 1995/07/28 04:22:17 bsmith Exp $";
 int MatLUFactor_BDiag(Mat matin,IS row,IS col,double f)
 {
   Mat_BDiag *mat = (Mat_BDiag *) matin->data;
-  int    info, i, nb = mat->nb;
+  int    info, i, nb = mat->nb, j, k, *piv;
+  Scalar *submat, *smi;
 
+  MatView(matin,STDOUT_VIEWER);
   if (mat->nd != 1 || mat->diag[0] !=0) SETERRQ(1,
     "MatLUFactor_BDiag: Currently supports factoriz only for main diagonal");
   if (!mat->pivots) {
     mat->pivots = (int *) PETSCMALLOC( mat->m*sizeof(int) );
     CHKPTRQ(mat->pivots);
   }
+  submat = mat->diagv[0];
   for (i=0; i<mat->bdlen[0]; i++) {
-    LAgetrf_(&nb,&nb,mat->diagv[0]+i*nb*nb,&nb,mat->pivots+i*nb,&info);
+    smi = &submat[i*nb*nb];
+    piv = &mat->pivots[i*nb];
+    for (j=0; j<nb; j++) {
+      printf("block number=%d, i=%d Before factoriz\n",i,j);
+      for (k=0; k<nb; k++) {
+         printf("  i=%d, j=%d, Aij=%g\n",j,k,smi[j*nb+k]);
+      }
+    }
+    LAgetrf_(&nb,&nb,smi,&nb,piv,&info);
+/*    LAgetrf_(&nb,&nb,&submat[i*nb*nb],&nb,&mat->pivots[i*nb],&info); */
+    for (j=0; j<nb; j++) {
+      printf("block number=%d, i=%d, pivot=%d\n",i,j,piv[j]);
+      for (k=0; k<nb; k++) {
+         printf("   i=%d, j=%d, Aij=%g\n",j,k,smi[j*nb+k]);
+      }
+    }
+    printf("bd = %d, LAPACK info = %d\n",i,info);
     if (info) SETERRQ(1,"MatLUFactor_BDiag:Bad LU factorization");
   }
   matin->factor = FACTOR_LU;
@@ -56,6 +75,7 @@ int MatSolve_BDiag(Mat matin,Vec xx,Vec yy)
     for (i=0; i<mat->bdlen[0]; i++) {
       LAgetrs_( "N", &nb, &one, mat->diagv[0]+i*nb*nb, &nb, mat->pivots+i*nb,
               y+i*nb, &nb, &info );
+      printf("bd = %d,LAPACK info = %d\n",i,info);
     }
   }
   else SETERRQ(1,"MatSolve_BDiag:Matrix must be factored to solve");
