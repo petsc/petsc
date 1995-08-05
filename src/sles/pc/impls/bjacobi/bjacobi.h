b1d8164bc9e948cc07744d52a85746f042502b5a
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-08-05 15:21:18 -0600

    *** empty log message ***

--- src/sles/pc/impls/bjacobi/bjacobi.h
+++ src/sles/pc/impls/bjacobi/bjacobi.h
@@ -7,9 +7,11 @@
 typedef struct {
   int  n, n_local;           /* number of blocks (global, local) */
   int  first_local;          /* number of first block on processor */
-  int  usetruelocal;         /* use true local matrix, not precond matrix */
+  int  use_true_local;       /* use true local matrix, not precond matrix */
   SLES *sles;                /* SLES contexts for blocks */
   void *data;                /* implementation-specific data */
+  int  same_local_solves;    /* flag indicating whether all local solvers
+                                are the same */
 } PC_BJacobi;
 
 #endif
