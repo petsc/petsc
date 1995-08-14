ac4267cd10358930b1cb8bf355fcd95fa8455deb
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-08-14 12:43:49 -0600

    *** empty log message ***

--- src/sles/ksp/impls/bcgs/bcgs.c
+++ src/sles/ksp/impls/bcgs/bcgs.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: bcgs.c,v 1.18 1995/08/11 13:45:17 curfman Exp bsmith $";
+static char vcid[] = "$Id: bcgs.c,v 1.19 1995/08/14 17:26:15 bsmith Exp curfman $";
 #endif
 
 /*                       
@@ -19,73 +19,74 @@ static int KSPSetUp_BCGS(KSP itP)
 
 static int  KSPSolve_BCGS(KSP itP,int *its)
 {
-int       i = 0, maxit, hist_len, cerr = 0,ierr;
-Scalar    rho, rhoold, alpha, beta, omega, omegaold, d1, d2;
-Scalar    zero = 0.0, tmp;
-Vec       X,B,V,P,R,RP,T,S, BINVF;
-double    dp, *history;
+  int       i = 0, maxit, hist_len, cerr = 0,ierr;
+  Scalar    rho, rhoold, alpha, beta, omega, omegaold, d1, d2;
+  Scalar    zero = 0.0, tmp;
+  Vec       X,B,V,P,R,RP,T,S, BINVF;
+  double    dp, *history;
 
-maxit   = itP->max_it;
-history = itP->residual_history;
-hist_len= itP->res_hist_size;
-X       = itP->vec_sol;
-B       = itP->vec_rhs;
-R       = itP->work[0];
-RP      = itP->work[1];
-V       = itP->work[2];
-T       = itP->work[3];
-S       = itP->work[4];
-P       = itP->work[5];
-BINVF   = itP->work[6];
+  maxit   = itP->max_it;
+  history = itP->residual_history;
+  hist_len= itP->res_hist_size;
+  X       = itP->vec_sol;
+  B       = itP->vec_rhs;
+  R       = itP->work[0];
+  RP      = itP->work[1];
+  V       = itP->work[2];
+  T       = itP->work[3];
+  S       = itP->work[4];
+  P       = itP->work[5];
+  BINVF   = itP->work[6];
 
-/* Compute initial preconditioned residual */
-KSPResidual(itP,X,V,T, R, BINVF, B );
+  /* Compute initial preconditioned residual */
+  ierr = KSPResidual(itP,X,V,T,R,BINVF,B); CHKERRQ(ierr);
 
-/* Test for nothing to do */
-VecNorm(R,&dp);
-if ((*itP->converged)(itP,0,dp,itP->cnvP)) {*its = 0; return 0;}
-MONITOR(itP,dp,0);
-if (history) history[0] = dp;
+  /* Test for nothing to do */
+  ierr = VecNorm(R,&dp); CHKERRQ(ierr);
+  if ((*itP->converged)(itP,0,dp,itP->cnvP)) {*its = 0; return 0;}
+  MONITOR(itP,dp,0);
+  if (history) history[0] = dp;
 
-/* Make the initial Rp == R */
-VecCopy(R,RP);
+  /* Make the initial Rp == R */
+  ierr = VecCopy(R,RP); CHKERRQ(ierr);
 
-rhoold   = 1.0;
-alpha    = 1.0;
-omegaold = 1.0;
-VecSet(&zero,P);
-VecSet(&zero,V);
+  rhoold   = 1.0;
+  alpha    = 1.0;
+  omegaold = 1.0;
+  ierr = VecSet(&zero,P); CHKERRQ(ierr);
+  ierr = VecSet(&zero,V); CHKERRQ(ierr);
 
-for (i=0; i<maxit; i++) {
-    VecDot(R,RP,&rho);                             /*   rho <- rp' r       */
+  for (i=0; i<maxit; i++) {
+    ierr = VecDot(R,RP,&rho); CHKERRQ(ierr);       /*   rho <- rp' r       */
     if (rho == 0.0) {fprintf(stderr,"Breakdown\n"); *its = -(i+1);return 0;} 
     beta = (rho/rhoold) * (alpha/omegaold);
     tmp = -omegaold; VecAXPY(&tmp,V,P);            /*   p <- p - w v       */
-    VecAYPX(&beta,R,P);                            /*   p <- r + p beta    */
+    ierr = VecAYPX(&beta,R,P); CHKERRQ(ierr);      /*   p <- r + p beta    */
     ierr = PCApplyBAorAB(itP->B,itP->right_pre,
                          P,V,T); CHKERRQ(ierr);    /*   v <- K p           */
-    VecDot(RP,V,&d1);
-    alpha = rho / d1;                              /*   a <- rho / (rp' v) */
-    tmp = -alpha; VecWAXPY(&tmp,V,R,S);            /*   s <- r - a v       */
+    ierr = VecDot(RP,V,&d1); CHKERRQ(ierr);
+    alpha = rho / d1; tmp = -alpha;                /*   a <- rho / (rp' v) */
+    ierr = VecWAXPY(&tmp,V,R,S); CHKERRQ(ierr);    /*   s <- r - a v       */
     ierr = PCApplyBAorAB(itP->B,itP->right_pre,
                          S,T,R); CHKERRQ(ierr);    /*   t <- K s           */
-    VecDot(S,T,&d1);
-    VecDot(T,T,&d2);
+    ierr = VecDot(S,T,&d1); CHKERRQ(ierr);
+    ierr = VecDot(T,T,&d2); CHKERRQ(ierr);
     if (d2 == 0.0) {
-	/* t is 0.  if s is 0, then alpha v == r, and hence alpha p
-	   may be our solution.  Give it a try? */
-	VecDot(S,S,&d1);
-	if (d1 != 0.0) {SETERRQ(1,"KSPSolve_BCGS: Breakdown");}
-	VecAXPY(&alpha,P,X);                     /*     x <- x + a p   */
-	if (history && hist_len > i+1) history[i+1] = 0.0;
-	MONITOR(itP,0.0,i+1);
-	break;
+      /* t is 0.  if s is 0, then alpha v == r, and hence alpha p
+	 may be our solution.  Give it a try? */
+      ierr = VecDot(S,S,&d1); CHKERRQ(ierr);
+      if (d1 != 0.0) {SETERRQ(1,"KSPSolve_BCGS: Breakdown");}
+      ierr = VecAXPY(&alpha,P,X); CHKERRQ(ierr);   /*   x <- x + a p       */
+      if (history && hist_len > i+1) history[i+1] = 0.0;
+      MONITOR(itP,0.0,i+1);
+      break;
     }
-    omega = d1 / d2;                      /*     w <- (s't) / (t't) */
-    VecAXPY(&alpha,P,X);                     /*     x <- x + a p   */
-    VecAXPY(&omega,S,X);                     /*     x <- x + w s   */
-    tmp = -omega; VecWAXPY(&tmp,T,S,R);          /*     r <- s - w t   */
-    VecNorm(R,&dp);
+    omega = d1 / d2;                               /*   w <- (s't) / (t't) */
+    ierr = VecAXPY(&alpha,P,X); CHKERRQ(ierr);     /*   x <- x + a p       */
+    ierr = VecAXPY(&omega,S,X); CHKERRQ(ierr);     /*   x <- x + w s       */
+    tmp = -omega; 
+    ierr = VecWAXPY(&tmp,T,S,R); CHKERRQ(ierr);    /*   r <- s - w t       */
+    ierr = VecNorm(R,&dp); CHKERRQ(ierr);
 
     rhoold   = rho;
     omegaold = omega;
@@ -98,9 +99,9 @@ for (i=0; i<maxit; i++) {
   if (i == maxit) i--;
   if (history) itP->res_act_size = (hist_len < i + 1) ? hist_len : i + 1;
 
-  KSPUnwindPre( itP, X, T );
+  ierr = KSPUnwindPre(itP,X,T); CHKERRQ(ierr);
   if (cerr <= 0) *its = -(i+1);
-  else          *its = i + 1;
+  else           *its = i + 1;
   return 0;
 }
 
