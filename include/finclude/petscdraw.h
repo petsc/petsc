7c014067ae9d040e9f30a3ab92383de5b0731c56
Barry Smith <bsmith@mcs.anl.gov>
1995-08-21 21:53:12 -0600

    Initial revision

---
+++ include/finclude/petscdraw.h
@@ -0,0 +1,7 @@
+
+C      Include file for for Fortran use of the KSP package
+C
+
+C
+C      End of Fortran include file for the KSP package
+
---
+++ include/finclude/petscksp.h
@@ -0,0 +1,15 @@
+
+C      Include file for for Fortran use of the Vec package
+C
+      integer INSERTVALUES, ADDVALUES
+      integer SCATTERREVERSE,SCATTERDOWN,SCATTERUP,SCATTERALL,
+     *        SCATTERALLREVERSE
+      integer PIPELINEDOWN,PIPELINEUP
+
+      parameter (INSERTVALUES = 1, ADDVALUES = 2)
+      parameter (SCATTERREVERSE = 1,SCATTERDOWN = 2,SCATTERUP = 4,
+     *           SCATTERALL = 8, SCATTERALLREVERSE = 9)
+      parameter (PIPELINEDOWN = 0,PIPELINEUP = 1)
+C
+C      End of Fortran include file for the Vec package
+
