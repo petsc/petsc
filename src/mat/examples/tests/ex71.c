80bd06c65ce957a6aebb2a285a26a885c372328b
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-08-24 08:18:21 -0600

    *** empty log message ***

--- src/mat/examples/tests/ex71.c
+++ src/mat/examples/tests/ex71.c
@@ -27,30 +27,29 @@ int main(int argc,char **args)
                                                            SETERRA(ierr,0);
   ierr = GridCreateUniform2d(MPI_COMM_WORLD,m,0.0,1.0,n,0.0,1.0,&grid);
   ierr = StencilCreate(MPI_COMM_WORLD,STENCIL_Uxx,&stencil); CHKERRA(ierr);
-  StencilAddStage(stencil,grid,0,0,0,A); CHKERRA(ierr);
-  StencilDestroy(stencil);
+  ierr = StencilAddStage(stencil,grid,0,0,0,A); CHKERRA(ierr);
+  ierr = StencilDestroy(stencil); CHKERRA(ierr);
   ierr = StencilCreate(MPI_COMM_WORLD,STENCIL_Uyy,&stencil); CHKERRA(ierr);
-  StencilAddStage(stencil,grid,0,0,0,A); CHKERRA(ierr);
-  StencilDestroy(stencil);
+  ierr = StencilAddStage(stencil,grid,0,0,0,A); CHKERRA(ierr);
+  ierr = StencilDestroy(stencil); CHKERRA(ierr);
   ierr = MatAssemblyBegin(A,FINAL_ASSEMBLY); CHKERRA(ierr);
   ierr = MatAssemblyEnd(A,FINAL_ASSEMBLY); CHKERRA(ierr);
-  ierr = StencilCreate(MPI_COMM_WORLD,STENCIL_DIRICHLET,&stencil); 
-  CHKERRA(ierr);  
+  ierr = StencilCreate(MPI_COMM_WORLD,STENCIL_DIRICHLET,&stencil); CHKERRA(ierr);  
   ierr = StencilAddStage(stencil,grid,0,0,0,A); CHKERRA(ierr);
-  StencilDestroy(stencil);
+  ierr = StencilDestroy(stencil); CHKERRA(ierr);
 
   ierr = MatView(A,viewer); CHKERRA(ierr);
 
   ierr = VecCreateSequential(MPI_COMM_SELF,m,&x); CHKERRA(ierr);
-  VecSet(&one,x);
+  ierr = VecSet(&one,x); CHKERRA(ierr);
   ierr = VecView(x,viewer); CHKERRA(ierr);
   
-  sleep(30);
+  PetscSleep(30);
   ierr = PetscObjectDestroy((PetscObject) viewer); CHKERRA(ierr);
 
-  VecDestroy(x); 
-  MatDestroy(A);
-  GridDestroy(grid);
+  ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = MatDestroy(A); CHKERRA(ierr);
+  ierr = GridDestroy(grid); CHKERRA(ierr);
   PetscFinalize();
   return 0;
 }
