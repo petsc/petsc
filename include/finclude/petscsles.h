ad32469860a24952bb58a124d3e43eeb65ef89f5
Barry Smith <bsmith@mcs.anl.gov>
1995-10-09 15:50:51 -0600

    *** empty log message ***

--- include/finclude/petscsles.h
+++ include/finclude/petscsles.h
@@ -1,16 +1,7 @@
 
-C      Include file for for Fortran use of the PC package
+C      Include file for for Fortran use of the SLES package
 C
-#define PC           integer
-#define PCMethod     integer
-#define MatStructure integer
+#define SLES         integer
 C
-      integer PCNONE, PCJACOBI, PCSOR, PCLU, PCSHELL, PCBJACOBI, PCMG,
-     *        PCEISENSTAT, PCILU, PCICC, PCSPAI
-
-      parameter (PCNONE = 0, PCJACOBI = 1, PCSOR = 2, PCLU = 3, 
-     *           PCSHELL = 4, PCBJACOBI = 5, PCMG = 6,
-     *           PCEISENSTAT = 7, PCILU = 8, PCICC = 9, PCSPAI = 10)
-C
-C      End of Fortran include file for the PC package
+C      End of Fortran include file for the SLES package
 
