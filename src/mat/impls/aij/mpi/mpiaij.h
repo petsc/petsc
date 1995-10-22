8c386bf4b0a50868c3d91938ae7d9e22bdc23962
Barry Smith <bsmith@mcs.anl.gov>
1995-10-21 21:22:25 -0600

    *** empty log message ***

--- src/mat/impls/aij/mpi/mpiaij.h
+++ src/mat/impls/aij/mpi/mpiaij.h
@@ -1,24 +1,33 @@
-/* $Id: pdvec.c,v 1.10 1995/06/07 17:30:43 bsmith Exp $ */
+/* $Id: mpiaij.h,v 1.10 1995/06/07 17:31:43 bsmith Exp curfman $ */
 
 #include "aij.h"
 
 typedef struct {
-  int           *rowners,*cowners;  /* ranges owned by each processor */
-  int           m,n,M,N;            /* local rows, cols, global rows, cols */
-  int           rstart,rend,cstart,cend;
-  Mat           A,B;                
-  int           numtids,mytid;
-/*  Used in Matrix assembly */
-  int           assembled;          /* MatAssemble has been called */
-  InsertMode    insertmode;
-  Stash         stash;
-  MPI_Request   *send_waits,*recv_waits;
-  int           nsends,nrecvs;
-  Scalar        *svalues,*rvalues;
-  int           rmax;
-  int           *colmap;    /* indicates local col number of off proc column*/
-  int           *garray;
-/*  Used in Matrix-vector product */
-  Vec           lvec;
-  VecScatterCtx Mvctx;
+  int           *rowners, *cowners;     /* ranges owned by each processor */
+  int           m, n;                   /* local rows and columns */
+  int           M, N;                   /* global rows and columns */
+  int           rstart, rend;           /* starting and ending owned rows */
+  int           cstart, cend;           /* starting and ending owned columns */
+  Mat           A, B;                   /* local submatrices: A (diag part),
+                                           B (off-diag part) */
+  int           size;                   /* size of communicator */
+  int           rank;                   /* rank of proc in communicator */ 
+
+  /* The following variables are used for matrix assembly */
+
+  int           assembled;              /* MatAssemble has been called */
+  InsertMode    insertmode;             /* mode for MatSetValues */
+  Stash         stash;                  /* stash for non-local elements */
+  MPI_Request   *send_waits;            /* array of send requests */
+  MPI_Request   *recv_waits;            /* array of receive requests */
+  int           nsends, nrecvs;         /* numbers of sends and receives */
+  Scalar        *svalues, *rvalues;     /* sending and receiving data */
+  int           rmax;                   /* maximum message length */
+  int           *colmap;                /* local col number of off-diag col */
+  int           *garray;                /* work array */
+
+  /* The following variables are used for matrix-vector products */
+
+  Vec           lvec;                   /* local vector */
+  VecScatter    Mvctx;                  /* scatter context for vector */
 } Mat_MPIAIJ;
