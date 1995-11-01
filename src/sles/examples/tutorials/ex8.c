aaaffff8abf8bcdbb895e2f42c27c5eec80d0c1e
Barry Smith <bsmith@mcs.anl.gov>
1995-10-31 18:18:45 -0600

    Initial revision

---
+++ src/sles/examples/tutorials/ex8.c
@@ -0,0 +1,59 @@
+#ifndef lint
+static char vcid[] = "$Id: ex11.c,v 1.10 1995/10/12 04:18:22 bsmith Exp $";
+#endif
+
+static char help[] = "Tests the preconditioner PCSPAI\n\n";
+
+#include "mat.h"
+#include "sles.h"
+#include <stdio.h>
+
+int main(int argc,char **args)
+{
+  Mat     C;
+  int     i, j, m = 15, n = 17, its, I, J, ierr, Istart, Iend;
+  Scalar  v,  one = 1.0;
+  Vec     u,b,x;
+  SLES    sles;
+
+  PetscInitialize(&argc,&args,0,0,help);
+  OptionsGetInt(0,"-m",&m);
+  OptionsGetInt(0,"-n",&n);
+
+  /* Create the matrix for the five point stencil, YET AGAIN */
+  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&C); CHKERRA(ierr);
+  ierr = MatGetOwnershipRange(C,&Istart,&Iend); CHKERRA(ierr);
+  for ( I=Istart; I<Iend; I++ ) { 
+    v = -1.0; i = I/n; j = I - i*n;  
+    if ( i>0 )   {J = I - n; MatSetValues(C,1,&I,1,&J,&v,INSERT_VALUES);}
+    if ( i<m-1 ) {J = I + n; MatSetValues(C,1,&I,1,&J,&v,INSERT_VALUES);}
+    if ( j>0 )   {J = I - 1; MatSetValues(C,1,&I,1,&J,&v,INSERT_VALUES);}
+    if ( j<n-1 ) {J = I + 1; MatSetValues(C,1,&I,1,&J,&v,INSERT_VALUES);}
+    v = 4.0; MatSetValues(C,1,&I,1,&I,&v,INSERT_VALUES);
+  }
+  ierr = MatAssemblyBegin(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatAssemblyEnd(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+
+  /* Create and set vectors */
+  ierr = VecCreate(MPI_COMM_WORLD,m*n,&b); CHKERRA(ierr);
+  ierr = VecDuplicate(b,&u); CHKERRA(ierr);
+  ierr = VecDuplicate(b,&x); CHKERRA(ierr);
+  ierr = VecSet(&one,u); CHKERRA(ierr);
+  ierr = MatMult(C,u,b); CHKERRA(ierr);
+
+  /* Create SLES context; set operators and options; solve linear system */
+  ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
+  ierr = SLESSetOperators(sles,C,C,ALLMAT_DIFFERENT_NONZERO_PATTERN); 
+  CHKERRA(ierr);
+  ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
+  ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
+
+  /* Free work space */
+  ierr = SLESDestroy(sles); CHKERRA(ierr);
+  ierr = VecDestroy(u); CHKERRA(ierr);
+  ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = VecDestroy(b); CHKERRA(ierr);
+  ierr = MatDestroy(C); CHKERRA(ierr);
+  PetscFinalize();
+  return 0;
+}
