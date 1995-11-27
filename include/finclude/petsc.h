320dac3a9341ec1c5fc445103646369d290e161d
Lois Curfman McInnes <curfman@mcs.anl.gov>
1995-11-27 12:58:55 -0600

    *** empty log message ***

--- include/finclude/petsc.h
+++ include/finclude/petsc.h
@@ -5,6 +5,7 @@ C
 C
 #include "mpif.h"
 
+C  Various flags
       integer   PETSC_TRUE, PETSC_FALSE, PETSC_DECIDE, PETSC_DEFAULT
       integer   FP_TRAP_OFF, FP_TRAP_ON, FP_TRAP_ALWAYS
 
@@ -12,17 +13,19 @@ C
      *           PETSC_DEFAULT = -2)
       parameter (FP_TRAP_OFF = 0, FP_TRAP_ON = 1, FP_TRAP_ALWAYS = 2)
 
-      integer   STDOUT_VIEWER_SELF,STDERR_VIEWER_SELF,
+C  Default Viewers
+      integer   STDOUT_VIEWER_SELF, STDERR_VIEWER_SELF,
      *          STDOUT_VIEWER_WORLD
 
+C  Miscellaneous
       integer   PetscInt(1), PetscNull
-
       double precision PetscDouble(1)
 
       common   /fortranview/  PetscDouble, PetscInt, PetscNull,
      *         STDOUT_VIEWER_SELF,STDERR_VIEWER_SELF,STDOUT_VIEWER_WORLD
 
 
+C  Macros for error checking
 #if defined(PETSC_DEBUG)
 #define SETERRA(n,s)   call MPI_Abort(MPI_COMM_WORLD,n)
 #define CHKERRA(n)     if (n .ne. 0) call MPI_Abort(MPI_COMM_WORLD,n)
