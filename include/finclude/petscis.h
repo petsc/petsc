617d1cbc193185309bb99354e1aff24ee3e3224a
Barry Smith <bsmith@mcs.anl.gov>
1995-11-27 13:31:28 -0600

    *** empty log message ***

--- include/finclude/petscis.h
+++ include/finclude/petscis.h
@@ -1,20 +1,8 @@
 C
-C      Include file for Fortran use of the DA package in PETSc
+C      Include file for Fortran use of the IS package in PETSc
 C
-#define DA             integer
-#define DAPeriodicType integer
-#define DAStencilType  integer
+#define IS             integer
 
-      integer DA_STENCIL_STAR, DA_STENCIL_BOX
-
-      integer DA_NONPERIODIC, DA_XPERIODIC, DA_YPERIODIC, DA_XYPERIODIC,
-     *        DA_XYZPERIODIC, DA_XZPERIODIC, DA_YZPERIODIC,DA_ZPERIODIC
-
-      parameter (DA_STENCIL_STAR = 0, DA_STENCIL_BOX = 1)
-
-      parameter (DA_NONPERIODIC = 0, DA_XPERIODIC = 1, DA_YPERIODIC = 2,
-     *         DA_XYPERIODIC = 3, DA_XYZPERIODIC = 4, DA_XZPERIODIC = 5,
-     *         DA_YZPERIODIC = 6, DA_ZPERIODIC = 7)
 C
-C      End of Fortran include file for the DA package in PETSc
+C      End of Fortran include file for the IS package in PETSc
 
