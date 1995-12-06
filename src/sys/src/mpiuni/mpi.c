3b036aee9362a6ee6648533ea143573c3c139205
Satish Balay <balay@mcs.anl.gov>
1995-12-06 15:17:10 -0600

    *** empty log message ***

--- src/sys/src/mpiuni/mpi.c
+++ src/sys/src/mpiuni/mpi.c
@@ -1,8 +1,14 @@
-long MPID_DUMMY = 0;
+#include <stdio.h>
 #define MPI_SUCCESS 0
+#if defined(PARCH_sun4) && !defined(__cplusplus)
+extern int fprintf(FILE*,const char*,...);
+#endif
+
+long MPID_DUMMY = 0;
+void * _v_ = 0;
 double MPI_Wtime()
 {
- printf("MPI_Wtime: use Petsctime instead\n");
+ fprintf(stderr,"MPI_Wtime: use Petsctime instead\n");
  return 0.0;
 }
 void mpi_init__(int *ierr)
