c3b03e6f3bc25ebdafd0f6685c8930ce5f3e30ba
Barry Smith <bsmith@mcs.anl.gov>
1996-01-22 13:01:05 -0600

    AutoCheckin

--- src/vec/impls/dvecimpl.h
+++ src/vec/impls/dvecimpl.h
@@ -1,9 +1,8 @@
-/* $Id: dvec2.c,v 1.11 1995/06/07 17:27:28 bsmith Exp $ */
+/* $Id: dvecimpl.h,v 1.3 1995/06/07 17:28:55 bsmith Exp bsmith $ */
 /* 
    This should not be included in users code.
 
   Includes definition of structure for seqential double precision vectors
-  Includes declarations of all Dv* functions 
 
   These are shared by dvec1.c dvec2.c dvec3.c bvec1.c bvec2.c 
   pvectors/pvec.c pvectors/pbvec.c 
