ee132b5ed5b7f948376b3022b077987aa2cc0d95
Satish Balay <balay@mcs.anl.gov>
1996-01-29 19:37:16 -0600

    *** empty log message ***

--- maint/co.c
+++ maint/co.c
@@ -1,23 +1,28 @@
 #include <sys/param.h>
-
+#include <string.h>
 int main(int argc, char **argv)
 {
   int i;
   char resolved_path[MAXPATHLEN];
   char path[MAXPATHLEN];
   char command[4*MAXPATHLEN];
+  char *c;
 
-  if (argc !=4) {
-    printf(" Wrong no of arg: %d \n", argc);
-    for (i =0; i < argc; i++)
-      printf("%s ",argv[i]);
-    printf("\n");
-    return 1;
-  }
 
+  
+  for (i =0; i < argc; i++)
+    printf("%s ",argv[i]);
+  printf("\n");
+  if (argc !=4) {      printf(" Wrong no of arg: %d \n", argc); return 1;  }
+  
   strcpy(path, argv[3]);
   realpath(path, resolved_path);
-  sscanf(resolved_path,"%[^RCS]",path);
+  /*  sscanf(resolved_path,"%[^R]",path);*/
+  strcpy(path, resolved_path);
+  c = strstr(path,"RCS");
+  *c = NULL;
+  
+  
 
 
   strcpy(command,"cd ");
@@ -28,7 +33,7 @@ int main(int argc, char **argv)
   strcat(command, argv[2]);
   strcat (command, " ");
   strcat (command, resolved_path);
-  printf("com:%s\n",command);
+  printf("command:%s\n",command);
   system(command);
   return 0;
 }
