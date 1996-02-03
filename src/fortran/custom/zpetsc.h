da2ab787eb701d0d6e9b20c2de01938283406c7a
Barry Smith <bsmith@mcs.anl.gov>
1996-02-03 12:23:11 -0600

    *** empty log message ***

--- src/fortran/custom/zpetsc.h
+++ src/fortran/custom/zpetsc.h
@@ -18,3 +18,11 @@ extern void MPIR_RmPointer(int);
 #define MPIR_FromPointer(a) (int)(a)
 #define MPIR_RmPointer(a)
 #endif
+
+/*
+    This defines the mappings from Fortran charactor strings 
+  to C charactor strings on the Cray T3D.
+  */
+#if defined(PARCH_t3d)
+#include <fortran.h>
+#endif
