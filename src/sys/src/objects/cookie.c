cf514535ceaa1d5aa6ca293846b40198b3673601
Barry Smith <bsmith@mcs.anl.gov>
1996-02-08 19:44:21 -0600

    Initial revision

---
+++ src/sys/src/objects/cookie.c
@@ -0,0 +1,14 @@
+
+
+/*@
+     PetscRegisterCookie - Registers a new cookie for use 
+        with a newly created PETSc object class. 
+
+  Output Parameter:
+.   cookie - the cookie you have been assigned
+
+@*/
+int PetscRegisterCookie(int *cookie);
+{
+
+}
