c9d17d2c83d406c8bbacb4e1caafac0787f62fae
Barry Smith <bsmith@mcs.anl.gov>
1996-02-08 21:44:54 -0600

    *** empty log message ***

--- src/sys/src/objects/cookie.c
+++ src/sys/src/objects/cookie.c
@@ -1,14 +1,22 @@
+#ifndef lint
+static char vcid[] = "$Id: gcookie.c,v 1.2 1996/02/08 18:26:06 bsmith Exp $";
+#endif
 
+int LARGEST_PETSC_COOKIE = 30;
 
 /*@
      PetscRegisterCookie - Registers a new cookie for use 
-        with a newly created PETSc object class. 
+        with a newly created PETSc object class. You should pass
+        in a variable preset to zero and it will be assigned a cookie,
+        repeated calls to this routine with the same variable will 
+        not change the cookie. 
 
   Output Parameter:
 .   cookie - the cookie you have been assigned
 
 @*/
-int PetscRegisterCookie(int *cookie);
+int PetscRegisterCookie(int *cookie)
 {
-
+  if (!*cookie) *cookie = LARGEST_PETSC_COOKIE++;
+  return 0;
 }
