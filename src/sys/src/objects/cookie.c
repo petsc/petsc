b0ba071d40fb6d1b02c970d50467cc644eab4620
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-02-14 20:17:32 -0600

    *** empty log message ***

--- src/sys/src/objects/cookie.c
+++ src/sys/src/objects/cookie.c
@@ -1,20 +1,25 @@
 #ifndef lint
-static char vcid[] = "$Id: cookie.c,v 1.2 1996/02/09 03:44:54 bsmith Exp bsmith $";
+static char vcid[] = "$Id: cookie.c,v 1.3 1996/02/09 14:57:35 bsmith Exp curfman $";
 #endif
 
 #include "petsc.h"
 int LARGEST_PETSC_COOKIE = LARGEST_PETSC_COOKIE_STATIC;
 
 /*@
-     PetscRegisterCookie - Registers a new cookie for use 
-        with a newly created PETSc object class. You should pass
-        in a variable preset to zero and it will be assigned a cookie,
-        repeated calls to this routine with the same variable will 
-        not change the cookie. 
+    PetscRegisterCookie - Registers a new cookie for use with a
+    newly created PETSc object class.  The user should pass in
+    a variable initialized to zero; then it will be assigned a cookie.
+    Repeated calls to this routine with the same variable will 
+    not change the cookie. 
 
-  Output Parameter:
+    Output Parameter:
 .   cookie - the cookie you have been assigned
 
+    Note:
+    The initial cookie variable MUST be set to zero on the
+    first call to this routine.
+
+.keywords:  register, cookie
 @*/
 int PetscRegisterCookie(int *cookie)
 {
