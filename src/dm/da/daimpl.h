789e6193f2d9c29854fbca40e11544117b293fe2
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-02-17 10:35:35 -0600

    *** empty log message ***

--- src/dm/da/daimpl.h
+++ src/dm/da/daimpl.h
@@ -1,7 +1,7 @@
-/* $Id: daimpl.h,v 1.9 1996/01/26 04:35:37 bsmith Exp curfman $ */
+/* $Id: daimpl.h,v 1.10 1996/02/16 00:31:58 curfman Exp curfman $ */
 
 /*
-
+   Distributed arrays - communication tools for parallel, rectangular grids.
 */
 
 #if !defined(_DAIMPL_H)
@@ -10,31 +10,33 @@
 
 struct _DA {
   PETSCHEADER
-  int           M,N,P;             /* array dimensions */
-  int           m,n,p;             /* processor layout */
-  int           w;                 /* degrees of freedom per node */
-  int           s;                 /* stencil width */
-  int           xs,xe,ys,ye,zs,ze; /* range of local values */
-  int           Xs,Xe,Ys,Ye,Zs,Ze; /* range including ghost values*/
-  int           *idx,Nl;           /* local to global map */
-  int           base;              /* global number of 1st local node */
-  int           wrap;              /* indicates if periodic boundaries */
-  VecScatter    gtol,ltog,ltol;      
-  Vec           global,local;
-  DAStencilType stencil_type;
-  int           dim;               /* DA dimension (1,2, or 3) */
+  int            M,N,P;             /* array dimensions */
+  int            m,n,p;             /* processor layout */
+  int            w;                 /* degrees of freedom per node */
+  int            s;                 /* stencil width */
+  int            xs,xe,ys,ye,zs,ze; /* range of local values */
+  int            Xs,Xe,Ys,Ye,Zs,Ze; /* range including ghost values */
+  int            *idx,Nl;           /* local to global map */
+  int            base;              /* global number of 1st local node */
+  DAPeriodicType wrap;              /* indicates type of periodic boundaries */
+  VecScatter     gtol, ltog, ltol;  /* scatters, see below for details */
+  Vec            global, local;     /* vectors */
+  DAStencilType  stencil_type;      /* stencil, either box or star */
+  int            dim;               /* DA dimension (1,2, or 3) */
 };
 
 /*
-     gtol - Global representation to local 
-            Global has on each processor interior degrees of freedom and no
-                   ghost points. This vector is what the solvers usually see.
-            Local has on each processor the ghost points as well. This is 
-                  what code to calculate Jacobians etc. sees
+  Vectors:
+     Global has on each processor the interior degrees of freedom and
+         no ghost points. This vector is what the solvers usually see.
+     Local has on each processor the ghost points as well. This is 
+          what code to calculate Jacobians, etc. usually sees.
+  Vector scatters:
+     gtol - Global representation to local
      ltog - Local representation to global (involves no communication)
-     ltol - Local representation to local representation
-                  updates the ghostpoint values in the second vector from 
-                  (correct) interior values in the first vector.
-                  This is good for explicit nearest neighbor time-stepping.
+     ltol - Local representation to local representation, updates the
+            ghostpoint values in the second vector from (correct) interior
+            values in the first vector.  This is good for explicit
+            nearest neighbor time-stepping.
 */
 #endif
