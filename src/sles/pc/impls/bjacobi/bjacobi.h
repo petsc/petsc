0e70ea211821fa0ade468994e1350ec7cc7386bf
Barry Smith <bsmith@mcs.anl.gov>
1996-02-26 13:22:06 -0600

    *** empty log message ***

--- src/sles/pc/impls/bjacobi/bjacobi.h
+++ src/sles/pc/impls/bjacobi/bjacobi.h
@@ -1,4 +1,4 @@
-/* $Id: bjacobi.h,v 1.14 1995/12/21 16:42:48 bsmith Exp bsmith $ */
+/* $Id: bjacobi.h,v 1.15 1995/12/21 18:31:05 bsmith Exp bsmith $ */
 
 #if !defined(__BJACOBI_H)
 #define __BJACOBI_H
@@ -8,17 +8,18 @@
 #include "sles.h"
 
 typedef struct {
-  int  n, n_local;          /* number of blocks (global, local) */
-  int  first_local;         /* number of first block on processor */
-  int  use_true_local;      /* use block from true matrix, not preconditioner matrix
-                               for local MatMult(). */
-  SLES *sles;               /* SLES contexts for blocks */
-  void *data;               /* implementation-specific data */
-  int  same_local_solves;   /* flag indicating whether all local solvers are same */
-  int  *l_lens,*g_lens;     /* lens of each block */
-  int  *l_true,*g_true;     /* select block from true matrix or preconditioner matrix */
+  int       n, n_local;        /* number of blocks (global, local) */
+  int       first_local;       /* number of first block on processor */
+  int       use_true_local;    /* use block from true matrix, not preconditioner matrix
+                                  for local MatMult(). */
+  SLES      *sles;             /* SLES contexts for blocks */
+  void      *data;             /* implementation-specific data */
+  int       same_local_solves; /* flag indicating whether all local solvers are same */
+  int       *l_lens,*g_lens;   /* lens of each block */
+  int       *l_true,*g_true;   /* select block from true matrix or preconditioner matrix */
+  int       block_setup;       /* if SLESSetUp() called on blocks */
   /* -----------------Options related to Gauss-Seidel ----------------------------*/
-  int       gs;                  /* flag indicating we are using Gauss-Seidel */
+  int       gs;                /* flag indicating we are using Gauss-Seidel */
   PCBGSType gstype;
 } PC_BJacobi;
 
