d10535464f885c3422f6047ddbfd398bc7943325
Satish Balay <balay@mcs.anl.gov>
1996-02-29 11:35:17 -0600

    ***

--- maint/ci.c
+++ maint/ci.c
@@ -2,36 +2,60 @@
 #include <string.h>
 int main(int argc, char **argv)
 {
-  int i;
+  int i, len;
   char resolved_path[MAXPATHLEN];
   char path[MAXPATHLEN];
   char command[4*MAXPATHLEN];
-  char *c;
-
+  char *c =NULL;
 
   
+  /* Print the comand as it was entered */
   for (i =0; i < argc; i++)
     printf("%s ",argv[i]);
   printf("\n");
-  if (argc !=4) {      printf(" Wrong no of arg: %d \n", argc); return 1;  }
   
-  strcpy(path, argv[3]);
+  /* Assume the last arfument is the filename */
+  strcpy(path, argv[argc-1]);
+  
+  /* Find the actual file corresponding to the link */
   realpath(path, resolved_path);
-  /*  sscanf(resolved_path,"%[^R]",path);*/
+  printf("resolved file: %s\n",resolved_path);
+  
+  /* Now extrext the dir so that I can "cd" to it */
   strcpy(path, resolved_path);
   c = strstr(path,"RCS");
-  *c = NULL;
   
-  
+  /* If "RCS" is not in the path, find the dir by brute force. 
+     Travel the string up to the last occurence of "/" */
+  if ( c != NULL) { *c = NULL; }
+  else {
+    len = strlen(path) -1;
+    while ((len >= 0 ) &&  ( path[len]  != '/' )) len --;
+    path[len+1] = NULL;
+  }
+
+
 
 
   strcpy(command,"cd ");
   strcat(command, path );
   strcat(command,"; /usr/local/bin/ci ");
-  strcat(command, argv[1]);
-  strcat (command, " ");
-  /*  strcat(command, argv[2]);
-  strcat (command, " "); */
+
+  for (i=1; i< argc-1; i++) {
+    /* if -m option is entered, the subsequent string should be in quotes */
+    if (!strncmp( argv[i], "-m", 2)) {
+      strcat(command, argv[i]);
+      strcat (command, "\"");
+      strcat(command, argv[i+1]);
+      strcat (command, "\" "); 
+      i++;
+    }
+    else {
+      strcat(command, argv[i]);
+      strcat (command, " ");
+    }
+  }
+    
   strcat (command, resolved_path);
   printf("command:%s\n",command);
   system(command);
