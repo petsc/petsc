2e7ac64766c5fb805bb4575aab735c32922a18c8
Satish Balay <balay@mcs.anl.gov>
1996-03-08 09:27:57 -0600

    *** empty log message ***

--- src/benchmarks/Index.c
+++ src/benchmarks/Index.c
@@ -6,9 +6,9 @@
 int main( int argc, char **argv)
 {
   double    t1, t2, value;
-  int       i, ierr,z[100000], intval, tmp;
+  int       i, ierr,z[10000], intval, tmp;
+  int       x[10000],y[10000];
   SYRandom  r;
-  Scalar    x[100000],y[100000];
 
   PetscInitialize(&argc, &argv,0,0,0);
   ierr = SYRandomCreate( MPI_COMM_SELF,RANDOM_DEFAULT,&r); CHKERRQ(ierr);
@@ -16,16 +16,16 @@ int main( int argc, char **argv)
   /* Take care of paging effects */
   t1 = PetscGetTime(); 
   
-  for (i=0; i<100000; i++) {
+  for (i=0; i<10000; i++) {
     x[i] = i;
-    x[i] = i;
-    x[i] = i;
+    y[i] = i;
+    z[i] = i;
   }
  
   /* Form the random set of integers */
-  for (i=0; i<100000; i++) {
+  for (i=0; i<10000; i++) {
     ierr   = SYRandomGetValue(r, &value); CHKERRQ(ierr);
-    intval = (int)(value*100000.0);
+    intval = (int)(value*10000.0);
     tmp    = z[i];
     z[i]   = z[intval];
     z[intval] = tmp;
@@ -33,20 +33,20 @@ int main( int argc, char **argv)
 
 
   t1 = PetscGetTime(); 
-  for (i=0; i<10000; i++) {  x[i] = y[z[i]]; }
+  for (i=0; i<1000; i++) {  x[i] = y[z[i]]; }
   t2 = PetscGetTime(); 
-  fprintf(stderr,"%-15s : %e sec\n","x[i] = y[idx[i]]",(t2-t1)/10000.0);
+  fprintf(stderr,"%-19s : %e sec\n","x[i] = y[idx[i]]",(t2-t1)/1000.0);
 
 
   t1 = PetscGetTime(); 
-  for (i=0; i<10000; i++) {  x[z[i]] = y[i]; }
+  for (i=0; i<1000; i++) {  x[z[i]] = y[i]; }
   t2 = PetscGetTime(); 
-  fprintf(stderr,"%-15s : %e sec\n","x[z[i]] = y[i]",(t2-t1)/10000.0);
+  fprintf(stderr,"%-19s : %e sec\n","x[z[i]] = y[i]",(t2-t1)/1000.0);
 
   t1 = PetscGetTime(); 
-  for (i=0; i<10000; i++) {  x[z[i]] = y[z[i]]; }
+  for (i=0; i<1000; i++) {  x[z[i]] = y[z[i]]; }
   t2 = PetscGetTime(); 
-  fprintf(stderr,"%-15s : %e sec\n","x[z[i]] = y[z[i]]",(t2-t1)/10000.0);
+  fprintf(stderr,"%-19s : %e sec\n","x[z[i]] = y[z[i]]",(t2-t1)/1000.0);
 
   PetscFinalize();
   return 0;
