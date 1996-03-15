41709943b8d3cec8766279237d21e89ffe59c8c7
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-03-15 16:29:33 -0600

    *** empty log message ***

--- src/vec/utils/cmesh.c
+++ src/vec/utils/cmesh.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: cmesh.c,v 1.21 1996/02/25 18:14:52 curfman Exp curfman $";
+static char vcid[] = "$Id: cmesh.c,v 1.22 1996/02/26 18:02:57 curfman Exp curfman $";
 #endif
 
 #include "drawimpl.h"   /*I "draw.h" I*/
@@ -11,9 +11,10 @@ static char vcid[] = "$Id: cmesh.c,v 1.21 1996/02/25 18:14:52 curfman Exp curfma
 
    Input Parameters:
 .   win - the window to draw in
-.   m,n - the number of mesh points in the x and y directions
-.   x,y - the locations of the mesh points, if null uses uniform [0,1]
-.   v - the array of values
+.   m,n - the local number of mesh points in the x and y directions
+.   x,y - the locations of the local mesh points (optional, use PETSC_NULL
+          to indicate uniform spacing on [0,1])
+.   V - the vector
 
     Note: 
     This may be a basic enough function to be a graphics primative
@@ -23,21 +24,71 @@ static char vcid[] = "$Id: cmesh.c,v 1.21 1996/02/25 18:14:52 curfman Exp curfma
 @*/
 int DrawTensorContour(Draw win,int m,int n,double *x,double *y,Vec V)
 {
-  int           xin = 1, yin = 1, c1, c2, c3, c4, i, N, rank, ierr;
-  double        h,x1,x2,x3,x4,y1,y2,y3,y4,*v,min,max;
+  int           xin = 1, yin = 1, c1, c2, c3, c4, i, N, rank, ierr, flag, Nloc;
+  int           isend[2], irecv[2], mglob, nglob;
+  double        h, x1, x2, x3, x4, y1, y2, y3, y4, *v, min, max;
   Scalar        scale;
   Vec           W;
-  IS            from,to;
+  IS            from, to;
   VecScatter    ctx;
   PetscObject   vobj = (PetscObject) win;
 
   if (vobj->cookie == DRAW_COOKIE && vobj->type == NULLWINDOW) return 0;
-
   MPI_Comm_rank(win->comm,&rank);
+  flag = 1;
+
+  if (!flag) {
+#if !defined(PETSC_COMPLEX)
+    ierr = VecGetArray(V,&v); CHKERRQ(ierr);
+    ierr = VecGetLocalSize(V,&Nloc); CHKERRQ(ierr);
+
+    /* Scale the color values between 32 and 256 */
+    ierr = VecMax(V,0,&max); CHKERRQ(ierr); ierr = VecMin(V,0,&min); CHKERRQ(ierr);
+    scale = (200.0 - 32.0)/(max - min);
+    ierr = VecScale(&scale,V); CHKERRQ(ierr);
+
+    isend[0] = m; isend[1] = n;
+    MPI_Allreduce(isend,irecv,2,MPI_INT,MPI_SUM,win->comm);
+    mglob = irecv[0]; nglob = irecv[1];
+    if (!x) {
+      /* must determine local starting point */
+      xin = 0; 
+      x = (double *) PetscMalloc( m*sizeof(double) ); CHKPTRQ(x);
+      h = 1.0/(mglob-1);
+      x[0] = 0.0;
+      for ( i=1; i<m; i++ ) x[i] = x[i-1] + h;
+    }
+    if (!y) {
+      /* must determine local starting point */
+      yin = 0; 
+      y = (double *) PetscMalloc( n*sizeof(double) ); CHKPTRQ(y);
+      h = 1.0/(nglob-1);
+      y[0] = 0.0;
+      for ( i=1; i<n; i++ ) y[i] = y[i-1] + h;
+    }
+
+    for ( i=0; i<Nloc; i++ ) {
+      if (!((i+1) % mglob) ) continue;  /* last column on right is skipped */
+      if (i+mglob+1 >= N) continue;
+
+      x1 = x[i % m];     y1 = y[i/m];        c1 = (int) (32. + v[i]);
+      x2 = x[(i+1) % m]; y2 = y1;            c2 = (int) (32. + v[i+1]);
+      x3 = x2;           y3 = y[(i/m) + 1];  c3 = (int) (32. + v[i+m+1]);
+      x4 = x1;           y4 = y3;            c4 = (int) (32. + v[i+m]);
+
+      ierr = DrawTriangle(win,x1,y1,x2,y2,x3,y3,c1,c2,c3); CHKERRQ(ierr);
+      ierr = DrawTriangle(win,x1,y1,x3,y3,x4,y4,c1,c3,c4); CHKERRQ(ierr);
+    }
+    ierr = VecRestoreArray(V,&v); CHKERRQ(ierr);
+    if (!xin) PetscFree(x); 
+    if (!yin) PetscFree(y);
+#endif
+
+  } else {
 
   /* move entire vector to first processor */
   if (rank == 0) {
-    VecGetSize(V,&N);
+    ierr = VecGetSize(V,&N); CHKERRQ(ierr);
     ierr = VecCreateSeq(MPI_COMM_SELF,N,&W); CHKERRQ(ierr);
     ierr = ISCreateStrideSeq(MPI_COMM_SELF,N,0,1,&from); CHKERRQ(ierr);
     ierr = ISCreateStrideSeq(MPI_COMM_SELF,N,0,1,&to); CHKERRQ(ierr);
@@ -62,12 +113,12 @@ int DrawTensorContour(Draw win,int m,int n,double *x,double *y,Vec V)
 
   if (rank == 0) {
 #if !defined(PETSC_COMPLEX)
-    VecGetArray(W,&v);
+    ierr = VecGetArray(W,&v); CHKERRQ(ierr);
 
-    /* scale the color values between 32 and 256 */
-    VecMax(W,0,&max); VecMin(W,0,&min);
+    /* Scale the color values between 32 and 256 */
+    ierr = VecMax(W,0,&max); CHKERRQ(ierr); ierr = VecMin(W,0,&min); CHKERRQ(ierr);
     scale = (200.0 - 32.0)/(max - min);
-    VecScale(&scale,W);
+    ierr = VecScale(&scale,W); CHKERRQ(ierr);
 
     if (!x) {
       xin = 0; 
@@ -93,14 +144,17 @@ int DrawTensorContour(Draw win,int m,int n,double *x,double *y,Vec V)
       x3 = x2;           y3 = y[(i/m) + 1];  c3 = (int) (32. + v[i+m+1]);
       x4 = x1;           y4 = y3;            c4 = (int) (32. + v[i+m]);
 
-      DrawTriangle(win,x1,y1,x2,y2,x3,y3,c1,c2,c3);
-      DrawTriangle(win,x1,y1,x3,y3,x4,y4,c1,c3,c4);
+      ierr = DrawTriangle(win,x1,y1,x2,y2,x3,y3,c1,c2,c3); CHKERRQ(ierr);
+      ierr = DrawTriangle(win,x1,y1,x3,y3,x4,y4,c1,c3,c4); CHKERRQ(ierr);
     }
-    VecRestoreArray(W,&v);
+    ierr = VecRestoreArray(W,&v); CHKERRQ(ierr);
     if (!xin) PetscFree(x); 
     if (!yin) PetscFree(y);
 #endif
   }
-  VecDestroy(W);
+  ierr = VecDestroy(W); CHKERRQ(ierr);
+
+  }
+
   return 0;
 }
