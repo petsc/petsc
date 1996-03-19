582fc40c5ebd3c6821ec45568e51c224b805d22c
Barry Smith <bsmith@mcs.anl.gov>
1996-03-19 11:17:51 -0600

    AutoCheckin

--- src/dm/da/examples/tests/ex12.m
+++ src/dm/da/examples/tests/ex12.m
@@ -1,11 +1,18 @@
 function ex1(np)
-#
-#   ex1(np) 
-# creates a series of vectors in PETSc and displays them in Matlab
-#
-err = launch('ex1 ',np);
-if (err != 0) return;
+%
+%   ex1(np) 
+% creates a series of vectors in PETSc and displays them in Matlab
+%
+time = 20;
+err = launch(['ex1 -on_error_attach_debugger -time ' int2str(time)] ,np);
+if (err ~= 0) then 
+  return;
+end
 
 p = openport;
-v = receive(p);
-plot(v); pause 0;
+for i=1:time,
+  v = receive(p);
+  plot(v); 
+  pause(1);
+end;
+closeport(p);
