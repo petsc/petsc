f834acb9e37d3e9a3e6fc7e94bf0a19e0f770fa6
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-04-12 14:28:07 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex9.c
+++ src/sles/examples/tutorials/ex9.c
@@ -1,25 +1,28 @@
 #ifndef lint
-static char vcid[] = "$Id: ex19.c,v 1.7 1996/04/02 15:18:02 balay Exp curfman $";
+static char vcid[] = "$Id: ex19.c,v 1.8 1996/04/05 00:00:16 curfman Exp curfman $";
 #endif
 
 static char help[] = "Illustrates the solution of 2 different linear systems\n\
 with different linear solvers.  Also, this example illustrates the repeated\n\
 solution of linear systems, while reusing matrix, vector, and solver data\n\
-structures throughout the process.  Note the various stages of event logging.\n\n";
+structures throughout the process.  In addition, note the use of a user-defined\n\
+KSP monitoring routine as well as the various stages of event logging\n\n";
 
 #include "sles.h"
 #include  <stdio.h>
 
-int CheckError(Vec,Vec,int,int);
+int CheckError(Vec,Vec,Vec,int,int);
+int MyKSPMonitor(KSP,int,double,void*);
 
 int main(int argc,char **args)
 {
-  Mat    C, C2; 
+  Vec    u, x1, b1, x2, b2;    /* vectors for systems #1 and #2 */
+  Mat    C1, C2;               /* matrices for systems #1 and #2 */
+  SLES   sles1, sles2;         /* SLES contexts for systems #1 and #2 */
+  KSP    ksp1;                 /* KSP context for system #1 */
   Scalar v;
   int    I, J, ldim, ierr, low, high, iglobal, Istart, Iend, Istart2, Iend2;
   int    i, j, m = 3, n = 2, rank, size, its, flg, t, ntimes = 3, CHECK_ERROR = 0;
-  Vec    u, x, b, x2, b2;
-  SLES   sles, sles2;
 
   PetscInitialize(&argc,&args,(char *)0,help);
   ierr = OptionsGetInt(PETSC_NULL,"-m",&m,&flg); CHKERRA(ierr);
@@ -36,13 +39,18 @@ int main(int argc,char **args)
 
    /* Create data structures for first linear system */
   PLogStagePush(0);
-  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&C); CHKERRA(ierr);
-  ierr = MatGetOwnershipRange(C,&Istart,&Iend); CHKERRA(ierr);
+  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&C1); CHKERRA(ierr);
+  ierr = MatGetOwnershipRange(C1,&Istart,&Iend); CHKERRA(ierr);
   ierr = VecCreateMPI(MPI_COMM_WORLD,PETSC_DECIDE,m*n,&u); CHKERRA(ierr);
-  ierr = VecDuplicate(u,&b); CHKERRA(ierr);
-  ierr = VecDuplicate(b,&x); CHKERRA(ierr);
-  ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
-  ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
+  ierr = VecDuplicate(u,&b1); CHKERRA(ierr);
+  ierr = VecDuplicate(u,&x1); CHKERRA(ierr);
+  ierr = SLESCreate(MPI_COMM_WORLD,&sles1); CHKERRA(ierr);
+  ierr = SLESSetFromOptions(sles1); CHKERRA(ierr);
+  ierr = SLESGetKSP(sles1,&ksp1); CHKERRA(ierr);
+
+  /* Set user-defined monitoring routine for first linear system */
+  ierr = OptionsHasName(PETSC_NULL,"-my_ksp_monitor",&flg); CHKERRA(ierr);
+  if (flg) {ierr = KSPSetMonitor(ksp1,MyKSPMonitor,PETSC_NULL); CHKERRQ(ierr);}
 
   /* Create data structures for second linear system */
   ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&C2); CHKERRA(ierr);
@@ -73,37 +81,43 @@ int main(int argc,char **args)
 
     /* Assemble first matrix */
     PLogStagePush(1);
-    ierr = MatZeroEntries(C); CHKERRA(ierr);
+    ierr = MatZeroEntries(C1); CHKERRA(ierr);
     for ( I=Istart; I<Iend; I++ ) { 
       v = -1.0; i = I/n; j = I - i*n;  
-      if ( i>0 )   {J = I - n; MatSetValues(C,1,&I,1,&J,&v,ADD_VALUES);}
-      if ( i<m-1 ) {J = I + n; MatSetValues(C,1,&I,1,&J,&v,ADD_VALUES);}
-      if ( j>0 )   {J = I - 1; MatSetValues(C,1,&I,1,&J,&v,ADD_VALUES);}
-      if ( j<n-1 ) {J = I + 1; MatSetValues(C,1,&I,1,&J,&v,ADD_VALUES);}
-      v = 4.0; MatSetValues(C,1,&I,1,&I,&v,ADD_VALUES);
+      if ( i>0 )   {J = I - n; MatSetValues(C1,1,&I,1,&J,&v,ADD_VALUES);}
+      if ( i<m-1 ) {J = I + n; MatSetValues(C1,1,&I,1,&J,&v,ADD_VALUES);}
+      if ( j>0 )   {J = I - 1; MatSetValues(C1,1,&I,1,&J,&v,ADD_VALUES);}
+      if ( j<n-1 ) {J = I + 1; MatSetValues(C1,1,&I,1,&J,&v,ADD_VALUES);}
+      v = 4.0; MatSetValues(C1,1,&I,1,&I,&v,ADD_VALUES);
     }
     for ( I=Istart; I<Iend; I++ ) { /* Make matrix nonsymmetric */
       v = -1.0*(t+0.5); i = I/n;
-      if ( i>0 )   {J = I - n; MatSetValues(C,1,&I,1,&J,&v,ADD_VALUES);}
+      if ( i>0 )   {J = I - n; MatSetValues(C1,1,&I,1,&J,&v,ADD_VALUES);}
     }
     PLogFlops(2*(Istart-Iend));
-    ierr = MatAssemblyBegin(C,FINAL_ASSEMBLY); CHKERRA(ierr);
-    ierr = MatAssemblyEnd(C,FINAL_ASSEMBLY); CHKERRA(ierr);
+    ierr = MatAssemblyBegin(C1,FINAL_ASSEMBLY); CHKERRA(ierr);
+    ierr = MatAssemblyEnd(C1,FINAL_ASSEMBLY); CHKERRA(ierr);
 
     /* Indicate same nonzero structure of successive linear system matrices */
-    ierr = MatSetOption(C,NO_NEW_NONZERO_LOCATIONS); CHKERRA(ierr);
+    ierr = MatSetOption(C1,NO_NEW_NONZERO_LOCATIONS); CHKERRA(ierr);
 
     /* Compute right-hand-side */
-    ierr = MatMult(C,u,b); CHKERRA(ierr);
+    ierr = MatMult(C1,u,b1); CHKERRA(ierr);
 
     /* Indicate same nonzero structure of successive preconditioner
        matrices by setting SAME_NONZERO_PATTERN below */
-    ierr = SLESSetOperators(sles,C,C,SAME_NONZERO_PATTERN); CHKERRA(ierr);
+    ierr = SLESSetOperators(sles1,C1,C1,SAME_NONZERO_PATTERN); CHKERRA(ierr);
+
+    /* Use the previous solution of linear system #1 as the initial guess
+       for the next solve of linear system #1 */
+    if (t>0) {
+      ierr = KSPSetInitialGuessNonzero(ksp1); CHKERRA(ierr);
+    }
 
     /* Solve first linear system */
-    ierr = SLESSetUp(sles,b,x); CHKERRA(ierr);
-    ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
-    ierr = CheckError(u,x,its,CHECK_ERROR); CHKERRA(ierr); 
+    ierr = SLESSetUp(sles1,b1,x1); CHKERRA(ierr);
+    ierr = SLESSolve(sles1,b1,x1,&its); CHKERRA(ierr);
+    ierr = CheckError(u,x1,b1,its,CHECK_ERROR); CHKERRA(ierr); 
 
     /* Assemble and solve another system */
     PLogStagePop();
@@ -135,41 +149,56 @@ int main(int argc,char **args)
     /* Compute right-hand-side */
     ierr = MatMult(C2,u,b2); CHKERRA(ierr);
 
-    /* Indicate reuse of identical preconditioner matrix during successive
-       iterations by setting SAME_PRECONDITIONER below */
-    ierr = SLESSetOperators(sles2,C2,C2,SAME_PRECONDITIONER); CHKERRA(ierr);
+    /* Indicate same nonzero structure of successive preconditioner
+       matrices by setting SAME_NONZERO_PATTERN below */
+    ierr = SLESSetOperators(sles2,C2,C2,SAME_NONZERO_PATTERN); CHKERRA(ierr);
 
     /* Solve second linear system */
     ierr = SLESSetUp(sles2,b2,x2); CHKERRA(ierr);
     ierr = SLESSolve(sles2,b2,x2,&its); CHKERRA(ierr);
-    ierr = CheckError(u,x2,its,CHECK_ERROR); CHKERRA(ierr); 
+    ierr = CheckError(u,x2,b2,its,CHECK_ERROR); CHKERRA(ierr); 
     PLogStagePop();
   }
 
   /* Free work space */
-  ierr = SLESDestroy(sles); CHKERRA(ierr); ierr = SLESDestroy(sles2); CHKERRA(ierr);
-  ierr = VecDestroy(x); CHKERRA(ierr);     ierr = VecDestroy(x2); CHKERRA(ierr);
-  ierr = VecDestroy(b); CHKERRA(ierr);     ierr = VecDestroy(b2); CHKERRA(ierr);
-  ierr = MatDestroy(C); CHKERRA(ierr);     ierr = MatDestroy(C2); CHKERRA(ierr);
+  ierr = SLESDestroy(sles1); CHKERRA(ierr); ierr = SLESDestroy(sles2); CHKERRA(ierr);
+  ierr = VecDestroy(x1); CHKERRA(ierr);     ierr = VecDestroy(x2); CHKERRA(ierr);
+  ierr = VecDestroy(b1); CHKERRA(ierr);     ierr = VecDestroy(b2); CHKERRA(ierr);
+  ierr = MatDestroy(C1); CHKERRA(ierr);     ierr = MatDestroy(C2); CHKERRA(ierr);
   ierr = VecDestroy(u); CHKERRA(ierr);
 
   PetscFinalize();
   return 0;
 }
 /* ------------------------ Check error ------------------------- */
-int CheckError(Vec u,Vec x,int its,int CHECK_ERROR)
+int CheckError(Vec u,Vec x,Vec b,int its,int CHECK_ERROR)
 {
   Scalar none = -1.0;
   double norm;
   int    ierr;
 
-  PLogEventBegin(CHECK_ERROR,u,x,0,0);
-  ierr = VecAXPY(&none,u,x); CHKERRA(ierr);
-  ierr = VecNorm(x,NORM_2,&norm); CHKERRA(ierr);
+  PLogEventBegin(CHECK_ERROR,u,x,b,0);
+  ierr = VecCopy(x,b); CHKERRA(ierr);       /* use b as a work vector */
+  ierr = VecAXPY(&none,u,b); CHKERRA(ierr);
+  ierr = VecNorm(b,NORM_2,&norm); CHKERRA(ierr);
   if (norm > 1.e-12)
     PetscPrintf(MPI_COMM_WORLD,"Norm of error %g, Iterations %d\n",norm,its);
   else 
     PetscPrintf(MPI_COMM_WORLD,"Norm of error < 1.e-12, Iterations %d\n",its);
-  PLogEventEnd(CHECK_ERROR,u,x,0,0);
+  PLogEventEnd(CHECK_ERROR,u,x,b,0);
   return 0;
 }
+/* ----------- User-defined KSP monitoring routine  -------------- */
+
+int MyKSPMonitor(KSP ksp,int n,double rnorm,void *dummy)
+{
+  Vec      x;
+  int      ierr;
+
+  ierr = KSPBuildSolution(ksp,PETSC_NULL,&x); CHKERRQ(ierr);
+  PetscPrintf(MPI_COMM_WORLD,"iteration %d solution vector:\n",n);
+  ierr = VecView(x,STDOUT_VIEWER_WORLD); CHKERRQ(ierr);
+  PetscPrintf(MPI_COMM_WORLD,"iteration %d KSP Residual norm %14.12e \n",n,rnorm);
+  return 0;
+}
+
