d979d3754de2292d51f412a7706c5d7b28f89307
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-04-14 08:03:54 -0600

    AutoCheckin

--- include/petscdfvec.h
+++ include/petscdfvec.h
@@ -1,38 +1,39 @@
 
-/* $Id: vgrid.h,v 1.9 1996/02/27 07:06:28 curfman Exp curfman $ */
+/* $Id: dfunc.h,v 1.10 1996/03/20 21:16:32 curfman Exp curfman $ */
 
-/* This file declares some utility routines for manipulating vectors that are
-   associated with multicomponent problems on grids.  VGI = VecGridInfo */
+/* This file declares some utility routines for manipulating discrete functions,
+   or vectors that are associated with grids, possibly with multiple degrees of
+   freedom per node).  DF = Discrete Function */
 
-#ifndef __VEC_GRID_UTILS
-#define __VEC_GRID_UTILS
+#ifndef __DISCRETE_FUNCTION
+#define __DISCRETE_FUNCTION
 
 #include "vec.h"
 #include "da.h"
 
-typedef enum {VGISEQ,VGIMPI} GridType;
+typedef enum {DFSEQ,DFMPI} GridType;
 
 typedef enum {ORDER_1,ORDER_2} GridComponentOrdering;
   /* ORDER_1 - ordering by interlacing components at each grid point
      ORDER_2 - ordering by segregating unknowns according to type
           (1 component for whole grid, then the next component, etc.) */
 
-extern int VECGRID_COOKIE;
+extern int DF_COOKIE;
 
-typedef struct _VGI* VGI;
+typedef struct _DF* DF;
 
-extern int VGICreateMPI(MPI_Comm,int,int,GridComponentOrdering,char**,DA,VGI*);
-extern int VGICreateSeq(MPI_Comm,int,int,GridComponentOrdering,char**,int,int,int,VGI*);
-extern int VGIDuplicate(VGI,VGI*);
-extern int VGIDestroy(VGI);
-extern int VGIGetComponentVectors(Vec,VGI,Vec**);
-extern int VGIAssembleFullVector(Vec*,VGI,Vec);
-extern int VGIGetInfo(VGI,GridType*,int*,int*,GridComponentOrdering*,int*,int*,int*);
-extern int VGIView(VGI,Viewer);
-extern int VGIDrawContours(Vec,VGI,int,int);
-extern int VGIRefineVector(Vec,VGI,Vec*,VGI*);
-extern int VGISetCoordinates(VGI,double*,double*,double*);
-extern int VGIGetCoordinates(VGI,double**,double**,double**);
-extern int VGIRefineCoordinates(VGI,double**,double**,double**);
+extern int DFCreateMPI(MPI_Comm,int,int,GridComponentOrdering,char**,DA,DF*);
+extern int DFCreateSeq(MPI_Comm,int,int,GridComponentOrdering,char**,int,int,int,DF*);
+extern int DFDuplicate(DF,DF*);
+extern int DFDestroy(DF);
+extern int DFGetComponentVectors(Vec,DF,Vec**);
+extern int DFAssembleFullVector(Vec*,DF,Vec);
+extern int DFGetInfo(DF,GridType*,int*,int*,GridComponentOrdering*,int*,int*,int*);
+extern int DFView(DF,Viewer);
+extern int DFDrawContours(Vec,DF,int,int);
+extern int DFRefineVector(Vec,DF,Vec*,DF*);
+extern int DFSetCoordinates(DF,double*,double*,double*);
+extern int DFGetCoordinates(DF,double**,double**,double**);
+extern int DFRefineCoordinates(DF,double**,double**,double**);
 
 #endif
