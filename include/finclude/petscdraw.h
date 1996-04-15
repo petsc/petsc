951bfebf758614f646c4eb6b338ef75337a44598
Satish Balay <balay@mcs.anl.gov>
1996-04-15 17:24:26 -0600

    *** empty log message ***

--- include/finclude/petscdraw.h
+++ include/finclude/petscdraw.h
@@ -1,5 +1,5 @@
 C
-C  $Id: draw.h,v 1.7 1996/02/12 20:31:49 bsmith Exp bsmith $;
+C  $Id: draw.h,v 1.8 1996/02/13 23:31:02 bsmith Exp balay $;
 C
 C  Include file for Fortran use of the Draw package in PETSc
 C
@@ -10,12 +10,18 @@ C
 C
 C  Colors for drawing
 C
-      integer DRAW_WHITE,DRAW_BLACK,DRAW_RED,DRAW_YELLOW,DRAW_GREEN,
-     *        DRAW_CYAN,DRAW_BLUE
+      integer DRAW_WHITE,DRAW_BLACK,DRAW_RED,DRAW_GREEN,DRAW_CYAN,
+     *        DRAW_BLUE,DRAW_MAGENTA,DRAW_AQUAMARINE,DRAW_FORESTGREEN,
+     *        DRAW_ORANGE,DRAW_VIOLET,DRAW_BROWN,DRAW_PINK,DRAW_CORAL,
+     *        DRAW_GRAY,DRAW_YELLOW
 
       parameter (DRAW_WHITE = 0,DRAW_BLACK = 1,DRAW_RED = 2,
-     *           DRAW_YELLOW = 3,DRAW_GREEN = 4,
-     *           DRAW_CYAN = 5,DRAW_BLUE= 6)
+     *           DRAW_GREEN = 3,DRAW_CYAN = 4,DRAW_BLUE = 5,
+     *           DRAW_MAGENTA = 6,DRAW_AQUAMARINE = 7,
+     *           DRAW_FORESTGREEN = 8,DRAW_ORANGE = 9,
+     *           DRAW_VIOLET = 10,DRAW_BROWN = 11,DRAW_PINK = 12,
+     *           DRAW_CORAL =13,DRAW_GRAY = 14,DRAW_YELLOW = 15)
+
 
       integer BUTTON_NONE,BUTTON_LEFT,BUTTON_CENTER,BUTTON_RIGHT
 
@@ -24,4 +30,8 @@ C
 
 C
 C  End of Fortran include file for the Draw package in PETSc
+
+
+
+
 
