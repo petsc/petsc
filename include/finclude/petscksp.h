c790b03f217154731a8dce578173985b662005d5
Satish Balay <balay@mcs.anl.gov>
1996-04-15 17:36:29 -0600

    *** empty log message ***

--- include/finclude/petscksp.h
+++ include/finclude/petscksp.h
@@ -1,10 +1,11 @@
 C
-C  "$Id: petsc.h,v 1.18 1996/02/12 20:26:22 bsmith Exp $";
+C  "$Id: ksp.h,v 1.7 1996/02/12 20:26:59 bsmith Exp balay $";
 C
 C  Include file for Fortran use of the KSP package in PETSc
 C
 #define KSP       integer
 #define KSPType   integer
+#define CGType    integer
 C
 C  Various Krylov subspace methods
 C
@@ -16,6 +17,13 @@ C
      *           KSPGMRES = 3,KSPTCQMR = 4, KSPBCGS = 5, KSPCGS = 6,
      *           KSPTFQMR = 7, KSPCR = 8, KSPLSQR = 9, KSPPREONLY = 10,
      *           KSPQCG = 11)
+
+C
+C  CG Types
+C
+      integer CG_SYMMETRIC,CG_HERMITIAN
+
+      parameter (CG_SYMMETRIC=1, CG_HERMITIAN=2)
 C
 C  End of Fortran include file for the KSP package in PETSc
 
