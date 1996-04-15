4537a579b0f5320813155a4d7d6974acd9616ec3
Satish Balay <balay@mcs.anl.gov>
1996-04-15 17:29:48 -0600

    *** empty log message ***

--- include/finclude/petscis.h
+++ include/finclude/petscis.h
@@ -1,9 +1,14 @@
 C
-C  $Id: is.h,v 1.4 1996/02/12 20:27:49 bsmith Exp bsmith $;
+C  $Id: is.h,v 1.5 1996/02/12 20:28:12 bsmith Exp balay $;
 C
 C  Include file for Fortran use of the IS (index set) package in PETSc
 C
 #define IS             integer
+#define IndexSetType   integer
+
+      integer IS_SEQ,IS_STRIDE_SEQ
+
+      parameter (IS_SEQ=0, IS_STRIDE_SEQ=2)
 
 C
 C  End of Fortran include file for the IS package in PETSc
