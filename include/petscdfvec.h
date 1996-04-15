12cb737b78e21adc652a8609dee95a48b5e28a7b
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-04-14 21:06:33 -0600

    AutoCheckin

--- include/petscdfvec.h
+++ include/petscdfvec.h
@@ -1,5 +1,5 @@
 
-/* $Id: dfunc.h,v 1.10 1996/03/20 21:16:32 curfman Exp curfman $ */
+/* $Id: dfunc.h,v 1.11 1996/04/14 14:03:54 curfman Exp curfman $ */
 
 /* This file declares some utility routines for manipulating discrete functions,
    or vectors that are associated with grids, possibly with multiple degrees of
@@ -11,9 +11,9 @@
 #include "vec.h"
 #include "da.h"
 
-typedef enum {DFSEQ,DFMPI} GridType;
+typedef enum {DFSEQ_GEN,DFMPI_GEN,DFSEQ_REG,DFMPI_REG} DFType;
 
-typedef enum {ORDER_1,ORDER_2} GridComponentOrdering;
+typedef enum {ORDER_1,ORDER_2} DFComponentOrdering;
   /* ORDER_1 - ordering by interlacing components at each grid point
      ORDER_2 - ordering by segregating unknowns according to type
           (1 component for whole grid, then the next component, etc.) */
@@ -22,13 +22,13 @@ extern int DF_COOKIE;
 
 typedef struct _DF* DF;
 
-extern int DFCreateMPI(MPI_Comm,int,int,GridComponentOrdering,char**,DA,DF*);
-extern int DFCreateSeq(MPI_Comm,int,int,GridComponentOrdering,char**,int,int,int,DF*);
+extern int DFCreateDA(MPI_Comm,char**,DA,DF*);
+extern int DFCreateGeneral(MPI_Comm,DFType,int,int,DFComponentOrdering,char**,int,int,int,DF*);
 extern int DFDuplicate(DF,DF*);
 extern int DFDestroy(DF);
 extern int DFGetComponentVectors(Vec,DF,Vec**);
 extern int DFAssembleFullVector(Vec*,DF,Vec);
-extern int DFGetInfo(DF,GridType*,int*,int*,GridComponentOrdering*,int*,int*,int*);
+extern int DFGetInfo(DF,DFType*,int*,int*,DFComponentOrdering*,int*,int*,int*);
 extern int DFView(DF,Viewer);
 extern int DFDrawContours(Vec,DF,int,int);
 extern int DFRefineVector(Vec,DF,Vec*,DF*);
