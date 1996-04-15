bdd2a8f08f90df2165b1d66b0da65fab74f719a9
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-04-15 10:36:59 -0600

    AutoCheckin

--- include/petscdfvec.h
+++ include/petscdfvec.h
@@ -1,9 +1,9 @@
 
-/* $Id: dfunc.h,v 1.11 1996/04/14 14:03:54 curfman Exp curfman $ */
+/* $Id: dfvec.h,v 1.12 1996/04/15 03:06:33 curfman Exp curfman $ */
 
 /* This file declares some utility routines for manipulating discrete functions,
    or vectors that are associated with grids, possibly with multiple degrees of
-   freedom per node).  DF = Discrete Function */
+   freedom per node).  DFVec = Discrete Function */
 
 #ifndef __DISCRETE_FUNCTION
 #define __DISCRETE_FUNCTION
@@ -11,29 +11,29 @@
 #include "vec.h"
 #include "da.h"
 
-typedef enum {DFSEQ_GEN,DFMPI_GEN,DFSEQ_REG,DFMPI_REG} DFType;
+typedef enum {DFVEC_SEQGEN,DFVEC_MPIGEN,DFVEC_SEQREG,DFVEC_MPIREG} DFVecType;
 
-typedef enum {ORDER_1,ORDER_2} DFComponentOrdering;
+typedef enum {ORDER_1,ORDER_2} DFVecComponentOrdering;
   /* ORDER_1 - ordering by interlacing components at each grid point
      ORDER_2 - ordering by segregating unknowns according to type
           (1 component for whole grid, then the next component, etc.) */
 
-extern int DF_COOKIE;
+extern int DFVEC_COOKIE;
 
-typedef struct _DF* DF;
+typedef struct _DFVec* DFVec;
 
-extern int DFCreateDA(MPI_Comm,char**,DA,DF*);
-extern int DFCreateGeneral(MPI_Comm,DFType,int,int,DFComponentOrdering,char**,int,int,int,DF*);
-extern int DFDuplicate(DF,DF*);
-extern int DFDestroy(DF);
-extern int DFGetComponentVectors(Vec,DF,Vec**);
-extern int DFAssembleFullVector(Vec*,DF,Vec);
-extern int DFGetInfo(DF,DFType*,int*,int*,DFComponentOrdering*,int*,int*,int*);
-extern int DFView(DF,Viewer);
-extern int DFDrawContours(Vec,DF,int,int);
-extern int DFRefineVector(Vec,DF,Vec*,DF*);
-extern int DFSetCoordinates(DF,double*,double*,double*);
-extern int DFGetCoordinates(DF,double**,double**,double**);
-extern int DFRefineCoordinates(DF,double**,double**,double**);
+extern int DFVecCreateDA(MPI_Comm,char**,DA,DFVec*);
+extern int DFVecCreateGeneral(MPI_Comm,DFVecType,int,int,DFVecComponentOrdering,char**,int,int,int,DFVec*);
+extern int DFVecDuplicate(DFVec,DFVec*);
+extern int DFVecDestroy(DFVec);
+extern int DFVecGetComponentVectors(Vec,DFVec,Vec**);
+extern int DFVecAssembleFullVector(Vec*,DFVec,Vec);
+extern int DFVecGetInfo(DFVec,DFVecType*,int*,int*,DFVecComponentOrdering*,int*,int*,int*);
+extern int DFVecView(DFVec,Viewer);
+extern int DFVecDrawContours(Vec,DFVec,int,int);
+extern int DFVecRefineVector(Vec,DFVec,Vec*,DFVec*);
+extern int DFVecSetCoordinates(DFVec,double*,double*,double*);
+extern int DFVecGetCoordinates(DFVec,double**,double**,double**);
+extern int DFVecRefineCoordinates(DFVec,double**,double**,double**);
 
 #endif
