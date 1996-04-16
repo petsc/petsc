697265888c1c6a583b27e9cf709c2ed817010f5a
Satish Balay <balay@mcs.anl.gov>
1996-04-15 18:31:33 -0600

    *** empty log message ***

--- include/finclude/petscsnes.h
+++ include/finclude/petscsnes.h
@@ -1,10 +1,22 @@
 C
-C  $Id: petsc.h,v 1.18 1996/02/12 20:26:22 bsmith Exp $;
+C  $Id: snes.h,v 1.10 1996/02/12 20:29:04 bsmith Exp balay $;
 C
 C  Include file for Fortran use of the SNES package in PETSc
 C
 #define SNES            integer
 #define SNESProblemType integer
+#define SNESType        integer
+
+C
+C  SNESType
+C
+      integer SNES_UNKNOWN_METHOD,SNES_EQ_LS,SNES_EQ_TR,
+     *        SNES_EQ_TR_DOG_LEG,SNES_EQ_TR2_LIN,SNES_EQ_TEST,
+     *        SNES_UM_LS,SNES_UM_TR 
+
+      parameter (SNES_UNKNOWN_METHOD=-1,SNES_EQ_LS=0,SNES_EQ_TR=1,
+     *           SNES_EQ_TR_DOG_LEG=2,SNES_EQ_TR2_LIN=3,SNES_EQ_TEST=4,
+     *           SNES_UM_LS=5,SNES_UM_TR=6)
 
 C
 C  Two classes of nonlinear solvers
@@ -17,4 +29,7 @@ C
 
 C
 C  End of Fortran include file for the SNES package in PETSc
+
+
+
 
