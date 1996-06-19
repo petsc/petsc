f76e4c48ba863f3dbbfc2c3369c7c88749a77fe1
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-06-18 19:51:10 -0600

    *** empty log message ***

--- src/dm/da/src/dasub.c
+++ src/dm/da/src/dasub.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: dacorn.c,v 1.4 1996/03/19 21:29:33 bsmith Exp $";
+static char vcid[] = "$Id: dasub.c,v 1.1 1996/06/19 00:18:16 curfman Exp curfman $";
 #endif
  
 /*
@@ -23,26 +23,47 @@ static char vcid[] = "$Id: dacorn.c,v 1.4 1996/03/19 21:29:33 bsmith Exp $";
 
    Notes:
    This routine is particularly useful to compute boundary conditions
-   or application statistics that require sums of data throughout
-   a logical plane of grid points.
+   or other application-specific calculations that require manipulating
+   sets of data throughout a logical plane of grid points.
 
 .keywords: distributed array, get, processor subset
 @*/
 int DAGetProcessorSubset(DA da,DADirection dir,int gp,MPI_Comm *comm)
 {
-  int xs,xm,xe,ys,ym,ye,zs,zm,ze;
+  int i,ict,flag,size,*ranks,*owners;
+  MPI_Group group, subgroup;
 
   PetscValidHeaderSpecific(da,DA_COOKIE);
-  ierr = DAGetCorners(da,&xs,&xm,&ys,&ym,&zs,&zm); CHKERRQ(ierr);
+  flag = 0; 
+  MPI_Comm_size(da->comm,&size);
   if (dir == DA_Z) {
     if (da->dim < 3) SETERRQ(1,"DAGetProcessorSubset:DA_Z invalid for DA dim < 3");
-    ze = zs+zm;
-    if (gp >= zs && gp < ze) 
+    if (gp < 0 || gp > da->P) SETERRQ(1,"DAGetProcessorSubset:invalid grid point");
+    if (gp >= da->zs && gp < da->ze) flag = 1;
   } else if (dir == DA_Y) {
     if (da->dim == 1) SETERRQ(1,"DAGetProcessorSubset:DA_Y invalid for DA dim = 1");
+    if (gp < 0 || gp > da->N) SETERRQ(1,"DAGetProcessorSubset:invalid grid point");
+    if (gp >= da->ys && gp < da->ye) flag = 1;
   } else if (dir == DA_X) {
+    if (gp < 0 || gp > da->M) SETERRQ(1,"DAGetProcessorSubset:invalid grid point");
+    if (gp >= da->xs && gp < da->xe) flag = 1;
   } else SETERRQ(1,"DAGetProcessorSubset:Invalid direction");
 
+  owners = (int *)PetscMalloc(2*size*sizeof(int)); CHKPTRQ(owners);
+  ranks = owners + size;
+  MPI_Allgather(&flag,1,MPI_INT,owners,1,MPI_INT,da->comm);
+  ict = 0;
+  PLogInfo(da,"DAGetProcessorSubset: dim=%d, direction=%d, procs: ",da->dim,(int)dir);
+  for (i=0; i<size; i++) {
+    if (owners[i]) {
+      ranks[ict] = i; ict++;
+      PLogInfo(da,"%d ",i);
+    }
+  }
+  PLogInfo(da,"\n");
+  MPI_Comm_group(da->comm,&group);
+  MPI_Group_incl(group,ict,ranks,&subgroup);
+  MPI_Comm_create(da->comm,subgroup,comm);
   return 0;
 } 
 
