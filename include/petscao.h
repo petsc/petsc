230e2716b432f9ebc47e9aeeccd6f33738fc49e8
Barry Smith <bsmith@mcs.anl.gov>
1996-06-25 13:10:32 -0600

    Initial revision

---
+++ include/petscao.h
@@ -0,0 +1,5 @@
+/* 
+   An application ordering is mapping between application-centric
+  ordering (the ordering that is "natural" to the application#endif
+/* $Id: is.h,v 1.25 1996/04/16 04:48:46 bsmith Exp bsmith $ */
+
