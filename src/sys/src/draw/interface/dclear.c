bf24795f69ed4c79d60e46bfa520087105758503
Barry Smith <bsmith@mcs.anl.gov>
1996-07-08 12:31:22 -0600

    *** empty log message ***

--- src/sys/src/draw/interface/dclear.c
+++ src/sys/src/draw/interface/dclear.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: dclear.c,v 1.3 1996/03/10 17:28:57 bsmith Exp bsmith $";
+static char vcid[] = "$Id: dclear.c,v 1.4 1996/03/19 21:28:06 bsmith Exp gropp $";
 #endif
 /*
        Provides the calling sequences for all the basic Draw routines.
@@ -21,3 +21,36 @@ int DrawClear(Draw draw)
   if (draw->ops.clear) return (*draw->ops.clear)(draw);
   return 0;
 }
+
+/*@
+  DrawBOP - Begins a new page or frame on the selected graphical device
+
+   Input Parameters:
+.  draw - the drawing context
+
+.keywords: draw, page, frame
+@*/
+int DrawBOP( Draw draw )
+{
+  PetscValidHeaderSpecific(draw,DRAW_COOKIE);
+  if (draw->type == NULLWINDOW) return 0;
+  if (draw->ops.beginpage) return (*draw->ops.beginpage)(draw);
+  return 0;
+}
+
+/*@
+  DrawEOP - Ends a page or frame on the selected graphical device
+
+   Input Parameters:
+.  draw - the drawing context
+
+.keywords: draw, page, frame
+@*/
+int DrawEOP( Draw draw )
+{
+  PetscValidHeaderSpecific(draw,DRAW_COOKIE);
+  if (draw->type == NULLWINDOW) return 0;
+  if (draw->ops.endpage) return (*draw->ops.endpage)(draw);
+  return 0;
+}
+
