b4b1f737547ea6880a60cccb6f94e27866a8295d
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-07-08 16:21:57 -0600

    *** empty log message ***

--- include/finclude/petsc.h
+++ include/finclude/petsc.h
@@ -1,5 +1,5 @@
 C
-C  $Id: petsc.h,v 1.22 1996/04/15 22:59:21 balay Exp balay $;
+C  $Id: petsc.h,v 1.23 1996/04/23 21:52:02 balay Exp bsmith $;
 C
 C  Base include file for Fortran use of the PETSc package
 C
@@ -11,17 +11,19 @@ C
 C     Flags
 C
       integer   PETSC_TRUE, PETSC_FALSE, PETSC_DECIDE, PETSC_DEFAULT
-      integer   FP_TRAP_OFF, FP_TRAP_ON, FP_TRAP_ALWAYS
+      integer   PETSC_FP_TRAP_OFF, PETSC_FP_TRAP_ON, 
+      integer   PETSC_FP_TRAP_ALWAYS
 
       parameter (PETSC_TRUE = 1, PETSC_FALSE = 0, PETSC_DECIDE = -1,
      *           PETSC_DEFAULT = -2)
-      parameter (FP_TRAP_OFF = 0, FP_TRAP_ON = 1, FP_TRAP_ALWAYS = 2)
+      parameter (PETSC_FP_TRAP_OFF = 0, PETSC_FP_TRAP_ON = 1, 
+     *           PETSC_FP_TRAP_ALWAYS = 2)
 
 C
 C     Default Viewers
 C
-      integer   STDOUT_VIEWER_SELF, STDERR_VIEWER_SELF,
-     *          STDOUT_VIEWER_WORLD
+      integer   VIEWER_STDOUT_SELF, VIEWER_STDERR_SELF,
+     *          VIEWER_STDOUT_WORLD
 
 C
 C     Fortran Null
@@ -30,8 +32,8 @@ C
       character*(80) PETSC_NULL_CHAR
 
       common   /petscfortran/  PETSC_NULL,
-     *         STDOUT_VIEWER_SELF,STDERR_VIEWER_SELF,
-     *         STDOUT_VIEWER_WORLD,PETSC_NULL_CHAR
+     *         VIEWER_STDOUT_SELF,VIEWER_STDERR_SELF,
+     *         VIEWER_STDOUT_WORLD,PETSC_NULL_CHAR
 C
 C     Macro for templating between real and complex
 C
