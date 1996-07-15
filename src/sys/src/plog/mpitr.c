4630a3fe7c2d3e794dc1708a2a0e55f0200acf87
Barry Smith <bsmith@mcs.anl.gov>
1996-07-14 19:36:13 -0600

    Initial revision

---
+++ src/sys/src/plog/mpitr.c
@@ -0,0 +1,12 @@
+
+/*
+    Code for tracing mistakes in MPI usage. For example, sends that are never received,
+  nonblocking messages that are not correctly waited for, etc.
+*/
+
+int PetscMPIDump(FILE *fd)
+{
+   
+
+  return 0;
+}
