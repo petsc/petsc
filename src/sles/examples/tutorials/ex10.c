715ddd977d7968e0cafbad8a19d7d00743bd33f0
Barry Smith <bsmith@mcs.anl.gov>
1996-07-23 14:12:23 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex10.c
+++ src/sles/examples/tutorials/ex10.c
@@ -1,13 +1,12 @@
 
 #ifndef lint
-static char vcid[] = "$Id: ex6.c,v 1.38 1996/05/03 19:27:27 bsmith Exp bsmith $";
+static char vcid[] = "$Id: ex21.c,v 1.1 1996/05/07 03:46:25 bsmith Exp bsmith $";
 #endif
 
 static char help[] = 
 "Reads a PETSc matrix and vector from a file and solves a linear system.\n\
-Input arguments are:\n\
-  -f <input_file> : file to load.  For a 5X5 example of the 5-pt. stencil,\n\
-                    use the file petsc/src/mat/examples/matbinary.ex\n\n";
+This version preloads a small matrix and solves with that, then loads \n\
+another matrix and solves with that.\n";
 
 #include "draw.h"
 #include "mat.h"
@@ -17,14 +16,14 @@ Input arguments are:\n\
 
 int main(int argc,char **args)
 {
-  int        ierr, its, set, flg;
+  int        ierr, its, set, flg, i;
   double     norm,tsetup,tsolve;
   Scalar     zero = 0.0, none = -1.0;
   Vec        x, b, u;
   Mat        A;
   MatType    mtype;
   SLES       sles;
-  char       file[128];
+  char       file[2][128];
   Viewer     fd;
   PetscTruth table = PETSC_FALSE;
 
@@ -38,101 +37,103 @@ int main(int argc,char **args)
 #else
 
   /* Read matrix and RHS */
-  ierr = OptionsGetString(PETSC_NULL,"-f",file,127,&flg); CHKERRA(ierr);
-  if (!flg) SETERRA(1,"Must indicate binary file with the -f option");
-  ierr = ViewerFileOpenBinary(MPI_COMM_WORLD,file,BINARY_RDONLY,&fd);CHKERRA(ierr);
-  ierr = MatGetTypeFromOptions(MPI_COMM_WORLD,0,&mtype,&set); CHKERRQ(ierr);
-  ierr = MatLoad(fd,mtype,&A); CHKERRA(ierr);
-  ierr = VecLoad(fd,&b); CHKERRA(ierr);
-  ierr = ViewerDestroy(fd); CHKERRA(ierr);
+  ierr = OptionsGetString(PETSC_NULL,"-f0",file[0],127,&flg); CHKERRA(ierr);
+  if (!flg) SETERRA(1,"Must indicate binary file with the -f0 option");
+  ierr = OptionsGetString(PETSC_NULL,"-f1",file[1],127,&flg); CHKERRA(ierr);
+  if (!flg) SETERRA(1,"Must indicate binary file with the -f1 option");
 
-  /* 
+  for ( i=0; i<2; i++ ) {
+
+    PLogStagePush(3*i);
+    ierr = ViewerFileOpenBinary(MPI_COMM_WORLD,file[i],BINARY_RDONLY,&fd);
+         CHKERRA(ierr);
+    ierr = MatGetTypeFromOptions(MPI_COMM_WORLD,0,&mtype,&set); CHKERRQ(ierr);
+    ierr = MatLoad(fd,mtype,&A); CHKERRA(ierr);
+    ierr = VecLoad(fd,&b); CHKERRA(ierr);
+    ierr = ViewerDestroy(fd); CHKERRA(ierr);
+
+    /* 
      If the load matrix is larger then the vector, due to being padded 
      to match the blocksize then create a new padded vector
-  */
-  { 
-    int m,n,mvec;
-    ierr = MatGetSize(A,&m,&n); CHKERRA(ierr);
-    ierr = VecGetSize(b,&mvec); CHKERRA(ierr);
-    if (m > mvec) {
+    */
+    /* 
+     If the load matrix is larger then the vector, due to being padded 
+     to match the blocksize then create a new padded vector
+    */
+    { 
+      int    m,n,j,mvec,start,end,index;
       Vec    tmp;
-      Scalar *bold,*bnew;
+      Scalar *bold;
       /* create a new vector b by padding the old one */
-      ierr = VecCreate(MPI_COMM_WORLD,m,&tmp); CHKERRA(ierr);
-      ierr = VecGetArray(tmp,&bnew); CHKERRA(ierr);
+      ierr = MatGetLocalSize(A,&m,&n); CHKERRA(ierr);
+      ierr = VecCreateMPI(MPI_COMM_WORLD,m,PETSC_DECIDE,&tmp);
+      ierr = VecGetOwnershipRange(b,&start,&end); CHKERRA(ierr);
+      ierr = VecGetLocalSize(b,&mvec); CHKERRA(ierr);
       ierr = VecGetArray(b,&bold); CHKERRA(ierr);
-      PetscMemcpy(bnew,bold,mvec*sizeof(Scalar)); CHKERRA(ierr);
-      VecDestroy(b);
+      for (j=0; j<mvec; j++ ) {
+        index = start+j;
+        ierr  = VecSetValues(tmp,1,&index,bold+j,INSERT_VALUES); CHKERRA(ierr);
+      }
+      ierr = VecRestoreArray(b,&bold); CHKERRA(ierr);
+      ierr = VecDestroy(b); CHKERRA(ierr);
+      ierr = VecAssemblyBegin(tmp); CHKERRA(ierr);
+      ierr = VecAssemblyEnd(tmp); CHKERRA(ierr);
       b = tmp;
     }
-  }
-  ierr = VecDuplicate(b,&x); CHKERRA(ierr);
-  ierr = VecDuplicate(b,&u); CHKERRA(ierr);
-
-  /* Do solve once to bring in all instruction pages */
-  ierr = OptionsHasName(PETSC_NULL,"-preload",&flg); CHKERRQ(ierr);
-  if (flg) {
+    ierr = VecDuplicate(b,&x); CHKERRA(ierr);
+    ierr = VecDuplicate(b,&u); CHKERRA(ierr);
     ierr = VecSet(&zero,x); CHKERRA(ierr);
+    PetscBarrier(A);
+
+    PLogStagePush(3*i+1);
+    tsetup = PetscGetTime();  
     ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
     ierr = SLESSetOperators(sles,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRA(ierr);
     ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
     ierr = SLESSetUp(sles,b,x); CHKERRA(ierr);
     ierr = SLESSetUpOnBlocks(sles); CHKERRA(ierr);
+    tsetup = PetscGetTime() - tsetup;
+    PLogStagePop();
+    PetscBarrier(A);
+
+
+    PLogStagePush(3*i+2);
+    tsolve = PetscGetTime();
     ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
-  }
+    tsolve = PetscGetTime() - tsolve;
+    PLogStagePop();
 
-  ierr = VecSet(&zero,x); CHKERRA(ierr);
-  PetscBarrier(A);
-
-  PLogStagePush(1);
-  tsetup = PetscGetTime();  
-  ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
-  ierr = SLESSetOperators(sles,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRA(ierr);
-  ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
-  ierr = SLESSetUp(sles,b,x); CHKERRA(ierr);
-  ierr = SLESSetUpOnBlocks(sles); CHKERRA(ierr);
-  tsetup = PetscGetTime() - tsetup;
-  PLogStagePop();
-  PetscBarrier(A);
-
-
-  PLogStagePush(2);
-  tsolve = PetscGetTime();
-  ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
-  tsolve = PetscGetTime() - tsolve;
-  PLogStagePop();
-
-  /* Show result */
-  ierr = MatMult(A,x,u);
-  ierr = VecAXPY(&none,b,u); CHKERRA(ierr);
-  ierr = VecNorm(u,NORM_2,&norm); CHKERRA(ierr);
-  /*  matrix PC   KSP   Options       its    residual setuptime solvetime  */
-  if (table) {
-    char   *matrixname, slesinfo[120];
-    Viewer viewer;
-    ViewerStringOpen(MPI_COMM_WORLD,slesinfo,120,&viewer);
-    SLESView(sles,viewer);
-    matrixname = PetscStrrchr(file,'/');
-    PetscPrintf(MPI_COMM_WORLD,"%-8.8s %3d %2.0e %2.1e %2.1e %2.1e %s \n",
+    /* Show result */
+    ierr = MatMult(A,x,u);
+    ierr = VecAXPY(&none,b,u); CHKERRA(ierr);
+    ierr = VecNorm(u,NORM_2,&norm); CHKERRA(ierr);
+    /*  matrix PC   KSP   Options       its    residual setuptime solvetime  */
+    if (table) {
+      char   *matrixname, slesinfo[120];
+      Viewer viewer;
+      ViewerStringOpen(MPI_COMM_WORLD,slesinfo,120,&viewer);
+      SLESView(sles,viewer);
+      matrixname = PetscStrrchr(file[i],'/');
+      PetscPrintf(MPI_COMM_WORLD,"%-8.8s %3d %2.0e %2.1e %2.1e %2.1e %s \n",
                 matrixname,its,norm,tsetup+tsolve,tsetup,tsolve,slesinfo);
-    ViewerDestroy(viewer);
-  } else {
-    PetscPrintf(MPI_COMM_WORLD,"Number of iterations = %3d\n",its);
-    if (norm < 1.e-10) {
-      PetscPrintf(MPI_COMM_WORLD,"Residual norm < 1.e-10\n");
+      ViewerDestroy(viewer);
     } else {
-      PetscPrintf(MPI_COMM_WORLD,"Residual norm = %10.4e\n",norm);
+      PetscPrintf(MPI_COMM_WORLD,"Number of iterations = %3d\n",its);
+      if (norm < 1.e-10) {
+        PetscPrintf(MPI_COMM_WORLD,"Residual norm < 1.e-10\n");
+      } else {
+        PetscPrintf(MPI_COMM_WORLD,"Residual norm = %10.4e\n",norm);
+      }
     }
+
+    /* Cleanup */
+    ierr = SLESDestroy(sles); CHKERRA(ierr);
+    ierr = VecDestroy(x); CHKERRA(ierr);
+    ierr = VecDestroy(b); CHKERRA(ierr);
+    ierr = VecDestroy(u); CHKERRA(ierr);
+    ierr = MatDestroy(A); CHKERRA(ierr);
+
   }
-
-  /* Cleanup */
-PetscTrValid(0,0);
-  ierr = SLESDestroy(sles); CHKERRA(ierr);
-  ierr = VecDestroy(x); CHKERRA(ierr);
-  ierr = VecDestroy(b); CHKERRA(ierr);
-  ierr = VecDestroy(u); CHKERRA(ierr);
-  ierr = MatDestroy(A); CHKERRA(ierr);
-
   PetscFinalize();
 #endif
   return 0;
