08f88d794341e947fb1b71294197f7320aec0fdc
Barry Smith <bsmith@mcs.anl.gov>
1996-08-13 20:56:20 -0600

    Initial revision

---
+++ src/vec/is/examples/tutorials/ex1.c
@@ -0,0 +1,33 @@
+#ifndef lint
+static char vcid[] = "$Id: ex1.c,v 1.13 1996/07/08 22:16:12 bsmith Exp $";
+#endif
+
+static char help[] = "Tests various IS routines\n\n";
+
+#include "petsc.h"
+#include "is.h"
+#include "sys.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int      n = 5, ierr,indices[5],rank;
+  IS       is;
+
+  PetscInitialize(&argc,&argv,(char*)0,help);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  /* create an index set */
+  indices[0] = rank + 1; 
+  indices[1] = rank + 2; 
+  indices[2] = rank + 3; 
+  indices[3] = rank + 4; 
+  indices[4] = rank + 5; 
+  ierr = ISCreateSeq(MPI_COMM_SELF,n,indices,&is); CHKERRA(ierr);
+
+  ierr = ISView(is,VIEWER_STDOUT_SELF); CHKERRA(ierr);
+  ierr = ISDestroy(is); CHKERRA(ierr);
+  PetscFinalize();
+  return 0;
+}
+ 
