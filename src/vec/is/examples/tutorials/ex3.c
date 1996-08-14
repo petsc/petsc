150502c2a35c2279dc252ee31cc122a98a421f4f
Barry Smith <bsmith@mcs.anl.gov>
1996-08-14 09:36:59 -0600

    Initial revision

---
+++ src/vec/is/examples/tutorials/ex3.c
@@ -0,0 +1,58 @@
+
+/*      "$Id: ex2.c,v 1.1 1996/08/14 15:25:09 bsmith Exp bsmith $"; */
+
+static char help[] = "Demonstrates creating a stride index set.\n\n";
+
+/*T
+    Concepts: Index set, indices, stride, accessing PETSc array from Fortran
+    Routines: ISCreateStrideSeq(), ISDestroy(), ISView()
+    Routines: ISGetIndices(), ISRestoreIndices()
+    
+      Creates an index set based on a stride. Views that index set
+  and then destroys it.
+
+
+T*/
+
+/*
+  Include is.h so we can use PETSc IS objects. Note that this automatically 
+  includes petsc.h.
+*/
+
+#include "is.h"
+
+int main(int argc,char **argv)
+{
+  int i, n, ierr,  *indices, first, step;
+  IS  set;
+
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);
+      
+  n     = 10;
+  first = 3;
+  step  = 2;
+
+  /*
+    Create stride index set, starting at 3 with a stride of 2
+    Note each processor is generating its own index set 
+    (in this case they are all identical)
+  */
+  ierr = ISCreateStrideSeq(MPI_COMM_SELF,n,first,step,&set); CHKERRA(ierr);
+  ierr = ISView(set,VIEWER_STDOUT_SELF); CHKERRA(ierr);
+
+  /*
+    Extract indices from set.
+  */
+  ierr = ISGetIndices(set,&indices); CHKERRA(ierr);
+  printf("Printing indices directly");
+  for (i=0; i<n; i++) {
+    printf("%d\n",indices[i]);
+  }
+
+  ierr = ISRestoreIndices(set,&indices); CHKERRA(ierr);
+  ierr = ISDestroy(set); CHKERRA(ierr);
+  PetscFinalize();
+  return 0;
+}
+
+
