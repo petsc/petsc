74a5887e4f3e35e9ccc6a17e3bb5f155c5ccf80e
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-08-25 14:45:00 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex7.c
+++ src/sles/examples/tutorials/ex7.c
@@ -1,15 +1,25 @@
 #ifndef lint
-static char vcid[] = "$Id: ex5.c,v 1.22 1996/07/08 22:20:55 bsmith Exp $";
+static char vcid[] = "$Id: ex5.c,v 1.23 1996/08/24 19:28:12 curfman Exp curfman $";
 #endif
 
 static char help[] = "Illustrates use of the block Jacobi preconditioner for\n\
 solving a linear system in parallel with SLES.  The code indicates the\n\
-procedure for using different linear solvers on the individual blocks.\n\n";
+procedures for setting the particular block sizes and for using different\n\
+linear solvers on the individual blocks.\n\n";
+
+/*
+   Note:  This example focuses on ways to customize the block Jacobi
+   preconditioner. See ex1.c and ex2.c for more detailed comments on
+   the basic usage of SLES (including working with matrices and vectors).
+ */
 
 /*T
-   Concepts: SLES; solving linear equations
-   Routines: SLESCreate(); SLESSetOperators(); SLESSetFromOptions()
-   Routines: SLESSolve(); SLESView()
+   Concepts: SLES; solving linear equations; block Jacobi preconditioner
+   Routines: SLESCreate(); SLESSetOperators(); SLESSetFromOptions();
+   Routines: SLESGetPC(); SLESGetKSP(); SLESSolve(); SLESView();
+   Routines: PCSetType(); PCGetType();
+   Routines: PCBJacobiSetTotalBlocks(); PCBJacobiGetSubSLES();
+   Routines: KSPSetType(); KSPSetTolerances();
    Processors: n
 T*/
 
@@ -35,18 +45,20 @@ int main(int argc,char **args)
   KSP     subksp;       /* KSP context for subdomain */
   PCType  pctype;       /* preconditioning technique */
   double  norm;         /* norm of solution error */
-  int       i, j, I, J, ierr, m = 3, n = 2;
-  int       rank, size, its, nlocal, first, Istart, Iend,flg;
-  Scalar    v, zero = 0.0, one = 1.0, none = -1.0;
+  int       i, j, I, J, ierr, *blks, m = 8, n;
+  int       rank, size, its, nlocal, first, Istart, Iend, flg;
+  Scalar    v, one = 1.0, none = -1.0;
 
   PetscInitialize(&argc,&args,(char *)0,help);
   ierr = OptionsGetInt(PETSC_NULL,"-m",&m,&flg); CHKERRA(ierr);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&size);  n = 2*size;
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  n = m+2;
 
-  /* Create and assemble matrix */
-  ierr = MatCreateMPIAIJ(MPI_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,m*n,m*n,
-         0,PETSC_NULL,0,PETSC_NULL,&A); CHKERRA(ierr);
+  /* 
+     Create and assemble parallel matrix
+  */
+  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&A); CHKERRA(ierr);
   ierr = MatGetOwnershipRange(A,&Istart,&Iend); CHKERRA(ierr);
   for ( I=Istart; I<Iend; I++ ) { 
     v = -1.0; i = I/n; j = I - i*n;  
@@ -59,54 +71,126 @@ int main(int argc,char **args)
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
 
-  /* Create vectors for exact solution, approx solution, and RHS */
-  ierr = VecCreateMPI(MPI_COMM_WORLD,PETSC_DECIDE,m*n,&u); CHKERRA(ierr);
+  /*
+     Create parallel vectors
+  */
+  ierr = VecCreate(MPI_COMM_WORLD,m*n,&u); CHKERRA(ierr);
   ierr = VecDuplicate(u,&b); CHKERRA(ierr);
   ierr = VecDuplicate(b,&x); CHKERRA(ierr);
+
+  /*
+     Set exact solution; then compute right-hand-side vector.
+  */
   ierr = VecSet(&one,u); CHKERRA(ierr);
-  ierr = VecSet(&zero,x); CHKERRA(ierr);
   ierr = MatMult(A,u,b); CHKERRA(ierr);
 
-  /* Create SLES context and set operators */
+  /*
+     Create linear solver context
+  */
   ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
-  ierr = SLESSetOperators(sles,A,A,DIFFERENT_NONZERO_PATTERN);
-         CHKERRA(ierr);
 
-  /* Set default preconditioner */
+  /*
+     Set default preconditioner for thie program to be block Jacobi.
+     This choice can be overridden at runtime with the option
+        -pc_type <type>
+  */
   ierr = SLESGetPC(sles,&pc); CHKERRA(ierr);
   ierr = PCSetType(pc,PCBJACOBI); CHKERRA(ierr);
 
-  /* Set options */
+  /* 
+     Call PCBJacobiSetTotalBlocks() to set individually the size of
+     each block in the preconditioner.  This could also be done with
+     the runtime option
+         -pc_bjacobi_blocks <blocks>
+     Also, see the command PCBJacobiSetLocalBlocks() to set the
+     local blocks.
+  */
+  blks = (int *) PetscMalloc( m*sizeof(int) ); CHKPTRA(blks);
+  for ( i=0; i<m; i++ ) blks[i] = n;
+  ierr = PCBJacobiSetTotalBlocks(pc,m,blks);
+  PetscFree(blks); 
+
+  /* 
+     Set operators. Here the matrix that defines the linear system
+     also serves as the preconditioning matrix.
+  */
+  ierr = SLESSetOperators(sles,A,A,DIFFERENT_NONZERO_PATTERN);
+         CHKERRA(ierr);
+
+  /* 
+    Set runtime options
+  */
   ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
 
-  /* Set local solvers for Block Jacobi method.  This code is intended as
-     a simple illustration of setting different linear solvers for the
-     individual blocks.  These choices are obviously not recommended for
-     solving this particular problem. */
+  /* 
+     Set local solvers for the block Jacobi method.  This code is intended
+     to be a simple illustration of setting different linear solvers for
+     the individual blocks.  These choices are obviously not recommended
+     for solving this particular problem.
+  */
   ierr = PCGetType(pc,&pctype,PETSC_NULL); CHKERRA(ierr);
   if (pctype == PCBJACOBI) {
-    /* Note that SLESSetUp() MUST be called before PCBJacobiGetSubSLES(). */
+    /* 
+       Call SLESSetUp() to set the block Jacobi data structures (including
+       creation of an internal SLES context for each block).
+
+       Note: SLESSetUp() MUST be called before PCBJacobiGetSubSLES().
+    */
     ierr = SLESSetUp(sles,x,b); CHKERRA(ierr);
+
+    /*
+       Extract the array of SLES contexts for the local blocks
+    */
     ierr = PCBJacobiGetSubSLES(pc,&nlocal,&first,&subsles); CHKERRA(ierr);
-    ierr = SLESGetPC(subsles[0],&subpc); CHKERRA(ierr);
-    ierr = SLESGetKSP(subsles[0],&subksp); CHKERRA(ierr);
-    if (rank == 0) {
-      ierr = PCSetType(subpc,PCILU); CHKERRA(ierr);
-      ierr = KSPSetTolerances(subksp,1.e-6,PETSC_DEFAULT,PETSC_DEFAULT,
-             PETSC_DEFAULT); CHKERRA(ierr);
-    } else {
-      ierr = PCSetType(subpc,PCJACOBI); CHKERRA(ierr);
-      ierr = KSPSetTolerances(subksp,1.e-7,PETSC_DEFAULT,PETSC_DEFAULT,
-             PETSC_DEFAULT); CHKERRA(ierr);
+
+    /*
+       Loop over the local blocks, setting various SLES options
+       for each block.  Note each block's SLES context is completely
+       independent of the others, and the full range of uniprocessor
+       SLES options is available for each block.
+
+       Related runtime options:
+       To set the same solver options on all blocks, use the prefix
+       -sub before the usual PC and KSP options, e.g.,
+          -sub_pc_type <pc> -sub_ksp_type <ksp> -sub_ksp_rtol 1.e-4
+    */
+    for (i=0; i<nlocal; i++) {
+      ierr = SLESGetPC(subsles[i],&subpc); CHKERRA(ierr);
+      ierr = SLESGetKSP(subsles[i],&subksp); CHKERRA(ierr);
+      if (rank == 0) {
+        if (i%2) {
+          ierr = PCSetType(subpc,PCILU); CHKERRA(ierr);
+        } else {
+          ierr = PCSetType(subpc,PCNONE); CHKERRA(ierr);
+          ierr = KSPSetType(subksp,KSPBCGS); CHKERRA(ierr);
+          ierr = KSPSetTolerances(subksp,1.e-6,PETSC_DEFAULT,PETSC_DEFAULT,
+                 PETSC_DEFAULT); CHKERRA(ierr);
+        }
+      } else {
+        ierr = PCSetType(subpc,PCJACOBI); CHKERRA(ierr);
+        ierr = KSPSetType(subksp,KSPGMRES); CHKERRA(ierr);
+        ierr = KSPSetTolerances(subksp,1.e-7,PETSC_DEFAULT,PETSC_DEFAULT,
+               PETSC_DEFAULT); CHKERRA(ierr);
+      }
     }
   }
+
+  /*
+     Solve the linear system
+  */
   ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
+
+  /*
+     View info about the solver
+  */
   ierr = OptionsHasName(PETSC_NULL,"-noslesview",&flg); CHKERRA(ierr);
   if (!flg) {
     ierr = SLESView(sles,VIEWER_STDOUT_WORLD); CHKERRA(ierr);
   }
 
-  /* Check the error */
+  /*
+     Check the error
+  */
   ierr = VecAXPY(&none,u,x); CHKERRA(ierr);
   ierr = VecNorm(x,NORM_2,&norm); CHKERRA(ierr);
   if (norm > 1.e-12)
@@ -114,7 +198,10 @@ int main(int argc,char **args)
   else 
     PetscPrintf(MPI_COMM_WORLD,"Norm of error < 1.e-12 Iterations %d\n",its);
 
-  /* Destroy work space */
+  /* 
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
   ierr = SLESDestroy(sles); CHKERRA(ierr);
   ierr = VecDestroy(u); CHKERRA(ierr);  ierr = VecDestroy(x); CHKERRA(ierr);
   ierr = VecDestroy(b); CHKERRA(ierr);  ierr = MatDestroy(A); CHKERRA(ierr);
