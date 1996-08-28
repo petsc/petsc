78e0af97ee0c6ef9b674f652db4622752ac6aaf7
Barry Smith <bsmith@mcs.anl.gov>
1996-08-28 12:24:53 -0600

    Initial revision

---
+++ src/mat/color/seq.c
@@ -0,0 +1,168 @@
+/* seq.f -- translated by f2c (version of 25 March 1992  12:58:56).
+   You must link the resulting object file with the libraries:
+	-lF77 -lI77 -lm -lc   (in that order)
+*/
+
+#include <f2c.h>
+
+/* Subroutine */ int seq_(n, indrow, jpntr, indcol, ipntr, list, ngrp, maxgrp,
+	 iwa)
+integer *n, *indrow, *jpntr, *indcol, *ipntr, *list, *ngrp, *maxgrp, *iwa;
+{
+    /* System generated locals */
+    integer i__1, i__2, i__3;
+
+    /* Local variables */
+    static integer jcol, j, ic, ip, jp, ir;
+
+/*     ********** */
+
+/*     subroutine seq */
+
+/*     Given the sparsity pattern of an m by n matrix A, this */
+/*     subroutine determines a consistent partition of the */
+/*     columns of A by a sequential algorithm. */
+
+/*     A consistent partition is defined in terms of the loopless */
+/*     graph G with vertices a(j), j = 1,2,...,n where a(j) is the */
+/*     j-th column of A and with edge (a(i),a(j)) if and only if */
+/*     columns i and j have a non-zero in the same row position. */
+
+/*     A partition of the columns of A into groups is consistent */
+/*     if the columns in any group are not adjacent in the graph G. */
+/*     In graph-theory terminology, a consistent partition of the */
+/*     columns of A corresponds to a coloring of the graph G. */
+
+/*     The subroutine examines the columns in the order specified */
+/*     by the array list, and assigns the current column to the */
+/*     group with the smallest possible number. */
+
+/*     Note that the value of m is not needed by seq and is */
+/*     therefore not present in the subroutine statement. */
+
+/*     The subroutine statement is */
+
+/*       subroutine seq(n,indrow,jpntr,indcol,ipntr,list,ngrp,maxgrp, */
+/*                      iwa) */
+
+/*     where */
+
+/*       n is a positive integer input variable set to the number */
+/*         of columns of A. */
+
+/*       indrow is an integer input array which contains the row */
+/*         indices for the non-zeroes in the matrix A. */
+
+/*       jpntr is an integer input array of length n + 1 which */
+/*         specifies the locations of the row indices in indrow. */
+/*         The row indices for column j are */
+
+/*               indrow(k), k = jpntr(j),...,jpntr(j+1)-1. */
+
+/*         Note that jpntr(n+1)-1 is then the number of non-zero */
+/*         elements of the matrix A. */
+
+/*       indcol is an integer input array which contains the */
+/*         column indices for the non-zeroes in the matrix A. */
+
+/*       ipntr is an integer input array of length m + 1 which */
+/*         specifies the locations of the column indices in indcol. */
+/*         The column indices for row i are */
+
+/*               indcol(k), k = ipntr(i),...,ipntr(i+1)-1. */
+
+/*         Note that ipntr(m+1)-1 is then the number of non-zero */
+/*         elements of the matrix A. */
+
+/*       list is an integer input array of length n which specifies */
+/*         the order to be used by the sequential algorithm. */
+/*         The j-th column in this order is list(j). */
+
+/*       ngrp is an integer output array of length n which specifies */
+/*         the partition of the columns of A. Column jcol belongs */
+/*         to group ngrp(jcol). */
+
+/*       maxgrp is an integer output variable which specifies the */
+/*         number of groups in the partition of the columns of A. */
+
+/*       iwa is an integer work array of length n. */
+
+/*     Argonne National Laboratory. MINPACK Project. July 1983. */
+/*     Thomas F. Coleman, Burton S. Garbow, Jorge J. More' */
+
+/*     ********** */
+
+/*     Initialization block. */
+
+    /* Parameter adjustments */
+    --iwa;
+    --ngrp;
+    --list;
+    --ipntr;
+    --indcol;
+    --jpntr;
+    --indrow;
+
+    /* Function Body */
+    *maxgrp = 0;
+    i__1 = *n;
+    for (jp = 1; jp <= i__1; ++jp) {
+	ngrp[jp] = *n;
+	iwa[jp] = 0;
+/* L10: */
+    }
+
+/*     Beginning of iteration loop. */
+
+    i__1 = *n;
+    for (j = 1; j <= i__1; ++j) {
+	jcol = list[j];
+
+/*        Find all columns adjacent to column jcol. */
+
+/*        Determine all positions (ir,jcol) which correspond */
+/*        to non-zeroes in the matrix. */
+
+	i__2 = jpntr[jcol + 1] - 1;
+	for (jp = jpntr[jcol]; jp <= i__2; ++jp) {
+	    ir = indrow[jp];
+
+/*           For each row ir, determine all positions (ir,ic) */
+/*           which correspond to non-zeroes in the matrix. */
+
+	    i__3 = ipntr[ir + 1] - 1;
+	    for (ip = ipntr[ir]; ip <= i__3; ++ip) {
+		ic = indcol[ip];
+
+/*              Array iwa marks the group numbers of the */
+/*              columns which are adjacent to column jcol. */
+
+		iwa[ngrp[ic]] = j;
+/* L20: */
+	    }
+/* L30: */
+	}
+
+/*        Assign the smallest un-marked group number to jcol. */
+
+	i__2 = *maxgrp;
+	for (jp = 1; jp <= i__2; ++jp) {
+	    if (iwa[jp] != j) {
+		goto L50;
+	    }
+/* L40: */
+	}
+	++(*maxgrp);
+L50:
+	ngrp[jcol] = jp;
+/* L60: */
+    }
+
+/*        End of iteration loop. */
+
+    return 0;
+
+/*     Last card of subroutine seq. */
+
+} /* seq_ */
+
---
+++ src/mat/color/setr.c
@@ -0,0 +1,121 @@
+/* setr.f -- translated by f2c (version of 25 March 1992  12:58:56).
+   You must link the resulting object file with the libraries:
+	-lF77 -lI77 -lm -lc   (in that order)
+*/
+
+#include <f2c.h>
+
+/* Subroutine */ int setr_(m, n, indrow, jpntr, indcol, ipntr, iwa)
+integer *m, *n, *indrow, *jpntr, *indcol, *ipntr, *iwa;
+{
+    /* System generated locals */
+    integer i__1, i__2;
+
+    /* Local variables */
+    static integer jcol, jp, ir;
+
+/*     ********** */
+
+/*     subroutine setr */
+
+/*     Given a column-oriented definition of the sparsity pattern */
+/*     of an m by n matrix A, this subroutine determines a */
+/*     row-oriented definition of the sparsity pattern of A. */
+
+/*     On input the column-oriented definition is specified by */
+/*     the arrays indrow and jpntr. On output the row-oriented */
+/*     definition is specified by the arrays indcol and ipntr. */
+
+/*     The subroutine statement is */
+
+/*       subroutine setr(m,n,indrow,jpntr,indcol,ipntr,iwa) */
+
+/*     where */
+
+/*       m is a positive integer input variable set to the number */
+/*         of rows of A. */
+
+/*       n is a positive integer input variable set to the number */
+/*         of columns of A. */
+
+/*       indrow is an integer input array which contains the row */
+/*         indices for the non-zeroes in the matrix A. */
+
+/*       jpntr is an integer input array of length n + 1 which */
+/*         specifies the locations of the row indices in indrow. */
+/*         The row indices for column j are */
+
+/*               indrow(k), k = jpntr(j),...,jpntr(j+1)-1. */
+
+/*         Note that jpntr(n+1)-1 is then the number of non-zero */
+/*         elements of the matrix A. */
+
+/*       indcol is an integer output array which contains the */
+/*         column indices for the non-zeroes in the matrix A. */
+
+/*       ipntr is an integer output array of length m + 1 which */
+/*         specifies the locations of the column indices in indcol. */
+/*         The column indices for row i are */
+
+/*               indcol(k), k = ipntr(i),...,ipntr(i+1)-1. */
+
+/*         Note that ipntr(1) is set to 1 and that ipntr(m+1)-1 is */
+/*         then the number of non-zero elements of the matrix A. */
+
+/*       iwa is an integer work array of length m. */
+
+/*     Argonne National Laboratory. MINPACK Project. July 1983. */
+/*     Thomas F. Coleman, Burton S. Garbow, Jorge J. More' */
+
+/*     ********** */
+
+/*     Store in array iwa the counts of non-zeroes in the rows. */
+
+    /* Parameter adjustments */
+    --iwa;
+    --ipntr;
+    --indcol;
+    --jpntr;
+    --indrow;
+
+    /* Function Body */
+    i__1 = *m;
+    for (ir = 1; ir <= i__1; ++ir) {
+	iwa[ir] = 0;
+/* L10: */
+    }
+    i__1 = jpntr[*n + 1] - 1;
+    for (jp = 1; jp <= i__1; ++jp) {
+	++iwa[indrow[jp]];
+/* L20: */
+    }
+
+/*     Set pointers to the start of the rows in indcol. */
+
+    ipntr[1] = 1;
+    i__1 = *m;
+    for (ir = 1; ir <= i__1; ++ir) {
+	ipntr[ir + 1] = ipntr[ir] + iwa[ir];
+	iwa[ir] = ipntr[ir];
+/* L30: */
+    }
+
+/*     Fill indcol. */
+
+    i__1 = *n;
+    for (jcol = 1; jcol <= i__1; ++jcol) {
+	i__2 = jpntr[jcol + 1] - 1;
+	for (jp = jpntr[jcol]; jp <= i__2; ++jp) {
+	    ir = indrow[jp];
+	    indcol[iwa[ir]] = jcol;
+	    ++iwa[ir];
+/* L40: */
+	}
+/* L50: */
+    }
+    return 0;
+
+/*     Last card of subroutine setr. */
+
+} /* setr_ */
+
