562870f5173cae727d4ee7006538031f5f21c011
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-09-23 12:15:08 -0600

    *** empty log message ***

--- src/sys/src/objects/pcomm.c
+++ src/sys/src/objects/pcomm.c
@@ -1,16 +1,21 @@
 
 #ifndef lint
-static char vcid[] = "$Id: pcomm.c,v 1.1 1996/09/14 03:05:57 bsmith Exp $";
+static char vcid[] = "$Id: pcomm.c,v 1.1 1996/09/14 15:39:18 curfman Exp curfman $";
 #endif
 
 #include "petsc.h"        /*I    "petsc.h"   I*/
 
 /*@C 
-   PetscSetCommWorld - Sets a communicator that is PETSc's world communicator
-   (default is MPI_COMM_WORLD).  Must call BEFORE PETScInitialize().
+   PetscSetCommWorld - Sets a communicator to be PETSc's world communicator
+   (default is MPI_COMM_WORLD).  Must call BEFORE PetscInitialize().
 
    Input Parameter:
 .  comm - the communicator
+
+   Note:
+   This routine is intended for users who need to initialize PETSc on a
+   subset of processors within a larger job.  Thus, most users need not
+   call this routine.
 
 .keywords: set, communicator, world
 @*/
