1368df8d5d9c887f72cde35b72d89e6fd6f766b7
Lois Curfman McInnes <curfman@mcs.anl.gov>
1996-10-25 09:03:29 -0600

    *** empty log message ***

--- src/vec/examples/tutorials/ex1.c
+++ src/vec/examples/tutorials/ex1.c
@@ -1,43 +1,87 @@
 #ifndef lint
-static char vcid[] = "$Id: ex1.c,v 1.31 1996/03/19 21:23:15 bsmith Exp bsmith $";
+static char vcid[] = "$Id: ex1.c,v 1.32 1996/10/23 22:13:25 bsmith Exp curfman $";
 #endif
 
-static char help[] = "Tests various vector routines\n\n";
+static char help[] = "Demonstrates various vector routines\n\n";
 
-#include "petsc.h"
-#include "is.h"
+/*T
+   Concepts: Vectors^Using basic vector routines;
+   Routines: VecCreate(); VecDuplicate(); VecSet(); VecValid(); VecDot();
+   Routines: VecMDot(); VecScale(); VecNorm(); VecCopy(); VecAXPY(); VecAYPX();
+   Routines: VecWAXPY(); VecPointwiseMult(); VecPointwiseDivide(); VecSwap();
+   Routines: VecDestroy(); VecDestroyVecs();
+   Processors: n
+T*/
+
+/* 
+  Include "vec.h" so that we can use SLES solvers.  Note that this file
+  automatically includes:
+     petsc.h  - base PETSc routines   is.h     - index sets
+     sys.h    - system routines       viewer.h - viewers
+*/
+
 #include "vec.h"
-#include "sys.h"
 #include <math.h>
 
 int main(int argc,char **argv)
 {
-  int      n = 20, ierr,flg;
-  Scalar   one = 1.0, two = 2.0, three = 3.0, dots[3],dot;
-  double   norm,v;
-  Vec      x,y,w,*z;
+  Vec      x, y, w;               /* vectors */
+  Vec      *z;                    /* array of vectors */
+  double   norm, v;
+  int      n = 20, ierr, flg;
+  Scalar   one = 1.0, two = 2.0, three = 3.0, dots[3], dot;
 
   PetscInitialize(&argc,&argv,(char*)0,help);
   OptionsGetInt(PETSC_NULL,"-n",&n,&flg);
 
-  /* create a vector */
+  /* 
+     Create a vector, specifying only its global dimension.
+     When using VecCreate(), the vector format (currently parallel
+     or sequential) is determined at runtime.  Also, the parallel
+     partitioning of the vector is determined by PETSc at runtime.
+
+     Routines for creating particular vector types directly are:
+        VecCreateSeq() - uniprocessor vector
+        VecCreateMPI() - distributed vector, where the user can
+                         determine the parallel partitioning
+  */
   ierr = VecCreate(PETSC_COMM_WORLD,n,&x); CHKERRA(ierr);
+
+  /*
+     Duplicate some work vectors (of the same format and
+     partitioning as the initial vector).
+  */
   ierr = VecDuplicate(x,&y); CHKERRA(ierr);
   ierr = VecDuplicate(x,&w); CHKERRA(ierr);
+
+  /*
+     Duplicate more work vectors (of the same format and
+     partitioning as the initial vector).  Here we duplicate
+     an array of vectors, which is often more convenient than
+     duplicating individual ones.
+  */
   ierr = VecDuplicateVecs(x,3,&z); CHKERRA(ierr); 
+
+  /*
+     Set the vectors to entries to a constant value.
+  */
   ierr = VecSet(&one,x); CHKERRA(ierr);
   ierr = VecSet(&two,y); CHKERRA(ierr);
   ierr = VecSet(&one,z[0]); CHKERRA(ierr);
   ierr = VecSet(&two,z[1]); CHKERRA(ierr);
   ierr = VecSet(&three,z[2]); CHKERRA(ierr);
 
-  /* Test whether vector has been corrupted (just to demonstrate this
-     routine) not needed in most application codes. */
-  ierr = VecValid(x,(PetscTruth*)&flg); CHKERRA(ierr);
-  if (!flg) SETERRA(1,"Corrupted vector.");
-  
+  /*
+     Demonstrate various basic vector routines.
+  */
   ierr = VecDot(x,x,&dot); CHKERRA(ierr);
   ierr = VecMDot(3,x,z,dots); CHKERRA(ierr);
+
+  /* 
+     Note: If using a complex numbers version of PETSc, then
+     PETSC_COMPLEX is defined in the makefiles; otherwise,
+     (when using real numbers) it is undefined.
+  */
 #if defined(PETSC_COMPLEX)
   PetscPrintf(PETSC_COMM_WORLD,"Vector length %d\n", int (real(dot)));
   PetscPrintf(PETSC_COMM_WORLD,"Vector length %d %d %d\n",(int)real(dots[0]),
@@ -49,6 +93,7 @@ int main(int argc,char **argv)
 #endif
 
   PetscPrintf(PETSC_COMM_WORLD,"All other values should be near zero\n");
+
   ierr = VecScale(&two,x); CHKERRA(ierr);
   ierr = VecNorm(x,NORM_2,&norm); CHKERRA(ierr);
   v = norm-2.0*sqrt((double) n); if (v > -1.e-10 && v < 1.e-10) v = 0.0; 
@@ -91,12 +136,22 @@ int main(int argc,char **argv)
   ierr = VecNorm(w,NORM_2,&norm); CHKERRA(ierr);
   v = norm-9.0*sqrt((double) n); if (v > -1.e-10 && v < 1.e-10) v = 0.0; 
   PetscPrintf(PETSC_COMM_WORLD,"VecPointwiseDivide %g\n",v);
-  
+
+  /* 
+     Test whether vector has been corrupted (just to demonstrate this
+     routine) not needed in most application codes.
+  */
+  ierr = VecValid(x,(PetscTruth*)&flg); CHKERRA(ierr);
+  if (!flg) SETERRA(1,"Corrupted vector.");
+
+  /* 
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
   ierr = VecDestroy(x); CHKERRA(ierr);
   ierr = VecDestroy(y); CHKERRA(ierr);
   ierr = VecDestroy(w); CHKERRA(ierr);
   ierr = VecDestroyVecs(z,3); CHKERRA(ierr);
-
   PetscFinalize();
   return 0;
 }
