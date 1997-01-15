9bec2fe33154df00294c83deda8db39d54d27164
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-01-15 12:18:45 -0600

    *** empty log message ***

--- src/contrib/euler/user1.c
+++ src/contrib/euler/user1.c
@@ -1682,7 +1682,6 @@ int UserSetGrid(Euler *app)
 
   /* Create local mesh and free global mesh if using > 1 processor;
      otherwise, return. */
-  /*
   if (app->size > 1) {
     mx_l = (app->gxef01 - app->gxsf1 + 1); mx_g = app->ni1-1;
     my_l = (app->gyef01 - app->gysf1 + 1); my_g = app->nj1-1;
@@ -1713,6 +1712,5 @@ int UserSetGrid(Euler *app)
     app->yc = yt;
     app->zc = zt;
   }
-  */
   return 0;
 }
