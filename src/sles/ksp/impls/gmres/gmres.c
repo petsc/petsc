0e123687e78d42995cf6220ba274fede18c43289
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-02-06 17:00:14 -0600

    *** empty log message ***

--- src/sles/ksp/impls/gmres/gmres.c
+++ src/sles/ksp/impls/gmres/gmres.c
@@ -1,6 +1,6 @@
 
 #ifndef lint
-static char vcid[] = "$Id: gmres.c,v 1.77 1997/01/27 18:15:31 bsmith Exp bsmith $";
+static char vcid[] = "$Id: gmres.c,v 1.78 1997/02/06 22:53:11 bsmith Exp curfman $";
 #endif
 
 /*
@@ -218,7 +218,7 @@ int GMREScycle(int *  itcount, int itsSoFar,int restart,KSP ksp,int *converged )
 
   if (!restart) {
     rtol      = ksp->rtol * res_norm;
-    ksp->ttol = PetscMin(ksp->atol,rtol);
+    ksp->ttol = PetscMax(ksp->atol,rtol);
   }
   rtol      = ksp->ttol;
   gmres->it = (it - 1);
