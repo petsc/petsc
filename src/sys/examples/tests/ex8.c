95655b0586ceeee5e2b571bbcb215747f000cdea
Satish Balay <balay@mcs.anl.gov>
1997-02-11 09:32:14 -0600

    *** empty log message ***

--- src/sys/examples/tests/ex8.c
+++ src/sys/examples/tests/ex8.c
@@ -1,8 +1,12 @@
 #ifndef lint
-static char vcid[] = "$Id: ex1.c,v 1.1 1996/12/03 21:10:14 balay Exp $";
+static char vcid[] = "$Id: ex8.c,v 1.1 1997/02/07 23:25:00 bsmith Exp balay $";
 #endif
 
-static char *help = "";
+static char *help = "Tests the option -trmalloc_nan which initializes the memory \n\
+allocated by PetscMalloc() with NaNs\n\
+Use Options: -tr_malloc_nan -fp_trap\n\n";
+
+
 #include "petsc.h"
 int main(int argc,char **args)
 {
