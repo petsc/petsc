a913aac0043632d0f2596c414ff36d4c18ad2b59
Satish Balay <balay@mcs.anl.gov>
1997-02-21 11:30:05 -0600

    Initial revision

---
+++ src/sys/src/time/nt_time.c
@@ -0,0 +1,48 @@
+#ifndef lint
+static char vcid[] = "$Id: mtr.c,v 1.74 1997/02/13 00:22:48 balay Exp $";
+#endif
+
+#include <petsc.h>
+#if defined (PARCH_nt_gnu)
+#include <Windows32/Base.h>
+#include <Windows32/Defines.h>
+#include <Windows32/Structures.h>
+#include <Windows32/Functions.h>
+#define FACTOR   4294967296.0
+
+double PetscTime_nt_gnu()
+{
+  static int    flag = 1;
+  int           ierr;
+
+  LARGE_INTEGER StartTime,PerfFreq,CurTime; 
+  static double SecInTick=0.0;
+  
+  DWORD dwStartHigh, dwCurHigh;
+  double dTime, dHigh;
+  double time;
+  
+  
+  if (flag) {
+  ierr = QueryPerformanceCounter( &StartTime ); CHKERRQ(ierr);
+  ierr = QueryPerformanceFrequency( &PerfFreq ); CHKERRQ(ierr);
+  SecInTick = 1.0/((double)PerfFreq.HighPart*FACTOR+(double)PerfFreq.LowPart);
+  }		
+  
+  ierr        = QueryPerformanceCounter( &CurTime ); CHKERRQ(ierr);
+  dwCurHigh   = (DWORD)CurTime.HighPart;
+  dwStartHigh = (DWORD)StartTime.HighPart;
+  dHigh       = (signed)(dwCurHigh - dwStartHigh);
+
+  dTime = dHigh*(double)FACTOR + (double)CurTime.LowPart - (double)StartTime.LowPart;
+  time  = (double)SecInTick*dTime;
+
+  return time;
+}
+
+#else
+double PetscTime_nt_gnu()
+{
+  SETERRQ(1,0,"Wrong Architecture");
+}
+#endif
