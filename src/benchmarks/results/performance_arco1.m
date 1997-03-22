cfb8fec217421baccc4c45fb9b88c89a1d87aef3
Satish Balay <balay@mcs.anl.gov>
1997-03-22 15:40:27 -0600

    *** empty log message ***

--- src/benchmarks/results/performance_arco1.m
+++ src/benchmarks/results/performance_arco1.m
@@ -1,7 +1,7 @@
 
 figure (1)
 
-Y = [ 117 101 88 65 64  31 31 30 21 16 15 15 12 12 6 4 ];
+Y = [ 117 101 88 65 64 50 31 31  21 16 15 15 12 12 6 4 ];
 bar(Y);
 axis([0 17 0 150])
 ylabel('MFlops');
@@ -12,9 +12,9 @@ text(2,22,'SGI Origin 2000','Rotation',90)
 text(3,22,'IBM SP Superchip, 4 memory cards','Rotation',90)
 text(4,22,'IBM SP Superchip, 2 memory cards','Rotation',90)
 text(5,22,'IBM SP2','Rotation',90)
-text(6,22,'200 MH Pentium-Pro NT-MDS','Rotation',90)
-text(7,22,'IBM SP1','Rotation',90)
-text(8,22,'Cray T3E','Rotation',90)
+text(6,22,'Cray T3E','Rotation',90)
+text(7,22,'200 MH Pentium-Pro NT-MDS','Rotation',90)
+text(8,22,'IBM SP1','Rotation',90)
 text(9,22,'SGI Indigo 2','Rotation',90)
 text(10,22,'200 MH Pentium NT-Gnu','Rotation',90)
 text(11,22,'Cray T3D','Rotation',90)
@@ -27,7 +27,7 @@ text(16,22,'Paragon','Rotation',90)
 
 figure(2)
 
-Y = [ 140 115 109 73 70  35 27 36 23 18 17  14 14 17 7 6 ];
+Y = [ 140 115 109 73 70 69 35 27 23 18 17  14 14 17 7 6 ];
 bar(Y);
 axis([0 17 0 150])
 ylabel('MFlops');
@@ -38,9 +38,9 @@ text(2,22,'SGI Origin 2000','Rotation',90)
 text(3,22,'IBM SP Superchip, 4 memory cards','Rotation',90)
 text(4,22,'IBM SP Superchip, 2 memory cards','Rotation',90)
 text(5,22,'IBM SP2','Rotation',90)
-text(6,22,'200 MH Pentium-Pro NT-MDS','Rotation',90)
-text(7,22,'IBM SP1','Rotation',90)
-text(8,22,'Cray T3E','Rotation',90)
+text(6,22,'Cray T3E','Rotation',90)
+text(7,22,'200 MH Pentium-Pro NT-MDS','Rotation',90)
+text(8,22,'IBM SP1','Rotation',90)
 text(9,22,'SGI Indigo 2','Rotation',90)
 text(10,22,'200 MH Pentium NT-Gnu','Rotation',90)
 text(11,22,'Cray T3D','Rotation',90)
