518baff381c93ff21d0a4d7863bcee1be35202d6
Satish Balay <balay@mcs.anl.gov>
1997-03-31 14:21:11 -0600

    *** empty log message ***

--- src/mat/examples/tests/ex56.c
+++ src/mat/examples/tests/ex56.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: ex56.c,v 1.4 1997/03/13 22:06:41 balay Exp balay $";
+static char vcid[] = "$Id: ex56.c,v 1.5 1997/03/31 20:17:34 balay Exp balay $";
 #endif
 static char help[] = "Test the use of MatSetValuesBlocked for MatBAIJ";
 
@@ -10,28 +10,12 @@ int main(int argc,char **args)
   Mat         A;
   int         bs=3,m=4,i,j,val = 10,ierr,flg,size,rank,rstart;
   Scalar      x[6][9],y[3][3];
-  int         row[2],col[3],eval=9;
+  int         row[2],col[3],eval;
 
   PetscInitialize(&argc,&args,(char *)0,help);
 
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-
-  ierr = OptionsHasName(PETSC_NULL,"-ass_extern",&flg); CHKERRA(ierr);
-  if (flg && (size == 1))    rstart = m*((rank+1)%size);
-  else                       rstart = m*(rank);
-
-  row[0] =rstart+0;  row[1] =rstart+2;
-  col[0] =rstart+0;  col[1] =rstart+1;  col[2] =rstart+3;
-  for (i=0; i<6; i++) {
-    for (j =0; j< 9; j++ ) x[i][j] = (Scalar)val++;
-  }
-
-  /* 
-  for (i=0; i<3; i++) {
-    for (j =0; j<3 ; j++ ) x[i][j] = -1.0 ;
-  }
-  */
   
   if (size == 1) {
     ierr = MatCreateSeqBAIJ(MPI_COMM_SELF,bs,m*bs,m*bs,1,PETSC_NULL,&A); CHKERRA(ierr);
@@ -44,7 +28,20 @@ int main(int argc,char **args)
   if (flg) { 
     ierr = MatSetOption(A,MAT_COLUMN_ORIENTED); CHKERRA(ierr); 
     eval = 6;
+  } else {
+    eval = 9;
   }
+
+  ierr = OptionsHasName(PETSC_NULL,"-ass_extern",&flg); CHKERRA(ierr);
+  if (flg && (size == 1))    rstart = m*((rank+1)%size);
+  else                       rstart = m*(rank);
+
+  row[0] =rstart+0;  row[1] =rstart+2;
+  col[0] =rstart+0;  col[1] =rstart+1;  col[2] =rstart+3;
+  for (i=0; i<6; i++) {
+    for (j =0; j< 9; j++ ) x[i][j] = (Scalar)val++;
+  }
+
   ierr = MatSetValuesBlocked(A,2,row,3,col,&x[0][0],INSERT_VALUES); CHKERRQ(ierr);
 
   /* Do another MatSetValues to test the case when only one local block is specified */
