6e5c438c4415586f860ee92d63d388d18cd9a741
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-04-02 10:12:57 -0600

    Initial revision

---
+++ src/sles/examples/tutorials/ex16.c
@@ -0,0 +1,197 @@
+#ifndef lint
+static char vcid[] = "$Id: ex2.c,v 1.63 1997/03/14 03:17:41 curfman Exp $";
+#endif
+
+/* Usage:  mpirun ex16 [-help] [all PETSc options] */
+
+static char help[] = "Solves with SLES a sequence of linear systems that\n\
+have the same matrix but different right-hand-side vectors.\n\
+Input parameters include:\n\
+  -ntimes <ntimes>  : number of linear systems to solve\n\
+  -view_exact_sol   : write exact solution vector to stdout\n\
+  -m <mesh_x>       : number of mesh points in x-direction\n\
+  -n <mesh_n>       : number of mesh points in y-direction\n\n";
+
+/*T
+   Concepts: SLES^Repeatedly solving linear systems;
+   Concepts: SLES^Laplacian, 2d
+   Concepts: Laplacian, 2d
+   Routines: SLESCreate(); SLESSetOperators(); SLESSetFromOptions();
+   Routines: SLESSolve();
+   Processors: n
+T*/
+
+/* 
+  Include "sles.h" so that we can use SLES solvers.  Note that this file
+  automatically includes:
+     petsc.h  - base PETSc routines   vec.h - vectors
+     sys.h    - system routines       mat.h - matrices
+     is.h     - index sets            ksp.h - Krylov subspace methods
+     viewer.h - viewers               pc.h  - preconditioners
+*/
+#include "sles.h"
+#include <stdio.h>
+
+int main(int argc,char **args)
+{
+  Vec         x, b, u;  /* approx solution, RHS, exact solution */
+  Mat         A;        /* linear system matrix */
+  SLES        sles;     /* linear solver context */
+  double      norm;     /* norm of solution error */
+  int         ntimes, i, j, k, I, J, Istart, Iend, ierr;
+  int         m = 8, n = 7, its, flg;
+  Scalar      v, one = 1.0, neg_one = -1.0, rhs;
+
+  PetscInitialize(&argc,&args,(char *)0,help);
+  ierr = OptionsGetInt(PETSC_NULL,"-m",&m,&flg); CHKERRA(ierr);
+  ierr = OptionsGetInt(PETSC_NULL,"-n",&n,&flg); CHKERRA(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+         Compute the matrix for use in solving a series of
+         linear systems of the form, A x_i = b_i, for i=1,2,...
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /* 
+     Create parallel matrix, specifying only its global dimensions.
+     When using MatCreate(), the matrix format can be specified at
+     runtime. Also, the parallel partitioning of the matrix is
+     determined by PETSc at runtime.
+  */
+  ierr = MatCreate(MPI_COMM_WORLD,m*n,m*n,&A); CHKERRA(ierr);
+
+  /* 
+     Currently, all PETSc parallel matrix formats are partitioned by
+     contiguous chunks of rows across the processors.  Determine which
+     rows of the matrix are locally owned. 
+  */
+  ierr = MatGetOwnershipRange(A,&Istart,&Iend); CHKERRA(ierr);
+
+  /* 
+     Set matrix elements for the 2-D, five-point stencil in parallel.
+      - Each processor needs to insert only elements that it owns
+        locally (but any non-local elements will be sent to the
+        appropriate processor during matrix assembly). 
+      - Always specify global rows and columns of matrix entries.
+   */
+  for ( I=Istart; I<Iend; I++ ) { 
+    v = -1.0; i = I/n; j = I - i*n;  
+    if ( i>0 )   {J = I - n; MatSetValues(A,1,&I,1,&J,&v,INSERT_VALUES); CHKERRA(ierr);}
+    if ( i<m-1 ) {J = I + n; MatSetValues(A,1,&I,1,&J,&v,INSERT_VALUES); CHKERRA(ierr);}
+    if ( j>0 )   {J = I - 1; MatSetValues(A,1,&I,1,&J,&v,INSERT_VALUES); CHKERRA(ierr);}
+    if ( j<n-1 ) {J = I + 1; MatSetValues(A,1,&I,1,&J,&v,INSERT_VALUES); CHKERRA(ierr);}
+    v = 4.0; MatSetValues(A,1,&I,1,&I,&v,INSERT_VALUES);
+  }
+
+  /* 
+     Assemble matrix, using the 2-step process:
+       MatAssemblyBegin(), MatAssemblyEnd()
+     Computations can be done while messages are in transition
+     by placing code between these two statements.
+  */
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
+
+  /* 
+     Create parallel vectors.
+      - When using VecCreate(), we specify only the vector's global
+        dimension; the parallel partitioning is determined at runtime. 
+      - When solving a linear system, the vectors and matrices MUST
+        be partitioned accordingly.  PETSc automatically generates
+        appropriately partitioned matrices and vectors when MatCreate()
+        and VecCreate() are used with the same communicator. 
+      - Note: We form 1 vector from scratch and then duplicate as needed.
+  */
+  ierr = VecCreate(MPI_COMM_WORLD,m*n,&u); CHKERRA(ierr);
+  ierr = VecDuplicate(u,&b); CHKERRA(ierr); 
+  ierr = VecDuplicate(b,&x); CHKERRA(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the linear solver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Create linear solver context
+  */
+  ierr = SLESCreate(MPI_COMM_WORLD,&sles); CHKERRA(ierr);
+
+  /* 
+     Set operators. Here the matrix that defines the linear system
+     also serves as the preconditioning matrix.
+  */
+  ierr = SLESSetOperators(sles,A,A,DIFFERENT_NONZERO_PATTERN); CHKERRA(ierr);
+
+  /* 
+    Set runtime options, e.g.,
+        -ksp_type <type> -pc_type <type> -ksp_monitor -ksp_rtol <rtol>
+    These options will override those specified above as long as
+    SLESSetFromOptions() is called _after_ any other customization
+    routines.
+  */
+  ierr = SLESSetFromOptions(sles); CHKERRA(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+       Solve several linear systems of the form  A x_i = b_i
+       I.e., we retain the same matrix (A) for all systems, but
+       change the right-hand-side vector (b_i) at each step.
+
+       In this case, we simply call SLESSolve() multiple times.  The
+       preconditioner setup operations (e.g., factorization for ILU)
+       be done during the first call to SLESSolve() only; such operations
+       will NOT be repeated for successive solves.
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ntimes = 2;
+  ierr = OptionsGetInt(PETSC_NULL,"-ntimes",&ntimes,&flg); CHKERRA(ierr);
+  for (k=1; k<ntimes+1; k++) {
+
+    /* 
+       Set exact solution; then compute right-hand-side vector.  We use
+       an exact solution of a vector with all elements equal to 1.0*k.
+    */
+    rhs = one * k;
+    ierr = VecSet(&rhs,u); CHKERRA(ierr);
+    ierr = MatMult(A,u,b); CHKERRA(ierr);
+
+    /*
+       View the exact solution vector if desired
+    */
+    ierr = OptionsHasName(PETSC_NULL,"-view_exact_sol",&flg); CHKERRA(ierr);
+    if (flg) {ierr = VecView(u,VIEWER_STDOUT_WORLD); CHKERRA(ierr);}
+
+    ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr);
+
+    /* 
+       Check the error
+    */
+    ierr = VecAXPY(&neg_one,u,x); CHKERRA(ierr);
+    ierr = VecNorm(x,NORM_2,&norm); CHKERRA(ierr);
+
+    /*
+       Print convergence information.  PetscPrintf() produces a single 
+       print statement from all processes that share a communicator.
+    */
+    if (norm > 1.e-12)
+      PetscPrintf(MPI_COMM_WORLD,"System %d: Norm of error %g iterations %d\n",k,norm,its);
+    else 
+      PetscPrintf(MPI_COMM_WORLD,"System %d: Norm of error < 1.e-12 Iterations %d\n",k,its);
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /* 
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
+  ierr = SLESDestroy(sles); CHKERRA(ierr);
+  ierr = VecDestroy(u); CHKERRA(ierr);  ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = VecDestroy(b); CHKERRA(ierr);  ierr = MatDestroy(A); CHKERRA(ierr);
+
+  /*
+     Always call PetscFinalize() before exiting a program.  This routine
+       - finalizes the PETSc libraries as well as MPI
+       - provides summary and diagnostic information if certain runtime
+         options are chosen (e.g., -log_summary). 
+  */
+  PetscFinalize();
+  return 0;
+}
