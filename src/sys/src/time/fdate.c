898682cf19b3bab96143bcc0dfdc441d589e816b
Satish Balay <balay@mcs.anl.gov>
1997-04-03 13:55:37 -0600

    *** empty log message ***

--- src/sys/src/time/fdate.c
+++ src/sys/src/time/fdate.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: fdate.c,v 1.9 1997/02/27 00:39:01 balay Exp balay $";
+static char vcid[] = "$Id: fdate.c,v 1.10 1997/04/03 19:07:33 balay Exp balay $";
 #endif
 
 #include "src/sys/src/files.h"
@@ -10,9 +10,11 @@ static char vcid[] = "$Id: fdate.c,v 1.9 1997/02/27 00:39:01 balay Exp balay $";
      Some versions of the Gnu g++ compiler require a prototype for gettimeofday()
   on the IBM rs6000. Also CC on some IRIX64 machines
 
+#if defined(__cplusplus)
 extern "C" {
 extern int gettimeofday(struct timeval *, struct timezone *);
 }
+#endif
 */
    
 #undef __FUNC__  
