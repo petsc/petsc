664270b7f097c6e038a4c595c341197c8ce4107b
Satish Balay <balay@mcs.anl.gov>
1997-04-04 10:05:36 -0600

    Initial revision

---
+++ src/sles/examples/tests/scripts/ex10.asm
@@ -0,0 +1,19 @@
+#! /bin/csh
+
+foreach matrix (arco1)
+
+  foreach np (1 2 3 4)
+
+      foreach blocks (4 5 8 12) 
+
+        foreach overlap (0 1 2 5) 
+        echo "matrix $matrix np $np  blocks $blocks overlap $overlap"
+          mpirun -np $np ex10 -f0 /home/bsmith/petsc/src/mat/examples/matrices/$matrix -pc_type asm -mat_mpiaij -pc_asm_blocks $blocks -pc_asm_overlap $overlap
+
+      end
+
+    end
+
+  end
+
+end
