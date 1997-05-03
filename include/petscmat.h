319b3e883e6aba44e40fbfc8bcb74b795d103ab5
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-05-03 14:44:48 -0600

    *** empty log message ***

--- include/petscmat.h
+++ include/petscmat.h
@@ -1,4 +1,4 @@
-/* $Id: mat.h,v 1.129 1997/04/02 21:03:40 bsmith Exp balay $ */
+/* $Id: mat.h,v 1.130 1997/04/03 17:06:47 balay Exp curfman $ */
 /*
      Include file for the matrix component of PETSc
 */
@@ -55,7 +55,8 @@ typedef enum {MAT_ROW_ORIENTED=1,MAT_COLUMN_ORIENTED=2,MAT_ROWS_SORTED=4,
               MAT_YES_NEW_DIAGONALS,MAT_INODE_LIMIT_1,MAT_INODE_LIMIT_2,
               MAT_INODE_LIMIT_3,MAT_INODE_LIMIT_4,MAT_INODE_LIMIT_5,
               MAT_IGNORE_OFF_PROC_ENTRIES,MAT_ROWS_UNSORTED,
-              MAT_COLUMNS_UNSORTED,MAT_NEW_NONZERO_LOCATION_ERROR} MatOption;
+              MAT_COLUMNS_UNSORTED,MAT_NEW_NONZERO_LOCATION_ERROR
+              MAT_NEW_NONZERO_ALLOCTION_ERROR} MatOption;
 extern int MatSetOption(Mat,MatOption);
 extern int MatGetType(Mat,MatType*,char**);
 extern int MatGetTypeFromOptions(MPI_Comm,char*,MatType*,int*);
