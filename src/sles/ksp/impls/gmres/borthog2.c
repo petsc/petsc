a508541ef130a8bdfe233acf3cf0f7dd6d4d3233
Barry Smith <bsmith@mcs.anl.gov>
1997-06-11 19:23:49 -0600

    *** empty log message ***

--- src/sles/ksp/impls/gmres/borthog2.c
+++ src/sles/ksp/impls/gmres/borthog2.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char vcid[] = "$Id: borthog2.c,v 1.2 1997/01/27 18:15:31 bsmith Exp bsmith $";
+static char vcid[] = "$Id: borthog2.c,v 1.3 1997/03/09 17:57:21 bsmith Exp bsmith $";
 #endif
 /*
     Routines used for the orthogonalization of the Hessenberg matrix.
@@ -12,19 +12,17 @@ static char vcid[] = "$Id: borthog2.c,v 1.2 1997/01/27 18:15:31 bsmith Exp bsmit
 #include <math.h>
 
 /*
-  This version uses UNMODIFIED Gram-Schmidt.  It is NOT recommended, 
-  but it can give better performance when running in a parallel 
-  environment
+  This version uses UNMODIFIED Gram-Schmidt.  It is NOT always recommended, 
+  but it can give MUCH better performance then the default modified form,
+  when running in a parallel environment
 
-  Multiple applications of this can be used to provide a better 
-  orthogonalization (but be careful of the HH and HES values).
  */
 #undef __FUNC__  
 #define __FUNC__ "KSPGMRESUnmodifiedGramSchmidtOrthogonalization"
 int KSPGMRESUnmodifiedGramSchmidtOrthogonalization(KSP  ksp,int it )
 {
   KSP_GMRES *gmres = (KSP_GMRES *)(ksp->data);
-  int       j;
+  int       j,ierr;
   Scalar    *hh, *hes;
 
   PLogEventBegin(KSP_GMRESOrthogonalization,ksp,0,0,0);
@@ -36,16 +34,59 @@ int KSPGMRESUnmodifiedGramSchmidtOrthogonalization(KSP  ksp,int it )
    This is really a matrix-vector product, with the matrix stored
    as pointer to rows 
   */
-  VecMDot( it+1, VEC_VV(it+1), &(VEC_VV(0)), hes );
+  ierr = VecMDot( it+1, VEC_VV(it+1), &(VEC_VV(0)), hes ); CHKERRQ(ierr);
 
   /*
     This is really a matrix-vector product: 
         [h[0],h[1],...]*[ v[0]; v[1]; ...] subtracted from v[it+1].
   */
   for (j=0; j<=it; j++) hh[j] = -hes[j];
-  VecMAXPY(it+1, hh, VEC_VV(it+1),&VEC_VV(0) );
+  ierr = VecMAXPY(it+1, hh, VEC_VV(it+1),&VEC_VV(0) ); CHKERRQ(ierr);
   for (j=0; j<=it; j++) hh[j] = -hh[j];
   PLogEventEnd(KSP_GMRESOrthogonalization,ksp,0,0,0);
   return 0;
 }
+
+extern int VecMDot_Seq(int, Vec, Vec *, Scalar *);
+/*
+  This version uses UNMODIFIED Gram-Schmidt; it orthogonalizes just the 
+  part of the vectors on each processor and thus requires no parallel
+  communication.  THIS DOES NOT WORK, DO NOT USE.
+
+ */
+#undef __FUNC__  
+#define __FUNC__ "KSPGMRESUnmodifiedGramSchmidtOrthogonalization"
+int KSPGMRESUnmodifiedGramSchmidtOrthogonalizationLocal(KSP  ksp,int it )
+{
+  KSP_GMRES *gmres = (KSP_GMRES *)(ksp->data);
+  int       j,ierr;
+  Scalar    *hh, *hes;
+
+  PLogEventBegin(KSP_GMRESOrthogonalization,ksp,0,0,0);
+  /* update hessenberg matrix and do unmodified Gram-Schmidt */
+  hh  = HH(0,it);
+  hes = HES(0,it);
+
+  /* 
+   This is really a matrix-vector product, with the matrix stored
+   as pointer to rows 
+  */
+  ierr = VecMDot_Seq( it+1, VEC_VV(it+1), &(VEC_VV(0)), hes ); CHKERRQ(ierr);
+
+  /*
+    This is really a matrix-vector product: 
+        [h[0],h[1],...]*[ v[0]; v[1]; ...] subtracted from v[it+1].
+  */
+  for (j=0; j<=it; j++) hh[j] = -hes[j];
+  ierr = VecMAXPY(it+1, hh, VEC_VV(it+1),&VEC_VV(0) ); CHKERRQ(ierr);
+  for (j=0; j<=it; j++) hh[j] = -hh[j];
+  PLogEventEnd(KSP_GMRESOrthogonalization,ksp,0,0,0);
+  return 0;
+}
+
+
+
+
+
+
 
