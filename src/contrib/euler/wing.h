4b152809ccee308c53bd2c44033ced15132d9ac9
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-06-24 15:01:49 -0600

    *** empty log message ***

--- src/contrib/euler/wing.h
+++ src/contrib/euler/wing.h
@@ -1,6 +1,8 @@
 c
 c Parameters to define wing, set in the function julianne()
 c
-      COMMON /WING/ ITL,ITU,ILE,KTIP,itl1,itlp1,itup,ilep,ktipp
-      integer       ITL,ITU,ILE,KTIP,itl1,itlp1,itup,ilep,ktipp
+      COMMON /WING/ ITL,ITU,ILE,KTIP,itl1,itlp1,itlp
+      COMMON /WING/ itu1,itup1,itup,ktipp
+      integer       ITL,ITU,ILE,KTIP,itl1,itlp1,itlp
+      integer       itu1,itup1,itup,ktipp
 c
