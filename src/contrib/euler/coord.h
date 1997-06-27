8757868315e346f5d617cba417ddb1caa0ef011f
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-06-27 10:23:05 -0600

    *** empty log message ***

--- src/contrib/euler/coord.h
+++ src/contrib/euler/coord.h
@@ -1,12 +1,21 @@
 c
 c  Parallel array sizes, including ghost points, for the
 c  mesh coordinates.  Space is allocated in UserSetLocalMesh().
-c
-      Double x(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
-      Double y(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
-      Double z(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
 
+      Double x(cx1:cxn,cy1:cyn,cz1:czn)
+      Double y(cx1:cxn,cy1:cyn,cz1:czn)
+      Double z(cx1:cxn,cy1:cyn,cz1:czn)
+
+c  These parameters enable use of either the local or
+c  global mesh; they are set in the routine parsetup().
+c  For the parallel case, we clearly want to use only
+c  the local part, but currently some of the post-processing
+c  requires the global mesh.  This will eventually be upgraded.
 c
-c      COMMON /COORD/ X(NI,NJ,NK),Y(NI,NJ,NK),Z(NI,NJ,NK)
-cc      Double X(NI,NJ,NK),Y(NI,NJ,NK),Z(NI,NJ,NK)
-c      Double x,y,z
+c  local:
+c      Double x(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
+c      Double y(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
+c      Double z(gxsf1:gxef01,gysf1:gyef01,gzsf1:gzef01)
+c  global:
+c      Double x(ni,nj,nk),y(ni,nj,nk),z(ni,nj,nk)
+c
