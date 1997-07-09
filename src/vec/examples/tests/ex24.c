fce31c2438deb62819e71ca94707db22a47b6e77
Barry Smith <bsmith@mcs.anl.gov>
1997-07-09 10:32:28 -0600

    Initial revision

---
+++ src/vec/examples/tests/ex24.c
@@ -0,0 +1,61 @@
+#ifndef lint
+static char vcid[] = "$Id: ex5.c,v 1.36 1997/04/10 00:00:31 bsmith Exp $";
+#endif
+
+static char help[] = "Scatters from a parallel vector to a sequential vector.\n\
+This does case when we are merely selecting the local part of the\n\
+parallel vector.\n\n";
+
+#include "petsc.h"
+#include "is.h"
+#include "vec.h"
+#include "sys.h"
+#include <math.h>
+
+int main(int argc,char **argv)
+{
+  int           n = 5, ierr, size,rank,i;
+  Scalar        value;
+  Vec           x,y;
+  IS            is1,is2;
+  VecScatter    ctx = 0;
+
+  PetscInitialize(&argc,&argv,(char*)0,help);
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  /* create two vectors */
+  ierr = VecCreateMPI(MPI_COMM_WORLD,PETSC_DECIDE,size*n,&x); CHKERRA(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_SELF,n,&y); CHKERRA(ierr);
+
+  /* create two index sets */
+  ierr = ISCreateStride(PETSC_COMM_SELF,n,n*rank,1,&is1); CHKERRA(ierr);
+  ierr = ISCreateStride(PETSC_COMM_SELF,n,0,1,&is2); CHKERRA(ierr);
+
+  /* each processor inserts the entire vector */
+  /* this is redundant but tests assembly */
+  for ( i=0; i<n*size; i++ ) {
+    value = (Scalar) i;
+    ierr = VecSetValues(x,1,&i,&value,INSERT_VALUES); CHKERRA(ierr);
+  }
+  ierr = VecAssemblyBegin(x); CHKERRA(ierr);
+  ierr = VecAssemblyEnd(x); CHKERRA(ierr);
+  ierr = VecView(x,VIEWER_STDOUT_WORLD); CHKERRA(ierr);
+
+  ierr = VecScatterCreate(x,is1,y,is2,&ctx); CHKERRA(ierr);
+  ierr = VecScatterBegin(x,y,INSERT_VALUES,SCATTER_FORWARD,ctx); CHKERRA(ierr);
+  ierr = VecScatterEnd(x,y,INSERT_VALUES,SCATTER_FORWARD,ctx); CHKERRA(ierr);
+  ierr = VecScatterDestroy(ctx); CHKERRA(ierr);
+  
+  if (!rank)
+   {printf("----\n"); VecView(y,VIEWER_STDOUT_SELF); CHKERRA(ierr);}
+
+  ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = VecDestroy(y); CHKERRA(ierr);
+  ierr = ISDestroy(is1); CHKERRA(ierr);
+  ierr = ISDestroy(is2); CHKERRA(ierr);
+
+  PetscFinalize(); 
+  return 0;
+}
+ 
