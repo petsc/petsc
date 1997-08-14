9dd048a5da409d67fc2f096f3f6008e6930154aa
Barry Smith <bsmith@mcs.anl.gov>
1997-08-14 11:19:31 -0600

    Initial revision

---
+++ src/sys/examples/tests/ex11.c
@@ -0,0 +1,27 @@
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: ex3.c,v 1.10 1997/07/09 20:52:08 balay Exp $";
+#endif
+
+static char help[] = "Tests catching of floating point exceptions.\n\n";
+
+#include "petsc.h"
+#include <stdio.h>
+
+int CreateError(double x)
+{
+  x = 1.0/x;
+  PetscPrintf(PETSC_COMM_SELF,"x = %g\n",x);
+  return 0;
+}
+
+int main(int argc,char **argv)
+{
+  int ierr;
+  PetscInitialize(&argc,&argv,(char *)0,help);
+  PetscPrintf(PETSC_COMM_SELF,"This is a contrived example to test floating pointing\n");
+  PetscPrintf(PETSC_COMM_SELF,"It is not a true error.\n");
+  PetscPrintf(PETSC_COMM_SELF,"Run with -fp_trap to catch the floating point error\n");
+  ierr = CreateError(0.0); CHKERRA(ierr);
+  return 0;
+}
+ 
