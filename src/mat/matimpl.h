70fdb4e3ba656266a14150f54ae0c699784a65d7
Barry Smith <bsmith@mcs.anl.gov>
1997-09-03 15:43:26 -0600

    *** empty log message ***

--- src/mat/matimpl.h
+++ src/mat/matimpl.h
@@ -1,4 +1,4 @@
-/* $Id: matimpl.h,v 1.72 1997/05/29 03:54:17 bsmith Exp bsmith $ */
+/* $Id: matimpl.h,v 1.73 1997/08/13 22:23:52 bsmith Exp bsmith $ */
 
 #if !defined(__MATIMPL)
 #define __MATIMPL
@@ -148,14 +148,17 @@ extern int MatCopy_Basic(Mat,Mat);
     ncolumns      = {2,1,1,0}
     columns       = {{0,2},{1},{3},{}}
     nrows         = {4,2,3,3}
-    rows          = {{0,1,2,3},{0,1},{1,2,3},{1,2,3}}
+    rows          = {{0,1,2,3},{0,1},{1,2,3},{0,1,2}}
     columnsforrow = {{0,0,2,2},{1,1},{4,3,3},{5,5,5}}
 
-    ncolumns      = {2,0,2,2}
+    ncolumns      = {1,0,1,1}
     columns       = {{6},{},{4},{5}}
     nrows         = {3,0,2,2}
     rows          = {{4,5,6},{},{4,6},{4,5}}
     columnsforrow = {{6,0,6},{},{4,4},{5,5}}
+
+    See the routine MatFDColoringApply() for how this data is used
+  to compute the Jacobian.
 
 */
 
@@ -169,7 +172,7 @@ struct  _p_MatFDColoring{
   int    *nrows;           /* number of local rows for each color */
   int    **rows;           /* lists the rows for each color */
   int    **columnsforrow;  /* lists the corresponding columns for those rows */ 
-  Scalar *scale,*wscale;   /* workspace using to hold FD scalings */
+  Scalar *scale,*wscale;   /* workspace used to hold FD scalings */
   double error_rel;        /* square root of relative error in computing function */
   double umin;             /* minimum allowable u'dx value */
   int    freq;             /* frequency at which new Jacobian is computed */
