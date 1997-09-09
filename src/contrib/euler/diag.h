194abdc23c8cd9958c1c3ce48c7b36a75ebb24d7
Lois Curfman McInnes <curfman@mcs.anl.gov>
1997-09-09 14:36:47 -0600

    *** empty log message ***

--- src/contrib/euler/diag.h
+++ src/contrib/euler/diag.h
@@ -2,9 +2,8 @@ c
 c  Parallel array sizes for the main diagonal blocks of the
 c  Jacobian matrix.  Space is allocated in UserCreateEuler().
 c
-      double precision  D(5,5,xsf2:xef01,ysf2:yef01,zsf2:zef01)
-c      double precision  D(5,5,xsf1:xef01,ysf1:yef01,zsf1:zef01)
-c      double precision  D(5,5,gxsf1w:xefp1,gysf1w:yefp1,gzsf1w:zefp1)
+      double precision
+     &  D(ndof_e,ndof_e,xsf2:xef01,ysf2:yef01,zsf2:zef01)
 c
 c  Uniprocessor array sizes:
-c      COMMON /DIAG/ D(5,5,NI1,NJ1,NK1)
+c      COMMON /DIAG/ D(ndof_e,ndof_e,NI1,NJ1,NK1)
