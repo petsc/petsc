eef3d4d22ff6f0256e22a5d49986df0744573326
Satish Balay <balay@mcs.anl.gov>
1997-09-17 12:56:41 -0600

    *** empty log message ***

--- src/sys/src/utils/memc.c
+++ src/sys/src/utils/memc.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: memc.c,v 1.36 1997/09/12 20:15:52 balay Exp balay $";
+static char vcid[] = "$Id: memc.c,v 1.37 1997/09/17 18:55:39 balay Exp balay $";
 #endif
 /*
     We define the memory operations here. The reason we just don't use 
@@ -126,7 +126,7 @@ int PetscMemcmp(void * str1, void *str2, int len)
 
 .keywords: Petsc, copy, memory
 
-.seealso: PetscMemcpy(), PetscMemset()
+.seealso: PetscMemcpy()
 @*/
 int PetscMemmove(void *a,void *b,int n)
 {
