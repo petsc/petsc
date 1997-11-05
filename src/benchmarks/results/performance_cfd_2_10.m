dd69a1ec6fd4966244328a996900b248ad8d513a
Satish Balay <balay@mcs.anl.gov>
1997-11-05 16:49:28 -0600

    *** empty log message ***

--- src/benchmarks/results/performance_cfd_2_10.m
+++ src/benchmarks/results/performance_cfd_2_10.m
@@ -1,7 +1,7 @@
 
 figure (1)
 
-Y = [65 62 56 50 47 33 30 25 23 ];
+Y = [65 62 56 50 47 33 30 30 25 ];
 bar(Y);
 axis([0 10 0 150])
 ylabel('MFlops');
@@ -14,14 +14,14 @@ text(4,22,'CRAY T3E','Rotation',90)
 text(5,22,'SGI Origin 2000','Rotation',90)
 text(6,22,'NT Pentium PRO 200Mhz','Rotation',90)
 text(7,22,'IBM SP1','Rotation',90)
-text(8,22,'SGI PowerChallenge','Rotation',90)
-text(9,22,'Sun Ultra 2 UPA/SBus (168MHz)','Rotation',90)
+text(8,22,'Sun Ultra 2 UPA/SBus (168MHz)','Rotation',90)
+text(9,22,'SGI PowerChallenge','Rotation',90)
 
 % print -dps performance_cfd_2_10_1.ps
 
 figure(2)
 
-Y = [138 87 100 57 53 39 36 28 24 ];
+Y = [138 87 100 57 53 39 36 32 28 ];
 bar(Y);
 axis([0 10 0 150])
 ylabel('MFlops');
@@ -34,7 +34,7 @@ text(4,22,'CRAY T3E','Rotation',90)
 text(5,22,'SGI Origin 2000','Rotation',90)
 text(6,22,'NT Pentium PRO 200Mhz ','Rotation',90)
 text(7,22,'IBM SP1','Rotation',90)
-text(8,22,'SGI PowerChallenge','Rotation',90)
-text(9,22,'Sun Ultra 2 UPA/SBus (168MHz)','Rotation',90)
+text(8,22,'Sun Ultra 2 UPA/SBus (168MHz)','Rotation',90)
+text(9,22,'SGI PowerChallenge','Rotation',90)
 
 % print -dps performance_cfd_2_10_2.ps
