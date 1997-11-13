1973fafa78b702e21dfb85bb5e62124d41aa2a43
Satish Balay <balay@mcs.anl.gov>
1997-11-13 12:44:59 -0600

    *** empty log message ***

--- include/finclude/petscao.h
+++ include/finclude/petscao.h
@@ -1,15 +1,18 @@
 C
-C  $Id: ao.h,v 1.2 1996/08/06 04:04:35 bsmith Exp bsmith $;
+C  $Id: ao.h,v 1.3 1997/09/26 02:22:28 bsmith Exp balay $;
 C
 C  Include file for Fortran use of the AO (application ordering) package in PETSc
 C
-#define AO       integer
-#define AOType   integer
+#define AO         integer
+#define AOType     integer
+#define AOData     integer
+#define AODataType integer
 
       integer AO_BASIC, AO_ADVANCED
-
       parameter (AO_BASIC = 0, AO_ADVANCED = 1)
 
+      integer AODATA_BASIC, AODATA_ADVANCED
+      parameter (AODATA_BASIC=0, AODATA_ADVANCED=1)
 C
 C  End of Fortran include file for the AO package in PETSc
 
