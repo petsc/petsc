ccdf650fbc284ee70ee41df7a0acad2e9b3cfb48
Barry Smith <bsmith@mcs.anl.gov>
1998-03-05 16:59:18 -0600

    *** empty log message ***

--- src/sys/src/draw/impls/x/xinit.c
+++ src/sys/src/draw/impls/x/xinit.c
@@ -1,6 +1,6 @@
 
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: xinit.c,v 1.42 1998/01/06 20:11:31 bsmith Exp bsmith $";
+static char vcid[] = "$Id: xinit.c,v 1.43 1998/02/27 14:49:13 bsmith Exp bsmith $";
 #endif
 
 /* 
@@ -64,7 +64,7 @@ int XiSetVisual(Draw_X* XiWin,int q_default_visual,Colormap cmap,int nc )
   else if (q_default_visual) XiWin->cmap  = DefaultColormap( XiWin->disp, XiWin->screen );
   else                       XiWin->cmap = 0;
 
-  if (XiWin->depth < 256) {
+  if (XiWin->depth < 8) {
     SETERRQ(1,1,"PETSc Graphics require monitors with at least 8 bit color (256 colors)");
   }
   PLogInfo(0,"XiSetVisual:Always opening default visual X window\n");
