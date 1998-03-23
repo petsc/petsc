5b22c773169e6c6cc8f28117a48fe9bd8125d3ed
Satish Balay <balay@mcs.anl.gov>
1998-03-23 14:02:45 -0600

    Initial revision

---
+++ include/f90impl/f90_rs6000.h
@@ -0,0 +1,48 @@
+/* $Id: f90_IRIX.h,v 1.1 1998/03/12 18:01:55 balay Exp balay $ */
+
+typedef struct {
+  long lower;   /* starting index of the fortran array */
+  long extent;  /* length of the array */
+  long mult;    /* in bytes */
+} tripple;
+
+/*
+  The following constants are just
+  guesses. The program behavies strangly
+  if these constants are not set in
+  the f90 pointer
+*/
+#define f90_CHAR_ID   770
+#define f90_INT_ID    781
+#define f90_DOUBLE_ID 782
+#define f90_COOKIE    20481
+
+#define f90_header() \
+void* addr;    /* Pointer to the data/array */ \       
+short id;      /* integer id representing the datatype */ \
+short cookie;  /* a wiered f90 cookie */ \
+int   sd;      /* sizeof(DataType) */          \          
+int   ndim;    /* No of dimentions */          \
+int   sum_d;   /* -sumof(lower*mult) */
+
+typedef struct {
+  f90_header()
+  tripple dim[1];
+}array1d;
+
+typedef struct {
+  f90_header()
+  tripple dim[2];   /* dim2,dim1 */
+}array2d;
+
+typedef struct {
+  f90_header()
+  tripple dim[3];   /* dim3,dim2,dim1 */
+}array3d;
+
+typedef struct {
+  f90_header()
+  tripple dim[4];   /* dim4,dim3,dim2,dim1 */
+}array4d;
+
+
---
+++ src/sys/src/f90/f90_rs6000.c
@@ -0,0 +1,191 @@
+#ifndef lint
+static char vcid[] = "$Id: f90_IRIX.c,v 1.4 1998/03/12 23:05:50 balay Exp $";
+#endif
+
+/*
+         This file contains the code to map between Fortran 90 
+  pointers and traditional C pointers for the NAG F90 compiler.
+*/
+#if defined(HAVE_XLF90)
+
+#include "src/fortran/custom/zpetsc.h"
+#include "src/fortran/f90/f90_rs6000.h"
+
+/* --------------------------------------------------------*/
+/*
+    PetscF90Create1dArrayScalar - Given a C pointer to a one dimensional
+  array and its length; this fills in the appropriate Fortran 90
+  pointer data structure.
+
+  Input Parameters:
+.   array - regular C pointer (address)
+.   len - length of array (in items)
+
+  Output Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Create1dArrayScalar(void *array,int len, array1d *ptr)
+{
+  ptr->addr          = array;
+  ptr->id            = F90_DOUBLE_ID;
+  ptr->cookie        = F90_COOKIE;
+  ptr->sd            = sizeof(Scalar);
+  ptr->ndim          = 1;
+  ptr->dim[0].extent = len;
+  ptr->dim[0].mult   = sizeof(Scalar);
+  ptr->dim[0].lower  = 1;
+  ptr->sum_d         = -(ptr->dim[0].lower*ptr->dim[0].mult)
+
+  return 0;
+}
+
+/*
+    PetscF90Get1dArrayScalar - Gets the address for the data 
+       stored in a Fortran pointer array.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+
+  Output Parameters:
+.   array - regular C pointer (address)
+
+*/ 
+int PetscF90Get1dArrayScalar(array1d *ptr,void **array)
+{
+  *array = (void *) ptr->addr;
+  return 0;
+}
+
+/*
+    PetscF90Destroy1dArrayScalar - Deletes a Fortran pointer.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Destroy1dArrayScalar(array1d *ptr)
+{
+  ptr->addr  = (void *)0;
+  return 0;
+}
+/* --------------------------------------------------------*/
+/*
+    PetscF90Create2dArrayScalar - Given a C pointer to a two dimensional
+  array and its length; this fills in the appropriate Fortran 90
+  pointer data structure.
+
+  Input Parameters:
+.   array - regular C pointer (address)
+.   m - number of rows in array
+.   n - number of columns in array
+
+  Output Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Create2dArrayScalar(void *array,int m,int n, array2d *ptr)
+{
+  ptr->addr          = array;
+  ptr->id            = F90_DOUBLE_ID;
+  ptr->cookie        = F90_COOKIE;
+  ptr->sd            = sizeof(Scalar);
+  ptr->ndim          = 2;
+  ptr->dim[1].extent = m;
+  ptr->dim[1].mult   = sizeof(Scalar);
+  ptr->dim[1].lower  = 1;
+  ptr->dim[0].extent = n;
+  ptr->dim[0].mult   = m*sizeof(Scalar);
+  ptr->dim[0].lower  = 1;
+  ptr->sum_d         = -(ptr->dim[0].lower*ptr->dim[0].mult+ptr->dim[1].lower*ptr->dim[1].mult)
+
+  return 0;
+}
+
+/*
+    PetscF90Get2dArrayScalar - Gets the address for the data 
+       stored in a 2d Fortran pointer array.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+
+  Output Parameters:
+.   array - regular C pointer (address)
+
+*/ 
+int PetscF90Get2dArrayScalar(array2d *ptr,void **array)
+{
+  *array = (void *) ptr->addr;
+  return 0;
+}
+
+/*
+    PetscF90Destroy2dArrayScalar - Deletes a Fortran pointer.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Destroy2dArrayScalar(array2d *ptr)
+{
+  ptr->addr  = (void *)0;
+  return 0;
+}
+/* -----------------------------------------------------------------*/
+
+/*
+    PetscF90Create1dArrayInt - Given a C pointer to a one dimensional
+  array and its length; this fills in the appropriate Fortran 90
+  pointer data structure.
+
+  Input Parameters:
+.   array - regular C pointer (address)
+.   len - length of array (in items)
+
+  Output Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Create1dArrayInt(void *array,int len, array1d *ptr)
+{
+  ptr->addr          = array;
+  ptr->id            = F90_INT_ID;
+  ptr->cookie        = F90_COOKIE;
+  ptr->sd            = sizeof(int);
+  ptr->ndim          = 1;
+  ptr->dim[0].extent = len;
+  ptr->dim[0].mult   = sizeof(int);
+  ptr->dim[0].lower  = 1;
+  ptr->sum_d         = -(ptr->dim[0].lower*ptr->dim[0].mult)
+
+  return 0;
+}
+
+/*
+    PetscF90Get1dArrayInt - Gets the address for the data 
+       stored in a Fortran pointer array.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+
+  Output Parameters:
+.   array - regular C pointer (address)
+
+*/ 
+int PetscF90Get1dArrayInt(array1d *ptr,void **array)
+{
+  *array = (void *) ptr->addr;
+  return 0;
+}
+
+/*
+    PetscF90Destroy1dArrayInt - Deletes a Fortran pointer.
+
+  Input Parameters:
+.   ptr - Fortran 90 pointer
+*/ 
+int PetscF90Destroy1dArrayInt(array1d *ptr)
+{
+  ptr->addr  = (void *)0;
+  return 0;
+}
+
+#endif
+
+
+
