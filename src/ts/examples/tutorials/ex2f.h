4146b6d0cbaf8f48859a786a10966ef43d1c26ad
Satish Balay <balay@mcs.anl.gov>
1998-03-24 10:39:46 -0600

    *** empty log message ***

--- src/ts/examples/tutorials/ex2f.h
+++ src/ts/examples/tutorials/ex2f.h
@@ -1,25 +1,24 @@
-
-C  This file contains include statements and a user-defined
-C  common block for application-specific data.  This file is
-C  included in each routine within the program ex2f. 
-C
-C  The following include statements are generally used in TS Fortran
-C  programs:
-C     petsc.h  - base PETSc routines
-C     vec.h    - vectors
-C     mat.h    - matrices
-C     ksp.h    - Krylov subspace methods
-C     pc.h     - preconditioners
-C     sles.h   - SLES interface
-C     snes.h   - SNES interface
-C     ts.h     - TS interface
-C     viewer.h - viewers
-C     draw.h   - drawing
-C  In addition, we need the following for use of distributed arrays
-C     da.h     - distributed arrays (DAs)
-C  Other include statements may be needed if using additional PETSc
-C  routines in a Fortran program, e.g.,
-C     is.h     - index sets
+!  This file contains include statements and a user-defined
+!  common block for application-specific data.  This file is
+!  included in each routine within the program ex2f. 
+!
+!  The following include statements are generally used in TS Fortran
+!  programs:
+!     petsc.h  - base PETSc routines
+!     vec.h    - vectors
+!     mat.h    - matrices
+!     ksp.h    - Krylov subspace methods
+!     pc.h     - preconditioners
+!     sles.h   - SLES interface
+!     snes.h   - SNES interface
+!     ts.h     - TS interface
+!     viewer.h - viewers
+!     draw.h   - drawing
+!  In addition, we need the following for use of distributed arrays
+!     da.h     - distributed arrays (DAs)
+!  Other include statements may be needed if using additional PETSc
+!  routines in a Fortran program, e.g.,
+!     is.h     - index sets
 
 #include "include/FINCLUDE/petsc.h"
 #include "include/FINCLUDE/vec.h"
@@ -33,27 +32,27 @@ C     is.h     - index sets
 #include "include/FINCLUDE/viewer.h"
 #include "include/FINCLUDE/draw.h"
 
-C - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-C
-C  The application context to contain data needed by the 
-C  application-provided call-back routines, RHSFunction(),
-C  RHSJacobian(), Monitor().  In this example the application context
-C  is a Fortran common block, /appctx/.  Note that we can store
-C  (pointers to) PETSc objects within this common block.
-C    appctx:  M         - total number of grid points  
-C             da        - distributed array
-C             localwork - local work vector (including ghost points)
-C             solution  - solution vector
-C             comm      - communicator
-C             rank      - processor rank within communicator
-C             size      - number of processors
-C             debug     - flag (1 indicates debugging printouts)
-C
-C  Store other misc problem parameters in common block /params/
-C             h         - mesh width h = 1/(M-1)
-C
-C - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-C  Common block data:
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+!
+!  The application context to contain data needed by the 
+!  application-provided call-back routines, RHSFunction(),
+!  RHSJacobian(), Monitor().  In this example the application context
+!  is a Fortran common block, /appctx/.  Note that we can store
+!  (pointers to) PETSc objects within this common block.
+!    appctx:  M         - total number of grid points  
+!             da        - distributed array
+!             localwork - local work vector (including ghost points)
+!             solution  - solution vector
+!             comm      - communicator
+!             rank      - processor rank within communicator
+!             size      - number of processors
+!             debug     - flag (1 indicates debugging printouts)
+!
+!  Store other misc problem parameters in common block /params/
+!             h         - mesh width h = 1/(M-1)
+!
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+!  Common block data:
       DA               da
       Vec              localwork, solution
       integer          M, rank, size, debug
@@ -64,4 +63,4 @@ C  Common block data:
       common /appctx/ M, debug, da, localwork, solution
       common /appctx/ comm, rank, size
 
-C - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
