41f19de70106904232dd7e303966f6d1e189400b
Barry Smith <bsmith@mcs.anl.gov>
1998-04-03 15:00:37 -0600

    AutoCheckin

--- src/sys/src/viewer/impls/socket/matlab/receivedense.c
+++ src/sys/src/viewer/impls/socket/matlab/receivedense.c
@@ -1,5 +1,5 @@
-#ifndef lint
-static char vcid[] = "$Id: receivedense.c,v 1.4 1996/12/18 17:05:34 balay Exp balay $";
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: receivedense.c,v 1.5 1996/12/18 17:05:48 balay Exp bsmith $";
 #endif
 /* 
    This is part of the MatlabSockettool Package. It is called by 
@@ -15,30 +15,55 @@ static char vcid[] = "$Id: receivedense.c,v 1.4 1996/12/18 17:05:34 balay Exp ba
 #include "mex.h"
 #define ERROR(a) {fprintf(stderr,"RECEIVE %s \n",a); return -1;}
 /*-----------------------------------------------------------------*/
-#undef __FUNCTION__  
-#define __FUNCTION__ "ReceiveDenseMatrix"
+#undef __FUNC__  
+#define __FUNC__ "ReceiveDenseMatrix"
 int ReceiveDenseMatrix(Matrix *plhs[],int t)
 {
   int    m,n,compx = 0,i;
   
   /* get size of matrix */
-  if (PetscBinaryRead(t,&m,1,BINARY_INT))   ERROR("reading number columns"); 
-  if (PetscBinaryRead(t,&n,1,BINARY_INT))   ERROR("reading number rows"); 
-  if (PetscBinaryRead(t,&compx,1,BINARY_INT))   ERROR("reading number rows"); 
+  if (PetscBinaryRead(t,&m,1,PETSC_INT))   ERROR("reading number columns"); 
+  if (PetscBinaryRead(t,&n,1,PETSC_INT))   ERROR("reading number rows"); 
+  if (PetscBinaryRead(t,&compx,1,PETSC_INT))   ERROR("reading number rows"); 
+  
   /*allocate matrix */
   plhs[0]  = mxCreateFull(m, n, compx);
   /* read in matrix */
   if (!compx) {
-    if (PetscBinaryRead(t,mxGetPr(plhs[0]),n*m,BINARY_DOUBLE)) ERROR("read dense matrix");
-  }
-  else {
+    if (PetscBinaryRead(t,mxGetPr(plhs[0]),n*m,PETSC_DOUBLE)) ERROR("read dense matrix");
+  } else {
     for ( i=0; i<n*m; i++ ) {
-      if (PetscBinaryRead(t,mxGetPr(plhs[0])+i,1,BINARY_DOUBLE))ERROR("read dense matrix");
-      if (PetscBinaryRead(t,mxGetPi(plhs[0])+i,1,BINARY_DOUBLE))ERROR("read dense matrix");
+      if (PetscBinaryRead(t,mxGetPr(plhs[0])+i,1,PETSC_DOUBLE))ERROR("read dense matrix");
+      if (PetscBinaryRead(t,mxGetPi(plhs[0])+i,1,PETSC_DOUBLE))ERROR("read dense matrix");
     }
   }
   return 0;
 }
 
+#undef __FUNC__  
+#define __FUNC__ "ReceiveIntDenseMatrix"
+int ReceiveDenseIntMatrix(Matrix *plhs[],int t)
+{
+  int    m,compx = 0,i,*array;
+  double *values;
+  
+  /* get size of matrix */
+  if (PetscBinaryRead(t,&m,1,PETSC_INT))   ERROR("reading number columns"); 
+  
+  /*allocate matrix */
+  plhs[0]  = mxCreateFull(m, 1, 0);
+
+  /* read in matrix */
+  array = (int *) malloc(m*sizeof(int)); if (!array) ERROR("reading allocating space");
+  if (PetscBinaryRead(t,array,m,PETSC_INT)) ERROR("read dense matrix");
+
+  values = mxGetPr(plhs[0]);
+  for ( i =0; i<m; i++ ) {
+    values[i] = array[i];
+  }
+  free(array);
+
+  return 0;
+}
     
  
--- src/sys/src/viewer/impls/socket/matlab/receivesparse.c
+++ src/sys/src/viewer/impls/socket/matlab/receivesparse.c
@@ -1,5 +1,5 @@
-#ifndef lint
-static char vcid[] = "$Id: receivesparse.c,v 1.4 1996/12/18 17:05:59 balay Exp balay $";
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: receivesparse.c,v 1.5 1996/12/18 17:06:13 balay Exp bsmith $";
 #endif
 /*
     Part of the MatlabSockettool Package. Receive a sparse matrix
@@ -15,8 +15,8 @@ static char vcid[] = "$Id: receivesparse.c,v 1.4 1996/12/18 17:05:59 balay Exp b
 #include "mex.h"
 
 #define ERROR(a) {fprintf(stderr,"RECEIVE: %s \n",a); return -1;}
-#undef __FUNCTION__  
-#define __FUNCTION__ "ReceiveSparseMatrix"
+#undef __FUNC__  
+#define __FUNC__ "ReceiveSparseMatrix"
 int ReceiveSparseMatrix(Matrix *plhs[],int t)
 {
   int    *tr,*tc, compx = 0;
@@ -25,11 +25,11 @@ int ReceiveSparseMatrix(Matrix *plhs[],int t)
   double *tv, *v, *diag,*vi;
 
   /* get size of matrix */
-  if (PetscBinaryRead(t,&m,1,BINARY_INT))   ERROR("reading number columns"); 
-  if (PetscBinaryRead(t,&n,1,BINARY_INT))   ERROR("reading number rows"); 
+  if (PetscBinaryRead(t,&m,1,PETSC_INT))   ERROR("reading number columns"); 
+  if (PetscBinaryRead(t,&n,1,PETSC_INT))   ERROR("reading number rows"); 
   /* get number of nonzeros */
-  if (PetscBinaryRead(t,&nnz,1,BINARY_INT))   ERROR("reading nnz"); 
-  if (PetscBinaryRead(t,&compx,1,BINARY_INT))   ERROR("reading number rows"); 
+  if (PetscBinaryRead(t,&nnz,1,PETSC_INT))   ERROR("reading nnz"); 
+  if (PetscBinaryRead(t,&compx,1,PETSC_INT))   ERROR("reading row lengths"); 
   /* Create a matrix for Matlab */
   /* since Matlab stores by columns not rows we actually will 
      create transpose of desired matrix */
@@ -38,17 +38,16 @@ int ReceiveSparseMatrix(Matrix *plhs[],int t)
   c = mxGetJc(plhs[0]);
   v = mxGetPr(plhs[0]);
   if (!compx) {
-    if (PetscBinaryRead(t,v,nnz,BINARY_DOUBLE)) ERROR("reading values");
-  }
-  else {
+    if (PetscBinaryRead(t,v,nnz,PETSC_DOUBLE)) ERROR("reading values");
+  } else {
     for ( i=0; i<nnz; i++ ) {
       vi = mxGetPi(plhs[0]);
-      if (PetscBinaryRead(t,v+i,1,BINARY_DOUBLE)) ERROR("reading values");
-      if (PetscBinaryRead(t,vi+i,1,BINARY_DOUBLE)) ERROR("reading values");
+      if (PetscBinaryRead(t,v+i,1,PETSC_DOUBLE)) ERROR("reading values");
+      if (PetscBinaryRead(t,vi+i,1,PETSC_DOUBLE)) ERROR("reading values");
     }
   }
-  if (PetscBinaryRead(t,c,m+1,BINARY_INT)) ERROR("reading column pointers");
-  if (PetscBinaryRead(t,r,nnz,BINARY_INT)) ERROR("reading row pointers");
+  if (PetscBinaryRead(t,c,m+1,PETSC_INT)) ERROR("reading column pointers");
+  if (PetscBinaryRead(t,r,nnz,PETSC_INT)) ERROR("reading row pointers");
   /* pointers start at 0 not 1 */
   for ( i=0; i<m+1; i++ ) {c[i]--;}
   for ( i=0; i<nnz; i++ ) {r[i]--;}
