78e778085b8fd0201e2d5b438d0a4ff73f0f5f57
Barry Smith <bsmith@mcs.anl.gov>
1998-04-09 14:37:15 -0600

    Initial revision

---
+++ bmake/alpha_nolog/petscconf.h
@@ -0,0 +1,27 @@
+/*
+    Defines the configuration for this machine
+*/
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define HAVE_64BITS 
+#define HAVE_PWD_H 
+#define HAVE_STRING_H 
+#define HAVE_MALLOC_H 
+#define HAVE_STDLIB_H 
+#define HAVE_SWAPPED_BYTES 
+#define HAVE_X11 
+#define HAVE_DRAND48  
+#define HAVE_GETDOMAINNAME  
+#define HAVE_UNISTD_H 
+#define HAVE_SYS_TIME_H 
+#define HAVE_UNAME  
+#define HAVE_64BITS
+
+#define HAVE_FORTRAN_UNDERSCORE
+
+#define HAVE_READLINK
+#define HAVE_MEMMOVE
+#define NEED_UTYPE_TYPEDEFS
+
+#endif
