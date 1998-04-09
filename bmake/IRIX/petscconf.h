489dfdb67c1ba3e914db7d04fa2820b57e237052
Satish Balay <balay@mcs.anl.gov>
1998-04-09 14:26:47 -0600

    Initial revision

---
+++ bmake/IRIX/petscconf.h
@@ -0,0 +1,26 @@
+/* $Id: petsc.h,v 1.206 1998/04/09 04:19:44 bsmith Exp bsmith $ */
+
+/*
+    Defines the configuration for this machine
+*/
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+ 
+
+#define HAVE_PWD_H 
+#define HAVE_STRING_H 
+#define HAVE_STROPTS_H 
+#define HAVE_MALLOC_H 
+#define HAVE_X11  
+#define HAVE_DRAND48  
+#define HAVE_GETDOMAINNAME
+#define HAVE_UNAME 
+#define HAVE_UNISTD_H 
+#define HAVE_SYS_TIME_H
+
+#define HAVE_FORTRAN_UNDERSCORE 
+
+#undef HAVE_READLINK
+
+#endif
