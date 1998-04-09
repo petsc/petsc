d69d8e2fa7674f8baec1c844359f947699f35b81
Satish Balay <balay@mcs.anl.gov>
1998-04-09 14:55:25 -0600

    Initial revision

---
+++ bmake/t3e/petscconf.h
@@ -0,0 +1,24 @@
+/* $Id: petscconf.h,v 1.1 1998/04/09 20:52:44 balay Exp $ */
+
+/*
+    Defines the configuration for this machine
+*/
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define HAVE_64BITS 
+#define HAVE_PWD_H 
+#define HAVE_STRING_H 
+#define HAVE_MALLOC_H 
+#define HAVE_64BIT_INT 
+#define HAVE_DRAND48 
+#define USES_CPTOFCD  
+#define HAVE_UNISTD_H
+#define HAVE_SYS_TIME_H 
+#define HAVE_UNAME
+#define HAVE_X11
+
+#define HAVE_FORTRAN_CAPS 
+#define USES_FORTRAN_SINGLE
+
+#endif
