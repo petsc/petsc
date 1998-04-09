ca2ae8e61c3373ca593cb5c02ffc1a7771197ca0
Satish Balay <balay@mcs.anl.gov>
1998-04-09 15:17:51 -0600

    Initial revision

---
+++ bmake/rs6000_gnu/petscconf.h
@@ -0,0 +1,35 @@
+/* $Id: petscconf.h,v 1.1 1998/04/09 21:00:31 balay Exp $ */
+
+/*
+    Defines the configuration for this machine
+*/
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define HAVE_STROPTS_H 
+#define HAVE_SEARCH_H 
+#define HAVE_PWD_H 
+#define HAVE_STDLIB_H
+#define HAVE_STRING_H 
+#define HAVE_STRINGS_H 
+#define HAVE_MALLOC_H 
+#define _POSIX_SOURCE
+#define HAVE_DRAND48  
+#define HAVE_GETDOMAINNAME  
+#define _XOPEN_SOURCE
+#define HAVE_UNISTD_H 
+#define HAVE_SYS_TIME_H 
+#define NEED_UTYPE_TYPEDEFS 
+#define _XOPEN_SOURCE_EXTENDED
+#define HAVE_UNAME  
+#define HAVE_BROKEN_REQUEST_FREE 
+#define NEEDS_GETTIMEOFDAY_PROTO
+#define USES_TEMPLATED_COMPLEX
+
+#define HAVE_FORTRAN_UNDERSCORE 
+#define HAVE_FORTRAN_UNDERSCORE_UNDERSCORE
+
+#define HAVE_READLINK
+#define HAVE_MEMMOVE
+
+#endif
