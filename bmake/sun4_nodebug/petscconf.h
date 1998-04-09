550500f060d008a93500dbce3a76a4a461557a7f
Barry Smith <bsmith@mcs.anl.gov>
1998-04-09 14:28:19 -0600

    Initial revision

---
+++ bmake/sun4_nodebug/petscconf.h
@@ -0,0 +1,25 @@
+/*
+    Defines the configuration for this machine
+*/
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define HAVE_STROPTS_H 
+#define HAVE_SEARCH_H 
+#define HAVE_PWD_H 
+#define HAVE_STRING_H 
+#define HAVE_MALLOC_H 
+#define HAVE_X11 
+#define HAVE_STRINGS_H 
+#define HAVE_DRAND48 
+#define HAVE_GETDOMAINNAME  
+#define HAVE_UNISTD_H 
+#define HAVE_SYS_TIME_H 
+#define HAVE_UNAME
+
+#define HAVE_FORTRAN_UNDERSCORE
+
+#define HAVE_READLINK
+#define HAVE_MEMMOVE
+
+#endif
