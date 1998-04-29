b4dab342a237ac75f15ebd0b6370b7174c06c5ff
Lois Curfman McInnes <curfman@mcs.anl.gov>
1998-04-29 07:37:21 -0600

    *** empty log message ***

--- src/dm/usg/2d/quadratic/appctx.h
+++ src/dm/usg/2d/quadratic/appctx.h
@@ -1,9 +1,9 @@
 
 /*
-       Defines some simple data structures for writing cell (element) based PDE codes.
+    Defines some simple data structures for writing cell-based (element-based) PDE codes.
 
     Generally one would write a code by starting with the data structures below and 
-    and to them and deleting from them unneeded information. 
+    adding to them and deleting from them unneeded information. 
 */
 #if !defined(__APPCTX_H)
 #define __APPCTX_H
@@ -14,21 +14,21 @@
 #include "snes.h"
 
 /*
-        cell_n               - number of cells on this processor 
-        cell_vertex          - vertices of the cells (in local numbering)
-        cell_global          - global number of each cell on this processor
+  AppGrid: grid data
 
-        vertex_n             - number of unique vertices on this processor 
-        vertex_n_ghosted     - number of vertices including ghost ones
-        vertex_global        - global number of each vertex on this processor (including ghosts)
-        vertex_value         - x,y coordinates of vertices on this processor (including ghosts)
-        vertex_boundary      - list of on processor vertices (including ghosts) that are on the boundary
-        vertex_boundary_flag - bit array indicating for all on processor vertices (including ghosts) 
-                               if the are on the boundary
-        ltog                 - mapping from local numbering of vertices (including ghosts)
-                               to global
-
-        cell_cell            - neighbors of each cell
+  cell_n               - number of cells on this processor 
+  cell_vertex          - vertices of the cells (in local numbering)
+  cell_global          - global number of each cell on this processor
+  vertex_n             - number of unique vertices on this processor 
+  vertex_n_ghosted     - number of vertices including ghost ones
+  vertex_global        - global number of each vertex on this processor (including ghosts)
+  vertex_value         - x,y coordinates of vertices on this processor (including ghosts)
+  vertex_boundary      - list of on processor vertices (including ghosts) that are on the boundary
+  vertex_boundary_flag - bit array indicating for all on processor vertices (including ghosts) 
+                         if the are on the boundary
+  ltog                 - mapping from local numbering of vertices (including ghosts)
+                         to global
+  cell_cell            - neighbors of each cell
 */
         
 typedef struct {
@@ -45,15 +45,18 @@ typedef struct {
 } AppGrid;
 
 /*
-    gtol             - global to local vector scatter
-                       (used to move data from x to w_local for example
-    A                - parallel sparse stiffness matrix
-    b                - parallel vector contains right hand side
-    x                - parallel vector contains solution
-    z                - parallel work vector
-    w_local, x_local
-    z_local          - (work) sequential vectors contains local plus ghosted elements
+  AppAlgebra: linear algebra data
+
+  gtol             - global-to-local vector scatter
+                     (e.g., used to move data from z to z_local)
+  A                - parallel sparse stiffness matrix
+  b                - parallel vector, containing right-hand side
+  x                - parallel vector, containing solution
+  z                - parallel work vector
+  w_local, x_local - sequential work vectors (containing local plus ghosted entries)
+  z_local          
 */
+
 typedef struct {
   Vec                    b,x,z;
   Vec                    w_local,x_local,z_local;  /* local ghosted work vectors */
@@ -62,36 +65,35 @@ typedef struct {
 } AppAlgebra;
 
 /*
-    drawlocal    - window where processor local portion is drawn
-    drawglobal   - window where entire grid is drawn
+  AppView: viewer data
 
-    shownumbers  - print the vertex and cell numbers 
-    showvertices - draw the vertices as points
-    showelements - draw the elements 
-    showboundary - draw boundary of domain
-    showboundaryvertices - draw points on boundary of domain
-
-    showsomething - flag indicating that at least one of the flags above is true
+  drawlocal            - window where processor local portion is drawn
+  drawglobal           - window where entire grid is drawn
+  shownumbers          - print the vertex and cell numbers 
+  showvertices         - draw the vertices as points
+  showelements         - draw the elements 
+  showboundary         - draw boundary of domain
+  showboundaryvertices - draw points on boundary of domain
+  showsomething        - flag indicating that at least one of the flags above is true
 */
 
 typedef struct {
   Draw       drawlocal;
   Draw       drawglobal;
-
   int        shownumbers;
   int        showvertices;
   int        showelements;
   int        showboundary;
   int        showboundaryvertices;
- 
   int        showsomething;            
   int        matlabgraphics;
 } AppView;
 
 /*
-      comm   - MPI communictor where grid etc are stored
-      aodata - grid database
-       
+   AppCtx: application context
+
+   comm   - MPI communictor where grid etc are stored
+   aodata - grid database
 */
 typedef struct {
   MPI_Comm   comm;
@@ -101,6 +103,9 @@ typedef struct {
   AppView    view;
 } AppCtx;
 
+/* 
+   Declare application-defined routines 
+*/
 extern int AppCtxViewGrid(Draw,void*);
 extern int AppCtxViewSolution(Draw,void*);
 extern int AppCtxCreate(MPI_Comm,AppCtx **);
