ad153f8a46055c949f01b4e706aa079265478871
Barry Smith <bsmith@mcs.anl.gov>
1998-05-19 09:41:22 -0600

    Initial revision

---
+++ src/sys/examples/tests/ex13.c
@@ -0,0 +1,18 @@
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: ex9.c,v 1.4 1997/09/22 15:18:56 balay Exp $";
+#endif
+
+/*
+     Tests PetscSequentialPhaseBegin() and PetscSequentialPhaseEnd()
+*/
+#include "petsc.h"
+
+int main(int argc, char **argv) {
+  int ierr;
+
+  PetscInitialize(&argc,&argv,PETSC_NULL,PETSC_NULL);
+  ierr = PetscSequentialPhaseBegin(PETSC_COMM_WORLD , 1); CHKERRA(ierr);
+  ierr = PetscSequentialPhaseEnd(PETSC_COMM_WORLD , 1); CHKERRA(ierr);
+  PetscFinalize();
+  return 0;
+}
