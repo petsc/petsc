82ec29a0ff392d37090223c85898dbfd6c6924af
Barry Smith <bsmith@mcs.anl.gov>
1998-06-08 15:22:59 -0600

    *** empty log message ***

--- src/dm/ao/examples/tests/ex7.c
+++ src/dm/ao/examples/tests/ex7.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: ex1.c,v 1.8 1997/10/28 14:25:35 bsmith Exp $";
+static char vcid[] = "$Id: ex7.c,v 1.1 1998/06/03 19:18:11 bsmith Exp bsmith $";
 #endif
 
 static char help[] = "Demonstrates constructing an application ordering\n\n";
@@ -10,8 +10,7 @@ static char help[] = "Demonstrates constructing an application ordering\n\n";
 
 int main(int argc,char **argv)
 {
-  int      n = 5, ierr,flg,rank,size,getpetsc[] = {0,3,4};
-  int      getapp[] = {2,1,3,4};
+  int      n = 5, ierr,flg,rank,size;
   IS       ispetsc,isapp;
   AO       ao;
 
@@ -27,18 +26,18 @@ int main(int argc,char **argv)
   /* create the application ordering */
   ierr = AOCreateBasicIS(isapp,ispetsc,&ao); CHKERRA(ierr);
 
-  ierr = ISDestroy(ispetsc); CHKERRA(ierr);
-  ierr = ISDestroy(isapp); CHKERRA(ierr);
 
   ierr = AOView(ao,VIEWER_STDOUT_WORLD); CHKERRA(ierr);
 
-  ierr = AOPetscToApplication(ao,4,getapp); CHKERRA(ierr);
-  printf("[%d] 2,1,3,4 PetscToApplication %d %d %d %d\n",rank,getapp[0],
-          getapp[1],getapp[2],getapp[3]);
+  ierr = ISView(ispetsc,VIEWER_STDOUT_WORLD);CHKERRA(ierr);
+  ierr = ISView(isapp,VIEWER_STDOUT_WORLD);CHKERRA(ierr);
+  ierr = AOPetscToApplicationIS(ao,ispetsc); CHKERRA(ierr);
+  ierr = ISView(isapp,VIEWER_STDOUT_WORLD);CHKERRA(ierr);
+  ierr = ISView(ispetsc,VIEWER_STDOUT_WORLD);CHKERRA(ierr);
 
-  ierr = AOApplicationToPetsc(ao,3,getpetsc); CHKERRA(ierr);
-  printf("[%d] 0,3,4 ApplicationToPetsc %d %d %d\n",rank,getpetsc[0],
-          getpetsc[1],getpetsc[2]);
+
+  ierr = ISDestroy(ispetsc); CHKERRA(ierr);
+  ierr = ISDestroy(isapp); CHKERRA(ierr);
 
   fflush(stdout);
   ierr = AODestroy(ao); CHKERRA(ierr);
