310f668ee3d1941e64cca79f876c8cc480a33dce
Satish Balay <balay@mcs.anl.gov>
1998-06-15 12:00:53 -0600

    *** empty log message ***

--- src/sys/examples/tests/ex1.c
+++ src/sys/examples/tests/ex1.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: ex1.c,v 1.13 1997/10/19 03:24:14 bsmith Exp bsmith $";
+static char vcid[] = "$Id: ex1.c,v 1.14 1997/10/28 14:21:56 bsmith Exp balay $";
 #endif
 
 /* 
@@ -22,6 +22,7 @@ int main(int argc,char **argv)
   PetscInitialize(&argc,&argv,(char *)0,0);
   fprintf(stdout,"Demonstrates PETSc Error Handlers\n");
   fprintf(stdout,"The error is a contrived error to test error handling\n");
+  fflush(stdout);
   ierr = CreateError(5); CHKERRA(ierr);
   PetscFinalize();
   return 0;
