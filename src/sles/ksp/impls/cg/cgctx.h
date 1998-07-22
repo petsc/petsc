c3e82ff2dc44b529a5d78ea9c8f80e3e350ad1a6
Barry Smith <bsmith@mcs.anl.gov>
1998-07-22 16:31:31 -0600

    *** empty log message ***

--- src/sles/ksp/impls/cg/cgctx.h
+++ src/sles/ksp/impls/cg/cgctx.h
@@ -8,11 +8,15 @@
 #include "petsc.h"
 #include "src/ksp/kspimpl.h"
 
+/*
+    The field should remain the same since it is shared by the BiCG code
+*/
+
 typedef struct {
+  KSPCGType type;                 /* type of system (symmetric or Hermitian) */
   Scalar    emin, emax;           /* eigenvalues */
   Scalar    *e, *d;
   double    *ee, *dd;             /* work space for Lanczos algorithm */
-  KSPCGType type;                 /* type of system (symmetric or Hermitian) */
 } KSP_CG;
 
 #endif
