7fc66a5f6d0e8a9c1f5d9e201f6347b3b5289fc7
Barry Smith <bsmith@mcs.anl.gov>
1998-09-14 12:26:06 -0600

    *** empty log message ***

--- src/vec/examples/tutorials/ex13.c
+++ src/vec/examples/tutorials/ex13.c
@@ -1,10 +1,8 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: ex2.c,v 1.29 1997/09/22 15:16:48 balay Exp $";
+static char vcid[] = "$Id: ex13.c,v 1.1 1998/09/12 01:43:04 bsmith Exp bsmith $";
 #endif
 
-static char help[] = "Builds a parallel vector with 1 component on the first\n\
-processor, 2 on the second, etc.  Then each processor adds one to all\n\
-elements except the last rank.\n\n";
+static char help[] = "Tests PetscObjectPublish().\n\n";
 
 /*T
    Concepts: Vectors^Assembling vectors;
@@ -20,13 +18,12 @@ T*/
      sys.h    - system routines       viewer.h - viewers
 */
 #include "vec.h"
-#include <math.h>
 
 int main(int argc,char **argv)
 {
-  int     i, N, ierr, rank;
-  Scalar  one = 1.0;
-  Vec     x;
+  int     i, n, ierr, rank;
+  Scalar  one = 1.0,*array;
+  Vec     x,xlocal;
 
   PetscInitialize(&argc,&argv,(char *)0,help);
   MPI_Comm_rank(PETSC_COMM_WORLD,&rank); 
@@ -39,38 +36,38 @@ int main(int argc,char **argv)
         local size PETSc will choose a reasonable partition trying 
         to put nearly an equal number of elements on each processor.
   */
-  ierr = VecCreateMPI(PETSC_COMM_WORLD,rank+1,PETSC_DECIDE,&x); CHKERRA(ierr);
-  ierr = VecGetSize(x,&N); CHKERRA(ierr);
+  ierr = VecCreateMPI(PETSC_COMM_WORLD,rank+4,PETSC_DECIDE,&x); CHKERRA(ierr);
+  ierr = PetscObjectPublish((PetscObject)x);CHKERRA(ierr);
+  ierr = VecGetLocalSize(x,&n); CHKERRA(ierr);
   ierr = VecSet(&one,x); CHKERRA(ierr);
 
-  /*
-     Set the vector elements.
-      - Always specify global locations of vector entries.
-      - Each processor can contribute any vector entries,
-        regardless of which processor "owns" them; any nonlocal
-        contributions will be transferred to the appropriate processor
-        during the assembly process.
-      - In this example, the flag ADD_VALUES indicates that all
-        contributions will be added together.
-  */
-  for ( i=0; i<N-rank; i++ ) {
-    ierr = VecSetValues(x,1,&i,&one,ADD_VALUES); CHKERRA(ierr);  
+  ierr = VecCreateSeq(PETSC_COMM_SELF,rank+4,&xlocal); CHKERRA(ierr);
+  ierr = PetscObjectPublish((PetscObject)xlocal);CHKERRA(ierr);
+  ierr = VecSet(&one,xlocal); CHKERRA(ierr);
+
+  while (1) {
+
+    /*
+       Access the vector entries and add to them
+    */
+    ierr = VecGetArray(x,&array);CHKERRA(ierr);
+    for ( i=0; i<n; i++ ) {
+      array[i]++;
+    }
+    ierr = VecRestoreArray(x,&array);CHKERRA(ierr);
+
+    ierr = VecGetArray(xlocal,&array);CHKERRA(ierr);
+    for ( i=0; i<n; i++ ) {
+      array[i]++;
+    }
+    ierr = VecRestoreArray(xlocal,&array);CHKERRA(ierr);
   }
 
-  /* 
-     Assemble vector, using the 2-step process:
-       VecAssemblyBegin(), VecAssemblyEnd()
-     Computations can be done while messages are in transition
-     by placing code between these two statements.
-  */
-  ierr = VecAssemblyBegin(x); CHKERRA(ierr);
-  ierr = VecAssemblyEnd(x); CHKERRA(ierr);
-
   /*
-      View the vector; then destroy it.
+        Destroy the vectors
   */
-  ierr = VecView(x,VIEWER_STDOUT_WORLD); CHKERRA(ierr);
   ierr = VecDestroy(x); CHKERRA(ierr);
+  ierr = VecDestroy(xlocal); CHKERRA(ierr);
 
   PetscFinalize();
   return 0;
