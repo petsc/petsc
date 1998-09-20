ed05da706688d80ef05298bce0bb1c152afbc715
Barry Smith <bsmith@mcs.anl.gov>
1998-09-19 20:55:58 -0600

    *** empty log message ***

--- src/fortran/custom/zdraw.c
+++ src/fortran/custom/zdraw.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: zdraw.c,v 1.19 1998/03/30 22:21:59 balay Exp balay $";
+static char vcid[] = "$Id: zdraw.c,v 1.20 1998/04/21 18:23:47 balay Exp bsmith $";
 #endif
 
 #include "src/fortran/custom/zpetsc.h"
@@ -50,7 +50,7 @@ static char vcid[] = "$Id: zdraw.c,v 1.19 1998/03/30 22:21:59 balay Exp balay $"
 extern "C" {
 #endif
 
-void drawtensorcontour_(Draw win,int *m,int *n,double *x,double *y,Vec V, int *__ierr )
+void drawtensorcontour_(Draw *win,int *m,int *n,double *x,double *y,Vec *V, int *__ierr )
 {
   double *xx,*yy;
   if (FORTRANNULLDOUBLE(x)) xx = PETSC_NULL; 
@@ -58,92 +58,75 @@ void drawtensorcontour_(Draw win,int *m,int *n,double *x,double *y,Vec V, int *_
   if (FORTRANNULLDOUBLE(y)) yy = PETSC_NULL; 
   else yy = y;
 
-  *__ierr = DrawTensorContour((Draw)PetscToPointer(win),*m,*n,xx,yy,
-	                      (Vec)PetscToPointer(V));
+  *__ierr = DrawTensorContour(*win,*m,*n,xx,yy,*V);
 }
 
 void viewerdrawgetdraw_(Viewer v,Draw *draw, int *__ierr )
 {
-  Draw d;
   PetscPatchDefaultViewers_Fortran(v);
-  *__ierr = ViewerDrawGetDraw(v,&d);
-  *(PetscFortranAddr*) draw = PetscFromPointer(d);
+  *__ierr = ViewerDrawGetDraw(v,draw);
 }
 
 void viewerdrawgetdrawlg_(Viewer v,DrawLG *drawlg, int *__ierr )
 {
-  DrawLG d;
   PetscPatchDefaultViewers_Fortran(v);
-  *__ierr = ViewerDrawGetDrawLG(v,&d);
-  *(PetscFortranAddr*) drawlg = PetscFromPointer(d);
+  *__ierr = ViewerDrawGetDrawLG(v,drawlg);
 }
 
-void drawstring_(Draw ctx,double* xl,double* yl,int* cl,CHAR text,
+void drawstring_(Draw *ctx,double* xl,double* yl,int* cl,CHAR text,
                int *__ierr, int len){
   char *t;
   FIXCHAR(text,len,t);
-  *__ierr = DrawString((Draw)PetscToPointer(ctx),*xl,*yl,*cl,t);
+  *__ierr = DrawString(*ctx,*xl,*yl,*cl,t);
   FREECHAR(text,t);
 }
-void drawstringvertical_(Draw ctx,double *xl,double *yl,int *cl,CHAR text, 
-                       int *__ierr,int len ){
+void drawstringvertical_(Draw *ctx,double *xl,double *yl,int *cl,CHAR text, 
+                       int *__ierr,int len )
+{
   char *t;
   FIXCHAR(text,len,t);
-  *__ierr = DrawStringVertical(
-	(Draw)PetscToPointer(ctx),*xl,*yl,*cl,t);
+  *__ierr = DrawStringVertical(*ctx,*xl,*yl,*cl,t);
   FREECHAR(text,t);
 }
 
-void drawdestroy_(Draw ctx, int *__ierr ){
-  *__ierr = DrawDestroy((Draw)PetscToPointer(ctx));
-  PetscRmPointer(ctx);
+void drawdestroy_(Draw *ctx, int *__ierr )
+{
+  *__ierr = DrawDestroy(*ctx);
 }
 
 void drawopenx_(MPI_Comm *comm,CHAR display,CHAR title,int *x,int *y,
                 int *w,int *h,Draw* inctx, int *__ierr,int len1,int len2 )
 {
-  Draw a;
   char *t1,*t2;
 
   FIXCHAR(display,len1,t1);
   FIXCHAR(title,len2,t2);
-  *__ierr = DrawOpenX((MPI_Comm)PetscToPointerComm( *comm),t1,t2,
-                       *x,*y,*w,*h,&a);
-  *(PetscFortranAddr*)inctx = PetscFromPointer(a);
+  *__ierr = DrawOpenX((MPI_Comm)PetscToPointerComm( *comm),t1,t2,*x,*y,*w,*h,inctx);
   FREECHAR(display,t1);
   FREECHAR(title,t2);
 }
 
-void drawlggetaxis_(DrawLG lg,DrawAxis *axis, int *__ierr )
+void drawlggetaxis_(DrawLG *lg,DrawAxis *axis, int *__ierr )
 {
-  DrawAxis a;
-  *__ierr = DrawLGGetAxis(
-	(DrawLG)PetscToPointer(lg),&a);
-  *(PetscFortranAddr*)axis = PetscFromPointer(a);
-}
-void drawlggetdraw_(DrawLG lg,Draw *win, int *__ierr )
-{
-  Draw a;
-  *__ierr = DrawLGGetDraw(
-	(DrawLG)PetscToPointer(lg),&a);
-  *(PetscFortranAddr*)win = PetscFromPointer(a);
+  *__ierr = DrawLGGetAxis(*lg,axis);
 }
 
-void drawlgdestroy_(DrawLG lg, int *__ierr )
+void drawlggetdraw_(DrawLG *lg,Draw *win, int *__ierr )
 {
-  *__ierr = DrawLGDestroy((DrawLG)PetscToPointer(lg));
-  PetscRmPointer(lg);
+  *__ierr = DrawLGGetDraw(*lg,win);
 }
 
-void drawlgcreate_(Draw win,int *dim,DrawLG *outctx, int *__ierr )
+void drawlgdestroy_(DrawLG *lg, int *__ierr )
 {
-  DrawLG lg;
-  *__ierr = DrawLGCreate(
-	(Draw)PetscToPointer(win),*dim,&lg);
-  *(PetscFortranAddr*)outctx = PetscFromPointer(lg);
+  *__ierr = DrawLGDestroy(*lg);
 }
 
-void drawaxissetlabels_(DrawAxis axis,CHAR top,CHAR xlabel,CHAR ylabel,
+void drawlgcreate_(Draw *win,int *dim,DrawLG *outctx, int *__ierr )
+{
+  *__ierr = DrawLGCreate(*win,*dim,outctx);
+}
+
+void drawaxissetlabels_(DrawAxis *axis,CHAR top,CHAR xlabel,CHAR ylabel,
                         int *__ierr,int len1,int len2,int len3 )
 {
   char *t1,*t2,*t3;
@@ -151,27 +134,23 @@ void drawaxissetlabels_(DrawAxis axis,CHAR top,CHAR xlabel,CHAR ylabel,
   FIXCHAR(top,len1,t1);
   FIXCHAR(xlabel,len2,t2);
   FIXCHAR(ylabel,len3,t3);
-  *__ierr = DrawAxisSetLabels(
-	 (DrawAxis)PetscToPointer(axis),t1,t2,t3);
+  *__ierr = DrawAxisSetLabels(*axis,t1,t2,t3);
   FREECHAR(top,t1);
   FREECHAR(xlabel,t2);
   FREECHAR(ylabel,t3);
 }
 
-void drawaxisdestroy_(DrawAxis axis, int *__ierr )
+void drawaxisdestroy_(DrawAxis *axis, int *__ierr )
 {
-  *__ierr = DrawAxisDestroy((DrawAxis)PetscToPointer(axis));
-  PetscRmPointer(axis);
+  *__ierr = DrawAxisDestroy(*axis);
 }
 
-void drawaxiscreate_(Draw win,DrawAxis *ctx, int *__ierr )
+void drawaxiscreate_(Draw *win,DrawAxis *ctx, int *__ierr )
 {
-  DrawAxis tmp;
-  *__ierr = DrawAxisCreate((Draw)PetscToPointer(win),&tmp);
-  *(PetscFortranAddr*)ctx = PetscFromPointer(tmp);
+  *__ierr = DrawAxisCreate(*win,ctx);
 }
 
-void drawgettitle_(Draw draw,CHAR title, int *__ierr,int len )
+void drawgettitle_(Draw *draw,CHAR title, int *__ierr,int len )
 {
   char *c3,*t;
   int  len3;
@@ -182,31 +161,29 @@ void drawgettitle_(Draw draw,CHAR title, int *__ierr,int len )
     c3   = title;
     len3 = len - 1;
 #endif
-  *__ierr = DrawGetTitle((Draw)PetscToPointer(draw),&t);
+  *__ierr = DrawGetTitle(*draw,&t);
   PetscStrncpy(c3,t,len3);
 }
 
-void drawsettitle_(Draw draw,CHAR title, int *__ierr,int len )
+void drawsettitle_(Draw *draw,CHAR title, int *__ierr,int len )
 {
   char *t1;
   FIXCHAR(title,len,t1);
-  *__ierr = DrawSetTitle((Draw)PetscToPointer(draw),t1);
+  *__ierr = DrawSetTitle(*draw,t1);
   FREECHAR(title,t1);
 }
 
-void drawappendtitle_(Draw draw,CHAR title, int *__ierr,int len )
+void drawappendtitle_(Draw *draw,CHAR title, int *__ierr,int len )
 {
   char *t1;
   FIXCHAR(title,len,t1);
-  *__ierr = DrawAppendTitle((Draw)PetscToPointer(draw),t1);
+  *__ierr = DrawAppendTitle(*draw,t1);
   FREECHAR(title,t1);
 }
 
-void drawgetpopup_(Draw draw,Draw *popup, int *__ierr )
+void drawgetpopup_(Draw *draw,Draw *popup, int *__ierr )
 {
-  Draw tmp;
-  *__ierr = DrawGetPopup((Draw)PetscToPointer(draw),&tmp);
-  *(PetscFortranAddr*) popup = PetscFromPointer(tmp);
+  *__ierr = DrawGetPopup(*draw,popup);
 }
 
 
