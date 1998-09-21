a9bfd7ee79024430f4ee67a12ce77c1f783b0249
Barry Smith <bsmith@mcs.anl.gov>
1998-09-20 19:44:12 -0600

    *** empty log message ***

--- src/fortran/custom/zsles.c
+++ src/fortran/custom/zsles.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: zsles.c,v 1.12 1997/07/09 20:55:52 balay Exp balay $";
+static char vcid[] = "$Id: zsles.c,v 1.13 1998/03/30 22:22:21 balay Exp bsmith $";
 #endif
 
 #include "src/fortran/custom/zpetsc.h"
@@ -26,48 +26,47 @@ static char vcid[] = "$Id: zsles.c,v 1.12 1997/07/09 20:55:52 balay Exp balay $"
 extern "C" {
 #endif
 
-void slessetoptionsprefix_(SLES sles,CHAR prefix, int *__ierr,int len ){
-  char *t;
-
-  FIXCHAR(prefix,len,t);
-  *__ierr = SLESSetOptionsPrefix((SLES)PetscToPointer(sles),t);
-  FREECHAR(prefix,t);
-}
-
-void slesappendoptionsprefix_(SLES sles,CHAR prefix, int *__ierr,int len ){
-  char *t;
-
-  FIXCHAR(prefix,len,t);
-  *__ierr = SLESAppendOptionsPrefix((SLES)PetscToPointer(sles),t);
-  FREECHAR(prefix,t);
-}
-
-void slesgetksp_(SLES sles,KSP *ksp, int *__ierr ){
-  KSP joe;
-  *__ierr = SLESGetKSP((SLES)PetscToPointer(sles),&joe);
-  *(PetscFortranAddr*) ksp = PetscFromPointer(joe);
-}
-
-void slesgetpc_(SLES sles,PC *pc, int *__ierr ){
-  PC joe;
-  *__ierr = SLESGetPC((SLES)PetscToPointer(sles),&joe);
-  *(PetscFortranAddr*) pc = PetscFromPointer(joe);
-}
-
-void slesdestroy_(SLES sles, int *__ierr )
+void slessetoptionsprefix_(SLES *sles,CHAR prefix, int *__ierr,int len )
 {
-  *__ierr = SLESDestroy((SLES)PetscToPointer(sles));
-  PetscRmPointer(sles);
+  char *t;
+
+  FIXCHAR(prefix,len,t);
+  *__ierr = SLESSetOptionsPrefix(*sles,t);
+  FREECHAR(prefix,t);
+}
+
+void slesappendoptionsprefix_(SLES *sles,CHAR prefix, int *__ierr,int len )
+{
+  char *t;
+
+  FIXCHAR(prefix,len,t);
+  *__ierr = SLESAppendOptionsPrefix(*sles,t);
+  FREECHAR(prefix,t);
+}
+
+void slesgetksp_(SLES *sles,KSP *ksp, int *__ierr )
+{
+  *__ierr = SLESGetKSP(*sles,ksp);
+}
+
+void slesgetpc_(SLES *sles,PC *pc, int *__ierr )
+{
+  *__ierr = SLESGetPC(*sles,pc);
+}
+
+void slesdestroy_(SLES *sles, int *__ierr )
+{
+  *__ierr = SLESDestroy(*sles);
 }
 
 void slescreate_(MPI_Comm *comm,SLES *outsles, int *__ierr )
 {
-  SLES sles;
-  *__ierr = SLESCreate((MPI_Comm)PetscToPointerComm( *comm ),&sles);
-  *(PetscFortranAddr*) outsles = PetscFromPointer(sles);
+  *__ierr = SLESCreate((MPI_Comm)PetscToPointerComm( *comm ),outsles);
 
 }
 
 #if defined(__cplusplus)
 }
 #endif
+
+
