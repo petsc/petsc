4880cde6be88f5c28aec1d7fd379817bf0a52d68
Barry Smith <bsmith@mcs.anl.gov>
1998-10-20 12:18:46 -0600

    *** empty log message ***

--- src/sles/ksp/impls/cg/cg.c
+++ src/sles/ksp/impls/cg/cg.c
@@ -1,8 +1,30 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: cg.c,v 1.76 1998/10/09 19:20:06 bsmith Exp bsmith $";
+static char vcid[] = "$Id: cg.c,v 1.77 1998/10/19 22:16:39 bsmith Exp bsmith $";
 #endif
 
-/*                       
+/*
+    This file implements the conjugate gradient method in PETSc as part of
+    KSP. You can use this as a starting point for implementing your own 
+    Krylov method that is not provided with PETSc.
+
+    The following basic routines are required for each Krylov method.
+        KSPCreate_XXX()          - Creates the Krylov context
+        KSPSetFromOptions_XXX()  - Sets runtime options
+        KSPSolve_XXX()           - Runs the Krylov method
+        KSPDestroy_XXX(0         - Destroys the Krylov context, freeing all 
+                                   memory it needed
+    Here the "_XXX" denotes a particular implementation, in this case 
+    we use _CG (e.g. KSPCreate_CG, KSPDestroy_CG). These routines are 
+    are actually called vai the common user interface routines
+    KSPSetType(), KSPSetFromOptions(), KSPSolve(), and KSPDestroy() so the
+    application code interface remains identical for all preconditioners.
+
+    Other basic routines for the KSP objects include
+        KSPSetUp_XXX()
+        KSPPrintHelp_XXX()        - Prints details of runtime options
+        KSPView_XXX()             - Prints details of solver being used.
+
+    Detailed notes:                         
     By default, this code implements the CG (Conjugate Gradient) method,
     which is valid for real symmetric (and complex Hermitian) positive
     definite matrices. Note that for the complex Hermitian case, the
@@ -18,11 +40,21 @@ static char vcid[] = "$Id: cg.c,v 1.76 1998/10/09 19:20:06 bsmith Exp bsmith $";
     Note, however, that the complex symmetric code is NOT valid for
     all such matrices ... and thus we don't recommend using this method.
 */
-#include <math.h>
+/*
+       cgctx.h defines the simple data structured used to store information
+    related to the type of matrix (e.g. complex symmetric) being solved and
+    data used during the optional Lanczo process used to compute eigenvalues
+*/
 #include "src/ksp/impls/cg/cgctx.h"       /*I "ksp.h" I*/
 extern int KSPComputeExtremeSingularValues_CG(KSP,double *,double *);
 extern int KSPComputeEigenvalues_CG(KSP,int,double *,double *,int *);
 
+/*
+     KSPSetUp_CG - Sets up the workspace needed by the CG method. 
+
+      This is called once, usually automatically by SLESSolve() or SLESSetUp()
+     but can be called directly by KSPSetUp()
+*/
 #undef __FUNC__  
 #define __FUNC__ "KSPSetUp_CG"
 int KSPSetUp_CG(KSP ksp)
@@ -31,16 +63,23 @@ int KSPSetUp_CG(KSP ksp)
   int    maxit = ksp->max_it,ierr;
 
   PetscFunctionBegin;
-  /* check user parameters and functions */
+  /* 
+       This implementation of CG only handles left preconditioning
+     so generate an error otherwise.
+  */
   if (ksp->pc_side == PC_RIGHT) {
-    SETERRQ(2,0,"no right preconditioning for KSPCG");
+    SETERRQ(2,0,"No right preconditioning for KSPCG");
   } else if (ksp->pc_side == PC_SYMMETRIC) {
-    SETERRQ(2,0,"no symmetric preconditioning for KSPCG");
+    SETERRQ(2,0,"No symmetric preconditioning for KSPCG");
   }
 
-  /* get work vectors from user code */
+  /* get work vectors needed by CG */
   ierr = KSPDefaultGetWork( ksp, 3 ); CHKERRQ(ierr);
 
+  /*
+     If user requested computations of eigenvalues then allocate work
+     work space needed
+  */
   if (ksp->calc_sings) {
     /* get space to store tridiagonal matrix for Lanczos */
     cgP->e = (Scalar *) PetscMalloc(2*(maxit+1)*sizeof(Scalar));CHKPTRQ(cgP->e);
@@ -55,6 +94,18 @@ int KSPSetUp_CG(KSP ksp)
   PetscFunctionReturn(0);
 }
 
+/*
+       KSPSolve_CG - This routine actually applies the conjugate gradient 
+    method
+
+   Input Parameter:
+.     ksp - the Krylov space object that was set to use conjugate gradient, by, for 
+            example, KSPCreate(MPI_Comm,KSP *ksp); KSPSetType(ksp,KSPCG);
+
+   Output Parameter:
+.     its - number of iterations used
+
+*/
 #undef __FUNC__  
 #define __FUNC__ "KSPSolve_CG"
 int  KSPSolve_CG(KSP ksp,int *its)
@@ -85,7 +136,7 @@ int  KSPSolve_CG(KSP ksp,int *its)
 #else
 #define VecXDot(x,y,a) \
   {if (cg->type == KSP_CG_HERMITIAN) {ierr = VecDot(x,y,a); CHKERRQ(ierr)} \
-   else                          {ierr = VecTDot(x,y,a); CHKERRQ(ierr)}}
+   else                              {ierr = VecTDot(x,y,a); CHKERRQ(ierr)}}
 #endif
 
   if (eigs) {e = cg->e; d = cg->d; e[0] = 0.0; b = 0.0; }
@@ -93,12 +144,12 @@ int  KSPSolve_CG(KSP ksp,int *its)
 
   ksp->its = 0;
   if (!ksp->guess_zero) {
-    ierr = MatMult(Amat,X,R); CHKERRQ(ierr);       /*   r <- b - Ax       */
+    ierr = MatMult(Amat,X,R); CHKERRQ(ierr);         /*   r <- b - Ax       */
     ierr = VecAYPX(&mone,B,R); CHKERRQ(ierr);
   } else { 
-    ierr = VecCopy(B,R); CHKERRQ(ierr);            /*     r <- b (x is 0) */
+    ierr = VecCopy(B,R); CHKERRQ(ierr);              /*     r <- b (x is 0) */
   }
-  ierr = PCApply(ksp->B,R,Z); CHKERRQ(ierr);       /*     z <- Br         */
+  ierr = PCApply(ksp->B,R,Z); CHKERRQ(ierr);         /*     z <- Br         */
   if (!ksp->avoidnorms) {
     if (pres) {
         ierr = VecNorm(Z,NORM_2,&dp); CHKERRQ(ierr); /*    dp <- z'*z       */
@@ -106,9 +157,9 @@ int  KSPSolve_CG(KSP ksp,int *its)
         ierr = VecNorm(R,NORM_2,&dp); CHKERRQ(ierr); /*    dp <- r'*r       */
     }
   }
-  cerr = (*ksp->converged)(ksp,0,dp,ksp->cnvP);
+  cerr = (*ksp->converged)(ksp,0,dp,ksp->cnvP);      /* test for convergence */
   if (cerr) {*its =  0; PetscFunctionReturn(0);}
-  KSPMonitor(ksp,0,dp);
+  KSPMonitor(ksp,0,dp);                              /* call any registered monitor routines */
   ksp->rnorm              = dp;
   if (history) history[0] = dp;
 
@@ -158,27 +209,39 @@ int  KSPSolve_CG(KSP ksp,int *its)
   else           *its = i+1;
   PetscFunctionReturn(0);
 }
+/*
+       KSPDestroy_CG - Frees all memory space used by the Krylov method
 
+*/
 #undef __FUNC__  
 #define __FUNC__ "KSPDestroy_CG" 
 int KSPDestroy_CG(KSP ksp)
 {
   KSP_CG *cg = (KSP_CG *) ksp->data;
+  int    ierr;
 
   PetscFunctionBegin;
-  /* free space used for Singularvalue calculations */
+  /* free space used for singular value calculations */
   if ( ksp->calc_sings ) {
     PetscFree(cg->e);
     PetscFree(cg->ee);
   }
 
-  KSPDefaultFreeWork( ksp );
+  ierr = KSPDefaultFreeWork( ksp );CHKERRQ(ierr);
   
-  /* free the context variables */
+  /* free the context variable */
   PetscFree(cg); 
   PetscFunctionReturn(0);
 }
 
+/*
+     KSPView_CG - Prints information about the current Krylov method being used
+
+      Currently this only prints information to a file (or stdout) about the 
+      symmetry of the problem. If your Krylov method has special options or 
+      flags that information should be printed here.
+
+*/
 #undef __FUNC__  
 #define __FUNC__ "KSPView_CG" 
 int KSPView_CG(KSP ksp,Viewer viewer)
@@ -207,6 +270,10 @@ int KSPView_CG(KSP ksp,Viewer viewer)
   PetscFunctionReturn(0);
 }
 
+/*
+    KSPPrint_Help - Prints a help message that indicates what run time options are
+                    available for this solver
+*/
 #undef __FUNC__  
 #define __FUNC__ "KSPPrintHelp_CG"
 static int KSPPrintHelp_CG(KSP ksp,char *p)
@@ -221,6 +288,11 @@ static int KSPPrintHelp_CG(KSP ksp,char *p)
   PetscFunctionReturn(0);
 }
 
+/*
+    KSPSetFromOptions_CG - Checks the options database for options related to the 
+                           conjugate gradient method. Any options checked here should
+                           also be indicated with the KSPPrintHelp_XXX() routine.
+*/ 
 #undef __FUNC__  
 #define __FUNC__ "KSPSetFromOptions_CG"
 int KSPSetFromOptions_CG(KSP ksp)
@@ -236,6 +308,13 @@ int KSPSetFromOptions_CG(KSP ksp)
   PetscFunctionReturn(0);
 }
 
+/*
+    KSPCGSetType_CG - This is an option that is SPECIFIC to this particular Krylov method.
+                      This routine is registered below in KSPCreate_CG() and called from the 
+                      routine KSPCGSetType() (see the file cgtype.c).
+
+        This must be wrapped in an EXTERN_C_BEGIN to be dynamically linkable in C++
+*/
 EXTERN_C_BEGIN
 #undef __FUNC__  
 #define __FUNC__ "KSPCGSetType_CG" 
@@ -250,6 +329,12 @@ int KSPCGSetType_CG(KSP ksp,KSPCGType type)
 }
 EXTERN_C_END
 
+/*
+    KSPCreate_CG - Creates the data structure for the Krylov method CG and sets the 
+       function pointers for all the routines it needs to call (KSPSolve_CG() etc)
+
+    It must be wrapped in EXTERN_C_BEGIN to be dynamically linkable in C++
+*/
 EXTERN_C_BEGIN
 #undef __FUNC__  
 #define __FUNC__ "KSPCreate_CG"
@@ -269,6 +354,11 @@ int KSPCreate_CG(KSP ksp)
   ksp->data                      = (void *) cg;
   ksp->pc_side                   = PC_LEFT;
   ksp->calc_res                  = 1;
+
+  /*
+       Sets the functions that are associated with this data structure 
+       (in C++ this is the same as defining virtual functions)
+  */
   ksp->ops->setup                = KSPSetUp_CG;
   ksp->ops->solve                = KSPSolve_CG;
   ksp->ops->destroy              = KSPDestroy_CG;
@@ -279,6 +369,11 @@ int KSPCreate_CG(KSP ksp)
   ksp->ops->buildsolution        = KSPDefaultBuildSolution;
   ksp->ops->buildresidual        = KSPDefaultBuildResidual;
 
+  /*
+      Attach the function KSPCGSetType_CG() to this object. The routine 
+      KSPCGSetType() checks for this attached function and calls it if it finds
+      it. (Sort of like a dynamic member function that can be added at run time
+  */
   ierr = PetscObjectComposeFunction((PetscObject)ksp,"KSPCGSetType_C","KSPCGSetType_CG",
                                      (void*)KSPCGSetType_CG);CHKERRQ(ierr);
   PetscFunctionReturn(0);
--- src/sles/ksp/impls/cg/cgctx.h
+++ src/sles/ksp/impls/cg/cgctx.h
@@ -1,11 +1,20 @@
+/* "$Id: cg.c,v 1.77 1998/10/19 22:16:39 bsmith Exp bsmith $"; */
+
 /*  
-    Private Context Structure for Conjugate Gradient 
+    Private Krylov Context Structure (KSP) for Conjugate Gradient 
+
+    This one is very simple. It contains a flag indicating the symmetry 
+   structure of the matrix and work space for (optionally) computing
+   eigenvalues.
+
 */
 
-#if !defined(__CG)
-#define __CG
+#if !defined(__CGCTX_H)
+#define __CGCTX_H
 
-#include "petsc.h"
+/*
+        Defines the basic KSP object
+*/
 #include "src/ksp/kspimpl.h"
 
 /*
