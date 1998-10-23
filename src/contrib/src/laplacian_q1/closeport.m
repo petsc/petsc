c56e8e3853158d23174976b61c1fe924d55bf0be
Adam Oberman <oberman@mcs.anl.gov>
1998-10-23 13:23:45 -0600

    Initial revision

---
+++ src/contrib/src/laplacian_q1/closeport.m
@@ -0,0 +1,6 @@
+function closeport(port)
+%
+%   closeport(port)
+%   Closes a PETSc port opened with openport()
+%see openport, receive
+disp('You must build the closeport mex file by doing make BOPT=g matlabcodes')
