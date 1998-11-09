306eef1ce0844f581eab5672677452faeb33c1be
Barry Smith <bsmith@mcs.anl.gov>
1998-11-09 13:32:10 -0600

    *** empty log message ***

--- src/snes/mf/wp.c
+++ src/snes/mf/wp.c
@@ -1,36 +1,19 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: snesmfjdef.c,v 1.3 1998/11/09 03:32:04 bsmith Exp bsmith $";
+static char vcid[] = "$Id: wp.c,v 1.1 1998/11/09 03:49:38 bsmith Exp bsmith $";
 #endif
 /*
-      Implements the default PETSc approach for computing the h 
+      Implements an alternative approach for computing the h
   parameter used with the finite difference based matrix free Jacobian.
 
-      To make your own: clone this file and modify for your needs.
+      This uses the Walker-Pernice strategy. See snesmfjdef.c for 
+  a full set of comments on the routines below.
 
-   Mandatory functions:
-   -------------------
-      MatSNESFDMFCompute_  - for a given point and direction computes h
+        h = error_rel * sqrt(1 + ||U||) / ||a||
 
-      MatSNESFDMFCreate_ - files in the MatSNESFDMF data structure
-                           for this particular implementation
+      Notes:
+   1) || U || does not change between linear iterations so can be reused
+   2) In GMRES || a || == 1 and so does not need to ever be computed
 
-      
-   Option functions:
-   ----------------
-      MatSNESFDMFView_ - prints information about the parameters being 
-                         used. This is called when SNESView() or -snes_view
-                         is used 
-
-      MatSNESFDMFPrintHelp_ - prints a help message on what options are
-                          available for this implementation
-
-      MatSNESFDMFSetFromOptions_ - checks the options database for options that 
-                               apply to this method.
-
-      MatSNESFDMFDestroy_ - frees any space allocated by the routines above
-
-   Function particular to this method:
-   ----------------------------------      
 */
 
 /*
@@ -40,25 +23,17 @@ static char vcid[] = "$Id: snesmfjdef.c,v 1.3 1998/11/09 03:32:04 bsmith Exp bsm
 */
 #include "src/snes/mf/snesmfj.h"   /*I  "snes.h"   I*/
 
-/*
-      The default method has one parameter that is used to 
-   "cutoff" very small values. This is stored in a data structure
-   that is only visible to this file. If your method has no parameters
-   it can omit this, if it has several simply reorganize the data structure.
-   The data structure is "hung-off" the MatSNESFDMF data structure in
-   the void *hctx; field.
-*/
 typedef struct {
-  double umin;          /* minimum allowable u'a value relative to |u|_1 */
-} MatSNESFDMFDefault;
-  
-extern int VecDot_Seq(Vec,Vec,Scalar *);
+  double     normUfact;                   /* previous sqrt(1.0 + || U ||) */
+  PetscTruth computenorma,computenormU;   
+} MatSNESFDMFWP;
+
 extern int VecNorm_Seq(Vec,NormType,double *);
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFCompute_Default"
+#define __FUNC__ "MatSNESFDMFCompute_WP"
 /*
-     MatSNESFDMFCompute_Default - Standard PETSc code for 
+     MatSNESFDMFCompute_WP - Standard PETSc code for 
    computing h with matrix-free finite differences.
 
   Input Parameters:
@@ -70,62 +45,56 @@ extern int VecNorm_Seq(Vec,NormType,double *);
 .   h - the scale computed
 
 */
-static int MatSNESFDMFCompute_Default(MatSNESFDMFCtx ctx,Vec U,Vec a,Scalar *h)
+static int MatSNESFDMFCompute_WP(MatSNESFDMFCtx ctx,Vec U,Vec a,Scalar *h)
 {
-  MatSNESFDMFDefault *hctx = (MatSNESFDMFDefault *) ctx->hctx;
+  MatSNESFDMFWP      *hctx = (MatSNESFDMFWP *) ctx->hctx;
   MPI_Comm           comm = ctx->comm;
-  double             norm, sum, umin = hctx->umin;
-  Scalar             dot;
+  double             normU, ovalues[2],values[2];
+  double             norma = 1.0, normUfact = hctx->normUfact;
   int                ierr;
-  Scalar             ovalues[3],values[3];
 
-  /*
-     This algorithm requires 2 norms and 1 inner product. Rather than
-     use directly the VecNorm() and VecDot() routines (and thus have 
-     three separate collective operations, we use the sequential routines
-     and manually call MPI for the collective phase.
+  if (hctx->computenorma && (hctx->computenormU || !ctx->ncurrenth)) {
+    /*
+       This algorithm requires 2 norms. Rather than
+       use directly the VecNorm()routine (and thus have 
+       two separate collective operations, we use the sequential routines
+       and manually call MPI for the collective phase.
+ 
+       We sparately log the VecNorm() stages to get 
+       accurate profiling. 
+    */
 
-     We sparately log the VecDot() and VecNorm() stages to get 
-     accurate profiling. Note that the collective time for the VecDot()
-     is "free", because it piggy-backs on the collective time for the norm
-  */
-
-
-  PLogEventBegin(VEC_Dot,U,a,0,0);
-  ierr = VecDot_Seq(U,a,ovalues); CHKERRQ(ierr);
-  PLogEventEnd(VEC_Dot,U,a,0,0);
-  PLogEventBegin(VEC_Norm,a,0,0,0);
-  ierr = VecNorm_Seq(a,NORM_1,(double *)(ovalues+1)); CHKERRQ(ierr);
-  ierr = VecNorm_Seq(a,NORM_2,(double *)(ovalues+2)); CHKERRQ(ierr);
-  ovalues[2] = ovalues[2]*ovalues[2];
-  PLogEventBarrierBegin(VEC_NormBarrier,0,0,0,0,comm);
+    PLogEventBegin(VEC_Norm,a,0,0,0);
+    ierr = VecNorm_Seq(U,NORM_2,&normU); CHKERRQ(ierr);
+    ierr = VecNorm_Seq(a,NORM_2,&norma); CHKERRQ(ierr);
+    ovalues[0] = normU*normU;
+    ovalues[1] = norma*norma;
+    PLogEventBarrierBegin(VEC_NormBarrier,0,0,0,0,comm);
 #if !defined(USE_PETSC_COMPLEX)
-  ierr = MPI_Allreduce(ovalues,values,3,MPI_DOUBLE,MPI_SUM,comm );CHKERRQ(ierr);
+    ierr = MPI_Allreduce(ovalues,values,2,MPI_DOUBLE,MPI_SUM,comm );CHKERRQ(ierr);
 #else
+    ierr = MPI_Allreduce(ovalues,values,4,MPI_DOUBLE,MPI_SUM,comm );CHKERRQ(ierr);
 #endif
-  PLogEventBarrierEnd(VEC_NormBarrier,0,0,0,0,comm);
-  dot = values[0]; sum = PetscReal(values[1]); norm = sqrt(PetscReal(values[2]));
-  PLogEventEnd(VEC_Norm,a,0,0,0);
+    PLogEventBarrierEnd(VEC_NormBarrier,0,0,0,0,comm);
+    normU = sqrt(values[0]);
+    norma = sqrt(values[1]);
+    PLogEventEnd(VEC_Norm,a,0,0,0);
+    hctx->normUfact = normUfact = sqrt(1.0+normU);
+  } else if (hctx->computenormU || !ctx->ncurrenth) {
+    ierr = VecNorm(U,NORM_2,&normU); CHKERRQ(ierr);
+    hctx->normUfact = normUfact = sqrt(1.0+normU);
+  } else if (hctx->computenorma) {
+    ierr = VecNorm(a,NORM_2,&norma); CHKERRQ(ierr);
+  }
 
-  /* 
-     Safeguard for step sizes that are "too small"
-  */
-  if (sum == 0.0) {dot = 1.0; norm = 1.0;}
-#if defined(USE_PETSC_COMPLEX)
-  else if (PetscAbsScalar(dot) < umin*sum && PetscReal(dot) >= 0.0) dot = umin*sum;
-  else if (PetscAbsScalar(dot) < 0.0 && PetscReal(dot) > -umin*sum) dot = -umin*sum;
-#else
-  else if (dot < umin*sum && dot >= 0.0) dot = umin*sum;
-  else if (dot < 0.0 && dot > -umin*sum) dot = -umin*sum;
-#endif
-  *h = ctx->error_rel*dot/(norm*norm);
+  *h = ctx->error_rel*normUfact/norma;
   PetscFunctionReturn(0);
 } 
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFView_Default"
+#define __FUNC__ "MatSNESFDMFView_WP"
 /*
-   MatSNESFDMFView_Default - Prints information about this particular 
+   MatSNESFDMFView_WP - Prints information about this particular 
      method for computing h. Note that this does not print the general
      information about the matrix free, that is printed by the calling
      routine.
@@ -135,24 +104,21 @@ static int MatSNESFDMFCompute_Default(MatSNESFDMFCtx ctx,Vec U,Vec a,Scalar *h)
 -   viewer - the PETSc viewer
 
 */   
-static int MatSNESFDMFView_Default(MatSNESFDMFCtx ctx,Viewer viewer)
+static int MatSNESFDMFView_WP(MatSNESFDMFCtx ctx,Viewer viewer)
 {
-  FILE               *fd;
-  ViewerType         vtype;
-  MatSNESFDMFDefault *hctx = (MatSNESFDMFDefault *)ctx->hctx;
-  int                ierr;
+  FILE          *fd;
+  ViewerType    vtype;
+  MatSNESFDMFWP *hctx = (MatSNESFDMFWP *)ctx->hctx;
+  int           ierr;
 
   PetscFunctionBegin;
   ierr = ViewerGetType(viewer,&vtype); CHKERRQ(ierr);
   ierr = ViewerASCIIGetPointer(viewer,&fd); CHKERRQ(ierr);
-  
-  /*
-     Currently this only handles the ascii file viewers, others
-     could be added, but for this type of object other viewers
-     make less sense
-  */
   if (vtype == ASCII_FILE_VIEWER || vtype == ASCII_FILES_VIEWER) {
-    PetscFPrintf(ctx->comm,fd,"    umin=%g (minimum iterate parameter)\n",hctx->umin);  
+    if (hctx->computenorma) PetscFPrintf(ctx->comm,fd,"    Computes normA\n");  
+    else                    PetscFPrintf(ctx->comm,fd,"    Does not compute normA\n");  
+    if (hctx->computenormU) PetscFPrintf(ctx->comm,fd,"    Computes normU\n");  
+    else                    PetscFPrintf(ctx->comm,fd,"    Does not compute normU\n");  
   } else {
     SETERRQ(1,1,"Viewer type not supported for this object");
   }    
@@ -160,58 +126,65 @@ static int MatSNESFDMFView_Default(MatSNESFDMFCtx ctx,Viewer viewer)
 }
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFPrintHelp_Default"
+#define __FUNC__ "MatSNESFDMFPrintHelp_WP"
 /*
-   MatSNESFDMFPrintHelp_Default - Prints a list of all the options 
+   MatSNESFDMFPrintHelp_WP - Prints a list of all the options 
       this particular method supports.
 
   Input Parameter:
 .  ctx - the matrix free context
 
 */
-static int MatSNESFDMFPrintHelp_Default(MatSNESFDMFCtx ctx)
+static int MatSNESFDMFPrintHelp_WP(MatSNESFDMFCtx ctx)
 {
-  char*              p;
-  MatSNESFDMFDefault *hctx = (MatSNESFDMFDefault *)ctx->hctx;
-  int                ierr;
+  char*         p;
+  MatSNESFDMFWP *hctx = (MatSNESFDMFWP *)ctx->hctx;
+  int           ierr;
 
   PetscFunctionBegin;
   ierr = PetscObjectGetOptionsPrefix((PetscObject)ctx->snes,&p); CHKERRQ(ierr);
   if (!p) p = "";
-  (*PetscHelpPrintf)(ctx->comm,"   -%ssnes_mf_umin <umin> see users manual (default %g)\n",p,hctx->umin);
+  (*PetscHelpPrintf)(ctx->comm,"   -%ssnes_mf_compute_norma <true or false>\n",p);
+  (*PetscHelpPrintf)(ctx->comm,"   -%ssnes_mf_compute_normu <true or false>\n",p);
+
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFSetFromOptions_Default"
+#define __FUNC__ "MatSNESFDMFSetFromOptions_WP"
 /*
-   MatSNESFDMFSetFromOptions_Default - Looks in the options database for 
+   MatSNESFDMFSetFromOptions_WP - Looks in the options database for 
      any options appropriate for this method
 
   Input Parameter:
 .  ctx - the matrix free context
 
 */
-static int MatSNESFDMFSetFromOptions_Default(MatSNESFDMFCtx ctx)
+static int MatSNESFDMFSetFromOptions_WP(MatSNESFDMFCtx ctx)
 {
-  char*              p;
-  int                ierr,flag;
-  double             umin;
+  int        flag, ierr;
+  PetscTruth set;
+  char       *p;
 
   PetscFunctionBegin;
   ierr = PetscObjectGetOptionsPrefix((PetscObject)ctx->snes,&p); CHKERRQ(ierr);
-  ierr = OptionsGetDouble(p,"-snes_mf_umin",&umin,&flag);CHKERRQ(ierr);
+  ierr = OptionsGetLogical(p,"-snes_mf_compute_norma",&set,&flag);CHKERRQ(ierr);
   if (flag) {
-    ierr = MatSNESFDMFDefaultSetUmin(ctx->mat,umin);CHKERRQ(ierr);
+    ierr = MatSNESFDMFWPSetComputeNormA(ctx->mat,set);CHKERRQ(ierr);
   }
+  ierr = OptionsGetLogical(p,"-snes_mf_compute_normu",&set,&flag);CHKERRQ(ierr);
+  if (flag) {
+    ierr = MatSNESFDMFWPSetComputeNormU(ctx->mat,set);CHKERRQ(ierr);
+  }
+
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFDestroy_Default"
+#define __FUNC__ "MatSNESFDMFDestroy_WP"
 /*
-   MatSNESFDMFDestroy_Default - Frees the space allocated by 
-       MatSNESFDMFCreate_Default(). 
+   MatSNESFDMFDestroy_WP - Frees the space allocated by 
+       MatSNESFDMFCreate_WP(). 
 
   Input Parameter:
 .  ctx - the matrix free context
@@ -219,7 +192,7 @@ static int MatSNESFDMFSetFromOptions_Default(MatSNESFDMFCtx ctx)
    Notes: does not free the ctx, that is handled by the calling routine
 
 */
-static int MatSNESFDMFDestroy_Default(MatSNESFDMFCtx ctx)
+static int MatSNESFDMFDestroy_WP(MatSNESFDMFCtx ctx)
 {
   PetscFunctionBegin;
   PetscFree(ctx->hctx);
@@ -228,38 +201,35 @@ static int MatSNESFDMFDestroy_Default(MatSNESFDMFCtx ctx)
 
 EXTERN_C_BEGIN
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFDefaultSetUmin_Private"
-/*
-      The following two routines use the PetscObjectCompose() and PetscObjectQuery()
-   mechanism to allow the user to change the Umin parameter used in this method.
-*/
-int MatSNESFDMFDefaultSetUmin_Private(Mat mat,double umin)
+#define __FUNC__ "MatSNESFDMFWPSetComputeNormA_P"
+int MatSNESFDMFWPSetComputeNormA_P(Mat mat,PetscTruth flag)
 {
-  MatSNESFDMFCtx     ctx;
-  MatSNESFDMFDefault *hctx;
-  int                ierr;
+  MatSNESFDMFCtx ctx;
+  MatSNESFDMFWP  *hctx;
+  int            ierr;
 
   PetscFunctionBegin;
   ierr = MatShellGetContext(mat,(void **)&ctx);CHKERRQ(ierr);
   if (!ctx) {
-    SETERRQ(1,1,"MatSNESFDMFDefaultSetUmin() attached to non-shell matrix");
+    SETERRQ(1,1,"MatSNESFDMFWPSetComputeNormA() attached to non-shell matrix");
   }
-  hctx = (MatSNESFDMFDefault *) ctx->hctx;
-  hctx->umin = umin;
+  hctx               = (MatSNESFDMFWP *) ctx->hctx;
+  hctx->computenorma = flag;
 
  PetscFunctionReturn(0);
 } 
 EXTERN_C_END
 
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFDefaultSetUmin"
+#define __FUNC__ "MatSNESFDMFWPSetComputeNormA"
 /*@
-    MatSNESFDMFDefaultSetUmin - Sets the "umin" parameter used by the default
-             PETSc routine for computing h
+    MatSNESFDMFWPSetComputeNormA - Sets whether it computes the ||a|| used by the WP
+             PETSc routine for computing h. With GMRES since the ||a|| is always
+             one, you can save communication by setting this to false.
 
   Input Parameters:
 +   A - the matrix created with MatCreateSNESFDMF()
--   umin - the parameter
+-   flag - PETSC_TRUE causes it to compute ||a||, PETSC_FALSE assumes it is 1.
 
   Notes:
    See the manual page for MatCreateSNESFDMF() for a complete description of the
@@ -268,60 +238,112 @@ EXTERN_C_END
 .seealso: MatSNESFDMFSetFunctionError(), MatCreateSNESFDMF()
 
 @*/
-int MatSNESFDMFDefaultSetUmin(Mat A,double umin)
+int MatSNESFDMFWPSetComputeNormA(Mat A,PetscTruth flag)
 {
-  int ierr, (*f)(Mat,double);
+  int ierr, (*f)(Mat,PetscTruth);
 
   PetscFunctionBegin;
   PetscValidHeaderSpecific(A,MAT_COOKIE);
-  ierr = PetscObjectQueryFunction((PetscObject)A,"MatSNESFDMFDefaultSetUmin_C",(void **)&f); CHKERRQ(ierr);
+  ierr = PetscObjectQueryFunction((PetscObject)A,"MatSNESFDMFWPSetComputeNormA_C",(void **)&f);CHKERRQ(ierr);
   if (f) {
-    ierr = (*f)(A,umin);CHKERRQ(ierr);
+    ierr = (*f)(A,flag);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
 
 EXTERN_C_BEGIN
 #undef __FUNC__  
-#define __FUNC__ "MatSNESFDMFCreate_Default"
+#define __FUNC__ "MatSNESFDMFWPSetComputeNormU_P"
+int MatSNESFDMFWPSetComputeNormU_P(Mat mat,PetscTruth flag)
+{
+  MatSNESFDMFCtx ctx;
+  MatSNESFDMFWP  *hctx;
+  int            ierr;
+
+  PetscFunctionBegin;
+  ierr = MatShellGetContext(mat,(void **)&ctx);CHKERRQ(ierr);
+  if (!ctx) {
+    SETERRQ(1,1,"MatSNESFDMFWPSetComputeNormU() attached to non-shell matrix");
+  }
+  hctx               = (MatSNESFDMFWP *) ctx->hctx;
+  hctx->computenormU = flag;
+
+ PetscFunctionReturn(0);
+} 
+EXTERN_C_END
+
+#undef __FUNC__  
+#define __FUNC__ "MatSNESFDMFWPSetComputeNormU"
+/*@
+    MatSNESFDMFWPSetComputeNormU - Sets whether it computes the ||U|| used by the WP
+             PETSc routine for computing h. With any Krylov solver this need only 
+             be computed during the first iteration and kept for later.
+
+  Input Parameters:
++   A - the matrix created with MatCreateSNESFDMF()
+-   flag - PETSC_TRUE causes it to compute ||U||, PETSC_FALSE uses the previous value
+
+  Notes:
+   See the manual page for MatCreateSNESFDMF() for a complete description of the
+   algorithm used to compute h.
+
+.seealso: MatSNESFDMFSetFunctionError(), MatCreateSNESFDMF()
+
+@*/
+int MatSNESFDMFWPSetComputeNormU(Mat A,PetscTruth flag)
+{
+  int ierr, (*f)(Mat,PetscTruth);
+
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(A,MAT_COOKIE);
+  ierr = PetscObjectQueryFunction((PetscObject)A,"MatSNESFDMFWPSetComputeNormU_C",(void **)&f);CHKERRQ(ierr);
+  if (f) {
+    ierr = (*f)(A,flag);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNC__  
+#define __FUNC__ "MatSNESFDMFCreate_WP"
 /*
-     MatSNESFDMFCreate_Default - Standard PETSc code for 
+     MatSNESFDMFCreate_WP - Standard PETSc code for 
    computing h with matrix-free finite differences.
 
    Input Parameter:
 .  ctx - the matrix free context created by MatSNESFDMFCreate()
 
 */
-int MatSNESFDMFCreate_Default(MatSNESFDMFCtx ctx)
+int MatSNESFDMFCreate_WP(MatSNESFDMFCtx ctx)
 {
-  MatSNESFDMFDefault *hctx;
-  int                ierr;
+  int           ierr;
+  MatSNESFDMFWP *hctx;
 
   PetscFunctionBegin;
 
   /* allocate my own private data structure */
-  hctx                     = (MatSNESFDMFDefault *)PetscMalloc(sizeof(MatSNESFDMFDefault));CHKPTRQ(hctx);
+  hctx                     = (MatSNESFDMFWP *)PetscMalloc(sizeof(MatSNESFDMFWP));CHKPTRQ(hctx);
   ctx->hctx                = (void *) hctx;
-  /* set a default for my parameter */
-  hctx->umin               = 1.e-6;
+  hctx->computenormU       = PETSC_TRUE;
+  hctx->computenorma       = PETSC_TRUE;
 
   /* set the functions I am providing */
-  ctx->ops->compute        = MatSNESFDMFCompute_Default;
-  ctx->ops->destroy        = MatSNESFDMFDestroy_Default;
-  ctx->ops->view           = MatSNESFDMFView_Default;  
-  ctx->ops->printhelp      = MatSNESFDMFPrintHelp_Default;  
-  ctx->ops->setfromoptions = MatSNESFDMFSetFromOptions_Default;  
+  ctx->ops->compute        = MatSNESFDMFCompute_WP;
+  ctx->ops->destroy        = MatSNESFDMFDestroy_WP;
+  ctx->ops->view           = MatSNESFDMFView_WP;  
+  ctx->ops->printhelp      = MatSNESFDMFPrintHelp_WP;  
+  ctx->ops->setfromoptions = MatSNESFDMFSetFromOptions_WP;  
 
-  ierr = PetscObjectComposeFunction((PetscObject)ctx->mat,"MatSNESFDMFDefaultSetUmin_C",
-                            "MatSNESFDMFDefaultSetUmin_Private",
-                            (void *) MatSNESFDMFDefaultSetUmin_Private);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunction((PetscObject)ctx->mat,"MatSNESFDMFWPSetComputeNormA_C",
+                            "MatSNESFDMFWPSetComputeNormA_P",
+                            (void *) MatSNESFDMFWPSetComputeNormA_P);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunction((PetscObject)ctx->mat,"MatSNESFDMFWPSetComputeNormU_C",
+                            "MatSNESFDMFWPSetComputeNormU_P",
+                            (void *) MatSNESFDMFWPSetComputeNormU_P);CHKERRQ(ierr);
+
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
-
-
-
-
 
 
 
