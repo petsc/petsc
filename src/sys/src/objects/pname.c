1bb29efb35c7796a7895fabf54ac4362cc5bf166
Barry Smith <bsmith@mcs.anl.gov>
1998-11-11 22:16:10 -0600

    *** empty log message ***

--- src/sys/src/objects/pname.c
+++ src/sys/src/objects/pname.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: pname.c,v 1.15 1998/07/23 22:46:55 bsmith Exp balay $";
+static char vcid[] = "$Id: pname.c,v 1.16 1998/08/26 22:01:46 balay Exp bsmith $";
 #endif
 
 #include "petsc.h"        /*I    "petsc.h"   I*/
@@ -61,3 +61,51 @@ int PetscObjectPublish(PetscObject obj)
   }
   PetscFunctionReturn(0);
 }
+
+/*
+    Publishes the common header part of any PETSc object. 
+ */
+#undef __FUNC__  
+#define __FUNC__ "PetscObjectPublishHeaderBegin"
+int PetscObjectPublishBaseBegin(PetscObject obj,char *memname)
+{
+#if defined(HAVE_AMS)
+  AMS_Memory amem;
+  AMS_Comm   acomm;
+  int        ierr;
+
+  PetscFunctionBegin;
+  ierr      = ViewerAMSGetAMSComm(VIEWER_AMS_(obj->comm),&acomm);CHKERRQ(ierr);
+  ierr      = AMS_Memory_create(acomm,memname,&amem);CHKERRQ(ierr);
+  obj->amem = (int) amem;
+
+  ierr = AMS_Memory_take_access(amem);CHKERRQ(ierr); 
+  ierr = AMS_Memory_add_field(amem,"Type",&obj->type_name,1,AMS_STRING,AMS_READ,
+                                AMS_COMMON,AMS_REDUCT_UNDEF);CHKERRQ(ierr);
+
+#else
+  PetscFunctionBegin;
+#endif
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "PetscObjectPublishHeaderEnd"
+int PetscObjectPublishBaseEnd(PetscObject obj)
+{
+#if defined(HAVE_AMS)
+  AMS_Memory amem = (AMS_Memory) obj->amem;
+  int        ierr;
+
+  PetscFunctionBegin;
+
+  if (amem < 0) SETERRQ(1,1,"Called without a call to PetscObjectPublishBaseBegin()");
+  ierr = AMS_Memory_publish(amem);CHKERRQ(ierr);
+  ierr = AMS_Memory_grant_access(amem);CHKERRQ(ierr);
+
+#else
+  PetscFunctionBegin;
+#endif
+  PetscFunctionReturn(0);
+}
+
