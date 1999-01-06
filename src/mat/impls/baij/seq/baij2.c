253ffaf7dd6c1b73ba1cabf4e6bfc84f79e98d36
Barry Smith <bsmith@mcs.anl.gov>
1999-01-06 09:55:07 -0600

    *** empty log message ***

--- src/mat/impls/baij/seq/baij2.c
+++ src/mat/impls/baij/seq/baij2.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: baij2.c,v 1.35 1999/01/04 21:50:55 bsmith Exp balay $";
+static char vcid[] = "$Id: baij2.c,v 1.36 1999/01/06 15:12:20 balay Exp bsmith $";
 #endif
 
 #include "sys.h"
@@ -1078,7 +1078,7 @@ int MatNorm_SeqBAIJ(Mat A,NormType type,double *norm)
   Mat_SeqBAIJ *a = (Mat_SeqBAIJ *) A->data;
   MatScalar   *v = a->a;
   double      sum = 0.0;
-  int         i,k,bs = a->bs, nz=a->nz,bs2=a->bs2;
+  int         i,j,k,bs = a->bs, nz=a->nz,bs2=a->bs2;
 
   PetscFunctionBegin;
   if (type == NORM_FROBENIUS) {
@@ -1091,8 +1091,6 @@ int MatNorm_SeqBAIJ(Mat A,NormType type,double *norm)
     }
     *norm = sqrt(sum);
   }  else if (type == NORM_INFINITY) { /* maximum row sum */
-    int j;
-
     *norm = 0.0;
     for ( k=0; k<bs; k++ ) {
       for ( j=0; j<a->m; j++ ) {
