da9e4dfa6c65477858ec1ff13f741b7980be96a6
Barry Smith <bsmith@mcs.anl.gov>
1999-01-22 16:03:35 -0600

    Initial revision

---
+++ src/sys/src/viewer/interface/dlregis.c
@@ -0,0 +1,55 @@
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: dlregis.c,v 1.9 1998/10/19 22:16:30 bsmith Exp bsmith $";
+#endif
+
+#include "sles.h"
+
+EXTERN_C_BEGIN
+#undef __FUNC__  
+#define __FUNC__ "DLLibraryRegister"
+/*
+  DLLibraryRegister - This function is called when the dynamic library it is in is opened.
+
+  This one registers all the KSP and PC methods that are in the basic PETSc libpetscsles
+  library.
+
+  Input Parameter:
+  path - library path
+ */
+int DLLibraryRegister(char *path)
+{
+  int ierr;
+
+  ierr = PetscInitializeNoArguments(); if (ierr) return 1;
+
+  /*
+      If we got here then PETSc was properly loaded
+  */
+  ierr = KSPRegisterAll(path); CHKERRQ(ierr);
+  ierr = PCRegisterAll(path); CHKERRQ(ierr);
+  return 0;
+}
+EXTERN_C_END
+
+/* --------------------------------------------------------------------------*/
+static char *contents = "PETSc Krylov subspace method and preconditioner library. Contains:\n\
+     GMRES, PCG, Bi-CG-stab, ...\n\
+     Jacobi, ILU, Block Jacobi, LU, Additive Schwarz, ...\n";
+
+static char *authors = PETSC_AUTHOR_INFO;
+static char *version = PETSC_VERSION_NUMBER;
+
+/* --------------------------------------------------------------------------*/
+EXTERN_C_BEGIN
+#undef __FUNC__  
+#define __FUNC__ "DLLibraryInfo"
+int DLLibraryInfo(char *path,char *type,char **mess) 
+{ 
+  if (!PetscStrcmp(type,"Contents"))     *mess = contents;
+  else if (!PetscStrcmp(type,"Authors")) *mess = authors;
+  else if (!PetscStrcmp(type,"Version")) *mess = version;
+  else *mess = 0;
+
+  return 0;
+}
+EXTERN_C_END
