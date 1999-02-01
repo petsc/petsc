cf057be8138563a69f6f7792cd02dae08aedc295
Barry Smith <bsmith@mcs.anl.gov>
1999-02-01 16:50:10 -0600

    Initial revision

---
+++ src/sys/src/viewer/interface/dlregispetsc.c
@@ -0,0 +1,54 @@
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: dlregis.c,v 1.2 1999/01/27 19:49:10 bsmith Exp $";
+#endif
+
+#include "sles.h"
+
+EXTERN_C_BEGIN
+#undef __FUNC__  
+#define __FUNC__ "DLLibraryRegister"
+/*
+  DLLibraryRegister - This function is called when the dynamic library it is in is opened.
+
+  This one registers all the KSP and PC methods that are in the basic PETSc libpetscsles
+  library.
+
+  Input Parameter:
+  path - library path
+ */
+int DLLibraryRegister(char *path)
+{
+  int ierr;
+
+  ierr = PetscInitializeNoArguments(); if (ierr) return 1;
+
+  /*
+      If we got here then PETSc was properly loaded
+  */
+  ierr = DrawRegisterAll(path); CHKERRQ(ierr);
+  ierr = ViewerRegisterAll(path); CHKERRQ(ierr);
+  return 0;
+}
+EXTERN_C_END
+
+/* --------------------------------------------------------------------------*/
+static char *contents = "PETSc Graphics and Viewer libraries. \n\
+     ASCII, Binary, Sockets, X-windows, ...\n";
+
+static char *authors = PETSC_AUTHOR_INFO;
+static char *version = PETSC_VERSION_NUMBER;
+
+/* --------------------------------------------------------------------------*/
+EXTERN_C_BEGIN
+#undef __FUNC__  
+#define __FUNC__ "DLLibraryInfo"
+int DLLibraryInfo(char *path,char *type,char **mess) 
+{ 
+  if (!PetscStrcmp(type,"Contents"))     *mess = contents;
+  else if (!PetscStrcmp(type,"Authors")) *mess = authors;
+  else if (!PetscStrcmp(type,"Version")) *mess = version;
+  else *mess = 0;
+
+  return 0;
+}
+EXTERN_C_END
