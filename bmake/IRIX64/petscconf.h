a691d99065c782caafe23264c9f5a45e95e3a50f
Satish Balay <balay@mcs.anl.gov>
1999-02-15 12:41:33 -0600

    *** empty log message ***

--- bmake/IRIX64/petscconf.h
+++ bmake/IRIX64/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.20 1998/10/19 22:14:14 bsmith Exp bsmith $"
+"$Id: petscconf.h,v 1.21 1999/02/08 22:22:33 bsmith Exp balay $"
 "Defines the configuration for this machine"
 #endif
 
@@ -25,8 +25,6 @@
 #define SIZEOF_VOIDP 8
 #define SIZEOF_INT 4
 #define SIZEOF_DOUBLE 8
-
-#define HAVE_IRIXF90
 
 #define WORDS_BIGENDIAN 1
 
