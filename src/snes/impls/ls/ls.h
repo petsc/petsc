9825173abfb6cca08a902f7175d60c76b1920a71
Lois Curfman McInnes <curfman@mcs.anl.gov>
1999-02-28 20:36:41 -0600

    *** empty log message ***

--- src/snes/impls/ls/ls.h
+++ src/snes/impls/ls/ls.h
@@ -1,4 +1,4 @@
-/* $Id: ls.h,v 1.5 1996/08/08 14:46:50 bsmith Exp curfman $ */
+/* $Id: ls.h,v 1.6 1998/04/21 23:48:28 curfman Exp curfman $ */
 
 /* 
    Private context for a Newton line search method for solving
@@ -10,11 +10,14 @@
 #include "src/snes/snesimpl.h"
 
 typedef struct {
-  int (*LineSearch)(SNES,Vec,Vec,Vec,Vec,Vec,double,double*,double*,int*);
-/* --------------- Parameters used by line search method ----------------- */
+  int    (*LineSearch)(SNES,Vec,Vec,Vec,Vec,Vec,double,double*,double*,int*);
+  /* --------------- Parameters used by line search method ----------------- */
   double alpha;			/* used to determine sufficient reduction */
   double maxstep;               /* maximum step size */
   double steptol;               /* step convergence tolerance */
+  int    (*CheckStep)(SNES,Vec,int*,void*); /* step-checking routine (optional) */
+  void   *checkP;                           /* user-defined step-checking context */
 } SNES_LS;
 
 #endif
+
