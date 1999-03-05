b105ea75cbc3762984ee98c1138cca29dbee0117
Barry Smith <bsmith@mcs.anl.gov>
1999-03-04 22:17:57 -0600

    *** empty log message ***

--- src/dm/da/src/dagtol.c
+++ src/dm/da/src/dagtol.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: dagtol.c,v 1.13 1998/05/29 20:39:21 bsmith Exp bsmith $";
+static char vcid[] = "$Id: dagtol.c,v 1.14 1999/01/31 16:11:27 bsmith Exp bsmith $";
 #endif
  
 /*
@@ -83,6 +83,96 @@ int DAGlobalToLocalEnd(DA da,Vec g, InsertMode mode,Vec l)
   PetscValidHeaderSpecific(l,VEC_COOKIE);
   PetscValidHeaderSpecific(g,VEC_COOKIE);
   ierr = VecScatterEnd(g,l,mode,SCATTER_FORWARD,da->gtol); CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "DAGlobalToNaturalBegin"
+/*@
+   DAGlobalToNaturalBegin - Maps values from the global vector to a global vector
+   in the "natural" grid ordering. Must be followed by 
+   DAGlobalToNaturalEnd() to complete the exchange.
+
+   Collective on DA
+
+   Input Parameters:
++  da - the distributed array context
+.  g - the global vector
+-  mode - one of INSERT_VALUES or ADD_VALUES
+
+   Output Parameter:
+.  l  - the natural ordering values
+
+   Level: advanced
+
+   Notes:
+   The global and natrual vectors used here need not be the same as those
+   obtained from DACreateGlobalVector() and DACreateNaturalVector(), BUT they
+   must have the same parallel data layout; they could, for example, be 
+   obtained with VecDuplicate() from the DA originating vectors.
+
+.keywords: distributed array, global to local, begin
+
+.seealso: DAGlobalToNaturalEnd(), DALocalToGlobal(), DACreate2d(), 
+          DAGlobalToLocalBegin(), DAGlobalToLocalEnd(), DACreateNaturalVector()
+@*/
+int DAGlobalToNaturalBegin(DA da,Vec g, InsertMode mode,Vec l)
+{
+  int ierr,m;
+  IS  from,to;
+
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(da,DA_COOKIE);
+  PetscValidHeaderSpecific(l,VEC_COOKIE);
+  PetscValidHeaderSpecific(g,VEC_COOKIE);
+  if (!da->gton) {
+    /* create the scatter context */
+    ierr = VecGetLocalSize(l,&m);CHKERRQ(ierr);
+    ierr = ISCreateStride(da->comm,0,1,m,&to);CHKERRQ(ierr);
+    ierr = AOPetscToApplicationIS(da->ao,to);CHKERRQ(ierr);
+    ierr = ISCreateStride(da->comm,0,1,m,&from);CHKERRQ(ierr);
+    ierr = VecScatterCreate(g,from,l,to,&da->gton);CHKERRQ(ierr);
+  }
+  ierr = VecScatterBegin(g,l,mode,SCATTER_FORWARD,da->gton); CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "DAGlobalToNaturalEnd"
+/*@
+   DAGlobalToNaturalEnd - Maps values from the global vector to a global vector
+   in the natural ordering. Must be preceeded by DAGlobalToNaturalBegin().
+
+   Collective on DA
+
+   Input Parameters:
++  da - the distributed array context
+.  g - the global vector
+-  mode - one of INSERT_VALUES or ADD_VALUES
+
+   Output Parameter:
+.  l  - the global values in the natural ordering
+
+   Notes:
+   The global and local vectors used here need not be the same as those
+   obtained from DACreateGlobalVector() and DACreateNaturalVector(), BUT they
+   must have the same parallel data layout; they could, for example, be 
+   obtained with VecDuplicate() from the DA originating vectors.
+
+.keywords: distributed array, global to local, end
+
+.seealso: DAGlobalToNaturalBegin(), DALocalToGlobal(), DACreate2d(),
+          DAGlobalToLocalBegin(), DAGlobalToLocalEnd(), DACreateNaturalVector()
+@*/
+int DAGlobalToNaturalEnd(DA da,Vec g, InsertMode mode,Vec l)
+{
+  int ierr;
+
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(da,DA_COOKIE);
+  PetscValidHeaderSpecific(l,VEC_COOKIE);
+  PetscValidHeaderSpecific(g,VEC_COOKIE);
+  ierr = VecScatterEnd(g,l,mode,SCATTER_FORWARD,da->gton); CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
