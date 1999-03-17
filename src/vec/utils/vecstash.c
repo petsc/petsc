224c839bf48c4aeb1a0ae68298b26491c5c0df1a
Satish Balay <balay@mcs.anl.gov>
1999-03-17 15:22:10 -0600

    *** empty log message ***

--- src/vec/utils/vecstash.c
+++ src/vec/utils/vecstash.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: vecstash.c,v 1.2 1999/03/17 19:13:22 balay Exp balay $";
+static char vcid[] = "$Id: vecstash.c,v 1.3 1999/03/17 21:14:45 balay Exp balay $";
 #endif
 
 #include "src/vec/vecimpl.h"
@@ -151,11 +151,6 @@ int VecStashGetInfo_Private(VecStash *stash,int *nstash, int *reallocs)
 int VecStashSetInitialSize_Private(VecStash *stash,int max)
 {
   PetscFunctionBegin;
-  if (stash->rank >2 ) {
-    printf("[%d]max = %d\n",stash->rank,max);
-  } else {
-    printf("[%d]max = %d\n",stash->rank,max);
-  }
   stash->oldnmax = max;
   stash->nmax    = 0;
   PetscFunctionReturn(0);
@@ -184,7 +179,6 @@ static int VecStashExpand_Private(VecStash *stash,int incr)
   if (stash->nmax == 0) newnmax = stash->oldnmax;
   else                  newnmax = stash->nmax*2;
   if (newnmax  < (stash->nmax + incr)) newnmax += 2*incr;
-  printf("[%d]oldnmax = %d, nmax=%d, newnmax = %d, bs = %d\n",stash->rank,stash->oldnmax,stash->nmax,newnmax,bs);
 
   n_array = (Scalar *)PetscMalloc((newnmax)*(sizeof(int)+bs*sizeof(Scalar)));CHKPTRQ(n_array);
   n_idx   = (int *) (n_array + bs*newnmax);
