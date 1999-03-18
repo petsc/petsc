f924e614e798e823150228278bf488171c0d8f7d
Barry Smith <bsmith@mcs.anl.gov>
1999-03-18 17:37:25 -0600

    *** empty log message ***

--- src/dm/da/src/daload.c
+++ src/dm/da/src/daload.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: daload.c,v 1.4 1999/03/15 22:11:15 balay Exp bsmith $";
+static char vcid[] = "$Id: daload.c,v 1.5 1999/03/18 22:23:14 bsmith Exp bsmith $";
 #endif
 
 #include "src/dm/da/daimpl.h"     /*I  "da.h"   I*/
@@ -58,11 +58,38 @@ int DALoad(Viewer viewer,int M,int N, int P,DA *da)
     SETERRQ1(1,1,"Dimension in info file is not 1, 2, or 3 it is %d",info[0]);
   }
   for ( i=0; i<info[4]; i++ ) {
-    sprintf(fieldnametag,"-daload_fieldname_%s",i);
+    sprintf(fieldnametag,"-daload_fieldname_%d",i);
     ierr = OptionsGetString(PETSC_NULL,fieldnametag,fieldname,64,&flag);CHKERRQ(ierr);
     if (flag) {
       ierr = DASetFieldName(*da,i,fieldname);CHKERRQ(ierr);
     }
+  }
+
+  /*
+    Read in coordinate information if kept in file
+  */
+  ierr = OptionsHasName(PETSC_NULL,"-daload_coordinates",&flag);CHKERRQ(ierr);
+  if (flag) {
+    DA  dac;
+    Vec natural,global;
+
+    if (info[0] == 1) {
+      ierr = DACreate1d(comm,DA_NONPERIODIC,info[1],1,0,0,&dac);CHKERRQ(ierr);
+    } else if (info[0] == 2) {
+      ierr = DACreate2d(comm,DA_NONPERIODIC,DA_STENCIL_BOX,info[1],info[2],M,N,2,
+                      0,0,0,&dac);CHKERRQ(ierr);
+    } else if (info[0] == 3) {
+      ierr = DACreate3d(comm,DA_NONPERIODIC,DA_STENCIL_BOX,info[1],info[2],info[3],M,N,P,
+                        3,0,0,0,0,&dac);CHKERRQ(ierr);
+    }
+    ierr = DACreateNaturalVector(dac,&natural);CHKERRQ(ierr);
+    ierr = VecLoadIntoVector(viewer,natural);CHKERRQ(ierr);
+    ierr = DACreateGlobalVector(dac,&global);CHKERRQ(ierr);
+    ierr = DANaturalToGlobalBegin(dac,natural,INSERT_VALUES,global);CHKERRQ(ierr);
+    ierr = DANaturalToGlobalEnd(dac,natural,INSERT_VALUES,global);CHKERRQ(ierr);
+    ierr = VecDestroy(natural);CHKERRQ(ierr); 
+    ierr = DADestroy(dac);CHKERRQ(ierr);
+    ierr = DASetCoordinates(*da,global);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
