7d148b77e145aa979ab18fde04876db7573e4554
Satish Balay <balay@mcs.anl.gov>
1999-03-18 13:32:17 -0600

    *** empty log message ***

--- src/sys/src/fileio/mprint.c
+++ src/sys/src/fileio/mprint.c
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-static char vcid[] = "$Id: mprint.c,v 1.24 1999/03/10 16:10:00 balay Exp bsmith $";
+static char vcid[] = "$Id: mprint.c,v 1.25 1999/03/17 23:21:32 bsmith Exp balay $";
 #endif
 /*
       Some PETSc utilites routines to add simple IO capability.
@@ -452,7 +452,7 @@ int PetscErrorPrintfDefault(const char format[],...)
     fprintf(stderr,"See docs/manualpages/index.html for manual pages.\n");
     fprintf(stderr,"--------------------------------------------\
 ---------------------------\n");
-    fprintf(stderr,"%s \n on a %s named %s by %s %s.",pname,arch,hostname,username,date);
+    fprintf(stderr,"%s \non a %s named %s by %s %s.",pname,arch,hostname,username,date);
 #if !defined (PARCH_win32)
     fprintf(stderr," Libraries linked from %s\n",PETSC_LDIR);
 #endif
