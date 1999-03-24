f5c54febf76de1badff0ae5388de48de81a880ad
Barry Smith <bsmith@mcs.anl.gov>
1999-03-24 10:21:19 -0600

    *** empty log message ***

--- include/finclude/petscdef.h
+++ include/finclude/petscdef.h
@@ -1,5 +1,5 @@
 !
-!  $Id: petscdef.h,v 1.7 1998/11/06 23:40:21 balay Exp balay $;
+!  $Id: petscdef.h,v 1.8 1998/12/03 19:40:24 balay Exp bsmith $;
 !
 !  Base include file for Fortran use of the PETSc package
 !
@@ -17,7 +17,7 @@
 ! compiler options like -r4,-r8, sometimes invoked 
 ! by the user. NAG compiler does not like integer*4,real*8
 !
-! ???? All integers should also be chnaged to PetscFortranInt ?????
+! ???? All integers should also be changed to PetscFortranInt ?????
 !
 
 #if (SIZEOF_VOIDP == 8)
@@ -67,20 +67,20 @@
 !     Some more viewers, which are initialized using common-block
 !     are declared in finclude/petsc.h
 !
-      PetscFortranAddr VIEWER_DRAWX_WORLD
-      PetscFortranAddr VIEWER_DRAWX_WORLD_0,VIEWER_DRAWX_WORLD_1
-      PetscFortranAddr VIEWER_DRAWX_WORLD_2,VIEWER_DRAWX_SELF
-      PetscFortranAddr VIEWER_MATLAB_WORLD
+      PetscFortranAddr VIEWER_DRAW_WORLD
+      PetscFortranAddr VIEWER_DRAW_WORLD_0,VIEWER_DRAW_WORLD_1
+      PetscFortranAddr VIEWER_DRAW_WORLD_2,VIEWER_DRAW_SELF
+      PetscFortranAddr VIEWER_SOCKET_WORLD
 !
 !     The numbers used below should match those in 
 !     src/fortran/custom/zpetsc.h
 !
-      parameter (VIEWER_DRAWX_WORLD_0 = -4) 
-      parameter (VIEWER_DRAWX_WORLD_1 = -5)
-      parameter (VIEWER_DRAWX_WORLD_2 = -6)
-      parameter (VIEWER_DRAWX_SELF = -7)
-      parameter (VIEWER_MATLAB_WORLD = -8)
-      parameter (VIEWER_DRAWX_WORLD = VIEWER_DRAWX_WORLD_0)
+      parameter (VIEWER_DRAW_WORLD_0 = -4) 
+      parameter (VIEWER_DRAW_WORLD_1 = -5)
+      parameter (VIEWER_DRAW_WORLD_2 = -6)
+      parameter (VIEWER_DRAW_SELF = -7)
+      parameter (VIEWER_SOCKET_WORLD = -8)
+      parameter (VIEWER_DRAW_WORLD = VIEWER_DRAW_WORLD_0)
 
 
 !
@@ -132,7 +132,24 @@
 #define PetscConj(a) a
 #define MPIU_SCALAR  MPI_DOUBLE_PRECISION
 #endif
+!
+!    Allows the matrix Fortran Kernels to work with single precision
+!    matrix data structures
+!
+#if defined(USE_PETSC_COMPLEX)
 
+#define MatScalar Scalar 
+
+#elif defined(USE_MAT_SINGLE)
+
+#define MatScalar real*4
+
+#else
+
+#define MatScalar Scalar
+
+#endif
+!
 !
 !     Basic constants
 ! 
