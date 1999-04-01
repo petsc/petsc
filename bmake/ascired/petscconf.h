5b2b8ae40e3b815d74a37892e187c653accc7d02
Satish Balay <balay@mcs.anl.gov>
1999-04-01 16:31:16 -0600

    Initial revision

---
+++ bmake/ascired/petscconf.h
@@ -0,0 +1,41 @@
+#ifdef PETSC_RCS_HEADER
+"$Id: petscconf.h,v 1.14 1999/02/08 22:22:33 bsmith Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define PARCH_ascired
+
+#define HAVE_SYS_WAIT_H
+#define HAVE_VPRINTF
+#define STDC_HEADERS
+#define TIME_WITH_SYS_TIME
+#define HAVE_FORTRAN_UNDERSCORE
+#define HAVE_DRAND48
+#define HAVE_GETCWD
+#define HAVE_GETHOSTNAME
+#define HAVE_GETTIMEOFDAY
+#define HAVE_GETWD
+#define HAVE_MEMMOVE
+#define HAVE_SIGACTION
+#define HAVE_SIGNAL
+#define HAVE_STRSTR
+#define HAVE_UNAME
+#define HAVE_FCNTL_H
+#define HAVE_LIMITS_H
+#define HAVE_MALLOC_H
+#define HAVE_PWD_H
+#define HAVE_SEARCH_H
+#define HAVE_STDLIB_H
+#define HAVE_STRING_H
+#define HAVE_STRINGS_H
+#define HAVE_STROPTS_H
+#define HAVE_SYS_TIME_H
+#define HAVE_UNISTD_H
+#define HAVE_SWAPPED_BYTES
+
+#define HAVE_SLOW_NRM2
+
+#endif
