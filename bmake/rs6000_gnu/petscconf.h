c1959989fee6807d86067b98c68e9783783dc235
Satish Balay <balay@mcs.anl.gov>
1999-04-18 01:10:49 -0600

    *** empty log message ***

--- bmake/rs6000_gnu/petscconf.h
+++ bmake/rs6000_gnu/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.12 1999/03/31 23:57:34 balay Exp balay $"
+"$Id: petscconf.h,v 1.13 1999/04/16 16:16:48 balay Exp balay $"
 "Defines the configuration for this machine"
 #endif
 
@@ -7,7 +7,7 @@
 #define INCLUDED_PETSCCONF_H
 
 #define PARCH_rs6000 
-#define USE_READ_REAL_TIME
+#define USE_IBM_ASM_CLOCK
 
 #define HAVE_STROPTS_H 
 #define HAVE_SEARCH_H 
