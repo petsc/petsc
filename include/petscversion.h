e247536ed911218cf49f07b20a567efbf5650dcf
Barry Smith <bsmith@mcs.anl.gov>
1999-04-20 13:29:24 -0600

    *** empty log message ***

--- include/petscversion.h
+++ include/petscversion.h
@@ -1,4 +1,4 @@
-/* $Id: petscversion.h,v 1.9 1999/04/16 22:27:08 balay Exp balay $ */
+/* $Id: petscversion.h,v 1.10 1999/04/19 20:37:57 balay Exp bsmith $ */
 #if !defined(__PETSCVERSION_H)
 #define __PETSCVERSION_H
 
@@ -9,12 +9,12 @@
     docs/tex/manual/manual.tex and
     docs/tex/manual/manual_tex.tex.
 */
-#define PETSC_VERSION_NUMBER "PETSc Version 2.0.24, Patch 4, Released April 6, 1999"
+#define PETSC_VERSION_NUMBER "PETSc Version 2.0.24, Patch 5, Released April 6, 1999"
 
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    0
 #define PETSC_VERSION_SUBMINOR 24
-#define PETSC_PATCH_LEVEL      4
+#define PETSC_PATCH_LEVEL      5
 #define PETSC_VERSION_DATE     "April 6, 1998"
 #define PETSC_AUTHOR_INFO      "The PETSc Team:\
  Satish Balay, Bill Gropp, Lois Curfman McInnes, Barry Smith\n\
