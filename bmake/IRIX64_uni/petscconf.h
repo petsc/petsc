57cfd5813ae7a5e829d082c4e3597b4aa80c5143
Satish Balay <balay@mcs.anl.gov>
1999-07-02 12:45:20 -0600

    *** empty log message ***

--- bmake/IRIX64_uni/petscconf.h
+++ bmake/IRIX64_uni/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.16 1999/02/08 22:22:33 bsmith Exp bsmith $"
+"$Id: petscconf.h,v 1.26 1999/06/11 16:14:54 balay Exp balay $"
 "Defines the configuration for this machine"
 #endif
 
@@ -12,18 +12,22 @@
 #define PETSC_HAVE_STRING_H 
 #define PETSC_HAVE_STROPTS_H 
 #define PETSC_HAVE_MALLOC_H 
-#define PETSC_HAVE_X11 
-#define PETSC_HAVE_DRAND48  
+#define PETSC_HAVE_X11
+#define PETSC_HAVE_DRAND48 
 #define PETSC_HAVE_GETDOMAINNAME 
 #define PETSC_HAVE_UNAME 
 #define PETSC_HAVE_UNISTD_H 
+#define PETSC_HAVE_STDLIB_H
 #define PETSC_HAVE_SYS_TIME_H 
+#define PETSC_HAVE_SYS_UTSNAME_H
 #define PETSC_USE_SHARED_MEMORY
 
 #define PETSC_HAVE_FORTRAN_UNDERSCORE 
 #define PETSC_SIZEOF_VOIDP 8
 #define PETSC_SIZEOF_INT 4
 #define PETSC_SIZEOF_DOUBLE 8
+
+#define PETSC_HAVE_IRIXF90
 
 #define PETSC_HAVE_IRIXF90
 
@@ -36,8 +40,15 @@
 
 #define PETSC_HAVE_MEMALIGN
 
+#define PETSC_HAVE_FAST_MPI_WTIME
+
 #define PETSC_USE_DBX_DEBUGGER
 #define PETSC_HAVE_SYS_RESOURCE_H
+
+
+#define PETSC_HAVE_RTLD_GLOBAL 1
+
+#define PETSC_CAN_SLEEP_AFTER_ERROR
 
 #define PETSC_HAVE_4ARG_SIGNAL_HANDLER
 
