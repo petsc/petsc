adb105f675b7cd4d3fb041eb41a055b69d5dc2dc
Satish Balay <balay@mcs.anl.gov>
1999-07-06 16:28:39 -0600

    *** empty log message ***

--- bmake/sun4/petscconf.h
+++ bmake/sun4/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.15 1999/05/12 03:24:58 bsmith Exp bsmith $"
+"$Id: petscconf.h,v 1.16 1999/06/30 22:48:03 bsmith Exp balay $"
 "Defines the configuration for this machine"
 #endif
 
@@ -50,5 +50,6 @@
 #define PETSC_NEED_SETSOCKETOPT_PROTO
 #define PETSC_NEED_CONNECT_PROTO
 #define PETSC_NEED_SOCKET_PROTO
+#define PETSC_HAVE_FREE_RETURN_INT
 
 #endif
--- bmake/sun4_nodebug/petscconf.h
+++ bmake/sun4_nodebug/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.12 1999/05/12 03:24:58 bsmith Exp bsmith $"
+"$Id: petscconf.h,v 1.13 1999/06/30 22:48:03 bsmith Exp balay $"
 "Defines the configuration for this machine"
 #endif
 
@@ -46,5 +46,6 @@
 #define PETSC_NEED_SETSOCKETOPT_PROTO
 #define PETSC_NEED_CONNECT_PROTO
 #define PETSC_NEED_SOCKET_PROTO
+#define PETSC_HAVE_FREE_RETURN_INT
 
 #endif
