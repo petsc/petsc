98ce19e36e559249d9d7424d25902c0f9319df59
Barry Smith <bsmith@mcs.anl.gov>
1999-09-14 12:37:24 -0600

    Initial revision

---
+++ src/vec/examples/tests/ex21.c
@@ -0,0 +1,56 @@
+#ifdef PETSC_RCS_HEADER
+static char vcid[] = "$Id: ex2.c,v 1.43 1999/05/04 20:30:57 balay Exp $";
+#endif
+
+static char help[] = "Tests vector scatter-gather operations.  Input arguments are\n\
+  -n <length> : vector length\n\n";
+
+#include "vec.h"
+#include "sys.h"
+
+#undef __FUNC__
+#define __FUNC__ "main"
+int main(int argc,char **argv)
+{
+  int           n = 5, ierr, idx1[2] = {0,3}, idx2[2] = {1,4},flg;
+  Scalar        one = 1.0, two = 2.0;
+  Vec           x,y;
+  IS            is1,is2;
+  VecScatter    ctx = 0;
+
+  PetscInitialize(&argc,&argv,(char*)0,help);
+  OptionsGetInt(PETSC_NULL,"-n",&n,&flg);
+
+  /* create two vector */
+  ierr = VecCreateSeq(PETSC_COMM_SELF,n,&x);CHKERRA(ierr);
+  ierr = VecDuplicate(x,&y);CHKERRA(ierr);
+
+  /* create two index sets */
+  ierr = ISCreateGeneral(PETSC_COMM_SELF,2,idx1,&is1);CHKERRA(ierr);
+  ierr = ISCreateGeneral(PETSC_COMM_SELF,2,idx2,&is2);CHKERRA(ierr);
+
+  ierr = VecSet(&one,x);CHKERRA(ierr);
+  ierr = VecSet(&two,y);CHKERRA(ierr);
+  ierr = VecScatterCreate(x,is1,y,is2,&ctx);CHKERRA(ierr);
+  ierr = VecScatterBegin(x,y,INSERT_VALUES,SCATTER_FORWARD,ctx);CHKERRA(ierr);
+  ierr = VecScatterEnd(x,y,INSERT_VALUES,SCATTER_FORWARD,ctx);CHKERRA(ierr);
+  
+  ierr = VecView(y,VIEWER_STDOUT_SELF);CHKERRA(ierr);
+
+  ierr = VecScatterBegin(y,x,INSERT_VALUES,SCATTER_FORWARD,ctx);CHKERRA(ierr);
+  ierr = VecScatterEnd(y,x,INSERT_VALUES,SCATTER_FORWARD,ctx);CHKERRA(ierr);
+  ierr = VecScatterDestroy(ctx);CHKERRA(ierr);
+
+  PetscPrintf(PETSC_COMM_SELF,"-------\n");
+  ierr = VecView(x,VIEWER_STDOUT_SELF);CHKERRA(ierr);
+
+  ierr = ISDestroy(is1);CHKERRA(ierr);
+  ierr = ISDestroy(is2);CHKERRA(ierr);
+
+  ierr = VecDestroy(x);CHKERRA(ierr);
+  ierr = VecDestroy(y);CHKERRA(ierr);
+
+  PetscFinalize();
+  return 0;
+}
+ 
