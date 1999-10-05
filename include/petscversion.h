14fbd10da59a0e393f7bb256b73dba03ead9b8c2
Lois Curfman McInnes <curfman@mcs.anl.gov>
1999-10-05 13:46:53 -0600

    *** empty log message ***

--- include/petscversion.h
+++ include/petscversion.h
@@ -1,4 +1,4 @@
-/* $Id: petscversion.h,v 1.14 1999/05/07 04:06:34 balay Exp balay $ */
+/* $Id: petscversion.h,v 1.15 1999/06/14 20:24:03 balay Exp curfman $ */
 #if !defined(__PETSCVERSION_H)
 #define __PETSCVERSION_H
 
@@ -9,13 +9,13 @@
     docs/tex/manual/manual.tex and
     docs/tex/manual/manual_tex.tex.
 */
-#define PETSC_VERSION_NUMBER "PETSc Version 2.0.25, Released June 14, 1999"
+#define PETSC_VERSION_NUMBER "PETSc Version 2.0.26, Released October 6, 1999"
 
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    0
-#define PETSC_VERSION_SUBMINOR 25
+#define PETSC_VERSION_SUBMINOR 26
 #define PETSC_PATCH_LEVEL      0
-#define PETSC_VERSION_DATE     "June 14, 1999"
+#define PETSC_VERSION_DATE     "October 6, 1999"
 #define PETSC_AUTHOR_INFO      "The PETSc Team:\
  Satish Balay, Bill Gropp, Lois Curfman McInnes, Barry Smith\n\
  Bug reports, questions: petsc-maint@mcs.anl.gov\n\
