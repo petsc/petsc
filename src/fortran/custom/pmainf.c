19f391b2be42924d3d7c74e4a76548d7a262bd94
Barry Smith <bsmith@mcs.anl.gov>
1999-11-13 18:58:48 -0600

    Initial revision

---
+++ src/fortran/custom/pmainf.c
@@ -0,0 +1,20 @@
+/*$Id: pmain.c,v 1.1 1999/11/14 00:42:58 bsmith Exp bsmith $*/
+/*
+   Provides a simple main program that initializes PETSc and then
+   calls PetscMain()
+*/
+#include "petsc.h"        /*I  "petsc.h"   I*/
+
+extern int PetscMain(void);
+
+#undef __FUNC__  
+#define __FUNC__ "main"
+int main(int argc,char **args)
+{
+  int ierr;
+
+  PetscInitialize(&argc,&args,PETSC_NULL,PETSC_NULL);
+  ierr = PetscMain();CHKERRA(ierr);
+  PetscFinalize();
+  return 0;
+}
