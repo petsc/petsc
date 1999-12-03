38f41016c6862d5c754081462e00d600ed471eaa
Satish Balay <balay@mcs.anl.gov>
1999-12-03 13:24:49 -0600

    *** empty log message ***

--- src/sles/ksp/impls/bcgs/bcgs.c
+++ src/sles/ksp/impls/bcgs/bcgs.c
@@ -1,4 +1,4 @@
-/*$Id: bcgs.c,v 1.66 1999/11/05 14:46:40 bsmith Exp bsmith $*/
+/*$Id: bcgs.c,v 1.67 1999/11/24 21:54:50 bsmith Exp bsmith $*/
 
 /*                       
     This code implements the BiCGStab (Stabilized version of BiConjugate
@@ -28,7 +28,7 @@ static int KSPSetUp_BCGS(KSP ksp)
 #define __FUNC__ "KSPSolve_BCGS"
 static int  KSPSolve_BCGS(KSP ksp,int *its)
 {
-  int       i, maxit, cerr = 0,ierr;
+  int       i, maxit, ierr;
   Scalar    rho, rhoold, alpha, beta, omega, omegaold, d1, d2,zero = 0.0, tmp;
   Vec       X,B,V,P,R,RP,T,S, BINVF;
   double    dp = 0.0;
@@ -59,7 +59,8 @@ static int  KSPSolve_BCGS(KSP ksp,int *its)
   ierr = PetscObjectGrantAccess(ksp);CHKERRQ(ierr);
   KSPLogResidualHistory(ksp,dp);
   KSPMonitor(ksp,0,dp);
-  if ((*ksp->converged)(ksp,0,dp,ksp->cnvP)) {*its = 0; PetscFunctionReturn(0);}
+  ierr = (*ksp->converged)(ksp,0,dp,&ksp->reason,ksp->cnvP);CHKERRQ(ierr);
+  if (ksp->reason) {*its = 0; PetscFunctionReturn(0);}
 
   /* Make the initial Rp == R */
   ierr = VecCopy(R,RP);CHKERRQ(ierr);
@@ -73,7 +74,7 @@ static int  KSPSolve_BCGS(KSP ksp,int *its)
   for (i=0; i<maxit; i++) {
 
     ierr = VecDot(R,RP,&rho);CHKERRQ(ierr);       /*   rho <- (r,rp)      */
-    if (rho == 0.0) SETERRQ(PETSC_ERR_KSP_BRKDWN,0,"Breakdown, rho = r . rp = 0");
+    if (!rho) SETERRQ(PETSC_ERR_KSP_BRKDWN,0,"Breakdown, rho = r . rp = 0");
     beta = (rho/rhoold) * (alpha/omegaold);
     tmp = -omegaold; VecAXPY(&tmp,V,P);            /*   p <- p - w v       */
     ierr = VecAYPX(&beta,R,P);CHKERRQ(ierr);      /*   p <- r + p beta    */
@@ -84,7 +85,7 @@ static int  KSPSolve_BCGS(KSP ksp,int *its)
     ierr = KSP_PCApplyBAorAB(ksp,ksp->B,ksp->pc_side,S,T,R);CHKERRQ(ierr);/*   t <- K s    */
     ierr = VecDot(S,T,&d1);CHKERRQ(ierr);
     ierr = VecDot(T,T,&d2);CHKERRQ(ierr);
-    if (d2 == 0.0) {
+    if (!d2) {
       /* t is 0.  if s is 0, then alpha v == r, and hence alpha p
 	 may be our solution.  Give it a try? */
       ierr = VecDot(S,S,&d1);CHKERRQ(ierr);
@@ -116,14 +117,16 @@ static int  KSPSolve_BCGS(KSP ksp,int *its)
     ierr = PetscObjectGrantAccess(ksp);CHKERRQ(ierr);
     KSPLogResidualHistory(ksp,dp);
     KSPMonitor(ksp,i+1,dp);
-    cerr = (*ksp->converged)(ksp,i+1,dp,ksp->cnvP);
-    if (cerr) break;    
+    ierr = (*ksp->converged)(ksp,i+1,dp,&ksp->reason,ksp->cnvP);CHKERRQ(ierr);
+    if (ksp->reason) break;    
   }
-  if (i == maxit) i--;
+  if (i == maxit) {
+    ksp->reason = KSP_DIVERGED_ITS;
+    i--;
+  }
+  *its = i;
 
   ierr = KSPUnwindPreconditioner(ksp,X,T);CHKERRQ(ierr);
-  if (cerr <= 0) *its = -(i+1);
-  else           *its = i + 1;
   PetscFunctionReturn(0);
 }
 
@@ -135,14 +138,14 @@ int KSPCreate_BCGS(KSP ksp)
   PetscFunctionBegin;
   ksp->data                 = (void *) 0;
   ksp->pc_side              = PC_LEFT;
-  ksp->calc_res             = 1;
+  ksp->calc_res             = PETSC_TRUE;
   ksp->ops->setup           = KSPSetUp_BCGS;
   ksp->ops->solve           = KSPSolve_BCGS;
   ksp->ops->destroy         = KSPDefaultDestroy;
   ksp->ops->buildsolution   = KSPDefaultBuildSolution;
   ksp->ops->buildresidual   = KSPDefaultBuildResidual;
   ksp->ops->view            = 0;
-  ksp->guess_zero           = 1; 
+  ksp->guess_zero           = PETSC_TRUE; 
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
