a54e74fd95959ad44870b213c1da1b6c1f35d410
Barry Smith <bsmith@mcs.anl.gov>
2000-01-15 20:37:09 -0600

    *** empty log message ***

--- src/contrib/oberman/laplacian_q1/main.c
+++ src/contrib/oberman/laplacian_q1/main.c
@@ -1,45 +1,42 @@
-/*$Id: main.c,v 1.3 2000/01/06 20:43:19 bsmith Exp bsmith $*/
+/*$Id: main.c,v 1.4 2000/01/06 21:32:13 bsmith Exp bsmith $*/
 static char help[] =
 "Solves 2d-laplacian on quadrilateral grid.\n\
    Options:\n\
-    -matlabgraphics pipe solution to matlab (visualize with bscript).\n\
+    -show_solution pipe solution to matlab (visualized with bscript.m).\n\
     -show_griddata print the local index sets and local to global mappings \n\
-    -show_solution plot the solution with a contour graph \n\
     -show_matrix visualize the sparsity structure of the stiffness matrix.\n\
     -show_grid visualize the global and local grids with numbering.\n";
 
+/*
+    The file appctx.h includes all the data structures used by this code
+*/
 #include "appctx.h"
 
 int main(int argc,char **argv)
 {
   int            ierr;
-  AppCtx         *appctx;
+  AppCtx         *appctx;     /* contains all the data used by this PDE solver */
 
   /* ---------------------------------------------------------------------
      Initialize PETSc
-     --------------------- ---------------------------------------------------*/
+     ----------------- ---------------------------------------------------*/
   PetscInitialize(&argc,&argv,(char *)0,help);
 
-  /*  Load the grid database 
-      in appload.c              */
+  /*  Load the grid database -- in appload.c              */
   ierr = AppCtxCreate(PETSC_COMM_WORLD,&appctx);CHKERRA(ierr);
 
-  /*   Setup the graphics routines to view the grid
-       in appview.c  */
+  /*   Setup the graphics routines to view the grid -- in appview.c  */
   ierr = AppCtxGraphics(appctx);CHKERRA(ierr);
  
-  /*   Setup the linear system and solve it 
-       in appsetalg.c */
+  /*   Setup the linear system and solve it -- in appsetalg.c */
   ierr = AppCtxSolve(appctx);CHKERRA(ierr);
 
-  /*   Send solution to  matlab viewer 
-       in appview.c */
+  /*   Send solution to  matlab viewer -- in appview.c */
   if (appctx->view.show_solution) {
     ierr = AppCtxViewMatlab(appctx);CHKERRA(ierr);  
   }
 
-  /*  Destroy all datastructures  
-      in appload.c */
+  /*  Destroy all datastructures  -- in appload.c */
   ierr = AppCtxDestroy(appctx);CHKERRA(ierr);
 
   /* Close down PETSc and stop the program */
