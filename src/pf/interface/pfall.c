2db0d52b4a559c96349e3dbd8a5cabfe991970fe
Barry Smith <bsmith@mcs.anl.gov>
2000-01-24 18:49:51 -0600

    Initial revision

---
+++ src/pf/interface/pfall.c
@@ -0,0 +1,62 @@
+/*$Id: pcregis.c,v 1.1 2000/01/25 00:49:22 bsmith Exp bsmith $*/
+
+#include "src/sles/pc/pcimpl.h"          /*I   "pc.h"   I*/
+
+EXTERN_C_BEGIN
+extern int PCCreate_Jacobi(PC);
+extern int PCCreate_BJacobi(PC);
+extern int PCCreate_ILU(PC);
+extern int PCCreate_None(PC);
+extern int PCCreate_LU(PC);
+extern int PCCreate_SOR(PC);
+extern int PCCreate_Shell(PC);
+extern int PCCreate_MG(PC);
+extern int PCCreate_Eisenstat(PC);
+extern int PCCreate_ICC(PC);
+extern int PCCreate_ASM(PC);
+extern int PCCreate_SLES(PC);
+extern int PCCreate_Composite(PC);
+extern int PCCreate_Redundant(PC);
+EXTERN_C_END
+
+#undef __FUNC__  
+#define __FUNC__ "PCRegisterAll"
+/*@C
+   PCRegisterAll - Registers all of the preconditioners in the PC package.
+
+   Not Collective
+
+   Input Parameter:
+.  path - the library where the routines are to be found (optional)
+
+   Level: advanced
+
+.keywords: PC, register, all
+
+.seealso: PCRegisterDynamic(), PCRegisterDestroy()
+@*/
+int PCRegisterAll(char *path)
+{
+  int ierr;
+
+  PetscFunctionBegin;
+  PCRegisterAllCalled = 1;
+
+  ierr = PCRegisterDynamic(PCNONE         ,path,"PCCreate_None",PCCreate_None);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCJACOBI       ,path,"PCCreate_Jacobi",PCCreate_Jacobi);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCBJACOBI      ,path,"PCCreate_BJacobi",PCCreate_BJacobi);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCSOR          ,path,"PCCreate_SOR",PCCreate_SOR);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCLU           ,path,"PCCreate_LU",PCCreate_LU);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCSHELL        ,path,"PCCreate_Shell",PCCreate_Shell);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCMG           ,path,"PCCreate_MG",PCCreate_MG);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCEISENSTAT    ,path,"PCCreate_Eisenstat",PCCreate_Eisenstat);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCILU          ,path,"PCCreate_ILU",PCCreate_ILU);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCICC          ,path,"PCCreate_ICC",PCCreate_ICC);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCASM          ,path,"PCCreate_ASM",PCCreate_ASM);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCSLES         ,path,"PCCreate_SLES",PCCreate_SLES);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCCOMPOSITE    ,path,"PCCreate_Composite",PCCreate_Composite);CHKERRQ(ierr);
+  ierr = PCRegisterDynamic(PCREDUNDANT    ,path,"PCCreate_Redundant",PCCreate_Redundant);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
