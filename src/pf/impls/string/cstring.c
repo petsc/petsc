80ccde00dc1f0e6ff8ea39378f6f68c5db68a549
Barry Smith <bsmith@mcs.anl.gov>
2000-02-04 15:37:18 -0600

    *** empty log message ***

--- src/pf/impls/string/cstring.c
+++ src/pf/impls/string/cstring.c
@@ -1,33 +1,14 @@
-/*$Id: const.c,v 1.2 2000/02/02 21:21:18 bsmith Exp $*/
+/*$Id: cstring.c,v 1.1 2000/02/02 21:28:09 bsmith Exp bsmith $*/
 #include "src/pf/pfimpl.h"            /*I "pf.h" I*/
 
-#undef __FUNC__  
-#define __FUNC__ "PFApply_Constant"
-int PFApply_Constant(void *value,int n,Scalar *x,Scalar *y)
-{
-  int    i;
-  Scalar v = ((Scalar*)value)[0];
-
-  PetscFunctionBegin;
-  n *= (int) PetscRealPart(((Scalar*)value)[1]);
-  for (i=0; i<n; i++) {
-    y[i] = v;
-  }
-  PetscFunctionReturn(0);
-}
+/*
+        Ths PF generates a function on the fly and loads it into the running 
+   program.
+*/
 
 #undef __FUNC__  
-#define __FUNC__ "PFApplyVec_Constant"
-int PFApplyVec_Constant(void *value,Vec x,Vec y)
-{
-  int ierr;
-  PetscFunctionBegin;
-  ierr = VecSet((Scalar*)value,y);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-#undef __FUNC__  
-#define __FUNC__ "PFView_Constant"
-int PFView_Constant(void *value,Viewer viewer)
+#define __FUNC__ "PFView_String"
+int PFView_String(void *value,Viewer viewer)
 {
   int        ierr;
   PetscTruth isascii;
@@ -35,53 +16,108 @@ int PFView_Constant(void *value,Viewer viewer)
   PetscFunctionBegin;
   ierr = PetscTypeCompare((PetscObject)viewer,ASCII_VIEWER,&isascii);CHKERRQ(ierr);
   if (isascii) {
-#if !defined(PETSC_USE_COMPLEX)
-    ierr = ViewerASCIIPrintf(viewer,"Constant = %g\n",*(double*)value);CHKERRQ(ierr);
-#else
-    ierr = ViewerASCIIPrintf(viewer,"Constant = %g + %gi\n",PetscRealPart(*(Scalar*)value),PetscImaginaryPart(*(Scalar*)value));CHKERRQ(ierr);
-#endif
+    ierr = ViewerASCIIPrintf(viewer,"String = %s\n",(char*)value);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
+
 #undef __FUNC__  
-#define __FUNC__ "PFDestroy_Constant"
-int PFDestroy_Constant(void *value)
+#define __FUNC__ "PFDestroy_String"
+int PFDestroy_String(void *value)
 {
-  int ierr;
+  int       ierr;
+
   PetscFunctionBegin;
-  ierr = PetscFree(value);CHKERRQ(ierr);
+  ierr = PetscStrfree((char*)value);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ "PFSetFromOptions_Constant"
-int PFSetFromOptions_Constant(PF pf)
+#define __FUNC__ "PFStringCreateFunction"
+int PFStringCreateFunction(PF pf,char *string,void **f)
+{
+#if defined(PETSC_USE_DYNAMIC_LIBRARIES)
+  int        ierr;
+  void       *handle;
+  char       task[1024],tmp[256],lib[256];
+  FILE       *fd;
+  PetscTruth tmpshared,wdshared;
+  MPI_Comm   comm;
+
+  PetscFunctionBegin;
+  ierr = PetscStrfree((char*)pf->data);CHKERRQ(ierr);
+  ierr = PetscStrallocpy(string,(char**)&pf->data);CHKERRQ(ierr);
+
+  /* create the new C function and compile it */
+  ierr = PetscSharedTmp(pf->comm,&tmpshared);CHKERRQ(ierr);
+  ierr = PetscSharedWorkingDirectory(pf->comm,&wdshared);CHKERRQ(ierr);
+  if (tmpshared) {  /* do it in /tmp since everyone has one */
+    ierr = PetscGetTmp(pf->comm,tmp,256);CHKERRQ(ierr);
+    comm = pf->comm;
+  } else if (!wdshared) {  /* each one does in private /tmp */
+    ierr = PetscGetTmp(pf->comm,tmp,256);CHKERRQ(ierr);
+    comm = PETSC_COMM_SELF;
+  } else { /* do it in current directory */
+    ierr = PetscStrcpy(tmp,".");CHKERRQ(ierr);
+    comm = pf->comm;
+  }
+  sprintf(task,"cd %s ; \\cp -f ${PETSC_DIR}/src/pf/impls/string/makefile ./petscmakefile ; make BOPT=${BOPT} MIN=%d NOUT=%d -f petscmakefile petscdlib STRINGFUNCTION=\"%s\" ; \\rm -f petscmakefile petscdlib.c libpetscdlib.a ;  sync\n",tmp,pf->dimin,pf->dimout,string);
+  ierr = PetscPOpen(comm,PETSC_NULL,task,"r",&fd);CHKERRQ(ierr);
+  ierr = PetscPClose(comm,fd);CHKERRQ(ierr);
+
+  ierr = MPI_Barrier(comm);CHKERRQ(ierr);
+
+  /* load the apply function from the dynamic library */
+  sprintf(lib,"%s/libpetscdlib",tmp);
+  ierr = DLLibrarySym(comm,PETSC_NULL,lib,"PFApply_String",f);CHKERRQ(ierr);
+  
+#else
+  PetscFunctionBegin;
+#endif
+  PetscFunctionReturn(0);    
+}
+
+#undef __FUNC__  
+#define __FUNC__ "PFSetFromOptions_String"
+int PFSetFromOptions_String(PF pf)
 {
   int        ierr;
   PetscTruth flag;
-  Scalar     *value = (Scalar *)pf->data;
+  char       value[256];
+  int        (*f)(void *,int,Scalar*,Scalar*) = 0;
 
   PetscFunctionBegin;
-  ierr = OptionsGetScalar(pf->prefix,"-pf_constant",value,&flag);CHKERRQ(ierr);
+  ierr = OptionsGetString(pf->prefix,"-pf_string",value,256,&flag);CHKERRQ(ierr);
+  if (flag) {
+    ierr = PFStringCreateFunction(pf,value,(void**)&f);CHKERRQ(ierr);
+    pf->ops->apply = f;
+  }
   PetscFunctionReturn(0);    
 }
 
 
 EXTERN_C_BEGIN
 #undef __FUNC__  
-#define __FUNC__ "PFCreate_Constant"
-int PFCreate_Constant(PF pf,void *value)
+#define __FUNC__ "PFCreate_String"
+int PFCreate_String(PF pf,void *value)
 {
-  int    ierr;
-  Scalar *loc;
+  int        ierr;
+  Scalar     *loc;
+  int        (*f)(void *,int,Scalar*,Scalar*) = 0;
 
   PetscFunctionBegin;
-  loc    = (Scalar*)PetscMalloc(2*sizeof(Scalar));CHKPTRQ(loc);
-  if (value) loc[0] = *(Scalar*)value; else loc[0] = 0.0;
-  loc[1] = pf->dimout;
-  ierr   = PFSet(pf,PFApply_Constant,PFApplyVec_Constant,PFView_Constant,PFDestroy_Constant,loc);CHKERRQ(ierr);
+  
+  if (value) {
+    ierr = PFStringCreateFunction(pf,(char*)value,(void**)&f);CHKERRQ(ierr);
+  }
+  ierr   = PFSet(pf,f,PETSC_NULL,PFView_String,PFDestroy_String,loc);CHKERRQ(ierr);
 
-  pf->ops->setfromoptions = PFSetFromOptions_Constant;
+  pf->ops->setfromoptions = PFSetFromOptions_String;
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
+
+
+
+
+
--- src/pf/impls/string/cstringbase.c
+++ src/pf/impls/string/cstringbase.c
@@ -1,16 +1,49 @@
-/*$Id: cstring.c,v 1.1 2000/02/02 21:28:09 bsmith Exp bsmith $*/
-#include "src/pf/pfimpl.h"            /*I "pf.h" I*/
+/*$Id: cstringbase.c,v 1.1 2000/02/02 23:09:35 bsmith Exp bsmith $*/
+#include "petsc.h"
+
+#define NOTLIKELY -1.234567890123
 
 EXTERN_C_BEGIN
 #undef __FUNC__  
-#define __FUNC__ "PFApply_String_1"
-int PFApply_String_1(void *value,int n,Scalar *x,Scalar *y)
+#define __FUNC__ "PFApply_String"
+int PFApply_String(void *value,int n,Scalar *in,Scalar *out)
 {
-  int    i;
+  int       i;
+  Scalar    x,y,z,f = NOTLIKELY,x1,x2,x3,x4,x5,f1,f2,f3,f4,f5;
 
   PetscFunctionBegin;
   for (i=0; i<n; i++) {
-    y[i] = 3.0;
+    x1 = x = in[_MIN_*i];
+#if (_MIN_ > 1) 
+    x2 = y = in[_MIN_*i+1];
+#endif
+#if (_MIN_ > 2)
+    x3 = z = in[_MIN_*i+2];
+#endif
+#if (_MIN_ > 3)
+    x4 = in[_MIN_*i+3];
+#endif
+#if (_MIN_ > 4)
+    x5 = in[_MIN_*i+4];
+#endif
+    FUNCTION;
+    if (f == NOTLIKELY) {
+      out[_NOUT_*i] = f1;
+    } else {
+      out[_NOUT_*i] = f;
+    }
+#if (_NOUT_ > 1) 
+    out[_NOUT_*i+1] = f2; 
+#endif
+#if (_NOUT_ > 2) 
+    out[_NOUT_*i+2] = f3;
+#endif
+#if (_NOUT_ > 3) 
+    out[_NOUT_*i+3] = f4;
+#endif
+#if (_NOUT_ > 4) 
+    out[_NOUT_*i+4] = f5;
+#endif
   }
   PetscFunctionReturn(0);
 }
