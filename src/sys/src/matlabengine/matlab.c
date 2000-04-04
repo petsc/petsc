a79761a16e57adc613194d45d871c013bc2b5af2
Barry Smith <bsmith@mcs.anl.gov>
2000-04-03 21:33:30 -0600

    *** empty log message ***

--- src/sys/src/matlabengine/matlab.c
+++ src/sys/src/matlabengine/matlab.c
@@ -1,41 +1,61 @@
-/* $Revision: 1.5 $ */
-/*
- *	engdemo.c
- *
- *	This is a simple program that illustrates how to call the MATLAB
- *	Engine functions from a C program.
- *
- * Copyright (c) 1984-98 by The MathWorks, Inc.
- * All rights reserved
- */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "engine.h"
-#define  BUFSIZE 256
+/* $Id: makefile,v 1.1 2000/04/02 04:46:38 bsmith Exp bsmith $ #include "petsc.h" */
 
-int main()
+#include "petsc.h"
+#include "engine.h"   /* Matlab include file  */
 
+/* Pointer to Matlab engine data structure */
+static Engine *ep = PETSC_NULL;
+static char   buffer[1024];
+
+#undef __FUNC__  
+#define __FUNC__ "PetscMatlabEngineInitialize"
+int PetscMatlabEngineInitialize(MPI_Comm comm,char *machine)
 {
-	Engine *ep;
+
+  PetscFunctionBegin;
+  if (ep) PetscFunctionReturn(0);
+  if (!machine) machine = "\0";
+  ep = engOpen(machine);
+  if (!ep) SETERRQ1(1,1,"Unable to start Matlab engine on %s\n",machine);
+  engOutputBuffer(ep,buffer,1024);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "PetscMatlabEngineFinalize"
+int PetscMatlabEngineFinalize(MPI_Comm comm)
+{
+  PetscFunctionBegin;
+  if (!ep) PetscFunctionReturn(0);
+  engClose(ep);
+  ep = PETSC_NULL;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "PetscMatlabEngineEvaluate"
+int PetscMatlabEngineEvaluate(MPI_Comm comm,char *string)
+{
+  PetscFunctionBegin;  
+  engEvalString(ep, string);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ "PetscMatlabEngineGetOutput"
+int PetscMatlabEngineGetOutput(MPI_Comm comm,char **string)
+{
+  PetscFunctionBegin;  
+  *string = buffer + 2;
+  PetscFunctionReturn(0);
+}
+
+#define  BUFSIZE 256
+int dummy()
+{
 	mxArray *T = NULL, *result = NULL;
 	char buffer[BUFSIZE];
 	double time[10] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
-
-	/*
-	 * Start the MATLAB engine locally by executing the string
-	 * "matlab"
-	 *
-	 * To start the session on a remote host, use the name of
-	 * the host as the string rather than \0
-	 *
-	 * For more complicated cases, use any string with whitespace,
-	 * and that string will be executed literally to start MATLAB
-	 */
-	if (!(ep = engOpen("\0"))) {
-		fprintf(stderr, "\nCan't start MATLAB engine\n");
-		return EXIT_FAILURE;
-	}
 
 	/*
 	 * PART I
@@ -49,38 +69,14 @@ int main()
 	 */
 	T = mxCreateDoubleMatrix(1, 10, mxREAL);
 	mxSetName(T, "T");
-	memcpy((void *)mxGetPr(T), (void *)time, sizeof(time));
+	/* memcpy((void *)mxGetPr(T), (void *)time, sizeof(time)); */
 	/*
 	 * Place the variable T into the MATLAB workspace
 	 */
 	engPutArray(ep, T);
 
-	/*
-	 * Evaluate a function of time, distance = (1/2)g.*t.^2
-	 * (g is the acceleration due to gravity)
-	 */
-	engEvalString(ep, "D = .5.*(-9.8).*T.^2;");
-
-	/*
-	 * Plot the result
-	 */
-	engEvalString(ep, "plot(T,D);");
-	engEvalString(ep, "title('Position vs. Time for a falling object');");
-	engEvalString(ep, "xlabel('Time (seconds)');");
-	engEvalString(ep, "ylabel('Position (meters)');");
-
-	/*
-	 * use fgetc() to make sure that we pause long enough to be
-	 * able to see the plot
-	 */
-	printf("Hit return to continue\n\n");
-	fgetc(stdin);
-	/*
-	 * We're done for Part I! Free memory, close MATLAB engine.
-	 */
-	printf("Done for Part I.\n");
 	mxDestroyArray(T);
-	engEvalString(ep, "close;");
+
 
 
 	/*
@@ -132,17 +128,8 @@ int main()
 		printf("X is class %s\t\n", mxGetClassName(result));
 	    }
 	}
-
-	/*
-	 * We're done! Free memory, close MATLAB engine and exit.
-	 */
-	printf("Done!\n");
-	mxDestroyArray(result);
-	engClose(ep);
-	
-	return EXIT_SUCCESS;
+return 0;
 }
-
 
 
 
