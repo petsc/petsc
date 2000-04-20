93ef6b4d786559c4c66292c21814252fe9f0b033
Barry Smith <bsmith@mcs.anl.gov>
2000-04-19 22:08:41 -0600

    Initial revision

---
+++ src/sys/examples/tests/ex15.c
@@ -0,0 +1,32 @@
+/*$Id: ex1.c,v 1.20 2000/04/09 03:09:21 bsmith Exp $*/
+
+/* 
+   Demonstrates PETSc error handlers.
+ */
+
+#include "petsc.h"
+
+#undef __FUNC__
+#define __FUNC__ "CreateError"
+int CreateError(int n)
+{
+  int ierr;
+  if (!n) SETERRQ(1,0,"Error Created");
+  ierr = CreateError(n-1);CHKERRQ(ierr);
+  return 0;
+}
+
+#undef __FUNC__
+#define __FUNC__ "main"
+int main(int argc,char **argv)
+{
+  int ierr;
+  PetscInitialize(&argc,&argv,(char *)0,0);
+  ierr = PetscFPrintf(PETSC_COMM_WORLD,stdout,"Demonstrates PETSc Error Handlers\n");CHKERRQ(ierr);
+  ierr = PetscFPrintf(PETSC_COMM_WORLD,stdout,"The error is a contrived error to test error handling\n");CHKERRQ(ierr);
+  ierr = PetscSynchronizedFlush(PETSC_COMM_WORLD);CHKERRQ(ierr);
+  ierr = CreateError(5);CHKERRQ(ierr);
+  PetscFinalize();
+  return 0;
+}
+ 
