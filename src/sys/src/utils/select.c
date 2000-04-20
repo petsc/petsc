a8881ad090ea3ed2d6203e2bdf8ed28edeb6b6fe
Barry Smith <bsmith@mcs.anl.gov>
2000-04-19 21:35:46 -0600

    Initial revision

---
+++ src/sys/src/utils/select.c
@@ -0,0 +1,36 @@
+/*$Id: arch.c,v 1.38 2000/04/12 04:21:38 bsmith Exp $*/
+#include "petsc.h"         /*I  "petsc.h"  I*/
+#include "sys.h"           /*I  "sys.h"  I*/
+
+#undef __FUNC__  
+#define __FUNC__ /*<a name=""></a>*/"PetscGetArchType"
+/*@C
+     PetscGetArchType - Returns a standardized architecture type for the machine
+     that is executing this routine. 
+
+     Not Collective
+
+     Input Parameter:
+.    slen - length of string buffer
+
+     Output Parameter:
+.    str - string area to contain architecture name, should be at least 
+           10 characters long. Name is truncated if string is not long enough.
+
+     Level: developer
+
+.keywords: architecture, machine     
+@*/
+int PetscGetArchType(char str[],int slen)
+{
+  int ierr;
+
+  PetscFunctionBegin;
+#if defined(PETSC_ARCH_NAME)
+  ierr = PetscStrncpy(str,PETSC_ARCH_NAME,slen);CHKERRQ(ierr);
+#else
+#error "bmake/$PETSC_ARCH/petscconf.h is missing PETSC_ARCH_NAME"
+#endif
+  PetscFunctionReturn(0);
+}
+
