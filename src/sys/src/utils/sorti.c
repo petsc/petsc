2a6b583728dbdee687a14bfe058a2a083e4c7589
Barry Smith <bsmith@mcs.anl.gov>
2000-04-22 22:20:27 -0600

    *** empty log message ***

--- src/sys/src/utils/sorti.c
+++ src/sys/src/utils/sorti.c
@@ -1,4 +1,4 @@
-/*$Id: sorti.c,v 1.23 2000/04/09 04:34:47 bsmith Exp bsmith $*/
+/*$Id: sorti.c,v 1.24 2000/04/12 04:21:38 bsmith Exp bsmith $*/
 /*
    This file contains routines for sorting integers. Values are sorted in place.
 
@@ -12,182 +12,18 @@
 
 #define SWAP(a,b,t) {t=a;a=b;b=t;}
 
-#if defined(PETSC_USE_KNUTH_QUICKSORT)
-
-/* 
-   This quick-sort is from George Karypis METIS, and he says it
-     originally comes from Donald Earvin Knuth TeX. 
-
-   Reportedly it runs faster on some machines than quicksort; with PETSc 
-   we have not seen faster performance, hence it is not used by default.
-
-   Contributed by: Matthew Knepley
-
-*/
-#define THRESH      1        /* Threshold for insertion */
-#define MTHRESH     6        /* Threshold for median */
-
-#undef __FUNC__
-#define __FUNC__ /*<a name=""></a>*/"TeXqsort_Private" 
-static int TeXqsort_Private(int *base,int *max)
-{
-  int *i;
-  int *j;
-  int *jj;
-  int *mid;
-  int c;
-  int *tmp;
-  int lo;
-  int hi;
-
-  PetscFunctionBegin;
-  lo = max - base;              /* number of elements as ints */
-  do
-  {
-    mid = base + ((unsigned) lo>>1);
-    if (lo >= MTHRESH)
-    {
-      j = (*base > *mid ? base : mid);
-      tmp = max - 1;
-      if (*j > *tmp)
-      {
-        j = (j == base ? mid : base); /* switch to first loser */
-        if (*j < *tmp)
-          j = tmp;
-      }
-
-      if (j != mid)
-      {  /* SWAP */
-        c = *mid;
-        *mid = *j;
-        *j = c;
-      }
-    }
-
-    /* Semi-standard quicksort partitioning/swapping */
-    for (i = base,j = max - 1; ;)
-    {
-      while(i < mid && *i <= *mid)
-        i++;
-      while (j > mid)
-      {
-        if (*mid <= *j)
-        {
-          j--;
-          continue;
-        }
-        tmp = i + 1;    /* value of i after swap */
-        if (i == mid)   /* j <-> mid, new mid is j */
-          mid = jj = j;
-        else            /* i <-> j */
-          jj = j--;
-        goto swap;
-      }
-
-      if (i == mid)
-        break;
-      else
-      {                 /* i <-> mid, new mid is i */
-        jj = mid;
-        tmp = mid = i;  /* value of i after swap */
-        j--;
-      }
-    swap:
-      c   = *i;
-      *i  = *jj;
-      *jj = c;
-      i   = tmp;
-    }
-
-    i = (j = mid) + 1;
-    if ((lo = j - base) <= (hi = max - i))
-    {
-      if (lo >= THRESH)
-        TeXqsort_Private(base,j);
-      base = i;
-      lo = hi;
-    }
-    else
-    {
-      if (hi >= THRESH)
-        TeXqsort_Private(i,max);
-      max = j;
-    }
-  } while (lo >= THRESH);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNC__
-#define __FUNC__ /*<a name=""></a>*/"PetsciIqsort" 
-static int PetsciIqsort(int *base,int right)
-{
-  int *i;
-  int *j;
-  int *lo;
-  int *hi;
-  int *min;
-  int c;
-  int *max;
-
-  PetscFunctionBegin;
-  max = base + right + 1;
-
-  if (right >= THRESH + 1)
-  {
-    TeXqsort_Private(base,max);
-    hi = base + THRESH;
-  }
-  else
-    hi = max;
-
-  for(j = lo = base; lo++ < hi;)
-  {
-    if (*j > *lo)
-      j = lo;
-  }
-  if (j != base)
-  { /* swap j into place */
-    c     = *base;
-    *base = *j;
-    *j    = c;
-  }
-
-  for(min = base; (hi = min += 1) < max;)
-  {
-    while (*(--hi) > *min);
-    if ((hi += 1) != min)
-    {
-      for(lo = min + 1; --lo >= min;)
-      {
-        c = *lo;
-        for(i = j = lo; (j -= 1) >= hi; i = j)
-          *i = *j;
-        *i = c;
-      }
-    }
-  }
-  PetscFunctionReturn(0);
-}
-
-#else
-
 /* -----------------------------------------------------------------------*/
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name=""></a>*/"PetsciIqsort"
+#define __FUNC__ /*<a name=""></a>*/"PetscSortInt_Private"
 /* 
    A simple version of quicksort; taken from Kernighan and Ritchie, page 87.
    Assumes 0 origin for v, number of elements = right+1 (right is index of
    right-most member). 
-
-   Tests on the IBM 590 show this performing about 10% better then the 
-   Knuth quicksort above. Thus this is the default. With the compile flag
-   PETSC_USE_KNUTH_QUICKSORT the above sort will be used.
-
 */
-static int PetsciIqsort(int *v,int right)
+static int PetscSortInt_Private(int *v,int right)
 {
-  int i,vl,last,tmp;
+  int i,vl,last,tmp,ierr;
 
   PetscFunctionBegin;
   if (right <= 1) {
@@ -203,12 +39,10 @@ static int PetsciIqsort(int *v,int right)
     if (v[i] < vl) {last++; SWAP(v[last],v[i],tmp);}
   }
   SWAP(v[0],v[last],tmp);
-  PetsciIqsort(v,last-1);
-  PetsciIqsort(v+last+1,right-(last+1));
+  ierr = PetscSortInt_Private(v,last-1);CHKERRQ(ierr);
+  ierr = PetscSortInt_Private(v+last+1,right-(last+1));CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
-
-#endif
 
 #undef __FUNC__  
 #define __FUNC__ /*<a name=""></a>*/"PetscSortInt" 
@@ -229,7 +63,7 @@ static int PetsciIqsort(int *v,int right)
 @*/
 int PetscSortInt(int n,int i[])
 {
-  int j,k,tmp,ik;
+  int ierr,j,k,tmp,ik;
 
   PetscFunctionBegin;
   if (n<8) {
@@ -242,8 +76,85 @@ int PetscSortInt(int n,int i[])
 	}
       }
     }
-  } else PetsciIqsort(i,n-1);
+  } else {
+    ierr = PetscSortInt_Private(i,n-1);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
+
+#define SWAP2(a,b,c,d,t) {t=a;a=b;b=t;t=c;c=d;d=t;}
+
+/* -----------------------------------------------------------------------*/
+
+#undef __FUNC__  
+#define __FUNC__ /*<a name=""></a>*/"PetscSortIntWithArray_Private"
+/* 
+   A simple version of quicksort; taken from Kernighan and Ritchie, page 87.
+   Assumes 0 origin for v, number of elements = right+1 (right is index of
+   right-most member). 
+*/
+static int PetscSortIntWithArray_Private(int *v,int *V,int right)
+{
+  int i,vl,last,tmp,ierr;
+
+  PetscFunctionBegin;
+  if (right <= 1) {
+    if (right == 1) {
+      if (v[0] > v[1]) SWAP2(v[0],v[1],V[0],V[1],tmp);
+    }
+    PetscFunctionReturn(0);
+  }
+  SWAP2(v[0],v[right/2],V[0],V[right/2],tmp);
+  vl   = v[0];
+  last = 0;
+  for (i=1; i<=right; i++) {
+    if (v[i] < vl) {last++; SWAP2(v[last],v[i],V[last],V[i],tmp);}
+  }
+  SWAP2(v[0],v[last],V[0],V[last],tmp);
+  ierr = PetscSortIntWithArray_Private(v,V,last-1);CHKERRQ(ierr);
+  ierr = PetscSortIntWithArray_Private(v+last+1,V+last+1,right-(last+1));CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNC__  
+#define __FUNC__ /*<a name=""></a>*/"PetscSortIntWithArray" 
+/*@
+   PetscSortIntWithArray - Sorts an array of integers in place in increasing order;
+       changes a second array to match the sorted first array.
+
+   Not Collective
+
+   Input Parameters:
++  n  - number of values
+.  i  - array of integers
+-  I - second array if integers
+
+   Level: intermediate
+
+.keywords: sort, integer
+
+.seealso: PetscSortDouble(), PetscSortIntPermutation(), PetscSortInt()
+@*/
+int PetscSortIntWithArray(int n,int i[],int I[])
+{
+  int ierr,j,k,tmp,ik;
+
+  PetscFunctionBegin;
+  if (n<8) {
+    for (k=0; k<n; k++) {
+      ik = i[k];
+      for (j=k+1; j<n; j++) {
+	if (ik > i[j]) {
+	  SWAP2(i[k],i[j],I[k],I[j],tmp);
+	  ik = i[k];
+	}
+      }
+    }
+  } else {
+    ierr = PetscSortIntWithArray_Private(i,I,n-1);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
 
 
