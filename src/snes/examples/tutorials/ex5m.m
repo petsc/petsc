11fff81c704836502451be06ed36c30061deee04
Barry Smith <bsmith@mcs.anl.gov>
2000-05-07 21:54:05 -0600

    *** empty log message ***

--- src/snes/examples/tutorials/ex5m.m
+++ src/snes/examples/tutorials/ex5m.m
@@ -1,5 +1,5 @@
 function localF = ex5m(localX,hx,hy,lambda)
-% $Id: launch.m,v 1.4 2000/02/02 20:07:58 bsmith Exp $
+% $Id: ex5m.m,v 1.2 2000/05/08 03:52:14 bsmith Exp bsmith $
 %
 %  Matlab routine that does the FormFunction() for ex5m.c
 %
@@ -16,5 +16,11 @@ localF = localX;
 u   = localX(2:m-1,2:n-1);
 uxx = (2.0*u - localX(1:m-2,2:n-1) - localX(3:m,2:n-1))*hydhx;
 uyy = (2.0*u - localX(2:m-1,1:n-2) - localX(2:m-1,3:n))*hxdhy;
-
+%
+%  evaluate interior part of function
+%
 localF(2:m-1,2:n-1) = uxx + uyy - sc*exp(u);
+%
+%  This uses a clever (though not particularly efficient) way of 
+% evaluating the function so that it works for any subdomain
+% (with or without any of the true boundary)
