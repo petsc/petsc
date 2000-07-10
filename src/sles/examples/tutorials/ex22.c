408f8ac5045cd6cdb609c9d721d0f10a232f708b
Barry Smith <bsmith@mcs.anl.gov>
2000-07-09 21:37:52 -0600

    *** empty log message ***

--- src/sles/examples/tutorials/ex22.c
+++ src/sles/examples/tutorials/ex22.c
@@ -1,4 +1,4 @@
-/*$Id: ex20.c,v 1.6 2000/05/05 22:18:00 balay Exp bsmith $*/
+/*$Id: ex22.c,v 1.1 2000/07/06 17:44:11 bsmith Exp bsmith $*/
 /*
 Laplacian in 3D. Modeled by the partial differential equation
 
@@ -23,138 +23,51 @@ The command line options are:\n\
 #include "petscsles.h"
 #include "petscmg.h"
 
-#define MAX_LEVELS 12
-
-typedef struct {
-   DA         da;
-   Vec        x,b,r;            /* global vectors */
-   Mat        J;                /* Jacobian on grid */
-   SLES       sles;
-   Mat        R;                /* R and Rscale are not set on the coarsest grid */
-} GridCtx;
-
-#define MAX_LEVELS 12
-
-typedef struct {
-   GridCtx     grid[MAX_LEVELS];
-   int         ratio;            /* ratio of grid lines between grid levels */
-   int         nlevels;
-} AppCtx;
 
 
 extern int ComputeJacobian(DA,Mat);
 
+#undef __FUNC__
+#define __FUNC__ "main"
 int main(int argc,char **argv)
 {
-  MPI_Comm     comm;
-  SLES         sles;
-  PC           pc;
-  Vec          x,b,r;
-  Mat          J;
-  DA           da;
-  AppCtx       user;
-  int          Nx = PETSC_DECIDE,Ny = PETSC_DECIDE,Nz = PETSC_DECIDE,i;
-  int          ierr,N;
-  int          its,m,mx,my,mz;
-  double       norm;
-  Scalar       one = 1.0,mone = -1.0;
-  PetscTruth   no_output;
+  int  ierr,i,sw = 1,dof = 1,mx = 2,my = 2,mz = 2;
+  DAMG *ctx;
+  DA   cda; /* DA for the coarsest grid */
+  SLES sles;
 
   PetscInitialize(&argc,&argv,(char *)0,help);
-  comm = PETSC_COMM_WORLD;
+  ierr = OptionsGetInt(0,"-stencil_width",&sw,0);CHKERRQ(ierr);
+  ierr = OptionsGetInt(0,"-dof",&dof,0);CHKERRQ(ierr);
+  ierr = OptionsGetInt(0,"-mx",&mx,0);CHKERRQ(ierr);
+  ierr = OptionsGetInt(0,"-my",&my,0);CHKERRQ(ierr);
+  ierr = OptionsGetInt(0,"-mz",&mz,0);CHKERRQ(ierr);
 
-  ierr = OptionsHasName(PETSC_NULL,"-no_output",&no_output);CHKERRA(ierr);
-  
-  user.ratio      = 2;
-  user.nlevels    = 2;
-  mx = 5;
-  my = 5;
-  mz = 5; 
-  ierr = OptionsGetInt(PETSC_NULL,"-ratio",&user.ratio,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-nlevels",&user.nlevels,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-mx",&mx,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-my",&my,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-mz",&mz,PETSC_NULL);CHKERRA(ierr);
+  ierr = DAMGCreate(PETSC_COMM_WORLD,3,&ctx);CHKERRQ(ierr);
 
-  ierr = OptionsGetInt(PETSC_NULL,"-Nx",&Nx,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-Ny",&Ny,PETSC_NULL);CHKERRA(ierr);
-  ierr = OptionsGetInt(PETSC_NULL,"-Nz",&Nz,PETSC_NULL);CHKERRA(ierr);
+  ierr = DACreate3d(PETSC_COMM_WORLD,DA_NONPERIODIC,DA_STENCIL_STAR,mx,my,mz,PETSC_DECIDE,
+                    PETSC_DECIDE,PETSC_DECIDE,sw,dof,0,0,0,&cda);CHKERRQ(ierr);  
+  ierr = DASetFieldName(cda,0,"First field");CHKERRQ(ierr);
+  ierr = DASetUniformCoordinates(cda,-1.0,1.0,-2.0,2.0,-3.0,3.0);CHKERRQ(ierr);
+  ierr = DAMGSetCoarseDA(ctx,cda);CHKERRQ(ierr);
+  ierr = DADestroy(cda);CHKERRQ(ierr);
 
-  ierr = SLESCreate(comm,&sles);CHKERRA(ierr);
-  ierr = SLESGetPC(sles,&pc);CHKERRA(ierr);
-  ierr = PCSetType(pc,PCMG);CHKERRA(ierr);
-  ierr = MGSetLevels(pc,user.nlevels);CHKERRA(ierr);
-  ierr = SLESSetFromOptions(sles);CHKERRA(ierr);
+  ierr = DAMGSetOperator(ctx,ComputeJacobian);CHKERRQ(ierr);
 
-  for (i=0; i<user.nlevels; i++) {
-    ierr = DACreate3d(comm,DA_NONPERIODIC,DA_STENCIL_BOX,mx,my,mz,Nx,Ny,Nz,1,1,PETSC_NULL,PETSC_NULL,PETSC_NULL,&da);CHKERRA(ierr);
-    ierr = DACreateGlobalVector(da,&x);CHKERRA(ierr);
-    ierr = VecDuplicate(x,&b);CHKERRA(ierr);
-    ierr = VecDuplicate(x,&r);CHKERRA(ierr);
-    ierr = VecGetLocalSize(x,&m);CHKERRA(ierr);
-    ierr = VecGetSize(x,&N);CHKERRA(ierr);
-    ierr = MatCreateMPIAIJ(comm,m,m,N,N,7,PETSC_NULL,5,PETSC_NULL,&J);CHKERRA(ierr);
-    ierr = ComputeJacobian(da,J);CHKERRA(ierr);
+  ierr = SLESCreate(PETSC_COMM_WORLD,&sles);CHKERRQ(ierr);
+  ierr = DAMGSetSLES(ctx,sles);CHKERRQ(ierr);
 
-    ierr = MGGetSmoother(pc,i,&user.grid[i].sles);CHKERRA(ierr);
-    ierr = SLESSetFromOptions(user.grid[i].sles);CHKERRA(ierr);
-    ierr = SLESSetOperators(user.grid[i].sles,J,J,DIFFERENT_NONZERO_PATTERN);CHKERRA(ierr);
-    ierr = MGSetX(pc,i,x);CHKERRA(ierr); 
-    ierr = MGSetRhs(pc,i,b);CHKERRA(ierr); 
-    ierr = MGSetR(pc,i,r);CHKERRA(ierr); 
-    ierr = MGSetResidual(pc,i,MGDefaultResidual,J);CHKERRA(ierr);
-
-    user.grid[i].da = da;
-    user.grid[i].J  = J;
-    user.grid[i].x  = x;
-    user.grid[i].b  = b;
-    user.grid[i].r  = r;
-
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"Grid %d size %d by %d by %d\n",i,mx,my,mz);CHKERRA(ierr);
-    mx = user.ratio*(mx-1)+1; 
-    my = user.ratio*(my-1)+1;
-    mz = user.ratio*(mz-1)+1;
-  }
-
-  /* Create interpolation between the levels */
-  for (i=1; i<user.nlevels; i++) {
-    ierr = DAGetInterpolation(user.grid[i-1].da,user.grid[i].da,&user.grid[i].R,PETSC_NULL);CHKERRA(ierr);
-    ierr = MGSetInterpolate(pc,i,user.grid[i].R);CHKERRA(ierr); 
-    ierr = MGSetRestriction(pc,i,user.grid[i].R);CHKERRA(ierr); 
-  }
-  
-  ierr = VecSet(&one,b);CHKERRA(ierr);
-  ierr = SLESSetOperators(sles,J,J,DIFFERENT_NONZERO_PATTERN);CHKERRA(ierr);
-  ierr = SLESSolve(sles,b,x,&its);CHKERRA(ierr);
-
-  /* compute residual */
-  ierr = MatMult(J,x,r);CHKERRA(ierr);
-  ierr = VecAXPY(&mone,b,r);CHKERRA(ierr);
-  ierr = VecNorm(r,NORM_2,&norm);CHKERRA(ierr);
-
-  if (!no_output) {
-    ierr = PetscPrintf(comm,"Residual norm %A iterations = %d,\n",norm,its);CHKERRA(ierr);
-  }
-
-  for (i=1; i<user.nlevels; i++) {
-    ierr = MatDestroy(user.grid[i].R);CHKERRA(ierr);
-  }
-  for (i=0; i<user.nlevels; i++) {
-    ierr = DADestroy(user.grid[i].da);CHKERRA(ierr);
-    ierr = MatDestroy(user.grid[i].J);CHKERRA(ierr);
-    ierr = VecDestroy(user.grid[i].x);CHKERRA(ierr);
-    ierr = VecDestroy(user.grid[i].b);CHKERRA(ierr);
-    ierr = VecDestroy(user.grid[i].r);CHKERRA(ierr);
-  }
-  ierr = SLESDestroy(sles);CHKERRA(ierr);
-
-  
+  ierr = DAMGView(ctx,VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = DAMGDestroy(ctx);CHKERRQ(ierr);
+  ierr = SLESDestroy(sles);CHKERRQ(ierr);
   PetscFinalize();
 
   return 0;
 }
 
     
+#undef __FUNC__
+#define __FUNC__ "ComputeJacobian"
 int ComputeJacobian(DA da,Mat jac)
 {
   int    *ltog,ierr,i,j,k,mx,my,mz,xm,ym,zm,xs,ys,zs,Xm,Ym,Zm,Xs,Ys,Zs,row,nloc,col[7],base1,grow;
