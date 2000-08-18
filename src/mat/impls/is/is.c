bc7037ae8ccbc012aae7cae600d52a3a31b9c1c5
Barry Smith <bsmith@mcs.anl.gov>
2000-08-18 13:38:31 -0600

    *** empty log message ***

--- src/mat/impls/is/is.c
+++ src/mat/impls/is/is.c
@@ -1,4 +1,4 @@
-/*$Id: nn.c,v 1.5 2000/06/20 03:58:26 bsmith Exp bsmith $*/
+/*$Id: is.c,v 1.5 2000/06/20 03:58:26 bsmith Exp bsmith $*/
 /*
     Creates a matrix class for using the Neumann-Neumann type preconditioners.
    This stores the matrices in globally unassembled form. Each processor 
@@ -12,14 +12,14 @@
 
 */
 
-#include "src/mat/impls/nn/nn.h"      /*I "mat.h" I*/
+#include "src/mat/impls/is/is.h"      /*I "mat.h" I*/
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatDestroy_NN"></a>*/"MatDestroy_NN" 
-int MatDestroy_NN(Mat A)
+#define __FUNC__ /*<a name="MatDestroy_IS"></a>*/"MatDestroy_IS" 
+int MatDestroy_IS(Mat A)
 {
   int    ierr;
-  Mat_NN *b = (Mat_NN*)A->data;
+  Mat_IS *b = (Mat_IS*)A->data;
 
   PetscFunctionBegin;
   if (b->A) {
@@ -34,9 +34,6 @@ int MatDestroy_NN(Mat A)
   if (b->y) {
     ierr = VecDestroy(b->y);CHKERRQ(ierr);
   }
-  if (b->zeroedrows) {
-    ierr = PetscFree(b->zeroedrows);CHKERRQ(ierr);
-  }
   if (b->mapping) {
     ierr = ISLocalToGlobalMappingDestroy(b->mapping);CHKERRQ(ierr);
   }
@@ -46,64 +43,55 @@ int MatDestroy_NN(Mat A)
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatMult_NN"></a>*/"MatMult_NN" 
-int MatMult_NN(Mat A,Vec x,Vec y)
+#define __FUNC__ /*<a name="MatMult_IS"></a>*/"MatMult_IS" 
+int MatMult_IS(Mat A,Vec x,Vec y)
 {
-  int    ierr,i;
-  Mat_NN *nn = (Mat_NN*)A->data;
-  Scalar zero = 0.0,*array,*xarray;
+  int    ierr;
+  Mat_IS *is = (Mat_IS*)A->data;
+  Scalar zero = 0.0;
 
   PetscFunctionBegin;
   /*  scatter the global vector x into the local work vector */
-  ierr = VecScatterBegin(x,nn->x,INSERT_VALUES,SCATTER_FORWARD,nn->ctx);CHKERRQ(ierr);
-  ierr = VecScatterEnd(x,nn->x,INSERT_VALUES,SCATTER_FORWARD,nn->ctx);CHKERRQ(ierr);
+  ierr = VecScatterBegin(x,is->x,INSERT_VALUES,SCATTER_FORWARD,is->ctx);CHKERRQ(ierr);
+  ierr = VecScatterEnd(x,is->x,INSERT_VALUES,SCATTER_FORWARD,is->ctx);CHKERRQ(ierr);
 
   /* multiply the local matrix */
-  ierr = MatMult(nn->A,nn->x,nn->y);CHKERRQ(ierr);
+  ierr = MatMult(is->A,is->x,is->y);CHKERRQ(ierr);
 
   /* scatter product back into global memory */
   ierr = VecSet(&zero,y);CHKERRQ(ierr);
-  ierr = VecScatterBegin(nn->y,y,ADD_VALUES,SCATTER_REVERSE,nn->ctx);CHKERRQ(ierr);
-  ierr = VecScatterEnd(nn->y,y,ADD_VALUES,SCATTER_REVERSE,nn->ctx);CHKERRQ(ierr);
-
-  /* fix the zeroed rows in the matrix */
-  ierr = VecGetArray(y,&array);CHKERRQ(ierr);
-  ierr = VecGetArray(x,&xarray);CHKERRQ(ierr);
-  for (i=0; i<nn->nzeroedrows;i++) {
-    array[nn->zeroedrows[i]] = nn->diag*xarray[nn->zeroedrows[i]];
-  }
-  ierr = VecRestoreArray(x,&xarray);CHKERRQ(ierr);
-  ierr = VecRestoreArray(y,&array);CHKERRQ(ierr);
+  ierr = VecScatterBegin(is->y,y,ADD_VALUES,SCATTER_REVERSE,is->ctx);CHKERRQ(ierr);
+  ierr = VecScatterEnd(is->y,y,ADD_VALUES,SCATTER_REVERSE,is->ctx);CHKERRQ(ierr);
 
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatSetLocalToGlobalMapping_NN"></a>*/"MatSetLocalToGlobalMapping_NN" 
-int MatSetLocalToGlobalMapping_NN(Mat A,ISLocalToGlobalMapping mapping)
+#define __FUNC__ /*<a name="MatSetLocalToGlobalMapping_IS"></a>*/"MatSetLocalToGlobalMapping_IS" 
+int MatSetLocalToGlobalMapping_IS(Mat A,ISLocalToGlobalMapping mapping)
 {
   int    ierr,n;
-  Mat_NN *nn = (Mat_NN*)A->data;
+  Mat_IS *is = (Mat_IS*)A->data;
   IS     from,to;
   Vec    global;
 
   PetscFunctionBegin;
-  nn->mapping = mapping;
+  is->mapping = mapping;
   ierr = PetscObjectReference((PetscObject)mapping);CHKERRQ(ierr);
 
   /* Create the local matrix A */
   ierr = ISLocalToGlobalMappingGetSize(mapping,&n);CHKERRQ(ierr);
-  ierr = MatCreate(PETSC_COMM_SELF,n,n,n,n,&nn->A);CHKERRQ(ierr);
+  ierr = MatCreate(PETSC_COMM_SELF,n,n,n,n,&is->A);CHKERRQ(ierr);
 
   /* Create the local work vectors */
-  ierr = VecCreateSeq(PETSC_COMM_SELF,n,&nn->x);CHKERRQ(ierr);
-  ierr = VecDuplicate(nn->x,&nn->y);CHKERRQ(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_SELF,n,&is->x);CHKERRQ(ierr);
+  ierr = VecDuplicate(is->x,&is->y);CHKERRQ(ierr);
 
   /* setup the global to local scatter */
   ierr = ISCreateStride(PETSC_COMM_SELF,n,0,1,&to);CHKERRQ(ierr);
   ierr = ISLocalToGlobalMappingApplyIS(mapping,to,&from);CHKERRQ(ierr);
   ierr = VecCreateMPI(A->comm,A->n,A->N,&global);CHKERRQ(ierr);
-  ierr = VecScatterCreate(global,from,nn->x,to,&nn->ctx);CHKERRQ(ierr);
+  ierr = VecScatterCreate(global,from,is->x,to,&is->ctx);CHKERRQ(ierr);
   ierr = VecDestroy(global);CHKERRQ(ierr);
   ierr = ISDestroy(to);CHKERRQ(ierr);
   ierr = ISDestroy(from);CHKERRQ(ierr);
@@ -112,93 +100,88 @@ int MatSetLocalToGlobalMapping_NN(Mat A,ISLocalToGlobalMapping mapping)
 
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatSetValuesLocal_NN"></a>*/"MatSetValuesLocal_NN" 
-int MatSetValuesLocal_NN(Mat A,int m,int *rows,int n,int *cols,Scalar *values,InsertMode addv)
+#define __FUNC__ /*<a name="MatSetValuesLocal_IS"></a>*/"MatSetValuesLocal_IS" 
+int MatSetValuesLocal_IS(Mat A,int m,int *rows,int n,int *cols,Scalar *values,InsertMode addv)
 {
   int    ierr;
-  Mat_NN *nn = (Mat_NN*)A->data;
+  Mat_IS *is = (Mat_IS*)A->data;
 
   PetscFunctionBegin;
-  ierr = MatSetValues(nn->A,m,rows,n,cols,values,addv);CHKERRQ(ierr);
+  ierr = MatSetValues(is->A,m,rows,n,cols,values,addv);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatZeroRowsLocal_NN"></a>*/"MatZeroRowsLocal_NN" 
-int MatZeroRowsLocal_NN(Mat A,IS isrows,Scalar *diag)
+#define __FUNC__ /*<a name="MatZeroRowsLocal_IS"></a>*/"MatZeroRowsLocal_IS" 
+int MatZeroRowsLocal_IS(Mat A,IS isrows,Scalar *diag)
 {
-  Mat_NN *nn = (Mat_NN*)A->data;
-  int    ierr,i,n,cnt,*rows;
-  Scalar *array,zero = 0.0;
-  Vec    y;
+  Mat_IS *is = (Mat_IS*)A->data;
+  int    ierr,i,n,*rows;
+  Scalar *array;
 
   PetscFunctionBegin;
-  if (diag) nn->diag = *diag; else nn->diag = 0.0;
 
-  /* 
-      zerorows[] contains the list of rows that have been zeroed and are owned by this
-    processor. During the MatMult() this list is used to "correct" these rows since
-    they may have redundant additions due to being represented in multiple subdomains.
-  */
+  {
+    /*
+       Set up is->x as a "counting vector". This is in order to MatMult_IS
+       work properly in the interface nodes.
+    */
+    Vec    counter;
+    Scalar one=1.0, zero=0.0;
+    ierr = VecCreateMPI(A->comm,A->n,A->N,&counter);CHKERRQ(ierr);
+    ierr = VecSet(&zero,counter);CHKERRQ(ierr);
+    ierr = VecSet(&one,is->x);CHKERRQ(ierr);
+    ierr = VecScatterBegin(is->x,counter,ADD_VALUES,SCATTER_REVERSE,is->ctx);CHKERRQ(ierr);
+    ierr = VecScatterEnd  (is->x,counter,ADD_VALUES,SCATTER_REVERSE,is->ctx);CHKERRQ(ierr);
+    ierr = VecScatterBegin(counter,is->x,INSERT_VALUES,SCATTER_FORWARD,is->ctx);CHKERRQ(ierr);
+    ierr = VecScatterEnd  (counter,is->x,INSERT_VALUES,SCATTER_FORWARD,is->ctx);CHKERRQ(ierr);
+    ierr = VecDestroy(counter); CHKERRQ(ierr);
+  }
   ierr = ISGetLocalSize(isrows,&n);CHKERRQ(ierr);
-  ierr = ISGetIndices(isrows,&rows);CHKERRQ(ierr);
-  ierr = VecSet(&zero,nn->x);CHKERRQ(ierr);
-  ierr = VecGetArray(nn->x,&array);CHKERRQ(ierr);
-  for (i=0; i<n; i++) {
-    array[rows[i]] = 1.0;
+  if (n == 0) {
+    is->pure_neumann = PETSC_TRUE;
+  } else {
+    is->pure_neumann = PETSC_FALSE;
+    ierr = ISGetIndices(isrows,&rows);CHKERRQ(ierr);
+    ierr = VecGetArray(is->x,&array);CHKERRQ(ierr);
+    ierr = MatZeroRows(is->A,isrows,diag);CHKERRQ(ierr);
+    for (i=0; i<n; i++) {
+      MatSetValue(is->A,rows[i],rows[i],(*diag)/(array[rows[i]]),INSERT_VALUES);
+    }
+    ierr = MatAssemblyBegin(is->A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd  (is->A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = VecRestoreArray(is->x,&array);CHKERRQ(ierr);
+    ierr = ISRestoreIndices(isrows,&rows);CHKERRQ(ierr);
   }
-  ierr = VecRestoreArray(nn->x,&array);CHKERRQ(ierr);
-  ierr = ISRestoreIndices(isrows,&rows);CHKERRQ(ierr);
 
-  ierr = VecCreateMPI(A->comm,A->n,A->N,&y);CHKERRQ(ierr);
-  ierr = VecSet(&zero,y);CHKERRQ(ierr);
-  ierr = VecScatterBegin(nn->x,y,ADD_VALUES,SCATTER_REVERSE,nn->ctx);CHKERRQ(ierr);
-  ierr = VecScatterEnd(nn->x,y,ADD_VALUES,SCATTER_REVERSE,nn->ctx);CHKERRQ(ierr);
-
-  ierr = VecGetLocalSize(y,&n);CHKERRQ(ierr);
-  ierr = VecGetArray(y,&array);CHKERRQ(ierr);
-  cnt  = 0;
-  for (i=0; i<n; i++) {
-    if (array[i] != 0.0) cnt++;
-  }  
-  nn->nzeroedrows = cnt;
-  nn->zeroedrows  = (int*)PetscMalloc((cnt+1)*sizeof(int));CHKPTRQ(nn->zeroedrows);
-  cnt = 0;
-  for (i=0; i<n; i++) {
-    if (array[i] != 0.0) nn->zeroedrows[cnt++] = i;
-  }
-  ierr = VecRestoreArray(y,&array);CHKERRQ(ierr);
-  ierr = VecDestroy(y);CHKERRQ(ierr);
-
-  ierr = MatZeroRows(nn->A,isrows,diag);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatAssemblyBegin_NN"></a>*/"MatAssemblyBegin_NN" 
-int MatAssemblyBegin_NN(Mat A,MatAssemblyType type)
+#define __FUNC__ /*<a name="MatAssemblyBegin_IS"></a>*/"MatAssemblyBegin_IS" 
+int MatAssemblyBegin_IS(Mat A,MatAssemblyType type)
 {
-  Mat_NN *nn = (Mat_NN*)A->data;
+  Mat_IS *is = (Mat_IS*)A->data;
   int    ierr;
   PetscFunctionBegin;
-  ierr = MatAssemblyBegin(nn->A,type);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(is->A,type);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatAssemblyEnd_NN"></a>*/"MatAssemblyEnd_NN" 
-int MatAssemblyEnd_NN(Mat A,MatAssemblyType type)
+#define __FUNC__ /*<a name="MatAssemblyEnd_IS"></a>*/"MatAssemblyEnd_IS" 
+int MatAssemblyEnd_IS(Mat A,MatAssemblyType type)
 {
-  Mat_NN *nn = (Mat_NN*)A->data;
+  Mat_IS *is = (Mat_IS*)A->data;
   int    ierr;
   PetscFunctionBegin;
-  ierr = MatAssemblyEnd(nn->A,type);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(is->A,type);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatGetSize_NN"></a>*/"MatGetSize_NN"
-int MatGetSize_NN(Mat A,int *m,int *n)
+#define __FUNC__ /*<a name="MatGetSize_IS"></a>*/"MatGetSize_IS"
+int MatGetSize_IS(Mat A,int *m,int *n)
 {
   PetscFunctionBegin;
   if (m) *m = A->M; 
@@ -207,8 +190,8 @@ int MatGetSize_NN(Mat A,int *m,int *n)
 }
 
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatGetLocalSize_NN"></a>*/"MatGetLocalSize_NN"
-int MatGetLocalSize_NN(Mat A,int *m,int *n)
+#define __FUNC__ /*<a name="MatGetLocalSize_IS"></a>*/"MatGetLocalSize_IS"
+int MatGetLocalSize_IS(Mat A,int *m,int *n)
 {
   PetscFunctionBegin;
   if (m) *m = A->m; 
@@ -218,28 +201,28 @@ int MatGetLocalSize_NN(Mat A,int *m,int *n)
 
 EXTERN_C_BEGIN
 #undef __FUNC__  
-#define __FUNC__ /*<a name="MatCreate_NN"></a>*/"MatCreate_NN" 
-int MatCreate_NN(Mat A)
+#define __FUNC__ /*<a name="MatCreate_IS"></a>*/"MatCreate_IS" 
+int MatCreate_IS(Mat A)
 {
   int    ierr;
-  Mat_NN *b;
+  Mat_IS *b;
 
   PetscFunctionBegin;
-  A->data             = (void*)(b = PetscNew(Mat_NN));CHKPTRQ(b);
-  ierr = PetscMemzero(b,sizeof(Mat_NN));CHKERRQ(ierr);
+  A->data             = (void*)(b = PetscNew(Mat_IS));CHKPTRQ(b);
+  ierr = PetscMemzero(b,sizeof(Mat_IS));CHKERRQ(ierr);
   ierr = PetscMemzero(A->ops,sizeof(struct _MatOps));CHKERRQ(ierr);
   A->factor           = 0;
   A->mapping          = 0;
 
-  A->ops->mult                    = MatMult_NN;
-  A->ops->destroy                 = MatDestroy_NN;
-  A->ops->setlocaltoglobalmapping = MatSetLocalToGlobalMapping_NN;
-  A->ops->setvalueslocal          = MatSetValuesLocal_NN;
-  A->ops->zerorowslocal           = MatZeroRowsLocal_NN;
-  A->ops->assemblybegin           = MatAssemblyBegin_NN;
-  A->ops->assemblyend             = MatAssemblyEnd_NN;
-  A->ops->getsize                 = MatGetSize_NN;
-  A->ops->getlocalsize            = MatGetLocalSize_NN;
+  A->ops->mult                    = MatMult_IS;
+  A->ops->destroy                 = MatDestroy_IS;
+  A->ops->setlocaltoglobalmapping = MatSetLocalToGlobalMapping_IS;
+  A->ops->setvalueslocal          = MatSetValuesLocal_IS;
+  A->ops->zerorowslocal           = MatZeroRowsLocal_IS;
+  A->ops->assemblybegin           = MatAssemblyBegin_IS;
+  A->ops->assemblyend             = MatAssemblyEnd_IS;
+  A->ops->getsize                 = MatGetSize_IS;
+  A->ops->getlocalsize            = MatGetLocalSize_IS;
 
   ierr = PetscSplitOwnership(A->comm,&A->m,&A->M);CHKERRQ(ierr);
   ierr = PetscSplitOwnership(A->comm,&A->n,&A->N);CHKERRQ(ierr);
@@ -250,7 +233,7 @@ int MatCreate_NN(Mat A)
   b->ctx        = 0;
   b->x          = 0;  
   b->y          = 0;  
-  b->zeroedrows = 0;
+
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
