f731ce2de7bb64edd6722a2fc87e3459d144d2e8
Satish Balay <balay@mcs.anl.gov>
2000-08-23 12:38:37 -0600

    *** empty log message ***

--- src/fortran/f90/tests/ex1.c
+++ src/fortran/f90/tests/ex1.c
@@ -1,3 +1,8 @@
+/*$Id: ex1.c,v 1.61 2000/08/17 04:51:19 bsmith Exp $*/
+
+#include <stdio.h>
+#include "src/fortran/f90/zf90.h"
+
 #ifdef PETSC_HAVE_FORTRAN_CAPS
 #define fortran_routine_ FORTRAN_ROUTINE
 #define c_routine_ C_ROUTINE
@@ -6,8 +11,6 @@
 #define c_routine_ c_routine
 #endif
 
-#include <stdio.h>
-#include "src/fortran/f90/zf90.h"
 typedef struct {
   int a;
   array1d b;
@@ -18,9 +21,12 @@ extern void fortran_routine_(abc *x);
 
 void c_routine_(abc *x)
 {
-  double *data = (double*)b->data;
+  double *data = (double*)x->b.addr;
+
+  printf("From C: %d %5.2e %d\n",x->a,data[0],x->c);
+
   x->a = 2;
-  data[0] = 22.0
+  data[0] = 22.0;
   x->c = 222;
   fortran_routine_(x); 
 }
