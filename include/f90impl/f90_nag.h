2d229acf308b0313dfdd3f9bf187b0ae70b1c5d6
Satish Balay <balay@mcs.anl.gov>
2000-09-06 21:05:23 -0600

    Initial revision

---
+++ include/f90impl/f90_nag.h
@@ -0,0 +1,11 @@
+/* $Id: f90impl.h,v 1.16 2000/07/21 01:12:21 balay Exp balay $ */
+
+#if !defined(__F90_NAG_H)
+#define __F90_NAG_H
+
+#include "/usr/local/lib/f90/f90.h"
+#define _p_F90Array1d Dope1
+#define _p_F90Array2d Dope2
+#define _p_F90Array3d Dope3
+
+#endif
