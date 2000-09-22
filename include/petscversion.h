21e19de8e2f6076de70869a95824c7a5f53b2a23
Satish Balay <balay@mcs.anl.gov>
2000-09-22 15:34:21 -0600

    *** empty log message ***

--- include/petscversion.h
+++ include/petscversion.h
@@ -1,4 +1,4 @@
-/* $Id: petscversion.h,v 1.21 2000/03/27 16:27:49 balay Exp bsmith $ */
+/* $Id: petscversion.h,v 1.22 2000/05/18 19:18:07 bsmith Exp balay $ */
 #if !defined(__PETSCVERSION_H)
 #define __PETSCVERSION_H
 
@@ -11,13 +11,13 @@
     docs/tex/manual/manual.tex and
     docs/tex/manual/manual_tex.tex.
 */
-#define PETSC_VERSION_NUMBER "PETSc Version 2.0.28, Released March 27, 2000"
+#define PETSC_VERSION_NUMBER "PETSc Version 2.0.29, Released Sept 26, 2000"
 
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    0
-#define PETSC_VERSION_SUBMINOR 28
+#define PETSC_VERSION_SUBMINOR 29
 #define PETSC_PATCH_LEVEL      0
-#define PETSC_VERSION_DATE     "March 27, 1999"
+#define PETSC_VERSION_DATE     "Sept 26, 2000"
 #define PETSC_AUTHOR_INFO      "\
        The PETSc Team\
     petsc-maint@mcs.anl.gov\n\
