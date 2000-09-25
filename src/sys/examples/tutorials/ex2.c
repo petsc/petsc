64e7d9d02f7512a6fe4c247b6dd1b8d4ca83bffd
Lois Curfman McInnes <curfman@mcs.anl.gov>
2000-09-25 09:25:17 -0600

    *** empty log message ***

--- src/sys/examples/tutorials/ex2.c
+++ src/sys/examples/tutorials/ex2.c
@@ -1,4 +1,4 @@
-/*$Id: ex2.c,v 1.9 2000/01/11 20:59:51 bsmith Exp bsmith $*/
+/*$Id: ex2.c,v 1.10 2000/09/22 20:42:48 bsmith Exp curfman $*/
 
 /* Program usage:  mpirun ex2 [-help] [all PETSc options] */
 
@@ -19,7 +19,7 @@ int main(int argc,char **argv)
   int ierr,rank,size;
 
   /*
-    Every PETSc routine should begin with the PetscInitialize() routine.
+    Every PETSc program should begin with the PetscInitialize() routine.
     argc, argv - These command line arguments are taken to extract the options
                  supplied to PETSc and options supplied to MPI.
     help       - When PETSc executable is invoked with the option -help, 
@@ -40,11 +40,11 @@ int main(int argc,char **argv)
      Here we would like to print only one message that represents
      all the processes in the group.  We use PetscPrintf() with the 
      communicator PETSC_COMM_WORLD.  Thus, only one message is
-     printed representng PETSC_COMM_WORLD, i.e., all the processors.
+     printed representing PETSC_COMM_WORLD, i.e., all the processors.
   */
   ierr = PetscPrintf(PETSC_COMM_WORLD,"Number of processors = %d, rank = %d\n",size,rank);CHKERRA(ierr);
   /* 
-     Here we would like to print info from each process such that
+     Here we would like to print info from each process, such that
      output from process "n" appears after output from process "n-1".
      To do this we use a combination of PetscSynchronizedPrintf() and
      PetscSynchronizedFlush() with the communicator PETSC_COMM_WORLD.
@@ -72,8 +72,8 @@ int main(int argc,char **argv)
      Always call PetscFinalize() before exiting a program.  This routine
        - finalizes the PETSc libraries as well as MPI
        - provides summary and diagnostic information if certain runtime
-         options are chosen (e.g., -log_summary).  See PetscFinalize()
-     manpage for more information.
+         options are chosen (e.g., -log_summary).  
+     See the PetscFinalize() manpage for more information.
   */
   ierr = PetscFinalize();CHKERRA(ierr);
   return 0;
