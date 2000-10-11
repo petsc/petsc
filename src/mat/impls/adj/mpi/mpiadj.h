21528e8626107c4e560e4fa08229eea60db927d8
Barry Smith <bsmith@mcs.anl.gov>
2000-10-11 11:49:33 -0600

    *** empty log message ***

--- src/mat/impls/adj/mpi/mpiadj.h
+++ src/mat/impls/adj/mpi/mpiadj.h
@@ -1,4 +1,4 @@
-/* $Id: mpiadj.h,v 1.6 2000/04/09 03:10:00 bsmith Exp bsmith $ */
+/* $Id: mpiadj.h,v 1.7 2000/08/01 20:02:19 bsmith Exp bsmith $ */
 
 #include "src/mat/matimpl.h"
 
@@ -15,7 +15,6 @@
 typedef struct {
   int              *rowners;         /* ranges owned by each processor */
   int              rstart,rend;     /* start and end of local rows */
-  int              m;                /* local rows */
   int              nz;
   int              *diag;            /* pointers to diagonal elements, if they exist */
   int              *i;               /* pointer to beginning of each row */
