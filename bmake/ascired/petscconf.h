845f2a8a775133e94696ac3acadb811fc1096280
Barry Smith <bsmith@mcs.anl.gov>
2000-11-02 16:25:57 -0600

    *** empty log message ***

--- bmake/ascired/petscconf.h
+++ bmake/ascired/petscconf.h
@@ -1,5 +1,5 @@
 #ifdef PETSC_RCS_HEADER
-"$Id: petscconf.h,v 1.7 2000/11/02 21:52:51 bsmith Exp bsmith $"
+"$Id: petscconf.h,v 1.8 2000/11/02 22:17:32 bsmith Exp bsmith $"
 "Defines the configuration for this machine"
 #endif
 
@@ -46,5 +46,6 @@
 #define PETSC_USE_KBYTES_FOR_SIZE
 
 #define PETSC_CANNOT_START_DEBUGGER
+#define PETSC_MISSING_GETPWUID
 
 #endif
