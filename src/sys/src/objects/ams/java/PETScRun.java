aac7609609ae628d10a8b77c64db46e4d798bbaa
Barry Smith <bsmith@mcs.anl.gov>
2000-11-03 15:30:33 -0600

    *** empty log message ***

--- src/sys/src/objects/ams/java/PETScRun.java
+++ src/sys/src/objects/ams/java/PETScRun.java
@@ -1,4 +1,4 @@
-/*$Id: PETScRun.java,v 1.3 2000/10/25 19:12:08 bsmith Exp bsmith $*/
+/*$Id: PETScRun.java,v 1.4 2000/10/26 18:01:10 bsmith Exp bsmith $*/
 /*
      Compiles and runs a PETSc program
 */
@@ -8,44 +8,124 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class PETScRun
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class PETScRun extends java.applet.Applet
 {
-  public PETScRun() {/*--------------------------------------------------------*/
-    ;
-  }
- 
-  public void start() throws java.io.IOException
-  {
-    Socket sock = new Socket("cclogin1",2000);
-    sock.setSoLinger(true,5);
+  static final int MACHINE = 0,DIRECTORY = 1,MAXNP = 2,EXAMPLES = 3;
+  Hashtable        systems[];
 
-    /* construct properties to send to server */
-    Properties   properties = new Properties();
-    properties.setProperty("PETSC_ARCH","solaris");
-    properties.setProperty("DIRECTORY","src/vec/examples/tutorials");
-    properties.setProperty("EXAMPLE","ex1");
-    properties.setProperty("NUMBERPROCESSORS","1");
-    properties.setProperty("COMMAND","mpirun");
+  JPanel      tpanel;
+    Choice    arch;
+    Choice    dir;
+    Choice    example;
+    Choice    np;
 
-    (new ObjectOutputStream(sock.getOutputStream())).writeObject(properties);
+  JTextArea   opanel;
 
-    /* get output and print to screen */
-    InputStreamReader stream = new InputStreamReader(sock.getInputStream());
-    char[] results = new char[128];
-    int    fd     = 0;
+  public void init() {
 
-    while (true) {
-      fd = stream.read(results);
-      if (fd == -1) {break;}
-      System.out.println(new String(results));
-    }
-  }
-
-  public static void main(String s[])
-  {
-    PETScRun prun = new PETScRun();
     try {
-      prun.start();
+      Socket sock = new Socket(this.getParameter("server"),2000);
+      sock.setSoLinger(true,5);
+
+      /* construct properties to send to server */
+      Properties   properties = new Properties();
+      properties.setProperty("Loadsystems","Yes");
+      (new ObjectOutputStream(sock.getOutputStream())).writeObject(properties);
+
+      ObjectInputStream os = new ObjectInputStream(sock.getInputStream());
+      systems            = new Hashtable[4];
+      systems[MACHINE]   = (Hashtable) os.readObject();
+      systems[DIRECTORY] = (Hashtable) os.readObject();
+      systems[MAXNP]     = (Hashtable) os.readObject();
+      systems[EXAMPLES]  = (Hashtable) os.readObject();
+      sock.close();
+    } catch (java.io.IOException ex) {;}
+      catch (ClassNotFoundException ex) {    System.out.println("no class");}
+ 
+    this.setLayout(new FlowLayout());
+
+    tpanel = new JPanel(new GridLayout(2,4));
+    this.add(tpanel, BorderLayout.NORTH);
+      arch = new Choice();
+      arch.add("solaris");
+      arch.add("linux");
+      tpanel.add(arch);
+        
+      dir = new Choice();
+      dir.add("src/vec/examples/tutorials");
+      dir.add("src/snes/examples/tutorials");
+      tpanel.add(dir);
+
+      example = new Choice();
+      example.add("ex1");
+      example.add("ex1f");
+      tpanel.add(example);
+
+      np = new Choice();
+      np.add("1");
+      np.add("2");
+      tpanel.add(np);
+
+      JButton rbutton = new JButton("Run");
+      tpanel.add(rbutton);
+      rbutton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) { 
+          runprogram("mpirun");
+        }
+      }); 
+
+      JButton mbutton = new JButton("Make");
+      tpanel.add(mbutton);
+      mbutton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) { 
+          runprogram("make");
+        }
+      }); 
+
+    opanel = new JTextArea(30,60);
+    this.add(new JScrollPane(opanel), BorderLayout.NORTH); 
+    opanel.setLineWrap(true);
+    opanel.setWrapStyleWord(true);
+  }
+
+  public void stop() {
+    System.out.println("Called stop");
+  }
+
+  public void runprogram(String what)
+  {
+
+    try {
+      Socket sock = new Socket(this.getParameter("server"),2000);
+      sock.setSoLinger(true,5);
+      InputStream sstream = sock.getInputStream();
+
+      /* construct properties to send to server */
+      Properties   properties = new Properties();
+      properties.setProperty("PETSC_ARCH",arch.getSelectedItem());
+      properties.setProperty("DIRECTORY",dir.getSelectedItem());
+      properties.setProperty("EXAMPLE",example.getSelectedItem());
+      properties.setProperty("NUMBERPROCESSORS",np.getSelectedItem());
+      properties.setProperty("COMMAND",what);
+
+      (new ObjectOutputStream(sock.getOutputStream())).writeObject(properties);
+
+      /* get output and print to screen */
+      InputStreamReader stream = new InputStreamReader(sstream);
+      char[] results = new char[128];
+      int    fd      = 0,cnt = 0;
+
+      opanel.setText(null);
+      while (true) {
+        fd  = stream.read(results);
+        if (fd == -1) {break;}
+        opanel.append(new String(results,0,fd));
+	cnt += fd;
+      }
     } catch (java.io.IOException ex) {;}
   }
 }
--- src/sys/src/objects/ams/java/PETScRund.java
+++ src/sys/src/objects/ams/java/PETScRund.java
@@ -1,4 +1,4 @@
-/*$Id: PETScRund.java,v 1.2 2000/10/25 19:03:11 bsmith Exp bsmith $*/
+/*$Id: PETScRund.java,v 1.3 2000/10/25 19:11:57 bsmith Exp bsmith $*/
 /*
      Compiles and runs a PETSc program
 */
@@ -6,6 +6,7 @@
 import java.lang.Thread;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 /*
     This is the class that this file implements (must always be the same as
@@ -13,37 +14,129 @@ import java.net.*;
 */
 public class PETScRund 
 {
-  public PETScRund() {
-    ;
-  }
+  Hashtable        systems[];
+  static final int MACHINE = 0,DIRECTORY = 1,MAXNP = 2,EXAMPLES = 3;
 
+  public PETScRund() {
+    try {
+      systems = load();
+    } catch (java.io.IOException e) {;}
+  }
   
   public void start() throws java.io.IOException
   {
     ServerSocket serve = new ServerSocket(2000);
     while (true) {
       Socket sock  = serve.accept();
+      sock.setSoLinger(true,5);
       (new ThreadRunProgram(sock)).start();
     }
   }
 
+  /*
+          This handles a single clients request
+  */
   class ThreadRunProgram extends Thread
   {
     Socket sock;
     public ThreadRunProgram(Socket s) {sock = s;}
     public void run() {
       try {
-        OutputStream out   = sock.getOutputStream();
-        Runtime      rtime = Runtime.getRuntime();
- 
-        System.out.println("petscrsh make fire solaris /sandbox/bsmith/petsc/ src/vec/examples/tutorials ex1");
+        Properties properties = (Properties) (new ObjectInputStream(sock.getInputStream())).readObject();
+        if (properties.getProperty("Loadsystems") != null) {
+  	  ObjectOutputStream os = new ObjectOutputStream(sock.getOutputStream());
+          os.writeObject(systems[MACHINE]);
+          os.writeObject(systems[DIRECTORY]);
+          os.writeObject(systems[MAXNP]);
+          os.writeObject(systems[EXAMPLES]);
+          return;
+        }
 
-        Process make = rtime.exec("ls");
-        PumpStream pump = new PumpStream(make.getInputStream(),out);
+        String     arch       = properties.getProperty("PETSC_ARCH");
+        int        np         = Integer.parseInt(properties.getProperty("NUMBERPROCESSORS"));
+        String     directory  = properties.getProperty("DIRECTORY");
+        if (!systems[EXAMPLES].containsKey(directory)) { /* bad directory sent from client */
+          System.out.println("Requested example directory "+directory+" that does not exist");
+          return;
+        }
+        String     example    = properties.getProperty("EXAMPLE");
+        ArrayList ex = (ArrayList) systems[EXAMPLES].get(directory);
+        if (!ex.contains(example)) {
+          System.out.println("Requested example "+example+" that does not exist");
+          return;
+        }
+        String     command    = null;
+        if (properties.getProperty("COMMAND").equals("make")) {
+          command = "petscmake ";
+        } else {
+	  /* make sure number of processors is reasonable */
+	  int maxnp = Integer.parseInt((String)systems[MAXNP].get(arch));
+	  if (maxnp < np) np = maxnp;
+          command = "petscrun "+np+" ";
+        }
+        command += systems[MACHINE].get(arch)+" "+
+                   arch+" "+
+                   systems[DIRECTORY].get(arch)+"/ "+
+                   directory+" "+
+                   example;
+        System.out.println("Running command:"+command);
+        Process make = Runtime.getRuntime().exec(command);
+        PumpStream pump = new PumpStream(make.getInputStream(),sock.getOutputStream());
         int len = pump.Pump();
-        System.out.println("done pump, length = "+len);
+        System.out.println("PETScRund: Done returning output, length = "+len);
       } catch (java.io.IOException ex) {System.out.println("bad");}
+        catch (java.lang.ClassNotFoundException ex) {System.out.println("bad");} 
     }
+  }
+
+  /*
+        Loads information about the available systems
+  */
+  public Hashtable[] load() throws java.io.IOException
+  {
+    LineNumberReader input = new LineNumberReader(new InputStreamReader(new FileInputStream("PETScRundrc")));
+    String           edirectory = null;
+    ArrayList        examples = null;
+       
+    systems = new Hashtable[4];
+    systems[MACHINE]   = new Hashtable();
+    systems[DIRECTORY] = new Hashtable();
+    systems[MAXNP]     = new Hashtable();
+    systems[EXAMPLES]  = new Hashtable();
+
+    String line = null;
+    while (true) {
+      try {
+        line = input.readLine();
+      } catch (java.io.IOException e) {break;}
+      if (line == null) break;
+      if (line.length() == 0) continue;
+      if (line.charAt(0) == '#') continue;
+      System.out.println(line);
+      java.util.StringTokenizer toke = new java.util.StringTokenizer(line.trim());
+      int cnt = toke.countTokens();
+      if (cnt == 4) { /* found a new machine arch */
+	String arch = toke.nextToken();
+        systems[MACHINE].put(arch,toke.nextToken());
+        systems[DIRECTORY].put(arch,toke.nextToken());
+        systems[MAXNP].put(arch,toke.nextToken());
+      } else if (cnt == 2) { /* found a new directory */
+        if (examples != null) {
+          System.out.println("putting directory "+edirectory);
+          systems[EXAMPLES].put(edirectory,examples);
+        };
+        examples = new ArrayList();
+        edirectory = toke.nextToken();
+        examples.add(toke.nextToken());
+      } else if (cnt == 1) { /* found a new example */
+        examples.add(toke.nextToken());
+      }
+    }
+    if (examples != null) {
+          System.out.println("putting directory "+edirectory);
+      systems[EXAMPLES].put(edirectory,examples);
+    } 
+    return systems;
   }
 
   public static void main(String s[])
