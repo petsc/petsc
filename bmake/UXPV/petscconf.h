f79b0f41a429d000a7bcab3570e20a304fe5646f
Barry Smith <bsmith@mcs.anl.gov>
2000-12-15 09:57:44 -0600

    Initial revision

---
+++ bmake/UXPV/petscconf.h
@@ -0,0 +1,35 @@
+#ifdef PETSC_RCS_HEADER
+"$Id: petscconf.h,v 1.16 1999/03/31 23:58:30 balay Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define PARCH_UXPV
+#define PETSC_ARCH_NAME "uxpv"
+
+#define PETSC_HAVE_PWD_H 
+#define PETSC_HAVE_STRING_H 
+#define PETSC_HAVE_MALLOC_H 
+#define PETSC_HAVE_STDLIB_H 
+#define PETSC_HAVE_LIMITS_H
+
+#define PETSC_HAVE_DRAND48  
+#define PETSC_HAVE_UNISTD_H 
+#define PETSC_HAVE_SYS_TIME_H 
+#define PETSC_HAVE_UNAME  
+#define PETSC_HAVE_FORTRAN_UNDERSCORE
+#define PETSC_HAVE_INT_MPI_COMM
+
+#define PETSC_HAVE_READLINK
+#define PETSC_HAVE_MEMMOVE
+#define PETSC_NEED_UTYPE_TYPEDEFS
+#define PETSC_HAVE_SYS_RESOURCE_H
+
+#define PETSC_SIZEOF_VOIDP 8
+#define PETSC_SIZEOF_INT 4
+#define PETSC_SIZEOF_DOUBLE 8
+
+#define PETSC_WORDS_BIGENDIAN 1
+#endif
