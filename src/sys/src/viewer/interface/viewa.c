8e3cda6747c59c3753f8aacec4253e2e4a606779
Satish Balay <balay@mcs.anl.gov>
2001-01-19 15:32:46 -0600

    *** empty log message ***

--- src/sys/src/viewer/interface/viewa.c
+++ src/sys/src/viewer/interface/viewa.c
@@ -1,4 +1,4 @@
-/*$Id: viewa.c,v 1.16 2001/01/15 21:43:19 bsmith Exp bsmith $*/
+/*$Id: viewa.c,v 1.17 2001/01/19 20:43:43 bsmith Exp balay $*/
 
 #include "src/sys/src/viewer/viewerimpl.h"  /*I "petsc.h" I*/  
 
@@ -18,24 +18,24 @@
 
    Notes:
    Available formats include
-+    PETSC_VIEWER_FORMAT_ASCII_DEFAULT - default format
-.    PETSC_VIEWER_FORMAT_ASCII_MATLAB - Matlab format
-.    PETSC_VIEWER_FORMAT_ASCII_DENSE - print matrix as dense
-.    PETSC_VIEWER_FORMAT_ASCII_IMPL - implementation-specific format
++    PETSC_VIEWER_ASCII_DEFAULT - default format
+.    PETSC_VIEWER_ASCII_MATLAB - Matlab format
+.    PETSC_VIEWER_ASCII_DENSE - print matrix as dense
+.    PETSC_VIEWER_ASCII_IMPL - implementation-specific format
       (which is in many cases the same as the default)
-.    PETSC_VIEWER_FORMAT_ASCII_INFO - basic information about object
-.    PETSC_VIEWER_FORMAT_ASCII_INFO_LONG - more detailed info
+.    PETSC_VIEWER_ASCII_INFO - basic information about object
+.    PETSC_VIEWER_ASCII_INFO_LONG - more detailed info
        about object
-.    PETSC_VIEWER_FORMAT_ASCII_COMMON - identical output format for
+.    PETSC_VIEWER_ASCII_COMMON - identical output format for
        all objects of a particular type
-.    PETSC_VIEWER_FORMAT_ASCII_INDEX - (for vectors) prints the vector
+.    PETSC_VIEWER_ASCII_INDEX - (for vectors) prints the vector
        element number next to each vector entry
-.    PETSC_VIEWER_FORMAT_BINARY_NATIVE - store the object to the binary
+.    PETSC_VIEWER_BINARY_NATIVE - store the object to the binary
       file in its native format (for example, dense
        matrices are stored as dense)
-.    PETSC_VIEWER_FORMAT_DRAW_BASIC - views the vector with a simple 1d plot
-.    PETSC_VIEWER_FORMAT_DRAW_LG - views the vector with a line graph
--    PETSC_VIEWER_FORMAT_DRAW_CONTOUR - views the vector with a contour plot
+.    PETSC_VIEWER_DRAW_BASIC - views the vector with a simple 1d plot
+.    PETSC_VIEWER_DRAW_LG - views the vector with a line graph
+-    PETSC_VIEWER_DRAW_CONTOUR - views the vector with a contour plot
 
    These formats are most often used for viewing matrices and vectors.
    Currently, the object name is used only in the Matlab format.
@@ -45,20 +45,14 @@
 .seealso: PetscViewerASCIIOpen(), PetscViewerBinaryOpen(), MatView(), VecView(),
           PetscViewerPushFormat(), PetscViewerPopFormat(), PetscViewerDrawOpenX(),PetscViewerSocketOpen()
 @*/
-int PetscViewerSetFormat(PetscViewer viewer,int format,char *name)
+int PetscViewerSetFormat(PetscViewer viewer,PetscViewerFormat format)
 {
   int        ierr;
   PetscTruth isascii;
 
   PetscFunctionBegin;
   PetscValidHeaderSpecific(viewer,PETSC_VIEWER_COOKIE);
-  ierr = PetscTypeCompare((PetscObject)viewer,PETSC_VIEWER_ASCII,&isascii);CHKERRQ(ierr);
-  if (isascii) {
-    viewer->format     = format;
-    viewer->outputname = name;
-  } else {
-    viewer->format     = format;
-  }
+  viewer->format     = format;
   PetscFunctionReturn(0);
 }
 
@@ -78,24 +72,24 @@ int PetscViewerSetFormat(PetscViewer viewer,int format,char *name)
 
    Notes:
    Available formats include
-+    PETSC_VIEWER_FORMAT_ASCII_DEFAULT - default format
-.    PETSC_VIEWER_FORMAT_ASCII_MATLAB - Matlab format
-.    PETSC_VIEWER_FORMAT_ASCII_IMPL - implementation-specific format
++    PETSC_VIEWER_ASCII_DEFAULT - default format
+.    PETSC_VIEWER_ASCII_MATLAB - Matlab format
+.    PETSC_VIEWER_ASCII_IMPL - implementation-specific format
       (which is in many cases the same as the default)
-.    PETSC_VIEWER_FORMAT_ASCII_INFO - basic information about object
-.    PETSC_VIEWER_FORMAT_ASCII_INFO_LONG - more detailed info
+.    PETSC_VIEWER_ASCII_INFO - basic information about object
+.    PETSC_VIEWER_ASCII_INFO_LONG - more detailed info
        about object
-.    PETSC_VIEWER_FORMAT_ASCII_COMMON - identical output format for
+.    PETSC_VIEWER_ASCII_COMMON - identical output format for
        all objects of a particular type
-.    PETSC_VIEWER_FORMAT_ASCII_INDEX - (for vectors) prints the vector
+.    PETSC_VIEWER_ASCII_INDEX - (for vectors) prints the vector
        element number next to each vector entry
-.    PETSC_VIEWER_FORMAT_BINARY_NATIVE - store the object to the binary
+.    PETSC_VIEWER_BINARY_NATIVE - store the object to the binary
       file in its native format (for example, dense
        matrices are stored as dense)
-.    PETSC_VIEWER_FORMAT_DRAW_BASIC - views the vector with a simple 1d plot
-.    PETSC_VIEWER_FORMAT_DRAW_LG - views the vector with a line graph
-.    PETSC_VIEWER_FORMAT_DRAW_CONTOUR - views the vector with a contour plot
--    PETSC_VIEWER_FORMAT_NATIVE - for DA vectors displays vectors in DA ordering, not natural
+.    PETSC_VIEWER_DRAW_BASIC - views the vector with a simple 1d plot
+.    PETSC_VIEWER_DRAW_LG - views the vector with a line graph
+.    PETSC_VIEWER_DRAW_CONTOUR - views the vector with a contour plot
+-    PETSC_VIEWER_NATIVE - for DA vectors displays vectors in DA ordering, not natural
 
    These formats are most often used for viewing matrices and vectors.
    Currently, the object name is used only in the Matlab format.
