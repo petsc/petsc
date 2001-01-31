1dfbe8499bd4fd112616f1d2c408cd058bdb5b56
Barry Smith <bsmith@mcs.anl.gov>
2001-01-31 16:41:54 -0600

    *** empty log message ***

--- src/sys/src/objects/ams/java/PETScRun.java
+++ src/sys/src/objects/ams/java/PETScRun.java
@@ -1,4 +1,4 @@
-/*$Id: PETScRun.java,v 1.9 2000/11/07 21:35:01 bsmith Exp bsmith $*/
+/*$Id: PETScRun.java,v 1.10 2000/11/13 19:18:07 bsmith Exp bsmith $*/
 /*
      Compiles and runs a PETSc program
 */
@@ -11,37 +11,47 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.print.*;
 
-public class PETScRun extends java.applet.Applet
+public class PETScRun extends java.applet.Applet implements Pageable, Printable
 {
-  static final int MACHINE = 0,DIRECTORY = 1,MAXNP = 2,EXAMPLES = 3;
+  static final int MACHINE = 0,DIRECTORY = 1,MAXNP = 2,EXAMPLES = 3,EXAMPLESHELP = 4;
   Hashtable        systems[];
+  Socket csock = null;
 
   java.applet.AppletContext appletcontext;
   java.applet.Applet applet;
 
-  JPanel      tpanel;
-  JTextField options;
+  JPanel     tpanel;
+  JTextField options,help;
 
-  Checkbox toptions;
-  Checkbox tbopt;
-  Checkbox snoop;
+  JCheckBox toptions;
+  JCheckBox tbopt;
+  JCheckBox snoop;
 
     Choice    arch;
     Choice    dir;
     Choice    example;
     Choice    np;
 
-  JTextArea   opanel;
+  JTextArea   opanel,epanel;
 
   boolean isviewsource = false;
 
   public void init() {
     appletcontext = getAppletContext();
     applet = this;
+    System.out.println("Codebase"+this.getDocumentBase());
+
     try {
       System.out.println("parameter"+this.getParameter("server"));
-      Socket sock = new Socket(this.getParameter("server"),2000);
+      Socket sock = null;
+      try {
+        sock = new Socket(this.getParameter("server"),2000);
+      } catch(java.net.ConnectException oops) {
+        appletcontext.showDocument(new URL("http://www.mcs.anl.gov/petsc/noserver.html"));
+        return;
+      }
       sock.setSoLinger(true,5);
 
       /* construct properties to send to server */
@@ -50,11 +60,12 @@ public class PETScRun extends java.applet.Applet
       (new ObjectOutputStream(sock.getOutputStream())).writeObject(properties);
 
       ObjectInputStream os = new ObjectInputStream(sock.getInputStream());
-      systems            = new Hashtable[4];
-      systems[MACHINE]   = (Hashtable) os.readObject();
-      systems[DIRECTORY] = (Hashtable) os.readObject();
-      systems[MAXNP]     = (Hashtable) os.readObject();
-      systems[EXAMPLES]  = (Hashtable) os.readObject();
+      systems                = new Hashtable[5];
+      systems[MACHINE]       = (Hashtable) os.readObject();
+      systems[DIRECTORY]     = (Hashtable) os.readObject();
+      systems[MAXNP]         = (Hashtable) os.readObject();
+      systems[EXAMPLES]      = (Hashtable) os.readObject();
+      systems[EXAMPLESHELP]  = (Hashtable) os.readObject();
       sock.close();
     } catch (java.io.IOException ex) {ex.printStackTrace(); System.out.println("no systems");}
       catch (ClassNotFoundException ex) {    System.out.println("no class");}
@@ -64,6 +75,16 @@ public class PETScRun extends java.applet.Applet
     tpanel = new JPanel(new GridLayout(3,4));
     this.add(tpanel, BorderLayout.NORTH);
       
+    help = new JTextField(50);
+    this.add(help, BorderLayout.NORTH);
+
+    JPanel grid = new JPanel(new GridBagLayout());
+    this.add(grid,BorderLayout.NORTH);
+    options = new JTextField(50);
+    grid.add(new JLabel("Options"));
+    grid.add(options);
+
+
       arch = new Choice();
       arch.addItemListener(new ItemListener() {
                             public void itemStateChanged(ItemEvent e) {
@@ -77,33 +98,48 @@ public class PETScRun extends java.applet.Applet
       tpanel.add(arch);
         
       dir = new Choice();
-      dir.addItemListener(new ItemListener() {
+      String pexampled = this.getParameter("EXAMPLEDIRECTORY");
+      if (pexampled == null) {
+        dir.addItemListener(new ItemListener() {
                             public void itemStateChanged(ItemEvent e) {
                               Choice choice = (Choice) e.getItemSelectable();
                               setexamples(example,choice.getSelectedItem());}
 	                  });
-      keys = systems[EXAMPLES].keys();
-      while (keys.hasMoreElements()) {
-        dir.add((String)keys.nextElement());
+        keys = systems[EXAMPLES].keys();
+        while (keys.hasMoreElements()) {
+          dir.add((String)keys.nextElement());
+        }
+      } else { /* .html file indicates which example to run */
+        dir.add(pexampled);
       }
       tpanel.add(dir);
 
       example = new Choice();
-      setexamples(example,(String)systems[EXAMPLES].keys().nextElement());
+      String pexample = this.getParameter("EXAMPLE");
+      if (pexample == null) {
+        example.addItemListener(new ItemListener() {
+                            public void itemStateChanged(ItemEvent e) {
+                              Choice choice = (Choice) e.getItemSelectable();
+                              String directory = dir.getSelectedItem();
+                              ArrayList ehelp = (ArrayList)systems[EXAMPLESHELP].get(directory);
+                              ArrayList ex = (ArrayList)systems[EXAMPLES].get(directory);
+                              int index = ex.indexOf(choice.getSelectedItem());
+                              help.setText((String)ehelp.get(index));}
+	                  });
+        setexamples(example,(String)systems[EXAMPLES].keys().nextElement());
+      } else {  /* .html file indicates which example to run */
+        example.add(pexample);
+      }
       tpanel.add(example);
 
+      String phelp = this.getParameter("HELP"); /* .html file indicates help message */
+      if (phelp != null) {
+        this.help.setText(phelp);
+      }
 
       np = new Choice();
       setnp(np,(String)systems[MAXNP].keys().nextElement());
       tpanel.add(np);
-
-      JButton rbutton = new JButton("Run");
-      tpanel.add(rbutton);
-      rbutton.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e) { 
-          runprogram("mpirun");
-        }
-      }); 
 
       JButton mbutton = new JButton("Make");
       tpanel.add(mbutton);
@@ -121,6 +157,14 @@ public class PETScRun extends java.applet.Applet
         }
       }); 
 
+      JButton rbutton = new JButton("Run");
+      tpanel.add(rbutton);
+      rbutton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) { 
+          runprogram("mpirun");
+        }
+      }); 
+
       JButton sbutton = new JButton("View source");
       tpanel.add(sbutton);
       sbutton.addActionListener(new ActionListener(){
@@ -130,19 +174,22 @@ public class PETScRun extends java.applet.Applet
         }
       }); 
 
-      toptions = new Checkbox("Set options graphically");
+      toptions = new JCheckBox("Set options graphically");
       tpanel.add(toptions);
 
-      snoop = new Checkbox("Snoop on running program");
+      snoop = new JCheckBox("Snoop on running program");
       tpanel.add(snoop);
 
-      tbopt = new Checkbox("Compile debug version");
+      tbopt = new JCheckBox("Compile debug version");
       tpanel.add(tbopt);
 
-    options = new JTextField(50);
-    this.add(options, BorderLayout.NORTH);
 
-    opanel = new JTextArea(30,60);
+    epanel = new JTextArea(4,60);
+    this.add(new JScrollPane(epanel), BorderLayout.NORTH); 
+    epanel.setLineWrap(true);
+    epanel.setWrapStyleWord(true);
+
+    opanel = new JTextArea(20,60);
     this.add(new JScrollPane(opanel), BorderLayout.NORTH); 
     opanel.setLineWrap(true);
     opanel.setWrapStyleWord(true);
@@ -151,13 +198,16 @@ public class PETScRun extends java.applet.Applet
   /*
       Fills in the examples pull down menu based on the directory selected
   */
-  public void setexamples(Choice example,String arch) {
-    ArrayList ex = (ArrayList)systems[EXAMPLES].get(arch);
+  public void setexamples(Choice example,String dir) {
+    ArrayList ex = (ArrayList)systems[EXAMPLES].get(dir);
     Iterator its = ex.iterator();
     example.removeAll();
     while (its.hasNext()) {
       example.add((String)its.next());
     }
+    ArrayList ehelp = (ArrayList)systems[EXAMPLESHELP].get(dir);
+    System.out.println("ehelp"+ehelp+(String)ehelp.get(0));
+    this.help.setText((String)ehelp.get(0));
   }
 
   /*
@@ -178,7 +228,8 @@ public class PETScRun extends java.applet.Applet
   public void runprogram(String what)
   {
     try {
-      final Socket sock = new Socket(this.getParameter("server"),2000);
+      csock = new Socket(this.getParameter("server"),2000);
+      final Socket sock = csock;
       sock.setSoLinger(true,5);
       final InputStream sstream = sock.getInputStream();
 
@@ -189,17 +240,17 @@ public class PETScRun extends java.applet.Applet
       properties.setProperty("EXAMPLE",example.getSelectedItem());
       properties.setProperty("NUMBERPROCESSORS",np.getSelectedItem());
       properties.setProperty("COMMAND",what);
-      if (tbopt.getState()) {
+      if (tbopt.isSelected()) {
         properties.setProperty("BOPT","g");
       } else {
         properties.setProperty("BOPT","O");
       }
       String soptions = options.getText();
-      if (toptions.getState() && what.equals("mpirun")) {
+      if (toptions.isSelected() && what.equals("mpirun")) {
 	URL urlb = this.getDocumentBase();
         soptions += " -ams_publish_options";
 	try {
-	  final URL url = new URL(""+urlb+"AMSPETScOptions.html");  
+	  final URL url = new URL(""+urlb+"PETScOptions.html");  
           System.out.println("loading url"+url);
           (new Thread() {
             public void run() {
@@ -210,7 +261,7 @@ public class PETScRun extends java.applet.Applet
 	  }).start();
         } catch (MalformedURLException ex) {System.out.println("bad luck");;} 
       } 
-      if (snoop.getState() && what.equals("mpirun")) {
+      if (snoop.isSelected() && what.equals("mpirun")) {
 	URL urlb = this.getDocumentBase();
         soptions += " -ams_publish_objects";
 	try {
@@ -228,12 +279,12 @@ public class PETScRun extends java.applet.Applet
       properties.setProperty("OPTIONS",soptions);
       System.out.println("getting back");
 
+      /* get output and print to screen */
       (new Thread() {
         public void run() {
           try {
             (new ObjectOutputStream(sock.getOutputStream())).writeObject(properties);
 
-            /* get output and print to screen */
             InputStreamReader stream = new InputStreamReader(sstream);
             char[] results = new char[128];
             int    fd      = 0,cnt = 0;
@@ -250,12 +301,47 @@ public class PETScRun extends java.applet.Applet
           donerun();
         }
       }).start();
+
+      /* get stderr and popup */
+      (new Thread() {
+        public void run() {
+          try {
+            Socket nsock = new Socket(applet.getParameter("server"),2002);
+            nsock.setSoLinger(true,5);
+            InputStreamReader stream = new InputStreamReader(nsock.getInputStream());
+            char[] results = new char[128];
+            int    fd      = 0;
+
+            epanel.setText(null);
+            while (true) {
+              fd  = stream.read(results);
+              if (fd == -1) {break;}
+              System.out.println("PETScRun:got stderr output"+results);
+              epanel.append(new String(results,0,fd));
+            }
+            stream.close();
+            nsock.close();
+          } catch (java.io.IOException ex) {System.out.println("PETScRun: error getting stderr");}
+          System.out.println("PETScRun: finished checking for stderr");
+        }
+      }).start();
+
     } catch (java.io.IOException ex) {;}
   }
+
+  PageFormat format;
 
   public void donerun() 
   {
     System.out.println("done run");
+    PrinterJob job = PrinterJob.getPrinterJob();
+    format = job.pageDialog(job.defaultPage());
+    job.setPageable(this);
+    job.printDialog();
+    try {
+      job.print();
+    } catch (java.awt.print.PrinterException e) {System.out.println("problem printing");;}
+
     if (isviewsource) {
       String ex = example.getSelectedItem();
       URL    urlb = applet.getDocumentBase();
@@ -270,6 +356,19 @@ public class PETScRun extends java.applet.Applet
       } catch (MalformedURLException oops) { System.out.println("bad:showing"+urlb);;} 
     }
     isviewsource = false;
+  }
+
+  public int        getNumberOfPages()         {return 1;}
+  public PageFormat getPageFormat(int pagenum) {return format;}
+  public Printable  getPrintable(int pagenum)  {return this;}
+
+  public int print(Graphics g,PageFormat format,int pagenum) {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.translate(format.getImageableX(),format.getImageableY());
+        System.out.println("about to paint");
+    this.paint(g2);
+        System.out.println("done painting");
+    return 1;
   }
 }
 
