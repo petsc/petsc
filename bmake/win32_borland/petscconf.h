4e94e00c88a88948cc5ba7697c698606a8cc92d4
Satish Balay <balay@mcs.anl.gov>
2001-03-09 13:52:31 -0600

    Initial revision

---
+++ bmake/win32_borland/petscconf.h
@@ -0,0 +1,46 @@
+#ifdef PETSC_RCS_HEADER
+"$Id: petscconf.h,v 1.2 2000/12/21 03:21:42 balay Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define PARCH_win32 
+#define PETSC_ARCH_NAME "win32"
+#define PETSC_HAVE_LIMITS_H
+#define PETSC_HAVE_STDLIB_H 
+#define PETSC_HAVE_STRING_H 
+#define PETSC_HAVE_SEARCH_H
+#define PETSC_HAVE_IO_H
+
+#define PETSC_HAVE_STD_COMPLEX
+
+#if !defined (PETSC_USE_COMPLEX)
+#define PETSC_USE_CBLASLAPACK
+#endif
+
+#define PETSC_HAVE_READLINK
+#define PETSC_HAVE_MEMMOVE
+
+#define PETSC_HAVE_RAND
+#define PETSC_CANNOT_START_DEBUGGER
+#define PETSC_HAVE_CLOCK
+
+#define PETSC_HAVE_GET_USER_NAME
+#define PETSC_SIZEOF_VOIDP 4
+#define PETSC_SIZEOF_INT 4
+#define PETSC_SIZEOF_DOUBLE 8
+
+#define PETSC_USE_NT_TIME
+#define PETSC_HAVE_NO_GETRUSAGE
+
+#define PETSC_MISSING_SIGBUS
+#define PETSC_MISSING_SIGQUIT
+#define PETSC_MISSING_SIGSYS
+
+#define PETSC_HAVE_DOS_H
+#define PETSC_HAVE_ACCESS
+
+#endif
+
