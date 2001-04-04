4839acb0497e4f0888b6c84af1be6c714247ea8c
Barry Smith <bsmith@mcs.anl.gov>
2001-04-04 14:00:42 -0600

    *** empty log message ***

--- include/petscversion.h
+++ include/petscversion.h
@@ -1,4 +1,4 @@
-/* $Id: petscversion.h,v 1.22 2000/05/18 19:18:07 bsmith Exp balay $ */
+/* $Id: petscversion.h,v 1.23 2000/09/22 21:34:51 balay Exp bsmith $ */
 #if !defined(__PETSCVERSION_H)
 #define __PETSCVERSION_H
 
@@ -11,13 +11,13 @@
     docs/tex/manual/manual.tex and
     docs/tex/manual/manual_tex.tex.
 */
-#define PETSC_VERSION_NUMBER "PETSc Version 2.0.29, Released Sept 26, 2000"
+#define PETSC_VERSION_NUMBER "PETSc Version 2.1.0, Released April 6, 2001"
 
 #define PETSC_VERSION_MAJOR    2
-#define PETSC_VERSION_MINOR    0
-#define PETSC_VERSION_SUBMINOR 29
+#define PETSC_VERSION_MINOR    1
+#define PETSC_VERSION_SUBMINOR 0
 #define PETSC_PATCH_LEVEL      0
-#define PETSC_VERSION_DATE     "Sept 26, 2000"
+#define PETSC_VERSION_DATE     "April 6, 2001"
 #define PETSC_AUTHOR_INFO      "\
        The PETSc Team\
     petsc-maint@mcs.anl.gov\n\
