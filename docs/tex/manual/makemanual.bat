9a8dc5eaae519310484d71a956bd1f71ebb37886
Barry Smith <bsmith@mcs.anl.gov>
2001-04-17 13:15:21 -0600

    *** empty log message ***

--- docs/tex/manual/makemanual.bat
+++ docs/tex/manual/makemanual.bat
@@ -1,16 +1,21 @@
+rem $Id: makefile,v 1.53 1998/01/03 16:04:05 bsmith Exp $ 
+rem
 rem makes manual in pdf format
+rem
         pdfelatex manual 
 	bibtex manual
 	pdfelatex manual 
 	makeindex manual
 	rm index.tex
-	sed -e s@_@\\_@g manual.ind > index.tex
+	sed -e s@_@\\_@g -e "s@{theindex}@{theindex}\\\\addcontentsline{toc}{chapter}{Index}\\\\label{sec:index}@g" manual.ind > index.tex
 	pdfelatex manual 
 	pdfelatex manual 
 	pdfelatex manual 
 	mv manual.pdf ../../manual.pdf
+rem
 rem Now make the intro part ... note that this depends on latexing the manual
+rem
 	cp manual.aux intro.aux
 	pdfelatex intro 
-	-@chmod g+w intro.pdf ../../manual.pdf
+	chmod go+w intro.pdf ../../manual.pdf
 	rm -r *.dvi *.aux *.toc *.log *.bbl *.blg *.hux *.err *.ilg *.idx *.ind index.tex  *.aus *.out
