a9ea9a48eb8110482dbc18b9de81e776743583b1
Barry Smith <bsmith@mcs.anl.gov>
2001-04-27 14:26:42 -0600

    *** empty log message ***

--- src/snes/examples/tutorials/ex24.c
+++ src/snes/examples/tutorials/ex24.c
@@ -1,4 +1,4 @@
-/*$Id: ex24.c,v 1.15 2001/04/10 19:37:05 bsmith Exp bsmith $*/
+/*$Id: ex24.c,v 1.16 2001/04/20 16:20:13 bsmith Exp bsmith $*/
 
 static char help[] = "Solves PDE optimization problem of ex22.c with finite differences for adjoint.\n\n";
 
@@ -39,7 +39,12 @@ static char help[] = "Solves PDE optimization problem of ex22.c with finite diff
 */
 
 extern int FormFunction(SNES,Vec,Vec,void*);
-extern int PDEFormFunction(Scalar*,Vec,Vec,DA);
+extern int PDEFormFunction(DA,Vec,Vec,Scalar*);
+
+typedef struct {
+  ISColoring iscoloring;  /* coloring of grid used for computing Jacobian of PDE */
+  Mat        J;         /* Jacobian of PDE system */
+} AppCtx;
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
@@ -49,8 +54,7 @@ int main(int argc,char **argv)
   DA         da;
   DMMG       *dmmg;
   VecPack    packer;
-  Mat        J;
-
+  AppCtx     *appctx;
 
   PetscInitialize(&argc,&argv,PETSC_NULL,help);
 
@@ -68,8 +72,7 @@ int main(int argc,char **argv)
   ierr = PetscOptionsSetValue("-snes_mf_compute_normu","no");CHKERRQ(ierr);
   ierr = PetscOptionsSetValue("-snes_eq_ls","basic");CHKERRQ(ierr);
   /* ierr = PetscOptionsSetValue("-snes_eq_ls","basicnonorms");CHKERRQ(ierr); */
-  ierr = PetscOptionsInsert(&argc,&argv,PETSC_NULL);CHKERRQ(ierr); 
-  
+  ierr = PetscOptionsInsert(&argc,&argv,PETSC_NULL);CHKERRQ(ierr);   
   /* Create a global vector from a da arrays */
   ierr = DACreate1d(PETSC_COMM_WORLD,DA_NONPERIODIC,-5,1,1,PETSC_NULL,&da);CHKERRQ(ierr);
   ierr = VecPackCreate(PETSC_COMM_WORLD,&packer);CHKERRQ(ierr);
@@ -89,16 +92,20 @@ int main(int argc,char **argv)
   for (i=0; i<nlevels; i++) {
     packer = (VecPack)dmmg[i]->dm;
     ierr   = VecPackGetEntries(packer,PETSC_NULL,&da,PETSC_NULL);CHKERRQ(ierr);
-    ierr   = DAGetColoring(da,IS_COLORING_LOCAL,MATMPIAIJ,PETSC_NULL,&J);CHKERRQ(ierr);
-    dmmg[i]->user = (void*)J;
+    ierr   = PetscNew(AppCtx,&appctx);CHKERRQ(ierr);
+    ierr   = DAGetColoring(da,IS_COLORING_GHOSTED,MATMPIAIJ,&appctx->iscoloring,&appctx->J);CHKERRQ(ierr);
+    ierr   = MatSetColoring(appctx->J,appctx->iscoloring);CHKERRQ(ierr);
+    dmmg[i]->user = (void*)appctx;
   }
 
   ierr = DMMGSetSNES(dmmg,FormFunction,PETSC_NULL);CHKERRQ(ierr);
   ierr = DMMGSolve(dmmg);CHKERRQ(ierr);
 
-  ierr = VecView(DMMGGetx(dmmg),PETSC_VIEWER_SOCKET_WORLD);CHKERRQ(ierr);
+  /* ierr = VecView(DMMGGetx(dmmg),PETSC_VIEWER_SOCKET_WORLD);CHKERRQ(ierr); */
   for (i=0; i<nlevels; i++) {
-    ierr = MatDestroy((Mat)dmmg[i]->user);CHKERRQ(ierr);
+    appctx = (AppCtx*)dmmg[i]->user;
+    ierr   = MatDestroy(appctx->J);CHKERRQ(ierr);
+    ierr   = ISColoringDestroy(appctx->iscoloring);CHKERRQ(ierr);
   }
   ierr = DMMGDestroy(dmmg);CHKERRQ(ierr);
 
@@ -111,35 +118,90 @@ int main(int argc,char **argv)
      This local function acts on the ghosted version of U (accessed via DAGetLocalVector())
      BUT the global, nonghosted version of FU
 
+     Process adiC: PDEFormFunctionLocal
 */
 #undef __FUNCT__
-#define __FUNCT__ "PDEFormFunction"
-int PDEFormFunction(Scalar *w,Vec vu,Vec vfu,DA da)
+#define __FUNCT__ "PDEFormFunctionLocal"
+int PDEFormFunctionLocal(DALocalInfo *info,Scalar *u,Scalar *fu,PassiveScalar *w)
 {
-  int     ierr,xs,xm,i,N;
-  Scalar  *u,*fu,d,h;
+  int     ierr,xs = info->xs,xm = info->xm,i,mx = info->mx;
+  Scalar  d,h;
 
-  PetscFunctionBegin;
-
-  ierr = DAGetCorners(da,&xs,PETSC_NULL,PETSC_NULL,&xm,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
-  ierr = DAGetInfo(da,0,&N,0,0,0,0,0,0,0,0,0);CHKERRQ(ierr);
-  ierr = DAVecGetArray(da,vu,(void**)&u);CHKERRQ(ierr);
-  ierr = DAVecGetArray(da,vfu,(void**)&fu);CHKERRQ(ierr);
-  d    = N-1.0;
+  d    = mx-1.0;
   h    = 1.0/d;
 
   for (i=xs; i<xs+xm; i++) {
-    if      (i == 0)   fu[0]   = 2.0*d*(u[0] - w[0]) + h*u[0]*u[0];
-    else if (i == N-1) fu[N-1] = 2.0*d*u[N-1] + h*u[N-1]*u[N-1];
-    else               fu[i]   = -(d*(u[i+1] - 2.0*u[i] + u[i-1]) - 2.0*h) + h*u[i]*u[i];
+    if      (i == 0)    fu[i]   = 2.0*d*(u[i] - w[0]) + h*u[i]*u[i];
+    else if (i == mx-1) fu[i]   = 2.0*d*u[i] + h*u[i]*u[i];
+    else                fu[i]   = -(d*(u[i+1] - 2.0*u[i] + u[i-1]) - 2.0*h) + h*u[i]*u[i];
   } 
+
+  PetscLogFlops(9*mx);
+  return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "PDEFormFunction"
+int PDEFormFunction(DA da,Vec vu,Vec vfu,Scalar *w)
+{
+  int         ierr;
+  Scalar      *u,*fu;
+  DALocalInfo info;
+
+  PetscFunctionBegin;
+
+  ierr = DAGetLocalInfo(da,&info);CHKERRQ(ierr);
+  ierr = DAVecGetArray(da,vu,(void**)&u);CHKERRQ(ierr);
+  ierr = DAVecGetArray(da,vfu,(void**)&fu);CHKERRQ(ierr);
+
+  ierr = PDEFormFunctionLocal(&info,u,fu,w);CHKERRQ(ierr);
 
   ierr = DAVecRestoreArray(da,vu,(void**)&u);CHKERRQ(ierr);
   ierr = DAVecRestoreArray(da,vfu,(void**)&fu);CHKERRQ(ierr);
-  PetscLogFlops(9*N);
   PetscFunctionReturn(0);
 }
 
+#if defined(PETSC_HAVE_ADIC)
+
+/* #include "adic_utils.h" */
+
+#undef __FUNCT__
+#define __FUNCT__ "PDEFormJacobian"
+int PDEFormJacobian(DA da,ISColoring iscoloring,Vec vu,Mat J,Scalar *w)
+{
+  int         ierr,gtdof,tdof;
+  Scalar      *u,*ustart;
+  DALocalInfo info;
+  void        *ad_u,*ad_f,*ad_ustart,*ad_fstart;
+
+  PetscFunctionBegin;
+
+  ierr = DAGetLocalInfo(da,&info);CHKERRQ(ierr);
+
+  /* get space for derivative objects.  */
+  ierr = DAGetADArray(da,PETSC_TRUE,(void **)&ad_u,&ad_ustart,&gtdof);CHKERRQ(ierr);
+  ierr = DAGetADArray(da,PETSC_FALSE,(void **)&ad_f,&ad_fstart,&tdof);CHKERRQ(ierr);
+  ierr = VecGetArray(vu,&ustart);CHKERRQ(ierr);
+  my_AD_SetValArray(((DERIV_TYPE*)ad_ustart),gtdof,ustart);
+  ierr = VecRestoreArray(vu,&ustart);CHKERRQ(ierr);
+
+  my_AD_ResetIndep();
+  my_AD_SetIndepArrayColored(ad_ustart,gtdof,iscoloring->colors);
+  my_AD_IncrementTotalGradSize(iscoloring->n);
+  my_AD_SetIndepDone();
+
+  ierr = ad_PDEFormFunctionLocal(&info,(DERIV_TYPE*)ad_u,(DERIV_TYPE*)ad_f,w);CHKERRQ(ierr);
+
+  /* stick the values into the matrix */
+  ierr = MatSetValuesAD(J,(Scalar**)ad_fstart);CHKERRQ(ierr);
+
+  /* return space for derivative objects.  */
+  ierr = DARestoreADArray(da,PETSC_TRUE,(void **)&ad_u,&ad_ustart,&gtdof);CHKERRQ(ierr);
+  ierr = DARestoreADArray(da,PETSC_FALSE,(void **)&ad_f,&ad_fstart,&tdof);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#endif
 
 /*
       Evaluates FU = Gradiant(L(w,u,lambda))
@@ -161,6 +223,7 @@ int FormFunction(SNES snes,Vec U,Vec FU,void* dummy)
   Vec     vu,vlambda,vfu,vflambda,vglambda;
   DA      da,dadummy;
   VecPack packer = (VecPack)dmmg->dm;
+  AppCtx  *appctx = (AppCtx*)dmmg->user;
 
   PetscFunctionBegin;
   ierr = VecPackGetEntries(packer,&nredundant,&da,&dadummy);CHKERRQ(ierr);
@@ -170,15 +233,15 @@ int FormFunction(SNES snes,Vec U,Vec FU,void* dummy)
   ierr = VecPackGetAccess(packer,U,0,0,&vglambda);CHKERRQ(ierr);
 
   /* Evaluate the Jacobian of PDEFormFunction() */
-  /*  ierr = PDEFormJacobian(da,w,vu,(Mat)dmmg->user);CHKERRQ(ierr);
-      ierr = MatMultTranspose((Mat)dmmg->user,vglambda,vflambda);CHKERRQ(ierr); */
+  ierr = PDEFormJacobian(da,appctx->iscoloring,vu,appctx->J,w);CHKERRQ(ierr);
+  ierr = MatMultTranspose(appctx->J,vglambda,vflambda);CHKERRQ(ierr); 
 
   PetscViewerPushFormat(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_ASCII_MATLAB);
   /*  ierr = MatView((Mat)dmmg->user,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr); */
   PetscViewerPopFormat(PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   /* derivative of constraint portion of L() w.r.t. u */
-  ierr = PDEFormFunction(w,vu,vfu,da);CHKERRQ(ierr);
+  ierr = PDEFormFunction(da,vu,vfu,w);CHKERRQ(ierr);
 
   ierr = DAGetCorners(da,&xs,PETSC_NULL,PETSC_NULL,&xm,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
   ierr = DAGetInfo(da,0,&N,0,0,0,0,0,0,0,0,0);CHKERRQ(ierr);
