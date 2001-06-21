f3e16f1380e89dff8a54087a926c29e1c56b8b6b
Kris Buschelman <buschelm@mcs.anl.gov>
2001-06-21 13:59:16 -0600

    *** empty log message ***

--- src/mat/impls/baij/seq/baijfact2.c
+++ src/mat/impls/baij/seq/baijfact2.c
@@ -1,4 +1,4 @@
-/*$Id: baijfact2.c,v 1.52 2001/04/13 20:48:17 bsmith Exp bsmith $*/
+/*$Id: baijfact2.c,v 1.53 2001/06/21 17:46:26 bsmith Exp buschelm $*/
 /*
     Factorization code for BAIJ format. 
 */
@@ -2447,11 +2447,11 @@ int MatILUFactorSymbolic_SeqBAIJ(Mat A,IS isrow,IS iscol,MatILUInfo *info,Mat *f
         PetscLogInfo(A,"MatILUFactorSymbolic_SeqBAIJ:sing special in-place natural ordering factor and solve BS=3\n");
         break; 
       case 4:
-#if defined(PETSC_HAVE_ICL_SSE)
+#if defined(PETSC_HAVE_SSE)
         PetscTruth sse_enabled;
         ierr = PetscSSEIsEnabled(&sse_enabled);CHKERRQ(ierr);
         if (sse_enabled) {
-          (*fact)->ops->lufactornumeric = MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_ICL_SSE;
+          (*fact)->ops->lufactornumeric = MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_SSE;
         } else {
           (*fact)->ops->lufactornumeric = MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering;
         }
