bde8abbff8e8f997b39ea3e814d4192540769e14
Barry Smith <bsmith@mcs.anl.gov>
2001-07-31 15:46:14 -0600

    AutoCheckin

--- src/contrib/esi/tests/ESI_IndexSpace_test.c
+++ src/contrib/esi/tests/ESI_IndexSpace_test.c
@@ -10,13 +10,13 @@ int ESI_MapAlgebraic_test(ESI_MapAlgebraic *map)
 {
   int ierr,length,offset,*offsets;
 
-  ierr = ESI_Map_test((ESI_Map*) map); if (ierr) return ierr;
+  ierr = ESI_Map_test((ESI_Map*) map);
   if (ierr) {printf("error calling ESI_Map_test\n");return ierr;}
 
-  ierr = map->getLocalInfo(length,offset); if (ierr) return ierr;
+  ierr = map->getLocalInfo(length,offset);
   if (ierr) {printf("error calling mapalgebraic->getLocalInfo\n");return ierr;}
   printf("ESI_MapAlgebraic_test: local length %d offset %d\n",length,offset);
-  ierr = map->getGlobalInfo(length,offsets); if (ierr) return ierr;
+  ierr = map->getGlobalInfo(length,offsets);
   if (ierr) {printf("error calling mapalgebraic->getGlobalInfo\n");return ierr;}
   printf("ESI_MapAlgebraic_test: total length %d first offset %d\n",length,offsets[0]);
   return 0;
--- src/contrib/esi/tests/ESI_Matrix_test.c
+++ src/contrib/esi/tests/ESI_Matrix_test.c
@@ -1,70 +1,26 @@
 
 /*
-       Tests the ESI_Vector interface
+       Tests the ESI_Matrix interface
 */
 #include "ESI.h"
 
-extern int ESI_Map_test(ESI_Map *);
+extern int ESI_Object_test(ESI_Object *);
 
-int ESI_Vector_test(ESI_Vector *vector)
+int ESI_Matrix_test(ESI_Matrix *matrix,ESI_Vector *vector,ESI_Vector *bvector)
 {
   int            ierr;
-  ESI_Map        *map;
-  ESI_Vector     *newvector;
-  double         mdot[1],dot,norm1,norm2,norm2squared,norminfinity;
 
-  ierr = vector->getMap((const ESI_Map *)map);
-  if (ierr) {printf("error calling vector->getMap()\n");return ierr;}
-  ierr = ESI_Map_test(map); 
-  if (ierr) {printf("error calling ESI_Map_test\n");return ierr;}
+  ierr = ESI_Object_test((ESI_Object*)matrix); 
+  if (ierr) {printf("error calling ESI_Object_test()\n");return ierr;}
 
-  ierr = vector->clone(newvector);
-  if (ierr) {printf("error calling vector->clone() \n");return ierr;}
+  ierr = matrix->setup();
+  if (ierr) {printf("error calling matrix->setup() \n");return ierr;}
 
-  ierr = vector->copy(*newvector);
-  if (ierr) {printf("error calling vector->copy() \n");return ierr;}
+  ierr = matrix->matvec(*vector,*bvector);
+  if (ierr) {printf("error calling matrix->matvec() \n");return ierr;}
 
-  ierr = vector->put(2.0);
-  ierr = newvector->put(1.0);
-  if (ierr) {printf("error calling vector->put() \n");return ierr;}
-
-  ierr = vector->scale(2.0);
-  if (ierr) {printf("error calling vector->scale() \n");return ierr;}
-
-  ierr = vector->scaleDiagonal(*newvector);
-  if (ierr) {printf("error calling vector->scalediagonal() \n");return ierr;}
-
-  ierr = vector->axpy(*newvector,3.0);
-  if (ierr) {printf("error calling vector->axpy() \n");return ierr;}
-
-  ierr = vector->aypx(5.0,*newvector);
-  if (ierr) {printf("error calling vector->aypx() \n");return ierr;}
-
-  ierr = vector->dot(*newvector,dot);
-  if (ierr) {printf("error calling vector->dot() \n");return ierr;}
-  printf("ESI_Vector_test: dot %g\n",dot);
-
-  ierr = vector->norm1(norm1);
-  if (ierr) {printf("error calling vector->norm1() \n");return ierr;}
-  printf("ESI_Vector_test: norm1 %g\n",norm1);
-
-  ierr = vector->norm2(norm2);
-  if (ierr) {printf("error calling vector->norm2() \n");return ierr;}
-  printf("ESI_Vector_test: norm2 %g\n",norm2);
-
-  ierr = vector->norm2squared(norm2squared);
-  if (ierr) {printf("error calling vector->norm2squared() \n");return ierr;}
-  printf("ESI_Vector_test: norm2squared %g\n",norm2squared);
-
-  ierr = vector->normInfinity(norminfinity);
-  if (ierr) {printf("error calling vector->normInfinity() \n");return ierr;}
-  printf("ESI_Vector_test: normInfinity %g\n",norminfinity);
-
-  ierr = vector->mdot(1,(const ESI_Vector **) &newvector,mdot);
-  if (ierr) {printf("error calling vector->mdot() \n");return ierr;}
-  printf("ESI_Vector_test: normmdot %g\n",mdot[0]);
-
-  delete newvector;
+  ierr = matrix->apply(*vector,*bvector);
+  if (ierr) {printf("error calling matrix->apply() \n");return ierr;}
 
   return 0;
 }
