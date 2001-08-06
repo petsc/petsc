19dc493e738d6f584b5444f4fa97177f8a84e960
Barry Smith <bsmith@mcs.anl.gov>
2001-08-06 09:42:42 -0600

    *** empty log message ***

--- include/petscts.h
+++ include/petscts.h
@@ -1,4 +1,4 @@
-/* $Id: petscts.h,v 1.45 2001/03/22 20:32:16 bsmith Exp bsmith $ */
+/* $Id: petscts.h,v 1.46 2001/04/10 19:37:08 bsmith Exp bsmith $ */
 /*
    User interface for the timestepping package. This is package
    is for use in solving time-dependent PDEs.
@@ -50,7 +50,7 @@ EXTERN int TSSetType(TS,TSType);
 EXTERN int TSGetProblemType(TS,TSProblemType*);
 EXTERN int TSDestroy(TS);
 
-EXTERN int TSSetMonitor(TS,int(*)(TS,int,double,Vec,void*),void *,int (*)(void*));
+EXTERN int TSSetMonitor(TS,int(*)(TS,int,PetscReal,Vec,void*),void *,int (*)(void*));
 EXTERN int TSClearMonitor(TS);
 EXTERN int TSGetType(TS,TSType*);
 
@@ -63,42 +63,42 @@ EXTERN int TSSetUp(TS);
 EXTERN int TSSetSolution(TS,Vec);
 EXTERN int TSGetSolution(TS,Vec*);
 
-EXTERN int TSSetDuration(TS,int,double);
+EXTERN int TSSetDuration(TS,int,PetscReal);
 
-EXTERN int TSDefaultMonitor(TS,int,double,Vec,void*);
-EXTERN int TSVecViewMonitor(TS,int,double,Vec,void*);
-EXTERN int TSStep(TS,int *,double*);
+EXTERN int TSDefaultMonitor(TS,int,PetscReal,Vec,void*);
+EXTERN int TSVecViewMonitor(TS,int,PetscReal,Vec,void*);
+EXTERN int TSStep(TS,int *,PetscReal*);
 
-EXTERN int TSSetInitialTimeStep(TS,double,double);
-EXTERN int TSGetTimeStep(TS,double*);
-EXTERN int TSGetTime(TS,double*);
+EXTERN int TSSetInitialTimeStep(TS,PetscReal,PetscReal);
+EXTERN int TSGetTimeStep(TS,PetscReal*);
+EXTERN int TSGetTime(TS,PetscReal*);
 EXTERN int TSGetTimeStepNumber(TS,int*);
-EXTERN int TSSetTimeStep(TS,double);
+EXTERN int TSSetTimeStep(TS,PetscReal);
 
-EXTERN int TSSetRHSFunction(TS,int (*)(TS,double,Vec,Vec,void*),void*);
-EXTERN int TSSetRHSMatrix(TS,Mat,Mat,int (*)(TS,double,Mat*,Mat*,MatStructure*,void*),void*);
-EXTERN int TSSetRHSJacobian(TS,Mat,Mat,int(*)(TS,double,Vec,Mat*,Mat*,MatStructure*,void*),void*);
-EXTERN int TSSetRHSBoundaryConditions(TS,int (*)(TS,double,Vec,void*),void*);
+EXTERN int TSSetRHSFunction(TS,int (*)(TS,PetscReal,Vec,Vec,void*),void*);
+EXTERN int TSSetRHSMatrix(TS,Mat,Mat,int (*)(TS,PetscReal,Mat*,Mat*,MatStructure*,void*),void*);
+EXTERN int TSSetRHSJacobian(TS,Mat,Mat,int(*)(TS,PetscReal,Vec,Mat*,Mat*,MatStructure*,void*),void*);
+EXTERN int TSSetRHSBoundaryConditions(TS,int (*)(TS,PetscReal,Vec,void*),void*);
 
-EXTERN int TSDefaultComputeJacobianColor(TS,double,Vec,Mat*,Mat*,MatStructure*,void*);
-EXTERN int TSDefaultComputeJacobian(TS,double,Vec,Mat*,Mat*,MatStructure*,void*);
+EXTERN int TSDefaultComputeJacobianColor(TS,PetscReal,Vec,Mat*,Mat*,MatStructure*,void*);
+EXTERN int TSDefaultComputeJacobian(TS,PetscReal,Vec,Mat*,Mat*,MatStructure*,void*);
 
 EXTERN int TSGetRHSMatrix(TS,Mat*,Mat*,void**);
 EXTERN int TSGetRHSJacobian(TS,Mat*,Mat*,void**);
 
-EXTERN int TSPseudoSetTimeStep(TS,int(*)(TS,double*,void*),void*);
-EXTERN int TSPseudoDefaultTimeStep(TS,double*,void*);
-EXTERN int TSPseudoComputeTimeStep(TS,double *);
+EXTERN int TSPseudoSetTimeStep(TS,int(*)(TS,PetscReal*,void*),void*);
+EXTERN int TSPseudoDefaultTimeStep(TS,PetscReal*,void*);
+EXTERN int TSPseudoComputeTimeStep(TS,PetscReal *);
 
-EXTERN int TSPseudoSetVerifyTimeStep(TS,int(*)(TS,Vec,void*,double*,int*),void*);
-EXTERN int TSPseudoDefaultVerifyTimeStep(TS,Vec,void*,double*,int*);
-EXTERN int TSPseudoVerifyTimeStep(TS,Vec,double*,int*);
-EXTERN int TSPseudoSetTimeStepIncrement(TS,double);
+EXTERN int TSPseudoSetVerifyTimeStep(TS,int(*)(TS,Vec,void*,PetscReal*,int*),void*);
+EXTERN int TSPseudoDefaultVerifyTimeStep(TS,Vec,void*,PetscReal*,int*);
+EXTERN int TSPseudoVerifyTimeStep(TS,Vec,PetscReal*,int*);
+EXTERN int TSPseudoSetTimeStepIncrement(TS,PetscReal);
 EXTERN int TSPseudoIncrementDtFromInitialDt(TS);
 
-EXTERN int TSComputeRHSFunction(TS,double,Vec,Vec);
-EXTERN int TSComputeRHSBoundaryConditions(TS,double,Vec);
-EXTERN int TSComputeRHSJacobian(TS,double,Vec,Mat*,Mat*,MatStructure*);
+EXTERN int TSComputeRHSFunction(TS,PetscReal,Vec,Vec);
+EXTERN int TSComputeRHSBoundaryConditions(TS,PetscReal,Vec);
+EXTERN int TSComputeRHSJacobian(TS,PetscReal,Vec,Mat*,Mat*,MatStructure*);
 
 extern PetscFList      TSList;
 EXTERN int        TSRegisterAll(char*);
@@ -121,7 +121,7 @@ EXTERN int TSSetApplicationContext(TS,void *);
 EXTERN int TSGetApplicationContext(TS,void **);
 
 EXTERN int TSLGMonitorCreate(char *,char *,int,int,int,int,PetscDrawLG *);
-EXTERN int TSLGMonitor(TS,int,double,Vec,void *);
+EXTERN int TSLGMonitor(TS,int,PetscReal,Vec,void *);
 EXTERN int TSLGMonitorDestroy(PetscDrawLG);
 
 /*
@@ -132,11 +132,11 @@ typedef enum { PVODE_ADAMS,PVODE_BDF } TSPVodeType;
 typedef enum { PVODE_MODIFIED_GS = 0,PVODE_CLASSICAL_GS = 1 } TSPVodeGramSchmidtType;
 EXTERN int TSPVodeSetType(TS,TSPVodeType);
 EXTERN int TSPVodeGetPC(TS,PC*);
-EXTERN int TSPVodeSetTolerance(TS,double,double);
+EXTERN int TSPVodeSetTolerance(TS,PetscReal,PetscReal);
 EXTERN int TSPVodeGetIterations(TS,int *,int *);
 EXTERN int TSPVodeSetGramSchmidtType(TS,TSPVodeGramSchmidtType);
 EXTERN int TSPVodeSetGMRESRestart(TS,int);
-EXTERN int TSPVodeSetLinearTolerance(TS,double);
+EXTERN int TSPVodeSetLinearTolerance(TS,PetscReal);
 EXTERN int TSPVodeSetExactFinalTime(TS,PetscTruth);
 
 #endif
