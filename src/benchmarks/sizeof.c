8c1b1b9efea78b03ec857aa7c3707e01794d210d
Satish Balay <balay@mcs.anl.gov>
2001-08-29 14:54:07 -0600

    *** empty log message ***

--- src/benchmarks/sizeof.c
+++ src/benchmarks/sizeof.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main()
+/*
+        Prints the size of various C data types
+*/
+int main(int argc,char *args)
 {
+  printf("Long Double %d\n",sizeof(long double));
+  printf("double %d\n",sizeof(double));
+  printf("int %d\n",sizeof(int));
+  printf("char %d\n",sizeof(char));
+  printf("short %d\n",sizeof(short));
+  printf("long %d\n",sizeof(long));
+  printf("long long %d\n",sizeof(long long));
+  printf("int * %d\n",sizeof(int*));
 
-  printf("Long Double %d double %d int %d ptr %d\n",sizeof(long double),sizeof(double),sizeof(int),sizeof(int*));
   return 0;
 }
