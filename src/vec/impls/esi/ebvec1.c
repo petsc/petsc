b7e4f9153a0339583ad36e47ba5b6b049bbce67d
Barry Smith <bsmith@mcs.anl.gov>
2001-08-31 14:54:08 -0600

    *** empty log message ***

--- src/vec/impls/esi/ebvec1.c
+++ src/vec/impls/esi/ebvec1.c
@@ -1,137 +1,224 @@
-/*$Id: bvec1.c,v 1.41 2001/08/07 03:02:21 balay Exp bsmith $*/
 
-/*
-   Defines the BLAS based vector operations. Code shared by parallel
-  and sequential vectors.
-*/
+/*$Id: ebvec1.c,v 1.1 2001/08/29 20:54:49 bsmith Exp bsmith $*/
+
 
 #include "src/vec/vecimpl.h" 
-#include "src/vec/impls/dvecimpl.h" 
-#include "petscblaslapack.h"
+#include "esi/ESI.h"
+#include "esi/petsc/vector.h"
+
+typedef struct { 
+  esi::Vector<double,int> *evec;
+} Vec_ESI;
+
 
 #undef __FUNCT__  
-#define __FUNCT__ "VecDot_Seq"
-int VecDot_Seq(Vec xin,Vec yin,PetscScalar *z)
+#define __FUNCT__ "VecESISetVector"
+int VecESISetVector(Vec xin,esi::Vector<double,int> *v)
 {
-  Vec_Seq *x = (Vec_Seq *)xin->data,*y = (Vec_Seq *)yin->data;
-#if !defined(PETSC_USE_COMPLEX)
-  int     one = 1;
-#endif
+  Vec_ESI    *x;
+  PetscTruth tesi;
+  int        ierr;
 
   PetscFunctionBegin;
-#if defined(PETSC_USE_COMPLEX)
-  /* cannot use BLAS dot for complex because compiler/linker is 
-     not happy about returning a double complex */
-  {
-    int         i;
-    PetscScalar sum = 0.0,*xa = x->array,*ya = y->array;
-    for (i=0; i<xin->n; i++) {
-      sum += xa[i]*PetscConj(ya[i]);
-    }
-    *z = sum;
+  ierr = PetscTypeCompare((PetscObject)xin,0,&tesi);CHKERRQ(ierr);
+  if (tesi) {
+    ierr = VecSetType(xin,VEC_ESI);CHKERRQ(ierr);
   }
-#else
-  *z = BLdot_(&xin->n,x->array,&one,y->array,&one);
-#endif
-  PetscLogFlops(2*xin->n-1);
-  PetscFunctionReturn(0);
-}
+  ierr = PetscTypeCompare((PetscObject)xin,VEC_ESI,&tesi);CHKERRQ(ierr);
+  if (tesi) {
+    int                    n,N;
+    esi::MapPartition<int> *map;
 
-#undef __FUNCT__  
-#define __FUNCT__ "VecTDot_Seq"
-int VecTDot_Seq(Vec xin,Vec yin,PetscScalar *z)
-{
-  Vec_Seq *x = (Vec_Seq *)xin->data,*y = (Vec_Seq *)yin->data;
-#if !defined(PETSC_USE_COMPLEX)
- int     one = 1;
-#endif
+    ierr = v->getGlobalSize(N);CHKERRQ(ierr);
+    if (xin->N == -1) xin->N = N;
+    else if (xin->N != N) SETERRQ2(1,"Global size of Vec %d not equal size of esi::Vector %d",xin->N,N);
 
-  PetscFunctionBegin;
-#if defined(PETSC_USE_COMPLEX)
-  /* cannot use BLAS dot for complex because compiler/linker is 
-     not happy about returning a double complex */
-  int         i;
-  PetscScalar sum = 0.0,*xa = x->array,*ya = y->array;
-  for (i=0; i<xin->n; i++) {
-    sum += xa[i]*ya[i];
+    ierr = v->getMapPartition(map);CHKERRQ(ierr); 
+    ierr = map->getLocalSize(n);CHKERRQ(ierr);
+    if (xin->n == -1) xin->n = n;
+    else if (xin->n != n) SETERRQ2(1,"Local size of Vec %d not equal size of esi::Vector %d",xin->n,n);
+
+    x       = (Vec_ESI*)xin->data;
+    x->evec = v;
+    v->addReference();
   }
-  *z = sum;
-#else
-  *z = BLdot_(&xin->n,x->array,&one,y->array,&one);
-#endif
-  PetscLogFlops(2*xin->n-1);
   PetscFunctionReturn(0);
 }
 
-#undef __FUNCT__  
-#define __FUNCT__ "VecScale_Seq"
-int VecScale_Seq(const PetscScalar *alpha,Vec xin)
-{
-  Vec_Seq *x = (Vec_Seq*)xin->data;
-  int     one = 1;
-
-  PetscFunctionBegin;
-  BLscal_(&xin->n,(PetscScalar *)alpha,x->array,&one);
-  PetscLogFlops(xin->n);
-  PetscFunctionReturn(0);
-}
+/* ---------------------------------------------------------------------------------*/
 
 #undef __FUNCT__  
-#define __FUNCT__ "VecCopy_Seq"
-int VecCopy_Seq(Vec xin,Vec yin)
+#define __FUNCT__ "VecSet_ESI"
+int VecSet_ESI(const PetscScalar *alpha,Vec xin)
 {
-  Vec_Seq *x = (Vec_Seq *)xin->data,*y = (Vec_Seq *)yin->data;
+  Vec_ESI *x = (Vec_ESI*)xin->data;
   int     ierr;
 
   PetscFunctionBegin;
-  if (x->array != y->array) {
-    ierr = PetscMemcpy(y->array,x->array,xin->n*sizeof(PetscScalar));CHKERRQ(ierr);
+  ierr = x->evec->put(*alpha);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "VecDuplicate_ESI"
+int VecDuplicate_ESI(Vec xin,Vec *xout)
+{
+  Vec_ESI                 *x = (Vec_ESI*)xin->data;
+  int                     ierr;
+  esi::Vector<double,int> *nevec;
+
+  PetscFunctionBegin;
+  ierr = VecCreate(xin->comm,xin->n,xin->N,xout);CHKERRQ(ierr);
+  ierr = x->evec->clone(nevec);
+  ierr = VecESISetVector(*xout,nevec);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "VecDot_ESI"
+int VecDot_ESI(Vec xin,Vec yin,PetscScalar *z)
+{
+  Vec_ESI                 *x = (Vec_ESI*)xin->data;
+  int                     ierr;
+  esi::Vector<double,int> *ytmp;
+
+  PetscFunctionBegin;
+  /* Make yin look like an esi:Vector */
+  ytmp = new esi::petsc::Vector<double,int>(yin);
+  ierr = x->evec->dot(*ytmp,*z);CHKERRQ(ierr);
+  delete ytmp;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "VecMDot_ESI"
+int VecMDot_ESI(int nv,Vec xin,const Vec yin[],PetscScalar *z)
+{
+  Vec_ESI                 *x = (Vec_ESI *)xin->data;
+  int                     ierr,i;
+  esi::Vector<double,int> *ytmp;
+
+  PetscFunctionBegin;
+  for (i=0; i<nv; i++) {
+    /* Make yin look like an esi:Vector */
+    ytmp = new esi::petsc::Vector<double,int>(yin[i]);
+    ierr = x->evec->dot(*ytmp,z[i]);CHKERRQ(ierr);
+    delete ytmp;
   }
   PetscFunctionReturn(0);
 }
 
+
 #undef __FUNCT__  
-#define __FUNCT__ "VecSwap_Seq"
-int VecSwap_Seq(Vec xin,Vec yin)
+#define __FUNCT__ "VecGetSize_ESI"
+int VecGetSize_ESI(Vec vin,int *size)
 {
-  Vec_Seq *x = (Vec_Seq *)xin->data,*y = (Vec_Seq *)yin->data;
-  int     one = 1;
+  Vec_ESI                 *x = (Vec_ESI*)vin->data;
+  int                     ierr;
 
   PetscFunctionBegin;
-  BLswap_(&xin->n,x->array,&one,y->array,&one);
+  ierr = x->evec->getGlobalSize(*size);CHKERRQ(ierr); 
   PetscFunctionReturn(0);
 }
 
 #undef __FUNCT__  
-#define __FUNCT__ "VecAXPY_Seq"
-int VecAXPY_Seq(const PetscScalar *alpha,Vec xin,Vec yin)
+#define __FUNCT__ "VecGetLocalSize_ESI"
+int VecGetLocalSize_ESI(Vec vin,int *size)
 {
-  Vec_Seq      *x = (Vec_Seq *)xin->data;
-  int          one = 1,ierr;
-  PetscScalar  *yarray;
+  Vec_ESI                *x = (Vec_ESI*)vin->data;
+  int                    ierr;
+  esi::MapPartition<int> *map;
 
   PetscFunctionBegin;
-  ierr = VecGetArray(yin,&yarray);CHKERRQ(ierr);
-  BLaxpy_(&xin->n,(PetscScalar *)alpha,x->array,&one,yarray,&one);
-  ierr = VecRestoreArray(yin,&yarray);CHKERRQ(ierr);
-  PetscLogFlops(2*xin->n);
+  ierr = x->evec->getMapPartition(map);CHKERRQ(ierr); 
+  ierr = map->getLocalSize(*size);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 #undef __FUNCT__  
-#define __FUNCT__ "VecAXPBY_Seq"
-int VecAXPBY_Seq(const PetscScalar *alpha,const PetscScalar *beta,Vec xin,Vec yin)
+#define __FUNCT__ "VecDestroy_ESI"
+int VecDestroy_ESI(Vec v)
 {
-  Vec_Seq      *x = (Vec_Seq *)xin->data,*y = (Vec_Seq *)yin->data;
-  int          n = xin->n,i;
-  PetscScalar  *xx = x->array,*yy = y->array,a = *alpha,b = *beta;
+  Vec_ESI *vs = (Vec_ESI*)v->data;
+  int     ierr;
 
   PetscFunctionBegin;
-  for (i=0; i<n; i++) {
-    yy[i] = a*xx[i] + b*yy[i];
+  if (vs->evec) {
+    vs->evec->deleteReference();
   }
-
-  PetscLogFlops(3*xin->n);
+  ierr = PetscFree(vs);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
+EXTERN_C_BEGIN
+#undef __FUNCT__  
+#define __FUNCT__ "VecCreate_PetscESI"
+int VecCreate_PetscESI(Vec V)
+{
+  int          ierr;
+  Vec          v;
+
+  PetscFunctionBegin;
+  ierr = VecSetType(V,VEC_ESI);CHKERRQ(ierr);
+  ierr = VecCreateMPI(V->comm,V->n,V->N,&v);CHKERRQ(ierr);
+  ierr = VecESISetVector(V,new esi::petsc::Vector<double,int>(v));CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+static struct _VecOps EvOps = {VecDuplicate_ESI,
+			       VecDuplicateVecs_Default,
+			       VecDestroyVecs_Default,
+			       VecDot_ESI,
+			       VecMDot_ESI,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       VecSet_ESI,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       VecGetSize_ESI,
+			       VecGetLocalSize_ESI,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+			       0,
+                               0};
+
+EXTERN_C_BEGIN
+#undef __FUNCT__  
+#define __FUNCT__ "VecCreate_ESI"
+int VecCreate_ESI(Vec V)
+{
+  Vec_ESI      *s;
+  int          ierr;
+
+  PetscFunctionBegin;
+  ierr    = PetscNew(Vec_ESI,&s);CHKERRQ(ierr);
+  s->evec = 0;
+  V->data = (void*)s;
+  ierr    = PetscMemcpy(V->ops,&EvOps,sizeof(EvOps));CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
