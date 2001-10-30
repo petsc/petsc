62775f36d3126dd9906b1d9b180abc2e37de4dd2
Hong Zhang <hzhang@mcs.anl.gov>
2001-10-30 11:47:46 -0600

    bk-changeset-1.129 hzhang@shakey.mcs.anl.gov|ChangeSet|20011030174746|48793 bmake/linux_superludist/variables     1.1 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +79 -0
      bmake/linux_superludist/rules
        1.1 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +84 -0
    
      bmake/linux_superludist/petscfix.h
        1.1 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +26 -0
    
      bmake/linux_superludist/petscconf.h
        1.1 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +45 -0
    
      bmake/linux_superludist/packages
        1.1 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +27 -0
    
    ChangeSet
      1.129 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +5 -0
      Add in PETSC_ARCH=linux_superludist
    
      bmake/linux_superludist/variables
        1.0 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc/bmake/linux_superludist/variables
    
      bmake/linux_superludist/rules
        1.0 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc/bmake/linux_superludist/rules
    
      bmake/linux_superludist/petscfix.h
        1.0 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc/bmake/linux_superludist/petscfix.h
    
      bmake/linux_superludist/petscconf.h
        1.0 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc/bmake/linux_superludist/petscconf.h
    
      bmake/linux_superludist/packages
        1.0 01/10/30 11:47:46 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc/bmake/linux_superludist/packages
    
    
    Hg-commit: 6c831d36a569ed8d6ad385b1d74d463292101fa4

---
+++ bmake/linux_superludist/petscconf.h
@@ -0,0 +1,45 @@
+#ifdef PETSC_RCS_HEADER
+"$Id: petscconf.h,v 1.26 2001/09/18 14:48:03 bsmith Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define PARCH_linux
+#define PETSC_ARCH_NAME "linux"
+
+#define HAVE_POPEN
+#define HAVE_LIMITS_H
+#define HAVE_PWD_H 
+#define HAVE_MALLOC_H 
+#define HAVE_STRING_H 
+#define HAVE_GETDOMAINNAME
+#define HAVE_DRAND48 
+#define HAVE_UNAME 
+#define HAVE_UNISTD_H 
+#define HAVE_SYS_TIME_H 
+#define HAVE_STDLIB_H
+#define HAVE_GETCWD
+#define BITS_PER_BYTE 8
+
+#define PETSC_HAVE_FORTRAN_UNDERSCORE 
+#define PETSC_HAVE_FORTRAN_UNDERSCORE_UNDERSCORE
+
+#define HAVE_READLINK
+#define HAVE_MEMMOVE
+#define PETSC_HAVE_STD_COMPLEX
+
+#define PETSC_HAVE_DOUBLE_ALIGN_MALLOC
+#define HAVE_MEMALIGN
+#define HAVE_SYS_RESOURCE_H
+#define SIZEOF_VOID_P 4
+#define SIZEOF_INT 4
+#define SIZEOF_DOUBLE 8
+
+#define PETSC_USE_DYNAMIC_LIBRARIES 1
+#define PETSC_HAVE_RTLD_GLOBAL 1
+
+#define HAVE_SYS_UTSNAME_H
+
+#endif
