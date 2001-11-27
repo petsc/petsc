0a7eb86f25d89a7b5fb427d7139f173ec0ed8968
Matthew Knepley <knepley@mcs.anl.gov>
2001-11-27 17:33:38 -0600

    bk-changeset-1.223.1.1 knepley@spinetta.mcs.anl.gov|ChangeSet|20011127233338|64145 ChangeSet   1.223.1.1 01/11/27 17:33:38 knepley@spinetta.mcs.anl.gov +3 -0   Fixed format
      src/sys/src/viewer/impls/ascii/vcreatea.c
        1.4 01/11/27 17:33:38 knepley@spinetta.mcs.anl.gov +10 -10
        Fixed format
    
      src/sys/src/draw/utils/lg.c
        1.7 01/11/27 17:33:38 knepley@spinetta.mcs.anl.gov +1 -1
        Fixed format
    
      src/sys/src/draw/utils/hists.c
        1.11 01/11/27 17:33:37 knepley@spinetta.mcs.anl.gov +2 -2
        Fixed format
    
    
    Hg-commit: 3673cf8ab7ce4af384578c071df9f10419b405e5

--- src/sys/src/viewer/impls/ascii/vcreatea.c
+++ src/sys/src/viewer/impls/ascii/vcreatea.c
@@ -40,17 +40,17 @@ PetscViewer PETSC_VIEWER_STDOUT_(MPI_Comm comm)
   PetscFunctionBegin;
   if (Petsc_Viewer_Stdout_keyval == MPI_KEYVAL_INVALID) {
     ierr = MPI_Keyval_create(MPI_NULL_COPY_FN,MPI_NULL_DELETE_FN,&Petsc_Viewer_Stdout_keyval,0);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   }
   ierr = MPI_Attr_get(comm,Petsc_Viewer_Stdout_keyval,(void **)&viewer,(int*)&flg);
-  if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+  if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   if (!flg) { /* PetscViewer not yet created */
     ierr = PetscViewerASCIIOpen(comm,"stdout",&viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
     ierr = PetscObjectRegisterDestroy((PetscObject)viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
     ierr = MPI_Attr_put(comm,Petsc_Viewer_Stdout_keyval,(void*)viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   } 
   PetscFunctionReturn(viewer);
 }
@@ -92,17 +92,17 @@ PetscViewer PETSC_VIEWER_STDERR_(MPI_Comm comm)
   PetscFunctionBegin;
   if (Petsc_Viewer_Stderr_keyval == MPI_KEYVAL_INVALID) {
     ierr = MPI_Keyval_create(MPI_NULL_COPY_FN,MPI_NULL_DELETE_FN,&Petsc_Viewer_Stderr_keyval,0);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   }
   ierr = MPI_Attr_get(comm,Petsc_Viewer_Stderr_keyval,(void **)&viewer,(int*)&flg);
-  if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+  if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   if (!flg) { /* PetscViewer not yet created */
     ierr = PetscViewerASCIIOpen(comm,"stderr",&viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
     ierr = PetscObjectRegisterDestroy((PetscObject)viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDOUT_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
     ierr = MPI_Attr_put(comm,Petsc_Viewer_Stderr_keyval,(void*)viewer);
-    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1,0); PetscFunctionReturn(0);}
+    if (ierr) {PetscError(__LINE__,"VIEWER_STDERR_",__FILE__,__SDIR__,1,1," "); PetscFunctionReturn(0);}
   } 
   PetscFunctionReturn(viewer);
 }
