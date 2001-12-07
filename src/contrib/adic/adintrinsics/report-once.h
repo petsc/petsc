20aa91e42169af643e4fbd20354c6c2496bf81bc
Satish Balay <balay@mcs.anl.gov>
2001-12-07 10:12:09 -0600

    bk-changeset-1.339 balay@shakey.mcs.anl.gov|ChangeSet|20011207161209|24418 ChangeSet   1.339 01/12/07 10:12:09 balay@shakey.mcs.anl.gov +1 -0   fix adic intrinsics warnings on solaris64
      src/contrib/adic/adintrinsics/report-once.h
        1.2 01/12/07 10:12:09 balay@shakey.mcs.anl.gov +4 -7
        reformat prototype definitions
    
    
    Hg-commit: 3da2a3cb030b29411d600d81257a3467ca49f5ed

--- src/contrib/adic/adintrinsics/report-once.h
+++ src/contrib/adic/adintrinsics/report-once.h
@@ -10,14 +10,11 @@ extern "C" {
 #define RO_EXTERN extern
 #endif
 
-#define _P(x) x;
+#define _P(x) x
 
-RO_EXTERN void
-reportonce_files _P((int));
-RO_EXTERN void
-reportonce_accumulate _P((int file, int line, int exception));
-RO_EXTERN void
-reportonce_summary _P((void));
+RO_EXTERN void reportonce_files _P((int));
+RO_EXTERN void reportonce_accumulate _P((int file, int line, int exception));
+RO_EXTERN void reportonce_summary _P((void));
 RO_EXTERN void reportonce_reset _P((void));
 RO_EXTERN void reportonce_ehsfid _P((int *g_ehfid, char *routine, char *filename));
 
