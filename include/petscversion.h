df0dcae577a7fee2e933b623f3c0f5e91c3b90d9
Satish Balay <balay@mcs.anl.gov>
2001-12-19 13:11:35 -0600

    bk-changeset-1.414 balay@shakey.mcs.anl.gov|ChangeSet|20011219191135|28586 ChangeSet   1.414 01/12/19 13:11:35 balay@shakey.mcs.anl.gov +4 -0   petsc-2.1.1 version info added
      maint/builddist
        1.20 01/12/19 13:11:35 balay@shakey.mcs.anl.gov +1 -1
        update version
    
      include/petscversion.h
        1.5 01/12/19 13:11:35 balay@shakey.mcs.anl.gov +3 -3
        version update
    
      docs/tex/manual/part1.tex
        1.9 01/12/19 13:11:35 balay@shakey.mcs.anl.gov +2 -2
        upate version number and date
    
      docs/tex/manual/manual.tex
        1.8 01/12/19 13:11:34 balay@shakey.mcs.anl.gov +1 -1
        update version date
    
    
    Hg-commit: f7e5572736dc9f7f8f28ed3eb8b87edbdfef894c

--- include/petscversion.h
+++ include/petscversion.h
@@ -10,13 +10,13 @@
     docs/tex/manual/intro.tex,
     docs/tex/manual/manual.tex.
 */
-#define PETSC_VERSION_NUMBER "PETSc Version 2.1.0, Released April 11, 2001"
+#define PETSC_VERSION_NUMBER "PETSc Version 2.1.1, Released December 19, 2001"
 
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    1
-#define PETSC_VERSION_SUBMINOR 0
+#define PETSC_VERSION_SUBMINOR 1
 #define PETSC_PATCH_LEVEL      0
-#define PETSC_VERSION_DATE     "April 11, 2001"
+#define PETSC_VERSION_DATE     "December 19, 2001"
 #define PETSC_AUTHOR_INFO      "\
        The PETSc Team\
     petsc-maint@mcs.anl.gov\n\
