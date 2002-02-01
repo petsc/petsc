79a566c923c54d1414d23e8deae378e1890559c2
Matthew Knepley <knepley@mcs.anl.gov>
2002-02-01 09:04:16 -0600

    bk-changeset-1.28 knepley@khan.mcs.anl.gov|ChangeSet|20020201150416|35430 ChangeSet   1.28 02/02/01 09:04:16 knepley@khan.mcs.anl.gov +1 -0   Beginning rewrite of BS to use tagged FileGroups
      bs.py
        1.6 02/02/01 09:04:16 knepley@khan.mcs.anl.gov +75 -217
        Put in Maker
          - Debug printing
          - Shell execution
          - Temporary directory management
        Put in BS
          - Source DB creation/persistence
    
    
    Hg-commit: c70799310ad51050150542cc47b30f66e32e26d8

--- bs.py
+++ bs.py
@@ -1,226 +1,84 @@
 #!/usr/bin/env python
-import commands
-import os.path
-import string
-import time
 
-echo = 1
+# Debugging
+debugLevel    = 1
+debugSections = []
 
 class Maker:
-  def executeShellCommand(self, command):
-    if echo: print command
-    (status, output) = commands.getstatusoutput(command)
-    if status: raise IOError('Could not execute \''+command+'\': '+output)
-    return output
+  def __init__(self):
+    self.setupTmpDir()
+    self.debugLevel    = debugLevel
+    self.debugSections = list(debugSections)
+    self.debugIndent   = '  '
 
-class FileGroup (Maker):
-  def __init__(self, data = [], func = None, children = []):
-    self.data     = data
-    self.func     = func
-    self.children = children
-
-  def append(self, item):
-    self.data.append(item)
-
-  def getFiles(self):
-    funcData = []
-    if (self.func):
-      funcData = self.func(self)
-    childData = []
-    for child in self.children:
-      childData += child.getFiles()
-    return self.data+funcData+childData
-
-class TreeFileGroup (FileGroup):
-  def __init__(self, root, fileTest):
-    FileGroup.__init__(self, func=self.walkTree)
-    self.root     = root
-    self.fileTest = fileTest
-
-  def walkTree(self, fileGroup):
-    files = []
-    os.path.walk(self.root, self.fileTest, files)
-    return files
-
-class ExtensionFileGroup (TreeFileGroup):
-  def __init__(self, root, ext):
-    TreeFileGroup.__init__(self, root, self.extTest)
-    self.ext = ext
-
-  def extTest(self, extFiles, directory, fileList):
-    if (os.path.basename(directory) == 'SCCS'): return
-    for file in fileList:
-      if (os.path.isdir(os.path.join(directory, file))): continue
-      (base, ext) = os.path.splitext(file)
-      if (ext == self.ext): extFiles.append(os.path.join(directory, file))
-
-class Precondition (Maker):
-  # True here means that the precondition was satisfied
-  def __nonzero__(self):
-    if echo: print 'Checking precondition'
-    return 1
-
-class Action (Maker):
-  def __init__(self, program, flags, sources):
-    self.program = program
-    self.flags   = flags
-    self.sources = sources
-
-  def execute(self):
-    command = self.program+' '+self.flags
-    for file in self.sources.getFiles():
-      command += ' '+file
-    return self.executeShellCommand(command)
-
-class Target (Maker):
-  preconditions = []
-  actions       = []
-
-  def __init__(self, preconditions=None, actions=None):
-    if preconditions: self.preconditions = preconditions
-    if actions:       self.actions       = actions
-
-  def execute(self):
-    if (reduce(lambda a,b: a or b, self.preconditions)):
-      map(lambda x: x.execute(), self.actions)
-
-class OlderThan (Precondition):
-  def __init__(self, target, sources):
-    self.target  = target
-    self.sources = sources
-
-  def __nonzero__(self):
-    if (not os.path.exists(self.target)): return 1
-    targetTime = os.path.getmtime(self.target)
-    files = self.sources.getFiles()
-    if (not len(files)): return 1
-    for source in files:
-      sourceTime = os.path.getmtime(source)
-      if (targetTime < sourceTime):
-        print self.target+' is older than '+source
-        return 1
-    return 0
-
-class NewerThan (Precondition):
-  def __init__(self, target, sources):
-    self.target  = target
-    self.sources = sources
-
-  def __nonzero__(self):
-    if (not os.path.exists(self.target)): return 1
-    targetTime = os.path.getmtime(self.target)
-    files = self.sources.getFiles()
-    if (not len(files)): return 1
-    for source in files:
-      sourceTime = os.path.getmtime(source)
-      if (targetTime > sourceTime):
-        print self.target+' is newer than '+source
-        return 1
-    return 0
-
-class LibraryOlderThan (OlderThan):
-  def __nonzero__(self):
-    if (not os.path.exists(self.target)): return 1
-    files = self.sources.getFiles()
-    if (not len(files)): return 1
-    for source in files:
-      (dir, file) = os.path.split(source)
-      (base, ext) = os.path.splitext(file)
-      object      = base+'.o'
-
-      output      = self.executeShellCommand('ar tv '+self.target+' '+object)
-      if (output[0:8] != 'no entry'):
-        objectTime = time.strptime(output[25:42], "%b %d %H:%M %Y")
+  def setupTmpDir(self):
+    try:
+      self.tmpDir = os.path.join(os.environ['TMPDIR'], 'bs')
+    except KeyError:
+      if (os.path.exists('/tmp')):
+        self.tmpDir = os.path.join('/tmp', 'bs')
       else:
-        objectTime = 0
-
-      sourceTime = os.path.getmtime(source)
-      if (objectTime < sourceTime):
-        print self.target+'('+object+')'+' is older than '+source
-        return 1
-    return 0
-
-class CompileFiles (Action):
-  def __init__(self, compiler, compilerFlags, sources):
-    self.compiler      = compiler
-    self.compilerFlags = compilerFlags
-    self.sources       = sources
-    self.includeDirs   = []
-    self.objects       = FileGroup([])
-
-  def addIncludeDir(self, dir):
-    self.includeDirs.append(dir)
-
-  def getIncludeFlags(self):
-    flags = ''
-    for dir in self.includeDirs: flags += ' -I'+dir
-    return flags
-
-  def allAtOnceCompile(self):
-    files = self.sources.getFiles()
-    if echo: print 'Compiling '+str(files)
-    flags   = self.compilerFlags + self.getIncludeFlags()
-    command = self.compiler+' '+flags
-    for source in files:
-      command    += ' '+source
-      (base, ext) = os.path.splitext(source)
-      object      = base+'.o'
-      self.objects.append(object)
-    output = self.executeShellCommand(command)
-    if (string.find(output, 'err') >= 0):
-      print output
-
-  def fileByFileCompile(self):
-    files = self.sources.getFiles()
-    if echo: print 'Compiling '+str(files)
-    flags = self.compilerFlags + self.getIncludeFlags()
-    for source in files:
-      (dir, file) = os.path.split(source)
-      (base, ext) = os.path.splitext(file)
-      object      = os.path.join(dir, string.replace(dir, '/', '_')+'_'+base+'.o')
-      command = self.compiler+' '+flags+' -c -o '+object+' '+source
-      self.executeShellCommand(command)
-      self.objects.append(object)
-
-  def execute(self):
-    self.allAtOnceCompile()
-
-class CompileCFiles (CompileFiles):
-  def __init__(self, sources, compiler='gcc', compilerFlags='-g -Wall'):
-    CompileFiles.__init__(self, compiler, compilerFlags, sources)
-
-  def execute(self):
-    self.fileByFileCompile()
-
-class CompileCxxFiles (CompileFiles):
-  def __init__(self, sources, compiler='g++', compilerFlags='-g -Wall'):
-    CompileFiles.__init__(self, compiler, compilerFlags, sources)
-
-  def execute(self):
-    self.fileByFileCompile()
-
-class ArchiveObjects (Action):
-  def __init__(self, archiver, archiverFlags, objects, library):
-    self.archiver      = archiver
-    self.archiverFlags = archiverFlags
-    self.objects       = objects
-    self.library       = library
-  
-  def execute(self):
-    (dir, file) = os.path.split(self.library)
-    if not os.path.exists(dir): os.makedirs(dir)
-    command = self.archiver+' '+self.archiverFlags+' '+self.library
-    for obj in self.objects.getFiles(): command += ' '+obj
-    self.executeShellCommand(command)
-
-class RemoveFiles (Action):
-  def __init__(self, files):
-    self.files = files
+        raise RuntimeError('Please set the TMPDIR variable')
 
   def forceRemove(self, file):
-    if (os.path.exists(file)): os.remove(file)
+    if (os.path.exists(file)):
+      if (os.path.isdir(file)):
+        for f in os.listdir(file):
+          self.forceRemove(os.path.join(file, f))
+        os.rmdir(file)
+      else:
+        os.remove(file)
+    
+  def cleanupTmpDir(self):
+    if not os.path.exists(self.tmpDir): os.makedirs(self.tmpDir)
+    oldDir = os.getcwd()
+    os.chdir(self.tmpDir)
+    map(self.forceRemove, os.listdir(self.tmpDir))
+    os.chdir(oldDir)
 
-  def execute(self):
-    files = self.files.getFiles()
-    if echo: print 'Removing '+str(files)
-    map(self.forceRemove, files)
+  def debugListStr(self, list):
+    if (self.debugLevel > 1) or (len(list) < 4):
+      return str(list)
+    else:
+      return '['+str(list[0])+'-<'+str(len(list)-2)+'>-'+str(list[-1])+']'
+
+  def debugPrint(self, msg, level = 1, section = None):
+    indentLevel = len(traceback.extract_stack())-4
+    if self.debugLevel >= level:
+      if (not section) or (not self.debugSections) or (section in self.debugSections):
+        for i in range(indentLevel):
+          sys.stdout.write(self.debugIndent)
+        print msg
+
+  def defaultCheckCommand(self, command, status, output):
+    if status: raise RuntimeError('Could not execute \''+command+'\': '+output)
+
+  def executeShellCommand(self, command, checkCommand = None):
+    self.debugPrint('sh: '+command, 4, 'shell')
+    (status, output) = commands.getstatusoutput(command)
+    if checkCommand:
+      checkCommand(command, status, output)
+    else:
+      self.defaultCheckCommand(command, status, output)
+    return output
+
+class BS (Maker):
+  sourceDBFilename = os.path.join(os.getcwd(), 'bsSource.db')
+
+  def saveSourceDB():
+    self.debugPrint('Saving source database in '+sourceDBFilename, 2, 'sourceDB')
+    dbFile = open(sourceDBFilename, 'w')
+    cPickle.dump(sourceDB, dbFile)
+    dbFile.close()
+
+  def main():
+    self.debugPrint('Reading source database from '+sourceDBFilename, 2, 'sourceDB')
+    global sourceDB
+
+    if os.path.exists(sourceDBFilename):
+      dbFile   = open(sourceDBFilename, 'r')
+      sourceDB = cPickle.load(dbFile)
+      dbFile.close()
+    else:
+      sourceDB = {}
+    atexit.register(saveSourceDB)
