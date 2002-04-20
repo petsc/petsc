151c5173ef716d438641f8f66052ff7bef1e9555
Satish Balay <balay@mcs.anl.gov>
2002-04-20 09:38:11 -0600

    bk-changeset-1.636 balay@asterix.mcs.anl.gov|ChangeSet|20020420153811|21798 ChangeSet   1.636 02/04/20 10:38:11 balay@asterix.mcs.anl.gov +1 -0   complex instantiation calues multiple definitions on windows
      src/sys/src/objects/init.c
        1.14 02/04/20 10:38:11 balay@asterix.mcs.anl.gov +2 -0
        complex instantiation calues multiple definitions on windows
    
    
    Hg-commit: e43c276f2aab7c2dab60e5a065021c6d8fb75ad7

--- src/sys/src/objects/init.c
+++ src/sys/src/objects/init.c
@@ -29,7 +29,9 @@ MPI_Comm   PETSC_COMM_WORLD = 0;
 MPI_Comm   PETSC_COMM_SELF  = 0;
 
 #if defined(PETSC_USE_COMPLEX)
+#if !defined(PARCH_win32) /* required only for MS cl */
 template <> class std::complex<double>; /* instantiate complex template class */
+#endif
 MPI_Datatype  MPIU_COMPLEX;
 PetscScalar   PETSC_i; 
 #else
