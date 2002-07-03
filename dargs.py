b8c5f10fac914ad5e3c4e4033a02447ba2c8e68c
Barry Smith <bsmith@mcs.anl.gov>
2002-07-03 16:47:56 -0600

    bk-changeset-1.167 bsmith@terra.mcs.anl.gov|ChangeSet|20020703224756|06371 ChangeSet   1.167 02/07/03 17:47:56 bsmith@terra.mcs.anl.gov +2 -0   more complete remote dictionaries
      rargs.py
        1.9 02/07/03 17:47:56 bsmith@terra.mcs.anl.gov +19 -18
        password for creating new dictionaries
    
      dargs.py
        1.11 02/07/03 17:46:22 bsmith@terra.mcs.anl.gov +27 -9
        *** empty log message ***
    
      rargs.py
        1.8 02/07/02 23:59:58 bsmith@terra.mcs.anl.gov +1 -0
        *** empty log message ***
    
      dargs.py
        1.10 02/07/02 23:56:12 bsmith@terra.mcs.anl.gov +12 -1
        *** empty log message ***
    
      dargs.py
        1.9 02/07/02 23:52:20 bsmith@terra.mcs.anl.gov +3 -2
        *** empty log message ***
    
      rargs.py
        1.7 02/07/02 23:51:13 bsmith@terra.mcs.anl.gov +9 -1
        *** empty log message ***
    
      dargs.py
        1.8 02/07/02 23:39:08 bsmith@terra.mcs.anl.gov +6 -1
        *** empty log message ***
    
      rargs.py
        1.6 02/07/02 23:32:35 bsmith@terra.mcs.anl.gov +14 -1
        *** empty log message ***
    
      dargs.py
        1.7 02/07/02 23:13:43 bsmith@terra.mcs.anl.gov +3 -3
        *** empty log message ***
    
      rargs.py
        1.5 02/07/02 20:31:39 bsmith@terra.mcs.anl.gov +15 -2
        *** empty log message ***
    
      dargs.py
        1.6 02/07/02 20:26:01 bsmith@terra.mcs.anl.gov +7 -0
        *** empty log message ***
    
      dargs.py
        1.5 02/07/02 20:14:26 bsmith@terra.mcs.anl.gov +7 -5
        *** empty log message ***
    
      rargs.py
        1.4 02/07/02 20:14:14 bsmith@terra.mcs.anl.gov +11 -8
        *** empty log message ***
    
    
    Hg-commit: 489d0bdc7e76261381fdec2e2c24f7957a7ffe4d

--- dargs.py
+++ dargs.py
@@ -13,60 +13,82 @@ import socket
 import time
 
 class Args (UserDict.UserDict):
-  def __init__(self,name,readpw,writepw):
+  def __init__(self,name,readpw,addpw,writepw):
     UserDict.UserDict.__init__(self)
     self.name    = name
     self.readpw  = readpw
+    self.addpw   = addpw
     self.writepw = writepw
 
 class ProcessHandler(SocketServer.StreamRequestHandler):
   def handle(self):
     object  = cPickle.load(self.rfile)
-    #  all messages are of the form ("request",dict,key,readpw,writepw <,value>)
+    #  all messages are of the form ("request",dict,key,readpw,addpw,writepw <,value>)
     request = object[0]
     name    = object[1]
     key     = object[2]
     readpw  = object[3]
-    writepw = object[4]
+    dictpw  = object[4]
+    addpw   = object[5]
+    writepw = object[6]
 
-    global dargs
-    dargs.logfile.write("Received request "+request+" in "+name+" "+time.asctime(time.localtime())+'\n')
+    
+    dargs = self.server.dargs
+    dargs.logfile.write("Received "+request+" in "+name+" "+" from "+self.client_address[0]+" "+time.asctime(time.localtime())+'\n')
     dargs.logfile.flush()
     if request == "__setitem__":
       if not dargs.data.has_key(name):
-        dargs.data[name] = Args(name,readpw,writepw)
-        
-      if dargs.data[name].writepw == writepw:
-         dargs.data[name].data[key] = object[5]
+        if dargs.dictpw == dictpw:
+          dargs.data[name] = Args(name,readpw,addpw,writepw)
+        else:
+          dargs.logfile.write("Rejected, wrong dictpw\n");
+          cPickle.dump((0,None),self.wfile)
+          return
+              
+      if dargs.data[name].writepw == writepw or (dargs.data[name].addpw == addpw and not dargs.data[name].has_key(key)):
+         dargs.data[name].data[key] = object[7]
       cPickle.dump((0,None),self.wfile)
         
     elif request == "__getitem__":
       if dargs.data.has_key(name) and dargs.data[name].data.has_key(key) and dargs.data[name].readpw == readpw:
         cPickle.dump((1,dargs.data[name].data[key]),self.wfile)
       else:
+        dargs.logfile.write("Rejected, missing dictionary, key or wrong readpw\n");
         cPickle.dump((0,None),self.wfile)
 
     elif request == "has_key":
       if dargs.data.has_key(name) and dargs.data[name].data.has_key(key) and dargs.data[name].readpw == readpw:
         cPickle.dump((1,None),self.wfile)
       else:
+        dargs.logfile.write("Rejected, missing dictionary, key or wrong readpw\n");
         cPickle.dump((0,None),self.wfile)
 
     elif request == "__len__":
       if dargs.data.has_key(name) and dargs.data[name].readpw == readpw:
         cPickle.dump((len(dargs.data[name].data),None),self.wfile)
       else:
+        dargs.logfile.write("Rejected, missing dictionary, or wrong readpw\n");
         cPickle.dump((0,None),self.wfile)
 
     elif request == "keys":
       if dargs.data.has_key(name) and dargs.data[name].readpw == readpw:
         cPickle.dump((1,dargs.data[name].data.keys()),self.wfile)
       else:
+        dargs.logfile.write("Rejected, missing dictionary, or wrong readpw\n");
         cPickle.dump((0,None),self.wfile)
+
+    elif request == "dicts":
+      di = []
+      for d in dargs.data.keys():
+        if dargs.data[d].readpw == readpw:
+          di.append(d)
+      cPickle.dump((0,tuple(di)),self.wfile)
 
     elif request == "clear":
       if dargs.data.has_key(name) and dargs.data[name].writepw == writepw:
         dargs.data[name].data.clear()
+      else:
+        dargs.logfile.write("Rejected, missing dictionary, wrong writepw\n");
       cPickle.dump((0,None),self.wfile)
 
     elif request == "__delitem__":
@@ -75,15 +97,20 @@ class ProcessHandler(SocketServer.StreamRequestHandler):
           try:
             del dargs.data[name].data[key]
           except:
-            pass
+            dargs.logfile.write("Rejected, missing key\n");
+        else:
+          dargs.logfile.write("Rejected, wrong writepw\n");
+      else:
+        dargs.logfile.write("Rejected, missing dictionary\n");
       cPickle.dump((0,None),self.wfile)
 
 class DArgs:
-  def __init__(self, filename = "DArgs.db"):
+  def __init__(self, filename = "DArgs.db", dictpw = "open"):
     self.data      = UserDict.UserDict()
     self.filename  = filename
     self.load(filename)
-    self.logfile   = open("DArgs.log",'w')
+    self.dictpw    = dictpw
+    self.logfile   = open("DArgs.log",'a')
     atexit.register(self.save)
 
   def load(self, filename):
@@ -96,15 +123,32 @@ class DArgs:
     dbFile = open(self.filename, 'w')
     cPickle.dump(self.data, dbFile)
     dbFile.close()
+    try: os.unlink('DArgs.loc')
+    except: pass
 
   def loop(self):
-    server = SocketServer.TCPServer(("terra.mcs.anl.gov",6001),ProcessHandler)
+    # wish there was a better way to get a usable socket
+    p    = 1
+    flag = "nosocket"
+    while p < 1000 and flag == "nosocket":
+      try:
+        server = SocketServer.TCPServer((socket.gethostname(),6000+p),ProcessHandler)
+        flag   = "socket"
+      except:
+        p = p + 1
+    if p == 1000:
+      raise RuntimeError,"Cannot get available socket"
+        
+    f = open("DArgs.loc", 'w')
+    cPickle.dump(server.server_address, f)
+    f.close()
+
     self.logfile.write("Started server"+time.asctime(time.localtime())+'\n')
     self.logfile.flush()
+    server.dargs = self
     server.serve_forever()
     
 
 if __name__ ==  '__main__':
-    global dargs
     dargs = DArgs()
     dargs.loop()
--- rargs.py
+++ rargs.py
@@ -11,21 +11,38 @@ import readline
 import socket
 
 class RArgs (UserDict.UserDict):
-  def __init__(self,name,readpw = "open",writepw = "open"):
+  def __init__(self,name = "default",readpw = "open",dictpw = "open",addpw = "open",writepw = "open"):
     UserDict.UserDict.__init__(self)
     self.name    = name
+    if dictpw  == "open": dictpw  = readpw
+    if addpw   == "open": addpw   = dict
+    if writepw == "open": writepw = addpw
     self.readpw  = readpw
+    self.dictpw  = dictpw
+    self.addpw   = addpw
     self.writepw = writepw
+    self.addr    = self.getServerAddr()
     
+  def getServerAddr(self):
+    import dargs
+    filename = os.path.join(os.path.dirname(sys.modules['dargs'].__file__), 'DArgs.loc')
+    if os.path.exists(filename):
+      f    = open(filename, 'r')
+      addr = cPickle.load(f)
+      f.close()
+    else:
+     raise RuntimeError,"No running server"
+    return addr
+
   def __setitem__(self,key,value):
     try:
-      self.send(("__setitem__",self.name,key,self.readpw,self.writepw,value))
+      self.send(("__setitem__",self.name,key,self.readpw,self.dictpw,self.addpw,self.writepw,value))
     except:
       pass
     
   def __getitem__(self, key):
     try:
-      obj = self.send(("__getitem__",self.name,key,self.readpw,self.writepw))
+      obj = self.send(("__getitem__",self.name,key,self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
     if obj[0] == 1:
@@ -35,13 +52,13 @@ class RArgs (UserDict.UserDict):
     
   def __delitem__(self, key):
     try:
-      obj = self.send(("__delitem__",self.name,key,self.readpw,self.writepw))
+      obj = self.send(("__delitem__",self.name,key,self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
 
   def has_key(self, key):
     try:
-      obj = self.send(("has_key",self.name,key,self.readpw,self.writepw))
+      obj = self.send(("has_key",self.name,key,self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
     if obj[0] == 1:
@@ -51,20 +68,27 @@ class RArgs (UserDict.UserDict):
 
   def clear(self):
     try:
-      obj = self.send(("clear",self.name,"dummykey",self.readpw,self.writepw))
+      obj = self.send(("clear",self.name,"dummykey",self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
 
   def keys(self):
     try:
-      obj = self.send(("keys",self.name,"dummykey",self.readpw,self.writepw))
+      obj = self.send(("keys",self.name,"dummykey",self.readpw,self.dictpw,self.addpw,self.writepw))
+    except:
+      pass
+    return obj[1]
+
+  def dicts(self):
+    try:
+      obj = self.send(("dicts",self.name,"dummykey",self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
     return obj[1]
 
   def __len__(self):
     try:
-      obj = self.send(("__len__",self.name,"dummykey",self.readpw,self.writepw))
+      obj = self.send(("__len__",self.name,"dummykey",self.readpw,self.dictpw,self.addpw,self.writepw))
     except:
       pass
     return obj[0]
@@ -72,7 +96,16 @@ class RArgs (UserDict.UserDict):
 
   def send(self,object):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("terra.mcs.anl.gov",6001))
+    # if the file DArgs.loc exists but no server is running we are screwed
+    try:
+      s.connect(self.addr)
+    except:
+      self.addr = self.getServerAddr()
+      try:
+        s.connect(self.addr)
+      except:
+        raise RuntimeError,"Cannot connect to server"
+              
     f = s.makefile("w")
     cPickle.dump(object,f)
     f.close()
@@ -94,5 +127,11 @@ if __name__ ==  '__main__':
     A[22] = 'newlow'
     print A[22]
     print A.keys()
-    A.clear()
     print len(A)
+    print A.dicts()
+    for d in RArgs().dicts():
+      b = RArgs(d)
+      for k in b.keys():
+        print d+" "+str(k)+" "+str(b[k])
+    A.clear()
+        
