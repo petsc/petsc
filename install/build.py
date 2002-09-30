da26b32c21e30d022109e77c87b42372d36271b4
Matthew Knepley <knepley@mcs.anl.gov>
2002-09-30 12:56:34 -0600

    bk-changeset-1.293 knepley@khan.mcs.anl.gov|ChangeSet|20020930185634|45899 ChangeSet   1.293 02/09/30 13:56:34 knepley@khan.mcs.anl.gov +1 -0   Fixing remote compilation   Changed some debug levels
      install/build.py
        1.10 02/09/30 13:56:34 knepley@khan.mcs.anl.gov +12 -6
        Fixing remote compilation
        Changed some debug levels
    
    
    Hg-commit: 0e4cd0effd508c9288ca5916f4b026d5363e90cd

--- install/build.py
+++ install/build.py
@@ -11,20 +11,26 @@ class Builder(install.base.Base):
     return
 
   def build(self, root, target = 'default'):
-    self.debugPrint('Building in '+root, 3, 'install')
+    self.debugPrint('Building in '+root, 1, 'install')
     maker = self.getMakeModule(root).PetscMake(sys.argv[1:])
     root  = maker.getRoot()
     for url in maker.executeTarget('getDependencies'):
-      self.debugPrint('  Building dependency '+url, 3, 'install')
+      self.debugPrint('  Building dependency '+url, 2, 'install')
       self.build(self.retriever.retrieve(url), target = target)
     # Check for remote compile
     try:
       import SIDL.Loader
       import SIDLLanguage.Parser
     except ImportError:
+      # First compile only the sidl
+      self.debugPrint('Compiling SIDL in '+root, 3, 'install')
+      maker.main('sidl')
       # We cannot execute splicing correctly, so we just revert the files
-      self.debugPrint('Reverting in '+root, 3, 'install')
-      output = self.executeShellCommand('cd '+root+'; bk sfiles -lg | bk unedit -')
-      output = self.executeShellCommand('cd '+root+'; bk -r co -q')
-    self.debugPrint('Compiling in '+root, 3, 'install')
+      try:
+        self.debugPrint('Reverting in '+root, 3, 'install')
+        output = self.executeShellCommand('cd '+root+'; bk sfiles -lg | bk unedit -')
+        output = self.executeShellCommand('cd '+root+'; bk -r co -q')
+      except RuntimeError:
+        self.debugPrint('  Got correct splice blocks in '+root, 3, 'install')
+    self.debugPrint('Compiling in '+root, 2, 'install')
     return maker.main(target)
