f4bc1b680048059ff1fe256f2ea2b839aa8229e3
Matthew Knepley <knepley@mcs.anl.gov>
2002-09-30 12:25:13 -0600

    bk-changeset-1.292 knepley@khan.mcs.anl.gov|ChangeSet|20020930182513|45893 ChangeSet   1.292 02/09/30 13:25:13 knepley@khan.mcs.anl.gov +1 -0   Fixup splicing for remote compilation
      install/build.py
        1.9 02/09/30 13:25:13 knepley@khan.mcs.anl.gov +11 -1
        Fixup splicing for remote compilation
    
    
    Hg-commit: e9be38e1ec5d921e456087111d1a1e3761d3ded5

--- install/build.py
+++ install/build.py
@@ -13,8 +13,18 @@ class Builder(install.base.Base):
   def build(self, root, target = 'default'):
     self.debugPrint('Building in '+root, 3, 'install')
     maker = self.getMakeModule(root).PetscMake(sys.argv[1:])
+    root  = maker.getRoot()
     for url in maker.executeTarget('getDependencies'):
       self.debugPrint('  Building dependency '+url, 3, 'install')
       self.build(self.retriever.retrieve(url), target = target)
-    self.debugPrint('Compiling in '+maker.getRoot(), 3, 'install')
+    # Check for remote compile
+    try:
+      import SIDL.Loader
+      import SIDLLanguage.Parser
+    except ImportError:
+      # We cannot execute splicing correctly, so we just revert the files
+      self.debugPrint('Reverting in '+root, 3, 'install')
+      output = self.executeShellCommand('cd '+root+'; bk sfiles -lg | bk unedit -')
+      output = self.executeShellCommand('cd '+root+'; bk -r co -q')
+    self.debugPrint('Compiling in '+root, 3, 'install')
     return maker.main(target)
