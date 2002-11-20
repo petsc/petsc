32d3ee782904f00fa9f76cb47900ce252bc46008
Matthew Knepley <knepley@mcs.anl.gov>
2002-11-20 08:24:08 -0600

    bk-changeset-1.331 knepley@khan.mcs.anl.gov|ChangeSet|20021120142408|49006 ChangeSet   1.331 02/11/20 08:24:08 knepley@khan.mcs.anl.gov +2 -0   Configure features
      config/libraries.py
        1.2 02/11/20 08:24:08 knepley@khan.mcs.anl.gov +20 -6
        Allow check() to take lists of both libraries and directories
    
      config/framework.py
        1.10 02/11/20 08:24:08 knepley@khan.mcs.anl.gov +1 -1
        Have log file overwritten
    
    
    Hg-commit: fb113bfc11d171556e7cc720fb8040c9e1d6726a

--- config/libraries.py
+++ config/libraries.py
@@ -18,33 +18,47 @@ class Configure(config.base.Configure):
   def haveLib(self, libName):
     return self.defines.has_key(self.getDefineName(libName))
 
-  def check(self, libName, funcName, libDir = '', otherLibs = '', prototype = '', call = '', fortranMangle = 0):
-    '''Checks that the library "libName" contains "funcName", and if it does adds "libName" to $LIBS and defines HAVE_LIB"libName"'''
+  def check(self, libName, funcName, libDir = None, otherLibs = '', prototype = '', call = '', fortranMangle = 0):
+    '''Checks that the library "libName" contains "funcName", and if it does adds "libName" to $LIBS and defines HAVE_LIB"libName"
+       - libDir may be a list of directories
+       - libName may be a list of library names'''
+    # Handle Fortran mangling
     if fortranMangle:
       funcName = self.compilers.mangleFortranFunction(funcName)
     includes = '/* Override any gcc2 internal prototype to avoid an error. */\n'
+    # Handle C++ mangling
     if self.language[-1] == 'C++':
       includes += '''
       #ifdef __cplusplus
       extern "C"
       #endif'''
+    # Construct prototype
     if prototype:
       includes += prototype
     else:
       includes += '/* We use char because int might match the return type of a gcc2 builtin and then its argument prototype would still apply. */\n'
       includes += 'char '+funcName+'();\n'
+    # Construct function call
     if call:
       body = call
     else:
       body = funcName+'()\n'
+    # Setup link line
     oldLibs = self.framework.argDB['LIBS']
-    if libDir: self.framework.argDB['LIBS'] += ' -L'+libDir
-    self.framework.argDB['LIBS'] += ' -l'+libName+' '+otherLibs
+    if libDir:
+      if not isinstance(libDir, list): libDir = [libDir]
+      for dir in libDir:
+        self.framework.argDB['LIBS'] += ' -L'+dir
+    if not isinstance(libName, list): libName = [libName]
+    for lib in libName:
+      self.framework.argDB['LIBS'] += ' -l'+lib
+    self.framework.argDB['LIBS'] += ' '+otherLibs
     self.pushLanguage(self.language[-1])
     if self.checkLink(includes, body):
       found = 1
-      self.framework.argDB['LIBS'] = oldLibs+' -l'+libName
-      self.addDefine(self.getDefineName(libName), 1)
+      for lib in libName:
+        self.framework.argDB['LIBS'] = oldLibs+' -l'+lib
+        self.addDefine(self.getDefineName(lib), 1)
     else:
       found = 0
       self.framework.argDB['LIBS'] = oldLibs
