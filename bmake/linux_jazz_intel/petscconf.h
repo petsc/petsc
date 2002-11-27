5ea4c517176767516853cf7412d466c2b19004d1
Satish Balay <balay@mcs.anl.gov>
2002-11-27 14:58:41 -0600

    bk-changeset-1.888 balay@asterix.mcs.anl.gov|ChangeSet|20021127205841|33570 ChangeSet   1.888 02/11/27 14:58:41 balay@asterix.mcs.anl.gov +5 -0   add in a PETSc arch for using intel compilers on jazz
      bmake/linux_jazz_intel/variables
        1.1 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +78 -0
    
      bmake/linux_jazz_intel/rules
        1.1 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +57 -0
    
      bmake/linux_jazz_intel/petscfix.h
        1.1 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +30 -0
    
      bmake/linux_jazz_intel/variables
        1.0 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/linux_jazz_intel/variables
    
      bmake/linux_jazz_intel/rules
        1.0 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/linux_jazz_intel/rules
    
      bmake/linux_jazz_intel/petscfix.h
        1.0 02/11/27 14:58:39 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/linux_jazz_intel/petscfix.h
    
      bmake/linux_jazz_intel/petscconf.h
        1.1 02/11/27 14:58:38 balay@asterix.mcs.anl.gov +59 -0
    
      bmake/linux_jazz_intel/packages
        1.1 02/11/27 14:58:38 balay@asterix.mcs.anl.gov +32 -0
    
      bmake/linux_jazz_intel/petscconf.h
        1.0 02/11/27 14:58:38 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/linux_jazz_intel/petscconf.h
    
      bmake/linux_jazz_intel/packages
        1.0 02/11/27 14:58:38 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/linux_jazz_intel/packages
    
    
    Hg-commit: b77dde945a02b9cc569f795bdbb2f70aa2decdef

---
+++ bmake/linux_jazz_intel/petscconf.h
@@ -0,0 +1,59 @@
+#ifdef PETSC_RCS_HEADER
+"$Id: petscconf.h,v 1.2 2001/06/20 21:11:25 buschelm Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if !defined(INCLUDED_PETSCCONF_H)
+#define INCLUDED_PETSCCONF_H
+
+#define PARCH_linux
+#define PETSC_ARCH_NAME "linux"
+
+#define PETSC_HAVE_POPEN
+#define PETSC_HAVE_LIMITS_H
+#define PETSC_HAVE_PWD_H 
+#define PETSC_HAVE_MALLOC_H 
+#define PETSC_HAVE_STRING_H 
+#define PETSC_HAVE_GETDOMAINNAME
+#define PETSC_HAVE_DRAND48 
+#define PETSC_HAVE_UNAME 
+#define PETSC_HAVE_UNISTD_H 
+#define PETSC_HAVE_SYS_TIME_H 
+#define PETSC_HAVE_STDLIB_H
+#define PETSC_HAVE_GETCWD
+#define PETSC_HAVE_SLEEP
+#define PETSC_HAVE_SYS_PARAM_H
+#define PETSC_HAVE_SYS_STAT_H
+
+#define PETSC_HAVE_FORTRAN_UNDERSCORE 
+
+#define PETSC_HAVE_READLINK
+#define PETSC_HAVE_MEMMOVE
+
+#define PETSC_HAVE_DOUBLE_ALIGN_MALLOC
+#define PETSC_HAVE_MEMALIGN
+#define PETSC_HAVE_SYS_RESOURCE_H
+#define PETSC_SIZEOF_VOID_P 4
+#define PETSC_SIZEOF_INT 4
+#define PETSC_SIZEOF_DOUBLE 8
+#define PETSC_BITS_PER_BYTE 8
+#define PETSC_SIZEOF_FLOAT 4
+#define PETSC_SIZEOF_LONG 4
+#define PETSC_SIZEOF_LONG_LONG 8
+
+#define PETSC_HAVE_RTLD_GLOBAL 1
+
+#define PETSC_HAVE_SYS_UTSNAME_H
+
+#define PETSC_MISSING_SIGSYS
+
+#ifdef PETSC_USE_MAT_SINGLE
+#  define PETSC_MEMALIGN 16
+#  define PETSC_HAVE_SSE "iclsse.h"
+#endif
+#define PETSC_HAVE_CXX_NAMESPACE
+
+#define PETSC_HAVE_F90_H "f90impl/f90_intel.h"
+#define PETSC_HAVE_F90_C "src/sys/src/f90/f90_intel.c"
+
+#endif
