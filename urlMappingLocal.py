17b5f0b407944b0de47193eb4024d6db99512e0b
Matthew Knepley <knepley@mcs.anl.gov>
2002-12-18 14:32:43 -0600

    bk-changeset-1.382 knepley@khan.mcs.anl.gov|ChangeSet|20021218203243|64960 ChangeSet   1.382 02/12/18 14:32:43 knepley@khan.mcs.anl.gov +1 -0   Added a URL mapping module which just bootstraps from /tmp    - This is intended as a template for users
      urlMappingLocal.py
        1.1 02/12/18 14:32:03 knepley@khan.mcs.anl.gov +18 -0
    
      urlMappingLocal.py
        1.0 02/12/18 14:32:03 knepley@khan.mcs.anl.gov +0 -0
        BitKeeper file /home/knepley/PETSc-new/testing/BuildSystem/urlMappingLocal.py
    
    
    Hg-commit: 67d1ce24527977371f349f19fdd3dfdeb45e6175

---
+++ urlMappingLocal.py
@@ -0,0 +1,18 @@
+import install.base
+
+import urlparse
+# Fix parsing for nonstandard schemes
+urlparse.uses_netloc.extend(['bk', 'ssh'])
+
+def bootstrapUrlMap(self, url):
+  import os
+  if self.checkBootstrap():
+    (scheme, location, path, parameters, query, fragment) = urlparse.urlparse(url)
+    if scheme == 'bk':
+      path = os.path.join('/tmp', self.getRepositoryPath(url))
+      return (1, urlparse.urlunparse(('file', '', path, parameters, query, fragment)))
+  return (0, url)
+
+def setupUrlMapping(self, urlMaps):
+  urlMaps.insert(0, lambda url, self = self: bootstrapUrlMap(self, url))
+  return
