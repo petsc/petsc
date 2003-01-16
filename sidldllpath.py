7e52d124219fa17efb821e69780df335efd8ef81
Barry Smith <bsmith@mcs.anl.gov>
2003-01-16 16:01:27 -0600

    bk-changeset-1.397.1.1 bsmith@terra.mcs.anl.gov|ChangeSet|20030116220127|60144 ChangeSet   1.397.1.1 03/01/16 16:01:27 bsmith@terra.mcs.anl.gov +2 -0   prevent duplicate entries in Matlab path
      sidldllpath.py
        1.5 03/01/15 20:11:05 bsmith@terra.mcs.anl.gov +1 -1
        bug fix
    
      sidldllpath.py
        1.4 03/01/15 19:57:16 bsmith@terra.mcs.anl.gov +7 -9
        prevent duplicate entries
    
      matlabpath.py
        1.4 03/01/15 19:17:40 bsmith@terra.mcs.anl.gov +10 -9
        prevent duplicate entries in Matlab path
    
    
    Hg-commit: 11996ae1287361f8b469a352a6e06ac325353535

--- sidldllpath.py
+++ sidldllpath.py
@@ -4,24 +4,22 @@ import os
 import sys
 import nargs
 import project
-
+import string
 
 def getSIDLDLLPath():
   if os.environ.has_key('SIDL_DLL_PATH'):
-    SIDL_DLL_PATH = os.environ['SIDL_DLL_PATH']
+    SIDL_DLL_PATH = os.environ['SIDL_DLL_PATH'].split(';')
   else:  
-    SIDL_DLL_PATH = '' 
+    SIDL_DLL_PATH = [] 
   argsDB = nargs.ArgDict('ArgDict')
   projects = argsDB['installedprojects']
   for p in projects:
     try:
-      root = p.getRoot()
-      if SIDL_DLL_PATH:
-        SIDL_DLL_PATH += ';' + root+'/lib'
-      else:
-        SIDL_DLL_PATH = root+'/lib'
+      root = p.getRoot()+'/lib'
+      if not root in SIDL_DLL_PATH:
+        SIDL_DLL_PATH += [root]
     except: pass
-  return SIDL_DLL_PATH  
+  return string.join(SIDL_DLL_PATH,';')
     
 if __name__ ==  '__main__':
   if len(sys.argv) > 1: sys.exit('Usage: sidldllpath.py')
