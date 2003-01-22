1f970b2ef59a77364be49a721f19a81fbe841863
Satish Balay <balay@mcs.anl.gov>
2003-01-22 17:37:57 -0600

    bk-changeset-1.973 balay@asterix.mcs.anl.gov|ChangeSet|20030122233757|35827 ChangeSet   1.973 03/01/22 17:37:57 balay@asterix.mcs.anl.gov +2 -0   add in shared targets for configure arches   fix IRIX shared target to use C_CC instead of LD - this way don't need to configure LD
      config/configure_solaris2.9.py
        1.1 03/01/22 17:37:55 balay@asterix.mcs.anl.gov +22 -0
    
      config/configure_solaris2.9.py
        1.0 03/01/22 17:37:55 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/config/configure_solaris2.9.py
    
      bmake/common/rules
        1.48 03/01/22 17:37:55 balay@asterix.mcs.anl.gov +8 -1
        add in shared targets for configure-arches
    
    
    Hg-commit: df98ef9ec78a639d19cbf43e4ffc0e7b373f26f0

---
+++ config/configure_solaris2.9.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    import configure
+
+    # build on harley
+    configure_options = [
+    '--with-cc=cc',
+    '--with-fc=f90',
+    '--with-cxx=CC',
+    '--with-f90-header=f90impl/f90_solaris.h',
+    '--with-f90-source=src/sys/src/f90/f90_solaris.c',
+    '--with-mpi',
+    '--with-mpi-dir=/home/petsc/soft/solaris-9/mpich-1.2.5'
+    '-PETSC_ARCH=solaris2.9',
+    '-PETSC_DIR=/sandbox/petsc/petsc-test',
+    '--with-blas=libsunperf.a'
+    ]
+
+    configure.petsc_configure(configure_options)
