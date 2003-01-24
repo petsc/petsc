136f8426b406c0f8208c1b1a9c97c27c87a91a1f
Satish Balay <balay@mcs.anl.gov>
2003-01-24 11:14:39 -0600

    bk-changeset-1.978 balay@asterix.mcs.anl.gov|ChangeSet|20030124171439|36249 ChangeSet   1.978 03/01/24 11:14:39 balay@asterix.mcs.anl.gov +1 -0   update solaris configure buid
      config/configure_solaris2.9.py
        1.2 03/01/24 11:14:37 balay@asterix.mcs.anl.gov +4 -2
        update solaris configure buid
    
    
    Hg-commit: e6f9b91d737734b4c4ad4d9c8abcd2e371770b8c

--- config/configure_solaris2.9.py
+++ config/configure_solaris2.9.py
@@ -13,10 +13,12 @@ if __name__ == '__main__':
     '--with-f90-header=f90impl/f90_solaris.h',
     '--with-f90-source=src/sys/src/f90/f90_solaris.c',
     '--with-mpi',
-    '--with-mpi-dir=/home/petsc/soft/solaris-9/mpich-1.2.5'
+    '--with-mpi-include=/home/petsc/soft/solaris-9/mpich-1.2.5/include',
+    '--with-mpi-lib=[/home/petsc/soft/solaris-9/mpich-1.2.5/lib/libmpich.a,libsocket.a,libnsl.a,librt.a,libnsl.a,libaio.a]',
     '-PETSC_ARCH=solaris2.9',
     '-PETSC_DIR=/sandbox/petsc/petsc-test',
-    '--with-blas=libsunperf.a'
+    '--with-blas=libsunperf.a',
+    '--with-lapack=libsunperf.a'
     ]
 
     configure.petsc_configure(configure_options)
