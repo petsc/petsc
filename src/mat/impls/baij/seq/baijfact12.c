7cf1b8d3bee2bb5f125300895b691a95a99b6229
Kris Buschelman <buschelm@mcs.anl.gov>
2003-02-28 16:06:31 -0600

    bk-changeset-1.1039.1.1 Kris@buschel2000.|ChangeSet|20030228220631|41490 ChangeSet   1.1039.1.1 03/02/28 14:06:31 Kris@buschel2000. +2 -0   Restore unscaled Factor and Solve routines with int j indices.
      src/mat/impls/baij/seq/baijfact2.c
        1.21 03/02/28 14:06:27 Kris@buschel2000. +227 -10
        Restore unscaled Factor and Solve routines with int j indices.
    
      src/mat/impls/baij/seq/baijfact12.c
        1.13 03/02/28 14:06:27 Kris@buschel2000. +437 -0
        Restore unscaled Factor and Solve routines with int j indices.
    
    
    Hg-commit: 9f9c93c2565e35f26a3707a6ca63736dbe7fb5bc

--- src/mat/impls/baij/seq/baijfact12.c
+++ src/mat/impls/baij/seq/baijfact12.c
@@ -157,6 +157,443 @@ int MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering(Mat A,Mat *B)
 #define __FUNCT__ "MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_SSE"
 int MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_SSE(Mat A,Mat *B)
 {
+  Mat         C = *B;
+  Mat_SeqBAIJ *a = (Mat_SeqBAIJ*)A->data,*b = (Mat_SeqBAIJ*)C->data;
+  int         ierr,i,j,n = a->mbs;
+  int         *bj = b->j,*bjtmp,*pj;
+  int         row;
+  int         *ajtmpold,nz,*bi=b->i;
+  int         *diag_offset = b->diag,*ai=a->i,*aj=a->j;
+  MatScalar   *pv,*v,*rtmp,*pc,*w,*x;
+  MatScalar   *ba = b->a,*aa = a->a;
+  int         nonzero=0;
+/*    int            nonzero=0,colscale = 16; */
+  PetscTruth  pivotinblocks = b->pivotinblocks;
+
+  PetscFunctionBegin;
+  SSE_SCOPE_BEGIN;
+
+  if ((unsigned long)aa%16!=0) SETERRQ(PETSC_ERR_ARG_BADPTR,"Pointer aa is not 16 byte aligned.  SSE will not work.");
+  if ((unsigned long)ba%16!=0) SETERRQ(PETSC_ERR_ARG_BADPTR,"Pointer ba is not 16 byte aligned.  SSE will not work.");
+  ierr = PetscMalloc(16*(n+1)*sizeof(MatScalar),&rtmp);CHKERRQ(ierr);
+  if ((unsigned long)rtmp%16!=0) SETERRQ(PETSC_ERR_ARG_BADPTR,"Pointer rtmp is not 16 byte aligned.  SSE will not work.");
+/*    if ((unsigned long)bj==(unsigned long)aj) { */
+/*      colscale = 4; */
+/*    } */
+  for (i=0; i<n; i++) {
+    nz    = bi[i+1] - bi[i];
+    bjtmp = bj + bi[i];
+    /* zero out the 4x4 block accumulators */
+    /* zero out one register */ 
+    XOR_PS(XMM7,XMM7);
+    for  (j=0; j<nz; j++) {
+      x = rtmp+16*bjtmp[j];
+/*        x = rtmp+4*bjtmp[j]; */
+      SSE_INLINE_BEGIN_1(x)
+        /* Copy zero register to memory locations */
+        /* Note: on future SSE architectures, STORE might be more efficient than STOREL/H */
+        SSE_STOREL_PS(SSE_ARG_1,FLOAT_0,XMM7)
+        SSE_STOREH_PS(SSE_ARG_1,FLOAT_2,XMM7)
+        SSE_STOREL_PS(SSE_ARG_1,FLOAT_4,XMM7)
+        SSE_STOREH_PS(SSE_ARG_1,FLOAT_6,XMM7)
+        SSE_STOREL_PS(SSE_ARG_1,FLOAT_8,XMM7)
+        SSE_STOREH_PS(SSE_ARG_1,FLOAT_10,XMM7)
+        SSE_STOREL_PS(SSE_ARG_1,FLOAT_12,XMM7)
+        SSE_STOREH_PS(SSE_ARG_1,FLOAT_14,XMM7)
+      SSE_INLINE_END_1;
+    }
+    /* load in initial (unfactored row) */
+    nz       = ai[i+1] - ai[i];
+    ajtmpold = aj + ai[i];
+    v        = aa + 16*ai[i];
+    for (j=0; j<nz; j++) {
+      x = rtmp+16*ajtmpold[j];
+/*        x = rtmp+colscale*ajtmpold[j]; */
+      /* Copy v block into x block */ 
+      SSE_INLINE_BEGIN_2(v,x)
+        /* Note: on future SSE architectures, STORE might be more efficient than STOREL/H */
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_0,XMM0)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_0,XMM0)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_2,XMM1)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_2,XMM1)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_4,XMM2)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_4,XMM2)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_6,XMM3)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_6,XMM3)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_8,XMM4)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_8,XMM4)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_10,XMM5)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_10,XMM5)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_12,XMM6)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_12,XMM6)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_14,XMM0)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_14,XMM0)
+      SSE_INLINE_END_2;
+
+      v += 16;
+    }
+/*      row = (*bjtmp++)/4; */
+    row = *bjtmp++;
+    while (row < i) {
+      pc  = rtmp + 16*row;
+      SSE_INLINE_BEGIN_1(pc)
+        /* Load block from lower triangle */ 
+        /* Note: on future SSE architectures, STORE might be more efficient than STOREL/H */
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_0,XMM0)
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_2,XMM0)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_4,XMM1)
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_6,XMM1)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_8,XMM2)
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_10,XMM2)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_12,XMM3)
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_14,XMM3)
+
+        /* Compare block to zero block */ 
+
+        SSE_COPY_PS(XMM4,XMM7)
+        SSE_CMPNEQ_PS(XMM4,XMM0)
+
+        SSE_COPY_PS(XMM5,XMM7)
+        SSE_CMPNEQ_PS(XMM5,XMM1)
+
+        SSE_COPY_PS(XMM6,XMM7)
+        SSE_CMPNEQ_PS(XMM6,XMM2)
+
+        SSE_CMPNEQ_PS(XMM7,XMM3)
+
+        /* Reduce the comparisons to one SSE register */
+        SSE_OR_PS(XMM6,XMM7)
+        SSE_OR_PS(XMM5,XMM4)
+        SSE_OR_PS(XMM5,XMM6)
+      SSE_INLINE_END_1;
+
+      /* Reduce the one SSE register to an integer register for branching */
+      /* Note: Since nonzero is an int, there is no INLINE block version of this call */
+      MOVEMASK(nonzero,XMM5);
+
+      /* If block is nonzero ... */
+      if (nonzero) {
+        pv = ba + 16*diag_offset[row];
+        PREFETCH_L1(&pv[16]);
+        pj = bj + diag_offset[row] + 1;
+
+        /* Form Multiplier, one column at a time (Matrix-Matrix Product) */
+        /* L_ij^(k+1) = L_ij^(k)*inv(L_jj^(k)) */ 
+        /* but the diagonal was inverted already */
+        /* and, L_ij^(k) is already loaded into registers XMM0-XMM3 columnwise */
+
+        SSE_INLINE_BEGIN_2(pv,pc)
+          /* Column 0, product is accumulated in XMM4 */ 
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_0,XMM4)
+          SSE_SHUFFLE(XMM4,XMM4,0x00)
+          SSE_MULT_PS(XMM4,XMM0)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_1,XMM5)
+          SSE_SHUFFLE(XMM5,XMM5,0x00)
+          SSE_MULT_PS(XMM5,XMM1)
+          SSE_ADD_PS(XMM4,XMM5)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_2,XMM6)
+          SSE_SHUFFLE(XMM6,XMM6,0x00)
+          SSE_MULT_PS(XMM6,XMM2)
+          SSE_ADD_PS(XMM4,XMM6)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_3,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM7,XMM3)
+          SSE_ADD_PS(XMM4,XMM7)
+
+          SSE_STOREL_PS(SSE_ARG_2,FLOAT_0,XMM4)
+          SSE_STOREH_PS(SSE_ARG_2,FLOAT_2,XMM4)
+
+          /* Column 1, product is accumulated in XMM5 */
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_4,XMM5)
+          SSE_SHUFFLE(XMM5,XMM5,0x00)
+          SSE_MULT_PS(XMM5,XMM0)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_5,XMM6)
+          SSE_SHUFFLE(XMM6,XMM6,0x00)
+          SSE_MULT_PS(XMM6,XMM1)
+          SSE_ADD_PS(XMM5,XMM6)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_6,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM7,XMM2)
+          SSE_ADD_PS(XMM5,XMM7)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_7,XMM6)
+          SSE_SHUFFLE(XMM6,XMM6,0x00)
+          SSE_MULT_PS(XMM6,XMM3)
+          SSE_ADD_PS(XMM5,XMM6)
+
+          SSE_STOREL_PS(SSE_ARG_2,FLOAT_4,XMM5)
+          SSE_STOREH_PS(SSE_ARG_2,FLOAT_6,XMM5)
+
+          SSE_PREFETCH_L1(SSE_ARG_1,FLOAT_24)
+
+          /* Column 2, product is accumulated in XMM6 */
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_8,XMM6)
+          SSE_SHUFFLE(XMM6,XMM6,0x00)
+          SSE_MULT_PS(XMM6,XMM0)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_9,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM7,XMM1)
+          SSE_ADD_PS(XMM6,XMM7)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_10,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM7,XMM2)
+          SSE_ADD_PS(XMM6,XMM7)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_11,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM7,XMM3)
+          SSE_ADD_PS(XMM6,XMM7)
+  
+          SSE_STOREL_PS(SSE_ARG_2,FLOAT_8,XMM6)
+          SSE_STOREH_PS(SSE_ARG_2,FLOAT_10,XMM6)
+
+          /* Note: For the last column, we no longer need to preserve XMM0->XMM3 */
+          /* Column 3, product is accumulated in XMM0 */
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_12,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM0,XMM7)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_13,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM1,XMM7)
+          SSE_ADD_PS(XMM0,XMM1)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_14,XMM1)
+          SSE_SHUFFLE(XMM1,XMM1,0x00)
+          SSE_MULT_PS(XMM1,XMM2)
+          SSE_ADD_PS(XMM0,XMM1)
+
+          SSE_LOAD_SS(SSE_ARG_1,FLOAT_15,XMM7)
+          SSE_SHUFFLE(XMM7,XMM7,0x00)
+          SSE_MULT_PS(XMM3,XMM7)
+          SSE_ADD_PS(XMM0,XMM3)
+
+          SSE_STOREL_PS(SSE_ARG_2,FLOAT_12,XMM0)
+          SSE_STOREH_PS(SSE_ARG_2,FLOAT_14,XMM0)
+
+          /* Simplify Bookkeeping -- Completely Unnecessary Instructions */
+          /* This is code to be maintained and read by humans afterall. */
+          /* Copy Multiplier Col 3 into XMM3 */
+          SSE_COPY_PS(XMM3,XMM0) 
+          /* Copy Multiplier Col 2 into XMM2 */
+          SSE_COPY_PS(XMM2,XMM6)
+          /* Copy Multiplier Col 1 into XMM1 */
+          SSE_COPY_PS(XMM1,XMM5)
+          /* Copy Multiplier Col 0 into XMM0 */
+          SSE_COPY_PS(XMM0,XMM4)
+        SSE_INLINE_END_2;
+
+        /* Update the row: */
+        nz = bi[row+1] - diag_offset[row] - 1;
+        pv += 16;
+        for (j=0; j<nz; j++) {
+          PREFETCH_L1(&pv[16]);
+          x = rtmp + 16*pj[j];
+/*            x = rtmp + 4*pj[j]; */
+
+          /* X:=X-M*PV, One column at a time */
+          /* Note: M is already loaded columnwise into registers XMM0-XMM3 */
+          SSE_INLINE_BEGIN_2(x,pv)
+            /* Load First Column of X*/ 
+            SSE_LOADL_PS(SSE_ARG_1,FLOAT_0,XMM4)
+            SSE_LOADH_PS(SSE_ARG_1,FLOAT_2,XMM4)
+
+            /* Matrix-Vector Product: */
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_0,XMM5)
+            SSE_SHUFFLE(XMM5,XMM5,0x00)
+            SSE_MULT_PS(XMM5,XMM0)
+            SSE_SUB_PS(XMM4,XMM5)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_1,XMM6)
+            SSE_SHUFFLE(XMM6,XMM6,0x00)
+            SSE_MULT_PS(XMM6,XMM1)
+            SSE_SUB_PS(XMM4,XMM6)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_2,XMM7)
+            SSE_SHUFFLE(XMM7,XMM7,0x00)
+            SSE_MULT_PS(XMM7,XMM2)
+            SSE_SUB_PS(XMM4,XMM7)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_3,XMM5)
+            SSE_SHUFFLE(XMM5,XMM5,0x00)
+            SSE_MULT_PS(XMM5,XMM3)
+            SSE_SUB_PS(XMM4,XMM5)
+
+            SSE_STOREL_PS(SSE_ARG_1,FLOAT_0,XMM4)
+            SSE_STOREH_PS(SSE_ARG_1,FLOAT_2,XMM4)
+
+            /* Second Column */
+            SSE_LOADL_PS(SSE_ARG_1,FLOAT_4,XMM5)
+            SSE_LOADH_PS(SSE_ARG_1,FLOAT_6,XMM5)          
+
+            /* Matrix-Vector Product: */
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_4,XMM6)
+            SSE_SHUFFLE(XMM6,XMM6,0x00)
+            SSE_MULT_PS(XMM6,XMM0)
+            SSE_SUB_PS(XMM5,XMM6)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_5,XMM7)
+            SSE_SHUFFLE(XMM7,XMM7,0x00)
+            SSE_MULT_PS(XMM7,XMM1)
+            SSE_SUB_PS(XMM5,XMM7)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_6,XMM4)
+            SSE_SHUFFLE(XMM4,XMM4,0x00)
+            SSE_MULT_PS(XMM4,XMM2)
+            SSE_SUB_PS(XMM5,XMM4)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_7,XMM6)
+            SSE_SHUFFLE(XMM6,XMM6,0x00)
+            SSE_MULT_PS(XMM6,XMM3)
+            SSE_SUB_PS(XMM5,XMM6)
+          
+            SSE_STOREL_PS(SSE_ARG_1,FLOAT_4,XMM5)
+            SSE_STOREH_PS(SSE_ARG_1,FLOAT_6,XMM5)
+
+            SSE_PREFETCH_L1(SSE_ARG_2,FLOAT_24)
+
+            /* Third Column */
+            SSE_LOADL_PS(SSE_ARG_1,FLOAT_8,XMM6)
+            SSE_LOADH_PS(SSE_ARG_1,FLOAT_10,XMM6)
+
+            /* Matrix-Vector Product: */
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_8,XMM7)
+            SSE_SHUFFLE(XMM7,XMM7,0x00)
+            SSE_MULT_PS(XMM7,XMM0)
+            SSE_SUB_PS(XMM6,XMM7)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_9,XMM4)
+            SSE_SHUFFLE(XMM4,XMM4,0x00)
+            SSE_MULT_PS(XMM4,XMM1)
+            SSE_SUB_PS(XMM6,XMM4)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_10,XMM5)
+            SSE_SHUFFLE(XMM5,XMM5,0x00)
+            SSE_MULT_PS(XMM5,XMM2)
+            SSE_SUB_PS(XMM6,XMM5)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_11,XMM7)
+            SSE_SHUFFLE(XMM7,XMM7,0x00)
+            SSE_MULT_PS(XMM7,XMM3)
+            SSE_SUB_PS(XMM6,XMM7)
+          
+            SSE_STOREL_PS(SSE_ARG_1,FLOAT_8,XMM6)
+            SSE_STOREH_PS(SSE_ARG_1,FLOAT_10,XMM6)
+          
+            /* Fourth Column */
+            SSE_LOADL_PS(SSE_ARG_1,FLOAT_12,XMM4)
+            SSE_LOADH_PS(SSE_ARG_1,FLOAT_14,XMM4)
+
+            /* Matrix-Vector Product: */
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_12,XMM5)
+            SSE_SHUFFLE(XMM5,XMM5,0x00)
+            SSE_MULT_PS(XMM5,XMM0)
+            SSE_SUB_PS(XMM4,XMM5)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_13,XMM6)
+            SSE_SHUFFLE(XMM6,XMM6,0x00)
+            SSE_MULT_PS(XMM6,XMM1)
+            SSE_SUB_PS(XMM4,XMM6)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_14,XMM7)
+            SSE_SHUFFLE(XMM7,XMM7,0x00)
+            SSE_MULT_PS(XMM7,XMM2)
+            SSE_SUB_PS(XMM4,XMM7)
+
+            SSE_LOAD_SS(SSE_ARG_2,FLOAT_15,XMM5)
+            SSE_SHUFFLE(XMM5,XMM5,0x00)
+            SSE_MULT_PS(XMM5,XMM3)
+            SSE_SUB_PS(XMM4,XMM5)
+          
+            SSE_STOREL_PS(SSE_ARG_1,FLOAT_12,XMM4)
+            SSE_STOREH_PS(SSE_ARG_1,FLOAT_14,XMM4)
+          SSE_INLINE_END_2;
+          pv   += 16;
+        }
+        PetscLogFlops(128*nz+112);
+      } 
+      row = *bjtmp++;
+/*        row = (*bjtmp++)/4; */
+    }
+    /* finished row so stick it into b->a */
+    pv = ba + 16*bi[i];
+    pj = bj + bi[i];
+    nz = bi[i+1] - bi[i];
+
+    /* Copy x block back into pv block */
+    for (j=0; j<nz; j++) {
+      x  = rtmp+16*pj[j];
+/*        x  = rtmp+4*pj[j]; */
+
+      SSE_INLINE_BEGIN_2(x,pv)
+        /* Note: on future SSE architectures, STORE might be more efficient than STOREL/H */
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_0,XMM1)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_0,XMM1)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_2,XMM2)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_2,XMM2)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_4,XMM3)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_4,XMM3)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_6,XMM4)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_6,XMM4)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_8,XMM5)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_8,XMM5)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_10,XMM6)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_10,XMM6)
+
+        SSE_LOADL_PS(SSE_ARG_1,FLOAT_12,XMM7)
+        SSE_STOREL_PS(SSE_ARG_2,FLOAT_12,XMM7)
+
+        SSE_LOADH_PS(SSE_ARG_1,FLOAT_14,XMM0)
+        SSE_STOREH_PS(SSE_ARG_2,FLOAT_14,XMM0)
+      SSE_INLINE_END_2;
+      pv += 16;
+    }
+    /* invert diagonal block */
+    w = ba + 16*diag_offset[i];
+    if (pivotinblocks) {
+      ierr = Kernel_A_gets_inverse_A_4(w);CHKERRQ(ierr);
+    } else {
+      ierr = Kernel_A_gets_inverse_A_4_nopivot(w);CHKERRQ(ierr);
+    }
+/*      ierr = Kernel_A_gets_inverse_A_4_SSE(w);CHKERRQ(ierr); */
+    /* Note: Using Kramer's rule, flop count below might be infairly high or low? */ 
+  }
+
+  ierr = PetscFree(rtmp);CHKERRQ(ierr);
+  C->factor    = FACTOR_LU;
+  C->assembled = PETSC_TRUE;
+  PetscLogFlops(1.3333*64*b->mbs);
+  /* Flop Count from inverting diagonal blocks */ 
+  SSE_SCOPE_END;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_SSE_usj"
+int MatLUFactorNumeric_SeqBAIJ_4_NaturalOrdering_SSE_usj(Mat A,Mat *B)
+{
   Mat            C = *B;
   Mat_SeqBAIJ    *a = (Mat_SeqBAIJ*)A->data,*b = (Mat_SeqBAIJ*)C->data;
   int            ierr,i,j,n = a->mbs;
