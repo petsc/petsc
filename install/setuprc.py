a172b8ad3a9e709ed845bd4ec23c03cd1dda8b6d
Barry Smith <bsmith@mcs.anl.gov>
2003-02-28 13:36:24 -0600

    bk-changeset-1.440.1.1 bsmith@harley.mcs.anl.gov|ChangeSet|20030228193624|32101 ChangeSet   1.440.1.1 03/02/28 13:36:24 bsmith@harley.mcs.anl.gov +7 -0   installer can run without setting pythonpath to buildsystem
      BitKeeper/etc/logging_ok
        1.10 03/02/28 13:36:24 bsmith@harley.mcs.anl.gov +6 -5
        Logging to logging@openlogging.org accepted
    
      sidldllpath.py
        1.7 03/02/28 13:36:21 bsmith@harley.mcs.anl.gov +2 -1
        correct way of getting location of parent RDict
    
      matlabpath.py
        1.6 03/02/28 13:36:21 bsmith@harley.mcs.anl.gov +1 -1
        corret way to find location of RDict parent
    
      install/setuprc.py
        1.3 03/02/28 13:36:21 bsmith@harley.mcs.anl.gov +23 -480
        support using installer without setting a PYTHONPATH to BuildSystem
    
      install/installerclass.py
        1.2 03/02/28 13:36:21 bsmith@harley.mcs.anl.gov +0 -19
        support using installer without setting a PYTHONPATH to BuildSystem
    
      install/installer.py
        1.27 03/02/28 13:36:21 bsmith@harley.mcs.anl.gov +5 -64
        support using installer without setting a PYTHONPATH to BuildSystem
    
      make.py
        1.39 03/02/28 13:25:37 bsmith@harley.mcs.anl.gov +1 -0
        support using make without any PYTHONPATH
    
      install/setuprc.py
        1.2 03/02/26 22:23:44 bsmith@harley.mcs.anl.gov +0 -0
        Rename: setuprc.py -> install/setuprc.py
    
      install/installerclass.py
        1.1 03/02/26 22:17:34 bsmith@harley.mcs.anl.gov +88 -0
    
      install/installerclass.py
        1.0 03/02/26 22:17:34 bsmith@harley.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/bsmith/petsc-3.0/BuildSystem/install/installerclass.py
    
      install/installer.py
        1.26 03/02/26 22:13:51 bsmith@harley.mcs.anl.gov +0 -0
        *** empty log message ***
    
      setuprc.py
        1.1 03/02/26 19:34:14 bsmith@harley.mcs.anl.gov +488 -0
    
      setuprc.py
        1.0 03/02/26 19:34:14 bsmith@harley.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/bsmith/petsc-3.0/BuildSystem/setuprc.py
    
    
    Hg-commit: 1ad6a41e2d4a0e411ed4d7318d821394fb34e777

---
+++ install/setuprc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''Creates a .pythonrc.py file and puts the path for BuildSystem in it'''
+
+import os
+import sys
+import os.path
+
+def setupRC(path):
+  import user
+  if path in sys.path: return
+  
+  fname = os.path.join(os.getenv('HOME'),'.pythonrc.py')
+  if os.path.isfile(fname):
+    f = open(fname)
+    contents = f.read()
+    f.close()
+  else:
+    contents = ''
+
+  contents = contents + '# Code added by BuildSystem/setuprc.py\n'
+  contents = contents + 'import sys\n'
+  contents = contents + 'sys.path.insert(0,"'+path+'")\n'
+  f = open(fname,'w')
+  f.write(contents)
+  f.close()
+
+  sys.path.append(path)
+  
+if __name__ ==  '__main__':
+  setupRC('hi')
+  
--- sidldllpath.py
+++ sidldllpath.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import user
 import os
 import sys
 import project
@@ -11,7 +12,7 @@ def getSIDLDLLPath():
     SIDL_DLL_PATH = os.environ['SIDL_DLL_PATH'].split(';')
   else:  
     SIDL_DLL_PATH = [] 
-  argsDB = RDict.RDict(parentDirectory = os.path.dirname(sys.modules['RDict'].__file__))
+  argsDB = RDict.RDict(parentDirectory = os.path.abspath(os.path.dirname(sys.modules['RDict'].__file__)))
   projects = argsDB['installedprojects']
   for p in projects:
     try:
