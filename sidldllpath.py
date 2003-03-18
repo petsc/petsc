398a951e4f2aaf3baf79dbc90a0577df610e4b72
Matthew Knepley <knepley@mcs.anl.gov>
2003-03-18 10:07:06 -0600

    bk-changeset-1.469 knepley@khan.mcs.anl.gov|ChangeSet|20030318160706|02702 ChangeSet   1.469 03/03/18 10:07:06 knepley@khan.mcs.anl.gov +1 -0   Code cleanup
      sidldllpath.py
        1.8 03/03/18 10:07:06 knepley@khan.mcs.anl.gov +8 -10
        Cleaned up
    
    
    Hg-commit: 580626d9b5583e63c275df35c1f184fd349643c0

--- sidldllpath.py
+++ sidldllpath.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-
 import user
-import os
-import sys
 import project
 import RDict
-import string
+
+import os
+import sys
 
 def getSIDLDLLPath():
-  if os.environ.has_key('SIDL_DLL_PATH'):
+  if 'SIDL_DLL_PATH' in os.environ:
     SIDL_DLL_PATH = os.environ['SIDL_DLL_PATH'].split(';')
   else:  
     SIDL_DLL_PATH = [] 
-  argsDB = RDict.RDict(parentDirectory = os.path.abspath(os.path.dirname(sys.modules['RDict'].__file__)))
+  argsDB   = RDict.RDict(parentDirectory = os.path.abspath(os.path.dirname(sys.modules['RDict'].__file__)))
   projects = argsDB['installedprojects']
   for p in projects:
     try:
-      root = p.getRoot()+'/lib'
+      root = os.path.join(p.getRoot(), 'lib')
       if not root in SIDL_DLL_PATH:
-        SIDL_DLL_PATH += [root]
+        SIDL_DLL_PATH.append(root)
     except: pass
-  return string.join(SIDL_DLL_PATH,';')
+  return ';'.join(SIDL_DLL_PATH)
     
 if __name__ ==  '__main__':
   if len(sys.argv) > 1: sys.exit('Usage: sidldllpath.py')
   print getSIDLDLLPath()
-  
 
