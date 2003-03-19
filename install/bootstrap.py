e304ecff0626008598fc30e59307eb649a790475
Barry Smith <bsmith@mcs.anl.gov>
2003-03-19 15:14:16 -0600

    bk-changeset-1.455.1.1 bsmith@harley.mcs.anl.gov|ChangeSet|20030319211416|02879 ChangeSet   1.455.1.1 03/03/19 15:14:16 bsmith@harley.mcs.anl.gov +2 -0   don't need to set type for python include or library, they are always set by   system
      BSTemplates/compileDefaults.py
        1.53 03/03/19 15:13:54 bsmith@harley.mcs.anl.gov +0 -2
        don't need to set type for python include or library, they are always set by 
        system
    
      install/bootstrap.py
        1.9 03/03/17 15:53:46 bsmith@harley.mcs.anl.gov +62 -4
        support for a varity of browsers
    
      install/bootstrap.py
        1.8 03/03/16 23:10:12 bsmith@harley.mcs.anl.gov +50 -5
        get browser to use from user
    
    
    Hg-commit: d9e76750f518f89175f53952ecd7290892235742

--- install/bootstrap.py
+++ install/bootstrap.py
@@ -20,10 +20,12 @@ def ConvertReturnToExit(key):
   if key == ord('\n'): return 7
   return key
 
-def CenterGetStr(stdscr,my,text = ''):
+def CenterGetStr(stdscr,my,width = 0,text = '',prompt = ''):
   (y,x) = stdscr.getmaxyx()
-  width = x - 10
+  if not width:  width = x - 10 - len(prompt)
   x = int((x - width)/2)
+  if prompt:
+    stdscr.addstr(my+1,x-len(prompt)-2,prompt)
   curses.textpad.rectangle(stdscr,my,x-1,my+2,x+width)
   subscr  = curses.newwin(1,width,my+1,x)
   subscr.addstr(0,0,text)
@@ -32,6 +34,25 @@ def CenterGetStr(stdscr,my,text = ''):
   textobj.stripspaces = 1
   text = textobj.edit(ConvertReturnToExit)
   return text
+
+def SelectFromList(stdscr,list,my = 1,text = 'Select desired value'):
+  CenterAddStr(stdscr,my,text)
+  (y,x) = stdscr.getmaxyx()
+  my = my + 2
+  i  = 0
+  for l in list:
+    stdscr.addstr(my,2,str(i)+') '+l)
+    i  = i + 1
+    my = my + 1
+  stdscr.addstr(my+1,2,'Type a number 0 to '+str(i-1))
+  stdscr.refresh()
+  ch = -1
+  while ch >= i or ch < 0:
+    ch = stdscr.getch() - ord('0')
+  return ch
+
+
+# -----------------------------------------------------------------    
 
 class CursesInstall:
   def __init__(self):
@@ -47,6 +68,62 @@ class CursesInstall:
     curses.halfdelay(50)
     c = stdscr.getch()
     curses.nocbreak()
+
+  def GetBrowser(self,stdscr):
+    list = ['A different browser or a browser on a different machine (you will be prompted for it)']
+    for l in ['netscape','lynx','opera','mozilla','galeon']:
+      (status,output) = commands.getstatusoutput('which '+l)
+      if status == 0:  # found it :-)
+        list.append(output)
+
+    stdscr.clear()
+    key = SelectFromList(stdscr,list,my = 1,text = 'Select browser to view documentation')
+    if key == 0:
+      stdscr.clear()
+      CenterAddStr(stdscr,1,'Enter complete path of browser (for example /usr/bin/netscape or ssh mymachine netscape)')
+      self.browser = CenterGetStr(stdscr,2)
+      while 1:
+        if (not os.path.isfile(self.browser)) and (not self.browser[0:3] == 'ssh'):
+          CenterAddStr(stdscr,5,'Program does not exist. Enter a valid program or nothing',curses.A_BLINK)
+          self.browser = CenterGetStr(stdscr,2,text = self.browser)
+        else:
+          break
+    else:
+      self.browser = list[key]
+
+    stdscr.clear()
+    CenterAddStr(stdscr,1,'Testing browser '+self.browser)
+    stdscr.refresh()
+
+    #set the method to open URLs for this browser
+    if not self.browser.find("opera") == -1:
+      self.OpenURL = self.OpenURL_Opera
+    elif not self.browser.find("netscape") == -1:
+      self.OpenURL = self.OpenURL_Netscape
+    elif not self.browser.find("lynx") == -1:
+      self.OpenURL = self.OpenURL_Lynx
+    elif not self.browser.find("mozilla") == -1:
+      self.OpenURL = self.OpenURL_Netscape
+    elif not self.browser.find("galeon") == -1:
+      self.OpenURL = self.OpenURL_Galeon
+                  
+    if self.OpenURL('http://www.mcs.anl.gov/petsc'):
+      stdscr.clear()
+      CenterAddStr(stdscr,1,'Unable to open browser '+self.browser)
+      CenterAddStr(stdscr,2,'(Hit any key to continue)')
+      stdscr.refresh()
+      c = stdscr.getkey()
+      self.browser = None
+      return
+
+    stdscr.clear()
+    CenterAddStr(stdscr,1,'Testing browser '+self.browser)
+    stdscr.refresh()
+    CenterAddStr(stdscr,2,'Hit y if your browser opened to the PETSc web page, otherwise hit n')
+    stdscr.refresh()
+    c = stdscr.getkey()
+    if c != 'y': self.browser = None
+      
   
   def IndicateBKMissing(self,stdscr):
     stdscr.clear()
@@ -66,18 +143,18 @@ class CursesInstall:
       else:
         if not os.path.isdir(self.bkpath):
           CenterAddStr(stdscr,8,'Directory does not exist. Enter a valid directory or nothing',curses.A_BLINK)
-          self.bkpath = CenterGetStr(stdscr,5,self.bkpath)    
+          self.bkpath = CenterGetStr(stdscr,5,text = self.bkpath)    
         else:
           if os.path.isfile(os.path.join(self.bkpath,'bk')): return
           CenterAddStr(stdscr,8,'Directory does not contain the bk command',curses.A_BLINK)
-          self.bkpath = CenterGetStr(stdscr,5,self.bkpath)    
+          self.bkpath = CenterGetStr(stdscr,5,text = self.bkpath)    
 
         
   def InstallDirectory(self,stdscr):
     stdscr.clear()
     CenterAddStr(stdscr,1,'Directory to install projects')
     path = os.getcwd()
-    self.installpath = CenterGetStr(stdscr,2,path)
+    self.installpath = CenterGetStr(stdscr,2,text = path)
 
   def AlreadyInstalled(self,stdscr):
     stdscr.clear()
@@ -106,11 +183,37 @@ class CursesInstall:
     stdscr.refresh()
     (self.status,output) = commands.getstatusoutput('cd '+self.installpath+';'+self.bkpath+'/bk clone bk://sidl@sidl.bkbits.net/BuildSystem')
     
+#------------------------------------------------------------------
+
+  def OpenURL_Netscape(self,url):
+    (status,output) = commands.getstatusoutput(self.browser+" -remote 'openURL("+url+")'")
+    if status == 0:  return
+    pipe = os.popen(self.browser+" "+url+" &")
+    import time
+    time.sleep(5)
+    return 0
+    
+  def OpenURL_Opera(self,url):
+    pipe = os.popen(self.browser+" -remote 'openURL("+url+")'&")
+    return 0
+
+  def OpenURL_Lynx(self,url):
+    return  os.system(self.browser+" "+url)
+
+  def OpenURL_Galeon(self,url):
+    pipe = os.popen(self.browser+" "+url)
+    return 0
+
+#------------------------------------------------------------------
+
 if __name__ ==  '__main__':
   
   installer = CursesInstall()
   curses.wrapper(installer.Welcome)
 
+  curses.wrapper(installer.GetBrowser)
+  print 'Browser '+installer.browser
+  
   # need to have a more complete search for bk, but cannot use configure stuff
   (status,output) = commands.getstatusoutput('which bk')
   if status == 0:  # found it :-)
