cdae210b79f2ba924383b4bc4e90d43f5c6a4f44
Barry Smith <bsmith@mcs.anl.gov>
2003-04-03 14:50:48 -0600

    bk-changeset-1.533 bsmith@harley.mcs.anl.gov|ChangeSet|20030403205048|21733 ChangeSet   1.533 03/04/03 14:50:48 bsmith@harley.mcs.anl.gov +1 -0   always create the .pythonrc.py file no good way to check current one
      install/setuprc.py
        1.7 03/04/03 14:50:46 bsmith@harley.mcs.anl.gov +12 -16
        always create the .pythonrc.py file no good way to check current one
    
    
    Hg-commit: 7eb0082c537c41c5941f6a53abd95a5c06558269

--- install/setuprc.py
+++ install/setuprc.py
@@ -6,24 +6,20 @@ import sys
 import os.path
 
 def setupRC(path):
-  import user
-  if path in sys.path:
-    sys.path.remove(path)
-  else:
-    fname = os.path.join(os.getenv('HOME'),'.pythonrc.py')
-    if os.path.isfile(fname):
-      f = open(fname)
-      contents = f.read()
-      f.close()
-    else:
-      contents = ''
-
-    contents = contents + '# Code added by BuildSystem/install/setuprc.py\n'
-    contents = contents + 'import sys\n'
-    contents = contents + 'sys.path.insert(0,"'+path+'")\n'
-    f = open(fname,'w')
-    f.write(contents)
+  fname = os.path.join(os.getenv('HOME'),'.pythonrc.py')
+  if os.path.isfile(fname):
+    f = open(fname)
+    contents = f.read()
     f.close()
+  else:
+    contents = ''
+
+  contents = contents + '# Code added by BuildSystem/install/setuprc.py\n'
+  contents = contents + 'import sys\n'
+  contents = contents + 'sys.path.insert(0,"'+path+'")\n'
+  f = open(fname,'w')
+  f.write(contents)
+  f.close()
   sys.path.insert(0, path)
   
 if __name__ ==  '__main__':
