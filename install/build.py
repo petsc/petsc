9c80d0a495c408e760f62a04a04651aa08a05aa6
Matthew Knepley <knepley@mcs.anl.gov>
2003-05-02 13:26:14 -0600

    bk-changeset-1.594 knepley@khan.mcs.anl.gov|ChangeSet|20030502192614|60031 ChangeSet   1.594 03/05/02 14:26:14 knepley@khan.mcs.anl.gov +1 -0   Install fix
      install/build.py
        1.32 03/05/02 14:26:14 knepley@khan.mcs.anl.gov +78 -1
        Totally redid build logic
    
    
    Hg-commit: 038014a38b3490f018f6fd41ea6e0c1dca0c1cef

--- install/build.py
+++ install/build.py
@@ -19,7 +19,84 @@ class Builder(install.urlMapping.UrlMapping):
     finally:
       if fp: fp.close()
 
-  def build(self, root, target = ['default'], setupTarget = None, ignoreDependencies = 0):
+  def loadLocalDict(self, root):
+    '''Load any existing local RDict into the one we are currently using'''
+    dictFilename = os.path.join(root, 'RDict.db')
+    data         = None
+    if os.path.exists(dictFilename):
+      try:
+        import cPickle
+        dbFile = file(dictFilename)
+        data   = cPickle.load(dbFile)
+        self.debugPrint('Loaded argument database from '+dictFilename, 2, 'install')
+        keys   = self.argDB.keys()
+        for k in filter(lambda k: not k in keys, data.keys()):
+          if data[k].isValueSet():
+            self.argDB.setType(k, data[k])
+          self.debugPrint('Set key "'+str(k)+'" in argument database', 4, 'install')
+        dbFile.close()
+      except Exception, e:
+        self.debugPrint('Problem loading dictionary from '+dictFilename+'\n--> '+str(e), 2, 'install')
+        raise e
+    return data
+
+  def unloadLocalDict(self, data):
+    '''Remove keys from any existing local RDict from our dict'''
+    if not data is None:
+      for k in filter(lambda k: not k in keys, data.keys()):
+        if data[k].isValueSet():
+          del self.argDB[k]
+    return
+
+  def buildDependenceGraph(self, maker):
+    '''Retrieve all dependencies and construct the dependence graph'''
+    for url in maker.executeTarget('getDependencies'):
+      self.debugPrint('Retrieving dependency '+url, 2, 'install')
+      try:
+        m = self.getMakeModule(self.retriever.retrieve(url)).PetscMake(sys.argv[1:], self.argDB)
+      except ImportError:
+        self.debugPrint('  No make module present in '+root, 2, 'install')
+        continue
+      self.buildDependenceGraph(m)
+      m.setupDependencies()
+    maker.setupDependencies()
+    return
+
+  def executeOverDependencies(self, proj, target):
+    '''Execute a set of targets over the project dependencies'''
+    import build.buildGraph
+
+    self.debugPrint('Executing '+str(target)+' for '+proj.getUrl()+' and dependencies', 3, 'install')
+    for p in build.buildGraph.BuildGraph.depthFirstVisit(self.argDB['projectDependenceGraph'], proj):
+      try:
+        maker = self.getMakeModule(p.getRoot()).PetscMake(sys.argv[1:], self.argDB)
+      except ImportError:
+        self.debugPrint('  No make module present in '+root, 2, 'install')
+        continue
+      maker.main(target)
+    return
+
+  def build(self, root, target = ['default'], ignoreDependencies = 0):
+    self.debugPrint('Building '+str(target)+' in '+root, 1, 'install')
+    try:
+      maker = self.getMakeModule(root).PetscMake(sys.argv[1:], self.argDB)
+    except ImportError:
+      self.debugPrint('  No make module present in '+root, 2, 'install')
+      return
+    if not ignoreDependencies:
+      self.executeOverDependencies(maker.project, ['activate', 'configure'])
+    self.buildDependenceGraph(maker)
+    self.debugPrint('Compiling in '+root, 2, 'install')
+    root      = maker.getRoot()
+    localDict = self.loadLocalDict(root)
+    ret       = maker.main(target)
+    self.unloadLocalDict(localDict)
+    if not ignoreDependencies:
+      # We must install project dependencies since the "install" target is purely local
+      self.executeOverDependencies(maker.project, ['install'])
+    return ret
+
+  def build_old(self, root, target = ['default'], setupTarget = None, ignoreDependencies = 0):
     self.debugPrint('Building '+str(target)+' in '+root, 1, 'install')
     try:
       maker = self.getMakeModule(root).PetscMake(sys.argv[1:], self.argDB)
