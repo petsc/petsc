e49f26bea3996618969555a9bbe2f66c0cb2bd75
Matthew Knepley <knepley@mcs.anl.gov>
2003-05-02 15:36:53 -0600

    bk-changeset-1.596 knepley@khan.mcs.anl.gov|ChangeSet|20030502213653|60017 ChangeSet   1.596 03/05/02 16:36:53 knepley@khan.mcs.anl.gov +1 -0   Install fix
      install/build.py
        1.34 03/05/02 16:36:53 knepley@khan.mcs.anl.gov +15 -50
        Purge all checkpoints
        Fixed marking of dependencies
    
    
    Hg-commit: 7647b4e21a4b8f69335927652240101301bd334d

--- install/build.py
+++ install/build.py
@@ -48,9 +48,21 @@ class Builder(install.urlMapping.UrlMapping):
           del self.argDB[k]
     return
 
-  def buildDependenceGraph(self, maker):
+  def purgeCheckpoint(self, proj):
+    '''Remove all checkpoints from the local dictionaires'''
+    import RDict
+    import build.buildGraph
+
+    oldDir = os.getcwd()
+    self.debugPrint('Purging checkpoints from '+proj.getUrl()+' and dependencies', 3, 'install')
+    for p in build.buildGraph.BuildGraph.depthFirstVisit(self.argDB['projectDependenceGraph'], proj):
+      os.chdir(p.getRoot())
+      del RDict.RDict()['checkpoint']
+    os.chdir(oldDir)
+    return
+
+  def buildDependenceGraph(self, maker, seen = []):
     '''Retrieve all dependencies and construct the dependence graph'''
-    seen = []
     for url in maker.executeTarget('getDependencies'):
       if url in seen: continue
       seen.append(url)
@@ -97,52 +109,5 @@ class Builder(install.urlMapping.UrlMapping):
     if not ignoreDependencies:
       # We must install project dependencies since the "install" target is purely local
       self.executeOverDependencies(maker.project, ['install'])
-    return ret
-
-  def build_old(self, root, target = ['default'], setupTarget = None, ignoreDependencies = 0):
-    self.debugPrint('Building '+str(target)+' in '+root, 1, 'install')
-    try:
-      maker = self.getMakeModule(root).PetscMake(sys.argv[1:], self.argDB)
-    except ImportError:
-      self.debugPrint('  No make module present in '+root, 2, 'install')
-      return
-    root = maker.getRoot()
-    if not ignoreDependencies:
-      for url in maker.executeTarget('getDependencies'):
-        self.debugPrint('  Retrieving and activating dependency '+url, 2, 'install')
-        self.build(self.retriever.retrieve(url), target = ['activate', 'configure'])
-    # Load any existing local RDict
-    dictFilename = os.path.join(root, 'RDict.db')
-    loadedRDict  = 0
-    if os.path.exists(dictFilename):
-      try:
-        import cPickle
-        dbFile = file(dictFilename)
-        data   = cPickle.load(dbFile)
-        self.debugPrint('Loaded argument database from '+dictFilename, 2, 'install')
-        keys   = self.argDB.keys()
-        for k in filter(lambda k: not k in keys, data.keys()):
-          if data[k].isValueSet():
-            self.argDB.setType(k, data[k])
-          self.debugPrint('Set key "'+str(k)+'" in argument database', 4, 'install')
-        dbFile.close()
-        loadedRDict = 1
-      except Exception, e:
-        self.debugPrint('Problem loading dictionary from '+dictFilename+'\n--> '+str(e), 2, 'install')
-        raise e
-    self.debugPrint('Compiling in '+root, 2, 'install')
-    if setupTarget is None:                 setupTarget = []
-    elif not isinstance(setupTarget, list): setupTarget = [setupTarget]
-    for t in setupTarget:
-      maker.executeTarget(t)
-    ret = maker.main(target)
-    if loadedRDict:
-      for k in filter(lambda k: not k in keys, data.keys()):
-        if data[k].isValueSet():
-          del self.argDB[k]
-    if not ignoreDependencies:
-      for url in maker.executeTarget('getDependencies'):
-        self.debugPrint('  Installing dependency '+url, 2, 'install')
-        self.build(self.getInstallRoot(url), target = ['install'])
-      maker.executeTarget('install')
+      self.purgeCheckpoint()
     return ret
