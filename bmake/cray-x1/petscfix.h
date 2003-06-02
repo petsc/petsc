b9f950baf50f21ded73a0c4cbe03131d1f150473
Satish Balay <balay@mcs.anl.gov>
2003-06-02 08:40:08 -0600

    bk-changeset-1.1244.1.7 balay@asterix.mcs.anl.gov|ChangeSet|20030602144008|34553 ChangeSet   1.1244.1.7 03/06/02 09:40:08 balay@asterix.mcs.anl.gov +5 -0   add in support for cray-x1. Fortran argument processing   still needs fixing
      bmake/cray-x1/variables
        1.1 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +74 -0
    
      bmake/cray-x1/rules
        1.1 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +15 -0
    
      bmake/cray-x1/petscfix.h
        1.1 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +36 -0
    
      bmake/cray-x1/petscconf.h
        1.1 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +61 -0
    
      bmake/cray-x1/packages
        1.1 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +29 -0
    
      bmake/cray-x1/variables
        1.0 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/cray-x1/variables
    
      bmake/cray-x1/rules
        1.0 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/cray-x1/rules
    
      bmake/cray-x1/petscfix.h
        1.0 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/cray-x1/petscfix.h
    
      bmake/cray-x1/petscconf.h
        1.0 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/cray-x1/petscconf.h
    
      bmake/cray-x1/packages
        1.0 03/06/02 09:40:05 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dev/bmake/cray-x1/packages
    
    
    Hg-commit: 971247712da04367b8265c36da917d14c5c4663c

---
+++ bmake/cray-x1/petscfix.h
@@ -0,0 +1,36 @@
+/*$Id: petscfix.h,v 1.100 2000/01/11 20:57:57 bsmith Exp $*/
+
+/*
+    This fixes various things in system files that are incomplete, for 
+  instance many systems don't properly prototype all system functions.
+  It is not intended to DUPLICATE anything in the system include files;
+  if the compiler reports a conflict between a prototye in a system file
+  and this file then the prototype in this file should be removed.
+
+    This is included by files in src/sys/src
+*/
+
+#if !defined(_PETSCFIX_H)
+#define _PETSCFIX_H
+
+#include "petsc.h"
+
+/*
+  This prototype lets us resolve the datastructure 'rusage' only in
+  the source files using getrusage, and not in other source files.
+*/
+typedef struct rusage* s_rusage;
+
+/* ------------------ Cray t3d/t3e --------------------------------*/
+#if defined(__cplusplus)
+extern "C" {
+extern unsigned int sleep(unsigned int);
+extern int          close(int);
+}
+#else
+#endif
+
+#define MPI_Comm_f2c(a)  (a)
+#define MPI_Comm_c2f(a)  (a)
+
+#endif
