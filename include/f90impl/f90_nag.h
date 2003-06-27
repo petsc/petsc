11fd4b9f7f19dd5b71a623d320964d9d4cd4fa62
Satish Balay <balay@mcs.anl.gov>
2003-06-27 14:00:11 -0600

    bk-changeset-1.1354.1.1 balay@smash.mcs.anl.gov|ChangeSet|20030627200011|14915 ChangeSet   1.1354.1.1 03/06/27 15:00:11 balay@smash.mcs.anl.gov +3 -0   update Nag compiler config
      include/f90impl/f90_nag.h
        1.6 03/06/27 15:00:10 balay@smash.mcs.anl.gov +1 -1
        update Nag compiler config
    
      bmake/linux_nagf90/variables
        1.3 03/06/27 15:00:10 balay@smash.mcs.anl.gov +2 -2
        update Nag compiler config
    
      bmake/linux_nagf90/petscconf.h
        1.3 03/06/27 15:00:10 balay@smash.mcs.anl.gov +1 -0
        update Nag compiler config
    
    
    Hg-commit: a8bd609228a79a1c52a9e0b0a42d47ac204a1af7

--- include/f90impl/f90_nag.h
+++ include/f90impl/f90_nag.h
@@ -3,7 +3,7 @@
 #if !defined(__F90_NAG_H)
 #define __F90_NAG_H
 
-#include "/homes/naumann/tmp/NGF95-020726/NAGWare_f95/lib/f95.h"
+#include "/home/petsc/soft/linux-rh73-nag/NGF95-030203/NAGWare_f95/lib/f95.h"
 /*#include "/usr/local/lib/f90/f90.h"*/
 #define F90Array1d Dope1
 #define F90Array2d Dope2
