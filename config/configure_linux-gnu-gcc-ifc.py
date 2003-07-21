8f6891aeaef85d60e8d173840c5bae5acda3355a
Satish Balay <balay@mcs.anl.gov>
2003-07-21 13:41:51 -0600

    bk-changeset-1.1413.1.3 balay@smash.mcs.anl.gov|ChangeSet|20030721194151|61424 ChangeSet   1.1413.1.3 03/07/21 14:41:51 balay@smash.mcs.anl.gov +6 -0   add in linux_gcc_ifc combination   g++ gives segv in exit() don't know why..
      config/configure_linux-gnu-gcc-ifc.py
        1.1 03/07/21 14:41:50 balay@smash.mcs.anl.gov +17 -0
    
      bmake/linux_gcc_ifc/variables
        1.1 03/07/21 14:41:50 balay@smash.mcs.anl.gov +75 -0
    
      bmake/linux_gcc_ifc/rules
        1.1 03/07/21 14:41:50 balay@smash.mcs.anl.gov +11 -0
    
      bmake/linux_gcc_ifc/petscfix.h
        1.1 03/07/21 14:41:50 balay@smash.mcs.anl.gov +30 -0
    
      config/configure_linux-gnu-gcc-ifc.py
        1.0 03/07/21 14:41:50 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/config/configure_linux-gnu-gcc-ifc.py
    
      bmake/linux_gcc_ifc/variables
        1.0 03/07/21 14:41:50 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/bmake/linux_gcc_ifc/variables
    
      bmake/linux_gcc_ifc/rules
        1.0 03/07/21 14:41:50 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/bmake/linux_gcc_ifc/rules
    
      bmake/linux_gcc_ifc/petscfix.h
        1.0 03/07/21 14:41:50 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/bmake/linux_gcc_ifc/petscfix.h
    
      bmake/linux_gcc_ifc/petscconf.h
        1.1 03/07/21 14:41:49 balay@smash.mcs.anl.gov +56 -0
    
      bmake/linux_gcc_ifc/packages
        1.1 03/07/21 14:41:49 balay@smash.mcs.anl.gov +23 -0
    
      bmake/linux_gcc_ifc/petscconf.h
        1.0 03/07/21 14:41:49 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/bmake/linux_gcc_ifc/petscconf.h
    
      bmake/linux_gcc_ifc/packages
        1.0 03/07/21 14:41:49 balay@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/balay/petsc-dev/bmake/linux_gcc_ifc/packages
    
    
    Hg-commit: 0b16ee4ba5ecbb2fe0e7b263e7616205877a3d67

---
+++ config/configure_linux-gnu-gcc-ifc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    import configure
+
+    # build on harley
+    configure_options = [
+        '--with-cc=gcc',
+	'--with-fc=ifc -w90 -w',
+	'--with-cxx=g++',
+        '-PETSC_ARCH=linux-gnu-gcc-ifc',
+        '-PETSC_DIR=/sandbox/petsc/petsc-test',
+	'--with-blas=/home/petsc/soft/linux-rh73-intel/fblaslapack/libfblas.a',
+	'--with-lapack=/home/petsc/soft/linux-rh73-intel/fblaslapack/libflapack.a'
+        ]
+
+    configure.petsc_configure(configure_options)
