98124ec1b12132752ef4449d3f147922544fa0a6
Matthew Knepley <knepley@mcs.anl.gov>
2003-07-24 16:03:47 -0600

    bk-changeset-1.1413.1.17 knepley@khan.mcs.anl.gov|ChangeSet|20030724220347|58228 ChangeSet   1.1413.1.17 03/07/24 17:03:47 knepley@khan.mcs.anl.gov +9 -0   Configure fix
      docs/website/documentation/installation.html
        1.8 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +1 -1
        Option --with-mpi is no longer necessary
    
      config/configure_solaris2.9.py
        1.4 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_osf5.0.py
        1.3 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_linux-gnu.py
        1.4 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_linux-gnu-intel.py
        1.3 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_irix6.5.py
        1.3 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_irix6.5-64.py
        1.2 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure_darwin6.6.py
        1.13 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
      config/configure.py
        1.38 03/07/24 17:03:45 knepley@khan.mcs.anl.gov +0 -1
        Option --with-mpi is no longer necessary
    
    
    Hg-commit: 77281eb0bec9d815792c2f308ea88a7d9d40eef6

--- config/configure_irix6.5-64.py
+++ config/configure_irix6.5-64.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     '-LDFLAGS=-Wl,-woff,84,-woff,85,-woff,113',
     '--with-f90-header=f90impl/f90_IRIX.h',
     '--with-f90-source=src/sys/src/f90/f90_IRIX.c',
-    '--with-mpi',
     '--with-mpi-lib=[libmpi.a,libmpi++.a]',
    '--with-mpirun=mpirun',
     '-PETSC_ARCH=irix6.5-64',
