2aa32adbb5e8a0c7c8984d1934cdfb31775e9c6d
Satish Balay <balay@mcs.anl.gov>
2003-08-06 12:48:31 -0600

    bk-changeset-1.1467.1.1 balay@smash.mcs.anl.gov|ChangeSet|20030806184831|24551 ChangeSet   1.1467.1.1 03/08/06 13:48:31 balay@smash.mcs.anl.gov +8 -0   Fix uninitialized variables in spooles, dscpack interface   disable hdf4 interface for complex
      src/vec/impls/mpi/pdvec.c
        1.18 03/08/06 13:48:29 balay@smash.mcs.anl.gov +2 -2
        hdf interface doesn't work with complex
    
      src/sys/src/viewer/interface/viewregall.c
        1.9 03/08/06 13:48:29 balay@smash.mcs.anl.gov +1 -1
        hdf interface doesn't work with complex
    
      src/sys/src/viewer/impls/hdf4/makefile
        1.2 03/08/06 13:48:29 balay@smash.mcs.anl.gov +3 -1
        hdf interface doesn't work with complex
    
      src/sys/src/viewer/impls/hdf4/hdf4v.c
        1.2 03/08/06 13:48:29 balay@smash.mcs.anl.gov +1 -1
        hdf interface doesn't work with complex
    
      src/mat/impls/baij/mpi/dscpack/dscpack.c
        1.38.1.1 03/08/06 13:48:29 balay@smash.mcs.anl.gov +11 -11
        fix uninitialized variables
    
      src/mat/impls/aij/seq/spooles/spooles.c
        1.77 03/08/06 13:48:29 balay@smash.mcs.anl.gov +1 -1
        fix uninitialized variables
    
      src/dm/da/src/gr2.c
        1.14 03/08/06 13:48:29 balay@smash.mcs.anl.gov +1 -1
        hdf interface doesn't work with complex
    
      include/petscversion.h
        1.76 03/08/06 13:48:28 balay@smash.mcs.anl.gov +2 -2
        update patch level
    
    
    Hg-commit: 955ad2bf7098367624531f85eba8811ad14cd354

--- include/petscversion.h
+++ include/petscversion.h
@@ -14,8 +14,8 @@
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    1
 #define PETSC_VERSION_SUBMINOR 6
-#define PETSC_VERSION_PATCH    0
-#define PETSC_VERSION_DATE     "Aug 05, 2003"
+#define PETSC_VERSION_PATCH    1
+#define PETSC_VERSION_DATE     "Aug 06, 2003"
 #define PETSC_AUTHOR_INFO      "\
        The PETSc Team\n\
     petsc-maint@mcs.anl.gov\n\
