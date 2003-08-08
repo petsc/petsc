5a63e50b1a490115be1bbfea514453eb38943920
Matthew Knepley <knepley@mcs.anl.gov>
2003-08-08 10:59:06 -0600

    bk-changeset-1.1485 knepley@khan.mcs.anl.gov|ChangeSet|20030808165906|62964 ChangeSet   1.1485 03/08/08 11:59:06 knepley@khan.mcs.anl.gov +2 -0   Added a test for signals
      src/sys/src/error/examples/tests/makefile
        1.1 03/08/08 11:58:33 knepley@khan.mcs.anl.gov +27 -0
    
      src/sys/src/error/examples/tests/ex1.c
        1.1 03/08/08 11:58:33 knepley@khan.mcs.anl.gov +46 -0
    
      src/sys/src/error/examples/tests/makefile
        1.0 03/08/08 11:58:33 knepley@khan.mcs.anl.gov +0 -0
        BitKeeper file /home/knepley/PETSc-dev/petsc/src/sys/src/error/examples/tests/makefile
    
      src/sys/src/error/examples/tests/ex1.c
        1.0 03/08/08 11:58:33 knepley@khan.mcs.anl.gov +0 -0
        BitKeeper file /home/knepley/PETSc-dev/petsc/src/sys/src/error/examples/tests/ex1.c
    
    
    Hg-commit: 337be27b33f0f760592e28538a9657711fec777d

---
+++ src/sys/src/error/examples/tests/ex1.c
@@ -0,0 +1,46 @@
+/*$Id: ex1.c,v 1.18 2003/08/08 21:30:50 knepley Exp $*/
+
+static char help[] = "Tests signal handling.\n\n";
+
+#include "petsc.h"
+#include <signal.h>
+
+typedef struct _handlerCtx {
+  int exitHandler;
+  int signum;
+} HandlerCtx;
+
+#undef __FUNCT__
+#define __FUNCT__ "handleSignal"
+int handleSignal(int signum, void *ctx)
+{
+  HandlerCtx *user = (HandlerCtx *) ctx;
+
+  user->signum = signum;
+  if (signum == SIGHUP) {
+    user->exitHandler = 1;
+  }
+  return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc, char *args[])
+{
+  HandlerCtx user;
+  int        ierr;
+
+  user.exitHandler = 0;
+
+  ierr = PetscInitialize(&argc, &args, (char *) 0, help);CHKERRQ(ierr);
+  ierr = PetscPushSignalHandler(handleSignal, &user);CHKERRQ(ierr);
+  while(!user.exitHandler) {
+    if (user.signum >= 0) {
+      ierr = PetscPrintf(PETSC_COMM_SELF, "Caught signal %d\n", user.signum);CHKERRQ(ierr);
+      user.signum = -1;
+    }
+  }
+  ierr = PetscPopSignalHandler();CHKERRQ(ierr);
+  ierr = PetscFinalize();CHKERRQ(ierr);
+  return 0;
+}
