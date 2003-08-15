98801c4b8415babe70a2dda723af6ccf8ed198d4
Kris Buschelman <buschelm@mcs.anl.gov>
2003-08-15 15:20:02 -0600

    bk-changeset-1.1503 Kris@p4buschel.|ChangeSet|20030815212002|63949 ChangeSet   1.1503 03/08/15 14:20:02 Kris@p4buschel. +2 -0   Add CFLAGS to ${BOPT}_COPTFLAGS   Add FFLAGS to ${BOPT}_FOPTFLAGS   Add CXXFLAGS to ${BOPT}_CXXOPTFLAGS
      config/configure_cygwin-intel.py
        1.2 03/08/15 14:20:01 Kris@p4buschel. +13 -10
        Use CFLAGS, FFLAGS, CXXFLAGS for compiler options that are used during configuration tests
        and are common across $BOPT.
    
      bmake/config/variables.in
        1.29 03/08/15 14:20:01 Kris@p4buschel. +12 -12
        Add CFLAGS to ${BOPT}_COPTFLAGS
        Add FFLAGS to ${BOPT}_FOPTFLAGS
        Add CXXFLAGS to ${BOPT}_CXXOPTFLAGS
    
    
    Hg-commit: 7a2afd1ee9eaf8736b7c170e152bc9b282cef707

--- config/configure_cygwin-intel.py
+++ config/configure_cygwin-intel.py
@@ -7,20 +7,23 @@ if __name__ == '__main__':
 
     configure_options = [
     '-PETSC_ARCH=cygwin-intel',
-    '-PETSC_DIR=/home/Kris/petsc/petsc-tmp',
+    '-PETSC_DIR=/home/Kris/petsc/petsc-2',
     # Using MPICH for Windows 2000/NT available from http://www.mcs.anl.gov/mpi/mpich
     '--with-mpi-include=/software/MPI/mpich-nt.1.2.5/SDK/include',
     '--with-mpi-lib=[/software/MPI/mpich-nt.1.2.5/SDK/lib/mpich.lib,ws2_32.lib]',
     '--with-blas-lapack=/software/BLAS/MKL/ia32/lib/mkl_c_dll.lib',
-    '--with-cc=win32fe icl --nodetect',
-    '-CFLAGS_g=-MT -W3 -Z7',
-    '-CFLAGS_O=-MT -W3 -O3 -QxW',
-    '--with-fc=win32fe ifl --nodetect',
-    '-FFLAGS_g=-MT -W0 -fpp -Z7',
-    '-FFLAGS_O=-MT -W0 -fpp -O3 -QxW',
-    '--with-cxx=win32fe icl --nodetect',
-    '-CXXFLAGS_g=-MT -W3 -TP -GX -GR -Z7',
-    '-CXXFLAGS_O=-MT -W3 -TP -GX -GR -O3 -QxW'
+    '--with-cc=win32fe icl',
+    '--CFLAGS=--nodetect -MT -W3',
+    '-CFLAGS_g=-Z7',
+    '-CFLAGS_O=-O3 -QxW',
+    '--with-fc=win32fe ifl',
+    '--FFLAGS=--nodetect -MT -fpp -W0',
+    '-FFLAGS_g=-Z7',
+    '-FFLAGS_O=-O3 -QxW',
+    '--with-cxx=win32fe icl',
+    '--CXXFLAGS=--nodetect -MT -W3 -GX -GR',
+    '-CXXFLAGS_g=-TP -Z7',
+    '-CXXFLAGS_O=-TP -O3 -QxW'
     ]
 
     configure.petsc_configure(configure_options)
