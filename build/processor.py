44f5fb1f4a88868ebd75cc720a05afafa38ad621
Matthew Knepley <knepley@mcs.anl.gov>
2003-09-13 14:38:19 -0600

    bk-changeset-1.763 knepley@khan.mcs.anl.gov|ChangeSet|20030913203819|22401 ChangeSet   1.763 03/09/13 15:38:19 knepley@khan.mcs.anl.gov +1 -0   Build fix
      build/processor.py
        1.44 03/09/13 15:38:19 knepley@khan.mcs.anl.gov +0 -2
        Forgot to fix this
    
    
    Hg-commit: f14c8feb9168953bd1f404463f21f34bccad3730

--- build/processor.py
+++ build/processor.py
@@ -508,8 +508,6 @@ class ExceptionSharedLinker(SharedLinker):
       for f in set:
         build.transform.Transform.handleFile(self, f, self.exceptions)
       library = self.getLibrary(set)
-      if not os.path.exists(library):
-        os.makedirs(library)
       self.debugPrint('Linking '+str(set)+' into '+library, 3, 'compile')
       command = ' '.join([self.getProcessor()]+set+self.getFlags(set))
       output  = self.executeShellCommand(command, self.handleErrors)
