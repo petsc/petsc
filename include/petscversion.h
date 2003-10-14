5176dac88adb4ec5577b72db233fe9ed5a7e78c6
kris <kris@192.168.1.104>
2003-10-14 04:43:28 -0600

    bk-changeset-1.1467.1.13 kris@192.168.1.104|ChangeSet|20031014104328|14938 ChangeSet   1.1467.1.13 03/10/14 03:43:28 kris@192.168.1.104 +3 -0   Bugfix for MatDuplicate related problems in MUMPS types.
      BitKeeper/etc/logging_ok
        1.57.1.1 03/10/14 03:43:28 kris@192.168.1.104 +2 -1
        Logging to logging@openlogging.org accepted
    
      src/mat/impls/aij/mpi/mumps/mumps.c
        1.41.1.1 03/10/14 03:42:58 kris@192.168.1.104 +37 -8
        remove usage of MatType for basetype related switches in MUMPS types.
    
      include/petscversion.h
        1.81.1.4 03/10/14 03:42:57 kris@192.168.1.104 +2 -2
        patch 2.1.6.10
    
    
    Hg-commit: fe3defdffe9ddbe6644efeac1fd5f77eec8a2aea

--- include/petscversion.h
+++ include/petscversion.h
@@ -14,8 +14,8 @@
 #define PETSC_VERSION_MAJOR    2
 #define PETSC_VERSION_MINOR    1
 #define PETSC_VERSION_SUBMINOR 6
-#define PETSC_VERSION_PATCH    9
-#define PETSC_VERSION_DATE     "Oct 08, 2003"
+#define PETSC_VERSION_PATCH    10
+#define PETSC_VERSION_DATE     "Oct 14, 2003"
 #define PETSC_AUTHOR_INFO      "\
        The PETSc Team\n\
     petsc-maint@mcs.anl.gov\n\
