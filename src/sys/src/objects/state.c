da05ed6dfa52d65e23e82fd45fa3cbbbc23c8277
victorle <victorle@tibook500ve.cs.utk.edu>
2003-10-28 23:12:33 -0600

    bk-changeset-1.1560.16.1 victorle@tibook500ve.cs.utk.edu|ChangeSet|20031029051233|19221 ChangeSet   1.1560.16.1 03/10/28 21:12:33 victorle@tibook500ve.cs.utk.edu +3 -0   added routines that attach quantities+state to objects;   state is now properly updated in vector changing routines;   the vector norm routine uses the new caching mechanism (old code   still needs to be removed; will do that once Barry gives his   imprimatur)
      src/vec/interface/vector.c
        1.52 03/10/28 21:12:29 victorle@tibook500ve.cs.utk.edu +57 -10
        state is now properly updated in vector changing routines;
        the norm routine uses the new caching mechanism (old code
        still needs to be removed; will do that once Barry gives his
        imprimatur)
    
      src/sys/src/objects/state.c
        1.2 03/10/28 21:12:29 victorle@tibook500ve.cs.utk.edu +171 -4
        data attaching routines added
    
      include/petschead.h
        1.14 03/10/28 21:12:29 victorle@tibook500ve.cs.utk.edu +2 -0
        prototypes for data-attaching routines
    
    
    Hg-commit: e32891f25ca826776e3b250bc2f781c3003dc8f9

--- src/sys/src/objects/state.c
+++ src/sys/src/objects/state.c
@@ -13,7 +13,7 @@
    Not Collective
 
    Input Parameter:
-.  obj - any PETSc object, for example a Vec, Mat or KSP. Thus must be
+.  obj - any PETSc object, for example a Vec, Mat or KSP. This must be
          cast with a (PetscObject), for example, 
          PetscObjectGetState((PetscObject)mat,&state);
 
@@ -23,7 +23,7 @@
    Notes: object state is an integer which gets increased every time
    the object is changed. By saving and later querying the object state
    one can determine whether information about the object is still current.
-   Currently, state is only maintained for Mat objects.
+   Currently, state is maintained for Vec and Mat objects.
 
    Level: advanced
 
@@ -49,14 +49,14 @@ int PetscObjectGetState(PetscObject obj,int *state)
    Not Collective
 
    Input Parameter:
-.  obj - any PETSc object, for example a Vec, Mat or KSP. Thus must be
+.  obj - any PETSc object, for example a Vec, Mat or KSP. This must be
          cast with a (PetscObject), for example, 
          PetscObjectIncreaseState((PetscObject)mat);
 
    Notes: object state is an integer which gets increased every time
    the object is changed. By saving and later querying the object state
    one can determine whether information about the object is still current.
-   Currently, state is only maintained for Mat objects.
+   Currently, state is maintained for Vec and Mat objects.
 
    This routine is mostly for internal use by PETSc; a developer need only
    call it after explicit access to an object's internals. Routines such
@@ -76,3 +76,170 @@ int PetscObjectIncreaseState(PetscObject obj)
   obj->state++;
   PetscFunctionReturn(0);
 }
+
+/*@C
+   PetscObjectSetComposedData - Attach a data item to an object, together
+   with the current state.
+
+   Not collective
+
+   Input parameters:
++  obj    - any PETSc object, for example a Vec, Mat or KSP. This must be
+            cast with a (PetscObject), for example, 
+            PetscObjectGetState((PetscObject)mat,&state);
+.  s      - the label under which the item is stored
+.  type   - the data type; currently only PETSC_SCALAR and PETSC_REAL
+            are supported.
+-  result - the data item
+
+   Notes: this routine is one of two that can be used to attach
+   data item to an object, which can be retrieved and reused if the
+   object has not been changed in the mean time. Presumably the data item
+   is some expensive to compute quantity.
+
+   Level: advanced
+
+   seealso: PetscObjectGetComposedData
+
+   Concepts: state
+
+@*/
+#undef __FUNCT__
+#define __FUNCT__ "PetscObjectSetComposedData"
+int PetscObjectSetComposedData(PetscObject obj,char *s,PetscDataType type,void *result)
+{
+  MPI_Comm comm; char *t;
+  PetscObjectContainer container; void *store; int *istore,state,l,ierr;
+
+  PetscFunctionBegin;
+  if (!obj) SETERRQ(PETSC_ERR_ARG_CORRUPT,"Null object");
+
+  ierr = PetscObjectGetComm(obj,&comm); CHKERRQ(ierr);
+  ierr = PetscObjectGetState(obj,&state); CHKERRQ(ierr);
+
+  /*
+   * Make a container that stores the current state under label
+   * <property>_state
+   */
+  ierr = PetscObjectContainerCreate(comm,&container); CHKERRQ(ierr);
+  ierr = PetscMalloc(sizeof(int),&istore); CHKERRQ(ierr);
+  istore[0] = state;
+  ierr = PetscObjectContainerSetPointer
+    (container,(void*)istore); CHKERRQ(ierr);
+  ierr = PetscStrlen(s,&l); CHKERRQ(ierr);
+  ierr = PetscMalloc((l+6)*sizeof(char),&t); CHKERRQ(ierr);
+  sprintf(t,"%s_state",s);
+  ierr = PetscObjectCompose
+    (obj,t,(PetscObject)container); CHKERRQ(ierr);
+
+  /*
+   * Make a container that stores the user object
+   */
+  ierr = PetscObjectContainerCreate(comm,&container); CHKERRQ(ierr);
+  {
+    int size;
+    switch (type) {
+    case PETSC_SCALAR : size = sizeof(PetscScalar); break;
+#if PETSC_SCALAR != PETSC_REAL
+    case PETSC_REAL : size = sizeof(PetscReal); break;
+#endif
+    default : SETERRQ1(1,"Can not attach objects of type",type);
+    }
+    ierr = PetscMalloc(size,&store); CHKERRQ(ierr);
+    /*printf("storing data for reuse: %e\n",*(PetscReal*)result);*/
+    ierr = PetscMemcpy(store,result,size); CHKERRQ(ierr);
+  }
+  ierr = PetscObjectContainerSetPointer
+    (container,(void*)store); CHKERRQ(ierr);
+  ierr = PetscObjectCompose
+    (obj,s,(PetscObject)container); CHKERRQ(ierr);
+
+  ierr = PetscFree(t); CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+/*@C
+   PetscObjectGetComposedData - Attach a data item from an object, provided
+   the current state of the object is the state under which the item was stored.
+
+   Not collective
+
+   Input parameters:
++  obj    - any PETSc object, for example a Vec, Mat or KSP. This must be
+            cast with a (PetscObject), for example, 
+            PetscObjectGetState((PetscObject)mat,&state);
+.  s      - the label under which the item is stored
+-  type   - the data type; currently only PETSC_SCALAR and PETSC_REAL
+            are supported.
+
+   Output parameters:
++  result - the data item
+-  flg    - PETSC_TRUE is the item was present and its stored state
+            equals the current state of the obj.
+
+   Notes: this routine is one of two that can be used to attach
+   data item to an object, which can be retrieved and reused if the
+   object has not been changed in the mean time. Presumably the data item
+   is some expensive to compute quantity.
+
+   Level: advanced
+
+   seealso: PetscObjectSetComposedData
+
+   Concepts: state
+
+@*/
+#undef __FUNCT__
+#define __FUNCT__ "PetscObjectGetComposedData"
+int PetscObjectGetComposedData
+(PetscObject obj,char *s,PetscDataType type,void *result,PetscTruth *flg)
+{
+  PetscObjectContainer container; void *store; int *istore,s1,l,ierr;
+  char *t;
+
+  PetscFunctionBegin;
+  if (!obj) SETERRQ(PETSC_ERR_ARG_CORRUPT,"Null object");
+
+  /*
+   * Generate the label under which the state of the user object
+   * was stored; test if anything is stored under this label
+   */
+  ierr = PetscStrlen(s,&l); CHKERRQ(ierr);
+  ierr = PetscMalloc((l+6)*sizeof(char),&t); CHKERRQ(ierr);
+  sprintf(t,"%s_state",s);
+  ierr = PetscObjectQuery(obj,t,(PetscObject*)&container); CHKERRQ(ierr);
+  *flg = PETSC_FALSE;
+  if (container) {
+    /*
+     * If the state label exists, get its content and compare to current state
+     */
+    ierr = PetscObjectGetState(obj,&s1); CHKERRQ(ierr);
+    ierr = PetscObjectContainerGetPointer
+      (container,(void**)&istore); CHKERRQ(ierr);
+    if (istore && s1==istore[0]) {
+      /*
+       * If the data is still valid, retrieve it
+       */
+      ierr = PetscObjectQuery(obj,s,(PetscObject*)&container); CHKERRQ(ierr);
+      ierr = PetscObjectContainerGetPointer
+	(container,(void**)&store); CHKERRQ(ierr);
+      {
+	int size;
+	switch (type) {
+	case PETSC_SCALAR : size = sizeof(PetscScalar); break;
+#if PETSC_SCALAR != PETSC_REAL
+	case PETSC_REAL : size = sizeof(PetscReal); break;
+#endif
+	default : SETERRQ1(1,"Cannot retrieve data of type",type);
+	}
+	ierr = PetscMemcpy(result,store,size); CHKERRQ(ierr);
+      }
+      /*printf("reused scalar <%s>=%e\n",s,*(double*)result);*/
+      *flg = PETSC_TRUE;
+      PetscFunctionReturn(0);
+    }
+  }
+  ierr = PetscFree(t); CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
