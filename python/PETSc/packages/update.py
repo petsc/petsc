72ea80682b5cfd09278d663fde55955060b2836d
Barry Smith <bsmith@mcs.anl.gov>
2003-11-04 15:29:14 -0600

    bk-changeset-1.1597 barrysmith@barrysmith.mcs.anl.gov|ChangeSet|20031104212914|00900 ChangeSet   1.1597 03/11/04 15:29:14 barrysmith@barrysmith.mcs.anl.gov +1 -0   using output when it wasn't set
      python/PETSc/packages/update.py
        1.36 03/11/04 15:28:50 barrysmith@barrysmith.mcs.anl.gov +1 -1
        using output when it wasn't set
    
    
    Hg-commit: 239086c5e27807c622106620cb7d5bc949c9e8fa

--- python/PETSc/packages/update.py
+++ python/PETSc/packages/update.py
@@ -41,7 +41,7 @@ class Configure(config.base.Configure):
       # Get full host description
       output = self.executeShellCommand(self.shell+' '+configSub+' '+host)
     except:
-      raise RuntimeError('Unable to determine host type using config.sub: '+output)
+      raise RuntimeError('Unable to determine host type using config.sub')
     # Parse output
     m = re.match(r'^(?P<cpu>[^-]*)-(?P<vendor>[^-]*)-(?P<os>.*)$', output)
     if not m: raise RuntimeError('Unable to parse output of config.sub: '+output)
