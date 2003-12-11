713f5328ca2bd07440daf5053ebcb7faf0420065
Barry Smith <bsmith@mcs.anl.gov>
2003-12-10 22:05:31 -0600

    bk-changeset-1.1791 barrysmith@barry-smiths-computer.local|ChangeSet|20031211040531|00882 ChangeSet   1.1791 03/12/10 22:05:31 barrysmith@barry-smiths-computer.local +2 -0   new tests for ifc and gcc
      bmake/linux_gcc_ifc/buildtest
        1.3 03/12/10 22:01:16 barrysmith@barry-smiths-computer.local +7 -7
        no c++
    
      bmake/linux_gcc_ifc/buildtest
        1.2 03/12/10 22:00:42 barrysmith@barry-smiths-computer.local +3 -2
        new tests for ifc and gcc
    
      bmake/linux_gcc_ifc/buildtest
        1.1 03/12/10 21:57:19 barrysmith@barry-smiths-computer.local +39 -0
    
      bmake/linux_gcc_ifc/buildtest
        1.0 03/12/10 21:57:19 barrysmith@barry-smiths-computer.local +0 -0
        BitKeeper file /Users/barrysmith/petsc-dev/bmake/linux_gcc_ifc/buildtest
    
      config/configure_linux-gnu-gcc-ifc.py
        1.4 03/12/10 21:56:27 barrysmith@barry-smiths-computer.local +2 -1
        *** empty log message ***
    
    
    Hg-commit: 66c9cb58e1403613b77c69ab626dc2aeaedd59d4

--- config/configure_linux-gnu-gcc-ifc.py
+++ config/configure_linux-gnu-gcc-ifc.py
@@ -4,10 +4,11 @@ if __name__ == '__main__':
     import configure
 
     # build on schwinn
+    # Note: Intel 7.1 Fortran cannot work with g++ 3.3
     configure_options = [
         '--with-cc=gcc',
 	'--with-fc=ifc',
-	'--with-cxx=g++',
+	'--with-cxx=0',
         '-PETSC_ARCH=linux-gnu-gcc-ifc',
         '-PETSC_DIR=/sandbox/petsc/petsc-test',
 	'--with-blas-lapack-dir=/home/petsc/soft/linux-rh73-intel/fblaslapack',
