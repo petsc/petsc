947a522b7573d8c54b01815e71b2e936d3b7fc3a
Barry Smith <bsmith@mcs.anl.gov>
2004-02-01 18:44:32 -0600

    bk-changeset-1.1922.1.9 barrysmith@barry-smiths-computer.local|ChangeSet|20040202004432|21671 ChangeSet   1.1922.1.9 04/02/01 18:44:32 barrysmith@barry-smiths-computer.local +1 -0   no cxx with mpi installed
      config/linux-gnu-gcc-pgf90.py
        1.10 04/02/01 18:44:13 barrysmith@barry-smiths-computer.local +1 -1
        no cxx with mpi installed
    
    
    Hg-commit: c570791e0684169e274b22a179d36e793e6b6482

--- config/linux-gnu-gcc-pgf90.py
+++ config/linux-gnu-gcc-pgf90.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     configure_options = [
         '--with-cc=gcc',
 	'--with-fc=pgf90',
-	'--with-cxx=g++',
+	'--with-cxx=0',
 	'--with-blas-lapack-dir=/home/petsc/soft/linux-rh73-intel/fblaslapack',
         '--with-mpi-dir=/home/petsc/soft/linux-rh73-gcc-pgf90/mpich-1.2.5'
         ]
