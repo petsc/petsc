09d4790e908dc162ed2ea8c20a3bf0027db70c34
Satish Balay <balay@mcs.anl.gov>
2004-02-06 00:10:42 -0600

    bk-changeset-1.1922.8.1 balay@asterix.mcs.anl.gov|ChangeSet|20040206061042|37813 config/cygwin     1.1 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +35 -0
      config/cygwin-ms
        1.1 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +35 -0
    
    ChangeSet
      1.1922.8.1 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +6 -0
      change parth to configure
      fix '1' file creation
    
      makefile
        1.87 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +1 -1
        fix redirection bug
    
      maint/confignightly/parth
        1.14 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +3 -1
        change parth to configure
    
      config/cygwin
        1.0 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/config/cygwin
    
      config/cygwin.py
        1.10 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +3 -6
        change to parth
    
      config/cygwin-ms
        1.0 04/02/06 00:10:42 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/config/cygwin-ms
    
      config/cygwin-ms.py
        1.9 04/02/06 00:10:41 balay@asterix.mcs.anl.gov +2 -6
        change to parth
    
    
    Hg-commit: 83b7c680fc399bded68dd466ae23ccc09f7e57d1

--- config/cygwin.py
+++ config/cygwin.py
@@ -6,13 +6,10 @@ if __name__ == '__main__':
     import configure
 
     configure_options = [
-    # Using mpiuni
-    #'--with-mpi=0',
-    # Using MPICH for Windows 2000/NT available from http://www.mcs.anl.gov/mpi/mpich
-    #'--with-mpi-dir=/cygdrive/c/Program\ Files/MPICH/SDK.gcc',
+    #with gcc + mpich autodetect
     '--with-mpi-compilers=0',
-    # Using reference BLAS/LAPACK available from http://www.netlib.org/lapack
-    '--with-blas-lapack-dir=/software/BLAS/LAPACK/LIB/win32_gnu_local_O'
+    '--with-blas=/cygdrive/c/software/fblaslapack/win32_gnu/libfblas.a',
+    '--with-lapack=/cygdrive/c/software/fblaslapack/win32_gnu/libflapack.a'
     ]
 
     configure.petsc_configure(configure_options)
