7c7ad229d6bf33827898961663707903f499850f
Barry Smith <bsmith@mcs.anl.gov>
2004-02-07 10:51:18 -0600

    bk-changeset-1.1929.1.17 barrysmith@barry-smiths-computer.local|ChangeSet|20040207165118|22026 ChangeSet   1.1929.1.17 04/02/07 10:51:18 barrysmith@barry-smiths-computer.local +1 -0   Barry dman fool screwed up .h
      src/sys/src/viewer/impls/mathematica/mathematica.c
        1.22 04/02/07 10:50:42 barrysmith@barry-smiths-computer.local +1 -1
        Barry dman fool screwed up .h
    
    
    Hg-commit: ed3fa52060c60ec56efc0dc59a8dd69ed5c58e15

--- src/sys/src/viewer/impls/mathematica/mathematica.c
+++ src/sys/src/viewer/impls/mathematica/mathematica.c
@@ -11,7 +11,7 @@ static char vcid[] = "$Id: mathematica.c,v 1.9 2000/01/26 15:46:22 baggag Exp $"
 #include "src/ksp/pc/pcimpl.h"
 #include "src/mat/impls/aij/seq/aij.h"
 #include "mathematica.h"
-#include "petscfix.if"
+#include "petscfix.h"
 
 #if defined (PETSC_HAVE__SNPRINTF) && !defined(PETSC_HAVE_SNPRINTF)
 #define snprintf _snprintf
