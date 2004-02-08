41997313d7f600050e83cab4573d3ee6d87d5981
Barry Smith <bsmith@mcs.anl.gov>
2004-02-07 20:56:17 -0600

    bk-changeset-1.897 barrysmith@barry-smiths-computer.local|ChangeSet|20040208025617|59447 ChangeSet   1.897 04/02/07 20:56:17 barrysmith@barry-smiths-computer.local +1 -0   Matt, I fucking hate you. I am putting it here because I need to get it out   in Matlab.py and I don't fucking know how to do it!
      config/setCompilers.py
        1.16 04/02/07 20:45:37 barrysmith@barry-smiths-computer.local +1 -0
        Matt, I fucking hate you. I am putting it here because I need to get it out
        in Matlab.py and I don't fucking know how to do it!
    
    
    Hg-commit: c11777d3230a3399d2574b3750aaa8109820bc63

--- config/setCompilers.py
+++ config/setCompilers.py
@@ -431,6 +431,7 @@ class Configure(config.base.Configure):
       flag = ''
     self.addSubstitution('RPATH', flag)
     self.slpath = flag
+    self.framework.slpath = flag
     return
 
   def configure(self):
