e9f3bb1725702b0f6d2625838d7304edb28acd15
Barry Smith <bsmith@mcs.anl.gov>
2004-02-07 20:56:05 -0600

    bk-changeset-1.1929.6.1 barrysmith@barry-smiths-computer.local|ChangeSet|20040208025605|23624 ChangeSet   1.1929.6.1 04/02/07 20:56:05 barrysmith@barry-smiths-computer.local +5 -0   configure fixes including better error message   installation docs about rebaseall
      python/PETSc/packages/Matlab.py
        1.26 04/02/07 20:55:57 barrysmith@barry-smiths-computer.local +4 -1
        I had screwed up the slpath; Matt won't like what I've done but I'm a stupid
        fuck and forgot how to access the setCompilers shit
    
      docs/makefile
        1.10 04/02/07 20:55:57 barrysmith@barry-smiths-computer.local +12 -1
        list website pages in TAGS
    
      config/configure.py
        1.56 04/02/07 20:55:57 barrysmith@barry-smiths-computer.local +12 -8
        better error message when configure fails or crashes
    
      docs/website/documentation/installation.html
        1.19 04/02/07 19:51:00 barrysmith@barry-smiths-computer.local +2 -3
        rebase for cygwin
    
      makefile
        1.90 04/02/07 19:13:46 barrysmith@barry-smiths-computer.local +1 -1
        files for TAGS
    
    
    Hg-commit: f70edfd7d03ce0feb500bae93befcc1b56949f7c

--- python/PETSc/packages/Matlab.py
+++ python/PETSc/packages/Matlab.py
@@ -81,7 +81,10 @@ class Configure(config.base.Configure):
               matlab_sys = ':'+os.path.join(matlab,'sys','os',matlab_arch)
             else:
               matlab_sys = ''
-            self.addSubstitution('MATLAB_LIB',self.setCompilers.slpath+os.path.join(matlab,'extern','lib',matlab_arch)+matlab_sys+' -L'+os.path.join(matlab,'extern','lib',matlab_arch)+' -leng -lmx -lmat -lut'+matlab_dl)
+            if self.framework.slpath:
+              self.addSubstitution('MATLAB_LIB',self.framework.slpath+os.path.join(matlab,'extern','lib',matlab_arch)+matlab_sys+' -L'+os.path.join(matlab,'extern','lib',matlab_arch)+' -leng -lmx -lmat -lut'+matlab_dl)
+            else:
+              self.addSubstitution('MATLAB_LIB','-L'+os.path.join(matlab,'extern','lib',matlab_arch)+' -leng -lmx -lmat -lut'+matlab_dl)              
             return
       except RuntimeError:
         self.framework.log.write('WARNING: Found Matlab at '+matlab+' but unable to run\n')
