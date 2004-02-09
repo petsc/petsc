70ea15d9c7b92031508c5f750a9f5eeef1ce05fb
Kris Buschelman <buschelm@mcs.anl.gov>
2004-02-09 12:02:38 -0600

    bk-changeset-1.1929.8.1 Kris@p4buschel.|ChangeSet|20040209180238|28598 ChangeSet   1.1929.8.1 04/02/09 10:02:38 Kris@p4buschel. +1 -0   os.environ['USER'] is not in all environments by default -- thank you cygwin.
      python/PETSc/Options.py
        1.23 04/02/09 10:02:32 Kris@p4buschel. +4 -3
        Check for key before using it -- please.
    
    
    Hg-commit: 7548ad636bb69828e802ff643344ddba22802109

--- python/PETSc/Options.py
+++ python/PETSc/Options.py
@@ -18,9 +18,10 @@ class Options(config.base.Configure):
       elif bopt == 'g':
         flags.append('-g3')
       elif bopt == 'O':
-        if os.environ['USER'] in ['barrysmith','bsmith','knepley','buschelm','balay','petsc']:
-          flags.extend(['-Wshadow', '-Wwrite-strings'])
-        flags.extend(['-O', '-fomit-frame-pointer', '-Wno-strict-aliasing'])
+        if os.environ.has_key('USER')
+          if os.environ['USER'] in ['barrysmith','bsmith','knepley','buschelm','balay','petsc']:
+            flags.extend(['-Wshadow', '-Wwrite-strings'])
+          flags.extend(['-O', '-fomit-frame-pointer', '-Wno-strict-aliasing'])
     # Alpha
     elif re.match(r'alphaev[5-9]', self.framework.host_cpu):
       # Compaq C
