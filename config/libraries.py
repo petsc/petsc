13fd85abffe703f82629d7c84c5e0881c456f344
Kris Buschelman <buschelm@mcs.anl.gov>
2004-02-13 15:20:25 -0600

    bk-changeset-1.913 Kris@mbuschel.|ChangeSet|20040213212025|49661 ChangeSet   1.913 04/02/13 15:20:25 Kris@mbuschel. +1 -0   Put LIB_SUFFIX check in BuildSystem.
      config/libraries.py
        1.15 04/02/13 15:20:23 Kris@mbuschel. +17 -0
        Put LIB_SUFFIX check in BuildSystem.
    
    
    Hg-commit: 0f662a62c8817c52788ebd9d67e3786e66c18fa5

--- config/libraries.py
+++ config/libraries.py
@@ -228,10 +228,27 @@ int checkInit(void) {
       self.check('m', 'sin', prototype = 'double sin(double);', call = 'sin(1.0);\n')
     return
 
+  def checkSuffix(self):
+    '''(Belongs in config.libraries) Determine the suffix used for libraries'''
+    # If MS Windows's kernel32.lib is available then use lib for the suffix, otherwise use a.
+    # Cygwin w32api uses libkernel32.a for this symbol.
+    oldLibs = self.framework.argDB['LIBS']
+    found = self.check(['kernel32.lib'],'GetCurrentProcess',prototype='int __stdcall GetCurrentProcess(void);\n')
+    if not found:
+      found = self.check(['PSDK/kernel32.lib'],'GetCurrentProcess',prototype='int __stdcall GetCurrentProcess(void);\n')
+    if found:
+      suffix = 'lib'
+    else:
+      suffix = 'a'
+    self.addSubstitution('LIB_SUFFIX', suffix)
+    self.framework.argDB['LIBS'] = oldLibs
+    return
+
   def configure(self):
     self.framework.argDB['LIBS'] = ''
     map(lambda args: self.executeTest(self.check, list(args)), self.libraries)
     self.executeTest(self.checkMath)
+    self.executeTest(self.checkSuffix)
     self.addArgumentSubstitution('LDFLAGS', 'LDFLAGS')
     self.addArgumentSubstitution('LIBS',    'LIBS')
     return
