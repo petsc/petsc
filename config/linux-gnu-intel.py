f7803b502413ef8fc07d7dd855d2a76827eaf079
Satish Balay <balay@mcs.anl.gov>
2004-02-17 17:32:26 -0600

    bk-changeset-1.1929.1.102 balay@asterix.mcs.anl.gov|ChangeSet|20040217233226|15252 ChangeSet   1.1929.1.102 04/02/17 17:32:26 balay@asterix.mcs.anl.gov +2 -0   mpich 1.2.5 -> mpich-2.1.5.2
      config/linux-gnu-intel.py
        1.10 04/02/17 17:32:25 balay@asterix.mcs.anl.gov +2 -2
        mpich 1.2.5 -> mpich-2.1.5.2
    
      bmake/linux_intel/packages
        1.25 04/02/17 17:32:25 balay@asterix.mcs.anl.gov +1 -1
        mpich 1.2.5 -> mpich-2.1.5.2
    
    
    Hg-commit: 994cb52832354221f2e0953105187ff1a4375bef

--- config/linux-gnu-intel.py
+++ config/linux-gnu-intel.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
         '--with-cc=icc',
 	'--with-fc=ifc',
 	'--with-cxx=icc',
-        '--with-mpi-include=/home/petsc/soft/linux-rh73-intel/mpich-1.2.5/include',
-        '--with-mpi-lib=[/home/petsc/soft/linux-rh73-intel/mpich-1.2.5/lib/libmpich.a,libpmpich.a]',
+        '--with-mpi-include=/home/petsc/soft/linux-rh73-intel/mpich-1.2.5.2/include',
+        '--with-mpi-lib=[/home/petsc/soft/linux-rh73-intel/mpich-1.2.5.2/lib/libmpich.a,libpmpich.a]',
         '--with-mpirun=mpirun',
 	'--with-blas-lapck-dir=/home/petsc/soft/linux-rh73-intel/mkl-52'
         ]
