482edd0eeff028c9537ea9cfd98c930c1564dc2d
Satish Balay <balay@mcs.anl.gov>
2004-02-18 17:48:41 -0600

    bk-changeset-1.1929.1.114 balay@asterix.mcs.anl.gov|ChangeSet|20040218234841|09984 ChangeSet   1.1929.1.114 04/02/18 17:48:41 balay@asterix.mcs.anl.gov +2 -0   update ia64 nightly builds
      maint/confignightly/ksl1
        1.19 04/02/18 17:48:40 balay@asterix.mcs.anl.gov +3 -0
        update ia64 nightly builds
    
      config/linux-gnu-ia64.py
        1.5 04/02/18 17:48:40 balay@asterix.mcs.anl.gov +3 -2
        update ia64 nightly builds
    
    
    Hg-commit: eabda7bef122dde05197493771c6081d8a049197

--- config/linux-gnu-ia64.py
+++ config/linux-gnu-ia64.py
@@ -4,8 +4,9 @@ if __name__ == '__main__':
     import configure
 
     configure_options = [
-        # cannot build shared libraries are our particular test machine
-         '--with-shared=0'
+        # -lg2c messes up shared libraries
+         '--with-shared=0',
+         '--with-mpi-dir=/home/petsc/soft/linux-ia64/mpich-1.2.5.2'
         ]
 
     configure.petsc_configure(configure_options)
