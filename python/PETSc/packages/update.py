e6ee457dd004bdfeadb95a03bb5822234d34a331
Barry Smith <bsmith@mcs.anl.gov>
2004-02-19 11:23:34 -0600

    bk-changeset-1.1929.33.2 barrysmith@barrysmith-57.mcs.anl.gov|ChangeSet|20040219172334|16496 ChangeSet   1.1929.33.2 04/02/19 11:23:34 barrysmith@barrysmith-57.mcs.anl.gov +2 -0   fix bug in checking if PETSC_ARCH ok and made make info only print info   about compilers being used
      makefile
        1.95 04/02/19 11:21:38 barrysmith@barrysmith-57.mcs.anl.gov +11 -5
        only print info about fortran and c++ compilers/linkers if they are being used
    
      python/PETSc/packages/update.py
        1.55 04/02/19 11:15:54 barrysmith@barrysmith-57.mcs.anl.gov +1 -1
        fool, put petsc_dir when meant _arch
    
    
    Hg-commit: 5277915fd5d18373412ee554c0dacf9f6263eaf1

--- python/PETSc/packages/update.py
+++ python/PETSc/packages/update.py
@@ -77,7 +77,7 @@ class Configure(config.base.Configure):
     self.addDefine('ARCH_NAME', '"'+arch+'"')
 
     # Check if PETSC_ARCH is a built-in arch
-    if os.path.isdir(os.path.join('bmake',self.framework.argDB['PETSC_DIR'])) and not os.path.isfile(os.path.join('bmake',self.framework.argDB['PETSC_DIR'],'configure.py')):
+    if os.path.isdir(os.path.join('bmake',self.framework.argDB['PETSC_ARCH'])) and not os.path.isfile(os.path.join('bmake',self.framework.argDB['PETSC_ARCH'],'configure.py')):
       raise RuntimeError('The selected PETSC_ARCH is not allowed with config/configure.py\nbecause it clashes with a built-in PETSC_ARCH\nplease rerun config/configure.py with -PETSC_ARCH=somethingelse')
     
     # if PETSC_ARCH is not set use one last created with configure
