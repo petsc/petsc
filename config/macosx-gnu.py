2c8eaf57587658ca92cc29409d1f0d12eec9ade5
Satish Balay <balay@mcs.anl.gov>
2004-02-23 11:33:25 -0600

    bk-changeset-1.1949 balay@asterix.mcs.anl.gov|ChangeSet|20040223173325|06394 ChangeSet   1.1949 04/02/23 11:33:25 balay@asterix.mcs.anl.gov +1 -0   macosx - no c++
      config/macosx-gnu.py
        1.29 04/02/23 11:33:24 balay@asterix.mcs.anl.gov +1 -0
        macosx - no c++
    
    
    Hg-commit: b4ed70206f57c91c320ee631bece29e18c9afd38

--- config/macosx-gnu.py
+++ config/macosx-gnu.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
 
     configure_options = [
         # mpi built with gcc/g77 (fink)
+        '--with-cxx=0',
         '--with-mpi-dir=/home/petsc/soft/darwin-7/mpich-1.2.5.2'
         ]
 
