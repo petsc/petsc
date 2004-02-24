71306c6034aad51e421752d8995127b0b6206e31
jroman <jroman@hypercat1-1.dsic.upv.es>
2004-02-24 05:25:02 -0600

    bk-changeset-1.1955.1.2 jroman@hypercat1-1.dsic.upv.es|ChangeSet|20040224112502|29158 ChangeSet   1.1955.1.2 04/02/24 12:25:02 jroman@hypercat1-1.dsic.upv.es +14 -0   Added subdirectories chaco, jostle, party and scotch in src/mat/partition/impls   to interface PETSc to several external libraries for sparse matrix partitioning.   The file src/mat/partitioning/impls/readme includes a short description.
      BitKeeper/etc/logging_ok
        1.78 04/02/24 12:25:02 jroman@hypercat1-1.dsic.upv.es +1 -0
        Logging to logging@openlogging.org accepted
    
      src/mat/partition/impls/scotch/scotch.c
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +667 -0
    
      src/mat/partition/impls/scotch/makefile
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +18 -0
    
      src/mat/partition/impls/readme
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +57 -0
    
      src/mat/partition/impls/party/party.c
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +428 -0
    
      src/mat/partition/impls/party/makefile
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +18 -0
    
      src/mat/partition/impls/jostle/makefile
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +18 -0
    
      src/mat/partition/impls/jostle/jostle.c
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +308 -0
    
      src/mat/partition/impls/chaco/makefile
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +18 -0
    
      src/mat/partition/impls/chaco/chaco.c
        1.1 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +527 -0
    
      src/mat/partition/spartition.c
        1.6 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +16 -0
        Register new partitioning types
    
      src/mat/partition/impls/scotch/scotch.c
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/scotch/scotch.c
    
      src/mat/partition/impls/scotch/makefile
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/scotch/makefile
    
      src/mat/partition/impls/readme
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/readme
    
      src/mat/partition/impls/party/party.c
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/party/party.c
    
      src/mat/partition/impls/party/makefile
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/party/makefile
    
      src/mat/partition/impls/makefile
        1.5 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +1 -1
        New directories for external partitioning libraries
    
      src/mat/partition/impls/jostle/makefile
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/jostle/makefile
    
      src/mat/partition/impls/jostle/jostle.c
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/jostle/jostle.c
    
      src/mat/partition/impls/chaco/makefile
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/chaco/makefile
    
      src/mat/partition/impls/chaco/chaco.c
        1.0 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +0 -0
        BitKeeper file /home/jroman/soft/petsc-dev/src/mat/partition/impls/chaco/chaco.c
    
      include/petscmat.h
        1.105 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +47 -0
        Added new MAT_PARTITIONING types, as well as prototypes of available user functions and enumerate types 
    
      bmake/common/variables
        1.57 04/02/24 12:24:54 jroman@hypercat1-1.dsic.upv.es +9 -2
        Added PETSC_HAVE_XXXX and XXXX_LIB for new partitioning interfaces
    
    
    Hg-commit: b3aa0659af61a40078ba557001941612c3251a5c

---
+++ src/mat/partition/impls/jostle/jostle.c
@@ -0,0 +1,308 @@
+
+#include "src/mat/impls/adj/mpi/mpiadj.h"       /*I "petscmat.h" I */
+
+#ifdef PETSC_HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef PETSC_HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "petscfix.h"
+
+EXTERN_C_BEGIN
+
+#include "jostle.h"
+/* this function is not declared in 'jostle.h' */
+extern void pjostle_comm(MPI_Comm * comm);
+
+EXTERN_C_END 
+
+typedef struct {
+    int output;
+    int coarse_seq;
+    int nbvtxcoarsed;           /* number of vertices for the coarse graph */
+    char *mesg_log;
+} MatPartitioning_Jostle;
+
+#define SIZE_LOG 10000          /* size of buffer for msg_log */
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningApply_Jostle"
+static int MatPartitioningApply_Jostle(MatPartitioning part, IS * partitioning)
+{
+    int ierr, size, rank, i;
+    Mat mat = part->adj, matMPI;
+    Mat_MPIAdj *adj = (Mat_MPIAdj *) mat->data;
+    MatPartitioning_Jostle *jostle_struct =
+        (MatPartitioning_Jostle *) part->data;
+    PetscTruth flg;
+#ifdef PETSC_HAVE_UNISTD_H
+    int fd_stdout, fd_pipe[2], count;
+#endif
+
+    PetscFunctionBegin;
+
+    /* check that the number of partitions is equal to the number of processors */
+    ierr = MPI_Comm_rank(mat->comm, &rank);CHKERRQ(ierr);
+    ierr = MPI_Comm_size(mat->comm, &size);CHKERRQ(ierr);
+    if (part->n != size) {
+        SETERRQ(PETSC_ERR_SUP, "Supports exactly one domain per processor");
+    }
+
+    /* convert adjacency matrix to MPIAdj if needed*/
+    ierr = PetscTypeCompare((PetscObject) mat, MATMPIADJ, &flg);CHKERRQ(ierr);
+    if (!flg) {
+        ierr = MatConvert(mat, MATMPIADJ, &matMPI);CHKERRQ(ierr);
+    } else
+        matMPI = mat;
+
+    adj = (Mat_MPIAdj *) matMPI->data;  /* adj contains adjacency graph */
+    {
+        /* definition of Jostle library arguments */
+        int nnodes = matMPI->M; /* number of vertices in full graph */
+        int offset = 0;         /* 0 for C array indexing */
+        int core = matMPI->m;
+        int halo = 0;           /* obsolete with contiguous format */
+        int *index_jostle;      /* contribution of each processor */
+        int nparts = part->n;
+        int *part_wt = NULL;
+
+        int *partition;         /* set number of each vtx (length n) */
+        int *degree;            /* degree for each core nodes */
+        int *edges = adj->j;
+        int *node_wt = NULL;    /* nodes weights */
+        int *edge_wt = NULL;    /* edges weights */
+        double *coords = NULL;  /* not used (cf jostle documentation) */
+
+        int local_nedges = adj->nz;
+        int dimension = 0;      /* not used */
+        int output_level = jostle_struct->output;
+        char env_str[256];
+
+        /* allocate index_jostle */
+        ierr = PetscMalloc(nparts * sizeof(int), &index_jostle);CHKERRQ(ierr);
+
+        /* compute number of core nodes for each one */
+        for (i = 0; i < nparts - 1; i++)
+            index_jostle[i] = adj->rowners[i + 1] - adj->rowners[i];
+        index_jostle[nparts - 1] = nnodes - adj->rowners[nparts - 1];
+
+        /* allocate the partition vector */
+        ierr = PetscMalloc(core * sizeof(int), &partition);CHKERRQ(ierr); 
+
+        /* build the degree vector and the local_nedges value */
+        ierr = PetscMalloc(core * sizeof(int), &degree);CHKERRQ(ierr);
+        for (i = 0; i < core; i++)
+            degree[i] = adj->i[i + 1] - adj->i[i];
+
+        /* library call */
+        pjostle_init(&size, &rank);
+        pjostle_comm(&matMPI->comm);
+        jostle_env("format = contiguous");
+        jostle_env("timer = off");
+
+        sprintf(env_str, "threshold = %d", jostle_struct->nbvtxcoarsed);
+        jostle_env(env_str);
+
+        if (jostle_struct->coarse_seq)
+            jostle_env("matching = local");
+
+        /* redirect output */
+#ifdef PETSC_HAVE_UNISTD_H
+        fd_stdout = dup(1);
+        pipe(fd_pipe);
+        close(1);
+        dup2(fd_pipe[1], 1);
+#endif
+
+        pjostle(&nnodes, &offset, &core, &halo, index_jostle, degree, node_wt,
+            partition, &local_nedges, edges, edge_wt, &nparts,
+            part_wt, &output_level, &dimension, coords);
+
+        printf("Jostle Partitioner statistics\ncut : %d, balance : %f, runtime : %f, mem used : %d\n",
+            jostle_cut(), jostle_bal(), jostle_tim(), jostle_mem());
+
+#ifdef PETSC_HAVE_UNISTD_H
+        ierr = PetscMalloc(SIZE_LOG * sizeof(char), &(jostle_struct->mesg_log));CHKERRQ(ierr);
+        fflush(stdout);
+        count = read(fd_pipe[0], jostle_struct->mesg_log, (SIZE_LOG - 1) * sizeof(char));
+        if (count < 0)
+            count = 0;
+        jostle_struct->mesg_log[count] = 0;
+        close(1);
+        dup2(fd_stdout, 1);
+        close(fd_stdout);
+        close(fd_pipe[0]);
+        close(fd_pipe[1]);
+#endif
+
+        /* We free the memory used by jostle */
+        ierr = PetscFree(index_jostle);CHKERRQ(ierr);
+        ierr = PetscFree(degree);CHKERRQ(ierr);
+
+        /* Creation of the index set */
+        ierr = ISCreateGeneral(part->comm, mat->m, partition, partitioning);CHKERRQ(ierr);
+
+        if (matMPI != mat) {
+            ierr = MatDestroy(matMPI);CHKERRQ(ierr);
+        }
+
+        ierr = PetscFree(partition);CHKERRQ(ierr);
+    }
+
+    PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningView_Jostle"
+int MatPartitioningView_Jostle(MatPartitioning part, PetscViewer viewer)
+{
+
+    MatPartitioning_Jostle *jostle_struct =
+        (MatPartitioning_Jostle *) part->data;
+    int ierr;
+    PetscTruth isascii;
+
+    PetscFunctionBegin;
+
+    ierr = PetscTypeCompare((PetscObject) viewer, PETSC_VIEWER_ASCII, &isascii);CHKERRQ(ierr);
+    if (isascii) {
+        if (jostle_struct->mesg_log) {
+            ierr = PetscViewerASCIIPrintf(viewer, "%s\n", jostle_struct->mesg_log);CHKERRQ(ierr);
+        }
+    } else {
+        SETERRQ1(1, "Viewer type %s not supported for this Jostle partitioner",
+            ((PetscObject) viewer)->type_name);
+    }
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningJostleSetCoarseLevel"
+/*@
+    MatPartitioningJostleSetCoarseLevel - Set the coarse level 
+    
+  Input Parameter:
+.  part - the partitioning context
+.  level - the coarse level in range [0.0,1.0]
+
+   Level: advanced
+
+@*/
+int MatPartitioningJostleSetCoarseLevel(MatPartitioning part, PetscReal level)
+{
+    MatPartitioning_Jostle *jostle_struct =
+        (MatPartitioning_Jostle *) part->data;
+
+    PetscFunctionBegin;
+
+    if (level < 0.0 || level > 1.0) {
+        SETERRQ(PETSC_ERR_ARG_OUTOFRANGE,
+            "Jostle: level of coarsening out of range [0.0-1.0]");
+    } else
+        jostle_struct->nbvtxcoarsed = part->adj->N * level;
+
+    if (jostle_struct->nbvtxcoarsed < 20)
+        jostle_struct->nbvtxcoarsed = 20;
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningJostleSetCoarseSequential"
+/*@
+     MatPartitioningJostleSetCoarseSequential - Use the sequential code to 
+         do the partitioning of the coarse grid.
+
+  Input Parameter:
+.  part - the partitioning context
+
+   Level: advanced
+
+@*/
+int MatPartitioningJostleSetCoarseSequential(MatPartitioning part)
+{
+    MatPartitioning_Jostle *jostle_struct =
+        (MatPartitioning_Jostle *) part->data;
+    PetscFunctionBegin;
+    jostle_struct->coarse_seq = 1;
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningSetFromOptions_Jostle"
+int MatPartitioningSetFromOptions_Jostle(MatPartitioning part)
+{
+    int ierr;
+    PetscTruth flag;
+    PetscReal level;
+
+    PetscFunctionBegin;
+    ierr = PetscOptionsHead("Set Jostle partitioning options");CHKERRQ(ierr);
+
+    ierr = PetscOptionsReal("-mat_partitioning_jostle_coarse_level",
+        "Coarse level", "MatPartitioningJostleSetCoarseLevel", 0,
+        &level, &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningJostleSetCoarseLevel(part, level);CHKERRQ(ierr);
+
+    ierr = PetscOptionsName("-mat_partitioning_jostle_coarse_sequential",
+        "Use sequential coarse partitioner",
+        "MatPartitioningJostleSetCoarseSequential", &flag);CHKERRQ(ierr);
+    if (flag) {
+        ierr = MatPartitioningJostleSetCoarseSequential(part);CHKERRQ(ierr);
+    }
+
+    ierr = PetscOptionsTail();CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningDestroy_Jostle"
+int MatPartitioningDestroy_Jostle(MatPartitioning part)
+{
+    MatPartitioning_Jostle *jostle_struct =
+        (MatPartitioning_Jostle *) part->data;
+    int ierr;
+
+    PetscFunctionBegin;
+
+    if (jostle_struct->mesg_log) {
+        ierr = PetscFree(jostle_struct->mesg_log);CHKERRQ(ierr);
+    }
+    ierr = PetscFree(jostle_struct);CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningCreate_Jostle"
+int MatPartitioningCreate_Jostle(MatPartitioning part)
+{
+    int ierr;
+    MatPartitioning_Jostle *jostle_struct;
+
+    PetscFunctionBegin;
+    ierr = PetscNew(MatPartitioning_Jostle, &jostle_struct);CHKERRQ(ierr);
+
+    jostle_struct->nbvtxcoarsed = 20;
+    jostle_struct->output = 0;
+    jostle_struct->coarse_seq = 0;
+    jostle_struct->mesg_log = NULL;
+
+    part->ops->apply = MatPartitioningApply_Jostle;
+    part->ops->view = MatPartitioningView_Jostle;
+    part->ops->destroy = MatPartitioningDestroy_Jostle;
+    part->ops->setfromoptions = MatPartitioningSetFromOptions_Jostle;
+    part->data = (void *) jostle_struct;
+
+    PetscFunctionReturn(0);
+}
+
+EXTERN_C_END
---
+++ src/mat/partition/impls/party/party.c
@@ -0,0 +1,428 @@
+
+#include "src/mat/impls/adj/mpi/mpiadj.h"       /*I "petscmat.h" I */
+
+#ifdef PETSC_HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef PETSC_HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "petscfix.h"
+
+/* 
+   Currently using Party-1.99
+*/
+EXTERN_C_BEGIN
+#include "party_lib.h"
+EXTERN_C_END 
+
+typedef struct {
+    char redm[15];
+    char redo[15];
+    int rec;
+    int output;
+    char global_method[15];     /* global method */
+    char local_method[15];      /* local method */
+    int nbvtxcoarsed;           /* number of vertices for the coarse graph */
+    char *mesg_log;
+} MatPartitioning_Party;
+
+#define SIZE_LOG 10000          /* size of buffer for msg_log */
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningApply_Party"
+static int MatPartitioningApply_Party(MatPartitioning part, IS * partitioning)
+{
+    int ierr, *locals, *parttab = NULL, rank, size;
+    Mat mat = part->adj, matMPI, matSeq;
+    int nb_locals;              
+    Mat_MPIAdj *adj = (Mat_MPIAdj *) mat->data;
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+    PetscTruth flg;
+#ifdef PETSC_HAVE_UNISTD_H
+    int fd_stdout, fd_pipe[2], count;
+#endif
+
+    PetscFunctionBegin;
+
+    /* check if the matrix is sequential, use MatGetSubMatrices if necessary */
+    ierr = PetscTypeCompare((PetscObject) mat, MATMPIADJ, &flg);CHKERRQ(ierr);
+    ierr = MPI_Comm_size(mat->comm, &size);CHKERRQ(ierr);
+    ierr = MPI_Comm_rank(part->comm, &rank);CHKERRQ(ierr);
+    if (size > 1) {
+        int M, N;
+        IS isrow, iscol;
+        Mat *A;
+
+        if (flg) {
+            SETERRQ(0,
+                "Distributed matrix format MPIAdj is not supported for sequential partitioners");
+        }
+        ierr =
+            PetscPrintf(part->comm,
+            "Converting distributed matrix to sequential: this could be a performance loss\n");CHKERRQ(ierr);
+
+        ierr = MatGetSize(mat, &M, &N);CHKERRQ(ierr);
+        ierr = ISCreateStride(PETSC_COMM_SELF, M, 0, 1, &isrow);CHKERRQ(ierr);
+        ierr = ISCreateStride(PETSC_COMM_SELF, N, 0, 1, &iscol);CHKERRQ(ierr);
+        ierr = MatGetSubMatrices(mat, 1, &isrow, &iscol, MAT_INITIAL_MATRIX, &A);CHKERRQ(ierr);
+        ierr = ISDestroy(isrow);CHKERRQ(ierr);
+        ierr = ISDestroy(iscol);CHKERRQ(ierr);
+        matSeq = *A;
+    } else
+        matSeq = mat;
+
+    /* check for the input format that is supported only for a MPIADJ type 
+       and set it to matMPI */
+
+    if (!flg) {
+        ierr = MatConvert(matSeq, MATMPIADJ, &matMPI);CHKERRQ(ierr);
+    } else
+        matMPI = matSeq;
+
+    adj = (Mat_MPIAdj *) matMPI->data;  /* finaly adj contains adjacency graph */
+
+    {
+        /* Party library arguments definition */
+        int n = mat->M;         /* number of vertices in full graph */
+        int *edge_p = adj->i;   /* start of edge list for each vertex */
+        int *edge = adj->j;     /* edge list data */
+        int *vertex_w = NULL;   /* weights for all vertices */
+        int *edge_w = NULL;     /* weights for all edges */
+        float *x = NULL, *y = NULL, *z = NULL;  /* coordinates for inertial method */
+        int p = part->n;        /* number of parts to create */
+        int *part_party;        /* set number of each vtx (length n) */
+        int cutsize;            /* number of edge cut */
+        char *global = party->global_method;    /* global partitioning algorithm */
+        char *local = party->local_method;      /* local partitioning algorithm */
+        int redl = party->nbvtxcoarsed; /* how many vertices to coarsen down to? */
+        char *redm = party->redm;
+        char *redo = party->redo;
+        int rec = party->rec;
+        int output = party->output;
+
+        ierr = PetscMalloc((mat->M) * sizeof(int), &part_party);CHKERRQ(ierr);
+
+        /* redirect output to buffer party->mesg_log */
+#ifdef PETSC_HAVE_UNISTD_H
+        fd_stdout = dup(1);
+        pipe(fd_pipe);
+        close(1);
+        dup2(fd_pipe[1], 1);
+        ierr = PetscMalloc(SIZE_LOG * sizeof(char), &(party->mesg_log));CHKERRQ(ierr);
+#endif
+
+        /* library call */
+
+        party_lib_times_start();
+
+        ierr = party_lib(n, vertex_w, x, y, z, edge_p, edge, edge_w,
+            p, part_party, &cutsize, redl, redm, redo,
+            global, local, rec, output);
+
+        party_lib_times_output(output);
+        part_info(n, vertex_w, edge_p, edge, edge_w, p, part_party, output);
+
+#ifdef PETSC_HAVE_UNISTD_H
+        fflush(stdout);
+        count =
+            read(fd_pipe[0], party->mesg_log, (SIZE_LOG - 1) * sizeof(char));
+        if (count < 0)
+            count = 0;
+        party->mesg_log[count] = 0;
+        close(1);
+        dup2(fd_stdout, 1);
+        close(fd_stdout);
+        close(fd_pipe[0]);
+        close(fd_pipe[1]);
+#endif
+        /* if in the call we got an error, we say it */
+
+        if (ierr) {
+            SETERRQ(1, party->mesg_log);
+        }
+
+        parttab = part_party;
+    }
+
+    /* Creation of the index set */
+    ierr = MPI_Comm_rank(part->comm, &rank);CHKERRQ(ierr);
+    ierr = MPI_Comm_size(part->comm, &size);CHKERRQ(ierr);
+    nb_locals = mat->M / size;
+    locals = parttab + rank * nb_locals;
+    if (rank < mat->M % size) {
+        nb_locals++;
+        locals += rank;
+    } else
+        locals += mat->M % size;
+    ierr = ISCreateGeneral(part->comm, nb_locals, locals, partitioning);CHKERRQ(ierr);
+
+    /* destroying old objects */
+    ierr = PetscFree(parttab);CHKERRQ(ierr);
+    if (matSeq != mat) {
+        ierr = MatDestroy(matSeq);CHKERRQ(ierr);
+    }
+    if (matMPI != mat) {
+        ierr = MatDestroy(matMPI);CHKERRQ(ierr);
+    }
+
+    PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningView_Party"
+int MatPartitioningView_Party(MatPartitioning part, PetscViewer viewer)
+{
+
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+    int ierr, rank;
+    PetscTruth isascii;
+
+    PetscFunctionBegin;
+
+    ierr = MPI_Comm_rank(part->comm, &rank);CHKERRQ(ierr);
+    ierr = PetscTypeCompare((PetscObject) viewer, PETSC_VIEWER_ASCII, &isascii);CHKERRQ(ierr);
+    if (isascii) {
+        if (!rank && party->mesg_log) {
+            ierr = PetscViewerASCIIPrintf(viewer, "%s\n", party->mesg_log);CHKERRQ(ierr);
+        }
+    } else {
+        SETERRQ1(1, "Viewer type %s not supported for this Party partitioner",
+            ((PetscObject) viewer)->type_name);
+    }
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningPartySetGlobal"
+/*@
+     MatPartitioningPartySetGlobal - Set method for global partitioning.
+
+  Input Parameter:
+.  part - the partitioning context
+.  method - May be one of MP_PARTY_OPT, MP_PARTY_LIN, MP_PARTY_SCA, 
+    MP_PARTY_RAN, MP_PARTY_GBF, MP_PARTY_GCF, MP_PARTY_BUB or MP_PARTY_DEF, or
+    alternatively a string describing the method. Two or more methods can be 
+    combined like "gbf,gcf". Check the Party Library Users Manual for details.
+
+   Level: advanced
+
+@*/
+int MatPartitioningPartySetGlobal(MatPartitioning part, const char *global)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+
+    PetscFunctionBegin;
+
+    PetscStrcpy(party->global_method, global);
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningPartySetLocal"
+/*@
+     MatPartitioningPartySetLocal - Set method for local partitioning.
+
+  Input Parameter:
+.  part - the partitioning context
+.  method - One of MP_PARTY_HELPFUL_SETS, MP_PARTY_KERNIGHAN_LIN, or MP_PARTY_NONE. 
+    Check the Party Library Users Manual for details.
+
+   Level: advanced
+
+@*/
+int MatPartitioningPartySetLocal(MatPartitioning part, const char *local)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+
+    PetscFunctionBegin;
+
+    PetscStrcpy(party->local_method, local);
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningPartySetCoarseLevel"
+/*@
+    MatPartitioningPartySetCoarseLevel - Set the coarse level 
+    
+  Input Parameter:
+.  part - the partitioning context
+.  level - the coarse level in range [0.0,1.0]
+
+   Level: advanced
+
+@*/
+int MatPartitioningPartySetCoarseLevel(MatPartitioning part, PetscReal level)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+
+    PetscFunctionBegin;
+
+    if (level < 0 || level > 1.0) {
+        SETERRQ(PETSC_ERR_ARG_OUTOFRANGE,
+            "Party: level of coarsening out of range [0.01-1.0]");
+    } else
+        party->nbvtxcoarsed = part->adj->N * level;
+
+    if (party->nbvtxcoarsed < 20)
+        party->nbvtxcoarsed = 20;
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningPartySetMatchOptimization"
+/*@
+    MatPartitioningPartySetMatchOptimization - Activate matching optimization for graph reduction 
+    
+  Input Parameter:
+.  part - the partitioning context
+.  opt - activate optimization
+
+   Level: advanced
+
+@*/
+int MatPartitioningPartySetMatchOptimization(MatPartitioning part,
+    PetscTruth opt)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+
+    PetscFunctionBegin;
+
+    if (opt)
+        PetscStrcpy(party->redo, "w3");
+    else
+        PetscStrcpy(party->redo, "");
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningPartySetBipart"
+/*@
+    MatPartitioningPartySetBipart - Activate or deactivate recursive bisection.
+    
+  Input Parameter:
+.  part - the partitioning context
+.  bp - PETSC_TRUE to activate recursive bisection 
+
+   Level: advanced
+
+@*/
+int MatPartitioningPartySetBipart(MatPartitioning part, PetscTruth bp)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+
+    PetscFunctionBegin;
+
+    if (bp)
+        party->rec = 1;
+    else
+        party->rec = 0;
+
+    PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningSetFromOptions_Party"
+int MatPartitioningSetFromOptions_Party(MatPartitioning part)
+{
+    int ierr;
+    PetscTruth flag, b;
+    char value[15];
+    PetscReal r;
+
+    PetscFunctionBegin;
+    ierr = PetscOptionsHead("Set Party partitioning options");CHKERRQ(ierr);
+
+    ierr = PetscOptionsString("-mat_partitioning_party_global",
+        "Global method to use", "MatPartitioningPartySetGlobal", "gcf,gbf",
+        value, 15, &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningPartySetGlobal(part, value);CHKERRQ(ierr);
+
+    ierr = PetscOptionsString("-mat_partitioning_party_local",
+        "Local method to use", "MatPartitioningPartySetLocal", "kl", value, 15,
+        &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningPartySetLocal(part, value);CHKERRQ(ierr);
+
+    ierr = PetscOptionsReal("-mat_partitioning_party_coarse_level",
+        "Coarse level", "MatPartitioningPartySetCoarseLevel", 0, &r,
+        &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningPartySetCoarseLevel(part, r);CHKERRQ(ierr);
+
+    ierr = PetscOptionsLogical("-mat_partitioning_party_match_optimization",
+        "Matching optimization on/off (boolean)",
+        "MatPartitioningPartySetMatchOptimization", PETSC_TRUE, &b, &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningPartySetMatchOptimization(part, b);CHKERRQ(ierr);
+
+    ierr = PetscOptionsLogical("-mat_partitioning_party_bipart",
+        "Bipartitioning option on/off (boolean)",
+        "MatPartitioningPartySetBipart", PETSC_TRUE, &b, &flag);CHKERRQ(ierr);
+    if (flag)
+        ierr = MatPartitioningPartySetBipart(part, b);CHKERRQ(ierr);
+
+    ierr = PetscOptionsTail();CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningDestroy_Party"
+int MatPartitioningDestroy_Party(MatPartitioning part)
+{
+    MatPartitioning_Party *party = (MatPartitioning_Party *) part->data;
+    int ierr;
+
+    PetscFunctionBegin;
+
+    if (party->mesg_log) {
+        ierr = PetscFree(party->mesg_log);CHKERRQ(ierr);
+    }
+
+    ierr = PetscFree(party);CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatPartitioningCreate_Party"
+int MatPartitioningCreate_Party(MatPartitioning part)
+{
+    int ierr;
+    MatPartitioning_Party *party;
+
+    PetscFunctionBegin;
+    ierr = PetscNew(MatPartitioning_Party, &party);CHKERRQ(ierr);
+
+    PetscStrcpy(party->global_method, "gcf,gbf");
+    PetscStrcpy(party->local_method, "kl");
+    PetscStrcpy(party->redm, "lam");
+    PetscStrcpy(party->redo, "w3");
+
+    party->nbvtxcoarsed = 200;
+    party->rec = 1;
+    party->output = 1;
+    party->mesg_log = NULL;
+
+    part->ops->apply = MatPartitioningApply_Party;
+    part->ops->view = MatPartitioningView_Party;
+    part->ops->destroy = MatPartitioningDestroy_Party;
+    part->ops->setfromoptions = MatPartitioningSetFromOptions_Party;
+    part->data = (void *) party;
+
+    PetscFunctionReturn(0);
+}
+
+EXTERN_C_END
