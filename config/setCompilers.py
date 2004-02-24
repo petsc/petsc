784c1c41735ef3ad8d7c951162175b6378a2fd7c
Barry Smith <bsmith@mcs.anl.gov>
2004-02-24 16:06:29 -0600

    bk-changeset-1.933.1.4 barrysmith@barrysmith-57.mcs.anl.gov|ChangeSet|20040224220629|56103 ChangeSet   1.933.1.4 04/02/24 16:06:29 barrysmith@barrysmith-57.mcs.anl.gov +1 -0   add to search path for win32fe
      config/setCompilers.py
        1.39 04/02/24 16:06:18 barrysmith@barrysmith-57.mcs.anl.gov +4 -1
        add to search path for win32fe
    
    
    Hg-commit: 6b3c5e646d42bbc5b6e337a16db8722628f2eb2b

--- config/setCompilers.py
+++ config/setCompilers.py
@@ -97,7 +97,10 @@ class Configure(config.base.Configure):
     if self.framework.argDB['with-vendor-compilers'] == 'yes': self.framework.argDB['with-vendor-compilers'] = ''      
     if self.framework.argDB['with-vendor-compilers'] == 'false': self.framework.argDB['with-vendor-compilers'] = '0'
     if self.framework.argDB['with-vendor-compilers'] == 'true': self.framework.argDB['with-vendor-compilers'] = ''      
-    
+
+    if 'PETSC_DIR' in self.framework.argDB:
+      self.framework.searchdirs.append(os.path.join(self.framework.argDB['PETSC_DIR'],'bin','win32fe'))
+        
     if self.framework.argDB.has_key('with-cc'):
       yield self.framework.argDB['with-cc']
       raise RuntimeError('C compiler you provided with -with-cc='+self.framework.argDB['with-cc']+' does not work')
