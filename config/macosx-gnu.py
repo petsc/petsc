1a774b9f349ebbfe92363542555e442023fc1191
Satish Balay <balay@mcs.anl.gov>
2004-03-08 18:49:25 -0600

    bk-changeset-1.2038 balay@asterix.mcs.anl.gov|ChangeSet|20040309004925|15497 ChangeSet   1.2038 04/03/08 18:49:25 balay@asterix.mcs.anl.gov +6 -0   fix macosx nightly builds - enable c++ builds with mpi (by disabling mpi-compilers)
      maint/confignightly/icrunch
        1.39 04/03/08 18:49:24 balay@asterix.mcs.anl.gov +1 -7
        don't use macosx-gnu on icrunch
    
      config/macosx-gnu
        1.6 04/03/08 18:49:24 balay@asterix.mcs.anl.gov +4 -4
        enable c++ builds on macos
    
      config/macosx-gnu.py
        1.30 04/03/08 18:49:24 balay@asterix.mcs.anl.gov +3 -3
        enable c++ builds on macos (by disabling mpicompilers)
    
      BitKeeper/deleted/.del-blaze~415f30e2e358bbe5
        1.24 04/03/08 18:28:42 balay@asterix.mcs.anl.gov +0 -0
        Delete: maint/confignightly/blaze
    
      BitKeeper/deleted/.del-darwin6.8~5352bdc66787044
        1.4 04/03/08 18:26:17 balay@asterix.mcs.anl.gov +0 -0
        Delete: config/darwin6.8
    
      BitKeeper/deleted/.del-darwin6.8.py~8e2e5aeebb77e62c
        1.22 04/03/08 18:26:17 balay@asterix.mcs.anl.gov +0 -0
        Delete: config/darwin6.8.py
    
    
    Hg-commit: bd40730844bb40cc56a145c012be00dec94d4633

--- config/macosx-gnu.py
+++ config/macosx-gnu.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     import configure
 
     configure_options = [
-        # mpi built with gcc/g77 (fink)
-        '--with-cxx=0',
-        '--with-mpi-dir=/home/petsc/soft/darwin-7/mpich-1.2.5.2'
+        # mpi built with gcc/g77 - however mpiCC is broken
+        '--with-mpi-dir=/home/petsc/soft/darwin-7/mpich-1.2.5.2',
+        '--with-mpi-compilers=0'
         ]
 
     configure.petsc_configure(configure_options)
