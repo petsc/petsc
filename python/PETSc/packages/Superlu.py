9a372d9fd6f5f438912330e729f0a3dfe332fa9e
Hong Zhang <hzhang@mcs.anl.gov>
2004-03-11 09:19:40 -0600

    bk-changeset-1.2045 hzhang@smash.mcs.anl.gov|ChangeSet|20040311151940|07536 ChangeSet   1.2045 04/03/11 09:19:40 hzhang@smash.mcs.anl.gov +2 -0   new config files for superlu and superlu_dist
      python/PETSc/packages/Superlu_dist.py
        1.1 04/03/11 09:16:36 hzhang@smash.mcs.anl.gov +147 -0
    
      python/PETSc/packages/Superlu_dist.py
        1.0 04/03/11 09:16:36 hzhang@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/python/PETSc/packages/Superlu_dist.py
    
      python/PETSc/packages/Superlu.py
        1.1 04/03/11 09:16:25 hzhang@smash.mcs.anl.gov +143 -0
    
      python/PETSc/packages/Superlu.py
        1.0 04/03/11 09:16:25 hzhang@smash.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/python/PETSc/packages/Superlu.py
    
    
    Hg-commit: 0962fcea812fee860ec2725853bb650bc94e1414

---
+++ python/PETSc/packages/Superlu.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+from __future__ import generators
+import user
+import config.base
+import os
+
+#Developed for the Superlu_3.0
+
+class Configure(config.base.Configure):
+  def __init__(self, framework):
+    config.base.Configure.__init__(self, framework)
+    self.headerPrefix = ''
+    self.substPrefix  = ''
+    self.compilers    = self.framework.require('config.compilers',self)
+    self.libraries    = self.framework.require('config.libraries',self)
+    self.found        = 0
+    self.lib          = []
+    self.include      = []
+    self.name         = 'Superlu'
+    return
+
+  def __str__(self):
+    output=''
+    if self.found:
+      output  = self.name+':\n'
+      output += '  Includes: '+self.include[0]+'\n'
+      output += '  Library: '+self.lib[0]+'\n'
+    return output
+  
+  def configureHelp(self,help):
+    import nargs
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    help.addArgument(PACKAGE,'-with-'+package+'=<bool>',nargs.ArgBool(None,1,'Indicate if you wish to test for '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-lib=<lib>',nargs.Arg(None,None,'Indicate the library containing '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-include=<dir>',nargs.ArgDir(None,None,'Indicate the directory of header files for '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the '+self.name+' installation'))
+    return
+
+  def generateIncludeGuesses(self):
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    if 'with-'+package in self.framework.argDB:
+      if 'with-'+package+'-include' in self.framework.argDB:
+        incl = self.framework.argDB['with-'+package+'-include']
+        yield('User specified '+PACKAGE+' header location',incl)
+      elif 'with-'+package+'-lib' in self.framework.argDB:
+        incl     = self.lib[0]
+        (incl,dummy) = os.path.split(incl)
+        yield('based on found library location',os.path.join(incl,'SRC'))
+      elif 'with-'+package+'-dir' in self.framework.argDB:
+        dir = self.framework.argDB['with-'+package+'-dir']
+        yield('based on found root directory',os.path.join(dir,'SRC'))
+
+  def checkInclude(self,incl,hfile):
+    if not isinstance(incl,list): incl = [incl]
+    oldFlags = self.framework.argDB['CPPFLAGS']
+    for inc in incl:
+      self.framework.argDB['CPPFLAGS'] += ' -I'+inc
+    found = self.checkPreprocess('#include <' +hfile+ '>\n')
+    self.framework.argDB['CPPFLAGS'] = oldFlags
+    if found:
+      self.framework.log.write('Found header file ' +hfile+ ' in '+incl[0]+'\n')
+    return found
+
+  def generateLibGuesses(self):
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    if 'with-'+package in self.framework.argDB:
+      if 'with-'+package+'-lib' in self.framework.argDB: #~SuperLU_3.0/superlu_linux_g.a
+        yield ('User specified '+PACKAGE+' library',self.framework.argDB['with-'+package+'-lib'])
+      elif 'with-'+package+'-include' in self.framework.argDB:
+        dir = self.framework.argDB['with-'+package+'-include'] #~SuperLU_3.0/SRC
+        (dir,dummy) = os.path.split(dir)
+        yield('User specified '+PACKAGE+'/Include',os.path.join(dir,'superlu_linux_g.a'))
+      elif 'with-'+package+'-dir' in self.framework.argDB: 
+        dir = self.framework.argDB['with-'+package+'-dir']
+        yield('User specified '+PACKAGE+' root directory',os.path.join(dir,'superlu_linux_g.a'))
+      else:
+        self.framework.log.write('Must specify either a library or installation root directory for '+PACKAGE+'\n')
+        
+  def checkLib(self,lib,libfile):
+    if not isinstance(lib,list): lib = [lib]
+    oldLibs = self.framework.argDB['LIBS']  
+    found = self.libraries.check(lib,libfile)
+    self.framework.argDB['LIBS']=oldLibs  
+    if found:
+      self.framework.log.write('Found functional '+libfile+' in '+lib[0]+'\n')
+    return found
+  
+  def configureLibrary(self):
+    '''Find a installation and check if it can work with PETSc'''
+    self.framework.log.write('==================================================================================\n')
+    found  = 0
+    foundh = 0
+    for (configstr,lib) in self.generateLibGuesses():
+      self.framework.log.write('Checking for a functional '+self.name+' in '+configstr+'\n')
+      found = self.executeTest(self.checkLib,[lib,'set_default_options'])  
+      if found:
+        self.lib = [lib]
+        break
+    if found:
+      for (inclstr,incl) in self.generateIncludeGuesses():
+        self.framework.log.write('Checking for headers '+inclstr+': '+incl+'\n')
+        foundh = self.executeTest(self.checkInclude,[incl,'dsp_defs.h'])
+        if foundh:
+          self.include = [incl]
+          self.found   = 1
+          self.setFoundOutput()
+          break
+    else:
+      self.framework.log.write('Could not find a functional '+self.name+'\n')
+      self.setEmptyOutput()
+    return
+
+  def setFoundOutput(self):
+    PACKAGE = self.name.upper()
+    self.addSubstitution(PACKAGE+'_INCLUDE','-I'+self.include[0])
+    self.addSubstitution(PACKAGE+'_LIB',' '.join(map(self.libraries.getLibArgument,self.lib)))
+    self.addDefine('HAVE_'+PACKAGE,1)
+    
+  def setEmptyOutput(self):
+    PACKAGE = self.name.upper()
+    self.addSubstitution(PACKAGE+'_INCLUDE', '')
+    self.addSubstitution(PACKAGE+'_LIB', '')
+    return
+
+  def configure(self):
+    package = self.name.lower()
+    if not 'with-'+package in self.framework.argDB:
+      self.setEmptyOutput()
+      return
+    self.executeTest(self.configureLibrary)
+    return
+
+if __name__ == '__main__':
+  import config.framework
+  import sys
+  framework = config.framework.Framework(sys.argv[1:])
+  framework.setupLogging(framework.clArgs)
+  framework.children.append(Configure(framework))
+  framework.configure()
+  framework.dumpSubstitutions()
