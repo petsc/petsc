ca14889bb8e97da85e150a9b2718d3bf167f4266
Satish Balay <balay@mcs.anl.gov>
2004-03-19 13:56:16 -0600

    bk-changeset-1.2070 balay@asterix.mcs.anl.gov|ChangeSet|20040319195616|13531 ChangeSet   1.2070 04/03/19 13:56:16 balay@asterix.mcs.anl.gov +3 -0   fix f90 options format for configure
      config/solaris2.9-lam.py
        1.2 04/03/19 13:56:15 balay@asterix.mcs.anl.gov +2 -2
        fix f90 options format for configure
    
      config/solaris-64.py
        1.14 04/03/19 13:56:15 balay@asterix.mcs.anl.gov +2 -2
        fix f90 options format for configure
    
      config/irix6.5.py
        1.10 04/03/19 13:56:15 balay@asterix.mcs.anl.gov +2 -2
        fix f90 options format for configure
    
    
    Hg-commit: 30aa2e460ac9e2421bb207e9a7d4cceb4059b56c

--- config/solaris-64.py
+++ config/solaris-64.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     '--with-64-bit',
     '--with-mpi-compilers=0',
     '--with-gnu-compilers=0',
-    '--with-f90-header=f90impl/f90_solaris.h',
-    '--with-f90-source=src/sys/src/f90/f90_solaris.c',
+    '--with-f90-header="f90impl/f90_solaris.h"',
+    '--with-f90-source="src/sys/src/f90/f90_solaris.c"',
     '--with-mpi-dir=/home/petsc/soft/solaris-9-64/mpich-1.2.5/'
     ]
 
--- config/solaris2.9-lam.py
+++ config/solaris2.9-lam.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     import configure
 
     configure_options = [
-    '--with-f90-header=f90impl/f90_solaris.h',
-    '--with-f90-source=src/sys/src/f90/f90_solaris.c',
+    '--with-f90-header="f90impl/f90_solaris.h"',
+    '--with-f90-source="src/sys/src/f90/f90_solaris.c"',
     '--with-mpi-dir=/home/petsc/soft/solaris-9-lam/lam-6.5.8'
     ]
 
