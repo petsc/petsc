b612cb482de66b7e8f94971a2449d9067e0c25b8
Matthew Knepley <knepley@mcs.anl.gov>
2004-04-03 19:28:40 -0600

    bk-changeset-1.2101.3.1 knepley@khan.mcs.anl.gov|ChangeSet|20040404012840|63750 ChangeSet   1.2101.3.1 04/04/03 19:28:40 knepley@khan.mcs.anl.gov +6 -0   Configure change
      python/PETSc/packages/Triangle.py
        1.9 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
      python/PETSc/packages/ParMetis.py
        1.9 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
      python/PETSc/packages/PVODE.py
        1.9 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
      python/PETSc/packages/PLAPACK.py
        1.9 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
      python/PETSc/packages/NetCDF.py
        1.3 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
      python/PETSc/packages/Mathematica.py
        1.9 04/04/03 19:28:37 knepley@khan.mcs.anl.gov +1 -0
        Added dependence which should exist when this is finished
    
    
    Hg-commit: 5ec4f3eee3ecb8ce381df43f4ba04d6adcbeb6ae

--- python/PETSc/packages/PVODE.py
+++ python/PETSc/packages/PVODE.py
@@ -5,6 +5,7 @@ class Configure(config.base.Configure):
     config.base.Configure.__init__(self, framework)
     self.headerPrefix = ''
     self.substPrefix  = ''
+    self.compilers    = self.framework.require('config.compilers', self)
     return
 
   def configureLibrary(self):
