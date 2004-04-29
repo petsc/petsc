ca01669a8d9885df06a81ff59787f83a7e674b91
victorle <victorle@tibook500ve.cs.utk.edu>
2004-04-29 13:03:55 -0600

    bk-changeset-1.2189.1.5 victorle@tibook500ve.cs.utk.edu|ChangeSet|20040429190355|32345 ChangeSet   1.2189.1.5 04/04/29 15:03:55 victorle@tibook500ve.cs.utk.edu +1 -0   restored accidental (?) comment-out of MPIU_INT
      include/petsc.h
        1.71 04/04/29 15:03:51 victorle@tibook500ve.cs.utk.edu +1 -1
        restored accidental (?) comment-out of MPIU_INT
    
    
    Hg-commit: 46f60300fedf84bb714aaa31c81274d36363d67e

--- include/petsc.h
+++ include/petsc.h
@@ -61,8 +61,8 @@ typedef int PetscEvent;
 typedef int PetscBLASInt;
 typedef int PetscMPIInt;
 typedef int PetscInt;
-/*
 #define MPIU_INT MPI_LONG_LONG_INT
+/*
 #undef  PETSC_PRINTF_FORMAT_CHECK
 #define PETSC_PRINTF_FORMAT_CHECK(a,b)
 #undef  PETSC_FPRINTF_FORMAT_CHECK
