b23837889b90e0862a1948d6409b185271c382fc
victorle <victorle@tibook500ve.cs.utk.edu>
2004-05-19 12:19:46 -0600

    bk-changeset-1.2255.1.1 victorle@tibook500ve.cs.utk.edu|ChangeSet|20040519181946|04448 ChangeSet   1.2255.1.1 04/05/19 14:19:46 victorle@tibook500ve.cs.utk.edu +2 -0   bcgsl code cleanup by Joel Malard
      src/ksp/ksp/impls/bcgsl/bcgsl.h
        1.3 04/05/19 14:19:42 victorle@tibook500ve.cs.utk.edu +6 -15
        bcgsl code cleanup by Joel Malard
    
      src/ksp/ksp/impls/bcgsl/bcgsl.c
        1.10 04/05/19 14:19:42 victorle@tibook500ve.cs.utk.edu +378 -430
        bcgsl code cleanup by Joel Malard
    
    
    Hg-commit: 7e5daf20c3ac81a341aa83efaf3d4d2ad0aef874

--- src/ksp/ksp/impls/bcgsl/bcgsl.h
+++ src/ksp/ksp/impls/bcgsl/bcgsl.h
@@ -8,8 +8,9 @@
 
 typedef struct {
 
-	int ell;		/* Number of search directions. */
-	PetscReal	delta;
+	int ell;			/* Number of search directions. */
+	PetscReal	delta;		/* Threshold for recomputing exact residual norm */
+	PetscTruth	bConvex;	/* Compute Enhanced BiCGstab polynomial when set to PETWSC_TRUE */
 
 	/* Workspace Vectors */
 	Vec	vB;
@@ -22,11 +23,8 @@ typedef struct {
 	/* Workspace Arrays */
 	int	ldz;	
 	int	ldzc;
-	PetscScalar	*vY0t, *vYlt;	
 	PetscScalar	*vY0c, *vYlc, *vYtc;
-	PetscScalar	*mZ, *mZc;
-
-
+	PetscScalar	*mZa, *mZb;
 } KSP_BiCGStabL;
 
 /* predefined shorthands */
@@ -37,17 +35,10 @@ typedef struct {
 #define	VTM	(bcgsl->vTm)
 #define	VVR	(bcgsl->vvR)
 #define	VVU	(bcgsl->vvU)
-#define	AY0t	(bcgsl->vY0t)
-#define	AYlt	(bcgsl->vYlt)
 #define	AY0c	(bcgsl->vY0c)
 #define	AYtc	(bcgsl->vYtc)
 #define	AYlc	(bcgsl->vYlc)
-#define MZ	(bcgsl->mZ)
-#define MZc	(bcgsl->mZc)
-
-#define LDZ	(bcgsl->ldz)
-#define LDZc	(bcgsl->ldzc)
-
-#define	DELTA	(bcgsl->delta)
+#define MZa	(bcgsl->mZa)
+#define MZb	(bcgsl->mZb)
 
 #endif
