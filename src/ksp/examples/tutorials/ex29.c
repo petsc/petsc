d68bfbca503373580de0fb4070ae6c29cf75cbe8
Barry Smith <bsmith@mcs.anl.gov>
2004-05-29 12:23:39 -0600

    bk-changeset-1.2276.9.4 barrysmith@barry-smiths-computer.local|ChangeSet|20040529182339|00592 ChangeSet   1.2276.9.4 04/05/29 13:23:39 barrysmith@barry-smiths-computer.local +5 -0   fix Matt's ex29.c for Neumman BC and cleanup slightly   add nightly tests for it
      src/docs/tex/petscapp.bib
        1.115 04/05/29 13:23:37 barrysmith@barry-smiths-computer.local +14 -5
        added two talks by folks Matt spoke with in NY
        hmm, should list presentation differently than papers
    
      src/ksp/examples/tutorials/ex29.c
        1.6 04/05/29 13:22:37 barrysmith@barry-smiths-computer.local +5 -1
        got to get inner pc from redundant for coarse grid to handle singular matrix
    
      src/ksp/examples/tutorials/makefile
        1.47 04/05/29 13:18:52 barrysmith@barry-smiths-computer.local +14 -1
        add nightly tests for ex29
    
      src/ksp/examples/tutorials/output/ex29_1.out
        1.1 04/05/29 13:16:13 barrysmith@barry-smiths-computer.local +3 -0
    
      src/ksp/examples/tutorials/output/ex29_1.out
        1.0 04/05/29 13:16:13 barrysmith@barry-smiths-computer.local +0 -0
        BitKeeper file /Users/barrysmith/petsc-dev/src/ksp/examples/tutorials/output/ex29_1.out
    
      src/ksp/examples/tutorials/ex29.c
        1.5 04/05/29 13:08:59 barrysmith@barry-smiths-computer.local +30 -35
        simplied command line argument processing and converted to use
        PetscInt
    
      src/ksp/examples/tutorials/ex29.c
        1.4 04/05/29 12:59:01 barrysmith@barry-smiths-computer.local +43 -23
        fixed many Neumann BC problems
    
      todo
        1.63 04/05/29 12:56:57 barrysmith@barry-smiths-computer.local +2 -0
        *** empty log message ***
    
    
    Hg-commit: 57061c1fb73ba7a93b6d1d239bface50add948c5

--- src/ksp/examples/tutorials/ex29.c
+++ src/ksp/examples/tutorials/ex29.c
@@ -13,9 +13,11 @@ with forcing function
 
    f = e^{-(1 - x)^2/\nu} e^{-(1 - y)^2/\nu}
 
-with boundary conditions
+with Dirichlet boundary conditions
 
    u = f(x,y) for x = 0, x = 1, y = 0, y = 1
+
+or pure Neumman boundary conditions
 
 This uses multigrid to solve the linear system
 */
@@ -24,29 +26,31 @@ static char help[] = "Solves 2D inhomogeneous Laplacian using multigrid.\n\n";
 
 #include "petscda.h"
 #include "petscksp.h"
+#include "petscmg.h"
 
-extern int ComputeJacobian(DMMG,Mat);
-extern int ComputeRHS(DMMG,Vec);
+extern PetscErrorCode ComputeJacobian(DMMG,Mat);
+extern PetscErrorCode ComputeRHS(DMMG,Vec);
 
 typedef enum {DIRICHLET, NEUMANN} BCType;
 
 typedef struct {
-  PetscScalar nu;
-  BCType      bcType;
+  PetscScalar   nu;
+  BCType        bcType;
+  MatNullSpace  nullSpace;
 } UserContext;
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
 int main(int argc,char **argv)
 {
-  DMMG       *dmmg;
-  DA          da;
-  UserContext user;
-  char        bcTypeName[256];
-  PetscReal   norm;
-  PetscScalar mone = -1.0;
-  PetscTruth  isDirichlet, isNeumann, iopt;
-  int         l, ierr;
+  DMMG           *dmmg;
+  DA             da;
+  UserContext    user;
+  PetscReal      norm;
+  PetscScalar    mone = -1.0;
+  const char     *bcTypes[2] = {"dirichlet","neumann"};
+  PetscErrorCode ierr;
+  PetscInt       l;
 
   PetscInitialize(&argc,&argv,(char *)0,help);
 
@@ -54,32 +58,42 @@ int main(int argc,char **argv)
   ierr = DACreate2d(PETSC_COMM_WORLD,DA_NONPERIODIC,DA_STENCIL_STAR,3,3,PETSC_DECIDE,PETSC_DECIDE,1,1,0,0,&da);CHKERRQ(ierr);  
   ierr = DMMGSetDM(dmmg,(DM)da);
   ierr = DADestroy(da);CHKERRQ(ierr);
-  for(l = 0; l < DMMGGetLevels(dmmg); l++) {
+  for (l = 0; l < DMMGGetLevels(dmmg); l++) {
     ierr = DMMGSetUser(dmmg,l,&user);CHKERRQ(ierr);
   }
 
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "", "Options for the inhomogeneous Poisson equation", "DMMG");
-  user.nu = 0.1;
-  ierr = PetscOptionsScalar("-nu", "The width of the Gaussian source", "ex29.c", 0.1, &user.nu, &iopt);CHKERRQ(ierr);
-  ierr = PetscOptionsString("-bc_type", "Type of boundary condition", "ex29.c", "dirichlet", bcTypeName, 256, &iopt);CHKERRQ(ierr);
-  ierr = PetscStrcasecmp(bcTypeName, "dirichlet", &isDirichlet);CHKERRQ(ierr);
-  ierr = PetscStrcasecmp(bcTypeName, "neumann", &isNeumann);CHKERRQ(ierr);
-  if (isDirichlet) {
+    user.nu = 0.1;
+    ierr = PetscOptionsScalar("-nu", "The width of the Gaussian source", "ex29.c", 0.1, &user.nu, PETSC_NULL);CHKERRQ(ierr);
     user.bcType = DIRICHLET;
-  } else if (isNeumann) {
-    user.bcType = NEUMANN;
-  } else {
-    user.bcType = DIRICHLET;
-  }
+    ierr = PetscOptionsEList("-bc_type","Type of boundary condition","ex29.c",bcTypes,2,bcTypes[0],(int*)&user.bcType,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();
 
   ierr = DMMGSetKSP(dmmg,ComputeRHS,ComputeJacobian);CHKERRQ(ierr);
   if (user.bcType == NEUMANN) {
-    MatNullSpace nullSpace;
+    PC           pc;
+    KSP          ksp;
+    PetscTruth   ismg,isred;
 
-    ierr = MatNullSpaceCreate(PETSC_COMM_WORLD, PETSC_TRUE, 0, PETSC_NULL, &nullSpace);CHKERRQ(ierr);
-    ierr = KSPSetNullSpace(DMMGGetKSP(dmmg), nullSpace);CHKERRQ(ierr);
-    ierr = MatNullSpaceDestroy(nullSpace);CHKERRQ(ierr);
+    ierr = MatNullSpaceCreate(PETSC_COMM_WORLD, PETSC_TRUE, 0, PETSC_NULL, &user.nullSpace);CHKERRQ(ierr);
+    ierr = KSPSetNullSpace(DMMGGetKSP(dmmg), user.nullSpace);CHKERRQ(ierr);
+    /* need to set null space for all levels */
+    ierr = KSPGetPC(DMMGGetKSP(dmmg), &pc);CHKERRQ(ierr);
+    ierr = PetscTypeCompare((PetscObject)pc,PCMG,&ismg);CHKERRQ(ierr);
+    if (ismg) {
+      for (l = 0; l < DMMGGetLevels(dmmg); l++) {
+	ierr = MGGetSmoother(pc,l,&ksp);CHKERRQ(ierr);
+	ierr = KSPSetNullSpace(ksp, user.nullSpace);CHKERRQ(ierr);
+      }
+      /* coarse direct LU factorization will fail due to null space hence add shift */
+      ierr = MGGetSmoother(pc,0,&ksp);CHKERRQ(ierr);
+      ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
+      ierr = PetscTypeCompare((PetscObject)pc,PCREDUNDANT,&isred);CHKERRQ(ierr);
+      if (isred) {
+        ierr = PCRedundantGetPC(pc,&pc);CHKERRQ(ierr);
+      }
+      ierr = PCLUSetShift(pc,PETSC_TRUE);CHKERRQ(ierr);
+    }
   }
 
   ierr = DMMGSolve(dmmg);CHKERRQ(ierr);
@@ -89,6 +103,9 @@ int main(int argc,char **argv)
   ierr = VecNorm(DMMGGetr(dmmg),NORM_2,&norm);CHKERRQ(ierr);
   /* ierr = PetscPrintf(PETSC_COMM_WORLD,"Residual norm %g\n",norm);CHKERRQ(ierr); */
 
+  if (user.bcType == NEUMANN) {
+    ierr = MatNullSpaceDestroy(user.nullSpace);CHKERRQ(ierr);
+  }
   ierr = DMMGDestroy(dmmg);CHKERRQ(ierr);
   ierr = PetscFinalize();CHKERRQ(ierr);
 
@@ -97,13 +114,14 @@ int main(int argc,char **argv)
 
 #undef __FUNCT__
 #define __FUNCT__ "ComputeRHS"
-int ComputeRHS(DMMG dmmg, Vec b)
+PetscErrorCode ComputeRHS(DMMG dmmg, Vec b)
 {
-  DA          da = (DA)dmmg->dm;
-  UserContext *user = (UserContext *) dmmg->user;
-  int         ierr,i,j,mx,my,xm,ym,xs,ys;
-  PetscScalar Hx,Hy;
-  PetscScalar **array;
+  DA             da = (DA)dmmg->dm;
+  UserContext    *user = (UserContext *) dmmg->user;
+  PetscErrorCode ierr;
+  PetscInt       i,j,mx,my,xm,ym,xs,ys;
+  PetscScalar    Hx,Hy;
+  PetscScalar    **array;
 
   PetscFunctionBegin;
   ierr = DAGetInfo(da, 0, &mx, &my, 0,0,0,0,0,0,0,0);CHKERRQ(ierr);
@@ -119,13 +137,18 @@ int ComputeRHS(DMMG dmmg, Vec b)
   ierr = DAVecRestoreArray(da, b, &array);CHKERRQ(ierr);
   ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
   ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+
+  /* force right hand side to be consistent for singular matrix */
+  if (user->bcType == NEUMANN) {
+    ierr = MatNullSpaceRemove(user->nullSpace,b,PETSC_NULL);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
 
     
 #undef __FUNCT__
 #define __FUNCT__ "ComputeRho"
-int ComputeRho(int i, int j, int mx, int my, PetscScalar *rho)
+PetscErrorCode ComputeRho(PetscInt i, PetscInt j, PetscInt mx, PetscInt my, PetscScalar *rho)
 {
   PetscFunctionBegin;
   if ((i > mx/3.0) && (i < 2.0*mx/3.0) && (j > my/3.0) && (j < 2.0*my/3.0)) {
@@ -138,13 +161,14 @@ int ComputeRho(int i, int j, int mx, int my, PetscScalar *rho)
 
 #undef __FUNCT__
 #define __FUNCT__ "ComputeJacobian"
-int ComputeJacobian(DMMG dmmg, Mat jac)
+PetscErrorCode ComputeJacobian(DMMG dmmg, Mat jac)
 {
-  DA           da = (DA) dmmg->dm;
-  UserContext *user = (UserContext *) dmmg->user;
-  int          ierr,i,j,mx,my,xm,ym,xs,ys,num;
-  PetscScalar  v[5],Hx,Hy,HydHx,HxdHy,rho;
-  MatStencil   row, col[5];
+  DA             da = (DA) dmmg->dm;
+  UserContext    *user = (UserContext *) dmmg->user;
+  PetscErrorCode ierr;
+  PetscInt       i,j,mx,my,xm,ym,xs,ys,num;
+  PetscScalar    v[5],Hx,Hy,HydHx,HxdHy,rho;
+  MatStencil     row, col[5];
 
   PetscFunctionBegin;
   ierr = DAGetInfo(da,0,&mx,&my,0,0,0,0,0,0,0,0);CHKERRQ(ierr);  
@@ -157,35 +181,30 @@ int ComputeJacobian(DMMG dmmg, Mat jac)
     for(i=xs; i<xs+xm; i++){
       row.i = i; row.j = j;
       if (i==0 || j==0 || i==mx-1 || j==my-1) {
-        if (user->bcType == DIRICHLET) {
-          ierr = ComputeRho(i, j, mx, my, &rho);CHKERRQ(ierr);
-          v[0] = 2.0*rho*(HxdHy + HydHx);
+       ierr = ComputeRho(i, j, mx, my, &rho);CHKERRQ(ierr);
+       if (user->bcType == DIRICHLET) {
+           v[0] = 2.0*rho*(HxdHy + HydHx);
           ierr = MatSetValuesStencil(jac,1,&row,1,&row,v,INSERT_VALUES);CHKERRQ(ierr);
         } else if (user->bcType == NEUMANN) {
           num = 0;
           if (j!=0) {
-            ierr = ComputeRho(i, j-1, mx, my, &rho);CHKERRQ(ierr);
-            v[0] = -rho*HxdHy;              col[0].i = i;   col[0].j = j-1;
+            v[num] = -rho*HxdHy;              col[num].i = i;   col[num].j = j-1;
             num++;
           }
           if (i!=0) {
-            ierr = ComputeRho(i-1, j, mx, my, &rho);CHKERRQ(ierr);
-            v[1] = -rho*HydHx;              col[1].i = i-1; col[1].j = j;
+            v[num] = -rho*HydHx;              col[num].i = i-1; col[num].j = j;
             num++;
           }
-          ierr   = ComputeRho(i, j, mx, my, &rho);CHKERRQ(ierr);
-          v[2]   = 2.0*rho*(HxdHy + HydHx); col[2].i = i;   col[2].j = j;
-          num++;
           if (i!=mx-1) {
-            ierr = ComputeRho(i+1, j, mx, my, &rho);CHKERRQ(ierr);
-            v[3] = -rho*HydHx;              col[3].i = i+1; col[3].j = j;
+            v[num] = -rho*HydHx;              col[num].i = i+1; col[num].j = j;
             num++;
           }
           if (j!=my-1) {
-            ierr = ComputeRho(i, j+1, mx, my, &rho);CHKERRQ(ierr);
-            v[4] = -rho*HxdHy;              col[4].i = i;   col[4].j = j+1;
+            v[num] = -rho*HxdHy;              col[num].i = i;   col[num].j = j+1;
             num++;
           }
+          v[num]   = (num/2.0)*rho*(HxdHy + HydHx); col[num].i = i;   col[num].j = j;
+          num++;
           ierr = MatSetValuesStencil(jac,1,&row,num,col,v,INSERT_VALUES);CHKERRQ(ierr);
         }
       } else {
