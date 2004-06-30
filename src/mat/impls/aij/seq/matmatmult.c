2499ec7893278572238d73a6b308a0d02f10e2c6
Hong Zhang <hzhang@mcs.anl.gov>
2004-06-30 10:14:05 -0600

    bk-changeset-1.2316.1.1 hzhang@harley.mcs.anl.gov|ChangeSet|20040630161405|06515 ChangeSet   1.2316.1.1 04/06/30 11:14:05 hzhang@harley.mcs.anl.gov +3 -0   mv mpi functions in aij/seq/matmatmult.c to   new file aij/mpi/mpimatmatmult.c
      src/mat/impls/aij/seq/matmatmult.c
        1.82 04/06/30 11:14:04 hzhang@harley.mcs.anl.gov +1 -121
        mv mpi functions in aij/seq/matmatmult.c to 
        new file aij/mpi/mpimatmatmult.c
    
      src/mat/impls/aij/mpi/makefile
        1.26 04/06/30 11:14:04 hzhang@harley.mcs.anl.gov +2 -2
        mv mpi functions in aij/seq/matmatmult.c to 
        new file aij/mpi/mpimatmatmult.c
    
      src/mat/impls/aij/mpi/mpimatmatmult.c
        1.1 04/06/30 11:12:23 hzhang@harley.mcs.anl.gov +128 -0
    
      src/mat/impls/aij/mpi/mpimatmatmult.c
        1.0 04/06/30 11:12:22 hzhang@harley.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/mat/impls/aij/mpi/mpimatmatmult.c
    
    
    Hg-commit: a9dcaf15c6dad7c166f1236f9e46f811e3aabf53

--- src/mat/impls/aij/seq/matmatmult.c
+++ src/mat/impls/aij/seq/matmatmult.c
@@ -1,11 +1,10 @@
 /*
-  Defines matrix-matrix product routines for pairs of AIJ matrices
+  Defines matrix-matrix product routines for pairs of SeqAIJ matrices
           C = A * B
 */
 
 #include "src/mat/impls/aij/seq/aij.h" /*I "petscmat.h" I*/
 #include "src/mat/utils/freespace.h"
-#include "src/mat/impls/aij/mpi/mpiaij.h"
 #include "petscbt.h"
 
 #undef __FUNCT__
@@ -70,23 +69,6 @@ PetscErrorCode MatMatMult(Mat A,Mat B,MatReuse scall,PetscReal fill,Mat *C)
   
   PetscFunctionReturn(0);
 } 
-
-#undef __FUNCT__
-#define __FUNCT__ "MatMatMult_MPIAIJ_MPIAIJ"
-PetscErrorCode MatMatMult_MPIAIJ_MPIAIJ(Mat A,Mat B,MatReuse scall,PetscReal fill, Mat *C) 
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBegin;
-  if (scall == MAT_INITIAL_MATRIX){ 
-    ierr = MatMatMultSymbolic_MPIAIJ_MPIAIJ(A,B,fill,C);CHKERRQ(ierr);/* numeric product is computed as well */
-  } else if (scall == MAT_REUSE_MATRIX){
-    ierr = MatMatMultNumeric_MPIAIJ_MPIAIJ(A,B,*C);CHKERRQ(ierr);
-  } else {
-    SETERRQ1(PETSC_ERR_ARG_WRONG,"Invalid MatReuse %d",(int)scall);
-  }
-  PetscFunctionReturn(0);
-}
 
 #undef __FUNCT__
 #define __FUNCT__ "MatMatMult_SeqAIJ_SeqAIJ"
@@ -162,76 +144,6 @@ PetscErrorCode MatMatMultSymbolic(Mat A,Mat B,PetscReal fill,Mat *C)
   ierr = (*Asymbolic)(A,B,fill,C);CHKERRQ(ierr);
   ierr = PetscLogEventEnd(MAT_MatMultSymbolic,A,B,0,0);CHKERRQ(ierr); 
 
-  PetscFunctionReturn(0);
-}
-
-EXTERN PetscErrorCode MatDestroy_MPIAIJ(Mat);
-#undef __FUNCT__  
-#define __FUNCT__ "MatDestroy_MPIAIJ_MatMatMult"
-PetscErrorCode MatDestroy_MPIAIJ_MatMatMult(Mat A)
-{
-  PetscErrorCode       ierr;
-  Mat_MatMatMultMPI    *mult;
-  PetscObjectContainer container;
-
-  PetscFunctionBegin;
-  ierr = PetscObjectQuery((PetscObject)A,"MatMatMultMPI",(PetscObject *)&container);CHKERRQ(ierr);
-  if (container) {
-    ierr  = PetscObjectContainerGetPointer(container,(void **)&mult);CHKERRQ(ierr); 
-    ierr = ISDestroy(mult->isrowb);CHKERRQ(ierr);
-    ierr = ISDestroy(mult->iscolb);CHKERRQ(ierr);
-    ierr = ISDestroy(mult->isrowa);CHKERRQ(ierr);
-    ierr = MatDestroy(mult->A_loc);CHKERRQ(ierr); 
-    ierr = MatDestroy(mult->B_seq);CHKERRQ(ierr); 
-    ierr = MatDestroy(mult->C_seq);CHKERRQ(ierr);  
-
-    ierr = PetscObjectContainerDestroy(container);CHKERRQ(ierr);
-    ierr = PetscObjectCompose((PetscObject)A,"MatMatMultMPI",0);CHKERRQ(ierr);
-  }
-  ierr = PetscFree(mult);CHKERRQ(ierr); 
-  ierr = MatDestroy_MPIAIJ(A);CHKERRQ(ierr);
-  
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__  
-#define __FUNCT__ "MatMatMultSymbolic_MPIAIJ_MPIAIJ"
-PetscErrorCode MatMatMultSymbolic_MPIAIJ_MPIAIJ(Mat A,Mat B,PetscReal fill,Mat *C)
-{
-  Mat_MPIAIJ           *a=(Mat_MPIAIJ*)A->data,*b=(Mat_MPIAIJ*)B->data;
-  PetscErrorCode       ierr;
-  PetscInt             start,end;
-  Mat_MatMatMultMPI    *mult;
-  PetscObjectContainer container;
- 
-  PetscFunctionBegin;
-  if (a->cstart != b->rstart || a->cend != b->rend){
-    SETERRQ4(PETSC_ERR_ARG_SIZ,"Matrix local dimensions are incompatible, (%D, %D) != (%D,%D)",a->cstart,a->cend,b->rstart,b->rend);
-  }
-  ierr = PetscNew(Mat_MatMatMultMPI,&mult);CHKERRQ(ierr);
-
-  /* create a seq matrix B_seq = submatrix of B by taking rows of B that equal to nonzero col of A */
-  ierr = MatGetBrowsOfAcols(A,B,MAT_INITIAL_MATRIX,&mult->isrowb,&mult->iscolb,&mult->brstart,&mult->B_seq);CHKERRQ(ierr);
-
-  /*  create a seq matrix A_seq = submatrix of A by taking all local rows of A */
-  start = a->rstart; end = a->rend;
-  ierr = ISCreateStride(PETSC_COMM_SELF,end-start,start,1,&mult->isrowa);CHKERRQ(ierr); 
-  ierr = MatGetLocalMatCondensed(A,MAT_INITIAL_MATRIX,&mult->isrowa,&mult->isrowb,&mult->A_loc);CHKERRQ(ierr); 
-
-  /* compute C_seq = A_seq * B_seq */
-  ierr = MatMatMult_SeqAIJ_SeqAIJ(mult->A_loc,mult->B_seq,MAT_INITIAL_MATRIX,fill,&mult->C_seq);CHKERRQ(ierr);
-
-  /* create mpi matrix C by concatinating C_seq */
-  ierr = PetscObjectReference((PetscObject)mult->C_seq);CHKERRQ(ierr); /* prevent C_seq being destroyed by MatMerge() */
-  ierr = MatMerge(A->comm,mult->C_seq,B->n,MAT_INITIAL_MATRIX,C);CHKERRQ(ierr); 
- 
-  /* attach the supporting struct to C for reuse of symbolic C */
-  ierr = PetscObjectContainerCreate(PETSC_COMM_SELF,&container);CHKERRQ(ierr);
-  ierr = PetscObjectContainerSetPointer(container,mult);CHKERRQ(ierr);
-  ierr = PetscObjectCompose((PetscObject)(*C),"MatMatMultMPI",(PetscObject)container);CHKERRQ(ierr);
-
-  (*C)->ops->destroy  = MatDestroy_MPIAIJ_MatMatMult; 
-  
   PetscFunctionReturn(0);
 }
 
@@ -386,38 +298,6 @@ PetscErrorCode MatMatMultNumeric(Mat A,Mat B,Mat C)
   ierr = PetscLogEventBegin(MAT_MatMultNumeric,A,B,0,0);CHKERRQ(ierr); 
   ierr = (*Anumeric)(A,B,C);CHKERRQ(ierr);
   ierr = PetscLogEventEnd(MAT_MatMultNumeric,A,B,0,0);CHKERRQ(ierr); 
-
-  PetscFunctionReturn(0);
-}
-
-/* This routine is called ONLY in the case of reusing previously computed symbolic C */
-#undef __FUNCT__  
-#define __FUNCT__ "MatMatMultNumeric_MPIAIJ_MPIAIJ"
-PetscErrorCode MatMatMultNumeric_MPIAIJ_MPIAIJ(Mat A,Mat B,Mat C)
-{
-  PetscErrorCode       ierr;
-  Mat                  *seq;
-  Mat_MatMatMultMPI    *mult; 
-  PetscObjectContainer container;
-
-  PetscFunctionBegin;
-  ierr = PetscObjectQuery((PetscObject)C,"MatMatMultMPI",(PetscObject *)&container);CHKERRQ(ierr);
-  if (container) {
-    ierr  = PetscObjectContainerGetPointer(container,(void **)&mult);CHKERRQ(ierr); 
-  }
-
-  seq = &mult->B_seq;
-  ierr = MatGetSubMatrices(B,1,&mult->isrowb,&mult->iscolb,MAT_REUSE_MATRIX,&seq);CHKERRQ(ierr);
-  mult->B_seq = *seq;
-  
-  seq = &mult->A_loc;
-  ierr = MatGetSubMatrices(A,1,&mult->isrowa,&mult->isrowb,MAT_REUSE_MATRIX,&seq);CHKERRQ(ierr);
-  mult->A_loc = *seq;
-
-  ierr = MatMatMult_SeqAIJ_SeqAIJ(mult->A_loc,mult->B_seq,MAT_REUSE_MATRIX,0.0,&mult->C_seq);CHKERRQ(ierr);
-
-  ierr = PetscObjectReference((PetscObject)mult->C_seq);CHKERRQ(ierr); 
-  ierr = MatMerge(A->comm,mult->C_seq,B->n,MAT_REUSE_MATRIX,&C);CHKERRQ(ierr); 
 
   PetscFunctionReturn(0);
 }
