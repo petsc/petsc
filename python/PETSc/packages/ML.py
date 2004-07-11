1c40a0eb56a9535463121edb7784b28565219c5d
Hong Zhang <hzhang@mcs.anl.gov>
2004-07-11 08:46:47 -0600

    bk-changeset-1.2348 hzhang@harley.mcs.anl.gov|ChangeSet|20040711144647|00447 ChangeSet   1.2348 04/07/11 09:46:47 hzhang@harley.mcs.anl.gov +4 -0   add new config file ML.py
      python/PETSc/packages/__init__.py
        1.5 04/07/11 09:46:45 hzhang@harley.mcs.anl.gov +1 -1
        add new config file ML.py
    
      bmake/config/packages.in
        1.34 04/07/11 09:46:45 hzhang@harley.mcs.anl.gov +5 -0
        add new config file ML.py
    
      bmake/common/variables
        1.66 04/07/11 09:46:45 hzhang@harley.mcs.anl.gov +4 -3
        add new config file ML.py
    
      python/PETSc/packages/ML.py
        1.1 04/07/11 09:45:57 hzhang@harley.mcs.anl.gov +150 -0
    
      python/PETSc/packages/ML.py
        1.0 04/07/11 09:45:57 hzhang@harley.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/python/PETSc/packages/ML.py
    
    
    Hg-commit: 4d5be91855f87ebcabc42e00bac87efeea1f0025

---
+++ python/PETSc/packages/ML.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+from __future__ import generators
+import user
+import config.base
+import os
+
+#Developed for the ML_3.0
+
+class Configure(config.base.Configure):
+  def __init__(self, framework):
+    config.base.Configure.__init__(self, framework)
+    self.headerPrefix = ''
+    self.substPrefix  = ''
+    self.compilers    = self.framework.require('config.compilers',self)
+    self.libraries    = self.framework.require('config.libraries',self)
+    self.mpi          = self.framework.require('PETSc.packages.MPI',self)
+    self.found        = 0
+    self.lib          = []
+    self.include      = []
+    self.name         = 'ML'
+    return
+
+  def __str__(self):
+    output=''
+    if self.found:
+      output  = self.name+':\n'
+      output += '  Includes: '+self.include[0]+'\n'
+      output += '  Library: '+self.lib[0]+'\n'
+    return output
+  
+  def setupHelp(self,help):
+    import nargs
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    help.addArgument(PACKAGE,'-with-'+package+'=<bool>',nargs.ArgBool(None,1,'Indicate if you wish to test for '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-lib=<lib>',nargs.Arg(None,None,'Indicate the library containing '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-include=<dir>',nargs.ArgDir(None,None,'Indicate the directory of header files for '+self.name))
+    help.addArgument(PACKAGE,'-with-'+package+'-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the '+self.name+' installation'))
+    return
+
+  def generateIncludeGuesses(self):
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    if 'with-'+package in self.framework.argDB:
+      if 'with-'+package+'-include' in self.framework.argDB:
+        incl = self.framework.argDB['with-'+package+'-include']
+        yield('User specified '+PACKAGE+' header location',incl)
+      elif 'with-'+package+'-lib' in self.framework.argDB:
+        incl     = self.lib[0]            #standalone/lib/libml.a
+        (incl,dummy) = os.path.split(incl)
+        (incl,dummy) = os.path.split(incl)
+        yield('based on found library location',os.path.join(incl,'include'))
+      elif 'with-'+package+'-dir' in self.framework.argDB:
+        dir = os.path.abspath(self.framework.argDB['with-'+package+'-dir'])
+        yield('based on found root directory',os.path.join(dir,'include'))
+    return
+
+  def checkInclude(self,incl,hfile):
+    incl.extend(self.mpi.include)
+    oldFlags = self.framework.argDB['CPPFLAGS']
+    for inc in incl:
+      self.framework.argDB['CPPFLAGS'] += ' -I'+inc
+    found = self.checkPreprocess('#include <' +hfile+ '>\n')
+    self.framework.argDB['CPPFLAGS'] = oldFlags
+    if found:
+      self.framework.log.write('Found header file ' +hfile+ ' in '+incl[0]+'\n')
+    return found
+
+  def generateLibGuesses(self):
+    PACKAGE = self.name.upper()
+    package = self.name.lower()
+    if 'with-'+package in self.framework.argDB:
+      if 'with-'+package+'-lib' in self.framework.argDB:       #~ml/standalone/lib/libml.a
+        yield ('User specified '+PACKAGE+' library', self.framework.argDB['with-'+package+'-lib'])
+      elif 'with-'+package+'-include' in self.framework.argDB:
+        dir = self.framework.argDB['with-'+package+'-include'] #~ml/standalone/include
+        (dir,dummy) = os.path.split(dir)
+        yield('User specified '+PACKAGE+'/Include', os.path.join(dir,'lib/libml.a'))
+      elif 'with-'+package+'-dir' in self.framework.argDB:     #~ml/standalone
+        dir = os.path.abspath(self.framework.argDB['with-'+package+'-dir'])
+        yield('User specified '+PACKAGE+' root directory', os.path.join(dir,'lib/libml.a'))
+      else:
+        self.framework.log.write('Must specify either a library or installation root directory for '+PACKAGE+'\n')
+    return
+        
+  def checkLib(self,lib,func):
+    '''We may need the MPI libraries here'''
+    oldLibs = self.framework.argDB['LIBS']  
+    found   = self.libraries.check(lib,func)
+    self.framework.argDB['LIBS'] = oldLibs
+    if found:
+      self.framework.log.write('Found function '+func+' in '+str(lib)+'\n')
+    return found
+  
+  def configureLibrary(self):
+    '''Find an installation and check if it can work with PETSc'''
+    self.framework.log.write('==================================================================================\n')
+    self.framework.log.write('Checking for a functional '+self.name+'\n')
+    foundLibrary = 0
+    foundHeader  = 0
+    for configstr, lib in self.generateLibGuesses():
+      if not isinstance(lib, list): lib = [lib]
+      self.framework.log.write('Checking for library '+configstr+': '+str(lib)+'\n')
+      #foundLibrary = self.executeTest(self.checkLib, [lib, 'ML_Set_PrintLevel'])  
+      foundLibrary = 1
+      if foundLibrary:
+        self.lib = lib
+        break
+    for inclstr, incl in self.generateIncludeGuesses():
+      if not isinstance(incl, list): incl = [incl]
+      self.framework.log.write('Checking for headers '+inclstr+': '+str(incl)+'\n')
+      foundHeader = self.executeTest(self.checkInclude, [incl, 'ml_include.h'])
+      if foundHeader:
+        self.include = incl
+        break
+    if foundLibrary and foundHeader:
+      self.setFoundOutput()
+    else:
+      self.framework.log.write('Could not find a functional '+self.name+'\n')
+      self.setEmptyOutput()
+    return
+
+  def setFoundOutput(self):
+    PACKAGE = self.name.upper()
+    self.addSubstitution(PACKAGE+'_INCLUDE','-I'+self.include[0])
+    self.addSubstitution(PACKAGE+'_LIB',' '.join(map(self.libraries.getLibArgument,self.lib)))
+    self.addDefine('HAVE_'+PACKAGE,1)
+    
+  def setEmptyOutput(self):
+    PACKAGE = self.name.upper()
+    self.addSubstitution(PACKAGE+'_INCLUDE', '')
+    self.addSubstitution(PACKAGE+'_LIB', '')
+    return
+
+  def configure(self):
+    package = self.name.lower()
+    if not 'with-'+package in self.framework.argDB or not self.mpi.foundMPI or self.framework.argDB['with-64-bit-int']:
+      self.setEmptyOutput()
+      return
+    self.executeTest(self.configureLibrary)
+    return
+
+if __name__ == '__main__':
+  import config.framework
+  import sys
+  framework = config.framework.Framework(sys.argv[1:])
+  framework.setupLogging(framework.clArgs)
+  framework.children.append(Configure(framework))
+  framework.configure()
+  framework.dumpSubstitutions()
