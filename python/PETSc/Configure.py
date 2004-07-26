6b77d266523c37eac9751621cc9ca78abbfdd70b
Barry Smith <bsmith@mcs.anl.gov>
2004-07-26 13:34:21 -0600

    bk-changeset-1.2363.1.31 barrysmith@barrysmith-57.mcs.anl.gov|ChangeSet|20040726193421|12645 ChangeSet   1.2363.1.31 04/07/26 14:34:21 barrysmith@barrysmith-57.mcs.anl.gov +1 -0   I hate systems
      python/PETSc/Configure.py
        1.246 04/07/26 14:33:39 barrysmith@barrysmith-57.mcs.anl.gov +1 -1
        I hate systems
    
    
    Hg-commit: 5bde637969cb00f22dc553fa4bea9fafab481327

--- python/PETSc/Configure.py
+++ python/PETSc/Configure.py
@@ -374,7 +374,7 @@ class Configure(config.base.Configure):
     return
 
   def checkPrototype(self, includes = '', body = '', cleanup = 1, codeBegin = None, codeEnd = None):
-    output = self.outputCompile(includes, body, cleanup, codeBegin, codeEnd)
+    (output,err,code) = self.outputCompile(includes, body, cleanup, codeBegin, codeEnd)
     if output.find('implicit') >= 0 or output.find('Implicit') >= 0: return 0
     return 1
     
