0f9da694631962f282fbdcb23870c65f41a82b75
Matthew Knepley <knepley@mcs.anl.gov>
2004-08-05 10:37:17 -0600

    bk-changeset-1.2368.1.2 knepley@khan.mcs.anl.gov|ChangeSet|20040805163717|53298 ChangeSet   1.2368.1.2 04/08/05 11:37:17 knepley@khan.mcs.anl.gov +1 -0   Configure fix
      python/PETSc/packages/ParMetis.py
        1.9.1.1 04/08/05 11:37:10 knepley@khan.mcs.anl.gov +32 -3
        Upgrading ParMetis
         - This is only partial
    
    
    Hg-commit: 994ed62a1964f1f7c3e6482ece3663b7ec68e7c2

--- python/PETSc/packages/ParMetis.py
+++ python/PETSc/packages/ParMetis.py
@@ -6,6 +6,26 @@ class Configure(config.base.Configure):
     self.headerPrefix = ''
     self.substPrefix  = ''
     self.compilers    = self.framework.require('config.compilers', self)
+    self.found        = 0
+    return
+
+  def __str__(self):
+    if self.found:
+      desc = ['ParMetis:']	
+      desc.append('  Version: '+self.version)
+      desc.append('  Includes: '+str(self.include))
+      desc.append('  Library: '+str(self.lib))
+      return '\n'.join(desc)+'\n'
+    else:
+      return ''
+
+  def setupHelp(self, help):
+    import nargs
+    help.addArgument('ParMetis', '-with-parmetis=<bool>',                nargs.ArgBool(None, 0, 'Activate ParMetis'))
+    help.addArgument('ParMetis', '-with-parmetis-dir=<root dir>',        nargs.ArgDir(None, None, 'Specify the root directory of the ParMetis installation'))
+    help.addArgument('ParMetis', '-with-parmetis-include=<dir>',         nargs.ArgDir(None, None, 'The directory containing metis.h'))
+    help.addArgument('ParMetis', '-with-parmetis-lib=<lib>',             nargs.Arg(None, None, 'The ParMetis library or list of libraries'))
+    help.addArgument('ParMetis', '-download-parmetis=<no,yes,ifneeded>', nargs.ArgFuzzyBool(None, 2, 'Install MPICH to provide ParMetis'))
     return
 
   def configureLibrary(self):
@@ -13,12 +33,21 @@ class Configure(config.base.Configure):
     return
 
   def setOutput(self):
-    #self.addDefine('HAVE_PARMETIS', 0)
-    self.addSubstitution('PARMETIS_INCLUDE', '')
-    self.addSubstitution('PARMETIS_LIB', '')
+    '''Add defines and substitutions
+       - HAVE_PARMETIS is defined if a working ParMetis is found
+       - PARMETIS_INCLUDE and PARMETIS are command line arguments for the compile and link
+       - PARMETIS_INCLUDE_DIR is the directory containing metis.h
+       - PARMETIS_LIBRARY is the list of ParMetis libraries'''
+    if self.found:
+      self.addDefine('HAVE_PARMETIS', 1)
+    else:
+      self.addSubstitution('PARMETIS_INCLUDE', '')
+      self.addSubstitution('PARMETIS_LIB', '')
     return
 
   def configure(self):
+    if not self.framework.argDB['with-parmetis']:
+      return
     self.executeTest(self.configureLibrary)
     self.setOutput()
     return
