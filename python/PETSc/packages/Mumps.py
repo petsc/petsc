f82314e8da701b9f2b3791518c177fbe6a5a2527
Barry Smith <bsmith@mcs.anl.gov>
2004-08-05 15:54:22 -0600

    bk-changeset-1.2380.1.1 barrysmith@barrysmith-11.mcs.anl.gov|ChangeSet|20040805215422|06073 ChangeSet   1.2380.1.1 04/08/05 16:54:22 barrysmith@barrysmith-11.mcs.anl.gov +3 -0   download and compile BLACS
      python/PETSc/packages/Mumps.py
        1.8 04/08/05 16:54:20 barrysmith@barrysmith-11.mcs.anl.gov +199 -110
        download BLACs
    
      python/PETSc/Configure.py
        1.250.1.1 04/08/05 16:54:20 barrysmith@barrysmith-11.mcs.anl.gov +0 -6
        move to BuildSystem since needed by packages/Mumps.py
    
      todo
        1.77 04/08/04 14:46:10 barrysmith@barrysmith-57.mcs.anl.gov +0 -4
        *** empty log message ***
    
    
    Hg-commit: 0f9ecb8b2406005d0d35161d8c55e94c0c775070

--- python/PETSc/packages/Mumps.py
+++ python/PETSc/packages/Mumps.py
@@ -12,8 +12,10 @@ class Configure(config.base.Configure):
     self.headerPrefix = ''
     self.substPrefix  = ''
     self.compilers    = self.framework.require('config.compilers',self)
+    self.setcompilers = self.framework.require('config.setCompilers',self)    
     self.libraries    = self.framework.require('config.libraries',self)
     self.mpi          = self.framework.require('PETSc.packages.MPI',self)
+    self.blasLapack   = self.framework.require('PETSc.packages.BlasLapack',self)
     self.found        = 0
     self.lib          = []
     self.include      = []
@@ -33,32 +35,133 @@ class Configure(config.base.Configure):
   def setupHelp(self,help):
     import nargs
 
-    PACKAGE = self.name.upper()
-    package = self.name.lower()
-    help.addArgument(PACKAGE,'-with-'+package+'=<bool>',nargs.ArgBool(None,1,'Indicate if you wish to test for '+self.name))
-    help.addArgument(PACKAGE,'-with-'+package+'-lib=<lib>',nargs.Arg(None,None,'Indicate the library containing '+self.name))
-    help.addArgument(PACKAGE,'-with-'+package+'-include=<dir>',nargs.ArgDir(None,None,'Indicate the directory of header files for '+self.name))
-    help.addArgument(PACKAGE,'-with-'+package+'-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the '+self.name+' installation'))
-    help.addArgument(PACKAGE,'-with-scalapack=<bool>',nargs.ArgBool(None,1,'Indicate if you wish to test for SCALAPACK'))
-    help.addArgument(PACKAGE,'-with-scalapack-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the SCALAPACK installation'))
-    help.addArgument(PACKAGE,'-with-blacs=<bool>',nargs.ArgBool(None,1,'Indicate if you wish to test for BLACS'))
-    help.addArgument(PACKAGE,'-with-blacs-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the BLACS installation'))
+    help.addArgument(self.PACKAGE,'-with-'+self.package+'=<bool>',nargs.ArgBool(None,0,'Indicate if you wish to test for '+self.name))
+    help.addArgument(self.PACKAGE,'-with-'+self.package+'-lib=<lib>',nargs.Arg(None,None,'Indicate the library containing '+self.name))
+    help.addArgument(self.PACKAGE,'-with-'+self.package+'-include=<dir>',nargs.ArgDir(None,None,'Indicate the directory of header files for '+self.name))
+    help.addArgument(self.PACKAGE,'-with-'+self.package+'-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the '+self.name+' installation'))
+    help.addArgument(self.PACKAGE,'-with-scalapack-lib',nargs.ArgBool(None,None,'SCALAPACK libraries'))
+    help.addArgument(self.PACKAGE,'-with-scalapack-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the SCALAPACK installation'))
+    help.addArgument(self.PACKAGE,'-with-blacs-lib',nargs.ArgBool(None,None,'BLACS libraries'))
+    help.addArgument(self.PACKAGE,'-with-blacs-dir=<dir>',nargs.ArgDir(None,None,'Indicate the root directory of the BLACS installation'))
+    help.addArgument(self.PACKAGE,'-download-blacs=<no,yes,ifneeded>',  nargs.ArgFuzzyBool(None, 0, 'Install BLACS'))
+    help.addArgument(self.PACKAGE,'-download-scalapack=<no,yes,ifneeded>',  nargs.ArgFuzzyBool(None, 0, 'Install Scalapack'))
+    help.addArgument(self.PACKAGE,'-download-mumps=<no,yes,ifneeded>',  nargs.ArgFuzzyBool(None, 0, 'Install Mumps'))    
     return
 
+  def getDirBLACS(self):
+    '''Find the directory containing BLACS'''
+    packages  = os.path.join(self.framework.argDB['PETSC_DIR'], 'packages')
+    if not os.path.isdir(packages):
+      os.mkdir(packages)
+      self.framework.actions.addArgument('PETSc', 'Directory creation', 'Created the packages directory: '+packages)
+    blacsDir = None
+    for dir in os.listdir(packages):
+      if dir == 'blacs-dev' and os.path.isdir(os.path.join(packages, dir)):
+        blacsDir = dir
+    if blacsDir is None:
+      raise RuntimeError('Error locating BLACS directory')
+    return os.path.join(packages, blacsDir)
+
+  def downLoadBLACS(self):
+    import commands
+    self.framework.log.write('Downloading BLACS\n')
+    try:
+      blacsDir = self.getDirBLACS()
+    except RuntimeError:
+      import urllib
+
+      packages = os.path.join(self.framework.argDB['PETSC_DIR'], 'packages')
+      try:
+        self.framework.log.write('Downloading it using "bk clone bk://petsc.bkbits.net/blacs-dev $PETSC_DIR/packages/blacs-dev"''\n')
+        (status,output) = commands.getstatusoutput('bk clone bk://petsc.bkbits.net/blacs-dev packages/blacs-dev')
+        if status:
+          if output.find('ommand not found') >= 0:
+            print '''******** Unable to locate bk (Bitkeeper) to download BuildSystem; make sure bk is in your path'''
+          elif output.find('Cannot resolve host') >= 0:
+            print '''******** Unable to download blacs. You must be off the network. Connect to the internet and run config/configure.py again******** '''
+          else:
+            import sys
+            print '''******** Unable to download blacs. Please send this message to petsc-maint@mcs.anl.gov******** '''
+            print output
+            sys.exit(3)
+      except RuntimeError, e:
+        raise RuntimeError('Error bk cloneing blacs '+str(e))        
+      self.framework.actions.addArgument('BLACS', 'Download', 'Downloaded blacs into '+self.getDirBLACS())
+
+    blacsDir  = self.getDirBLACS()
+    installDir = os.path.join(blacsDir, self.framework.argDB['PETSC_ARCH'])
+    f = open(os.path.join(blacsDir,'Bmake.Inc.template'))
+    g = open(os.path.join(blacsDir,'Bmake.Inc'),'w')
+    line = f.readline()
+    while line:
+      if line.find('BLACSdir =') >= 0:
+        line = 'BLACSdir = '+blacsDir+'\n'
+      if line.find('BLACSLIB    =') >= 0:
+        line = 'BLACSLIB = '+os.path.join(installDir,'libblacs.a')+'\n'
+      if line.find('MPIINCdir =') >= 0:
+        line = 'MPIINCdir='+self.mpi.include[0]+'\n'
+      if line.find('MPILIB =') >= 0:
+        line = 'MPILIB='+' '.join(map(self.libraries.getLibArgument, self.mpi.lib))+'\n'
+      if line.find('INTFACE =') >= 0:
+        if self.compilers.fortranManglingDoubleUnderscore:
+          blah = 'f77IsF2C'
+        elif self.compilers.fortranMangling == 'underscore':
+          blah = 'Add_'
+        elif self.compilers.fortranMangling == 'capitalize':
+          blah = 'UpCase'
+        else:
+          blah = 'NoChange'
+        line = 'INTFACE=-D'+blah+'\n'
+      g.write(line)
+      line = f.readline()
+      
+    self.setcompilers.pushLanguage('F77')  
+    g.write('  F77 ='+self.setcompilers.getCompiler()+'\n')
+    g.write('  F77FLAGS ='+self.setcompilers.getCompilerFlags()+'\n')
+    g.write('  F77LOADER ='+self.setcompilers.getLinker()+'\n')      
+    g.write('  F77LOADFLAGS ='+self.setcompilers.getLinkerFlags()+'\n')
+    self.setcompilers.popLanguage()
+    self.setcompilers.pushLanguage('C')
+    g.write('  CC ='+self.setcompilers.getCompiler()+'\n')
+    g.write('  CCFLAGS ='+self.setcompilers.getCompilerFlags()+'\n')      
+    g.write('  CCLOADER ='+self.setcompilers.getLinker()+'\n')
+    g.write('  CCLOADFLAGS ='+self.setcompilers.getLinkerFlags()+'\n')
+    self.setcompilers.popLanguage()
+    g.write('  ARCH ='+self.setcompilers.AR+'\n')
+    g.write('  ARCHFLAGS ='+self.setcompilers.AR_FLAGS+'\n')    
+    g.write('  RANLIB ='+self.setcompilers.RANLIB+'\n')    
+    f.close()
+    g.close()
+    if not os.path.isdir(installDir):
+      os.mkdir(installDir)
+    try:
+      output  = config.base.Configure.executeShellCommand('cd '+os.path.join(blacsDir,'SRC','MPI')+';make', timeout=2500, log = self.framework.log)[0]
+    except RuntimeError, e:
+      raise RuntimeError('Error running make on BLACS: '+str(e))
+    if not os.path.isfile(os.path.join(installDir,'libblacs.a')):
+      self.framework.log.write('Error running make on BLACS   ******(libraries not installed)*******\n')
+      self.framework.log.write('********Output of running make on BLACS follows *******\n')        
+      self.framework.log.write(output)
+      self.framework.log.write('********End of Output of running make on BLACS *******\n')
+      raise RuntimeError('Error running make on BLACS, libraries not installed')
+
+    self.framework.actions.addArgument('blacs', 'Install', 'Installed blacs into '+installDir)
+
+    return os.path.join(installDir,'libblacs.a')
+
+
   def generateIncludeGuesses(self):
-    PACKAGE = self.name.upper()
-    package = self.name.lower()
-    if 'with-'+package in self.framework.argDB:
-      if 'with-'+package+'-include' in self.framework.argDB:
-        incl = self.framework.argDB['with-'+package+'-include']
-        yield('User specified '+PACKAGE+' header location',incl)
-      elif 'with-'+package+'-lib' in self.framework.argDB:
+    if 'with-'+self.package in self.framework.argDB:
+      if 'with-'+self.package+'-include' in self.framework.argDB:
+        incl = self.framework.argDB['with-'+self.package+'-include']
+        yield('User specified '+self.PACKAGE+' header location',incl)
+      elif 'with-'+self.package+'-lib' in self.framework.argDB:
         incl     = self.lib[0]
         for i in 1,2:
           (incl,dummy) = os.path.split(incl)
         yield('based on found library location',os.path.join(incl,'include'))
-      elif 'with-'+package+'-dir' in self.framework.argDB:
-        dir = os.path.abspath(self.framework.argDB['with-'+package+'-dir'])
+      elif 'with-'+self.package+'-dir' in self.framework.argDB:
+        dir = os.path.abspath(self.framework.argDB['with-'+self.package+'-dir'])
         yield('based on found root directory',os.path.join(dir,'include'))
 
   def checkInclude(self,incl,hfile):
@@ -73,68 +176,87 @@ class Configure(config.base.Configure):
     return found
 
   def generateLibGuesses(self):
-    PACKAGE = self.name.upper()
-    package = self.name.lower()
-    if 'with-'+package in self.framework.argDB:
-      if 'with-'+package+'-lib' in self.framework.argDB: #~MUMPS_4.3.1/lib/libdmumps.a
-        yield ('User specified '+PACKAGE+' library',self.framework.argDB['with-'+package+'-lib'])
-      elif 'with-'+package+'-include' in self.framework.argDB:
-        dir = self.framework.argDB['with-'+package+'-include'] #~MUMPS_4.3.1/include
-        (dir,dummy) = os.path.split(dir)
-        yield('User specified '+PACKAGE+'/Include',os.path.join(dir,'lib/libdmumps.a'))
-      elif 'with-'+package+'-dir' in self.framework.argDB: 
-        dir = os.path.abspath(self.framework.argDB['with-'+package+'-dir'])
-        dir = os.path.join(dir,'lib')
-        libs = []
-        libs.append(os.path.join(dir,'libdmumps.a'))
-        libs.append(os.path.join(dir,'libzmumps.a'))
-        libs.append(os.path.join(dir,'libpord.a'))
-        yield('User specified '+PACKAGE+' root directory',libs)
-      else:
-        self.framework.log.write('Must specify either a library or installation root directory for '+PACKAGE+'\n')
+    if 'with-'+self.package+'-lib' in self.framework.argDB: #~MUMPS_4.3.1/lib/libdmumps.a
+      yield ('User specified '+self.PACKAGE+' library',self.framework.argDB['with-'+self.package+'-lib'])
+    elif 'with-'+self.package+'-dir' in self.framework.argDB: 
+      dir = os.path.abspath(self.framework.argDB['with-'+self.package+'-dir'])
+      dir = os.path.join(dir,'lib')
+      libs = []
+      libs.append(os.path.join(dir,'libdmumps.a'))
+      libs.append(os.path.join(dir,'libzmumps.a'))
+      libs.append(os.path.join(dir,'libpord.a'))
+      yield('User specified '+self.PACKAGE+' root directory',libs)
+    else:
+      self.framework.log.write('Must specify either a library or installation root directory for '+self.PACKAGE+'\n')
 
   def generateScalapackLibGuesses(self):
-    if 'with-scalapack' in self.framework.argDB:
-      if 'with-scalapack-lib' in self.framework.argDB: 
-        yield ('User specified SCALAPACK library',self.framework.argDB['with-scalapack-lib'])
-      elif 'with-scalapack-dir' in self.framework.argDB:
-        dir = os.path.abspath(self.framework.argDB['with-scalapack-dir'])
-        libs = []
-        libs.append(os.path.join(dir,'libscalapack.a'))
-        yield('User specified SCALAPACK root directory',libs)
-      else:
-        self.framework.log.write('Must specify either a library or installation root directory for SCALAPACK\n')
+    if 'with-scalapack-lib' in self.framework.argDB: 
+      yield ('User specified SCALAPACK library',self.framework.argDB['with-scalapack-lib'])
+    elif 'with-scalapack-dir' in self.framework.argDB:
+      dir = os.path.abspath(self.framework.argDB['with-scalapack-dir'])
+      libs = []
+      libs.append(os.path.join(dir,'libscalapack.a'))
+      yield('User specified SCALAPACK root directory',libs)
+    else:
+      self.framework.log.write('Must specify either a library or installation root directory for SCALAPACK\n')
   
   def generateBlacsLibGuesses(self):
-    if 'with-blacs' in self.framework.argDB:
-      if 'with-blacs-lib' in self.framework.argDB: 
-        yield ('User specified BLACS library',self.framework.argDB['with-blacs-lib'])
-      elif 'with-blacs-dir' in self.framework.argDB:
-        dir = os.path.abspath(self.framework.argDB['with-blacs-dir'])
-        dir = os.path.join(dir,'LIB')
-        libs = []
-        libs.append(os.path.join(dir,'libblacs_MPI-LINUX-0.a'))
-        libs.append(os.path.join(dir,'libblacsF77init_MPI-LINUX-0.a'))
-        libs.append(os.path.join(dir,'libblacs_MPI-LINUX-0.a'))
-        libs.append(os.path.join(dir,'libblacsF77init_MPI-LINUX-0.a'))
-        yield('User specified BLACS root directory',libs)
-      else:
-        self.framework.log.write('Must specify either a library or installation root directory for BLACS\n')
-        
-  def checkLib(self,lib,libfile):
-    if not isinstance(lib,list): lib = [lib]
+    if self.framework.argDB['download-blacs'] == 1:
+      yield ('Downloaded BLACS library',self.downLoadBLACS())
+      raise RuntimeError('Downloaded BLACS could not be used. Please check install in '+os.path.dirname(libs[0])+'\n')
+    if 'with-blacs-lib' in self.framework.argDB: 
+      yield ('User specified BLACS library',self.framework.argDB['with-blacs-lib'])
+    elif 'with-blacs-dir' in self.framework.argDB:
+      dir = os.path.abspath(self.framework.argDB['with-blacs-dir'])
+      libs = os.path.join(dir,'libblacs.a')
+      yield('User specified BLACS root directory',libs)
+    elif self.framework.argDB['download-blacs'] == 2 or self.framework.argDB['download-scalapack'] or self.framework.argDB['download-mumps']:
+      yield ('Downloaded BLACS library',self.downLoadBLACS())
+      raise RuntimeError('Downloaded BLACS could not be used. Please check install in '+os.path.dirname(libs[0])+'\n')
+    else:
+      self.framework.log.write('Must specify either a library or installation root directory for BLACS, or -download-blacs=yes\n')
+
+  def checkLib(self,lib,func):
     oldLibs = self.framework.argDB['LIBS']
-    mangleFunc  = 'FC' in self.framework.argDB
-    #found = self.libraries.check(lib,libfile)
-    found = self.libraries.check(lib,libfile,otherLibs = self.mpi.lib+self.compilers.flibs)
-    self.framework.argDB['LIBS']=oldLibs  
+    otherLibs = self.blasLapack.lapackLibrary
+    if not None in self.blasLapack.blasLibrary:
+      otherLibs = otherLibs+self.blasLapack.blasLibrary
+    otherLibs = ' '.join([self.libraries.getLibArgument(lib1) for lib1 in otherLibs])
+    self.framework.log.write('Otherlibs '+otherLibs+'\n')
+    otherLibs += ' '+' '.join(map(self.libraries.getLibArgument, self.mpi.lib))
+    if hasattr(self.compilers,'flibs'): otherLibs += ' '+self.compilers.flibs
+    self.framework.log.write('Otherlibs '+otherLibs+'\n')
+    found = self.libraries.check(lib,func, otherLibs = otherLibs,fortranMangle=1)
+    self.framework.argDB['LIBS']=oldLibs
     if found:
-      self.framework.log.write('Found functional '+libfile+' in '+lib[0]+'\n')
+      self.framework.log.write('Found function '+func+' in '+str(lib)+'\n')
     return found
   
   def configureLibrary(self):
     '''Find a installation and check if it can work with PETSc'''
     self.framework.log.write('==================================================================================\n')
+    self.framework.log.write('Find a installation of BLACS\n')
+    found  = 0
+    for (configstr,libs) in self.generateBlacsLibGuesses():
+      self.framework.log.write('Checking for a functional BLACS in '+configstr+'\n')
+      found = self.executeTest(self.checkLib,[libs,'blacs_pinfo'])
+      break  
+    if found:
+          self.blacslib = libs
+    else:
+      raise RuntimeError('Could not find a functional BLACS: use --with-blacs-dir or --with-blacs-lib to indicate location\n')
+
+    self.framework.log.write('Find a installation of SCALAPACK\n')
+    found  = 0
+    for (configstr,libs) in self.generateScalapackLibGuesses():
+      self.framework.log.write('Checking for a functional SCALAPACK in '+configstr+'\n')
+      if found:
+        self.scalapacklib = libs
+        break
+      else:
+        raise RuntimeError('Could not find a functional SCALAPACK: use --with-scalapack-dir or --with-scalapack-lib to indicate location\n')
+
+
     found  = 0
     foundlibs = 0
     foundh = 0
@@ -157,61 +279,28 @@ class Configure(config.base.Configure):
           self.found   = 1
           break
     else:
-      self.framework.log.write('Could not find a functional '+self.name+'\n')
-      self.setEmptyOutput()
-      return
+      raise RuntimeError('Could not find a functional '+self.name+': Use --with-'+self.package+'-dir to indicate is location\n')
     
-    self.framework.log.write('Find a installation of SCALAPACK\n')
-    found  = 0
-    for (configstr,libs) in self.generateScalapackLibGuesses():
-      self.framework.log.write('Checking for a functional SCALAPACK in '+configstr+'\n')
-      lib = libs[0]
-      #found = self.executeTest(self.checkLib,lib,'???']) #???
-      found = 1  #???
-      if found:
-        self.lib.append(lib)
-        break
-      else:
-        self.framework.log.write('Could not find a functional SCALAPACK\n')
-        self.setEmptyOutput()
-        return
       
-    self.framework.log.write('Find a installation of BLACS\n')
-    found  = 0
-    for (configstr,libs) in self.generateBlacsLibGuesses():
-      self.framework.log.write('Checking for a functional BLACS in '+configstr+'\n')
-      for lib in libs: 
-        #found = self.executeTest(self.checkLib,lib,'???']) #???
-        found = 1  #???
-        if found:
-          self.lib.append(lib)
-    
-    if not found:
-        self.framework.log.write('Could not find a functional BLACS\n')
-        self.setEmptyOutput()
-        return
     self.setFoundOutput()
     return
 
   def setFoundOutput(self):
-    PACKAGE = self.name.upper()
     incl_str = ''
     for i in range(len(self.include)):
       incl_str += self.include[i]+ ' '
-    self.addSubstitution(PACKAGE+'_INCLUDE','-I' +incl_str)
-    self.addSubstitution(PACKAGE+'_LIB',' '.join(map(self.libraries.getLibArgument,self.lib)))
-    self.addDefine('HAVE_'+PACKAGE,1)
+    self.addSubstitution(self.PACKAGE+'_INCLUDE','-I' +incl_str)
+    self.addSubstitution(self.PACKAGE+'_LIB',' '.join(map(self.libraries.getLibArgument,self.lib)))
+    self.addDefine('HAVE_'+self.PACKAGE,1)
     self.framework.packages.append(self)
             
   def setEmptyOutput(self):
-    PACKAGE = self.name.upper()
-    self.addSubstitution(PACKAGE+'_INCLUDE', '')
-    self.addSubstitution(PACKAGE+'_LIB', '')
+    self.addSubstitution(self.PACKAGE+'_INCLUDE', '')
+    self.addSubstitution(self.PACKAGE+'_LIB', '')
     return
 
   def configure(self):
-    package = self.name.lower()
-    if not 'with-'+package in self.framework.argDB or self.framework.argDB['with-64-bit-ints']:
+    if not 'with-'+self.package in self.framework.argDB or self.framework.argDB['with-64-bit-ints']:
       self.setEmptyOutput()
       return
     self.executeTest(self.configureLibrary)
