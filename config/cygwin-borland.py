0bf4740d9aac861491fef915f1d9d07938dc4255
Kris Buschelman <buschelm@mcs.anl.gov>
2004-08-06 03:05:33 -0600

    bk-changeset-1.2389 Kris@p4buschel.|ChangeSet|20040806090533|07938 ChangeSet   1.2389 04/08/06 02:05:33 Kris@p4buschel. +1 -0   Much simpler cygwin-borland configure script.
      config/cygwin-borland.py
        1.4 04/08/06 02:05:32 Kris@p4buschel. +1 -5
        Much simpler cygwin-borland configure script.
    
    
    Hg-commit: ef4028c829886b5fc455e327734d234fde07d92e

--- config/cygwin-borland.py
+++ config/cygwin-borland.py
@@ -7,12 +7,8 @@ if __name__ == '__main__':
 
     configure_options = [
     # path set to $PETSC_DIR/bin/win32fe
-    '--with-cc=win32fe bcc32 -w-8019 -w-8060 -w-8057 -w-8004 -w-8066',
-    '--with-cxx=win32fe bcc32 -P -RT -w-8019 -w-8060 -w-8057 -w-8004 -w-8066',
-    '--with-cpp=win32fe bcc32 --use cpp32',
+    '--with-vendor-compilers=borland',
     '--with-fc=0',
-    '--with-ar=win32fe tlib -C -P512',
-    '-AR_FLAGS=-u',
     '--with-ranlib=true',
     '--with-blas-lapack-dir=/cygdrive/c/software/f2cblaslapack/win32_borland',
     '--with-mpi=0'
