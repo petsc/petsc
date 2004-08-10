a2d44cde4a6ec3615177de5e2730928055b5f1f0
Matthew Knepley <knepley@mcs.anl.gov>
2004-08-10 11:57:09 -0600

    bk-changeset-1.1052.1.1 knepley@khan.mcs.anl.gov|ChangeSet|20040810175709|05492 ChangeSet   1.1052.1.1 04/08/10 12:57:09 knepley@khan.mcs.anl.gov +3 -0   Added VC configure   Small fixes
      config/sourceControl.py
        1.1 04/08/10 12:57:08 knepley@khan.mcs.anl.gov +42 -0
    
      nargs.py
        1.60 04/08/10 12:57:08 knepley@khan.mcs.anl.gov +1 -2
        Fixed import
    
      maker.py
        1.22 04/08/10 12:57:08 knepley@khan.mcs.anl.gov +1 -0
        Added --prefix
    
      config/sourceControl.py
        1.0 04/08/10 12:57:08 knepley@khan.mcs.anl.gov +0 -0
        BitKeeper file /PETSc3/sidl/BuildSystem/config/sourceControl.py
    
    
    Hg-commit: fc7a6ce5dfa067218a726fc05766dc66c4726596

---
+++ config/sourceControl.py
@@ -0,0 +1,42 @@
+import config.base
+
+import os
+
+class Configure(config.base.Configure):
+  def __init__(self, framework):
+    config.base.Configure.__init__(self, framework)
+    self.headerPrefix = ''
+    self.substPrefix  = ''
+    return
+
+  def __str__(self):
+    return ''
+
+  def setupHelp(self, help):
+    import nargs
+
+    help.addArgument('SourceControl', '-with-bk=<executable>',  nargs.Arg(None, 'bk', 'Specify the BitKeeper executable'))
+    help.addArgument('SourceControl', '-with-cvs=<executable>', nargs.Arg(None, 'cvs', 'Specify the CVS executable'))
+    help.addArgument('SourceControl', '-with-svn=<executable>', nargs.Arg(None, 'svn', 'Specify the Subversion executable'))
+    return
+
+  def configureBitKeeper(self):
+    '''Find the BitKeeper executable'''
+    self.getExecutable(self.framework.argDB['with-bk'], resultName = 'bk')
+    return
+
+  def configureCVS(self):
+    '''Find the CVS executable'''
+    self.getExecutable(self.framework.argDB['with-cvs'], resultName = 'cvs')
+    return
+
+  def configureSubversion(self):
+    '''Find the Subversion executable'''
+    self.getExecutable(self.framework.argDB['with-svn'], resultName = 'svn')
+    return
+
+  def configure(self):
+    self.executeTest(self.configureBitKeeper)
+    self.executeTest(self.configureCVS)
+    self.executeTest(self.configureSubversion)
+    return
