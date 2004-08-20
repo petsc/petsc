cf2681df9071a9f10fbdd7a945a5db132b83314b
Barry Smith <bsmith@mcs.anl.gov>
2004-08-20 13:36:01 -0600

    bk-changeset-1.1063 barrysmith@barrysmith-11.mcs.anl.gov|ChangeSet|20040820193601|11215 ChangeSet   1.1063 04/08/20 14:36:01 barrysmith@barrysmith-11.mcs.anl.gov +1 -0   bug fix for directory where absoft has caps
      config/compilers.py
        1.116.1.1 04/08/20 14:35:48 barrysmith@barrysmith-11.mcs.anl.gov +1 -1
        bug fix for directory where absoft has caps
    
    
    Hg-commit: 70fb9f706e2ba9992af011ce98c2061ad878163a

--- config/compilers.py
+++ config/compilers.py
@@ -233,7 +233,7 @@ class Configure(config.base.Configure):
     # replace \CR that ifc puts in each line of output
     output = output.replace('\\\n', '')
 
-    if output.find('absoft') >= 0:
+    if output.lower().find('absoft') >= 0:
       loc = output.find(' -lf90math')
       if loc == -1: loc = output.find(' -lf77math')
       if loc >= -1:
