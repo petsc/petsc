cd9b048709ea45ea87900a2e23d07b71804de1b2
Satish Balay <balay@mcs.anl.gov>
2004-09-30 12:56:59 -0600

    bk-changeset-1.2468.6.21 balay@asterix.mcs.anl.gov|ChangeSet|20040930185659|37646 ChangeSet   1.2468.6.21 04/09/30 13:56:59 balay@asterix.mcs.anl.gov +1 -0   remove superlu dep - as this should be correctly handled by hypre build system without -DHAVE_SUPERLU
      python/PETSc/packages/hypre.py
        1.15.1.3 04/09/30 13:56:58 balay@asterix.mcs.anl.gov +0 -2
        remove superlu dep - as this should be correctly handled by hypre build system without -DHAVE_SUPERLU
    
    
    Hg-commit: 02ddf5981da7548f2e975a8561f16894430e7b4d

--- python/PETSc/packages/hypre.py
+++ python/PETSc/packages/hypre.py
@@ -65,8 +65,6 @@ class Configure(config.base.Configure):
             'Euclid',
             'IJ_mv',
             'LSI',
-            'superlu',
-            'blas',
             'MatrixMatrix',
             'ParaSails',
             'krylov',
