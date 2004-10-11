66756e5ef63e90c27a84dbb211086eccb66013e4
Kris Buschelman <buschelm@mcs.anl.gov>
2004-10-11 15:54:28 -0600

    bk-changeset-1.2621 kris@mlbuschel2.home|ChangeSet|20041011215428|04373 ChangeSet   1.2621 04/10/11 14:54:28 kris@mlbuschel2.home +3 -0   Move "aij" type specific routines to aijtype.c   Affected routine: matcreate_aij
      BitKeeper/etc/logging_ok
        1.87 04/10/11 14:54:28 kris@mlbuschel2.home +1 -0
        Logging to logging@openlogging.org accepted
    
      src/mat/impls/aij/mpi/aijtype.c
        1.1 04/10/11 14:54:24 kris@mlbuschel2.home +37 -0
    
      src/mat/impls/aij/mpi/mpiaij.c
        1.218 04/10/11 14:54:24 kris@mlbuschel2.home +0 -37
        Move "aij" type specific routines to aijtype.c
    
      src/mat/impls/aij/mpi/aijtype.c
        1.0 04/10/11 14:54:24 kris@mlbuschel2.home +0 -0
        BitKeeper file /home/kris/petsc/dev/src/mat/impls/aij/mpi/aijtype.c
    
    
    Hg-commit: 12c863451183c8f542c939777b4492c471f3ffff

---
+++ src/mat/impls/aij/mpi/aijtype.c
@@ -0,0 +1,37 @@
+/*MC
+   MATAIJ - MATAIJ = "aij" - A matrix type to be used for sparse matrices.
+
+   This matrix type is identical to MATSEQAIJ when constructed with a single process communicator,
+   and MATMPIAIJ otherwise.  As a result, for single process communicators, 
+  MatSeqAIJSetPreallocation is supported, and similarly MatMPIAIJSetPreallocation is supported 
+  for communicators controlling multiple processes.  It is recommended that you call both of
+  the above preallocation routines for simplicity.
+
+   Options Database Keys:
+. -mat_type aij - sets the matrix type to "aij" during a call to MatSetFromOptions()
+
+  Level: beginner
+
+.seealso: MatCreateMPIAIJ,MATSEQAIJ,MATMPIAIJ
+M*/
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatCreate_AIJ"
+PetscErrorCode MatCreate_AIJ(Mat A) 
+{
+  PetscErrorCode ierr;
+  PetscMPIInt    size;
+
+  PetscFunctionBegin;
+  ierr = PetscObjectChangeTypeName((PetscObject)A,MATAIJ);CHKERRQ(ierr);
+  ierr = MPI_Comm_size(A->comm,&size);CHKERRQ(ierr);
+  if (size == 1) {
+    ierr = MatSetType(A,MATSEQAIJ);CHKERRQ(ierr);
+  } else {
+    ierr = MatSetType(A,MATMPIAIJ);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
