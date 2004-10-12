3d679f9f2d5a3ac09a9543ad3f4036e3bf027c87
Kris Buschelman <buschelm@mcs.anl.gov>
2004-10-11 18:40:39 -0600

    bk-changeset-1.2623 kris@mlbuschel2.home|ChangeSet|20041012004039|14265 ChangeSet   1.2623 04/10/11 17:40:39 kris@mlbuschel2.home +3 -0   Implement converters for "aij" type.
      src/mat/impls/aij/aij/makefile
        1.1 04/10/11 17:40:38 kris@mlbuschel2.home +17 -0
    
      src/mat/utils/matioall.c
        1.30 04/10/11 17:40:38 kris@mlbuschel2.home +2 -0
        Register "to converter" for "aij" type.
    
      src/mat/impls/aij/aij/makefile
        1.0 04/10/11 17:40:38 kris@mlbuschel2.home +0 -0
        BitKeeper file /home/kris/petsc/dev/src/mat/impls/aij/aij/makefile
    
      src/mat/impls/aij/aij/aijtype.c
        1.3 04/10/11 17:40:38 kris@mlbuschel2.home +219 -1
        Implement comverters for "aij" type.
    
    
    Hg-commit: 1c4a29b176ffbebea3adf52926c966bba5342c50

--- src/mat/impls/aij/aij/aijtype.c
+++ src/mat/impls/aij/aij/aijtype.c
@@ -1,3 +1,220 @@
+#include "src/mat/matimp.h"
+
+typedef struct {
+  PetscErrorCode (*MatConvert)(Mat,const MatType,Mat*);
+  PetscErrorCode (*MatDestroy)(Mat);
+} Mat_AIJ;
+
+EXTERN_C_BEGIN
+EXTERN PetscErrorCode MatConvert_SeqAIJ_AIJ(Mat,const MatType,Mat *);
+EXTERN PetscErrorCode MatConvert_MPIAIJ_AIJ(Mat,const MatType,Mat *);
+EXTERN PetscErrorCode MatConvert_AIJ_SeqAIJ(Mat,const MatType,Mat *);
+EXTERN PetscErrorCode MatConvert_AIJ_MPIAIJ(Mat,const MatType,Mat *);
+EXTERN_C_END
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatConvert_AIJ_SeqAIJ"
+PetscErrorCode MatConvert_AIJ_SeqAIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  Mat_AIJ        *aij=(Mat_AIJ *)A->spptr;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    ierr = MatDuplicate(A,MAT_COPY_VALUES, &B);CHKERRQ(ierr);
+  }
+
+  B->ops->convert = aij->MatConvert;
+  B->ops->destroy = aij->MatDestroy;
+
+  ierr = PetscFree(aij);CHKERRQ(ierr);
+
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_aij_seqaij_C","",PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_seqaij_aij_C","",PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscObjectChangeTypeName((PetscObject),B,MATSEQAIJ);CHKERRQ(ierr);
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatConvert_AIJ_MPIAIJ"
+PetscErrorCode MatConvert_AIJ_MPIAIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  Mat_AIJ        *aij=(Mat_AIJ *)A->spptr;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    ierr = MatDuplicate(A,MAT_COPY_VALUES, &B);CHKERRQ(ierr);
+  }
+
+  B->ops->convert = aij->MatConvert;
+  B->ops->destroy = aij->MatDestroy;
+
+  ierr = PetscFree(aij);CHKERRQ(ierr);
+
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_aij_mpiaij_C","",PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_mpiaij_aij_C","",PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscObjectChangeTypeName((PetscObject),B,MATMPIAIJ);CHKERRQ(ierr);
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+#undef __FUNCT__
+#define __FUNCT__ "MatDestroy_AIJ"
+PetscErrorCode MatDestroy_AIJ(Mat A)
+{
+  Mat_AIJ        *aij = (Mat_AIJ *)A->spptr;
+  PetscMPIInt    size;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = MPI_Comm_size(A->comm,&size);CHKERRQ(ierr);
+  if (size == 1) {
+    ierr = MatConvert_AIJ_SeqAIJ(A,MATSEQAIJ,&A);CHKERRQ(ierr);
+  } else {
+    ierr = MatConvert_AIJ_MPIAIJ(A,MATMPIAIJ,&A);CHKERRQ(ierr);
+  }
+  ierr = (*A->ops->destroy)(A);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatConvertFrom_AIJviaSeqAIJ"
+PetscErrorCode MatConvertFrom_AIJviaSeqAIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  PetscTruth     inplace=PETSC_TRUE;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    inplace = PETSC_FALSE;
+  }
+  ierr = MatConvert_AIJ_SeqAIJ(A,MATSEQAIJ,&A);CHKERRQ(ierr);
+  ierr = MatConvert(A,type,&B);CHKERRQ(ierr);
+  if (!inplace) {
+    ierr = MatConvert_SeqAIJ_AIJ(A,MATAIJ,&A);CHKERRQ(ierr);
+  }
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatConvertFrom_AIJviaMPIAIJ"
+PetscErrorCode MatConvertFrom_AIJviaMPIAIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  PetscTruth     inplace=PETSC_TRUE;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    inplace = PETSC_FALSE;
+  }
+  ierr = MatConvert_AIJ_MPIAIJ(A,MATSEQAIJ,&A);CHKERRQ(ierr);
+  ierr = MatConvert(A,type,&B);CHKERRQ(ierr);
+  if (!inplace) {
+    ierr = MatConvert_MPIAIJ_AIJ(A,MATAIJ,&A);CHKERRQ(ierr);
+  }
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatConvert_SeqAIJ_AIJ"
+PetscErrorCode MatConvert_SeqAIJ_AIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  Mat_AIJ        *aij;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    ierr = MatDuplicate(A,MAT_COPY_VALUES, &B);CHKERRQ(ierr);
+  }
+  
+  ierr = PetscNew(Mat_AIJ,&aij);CHKERRQ(ierr);
+  aij->MatConvert = A->ops->convert;
+  aij->MatDestroy = A->ops->destroy;
+
+  B->spptr        = (void *)aij;
+  B->ops->convert = MatConvertFrom_AIJviaSeqAIJ;
+  B->ops->destroy = MatDestroy_AIJ;
+
+  ierr = PetscObjectComposeFunctionDynamic((PetscObject)B,"MatConvert_aij_seqaij_C",
+                                    "MatConvert_AIJ_SeqAIJ",MatConvert_AIJ_SeqAIJ);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunctionDynamic((PetscObject)B,"MatConvert_seqaij_aij_C",
+                                    "MatConvert_SeqAIJ_AIJ",MatConvert_SeqAIJ_AIJ);CHKERRQ(ierr);
+  ierr = PetscObjectChangeTypeName((PetscObject),B,MATAIJ);CHKERRQ(ierr);
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatConvert_MPIAIJ_AIJ"
+PetscErrorCode MatConvert_MPIAIJ_AIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  Mat            B=*newmat;
+  Mat_AIJ        *aij;
+
+  PetscFunctionBegin;
+  if (B != A) {
+    ierr = MatDuplicate(A,MAT_COPY_VALUES, &B);CHKERRQ(ierr);
+  }
+
+  ierr = PetscNew(Mat_AIJ,&aij);CHKERRQ(ierr);
+  aij->MatConvert = A->ops->convert;
+  aij->MatDestroy = A->ops->destroy;
+
+  B->spptr        = (void *)aij;
+  B->ops->convert = MatConvertFrom_AIJviaMPIAIJ;
+  B->ops->destroy = MatDestroy_AIJ;
+
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_aij_mpiaij_C",
+                                    "MatConvert_AIJ_MPIAIJ",MatConvert_AIJ_MPIAIJ);CHKERRQ(ierr);
+  ierr = PetscObjectComposeFunction((PetscObject)B,"MatConvert_mpiaij_aij_C",
+                                    "MatConvert_MPIAIJ_AIJ",MatConvert_MPIAIJ_AIJ);CHKERRQ(ierr);
+  ierr = PetscObjectChangeTypeName((PetscObject),B,MATAIJ);CHKERRQ(ierr);
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+EXTERN_C_BEGIN
+#undef __FUNCT__
+#define __FUNCT__ "MatConvertTo_AIJ"
+PetscErrorCode MatConvertTo_AIJ(Mat A, const MatType type, Mat *newmat)
+{
+  PetscErrorCode ierr;
+  MPI_Comm       comm;
+  PetscMPIInt    size;
+  Mat            B=*newmat;
+
+  PetscFunctionBegin;
+  ierr = MPI_Comm_size(A->comm,&size);
+  if (size == 1) {
+    ierr = MatConvert(A,MATSEQAIJ,&B);CHKERRQ(ierr);
+    ierr = MatConvert_SeqAIJ_AIJ(B,MATAIJ,&B);CHKERRQ(ierr);
+  } else {
+    ierr = MatConvert(A,MATMPIAIJ,&B);CHKERRQ(ierr);
+    ierr = MatConvert_MPIAIJ_AIJ(B,MATAIJ,&B);CHKERRQ(ierr);
+  }
+  *newmat = B;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
 /*MC
    MATAIJ - MATAIJ = "aij" - A matrix type to be used for sparse matrices.
 
@@ -28,10 +245,11 @@ PetscErrorCode MatCreate_AIJ(Mat A)
   ierr = MPI_Comm_size(A->comm,&size);CHKERRQ(ierr);
   if (size == 1) {
     ierr = MatSetType(A,MATSEQAIJ);CHKERRQ(ierr);
+    ierr = MatConvert_SeqAIJ_AIJ(A,MATAIJ,&A);CHKERRQ(ierr);
   } else {
     ierr = MatSetType(A,MATMPIAIJ);CHKERRQ(ierr);
+    ierr = MatConvert_MPIAIJ_AIJ(A,MATAIJ,&A);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
-
