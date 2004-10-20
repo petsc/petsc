28ce894fea304713c7e57d46b0ce3d37a62ada4f
victor <victor@tibook500ve.cs.utk.edu>
2004-10-20 13:33:23 -0600

    bk-changeset-1.2629.1.30 victor@tibook500ve.cs.utk.edu|ChangeSet|20041020193323|44866 ChangeSet   1.2629.1.30 04/10/20 15:33:23 victor@tibook500ve.cs.utk.edu +1 -0   C standard enforced
      src/mat/impls/aij/seq/aijfact.c
        1.74 04/10/20 15:33:19 victor@tibook500ve.cs.utk.edu +1 -1
        moved declaration
    
    
    Hg-commit: c8c8e54d1ec02408c555c7843419dbfb06799f09

--- src/mat/impls/aij/seq/aijfact.c
+++ src/mat/impls/aij/seq/aijfact.c
@@ -1134,9 +1134,9 @@ PetscErrorCode MatCholeskyFactorNumeric_SeqAIJ(Mat A,Mat *B)
     } 
  
     for (k = 0; k<mbs; k++){
+      PetscScalar *baval = ba + bi[k];
       /*initialize k-th row by the perm[k]-th row of A */
       jmin = ai[rip[k]]; jmax = ai[rip[k]+1];
-      PetscScalar *baval = ba + bi[k];
       for (j = jmin; j < jmax; j++){
         col = rip[aj[j]];
         if (col >= k){ /* only take upper triangular entry */
