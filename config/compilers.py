7de6d41dc123280c05e50f01366190d30cac5af9
Matthew Knepley <knepley@mcs.anl.gov>
2004-10-27 14:54:38 -0600

    bk-changeset-1.1108 knepley@khan.mcs.anl.gov|ChangeSet|20041027205438|10812 ChangeSet   1.1108 04/10/27 13:05:17 knepley@khan.mcs.anl.gov +1 -0   Merge bk://sidl@sidl.bkbits.net/BuildSystem   into khan.mcs.anl.gov:/PETSc3/sidl/BuildSystem
      config/compilers.py
        1.127 04/10/27 13:05:16 knepley@khan.mcs.anl.gov +0 -0
        Auto merged
    
    
    Hg-commit: 0499c580e97255a075f05a7b069143840d7c77cf

--- config/compilers.py
+++ config/compilers.py
@@ -511,10 +511,10 @@ class Configure(config.base.Configure):
       self.executeTest(self.checkFortranTypeSizes)
       self.executeTest(self.checkFortranNameMangling)
       self.executeTest(self.checkFortranPreprocessor)
-    if self.framework.compilers is None:
-      self.logPrint('Setting framework compilers to this module', 2, 'compilers')
-      self.framework.compilers = self
     self.executeTest(self.checkFortranLibraries)
     self.executeTest(self.checkFortran90Interface)
     self.executeTest(self.output)
+    if self.framework.compilers is None:
+      self.logPrint('Setting framework compilers to this module', 2, 'compilers')
+      self.framework.compilers = self
     return
