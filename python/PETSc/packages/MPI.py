310a4dc885bceea369f9dff4a4bb5acf0d6916c3
Matthew Knepley <knepley@mcs.anl.gov>
2004-10-27 06:36:16 -0600

    bk-changeset-1.2631 knepley@khan.mcs.anl.gov|ChangeSet|20041027123616|34783 ChangeSet   1.2631 04/10/27 07:36:16 knepley@khan.mcs.anl.gov +1 -0   Small MPI fix
      python/PETSc/packages/MPI.py
        1.139.1.1 04/10/27 07:35:32 knepley@khan.mcs.anl.gov +7 -9
        Changed logic slightly
    
    
    Hg-commit: 081d284f239bbb86c980edef511b43a846344aae

--- python/PETSc/packages/MPI.py
+++ python/PETSc/packages/MPI.py
@@ -126,17 +126,15 @@ class Configure(config.base.Configure):
           return 0
       if not self.checkMPILink('', '          include \'mpif.h\'\n          call MPI_Init(ierr)\n'):
         self.framework.log.write('MPI cannot locate Fortran includes, but can find the MPI libraries\n')
+        # Get around bug in g77 on SGI IA64 systems
         if not self.include:
-          # get around bug in g77 on SGI IA64 systems
-          savetmp = self.framework.argDB['CPPFLAGS']
-          self.framework.argDB['CPPFLAGS'] += ' -I/usr/include'
-          if self.checkMPILink('', '          include \'mpif.h\'\n          call MPI_Init(ierr)\n'):
+          self.include.append(os.path.join('/usr', 'include'))
+          if not self.checkMPILink('', '          include \'mpif.h\'\n          call MPI_Init(ierr)\n'):
+            self.include.pop()
             self.popLanguage()
-            self.framework.log.write('Added /usr/include to search path to find MPI includes from Fortran. Succeeded\n')
-            return 1
-          self.popLanguage()
-          self.framework.argDB['CPPFLAGS'] = savetmp
-        return 0  
+            self.framework.log.write('Adding /usr/include to search path did not fix MPI includes from Fortran. Failed\n')
+            return 0  
+          self.framework.log.write('Added /usr/include to search path to find MPI includes from Fortran. Succeeded\n')
       # Also do Satish check for broken mpif90 (MPICH)
       # 1) bug.F
       #       program main
