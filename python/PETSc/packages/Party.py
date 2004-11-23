d73c20ad7b77610f2ca738f399b6b1f775098ac7
Hong Zhang <hzhang@mcs.anl.gov>
2004-11-23 14:26:05 -0600

    bk-changeset-1.2736.1.10 hzhang@harley.mcs.anl.gov|ChangeSet|20041123202605|63701 ChangeSet   1.2736.1.10 04/11/23 14:26:05 hzhang@harley.mcs.anl.gov +1 -0   add download method to Party.py
      python/PETSc/packages/Party.py
        1.3 04/11/23 14:26:03 hzhang@harley.mcs.anl.gov +56 -2
        add download method
    
    
    Hg-commit: 63cb45dd4fc03c766a119199a09f0122b05f1c89

--- python/PETSc/packages/Party.py
+++ python/PETSc/packages/Party.py
@@ -35,6 +35,7 @@ class Configure(config.base.Configure):
     help.addArgument('Party', '-with-party-dir=<root dir>',        nargs.ArgDir(None, None, 'Specify the root directory of the Party installation'))
     help.addArgument('Party', '-with-party-include=<dir>',         nargs.ArgDir(None, None, 'The directory containing party_lib.h'))
     help.addArgument('Party', '-with-party-lib=<lib>',             nargs.Arg(None, None, 'The Party library or list of libraries'))
+    help.addArgument('Party', '-download-party=<no,yes,ifneeded>', nargs.ArgFuzzyBool(None, 0, 'Automatically install Party'))
     return
 
   def checkLib(self, libraries):
@@ -56,6 +57,10 @@ class Configure(config.base.Configure):
   def generateGuesses(self):
     if 'with-party-lib' in self.framework.argDB and 'with-party-dir' in self.framework.argDB:
       raise RuntimeError('You cannot give BOTH Party library with --with-party-lib=<lib> and search directory with --with-party-dir=<dir>')
+    if self.framework.argDB['download-party'] == 1:
+      (name, lib, include) = self.downloadParty()
+      yield (name, lib, include)
+      raise RuntimeError('Downloaded Party could not be used. Please check install in '+os.path.dirname(include[0][0])+'\n')
     # Try specified library (and include)
     if 'with-party-lib' in self.framework.argDB: #~PARTY_1.99/libparty.a
       libs = self.framework.argDB['with-party-lib'] #='~PARTY_1.99/libparty.a'
@@ -83,8 +88,57 @@ class Configure(config.base.Configure):
       includes = dir
       yield ('User specified installation root', [[libs]], [includes])
       raise RuntimeError('You set a value for --with-party-dir, but '+self.framework.argDB['with-party-dir']+' cannot be used.\n')
+    # If necessary, download Party
+    if not self.found and self.framework.argDB['download-party'] == 2:
+      (name, lib, include) = self.downloadParty()
+      yield (name, lib, include)
+      raise RuntimeError('Downloaded Party could not be used. Please check in install in '+os.path.dirname(include[0][0])+'\n')
     return
+  
+  def getDir(self):
+    '''Find the directory containing Party'''
+    packages  = self.framework.argDB['with-external-packages-dir'] 
+    if not os.path.isdir(packages):
+      os.mkdir(packages)
+      self.framework.actions.addArgument('PETSc', 'Directory creation', 'Created the packages directory: '+packages)
+    PartyDir = None
+    for dir in os.listdir(packages):
+      if dir.startswith('PARTY_1.99') and os.path.isdir(os.path.join(packages, dir)):
+        PartyDir = dir
+    if PartyDir is None:
+      self.framework.logPrint('Could not locate already downloaded Party')
+      raise RuntimeError('Error locating Party directory')
+    return os.path.join(packages, PartyDir)
 
+  def downloadParty(self):
+    self.framework.logPrint('Downloading Party')
+    try:
+      PartyDir = self.getDir()  
+      self.framework.logPrint('Party already downloaded, no need to ftp')
+    except RuntimeError:
+      import urllib
+      packages = self.framework.argDB['with-external-packages-dir']
+      try:
+        self.logPrint("Retrieving Party; this may take several minutes\n", debugSection='screen')
+        urllib.urlretrieve('ftp://ftp.mcs.anl.gov/pub/petsc/PARTY_1.99.tar.gz', os.path.join(packages, 'PARTY_1.99.tar.gz'))
+      except Exception, e:
+        raise RuntimeError('Error downloading Party: '+str(e))
+      try:
+        config.base.Configure.executeShellCommand('cd '+packages+'; gunzip PARTY_1.99.tar.gz', log = self.framework.log)
+      except RuntimeError, e:
+        raise RuntimeError('Error unzipping PARTY_1.99.tar.gz: '+str(e))
+      try:
+        config.base.Configure.executeShellCommand('cd '+packages+'; tar -xf PARTY_1.99.tar', log = self.framework.log)
+      except RuntimeError, e:
+        raise RuntimeError('Error doing tar -xf PARTY_1.99.tar: '+str(e))
+      os.unlink(os.path.join(packages, 'PARTY_1.99.tar'))
+      self.framework.actions.addArgument('Party', 'Download', 'Downloaded Party into '+self.getDir())
+    
+    PartyDir = self.getDir()
+    lib     = [[os.path.join(PartyDir, 'libparty.a')]] 
+    include = [PartyDir]
+    return ('Downloaded Party', lib, include)
+  
   def configureVersion(self):
     '''Determine the Party version, but there is no reliable way right now'''
     return 'Unknown'
@@ -123,7 +177,7 @@ class Configure(config.base.Configure):
     return
 
   def configure(self):
-    if (self.framework.argDB['with-party']):
+    if (self.framework.argDB['with-party'] or self.framework.argDB['download-party'] == 1):
       if self.framework.argDB['with-64-bit-ints']:
         raise RuntimeError('Cannot use '+self.name+' with 64 bit integers, it is not coded for this capability')   
       self.executeTest(self.configureLibrary)
