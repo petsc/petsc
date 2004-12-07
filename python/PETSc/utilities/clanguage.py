a1f0bb1a2d8129ef57963ca822635b5481d64f40
Satish Balay <balay@mcs.anl.gov>
2004-12-07 17:44:59 -0600

    bk-changeset-1.2798 balay@asterix.mcs.anl.gov|ChangeSet|20041207234459|33899 ChangeSet   1.2798 04/12/07 17:44:59 balay@asterix.mcs.anl.gov +1 -0   if 'complex' is enabled - bump the language to 'cxx'
      python/PETSc/utilities/clanguage.py
        1.6 04/12/07 17:44:58 balay@asterix.mcs.anl.gov +5 -3
        if 'complex' is enabled - bump the language to 'cxx'
    
    
    Hg-commit: 19e5d420f92aed1668f3441d1b8cdc5e449549c1

--- python/PETSc/utilities/clanguage.py
+++ python/PETSc/utilities/clanguage.py
@@ -21,9 +21,6 @@ class Configure(config.base.Configure):
     return
 
   def configureLanguage(self):
-    # should do error checking
-    self.language   = self.framework.argDB['with-language'].upper().replace('+','x').replace('X','x')
-
     self.precision  = self.framework.argDB['with-precision']
     if self.precision.lower() == 'single':
       self.addDefine('USE_SINGLE','1')
@@ -33,10 +30,15 @@ class Configure(config.base.Configure):
       raise RuntimeError('--with-precision must be single, double or matsingle')
 
     self.scalartype = self.framework.argDB['with-scalar-type']
+    # make sure the language is cxx for complex
     if self.scalartype.lower() == 'complex':
+      self.framework.argDB['with-language']='cxx'
       self.addDefine('USE_COMPLEX','1')
     elif not self.scalartype.lower() == 'real':
       raise RuntimeError('--with-scalar-type must be real or complex')
+
+    # should do error checking
+    self.language   = self.framework.argDB['with-language'].upper().replace('+','x').replace('X','x')
       
   def configure(self):
     self.executeTest(self.configureLanguage)
