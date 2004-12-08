7aafae78bffce97f07fda69981d941535b7f97a6
Matthew Knepley <knepley@mcs.anl.gov>
2004-12-08 15:18:18 -0600

    bk-changeset-1.2804 knepley@khan.mcs.anl.gov|ChangeSet|20041208211818|27643 ChangeSet   1.2804 04/12/04 10:39:47 knepley@khan.mcs.anl.gov +1 -0   Configure fix
      python/PETSc/utilities/clanguage.py
        1.7 04/12/04 10:39:45 knepley@khan.mcs.anl.gov +36 -17
        Added -with-extern-c to turn on PETSC_USE_EXTERN_CXX
    
    
    Hg-commit: 31d7f3bac39971d5406309dcc1cc1df33996fbe9

--- python/PETSc/utilities/clanguage.py
+++ python/PETSc/utilities/clanguage.py
@@ -16,30 +16,49 @@ class Configure(config.base.Configure):
   def setupHelp(self, help):
     import nargs
     help.addArgument('PETSc', '-with-language=<C or C++>', nargs.Arg(None, 'C', 'Specify C or C++ language'))
+    help.addArgument('PETSc', '-with-extern-c', nargs.ArgBool(None, 0, 'In C++ mode, wrap all symbols in "extern C"'))
     help.addArgument('PETSc', '-with-precision=<single,double,matsingle>', nargs.Arg(None, 'double', 'Specify numerical precision'))    
     help.addArgument('PETSc', '-with-scalar-type=<real or complex>', nargs.Arg(None, 'real', 'Specify real or complex numbers'))
     return
 
-  def configureLanguage(self):
-    self.precision  = self.framework.argDB['with-precision']
-    if self.precision.lower() == 'single':
-      self.addDefine('USE_SINGLE','1')
-    elif self.precision.lower() == 'matsingle':
-      self.addDefine('USE_MAT_SINGLE','1')
-    elif not self.precision.lower() == 'double':
-      raise RuntimeError('--with-precision must be single, double or matsingle')
-
-    self.scalartype = self.framework.argDB['with-scalar-type']
-    # make sure the language is cxx for complex
-    if self.scalartype.lower() == 'complex':
-      self.framework.argDB['with-language']='cxx'
-      self.addDefine('USE_COMPLEX','1')
-    elif not self.scalartype.lower() == 'real':
+  def configureScalarType(self):
+    '''Choose between real and complex numbers'''
+    self.scalartype = self.framework.argDB['with-scalar-type'].lower()
+    if self.scalartype == 'complex':
+      self.framework.argDB['with-language'] = 'Cxx'
+      self.addDefine('USE_COMPLEX', '1')
+    elif not self.scalartype == 'real':
       raise RuntimeError('--with-scalar-type must be real or complex')
+    return
 
-    # should do error checking
-    self.language   = self.framework.argDB['with-language'].upper().replace('+','x').replace('X','x')
-      
+  def configurePrecision(self):
+    '''Set the default real number precision for PETSc objects'''
+    self.precision = self.framework.argDB['with-precision'].lower()
+    if self.precision == 'single':
+      self.addDefine('USE_SINGLE', '1')
+    elif self.precision == 'matsingle':
+      self.addDefine('USE_MAT_SINGLE', '1')
+    elif not self.precision == 'double':
+      raise RuntimeError('--with-precision must be single, double, or matsingle')
+    return
+
+  def configureLanguage(self):
+    '''Choose between C and C++ bindings'''
+    self.language = self.framework.argDB['with-language'].upper().replace('+','x').replace('X','x')
+    if not self.language in ['C', 'Cxx']:
+      raise RuntimeError('Invalid language specified: '+str(self.language))
+    return
+
+  def configureExternC(self):
+    '''Choose between C and C++ bindings'''
+    if self.language == 'Cxx':
+      if self.framework.argDB['with-extern-c']:
+        self.addDefine('USE_EXTERN_CXX')
+    return
+
   def configure(self):
+    self.executeTest(self.configureScalarType)
+    self.executeTest(self.configurePrecision)
     self.executeTest(self.configureLanguage)
+    self.executeTest(self.configureExternC)
     return
