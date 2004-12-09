a5d1af9ebc3583cb706b220b0be5f8c7cafee276
Satish Balay <balay@mcs.anl.gov>
2004-12-09 10:26:05 -0600

    bk-changeset-1.2468.6.66 balay@asterix.mcs.anl.gov|ChangeSet|20041209162605|18508 ChangeSet   1.2468.6.66 04/12/09 10:26:05 balay@asterix.mcs.anl.gov +1 -0   create a new patchlevel for matlab fixes
      include/petscversion.h
        1.120 04/12/09 10:26:04 balay@asterix.mcs.anl.gov +2 -2
        create a new patchlevel for matlab fixes
    
    
    Hg-commit: c59267d423ac32f0e78063cddb971b35dd4141cc

--- include/petscversion.h
+++ include/petscversion.h
@@ -14,9 +14,9 @@ PETSC_EXTERN_CXX_BEGIN
 #define PETSC_VERSION_MAJOR      2
 #define PETSC_VERSION_MINOR      2
 #define PETSC_VERSION_SUBMINOR   1
-#define PETSC_VERSION_PATCH      27
+#define PETSC_VERSION_PATCH      28
 #define PETSC_VERSION_DATE       "August, 18, 2004"
-#define PETSC_VERSION_PATCH_DATE "November, 23, 2004"
+#define PETSC_VERSION_PATCH_DATE "December, 9, 2004"
 #define PETSC_AUTHOR_INFO        "\
        The PETSc Team\n\
     petsc-maint@mcs.anl.gov\n\
