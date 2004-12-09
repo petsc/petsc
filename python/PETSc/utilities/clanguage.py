36c314c99917e5144e4c4469e554f2937ffadd32
Barry Smith <bsmith@mcs.anl.gov>
2004-12-09 09:52:09 -0600

    bk-changeset-1.2804.1.2 barrysmith@barrysmith-57.mcs.anl.gov|ChangeSet|20041209155209|28405 ChangeSet   1.2804.1.2 04/12/09 09:52:09 barrysmith@barrysmith-57.mcs.anl.gov +1 -0   fixed Matt's fix of when to set USE_EXTERN_CXX
      python/PETSc/utilities/clanguage.py
        1.7.1.1 04/12/09 09:51:34 barrysmith@barrysmith-57.mcs.anl.gov +3 -5
        fixed Matt's fix of when to set USE_EXTERN_CXX
    
    
    Hg-commit: 4b6962c351cf8206afe8cedb48495b4fffc8fa18

--- python/PETSc/utilities/clanguage.py
+++ python/PETSc/utilities/clanguage.py
@@ -16,7 +16,6 @@ class Configure(config.base.Configure):
   def setupHelp(self, help):
     import nargs
     help.addArgument('PETSc', '-with-language=<C or C++>', nargs.Arg(None, 'C', 'Specify C or C++ language'))
-    help.addArgument('PETSc', '-with-extern-c', nargs.ArgBool(None, 0, 'In C++ mode, wrap all symbols in "extern C"'))
     help.addArgument('PETSc', '-with-precision=<single,double,matsingle>', nargs.Arg(None, 'double', 'Specify numerical precision'))    
     help.addArgument('PETSc', '-with-scalar-type=<real or complex>', nargs.Arg(None, 'real', 'Specify real or complex numbers'))
     return
@@ -50,10 +49,9 @@ class Configure(config.base.Configure):
     return
 
   def configureExternC(self):
-    '''Choose between C and C++ bindings'''
-    if self.language == 'Cxx':
-      if self.framework.argDB['with-extern-c']:
-        self.addDefine('USE_EXTERN_CXX')
+    '''C version of libraries needs to wrap headers in extern C, for when used with a C++ compiler'''
+    if self.language == 'C':
+      self.addDefine('USE_EXTERN_CXX')
     return
 
   def configure(self):
