a08dad1f447a26af00d5e517e261f3f2c4f5c925
Kris Buschelman <buschelm@mcs.anl.gov>
2004-12-20 20:04:03 -0600

    bk-changeset-1.2821.5.3 Kris@p4buschel.|ChangeSet|20041221020403|31704 ChangeSet   1.2821.5.3 04/12/20 18:04:03 Kris@p4buschel. +2 -0   Separate ctable public interface from private implementation.
      src/sys/ctable.h
        1.8 04/12/20 18:04:01 Kris@p4buschel. +2 -13
        Separate ctable public interface from private implementation.
    
      include/petscctable.h
        1.2 04/12/20 18:04:01 Kris@p4buschel. +10 -10
        Separate ctable public interface from private implementation.
    
    
    Hg-commit: 0435ef73ce3223855d8f8151488a0a8fa90d1b84

--- include/petscctable.h
+++ include/petscctable.h
@@ -5,15 +5,15 @@
 typedef struct _p_PetscTable * PetscTable;
 typedef PetscInt* PetscTablePosition;  
 
-EXTERN PetscErrorCode PetscTableCreate(const PetscInt,PetscTable*);
-EXTERN PetscErrorCode PetscTableCreateCopy(const PetscTable,PetscTable*);
-EXTERN PetscErrorCode PetscTableDelete(PetscTable);
-EXTERN PetscErrorCode PetscTableGetCount(const PetscTable,PetscInt*);
-EXTERN PetscErrorCode PetscTableIsEmpty(const PetscTable,PetscInt*);
-EXTERN PetscErrorCode PetscTableAdd(PetscTable,const PetscInt,const PetscInt);
-EXTERN PetscErrorCode PetscTableFind(PetscTable,const PetscInt,PetscInt*);
-EXTERN PetscErrorCode PetscTableGetHeadPosition(PetscTable,PetscTablePosition*);
-EXTERN PetscErrorCode PetscTableGetNext(PetscTable,PetscTablePosition*,PetscInt*,PetscInt*);
-EXTERN PetscErrorCode PetscTableRemoveAll(PetscTable);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableCreate(const PetscInt,PetscTable*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableCreateCopy(const PetscTable,PetscTable*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableDelete(PetscTable);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableGetCount(const PetscTable,PetscInt*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableIsEmpty(const PetscTable,PetscInt*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableAdd(PetscTable,const PetscInt,const PetscInt);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableFind(PetscTable,const PetscInt,PetscInt*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableGetHeadPosition(PetscTable,PetscTablePosition*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableGetNext(PetscTable,PetscTablePosition*,PetscInt*,PetscInt*);
+EXTERN PetscErrorCode PETSC_DLLEXPORT PetscTableRemoveAll(PetscTable);
 
 #endif
