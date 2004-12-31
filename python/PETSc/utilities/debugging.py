4b55f32ec0b6293f594e6a72da687e4942209908
Barry Smith <bsmith@mcs.anl.gov>
2004-12-30 22:08:13 -0600

    bk-changeset-1.2825.1.1 barrysmith@barry-smiths-computer.local|ChangeSet|20041231040813|10517 ChangeSet   1.2825.1.1 04/12/30 22:08:13 barrysmith@barry-smiths-computer.local +2 -0   add option to turn off error checking
      python/PETSc/utilities/debugging.py
        1.8 04/12/30 22:08:09 barrysmith@barry-smiths-computer.local +14 -2
        add option to turn off error checking
    
      include/petscerror.h
        1.44 04/12/30 17:54:41 barrysmith@barry-smiths-computer.local +1 -1
        always do error checking unless configure flag is set
        it was a mistake for me to only check for debug because
        we use error checking for exceptions that users may depend on
    
    
    Hg-commit: f65e0873d85b4557789f05448759939ceea73056

--- python/PETSc/utilities/debugging.py
+++ python/PETSc/utilities/debugging.py
@@ -16,17 +16,29 @@ class Configure(config.base.Configure):
   def setupHelp(self, help):
     import nargs
     help.addArgument('PETSc', '-with-debugging=<yes or no>', nargs.ArgBool(None, 1, 'Specify debugging version of libraries'))
+    help.addArgument('PETSc', '-with-errorchecking=<yes or no>', nargs.ArgBool(None, 1, 'Specify error checking/exceptions in libraries'))    
     return
 
   def configureDebugging(self):
     # should do error checking
+    if self.framework.argDB['with-errorchecking']:
+      self.addDefine('USE_ERRORCHECKING',1=1)
+    else:
+      self.framework.logClear()
+      print '=================================================================================\r'
+      print '          WARNING! Compiling PETSc with NO error checking/exception handling,  \r'
+      print '        this should only be done for timing and production runs where you DO NOT \r'
+      print '        use PETSc exceptions. All development should be done when configured using\r'
+      print '         --with-errorchecking=1 \r'          
+      print '=================================================================================\r'  
+
     self.debugging = self.framework.argDB['with-debugging']
     if not self.debugging:
       self.framework.logClear()
       print '=================================================================================\r'
-      print '          WARNING! Compiling PETSc with no error checking, this should \r'
+      print '          WARNING! Compiling PETSc with no debugging, this should \r'
       print '        only be done for timing and production runs. All development should \r'
-      print '        be done when configured with --with-debugging=1 \r'          
+      print '        be done when configured using --with-debugging=1 \r'          
       print '=================================================================================\r'  
     
   def configure(self):
