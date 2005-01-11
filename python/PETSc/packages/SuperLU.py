e5dfe79377c0cb0eb724b0fd28ffd095f2fb5ca8
Hong Zhang <hzhang@mcs.anl.gov>
2005-01-10 19:58:38 -0600

    bk-changeset-1.2842.1.3 hzhang@harley.mcs.anl.gov|ChangeSet|20050111015838|45010 ChangeSet   1.2842.1.3 05/01/10 19:58:38 hzhang@harley.mcs.anl.gov +1 -0   bug fix on Install() for SuperLU.py
      python/PETSc/packages/SuperLU.py
        1.15 05/01/10 19:58:36 hzhang@harley.mcs.anl.gov +52 -32
        bug fix
    
    
    Hg-commit: 95aa613b25e7ec218c0c50cc6010698f0d1004ac

--- python/PETSc/packages/SuperLU.py
+++ python/PETSc/packages/SuperLU.py
@@ -4,6 +4,7 @@ import user
 import config.base
 import os
 import PETSc.package
+import md5
 
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
@@ -17,11 +18,27 @@ class Configure(PETSc.package.Package):
     self.includedir   = 'SRC'
     return
 
+  def getChecksum(self,source, chunkSize = 1024*1024):  
+    '''Return the md5 checksum for a given file, which may also be specified by its filename
+       - The chunkSize argument specifies the size of blocks read from the file'''
+    if isinstance(source, file):
+      f = source
+    else:
+      f = file(source)
+    m = md5.new()
+    size = chunkSize
+    buf  = f.read(size)
+    while buf:
+      m.update(buf)
+      buf = f.read(size)
+    f.close()
+    return m.hexdigest()
+
   def generateLibList(self,dir):
     '''Normally the one in package.py is used'''
     alllibs = []
-    alllibs.append(os.path.join(dir,'superlu_linux.a'))
-    import config.setCompilers
+    alllibs.append(os.path.join(dir,'superlu.a'))
+    import config.setCompilers 
     self.framework.pushLanguage('C')
     self.framework.popLanguage()    
     return alllibs
@@ -32,46 +49,49 @@ class Configure(PETSc.package.Package):
     installDir = os.path.join(superluDir, self.arch.arch)
 
     # Configure and Build SUPERLU
-    self.framework.pushLanguage('C')
-    args = ['--prefix='+installDir, '--with-CC="'+self.framework.getCompiler()+' '+self.framework.getCompilerFlags()+'"']
-    self.framework.popLanguage()
-    if 'CXX' in self.framework.argDB:
-      self.framework.pushLanguage('Cxx')
-      args.append('--with-CXX="'+self.framework.getCompiler()+' '+self.framework.getCompilerFlags()+'"')
-      self.framework.popLanguage()
-    if 'FC' in self.framework.argDB:
-      self.framework.pushLanguage('FC')
-      args.append('--with-F77="'+self.framework.getCompiler()+' '+self.framework.getCompilerFlags()+'"')
-      self.framework.popLanguage()
-    args.append('--with-blas="'+self.libraries.toString(self.blasLapack.dlib)+'"')        
-    args = ' '.join(args)
-
-    try:
-      fd      = file(os.path.join(installDir,'config.args'))
-      oldargs = fd.readline()
-      fd.close()
-      
-    except:
-      oldargs = ''
-    if not oldargs == args:
-      self.framework.log.write('Have to rebuild SUPERLU oldargs = '+oldargs+' new args '+args+'\n')
+    if os.path.isfile(os.path.join(superluDir,'make.inc')):
+      output  = config.base.Configure.executeShellCommand('cd '+superluDir+'; rm -f make.inc', timeout=2500, log = self.framework.log)[0]
+    g = open(os.path.join(superluDir,'make.inc'),'w')
+    g.write('PLAT         =\n')
+    g.write('TMGLIB       = tmglib$(PLAT).a\n')
+    g.write('SUPERLULIB   = superlu$(PLAT).a\n')
+    g.write('BLASLIB      = '+self.libraries.toString(self.blasLapack.dlib)+'\n')
+    g.write('ARCH         = '+self.setcompilers.AR+'\n')
+    g.write('ARCHFLAGS    = '+self.setcompilers.AR_FLAGS+'\n')
+    g.write('RANLIB       = '+self.setcompilers.RANLIB+'\n')
+    self.setcompilers.pushLanguage('C')
+    g.write('CC           = '+self.setcompilers.getCompiler()+'\n')
+    g.write('CFLAGS       = -O2\n') #'+self.setcompilers.getCompilerFlags()+'\n')-> -fPIC -Wall -Wshadow -Wwrite-strings -g3 ???
+    g.write('LOADER       = '+self.setcompilers.getCompiler()+'\n') #gcc???
+    g.write('LOADOPTS     =\n')
+    self.setcompilers.popLanguage()
+    self.setcompilers.pushLanguage('FC')
+    g.write('FORTRAN      = '+self.setcompilers.getCompiler()+'\n')
+    g.write('FFLAGS       = -O2\n')
+    self.setcompilers.popLanguage()
+    g.write('CDEFS        = -DAdd_\n')
+    g.write('MATLAB       =\n')
+    g.close()
+    if not os.path.isdir(installDir):
+      os.mkdir(installDir)
+    if not os.path.isfile(os.path.join(installDir,'make.inc')) or not (self.getChecksum(os.path.join(installDir,'make.inc')) == self.getChecksum(os.path.join(superluDir,'make.inc'))):
+      self.framework.log.write('Have to rebuild SuperLU, make.inc != '+installDir+'/make.inc\n')
       try:
         self.logPrint("Compiling superlu; this may take several minutes\n", debugSection='screen')
-        output = config.base.Configure.executeShellCommand('cd '+superluDir+'; SUPERLU_INSTALL_DIR='+installDir+'; export SUPERLU_INSTALL_DIR; cp MAKE_INC/make.inc .; make install lib; mv *.a '+os.path.join(installDir,self.libdir)+'; mkdir '+os.path.join(installDir,self.includedir)+'; cp SRC/*.h '+os.path.join(installDir,self.includedir)+'/.', timeout=2500, log = self.framework.log)[0]
+        output = config.base.Configure.executeShellCommand('cd '+superluDir+'; SUPERLU_INSTALL_DIR='+installDir+'; export SUPERLU_INSTALL_DIR; make install lib; mv *.a '+os.path.join(installDir,self.libdir)+'; mkdir '+os.path.join(installDir,self.includedir)+'; cp SRC/*.h '+os.path.join(installDir,self.includedir)+'/.', timeout=2500, log = self.framework.log)[0]
       except RuntimeError, e:
         raise RuntimeError('Error running make on SUPERLU: '+str(e))
-      if not os.path.isdir(os.path.join(installDir,self.libdir)):
+    else:
+      self.framework.log.write('Do NOT need to compile SuperLU downloaded libraries\n')  
+    if not os.path.isfile(os.path.join(installDir,self.libdir,'superlu.a')):
         self.framework.log.write('Error running make on SUPERLU   ******(libraries not installed)*******\n')
         self.framework.log.write('********Output of running make on SUPERLU follows *******\n')        
         self.framework.log.write(output)
         self.framework.log.write('********End of Output of running make on SUPERLU *******\n')
         raise RuntimeError('Error running make on SUPERLU, libraries not installed')
       
-      fd = file(os.path.join(installDir,'config.args'), 'w')
-      fd.write(args)
-      fd.close()
-
-      self.framework.actions.addArgument(self.PACKAGE, 'Install', 'Installed SUPERLU into '+installDir)
+    output  = config.base.Configure.executeShellCommand('cp -f '+os.path.join(superluDir,'make.inc')+' '+installDir, timeout=5, log = self.framework.log)[0]
+    self.framework.actions.addArgument(self.PACKAGE, 'Install', 'Installed SUPERLU into '+installDir)
     return self.getDir()
   
 if __name__ == '__main__':
