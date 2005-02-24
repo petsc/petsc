b8f86f4db388aab8c8905a3972426c9139762d02
Satish Balay <balay@mcs.anl.gov>
2005-02-23 20:02:45 -0600

    bk-changeset-1.2930.1.7 balay@asterix.mcs.anl.gov|ChangeSet|20050224020245|63877 ChangeSet   1.2930.1.7 05/02/23 20:02:45 balay@asterix.mcs.anl.gov +2 -0   priliminary f90 interface for g95 [It doesn't work yet]
      src/sys/src/f90/f90_g95.c
        1.1 05/02/23 20:02:43 balay@asterix.mcs.anl.gov +47 -0
    
      include/f90impl/f90_g95.h
        1.1 05/02/23 20:02:43 balay@asterix.mcs.anl.gov +42 -0
    
      src/sys/src/f90/f90_g95.c
        1.0 05/02/23 20:02:43 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/src/sys/src/f90/f90_g95.c
    
      include/f90impl/f90_g95.h
        1.0 05/02/23 20:02:43 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/include/f90impl/f90_g95.h
    
    
    Hg-commit: 192d5f083046066f241b8499cab4c740e011997a

---
+++ include/f90impl/f90_g95.h
@@ -0,0 +1,42 @@
+/* $Id: f90_g95.h,v 1.3 2000/09/22 18:54:10 balay Exp $ */
+
+#if !defined (__F90_G95_H)
+#define __F90_G95_H
+
+ 
+typedef struct {
+  long mult;    /* stride in no of datatype units */
+  long lower;   /* starting index of the fortran array */
+  long upper;  /*  ending index of the array */
+} tripple;
+
+
+
+#define f90_header() \
+void*   addr_d;      /* addr -sumof(lower*mult) */ \
+long    ndim;        /* number of dimentions */\
+long    sd;          /* sizeof datatype */ \
+void*   addr;        /* Pointer to the data */
+
+typedef struct {
+  f90_header()
+  tripple dim[1];     
+}F90Array1d;
+
+typedef struct {
+  f90_header()
+  tripple dim[2];     
+}F90Array2d;
+
+typedef struct {
+  f90_header()
+  tripple dim[3];     
+}F90Array3d;
+
+typedef struct {
+  f90_header()
+  tripple dim[4];     
+}F90Array4d;
+
+
+#endif
