2e55d91b8e372754f951dd645b1bd59beddc5e27
Satish Balay <balay@mcs.anl.gov>
2005-03-07 15:42:33 -0600

    bk-changeset-1.2957.1.1 balay@asterix.mcs.anl.gov|ChangeSet|20050307214233|08820 ChangeSet   1.2957.1.1 05/03/07 15:42:33 balay@asterix.mcs.anl.gov +2 -0   start a nightly build on opteron-ibm [amd64]
      maint/confignightly/opteron-ibm
        1.1 05/03/07 15:42:32 balay@asterix.mcs.anl.gov +7 -0
    
      maint/confignightly/opteron-ibm
        1.0 05/03/07 15:42:32 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/maint/confignightly/opteron-ibm
    
      config/linux-gnu-amd64.py
        1.1 05/03/07 15:42:31 balay@asterix.mcs.anl.gov +15 -0
    
      config/linux-gnu-amd64.py
        1.0 05/03/07 15:42:31 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/config/linux-gnu-amd64.py
    
    
    Hg-commit: 04d8b0bd3f1f5f5d4af186cefa526dd295b3d223

---
+++ config/linux-gnu-amd64.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+configure_options = [
+  '--download-mpich=1',
+  '--download-f-blas-lapack=1'
+  ]
+
+if __name__ == '__main__':
+  import configure
+  configure.petsc_configure(configure_options)
+
+# Extra options used for testing locally
+test_options = [
+  '--with-debugging=0',
+  '--with-lang=c++']
