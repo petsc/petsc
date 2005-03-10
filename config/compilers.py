82b8e18ea06991201d9f913e8d99d5ca898e4f1e
Barry Smith <bsmith@mcs.anl.gov>
2005-03-10 12:55:48 -0600

    bk-changeset-1.1063.1.9 barrysmith@barrysmith-57.mcs.anl.gov|ChangeSet|20050310185548|05741 ChangeSet   1.1063.1.9 05/03/10 12:55:48 barrysmith@barrysmith-57.mcs.anl.gov +1 -0   handle -z option that solaris loader uses
      config/compilers.py
        1.116.1.3 05/03/10 12:55:47 barrysmith@barrysmith-57.mcs.anl.gov +4 -0
        handle -z option that solaris loader uses
    
    
    Hg-commit: 754c86322f518c76b474d113f86ad665c827b8f2

--- config/compilers.py
+++ config/compilers.py
@@ -323,6 +323,10 @@ class Configure(config.base.Configure):
           self.framework.log.write( 'Found -rpath library: '+lib+'\n')
           flibs.append(self.setCompilers.CSharedLinkerFlag+lib)
           continue
+        if arg.startswith('-z'):
+          self.framework.log.write( 'Found Solaris -z option: '+arg+'\n')
+          flibs.append(arg)
+          continue
         # Check for ???
         # Should probably try to ensure unique directory options here too.
         # This probably only applies to Solaris systems, and then will only
