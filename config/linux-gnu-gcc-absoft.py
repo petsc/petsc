3a941cc287dde1e7edaf8cdbca025167d99c1542
Satish Balay <balay@mcs.anl.gov>
2005-03-25 17:23:20 -0600

    bk-changeset-1.3025 balay@asterix.mcs.anl.gov|ChangeSet|20050325232320|27507 ChangeSet   1.3025 05/03/25 17:23:20 balay@asterix.mcs.anl.gov +1 -0   update absoft nightly build
      config/linux-gnu-gcc-absoft.py
        1.6 05/03/25 17:23:18 balay@asterix.mcs.anl.gov +2 -3
        update absoft nightly build
    
    
    Hg-commit: 98030ebd3f992207dd70760cd7368f5d408031c7

--- config/linux-gnu-gcc-absoft.py
+++ config/linux-gnu-gcc-absoft.py
@@ -5,9 +5,8 @@ configure_options = [
   '--with-fc=f90',
   '--with-cxx=g++',
   '--with-language=c++',
-  '--with-blas-lib=/home/petsc/software/LAPACK/libblas_linux_absoft.a',
-  '--with-lapack-lib=/home/petsc/software/LAPACK/liblapack_linux_absoft.a',
-  '--with-mpi-dir=/home/petsc/software/mpich-1.2.0/linux_absoft',
+  '--download-f-blas-lapack=1',
+  '--download-mpich=1',
   '--with-matlab=0'
   ]
 
