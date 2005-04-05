3bbe0c384a7beb84a42cc1dc0d35fff45d651e45
Dmitry Karpeev <karpeev@mcs.anl.gov>
2005-04-05 15:54:14 -0600

    bk-changeset-1.1251.1.1 karpeev@lenz.(none)|ChangeSet|20050405215414|37271 ChangeSet   1.1251.1.1 05/04/05 16:54:14 karpeev@lenz.(none) +3 -0   Added 'includeDirectories' to config.compile.FC
      BitKeeper/etc/logging_ok
        1.34.1.1 05/04/05 16:52:18 karpeev@lenz.(none) +1 -0
        Logging to logging@openlogging.org accepted
    
      config/compile/FC.py
        1.16.1.1 05/04/05 16:52:16 karpeev@lenz.(none) +1 -0
        Added the 'includeDirectories' attribute.
    
      docs/manual-old.tex
        1.5 05/03/14 20:54:32 karpeev@lenz.(none) +0 -0
        Rename: docs/manual.tex -> docs/manual-old.tex
    
    
    Hg-commit: 26592eb7fc7f0fb6e53be2ce26d496e2adc95bc9

--- config/compile/FC.py
+++ config/compile/FC.py
@@ -22,6 +22,7 @@ class Compiler(config.compile.processor.Processor):
     config.compile.processor.Processor.__init__(self, argDB, 'FC', 'FFLAGS', '.F', '.o')
     self.requiredFlags[-1] = '-c'
     self.outputFlag        = '-o'
+    self.includeDirectories = sets.Set()
     self.flagsName.extend(Preprocessor(argDB).flagsName)
     return
 
