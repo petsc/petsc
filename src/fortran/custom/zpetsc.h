faa578e1df1dd4e67fab787ffedd3b44b8511472
Barry Smith <bsmith@mcs.anl.gov>
2005-04-06 21:23:37 -0600

    bk-changeset-1.3069 barrysmith@barry-smiths-computer.local|ChangeSet|20050407032337|20429 ChangeSet   1.3069 05/04/06 22:23:37 barrysmith@barry-smiths-computer.local +1 -0   zpetsc.h:     get rid of     zvec.c: In function `vecscattercreate_':     zvec.c:232: warning: use of cast expressions as lvalues is deprecated     zvec.c:233: warning: use of cast expressions as lvalues is deprecated     zis.c: In function `islocaltoglobalmappingview_':     zis.c:71: warning: use of cast expressions as lvalues is deprecated
      src/fortran/custom/zpetsc.h
        1.27 05/04/06 22:23:14 barrysmith@barry-smiths-computer.local +1 -1
        get rid of 
        zvec.c: In function `vecscattercreate_':
        zvec.c:232: warning: use of cast expressions as lvalues is deprecated
        zvec.c:233: warning: use of cast expressions as lvalues is deprecated
        zis.c: In function `islocaltoglobalmappingview_':
        zis.c:71: warning: use of cast expressions as lvalues is deprecated
    
    
    Hg-commit: 8d387f18838f09c659fd814ebe6c82c344563624

--- src/fortran/custom/zpetsc.h
+++ src/fortran/custom/zpetsc.h
@@ -152,7 +152,7 @@ extern void *PETSCNULLPOINTERADDRESS;
   if (FORTRANNULL(a) || FORTRANNULLSCALAR(a) || FORTRANNULLDOUBLE(a) || FORTRANNULLREAL(a) || FORTRANNULLINTEGER(a)) { \
     PetscError(__LINE__,"fortran_interface_unknown_file",__FILE__,__SDIR__,PETSC_ERR_ARG_WRONG,1, \
     "Use PETSC_NULL_OBJECT"); *ierr = 1; return; } \
-  else if (FORTRANNULLOBJECT(a)) { ((void**)a) = &PETSCNULLPOINTERADDRESS; }
+  else if (FORTRANNULLOBJECT(a)) { *((void***)&a) = &PETSCNULLPOINTERADDRESS; }
   
 /*
     These are used to support the default viewers that are 
