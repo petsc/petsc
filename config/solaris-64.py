935b00a9742d21bf02e90fe3c7e9fb18ad6082f0
Barry Smith <bsmith@mcs.anl.gov>
2005-04-10 20:16:27 -0600

    bk-changeset-1.3093 barrysmith@barry-smiths-computer.local|ChangeSet|20050411021627|53727 ChangeSet   1.3093 05/04/10 21:16:27 barrysmith@barry-smiths-computer.local +1 -0   solaris-64.py:     how come this one has quotes around the f90_solaris stuff and no other one does?
      config/solaris-64.py
        1.18 05/04/10 21:15:57 barrysmith@barry-smiths-computer.local +2 -2
        how come this one has quotes around the f90_solaris stuff and no other one does?
    
    
    Hg-commit: 8326185b761caffe5e1b16440a164cc42ee576f8

--- config/solaris-64.py
+++ config/solaris-64.py
@@ -4,8 +4,8 @@ configure_options = [
   '--with-64-bit-pointers',
   '--with-mpi-compilers=0',
   '--with-gnu-compilers=0',
-  '--with-f90-header="f90_solaris.h"',
-  '--with-f90-source="f90_solaris.c"',
+  '--with-f90-header=f90_solaris.h',
+  '--with-f90-source=f90_solaris.c',
   '--with-mpi-dir=/home/petsc/soft/solaris-9-64/mpich-1.2.5/'
   ]
 
