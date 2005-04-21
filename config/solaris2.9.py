c36d354fd611e0234bb9be5298674246e818af76
Satish Balay <balay@mcs.anl.gov>
2005-04-20 19:42:14 -0600

    bk-changeset-1.3158 balay@asterix.mcs.anl.gov|ChangeSet|20050421014214|23564 ChangeSet   1.3158 05/04/20 20:42:14 balay@asterix.mcs.anl.gov +2 -0   make configure happy
      src/sys/examples/tutorials/binaryoutput.info
        1.1 05/04/20 20:42:12 balay@asterix.mcs.anl.gov +0 -0
    
      src/sys/examples/tutorials/binaryoutput.info
        1.0 05/04/20 20:42:12 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/src/sys/examples/tutorials/binaryoutput.info
    
      config/solaris2.9.py
        1.14 05/04/20 20:42:12 balay@asterix.mcs.anl.gov +0 -1
        make configure happy
    
    
    Hg-commit: bb961d4caff4f0315ae45ca20cafdbbf2970c712

--- config/solaris2.9.py
+++ config/solaris2.9.py
@@ -3,7 +3,6 @@
 configure_options = [
   '--with-cc=cc',
   '--with-fc=f90',
-  '--with-cxx=CC',
   '--with-f90-header=f90_solaris.h',
   '--with-f90-source=f90_solaris.c',
   '--with-mpi-dir=/home/petsc/soft/solaris-9/mpich-1.2.5/'
