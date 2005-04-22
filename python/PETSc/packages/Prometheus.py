2a18b76b2eabc23ebe15cb77005e6b032199dc33
Matthew Knepley <knepley@mcs.anl.gov>
2005-04-21 18:30:22 -0600

    bk-changeset-1.3160 knepley@khan.(none)|ChangeSet|20050422003022|34935 ChangeSet   1.3160 05/04/21 19:30:22 knepley@khan.(none) +1 -0   Configure fix
      python/PETSc/packages/Prometheus.py
        1.17.1.2 05/04/21 19:30:17 knepley@khan.(none) +1 -1
        Fixed merge
    
    
    Hg-commit: db7f16b19f221c9f7604b6a5a35843975983103b

--- python/PETSc/packages/Prometheus.py
+++ python/PETSc/packages/Prometheus.py
@@ -8,7 +8,7 @@ import PETSc.package
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
     PETSc.package.Package.__init__(self, framework)
-v/pub/petsc/externalpackages/Prometheus-1.8.1-Petsc-2.3.tar.gz']
+    self.download          = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/Prometheus-1.8.1-Petsc-2.3.tar.gz']
     self.functions         = []
     self.includes          = []
     self.liblist           = [['libpromfei.a','libprometheus.a']]
