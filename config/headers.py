5a85ba46a2cf04bf52ba5912b3393166a7001f7a
Matthew Knepley <knepley@mcs.anl.gov>
2005-04-21 18:30:36 -0600

    bk-changeset-1.1294 knepley@khan.(none)|ChangeSet|20050422003036|29862 ChangeSet   1.1294 05/04/21 19:30:36 knepley@khan.(none) +1 -0   Configure fix
      config/headers.py
        1.12 05/04/21 19:30:35 knepley@khan.(none) +3 -3
        Fixed operation
    
    
    Hg-commit: a07db215817a3521d2bf9057eae143925c8de564

--- config/headers.py
+++ config/headers.py
@@ -43,10 +43,10 @@ class Configure(config.base.Configure):
     if not isinstance(hfiles, list):
       hfiles = [hfiles]
     for hfile in hfiles:
-      oldFlags = self.framework.argDB['CPPFLAGS']
-      self.framework.argDB['CPPFLAGS'] += ' '+' '.join([self.getIncludeArgument(inc) for inc in incl+otherIncludes])
+      oldFlags = self.compilers.CPPFLAGS
+      self.compilers.CPPFLAGS += ' '+' '.join([self.getIncludeArgument(inc) for inc in incl+otherIncludes])
       found = self.checkPreprocess('#include <' +hfile+ '>\n')
-      self.framework.argDB['CPPFLAGS'] = oldFlags
+      self.compilers.CPPFLAGS = oldFlags
       if not found: return 0
     self.framework.log.write('Found header files ' +str(hfiles)+ ' in '+str(incl)+'\n')
     return 1
