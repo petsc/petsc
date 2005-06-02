e40ea289f636113cd093f4c09aa6bc2a8c222717
Satish Balay <balay@mcs.anl.gov>
2005-06-02 09:28:46 -0600

    bk-changeset-1.3341 balay@asterix.mcs.anl.gov|ChangeSet|20050602152846|20022 ChangeSet   1.3341 05/06/02 10:28:46 balay@asterix.mcs.anl.gov +1 -0   eliminate uneeded file
      BitKeeper/deleted/.del-test.sh~9f90d9c197b0d4a6
        1.2 05/06/02 10:28:22 balay@asterix.mcs.anl.gov +3 -2
        Delete: test.sh
    
    
    Hg-commit: ca7c17b3bc33fa985b6c7baf211ab3bb78543eae

--- test.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-export PETSC_DIR=/home/balay/petsc-dl
-export PETSC_ARCH=asterix-cxx
-./config/asterix.py --with-clanguage=cxx -PETSC_ARCH=asterix-cxx
-make PETSC_ARCH=asterix allfortranstubs
-make
-make testexamples testfortran
-make ACTION=testexamples_Fortran_NoComplex tree DATAFILESPATH=/home/balay/datafiles
-
