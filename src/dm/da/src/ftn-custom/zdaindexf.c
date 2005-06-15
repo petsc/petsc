54ccb7cc64a46d89d5af64ea5253a7d4d71e81af
Satish Balay <balay@mcs.anl.gov>
2005-06-15 17:52:38 -0600

    bk-changeset-1.3349.14.1 balay@asterix.mcs.anl.gov|ChangeSet|20050615235238|19531 ChangeSet   1.3349.14.1 05/06/15 18:52:38 balay@asterix.mcs.anl.gov +13 -0   splitup zda.c [ftn-custom]
      src/dm/da/utils/ftn-custom/zfddaf.c
        1.1 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +20 -0
    
      src/dm/da/utils/ftn-custom/makefile
        1.1 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +14 -0
    
      src/dm/da/src/ftn-custom/zdaviewf.c
        1.1 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +37 -0
    
      src/dm/da/utils/ftn-custom/zfddaf.c
        1.0 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/utils/ftn-custom/zfddaf.c
    
      src/dm/da/utils/ftn-custom/makefile
        1.0 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/utils/ftn-custom/makefile
    
      src/dm/da/src/ftn-custom/zdaviewf.c
        1.0 05/06/15 18:52:37 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdaviewf.c
    
      src/dm/da/src/ftn-custom/zdaloadf.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +18 -0
    
      src/dm/da/src/ftn-custom/zdainterpf.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +19 -0
    
      src/dm/da/src/ftn-custom/zdaindexf.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +19 -0
    
      src/dm/da/src/ftn-custom/zdagetscatterf.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +20 -0
    
      src/dm/da/src/ftn-custom/zdacornf.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +37 -0
    
      src/dm/da/src/ftn-custom/zda3f.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +24 -0
    
      src/dm/da/src/ftn-custom/zda2f.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +114 -0
    
      src/dm/da/src/ftn-custom/zda1f.c
        1.1 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +21 -0
    
      src/dm/da/src/ftn-custom/zdaloadf.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdaloadf.c
    
      src/dm/da/src/ftn-custom/zdainterpf.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdainterpf.c
    
      src/dm/da/src/ftn-custom/zdaindexf.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdaindexf.c
    
      src/dm/da/src/ftn-custom/zdagetscatterf.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdagetscatterf.c
    
      src/dm/da/src/ftn-custom/zdacornf.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zdacornf.c
    
      src/dm/da/src/ftn-custom/zda3f.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zda3f.c
    
      src/dm/da/src/ftn-custom/zda2f.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zda2f.c
    
      src/dm/da/src/ftn-custom/zda1f.c
        1.0 05/06/15 18:52:36 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/zda1f.c
    
      src/dm/da/src/ftn-custom/makefile
        1.1 05/06/15 18:52:35 balay@asterix.mcs.anl.gov +14 -0
    
      src/dm/ftn-custom/zda.c
        1.24 05/06/15 18:52:35 balay@asterix.mcs.anl.gov +0 -221
        splitup zda.c [ftn-custom]
    
      src/dm/da/src/ftn-custom/makefile
        1.0 05/06/15 18:52:35 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/petsc-dl/src/dm/da/src/ftn-custom/makefile
    
    
    Hg-commit: cde47292574703980b38d06d1c4f8e002efce07f

---
+++ src/dm/da/src/ftn-custom/zdaindexf.c
@@ -0,0 +1,19 @@
+
+#include "zpetsc.h"
+#include "petscda.h"
+
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define dagetglobalindices_          DAGETGLOBALINDICES
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define dagetglobalindices_          dagetglobalindices
+#endif
+
+EXTERN_C_BEGIN
+void PETSC_STDCALL dagetglobalindices_(DA *da,PetscInt *n,PetscInt *indices,size_t *ia,PetscErrorCode *ierr)
+{
+  PetscInt *idx;
+  *ierr = DAGetGlobalIndices(*da,n,&idx);
+  *ia   = PetscIntAddressToFortran(indices,idx);
+}
+
+EXTERN_C_END
