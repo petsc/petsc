8881484b78d15983ca46b34c00d5077b2ba3bb31
Satish Balay <balay@mcs.anl.gov>
2005-06-16 14:06:22 -0600

    bk-changeset-1.3349.18.1 balay@asterix.mcs.anl.gov|ChangeSet|20050616200622|58125 ChangeSet   1.3349.18.1 05/06/16 15:06:22 balay@asterix.mcs.anl.gov +3 -0   splitup zdmmg.c [ftn-custom]
      src/snes/utils/ftn-custom/zdamgf.c
        1.1 05/06/16 15:06:20 balay@asterix.mcs.anl.gov +120 -0
    
      src/snes/utils/ftn-custom/zdamgf.c
        1.0 05/06/16 15:06:20 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/src/snes/utils/ftn-custom/zdamgf.c
    
      src/snes/utils/ftn-custom/makefile
        1.2 05/06/16 15:06:20 balay@asterix.mcs.anl.gov +1 -1
        splitup zdmmg.c
    
      src/snes/ftn-custom/zdmmg.c
        1.23 05/06/16 15:06:20 balay@asterix.mcs.anl.gov +0 -111
        splitup zdmmg.c
    
    
    Hg-commit: 668a3b0a53687bbc86afd6b17f235e6dc5a2dd59

--- src/snes/ftn-custom/zdmmg.c
+++ src/snes/ftn-custom/zdmmg.c
@@ -1,25 +1,15 @@
 
 #include "zpetsc.h"
-#include "petscksp.h"
-#include "petscda.h"
 #include "petscdmmg.h"
 
 #ifdef PETSC_HAVE_FORTRAN_CAPS
-#define dmmgcreate_              DMMGCREATE
-#define dmmgdestroy_             DMMGDESTROY
-#define dmmgsetup_               DMMGSETUP
-#define dmmgsetdm_               DMMGSETDM
-#define dmmgview_                DMMGVIEW
-#define dmmgsolve_               DMMGSOLVE
 #define dmmggetda_               DMMGGETDA
-#define dmmgsetksp_              DMMGSETKSP
 #define dmmggetx_                DMMGGETX
 #define dmmggetj_                DMMGGETJ
 #define dmmggetb_                DMMGGETB
 #define dmmggetrhs_              DMMGGETRHS
 #define dmmggetksp_              DMMGGETKSP
 #define dmmggetlevels_           DMMGGETLEVELS
-#define dmmgsetinitialguess_     DMMGSETINITIALGUESS
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define dmmggetrhs_              dmmggetrhs
 #define dmmggetx_                dmmggetx
@@ -28,44 +18,7 @@
 #define dmmggetksp_              dmmggetksp
 #define dmmggetda_               dmmggetda
 #define dmmggetlevels_           dmmggetlevels
-#define dmmgsetksp_              dmmgsetksp
-#define dmmgdestroy_             dmmgdestroy
-#define dmmgcreate_              dmmgcreate
-#define dmmgsetup_               dmmgsetup
-#define dmmgsetdm_               dmmgsetdm
-#define dmmgview_                dmmgview
-#define dmmgsolve_               dmmgsolve
-#define dmmgsetinitialguess_     dmmgsetinitialguess
 #endif
-
-EXTERN_C_BEGIN
-static void (PETSC_STDCALL *theirmat)(DMMG*,Mat*,PetscErrorCode*);
-EXTERN_C_END
-
-static PetscErrorCode ourrhs(DMMG dmmg,Vec vec)
-{
-  PetscErrorCode ierr = 0;
-  (*(void (PETSC_STDCALL *)(DMMG*,Vec*,PetscErrorCode*))(((PetscObject)dmmg->dm)->fortran_func_pointers[0]))(&dmmg,&vec,&ierr);
-  return ierr;
-}
-
-static PetscErrorCode ourinitialguess(DMMG dmmg,Vec vec)
-{
-  PetscErrorCode ierr = 0;
-  (*(void (PETSC_STDCALL *)(DMMG*,Vec*,PetscErrorCode*))(((PetscObject)dmmg->dm)->fortran_func_pointers[1]))(&dmmg,&vec,&ierr);
-  return ierr;
-}
-
-/*
-   Since DMMGSetKSP() immediately calls the matrix functions for each level we do not need to store
-  the mat() function inside the DMMG object
-*/
-static PetscErrorCode ourmat(DMMG dmmg,Mat mat)
-{
-  PetscErrorCode ierr = 0;
-  (*theirmat)(&dmmg,&mat,&ierr);
-  return ierr;
-}
 
 EXTERN_C_BEGIN
 
@@ -106,75 +59,11 @@ void PETSC_STDCALL dmmggetlevels_(DMMG **dmmg,PetscInt *x,PetscErrorCode *ierr)
 }
 
 /* ----------------------------------------------------------------------------------------------------------*/
-void PETSC_STDCALL dmmgsetinitialguess_(DMMG **dmmg,void (PETSC_STDCALL *initialguess)(DMMG*,Vec*,PetscErrorCode*),PetscErrorCode *ierr)
-{
-  PetscInt i;
-
-  *ierr = DMMGSetInitialGuess(*dmmg,ourinitialguess);
-  /*
-    Save the fortran initial guess function in the DM on each level; ourinitialguess() pulls it out when needed
-  */
-  for (i=0; i<(**dmmg)->nlevels; i++) {
-    ((PetscObject)(*dmmg)[i]->dm)->fortran_func_pointers[1] = (FCNVOID)initialguess;
-  }
-}
-
-void PETSC_STDCALL dmmgsetksp_(DMMG **dmmg,void (PETSC_STDCALL *rhs)(DMMG*,Vec*,PetscErrorCode*),void (PETSC_STDCALL *mat)(DMMG*,Mat*,PetscErrorCode*),PetscErrorCode *ierr)
-{
-  PetscInt i;
-  theirmat = mat;
-  *ierr = DMMGSetKSP(*dmmg,ourrhs,ourmat);
-  /*
-    Save the fortran rhs function in the DM on each level; ourrhs() pulls it out when needed
-  */
-  for (i=0; i<(**dmmg)->nlevels; i++) {
-    ((PetscObject)(*dmmg)[i]->dm)->fortran_func_pointers[0] = (FCNVOID)rhs;
-  }
-}
-
-/* ----------------------------------------------------------------------------------------------------------*/
 
 void PETSC_STDCALL dmmggetda_(DMMG *dmmg,DA *da,PetscErrorCode *ierr)
 {
   *da   = (DA)(*dmmg)->dm;
   *ierr = 0;
-}
-
-void PETSC_STDCALL dmmgsetdm_(DMMG **dmmg,DM *dm,PetscErrorCode *ierr)
-{
-  PetscInt i;
-  *ierr = DMMGSetDM(*dmmg,*dm);if (*ierr) return;
-  /* loop over the levels added a place to hang the function pointers in the DM for each level*/
-  for (i=0; i<(**dmmg)->nlevels; i++) {
-    *ierr = PetscMalloc(4*sizeof(FCNVOID),&((PetscObject)(*dmmg)[i]->dm)->fortran_func_pointers);if (*ierr) return;
-  }
-}
-
-void PETSC_STDCALL dmmgview_(DMMG **dmmg,PetscViewer *viewer,PetscErrorCode *ierr)
-{
-  PetscViewer v;
-  PetscPatchDefaultViewers_Fortran(viewer,v);
-  *ierr = DMMGView(*dmmg,v);
-}
-
-void PETSC_STDCALL dmmgsolve_(DMMG **dmmg,PetscErrorCode *ierr)
-{
-  *ierr = DMMGSolve(*dmmg);
-}
-
-void PETSC_STDCALL dmmgcreate_(MPI_Comm *comm,PetscInt *nlevels,void *user,DMMG **dmmg,PetscErrorCode *ierr)
-{
-  *ierr = DMMGCreate((MPI_Comm)PetscToPointerComm(*comm),*nlevels,user,dmmg);
-}
-
-void PETSC_STDCALL dmmgdestroy_(DMMG **dmmg,PetscErrorCode *ierr)
-{
-  *ierr = DMMGDestroy(*dmmg);
-}
-
-void PETSC_STDCALL dmmgsetup_(DMMG **dmmg,PetscErrorCode *ierr)
-{
-  *ierr = DMMGSetUp(*dmmg);
 }
 
 EXTERN_C_END
