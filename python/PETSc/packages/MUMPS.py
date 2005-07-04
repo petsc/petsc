27b6b2761f57c2145c7085cee245f56e387ee5f6
Barry Smith <bsmith@mcs.anl.gov>
2005-07-04 16:37:48 -0600

    bk-changeset-1.3215.1.37 barrysmith@barry-smiths-computer.local|ChangeSet|20050704223748|32823 ChangeSet   1.3215.1.37 05/07/04 17:37:48 barrysmith@barry-smiths-computer.local +1 -0   petsc-maint 13281 typo in compiler(s)
      python/PETSc/packages/MUMPS.py
        1.42.2.1 05/07/04 15:48:47 barrysmith@barry-smiths-computer.local +1 -1
        petsc-maint 13281 typo in compiler(s)
    
    
    Hg-commit: 2423a042f939e7ed1af66236caf956de793c2664

--- python/PETSc/packages/MUMPS.py
+++ python/PETSc/packages/MUMPS.py
@@ -44,7 +44,7 @@ class Configure(PETSc.package.Package):
     g.write('CC = '+self.setCompilers.getCompiler()+'\n')
     g.write('OPTC    = ' + self.setCompilers.getCompilerFlags() +'\n')
     self.setCompilers.popLanguage()
-    if not self.compiler.fortranIsF90:
+    if not self.compilers.fortranIsF90:
       raise RuntimeError('Invalid F90 compiler') 
     self.setCompilers.pushLanguage('FC') 
     g.write('FC = '+self.setCompilers.getCompiler()+'\n')
