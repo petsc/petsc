378f148e5cb898dd091a14166ec41c1660c4bb46
Barry Smith <bsmith@mcs.anl.gov>
2005-07-12 09:41:39 -0600

    bk-changeset-1.3383 barrysmith@barrysmith-11.mcs.anl.gov|ChangeSet|20050712154139|28429 ChangeSet   1.3383 05/07/12 10:41:39 barrysmith@barrysmith-11.mcs.anl.gov +1 -0   configure.py:     also make sure first digit in version is at least 2.
      config/configure.py
        1.101 05/07/12 10:39:51 barrysmith@barry-smiths-computer.local +1 -1
        also make sure first digit in version is at least 2.
    
    
    Hg-commit: 7551720466f803d3345cef8ac255851170e524f2

--- config/configure.py
+++ config/configure.py
@@ -4,7 +4,7 @@ import sys
 import commands
 
 
-if not hasattr(sys, 'version_info') or not sys.version_info[1] >= 2:
+if not hasattr(sys, 'version_info') or not sys.version_info[1] >= 2 or not sys.version_info[0] >= 2:
   print '**** You must have Python version 2.2 or higher to run config/configure.py ******'
   print '*           Python is easy to install for end users or sys-admin.               *'
   print '*                   http://www.python.org/download/                             *'
