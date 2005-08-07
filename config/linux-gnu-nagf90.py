1a72f17aef1c2e354af278b5104abd3b7a228385
Satish Balay <balay@mcs.anl.gov>
2005-08-07 11:46:29 -0600

    bk-changeset-1.3447.1.2 balay@asterix.mcs.anl.gov|ChangeSet|20050807174629|11056 ChangeSet   1.3447.1.2 05/08/07 12:46:29 balay@asterix.mcs.anl.gov +2 -0   add nag f90 test
      maint/confignightly/smash2
        1.1 05/08/07 12:46:27 balay@asterix.mcs.anl.gov +7 -0
        New BitKeeper file ``maint/confignightly/smash2''
    
      config/linux-gnu-nagf90.py
        1.1 05/08/07 12:46:27 balay@asterix.mcs.anl.gov +16 -0
        New BitKeeper file ``config/linux-gnu-nagf90.py''
    
      maint/confignightly/smash2
        1.0 05/08/07 12:46:27 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/maint/confignightly/smash2
    
      config/linux-gnu-nagf90.py
        1.0 05/08/07 12:46:27 balay@asterix.mcs.anl.gov +0 -0
        BitKeeper file /home/balay/bk-rep/petsc-dev/config/linux-gnu-nagf90.py
    
    
    Hg-commit: ef054abbe6bc1d8b9575f73492b36947ad7dc217

---
+++ config/linux-gnu-nagf90.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+configure_options = [
+  '--with-blas-lapack-lib=[/home/petsc/soft/linux-debian_sarge-nagf90/fblaslapack/libflapack.a,libfblas.a,libg2c.a]',
+  '--with-mpi-dir=/home/petsc/soft/linux-debian_sarge-nagf90/mpich2-1.0.2p1',
+  '--with-shared=0',
+  '--with-scalar-type=complex',
+  '--download-superlu_dist=1'
+  ]
+
+if __name__ == '__main__':
+  import configure
+  configure.petsc_configure(configure_options)
+
+# Extra options used for testing locally
+test_options = []
