0d797e2f0a321424e48be8d0d5bde0248f4532ac
Satish Balay <balay@mcs.anl.gov>
2005-09-28 23:02:19 -0600

    bk-changeset-1.3552.12.1 balay@asterix.mcs.anl.gov|ChangeSet|20050929050219|59808 ChangeSet   1.3552.12.1 05/09/29 00:02:19 balay@asterix.mcs.anl.gov +1 -0   use bk unique keys instead of revision-numbers [which are not unique]
      maint/bk2darcs.py
        1.3 05/09/29 00:02:17 balay@asterix.mcs.anl.gov +39 -29
        use bk unique keys instead of revision-numbers [which are not unique]
    
      maint/bk2darcs.py
        1.2 05/09/28 16:40:01 balay@asterix.mcs.anl.gov +30 -9
        add -i option for create/initialize the darcs repo
    
    
    Hg-commit: d68cb0d0fa4cf35db35807d808bd68ef054d3a57

--- maint/bk2darcs.py
+++ maint/bk2darcs.py
@@ -2,6 +2,9 @@
 #
 # Usage bk2darcs bk-repo darcs-repo
 #
+#  options:
+#    -i : create/initialize a new darcs repo
+#
 # bk-repo is a valid bk repository
 # darcs-repo is a new location
 
@@ -9,26 +12,44 @@ import sys
 import os
 
 def main():
+  createdarcsrepo=0
+  if '-i' in sys.argv:
+    sys.argv.remove('-i')
+    createdarcsrepo=1
+
   arg_len = len(sys.argv)
   if arg_len != 3:
     print 'Error Insufficient arguments.'
-    print 'Usage:', sys.argv[0], 'bk-repo darcs-repo'
+    print 'Usage:', sys.argv[0], '[-i] bk-repo darcs-repo'
     sys.exit()
   bk_repo = sys.argv[1]
   darcs_repo= sys.argv[2]
 
-  # verify the dirs exist 
-  if not os.path.exists(bk_repo):
-    print 'Error! specified path does not exist: ' + bk_repo
-    sys.exit()
-  if not os.path.exists(darcs_repo):
-    print 'Error! specified path does not exist: ' + darcs_repo
-    sys.exit()
-
   # get absolute paths - this way - os.chdir() works
   bk_repo = os.path.abspath(bk_repo)
   darcs_repo = os.path.abspath(darcs_repo)
-  
+
+  # verify if bkdir exists
+  if not os.path.exists(bk_repo):
+    print 'Error! specified path does not exist: ' + bk_repo
+    sys.exit()
+
+  # if createdarcsrepo - then create & initialize the repo [if dir exists]
+  # otherwise - make sure the dir exists [if not error]
+  if os.path.exists(darcs_repo):
+    if createdarcsrepo:
+      print 'Warning! ignoring option -i as specified darcsrepo exists: ' + darcs_repo
+  else:
+    if createdarcsrepo:
+      print 'Creating darcsrepo: ' + darcs_repo
+      os.mkdir(darcs_repo)
+      os.chdir(darcs_repo)
+      os.system("darcs initialize")
+    else:
+      print 'Error! specified path does not exist: ' + darcs_repo
+      print 'If you need to create a new darcs-repo, use option: -i'
+      sys.exit()
+
   # verify the specified dirs are valid repositories
   os.chdir(bk_repo)
   if os.system("bk changes -r+ > /dev/null 2>&1"):
@@ -42,13 +63,9 @@ def main():
 
   # now get the latest bk cset number
   os.chdir(bk_repo)
-  fd=os.popen('bk changes -r+')
-  buf=fd.read()
+  fd=os.popen('bk changes -k -r+')
+  bk_cset_max=fd.read().strip()
   fd.close()
-  bk_cset_max = ((buf.split(',')[0]).split('.')[1]).strip()
-  if not bk_cset_max.isdigit():
-    print 'Error! bk_cset_max not a digit!',bk_cset_max
-    sys.exit()
 
   # similarly get the latest darcs cset number
   os.chdir(darcs_repo)
@@ -56,40 +73,54 @@ def main():
   buf=fd.read()
   fd.close()
   if buf == '':
-    bk_cset_min = '0'
+    bk_cset_min = '1.0'
   else:
-    bk_cset_min = buf.split('bk-import-')[1].splitlines()[0].split('.')[1].strip()
+    bk_cset_min = buf.splitlines()[1].strip()[2:]
 
-  if not bk_cset_min.isdigit():
-    print 'Error! bk_cset_min not a digit!',bk_cset_min
+  if bk_cset_min == bk_cset_max:
+    print 'No new changesets Quitting! Last commit:', bk_cset_min
     sys.exit()
-
-  if eval(bk_cset_min) > eval(bk_cset_max):
-    print 'Error! min > max - repositories are not related?'
-    sys.exit()
-    
-  print 'Processing changesets from: ' + bk_cset_min + ' to: ' + bk_cset_max
 
   log_file=os.path.join(bk_repo,'darcs_log.tmp')
   if os.path.isfile(log_file):
     os.remove(log_file)
-  for i in range(eval(bk_cset_min)+1,eval(bk_cset_max)+1):
-    rev = '1.'+str(i)
-    print 'processing changeset-'+rev
+
+  # find the bk-changesets that need to be exported to darcs
+  os.chdir(bk_repo)
+  fd=os.popen('bk changes -k -f -r'+'"'+bk_cset_min+'".."'+bk_cset_max+'"')
+  buf=fd.read()
+  fd.close()
+  revs=buf.splitlines()
+
+
+  #now process each revision [ignore the first]
+  for rev in revs:
+    # rev  - basic string
+    # revq - rev with quotes  [useable with -r]
+    # revn - rev number [ 1.234.4 etc..]
+    # revi - just the 2nd number in rev
     os.chdir(bk_repo)
-    # get username
-    fd=os.popen('bk changes -k -r'+rev)
-    buf=fd.read()
+    revq='"'+rev+'"'
+    # get the rev-number
+    fd=os.popen('bk changes -and:I: -r'+revq)
+    revn = fd.read()
     fd.close()
-    auth_email=buf.split('|')[0]
-    # fix things like knepley@khan.(none)
+    print 'processing changeset: '+revn
+    # get revi
+    fd=os.popen('bk changes -and:L: -r'+revq)
+    revi = int(fd.read().strip())
+    fd.close()
+    # get username
+    fd=os.popen('bk changes -and:USER:@:HOST: -r'+revq)
+    auth_email=fd.read().strip()
+    fd.close()
     auth_email=auth_email.replace('.(none)','')
     
     #get comment string
-    fd=os.popen('bk changes -r'+rev+' | grep -v ^ChangeSet@')
+    fd=os.popen('bk changes -r'+revq+' | grep -v ^ChangeSet@')
     buf=fd.read()
     fd.close()
-    msg = 'bk-import-1.'+str(i) + '\n' + buf.strip() + '\n'
+    msg = rev+ '\n' + buf.strip() + '\n'
     fd=open(log_file,'w')
     fd.write(msg)
     fd.close()
@@ -102,12 +133,12 @@ def main():
 
     # Now remove the old files - and export the new modified files
     os.system('ls -a | grep -v _darcs | xargs rm -rf >/dev/null 2>&1')
-    os.system('bk export -r1.'+str(i) +' ' + bk_repo + ' ' + darcs_repo)
+    os.system('bk export -r'+revq+' ' + bk_repo + ' ' + darcs_repo)
     os.system('darcs record --test -l -a -A ' + auth_email + ' --delete-logfile --logfile='+log_file)
     # optimize/checkpoint every 250 patches
-    if  i%250 == 0 :
-      os.system('darcs tag -A snapshot@petsc snapshot-'+rev)
-      os.system('darcs optimize --checkpoint -t snapshot-'+rev)
+    if revi%250 == 0 :
+      os.system('darcs tag -A snapshot@petsc snapshot-'+revn)
+      os.system('darcs optimize --checkpoint -t snapshot-'+revn)
   return 0
 
 # The classes in this file can also
