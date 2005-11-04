58fd9ad95c2ffc313aa3e5ab76864af864d7c4bc
Matthew Knepley <knepley@mcs.anl.gov>
2005-11-03 22:53:06 -0600

    bk-changeset-1.3647.1.2 knepley@khan.(none)|ChangeSet|20051104045306|06037 ChangeSet   1.3647.1.2 05/11/03 22:53:06 knepley@khan.(none) +1 -0   Configure fix
      python/PETSc/utilities/languages.py
        1.44 05/11/03 22:53:04 knepley@khan.(none) +3 -0
        Better logging
    
    
    Hg-commit: ce81c563ee873b4560aa4b66935b7fde9f536d5e

--- python/PETSc/utilities/languages.py
+++ python/PETSc/utilities/languages.py
@@ -61,11 +61,14 @@ class Configure(config.base.Configure):
       self.clanguage = 'Cxx'
     if self.clanguage == 'C' and not self.framework.argDB['with-c++-support'] and not self.framework.argDB['download-prometheus'] and not self.framework.argDB['download-hypre']:
       self.framework.argDB['with-cxx'] = '0'
+      self.framework.logPrint('Turning off C++ support')
     if self.clanguage == 'Cxx' and self.framework.argDB['with-c-support']:
       self.cSupport = 1
       self.addDefine('USE_EXTERN_CXX', '1')
+      self.framework.logPrint('Turning off C++ name mangling')
     else:
       self.cSupport = 0
+      self.framework.logPrint('Allowing C++ name mangling')
     self.framework.logPrint('C language is '+str(self.clanguage))
     self.addDefine('CLANGUAGE_'+self.clanguage.upper(),'1')
     self.framework.require('config.setCompilers', None).mainLanguage = self.clanguage
