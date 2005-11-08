1eec871542a790b7a8840f70e0e8f6604bc51a99
Hong Zhang <hzhang@mcs.anl.gov>
2005-11-07 21:32:47 -0600

    bk-changeset-1.3678.1.1 hzhang@shakey.mcs.anl.gov|ChangeSet|20051108033247|64806 ChangeSet   1.3678.1.1 05/11/07 21:32:47 hzhang@shakey.mcs.anl.gov +8 -0   add external package blopex and contributed petsc/blopex interface
      src/contrib/blopex/makefile
        1.1 05/11/07 21:32:43 hzhang@shakey.mcs.anl.gov +7 -0
        New BitKeeper file ``src/contrib/blopex/makefile''
    
      python/PETSc/packages/blopex.py
        1.1 05/11/07 21:32:43 hzhang@shakey.mcs.anl.gov +72 -0
        New BitKeeper file ``python/PETSc/packages/blopex.py''
    
      src/contrib/makefile
        1.12 05/11/07 21:32:43 hzhang@shakey.mcs.anl.gov +1 -1
        add external package blopex and contributed petsc/blopex interface
    
      src/contrib/blopex/makefile
        1.0 05/11/07 21:32:43 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/makefile
    
      python/PETSc/packages/blopex.py
        1.0 05/11/07 21:32:43 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/python/PETSc/packages/blopex.py
    
      src/contrib/blopex/petsc-interface/makefile
        1.1 05/11/07 21:24:59 hzhang@shakey.mcs.anl.gov +16 -0
    
      src/contrib/blopex/petsc-interface/makefile
        1.0 05/11/07 21:24:59 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/petsc-interface/makefile
    
      src/contrib/blopex/driver/makefile
        1.1 05/11/07 21:24:24 hzhang@shakey.mcs.anl.gov +18 -0
    
      src/contrib/blopex/driver/makefile
        1.0 05/11/07 21:24:23 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/driver/makefile
    
      src/contrib/blopex/petsc-interface/petsc-interface.h
        1.1 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +55 -0
    
      src/contrib/blopex/petsc-interface/petsc-interface.c
        1.1 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +199 -0
    
      src/contrib/blopex/driver/driver.c
        1.1 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +382 -0
    
      src/contrib/blopex/petsc-interface/petsc-interface.h
        1.0 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/petsc-interface/petsc-interface.h
    
      src/contrib/blopex/petsc-interface/petsc-interface.c
        1.0 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/petsc-interface/petsc-interface.c
    
      src/contrib/blopex/driver/driver.c
        1.0 05/11/07 21:23:36 hzhang@shakey.mcs.anl.gov +0 -0
        BitKeeper file /sandbox/hzhang/petsc-dev/src/contrib/blopex/driver/driver.c
    
    
    Hg-commit: 98893fc3fe0e87989b27c871ff17168c650da70e

---
+++ python/PETSc/packages/blopex.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from __future__ import generators
+import user
+import config.base
+import os
+import PETSc.package
+
+class Configure(PETSc.package.Package):
+  def __init__(self, framework):
+    PETSc.package.Package.__init__(self, framework)
+    self.download   = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/blopex_abstract_Nov_2005.tar.gz']
+    self.functions  = ['lobpcg_solve']
+    self.includes   = ['interpreter.h']
+    self.liblist    = [['libBLOPEX.a']]
+    self.libdir     = 'lib'
+    self.includedir = 'include'
+    self.complex    = 0
+    return
+
+  def setupDependencies(self, framework):
+    PETSc.package.Package.setupDependencies(self, framework)
+    self.mpi        = framework.require('PETSc.packages.MPI',self)
+    self.blasLapack = framework.require('PETSc.packages.BlasLapack',self)
+    self.deps       = [self.mpi,self.blasLapack]
+    return
+
+  def Install(self):
+    # Get the BLOPEX directories
+    blopexDir = self.getDir()
+    installDir = os.path.join(blopexDir, self.arch.arch)
+    
+    # Configure and Build BLOPEX
+    if os.path.isfile(os.path.join(blopexDir,'Makefile.inc')):
+      output  = config.base.Configure.executeShellCommand('cd '+blopexDir+'; rm -f Makefile.inc', timeout=2500, log = self.framework.log)[0]
+    g = open(os.path.join(blopexDir,'Makefile.inc'),'w')
+    self.setCompilers.pushLanguage('C')
+    g.write('CC          = '+self.setCompilers.getCompiler()+'\n') 
+    g.write('CFLAGS      = ' + self.setCompilers.getCompilerFlags().replace('-Wall','').replace('-Wshadow','') +'\n')
+    self.setCompilers.popLanguage()
+    g.write('AR          = '+self.setCompilers.AR+' '+self.setCompilers.AR_FLAGS+'\n')
+    g.write('RANLIB      = '+self.setCompilers.RANLIB+'\n')
+    g.close()
+    if not os.path.isdir(installDir):
+      os.mkdir(installDir)
+    if not os.path.isfile(os.path.join(installDir,'Makefile.inc')) or not (self.getChecksum(os.path.join(installDir,'Makefile.inc')) == self.getChecksum(os.path.join(blopexDir,'Makefile.inc'))):
+      self.framework.log.write('Have to rebuild BLOPEX, Makefile.inc != '+installDir+'/Makefile.inc\n')
+      try:
+        self.logPrintBox('Compiling blopex; this may take several minutes')
+        output  = config.base.Configure.executeShellCommand('cd '+blopexDir+';BLOPEX_INSTALL_DIR='+installDir+';export BLOPEX_INSTALL_DIR; make clean; rm -f '+os.path.join(installDir,self.libdir)+'; rm -f '+os.path.join(installDir,self.includedir)+'; make; mv lib '+os.path.join(installDir,self.libdir)+'; cp -fp multivector/temp_multivector.h include/.; mv include '+os.path.join(installDir,self.includedir)+'', timeout=2500, log = self.framework.log)[0]
+      except RuntimeError, e:
+        raise RuntimeError('Error running make on BLOPEX: '+str(e))
+      else:
+        self.framework.log.write('Do NOT need to compile BLOPEX downloaded libraries\n')  
+      if not os.path.isfile(os.path.join(installDir,self.libdir,'libBLOPEX.a')):
+        self.framework.log.write('Error running make on BLOPEX   ******(libraries not installed)*******\n')
+        self.framework.log.write('********Output of running make on BLOPEX follows *******\n')        
+        self.framework.log.write(output)
+        self.framework.log.write('********End of Output of running make on BLOPEX *******\n')
+        raise RuntimeError('Error running make on BLOPEX, libraries not installed')
+
+      output  = config.base.Configure.executeShellCommand('cp -f '+os.path.join(blopexDir,'Makefile.inc')+' '+installDir, timeout=5, log = self.framework.log)[0]
+      self.framework.actions.addArgument(self.PACKAGE, 'Install', 'Installed BLOPEX into '+installDir)
+    return self.getDir()
+
+if __name__ == '__main__':
+  import config.framework
+  import sys
+  framework = config.framework.Framework(sys.argv[1:])
+  framework.setup()
+  framework.addChild(Configure(framework))
+  framework.configure()
+  framework.dumpSubstitutions()
