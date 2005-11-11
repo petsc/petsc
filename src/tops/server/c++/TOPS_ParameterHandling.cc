94c1a0f2e9ceacc9a177897b5141d74d9987ad7e
Satish Balay <balay@mcs.anl.gov>
2005-11-11 09:41:59 -0600

    bk-changeset-1.3723 balay@asterix.mcs.anl.gov|ChangeSet|20051111154159|46889 ChangeSet   1.3723 05/11/11 09:41:59 balay@asterix.mcs.anl.gov +1 -0   modify processTOPSOptions() to handle options of the following 2 types   "-ksp_monitor -snes_monitor"   -ksp_monitor -snes_monitor
      src/tops/server/c++/TOPS_ParameterHandling.cc
        1.3 05/11/11 09:41:57 balay@asterix.mcs.anl.gov +11 -5
        modify processTOPSOptions() to handle options of the following 2 types
        "-ksp_monitor -snes_monitor"
        -ksp_monitor -snes_monitor
    
    
    Hg-commit: 0ee24dab274521dcb671c31059c5c41fca9f4948

--- src/tops/server/c++/TOPS_ParameterHandling.cc
+++ src/tops/server/c++/TOPS_ParameterHandling.cc
@@ -7,16 +7,22 @@ void processTOPSOptions(std::string options) {
   std::string key = "", val = ""; 
   bool inKey = true, inVal = false, newOption = true;
   int len = options.length();
-  for (int i = 1; i < len; ++i) {
-    if (options[i] == ' ') { 
-      newOption = true; inKey = false; inVal = false; continue;
-    }
-    if (newOption && (options[i] == '-') || i == (len-1)) { 
+  int start=0, end=len;
+
+  // options can be in the form "options"
+  if (options[0] == '"' && options[len-1] =='"') {
+    start = 1; end = len-1;
+  }
+
+  for (int i = start; i <= end; ++i) {
+    if ((i == end) || (newOption && (options[i] == '-'))) { 
       //std::cout << "Setting petsc option: " << key << " " << val << std::endl;
       if (val != "") PetscOptionsSetValue(key.c_str(), val.c_str());
       else if (key != "") PetscOptionsSetValue(key.c_str(), 0);
       inKey = true; inVal = false; 
       key = "-"; val = ""; continue; 
+    } else if (options[i] == ' ') { 
+      newOption = true; inKey = false; inVal = false;
     } else {
       newOption = false;
       if (inKey) key += options[i];   
