8a8e1687eab345a2915db26c2dab3a1c55cfbcff
Boyana Norris <norris@mcs.anl.gov>
2005-11-17 19:46:34 -0600

    bk-changeset-1.3749.1.1 norris@boyana.mcs.anl.gov|ChangeSet|20051118014634|39493 ChangeSet   1.3749.1.1 05/11/17 19:46:34 norris@boyana.mcs.anl.gov +3 -0   revised the babel and ccafe configure macros; specifying   --with-babel-dir is now not necessary when --with-ccafe-dir   is given
      src/tops/examples/c++/ex3/Ex3_rc.in
        1.6 05/11/17 19:46:31 norris@boyana.mcs.anl.gov +3 -0
        added parameter settings
    
      python/PETSc/packages/ccafe.py
        1.6 05/11/17 19:46:31 norris@boyana.mcs.anl.gov +48 -5
        figure out babel settings from ccafe-config, also added
        make macros that will be used in multi-language builds
    
      python/PETSc/packages/babel.py
        1.3 05/11/17 19:46:30 norris@boyana.mcs.anl.gov +7 -7
        get the version from babel-config, not hardcoded
    
    
    Hg-commit: c920bbac444d06c5f7419180a1f0072c3a0c1ce7

--- python/PETSc/packages/ccafe.py
+++ python/PETSc/packages/ccafe.py
@@ -4,28 +4,71 @@ import user
 import config.base
 import os
 import PETSc.package
+import babel
 
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
     PETSc.package.Package.__init__(self, framework)
-    self.version  = '0.5.9'
-    #self.liblist  = [['libccaffeine_'+self.version.replace('.','_')+'.a']]
-    self.includes = ['ccafe-'+self.version+'/cmd/Cmd.h']
+    self.version       = ''
     return
 
+  def miscSetup(self):
+    ccafe_bin_dir = os.path.join(self.framework.argDB['with-ccafe-dir'], 'bin')
+    self.getExecutable('ccafe-config', path = ccafe_bin_dir, getFullPath=1, resultName = 'ccafe_config')
+    if not hasattr(self,'ccafe_config'):
+      raise RuntimeError('Cannot locate ccafe-config executable in the location specified with --with-ccafe-dir')
+    
+    try:
+      self.version  = config.base.Configure.executeShellCommand(self.ccafe_config + ' --var CCAFE_VERSION')[0].rstrip()
+    except RuntimeError, e:
+      raise RuntimeError('Error when attempting to determine ccafe version')
+    
+    #self.liblist  = [['libccaffeine_'+self.version.replace('.','_')+'.a']]
+    try:
+      self.ccaspec_config = config.base.Configure.executeShellCommand(self.ccafe_config + ' --var CCAFE_CCA_SPEC_BABEL_CONFIG')[0].rstrip()
+      if not os.path.exists(self.ccaspec_config):
+        raise RuntimeError('Cannot locate cca-spec-babel-config executable')
+      self.specversion = config.base.Configure.executeShellCommand(self.ccaspec_config + ' --var CCASPEC_VERSION')[0].rstrip()
+    except RuntimeError, e:
+      raise RuntimeError('Error when attempting to determine ccafe cca-spec-babel version')
+
+    self.babel = config.base.Configure.executeShellCommand(self.ccaspec_config + ' --var CCASPEC_BABEL_BABEL')[0].rstrip()
+    if not self.getExecutable(self.babel, resultName = 'babel'):
+      raise RuntimeError('Located Babel library and include file but could not find babel executable')
+    if not self.getExecutable(self.babel + '-config', resultName = 'babel_config'):
+      raise RuntimeError('Located Babel library, includes, and babel executable but could not find babel-config executable')
+
+    ccafePkgName = config.base.Configure.executeShellCommand(self.ccafe_config + ' --var CCAFE_PKG_NAME')[0].rstrip()
+    self.includes = [ccafePkgName + '/cmd/Cmd.h']
+
+    dir = config.base.Configure.executeShellCommand(self.babel_config + ' --query-var=prefix')[0].rstrip()
+    if os.path.isdir(dir):
+      self.framework.argDB['with-babel-dir'] = dir
+    return
+    
   def setupDependencies(self, framework):
     PETSc.package.Package.setupDependencies(self, framework)
-    self.babel      = framework.require('PETSc.packages.babel',self)
     return
 
   def configureLibrary(self):
     '''Calls the regular package configureLibrary and then does an additional test needed by babel'''
     '''Normally you do not need to provide this method'''
+
+    self.miscSetup()
+    self.babel      = self.framework.require('PETSc.packages.babel', self)
+    
     PETSc.package.Package.configureLibrary(self)
+
     self.addMakeMacro('CCAFE_HOME',self.framework.argDB['with-ccafe-dir'])
+    self.addMakeMacro('CCAFE_CONFIG', self.ccafe_config)
     self.addMakeMacro('CCAFE_VERSION',self.version)
-    self.addMakeMacro('CCA_REPO','${CCAFE_HOME}/share/cca-spec-babel-0_7_8-babel-${BABEL_VERSION}/xml')
+    self.addMakeMacro('CCASPEC_VERSION', config.base.Configure.executeShellCommand(self.ccaspec_config + ' -var CCASPEC_VERSION')[0].rstrip())
+    self.addMakeMacro('CCASPEC_BABEL_BABEL',  self.babel)
+    self.addMakeMacro('CCASPEC_BABEL_VERSION',  config.base.Configure.executeShellCommand(self.ccaspec_config + ' -var CCASPEC_BABEL_VERSION')[0].rstrip())
+    self.addMakeMacro('BABEL_CONFIG', self.babel_config)
+    self.addMakeMacro('CCA_REPO','${CCAFE_HOME}/share/cca-spec-babel-${CCASPEC_VERSION}-babel-${BABEL_VERSION}/xml')
     self.addMakeMacro('HAVE_CCA','-DHAVE_CCA')
+    return
 
 if __name__ == '__main__':
   import config.framework
