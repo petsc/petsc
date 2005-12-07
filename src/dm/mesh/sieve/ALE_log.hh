afd5d8ac29b278a589cecffe17c281e46a4e1f2f
Dmitry Karpeev <karpeev@mcs.anl.gov>
2005-12-07 13:40:39 -0600

    bk-changeset-1.3844.1.1 karpeev@lenz.(none)|ChangeSet|20051207194039|19949 ChangeSet   1.3844.1.1 05/12/07 13:40:39 karpeev@lenz.(none) +3 -0   mesh/sieve: minor improvements to logging, new container interface.
      src/dm/mesh/sieve/ALE_log.hh
        1.8 05/12/07 13:40:36 karpeev@lenz.(none) +51 -21
        LogEvent is now a type (int).
        Extra helper macros: ALE_LOG_STAGE_START/FINISH, ALE_LOG_EVENT_START/FINISH.
    
      src/dm/mesh/sieve/ALE_containers.hh
        1.7 05/12/07 13:40:36 karpeev@lenz.(none) +73 -106
        Refinements of the container interface (namespace ALE::def); still in progress.
    
      src/dm/mesh/sieve/ALE.cxx
        1.14 05/12/07 13:40:36 karpeev@lenz.(none) +11 -5
        Minor improvements to the logging interface.
    
    
    Hg-commit: bc9e0ed325018fec08e8d0487d2776342e6610d4

--- src/dm/mesh/sieve/ALE_log.hh
+++ src/dm/mesh/sieve/ALE_log.hh
@@ -8,44 +8,74 @@ namespace ALE {
   void setVerbosity(const int& verbosity);
 
   typedef PetscCookie LogCookie;
+  typedef int         LogStage;
   typedef PetscEvent  LogEvent;
 
   LogCookie LogCookieRegister(const char *name);
 
-  int       LogStageRegister(const char *name);
-  void      LogStagePush(int stage);
-  void      LogStagePop(int stage);
+  LogStage  LogStageRegister(const char *name);
+  void      LogStagePush(LogStage stage);
+  void      LogStagePop(LogStage stage);
 
   LogEvent  LogEventRegister(LogCookie cookie, const char* event_name);
-  void      LogEventBegin(int e);
-  void      LogEventEnd(int e);
+  LogEvent  LogEventRegister(const char* event_name);
+  void      LogEventBegin(LogEvent e);
+  void      LogEventEnd(LogEvent e);
   
 
 
 } // namespace ALE
 
-// Helper macros that push and pop log stages bracketing method invocations.
+//    Helper macros that push and pop log stages bracketing method invocations.
 // These depend on the __FUNCT__ macro being declared correctly -- as the qualified method name (e.g., PreSieve::cone).
-// Every ALE_LOG_STAGE_BEGIN must be matched by a corresponding ALE_LOG_STAGE_END.
+//    Every ALE_LOG_STAGE_BEGIN must be matched by a corresponding ALE_LOG_STAGE_END.
 // For proper logging, these macro calls must be placed outside of all code in a function, including variable declaration,
 // except return value declaration and the actual return statement. This might require some code rearrangement.
 // In particular, returns from inside the block bracketed by the macros will break the stage stack.
+//    ALE_LOG_STAGE_START and ALE_LOG_STAGE_FINISH mirror the corresponding BEGIN and END macros, except that they do not contain
+// opening and closing braces and can be used more freely throughout the code
+//    ALE_LOG_EVENT_START and ALE_LOG_EVENT_FINISH can likewise be used throughout the code to start and stop logging of an event 
+// associate with the function __FUNCT__.  The difference between function stages and events is implementation-dependent 
+// (currently PETSc logging).
+
 #if (defined ALE_USE_LOGGING) && (defined ALE_LOGGING_USE_STAGES)
-#define ALE_LOG_STAGE_BEGIN                                                             \
-  {                                                                                     \
-    int stage = ALE::LogStageRegister(__FUNCT__);                       \
-    ALE::LogStagePush(stage);                                           \
-  }                                                                                     \
-  {                                                                               
-#define ALE_LOG_STAGE_END                                                               \
-  }                                                                                     \
-  {                                                                                     \
-    int stage = ALE::LogStageRegister(__FUNCT__);                       \
-    ALE::LogStagePop(stage);                                            \
-  }                                                                                     
+
+#define ALE_LOG_STAGE_START                                 \
+  {                                                         \
+    ALE::LogEvent stage = ALE::LogStageRegister(__FUNCT__); \
+    ALE::LogStagePush(stage);                               \
+  }                                                     
+
+#define ALE_LOG_STAGE_FINISH                                \
+  {                                                         \
+    ALE::LogStage stage = ALE::LogStageRegister(__FUNCT__); \
+    ALE::LogStagePop(stage);                                \
+  }                                                     
+
+#define ALE_LOG_STAGE_BEGIN    ALE_LOG_STAGE_START  {
+#define ALE_LOG_STAGE_END      } ALE_LOG_STAGE_FINISH 
+
+#define ALE_LOG_EVENT_BEGIN                                 \
+  {                                                         \
+    ALE::LogEvent event = ALE::LogEventRegister(__FUNCT__); \
+    ALE::LogEventBegin(event);                              \
+  }                                                                        
+
+#define ALE_LOG_EVENT_END                                   \
+  {                                                         \
+    ALE::LogEvent event = ALE::LogEventRegister(__FUNCT__); \
+    ALE::LogEventEnd(event);                                \
+  }                                                         
+
 #else
-#define ALE_LOG_STAGE_BEGIN {}
-#define ALE_LOG_STAGE_BEGIN {}
+
+#define ALE_LOG_STAGE_START  {}
+#define ALE_LOG_STAGE_FINISH {}
+#define ALE_LOG_STAGE_BEGIN  {}
+#define ALE_LOG_STAGE_BEGIN  {}
+#define ALE_LOG_EVENT_BEGIN  {}
+#define ALE_LOG_EVENT_END    {}
+
 #endif
 
 
