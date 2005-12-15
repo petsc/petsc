9e175f8a69c10c2328cef4b55a7a463f65179698
Barry Smith <bsmith@mcs.anl.gov>
2005-12-14 20:36:15 -0600

    bk-changeset-1.3869.3.1 barrysmith@barry-smiths-computer.local|ChangeSet|20051215023615|37918 ChangeSet   1.3869.3.1 05/12/14 20:36:15 barrysmith@barry-smiths-computer.local +5 -0   cleanup of outdated stuff
      src/docs/website/miscellaneous/external.html
        1.7 05/12/14 20:36:10 barrysmith@barry-smiths-computer.local +140 -25
        cleanup of outdated stuff
    
      src/docs/website/documentation/troubleshooting.html
        1.30 05/12/14 20:36:10 barrysmith@barry-smiths-computer.local +182 -1328
        cleanup of outdated stuff
    
      src/docs/website/documentation/faq.html
        1.29 05/12/14 20:36:10 barrysmith@barry-smiths-computer.local +795 -55
        cleanup of outdated stuff
    
      src/docs/website/documentation/bugreporting.html
        1.15 05/12/14 20:36:10 barrysmith@barry-smiths-computer.local +133 -36
        cleanup of outdated stuff
    
      python/PETSc/packages/BlasLapack.py
        1.147 05/12/14 19:38:13 barrysmith@barry-smiths-computer.local +6 -2
        petsc-maint 14133 only tell user to use appropriate --download-c or f-blas-lapack
    
    
    Hg-commit: 7562777cf7068e3ac49f09a6968e99e2debd6ee1

--- python/PETSc/packages/BlasLapack.py
+++ python/PETSc/packages/BlasLapack.py
@@ -397,14 +397,18 @@ class Configure(PETSc.package.Package):
         blib = glob.glob('/usr/lib/libblas.*')
         if blib != [] and not (os.path.isfile('/usr/lib/libblas.so') or os.path.isfile('/usr/lib/libblas.a')):
           raise RuntimeError('Incomplete BLAS install? Perhaps blas package is installed - but blas-dev/blas-devel is required.')
-        raise RuntimeError('Could not find a functional BLAS. Run with --with-blas-lib=<lib> to indicate the library containing BLAS.\n Or --download-c-blas-lapack=1 or --download-f-blas-lapack=1 to have one automatically downloaded and installed\n')
+        if hasattr(self.compilers, 'FC'): C = 'f'
+        else: C = 'c'
+        raise RuntimeError('Could not find a functional BLAS. Run with --with-blas-lib=<lib> to indicate the library containing BLAS.\n Or --download-'+C+'-blas-lapack=1 to have one automatically downloaded and installed\n')
       if not self.foundLapack:
         # check for split blas/blas-dev packages
         import glob
         llib = glob.glob('/usr/lib/liblapack.*')
         if llib != [] and not (os.path.isfile('/usr/lib/liblapack.so') or os.path.isfile('/usr/lib/liblapack.a')):
           raise RuntimeError('Incomplete LAPACK install? Perhaps lapack package is installed - but lapack-dev/lapack-devel is required.')
-        raise RuntimeError('Could not find a functional LAPACK. Run with --with-lapack-lib=<lib> to indicate the library containing LAPACK.\n Or --download-c-blas-lapack=1 or --download-f-blas-lapack=1 to have one automatically downloaded and installed\n')
+        if hasattr(self.compilers, 'FC'): C = 'f'
+        else: C = 'c'
+        raise RuntimeError('Could not find a functional LAPACK. Run with --with-lapack-lib=<lib> to indicate the library containing LAPACK.\n Or --download-'+C+'-blas-lapack=1 to have one automatically downloaded and installed\n')
     self.found = 1
     return
 
