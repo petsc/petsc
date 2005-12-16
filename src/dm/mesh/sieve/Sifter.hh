bb98681f0dedae29507f77fa347c092a77297c0f
Matthew Knepley <knepley@mcs.anl.gov>
2005-12-15 22:19:59 -0600

    bk-changeset-1.3875 knepley@khan.(none)|ChangeSet|20051216041959|46090 ChangeSet   1.3875 05/12/15 22:19:59 knepley@khan.(none) +2 -0   Put in Sifter
      src/dm/mesh/sieve/makefile
        1.13 05/12/15 22:19:56 knepley@khan.(none) +4 -4
        Added path to boost
        Added Sifter
    
      src/dm/mesh/sieve/Sifter.hh
        1.5 05/12/15 22:19:56 knepley@khan.(none) +185 -59
        Added more implementation
    
    
    Hg-commit: 4afcad165d55fcc40f67af4d500ba1c4d25d5d7d

--- src/dm/mesh/sieve/Sifter.hh
+++ src/dm/mesh/sieve/Sifter.hh
@@ -1,6 +1,11 @@
 #ifndef included_ALE_Sifter_hh
 #define included_ALE_Sifter_hh
 
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <ostream>
+
 // ALE extensions
 
 #ifndef  included_ALE_hh
@@ -18,109 +23,230 @@ namespace ALE {
     class Point {
     public:
       int32_t prefix, index;
+      // Constructors
+      Point() : prefix(0), index(0){};
+      Point(const int32_t& p, const int32_t& i) : prefix(p), index(i){};
+      Point(const Point& p) : prefix(p.prefix), index(p.index){};
+      // Comparisons
+      bool operator==(const Point& q) const {
+        return ( (this->prefix == q.prefix) && (this->index == q.index) );
+      };
+      bool operator!=(const Point& q) const {
+        return ( (this->prefix != q.prefix) || (this->index != q.index) );
+      };
+      bool operator<(const Point& q) const {
+        return( (this->prefix < q.prefix) || ((this->prefix == q.prefix) && (this->index < q.index)));
+      };
+      // Printing
+      friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        os << "(" << p.prefix << ", "<< p.index << ")" << std::endl;
+        return os;
+      };
     };
     
     // iterator interface
-    template <typename Color>
+    template <typename Data>
     class const_iterator {
-      virtual ~iterator() = 0;
+    public:
+      virtual ~const_iterator() = 0;
       //
       virtual void                  operator++();
       virtual void                  operator++(int);
-      virtual bool                  operator==(const iterator& itor);
-      virtual bool                  operator!=(const iterator& itor);
-      virtual const Color&          operator*()  = 0;
+      virtual bool                  operator==(const const_iterator& itor);
+      virtual bool                  operator!=(const const_iterator& itor);
+      virtual const Data&           operator*()  = 0;
     };
 
     // const_sequence interface:
     // a constant sequence of (not necesserily unique) colors delineated by begin() & end() iterators; can be traversed linearly
-    template <typename Color>
+    template <typename Data>
     class const_sequence {
-      typedef const_iterator iterator;
+    public:
+      typedef const_iterator<Data> iterator;
       virtual ~const_sequence() = 0;
       //
-      virtual const_iterator& begin();
-      virtual const_iterator& end();
-      virtual std::size_t     size();
+      virtual const_iterator<Data>& begin();
+      virtual const_iterator<Data>& end();
+      virtual std::size_t           size();
     };
 
     // const_collection interface:
     // a constant collection no particular order; can queried for containment of a given color
+    template <typename Data>
     class const_collection {
       virtual ~const_collection() = 0;
       //
-      virtual bool contains(const Color& p);
+      virtual bool contains(const Data& p);
     };
 
     // const_set interface
     // combines const_sequence & const_collection interfaces
-    template <typename Color>
-    class const_set : public const_sequence<Color>, public const_collection<Color> {
+    template <typename Data>
+    class const_set : public const_sequence<Data>, public const_collection<Data> {
       virtual ~const_set();
     };
 
     // set interface:
     // extends const_set interface to allows point addition and removal
-    template <typename Color>
-    class set : public const_set<Color> {
+    template <typename Data>
+    class set : public const_set<Data> {
+      const std::set<Data>& delegate;
+    public:
+      set();
+      set(const std::set<Data>& delegate);
       // destructor
       virtual ~set();
       // mutating methods
-      virtual void insert(const Color& p);                     // post: contains(p) == true 
-      virtual void remove(const Color& p);                     // post: contains(p) == false
-      virtual void add(const const_sequence& s);               // post: contains colors from s and '*this before the call'
-      virtual void add(const const_collection& s);             // post: contains colors from s and '*this before the call'
-      virtual void intersect(const const_sequence& s);         // post: contains colors common to s and '*this before the call'
-      virtual void intersect(const const_collection& s);       // post: contains colors common to s and '*this before the call'
-      virtual void subtract(const const_sequence&  s);         // post: contains colors of '*this before call' that are not in s
-      virtual void subtract(const const_collection&  s);       // post: contains colors of '*this before call' that are not in s
+      virtual void insert(const Data& p);                            // post: contains(p) == true 
+      virtual void remove(const Data& p);                            // post: contains(p) == false
+      virtual void add(const const_sequence<Data>& s);               // post: contains colors from s and '*this before the call'
+      virtual void add(const const_collection<Data>& s);             // post: contains colors from s and '*this before the call'
+      virtual void intersect(const const_sequence<Data>& s);         // post: contains colors common to s and '*this before the call'
+      virtual void intersect(const const_collection<Data>& s);       // post: contains colors common to s and '*this before the call'
+      virtual void subtract(const const_sequence<Data>&  s);         // post: contains colors of '*this before call' that are not in s
+      virtual void subtract(const const_collection<Data>&  s);       // post: contains colors of '*this before call' that are not in s
     };
-    
-    //
-    // Sifter:  
-    //
+
     template <typename Color>
-    class Sifter {
-      Obj<const_sequence<Color> > cone(const Obj<const_sequence<Point> >& p); 
-      Obj<const_sequence<Color> > nCone(const Obj<const_sequence<Point> >& p, const int& n); 
-      Obj<const_sequence<Color> > support(const Obj<const_sequence<Point> >& p); 
-      Obj<const_sequence<Color> > nSupport(const Obj<const_sequence<Point> >& p, const int& n); 
-      //
-      Obj<const_sequence<Color> > closure(const Obj<const_sequence<Point> >& p); 
-      Obj<const_sequence<Color> > nClosure(const Obj<const_sequence<Point> >& p, const int& n); 
-      Obj<const_sequence<Color> > star(const Obj<const_sequence<Point> >& p); 
-      Obj<const_sequence<Color> > nStar(const Obj<const_sequence<Point> >& p, const int& n); 
-      //
-      Obj<const_sequence<Color> > meet(const const_sequence<Point>& pp);
-      Obj<const_sequence<Color> > nMeet(const const_sequence<Point>& pp, const int& n);
-      Obj<const_sequence<Color> > join(const const_sequence<Point>& pp);
-      Obj<const_sequence<Color> > nJoin(const const_sequence<Point>& pp, const int& n);
+    struct Arrow {
+      Point source;
+      Point target;
+      Color color;
 
-      int depth(const Point& p);
-      int height(const Point& p);
-      int diameter(const Point& p);
-      int diameter();
-      
-      //
-      Obj<const_sequence<Point> > depthStratum(const int& depth);
-      Obj<const_sequence<Point> > heightStratum(const int& height);
-      //
-      void                        setStratification(bool on);
-      bool                        getStratification();
-      void                        stratify();
-
-      void                        addColor(const Obj<const_sequence<Color> >& colors, const Point& p);
-      void                        addCone(const Obj<const_sequence<Point> >& points,  const Point& p);
-
-      void                        add(const Obj<Sifter<Color> >& sifter);  // pointwise addition of fibers
-      Sifter<Color>               completion(const Sifter<Point>& base);   // prescribes fibers to be exchanged
+      Arrow(Point s, Point t, Color c) : source(s), target(t), color(c) {};
+      friend std::ostream& operator<<(std::ostream& os, const Arrow& a) {
+        os << a.source << " --" << a.color << "--> " << a.target << std::endl;
+        return os;
+      }
     };
 
+    //
+    // Sieve:
+    //   A Sieve is a set of {\emph arrows} connecting {\emph points} of type Data. Thus we
+    // could realize a sieve, for instance, as a directed graph. In addition, we will often
+    // impose an acyclicity constraint, arriving at a DAG. Each arrow may also carry a label,
+    // or {\emph color}, and the interface allows sets of arrows to be filtered by color.
+    //
+    template <typename Data, typename Color>
+    class Sieve {
+      // tags for accessing the corresponding indices of employee_set
+      struct source{};
+      struct target{};
+      struct color{};
+      typedef Arrow<Color> SieveArrow;
+      typedef ::boost::multi_index::multi_index_container<
+        SieveArrow,
+        ::boost::multi_index::indexed_by<
+          ::boost::multi_index::ordered_non_unique<
+            ::boost::multi_index::tag<source>,  BOOST_MULTI_INDEX_MEMBER(SieveArrow,Point,source)>,
+          ::boost::multi_index::ordered_non_unique<
+            ::boost::multi_index::tag<target>,  BOOST_MULTI_INDEX_MEMBER(SieveArrow,Point,target)>,
+          ::boost::multi_index::ordered_non_unique<
+            ::boost::multi_index::tag<color>,  BOOST_MULTI_INDEX_MEMBER(SieveArrow,Color,color)>
+        >
+      > ArrowSet;
+
+      class coneIterator : public const_iterator<Data> {
+        //const ArrowSet::index<target>::type::iterator arrowIter;
+        const typename boost::multi_index::index<ArrowSet,target>::type::iterator& arrowIter;
+      public:
+        coneIterator(const typename boost::multi_index::index<ArrowSet,target>::type::iterator& iter) : arrowIter(iter) {};
+        virtual ~coneIterator() {};
+        //
+        virtual void                  operator++() {this->arrowIter++;};
+        virtual void                  operator++(int n) {for(int i = 0; i < n; i++) this->arrowIter++;};
+        virtual bool                  operator==(const const_iterator<Data>& itor) {return this->arrowIter == itor;};
+        virtual bool                  operator!=(const const_iterator<Data>& itor) {return this->arrowIter == itor;};
+        virtual const Data&           operator*() {return *this->arrowIter;};
+      };
+
+      class coneSequence : public const_sequence<Data> {
+        const typename ::boost::multi_index::index<ArrowSet,target>::type& coneIndex;
+        const Point& key;
+      public:
+        coneSequence(const typename ::boost::multi_index::index<ArrowSet,target>::type& cone, const Point& p) : coneIndex(cone), key(p) {};
+        virtual ~coneSequence() {};
+        virtual const_iterator<Data>& begin() {return coneIterator(this->coneIndex.lower_bound(key));};
+        virtual const_iterator<Data>& end() {return coneIterator(this->coneIndex.upper_bound(key));};
+        virtual std::size_t           size() {return sizeof(Point);};
+      };
+      //
+      ArrowSet        arrows;
+      std::set<Color> colors;
+    public:
+      Sieve();
+      // The basic Sieve interface
+      Obj<const_sequence<Data> > cone(const Obj<const_sequence<Data> >& p);
+      Obj<const_sequence<Data> > cone(const Obj<const_sequence<Data> >& p, const Color& color);
+      Obj<const_sequence<Data> > nCone(const Obj<const_sequence<Data> >& p, const int& n);
+      Obj<const_sequence<Data> > nCone(const Obj<const_sequence<Data> >& p, const int& n, const Color& color);
+      Obj<const_sequence<Data> > support(const Obj<const_sequence<Data> >& p);
+      Obj<const_sequence<Data> > support(const Obj<const_sequence<Data> >& p, const Color& color);
+      Obj<const_sequence<Data> > nSupport(const Obj<const_sequence<Data> >& p, const int& n);
+      Obj<const_sequence<Data> > nSupport(const Obj<const_sequence<Data> >& p, const int& n, const Color& color);
+      // Iterated versions
+      Obj<const_sequence<Data> > closure(const Obj<const_sequence<Data> >& p);
+      Obj<const_sequence<Data> > closure(const Obj<const_sequence<Data> >& p, const int& n, const Color& color);
+      Obj<const_sequence<Data> > nClosure(const Obj<const_sequence<Data> >& p, const int& n);
+      Obj<const_sequence<Data> > nClosure(const Obj<const_sequence<Data> >& p, const int& n, const int& n, const Color& color);
+      Obj<const_sequence<Data> > star(const Obj<const_sequence<Data> >& p);
+      Obj<const_sequence<Data> > star(const Obj<const_sequence<Data> >& p, const int& n, const Color& color);
+      Obj<const_sequence<Data> > nStar(const Obj<const_sequence<Data> >& p, const int& n);
+      Obj<const_sequence<Data> > nStar(const Obj<const_sequence<Data> >& p, const int& n, const int& n, const Color& color);
+      // Lattice methods
+      Obj<const_sequence<Data> > meet(const const_sequence<Data>& pp);
+      Obj<const_sequence<Data> > meet(const const_sequence<Data>& pp, const Color& color);
+      Obj<const_sequence<Data> > nMeet(const const_sequence<Data>& pp, const int& n);
+      Obj<const_sequence<Data> > nMeet(const const_sequence<Data>& pp, const int& n, const Color& color);
+      Obj<const_sequence<Data> > join(const const_sequence<Data>& pp);
+      Obj<const_sequence<Data> > join(const const_sequence<Data>& pp, const Color& color);
+      Obj<const_sequence<Data> > nJoin(const const_sequence<Data>& pp, const int& n);
+      Obj<const_sequence<Data> > nJoin(const const_sequence<Data>& pp, const int& n, const Color& color);
+      // Manipulation
+      void                       addArrow(const Data& p,  const Data& q);
+      void                       addArrow(const Data& p,  const Data& q, const Color& color);
+      void                       setColor(const Data& p,  const Data& q, const Color& color);
+      void                       addCone(const Obj<const_sequence<Data> >& points,  const Data& p);
+      void                       addSupport(const Data& p,const Obj<const_sequence<Data> >& points);
+      void                       add(const Obj<Sieve<Data,Color> >& sieve);
+      // Parallelism
+      Obj<std::map<Point, Sieve<Data,Color> > > completion(const Sieve<Data,Color>& base);
+      // These methods are meaningful only for acyclic sieves
+      int depth(const Data& p);
+      int height(const Data& p);
+      int diameter(const Data& p);
+      int diameter();
+      Obj<const_sequence<Data> > depthStratum(const int& depth);
+      Obj<const_sequence<Data> > heightStratum(const int& height);
+      void                       setStratification(bool on);
+      bool                       getStratification();
+      void                       stratify();
+    };
+
+    //
+    // CoSieve:
+    //   Value is the type of function space (PreSheaf?) for fibers over the mesh
+    //
+    template <typename Data, typename Value>
+    class CoSieve {
+      const Value *restrict(const Data& support, const const_sequence<Data>& chain);
+      void         assemble(const Data& support, const const_sequence<Data>& chain, const Value values[]);
+
+      // \rho
+      //     This needs some sort of policy argument
+      void reduce();
+
+      // Refine the support chain
+      void refine();
+      void refine(const const_sequence<Data>& chain);
+      // Coarsen the support chain
+      void coarsen();
+      void coarsen(const const_sequence<Data>& chain);
+    };
   } // namespace def
 
 
 
 } // namespace ALE
-
 
 #endif
