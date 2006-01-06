d2452a4d3be4755cef5882404f085a96a55b5f2a
Satish Balay <balay@mcs.anl.gov>
2006-01-06 15:35:49 -0600

    bk-changeset-1.3925 balay@asterix.mcs.anl.gov|ChangeSet|20060106213549|30496 ChangeSet   1.3925 06/01/06 15:35:49 balay@asterix.mcs.anl.gov +1 -0   add in slamch()/dlamch() error check for superlu_dist as well
      python/PETSc/packages/SuperLU_DIST.py
        1.48 06/01/06 15:35:46 balay@asterix.mcs.anl.gov +15 -0
        add in slamch()/dlamch() error check for superlu_dist as well
    
    
    Hg-commit: 2b53738279f55ba71ef3aba6ac64842c9210d4b3

--- python/PETSc/packages/SuperLU_DIST.py
+++ python/PETSc/packages/SuperLU_DIST.py
@@ -87,6 +87,21 @@ class Configure(PETSc.package.Package):
       self.framework.actions.addArgument(self.PACKAGE, 'Install', 'Installed SUPERLU_DIST into '+installDir)
     return self.getDir()
 
+  def configureLibrary(self):
+    '''Calls the regular package configureLibrary and then does an additional test needed by SuperLU_DIST'''
+    '''Normally you do not need to provide this method'''
+    PETSc.package.Package.configureLibrary(self)
+    # SuperLU_DIST requires slamch() & dlamch() LAPACK routines and PETSc version of superlu
+    # have the internal versions disabled in favour of generic blas/lapack
+    if not self.blasLapack.checkForRoutine('slamch'): 
+      raise RuntimeError('SuperLU_DIST requires the LAPACK routine slamch(), the current Lapack libraries '+str(self.blasLapack.lib)+' does not have it\nIf you are using the IBM ESSL library, it does not contain this function. After installing a complete copy of lapack\n You can run config/configure.py with --with-blas-lib=libessl.a --with-lapack-lib=/usr/local/lib/liblapack.a')
+    self.framework.log.write('Found slamch() in Lapack library as needed by SuperLU_DIST\n')
+
+    if not self.blasLapack.checkForRoutine('dlamch'): 
+      raise RuntimeError('SuperLU_DIST requires the LAPACK routine dlamch(), the current Lapack libraries '+str(self.blasLapack.lib)+' does not have it\nIf you are using the IBM ESSL library, it does not contain this function. After installing a complete copy of lapack\n You can run config/configure.py with --with-blas-lib=libessl.a --with-lapack-lib=/usr/local/lib/liblapack.a')
+    self.framework.log.write('Found dlamch() in Lapack library as needed by SuperLU_DIST\n')
+    return
+  
 if __name__ == '__main__':
   import config.framework
   import sys
