c3a666ddc919c21bbd90397aafb1ed7dfc5c716b
Matthew Knepley <knepley@mcs.anl.gov>
2006-01-22 09:42:50 -0600

    bk-changeset-1.3972.2.1 knepley@khan.(none)|ChangeSet|20060122154250|64993 ChangeSet   1.3972.2.1 06/01/22 09:42:50 knepley@khan.(none) +1 -0   Configure fix
      python/PETSc/packages/Boost.py
        1.1 06/01/22 09:42:47 knepley@khan.(none) +21 -0
        New BitKeeper file ``python/PETSc/packages/Boost.py''
    
      python/PETSc/packages/Boost.py
        1.0 06/01/22 09:42:47 knepley@khan.(none) +0 -0
        BitKeeper file /PETSc3/petsc/petsc-dev/python/PETSc/packages/Boost.py
    
    
    Hg-commit: 7229a143d91e894db95e62b1d11076068dcd2375

---
+++ python/PETSc/packages/Boost.py
@@ -0,0 +1,21 @@
+import PETSc.package
+
+class Configure(PETSc.package.Package):
+  def __init__(self, framework):
+    PETSc.package.Package.__init__(self, framework)
+    self.download  = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/boost.tar.gz']
+    self.includes  = ['multi_index_container.hpp']
+    self.cxx       = 1
+    self.complex   = 1
+    return
+
+  def setupDependencies(self, framework):
+    PETSc.package.Package.setupDependencies(self, framework)
+    self.deps = []
+    return
+
+  def Install(self):
+    import sys
+    boostDir = self.getDir()
+    self.framework.actions.addArgument('Boost', 'Install', 'Installed Boost into '+boostDir)
+    return boostDir
