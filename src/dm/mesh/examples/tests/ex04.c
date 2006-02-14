fa3f2a6fe12dd0aa66b8ef410a9ea2da62f041c2
Matthew Knepley <knepley@mcs.anl.gov>
2006-02-13 18:26:50 -0600

    bk-changeset-1.4043 knepley@khan.(none)|ChangeSet|20060214002650|40405 ChangeSet   1.4043 06/02/13 18:26:50 knepley@khan.(none) +1 -0   More mesh
      src/dm/mesh/examples/tests/ex04.c
        1.5 06/02/13 18:26:49 knepley@khan.(none) +1 -1
        Small fix
    
    
    Hg-commit: 2d818f56be1caf724001ef9947bb1fe821ad569b

--- src/dm/mesh/examples/tests/ex04.c
+++ src/dm/mesh/examples/tests/ex04.c
@@ -13,7 +13,7 @@ static char help[] = "Constructs a series of parallel BiGraphs and performs Delt
 #include <ALE.hh>
 
 
-typedef ALE::Two::BiGraph<int,ALE::def::Point,int>                       PointBiGraph;
+typedef ALE::Two::BiGraph<int,ALE::Two::Rec<int>,ALE::def::Point,ALE::Two::Rec<ALE::def::Point>,int> PointBiGraph;
 typedef ALE::Two::RightConeDuplicationFuser<PointBiGraph,PointBiGraph>   PointConeFuser;
 typedef ALE::Two::ParDelta<PointBiGraph,PointConeFuser>                  PointParDelta;
 typedef PointParDelta::overlap_type                                      PointOverlap;
