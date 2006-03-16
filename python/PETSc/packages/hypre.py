5c73608d30dbfa57587d857514e5dfa1907d0e0a
Satish Balay <balay@mcs.anl.gov>
2006-03-16 09:17:25 -0600

    bk-changeset-1.4147 balay@asterix.mcs.anl.gov|ChangeSet|20060316151725|63226 ChangeSet   1.4147 06/03/16 09:17:25 balay@asterix.mcs.anl.gov +1 -0   upgrade hypre to 1.10.0b
      python/PETSc/packages/hypre.py
        1.73 06/03/16 09:17:22 balay@asterix.mcs.anl.gov +1 -2
        upgrade hypre to 1.10.0b
    
    
    Hg-commit: a917b89d8b9a7c557601eca739c46e8f645227e9

--- python/PETSc/packages/hypre.py
+++ python/PETSc/packages/hypre.py
@@ -8,7 +8,7 @@ import PETSc.package
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
     PETSc.package.Package.__init__(self, framework)
-    self.download     = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/hypre-1.9.0b-mod.tar.gz']
+    self.download     = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/hypre-1.10.0b-mod.tar.gz']
     self.functions = ['HYPRE_IJMatrixCreate']
     self.includes  = ['HYPRE.h']
     self.license   = 'http://www.llnl.gov/CASC/hypre/download/hyprebeta_cur_agree.html'
@@ -31,7 +31,6 @@ class Configure(PETSc.package.Package):
             'MatrixMatrix',
             'ParaSails',
             'krylov',
-            'lobpcg',
             'parcsr_ls',
             'parcsr_mv',
             'seq_mv',
