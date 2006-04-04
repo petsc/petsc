79adfffaad71417e1e0916cdf940663c0c190b2f
Matthew Knepley <knepley@mcs.anl.gov>
2006-04-04 14:42:39 -0600

    bk-changeset-1.4224.2.1 knepley@khan.(none)|ChangeSet|20060404204239|56372 ChangeSet   1.4224.2.1 06/04/04 15:42:39 knepley@khan.(none) +2 -0   Configure fix
      python/PETSc/packages/Sieve.py
        1.3 06/04/04 15:42:37 knepley@khan.(none) +9 -2
        Need a bunch of include directories
        Need the petscdir
        Need a search directory
    
      python/PETSc/package.py
        1.92 06/04/04 15:42:37 knepley@khan.(none) +12 -7
        Allow lists of include directories
        Allow an extended timeout for include checking
         - C++ is SO SLOW
    
    
    Hg-commit: 566a0126a636091dd2629f3854734e30adf42fdd

--- python/PETSc/packages/Sieve.py
+++ python/PETSc/packages/Sieve.py
@@ -1,11 +1,14 @@
 import PETSc.package
 
+import os
+
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
     PETSc.package.Package.__init__(self, framework)
     self.download        = ['ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/sieve.tar.gz']
     self.includes        = ['Mesh.hh']
-    self.includedir      = ''
+    self.includedir      = ['include', os.path.join('include', 'mpiuni'), os.path.join('bmake', 'docsonly'), os.path.join('src', 'dm', 'mesh', 'sieve')]
+    self.libdir          = ''
     self.archIndependent = 1
     self.cxx             = 1
     self.complex         = 1
@@ -13,11 +16,15 @@ class Configure(PETSc.package.Package):
 
   def setupDependencies(self, framework):
     PETSc.package.Package.setupDependencies(self, framework)
-    self.boost = self.framework.require('PETSc.packages.Boost',self)
+    self.petscdir = self.framework.require('PETSc.utilities.petscdir',self)
+    self.boost    = self.framework.require('PETSc.packages.Boost',self)
     #self.boost = self.framework.require('PETSc.packages.Triangle',self)
     #self.boost = self.framework.require('PETSc.packages.TetGen',self)
     self.deps = [self.boost]
     return
+
+  def getSearchDirectories(self):
+    return [self.petscdir.dir]
 
   def Install(self):
     import sys
