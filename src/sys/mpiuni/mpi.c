dd46ca7384129cb2faad540f0eeede6c119db9e0
Satish Balay <balay@mcs.anl.gov>
2006-07-22 22:59:21 -0500

    another comm_split fix
    Hg-commit: 915cd6c344a5d2cdd9f29aa7cccebec8b429df46

--- src/sys/mpiuni/mpi.c
+++ src/sys/mpiuni/mpi.c
@@ -206,7 +206,7 @@ void PETSC_STDCALL mpi_comm_rank_(MPI_Comm *comm,int *rank,int *ierr)
   *ierr=MPI_SUCCESS;
 }
 
-void PETSC_STDCALL mpi_comm_split(MPI_Comm *comm,int *color,int *key, MPI_Comm *newcomm, int *ierr)
+void PETSC_STDCALL mpi_comm_split_(MPI_Comm *comm,int *color,int *key, MPI_Comm *newcomm, int *ierr)
 {
   *newcomm = *comm;
   *ierr=MPI_SUCCESS;
