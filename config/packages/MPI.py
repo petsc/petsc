825d136af19f31b28ff0e9f0bdb736aacc2dd7a5
Satish Balay <balay@mcs.anl.gov>
2006-07-24 00:10:42 -0500

    try the latest mpich beta
    Hg-commit: f76f2d9617e6c107287c508c439a99d9d3644993

--- config/packages/MPI.py
+++ config/packages/MPI.py
@@ -10,7 +10,7 @@ class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
     self.download_lam     = ['http://www.lam-mpi.org/download/files/lam-7.1.1.tar.gz']
-    self.download_mpich   = ['ftp://ftp.mcs.anl.gov/pub/mpi/mpich2-1.0.3.tar.gz']
+    self.download_mpich   = ['ftp://ftp.mcs.anl.gov/pub/mpi/mpich2-1.0.4-rc1.tar.gz']
     self.download         = ['redefine']
     self.functions        = ['MPI_Init', 'MPI_Comm_create']
     self.includes         = ['mpi.h']
