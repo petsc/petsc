9671dfe8697b43ea18b99df90a13a25f8be9b9c2
Matthew Knepley <knepley@mcs.anl.gov>
2006-07-30 19:11:01 -0500

    CoSieve.hh:  - Now maintain heights and depths for each patch  - Added an update() for Section which takes a sequence  - Fixed DiscreteSieve  - Fixed OverlapSection Overlap test now working
    Hg-commit: 918e6ecb60a5b30fc347b743d3cdf14bb2d7de0e

--- src/dm/mesh/examples/tests/overlap1.cxx
+++ src/dm/mesh/examples/tests/overlap1.cxx
@@ -33,11 +33,11 @@ public:
 #define __FUNCT__ "DoubletTest"
 // The doublet is
 //
-// p0    2 | 2    p1
-//        /|\ 
+//       2 | 2
+// p0     /|\      p1
 //     5 / | \ 4
-//      /  |  \ 
-//     /   |   \ 
+//      /  |  \     _
+//     /   |   \    _
 //  0 /   4|5   \ 1
 //    \    |    /
 //     \ 6 | 6 /
@@ -45,6 +45,10 @@ public:
 //       \ | /
 //        \|/
 //       1 | 0
+//
+// This test has
+//  - A mesh overlapping itself
+//  - Single points overlapping single points
 PetscErrorCode DoubletTest(const Obj<send_section_type>& sendSection, const Obj<recv_section_type>& recvSection, Options *options)
 {
   Obj<sieve_type>        sieve       = new sieve_type(sendSection->comm(), options->debug);
@@ -52,8 +56,10 @@ PetscErrorCode DoubletTest(const Obj<send_section_type>& sendSection, const Obj<
   Obj<recv_overlap_type> recvOverlap = new recv_overlap_type(recvSection->comm(), options->debug);
   Obj<dsieve_type>       dSieve      = new dsieve_type();
   Obj<std::set<int> >    cone        = new std::set<int>();
+  int                    debug       = options->debug;
 
   PetscFunctionBegin;
+  if (sieve->commSize() != 2) SETERRQ(PETSC_ERR_SUP, "DoubletTest can only be run with 2 processes");
   // Make the sieve
   cone->insert(3);cone->insert(4);cone->insert(5);
   sieve->addCone(cone, 6);cone->clear();
@@ -94,22 +100,73 @@ PetscErrorCode DoubletTest(const Obj<send_section_type>& sendSection, const Obj<
   sendSection->getAtlas()->getTopology()->stratify();
   recvSection->getAtlas()->getTopology()->stratify();
   // Setup sections
+  sendSection->construct(sendOverlap, SupportSizer(sieve));
+  recvSection->construct(recvOverlap, SupportSizer(sieve));
   sendSection->getAtlas()->orderPatches();
   recvSection->getAtlas()->orderPatches();
   sendSection->allocate();
   recvSection->allocate();
-  sendSection->construct(sendOverlap, SupportSizer(sieve));
-  recvSection->construct(recvOverlap, SupportSizer(sieve));
   sendSection->constructCommunication();
   recvSection->constructCommunication();
   // Fill up sections
+  Obj<send_overlap_type::traits::capSequence> sendPoints = sendOverlap->cap();
+
+  if (debug) {std::cout << "Send information" << std::endl;}
+  for(send_overlap_type::traits::capSequence::iterator s_iter = sendPoints->begin(); s_iter != sendPoints->end(); ++s_iter) {
+    const Obj<send_overlap_type::traits::supportSequence>& sendPatches = sendOverlap->support(*s_iter);
+    
+    if (debug) {std::cout << "[" << sendOverlap->commRank() << "]Point " << *s_iter << std::endl;}
+    for(send_overlap_type::traits::supportSequence::iterator p_iter = sendPatches->begin(); p_iter != sendPatches->end(); ++p_iter) {
+      sendSection->update(*p_iter, *s_iter, sieve->support(*s_iter));
+
+      if (debug) {
+        std::cout << "[" << sendOverlap->commRank() << "]  Receiver " << *p_iter << " Matching Point " << p_iter.color() << std::endl;
+        const send_section_type::value_type *values = sendSection->restrict(*p_iter, *s_iter);
+        for(int i = 0; i < sendSection->getAtlas()->size(*p_iter, *s_iter); i++) {
+          std::cout << "[" << recvOverlap->commRank() << "]    " << values[i] << std::endl;
+        }
+      }
+    }
+  }
+  if (debug) {sendSection->view("Send section");}
   // Communicate
   sendSection->startCommunication();
   recvSection->startCommunication();
   sendSection->endCommunication();
   recvSection->endCommunication();
-  // Read out sections
-  // Check answer
+  // Read out sections and check answer
+  if (debug) {recvSection->view("Receive section");}
+  if (debug) {std::cout << "Receive information" << std::endl;}
+  Obj<recv_overlap_type::traits::baseSequence> recvPoints = recvOverlap->base();
+
+  for(recv_overlap_type::traits::baseSequence::iterator r_iter = recvPoints->begin(); r_iter != recvPoints->end(); ++r_iter) {
+    const Obj<recv_overlap_type::traits::coneSequence>& recvPatches = recvOverlap->cone(*r_iter);
+    
+    if (debug) {std::cout << "[" << recvOverlap->commRank() << "]Point " << *r_iter << std::endl;}
+    for(recv_overlap_type::traits::coneSequence::iterator p_iter = recvPatches->begin(); p_iter != recvPatches->end(); ++p_iter) {
+      const recv_section_type::value_type *values = recvSection->restrict(*p_iter, *r_iter);
+
+      if (debug) {
+        std::cout << "[" << recvOverlap->commRank() << "]  Sender " << *p_iter << " Matching Point " << p_iter.color() << std::endl;
+        for(int i = 0; i < recvSection->getAtlas()->size(*p_iter, *r_iter); i++) {
+          std::cout << "[" << recvOverlap->commRank() << "]    " << values[i] << std::endl;
+        }
+      }
+      if (recvOverlap->commRank() == 0) {
+        if (*r_iter == 1) {
+          if ((values[0] != 3) || (values[1] != 5)) SETERRQ1(PETSC_ERR_PLIB, "Invalid received value for point %d", *r_iter);
+        } else {
+          if ((values[0] != 4) || (values[1] != 5)) SETERRQ1(PETSC_ERR_PLIB, "Invalid received value for point %d", *r_iter);
+        }
+      } else {
+        if (*r_iter == 0) {
+          if ((values[0] != 3) || (values[1] != 4)) SETERRQ1(PETSC_ERR_PLIB, "Invalid received value for point %d", *r_iter);
+        } else {
+          if ((values[0] != 4) || (values[1] != 5)) SETERRQ1(PETSC_ERR_PLIB, "Invalid received value for point %d", *r_iter);
+        }
+      }
+    }
+  }
   PetscFunctionReturn(0);
 }
 
@@ -142,7 +199,7 @@ int main(int argc, char *argv[])
   ierr = ProcessOptions(comm, &options);CHKERRQ(ierr);
   try {
     Obj<send_section_type> sendSection = new send_section_type(comm, send_section_type::SEND, options.debug);
-    Obj<recv_section_type> recvSection = new recv_section_type(comm, recv_section_type::RECEIVE, options.debug);
+    Obj<recv_section_type> recvSection = new recv_section_type(comm, recv_section_type::RECEIVE, sendSection->getTag(), options.debug);
 
     ierr = DoubletTest(sendSection, recvSection, &options);CHKERRQ(ierr);
   } catch (ALE::Exception e) {
