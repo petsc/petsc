b7eed6a6c2a6da575e5d1604177cbd731d2d0c91
Matthew Knepley <knepley@mcs.anl.gov>
2006-08-13 15:55:12 -0500

    Take out Vec_Sieve
    Hg-commit: e0a60a4761647ee8c49e7bb8b74947d01c722f8b

--- src/vec/vec/interface/vecregall.c
+++ src/vec/vec/interface/vecregall.c
@@ -6,8 +6,10 @@ EXTERN PetscErrorCode PETSCVEC_DLLEXPORT VecCreate_Seq(Vec);
 EXTERN PetscErrorCode PETSCVEC_DLLEXPORT VecCreate_MPI(Vec);
 EXTERN PetscErrorCode PETSCVEC_DLLEXPORT VecCreate_Shared(Vec);
 EXTERN PetscErrorCode PETSCVEC_DLLEXPORT VecCreate_FETI(Vec);
+#if 0
 #if defined(PETSC_HAVE_SIEVE)
 EXTERN PetscErrorCode PETSCVEC_DLLEXPORT VecCreate_Sieve(Vec);
+#endif
 #endif
 EXTERN_C_END
 
@@ -37,8 +39,10 @@ PetscErrorCode PETSCVEC_DLLEXPORT VecRegisterAll(const char path[])
   ierr = VecRegisterDynamic(VECMPI,      path, "VecCreate_MPI",      VecCreate_MPI);CHKERRQ(ierr);
   ierr = VecRegisterDynamic(VECSHARED,   path, "VecCreate_Shared",   VecCreate_Shared);CHKERRQ(ierr);
   ierr = VecRegisterDynamic(VECFETI,     path, "VecCreate_FETI",     VecCreate_FETI);CHKERRQ(ierr);
+#if 0
 #if defined(PETSC_HAVE_SIEVE)
   ierr = VecRegisterDynamic(VECSIEVE,    path, "VecCreate_Sieve",    VecCreate_Sieve);CHKERRQ(ierr);
+#endif
 #endif
   PetscFunctionReturn(0);
 }
