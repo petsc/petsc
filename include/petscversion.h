45708ec1a0e62fe7ff3e6a5d4d6e9c182222c251
Satish Balay <balay@mcs.anl.gov>
2006-12-01 11:12:15 -0600

    fix typo, fortran include path
    Hg-commit: 06117b81da9572acc2be4ae0e9dbb9fba3f01930

--- include/petscversion.h
+++ include/petscversion.h
@@ -16,7 +16,7 @@ PETSC_EXTERN_CXX_BEGIN
 #define PETSC_VERSION_MINOR      3
 #define PETSC_VERSION_SUBMINOR   2
 #define PETSC_VERSION_PATCH      6
-#define PETSC_VERSION_DATE       "Septenber, 1, 2006"
+#define PETSC_VERSION_DATE       "September, 1, 2006"
 #define PETSC_VERSION_PATCH_DATE "unknown"
 #define PETSC_VERSION_HG         "unknown"
 #define PETSC_AUTHOR_INFO        "\
