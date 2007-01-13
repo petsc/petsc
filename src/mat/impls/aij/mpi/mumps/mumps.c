6f8312c54f30a3f4f1438f863d9808a71b5a7ae1
Hong Zhang <hzhang@mcs.anl.gov>
2007-01-12 20:31:02 -0500

    update mumps interface to v4.6.4.
    Hg-commit: 869a3b4cb87e7d5ef2d64f65431f8adaf0c94dfc

--- src/mat/impls/aij/mpi/mumps/mumps.c
+++ src/mat/impls/aij/mpi/mumps/mumps.c
@@ -308,7 +308,11 @@ PetscErrorCode MatSolve_MUMPS(Mat A,Vec b,Vec x) {
       ierr = PetscMalloc((1+lsol_loc)*(sizeof(PetscScalar)+sizeof(PetscInt)),&sol_loc);CHKERRQ(ierr);
       lu->id.isol_loc = (PetscInt *)(sol_loc + lsol_loc);
       lu->id.lsol_loc = lsol_loc;
-      lu->id.sol_loc  = (F_DOUBLE *)sol_loc; 
+#if defined(PETSC_USE_COMPLEX)
+      lu->id.sol_loc  = (ZMUMPS_DOUBLE *)sol_loc;
+#else
+      lu->id.sol_loc  = (DMUMPS_DOUBLE *)sol_loc;
+#endif
       ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,lsol_loc,sol_loc,&lu->x_seq);CHKERRQ(ierr);
     }
   }
