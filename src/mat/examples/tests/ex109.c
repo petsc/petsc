14459f091ed32b600f4cb73c4e9a18d8907fe886
Hong Zhang <hzhang@mcs.anl.gov>
2007-03-23 17:35:53 -0400

    bugfix of MatEqual_mpidense()
    Hg-commit: 6cb1efd0ff88cb7ba2c4bc56f73d03707b4763b8

--- src/mat/examples/tests/ex109.c
+++ src/mat/examples/tests/ex109.c
@@ -11,7 +11,7 @@ int main(int argc,char **argv)
   PetscScalar    *array,*a;
   PetscErrorCode ierr;
   PetscRandom    r;
-  PetscTruth     equal;
+  PetscTruth     equal=PETSC_FALSE;
   PetscReal      fill = 1.0;
   PetscMPIInt    size;
   PetscInt       rstart,rend,nza,col,am,an,bm,bn;
