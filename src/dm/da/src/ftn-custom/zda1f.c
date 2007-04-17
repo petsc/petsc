8fb92f5daebb7fb9d86bab7b8ddc413e872a9259
Satish Balay <balay@mcs.anl.gov>
2007-04-17 17:51:09 -0500

    split up f90 interface sources into f90-custom location [for da]
    Hg-commit: f1fb0c097502e4cb58979cae2a57bc590fb990c6

--- src/dm/da/src/ftn-custom/zda1f.c
+++ src/dm/da/src/ftn-custom/zda1f.c
@@ -4,20 +4,11 @@
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
 #define dacreate1d_                  DACREATE1D
-#define dagetlocalinfof90_           DAGETLOCALINFOF90
-#define davecgetarrayf901_           DAVECGETARRAYF901
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define dacreate1d_                  dacreate1d
-#define dagetlocalinfof90_           dagetlocalinfof90
-#define davecgetarrayf901_           davecgetarrayf901
 #endif
 
 EXTERN_C_BEGIN
-
-void PETSC_STDCALL dagetlocalinfof90_(DA *da,DALocalInfo *info,PetscErrorCode *ierr)
-{
-  *ierr = DAGetLocalInfo(*da,info);
-}
 
 void PETSC_STDCALL dacreate1d_(MPI_Comm *comm,DAPeriodicType *wrap,PetscInt *M,PetscInt *w,PetscInt *s,
                  PetscInt *lc,DA *inra,PetscErrorCode *ierr)
@@ -27,72 +18,3 @@ void PETSC_STDCALL dacreate1d_(MPI_Comm *comm,DAPeriodicType *wrap,PetscInt *M,P
 }
 EXTERN_C_END
 
-#if defined(PETSC_HAVE_F90_C)
-#include "src/sys/f90/f90impl.h"
-EXTERN_C_BEGIN
-void PETSC_STDCALL davecgetarrayf901_(DA *da,Vec *v,F90Array1d *a,PetscErrorCode *ierr PETSC_F90_2PTR_PROTO(ptrd))
-{
-  PetscInt    xs,ys,zs,xm,ym,zm,gxs,gys,gzs,gxm,gym,gzm,N,dim,dof;
-  PetscScalar *aa;
-  
-  PetscFunctionBegin;
-  *ierr = DAGetCorners(*da,&xs,&ys,&zs,&xm,&ym,&zm);
-  *ierr = DAGetGhostCorners(*da,&gxs,&gys,&gzs,&gxm,&gym,&gzm);
-  *ierr = DAGetInfo(*da,&dim,0,0,0,0,0,0,&dof,0,0,0);
-
-  /* Handle case where user passes in global vector as opposed to local */
-  *ierr = VecGetLocalSize(*v,&N);
-  if (N == xm*ym*zm*dof) {
-    gxm = xm;
-    gym = ym;
-    gzm = zm;
-    gxs = xs;
-    gys = ys;
-    gzs = zs;
-  } else if (N != gxm*gym*gzm*dof) {
-    *ierr = PETSC_ERR_ARG_INCOMP;
-  }
-  *ierr = VecGetArray(*v,&aa);
-  *ierr = F90Array1dCreate(aa,PETSC_SCALAR,gxs,gxs+gxm-1,a PETSC_F90_2PTR_PARAM(ptrd));
-}
-
-void PETSC_STDCALL davecrestorearrayf901_(DA *da,Vec *v,F90Array1d *a,PetscErrorCode *ierr PETSC_F90_2PTR_PROTO(ptrd))
-{
-  *ierr = VecRestoreArray(*v,0);
-  *ierr = F90Array1dDestroy(a,PETSC_SCALAR PETSC_F90_2PTR_PARAM(ptrd));
-}
-
-void PETSC_STDCALL davecgetarrayf90user1_(DA *da,Vec *v,F90Array1d *a,PetscErrorCode *ierr PETSC_F90_2PTR_PROTO(ptrd))
-{
-  PetscInt    xs,ys,zs,xm,ym,zm,gxs,gys,gzs,gxm,gym,gzm,N,dim,dof;
-  PetscScalar *aa;
-  
-  PetscFunctionBegin;
-  *ierr = DAGetCorners(*da,&xs,&ys,&zs,&xm,&ym,&zm);
-  *ierr = DAGetGhostCorners(*da,&gxs,&gys,&gzs,&gxm,&gym,&gzm);
-  *ierr = DAGetInfo(*da,&dim,0,0,0,0,0,0,&dof,0,0,0);
-
-  /* Handle case where user passes in global vector as opposed to local */
-  *ierr = VecGetLocalSize(*v,&N);
-  if (N == xm*ym*zm*dof) {
-    gxm = xm;
-    gym = ym;
-    gzm = zm;
-    gxs = xs;
-    gys = ys;
-    gzs = zs;
-  } else if (N != gxm*gym*gzm*dof) {
-    *ierr = PETSC_ERR_ARG_INCOMP;
-  }
-  *ierr = VecGetArray(*v,&aa);
-  *ierr = F90Array1dCreate(aa,(PetscDataType)(-dof*sizeof(PetscScalar)),gxs,gxs+gxm-1,a PETSC_F90_2PTR_PARAM(ptrd));
-}
-
-void PETSC_STDCALL davecrestorearrayf90user1_(DA *da,Vec *v,F90Array1d *a,PetscErrorCode *ierr PETSC_F90_2PTR_PROTO(ptrd))
-{
-  *ierr = VecRestoreArray(*v,0);
-  *ierr = F90Array1dDestroy(a,PETSC_SCALAR PETSC_F90_2PTR_PARAM(ptrd));
-}
-
-EXTERN_C_END
-#endif
