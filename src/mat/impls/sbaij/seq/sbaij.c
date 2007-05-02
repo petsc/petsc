8608aa04b2a6dc8d52d2067b7ceb4ff36b3a4d0b
Hong Zhang <hzhang@mcs.anl.gov>
2007-05-02 11:02:51 -0500

    mv warning "MatView may not displ ..." for sbaij matrix into a lower level routine
    Hg-commit: ad473beb666b3cd784a8c6f812df1704c6b30553

--- src/mat/impls/sbaij/seq/sbaij.c
+++ src/mat/impls/sbaij/seq/sbaij.c
@@ -376,6 +376,9 @@ static PetscErrorCode MatView_SeqSBAIJ_ASCII(Mat A,PetscViewer viewer)
   } else if (format == PETSC_VIEWER_ASCII_FACTOR_INFO) {
      PetscFunctionReturn(0);
   } else {
+    if (A->factor && bs>1){
+      ierr = PetscPrintf(PETSC_COMM_SELF,"Warning: matrix is factored. MatView_SeqSBAIJ_ASCII() may not display complete or logically correct entries!\n");CHKERRQ(ierr);
+    }
     ierr = PetscViewerASCIIUseTabs(viewer,PETSC_NO);CHKERRQ(ierr);
     for (i=0; i<a->mbs; i++) {
       for (j=0; j<bs; j++) {
@@ -492,7 +495,6 @@ static PetscErrorCode MatView_SeqSBAIJ_Draw(Mat A,PetscViewer viewer)
   PetscTruth     isnull;
   
   PetscFunctionBegin; 
-  
   ierr = PetscViewerDrawGetDraw(viewer,0,&draw);CHKERRQ(ierr);
   ierr = PetscDrawIsNull(draw,&isnull);CHKERRQ(ierr); if (isnull) PetscFunctionReturn(0);
   
@@ -513,9 +515,6 @@ PetscErrorCode MatView_SeqSBAIJ(Mat A,PetscViewer viewer)
   PetscTruth     iascii,isdraw;
   
   PetscFunctionBegin;
-  if (A->factor){
-    ierr = PetscPrintf(PETSC_COMM_SELF,"Warning: matrix is factored. MatView_SeqSBAIJ() may not display complete or logically correct entries!\n");CHKERRQ(ierr);
-  }
   ierr = PetscTypeCompare((PetscObject)viewer,PETSC_VIEWER_ASCII,&iascii);CHKERRQ(ierr);
   ierr = PetscTypeCompare((PetscObject)viewer,PETSC_VIEWER_DRAW,&isdraw);CHKERRQ(ierr);
   if (iascii){
