0b3eb1e957419513d48907094119bb6a9d28823d
Satish Balay <balay@mcs.anl.gov>
2007-05-03 18:24:41 -0500

    make PETSC_VERSION macros available to fortran codes aswell
    Hg-commit: eccaaaf1e2aaa1c86dc3cd96183b3b5e9a98e064

--- include/petsc.h
+++ include/petsc.h
@@ -30,9 +30,52 @@
 #endif
 /* ========================================================================== */
 /* 
-   Current PETSc version number and release date
+   Current PETSc version number and release date. Also listed in
+    Web page
+    src/docs/tex/manual/intro.tex,
+    src/docs/tex/manual/manual.tex.
+    src/docs/website/index.html.
 */
 #include "petscversion.h"
+#define PETSC_AUTHOR_INFO        "\
+       The PETSc Team\n\
+    petsc-maint@mcs.anl.gov\n\
+ http://www.mcs.anl.gov/petsc/\n"
+#if (PETSC_VERSION_RELEASE == 1)
+#define PetscGetVersion(version,len) (PetscSNPrintf(*(version),len,"Petsc Release Version %d.%d.%d, Patch %d, ", \
+                                         PETSC_VERSION_MAJOR,PETSC_VERSION_MINOR, PETSC_VERSION_SUBMINOR, \
+                                         PETSC_VERSION_PATCH),PetscStrcat(*(version),PETSC_VERSION_PATCH_DATE), \
+                                         PetscStrcat(*(version)," HG revision: "),PetscStrcat(*(version),PETSC_VERSION_HG),0)
+#else
+#define PetscGetVersion(version,len) (PetscSNPrintf(*(version),len,"Petsc Development Version %d.%d.%d, Patch %d, ", \
+                                         PETSC_VERSION_MAJOR,PETSC_VERSION_MINOR, PETSC_VERSION_SUBMINOR, \
+                                         PETSC_VERSION_PATCH),PetscStrcat(*(version),PETSC_VERSION_PATCH_DATE), \
+                                         PetscStrcat(*(version)," HG revision: "),PetscStrcat(*(version),PETSC_VERSION_HG),0)
+#endif
+
+/*MC
+    PetscGetVersion - Gets the Petsc Version information in a string.
+
+    Output Parameter:
+.   version - version string
+
+    Input Parameter:
+.   len - length of the string
+
+    Level: developer
+
+    Usage:
+    char version[256];
+    PetscGetVersion(&version,256);
+
+    Fortran Note:
+    This routine is not supported in Fortran.
+
+.seealso: PetscGetProgramName()
+
+M*/
+
+/* ========================================================================== */
 
 /*
    Currently cannot check formatting for PETSc print statements because we have our
