f06958a467ceb8e8fab6ea6e41a92adb907af5a9
Satish Balay <balay@mcs.anl.gov>
2007-05-11 13:20:38 -0500

    change parmetis build to use makefiles
    Hg-commit: 50264a3cfe38058567eebd96b9ec9988fbb8e1d1

--- python/PETSc/packages/ParMetis.py
+++ python/PETSc/packages/ParMetis.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import PETSc.package
+import config.base
 
 class Configure(PETSc.package.Package):
   def __init__(self, framework):
     PETSc.package.Package.__init__(self, framework)
-    self.download     = ['hg://petsc.cs.iit.edu/petsc/ParMetis-dev','ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/ParMetis-dev.tar.gz']
+    self.download     = ['hg://petsc.cs.iit.edu/petsc/ParMetis-dev','ftp://ftp.mcs.anl.gov/pub/petsc/externalpackages/ParMetis-dev-p1.tar.gz']
     self.functions    = ['ParMETIS_V3_PartKway']
     self.includes     = ['parmetis.h']
     self.liblist      = [['libparmetis.a','libmetis.a']]
@@ -19,31 +20,69 @@ class Configure(PETSc.package.Package):
     return
 
   def Install(self):
-    import os, sys
-    parmetisDir = self.getDir()
-    installDir = os.path.join(parmetisDir, self.arch.arch)
+    import os
+    import sys
+    # Get the ParMetis directories
+    parmetisDir    = self.getDir()
+    installDir     = os.path.join(parmetisDir, self.arch.arch)
+    makeinc        = os.path.join(parmetisDir,'make.inc')
+    installmakeinc = os.path.join(installDir,'make.inc')
+    configheader   = os.path.join(parmetisDir,'ParMETISLib','configureheader.h')
+
+    # Configure ParMetis 
+    if os.path.isfile(makeinc):
+      os.unlink(makeinc)
+    g = open(makeinc,'w')
+    g.write('SHELL          = '+self.programs.SHELL+'\n')
+    g.write('CP             = '+self.programs.cp+'\n')
+    g.write('RM             = '+self.programs.RM+'\n')
+    g.write('MKDIR          = '+self.programs.mkdir+'\n')
+
+    g.write('AR             = '+self.setCompilers.AR+'\n')
+    g.write('ARFLAGS        = '+self.setCompilers.AR_FLAGS+'\n')
+    g.write('AR_LIB_SUFFIX  = '+self.setCompilers.AR_LIB_SUFFIX+'\n')
+    g.write('RANLIB         = '+self.setCompilers.RANLIB+'\n')
+
+    g.write('PARMETIS_ROOT  = '+parmetisDir+'\n')
+    g.write('PREFIX         = '+installDir+'\n')
+    g.write('METISLIB       = $(PARMETIS_ROOT)/libmetis.$(AR_LIB_SUFFIX)\n')
+    g.write('PARMETISLIB    = $(PARMETIS_ROOT)/libparmetis.$(AR_LIB_SUFFIX)\n')
+    
+    self.setCompilers.pushLanguage('C')
+    cflags = self.setCompilers.getCompilerFlags().replace('-Wall','').replace('-Wshadow','')
+    cflags += ' ' + self.headers.toString(self.mpi.include)+' '+self.headers.toString('.')
+        
+    g.write('CC             = '+self.setCompilers.getCompiler()+'\n')
+    g.write('CFLAGS         = '+cflags)
+    self.setCompilers.popLanguage()
+    g.close()
+
+    # Now compile & install
     if not os.path.isdir(installDir):
       os.mkdir(installDir)
-    # We could make a check of the md5 of the current configure framework
-    self.logPrintBox('Configuring and compiling ParMetis; this may take several minutes')
-    try:
-      import cPickle
-      import logger
-      # Split Graphs into its own repository
-      oldDir = os.getcwd()
-      os.chdir(parmetisDir)
-      oldLog = logger.Logger.defaultLog
-      logger.Logger.defaultLog = file(os.path.join(parmetisDir, 'build.log'), 'w')
-      mod  = self.getModule(parmetisDir, 'make')
-      make = mod.Make(configureParent = cPickle.loads(cPickle.dumps(self.framework)), module = mod)
-      make.prefix = installDir
-      make.framework.argDB['with-petsc'] = 1
-      make.builder.argDB['ignoreCompileOutput'] = 1
-      make.run()
-      del sys.modules['make']
-      logger.Logger.defaultLog = oldLog
-      os.chdir(oldDir)
-    except RuntimeError, e:
-      raise RuntimeError('Error running configure on ParMetis: '+str(e))
+    
+    if not os.path.isfile(installmakeinc) or not (self.getChecksum(installmakeinc) == self.getChecksum(makeinc)):
+      self.framework.log.write('Have to rebuild ParMetis, make.inc != '+installmakeinc+'\n')
+      self.framework.outputHeader(configheader)
+      try:
+        self.logPrintBox('Compiling & installing Parmetis; this may take several minutes')
+        output  = config.base.Configure.executeShellCommand('cd '+parmetisDir+'; make clean; make lib; make install; make clean', timeout=2500, log = self.framework.log)[0]
+      except RuntimeError, e:
+        raise RuntimeError('Error running make on ParMetis: '+str(e))
+    else:
+      self.framework.log.write('Did not need to compile downloaded ParMetis\n')
+    output  = config.base.Configure.executeShellCommand('cp -f '+makeinc+' '+installDir, timeout=5, log = self.
+framework.log)[0]
     self.framework.actions.addArgument('ParMetis', 'Install', 'Installed ParMetis into '+installDir)
-    return parmetisDir
+
+
+    return self.getDir()
+  
+if __name__ == '__main__':
+  import config.framework
+  import sys
+  framework = config.framework.Framework(sys.argv[1:])
+  framework.setup()
+  framework.addChild(Configure(framework))
+  framework.configure()
+  framework.dumpSubstitutions()
