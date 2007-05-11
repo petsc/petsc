e2bc2eb624423d91313a06b38b0183f2930cbd20
Richard Tran Mills <rmills@ornl.gov>
2007-05-11 16:57:59 -0400

    Fleshed out Barry's PetscViewerBinaryRead()/Write() for Fortran.
    Hg-commit: 07bf650768590ad8f482e5b280490a1a5f7ce900

--- src/sys/viewer/impls/binary/f90-custom/zsysiof.c
+++ src/sys/viewer/impls/binary/f90-custom/zsysiof.c
@@ -4,8 +4,18 @@
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
 #define petscviewerbinarywriteint_ PETSCVIEWERBINARYWRITEINT
+#define petscviewerbinarywritescalar_ PETSCVIEWERBINARYWRITESCALAR
+#define petscviewerbinarywritereal_ PETSCVIEWERBINARYWRITEREAL
+#define petscviewerbinaryreadint_ PETSCVIEWERBINARYREADINT
+#define petscviewerbinaryreadscalar_ PETSCVIEWERBINARYREADSCALAR
+#define petscviewerbinaryreadreal_ PETSCVIEWERBINARYREADREAL
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define petscviewerbinarywriteint_ petscviewerbinarywriteint
+#define petscviewerbinarywritescalar_ petscviewerbinarywritescalar
+#define petscviewerbinarywritereal_ petscviewerbinarywritereal
+#define petscviewerbinaryreadint_ petscviewerbinaryreadint
+#define petscviewerbinaryreadscalar_ petscviewerbinaryreadscalar
+#define petscviewerbinaryreadreal_ petscviewerbinaryreadreal
 #endif
 
 EXTERN_C_BEGIN
@@ -18,5 +28,39 @@ void PETSC_STDCALL petscviewerbinarywriteint_(PetscViewer *viewer,PetscInt *a,Pe
   *ierr = PetscViewerBinaryWrite(v,a,*len,PETSC_INT,*tmp);
 }
 
+void PETSC_STDCALL petscviewerbinarywritescalar_(PetscViewer *viewer,PetscScalar *a,PetscInt *len,PetscTruth *tmp,PetscErrorCode *ierr)
+{
+  PetscViewer v;
+  PetscPatchDefaultViewers_Fortran(viewer,v);
+  *ierr = PetscViewerBinaryWrite(v,a,*len,PETSC_SCALAR,*tmp);
+}
+
+void PETSC_STDCALL petscviewerbinarywritereal_(PetscViewer *viewer,PetscReal *a,PetscInt *len,PetscTruth *tmp,PetscErrorCode *ierr)
+{
+  PetscViewer v;
+  PetscPatchDefaultViewers_Fortran(viewer,v);
+  *ierr = PetscViewerBinaryWrite(v,a,*len,PETSC_REAL,*tmp);
+}
+
+void PETSC_STDCALL petscviewerbinaryreadint_(PetscViewer *viewer,PetscInt *a,PetscInt *len,PetscErrorCode *ierr)
+{
+  PetscViewer v;
+  PetscPatchDefaultViewers_Fortran(viewer,v);
+  *ierr = PetscViewerBinaryRead(v,a,*len,PETSC_INT);
+}
+
+void PETSC_STDCALL petscviewerbinaryreadscalar_(PetscViewer *viewer,PetscScalar *a,PetscInt *len,PetscErrorCode *ierr)
+{
+  PetscViewer v;
+  PetscPatchDefaultViewers_Fortran(viewer,v);
+  *ierr = PetscViewerBinaryRead(v,a,*len,PETSC_SCALAR);
+}
+
+void PETSC_STDCALL petscviewerbinaryreadreal_(PetscViewer *viewer,PetscReal *a,PetscInt *len,PetscErrorCode *ierr)
+{
+  PetscViewer v;
+  PetscPatchDefaultViewers_Fortran(viewer,v);
+  *ierr = PetscViewerBinaryRead(v,a,*len,PETSC_REAL);
+}
 
 EXTERN_C_END
