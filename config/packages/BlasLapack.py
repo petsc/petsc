68d62aec2e05c364c0e54e02b7dd857b4163dfdb
Satish Balay <balay@mcs.anl.gov>
2007-08-16 11:18:59 -0500

    #16573 exclude blas functions DSTEBZ, DBTTRF etc for essl
    Hg-commit: 36413ae98841efb2170354f71aa3d38e5d72dde8

--- config/packages/BlasLapack.py
+++ config/packages/BlasLapack.py
@@ -447,7 +447,7 @@ class Configure(config.package.Package):
     '''Check for missing LAPACK routines'''
     if self.foundLapack:
       mangleFunc = hasattr(self.compilers, 'FC') and not self.f2c
-      for baseName in ['gesvd','geev','getrf','potrf','getrs','potrs']:
+      for baseName in ['gesvd','geev','getrf','potrf','getrs','potrs','dstebz','dpttrf','dpttrs','dstein']:
         if self.f2c:
           routine = 'd'+baseName+'_'
         else:
