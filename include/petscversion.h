b3b7950247d1b8cd6ef04b163963a7fd1453d915
Satish Balay <balay@mcs.anl.gov>
2007-08-28 13:00:41 -0500

    Increase patchlevel to 2.3.3-p5
    Hg-commit: 6ee9657d88d643b0426f277f6a10ac17adeb195d

--- include/petscversion.h
+++ include/petscversion.h
@@ -5,7 +5,7 @@
 #define PETSC_VERSION_MAJOR      2
 #define PETSC_VERSION_MINOR      3
 #define PETSC_VERSION_SUBMINOR   3
-#define PETSC_VERSION_PATCH      4
+#define PETSC_VERSION_PATCH      5
 #define PETSC_VERSION_DATE       "May, 23, 2007"
 #define PETSC_VERSION_PATCH_DATE "unknown"
 #define PETSC_VERSION_HG         "unknown"
