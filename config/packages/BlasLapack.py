c2ea2d76bdf7ee248128277dee4d0cec29fcee5c
Satish Balay <balay@mcs.anl.gov>
2007-09-21 12:04:21 -0500

    PETSC_MISSING_LAPACK_DPTTRF etc should be PETSC_MISSING_LAPACK_PTTRF etc..
    Hg-commit: 4290ba38b3bfc496657a5018d3c25802960620d4

--- config/packages/BlasLapack.py
+++ config/packages/BlasLapack.py
@@ -447,7 +447,7 @@ class Configure(config.package.Package):
     '''Check for missing LAPACK routines'''
     if self.foundLapack:
       mangleFunc = hasattr(self.compilers, 'FC') and not self.f2c
-      for baseName in ['gesvd','geev','getrf','potrf','getrs','potrs','dstebz','dpttrf','dpttrs','dstein']:
+      for baseName in ['gesvd','geev','getrf','potrf','getrs','potrs','stebz','pttrf','pttrs','stein']:
         if self.f2c:
           routine = 'd'+baseName+'_'
         else:
