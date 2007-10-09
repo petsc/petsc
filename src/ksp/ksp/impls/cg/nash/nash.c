117f1891d93aefcd74923c7c6093df5c175dedf0
tmunson <tmunson@shepherd.mcs.anl.gov>
2007-10-09 08:23:19 -0500

    Fix for icc factorization infinite loop on some bad problems. Fix for terminations tests in conjugate gradient methods.
    Hg-commit: a53fd2c0272c3b2300f58ba5fef8b89cc2c6fa89

--- src/ksp/ksp/impls/cg/nash/nash.c
+++ src/ksp/ksp/impls/cg/nash/nash.c
@@ -249,7 +249,7 @@ PetscErrorCode KSPSolve_NASH(KSP ksp)
     PetscFunctionReturn(0);
   }
 
-  if (rz <= 0.0) {
+  if (rz < 0.0) {
     /*************************************************************************/
     /* The preconditioner is indefinite.  Because this is the first          */
     /* and we do not have a direction yet, we use the gradient step.  Note   */
@@ -525,7 +525,7 @@ PetscErrorCode KSPSolve_NASH(KSP ksp)
 
     rzm1 = rz;
     ierr = VecDot(r, z, &rz); CHKERRQ(ierr);		/* rz = r^T z        */
-    if (rz <= 0.0) {
+    if (rz < 0.0) {
       /***********************************************************************/
       /* The preconditioner is indefinite.                                   */
       /***********************************************************************/
