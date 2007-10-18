46595985e116000d63e95cdf4975b0190bc31b18
Satish Balay <balay@mcs.anl.gov>
2007-10-18 15:02:39 -0500

    update sicortex configure
    Hg-commit: bb63297ad9264d58c98a9a4565755466ca0ecf99

--- config/sicortex.py
+++ config/sicortex.py
@@ -2,9 +2,15 @@
 #
 configure_options = [
   '--with-cc=scmpicc --pathscale',
+  '--with-cxx=scmpicxx --pathscale',
   '--with-fc=scmpif90 --pathscale',
   '--LIBS=-lpathfstart -lpathfortran -lpathfstart',
   '--with-fortranlib-autodetect=0',
+
+  '--with-debugging=0',
+  'COPTFLAGS=-g -O2',
+  'FOPTFLAGS=-g -O2',
+  'CXXOPTFLAGS=-g -O2',
 
   '--with-batch=1',
   '--with-mpi-shared=0',
@@ -22,7 +28,29 @@ configure_options = [
   '--sizeof_MPI_Comm=4',
   '--sizeof_MPI_Fint=4',
 
-  '--download-f-blas-lapack=1'
+  '--download-plapack=1',
+  '--download-parmetis=1',
+  '--download-umfpack=1',
+  '--download-triangle=1',
+  '--download-spooles=1',
+  '--download-superlu=1',
+  '--download-superlu_dist=1',
+  '--download-blacs=1',
+  '--download-scalapack=1',
+  '--download-mumps=1',
+  '--download-spai=1',
+  '--download-chaco=1',
+  '--download-prometheus=1',
+
+  # PETSc configure cannot cross-compile the following packages
+  #'--download-mpe=1',
+  # '--download-fftw',
+  #'--download-hdf5=1',
+  #'--download-sundials=1',
+  #'--download-hypre=1',
+
+  # not required
+  #'--download-f-blas-lapack=1',
   ]
 
 if __name__ == '__main__':
