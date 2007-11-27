32dfa37c78aaa2e2abf35c6d7ce9bde93d182345
Lisandro Dalcin <dalcinl@gmail.com>
2007-11-27 11:03:02 -0300

    make -log_summary only show finalized events
    Hg-commit: efee9d4321b02a191650c490f271026566088af5

--- src/sys/plog/plog.c
+++ src/sys/plog/plog.c
@@ -1483,7 +1483,7 @@ PetscErrorCode PETSC_DLLEXPORT PetscLogPrintSummary(MPI_Comm comm, const char fi
     }
     ierr = MPI_Allreduce(&localNumEvents, &numEvents, 1, MPI_INT, MPI_MAX, comm);CHKERRQ(ierr);
     for(event = 0; event < numEvents; event++) {
-      if (localStageUsed[stage] && (event < stageLog->stageInfo[stage].eventLog->numEvents)) {
+      if (localStageUsed[stage] && (event < stageLog->stageInfo[stage].eventLog->numEvents) && (eventInfo[event].depth == 0)) {
         if ((eventInfo[event].count > 0) && (eventInfo[event].time > 0.0)) {
           flopr = eventInfo[event].flops/eventInfo[event].time;
         } else {
@@ -1517,7 +1517,7 @@ PetscErrorCode PETSC_DLLEXPORT PetscLogPrintSummary(MPI_Comm comm, const char fi
         name = "";
       }
       if (mint < 0.0) {
-        ierr = PetscFPrintf(comm, fd, "WARNING!!! Minimum time %g over all processors for %s is negative! This happens\n on some machines whose times cannot handle to rapid calls.!\n artificially changing minimum to zero.",mint,name);
+        ierr = PetscFPrintf(comm, fd, "WARNING!!! Minimum time %g over all processors for %s is negative! This happens\n on some machines whose times cannot handle too rapid calls.!\n artificially changing minimum to zero.\n",mint,name);
         mint = 0;
       }
       if (minf < 0.0) SETERRQ2(PETSC_ERR_PLIB,"Minimum flops %g over all processors for %s is negative! Not possible!",minf,name);
