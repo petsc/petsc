6be210a0f47355b0dfd9cc21d08d6d275656bc1b
Barry Smith <bsmith@mcs.anl.gov>
2008-01-08 16:21:35 -0600

    more java junk
    Hg-commit: b095404300cfffb003524fcca14502a4ddfb8c1a

--- bin/maint/generators/java.py
+++ bin/maint/generators/java.py
@@ -18,6 +18,19 @@ import sys
 from string import *
 import pickle
 
+def replace(enums,senums,structs,aliases,classes,k):
+  k = k.replace("*","")
+  for l in aliases: k = k.replace(l,aliases[l])
+  k = k.replace("char[]","String").replace("char*","String").replace("char","String");
+  for l in senums: k = k.replace(l,"String")
+  k = k.replace("void","int")
+  k = k.replace("PetscTruth","boolean")
+  k = k.replace("*","")
+  k = k.replace("unsigned","")
+  k = k.replace("const","")
+  k = k.replace("ushort","short")
+  k = k.replace("const ","").replace("unsigned long","ulong")
+  return k
 
 def main(args):
   file = open('classes.data')
@@ -26,7 +39,8 @@ def main(args):
   structs = pickle.load(file)    
   aliases = pickle.load(file)  
   classes = pickle.load(file)
-  outfile = open('petsc.java','w')
+  if not os.path.isdir('src/java'): os.mkdir('src/java')
+  outfile = open('src/java/petsc.java','w')
 
   for i in enums:
     outfile.write("enum "+i+"\n")
@@ -36,9 +50,10 @@ def main(args):
       outfile.write("    "+j.replace("=","(")+")")
       cnt = cnt + 1
       if not cnt == len(enums[i]): outfile.write(",")
+      else: outfile.write(";")      
       outfile.write("\n")
-    outfile.write(";private final int value;")
-    outfile.write(i+"(int v) { this.value = v; }")
+    outfile.write("    private final int value;\n")
+    outfile.write("    "+i+"(int v) { this.value = v; }\n")
     outfile.write("}\n")      
   outfile.write("\n")
 
@@ -46,16 +61,7 @@ def main(args):
     outfile.write("class "+i+"\n")
     outfile.write("{\n")
     for k in structs[i]:
-      k = k.replace("*","")
-      for l in aliases: k = k.replace(l,aliases[l])
-      k = k.replace("char[]","String").replace("char*","String").replace("char","String");
-      for l in senums: k = k.replace(l,"String")
-      k = k.replace("void","int")
-      k = k.replace("PetscTruth","boolean")
-      k = k.replace("*","")
-      k = k.replace("unsigned","")
-      k = k.replace("const","")
-      k = k.replace("ushort","short")                    
+      k = replace(enums,senums,structs,aliases,classes,k)
       outfile.write("    "+k+";\n")
     outfile.write("};\n")      
   outfile.write("\n")
@@ -64,25 +70,51 @@ def main(args):
     outfile.write("class "+i+"\n")
     outfile.write("{\n")
     for j in classes[i]:
-      outfile.write(" void "+j+"(")
+      outfile.write("native void _"+j+"();\n")
+    for j in classes[i]:
+      outfile.write("    void "+j+"(")
       cnt = 0
       for k in classes[i][j]:
-        k = k.replace("*","")
-        for l in aliases: k = k.replace(l,aliases[l])
-        k = k.replace("char[]","String").replace("char*","String").replace("char","String");
-        for l in senums: k = k.replace(l,"String")
-        k = k.replace("void","int")
-        k = k.replace("PetscTruth","boolean")
-        k = k.replace("*","")
-        k = k.replace("unsigned","")
-        k = k.replace("ushort","short")                            
+        k = replace(enums,senums,structs,aliases,classes,k)
         if cnt > 0:
-          outfile.write(k.replace("const ","").replace("unsigned long","ulong"))
+          outfile.write(k)
           outfile.write(" a"+str(cnt))
           if cnt < len(classes[i][j])-1: outfile.write(",")
         cnt = cnt + 1
-      outfile.write("){;}\n")
-    outfile.write("}\n")        
+      outfile.write("){_"+j+"(")
+      outfile.write(");}\n")      
+    outfile.write("    int ptr;\n")
+    if i == "Petsc":
+      outfile.write('static {System.loadLibrary("petsc");}\n')
+    outfile.write("}\n")
+  outfile.close()
+  
+  os.system("cd src/java; javac petsc.java")
+  for i in classes:
+    os.system("cd src/java; javah "+i+";cp "+i+".h "+i+".c")
+    f = open('src/java/'+i+".c","r")
+    w = f.read()
+    f.close()
+    w = w.replace("  (JNIEnv *, jobject);","  (JNIEnv *a1, jobject a2) {;}; ")
+    w = w.replace("#include <jni.h>",'#include "'+i+'.h"')
+    w = w.replace("#ifndef _Included_","#ifndef _string_that_does_not_exist_")
+    w = w.replace("#define _Included_","#define _another_string_that_does_not_exist_")
+    w = w.replace("#include <jni.h>","#include <JavaVM/jni.h>")
+    f = open('src/java/'+i+".c","w")    
+    f.write(w)
+    f.close()
+    f = open('src/java/'+i+".h","r")
+    w = f.read()
+    f.close()
+    w = w.replace("#include <jni.h>","#include <JavaVM/jni.h>")
+    f = open('src/java/'+i+".h","w")    
+    f.write(w)
+    f.close()
+    os.system("cd src/java; gcc -c -fPIC "+i+".c")
+
+  os.system("cd src/java; gcc -dynamiclib -single_module -multiply_defined suppress -undefined dynamic_lookup -framework JavaVM -o petsc.dylib *.o")    
+
+    
   
     
 #
