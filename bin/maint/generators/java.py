c49f483660db653e47ea87744b883f602ef6b633
Barry Smith <bsmith@mcs.anl.gov>
2008-01-10 16:36:51 -0600

    compile and run dummy PETSc java program
    Hg-commit: 0752f118eac3bdc9d0bbeb336796dc44c92f1d69

--- bin/maint/generators/java.py
+++ bin/maint/generators/java.py
@@ -42,6 +42,7 @@ def main(args):
   if not os.path.isdir('src/java'): os.mkdir('src/java')
   outfile = open('src/java/petsc.java','w')
 
+  notclasses = ['PetscMalloc','PetscStr','PetscDLLibrary','PetscFList']
   for i in enums:
     outfile.write("enum "+i+"\n")
     outfile.write("{\n")
@@ -67,11 +68,16 @@ def main(args):
   outfile.write("\n")
 
   for i in classes:
+    if i in notclasses: continue
     outfile.write("class "+i+"\n")
     outfile.write("{\n")
     for j in classes[i]:
+      if not classes[i][j] or not classes[i][j][0] == i:
+        outfile.write("  static ")      
       outfile.write("native void _"+j+"();\n")
     for j in classes[i]:
+      if not classes[i][j] or not classes[i][j][0] == i:
+        outfile.write("  static ")      
       outfile.write("    void "+j+"(")
       cnt = 0
       for k in classes[i][j]:
@@ -90,19 +96,26 @@ def main(args):
   outfile.close()
   
   os.system("cd src/java; javac petsc.java")
+  import re
   for i in classes:
+    if i in notclasses: continue
     os.system("cd src/java; javah "+i+";cp "+i+".h "+i+".c")
     f = open('src/java/'+i+".c","r")
     w = f.read()
     f.close()
-    w = w.replace("  (JNIEnv *, jobject);","  (JNIEnv *a1, jobject a2) {;}; ")
+#    w = w.replace("  (JNIEnv *, jobject);","  (JNIEnv *a1, jobject a2) {;}; ")
+#    w = w.replace("  (JNIEnv *, jclass);","  (JNIEnv *a1, jclass a2) {;}; ")    
     w = w.replace("#include <jni.h>",'#include "'+i+'.h"')
     w = w.replace("#ifndef _Included_","#ifndef _string_that_does_not_exist_")
     w = w.replace("#define _Included_","#define _another_string_that_does_not_exist_")
     w = w.replace("#include <jni.h>","#include <JavaVM/jni.h>")
+    for j in classes[i]:    
+#      w = re.sub('JNIEXPORT void JNICALL Java_'+i+'([_0-9]*)'+j+'\n','JNIEXPORT void JNICALL Java_'+i+'\\1'+j+' ',w)
+      w = re.sub('JNIEXPORT void JNICALL Java_'+i+'([_0-9]*)'+j+'\n [ ]* \(JNIEnv \*, ([a-z]*)\)','JNIEXPORT void JNICALL Java_'+i+'\\1'+j+' (JNIEnv *a1,\\2 a2) {'+i+j+'();}',w)
     f = open('src/java/'+i+".c","w")    
     f.write(w)
     f.close()
+    
     f = open('src/java/'+i+".h","r")
     w = f.read()
     f.close()
@@ -112,7 +125,7 @@ def main(args):
     f.close()
     os.system("cd src/java; gcc -c -fPIC "+i+".c")
 
-  os.system("cd src/java; gcc -dynamiclib -single_module -multiply_defined suppress -undefined dynamic_lookup -framework JavaVM -o petsc.dylib *.o")    
+  os.system("cd src/java; gcc -dynamiclib -single_module -multiply_defined suppress -undefined dynamic_lookup -framework JavaVM -o libpetsc.dylib *.o ${PETSC_DIR}/${PETSC_ARCH}/lib/libpetscts.dylib")    
 
     
   
