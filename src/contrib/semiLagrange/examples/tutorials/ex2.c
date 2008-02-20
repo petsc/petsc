17a1b3d386f90e00a1e517e6df6b9d515592bfec
Matthew Knepley <knepley@mcs.anl.gov>
2008-02-20 17:33:56 -0600

    Rich's correction for semiLagrange example
    Hg-commit: f8a285a88eac7860b7da247bd37cce446a8162d2

--- src/contrib/semiLagrange/examples/tutorials/ex2.c
+++ src/contrib/semiLagrange/examples/tutorials/ex2.c
@@ -11,8 +11,7 @@ static char help[] = "\n";
 #include "petscda.h"
 #include "petscdmmg.h"
 #include "petscbag.h"
-#include "characteristic.h"
-#include "PetscSimOutput.h"
+#include "src/contrib/semiLagrange/characteristic.h"
 
 #define SHEAR_CELL     0
 #define SOLID_BODY     1
@@ -436,11 +435,11 @@ int DoOutput(DMMG *dmmg, int n_plot)
     ierr = PetscPrintf(PETSC_COMM_WORLD,"file = \"%s\"\n",filename);
 
     /* make output files */
-    ierr = PetscWriteOutputInitialize(PETSC_COMM_WORLD,filename,&viewer);CHKERRQ(ierr);
-    ierr = PetscWriteOutputBag(viewer,"par",user->bag);CHKERRQ(ierr);
+    ierr = PetscViewerBinaryMatlabOpen(PETSC_COMM_WORLD,filename,&viewer);CHKERRQ(ierr);
+    ierr = PetscViewerBinaryMatlabOutputBag(viewer,"par",user->bag);CHKERRQ(ierr);
     ierr = DASetFieldNames("u","v","c1","c2","c3","c4",da);CHKERRQ(ierr);
-    ierr = PetscWriteOutputVecDA(viewer,"field",DMMGGetx(dmmg),da);CHKERRQ(ierr);
-    ierr = PetscWriteOutputFinalize(viewer);CHKERRQ(ierr);
+    ierr = PetscViewerBinaryMatlabOutputVecDA(viewer,"field",DMMGGetx(dmmg),da);CHKERRQ(ierr);
+    ierr = PetscViewerBinaryMatlabDestroy(viewer);CHKERRQ(ierr);
   }  
   return 0;
 }
