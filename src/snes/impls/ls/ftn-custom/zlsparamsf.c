c966f8584065e60fcf911fa65a0e9fcdd9215558
Satish Balay <balay@mcs.anl.gov>
2008-02-29 10:22:55 -0600

    fix SNESLineSearchGetParams() fortran interface compile
    Hg-commit: 6809c8c5cd04d680f06c484c6aab33f5d684eaab

--- src/snes/impls/ls/ftn-custom/zlsparamsf.c
+++ src/snes/impls/ls/ftn-custom/zlsparamsf.c
@@ -8,12 +8,11 @@
 #endif
 
 EXTERN_C_BEGIN
-void PETSC_STDCALL sneslinesearchgetparams_(SNES *snes,PetscReal *alpha,PetscReal *maxstep,PetscReal *steptol,PetscErrorCode *ierr)
+void PETSC_STDCALL sneslinesearchgetparams_(SNES *snes,PetscReal *alpha,PetscReal *maxstep,PetscErrorCode *ierr)
 {
   CHKFORTRANNULLREAL(alpha);
   CHKFORTRANNULLREAL(maxstep);
-  CHKFORTRANNULLREAL(steptol);
-  *ierr = SNESLineSearchGetParams(*snes,alpha,maxstep,steptol);
+  *ierr = SNESLineSearchGetParams(*snes,alpha,maxstep);
 }
 
 
