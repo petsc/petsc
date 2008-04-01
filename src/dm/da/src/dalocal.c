791519ede46122db7333be1fe5c4e2ba01de420e
Satish Balay <balay@mcs.anl.gov>
2008-04-01 16:34:35 -0500

    Add fortran interface for macros:   DAGetLocalVector   DARestoreLocalVector   DAGetGlobalVector   DARestoreGlobalVector
    Hg-commit: 140fdaab245b918a8161c6758002c38fef53747b

--- src/dm/da/src/dalocal.c
+++ src/dm/da/src/dalocal.c
@@ -1279,3 +1279,34 @@ PetscErrorCode PETSCDM_DLLEXPORT admf_DARestoreArray(DA da,PetscTruth ghosted,vo
   ierr = DARestoreAdicMFArray(da,ghosted,iptr,0,0);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
+
+/*M
+  DAGetLocalVector - same as DMGetLocalVector
+
+  Synopsis:
+  PetscErrorCode DAGetLocalVector(DM dm,Vec* g)
+M*/
+
+
+/*M
+  DARestoreLocalVector - same as DMRestoreLocalVector
+
+  Synopsis:
+  PetscErrorCode DARestoreLocalVector(DM dm,Vec* g)
+M*/
+
+
+/*M
+  DAGetGlobalVector - same as DMGetGlobalVector
+
+  Synopsis:
+  PetscErrorCode  DAGetGlobalVector(DM dm,Vec* g)
+M*/
+
+
+/*M
+  DARestoreGlobalVector - same as DMRestoreGlobalVector
+
+  Synopsis:
+  PetscErrorCode DARestoreGlobalVector(DM dm,Vec* g)
+M*/
