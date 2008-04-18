59f1c12064a33a76d80262e58c3b34302ddc6dfb
Satish Balay <balay@mcs.anl.gov>
2008-04-18 11:39:35 -0500

    remove obsolete comments about pic
    Hg-commit: d37340abae17affa1ffcbd65522f55b6edc067cd

--- config/linux-gnu-ia64-intel.py
+++ config/linux-gnu-ia64-intel.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 
-# Force parmetis to not build sharedlibs on IA64 by using --with-pic=0
-# [as it doesn't explicitly check if mpich libraries are built with
-# -fPIC flag
-
-#  **** --with-pic=0 does not work anymore ***
-#  so mpich is built with -fPIC flag - so that parmetis gets built.
-
 configure_options = [
   '--with-vendor-compilers=intel',
   '--with-mpi-dir=/home/balay/soft/mpich2-1.0.4-intel', 
   '--download-parmetis=1',
-  '--with-pic=0'
   ]
 
 if __name__ == '__main__':
