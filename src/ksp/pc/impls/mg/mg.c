797d2ccb3b237478331569bdd2b9f5105cfa5247
Barry Smith <bsmith@mcs.anl.gov>
2008-06-19 15:38:12 -0500

    over reference counted vector in PCMG
    Hg-commit: 931674f91712db08278257e6ef13c4d3ed8432f5

--- src/ksp/pc/impls/mg/mg.c
+++ src/ksp/pc/impls/mg/mg.c
@@ -474,6 +474,7 @@ static PetscErrorCode PCSetUp_MG(PC pc)
       Vec *vec;
       ierr = KSPGetVecs(mg[n-1]->smoothd,1,&vec,0,PETSC_NULL);CHKERRQ(ierr);
       ierr = PCMGSetR(pc,n-1,*vec);CHKERRQ(ierr);
+      ierr = VecDestroy(*vec);CHKERRQ(ierr);
       ierr = PetscFree(vec);CHKERRQ(ierr);
     }
   }
