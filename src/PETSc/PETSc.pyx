3c91cf59d58bef04a5e76b7192a1e82e0c3880ea
Lisandro Dalcin <dalcinl@gmail.com>
2008-09-17 16:15:14 -0300

    Module docstring have to be unicode

--- src/PETSc/PETSc.pyx
+++ src/PETSc/PETSc.pyx
@@ -95,8 +95,9 @@ include "petscda.pxi"
 
 # --------------------------------------------------------------------
 
-__doc__ = \
-"""Portable, Extensible Toolkit for Scientific Computation"""
+__doc__ = u"""
+Portable, Extensible Toolkit for Scientific Computation
+"""
 
 include "Const.pyx"
 include "Error.pyx"
