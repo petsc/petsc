89b077602a54e3a6c672b35ed967ba62737b71ff
Satish Balay <balay@mcs.anl.gov>
2008-10-16 02:27:38 -0500

    #18429 prefer vsnprintf() over _vsnprintf()
    Hg-commit: 2d57fbeacc0d7b30b3f0d717a22f5e12ff02c9d5

--- config/PETSc/Configure.py
+++ config/PETSc/Configure.py
@@ -80,7 +80,7 @@ class Configure(config.base.Configure):
                  'gettimeofday', 'getwd', 'memalign', 'memmove', 'mkstemp', 'popen', 'PXFGETARG', 'rand', 'getpagesize',
                  'readlink', 'realpath',  'sigaction', 'signal', 'sigset', 'sleep', '_sleep', 'socket', 'times', 'gethostbyname',
                  'uname','snprintf','_snprintf','_fullpath','lseek','_lseek','time','fork','stricmp','strcasecmp','bzero','dlerror',
-                 '_intel_fast_memcpy','_intel_fast_memset','isinf','isnan','_finite','_isnan','_vsnprintf']
+                 '_intel_fast_memcpy','_intel_fast_memset','isinf','isnan','_finite','_isnan','vsnprintf','_vsnprintf']
     libraries1 = [(['socket', 'nsl'], 'socket'), (['fpe'], 'handle_sigfpes')]
     self.headers.headers.extend(headersC)
     self.functions.functions.extend(functions)
