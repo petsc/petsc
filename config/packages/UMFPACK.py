1c406e5101cdf6ea163a9624f5ec427ac4d6e326
Satish Balay <balay@mcs.anl.gov>
2008-10-28 16:26:10 -0500

    get umfpack compiling on BGP
    Hg-commit: 863ad8da1d08c4797b1abff0e795bac24741dd71

--- config/packages/UMFPACK.py
+++ config/packages/UMFPACK.py
@@ -28,14 +28,24 @@ class Configure(config.package.Package):
     g = open(os.path.join(self.packageDir, mkfile), 'w')
     self.setCompilers.pushLanguage('C')
     g.write('CC           = '+self.setCompilers.getCompiler()+'\n')
-    g.write('CFLAGS       = '+self.setCompilers.getCompilerFlags()+''' -DUF_long="long long" -DUF_long_max=LONG_LONG_MAX -DUF_long_id='"%lld"' \n''')
+    if self.checkCompile('#ifdef PETSC_HAVE_LIMITS_H\n  #include <limits.h>\n#endif\n', 'long long i=ULONG_MAX;\n\nif (i);\n'):
+      ulong_max = 'ULONG_MAX'
+    else:
+      ulong_max = '9223372036854775807LL'
+    g.write('CFLAGS       = '+self.setCompilers.getCompilerFlags()+''' -DUF_long="long long" -DUF_long_max=''' + ulong_max + ''' -DUF_long_id='"%lld"' \n''')
     self.setCompilers.popLanguage()
     g.write('RANLIB       = '+self.setCompilers.RANLIB+'\n')
     g.write('AR = ar cr\n')
     g.write('RM = rm -f\n')
     g.write('MV = mv -f\n')
     g.write('BLAS      = '+self.libraries.toString(self.blasLapack.dlib)+'\n')
-    g.write('UMFPACK_CONFIG =\n')
+    if self.blasLapack.mangling == 'underscore':
+      flg = ''
+    elif self.blasLapack.mangling == 'caps':
+      flg = '-DBLAS_CAPS_DOES_NOT_WORK'
+    else:
+      flg = '-DBLAS_NO_UNDERSCORE'
+    g.write('UMFPACK_CONFIG   = '+flg+'\n')
     g.write('CLEAN = *.o *.obj *.ln *.bb *.bbg *.da *.tcov *.gcov gmon.out *.bak *.d\n')
     g.close()
     
