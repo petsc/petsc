24a64cb28e8af1abbb28126b36685d8f9f1f8c20
Dmitry Karpeev <karpeev@mcs.anl.gov>
2008-11-05 16:55:14 -0600

    Minor improvement to basis USFFT test.
    Hg-commit: 464485d7d0f571b34004fb90bf46b5284a06e745

--- src/dm/da/examples/tests/ex27.c
+++ src/dm/da/examples/tests/ex27.c
@@ -15,10 +15,10 @@ PetscInt main(PetscInt argc,char **args)
 {
   typedef enum {RANDOM, CONSTANT, TANH, NUM_FUNCS} FuncType;
   const char    *funcNames[NUM_FUNCS] = {"random", "constant", "tanh"};
-  Mat            A;    
+  Mat            A, AA;    
   PetscMPIInt    size;
   PetscInt       n = 10,N,dim[3],i, stencil=1,dof=1;
-  Vec            x,y,z;
+  Vec            x,y,z,yy,zz;
   PetscScalar    s;  
   PetscRandom    rdm;
   PetscReal      enorm;
@@ -53,15 +53,19 @@ PetscInt main(PetscInt argc,char **args)
   ierr = DAGetGlobalVector(da, &x); CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject) x, "Real space vector");CHKERRQ(ierr);
   ierr = DAGetGlobalVector(da, &y); CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject) y, "Frequency space vector");CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) y, "USFFT frequency space vector");CHKERRQ(ierr);
+  ierr = DAGetGlobalVector(da, &yy); CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) yy, "FFTW frequency space vector");CHKERRQ(ierr);
   ierr = DAGetGlobalVector(da, &z); CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject) z, "Reconstructed vector");CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) z, "USFFT reconstructed vector");CHKERRQ(ierr);
+  ierr = DAGetGlobalVector(da, &zz); CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) zz, "FFTW reconstructed vector");CHKERRQ(ierr);
 
   for(i = 0, N = 1; i < 3; i++) {
     N *= dim[i];
   }
 
-  ierr = PetscPrintf(PETSC_COMM_SELF, "\n %3-D: USFFT on vector of total size %d \n",N);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_SELF, "%3-D: USFFT on vector of total size %d \n",N);CHKERRQ(ierr);
 
   
   if (function == RANDOM) {
@@ -91,6 +95,8 @@ PetscInt main(PetscInt argc,char **args)
   
   /* create USFFT object */
   ierr = MatCreateSeqUSFFT(da,da,&A);CHKERRQ(ierr);
+  /* create FFTW object */
+  ierr = MatCreateSeqFFTW(PETSC_COMM_SELF,1,&N,&AA);CHKERRQ(ierr);
   
   /* apply USFFT_FORWARD several times, so the fftw_plan can be reused on different vectors */
   ierr = MatMult(A,x,z);CHKERRQ(ierr);
@@ -101,6 +107,22 @@ PetscInt main(PetscInt argc,char **args)
     ierr = MatMultTranspose(A,y,z);CHKERRQ(ierr);
   }
   
+  /* compare y and yy: USFFT and FFTW results*/
+  ierr = VecAXPY(y,-1.0,yy);CHKERRQ(ierr);
+  ierr = VecNorm(y,NORM_1,&enorm);CHKERRQ(ierr);
+  if (enorm > 1.e-11){
+    ierr = PetscPrintf(PETSC_COMM_SELF,"  Error norm of |y - yy| %A\n",enorm);CHKERRQ(ierr);
+  }
+  ierr = PetscPrintf(PETSC_COMM_SELF, "|y-yy| = %g\n",enorm);CHKERRQ(ierr);
+  
+  /* compare z and zz: USFFT and FFTW results*/
+  ierr = VecAXPY(z,-1.0,zz);CHKERRQ(ierr);
+  ierr = VecNorm(z,NORM_1,&enorm);CHKERRQ(ierr);
+  if (enorm > 1.e-11){
+    ierr = PetscPrintf(PETSC_COMM_SELF,"  Error norm of |z - zz| %A\n",enorm);CHKERRQ(ierr);
+  }
+  ierr = PetscPrintf(PETSC_COMM_SELF, "|z-zz| = %g\n",enorm);CHKERRQ(ierr);
+  
   /* compare x and z. USFFT computes an unnormalized DFT, thus z = N*x */
   s = 1.0/(PetscReal)N;
   ierr = VecScale(z,s);CHKERRQ(ierr);
@@ -110,12 +132,14 @@ PetscInt main(PetscInt argc,char **args)
   if (enorm > 1.e-11){
     ierr = PetscPrintf(PETSC_COMM_SELF,"  Error norm of |x - z| %A\n",enorm);CHKERRQ(ierr);
   }
-  
+  ierr = PetscPrintf(PETSC_COMM_SELF, "|x-z| = %g\n",enorm);CHKERRQ(ierr);
+
   /* free spaces */
-  ierr = VecDestroy(x);CHKERRQ(ierr);
-  ierr = VecDestroy(y);CHKERRQ(ierr);
-  ierr = VecDestroy(z);CHKERRQ(ierr);
-  ierr = MatDestroy(A);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da,&x);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da,&y);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da,&yy);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da,&z);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da,&zz);CHKERRQ(ierr);
 
   ierr = PetscFinalize();CHKERRQ(ierr);
   return 0;
