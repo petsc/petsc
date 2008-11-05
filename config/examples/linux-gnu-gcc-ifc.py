ea82b4aa0f56b353ddeb53beae329b1828a9df07
Satish Balay <balay@mcs.anl.gov>
2008-11-05 16:03:49 -0600

    fix pythonpath for example config scripts
    Hg-commit: b324d1bfb38a4a2b7a71c383ccc4e0f1a1d88db0

--- config/examples/linux-gnu-gcc-ifc.py
+++ config/examples/linux-gnu-gcc-ifc.py
@@ -12,5 +12,7 @@ configure_options = [
   ]
 
 if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
   import configure
   configure.petsc_configure(configure_options)
--- config/examples/solaris2.9.py
+++ config/examples/solaris2.9.py
@@ -8,5 +8,7 @@ configure_options = [
   ]
 
 if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
   import configure
   configure.petsc_configure(configure_options)
