44ea04b1bf6a54c78b65c09b610f4613e88b8041
Satish Balay <balay@mcs.anl.gov>
2008-11-10 17:31:47 -0600

    mumps complex compile fix
    Hg-commit: 699b6b5e3c3fd5b96b31b1558536ed1853e21bd7

--- src/mat/impls/aij/mpi/mumps/mumps.c
+++ src/mat/impls/aij/mpi/mumps/mumps.c
@@ -223,7 +223,11 @@ PetscErrorCode MatSolve_MUMPS(Mat A,Vec b,Vec x)
       ierr = PetscMalloc((1+lsol_loc)*(sizeof(PetscScalar)+sizeof(PetscInt)),&sol_loc);CHKERRQ(ierr);
       lu->id.isol_loc = (PetscInt *)(sol_loc + lsol_loc);
       lu->id.lsol_loc = lsol_loc;
-      lu->id.sol_loc  = (PetscScalar *)sol_loc;
+#if defined(PETSC_USE_COMPLEX)
+      lu->id.sol_loc  = (mumps_double_complex*)sol_loc;
+#else
+      lu->id.sol_loc  = sol_loc;
+#endif
       ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,lsol_loc,sol_loc,&lu->x_seq);CHKERRQ(ierr);
     }
   }
