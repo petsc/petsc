a12853b4559fd103c6ea3c5d226211555d77915f
Barry Smith <bsmith@mcs.anl.gov>
2008-11-12 11:26:28 -0600

    fixed typo in axpy.h of PetscBlasInt
    Hg-commit: e6639ef730ecf5007f7d09f46e72a055d6ce3d9d

--- src/inline/axpy.h
+++ src/inline/axpy.h
@@ -85,7 +85,7 @@ EXTERN void fortranmaxpy3_(void*,void*,void*,void*,void*,void*,void*,PetscInt*);
 EXTERN void fortranmaxpy2_(void*,void*,void*,void*,void*,PetscInt*);
 EXTERN_C_END
 
-#define APXY(U,a1,p1,n)  {PetscBLASInt one=1; PetscBlasInt nn = (PetscBlasInt) n; \
+#define APXY(U,a1,p1,n)  {PetscBLASInt one=1; PetscBLASInt nn = (PetscBLASInt) n; \
   BLASaxpy_(&nn,&a1,p1,&one,U,&one);}
 #define APXY2(U,a1,a2,p1,p2,n) { \
   fortranmaxpy2_(U,&a1,&a2,p1,p2,&n);}
@@ -144,7 +144,7 @@ EXTERN_C_END
 
 #elif defined(PETSC_USE_BLAS_KERNELS)
 
-#define APXY(U,a1,p1,n)  {PetscBLASInt one=1; PetscBlasInt nn = (PetscBlasInt) n;\
+#define APXY(U,a1,p1,n)  {PetscBLASInt one=1; PetscBLASInt nn = (PetscBLASInt) n;\
   BLASaxpy_(&nn,&a1,p1,&one,U,&one);}
 #define APXY2(U,a1,a2,p1,p2,n){APXY(U,a1,p1,n);\
   APXY(U,a2,p2,n);}
