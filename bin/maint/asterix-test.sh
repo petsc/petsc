de7b8f038da2a8453d42f5782eb035840c2f3031
Barry Smith <bsmith@mcs.anl.gov>
2008-12-02 22:44:49 -0600

    added tests_DATAFILESPATH and the variable testexamples_DATAFILESPATH to handle all the examples that use the matrix files
    Hg-commit: c37110e4b4503f02823ae381a742b0e6a545e769

--- bin/maint/asterix-test.sh
+++ bin/maint/asterix-test.sh
@@ -3,8 +3,8 @@ set -x
 
 # basic gcc tests
 ./config/asterix.py
-make PETSC_ARCH=asterix all alltests DATAFILESPATH=/home/balay/datafiles 
-make PETSC_ARCH=asterix tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_C_NoComplex
+make PETSC_ARCH=asterix all alltests tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles 
+make PETSC_ARCH=asterix tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles
 
 ./config/configure.py  CC=gcc FC=gfortran CXX=g++ PETSC_ARCH=asterix-cxx-sieve --with-clanguage=cxx \
 --with-sieve=1 --download-mpich=1 --download-boost=1 --download-chaco=1 \
@@ -12,13 +12,13 @@ make PETSC_ARCH=asterix tree DATAFILESPATH=/home/balay/datafiles ACTION=testexam
 make PETSC_ARCH=asterix-cxx-sieve all testexamples testfortran
 
 ./config/asterix-openmpi.py
-make PETSC_ARCH=asterix-openmpi all alltests DATAFILESPATH=/home/balay/datafiles 
+make PETSC_ARCH=asterix-openmpi all alltests tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles 
 
 ./config/asterix.py --with-clanguage=cxx -PETSC_ARCH=asterix-cxx-opt --with-debugging=0
 make PETSC_ARCH=asterix-cxx-opt all test
 ./config/asterix.py --with-scalar-type=complex --with-clanguage=cxx -PETSC_ARCH=asterix-complex
 make PETSC_ARCH=asterix-complex all test
-make PETSC_ARCH=asterix-complex testexamples testfortran 
+make PETSC_ARCH=asterix-complex testexamples testfortran tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles 
 make PETSC_ARCH=asterix-complex tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_C_X11
 make PETSC_ARCH=asterix-complex tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_C_Complex
 make PETSC_ARCH=asterix-complex tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_Fortran_Complex
@@ -27,7 +27,7 @@ make PETSC_ARCH=asterix-complex tree DATAFILESPATH=/home/balay/datafiles ACTION=
 --with-dscpack=1 --with-dscpack-include=/home/balay/soft/linux-fc/DSCPACK1.0/DSC_LIB \
 --with-dscpack-lib=/home/balay/soft/linux-fc/DSCPACK1.0/DSC_LIB/dsclibdbl.a \
 --download-umfpack=1
-make PETSC_ARCH=asterix-prometheus all test
+make PETSC_ARCH=asterix-prometheus all test tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles
 make PETSC_ARCH=asterix-prometheus tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_PARMETIS
 
 # basic intel tests
@@ -35,7 +35,7 @@ make PETSC_ARCH=asterix-prometheus tree DATAFILESPATH=/home/balay/datafiles ACTI
 --download-superlu_dist=1 --download-hypre=1 --download-spai=1 --download-blacs=1 --download-scalapack=1 \
 --download-mumps=1 --download-mpe=1 --download-sundials=1 LIBS=/usr/lib/libm.a
 make PETSC_ARCH=asterix-intel-opt all test
-make PETSC_ARCH=asterix-intel-opt testexamples testfortran
+make PETSC_ARCH=asterix-intel-opt testexamples testfortran tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles
 make PETSC_ARCH=asterix-intel-opt tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_F90
 make PETSC_ARCH=asterix-intel-opt tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_SUPERLU
 make PETSC_ARCH=asterix-intel-opt tree DATAFILESPATH=/home/balay/datafiles ACTION=testexamples_SUPERLU_DIST
@@ -56,13 +56,13 @@ make PETSC_ARCH=asterix-intel-cxx-prometheus all test
 #make PETSC_ARCH=asterix-tops all test
 
 ./config/asterix-c89.py
-make PETSC_ARCH=asterix-c89 all test alltests DATAFILESPATH=/home/balay/datafiles
+make PETSC_ARCH=asterix-c89 all test alltests tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles
 
 # basic sun tests
 ./config/asterix-sun.py
 make PETSC_ARCH=asterix-sun all test
 ./config/asterix-sun.py --with-shared=1 --with-dynamic=1 -PETSC_ARCH=asterix-sun-dynamic
-make PETSC_ARCH=asterix-sun-dynamic all alltests DATAFILESPATH=/home/balay/datafiles
+make PETSC_ARCH=asterix-sun-dynamic all alltests tests_DATAFILESPATH DATAFILESPATH=/home/balay/datafiles
 ./config/asterix-sun.py --with-clanguage=cxx -PETSC_ARCH=asterix-sun-cxx  --with-debugging=0 --with-pic=0 \
 --download-f-blaslapack=1 --download-spooles=1 --download-superlu=1 \
 --download-superlu_dist=1 --download-hypre=1 --download-spai=1 --download-blacs=1 --download-scalapack=1 \
