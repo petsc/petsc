55dee81ecb385b18975ba9e74c560c93944f1054
Matthew Knepley <knepley@mcs.anl.gov>
2009-01-05 07:45:48 -0600

    Fortran numbering fix
    Hg-commit: 46760c5a05497552d1680fc48f5328bb519a27ae

--- src/dm/mesh/meshexodus.c
+++ src/dm/mesh/meshexodus.c
@@ -123,7 +123,7 @@ PetscErrorCode PetscReadExodusII(MPI_Comm comm, const char filename[], ALE::Obj<
   if (rank == 0) {
     for(int ns = 0; ns < num_node_sets; ++ns) {
       for(int n = 0; n < num_nodes_in_set[ns]; ++n) {
-        mesh->setValue(vertexSets, node_list[ns][n]+num_elem, ns_ids[ns]);
+        mesh->setValue(vertexSets, node_list[ns][n]+num_elem-1, ns_ids[ns]);
       }
       ierr = PetscFree(node_list[ns]);CHKERRQ(ierr);
     }
