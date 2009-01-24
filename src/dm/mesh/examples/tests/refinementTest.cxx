2f7b60398f0be93c96576ccf70bf17929b1d5f8d
Matthew Knepley <knepley@mcs.anl.gov>
2009-01-23 19:23:17 -0600

    More work on refinement test
    Hg-commit: 4906460d347e6a4ecbeb65485268920581fcd008

--- src/dm/mesh/examples/tests/refinementTest.cxx
+++ src/dm/mesh/examples/tests/refinementTest.cxx
@@ -1,6 +1,7 @@
 static char help[] = "Mesh Refinement Tests.\n\n";
 
 #include <petscmesh_viewers.hh>
+#include <list>
 
 using ALE::Obj;
 
@@ -91,6 +92,19 @@ PetscErrorCode SerialTetrahedronTest(const Options *options)
   PetscFunctionReturn(0);
 }
 
+template<typename Point>
+class Edge : public std::pair<Point, Point> {
+public:
+  Edge() : std::pair<Point, Point>() {};
+  Edge(const Point l) : std::pair<Point, Point>(l, l) {};
+  Edge(const Point l, const Point r) : std::pair<Point, Point>(l, r) {};
+  ~Edge() {};
+  friend std::ostream& operator<<(std::ostream& stream, const Edge& edge) {
+    stream << "(" << edge.first << ", " << edge.second << ")";
+    return stream;
+  };
+};
+
 #undef __FUNCT__
 #define __FUNCT__ "ParallelTetrahedronTest"
 PetscErrorCode ParallelTetrahedronTest(const Options *options)
@@ -98,7 +112,7 @@ PetscErrorCode ParallelTetrahedronTest(const Options *options)
   typedef PETSC_MESH_TYPE       mesh_type;
   typedef mesh_type::sieve_type sieve_type;
   typedef mesh_type::point_type point_type;
-  typedef std::pair<point_type,point_type> edge_type;
+  typedef Edge<point_type>      edge_type;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
@@ -157,7 +171,6 @@ PetscErrorCode ParallelTetrahedronTest(const Options *options)
 
     newMesh->setSieve(newSieve);
     ALE::MeshBuilder<mesh_type>::refineTetrahedra(*mesh, *newMesh, edge2vertex);
-    edge2vertex.clear();
     newMesh->view("Refined Parallel Mesh");
     // Create the parallel overlap
     size_t  nCells      = mesh->heightStratum(0)->size();
@@ -203,12 +216,99 @@ PetscErrorCode ParallelTetrahedronTest(const Options *options)
     newMesh->setCalculatedOverlap(true);
     delete [] numCells;
     delete [] numNewCells;
-    // Check edges in edge2vertex for both endpoints sent to same process
-    //   Put it in section with point being the lowest numbered vertex and value (other endpoint, new vertex)
-    // Copy across overlap
-    // Merge by translating edge to local points, finding edge in edge2vertex, and adding (local new vetex, remote new vertex) to overlap
     newSendOverlap->view("Refined Send Overlap");
     newRecvOverlap->view("Refined Recv Overlap");
+    // Check edges in edge2vertex for both endpoints sent to same process
+    //   Put it in section with point being the lowest numbered vertex and value (other endpoint, new vertex)
+    Obj<ALE::Section<point_type, edge_type> > newVertices = new ALE::Section<point_type, edge_type>(mesh->comm());
+    std::map<edge_type, std::vector<int> > bdedge2rank;
+
+    for(std::map<edge_type, point_type>::const_iterator e_iter = edge2vertex.begin(); e_iter != edge2vertex.end(); ++e_iter) {
+      const point_type left  = e_iter->first.first;
+      const point_type right = e_iter->first.second;
+
+      if (newSendOverlap->capContains(left) && newSendOverlap->capContains(right)) {
+        const Obj<mesh_type::send_overlap_type::traits::supportSequence>& leftRanksSeq = newSendOverlap->support(left);
+        std::list<int> leftRanks(leftRanksSeq->begin(), leftRanksSeq->end());
+        const Obj<mesh_type::send_overlap_type::traits::supportSequence>& rightRanks   = newSendOverlap->support(right);
+        std::list<int> ranks;
+        std::set_intersection(leftRanks.begin(), leftRanks.end(), rightRanks->begin(), rightRanks->end(),
+                              std::insert_iterator<std::list<int> >(ranks, ranks.begin()));
+
+        if(ranks.size()) {
+          newVertices->addFiberDimension(std::min(e_iter->first.first, e_iter->first.second), 1);
+          for(std::list<int>::const_iterator r_iter = ranks.begin(); r_iter != ranks.end(); ++r_iter) {
+            bdedge2rank[e_iter->first].push_back(*r_iter);
+          }
+        }
+      }
+    }
+    newVertices->allocatePoint();
+    const ALE::Section<point_type, edge_type>::chart_type& chart = newVertices->getChart();
+
+    for(ALE::Section<point_type, edge_type>::chart_type::const_iterator c_iter = chart.begin(); c_iter != chart.end(); ++c_iter) {
+      typedef ALE::Section<point_type, edge_type>::value_type value_type;
+      const point_type p      = *c_iter;
+      const int        dim    = newVertices->getFiberDimension(p);
+      int              v      = 0;
+      value_type      *values = new value_type[dim];
+
+      for(std::map<edge_type, std::vector<int> >::const_iterator e_iter = bdedge2rank.begin(); e_iter != bdedge2rank.end() && v < dim; ++e_iter) {
+        if (std::min(e_iter->first.first, e_iter->first.second) == p) {
+          values[v] = edge_type(std::max(e_iter->first.first, e_iter->first.second), edge2vertex[e_iter->first]);
+        }
+      }
+      newVertices->updatePoint(p, values);
+      delete [] values;
+    }
+    newVertices->view("New vertices");
+    // Copy across overlap
+    typedef ALE::Pair<int, point_type> overlap_point_type;
+    Obj<ALE::Section<overlap_point_type, edge_type> > overlapVertices = new ALE::Section<overlap_point_type, edge_type>(mesh->comm());
+
+    ALE::Pullback::SimpleCopy::copy(newSendOverlap, newRecvOverlap, newVertices, overlapVertices);
+    overlapVertices->view("Overlap vertices");
+    // Merge by translating edge to local points, finding edge in edge2vertex, and adding (local new vetex, remote new vertex) to overlap
+    for(std::map<edge_type, std::vector<int> >::const_iterator e_iter = bdedge2rank.begin(); e_iter != bdedge2rank.end(); ++e_iter) {
+      const point_type localPoint = edge2vertex[e_iter->first];
+
+      for(std::vector<int>::const_iterator r_iter = e_iter->second.begin(); r_iter != e_iter->second.end(); ++r_iter) {
+        point_type remoteLeft = -1, remoteRight = -1;
+        const int  rank       = *r_iter;
+
+        const Obj<mesh_type::send_overlap_type::traits::supportSequence>& leftRanks = newSendOverlap->support(e_iter->first.first);
+        for(mesh_type::send_overlap_type::traits::supportSequence::iterator lr_iter = leftRanks->begin(); lr_iter != leftRanks->end(); ++lr_iter) {
+          if (rank == *lr_iter) {
+            remoteLeft = lr_iter.color();
+            break;
+          }
+        }
+        const Obj<mesh_type::send_overlap_type::traits::supportSequence>& rightRanks = newSendOverlap->support(e_iter->first.second);
+        for(mesh_type::send_overlap_type::traits::supportSequence::iterator rr_iter = rightRanks->begin(); rr_iter != rightRanks->end(); ++rr_iter) {
+          if (rank == *rr_iter) {
+            remoteRight = rr_iter.color();
+            break;
+          }
+        }
+        const point_type remoteMin   = std::min(remoteLeft, remoteRight);
+        const point_type remoteMax   = std::max(remoteLeft, remoteRight);
+        const int        remoteSize  = overlapVertices->getFiberDimension(overlap_point_type(rank, remoteMin));
+        const edge_type *remoteVals  = overlapVertices->restrictPoint(overlap_point_type(rank, remoteMin));
+        point_type       remotePoint = -1;
+
+        for(int d = 0; d < remoteSize; ++d) {
+          if (remoteVals[d].first == remoteMax) {
+            remotePoint = remoteVals[d].second;
+            break;
+          }
+        }
+        newSendOverlap->addArrow(localPoint, rank, remotePoint);
+        newRecvOverlap->addArrow(rank, localPoint, remotePoint);
+      }
+    }
+    newSendOverlap->view("Augmented Refined Send Overlap");
+    newRecvOverlap->view("Augmented Refined Recv Overlap");
+    edge2vertex.clear();
     if (options->debug) {
       PetscViewer viewer;
 
