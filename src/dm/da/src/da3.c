78102f6c55fb0c40f4da559a448bd6b14eec00b1
Matthew Knepley <knepley@mcs.anl.gov>
2009-04-28 12:43:34 -0500

    Doc fixes
    Hg-commit: 203699381c9d014eaa710c1b29552d8cdcb0b419

--- src/dm/da/src/da3.c
+++ src/dm/da/src/da3.c
@@ -161,7 +161,7 @@ EXTERN PetscErrorCode DAPublish_Petsc(PetscObject);
    Input Parameters:
 +  comm - MPI communicator
 .  wrap - type of periodicity the array should have, if any.  Use one
-          of DA_NONPERIODIC, DA_XPERIODIC, DA_YPERIODIC, DA_XYPERIODIC, DA_XYZPERIODIC, DA_XZPERIODIC, or DA_YZPERIODIC.
+          of DA_NONPERIODIC, DA_XPERIODIC, DA_YPERIODIC, DA_ZPERIODIC, DA_XYPERIODIC, DA_XZPERIODIC, DA_YZPERIODIC, DA_XYZPERIODIC, or DA_XYZGHOSTED.
 .  stencil_type - Type of stencil (DA_STENCIL_STAR or DA_STENCIL_BOX)
 .  M,N,P - global dimension in each direction of the array (use -M, -N, and or -P to indicate that it may be set to a different value 
             from the command line with -da_grid_x <M> -da_grid_y <N> -da_grid_z <P>)
