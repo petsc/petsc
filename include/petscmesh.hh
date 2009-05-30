4f927d2133768f43f07755f4db391bb923ba1165
Matthew Knepley <knepley@mcs.anl.gov>
2009-05-30 16:51:47 -0500

    Symmetric matrices from the Mesh ignore the lower triangle by default
    Hg-commit: efa7d01cc734d46da8fad9069084eb7f66341b58

--- include/petscmesh.hh
+++ include/petscmesh.hh
@@ -31,6 +31,9 @@ PetscErrorCode PETSCDM_DLLEXPORT MeshCreateMatrix(const Obj<Mesh>& mesh, const O
   ierr = PetscStrcmp(mtype, MATMPISBAIJ, &isSymMPIBlock);CHKERRQ(ierr);
   // Check for symmetric storage
   isSymmetric = (PetscTruth) (isSymBlock || isSymSeqBlock || isSymMPIBlock);
+  if (isSymmetric) {
+    ierr = MatSetOptions(*J, MAT_IGNORE_LOWER_TRIANGULAR, PETSC_TRUE);CHKERRQ(ierr);
+  }
   if (!isShell) {
     PetscInt *dnz, *onz;
 
