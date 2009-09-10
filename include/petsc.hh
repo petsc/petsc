46e993b3808c9a94d79db95456042fa23673b669
Matthew Knepley <knepley@mcs.anl.gov>
2009-09-10 17:00:08 -0500

    Optimization for PETSc::Point
    Hg-commit: 807f6e25cadc751ee61716029495b2651d24ad3b

--- include/petsc.hh
+++ include/petsc.hh
@@ -41,19 +41,19 @@ namespace PETSc {
     template<typename Value2_>
     Point(const Point<dim,Value2_>& p) {for(int d = 0; d < dim; ++d) {x[d] = (Value2_) p.x[d];}}
 
-    int size() const {return dim;};
+    inline int size() const {return dim;};
 
-    void operator=(value_type v) {for(int d = 0; d < dim; ++d) {x[d] = v;}}
-    void operator=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] = p.x[d];}}
-    bool operator==(const Point& p) {for(int d = 0; d < dim; ++d) {if (x[d] != p.x[d]) return false;} return true;}
-    void operator+=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] += p.x[d];}}
-    void operator-=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] -= p.x[d];}}
+    inline void operator=(value_type v) {for(int d = 0; d < dim; ++d) {x[d] = v;}}
+    inline void operator=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] = p.x[d];}}
+    inline bool operator==(const Point& p) {for(int d = 0; d < dim; ++d) {if (x[d] != p.x[d]) return false;} return true;}
+    inline void operator+=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] += p.x[d];}}
+    inline void operator-=(const Point& p) {for(int d = 0; d < dim; ++d) {x[d] -= p.x[d];}}
     template<int d>
     static bool lessThan(const Point& a, const Point &b) {
       return a.x[d] < b.x[d];
     }
-    value_type operator[](const int i) const {return this->x[i];};
-    value_type& operator[](const int i) {return this->x[i];};
+    inline value_type operator[](const int i) const {return this->x[i];};
+    inline value_type& operator[](const int i) {return this->x[i];};
     friend std::ostream& operator<<(std::ostream& stream, const Point& p) {
       for(int d = 0; d < dim; ++d) {
         if (d > 0) stream << ",";
@@ -61,36 +61,36 @@ namespace PETSc {
       }
       return stream;
     }
-    Point& operator-() {
+    inline Point& operator-() {
       for(int d = 0; d < dim; ++d) {x[d] = -x[d];}
       return *this;
     }
-    friend Point operator+ (const Point& a, const Point &b) {
+    inline friend Point operator+ (const Point& a, const Point &b) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] + b.x[d];}
       return tmp;
     }
-    friend Point operator+ (const Point& a, const double c) {
+    inline friend Point operator+ (const Point& a, const double c) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] + c;}
       return tmp;
     }
-    friend Point operator- (const Point& a, const Point &b) {
+    inline friend Point operator- (const Point& a, const Point &b) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] - b.x[d];}
       return tmp;
     }
-    friend Point operator* (const Point& a, const Point &b) {
+    inline friend Point operator* (const Point& a, const Point &b) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] * b.x[d];}
       return tmp;
     }
-    friend Point operator* (const Point& a, const double c) {
+    inline friend Point operator* (const Point& a, const double c) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] * c;}
       return tmp;
     }
-    friend Point operator/ (const Point& a, const Point &b) {
+    inline friend Point operator/ (const Point& a, const Point &b) {
       Point tmp;
       for(int d = 0; d < dim; ++d) {tmp[d] = a.x[d] / b.x[d];}
       return tmp;
