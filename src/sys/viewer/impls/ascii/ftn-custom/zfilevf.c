e618ac5f376bd7f1e61b880b62369e3303616dfe
Matthew Knepley <knepley@mcs.anl.gov>
2009-09-25 14:44:14 -0500

    Now Fortran file printing also fixes the newline
    Hg-commit: 86baaddb92f51c159f53f1f027f64266498f9fc0

--- src/sys/viewer/impls/ascii/ftn-custom/zfilevf.c
+++ src/sys/viewer/impls/ascii/ftn-custom/zfilevf.c
@@ -24,25 +24,44 @@ void PETSC_STDCALL petscviewerfilesetname_(PetscViewer *viewer,CHAR name PETSC_M
   FREECHAR(name,c1);
 }
 
+static PetscErrorCode PetscFixSlashN(const char *in, char **out)
+{
+  PetscErrorCode ierr;
+  PetscInt       i;
+  size_t         len;
+
+  PetscFunctionBegin;
+  ierr = PetscStrallocpy(in,out);CHKERRQ(ierr);
+  ierr = PetscStrlen(*out,&len);CHKERRQ(ierr);
+  for (i=0; i<(int)len-1; i++) {
+    if ((*out)[i] == '\\' && (*out)[i+1] == 'n') {(*out)[i] = ' '; (*out)[i+1] = '\n';}
+  }
+  PetscFunctionReturn(0);
+}
+
 void PETSC_STDCALL petscviewerasciiprintf_(PetscViewer *viewer,CHAR str PETSC_MIXED_LEN(len1),PetscErrorCode *ierr PETSC_END_LEN(len1))
 {
-  char        *c1;
+  char        *c1, *tmp;
   PetscViewer v;
 
   PetscPatchDefaultViewers_Fortran(viewer,v);
   FIXCHAR(str,len1,c1);
-  *ierr = PetscViewerASCIIPrintf(v,c1);
+  *ierr = PetscFixSlashN(c1,&tmp);if (*ierr) return;
+  *ierr = PetscViewerASCIIPrintf(v,tmp);if (*ierr) return;
+  *ierr = PetscStrfree(tmp);if (*ierr) return;
   FREECHAR(str,c1);
 }
 
 void PETSC_STDCALL petscviewerasciisynchronizedprintf_(PetscViewer *viewer,CHAR str PETSC_MIXED_LEN(len1),PetscErrorCode *ierr PETSC_END_LEN(len1))
 {
-  char        *c1;
+  char        *c1, *tmp;
   PetscViewer v;
 
   PetscPatchDefaultViewers_Fortran(viewer,v);
   FIXCHAR(str,len1,c1);
-  *ierr = PetscViewerASCIISynchronizedPrintf(v,c1);
+  *ierr = PetscFixSlashN(c1,&tmp);if (*ierr) return;
+  *ierr = PetscViewerASCIISynchronizedPrintf(v,tmp);if (*ierr) return;
+  *ierr = PetscStrfree(tmp);if (*ierr) return;
   FREECHAR(str,c1);
 }
 
