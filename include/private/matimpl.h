b22b330c5ca615f114690330d6518ed7fe08adeb
Barry Smith <bsmith@mcs.anl.gov>
2009-10-26 20:25:04 -0500

    added MatNullSpaceSetFunction() for Fortran   changed the function calling sequence to take MatNullSpace as the first argument
    Hg-commit: c93e4c0dfd121a1c0fc75ca12ee19bdc3ca15a07

--- include/private/matimpl.h
+++ include/private/matimpl.h
@@ -377,7 +377,7 @@ struct _p_MatNullSpace {
   Vec*           vecs;
   PetscScalar*   alpha;                 /* for projections */
   Vec            vec;                   /* for out of place removals */
-  PetscErrorCode (*remove)(Vec,void*);  /* for user provided removal function */
+  PetscErrorCode (*remove)(MatNullSpace,Vec,void*);  /* for user provided removal function */
   void*          rmctx;                 /* context for remove() function */
 };
 
