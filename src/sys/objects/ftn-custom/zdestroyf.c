06203a5e57e43cd399e0cd5e316d54711e730b31
Matthew Knepley <knepley@mcs.anl.gov>
2009-11-05 10:56:13 -0600

    Added Fortran wrapper for PetscTypeCompare
    Hg-commit: 4779e158743c7ba198f0a74c540b820436aaf593

---
+++ src/sys/objects/ftn-custom/zdestroyf.c
@@ -0,0 +1,21 @@
+#include "private/fortranimpl.h"
+#include "petsc.h"
+
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define petsctypecompare_          PETSCTYPECOMPARE
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define petsctypecompare_          petsctypecompare
+#endif
+
+EXTERN_C_BEGIN
+void PETSC_STDCALL petsctypecompare_(PetscObject *obj,CHAR type_name PETSC_MIXED_LEN(len),
+                                     PetscTruth *same,PetscErrorCode *ierr PETSC_END_LEN(len))
+{
+  char *c1;
+  FIXCHAR(type_name,len,c1);
+  *ierr = PetscTypeCompare(*obj,c1,same);
+  FREECHAR(type_name,c1);
+}
+
+
+EXTERN_C_END
