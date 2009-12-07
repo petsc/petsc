bd7c7dddb24a67b8eebe9f5ca9730c2e146ac916
Jed Brown <jed@59A2.org>
2009-12-07 22:35:01 +0100

    Use explicit cast and drop unused variable
    Hg-commit: 46603648537788588e894452d661377c32a43e0c

--- src/ts/impls/explicit/ssp/ssp.c
+++ src/ts/impls/explicit/ssp/ssp.c
@@ -98,7 +98,7 @@ static PetscErrorCode SSPStep_RK_3(TS ts,PetscReal t0,PetscReal dt,Vec sol)
 
   PetscFunctionBegin;
   s = ssp->nstages;
-  n = floor(sqrt(s));
+  n = (PetscInt)sqrt(s);
   r = s-n;
   if (n*n != s) SETERRQ1(PETSC_ERR_SUP,"No support for optimal third order schemes with %d stages, must be a square number at least 4",s);
   ierr = SSPGetWorkVectors(ts,3,&work);CHKERRQ(ierr);
