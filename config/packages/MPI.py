c82ede9f63cca02311f9cf2e2b042a1c0c4621df
Satish Balay <balay@mcs.anl.gov>
2010-01-31 22:33:54 -0600

    add MPICH-32 path on win64
    Hg-commit: 0548e4d3eb3e6f7f278f4cf36e9985c13731246a

--- config/packages/MPI.py
+++ config/packages/MPI.py
@@ -118,6 +118,7 @@ class Configure(config.package.Package):
     yield(os.path.join('/cygdrive','c','Program Files','Microsoft HPC Pack 2008 SDK'))
     yield(os.path.join('/cygdrive','c','Program Files','Microsoft Compute Cluster Pack'))
     yield(os.path.join('/cygdrive','c','Program Files','MPICH2'))
+    yield(os.path.join('/cygdrive','c','Program Files (x86)','MPICH2'))
     yield(os.path.join('/cygdrive','c','Program Files','MPICH'))
     yield(os.path.join('/cygdrive','c','Program Files','MPICH','SDK.gcc'))
     yield(os.path.join('/cygdrive','c','Program Files','MPICH','SDK'))
