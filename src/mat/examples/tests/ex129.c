a96a99f5656dbbf30d5ba5c0738ab4b6404ee7f3
Shri Abhyankar <abhyshr@mcs.anl.gov>
2010-02-04 13:59:39 -0600

    Added test for MatMatSolve newdatastruct
    Hg-commit: a65c352b7871a792423c7d5dd4045ec8d3b97c13

--- src/mat/examples/tests/ex129.c
+++ src/mat/examples/tests/ex129.c
@@ -9,7 +9,7 @@
    u = 1 for x = 0, x = 1, y = 0, y = 1, z = 0, z = 1.
 */
 
-static char help[] = "This example is for testing different MatSolve routines,namely MatSolve,MatSolveAdd,MatSolveTranspose and MatSolveTransposeAdd.\n\
+static char help[] = "This example is for testing different MatSolve routines,namely MatSolve,MatSolveAdd,MatSolveTranspose,MatSolveTransposeAdd and MatMatSolve.\n\
 Example usage: ./ex129 -solve_type 1 -dof 1\n\n";
 
 #include "petscda.h"
@@ -17,23 +17,24 @@ Example usage: ./ex129 -solve_type 1 -dof 1\n\n";
 
 extern PetscErrorCode ComputeMatrix(DA,Mat);
 extern PetscErrorCode ComputeRHS(DA,Vec);
+extern PetscErrorCode ComputeRHSMatrix(PetscInt,PetscInt,Mat*);
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
-int main(int argc,char **argv)
+int main(int argc,char **args)
 {
   PetscErrorCode    ierr;
   PetscMPIInt       size;
   Vec               x,b,y,b1;
   DA                da;
-  Mat               A,F;
+  Mat               A,F,C,X,C1;
   MatFactorInfo     info;
   IS                perm,iperm;
-  PetscInt          dof=1,M=-8;
+  PetscInt          dof=1,M=-8,m,n,nrhs;
   PetscScalar       one = 1.0;
   PetscReal         norm;
 
-  PetscInitialize(&argc,&argv,(char *)0,help);
+  PetscInitialize(&argc,&args,(char *)0,help);
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   if(size != 1) SETERRQ(1,"This is a uniprocessor example only\n");
   ierr = PetscOptionsGetInt(PETSC_NULL,"-dof",&dof,PETSC_NULL);CHKERRQ(ierr);
@@ -57,6 +58,12 @@ int main(int argc,char **argv)
   ierr = VecSet(y,one);CHKERRQ(ierr);
   ierr = DAGetMatrix(da,MATBAIJ,&A);CHKERRQ(ierr);
   ierr = ComputeMatrix(da,A);CHKERRQ(ierr);
+  ierr = MatGetSize(A,&m,&n);CHKERRQ(ierr);
+  nrhs = 2;
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-nrhs",&nrhs,PETSC_NULL);CHKERRQ(ierr);
+  ierr = ComputeRHSMatrix(m,nrhs,&C);CHKERRQ(ierr);
+  ierr = MatDuplicate(C,MAT_DO_NOT_COPY_VALUES,&X);CHKERRQ(ierr);
+  
 
   ierr = MatGetOrdering(A,MATORDERING_ND,&perm,&iperm);CHKERRQ(ierr);
   ierr = MatGetFactor(A,MAT_SOLVER_PETSC,MAT_FACTOR_LU,&F);CHKERRQ(ierr);
@@ -70,14 +77,14 @@ int main(int argc,char **argv)
   ierr = MatMult(A,x,b1);CHKERRQ(ierr);
   ierr = VecAXPY(b1,-1.0,b);CHKERRQ(ierr);
   ierr = VecNorm(b1,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolve:             Error of norm %A\n",norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolve              : Error of norm %A\n",norm);CHKERRQ(ierr);
    
   /* MatSolveTranspose */
   ierr = MatSolveTranspose(F,b,x);CHKERRQ(ierr);
   ierr = MatMultTranspose(A,x,b1);CHKERRQ(ierr);
   ierr = VecAXPY(b1,-1.0,b);CHKERRQ(ierr);
   ierr = VecNorm(b1,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveTranspose :   Error of norm %A\n",norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveTranspose     : Error of norm %A\n",norm);CHKERRQ(ierr);
    
   /* MatSolveAdd */
   ierr = MatSolveAdd(F,b,y,x);CHKERRQ(ierr);
@@ -86,7 +93,7 @@ int main(int argc,char **argv)
   ierr = MatMultAdd(A,x,b1,b1);
   ierr = VecAXPY(b1,-1.0,b);CHKERRQ(ierr);
   ierr = VecNorm(b1,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveAdd:          Error or norm %A\n",norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveAdd           : Error of norm %A\n",norm);CHKERRQ(ierr);
   
   /* MatSolveTransposeAdd */
   ierr = MatSolveTransposeAdd(F,b,y,x);CHKERRQ(ierr); 
@@ -95,14 +102,24 @@ int main(int argc,char **argv)
   ierr = MatMultTransposeAdd(A,x,b1,b1);
   ierr = VecAXPY(b1,-1.0,b);CHKERRQ(ierr);
   ierr = VecNorm(b1,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveTransposeAdd: Error or norm %A\n",norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatSolveTransposeAdd  : Error of norm %A\n",norm);CHKERRQ(ierr);
   
+  /* MatMatSolve */
+  ierr = MatMatSolve(F,C,X);CHKERRQ(ierr);
+  ierr = MatMatMult(A,X,MAT_INITIAL_MATRIX,2.0,&C1);CHKERRQ(ierr);
+  ierr = MatAXPY(C1,-1.0,C,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+  ierr = MatNorm(C1,NORM_FROBENIUS,&norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"MatMatSolve           : Error of norm %A\n",norm);CHKERRQ(ierr);
+
   ierr = VecDestroy(x);CHKERRQ(ierr);
   ierr = VecDestroy(b);CHKERRQ(ierr);
   ierr = VecDestroy(b1);CHKERRQ(ierr);
   ierr = VecDestroy(y);CHKERRQ(ierr);
   ierr = MatDestroy(A);CHKERRQ(ierr);
   ierr = MatDestroy(F);CHKERRQ(ierr);
+  ierr = MatDestroy(C);CHKERRQ(ierr);
+  ierr = MatDestroy(C1);CHKERRQ(ierr);
+  ierr = MatDestroy(X);CHKERRQ(ierr);
   ierr = ISDestroy(perm);CHKERRQ(ierr);
   ierr = ISDestroy(iperm);CHKERRQ(ierr);
   ierr = DADestroy(da);CHKERRQ(ierr);
@@ -124,6 +141,43 @@ PetscErrorCode ComputeRHS(DA da,Vec b)
   ierr = VecSet(b,h);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
+
+#undef __FUNCT__
+#define __FUNCT__ "ComputeRHSMatrix"
+PetscErrorCode ComputeRHSMatrix(PetscInt m,PetscInt nrhs,Mat* C)
+{
+  PetscErrorCode ierr;
+  PetscRandom    rand;
+  Mat            RHS;
+  PetscScalar    *array,rval;
+  PetscInt       i,k;
+
+  PetscFunctionBegin;
+  ierr = MatCreate(PETSC_COMM_WORLD,&RHS);CHKERRQ(ierr);
+  ierr = MatSetSizes(RHS,m,PETSC_DECIDE,PETSC_DECIDE,nrhs);CHKERRQ(ierr);
+  ierr = MatSetType(RHS,MATSEQDENSE);CHKERRQ(ierr);
+    
+  ierr = PetscRandomCreate(PETSC_COMM_WORLD,&rand);CHKERRQ(ierr);
+  ierr = PetscRandomSetFromOptions(rand);CHKERRQ(ierr);
+  ierr = MatGetArray(RHS,&array);CHKERRQ(ierr);
+  for (i=0; i<m; i++){
+    ierr = PetscRandomGetValue(rand,&rval);CHKERRQ(ierr);
+    array[i] = rval; 
+  }
+  if (nrhs > 1){
+    for (k=1; k<nrhs; k++){
+      for (i=0; i<m; i++){
+        array[m*k+i] = array[i]; 
+      }
+    }
+  }
+  ierr = MatRestoreArray(RHS,&array);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(RHS,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(RHS,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  *C = RHS;
+  PetscFunctionReturn(0);
+}
+
     
 #undef __FUNCT__
 #define __FUNCT__ "ComputeMatrix"
@@ -205,7 +259,6 @@ PetscErrorCode ComputeMatrix(DA da,Mat B)
   ierr = MatAssemblyEnd(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = PetscFree(v);CHKERRQ(ierr);
   ierr = PetscRandomDestroy(rand);CHKERRQ(ierr);
-  /* ierr = MatView(B,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr); */ 
   PetscFunctionReturn(0);
 }
 
