f1d6b31f4f17d59604dd8f8e45982932831edb91
Barry Smith <bsmith@mcs.anl.gov>
2010-02-05 21:35:14 -0600

    added determination of HAVE_FORTRAN_GET_COMMAND_ARGUMENT and HAVE_FORTRAN_GETARG to be used by PFLOTRAN
    Hg-commit: 38600eb7c003d472ecba893ea967a802b23056af

--- config/PETSc/utilities/fortranCommandLine.py
+++ config/PETSc/utilities/fortranCommandLine.py
@@ -21,11 +21,29 @@ class Configure(config.base.Configure):
     config.base.Configure.setupDependencies(self, framework)
     self.compilers = self.framework.require('config.compilers', self)
     self.functions = self.framework.require('config.functions', self)
+    self.libraries     = framework.require('config.libraries',  self)
     return
 
   def configureFortranCommandLine(self):
     '''Check for the mechanism to retrieve command line arguments in Fortran'''
+
+    # These are for when the routines are called from Fortran 
+    if hasattr(self.compilers, 'FC'):
+      self.libraries.pushLanguage('FC')
+      if self.libraries.check('','', call = '      integer i\n      character*(80) arg\n       call get_command_argument(i,arg)'):
+        self.addDefine('HAVE_FORTRAN_GET_COMMAND_ARGUMENT',1)
+      elif self.libraries.check('','', call = '      integer i\n      character*(80) arg\n       call getarg(i,arg)'):
+        self.addDefine('HAVE_FORTRAN_GETARG',1)
+      self.libraries.popLanguage()
+
+    # These are for when the routines are called fraom C
+    # We should unify the naming conventions of these.
     self.pushLanguage('C')
+    # This one is not currently used in PETSc source code
+    if self.libraries.check('','get_command_argument', otherLibs = self.compilers.flibs, fortranMangle = 1):
+      self.addDefine('HAVE_GET_COMMAND_ARGUMENT',1)
+    if self.libraries.check('','getarg', otherLibs = self.compilers.flibs, fortranMangle = 1):
+      self.addDefine('HAVE_GETARG',1)
     if self.functions.check('ipxfargc_', libraries = self.compilers.flibs):
       self.addDefine('HAVE_PXFGETARG_NEW',1)
     elif self.functions.check('f90_unix_MP_iargc', libraries = self.compilers.flibs):
