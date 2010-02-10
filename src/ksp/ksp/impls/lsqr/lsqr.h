d2c855a832312c8611f1816080c9bb53ce9530ad
Barry Smith <bsmith@mcs.anl.gov>
2010-02-10 14:34:44 -0600

    added lsqr.h how it disappeared I don't knpw
    Hg-commit: 213765cee7876f2339679e8b179be2a2d6588be2

---
+++ src/ksp/ksp/impls/lsqr/lsqr.h
@@ -0,0 +1,26 @@
+#define PCBOOMERAMG   "boomeramg"
+#define PCEUCLID      "euclid"
+#define PCSPBASICC    "spbasicc"
+
+PetscErrorCode PETSCKSP_DLLEXPORT KSPCreate_PrecondLSQR(KSP ksp);
+PetscErrorCode PETSCKSP_DLLEXPORT KSPLSQRGetArnorm( KSP ksp,
+     PetscReal *arnorm, PetscReal *rhs_norm , PetscReal *anorm);
+PetscErrorCode precond_lsqr_monitor(
+                  KSP       solksp, /* Krylov Subspace method context */
+                  PetscInt  iter,   /* Current iteration number */
+                  PetscReal rnorm,  /* Current residual norm */
+                  void      *ctx    /* Pointer to user defined context */
+                  );
+
+PetscErrorCode read_command_line( int argc, char **argv,
+     int * order, int *maxit, char ** dirname, PetscScalar * droptol, 
+     PetscScalar * epsdiag, PCType *pc_type);
+
+PetscErrorCode precond_lsqr_converged(
+			KSP       solksp, /* Krylov Subspace method context */
+			PetscInt  iter,   /* Current iteration number */
+			PetscReal rnorm,  /* Current residual norm */
+			KSPConvergedReason *reason, /* duh... */
+			void      *ctx    /* Pointer to user defined context */
+		       );
+
