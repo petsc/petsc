1921852fd1b95c8c17d5d613ba9fc8fe13a456d3
Satish Balay <balay@mcs.anl.gov>
2010-03-19 17:21:19 -0500

    cygwin-link breaks intel f90 aswell - so update this check..
    Hg-commit: e53ebc4f8e84dfff28ea4093e2931e0f96ab4d66

--- config/configure.py
+++ config/configure.py
@@ -51,17 +51,17 @@ def check_petsc_arch(opts):
         opts.append(useName)
   return 0
 
-def chkcompaqf90():
+def chkwinf90():
   for arg in sys.argv:
-    if (arg.find('win32fe') >= 0 and arg.find('f90') >=0):
+    if (arg.find('win32fe') >= 0 and (arg.find('f90') >=0 or arg.find('ifort') >=0)):
       return 1
   return 0
 
 def chkcygwinlink():
-  if os.path.exists('/usr/bin/cygcheck.exe') and os.path.exists('/usr/bin/link.exe') and chkcompaqf90():
+  if os.path.exists('/usr/bin/cygcheck.exe') and os.path.exists('/usr/bin/link.exe') and chkwinf90():
       if '--ignore-cygwin-link' in sys.argv: return 0
       print '==============================================================================='
-      print ' *** Cygwin /usr/bin/link.exe detected! Compiles with Compaq f90 can break! **'
+      print ' *** Cygwin /usr/bin/link detected! Compiles with CVF/Intel f90 can break!  **'
       print ' *** To workarround do: "mv /usr/bin/link.exe /usr/bin/link-cygwin.exe"     **'
       print ' *** Or to ignore this check, use configure option: --ignore-cygwin-link    **'
       print '==============================================================================='
