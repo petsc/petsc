9a95a1b9356ed3b5806a20af85c5109579569762
Jed Brown <jed@59A2.org>
2010-03-21 17:50:31 +0100

    Remove final VLA use in mhyp.c
    Also remove the vartypes member which was completely unnecessary, but
    set to point at a stack allocation, so that any use in a different
    context would be erroneous.
    
    Hg-commit: 95c88ed87da05bb2b5b717dc8c9a5f105435e43f

--- src/dm/da/utils/mhyp.c
+++ src/dm/da/utils/mhyp.c
@@ -888,22 +888,22 @@ PetscErrorCode PETSCKSP_DLLEXPORT MatSetDA_HYPRESStruct(Mat mat,DA da)
 
   /* assuming that the same number of dofs on each gridpoint. Also assume all cell-centred based */
   ex->nvars= dof;
-  {
-     HYPRE_SStructVariable vartypes[ex->nvars];
-  
-     for (i= 0; i< dof; i++) {
-        vartypes[i]= HYPRE_SSTRUCT_VARIABLE_CELL;
-     }
-
-     ex->vartypes= vartypes;
-  }
 
   /* create the hypre grid object and set its information */
   if (p) SETERRQ(PETSC_ERR_SUP,"Ask us to add periodic support by calling HYPRE_SStructGridSetPeriodic()");
   ierr = HYPRE_SStructGridCreate(ex->hcomm,dim,nparts,&ex->ss_grid);CHKERRQ(ierr);
 
   ierr = HYPRE_SStructGridSetExtents(ex->ss_grid,part,ex->hbox.imin,ex->hbox.imax);CHKERRQ(ierr);
-  ierr = HYPRE_SStructGridSetVariables(ex->ss_grid, part, ex->nvars,ex->vartypes);CHKERRQ(ierr);
+
+  {
+    HYPRE_SStructVariable *vartypes;
+    ierr = PetscMalloc(ex->nvars*sizeof(HYPRE_SStructVariable),&vartypes);CHKERRQ(ierr);
+    for (i= 0; i< ex->nvars; i++) {
+      vartypes[i]= HYPRE_SSTRUCT_VARIABLE_CELL;
+    }
+    ierr = HYPRE_SStructGridSetVariables(ex->ss_grid, part, ex->nvars,vartypes);CHKERRQ(ierr);
+    ierr = PetscFree(vartypes);CHKERRQ(ierr);
+  }
 
   ierr = HYPRE_SStructGridAssemble(ex->ss_grid);CHKERRQ(ierr);
 
