abc2ff1381cd575fb4e9aa46a8f0cc143356451e
Lisandro Dalcin <dalcinl@gmail.com>
2010-03-26 13:40:41 -0300

    Bump version number to 1.1

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ communication.
 """
 
 __author__    = 'Lisandro Dalcin'
-__version__   = '1.0.3'
+__version__   = '1.1'
 __credits__   = "PETSc Team <petsc-maint@mcs.anl.gov>"
 
 # --------------------------------------------------------------------
