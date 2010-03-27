f7639f2c2e7edbfcee7f181cd2176ef07281189e
Lisandro Dalcin <dalcinl@gmail.com>
2010-03-27 00:16:14 -0300

    Disable some tests for PETSc < 3.0.0

--- test/test_mat_py.py
+++ test/test_mat_py.py
@@ -255,7 +255,10 @@ class TestDiagonal(TestMatrix):
         self.assertTrue(xt.equal(y))
         del A
 
-if PETSc.Sys.getVersion() == (2,3,2):
+if PETSc.Sys.getVersion() < (3,0,0):
+    del TestDiagonal.testCreateTranspose
+
+if PETSc.Sys.getVersion() < (2,3,3):
     del TestMatrix
     del TestIdentity
     del TestDiagonal
