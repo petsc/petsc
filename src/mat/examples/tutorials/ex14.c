ad4c773d4afedf0775818d33673c7365f3aaf292
Dmitry Karpeev <karpeev@mcs.anl.gov>
2010-03-30 22:46:38 -0500

    Multiple bugs fixed, extra test added to ex14, pure block matrices with no scatter/gather work.
    Hg-commit: b1b24869d167a97363332db63f8ddd17e969bb27

--- src/mat/examples/tutorials/ex14.c
+++ src/mat/examples/tutorials/ex14.c
@@ -11,10 +11,10 @@ Each block is a matrix of (generally) any type.\n\n";
 */
 #include "petscmat.h"
 
+
 #undef __FUNCT__
-#define __FUNCT__ "main"
-int main(int argc,char **args)
-{
+#define __FUNCT__ "TestMatFwkGlobal"
+PetscErrorCode TestMatFwkGlobal() {
   Mat M; /* Matrix framekwork */
   Mat B; /* Framework block */
   PetscInt e,n,N;
@@ -33,10 +33,13 @@ int main(int argc,char **args)
   PetscInt ii[2] = {0,1}, jj[2] = {0,1};
   PetscErrorCode ierr;
 
-  PetscInitialize(&argc,&args,(char *)0,help);
-  ierr = MPI_Comm_size(PETSC_COMM_WORLD, &size); CHKERRQ(ierr);
+  PetscFunctionBegin;
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Testing global MatFwk\n"); CHKERRQ(ierr);
+
 
   /* Construct a 1D mesh with e local elements.  Use the P1 function space on this mesh. */
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD, &size); CHKERRQ(ierr);
   e = 5;
   ierr = PetscOptionsGetInt(PETSC_NULL, "-e", &e, &flag); CHKERRQ(ierr);
   h = 1.0;
@@ -94,14 +97,21 @@ int main(int argc,char **args)
   ierr = MatFwkSetGather(M, gathermat, e, blocks); CHKERRQ(ierr);
 
   /* Now set up the blocks */
+  /* We only need to construct a single block and then reuse it */
+  B = PETSC_NULL;
   for(i = 0; i < 4; ++i) {
     values[i] *= h;
   }
   for(i = 0; i < e; ++i) {
-    ierr = MatFwkAddBlock(M, i, i, MATDENSE, &B); CHKERRQ(ierr); /* Acquire reference to B */
-    ierr = MatSetValues(B, 2,ii, 2,jj, values, INSERT_VALUES); CHKERRQ(ierr);
-    ierr = MatDestroy(B); CHKERRQ(ierr); /* Drop reference to B */
+    if(B == PETSC_NULL) {
+      ierr = MatFwkAddBlock(M, i, i, MATDENSE, &B); CHKERRQ(ierr); /* Acquire reference to B */
+      ierr = MatSetValues(B, 2,ii, 2,jj, values, INSERT_VALUES); CHKERRQ(ierr);
+    }
+    else {
+      ierr = MatFwkSetBlock(M, i, i, B); CHKERRQ(ierr);
+    }
   }
+  ierr = MatDestroy(B); CHKERRQ(ierr); /* Drop reference to B */
 
   /* Assemble MatFwk */
   ierr = MatAssemblyBegin(M, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
@@ -128,6 +138,7 @@ int main(int argc,char **args)
   ierr = PetscPrintf(PETSC_COMM_WORLD, "Final vector W = M'*V:\n"); CHKERRQ(ierr);
   ierr = VecView(W, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
 
+
   /* Clean up */
   ierr = VecDestroy(v); CHKERRQ(ierr);
   ierr = VecDestroy(V); CHKERRQ(ierr);
@@ -142,6 +153,120 @@ int main(int argc,char **args)
   ierr = MatDestroy(scattermat);     CHKERRQ(ierr);
   ierr = MatDestroy(M);              CHKERRQ(ierr);
 
+  PetscFunctionReturn(0);
+
+}/* TestMatFwkGlobal() */
+
+#undef __FUNCT__
+#define __FUNCT__ "TestMatFwkLocal"
+PetscErrorCode TestMatFwkLocal() {
+  Mat M; /* Matrix framekwork */
+  Mat B; /* Framework block */
+  PetscInt e,n;
+  PetscReal h;
+  PetscTruth flag;
+  Vec V, W;
+  PetscInt i, *blocks;
+  PetscReal third = 1.0/3.0, sixth = 1.0/6.0;
+  PetscScalar values[4] = {third, sixth, sixth,third};
+  PetscInt ii[2] = {0,1}, jj[2] = {0,1};
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Testing local MatFwk\n"); CHKERRQ(ierr);
+
+
+  /* Construct a 1D mesh with e local elements.  Use the P1 function space on this mesh. */
+  e = 5;
+  ierr = PetscOptionsGetInt(PETSC_NULL, "-e", &e, &flag); CHKERRQ(ierr);
+  h = 1.0;
+  ierr = PetscOptionsGetReal(PETSC_NULL, "-h", &h, &flag); CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Using %d 1D elements per process with mesh spacing %g\n", e,h); CHKERRQ(ierr);
+
+  /* Construct vectors with local elementwise layouts of degrees of freedom */
+  /* Local number of elementwise degrees of freedom is twice the local number of elements e*/
+  ierr = VecCreateMPI(PETSC_COMM_WORLD, e*2, PETSC_DECIDE, &V); CHKERRQ(ierr);
+
+  /* Now create the matrix framework with no scatter or gather */
+  ierr = MatCreate(PETSC_COMM_WORLD, &M); CHKERRQ(ierr);
+  n = 2*e;
+  ierr = MatSetSizes(M, n, n, PETSC_DECIDE, PETSC_DECIDE); CHKERRQ(ierr);
+  ierr = MatSetType(M, MATFWK); CHKERRQ(ierr);
+
+  /* Set scatter, gather and define the local block structure: e blocks with 2 vec elements per block */
+  ierr = PetscMalloc(e*sizeof(PetscInt), &blocks); CHKERRQ(ierr);
+  for(i = 0; i < e; ++i) {
+    blocks[i] = 2;
+  }
+  ierr = MatFwkSetScatter(M, PETSC_NULL, e, blocks); CHKERRQ(ierr);
+  ierr = MatFwkSetGather(M, PETSC_NULL, e, blocks); CHKERRQ(ierr);
+
+  /* Now set up the blocks */
+  /* We only need to construct a single block and then reuse it */
+  B = PETSC_NULL;
+  for(i = 0; i < 4; ++i) {
+    values[i] *= h;
+  }
+  for(i = 0; i < e; ++i) {
+    if(B == PETSC_NULL) {
+      ierr = MatFwkAddBlock(M, i, i, MATDENSE, &B); CHKERRQ(ierr); /* Acquire reference to B */
+      ierr = MatSetValues(B, 2,ii, 2,jj, values, INSERT_VALUES); CHKERRQ(ierr);
+    }
+    else {
+      ierr = MatFwkSetBlock(M, i, i, B); CHKERRQ(ierr);
+    }
+  }
+  ierr = MatDestroy(B); CHKERRQ(ierr); /* Drop reference to B */
+
+  /* Assemble MatFwk */
+  ierr = MatAssemblyBegin(M, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(M, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+  /* Now we apply the matrix */
+  ierr = VecSet(V, 1.0);      CHKERRQ(ierr);
+  ierr = VecDuplicate(V, &W); CHKERRQ(ierr);
+  ierr = MatMult(M,V,W);      CHKERRQ(ierr);
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Initial vector V:\n"); CHKERRQ(ierr);
+  ierr = VecView(V, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Final vector W = M*V:\n"); CHKERRQ(ierr);
+  ierr = VecView(W, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+  /* Now we apply the transpose matrix */
+  ierr = VecSet(V, 1.0);      CHKERRQ(ierr);
+  ierr = MatMultTranspose(M,V,W);      CHKERRQ(ierr);
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Initial vector V:\n"); CHKERRQ(ierr);
+  ierr = VecView(V, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Final vector W = M'*V:\n"); CHKERRQ(ierr);
+  ierr = VecView(W, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+
+  /* Clean up */
+  ierr = VecDestroy(V); CHKERRQ(ierr);
+  ierr = VecDestroy(W); CHKERRQ(ierr);
+  
+  ierr = PetscFree(blocks);          CHKERRQ(ierr);
+  ierr = MatDestroy(M);              CHKERRQ(ierr);
+
+  PetscFunctionReturn(0);
+
+}/* TestMatFwkLocal() */
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args) {
+  PetscErrorCode ierr;
+  PetscInitialize(&argc,&args,(char *)0,help);
+
+  ierr = TestMatFwkGlobal();
+  ierr = TestMatFwkLocal();
+
   ierr = PetscFinalize(); CHKERRQ(ierr);
   return 0;
 }
+
+
