23d101e362e91cff0fe512449439716a70b8c3a5
Barry Smith <bsmith@mcs.anl.gov>
2010-04-02 21:06:05 -0500

    moved useful troubleshooting files to faq.html and removed troubleshooting page
    Hg-commit: 807c8f03460a20295db1b3da3eb200ab86f45288

--- src/sys/error/err.c
+++ src/sys/error/err.c
@@ -224,14 +224,14 @@ static const char *PetscErrorStrings[] = {
           "Argument aliasing not permitted",
           "Invalid argument",
   /*63 */ "Argument out of range",
-          "Corrupt argument: see http://www.mcs.anl.gov/petsc/petsc-as/documentation/troubleshooting.html#Corrupt",
+          "Corrupt argument: see http://www.mcs.anl.gov/petsc/petsc-as/documentation/faq.html#valgrind",
           "Unable to open file",
           "Read from file failed",
           "Write to file failed",
           "Invalid pointer",
   /*69 */ "Arguments must have same type",
           "",
-  /*71 */ "Detected zero pivot in LU factorization\nsee http://www.mcs.anl.gov/petsc/petsc-as/documentation/troubleshooting.html#ZeroPivot",
+  /*71 */ "Detected zero pivot in LU factorization\nsee http://www.mcs.anl.gov/petsc/petsc-as/documentation/faq.html#ZeroPivot",
   /*72 */ "Floating point exception",
   /*73 */ "Object is in wrong state",
           "Corrupted Petsc object",
@@ -241,7 +241,7 @@ static const char *PetscErrorStrings[] = {
           "Memory corruption",
           "Unexpected data in file",
   /*80 */ "Arguments must have same communicators",
-  /*81 */ "Detected zero pivot in Cholesky factorization\nsee http://www.mcs.anl.gov/petsc/petsc-as/documentation/troubleshooting.html#ZeroPivot",
+  /*81 */ "Detected zero pivot in Cholesky factorization\nsee http://www.mcs.anl.gov/petsc/petsc-as/documentation/faq.html#ZeroPivot",
           "  ",
           "  ",
           "  ",
@@ -249,7 +249,7 @@ static const char *PetscErrorStrings[] = {
   /*86 */ "Unknown type. Check for miss-spelling or missing external package needed for type\n seehttp://www.mcs.anl.gov/petsc/petsc-as/documentation/installation.html#external",
   /*87 */ "Not used",
   /*88 */ "Error in system call",
-  /*89 */ "Object Type not set: see http://www.mcs.anl.gov/petsc/petsc-as/documentation/troubleshooting.html#typenotset"};
+  /*89 */ "Object Type not set: see http://www.mcs.anl.gov/petsc/petsc-as/documentation/faq.html#objecttypenotset"};
 
 #undef __FUNCT__  
 #define __FUNCT__ "PetscErrorMessage" 
