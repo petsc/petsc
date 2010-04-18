76539f063ae2516c29673f4eac05b0bca6a17f33
Barry Smith <bsmith@mcs.anl.gov>
2010-04-17 19:59:43 -0500

    if downloaded .tar.gz file is not in gzip format then assume it may be because you are behind a firewall and print appropriate messages
    Hg-commit: 230b2d530c394b5ac9dcf528dee4b54b37ba4e6d

--- retrieval.py
+++ retrieval.py
@@ -80,7 +80,18 @@ Unable to download package %s from: %s
       else:
         config.base.Configure.executeShellCommand('cd '+root+'; gunzip '+archiveZip, log = self.log)
     except RuntimeError, e:
-      raise RuntimeError('Error unzipping '+archiveZip+': '+str(e))
+      filename   = os.path.basename(urlparse.urlparse(url)[2])
+      if str(e).find("not in gzip format") > -1:
+        failureMessage = '''\
+Unable to unzip downloaded package %s from: %s
+* If you are behind a firewall - please fix your proxy and rerun config/configure.py
+* Alternatively, you can download the above URL manually, to /yourselectedlocation/%s
+  and use the configure option:
+  --download-%s=/yourselectedlocation/%s
+''' % (name, url, filename, name, filename)
+        raise RuntimeError(failureMessage)
+      else:
+        raise RuntimeError('Error unzipping '+archiveZip+': '+str(e))
     self.logPrint('Expanding '+localFile)
     try:
       config.base.Configure.executeShellCommand('cd '+root+'; tar -xf '+archive, log = self.log)
