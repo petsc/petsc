ffbb1f41549681795cb93ed1ec272f6090757678
Matthew G Knepley <knepley@gmail.com>
2010-04-27 09:54:52 -0500

    Added check against FFT
    Hg-commit: 665e9bc8c1078fce1d8891227f9bf849ac2d8309

--- src/ksp/ksp/examples/tutorials/ex47.c
+++ src/ksp/ksp/examples/tutorials/ex47.c
@@ -20,6 +20,7 @@ static char help[] = "Solves 3D Laplacian using multigrid.\n\n";
 
 extern PetscErrorCode ComputeMatrix(DMMG,Mat,Mat);
 extern PetscErrorCode ComputeRHS(DMMG,Vec);
+extern PetscErrorCode Solve_FFT(DA, Vec, Vec);
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
@@ -29,6 +30,7 @@ int main(int argc,char **argv)
   DMMG           *dmmg;
   PetscReal      norm;
   DA             da;
+  Vec            phi;
 
   ierr = PetscInitialize(&argc,&argv,(char *)0,help);CHKERRQ(ierr);
 
@@ -47,6 +49,13 @@ int main(int argc,char **argv)
   ierr = VecNorm(DMMGGetr(dmmg),NORM_2,&norm);CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD,"Residual norm %G\n",norm);CHKERRQ(ierr);
 
+  ierr = VecDuplicate(DMMGGetx(dmmg), &phi);CHKERRQ(ierr);
+  ierr = Solve_FFT(DMMGGetDA(dmmg), DMMGGetRHS(dmmg), phi);CHKERRQ(ierr);
+
+  ierr = VecAXPY(phi,-1.0,DMMGGetx(dmmg));CHKERRQ(ierr);
+  ierr = VecNorm(phi,NORM_2,&norm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Error norm (FFT vs. FD) %G\n",norm);CHKERRQ(ierr);
+
   ierr = DMMGDestroy(dmmg);CHKERRQ(ierr);
   ierr = PetscFinalize();CHKERRQ(ierr);
 
@@ -59,14 +68,14 @@ PetscErrorCode ComputeRHS(DMMG dmmg,Vec b)
 {
   DA             da = (DA) dmmg->dm;
   PetscScalar ***a;
-  PetscScalar    h;
+  PetscScalar    sc;
   PetscInt       mx, my, mz, xm, ym, zm, xs, ys, zs, i, j, k;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
   ierr = DAGetInfo(da,0,&mx,&my,&mz,0,0,0,0,0,0,0);CHKERRQ(ierr);
   ierr = DAGetCorners(da,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
-  h    = 1.0/((mx-1)*(my-1)*(mz-1));
+  sc   = 1.0/((mx-1)*(my-1)*(mz-1));
   ierr = DAVecGetArray(da, b, &a);CHKERRQ(ierr);
   for(k = zs; k < zs+zm; ++k) {
     for(j = ys; j < ys+ym; ++j) {
@@ -75,7 +84,7 @@ PetscErrorCode ComputeRHS(DMMG dmmg,Vec b)
           a[k][j][i] = 0.0;
         } else {
           if (k > 5) {
-            a[k][j][i] = h;
+            a[k][j][i] = sc;
           } else {
             a[k][j][i] = 0.0;
           }
@@ -125,5 +134,85 @@ PetscErrorCode ComputeMatrix(DMMG dmmg,Mat jac,Mat B)
   }
   ierr = MatAssemblyBegin(B, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(B, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "Solve_FFT"
+PetscErrorCode Solve_FFT(DA da, Vec rhs, Vec phi)
+{
+  PetscReal      L[3] = {2.0, 2.0, 6.0};
+  PetscReal      h[3];
+  PetscInt       dim[3];
+  PetscReal      scale, sc;
+  PetscScalar ***rhsHatArray;
+  PetscScalar ***phiHatArray;
+  Mat            F;
+  Vec            rhsHat, phiHat;
+  PetscInt       M, N, P, xm, ym, zm, xs, ys, zs;
+  PetscErrorCode ierr;
+	
+  PetscFunctionBegin;
+  ierr = DAGetInfo(da, 0, &M, &N, &P, 0, 0, 0, 0, 0, 0, 0);CHKERRQ(ierr);
+  ierr = DAGetInfo(da, 0, &dim[2], &dim[1], &dim[0], 0, 0, 0, 0, 0, 0, 0);CHKERRQ(ierr);
+  ierr = MatCreateSeqFFTW(PETSC_COMM_WORLD, 3, dim, &F);CHKERRQ(ierr);
+  ierr = DAGetCorners(da, &xs, &ys, &zs, &xm, &ym, &zm);CHKERRQ(ierr);
+  h[0] = L[0]/(M - 1);
+  h[1] = L[1]/(N - 1);
+  h[2] = L[2]/(P - 1);
+  sc   = 1.0/((PetscReal) (M - 1)*(N - 1)*(P - 1));
+  scale = 1.0/((PetscReal) M*N*P);
+  ierr = DAGetGlobalVector(da, &rhsHat);CHKERRQ(ierr);
+  ierr = MatMult(F, rhs, rhsHat);CHKERRQ(ierr);
+  ierr = DAGetGlobalVector(da, &phiHat);CHKERRQ(ierr);
+  ierr = DAVecGetArray(da, rhsHat, &rhsHatArray);CHKERRQ(ierr);
+  ierr = DAVecGetArray(da, phiHat, &phiHatArray);CHKERRQ(ierr);
+  for(PetscInt k = zs; k < zs+zm; ++k) {
+    PetscReal kz = k <= P/2 ? 2.0*M_PI*k/(P) : -2.0*M_PI*(P-k)/(P);
+
+    for(PetscInt j = ys; j < ys+ym; ++j) {
+      PetscReal ky = j <= N/2 ? 2.0*M_PI*j/(N) : -2.0*M_PI*(N-j)/(N);
+
+      for(PetscInt i = xs; i < xs+xm; ++i) {
+        PetscReal kx = i <= M/2 ? 2.0*M_PI*i/(M) : -2.0*M_PI*(M-i)/(M);
+        PetscScalar charge = 0.0;
+
+#if 0
+        for(PetscInt sp = 0; sp < geomOptions->numSpecies; ++sp) {
+          charge += (e*e/epsilon)*z[sp]*rhoHatArray[k][j][i].v[sp];
+        }
+#else
+        charge = rhsHatArray[k][j][i];
+#endif
+        PetscReal denom = 2.0*((1.0-cos(kx))/PetscSqr(h[0]) + (1.0-cos(ky))/PetscSqr(h[1]) + (1.0-cos(kz))/PetscSqr(h[2]));
+        // Omit the zeroth moment
+        if (kx == 0 && ky == 0 && kz == 0) {
+          phiHatArray[k][j][i] = 0.0;
+        } else {
+          phiHatArray[k][j][i] = charge/denom; // Changed units by scaling by e
+        }
+        if (PetscIsInfOrNanScalar(phiHatArray[k][j][i])) {
+          SETERRQ4(PETSC_ERR_FP, "Nan or inf at phiHat[%d][%d][%d]: %g ", k, j, i, PetscRealPart(phiHatArray[k][j][i]));
+        }
+      }
+    }
+  }
+  ierr = DAVecRestoreArray(da, phiHat, &phiHatArray);CHKERRQ(ierr);
+  ierr = MatMultTranspose(F, phiHat, phi);CHKERRQ(ierr);
+  ierr = VecScale(phi, scale);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da, &phiHat);CHKERRQ(ierr);
+  ierr = DARestoreGlobalVector(da, &rhsHat);CHKERRQ(ierr);
+  ierr = MatDestroy(F);CHKERRQ(ierr);
+
+  // Force potential in the bath to be 0
+  PetscInt       bathIndex[3] = {10, 0, 0};
+  PetscScalar ***phiArray;
+  PetscScalar    bathPotential;
+
+  ierr = DAVecGetArray(da, phi, &phiArray);CHKERRQ(ierr);
+  bathPotential = phiArray[bathIndex[2]][bathIndex[1]][bathIndex[0]];
+  ierr = DAVecRestoreArray(da, phi, &phiArray);CHKERRQ(ierr);
+  ierr = VecShift(phi, -bathPotential);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
