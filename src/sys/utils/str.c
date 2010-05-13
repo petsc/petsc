675282fd4d801d41a0bc5d7c0eb3ff420dd13aed
Hong Zhang <hzhang@mcs.anl.gov>
2010-05-13 16:41:04 -0500

    add mising cast for malloc()
    Hg-commit: a8894b36a8005796d43e81c5be26534644caed28

--- src/sys/utils/str.c
+++ src/sys/utils/str.c
@@ -49,7 +49,7 @@ PetscErrorCode PETSC_DLLEXPORT PetscStrToArray(const char s[],int *argc,char ***
     else if (s[i] != ' ') {flg = PETSC_FALSE;}
   }
   (*args) = (char **) malloc((*argc)*sizeof(char**)); if (!*args) return PETSC_ERR_MEM;
-  lens  = malloc((*argc)*sizeof(int)); if (!lens) return PETSC_ERR_MEM;
+  lens    = (int*) malloc((*argc)*sizeof(int)); if (!lens) return PETSC_ERR_MEM;
   for (i=0; i<*argc; i++) lens[i] = 0;
 
   *argc = 0;
@@ -62,7 +62,7 @@ PetscErrorCode PETSC_DLLEXPORT PetscStrToArray(const char s[],int *argc,char ***
   }
 
   for (i=0; i<*argc; i++) {
-    (*args)[i] = malloc((lens[i]+1)*sizeof(char)); if (!(*args)[i]) return PETSC_ERR_MEM;
+    (*args)[i] = (char*) malloc((lens[i]+1)*sizeof(char)); if (!(*args)[i]) return PETSC_ERR_MEM;
   }
 
   *argc = 0;
