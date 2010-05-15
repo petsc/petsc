f35f84d1309daa762bf0f2126ab5682951544c8c
Barry Smith <bsmith@mcs.anl.gov>
2010-05-15 13:00:38 -0500

    added xcode project to make library of PETSc functions
    Hg-commit: ed28b80f119d7beb3538b4d617276445e6647ce3

--- bin/maint/xcodebuilder.py
+++ bin/maint/xcodebuilder.py
@@ -5,20 +5,20 @@
 #   Before using removed /usr/include/mpi.h and /Developer/SDKs/MacOSX10.5.sdk/usr/include/mpi.h or
 #      Xcode will use those instead of the MPIuni one we point to
 #
-#   Run ./configure with the options --with-valgrind=0 --with-mpi=0 --with-x=0 --with-cc="gcc -m32" --download-c-blas-lapack
+#   Run ./configure with the options --with-valgrind=0 [--with-mpi=0 --with-x=0 --with-cc="gcc -m32" --download-c-blas-lapack ](when building for iPhone)
 #
-#   Remove mention of xmm*.h in $PETSC_ARCH/include/petscconf.h and change PETSc_Prefetch() to do nothing
+#   Remove mention of xmm*.h in $PETSC_ARCH/include/petscconf.h and change PETSc_Prefetch() to do nothing (when building for iPhone)
 #
 #   After running xcodebuilder.py
 #      In Project->Add to Project put in the directory $PETSC_DIR/PETSC_ARCH/xcode-links
 #      In Project->Edit Project Settings->Search Paths->Header Search Paths add
 #         $PETSC_DIR/include $PETSC_DIR/include/mpiuni $PETSC_DIR/$PETSC_ARCH/include  replacing the variables with their values, for example
 #         /Users/barrysmith/Src/petsc-dev/include /Users/barrysmith/Src/petsc-dev/include/mpiuni /Users/barrysmith/Src/petsc-dev/arch-uni/include
-#      Press control mouse on Frameworks bullet->Add Existing Frameworks then select libblas and liblapack
+#      Press control mouse on Frameworks bullet->Add Existing Frameworks then select libblas and liblapack (when building for Mac)
 #
-#  Notes - if you skip the --with-mpi=0 and let it use the NATIVE Apple MPI that may work, I have not tried it.
+#  Notes - if you skip the --with-mpi=0 and let it use the NATIVE Apple MPI that may work, I have not tried it (When building for Mac)
 #        - have not tried anything with Fortran or C++
-#        - if you link against the X11 libraries you can probably skip the --with-x=0
+#        - if you link against the X11 libraries you can probably skip the --with-x=0 (When building for Mac)
 #
 import os, sys
 
@@ -243,11 +243,11 @@ class PETScMaker(script.Script):
      self.buildDir(root)
      for badDir in [d for d in dirs if not self.checkDir(os.path.join(root, d))]:
        dirs.remove(badDir)
-   self.buildDir(os.path.join(os.environ['PETSC_DIR'],'externalpackages','f2cblaslapack-3.1.1','blas'))
-   self.buildDir(os.path.join(os.environ['PETSC_DIR'],'externalpackages','f2cblaslapack-3.1.1','lapack'))
+   #self.buildDir(os.path.join(os.environ['PETSC_DIR'],'externalpackages','f2cblaslapack-3.1.1','blas'))
+   #self.buildDir(os.path.join(os.environ['PETSC_DIR'],'externalpackages','f2cblaslapack-3.1.1','lapack'))
    # manually link f2c include file
-   if not os.path.islink(os.path.join(self.petscdir.dir, self.arch.arch, 'include','f2c.h')):   
-   os.symlink(os.path.join(self.petscdir.dir, 'externalpackages', 'f2cblaslapack-3.1.1','blas','f2c.h'),os.path.join(self.petscdir.dir, self.arch.arch, 'include','f2c.h'))   
+   #if not os.path.islink(os.path.join(self.petscdir.dir, self.arch.arch, 'include','f2c.h')):   
+   #os.symlink(os.path.join(self.petscdir.dir, 'externalpackages', 'f2cblaslapack-3.1.1','blas','f2c.h'),os.path.join(self.petscdir.dir, self.arch.arch, 'include','f2c.h'))   
    # do not need to link these because xcode project points to original source code directory
    #os.symlink(os.path.join(self.petscdir.dir, self.arch.arch, 'petscconf.h'),os.path.join(self.petscdir.dir, self.arch.arch, 'xcode-links','include','petscconf.h'))
    #os.symlink(os.path.join(self.petscdir.dir, self.arch.arch, 'petscfix.h'),os.path.join(self.petscdir.dir, self.arch.arch, 'xcode-links','include','petscfix.h'))     
