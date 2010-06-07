5d339270ede3ed3ccd934fbfe4855d894265c235
Jed Brown <jed@59A2.org>
2010-06-08 01:03:41 +0200

    Make CHOLMOD work with complex and int64, add options database support
    Hg-commit: 905489ba21b0fd9026dd316e12051d7bf533d035

--- src/mat/impls/sbaij/seq/cholmod/cholmod.c
+++ src/mat/impls/sbaij/seq/cholmod/cholmod.c
@@ -11,64 +11,38 @@
 */
 #include "../src/mat/impls/sbaij/seq/sbaij.h"
 
+#if defined(PETSC_USE_COMPLEX)
+#  define CHOLMOD_SCALAR_TYPE       CHOLMOD_COMPLEX
+#else
+#  define CHOLMOD_SCALAR_TYPE       CHOLMOD_REAL
+#endif
+
 #if defined(PETSC_USE_64BIT_INDICES)
-#if defined(PETSC_USE_COMPLEX)
-#define umfpack_UMF_free_symbolic   umfpack_zl_free_symbolic
-#define umfpack_UMF_free_numeric    umfpack_zl_free_numeric
-#define umfpack_UMF_wsolve          umfpack_zl_wsolve
-#define umfpack_UMF_numeric         umfpack_zl_numeric
-#define umfpack_UMF_report_numeric  umfpack_zl_report_numeric
-#define umfpack_UMF_report_control  umfpack_zl_report_control
-#define umfpack_UMF_report_status   umfpack_zl_report_status
-#define umfpack_UMF_report_info     umfpack_zl_report_info
-#define umfpack_UMF_report_symbolic umfpack_zl_report_symbolic
-#define umfpack_UMF_qsymbolic       umfpack_zl_qsymbolic
-#define umfpack_UMF_symbolic        umfpack_zl_symbolic
-#define umfpack_UMF_defaults        umfpack_zl_defaults
-
+#  define CHOLMOD_INT_TYPE          CHOLMOD_LONG
+#  define cholmod_X_start           cholmod_l_start
+#  define cholmod_X_analyze         cholmod_l_analyze
+#  define cholmod_X_analyze_p       cholmod_l_analyze_p
+#  define cholmod_X_factorize       cholmod_l_factorize
+#  define cholmod_X_finish          cholmod_l_finish
+#  define cholmod_X_free_factor     cholmod_l_free_factor
+#  define cholmod_X_free_dense      cholmod_l_free_dense
+#  define cholmod_X_solve           cholmod_l_solve
 #else
-#define umfpack_UMF_free_symbolic   umfpack_dl_free_symbolic
-#define umfpack_UMF_free_numeric    umfpack_dl_free_numeric
-#define umfpack_UMF_wsolve          umfpack_dl_wsolve
-#define umfpack_UMF_numeric         umfpack_dl_numeric
-#define umfpack_UMF_report_numeric  umfpack_dl_report_numeric
-#define umfpack_UMF_report_control  umfpack_dl_report_control
-#define umfpack_UMF_report_status   umfpack_dl_report_status
-#define umfpack_UMF_report_info     umfpack_dl_report_info
-#define umfpack_UMF_report_symbolic umfpack_dl_report_symbolic
-#define umfpack_UMF_qsymbolic       umfpack_dl_qsymbolic
-#define umfpack_UMF_symbolic        umfpack_dl_symbolic
-#define umfpack_UMF_defaults        umfpack_dl_defaults
+#  define CHOLMOD_INT_TYPE          CHOLMOD_INT
+#  define cholmod_X_start           cholmod_start
+#  define cholmod_X_analyze         cholmod_analyze
+#  define cholmod_X_analyze_p       cholmod_analyze_p
+#  define cholmod_X_factorize       cholmod_factorize
+#  define cholmod_X_finish          cholmod_finish
+#  define cholmod_X_free_factor     cholmod_free_factor
+#  define cholmod_X_free_dense      cholmod_free_dense
+#  define cholmod_X_solve           cholmod_solve
 #endif
-
-#else
-#if defined(PETSC_USE_COMPLEX)
-#define umfpack_UMF_free_symbolic   umfpack_zi_free_symbolic
-#define umfpack_UMF_free_numeric    umfpack_zi_free_numeric
-#define umfpack_UMF_wsolve          umfpack_zi_wsolve
-#define umfpack_UMF_numeric         umfpack_zi_numeric
-#define umfpack_UMF_report_numeric  umfpack_zi_report_numeric
-#define umfpack_UMF_report_control  umfpack_zi_report_control
-#define umfpack_UMF_report_status   umfpack_zi_report_status
-#define umfpack_UMF_report_info     umfpack_zi_report_info
-#define umfpack_UMF_report_symbolic umfpack_zi_report_symbolic
-#define umfpack_UMF_qsymbolic       umfpack_zi_qsymbolic
-#define umfpack_UMF_symbolic        umfpack_zi_symbolic
-#define umfpack_UMF_defaults        umfpack_zi_defaults
-
-#else
-#define CHOLMOD_INT_TYPE          CHOLMOD_INT
-#define CHOLMOD_SCALAR_TYPE       CHOLMOD_REAL
-#define cholmod_X_analyze         cholmod_analyze
-#define cholmod_X_factorize       cholmod_factorize
-#define cholmod_X_solve           cholmod_solve
-#endif
-#endif
-
 
 #define UF_long long long
 #define UF_long_max LONG_LONG_MAX
 #define UF_long_id "%lld"
+#undef I  /* complex.h defines I=_Complex_I, but cholmod_core.h uses I as a field member */
 
 EXTERN_C_BEGIN
 #include <cholmod.h>
@@ -86,11 +60,73 @@ static PetscErrorCode CholmodStart(Mat F)
 {
   PetscErrorCode ierr;
   Mat_CHOLMOD    *chol=(Mat_CHOLMOD*)F->spptr;
+  cholmod_common *c;
+  PetscTruth     flg;
 
   PetscFunctionBegin;
   if (chol->common) PetscFunctionReturn(0);
   ierr = PetscMalloc(sizeof(*chol->common),&chol->common);CHKERRQ(ierr);
-  ierr = !cholmod_start(chol->common);CHKERRQ(ierr);
+  ierr = !cholmod_X_start(chol->common);CHKERRQ(ierr);
+  c = chol->common;
+#define CHOLMOD_OPTION_DOUBLE(name,help) do {                            \
+    PetscReal tmp = (PetscReal)c->name;                                  \
+    ierr = PetscOptionsReal("-mat_cholmod_" #name,help,"None",tmp,&tmp,0);CHKERRQ(ierr); \
+    c->name = (double)tmp;                                               \
+  } while (0)
+#define CHOLMOD_OPTION_INT(name,help) do {                               \
+    PetscInt tmp = (PetscInt)c->name;                                    \
+    ierr = PetscOptionsInt("-mat_cholmod_" #name,help,"None",tmp,&tmp,0);CHKERRQ(ierr); \
+    c->name = (int)tmp;                                                  \
+  } while (0)
+#define CHOLMOD_OPTION_SIZE_T(name,help) do {                            \
+    PetscInt tmp = (PetscInt)c->name;                                    \
+    ierr = PetscOptionsInt("-mat_cholmod_" #name,help,"None",tmp,&tmp,0);CHKERRQ(ierr); \
+    if (tmp < 0) SETERRQ(((PetscObject)F)->comm,PETSC_ERR_ARG_OUTOFRANGE,"value must be positive"); \
+    c->name = (size_t)tmp;                                               \
+  } while (0)
+#define CHOLMOD_OPTION_TRUTH(name,help) do {                             \
+    PetscTruth tmp = (PetscTruth)!!c->name;                              \
+    ierr = PetscOptionsTruth("-mat_cholmod_" #name,help,"None",tmp,&tmp,0);CHKERRQ(ierr); \
+    c->name = (int)tmp;                                                  \
+  } while (0)
+
+  ierr = PetscOptionsBegin(((PetscObject)F)->comm,((PetscObject)F)->prefix,"CHOLMOD Options","Mat");CHKERRQ(ierr);
+  CHOLMOD_OPTION_DOUBLE(dbound,"Minimum absolute value of diagonal entries of D");
+  CHOLMOD_OPTION_DOUBLE(grow0,"Global growth ratio when factors are modified");
+  CHOLMOD_OPTION_DOUBLE(grow1,"Column growth ratio when factors are modified");
+  CHOLMOD_OPTION_SIZE_T(grow2,"Affine column growth constant when factors are modified");
+  CHOLMOD_OPTION_SIZE_T(maxrank,"Max rank of update, larger values are faster but use more memory [2,4,8]");
+  {
+    static const char *const list[] = {"SIMPLICIAL","AUTO","SUPERNODAL","MatCholmodFactorType","MAT_CHOLMOD_FACTOR_",0};
+    PetscEnum choice = (PetscEnum)c->supernodal;
+    ierr = PetscOptionsEnum("-mat_cholmod_factor","Factorization method","None",list,(PetscEnum)c->supernodal,&choice,0);CHKERRQ(ierr);
+    c->supernodal = (int)choice;
+  }
+  if (c->supernodal) CHOLMOD_OPTION_DOUBLE(supernodal_switch,"flop/nnz_L threshold for switching to supernodal factorization");
+  CHOLMOD_OPTION_TRUTH(final_asis,"Leave factors \"as is\"");
+  CHOLMOD_OPTION_TRUTH(final_pack,"Pack the columns when finished (use FALSE if the factors will be updated later)");
+  if (!c->final_asis) {
+    CHOLMOD_OPTION_TRUTH(final_super,"Leave supernodal factors instead of converting to simplicial");
+    CHOLMOD_OPTION_TRUTH(final_ll,"Turn LDL' factorization into LL'");
+    CHOLMOD_OPTION_TRUTH(final_monotonic,"Ensure columns are monotonic when done");
+    CHOLMOD_OPTION_TRUTH(final_resymbol,"Remove numerically zero values resulting from relaxed supernodal amalgamation");
+  }
+  {
+    PetscReal tmp[] = {(PetscReal)c->zrelax[0],(PetscReal)c->zrelax[1],(PetscReal)c->zrelax[2]};
+    PetscInt n = 3;
+    ierr = PetscOptionsRealArray("-mat_cholmod_zrelax","3 real supernodal relaxed amalgamation parameters","None",tmp,&n,&flg);CHKERRQ(ierr);
+    if (flg && n != 3) SETERRQ(((PetscObject)F)->comm,PETSC_ERR_ARG_OUTOFRANGE,"must provide exactly 3 parameters to -mat_cholmod_zrelax");
+    if (flg) while (n--) c->zrelax[n] = (double)tmp[n];
+  }
+  {
+    PetscInt n,tmp[] = {(PetscInt)c->nrelax[0],(PetscInt)c->nrelax[1],(PetscInt)c->nrelax[2]};
+    ierr = PetscOptionsIntArray("-mat_cholmod_nrelax","3 size_t supernodal relaxed amalgamation parameters","None",tmp,&n,&flg);CHKERRQ(ierr);
+    if (flg && n != 3) SETERRQ(((PetscObject)F)->comm,PETSC_ERR_ARG_OUTOFRANGE,"must provide exactly 3 parameters to -mat_cholmod_nrelax");
+    if (flg) while (n--) c->nrelax[n] = (size_t)tmp[n];
+  }
+  CHOLMOD_OPTION_TRUTH(prefer_upper,"Work with upper triangular form (faster when using fill-reducing ordering, slower in natural ordering)");
+  CHOLMOD_OPTION_INT(print,"Verbosity level");
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -150,8 +186,8 @@ static PetscErrorCode MatDestroy_CHOLMOD(Mat F)
   Mat_CHOLMOD    *chol=(Mat_CHOLMOD*)F->spptr;
 
   PetscFunctionBegin;
-  ierr = !cholmod_free_factor(&chol->factor,chol->common);CHKERRQ(ierr);
-  ierr = !cholmod_finish(chol->common);CHKERRQ(ierr);
+  ierr = !cholmod_X_free_factor(&chol->factor,chol->common);CHKERRQ(ierr);
+  ierr = !cholmod_X_finish(chol->common);CHKERRQ(ierr);
   ierr = PetscFree(chol->common);CHKERRQ(ierr);
   ierr = PetscFree(chol->matrix);CHKERRQ(ierr);
   ierr = MatDestroy_SeqSBAIJ(F);CHKERRQ(ierr);
@@ -178,8 +214,8 @@ static PetscErrorCode MatFactorInfo_CHOLMOD(Mat F,PetscViewer viewer)
   ierr = PetscViewerASCIIPrintf(viewer,"Common.dbound            %g  (Smallest absolute value of diagonal entries of D)\n",c->dbound);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.grow0             %g\n",c->grow0);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.grow1             %g\n",c->grow1);CHKERRQ(ierr);
-  ierr = PetscViewerASCIIPrintf(viewer,"Common.grow2             %ud\n",(unsigned)c->grow2);CHKERRQ(ierr);
-  ierr = PetscViewerASCIIPrintf(viewer,"Common.maxrank           %ud\n",(unsigned)c->maxrank);CHKERRQ(ierr);
+  ierr = PetscViewerASCIIPrintf(viewer,"Common.grow2             %u\n",(unsigned)c->grow2);CHKERRQ(ierr);
+  ierr = PetscViewerASCIIPrintf(viewer,"Common.maxrank           %u\n",(unsigned)c->maxrank);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.supernodal_switch %g\n",c->supernodal_switch);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.supernodal        %d\n",c->supernodal);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.final_asis        %d\n",c->final_asis);CHKERRQ(ierr);
@@ -189,17 +225,13 @@ static PetscErrorCode MatFactorInfo_CHOLMOD(Mat F,PetscViewer viewer)
   ierr = PetscViewerASCIIPrintf(viewer,"Common.final_monotonic   %d\n",c->final_monotonic);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.final_resymbol    %d\n",c->final_resymbol);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.zrelax            [%g,%g,%g]\n",c->zrelax[0],c->zrelax[1],c->zrelax[2]);CHKERRQ(ierr);
-  ierr = PetscViewerASCIIPrintf(viewer,"Common.nrelax            [%d,%d,%d]\n",(unsigned)c->nrelax[0],(unsigned)c->nrelax[1],(unsigned)c->nrelax[2]);CHKERRQ(ierr);
+  ierr = PetscViewerASCIIPrintf(viewer,"Common.nrelax            [%u,%u,%u]\n",(unsigned)c->nrelax[0],(unsigned)c->nrelax[1],(unsigned)c->nrelax[2]);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.prefer_upper      %d\n",c->prefer_upper);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer,"Common.print             %d\n",c->print);CHKERRQ(ierr);
-  {
-    //char buf[512],*p = buf;
-    for (i=0; i<c->nmethods; i++) {
-      //ierr = PetscSNPrintf(p,p-buf+sizeof buf,"%s%s",CholmodOrderingMethods[i],(i+1==c->nmethods)?"",",");
-      ierr = PetscViewerASCIIPrintf(viewer,"Ordering method %D%s:\n",i,i==c->selected?" [SELECTED]":"");CHKERRQ(ierr);
-      ierr = PetscViewerASCIIPrintf(viewer,"  lnz %g, fl %g, prune_dense %g, prune_dense2 %g\n",
-          c->method[i].lnz,c->method[i].fl,c->method[i].prune_dense,c->method[i].prune_dense2);CHKERRQ(ierr);
-    }
+  for (i=0; i<c->nmethods; i++) {
+    ierr = PetscViewerASCIIPrintf(viewer,"Ordering method %D%s:\n",i,i==c->selected?" [SELECTED]":"");CHKERRQ(ierr);
+    ierr = PetscViewerASCIIPrintf(viewer,"  lnz %g, fl %g, prune_dense %g, prune_dense2 %g\n",
+        c->method[i].lnz,c->method[i].fl,c->method[i].prune_dense,c->method[i].prune_dense2);CHKERRQ(ierr);
   }
   ierr = PetscViewerASCIIPrintf(viewer,"Common.postorder         %d\n",chol->common->postorder);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPopTab(viewer);CHKERRQ(ierr);
@@ -237,11 +269,11 @@ static PetscErrorCode MatSolve_CHOLMOD(Mat F,Vec B,Vec X)
 
   PetscFunctionBegin;
   ierr = VecWrapCholmod(B,&cholB);CHKERRQ(ierr);
-  cholX = cholmod_solve(CHOLMOD_A,chol->factor,&cholB,chol->common);
+  cholX = cholmod_X_solve(CHOLMOD_A,chol->factor,&cholB,chol->common);
   if (!cholX) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_LIB,"CHOLMOD failed");
   ierr = VecGetArray(X,&x);CHKERRQ(ierr);
   ierr = PetscMemcpy(x,cholX->x,cholX->nrow*sizeof(*x));CHKERRQ(ierr);
-  ierr = !cholmod_free_dense(&cholX,chol->common);CHKERRQ(ierr);
+  ierr = !cholmod_X_free_dense(&cholX,chol->common);CHKERRQ(ierr);
   ierr = VecRestoreArray(X,&x);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
@@ -256,7 +288,7 @@ static PetscErrorCode MatCholeskyFactorNumeric_CHOLMOD(Mat F,Mat A,const MatFact
 
   PetscFunctionBegin;
   ierr = MatWrapCholmod_seqsbaij(A,&cholA);CHKERRQ(ierr);
-  ierr = !cholmod_factorize(&cholA,chol->factor,chol->common);CHKERRQ(ierr);
+  ierr = !cholmod_X_factorize(&cholA,chol->factor,chol->common);CHKERRQ(ierr);
   F->ops->solve          = MatSolve_CHOLMOD;
   F->ops->solvetranspose = MatSolve_CHOLMOD;
   PetscFunctionReturn(0);
@@ -271,15 +303,14 @@ static PetscErrorCode MatCholeskyFactorSymbolic_CHOLMOD(Mat F,Mat A,IS perm,cons
   cholmod_sparse cholA;
 
   PetscFunctionBegin;
-  ierr = CholmodStart(F);CHKERRQ(ierr);
   ierr = MatWrapCholmod_seqsbaij(A,&cholA);CHKERRQ(ierr);
   if (perm) {
     const PetscInt *ip;
     ierr = ISGetIndices(perm,&ip);CHKERRQ(ierr);
-    chol->factor = cholmod_analyze_p(&cholA,(PetscInt*)ip,0,0,chol->common);
+    chol->factor = cholmod_X_analyze_p(&cholA,(PetscInt*)ip,0,0,chol->common);
     ierr = ISRestoreIndices(perm,&ip);CHKERRQ(ierr);
   } else {
-    chol->factor = cholmod_analyze(&cholA,chol->common);
+    chol->factor = cholmod_X_analyze(&cholA,chol->common);
   }
   if (!chol->factor) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_LIB,"CHOLMOD factorization failed");
   F->ops->choleskyfactornumeric = MatCholeskyFactorNumeric_CHOLMOD;
@@ -297,36 +328,33 @@ PetscErrorCode MatFactorGetSolverPackage_seqsbaij_cholmod(Mat A,const MatSolverP
 }
 EXTERN_C_END
 
-
 /*MC
-  MATSOLVERCHOLMOD = "umfpack" - A matrix type providing direct solvers (LU) for sequential matrices
-  via the external package UMFPACK.
+  MATSOLVERCHOLMOD = "cholmod" - A matrix type providing direct solvers (Cholesky) for sequential matrices
+  via the external package CHOLMOD.
 
-  ./configure --download-umfpack to install PETSc to use UMFPACK
+  ./configure --download-cholmod to install PETSc to use CHOLMOD
 
-  Consult UMFPACK documentation for more information about the Control parameters
+  Consult CHOLMOD documentation for more information about the Common parameters
   which correspond to the options database keys below.
 
   Options Database Keys:
-+ -Mat_CHOLMOD_prl - UMFPACK print level: Control[UMFPACK_PRL]
-. -Mat_CHOLMOD_strategy <AUTO> (choose one of) AUTO UNSYMMETRIC SYMMETRIC 2BY2
-. -Mat_CHOLMOD_dense_col <alpha_c> - UMFPACK dense column threshold: Control[UMFPACK_DENSE_COL]
-. -Mat_CHOLMOD_dense_row <0.2>: Control[UMFPACK_DENSE_ROW]
-. -Mat_CHOLMOD_amd_dense <10>: Control[UMFPACK_AMD_DENSE]
-. -Mat_CHOLMOD_block_size <bs> - UMFPACK block size for BLAS-Level 3 calls: Control[UMFPACK_BLOCK_SIZE]
-. -Mat_CHOLMOD_2by2_tolerance <0.01>: Control[UMFPACK_2BY2_TOLERANCE]
-. -Mat_CHOLMOD_fixq <0>: Control[UMFPACK_FIXQ]
-. -Mat_CHOLMOD_aggressive <1>: Control[UMFPACK_AGGRESSIVE]
-. -Mat_CHOLMOD_pivot_tolerance <delta> - UMFPACK partial pivot tolerance: Control[UMFPACK_PIVOT_TOLERANCE]
-.  -Mat_CHOLMOD_sym_pivot_tolerance <0.001>: Control[UMFPACK_SYM_PIVOT_TOLERANCE]
-.  -Mat_CHOLMOD_scale <NONE> (choose one of) NONE SUM MAX
-. -Mat_CHOLMOD_alloc_init <delta> - UMFPACK factorized matrix allocation modifier: Control[UMFPACK_ALLOC_INIT]
-.  -Mat_CHOLMOD_droptol <0>: Control[UMFPACK_DROPTOL]
-- -Mat_CHOLMOD_irstep <maxit> - UMFPACK maximum number of iterative refinement steps: Control[UMFPACK_IRSTEP]
+  -mat_cholmod_dbound <0>: Minimum absolute value of diagonal entries of D (None)
+  -mat_cholmod_grow0 <1.2>: Global growth ratio when factors are modified (None)
+  -mat_cholmod_grow1 <1.2>: Column growth ratio when factors are modified (None)
+  -mat_cholmod_grow2 <5>: Affine column growth constant when factors are modified (None)
+  -mat_cholmod_maxrank <8>: Max rank of update, larger values are faster but use more memory [2,4,8] (None)
+  -mat_cholmod_factor <AUTO> (choose one of) SIMPLICIAL AUTO SUPERNODAL
+  -mat_cholmod_supernodal_switch <40>: flop/nnz_L threshold for switching to supernodal factorization (None)
+  -mat_cholmod_final_asis: <TRUE> Leave factors "as is" (None)
+  -mat_cholmod_final_pack: <TRUE> Pack the columns when finished (use FALSE if the factors will be updated later) (None)
+  -mat_cholmod_zrelax <0.8>: 3 real supernodal relaxed amalgamation parameters (None)
+  -mat_cholmod_nrelax <4>: 3 size_t supernodal relaxed amalgamation parameters (None)
+  -mat_cholmod_prefer_upper: <TRUE> Work with upper triangular form (faster when using fill-reducing ordering, slower in natural ordering) (None)
+  -mat_cholmod_print <3>: Verbosity level (None)
 
    Level: beginner
 
-.seealso: PCLU, MATSOLVERSUPERLU, MATSOLVERMUMPS, MATSOLVERSPOOLES, PCFactorSetMatSolverPackage(), MatSolverPackage
+.seealso: PCCHOLESKY, PCFactorSetMatSolverPackage(), MatSolverPackage
 M*/
 EXTERN_C_BEGIN
 #undef __FUNCT__
@@ -358,8 +386,9 @@ PetscErrorCode MatGetFactor_seqsbaij_cholmod(Mat A,MatFactorType ftype,Mat *F)
   B->assembled             = PETSC_TRUE;  /* required by -ksp_view */
   B->preallocated          = PETSC_TRUE;
 
+  ierr = CholmodStart(B);CHKERRQ(ierr);
+
   *F = B;
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
-
