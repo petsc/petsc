e7ab429c0f49301380518fc506fa104e1471be83
Shri Abhyankar <abhyshr@mcs.anl.gov>
2010-06-09 11:37:34 -0500

    Added DA support for the new vecload interface for binary and HDF5 viewer Added a test case for testing the new vecload interface with DA. Minor changes to VecLoadnew interface.
    Hg-commit: 2a4b6fb984a471be62296eebec3efc417b5e07d8

--- include/private/vecimpl.h
+++ include/private/vecimpl.h
@@ -186,6 +186,7 @@ typedef enum {UNALLOCATED,GPU,CPU,SAME} VecGPUFlag;
 EXTERN PetscErrorCode VecDuplicateVecs_Default(Vec,PetscInt,Vec *[]);
 EXTERN PetscErrorCode VecDestroyVecs_Default(Vec [],PetscInt);
 EXTERN PetscErrorCode VecLoadIntoVector_Default(PetscViewer,Vec);
+EXTERN PetscErrorCode VecLoadnew_Binary(PetscViewer,Vec);
 
 extern PetscInt NormIds[7];  /* map from NormType to IDs used to cache/retreive values of norms */
 
