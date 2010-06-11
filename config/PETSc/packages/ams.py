d8102e02c9dc3056b8e63dfd55f572440f05c22f
Barry Smith <bsmith@mcs.anl.gov>
2010-06-11 11:35:48 -0500

    added AMS webpage added forgotten ams.py added java.py
    Hg-commit: 45df84c1c89c0c54ef24ced526ca5d9570ee997b

---
+++ config/PETSc/packages/ams.py
@@ -0,0 +1,22 @@
+import PETSc.package
+import os
+
+class Configure(PETSc.package.NewPackage):
+  def __init__(self, framework):
+    PETSc.package.NewPackage.__init__(self, framework)
+#    self.download  = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/ams-dev.tar.gz']
+#                      ssh://petsc@petsc.cs.iit.edu//hg/petsc/ams-dev 
+#    Does not currently support automatic download and install
+    self.functions = ['AMS_Memory_create']
+    self.includes  = ['ams.h']
+    self.liblist   = [['libamspub.a','libamsutilmt.a']]
+    return
+
+  def setupDependencies(self, framework):
+    PETSc.package.NewPackage.setupDependencies(self, framework)
+    self.deps       = [self.mpi]  
+    return
+
+  def configureLibrary(self):
+    PETSc.package.NewPackage.configureLibrary(self)
+    self.addDefine('AMS_DIR', '"'+os.path.dirname(self.include[0])+'"')
