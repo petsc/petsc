554bce71b4ba1e94422acd2309fd50eb18b850ea
Barry Smith <bsmith@mcs.anl.gov>
2010-06-15 13:54:35 -0500

    changed --with-c-support to use ArgBool because --with-c-support=0 actually turned on c support did not turn it off
    Hg-commit: 8cf70ae3fe6c5a40623d44910d8f2a4669cc4775

--- config/PETSc/utilities/languages.py
+++ config/PETSc/utilities/languages.py
@@ -18,8 +18,8 @@ class Configure(config.base.Configure):
   def setupHelp(self, help):
     import nargs
     help.addArgument('PETSc', '-with-clanguage=<C or C++>', nargs.Arg(None, 'C', 'Specify C or C++ language'))
-    help.addArgument('PETSc', '-with-c++-support', nargs.Arg(None, 0, 'When building C, compile C++ portions of external libraries (e.g. Prometheus)'))
-    help.addArgument('PETSc', '-with-c-support', nargs.Arg(None, 0, 'When building with C++, compile so may be used directly from C'))
+    help.addArgument('PETSc', '-with-c++-support', nargs.ArgBool(None, 0, 'When building C, compile C++ portions of external libraries (e.g. Prometheus)'))
+    help.addArgument('PETSc', '-with-c-support', nargs.ArgBool(None, 0, 'When building with C++, compile so may be used directly from C'))
     help.addArgument('PETSc', '-with-fortran', nargs.ArgBool(None, 1, 'Create and install the Fortran wrappers'))
     return
 
