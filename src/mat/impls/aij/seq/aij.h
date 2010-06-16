dd64fd00e44d03c4437a07269fe6b415adc258cc
Victor Minden <victorminden@gmail.com>
2010-06-16 10:46:53 -0500

    created cubuilder.py file to make symlinks with extension .cu to all .c files for use with nvcc for cuda
    Hg-commit: e5465f2536b8efde45ebfa6ff6388d80c6d0faa6

--- src/mat/impls/aij/seq/aij.h
+++ src/mat/impls/aij/seq/aij.h
@@ -78,6 +78,11 @@ typedef struct {
   PetscScalar      fshift,omega;                   /* last used omega and fshift */
 
   ISColoring       coloring;                  /* set with MatADSetColoring() used by MatADSetValues() */
+  //#if defined(PETSC_HAVE_CUDA)
+  //cusp::csr_matrix<PetscInt,PetscScalar,cusp::device_memory>* GPUmatrix; /* pointer to the csr matrix on the GPU */
+  //PetscCudaFlag    valid_GPU_matrix; /* this flag type is defined in vecimpl.h */
+  //#endif
+
 } Mat_SeqAIJ;
 
 /*
