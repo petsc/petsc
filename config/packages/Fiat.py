ed2b26757b5fbd3a12ac353d116fda25aee6a3b7
Matthew G Knepley <knepley@gmail.com>
2010-06-19 13:09:52 -0500

    Added Windows flag to FIAT
    Hg-commit: aa6fb43fb08a2f066e96d02859841f0abb4b7ddb

--- config/packages/Fiat.py
+++ config/packages/Fiat.py
@@ -6,6 +6,8 @@ class Configure(config.package.Package):
     self.download = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/fiat-dev.tar.gz', 'hg://www.fenics.org/hg/fiat', 'http://www.fenics.org/pub/software/fiat/FIAT-0.3.0.tar.gz']
     self.downloadname    = self.name.lower()
     self.archIndependent = 1
+    self.worksonWindows    = 1
+    self.downloadonWindows = 1
     return
 
   def setupDependencies(self, framework):
