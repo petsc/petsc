f89e2e87fbc2bf363b9a1b6d11cc8db8663cdbc0
Barry Smith <bsmith@mcs.anl.gov>
2010-06-18 23:10:38 -0500

    totally messed up cuda configure stuff
    Hg-commit: 35a68003d3768b062f1b14ee6609aec9d10bd705

--- config/PETSc/packages/cuda.py
+++ config/PETSc/packages/cuda.py
@@ -7,8 +7,26 @@ class Configure(PETSc.package.NewPackage):
     self.functions = ['cublasInit']
     self.includes  = ['cublas.h']
     self.liblist   = [['libcublas.a']]
-    self.double           = 0   # 1 means requires double precision 
+    self.double    = 0   # 1 means requires double precision 
+    self.cxx       = 1
     return
+
+  def setupDependencies(self, framework):
+    PETSc.package.NewPackage.setupDependencies(self, framework)
+    self.headers        = framework.require('config.headers',self)
 
   def getSearchDirectories(self):
     yield os.path.join('/usr','local','cuda')
+
+  def configureLibrary(self):
+    PETSc.package.NewPackage.configureLibrary(self)
+    self.setCompilers.pushLanguage('C++')
+#    if not self.headers.checkInclude([os.path.join('/usr','local','cuda')],[os.path.join('thrust','advance.h')]):
+#       raise RuntimeError('Cannot find thrust include files') 
+#    if not self.headers.checkInclude([os.path.join('/usr','local','cuda')],[os.path.join('cusp','monitor.h')]):
+#       raise RuntimeError('Cannot find cusp include files') 
+    self.setCompilers.popLanguage()
+    self.include = self.include+[os.path.join('/usr','local','cuda')]
+
+    
+# add checks that it is proper version o Cuda
---
+++ config/examples/arch-cuda.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+if __name__ == '__main__':
+  import sys
+  import os
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--with-cc=nvcc',
+    '--with-cxx=nvcc',
+    '--with-mpi=0',
+    '--with-cuda',
+    'PETSC_ARCH=arch-cuda',
+    '--with-precision=single',
+    '--with-fc=0',
+    '--with-clanguage=c++',
+    '-CXX_CXXFLAGS=-x cu'
+  ]
+  configure.petsc_configure(configure_options)
