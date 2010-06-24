3cca645c27e1f0e6f216f1de0f47cda0339762e5
Matthew G Knepley <knepley@gmail.com>
2010-06-24 20:32:42 +0800

    Added Thrust and Cusp configure
    Hg-commit: 2c056f6422b1c9b3142ecedbf73521c2986726ff

---
+++ config/packages/cusp.py
@@ -0,0 +1,36 @@
+from __future__ import generators
+import config.package
+
+class Configure(config.package.Package):
+  def __init__(self, framework):
+    config.package.Package.__init__(self, framework)
+    self.download        = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/cusp_1_2.tar.gz']
+    self.includes        = ['cusp/version.h']
+    self.includedir      = ''
+    self.cxx             = 1
+    self.archIndependent = 1
+    self.worksonWindows  = 1
+    return
+
+  def setupDependencies(self, framework):
+    config.package.Package.setupDependencies(self, framework)
+    self.thrust = framework.require('config.packages.thrust', self)
+    self.deps   = [self.thrust]
+    return
+
+  def Install(self):
+    import shutil
+    import os
+    self.framework.log.write('cuspDir = '+self.packageDir+' installDir '+self.installDir+'\n')
+    srcdir = os.path.join(self.packageDir, 'cusp')
+    destdir = os.path.join(self.installDir, 'include', 'cusp')
+    try:
+      if os.path.isdir(destdir): shutil.rmtree(destdir)
+      shutil.copytree(srcdir,destdir)
+    except RuntimeError,e:
+      raise RuntimeError('Error installing Cusp include files: '+str(e))
+    return self.installDir
+
+  def getSearchDirectories(self):
+    yield ''
+    return
---
+++ config/packages/thrust.py
@@ -0,0 +1,30 @@
+from __future__ import generators
+import config.package
+
+class Configure(config.package.Package):
+  def __init__(self, framework):
+    config.package.Package.__init__(self, framework)
+    self.download        = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/thrust_0_1.tar.gz']
+    self.includes        = ['thrust/version.h']
+    self.includedir      = ''
+    self.cxx             = 1
+    self.archIndependent = 1
+    self.worksonWindows  = 1
+    return
+
+  def Install(self):
+    import shutil
+    import os
+    self.framework.log.write('thrustDir = '+self.packageDir+' installDir '+self.installDir+'\n')
+    srcdir = os.path.join(self.packageDir, 'thrust')
+    destdir = os.path.join(self.installDir, 'include', 'thrust')
+    try:
+      if os.path.isdir(destdir): shutil.rmtree(destdir)
+      shutil.copytree(srcdir,destdir)
+    except RuntimeError,e:
+      raise RuntimeError('Error installing Thrust include files: '+str(e))
+    return self.installDir
+
+  def getSearchDirectories(self):
+    yield ''
+    return
