2082002fa456e744b3fbc7620b815c3fc59302ca
Barry Smith <bsmith@mcs.anl.gov>
2010-06-28 22:09:33 -0500

    display basic information about Mat and Vec object for Totalview debugger :w! :w!
    Hg-commit: d5bf9a455370c2b92b8a8df0eab291460186a475

--- src/mat/interface/matrix.c
+++ src/mat/interface/matrix.c
@@ -730,12 +730,23 @@ PetscErrorCode PETSCMAT_DLLEXPORT MatView(Mat mat,PetscViewer viewer)
   PetscFunctionReturn(0);
 }
 
+#if defined(PETSC_USE_DEBUGGING)
 #include "../src/sys/totalview/tv_data_display.h"
 static int TV_display_type(const struct _p_Mat *mat)
 {
+  PetscErrorCode ierr;
+  size_t         len;
+
   TV_add_row("Local rows", "int", &mat->rmap->n);
-  return TV_format_OK ;
+  TV_add_row("Local columns", "int", &mat->cmap->n);
+  TV_add_row("Global rows", "int", &mat->rmap->N);
+  TV_add_row("Global columns", "int", &mat->cmap->N);
+  ierr = PetscStrlen(((PetscObject)mat)->type_name,&len);CHKERRQ(ierr);
+  ierr = PetscSNPrintf(type,32,"char[%d]",(int)len);CHKERRQ(ierr);
+  TV_add_row("Typename", type, ((PetscObject)mat)->type_name);
+  return TV_format_OK;
 }
+#endif
 
 #undef __FUNCT__  
 #define __FUNCT__ "MatLoadnew"
