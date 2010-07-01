86d161a7bd4c427c3b36a005a86631e39ebfd053
Shri Abhyankar <abhyshr@mcs.anl.gov>
2010-07-01 15:38:12 -0500

    Fixed bug in MatSetSizes_SeqDense. Added new matload interface routines for dense mpi matrix.
    Hg-commit: a448861068ae3fe5cef7d25ab2c832cbe1141fec

--- src/mat/impls/dense/mpi/mpidense.h
+++ src/mat/impls/dense/mpi/mpidense.h
@@ -25,6 +25,7 @@ typedef struct {
 } Mat_MPIDense;
 
 EXTERN PetscErrorCode MatLoad_MPIDense(PetscViewer, const MatType,Mat*);
+EXTERN PetscErrorCode MatLoadnew_MPIDense(PetscViewer,Mat);
 EXTERN PetscErrorCode MatSetUpMultiply_MPIDense(Mat);
 EXTERN PetscErrorCode MatGetSubMatrices_MPIDense(Mat,PetscInt,const IS[],const IS[],MatReuse,Mat *[]);
 EXTERN PetscErrorCode MatEqual_MPIDense(Mat,Mat,PetscTruth*);
