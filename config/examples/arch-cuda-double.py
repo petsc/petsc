267d0bc4dfdf244f9d69e225e7a10460be5be027
Victor Minden <victorminden@gmail.com>
2010-07-12 14:52:25 -0500

    switched arch-cuda-double to use -arch=sm_13 instead of -arch=sm_20
    Hg-commit: 9a86c66b249d936d2787213e53659810731c1b26

--- config/examples/arch-cuda-double.py
+++ config/examples/arch-cuda-double.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     '--with-precision=double',
     '--with-fc=0',
     '--with-clanguage=c++',
-    '--CXX_CXXFLAGS=-x cu -arch sm_20'
+    '--CXX_CXXFLAGS=-x cu -arch sm_13'
   ]
   configure.petsc_configure(configure_options)
