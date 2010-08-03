7fdfa20c8a3026baf0a020fca4750efe180fe22f
Dmitry Karpeev <karpeev@mcs.anl.gov>
2010-08-03 13:16:48 -0500

    Simple python test of PetscFwk (needs petsc4py).
    Hg-commit: be6cffcf8d32dbc93a2cb30d1934480a501bb9d0

---
+++ src/sys/fwk/examples/tutorials/ex1.py
@@ -0,0 +1,36 @@
+import sys, petsc4py
+petsc4py.init(sys.argv)
+
+from petsc4py import PETSc
+
+def DensityField(fwk, conf, c):
+    if c is None:
+        # Create a Fwk to serve as the component
+        c = PETSc.Fwk().create(fwk.comm)
+        N = 32
+        d = 2
+        da = PETSc.DA().create(dim=3,dof=d,sizes=(N,N,N))
+        vec = da.createNaturalVector()
+        v = vec.getArray()
+        v = v.reshape((N,N,N,d))
+        from math import sin as sin, pi as pi
+        for i in range(N):
+            for j in range(N):
+                for k in range(N):
+                    for l in range(d):
+                        v[i,j,k,l] = sin(2*pi*i/N)*sin(2*pi*j/N)*sin(2*pi*k/N)
+        c.compose("mesh", da)
+        c.compose("density",vec)
+    return c
+
+fwk = PETSc.Fwk().create()
+fwk.registerComponent("DensityField", "./ex1.py:DensityField")
+fwk.registerComponent("Viz", "./ex1_viz.py:Viz")
+#fwk.view()
+fwk.configure("test")
+
+
+for i in range(10000000):
+    pass
+
+del fwk
