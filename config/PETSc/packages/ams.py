9f9412a2b0b0aa0af6883efd319d86e8e30a0a10
Barry Smith <bsmith@mcs.anl.gov>
2010-09-29 13:02:43 -0500

    finished test example for MatGetColumnNorms() starting the --download-ams code
    Hg-commit: 3a4afe7032f0b73fe6a72e53afdf0da421675395

--- config/PETSc/packages/ams.py
+++ config/PETSc/packages/ams.py
@@ -4,7 +4,7 @@ import os
 class Configure(PETSc.package.NewPackage):
   def __init__(self, framework):
     PETSc.package.NewPackage.__init__(self, framework)
-#    self.download  = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/ams-dev.tar.gz']
+    self.download  = ['http://petsc.cs.iit.edu/petsc/petsc-dev/archive/tip.tar.gz']
 #                      ssh://petsc@petsc.cs.iit.edu//hg/petsc/ams-dev 
 #    Does not currently support automatic download and install
     self.functions = ['AMS_Memory_create']
@@ -17,6 +17,33 @@ class Configure(PETSc.package.NewPackage):
     self.deps       = [self.mpi]  
     return
 
+  def Install(self):
+    import os
+
+    g = open(os.path.join(self.packageDir,'makeinc'),'w')
+    g.write('AR           = '+self.setCompilers.AR+'\n')
+    g.write('RANLIB       = '+self.setCompilers.RANLIB+'\n')
+    g.write('RM           = rm -f\n')
+    g.write('CP           = cp -f\n')
+    self.setCompilers.pushLanguage('C')
+    g.write('CC           = '+self.setCompilers.getCompiler()+'\n')
+    g.write('CLINKER      = ${CC}\n')
+    if self.setCompilers.isDarwin():    
+      g.write('LINKSHARED   = ${CC} -dynamiclib -single_module -multiply_defined suppress -undefined dynamic_lookup\n')
+    else:
+      g.write('LINKSHARED   = ${CC} -dynamiclib\n')      
+    self.setCompilers.popLanguage()
+
+    if self.installNeeded('makeinc'):
+      try:
+        self.logPrintBox('Compiling ams; this may take several minutes')
+        output,err,ret = PETSc.package.NewPackage.executeShellCommand('cd '+self.packageDir+' && make all && cp lib/* '+os.path.join(self.installDir,'lib')+' &&  cp -f include/*.h '+os.path.join(self.installDir,self.includedir)+'/.', timeout=2500, log = self.framework.log)
+      except RuntimeError, e:
+        raise RuntimeError('Error running make on ams: '+str(e))
+      self.postInstall(output+err,'makeinc')
+    return self.installDir
+
+  
   def configureLibrary(self):
     PETSc.package.NewPackage.configureLibrary(self)
     self.addDefine('AMS_DIR', '"'+os.path.dirname(self.include[0])+'"')
--- src/mat/examples/tests/ex138.c
+++ src/mat/examples/tests/ex138.c
@@ -1,5 +1,5 @@
 
-static char help[] = "Tests MatGetColumnVector() for matrix read from file.";
+static char help[] = "Tests MatGetColumnNorms() for matrix read from file.";
 
 #include "petscmat.h"
 
@@ -9,12 +9,15 @@ int main(int argc,char **args)
 {
   Mat            A;
   PetscErrorCode ierr;
-  PetscReal      norm;
+  PetscReal      *norms;
   char           file[PETSC_MAX_PATH_LEN];
   PetscBool      flg;
   PetscViewer    fd;
+  PetscInt       n;
+  PetscMPIInt    rank;
 
   PetscInitialize(&argc,&args,(char *)0,help);
+  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = PetscOptionsGetString(PETSC_NULL,"-f",file,PETSC_MAX_PATH_LEN,&flg);CHKERRQ(ierr);
   if (!flg) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_USER,"Must indicate binary file with the -f option");
   ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,file,FILE_MODE_READ,&fd);CHKERRQ(ierr);
@@ -22,9 +25,14 @@ int main(int argc,char **args)
   ierr = MatLoad(A,fd);CHKERRQ(ierr);
   ierr = PetscViewerDestroy(fd);CHKERRQ(ierr);
 
-  ierr = MatGetColumnNorms(A,NORM_2,&norm);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"2 norm %G\n",norm);CHKERRQ(ierr);
+  ierr = MatGetSize(A,PETSC_NULL,&n);CHKERRQ(ierr);
+  ierr = PetscMalloc(n*sizeof(PetscReal),&norms);CHKERRQ(ierr);
+  ierr = MatGetColumnNorms(A,NORM_2,norms);CHKERRQ(ierr); 
+  if (!rank) {
+    ierr = PetscRealView(n,norms,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  }
 
+  ierr = PetscFree(norms);CHKERRQ(ierr);
   ierr = MatDestroy(A);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return 0;
