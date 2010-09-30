bc5a2726c44698c75ff9825b5afb5fb30b6437b5
Shri Abhyankar <abhyshr@mcs.anl.gov>
2010-09-30 00:08:52 -0500

    Copy over the matrix name for Seq and MPI MatAXPY
    Hg-commit: 02db16a72472d0374b34e9b42cced86748c28001

--- src/mat/impls/aij/seq/aij.c
+++ src/mat/impls/aij/seq/aij.c
@@ -2347,6 +2347,7 @@ PetscErrorCode MatAXPY_SeqAIJ(Mat Y,PetscScalar a,Mat X,MatStructure str)
     PetscInt *nnz;
     ierr = PetscMalloc(Y->rmap->N*sizeof(PetscInt),&nnz);CHKERRQ(ierr);
     ierr = MatCreate(((PetscObject)Y)->comm,&B);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)B,((PetscObject)Y)->name);CHKERRQ(ierr);
     ierr = MatSetSizes(B,Y->rmap->n,Y->cmap->n,Y->rmap->N,Y->cmap->N);CHKERRQ(ierr);
     ierr = MatSetType(B,MATSEQAIJ);CHKERRQ(ierr);
     ierr = MatAXPYGetPreallocation_SeqAIJ(Y,X,nnz);CHKERRQ(ierr);
