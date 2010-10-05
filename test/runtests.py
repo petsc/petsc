c1a89dd6c8bbc9e194fe24e6133f828111343d0c
Lisandro Dalcin <dalcinl@gmail.com>
2010-10-05 19:27:37 -0300

    Reimplement runtests.py from scratch

--- test/runtests.py
+++ test/runtests.py
@@ -1,104 +1,176 @@
-import sys, os, glob
+import sys, os
+import optparse
 import unittest
 
-try:
-    import petsc4py
-except ImportError:
+def getoptionparser():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-q", "--quiet",
+                      action="store_const", const=0, dest="verbose", default=1,
+                      help="do not print status messages to stdout")
+    parser.add_option("-v", "--verbose",
+                      action="store_const", const=2, dest="verbose", default=1,
+                      help="print status messages to stdout")
+    parser.add_option("-i", "--include", type="string",
+                      action="append",  dest="include", default=[],
+                      help="include tests matching PATTERN", metavar="PATTERN")
+    parser.add_option("-e", "--exclude", type="string",
+                      action="append", dest="exclude", default=[],
+                      help="exclude tests matching PATTERN", metavar="PATTERN")
+    parser.add_option("--path", type="string",
+                      action="append", dest="path", default=[],
+                      help="prepend PATH to sys.path", metavar="PATH")
+    parser.add_option("--refleaks", type="int",
+                      action="store", dest="repeats", default=3,
+                      help="run tests REPEAT times in a loop to catch refleaks",
+                      metavar="REPEAT")
+    parser.add_option("--arch", type="string",
+                      action="store", dest="arch", default=None,
+                      help="use PETSC_ARCH",
+                      metavar="PETSC_ARCH")
+    parser.add_option("-s","--summary",
+                      action="store_true", dest="summary", default=0,
+                      help="print PETSc log summary")
+    return parser
+
+def getbuilddir():
     from distutils.util import get_platform
-    plat_specifier = ".%s-%s" % (get_platform(), sys.version[0:3])
-    os.path.split(__file__)[0]
-    path = os.path.join(os.path.split(__file__)[0], os.path.pardir,
-                        'build', 'lib' + plat_specifier)
-    if not os.path.exists(path) and os.path.exists(path+'-pydebug'):
-        path += '-pydebug'
-    sys.path.append(path)
-    import petsc4py
+    s = os.path.join("build", "lib.%s-%.3s" % (get_platform(), sys.version))
+    if (sys.version[:3] >= '2.6' and
+        hasattr(sys, 'gettotalrefcount')):
+        s += '-pydebug'
+    return s
 
-args=['-malloc',
-      '-malloc_debug',
-      '-malloc_dump',
-      #'-log_summary',
-      ]
-if '-petsc' in sys.argv:
-    idx = sys.argv.index('-petsc')
-    args.extend(sys.argv[idx+1:])
-    del sys.argv[idx:]
-    del idx
+def setup_python(options):
+    rootdir = os.path.dirname(os.path.dirname(__file__))
+    builddir = os.path.join(rootdir, getbuilddir())
+    if os.path.exists(builddir):
+        sys.path.insert(0, builddir)
+    if options.path:
+        path = options.path[:]
+        path.reverse()
+        for p in path:
+            sys.path.insert(0, p)
 
-petsc4py.init(args)
-from petsc4py import PETSc
+def setup_unittest(options):
+    from unittest import TestSuite
+    try:
+        from unittest.runner import _WritelnDecorator
+    except ImportError:
+        from unittest import _WritelnDecorator
+    #
+    if sys.version[:3] < '2.4':
+        TestSuite.__iter__ = lambda self: iter(self._tests)
+    #
+    writeln_orig = _WritelnDecorator.writeln
+    def writeln(self, message=''):
+        try: self.stream.flush()
+        except: pass
+        writeln_orig(self, message)
+        try: self.stream.flush()
+        except: pass
+    _WritelnDecorator.writeln = writeln
 
-version = PETSc.Sys.getVersion()
-exclude = {}
+def import_package(options, pkgname):
+    args=[sys.argv[0],
+          '-malloc',
+          '-malloc_debug',
+          '-malloc_dump',
+          '-log_summary',
+          ]
+    if not options.summary:
+        del args[-1]
+    package = __import__(pkgname)
+    package.init(args, arch=options.arch)
+    return package
 
-def test_cases():
+def getprocessorinfo():
+    from petsc4py.PETSc import COMM_WORLD
+    rank = COMM_WORLD.getRank()
+    name =  os.uname()[1]
+    return (rank, name)
+
+def writeln(message='', endl='\n'):
+    sys.stderr.flush()
+    sys.stderr.write(message+endl)
+    sys.stderr.flush()
+
+def print_banner(options, package):
+    rank, name = getprocessorinfo()
+    x, y = sys.version_info[:2]
+    if options.verbose:
+        writeln("[%d@%s] Py%d%d (%s) - %s (%s)"
+                % (rank, name, x, y,
+                   sys.executable,
+                   package.__name__,
+                   package.__path__[0]))
+
+def load_tests(options, args):
     from glob import glob
-    directory = os.path.split(__file__)[0]
-    pattern = os.path.join(directory, 'test_*.py')
-    test_list = []
-    for test_file in glob(pattern):
-        filename = os.path.basename(test_file)
-        modulename = os.path.splitext(filename)[0]
-        if modulename in exclude:
-            if exclude[modulename] is True or \
-               exclude[modulename] == version:
-                continue
-        test = __import__(modulename)
-        test_list.append(test)
-    return test_list
+    testsuitedir = os.path.dirname(__file__)
+    sys.path.insert(0, testsuitedir)
+    pattern = 'test_*.py'
+    wildcard = os.path.join(testsuitedir, pattern)
+    testfiles = glob(wildcard)
+    testfiles.sort()
+    testsuite = unittest.TestSuite()
+    for testfile in testfiles:
+        filename = os.path.basename(testfile)
+        modname = os.path.splitext(filename)[0]
+        testname = modname.replace('test_','')
+        if (modname in options.exclude or
+            testname in options.exclude):
+            continue
+        if (options.include and
+            not (modname  in options.include or
+                 testname in options.include)):
+            continue
+        module = __import__(modname)
+        loader = unittest.TestLoader()
+        for arg in args:
+            try:
+                cases = loader.loadTestsFromNames((arg,), module)
+                testsuite.addTests(cases)
+            except AttributeError:
+                pass
+        if not args:
+            cases = loader.loadTestsFromModule(module)
+            testsuite.addTests(cases)
+    return testsuite
 
+def run_tests(options, testsuite):
+    runner = unittest.TextTestRunner(verbosity=options.verbose)
+    result = runner.run(testsuite)
+    return result.wasSuccessful()
 
-def runtests(*args, **kargs):
-    PETSc.COMM_WORLD.barrier()
-    sys.stderr.flush()
-    sys.stderr.write("petsc4py imported from '%s'\n" % petsc4py.__path__[0])
-    (major, minor, micro), patch = PETSc.Sys.getVersion(patch=True)
-    r = PETSc.Sys.getVersionInfo()['release']
-    if r: release = 'release'
-    else: release = 'development'
-    arch = PETSc.__arch__
-    sys.stderr.write(
-        "using PETSc %d.%d.%d-p%d %s (configuration: '%s')\n" % \
-        (major, minor, micro, patch, release, arch) )
-    sys.stderr.flush()
-    PETSc.COMM_WORLD.barrier()
+def run_tests_leaks(options, testsuite):
+    from sys import gettotalrefcount
+    from gc import collect
+    rank, name = getprocessorinfo()
+    r1 = r2 = 0
+    repeats = options.repeats
+    while repeats:
+        repeats -= 1
+        collect()
+        r1 = gettotalrefcount()
+        run_tests(options, testsuite)
+        collect()
+        r2 = gettotalrefcount()
+        writeln('[%d@%s] refleaks:  (%d - %d) --> %d'
+                % (rank, name, r2, r1, r2-r1))
 
-    for test in test_cases():
-        try:
-            if PETSc.COMM_WORLD.getRank() == 0:
-                sys.stderr.flush()
-                sys.stderr.write("\nrunning %s\n" % test.__name__)
-                sys.stderr.flush()
-            PETSc.COMM_WORLD.barrier()
-            unittest.main(test, *args, **kargs)
-            PETSc.COMM_WORLD.barrier()
-        except SystemExit:
-            pass
-
-def runtestsleak(repeats, *args, **kargs):
-    import gc
-    alltests = test_cases()
-    gc.collect()
-    for i in range(repeats):
-        gc.collect()
-        r1 = sys.gettotalrefcount()
-        for test in alltests:
-            try: unittest.main(test, *args, **kargs)
-            except SystemExit: pass
-        gc.collect()
-        r2 = sys.gettotalrefcount()
-        sys.stderr.flush()
-        sys.stderr.write('\nREF LEAKS -- before: %d, after: %d, diff: [%d]\n' % (r1, r2, r2-r1))
-        sys.stderr.flush()
+def main(pkgname):
+    parser = getoptionparser()
+    (options, args) = parser.parse_args()
+    setup_python(options)
+    setup_unittest(options)
+    package = import_package(options, pkgname)
+    print_banner(options, package)
+    testsuite = load_tests(options, args)
+    success = run_tests(options, testsuite)
+    if hasattr(sys, 'gettotalrefcount'):
+        run_tests_leaks(options, testsuite)
+    sys.exit(not success)
 
 if __name__ == '__main__':
-    runtests()
-    if hasattr(sys, 'gettotalrefcount'):
-        try:
-            from unittest.runner import _WritelnDecorator
-        except ImportError:
-            from unittest import _WritelnDecorator
-        def dummy_write(self,*args): pass
-        _WritelnDecorator.write   = dummy_write
-        _WritelnDecorator.writeln = dummy_write
-        runtestsleak(5)
+    main('petsc4py')
