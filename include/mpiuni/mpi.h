0d14e032efa140ece4e6bb5833eab43261d891e5
Barry Smith <bsmith@mcs.anl.gov>
2010-10-12 20:02:44 -0500

    bug fix, MPI_Comm_split() was all wrong in MPIUni, needs to call MPI_Comm_dup()
    Hg-commit: 4a4fe3a71c0340d61114a98250fee935bdcae804

--- include/mpiuni/mpi.h
+++ include/mpiuni/mpi.h
@@ -633,7 +633,7 @@ extern int    MPI_Finalized(int*);
      MPIUNI_TMP = (void*)(MPIUNI_INTPTR) (comm2),\
      *(result)=MPI_IDENT,\
      MPI_SUCCESS)
-#define MPI_Comm_split(comm,color,key,newcomm) MPI_SUCCESS
+#define MPI_Comm_split(comm,color,key,newcomm) MPI_Comm_dup(comm,newcomm)
 #define MPI_Comm_test_inter(comm,flag) (*(flag)=1,MPI_SUCCESS)
 #define MPI_Comm_remote_size(comm,size) (*(size)=1,MPI_SUCCESS)
 #define MPI_Comm_remote_group(comm,group) MPI_SUCCESS
