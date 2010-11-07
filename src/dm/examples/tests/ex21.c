b1757049e4937acd8e699a08012e0f5de9be3b21
Jed Brown <jed@59A2.org>
2010-11-07 22:50:29 +0100

    Add DMGetInjection_DA_3D and test from Dave May
    Hg-commit: 61b9996763ec9977a6f017f74e058a735548fe9d

---
+++ src/dm/examples/tests/ex21.c
@@ -0,0 +1,121 @@
+static const char help[] = "Test DMGetInjection for mapping coordinates in 3D";
+
+#include <petscvec.h>
+#include <petscmat.h>
+#include <petscdm.h>
+
+#undef __FUNCT__  
+#define __FUNCT__ "test1_DAInjection3d"
+PetscErrorCode test1_DAInjection3d( PetscInt mx, PetscInt my, PetscInt mz )
+{
+  PetscErrorCode ierr;
+  DM dac,daf;
+  PetscViewer vv;
+  Vec ac,af;
+  PetscInt periodicity;
+  DMDAPeriodicType pt;
+
+  PetscFunctionBegin;
+  periodicity = 0;
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-periodic", &periodicity, PETSC_NULL);CHKERRQ(ierr);
+  if (periodicity==1) {
+    pt = DMDA_XPERIODIC;
+  } else if (periodicity==2) {
+    pt = DMDA_YPERIODIC;
+  } else if (periodicity==3) {
+    pt = DMDA_ZPERIODIC;
+  } else {
+    pt = DMDA_NONPERIODIC;
+  }
+
+  ierr = DMDACreate3d(PETSC_COMM_WORLD, pt, DMDA_STENCIL_BOX,
+                      mx+1, my+1,mz+1,
+                      PETSC_DECIDE, PETSC_DECIDE,PETSC_DECIDE,
+                      1, /* 1 dof */
+                      1, /* stencil = 1 */
+                      PETSC_NULL,PETSC_NULL,PETSC_NULL,
+                      &daf); CHKERRQ(ierr);
+
+  ierr = DMSetFromOptions(daf);CHKERRQ(ierr);
+
+  ierr = DMCoarsen(daf,PETSC_NULL,&dac);CHKERRQ(ierr);
+
+  ierr = DMDASetUniformCoordinates(dac, -1.0,1.0, -1.0,1.0, -1.0,1.0 );CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(daf, -1.0,1.0, -1.0,1.0, -1.0,1.0 );CHKERRQ(ierr);
+
+  {
+    DM         cdaf,cdac;
+    Vec        coordsc,coordsf,coordsf2;
+    VecScatter inject;
+    Mat        interp;
+    PetscReal  norm;
+
+    ierr = DMDAGetCoordinateDA(dac,&cdac);CHKERRQ(ierr);
+    ierr = DMDAGetCoordinateDA(daf,&cdaf);CHKERRQ(ierr);
+
+    ierr = DMDAGetCoordinates(dac,&coordsc);CHKERRQ(ierr);
+    ierr = DMDAGetCoordinates(daf,&coordsf);CHKERRQ(ierr);
+
+    ierr = DMGetInjection(cdac,cdaf,&inject);CHKERRQ(ierr);
+
+    ierr = VecScatterBegin(inject,coordsf,coordsc,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+    ierr = VecScatterEnd(inject  ,coordsf,coordsc,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+    ierr = VecScatterDestroy(inject);CHKERRQ(ierr);
+
+    ierr = DMGetInterpolation(cdac,cdaf,&interp,PETSC_NULL);CHKERRQ(ierr);
+    ierr = VecDuplicate(coordsf,&coordsf2);CHKERRQ(ierr);
+    ierr = MatInterpolate(interp,coordsc,coordsf2);CHKERRQ(ierr);
+    ierr = VecAXPY(coordsf2,-1.0,coordsf);CHKERRQ(ierr);
+    ierr = VecNorm(coordsf2,NORM_MAX,&norm);CHKERRQ(ierr);
+    /* The fine coordinates are only reproduced in certain cases */
+    if (pt == DMDA_NONPERIODIC && norm > 1.e-10) {ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm %A\n",norm);CHKERRQ(ierr);}
+    ierr = VecDestroy(coordsf2);CHKERRQ(ierr);
+    ierr = MatDestroy(interp);CHKERRQ(ierr);
+  }
+
+  if (0) {
+    ierr = DMCreateGlobalVector(dac,&ac); CHKERRQ(ierr);
+    ierr = VecZeroEntries(ac);CHKERRQ(ierr);
+
+    ierr = DMCreateGlobalVector(daf,&af); CHKERRQ(ierr);
+    ierr = VecZeroEntries(af);CHKERRQ(ierr);
+
+    ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD, "dac_7.vtk", &vv);CHKERRQ(ierr);
+    ierr = PetscViewerSetFormat(vv, PETSC_VIEWER_ASCII_VTK);CHKERRQ(ierr);
+    ierr = DMView(dac, vv);CHKERRQ(ierr);
+    ierr = VecView(ac, vv);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(vv);CHKERRQ(ierr);
+
+    ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD, "daf_7.vtk", &vv);CHKERRQ(ierr);
+    ierr = PetscViewerSetFormat(vv, PETSC_VIEWER_ASCII_VTK);CHKERRQ(ierr);
+    ierr = DMView(daf, vv);CHKERRQ(ierr);
+    ierr = VecView(af, vv);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(vv);CHKERRQ(ierr);
+    ierr = VecDestroy(ac);CHKERRQ(ierr);
+    ierr = VecDestroy(af);CHKERRQ(ierr);
+  }
+  ierr = DMDestroy(dac);CHKERRQ(ierr);
+  ierr = DMDestroy(daf);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  PetscErrorCode ierr;
+  PetscInt mx,my,mz;
+
+  ierr = PetscInitialize(&argc,&argv,0,0);
+  mx = 2;
+  my = 2;
+  mz = 2;
+  PetscOptionsGetInt(PETSC_NULL,"-mx", &mx, 0 );
+  PetscOptionsGetInt(PETSC_NULL,"-my", &my, 0 );
+  PetscOptionsGetInt(PETSC_NULL,"-mz", &mz, 0 );
+
+  test1_DAInjection3d(mx,my,mz);
+
+  ierr = PetscFinalize();
+  return 0;
+}
