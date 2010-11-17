8dd709a108ad52ff6a2b754ecf8d8aec2332f210
Barry Smith <bsmith@mcs.anl.gov>
2010-11-17 16:20:29 -0600

    got simple example working that has both float and double Vecs. Very crude vec/examples/tutorials/ex43.c (transplanted from 032d19a78960421f7f2c6d1b17bb899399adee74)
    Hg-commit: abe1d95df42623546700651665d9a72f9dddbb24

--- src/sys/objects/gtype.c
+++ src/sys/objects/gtype.c
@@ -33,33 +33,6 @@ PetscErrorCode PETSCSYS_DLLEXPORT PetscObjectGetType(PetscObject obj, const char
 }
 
 #undef __FUNCT__  
-#define __FUNCT__ "PetscObjectSetPrecision"
-/*@C
-   PetscObjectSetPrecision - sets the precision of numerical values used within a PetscObject
-
-   Not Collective
-
-   Input Parameter:
-+  obj - any PETSc object, for example a Vec, Mat or KSP.
-         Thus must be cast with a (PetscObject), for example, 
-         PetscObjectGetType((PetscObject)mat,&type);
--  precision - currently either PETSC_PRECISION_SINGLE or PETSC_PRECISION_DOUBLE
-
-
-   Level: advanced
-
-   Concepts: object type
-@*/
-PetscErrorCode PETSCSYS_DLLEXPORT PetscObjectSetType(PetscObject obj, PetscPrecision precision)
-{
-  PetscFunctionBegin;
-  PetscValidHeader(obj,1);
-  if (obj->type_name && obj->precision != precision) SETERRQ(obj->comm,PETSC_ERR_SUP,"Cannot change precision after object type is set");
-  obj->precision = precision;
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__  
 #define __FUNCT__ "PetscObjectSetType"
 /*@C
    PetscObjectSetType - Sets the object type of any PetscObject.
