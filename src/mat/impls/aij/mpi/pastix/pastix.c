b09c0905e0400a6d0e50b04b96aaa5afc8b9512d
Barry Smith <bsmith@mcs.anl.gov>
2010-12-30 20:41:50 -0600

    fixed bug in pastix interface, arrays are possibly freed by pastix so must use malloc()/free()  not PetscMalloc
    Hg-commit: f08535eeff57429b86894369c8244c4e0f4a286f

--- src/mat/impls/aij/mpi/pastix/pastix.c
+++ src/mat/impls/aij/mpi/pastix/pastix.c
@@ -164,6 +164,10 @@ PetscErrorCode MatConvertToCSC(Mat A,PetscTruth valOnly,PetscInt *n,PetscInt **c
   {
     PetscScalar *tmpvalues;
     PetscInt    *tmprows,*tmpcolptr;
+    tmpvalues = malloc(nnz*sizeof(PetscScalar)); if (!tmpvalues) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_MEM,"Unable to allocate memory");
+    tmprows   = malloc(nnz*sizeof(PetscInt));if (!tmprows) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_MEM,"Unable to allocate memory");
+    tmpcolptr = malloc((*n+1)*sizeof(PetscInt));if (!tmpcolptr) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_MEM,"Unable to allocate memory");
+
     ierr = PetscMalloc3(nnz,PetscScalar,&tmpvalues,nnz,PetscInt,&tmprows,(*n+1),PetscInt,&tmpcolptr);CHKERRQ(ierr);
     if (sizeof(PetscScalar) != sizeof(pastix_float_t)) {
       SETERRQ2(PETSC_ERR_SUP,"sizeof(PetscScalar) %d != sizeof(pastix_float_t) %d",sizeof(PetscScalar),sizeof(pastix_float_t));
@@ -185,7 +189,9 @@ PetscErrorCode MatConvertToCSC(Mat A,PetscTruth valOnly,PetscInt *n,PetscInt **c
     ierr = PetscMemcpy(*row,tmprows,((*colptr)[*n]-1)*sizeof(PetscInt));CHKERRQ(ierr);
     ierr = PetscMalloc(((*colptr)[*n]-1)*sizeof(PetscScalar),values);CHKERRQ(ierr);
     ierr = PetscMemcpy(*values,tmpvalues,((*colptr)[*n]-1)*sizeof(PetscScalar));CHKERRQ(ierr);
-    ierr = PetscFree3(tmpvalues,tmprows,tmpcolptr);CHKERRQ(ierr);
+    free(tmpvalues);
+    free(tmprows);
+    free(tmpcolptr);
   }
   PetscFunctionReturn(0);
 }
