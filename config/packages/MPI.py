4adfcfae02e7eb85e035f0c64d1536fc06c404a0
Satish Balay <balay@mcs.anl.gov>
2011-02-01 14:44:13 -0600

    Backed out changeset 55708eeba30d latest mpich install breaks on Mac
    Hg-commit: a4acbddc2c6a8f83cd58dfa093ebe56becec60e1

--- config/packages/MPI.py
+++ config/packages/MPI.py
@@ -12,7 +12,7 @@ class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
     self.download_openmpi   = ['http://www.open-mpi.org/software/ompi/v1.4/downloads/openmpi-1.4.3.tar.gz']
-    self.download_mpich     = ['http://www.mcs.anl.gov/research/projects/mpich2/downloads/tarballs/1.3.2/mpich2-1.3.2.tar.gz']
+    self.download_mpich     = ['http://www.mcs.anl.gov/research/projects/mpich2/downloads/tarballs/1.3.1/mpich2-1.3.1.tar.gz']
     self.download           = ['redefine']
     self.functions          = ['MPI_Init', 'MPI_Comm_create']
     self.includes           = ['mpi.h']
