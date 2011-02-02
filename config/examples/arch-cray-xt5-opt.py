e30a38920a37351d71d31fed751a5bca20046a47
Satish Balay <balay@mcs.anl.gov>
2011-02-02 11:35:41 -0600

    removed unnecessary configure options for the nersc cray build
    Hg-commit: eb6b15bbed2f9428dbdf6b426083f928bffcf384

--- config/examples/arch-cray-xt5-opt.py
+++ config/examples/arch-cray-xt5-opt.py
@@ -6,25 +6,22 @@
 # module add acml
 
 configure_options = [
+# On cray cc,CC,ftn are eqivalent to mpicc,mpiCC,mpif90
   '--with-cc=cc',
   '--with-cxx=CC',
   '--with-fc=ftn',
-  '--with-cpp=cpp',
-  '--with-ld=cc',
-  
+
   '--with-shared-libraries=0',
   '--with-debugging=0',
   '--COPTFLAGS=-fastsse -O3 -tp barcelona-64',
   '--CXXOPTFLAGS=-fastsse -O3 -tp barcelona-64',
   '--FOPTFLAGS=-fastsse -O3 -tp barcelona-64',
-  
+
   '--with-clib-autodetect=0',
   '--with-cxxlib-autodetect=0',
   '--with-fortranlib-autodetect=0',
-  
-  '--with-mpi-dir=/opt/mpt/3.5.0/xt/mpich2-pgi',
+
   '--with-mpiexec=/usr/common/acts/PETSc/3.0.0/bin/mpiexec.aprun',
-  
   '--with-x=0',
   '--with-blas-lapack-lib=-L/opt/acml/4.3.0/pgi64/lib -lacml -lacml_mv',
   ]
