d5994de3377a86eb44ba9c5a18375785ced1a5a6
Matthew G Knepley <knepley@gmail.com>
2011-02-20 17:03:58 -0600

    Added Scientific Python and fixed FIAT
    Hg-commit: 9eee4c8286a159ad771f31a5fd14c3a568904193

--- config/packages/ScientificPython.py
+++ config/packages/ScientificPython.py
@@ -4,35 +4,34 @@ import os, sys
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.download = ['http://launchpad.net/fiat/trunk/0.9.2/+download/fiat-0.9.2.tar.gz'] # 'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/fiat-dev.tar.gz'
+    self.download = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/ScientificPythonSimple.tar.gz']
     self.downloadname      = self.name.lower()
     self.archIndependent   = 1
     self.worksonWindows    = 1
     self.downloadonWindows = 1
-    self.liblist           = [['Lagrange.py']]
+    self.liblist           = [['Derivatives.py']]
     self.libdir            = os.path.join('lib', 'python', 'site-packages')
     self.altlibdir         = os.path.join('lib', 'python'+'.'.join(map(str, sys.version_info[0:2])), 'site-packages')
     return
 
   def setupDependencies(self, framework):
     config.package.Package.setupDependencies(self, framework)
-    self.scipy = framework.require('config.packages.ScientificPython', self)
-    self.deps  = [self.scipy]
+    self.deps = []
     return
 
   def Install(self):
     import shutil
     # We could make a check of the md5 of the current configure framework
-    self.logPrintBox('Installing FIAT')
-    # Copy FIAT into $PETSC_ARCH/lib/python/site-packages
+    self.logPrintBox('Installing Scientific Python (PETSc version)')
+    # Copy ScientificPython into $PETSC_ARCH/lib/python2.*/site-packages
     installLoc = os.path.join(self.installDir, self.altlibdir)
-    packageDir = os.path.join(installLoc, 'FIAT')
+    packageDir = os.path.join(installLoc, 'Scientific')
     if not os.path.isdir(installLoc):
       os.makedirs(installLoc)
     if os.path.exists(packageDir):
       shutil.rmtree(packageDir)
-    shutil.copytree(os.path.join(self.packageDir, 'FIAT'), packageDir)
-    self.framework.actions.addArgument('FIAT', 'Install', 'Installed FIAT into '+self.installDir)
+    shutil.copytree(os.path.join(self.packageDir, 'Scientific'), packageDir)
+    self.framework.actions.addArgument('Scientific Python', 'Install', 'Installed Scientific Python into '+self.installDir)
     return self.installDir
 
   def configureLibrary(self):
@@ -47,11 +46,8 @@ class Configure(config.package.Package):
         self.framework.logPrint('Checking location '+location)
         self.framework.logPrint('Added directory '+libDir+' to Python path')
         sys.path.insert(0, libDir)
-        import FIAT
-        from FIAT import ufc_simplex
-        import FIAT.lagrange
-        import FIAT.quadrature
+        import Scientific.Functions.Derivatives
         return
       except ImportError, e:
-        self.framework.logPrint('ERROR: Could not import FIAT: '+str(e))
+        self.framework.logPrint('ERROR: Could not import Scientific Python: '+str(e))
     raise RuntimeError('Could not find a functional '+self.name+'\n')
--- config/packages/__init__.py
+++ config/packages/__init__.py
@@ -1,1 +1,1 @@
-all = ['BlasLapack', 'CHOLMOD', 'ExodusII', 'Fiat', 'MOAB', 'MPI', 'NetCDF', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust']
+all = ['BlasLapack', 'CHOLMOD', 'ExodusII', 'ScientificPython', 'Fiat', 'MOAB', 'MPI', 'NetCDF', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust']
