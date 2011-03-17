dad1859aea1a7ff6c88f81f864a3d8cac9350ccd
Matthew G Knepley <knepley@gmail.com>
2011-03-16 22:34:12 -0500

    Cleaning up link and test run
    Hg-commit: 3b33135462c46aab9af5a6d1de284f46a89b1f35

--- config/builder.py
+++ config/builder.py
@@ -37,7 +37,8 @@ regressionParameters = {'src/vec/vec/examples/tests/ex1_2':  {'numProcs': 2},
                         'src/vec/vec/examples/tests/ex29':   {'numProcs': 3, 'args': '-n 126'},
                         'src/vec/vec/examples/tests/ex30f':  {'numProcs': 4},
                         'src/vec/vec/examples/tests/ex33':   {'numProcs': 4},
-                        'src/vec/vec/examples/tests/ex36':   {'numProcs': 2, 'args': '-set_option_negidx -set_values_negidx -get_values_negidx'}
+                        'src/vec/vec/examples/tests/ex36':   {'numProcs': 2, 'args': '-set_option_negidx -set_values_negidx -get_values_negidx'},
+                        'src/snes/examples/tutorials/ex5':   {'numProcs': 4, 'args': '-snes_mf -da_processors_x 4 -da_processors_y 1 -snes_monitor_short -ksp_gmres_cgs_refinement_type refine_always'}
                         }
 
 def noCheckCommand(command, status, output, error):
@@ -763,22 +764,22 @@ class PETScMaker(script.Script):
  def link(self, executable, objects, language):
    '''${CLINKER} -o $@ $^ ${PETSC_LIB}
       ${DSYMUTIL} $@'''
-   self.compilers.pushLanguage(language)
-   cmd = self.compilers.getFullLinkerCmd(objects+' -lpetsc', executable)
-   self.logWrite(cmd+'\n', debugSection = self.debugSection, forceScroll = True)
+   self.logWrite('Linking object '+str(objects)+' into '+executable+'\n', debugSection = self.debugSection, forceScroll = True)
+   self.configInfo.compilers.pushLanguage(language)
+   cmd = self.configInfo.compilers.getFullLinkerCmd(' '.join(objects)+' -lpetsc', executable)
    if not self.dryRun:
      (output, error, status) = self.executeShellCommand(cmd, checkCommand = noCheckCommand, log=self.log)
      if status:
        self.logPrint("ERROR IN LINK ******************************", debugSection='screen')
        self.logPrint(output+error, debugSection='screen')
      # TODO: Move dsymutil stuff from PETSc.utilities.debuggers to config.compilers
-     if hasattr(self.debuggers, 'dsymutil'):
-       (output, error, status) = self.executeShellCommand(self.debuggers.dsymutil+' '+executable, checkCommand = noCheckCommand, log=self.log)
+     if hasattr(self.configInfo.debuggers, 'dsymutil'):
+       (output, error, status) = self.executeShellCommand(self.configInfo.debuggers.dsymutil+' '+executable, checkCommand = noCheckCommand, log=self.log)
        if status:
          self.operationFailed = True
          self.logPrint("ERROR IN LINK ******************************", debugSection='screen')
          self.logPrint(output+error, debugSection='screen')
-   self.compilers.popLanguage()
+   self.configInfo.compilers.popLanguage()
    return [executable]
 
  def buildDir(self, dirname, files, objDir):
@@ -905,36 +906,39 @@ class PETScMaker(script.Script):
        os.remove(fname)
    return
 
- def checkTestOutput(self, executable, output, testNum):
-   outputName = os.path.abspath(os.path.join('output', executable+'_'+str(testNum)+'.out'))
+ def checkTestOutput(self, testDir, executable, output, testNum):
+   outputName = os.path.join(testDir, 'output', os.path.basename(executable)+'_'+str(testNum)+'.out')
+   ret        = 0
    if not os.path.isfile(outputName):
      self.logPrint("MISCONFIGURATION: Regression output file %s (test %d) is missing" % (outputName, testNum), debugSection='screen')
    else:
      with file(outputName) as f:
        validOutput = f.read()
        if not validOutput == output:
-         self.logPrint("TEST ERROR: Regression output for %s (test %d) does not match" % (executable, testNum), debugSection='screen')
-         self.logPrint(validOutput, debugSection='screen')
-         self.logPrint(output, debugSection='screen')
+         self.logPrint("TEST ERROR: Regression output for %s (test %d) does not match" % (executable, testNum))
+         self.logPrint(validOutput, indent = 0)
+         self.logPrint(output, indent = 0)
+         ret = -1
        else:
-         self.logPrint("TEST SUCCESS: Regression output for %s (test %d) matches" % (executable, testNum), debugSection='screen')
-   return
+         self.logPrint("TEST SUCCESS: Regression output for %s (test %d) matches" % (executable, testNum))
+   return ret
 
- def runTest(self, executable, testNum, **params):
+ def runTest(self, testDir, executable, testNum, **params):
    numProcs = params.get('numProcs', 1)
    args     = params.get('args', '')
    # TODO: Take this line out when configure is fixed
    # mpiexec = self.mpi.mpiexec.replace(' -n 1','').replace(' ', '\\ ')
-   cmd = ' '.join([self.mpi.mpiexec, '-n', str(numProcs), os.path.abspath(executable), args])
-   self.logWrite('Running test for '+executable+'\n'+cmd+'\n', debugSection = self.debugSection, forceScroll = True)
+   cmd = ' '.join([self.configInfo.mpi.mpiexec, '-n', str(numProcs), os.path.abspath(executable), args])
+   self.logPrint('Running test for '+executable)
    if not self.dryRun:
      (output, error, status) = self.executeShellCommand(cmd, checkCommand = noCheckCommand, log=self.log)
      if status:
-       self.logPrint("TEST ERROR: Failed to execute %s\n" % executable, debugSection = 'screen', forceScroll = True)
-       self.logPrint(output+error, debugSection='screen', indent = 0, forceScroll = True)
+       self.logPrint("TEST ERROR: Failed to execute %s\n" % executable)
+       self.logPrint(output+error, indent = 0)
+       ret = -2
      else:
-       self.checkTestOutput(executable, output+error, testNum)
-   return
+       ret = self.checkTestOutput(testDir, executable, output+error, testNum)
+   return ret
 
  def regressionTestsDir(self, dirname, dummy):
    ''' This is run in a PETSc source directory'''
@@ -967,10 +971,10 @@ class PETScMaker(script.Script):
          self.link(executable, obj, 'FC')
        else:
          self.link(executable, obj, self.languages.clanguage)
-       self.runTest(executable, testNum, **regressionParameters.get(paramKey, {}))
+       self.runTest(dirname, executable, testNum, **regressionParameters.get(paramKey, {}))
        testNum += 1
        while '%s_%d' % (paramKey, testNum) in regressionParameters:
-         self.runTest(executable, testNum, **regressionParameters.get('%s_%d' % (paramKey, testNum), {}))
+         self.runTest(dirname, executable, testNum, **regressionParameters.get('%s_%d' % (paramKey, testNum), {}))
          testNum += 1
        self.cleanupTest(dirname, executable)
    return
@@ -1003,6 +1007,7 @@ class PETScMaker(script.Script):
    return
 
  def buildFortranStubs(self):
+   self.logWrite('Building Fortran stubs\n', debugSection = 'screen', forceScroll = True)
    oldPath = sys.path
    sys.path.append(os.path.join(self.petscDir, 'bin', 'maint'))
    from generatefortranstubs import main, processf90interfaces
@@ -1015,7 +1020,12 @@ class PETScMaker(script.Script):
    sys.path = oldPath
    return
 
+ def check(self):
+   self.logWrite('Checking build\n', debugSection = 'screen', forceScroll = True)
+   return
+
  def clean(self, libname):
+   self.logWrite('Cleaning build\n', debugSection = 'screen', forceScroll = True)
    shutil.rmtree(self.sourceDBFilename)
    shutil.rmtree(self.getObjDir(libname))
    return
