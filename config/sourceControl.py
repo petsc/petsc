b713d8ca721659d78ee73cebf66b9df318183998
Satish Balay <balay@mcs.anl.gov>
2011-03-23 12:14:52 -0500

    make hg-version-check error safe, and add version check for svn,cvs
    Hg-commit: 4edb5cf583d766e7cbd6a8f2759b5f5d8a1f5438

--- config/sourceControl.py
+++ config/sourceControl.py
@@ -25,17 +25,28 @@ class Configure(config.base.Configure):
     if 'with-hg' in self.framework.argDB and self.framework.argDB['with-hg'] == '0':
       return
     self.getExecutable(self.framework.argDB['with-hg'], resultName = 'hg')
-    self.hgversion = self.executeShellCommand(self.hg + ' version -q')
+    if hasattr(self,'hg'):
+      try:
+        self.hgversion = self.executeShellCommand(self.hg + ' version -q')
+      except: pass
     return
 
   def configureCVS(self):
     '''Find the CVS executable'''
     self.getExecutable(self.framework.argDB['with-cvs'], resultName = 'cvs')
+    if hasattr(self,'cvs'):
+      try:
+        self.cvxversion = self.executeShellCommand(self.cvs + ' --version')
+      except: pass
     return
 
   def configureSubversion(self):
     '''Find the Subversion executable'''
     self.getExecutable(self.framework.argDB['with-svn'], resultName = 'svn')
+    if hasattr(self,'svn'):
+      try:
+        self.svnversion = self.executeShellCommand(self.svn + ' --version -q')
+      except: pass
     return
 
   def configure(self):
