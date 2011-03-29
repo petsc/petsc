dfe9a5c5bb2fc50823f4e4e67d460b21dbf443e3
Matthew G Knepley <knepley@gmail.com>
2011-03-29 01:38:31 -0500

    Moved HDF5 to BuildSystem - Small cleanup of NewPackage
    Hg-commit: e50240cc088e93d8f6590ca6422a42012cd7ce4c

--- config/PETSc/package.py
+++ config/PETSc/package.py
@@ -15,7 +15,7 @@ class NewPackage(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
     # These are specified for the package
-    self.double           = 1   # 1 means requires double precision 
+    self.double           = 1   # 1 means requires double precision
     self.complex          = 0   # 0 means cannot use complex
     self.requires32bitint = 1;  # 1 means that the package will not work with 64 bit integers
     return
@@ -24,7 +24,7 @@ class NewPackage(config.package.Package):
     config.package.Package.setupDependencies(self, framework)
     self.petscarch      = framework.require('PETSc.utilities.arch', self)
     self.languages      = framework.require('PETSc.utilities.languages', self)
-    self.scalartypes    = self.framework.require('PETSc.utilities.scalarTypes',self)
+    self.scalartypes    = framework.require('PETSc.utilities.scalarTypes',self)
     self.libraryOptions = framework.require('PETSc.utilities.libraryOptions', self)
     self.petscdir       = framework.require('PETSc.utilities.petscdir', self.setCompilers)
     return
@@ -33,11 +33,11 @@ class NewPackage(config.package.Package):
     config.package.Package.consistencyChecks(self)
     if self.framework.argDB['with-'+self.package]:
       if self.cxx and not self.languages.clanguage == 'Cxx':
-        raise RuntimeError('Cannot use '+self.name+' without C++, run ./configure --with-clanguage=C++')    
+        raise RuntimeError('Cannot use '+self.name+' without C++, run ./configure --with-clanguage=C++')
       if self.double and not self.scalartypes.precision.lower() == 'double':
-        raise RuntimeError('Cannot use '+self.name+' withOUT double precision numbers, it is not coded for this capability')    
+        raise RuntimeError('Cannot use '+self.name+' withOUT double precision numbers, it is not coded for this capability')
       if not self.complex and self.scalartypes.scalartype.lower() == 'complex':
-        raise RuntimeError('Cannot use '+self.name+' with complex numbers it is not coded for this capability')    
+        raise RuntimeError('Cannot use '+self.name+' with complex numbers it is not coded for this capability')
       if self.libraryOptions.integerSize == 64 and self.requires32bitint:
-        raise RuntimeError('Cannot use '+self.name+' with 64 bit integers, it is not coded for this capability')    
+        raise RuntimeError('Cannot use '+self.name+' with 64 bit integers, it is not coded for this capability')
     return
