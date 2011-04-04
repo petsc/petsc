f78d1622446f215e568953bf047c375ef9308745
Matthew G Knepley <knepley@gmail.com>
2011-04-04 10:30:09 -0500

    Added Metis package
    Hg-commit: c388dd9703cc7d7092a30ded14c802976ca3deec

--- config/packages/__init__.py
+++ config/packages/__init__.py
@@ -1,1 +1,1 @@
-all = ['BlasLapack', 'CHOLMOD', 'ExodusII', 'ScientificPython', 'Fiat', 'MOAB', 'MPI', 'NetCDF', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust', 'hdf5']
+all = ['BlasLapack', 'CHOLMOD', 'ExodusII', 'ScientificPython', 'Fiat', 'MOAB', 'MPI', 'NetCDF', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust', 'hdf5', 'metis']
