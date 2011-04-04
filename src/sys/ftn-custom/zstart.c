7ad2ef1bbd9e94afaa6eb9f27790d6a2a9d0ab9b
Satish Balay <balay@mcs.anl.gov>
2011-04-04 17:05:13 -0500

    compile fix for cublasinit
    Hg-commit: 8dd3dbf7b609198f686fb6504ab1bdb1c71f4e99

--- src/sys/ftn-custom/zstart.c
+++ src/sys/ftn-custom/zstart.c
@@ -11,7 +11,11 @@
 #define T3DMPI_FORTRAN
 #define T3EMPI_FORTRAN
 
-#include <private/fortranimpl.h> 
+#include <private/fortranimpl.h>
+
+#if defined(PETSC_HAVE_CUSP)
+#include <cublas.h>
+#endif
 
 extern  PetscBool  PetscBeganMPI;
 
