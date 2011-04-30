04cbb33e36487262dda92a8cf0fba7bc38b2a557
Barry Smith <bsmith@mcs.anl.gov>
2011-04-30 15:15:58 -0500

    VecXDot() defined in KSPSolve_CG() does not have have to be different for non-complex case.
    Hg-commit: cc31d88335e625ca91137eb0e2cc6152fe3b01dd

--- src/ksp/ksp/impls/cg/cg.c
+++ src/ksp/ksp/impls/cg/cg.c
@@ -128,11 +128,7 @@ PetscErrorCode  KSPSolve_CG(KSP ksp)
     W           = Z;
   } 
 
-#if !defined(PETSC_USE_COMPLEX)
-#define VecXDot(x,y,a) VecDot(x,y,a)
-#else
 #define VecXDot(x,y,a) (((cg->type) == (KSP_CG_HERMITIAN)) ? VecDot(x,y,a) : VecTDot(x,y,a))
-#endif
 
   if (eigs) {e = cg->e; d = cg->d; e[0] = 0.0; }
   ierr = PCGetOperators(ksp->pc,&Amat,&Pmat,&pflag);CHKERRQ(ierr);
