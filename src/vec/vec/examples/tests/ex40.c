d1191070a39f0bc7fc3128d5932258b441a8d5eb
Hong Zhang <hzhang@mcs.anl.gov>
2011-06-20 14:19:55 -0500

    add an example of "Taking part of existing array to create a new vector" [petsc-maint #76989]
    Hg-commit: ad3bca521c6426cb4cc2a74443e3f04d1ad497b8

---
+++ src/vec/vec/examples/tests/ex40.c
@@ -0,0 +1,29 @@
+
+static char help[] = "Tests taking part of existing array to create a new vector.\n\n";
+
+#include <petscvec.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  PetscErrorCode ierr;
+  PetscMPIInt    size;
+  PetscInt       n = 10,i;
+  PetscScalar    array[n];
+  Vec            x;
+
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr); 
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+  if (size != 1) SETERRQ(PETSC_COMM_SELF,1,"This is a uniprocessor example only!");
+
+  /* create vector */
+  for (i=0; i<n; i++) array[i] = i;
+  n = n-1;
+  ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,n,array+1,&x);CHKERRQ(ierr);
+  ierr = VecView(x,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  ierr = PetscFinalize();
+  return 0;
+}
+ 
