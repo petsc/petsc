baf173800e845d45e1e366b8190bf3d137baa8d4
Lisandro Dalcin <dalcinl@gmail.com>
2011-07-08 12:21:12 -0300

    Disable testing of SNES and TS Python types for petsc.3.1|0

--- test/test_ts_py.py
+++ test/test_ts_py.py
@@ -170,16 +170,15 @@ class TestTSPython(unittest.TestCase):
 # --------------------------------------------------------------------
 
 PETSC_VERSION = PETSc.Sys.getVersion()
-
 i = PETSc.Sys.getVersionInfo()
 if (PETSC_VERSION == (3, 1, 0) and
     not i['release']):
     PETSC_VERSION = (3, 2, 0)
 
 if  PETSC_VERSION < (3, 2, 0):
-    del TestTSPython.testResetAndSolve
-
-# --------------------------------------------------------------------
+    del TestTSPython
 
 if __name__ == '__main__':
     unittest.main()
+
+# --------------------------------------------------------------------
