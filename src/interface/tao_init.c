84442b39d6d1126b5ff6a7a2b9cb3d83bb6b9185
Jason Sarich <sarich@mcs.anl.gov>
2011-07-19 15:39:10 -0500

    taoinitialize starts petsc if needed

--- src/interface/tao_init.c
+++ src/interface/tao_init.c
@@ -1,8 +1,11 @@
 #include "taosolver.h"   /*I "taosolver.h" I*/
 
 PetscBool TaoInitializeCalled = PETSC_FALSE;
+PetscBool TaoBeganPetsc = PETSC_FALSE;
 
-
+static int tao_one=1;
+static char* tao_executable = (char*)"tao";
+static char** tao_executablePtr = &tao_executable;
 #undef __FUNCT__
 #define __FUNCT__ "TaoInitialize"
 /*@C 
@@ -27,20 +30,31 @@ PetscBool TaoInitializeCalled = PETSC_FALSE;
 
 .seealso: TaoInitializeFortran(), TaoFinalize(), PetscInitialize()
 @*/
-
 PetscErrorCode TaoInitialize(int *argc, char ***args, const char file[], 
 			     const char help[])
 {
   PetscErrorCode ierr;
-  PetscFunctionBegin;
-  if (TaoInitializeCalled) {
-    PetscFunctionReturn(0);
-  }
-  TaoInitializeCalled = PETSC_TRUE;
-  ierr = TaoInitialize_DynamicLibraries(); CHKERRQ(ierr);
-  ierr = PetscInfo(0,"TAO successfully initialized.\n"); CHKERRQ(ierr);
 
-  PetscFunctionReturn(0);
+  if (TaoInitializeCalled) {
+    return(0);
+  }
+  if (PetscInitializeCalled) {
+    PetscInfo(0,"TAO successfully initialized.\n"); CHKERRQ(ierr);
+  } else {
+    if (argc&&args) {
+      PetscInitialize(argc,args,file,help); 
+    } else {
+      PetscInitialize(&tao_one,&tao_executablePtr,0,0); 
+    }
+    TaoBeganPetsc=PETSC_TRUE;
+  }
+  if (!PetscInitializeCalled) {
+    printf("Error initializing PETSc -- aborting.\n");
+    exit(1);
+  }
+  ierr = TaoInitialize_DynamicLibraries();  CHKERRQ(ierr);
+  TaoInitializeCalled = PETSC_TRUE;
+  return 0;
 }
 
 #undef __FUNCT__
@@ -60,9 +74,11 @@ PetscErrorCode TaoInitialize(int *argc, char ***args, const char file[],
 PetscErrorCode TaoFinalize()
 {
   PetscErrorCode ierr;
-  PetscFunctionBegin;
-  ierr = TaoFinalize_DynamicLibraries(); CHKERRQ(ierr);
-  PetscFunctionReturn(0);
+  TaoFinalize_DynamicLibraries();
+  if (TaoBeganPetsc) {
+    PetscFinalize();
+  } 
+  return(0);
 }
 
 #undef __FUNCT__
