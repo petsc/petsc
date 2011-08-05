5ec3c338f21f03b3f0faa624e3b77be791805190
Matthew G Knepley <knepley@gmail.com>
2011-08-05 15:29:00 -0400

    Reworking benchmarking
    Hg-commit: 9b6a95a49e9f419083c3971fc8529d50b65e5fda

--- src/benchmarks/benchmarkAssembly.py
+++ src/benchmarks/benchmarkAssembly.py
@@ -2,6 +2,10 @@
 import os
 from benchmarkExample import PETScExample
 
+savedTiming = {'baconost': {'ElemAssembly': [(0.040919999999999998, 0.0), (0.1242, 0.0), (0.24410000000000001, 0.0), (0.374, 0.0), (0.56259999999999999, 0.0), (0.79049999999999998, 0.0), (1.0880000000000001, 0.0), (1.351, 0.0), (1.6930000000000001, 0.0), (2.0609999999999999, 0.0), (2.4820000000000002, 0.0), (3.0640000000000001, 0.0)],
+                            'MatCUSPSetValBch': [(0.0123, 0.0), (0.023429999999999999, 0.0), (0.043540000000000002, 0.0), (0.06608, 0.0), (0.09579, 0.0), (0.12920000000000001, 0.0), (0.17169999999999999, 0.0), (0.2172, 0.0), (0.27179999999999999, 0.0), (0.48309999999999997, 0.0), (0.44180000000000003, 0.0), (0.51529999999999998, 0.0)]}
+               }
+
 def calculateNonzeros(n):
   num = 0
   # corners
@@ -80,26 +84,33 @@ def plotSummary(library, num, sizes, nonzeros, times, events):
   return
 
 if __name__ == '__main__':
-  library = 'KSP'
-  num     = 4
-  ex      = PETScExample(library, num, log_summary_python='summary.py', preload='off')
-  if 0:
-    sizes    = []
-    nonzeros = []
-    times    = []
+  import argparse
+
+  parser = argparse.ArgumentParser(description     = 'PETSc Benchmarking',
+                                   epilog          = 'This script runs src/<library>/examples/tutorials/ex<num>, For more information, visit http://www.mcs.anl.gov/petsc',
+                                   formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('--library', default='KSP',           help='The PETSc library used in this example')
+  parser.add_argument('--num',     type = int, default='4', help='The example number')
+  parser.add_argument('--module',  default='summary',       help='The module for timing output')
+  parser.add_argument('--saved',                            help='Name of saved data')
+
+  args = parser.parse_args()
+  print(args)
+  ex       = PETScExample(args.library, args.num, log_summary_python=args.module+'.py', preload='off')
+  sizes    = []
+  nonzeros = []
+  times    = []
+  if args.saved is None:
     events   = {}
-    for n in [10, 20, 50, 100, 150, 200, 250, 300, 350]:
+    #for n in [10, 20, 50, 100, 150, 200, 250, 300, 350]:
+    for n in [10, 20]:
       ex.run(da_grid_x=n, da_grid_y=n, cusp_synchronize=1)
       sizes.append(n*n)
       nonzeros.append(calculateNonzeros(n))
-      processSummary('summary', times, events)
-    plotSummary(library, num, sizes, nonzeros, times, events)
+      processSummary(args.module, times, events)
   else:
-    sizes    = []
-    nonzeros = []
-    times    = []
+    events = savedTiming[args.saved]
     for n in range(150, 1350, 100):
       sizes.append(n*n)
       nonzeros.append(calculateNonzeros(n))
-    baconostEvents = {'ElemAssembly': [(0.040919999999999998, 0.0), (0.1242, 0.0), (0.24410000000000001, 0.0), (0.374, 0.0), (0.56259999999999999, 0.0), (0.79049999999999998, 0.0), (1.0880000000000001, 0.0), (1.351, 0.0), (1.6930000000000001, 0.0), (2.0609999999999999, 0.0), (2.4820000000000002, 0.0), (3.0640000000000001, 0.0)], 'MatCUSPSetValBch': [(0.0123, 0.0), (0.023429999999999999, 0.0), (0.043540000000000002, 0.0), (0.06608, 0.0), (0.09579, 0.0), (0.12920000000000001, 0.0), (0.17169999999999999, 0.0), (0.2172, 0.0), (0.27179999999999999, 0.0), (0.48309999999999997, 0.0), (0.44180000000000003, 0.0), (0.51529999999999998, 0.0)]}
-    plotSummary(library, num, sizes, nonzeros, times, baconostEvents)
+  plotSummary(args.library, args.num, sizes, nonzeros, times, events)
