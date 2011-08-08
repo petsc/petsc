9d3211cb66ff9b9f1b35054852d57d2527205372
Matthew G Knepley <knepley@gmail.com>
2011-08-08 14:35:11 -0400

    Can now analyze single proc output from CCV
    Hg-commit: 33bd45d736a1e8606cfc21315295bd262fef49f9

--- src/benchmarks/benchmarkAssembly.py
+++ src/benchmarks/benchmarkAssembly.py
@@ -124,7 +124,28 @@ if __name__ == '__main__':
       if not args.batch: processSummary(args.module, times, events)
   else:
     if args.batch: raise RuntimeException('Cannot use batch option with saved data')
-    events = savedTiming[args.saved]
+    if args.saved in savedTiming:
+      events = savedTiming[args.saved]
+    else:
+      # Process output to produce module
+      events       = {}
+      filenameBase = args.saved[:-7]
+      jobnumBase   = int(args.saved[-7:])
+      for i, n in enumerate(range(150, 1350, 100)):
+        filename = filenameBase+str(jobnumBase+i)
+        print 'Processing',filename
+        headerSeen = False
+        with file(filename) as f, file(args.module+'.py', 'w') as o:
+          for line in f.readlines():
+            if not headerSeen:
+              if not line[0] == '#': continue
+              headerSeen = True
+            if line[0] == '#' and line[-6:] == '=====\n': break
+            o.write(line)
+            #print line
+        processSummary(args.module, times, events)
+        # I can't believe that this is necessary
+        os.remove(args.module+'.pyc')
     for n in range(150, 1350, 100):
       sizes.append(n*n)
       nonzeros.append(calculateNonzeros(n))
