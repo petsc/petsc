a36306386cafcfd646eca44e6635f8f93e2148e5
Satish Balay <balay@mcs.anl.gov>
2011-08-19 15:52:39 -0500

    Move PetscIsInfOrNanScalar/Real() from inline macros to proper functions to avoid errors with configuring with C, and using from c++ etc..
    Hg-commit: 7ef7d5f965419df9e80b213135b39dce8e1a8757

--- include/petscmath.h
+++ include/petscmath.h
@@ -341,56 +341,8 @@ extern PetscErrorCode  PetscGlobalMin(MPI_Comm, const PetscReal*,PetscReal*);
 extern PetscErrorCode  PetscGlobalSum(MPI_Comm, const PetscScalar*,PetscScalar*);
 #endif
 
-/*MC
-      PetscIsInfOrNan - Returns 1 if the input double has an infinity for Not-a-number (Nan) value, otherwise 0.
-
-    Input Parameter:
-.     a - the double
-
-
-     Notes: uses the C99 standard isinf() and isnan() on systems where they exist.
-      Otherwises uses ( (a - a) != 0.0), note that some optimizing compiles compile
-      out this form, thus removing the check.
-
-     Level: beginner
-
-M*/
-#if defined(PETSC_USE_REAL___FLOAT128)
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanScalar(PetscScalar a) {
-  return isinfq(PetscAbsScalar(a)) || isnanq(PetscAbsScalar(a));
-}
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanReal(PetscReal a) {
-  return isinfq(a) || isnanq(a);
-}
-#elif defined(PETSC_HAVE_ISINF) && defined(PETSC_HAVE_ISNAN) && !defined(_GLIBCXX_CMATH)
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanScalar(PetscScalar a) {
-  return isinf(PetscAbsScalar(a)) || isnan(PetscAbsScalar(a));
-}
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanReal(PetscReal a) {
-  return isinf(a) || isnan(a);
-}
-#elif defined(PETSC_HAVE__FINITE) && defined(PETSC_HAVE__ISNAN)
-#if defined(PETSC_HAVE_FLOAT_H)
-#include "float.h"  /* Microsoft Windows defines _finite() in float.h */
-#endif
-#if defined(PETSC_HAVE_IEEEFP_H)
-#include "ieeefp.h"  /* Solaris prototypes these here */
-#endif
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanScalar(PetscScalar a) {
-  return !_finite(PetscAbsScalar(a)) || _isnan(PetscAbsScalar(a));
-}
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanReal(PetscReal a) {
-  return !_finite(a) || _isnan(a);
-}
-#else
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanScalar(PetscScalar a) {
-  return  ((a - a) != (PetscScalar)0);
-}
-PETSC_STATIC_INLINE PetscErrorCode PetscIsInfOrNanReal(PetscReal a) {
-  return ((a - a) != 0);
-}
-#endif
-
+extern PetscErrorCode PetscIsInfOrNanScalar(PetscScalar);
+extern PetscErrorCode PetscIsInfOrNanReal(PetscReal);
 
 /* ----------------------------------------------------------------------------*/
 /*
