bd052dfec9a6232e7a380a7ecca83e44d81a233f
Peter Brune <brune@cs.uchicago.edu>
2011-09-09 15:25:13 -0500

    quasi-newton now is L-BFGS
    Hg-commit: 760081b4986dec11deb32ce0b8c0924d40d5cc04

--- src/snes/impls/qn/qn.c
+++ src/snes/impls/qn/qn.c
@@ -6,6 +6,9 @@ typedef struct {
   Vec * dX;           /* The change in X */
   Vec * dF;           /* The change in F */
   PetscInt m;         /* the number of kept previous steps */
+  PetscScalar * alpha;
+  PetscScalar * beta;
+  PetscScalar * rho;
 } QNContext;
 
 #undef __FUNCT__
@@ -19,34 +22,27 @@ PetscErrorCode LBGFSApplyJinv_Private(SNES snes, PetscInt it, Vec g, Vec z) {
   Vec * dX = qn->dX;
   Vec * dF = qn->dF;
 
+  PetscScalar * alpha = qn->alpha;
+  PetscScalar * beta = qn->beta;
+  PetscScalar * rho = qn->rho;
+
   PetscInt k, i;
   PetscInt m = qn->m;
-  PetscScalar * alpha = PETSC_NULL;
-  PetscScalar * beta = PETSC_NULL;
-  PetscScalar * rho = PETSC_NULL;
   PetscScalar t;
   PetscInt l = m;
 
   PetscFunctionBegin;
 
-  if (it < m) l = it+1;
-  ierr = PetscMalloc3(m, PetscScalar, &alpha, m, PetscScalar, &beta, m, PetscScalar, &rho);CHKERRQ(ierr);
-
-  /* precalculate alpha, beta, rho corresponding to the normal indices*/
-  for (i = 0; i < l; i++) {
-    ierr = VecDot(dX[i], dF[i], &t);CHKERRQ(ierr);
-    rho[i] = 1. / t;
-    beta[i] = 0;
-    alpha[i] = 0;
-  }
+  if (it < m) l = it;
 
   ierr = VecCopy(g, z);CHKERRQ(ierr);
 
   /* outward recursion starting at iteration k's update and working back */
   for (i = 0; i < l; i++) {
-    k = (it - i) % m; 
+    /* k = (it - i - 1) % m; */
+    k = (it + i - l) % m; 
     ierr = VecDot(dX[k], z, &t);CHKERRQ(ierr);
-    alpha[k] = t / rho[k];
+    alpha[k] = t*rho[k];
     ierr = VecAXPY(z, -alpha[k], dF[k]);CHKERRQ(ierr);
   }
 
@@ -54,16 +50,74 @@ PetscErrorCode LBGFSApplyJinv_Private(SNES snes, PetscInt it, Vec g, Vec z) {
   /* right now it's just the identity. Nothing needs to go here. */
 
   /* inward recursion starting at the first update and working forward*/
-  for (i = l - 1; i >= 0; i--) {
-    k = (it - i) % m;
+  for (i = 0; i < l; i++) {
+    k = (it - i - 1) % m; 
+    /* k = (it + i - l) % m; */
     ierr = VecDot(dF[k], z, &t);CHKERRQ(ierr);
     beta[k] = rho[k]*t;
     ierr = VecAXPY(z, (alpha[k] - beta[k]), dX[k]);
   }
-  ierr = PetscFree3(alpha, beta, rho);CHKERRQ(ierr);
+  ierr = VecScale(z, 1.0);CHKERRQ(ierr);
 
   PetscFunctionReturn(0);
 }
+
+
+#undef __FUNCT__
+#define __FUNCT__ "QNLineSearchQuadratic"
+PetscErrorCode QNLineSearchQuadratic(SNES snes,void *lsctx,Vec X,Vec F,Vec G,Vec Y,Vec W,PetscReal fnorm,PetscReal dummyXnorm,PetscReal *dummyYnorm,PetscReal *gnorm,PetscBool *flag)
+{
+  PetscInt       i;
+  PetscReal      alphas[3] = {0.0, 0.5, 1.0};
+  PetscReal      norms[3];
+  PetscReal      alpha,a,b;
+  PetscErrorCode ierr;
+  PetscFunctionBegin;
+  norms[0]  = fnorm;
+  /* Calculate trial solutions */
+  for(i=1; i < 3; ++i) {
+    /* Calculate X^{n+1} = (1 - \alpha) X^n + \alpha Y */
+    ierr = VecCopy(X, W);CHKERRQ(ierr);
+    ierr = VecAXPBY(W, alphas[i], 1 - alphas[i], Y);CHKERRQ(ierr);
+    ierr = SNESComputeFunction(snes, W, F);CHKERRQ(ierr);
+    ierr = VecNorm(F, NORM_2, &norms[i]);CHKERRQ(ierr);
+  }
+  for(i = 0; i < 3; ++i) {
+    norms[i] = PetscSqr(norms[i]);
+  }
+  /* Fit a quadratic:
+       If we have x_{0,1,2} = 0, x_1, x_2 which generate norms y_{0,1,2}
+       a = (x_1 y_2 - x_2 y_1 + (x_2 - x_1) y_0)/(x^2_2 x_1 - x_2 x^2_1)
+       b = (x^2_1 y_2 - x^2_2 y_1 + (x^2_2 - x^2_1) y_0)/(x_2 x^2_1 - x^2_2 x_1)
+       c = y_0
+       x_min = -b/2a
+
+       If we let x_{0,1,2} = 0, 0.5, 1.0
+       a = 2 y_2 - 4 y_1 + 2 y_0
+       b =  -y_2 + 4 y_1 - 3 y_0
+       c =   y_0
+  */
+  a = (alphas[1]*norms[2] - alphas[2]*norms[1] + (alphas[2] - alphas[1])*norms[0])/(PetscSqr(alphas[2])*alphas[1] - alphas[2]*PetscSqr(alphas[1]));
+  b = (PetscSqr(alphas[1])*norms[2] - PetscSqr(alphas[2])*norms[1] + (PetscSqr(alphas[2]) - PetscSqr(alphas[1]))*norms[0])/(alphas[2]*PetscSqr(alphas[1]) - PetscSqr(alphas[2])*alphas[1]);
+  /* Check for positive a (concave up) */
+  if (a >= 0.0) {
+    alpha = -b/(2.0*a);
+    alpha = PetscMin(alpha, alphas[2]);
+    alpha = PetscMax(alpha, alphas[0]);
+  } else {
+    alpha = 1.0;
+  }
+  ierr = VecAXPBY(X, alpha, 1 - alpha, Y);CHKERRQ(ierr);
+  ierr = SNESComputeFunction(snes, X, F);CHKERRQ(ierr);
+  if (alpha != 1.0) {
+    ierr = VecNorm(F, NORM_2, gnorm);CHKERRQ(ierr);
+  } else {
+    *gnorm = PetscSqrtReal(norms[2]); 
+  }
+  *flag = PETSC_TRUE;
+  PetscFunctionReturn(0);
+}
+
 
 #undef __FUNCT__  
 #define __FUNCT__ "SNESSolve_QN"
@@ -73,26 +127,31 @@ static PetscErrorCode SNESSolve_QN(SNES snes)
   PetscErrorCode ierr;
   QNContext * qn = (QNContext*) snes->data;
 
-  Vec x;
-  Vec f;
-  Vec p, pold;
+  Vec x, xold;
+  Vec f, fold;
+  Vec w, y;
 
-  PetscInt i, j, k, l;
+  PetscInt i, k;
 
-  PetscReal fnorm;
-  PetscScalar gdot;
+  PetscReal fnorm, xnorm;
   PetscInt m = qn->m;
+  PetscBool ls_OK;
+
+  PetscScalar rhosc;
   
-  Vec * W = qn->dX;
-  Vec * V = qn->dF;
+  Vec * dX = qn->dX;
+  Vec * dF = qn->dF;
+  PetscScalar * rho = qn->rho;
 
   /* basically just a regular newton's method except for the application of the jacobian */
   PetscFunctionBegin;
 
   x = snes->vec_sol;
+  xold = snes->vec_sol_update; /* dX_k */
+  w = snes->work[1];
   f = snes->vec_func;
-  p = snes->vec_sol_update;
-  pold = snes->work[0];
+  fold = snes->work[0];
+  y = snes->work[2];
 
   snes->reason = SNES_CONVERGED_ITERATING;
 
@@ -114,14 +173,24 @@ static PetscErrorCode SNESSolve_QN(SNES snes)
   ierr = SNESMonitor(snes,0,fnorm);CHKERRQ(ierr);
 
   /* set parameter for default relative tolerance convergence test */
-  snes->ttol = fnorm*snes->rtol;
+   snes->ttol = fnorm*snes->rtol;
   /* test convergence */
   ierr = (*snes->ops->converged)(snes,0,0.0,0.0,fnorm,&snes->reason,snes->cnvP);CHKERRQ(ierr);
   if (snes->reason) PetscFunctionReturn(0);
-  ierr = VecCopy(f, pold);CHKERRQ(ierr);
-  ierr = VecAXPY(x, -1.0, pold);CHKERRQ(ierr);
+  ierr = VecCopy(f, fold);CHKERRQ(ierr);
+  ierr = VecCopy(x, xold);CHKERRQ(ierr);
   for(i = 0; i < snes->max_its; i++) {
+    /* general purpose update */
+    if (snes->ops->update) {
+      ierr = (*snes->ops->update)(snes, snes->iter);CHKERRQ(ierr);
+    }
 
+    /* apply the current iteration of the approximate jacobian */
+    ierr = LBGFSApplyJinv_Private(snes, i, f, y);CHKERRQ(ierr);
+
+    /* line search for lambda */
+    ierr = VecAYPX(y,-1.0,x);CHKERRQ(ierr);
+    ierr = QNLineSearchQuadratic(snes, PETSC_NULL, x, f, 0, y, w, fnorm, xnorm, &xnorm, &fnorm, &ls_OK);CHKERRQ(ierr);
     ierr = SNESComputeFunction(snes, x, f);CHKERRQ(ierr);
     ierr = VecNorm(f, NORM_2, &fnorm);CHKERRQ(ierr);
     if (PetscIsInfOrNanReal(fnorm)) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_FP,"Infinite or not-a-number generated in norm");
@@ -129,32 +198,21 @@ static PetscErrorCode SNESSolve_QN(SNES snes)
     snes->norm = fnorm;
     ierr = PetscObjectGrantAccess(snes);CHKERRQ(ierr);
     SNESLogConvHistory(snes,fnorm,i+1);
-    ierr = SNESMonitor(snes,i+1,fnorm);CHKERRQ(ierr);
-    
+    ierr = SNESMonitor(snes,i+1,fnorm);CHKERRQ(ierr);  
     /* set parameter for default relative tolerance convergence test */
-    snes->ttol = fnorm*snes->rtol;
-    /* test convergence */
     ierr = (*snes->ops->converged)(snes,i+1,0.0,0.0,fnorm,&snes->reason,snes->cnvP);CHKERRQ(ierr);
     if (snes->reason) PetscFunctionReturn(0);
-    k = (i) % m;
-    l = m;
-    if (i < l) l = i;
-    ierr = VecCopy(f, p);CHKERRQ(ierr);
-    for (j=0; j<k; j++) {                                     /* p = product_{j<i} [I+v(j)w(j)^T]*p */
-      ierr = VecDot(W[j],p,&gdot);CHKERRQ(ierr);
-      ierr = VecAXPY(p,gdot,V[j]);CHKERRQ(ierr);
-    }
-    ierr = VecCopy(pold,W[k]);CHKERRQ(ierr);                  /* w[i] = pold   */
-    ierr = VecAXPY(pold,-1.0,p);CHKERRQ(ierr);                /* v[i] =         p         */
-    ierr = VecDot(W[k],pold,&gdot);CHKERRQ(ierr);             /*        ----------------- */
-    ierr = VecCopy(p,V[k]);CHKERRQ(ierr);                     /*         w[i]'*(Pold - p) */
-    ierr = VecScale(V[k],1.0/gdot);CHKERRQ(ierr);
-    
-    ierr = VecDot(W[k],p,&gdot);CHKERRQ(ierr);                /* p = (I + v[i]*w[i]')*p   */
-    ierr = VecAXPY(p,gdot,V[k]);CHKERRQ(ierr);
-    ierr = VecCopy(p,pold);CHKERRQ(ierr);
-    
-    ierr = VecAXPY(x, -1.0, p);CHKERRQ(ierr);
+
+    /* set the differences */
+    k = i % m;
+    ierr = VecCopy(f, dF[k]);CHKERRQ(ierr);
+    ierr = VecAXPY(dF[k], -1.0, fold);CHKERRQ(ierr);
+    ierr = VecCopy(x, dX[k]);CHKERRQ(ierr);
+    ierr = VecAXPY(dX[k], -1.0, xold);CHKERRQ(ierr);
+    ierr = VecDot(dX[k], dF[k], &rhosc);CHKERRQ(ierr);
+    rho[k] = 1. / rhosc;
+    ierr = VecCopy(f, fold);CHKERRQ(ierr);
+    ierr = VecCopy(x, xold);CHKERRQ(ierr);
   }
   if (i == snes->max_its) {
     ierr = PetscInfo1(snes, "Maximum number of iterations has been reached: %D\n", snes->max_its);CHKERRQ(ierr);
@@ -173,7 +231,8 @@ static PetscErrorCode SNESSetUp_QN(SNES snes)
   PetscFunctionBegin;
   ierr = VecDuplicateVecs(snes->vec_sol, qn->m, &qn->dX);CHKERRQ(ierr);
   ierr = VecDuplicateVecs(snes->vec_sol, qn->m, &qn->dF);CHKERRQ(ierr);
-  ierr = SNESDefaultGetWork(snes,1);CHKERRQ(ierr);
+  ierr = PetscMalloc3(qn->m, PetscScalar, &qn->alpha, qn->m, PetscScalar, &qn->beta, qn->m, PetscScalar, &qn->rho);CHKERRQ(ierr);
+  ierr = SNESDefaultGetWork(snes,3);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -192,6 +251,7 @@ static PetscErrorCode SNESReset_QN(SNES snes)
     if (qn->dF) {      
       ierr = VecDestroyVecs(qn->m, &qn->dF);CHKERRQ(ierr);
     }
+    ierr = PetscFree3(qn->alpha, qn->beta, qn->rho);CHKERRQ(ierr);
   }
   if (snes->work) {ierr = VecDestroyVecs(snes->nwork,&snes->work);CHKERRQ(ierr);}
   PetscFunctionReturn(0);
@@ -256,7 +316,7 @@ PetscErrorCode  SNESCreate_QN(SNES snes)
 
   ierr = PetscNewLog(snes, QNContext, &qn);CHKERRQ(ierr);
   snes->data = (void *) qn;
-  qn->m = 10;
+  qn->m = 100;
   qn->lambda = 1.;
   qn->dX = PETSC_NULL;
   qn->dF = PETSC_NULL;
