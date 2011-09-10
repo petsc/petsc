953d969f75cc165c036db8b08aae58fd24010170
Jed Brown <jed@59A2.org>
2011-09-10 13:47:56 +0200

    Fix Fortran line length issues in example
    Hg-commit: 0a7d39cc16366969505de795575bc44bda3877cf

--- src/ksp/ksp/examples/tutorials/ex44f.F90
+++ src/ksp/ksp/examples/tutorials/ex44f.F90
@@ -1,11 +1,17 @@
       program main   !   Solves the linear system  J x = f
 #include <finclude/petscdef.h>
       use petscksp; use petscdm
-      Vec x,f; Mat J; DM da; KSP ksp; PetscErrorCode ierr
+      Vec x,f
+      Mat J
+      DM da
+      KSP ksp
+      PetscErrorCode ierr
       call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
 
-      call DMDACreate1d(MPI_COMM_WORLD,DMDA_BOUNDARY_NONE,8,1,1,PETSC_NULL_INTEGER,da,ierr)
-      call DMCreateGlobalVector(da,x,ierr); call VecDuplicate(x,f,ierr)
+      call DMDACreate1d(MPI_COMM_WORLD,DMDA_BOUNDARY_NONE,8,1,1,        &
+     &  PETSC_NULL_INTEGER,da,ierr)
+      call DMCreateGlobalVector(da,x,ierr)
+      call VecDuplicate(x,f,ierr)
       call DMGetMatrix(da,MATAIJ,J,ierr)
 
       call ComputeRHS(da,f,ierr)
@@ -16,16 +22,29 @@
       call KSPSetFromOptions(ksp,ierr)
       call KSPSolve(ksp,f,x,ierr)
 
-      call MatDestroy(J,ierr); call VecDestroy(x,ierr); call VecDestroy(f,ierr)
-      call KSPDestroy(ksp,ierr); call DMDestroy(da,ierr)
+      call MatDestroy(J,ierr)
+      call VecDestroy(x,ierr)
+      call VecDestroy(f,ierr)
+      call KSPDestroy(ksp,ierr)
+      call DMDestroy(da,ierr)
       call PetscFinalize(ierr)
       end
       subroutine  ComputeRHS(da,x,ierr)
 #include <finclude/petscdef.h>
       use petscdm
-      DM da; Vec x; PetscErrorCode ierr; PetscInt xs,xm,i,mx; PetscScalar hx; PetscScalar, pointer :: xx(:)
-      call DMDAGetInfo(da,PETSC_NULL_INTEGER,mx,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
-      call DMDAGetCorners(da,xs,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,xm,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
+      DM da
+      Vec x
+      PetscErrorCode ierr
+      PetscInt xs,xm,i,mx
+      PetscScalar hx
+      PetscScalar, pointer :: xx(:)
+      call DMDAGetInfo(da,PETSC_NULL_INTEGER,mx,PETSC_NULL_INTEGER,     &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,ierr)
+      call DMDAGetCorners(da,xs,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,  &
+     &  xm,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
       hx     = 1.d0/(mx-1)
       call VecGetArrayF90(x,xx,ierr)
       do i=xs,xs+xm-1
@@ -37,9 +56,18 @@
       subroutine ComputeMatrix(da,J,ierr)
 #include <finclude/petscdef.h>
       use petscdm
-      Mat J; DM da; PetscErrorCode ierr; PetscInt xs,xm,i,mx; PetscScalar hx
-      call DMDAGetInfo(da,PETSC_NULL_INTEGER,mx,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
-      call DMDAGetCorners(da,xs,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,xm,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
+      Mat J
+      DM da
+      PetscErrorCode ierr
+      PetscInt xs,xm,i,mx
+      PetscScalar hx
+      call DMDAGetInfo(da,PETSC_NULL_INTEGER,mx,PETSC_NULL_INTEGER,     &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,       &
+     &  PETSC_NULL_INTEGER,ierr)
+      call DMDAGetCorners(da,xs,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,  &
+     &  xm,PETSC_NULL_INTEGER,PETSC_NULL_INTEGER,ierr)
       hx     = 1.d0/(mx-1)
       do i=xs,xs+xm-1
         if ((i .eq. 0) .or. (i .eq. mx-1)) then
@@ -50,6 +78,7 @@
           call MatSetValue(J,i,i,2*hx,INSERT_VALUES,ierr)
         endif
       enddo
-      call MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY,ierr); call MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY,ierr)
+      call MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY,ierr)
+      call MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY,ierr)
       return 
       end
