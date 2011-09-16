14573d78a028a8148051215a3e34fef9eb94c4b9
Barry Smith <bsmith@mcs.anl.gov>
2011-09-15 21:46:59 -0500

    updated fortran interfaces for line search
    Hg-commit: 41d358b37207cc3e880a1e35878c4b58f1e1b8ee

--- src/snes/impls/ls/ftn-custom/zlsf.c
+++ src/snes/impls/ls/ftn-custom/zlsf.c
@@ -23,10 +23,10 @@
 
 
 /* These are not extern C because they are passed into non-extern C user level functions */
-PetscErrorCode OurSNESLineSearch(SNES snes,void *ctx,Vec x,Vec f,Vec g,Vec y,Vec w,PetscReal fnorm,PetscReal xnorm,PetscReal*ynorm,PetscReal*gnorm,PetscBool  *flag)
+PetscErrorCode OurSNESLineSearch(SNES snes,void *ctx,Vec x,Vec f,Vec y,PetscReal fnorm,PetscReal xnorm,Vec g,Vec w,PetscReal*ynorm,PetscReal*gnorm,PetscBool  *flag)
 {
   PetscErrorCode ierr = 0;
-  (*(void (PETSC_STDCALL *)(SNES*,void*,Vec*,Vec*,Vec*,Vec*,Vec*,PetscReal*,PetscReal*,PetscReal*,PetscReal*,PetscBool *,PetscErrorCode*))(((PetscObject)snes)->fortran_func_pointers[6]))(&snes,ctx,&x,&f,&g,&y,&w,&fnorm,&xnorm,ynorm,gnorm,flag,&ierr);CHKERRQ(ierr);
+  (*(void (PETSC_STDCALL *)(SNES*,void*,Vec*,Vec*,Vec*,PetscReal*,PetscReal*,Vec*,Vec*,PetscReal*,PetscReal*,PetscBool *,PetscErrorCode*))(((PetscObject)snes)->fortran_func_pointers[6]))(&snes,ctx,&x,&f,&y,&fnorm,&xnorm,&g,&w,ynorm,gnorm,flag,&ierr);CHKERRQ(ierr);
   return 0;
 }
 
@@ -72,24 +72,24 @@ void PETSC_STDCALL snessetupdate_(SNES *snes,void (PETSC_STDCALL *f)(SNES*,Petsc
   *ierr = SNESSetUpdate(*snes,OurSNESSetUpdate);
 }  
 /* -----------------------------------------------------------------------------------------------------*/
-void sneslinesearchcubic_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *g,Vec *y,Vec *w,PetscReal*fnorm,PetscReal *xnorm,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
+void sneslinesearchcubic_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *y,PetscReal*fnorm,PetscReal *xnorm,Vec *g,Vec *w,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
 {
-  *ierr = SNESLineSearchCubic(*snes,lsctx,*x,*f,*g,*y,*w,*fnorm,*xnorm,ynorm,gnorm,flag);
+  *ierr = SNESLineSearchCubic(*snes,lsctx,*x,*f,*y,*fnorm,*xnorm,*g,*w,ynorm,gnorm,flag);
 }
-void sneslinesearchquadratic_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *g,Vec *y,Vec *w,PetscReal*fnorm,PetscReal *xnorm,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
+void sneslinesearchquadratic_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *y,PetscReal*fnorm,PetscReal *xnorm,Vec *g,Vec *w,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
 {
-  *ierr = SNESLineSearchQuadratic(*snes,lsctx,*x,*f,*g,*y,*w,*fnorm,*xnorm,ynorm,gnorm,flag);
+  *ierr = SNESLineSearchQuadratic(*snes,lsctx,*x,*f,*y,*fnorm,*xnorm,*g,*w,ynorm,gnorm,flag);
 }
-void sneslinesearchno_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *g,Vec *y,Vec *w,PetscReal*fnorm,PetscReal *xnorm,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
+void sneslinesearchno_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *y,PetscReal*fnorm,PetscReal *xnorm,Vec *g,Vec *w,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
 {
-  *ierr = SNESLineSearchNo(*snes,lsctx,*x,*f,*g,*y,*w,*fnorm,*xnorm,ynorm,gnorm,flag);
+  *ierr = SNESLineSearchNo(*snes,lsctx,*x,*f,*y,*fnorm,*xnorm,*g,*w,ynorm,gnorm,flag);
 }
-void sneslinesearchnonorms_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *g,Vec *y,Vec *w,PetscReal*fnorm,PetscReal *xnorm,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
+void sneslinesearchnonorms_(SNES *snes,void *lsctx,Vec *x,Vec *f,Vec *y,PetscReal*fnorm,PetscReal *xnorm,Vec *g,Vec *w,PetscReal *ynorm,PetscReal *gnorm,PetscBool  *flag,PetscErrorCode *ierr)
 {
-  *ierr = SNESLineSearchNoNorms(*snes,lsctx,*x,*f,*g,*y,*w,*fnorm,*xnorm,ynorm,gnorm,flag);
+  *ierr = SNESLineSearchNoNorms(*snes,lsctx,*x,*f,*y,*fnorm,*xnorm,*g,*w,ynorm,gnorm,flag);
 }
 
-void PETSC_STDCALL sneslinesearchset_(SNES *snes,void (PETSC_STDCALL *f)(SNES*,void *,Vec*,Vec*,Vec*,Vec*,Vec*,PetscReal*,PetscReal*,PetscReal*,PetscReal*,PetscBool *,PetscErrorCode*),void *ctx,PetscErrorCode *ierr)
+void PETSC_STDCALL sneslinesearchset_(SNES *snes,void (PETSC_STDCALL *f)(SNES*,void *,Vec*,Vec*,Vec*,PetscReal*,PetscReal*,Vec*,Vec*,PetscReal*,PetscReal*,PetscBool *,PetscErrorCode*),void *ctx,PetscErrorCode *ierr)
 {
   PetscObjectAllocateFortranPointers(*snes,12);
   if ((PetscVoidFunction)f == (PetscVoidFunction)sneslinesearchcubic_) {
