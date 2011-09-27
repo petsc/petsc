499437a5b6c97ae3198ed9e91b8029ff79645a81
Lisandro Dalcin <dalcinl@gmail.com>
2011-09-27 10:44:07 -0300

    Move around compatibility headers

--- src/include/compat.h
+++ src/include/compat.h
@@ -5,15 +5,15 @@
 #include "compat/mpi.h"
 
 #if PETSC_VERSION_(3,2,0)
-#include "compat-32.h"
+#include "compat/petsc-32.h"
 #endif
 
 #if PETSC_VERSION_(3,1,0)
-#include "compat-31.h"
+#include "compat/petsc-31.h"
 #endif
 
 #if PETSC_VERSION_(3,0,0)
-#include "compat-31.h"
+#include "compat/petsc-31.h"
 #endif
 
 #endif/*PETSC4PY_COMPAT_H*/
