a1630550d816993d1c082e4134e41e45340b2389
Shri Abhyankar <abhyshr@mcs.anl.gov>
2011-09-29 15:09:05 -0500

    Fixed bug in VecSeqPThread_MAXPY. More work on the pthread vector example.
    Hg-commit: 20aa962d541f03e3b99ae9bba4df97f0aebf99df

--- src/vec/vec/examples/tests/ex41.c
+++ src/vec/vec/examples/tests/ex41.c
@@ -15,9 +15,11 @@ int main(int argc,char **argv)
   PetscInt       n = 10;
   PetscScalar    one = 1.0,two = 2.0;
   Vec            x,y,w;
-  PetscScalar    xdoty;
+  Vec*           yvecs;
+  PetscScalar    xdoty,xnorm;
   PetscInt       nthreads=2;
   PetscScalar    alpha=2.0;
+  PetscScalar    mdot[2],alphas[2] ={1.0,2.0};
 
   ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetInt(PETSC_NULL,"-nthreads",&nthreads,PETSC_NULL);CHKERRQ(ierr);
@@ -34,22 +36,52 @@ int main(int argc,char **argv)
   ierr = VecSetType(w,"seqpthread");CHKERRQ(ierr);
   ierr = VecSetFromOptions(w);CHKERRQ(ierr); /* use runtime option -vec_threads option to set number of threads associated with the vector */
 
+  ierr = VecDuplicateVecs(x,2,&yvecs);CHKERRQ(ierr);
   /* Test VecSet */
   ierr = VecSet(x,one);CHKERRQ(ierr);
   ierr = VecSet(y,two);CHKERRQ(ierr);
-  /* Test WAXPY */
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"w = alpha*x + y\n");CHKERRQ(ierr);
+  /* Test WAXPY w = 2.0*1.0 + 2.0 = 4.0 */
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"w = 2.0*x + y\n");CHKERRQ(ierr);
   ierr = VecWAXPY(w,alpha,x,y);CHKERRQ(ierr);
   ierr = VecView(w,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
-  /* Test VecScale */
+
+  /* Test VecScale y = 2.0*0.5 = 1.0 */
   ierr = VecScale(y,0.5);CHKERRQ(ierr);
 
   /* Test VecDot */
   ierr = VecDot(x,y,&xdoty);CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD," x.y = %4.3f\n",xdoty);CHKERRQ(ierr);
+  /* Test VecNorm */
+  ierr = VecNorm(x,NORM_2,&xnorm);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"||x|| = %4.3f\n",xnorm);CHKERRQ(ierr);
+  /* Test AXPY y = 1.0 + 2.0*1.0 = 3.0 */
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"y = y + 2.0*x\n");CHKERRQ(ierr);
+  ierr = VecAXPY(y,alpha,x);CHKERRQ(ierr);
+  ierr = VecView(y,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  /* Test AYPX y = 2.0*3.0 + 1.0 = 7.0 */
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"y = 2.0*y + x\n");CHKERRQ(ierr);
+  ierr = VecAYPX(y,alpha,x);CHKERRQ(ierr);
+  ierr = VecView(y,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  /* Test VecCopy */
+  ierr = VecCopy(x,yvecs[0]);CHKERRQ(ierr);
+  ierr = VecCopy(y,yvecs[1]);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"yvec[0]\n");CHKERRQ(ierr);
+  ierr = VecView(yvecs[0],PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"yvec[1]\n");CHKERRQ(ierr);
+  ierr = VecView(yvecs[1],PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  /* Test VecMDot */
+  ierr = VecMDot(x,2,yvecs,mdot);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"x.yvec[0] = %4.3f x.yvec2 = %4.3f\n",mdot[0],mdot[1]);CHKERRQ(ierr);
+  /* Test VecMAXPY w = 1.0*yvec[0] + 2.0*yvec[2] */
+  ierr = VecSet(w,0.0);CHKERRQ(ierr);
+  ierr = VecMAXPY(w,2,(const PetscScalar*)alphas,yvecs);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"w = 1.0*.yvec[0] + 2.0*yvec[1]\n");CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&y);CHKERRQ(ierr);
   ierr = VecDestroy(&w);CHKERRQ(ierr);
+  ierr = VecDestroyVecs(2,&yvecs);CHKERRQ(ierr);
 #endif
   ierr = PetscFinalize();
   return 0;
