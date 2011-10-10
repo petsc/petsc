f639d13fcc38465e938c5108db803649fd2c407e
Hong Zhang <hzhang@mcs.anl.gov>
2011-10-10 12:57:41 -0500

    cleanup Mat_MatMatMultMPI
    Hg-commit: 7c55205c3b9f22153e4e8fd3eed1e8b0593abd9e

--- src/mat/impls/aij/mpi/mpiaij.h
+++ src/mat/impls/aij/mpi/mpiaij.h
@@ -43,16 +43,18 @@ typedef struct {
 } Mat_MPIAIJ;
 
 typedef struct { /* used by MatMatMult_MPIAIJ_MPIAIJ and MatPtAP_MPIAIJ_MPIAIJ for reusing symbolic mat product */
+  /* used by MatMatMult_MPIAIJ_MPIAIJ() */
+  IS             isrowa,isrowb,iscolb; 
+  Mat            B_seq,A_loc,C_seq;
+
+  /* used by MatPtAP_MPIAIJ_MPIAIJ */
   PetscInt       *startsj,*startsj_r;
   PetscScalar    *bufa;
-  IS             isrowa,isrowb,iscolb; 
-  Mat            *aseq,*bseq,C_seq; /* A_seq=aseq[0], B_seq=bseq[0] */
-  Mat            A_loc,B_seq;
   Mat            B_loc,B_oth;  /* partial B_seq -- intend to replace B_seq */
-  //PetscInt       brstart; /* starting owned rows of B in matrix bseq[0]; brend = brstart+B->m */ // not being used!!!
-  PetscInt       *abi,*abj; /* symbolic i and j arrays of the local product A_loc*B_seq */
-  PetscInt       abnz_max;  /* max(abi[i+1] - abi[i]), max num of nnz in a row of A_loc*B_seq */
+  PetscInt       *abi,*abj;    /* symbolic i and j arrays of the local product A_loc*B_seq */
+  PetscInt       abnz_max;     /* max(abi[i+1] - abi[i]), max num of nnz in a row of A_loc*B_seq */
   MatReuse       reuse; 
+
   PetscErrorCode (*destroy)(Mat);
   PetscErrorCode (*duplicate)(Mat,MatDuplicateOption,Mat*);
 } Mat_MatMatMultMPI;
