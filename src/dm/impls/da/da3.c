6f951b9505d1f14bccad7423b8c71a09c758204a
ecoon <ecoon@lanl.gov>
2011-10-11 16:56:31 -0600

    bug fix in when indices get altered to look like 2D for 3D das with 1 node in the z-direction
    Hg-commit: c538fc85d7fdfa048cce837a57ee9f596d985cd9

--- src/dm/impls/da/da3.c
+++ src/dm/impls/da/da3.c
@@ -197,7 +197,9 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
   Vec                    local,global;
   VecScatter             ltog,gtol;
   IS                     to,from,ltogis;
+  PetscBool              twod;
   PetscErrorCode         ierr;
+
 
   PetscFunctionBegin;
   if (dof < 1) SETERRQ1(((PetscObject)da)->comm,PETSC_ERR_ARG_OUTOFRANGE,"Must have 1 or more degrees of freedom per node: %D",dof);
@@ -335,7 +337,10 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
   /* note this is different than x- and y-, as we will handle as an important special
    case when p=P=1 and DMDA_BOUNDARY_PERIODIC and s > z.  This is to deal with 2D problems
    in a 3D code.  Additional code for this case is noted with "2d case" comments */
-  if ((z < s) && ((p > 1) || ((P > 1) && bz == DMDA_BOUNDARY_PERIODIC))) {
+  twod = PETSC_FALSE;
+  if (P == 1) {
+    twod = PETSC_TRUE;
+  } else if ((z < s) && ((p > 1) || (bz == DMDA_BOUNDARY_PERIODIC))) {
     SETERRQ2(PETSC_COMM_SELF,PETSC_ERR_ARG_OUTOFRANGE,"Local z-width of domain z %D is smaller than stencil width s %D",z,s);
   }
   zs = 0;
@@ -743,9 +748,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n0 % (m*n))/m];
         z_t = lz[n0 / (m*n)];
         s_t = bases[n0] + x_t*y_t*z_t - (s_y-i)*x_t - s_x - (s_z-k-1)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n0] + x_t*y_t*z_t - (s_y-i)*x_t - s_x;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n0] + x_t*y_t*z_t - (s_y-i)*x_t - s_x;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
       if (n1 >= 0) { /* directly below */
@@ -753,9 +756,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n1 % (m*n))/m];
         z_t = lz[n1 / (m*n)];
         s_t = bases[n1] + x_t*y_t*z_t - (s_y+1-i)*x_t - (s_z-k-1)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n1] + x_t*y_t*z_t - (s_y+1-i)*x_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n1] + x_t*y_t*z_t - (s_y+1-i)*x_t;} /* 2D case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
       if (n2 >= 0) { /* right below */
@@ -763,9 +764,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n2 % (m*n))/m];
         z_t = lz[n2 / (m*n)];
         s_t = bases[n2] + x_t*y_t*z_t - (s_y+1-i)*x_t - (s_z-k-1)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n2] + x_t*y_t*z_t - (s_y+1-i)*x_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n2] + x_t*y_t*z_t - (s_y+1-i)*x_t;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
@@ -776,9 +775,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         z_t = lz[n3 / (m*n)];
         s_t = bases[n3] + (i+1)*x_t - s_x + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n3] + (i+1)*x_t - s_x + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n3] + (i+1)*x_t - s_x + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
 
@@ -787,9 +784,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         z_t = lz[n4 / (m*n)];
         s_t = bases[n4] + i*x_t + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n4] + i*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n4] + i*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
 
@@ -798,9 +793,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         z_t = lz[n5 / (m*n)];
         s_t = bases[n5] + i*x_t + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n5] + i*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n5] + i*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
@@ -811,9 +804,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n6 % (m*n))/m];
         z_t = lz[n6 / (m*n)];
         s_t = bases[n6] + i*x_t - s_x + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n6] + i*x_t - s_x + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n6] + i*x_t - s_x + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
       if (n7 >= 0) { /* directly above */
@@ -821,9 +812,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n7 % (m*n))/m];
         z_t = lz[n7 / (m*n)];
         s_t = bases[n7] + (i-1)*x_t + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n7] + (i-1)*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n7] + (i-1)*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
       if (n8 >= 0) { /* right above */
@@ -831,9 +820,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n8 % (m*n))/m];
         z_t = lz[n8 / (m*n)];
         s_t = bases[n8] + (i-1)*x_t + x_t*y_t*z_t - (s_z-k)*x_t*y_t;
-#if defined(foo)
-        if (s_t < 0) {s_t = bases[n8] + (i-1)*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
-#endif
+        if (twod && (s_t < 0)) {s_t = bases[n8] + (i-1)*x_t + x_t*y_t*z_t - x_t*y_t;} /* 2D case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
@@ -920,9 +907,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n18 % (m*n))/m]; 
         /* z_t = lz[n18 / (m*n)]; */
         s_t = bases[n18] - (s_y-i)*x_t -s_x + (k+1)*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n18] - (s_y-i)*x_t -s_x + x_t*y_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n18] - (s_y-i)*x_t -s_x + x_t*y_t;} /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
       if (n19 >= 0) { /* directly below */
@@ -930,9 +915,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n19 % (m*n))/m]; 
         /* z_t = lz[n19 / (m*n)]; */
         s_t = bases[n19] - (s_y+1-i)*x_t + (k+1)*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n19] - (s_y+1-i)*x_t + x_t*y_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n19] - (s_y+1-i)*x_t + x_t*y_t;} /* 2d case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
       if (n20 >= 0) { /* right below */
@@ -940,9 +923,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n20 % (m*n))/m];
         /* z_t = lz[n20 / (m*n)]; */
         s_t = bases[n20] - (s_y+1-i)*x_t + (k+1)*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n20] - (s_y+1-i)*x_t + x_t*y_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n20] - (s_y+1-i)*x_t + x_t*y_t;} /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
@@ -953,9 +934,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         /* z_t = lz[n21 / (m*n)]; */
         s_t = bases[n21] + (i+1)*x_t - s_x + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n21] + (i+1)*x_t - s_x;}  /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n21] + (i+1)*x_t - s_x;}  /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
 
@@ -964,9 +943,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         /* z_t = lz[n22 / (m*n)]; */
         s_t = bases[n22] + i*x_t + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n22] + i*x_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n22] + i*x_t;} /* 2d case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
 
@@ -975,9 +952,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = y;
         /* z_t = lz[n23 / (m*n)]; */
         s_t = bases[n23] + i*x_t + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n23] + i*x_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n23] + i*x_t;} /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
@@ -988,9 +963,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n24 % (m*n))/m]; 
         /* z_t = lz[n24 / (m*n)]; */
         s_t = bases[n24] + i*x_t - s_x + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n24] + i*x_t - s_x;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n24] + i*x_t - s_x;} /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
       if (n25 >= 0) { /* directly above */
@@ -998,9 +971,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n25 % (m*n))/m];
         /* z_t = lz[n25 / (m*n)]; */
         s_t = bases[n25] + (i-1)*x_t + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n25] + (i-1)*x_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n25] + (i-1)*x_t;} /* 2d case */
         for (j=0; j<x_t; j++) { idx[nn++] = s_t++;}
       }
       if (n26 >= 0) { /* right above */
@@ -1008,9 +979,7 @@ PetscErrorCode  DMSetUp_DA_3D(DM da)
         y_t = ly[(n26 % (m*n))/m]; 
         /* z_t = lz[n26 / (m*n)]; */
         s_t = bases[n26] + (i-1)*x_t + k*x_t*y_t;
-#if defined(foo)
-        if (s_t >= x*y*z) {s_t = bases[n26] + (i-1)*x_t;} /* 2d case */
-#endif
+        if (twod && (s_t >= M*N*P)) {s_t = bases[n26] + (i-1)*x_t;} /* 2d case */
         for (j=0; j<s_x; j++) { idx[nn++] = s_t++;}
       }
     }
