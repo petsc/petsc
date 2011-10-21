7e1ba4dc58fd55ef9f38513bc6ab053198bccaff
Jed Brown <jed@59A2.org>
2011-10-21 17:56:14 -0500

    Improve step rejection semantics and add aggressive shortening for repeated rejections
    Hg-commit: e0a3bf21d219135a05e9efe7e1ae4cfd5e726143

--- src/ts/adapt/impls/basic/adaptbasic.c
+++ src/ts/adapt/impls/basic/adaptbasic.c
@@ -4,6 +4,7 @@ typedef struct {
   PetscBool always_accept;
   PetscReal clip[2];            /* admissible decrease/increase factors */
   PetscReal safety;             /* safety factor relative to target error */
+  PetscReal reject_safety;      /* extra safety factor if the last step was rejected */
   Vec       Y;
 } TSAdapt_Basic;
 
@@ -14,7 +15,7 @@ static PetscErrorCode TSAdaptChoose_Basic(TSAdapt adapt,TS ts,PetscReal h,PetscI
   TSAdapt_Basic     *basic = (TSAdapt_Basic*)adapt->data;
   PetscErrorCode    ierr;
   Vec               X,Y;
-  PetscReal         enorm,hfac_lte,h_lte;
+  PetscReal         enorm,hfac_lte,h_lte,safety;
   PetscInt          order,stepno;
 
   PetscFunctionBegin;
@@ -25,10 +26,15 @@ static PetscErrorCode TSAdaptChoose_Basic(TSAdapt adapt,TS ts,PetscReal h,PetscI
   order = adapt->candidates.order[0];
   ierr = TSEvaluateStep(ts,order-1,Y,PETSC_NULL);CHKERRQ(ierr);
 
+  safety = basic->safety;
   ierr = TSErrorNormWRMS(ts,Y,&enorm);CHKERRQ(ierr);
   if (enorm > 1.) {
+    if (!*accept) safety *= basic->reject_safety; /* The last attempt also failed, shorten more aggressively */
     if (h < (1 + PETSC_SQRT_MACHINE_EPSILON)*adapt->dt_min) {
       ierr = PetscInfo2(adapt,"Estimated scaled local truncation error %G, accepting because step size %G is at minimum\n",enorm,h);CHKERRQ(ierr);
+      *accept = PETSC_TRUE;
+    } else if (basic->always_accept) {
+      ierr = PetscInfo2(adapt,"Estimated scaled local truncation error %G, accepting step of size %G because always_accept is set\n",enorm,h);CHKERRQ(ierr);
       *accept = PETSC_TRUE;
     } else {
       ierr = PetscInfo2(adapt,"Estimated scaled local truncation error %G, rejecting step of size %G\n",enorm,h);CHKERRQ(ierr);
@@ -40,7 +46,7 @@ static PetscErrorCode TSAdaptChoose_Basic(TSAdapt adapt,TS ts,PetscReal h,PetscI
   }
 
   /* The optimal new step based purely on local truncation error for this step. */
-  hfac_lte = basic->safety * PetscRealPart(PetscPowScalar((PetscScalar)enorm,(PetscReal)(-1./order)));
+  hfac_lte = safety * PetscRealPart(PetscPowScalar((PetscScalar)enorm,(PetscReal)(-1./order)));
   h_lte = h * PetscClipInterval(hfac_lte,basic->clip[0],basic->clip[1]);
 
   *next_sc = 0;
@@ -78,8 +84,8 @@ static PetscErrorCode TSAdaptSetFromOptions_Basic(TSAdapt adapt)
   if (set && (two != 2 || basic->clip[0] > basic->clip[1]))
     SETERRQ(((PetscObject)adapt)->comm,PETSC_ERR_ARG_OUTOFRANGE,"Must give exactly two values to -ts_adapt_basic_clip");
   ierr = PetscOptionsReal("-ts_adapt_basic_safety","Safety factor relative to target error","",basic->safety,&basic->safety,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsReal("-ts_adapt_basic_reject_safety","Extra safety factor to apply if the last step was rejected","",basic->reject_safety,&basic->reject_safety,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsBool("-ts_adapt_basic_always_accept","Always accept the step regardless of whether local truncation error meets goal","",basic->always_accept,&basic->always_accept,PETSC_NULL);CHKERRQ(ierr);
-  if (!basic->always_accept) SETERRQ(((PetscObject)adapt)->comm,PETSC_ERR_SUP,"step rejection not implemented yet");
   ierr = PetscOptionsTail();CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
@@ -106,10 +112,11 @@ PetscErrorCode TSAdaptCreate_Basic(TSAdapt adapt)
   adapt->ops->setfromoptions = TSAdaptSetFromOptions_Basic;
   adapt->ops->destroy        = TSAdaptDestroy_Basic;
 
-  a->clip[0] = 0.1;
-  a->clip[1] = 10.;
-  a->safety  = 0.9;
-  a->always_accept = PETSC_TRUE; /* fix this */
+  a->clip[0]       = 0.1;
+  a->clip[1]       = 10.;
+  a->safety        = 0.9;
+  a->reject_safety = 0.5;
+  a->always_accept = PETSC_FALSE;
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
