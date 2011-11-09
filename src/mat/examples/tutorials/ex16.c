e18fc0eb057be7f809d53c0de4e8596c558fbb14
Hong Zhang <hzhang@mcs.anl.gov>
2011-11-09 09:46:27 -0600

    update src/mat/examples/tutorials/ex16.c
    Hg-commit: 564c24e4bb568e7c26064ffd86848f51cdc13a4c

--- src/mat/examples/tutorials/ex16.c
+++ src/mat/examples/tutorials/ex16.c
@@ -11,17 +11,20 @@ static char help[] = "Reads a matrix from PETSc binary file. Use for view or inv
 #define __FUNCT__ "main"
 int main(int argc,char **args)
 {
-  Mat                   A;          
+  Mat                   A,Asp;          
   PetscViewer           fd;               /* viewer */
   char                  file[PETSC_MAX_PATH_LEN];     /* input file name */
   PetscErrorCode        ierr;
   PetscInt              m,n,rstart,rend;
   PetscBool             flg;
-  PetscInt             row,ncols,j,nrows,nnz=0;
+  PetscInt             row,ncols,j,nrows,nnzA=0,nnzAsp=0;
   const PetscInt       *cols;
   const PetscScalar    *vals;
-  PetscReal            norm,percent;
+  PetscReal            norm,percent,val,dtol=1.e-16;
   PetscMPIInt          rank;
+  MatInfo              matinfo;
+  PetscInt             Dnnz,Onnz;
+  
 
   PetscInitialize(&argc,&args,(char *)0,help);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
@@ -40,31 +43,75 @@ int main(int argc,char **args)
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
   ierr = MatLoad(A,fd);CHKERRQ(ierr);
   ierr = PetscViewerDestroy(&fd);CHKERRQ(ierr);
+  ierr = MatGetSize(A,&m,&n);CHKERRQ(ierr);
+  ierr = MatGetInfo(A,MAT_LOCAL,&matinfo);CHKERRQ(ierr);
+  //printf("matinfo.nz_used %g\n",matinfo.nz_used);
 
+  /* Get a sparse matrix Asp by dumping zero entries of A */
+  ierr = MatCreate(PETSC_COMM_WORLD,&Asp);CHKERRQ(ierr);
+  ierr = MatSetSizes(Asp,m,n,PETSC_DECIDE,PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = MatSetOptionsPrefix(Asp,"asp_");CHKERRQ(ierr);
+  ierr = MatSetFromOptions(Asp);CHKERRQ(ierr);
+  Dnnz  = (PetscInt)matinfo.nz_used/m + 1;
+  Onnz  = Dnnz/2;
+  printf("Dnnz %d %d\n",Dnnz,Onnz);
+  ierr = MatSeqAIJSetPreallocation(Asp,Dnnz,PETSC_NULL);CHKERRQ(ierr);
+  ierr = MatMPIAIJSetPreallocation(Asp,Dnnz,PETSC_NULL,Onnz,PETSC_NULL);CHKERRQ(ierr);
+ 
   /* Check zero rows */
-  ierr = MatGetLocalSize(A,&m,&n);CHKERRQ(ierr);
   ierr = MatGetOwnershipRange(A,&rstart,&rend);CHKERRQ(ierr);
-  //printf("zero rows:\n");
   nrows = 0;
   for (row=rstart; row<rend; row++){
     ierr = MatGetRow(A,row,&ncols,&cols,&vals);CHKERRQ(ierr);
-    nnz += ncols;
+    nnzA += ncols;
     norm = 0.0;
     for (j=0; j<ncols; j++){
-      if (norm < PetscAbsScalar(vals[j])) norm = vals[j];
+      val = PetscAbsScalar(vals[j]);
+      if (norm < val) norm = norm;
+      if (val > dtol){
+        ierr = MatSetValues(Asp,1,&row,1,&cols[j],&vals[j],INSERT_VALUES);CHKERRQ(ierr);
+        nnzAsp++;
+      }
     }
-    if (!norm) {
-      //printf("  %d,",row); 
-      nrows++;
-    }
+    if (!norm) nrows++;
     ierr = MatRestoreRow(A,row,&ncols,&cols,&vals);CHKERRQ(ierr);
   }
+  ierr = MatAssemblyBegin(Asp,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(Asp,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
  
-  percent=(PetscReal)nnz*100/(m*n);
-  //percent = percent*100/(m*n);
-  ierr = PetscPrintf(PETSC_COMM_SELF,"\n [%d] Matrix local size %d,%d; nnz %d, %g percent; No. of zero rows: %d\n",rank,m,n,nnz,percent,nrows);
-  
+  percent=(PetscReal)nnzA*100/(m*n);
+  ierr = PetscPrintf(PETSC_COMM_SELF," [%d] Matrix A local size %d,%d; nnzA %d, %g percent; No. of zero rows: %d\n",rank,m,n,nnzA,percent,nrows);
+  percent=(PetscReal)nnzAsp*100/(m*n);
+  ierr = PetscPrintf(PETSC_COMM_SELF," [%d] Matrix Asp nnzAsp %d, %g percent\n",rank,nnzAsp,percent);
+
+  /* investigate matcoloring for Asp */
+  PetscBool     Asp_coloring = PETSC_FALSE;
+  ierr = PetscOptionsHasName(PETSC_NULL,"-Asp_color",&Asp_coloring);CHKERRQ(ierr);
+  if (Asp_coloring){
+    ISColoring    iscoloring;
+    MatFDColoring matfdcoloring;
+    ierr = PetscPrintf(PETSC_COMM_WORLD," Create coloring of Asp...\n");
+    ierr = MatGetColoring(Asp,MATCOLORINGSL,&iscoloring);CHKERRQ(ierr);
+    ierr = MatFDColoringCreate(Asp,iscoloring,&matfdcoloring);CHKERRQ(ierr);
+    ierr = MatFDColoringSetFromOptions(matfdcoloring);CHKERRQ(ierr);
+    //ierr = MatFDColoringView(matfdcoloring,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+    ierr = ISColoringDestroy(&iscoloring);CHKERRQ(ierr);
+    ierr = MatFDColoringDestroy(&matfdcoloring);CHKERRQ(ierr);
+  }
+
+  /* Write Asp in binary for study - see ~petsc/src/mat/examples/tests/ex124.c */
+  PetscBool Asp_write = PETSC_FALSE;
+  ierr = PetscOptionsHasName(PETSC_NULL,"-Asp_write",&Asp_write);CHKERRQ(ierr);
+  if (Asp_write){
+    PetscViewer    viewer;
+    ierr = PetscPrintf(PETSC_COMM_SELF,"Write Asp into file Asp.dat ...\n");
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"Asp.dat",FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+    ierr = MatView(Asp,viewer);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+  }
+
   ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = MatDestroy(&Asp);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return 0;
 }
