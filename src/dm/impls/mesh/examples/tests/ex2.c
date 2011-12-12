849e0edb65be98a1d937daeb388a22b0a6ba7bc7
Matthew G Knepley <knepley@gmail.com>
2011-12-12 11:35:02 -0600

    Added test for Jed's PetscBG
    Hg-commit: 1096375efc254c827613074ca7a9c4ff542c8b46

---
+++ src/dm/impls/mesh/examples/tests/ex2.c
@@ -0,0 +1,142 @@
+static char help[] = "Test of Sieve vs. New Mesh and Field Distribution.\n\n";
+#include <petscdmmesh.h>
+
+typedef struct {
+  DM            dm;                /* REQUIRED in order to use SNES evaluation functions */
+  PetscInt      debug;             /* The debugging level */
+  PetscMPIInt   rank;              /* The process rank */
+  PetscMPIInt   numProcs;          /* The number of processes */
+  PetscInt      dim;               /* The topological mesh dimension */
+  PetscBool     interpolate;       /* Generate intermediate mesh elements */
+  PetscReal     refinementLimit;   /* The largest allowable cell volume */
+  char          filename[2048];    /* Optional filename to read mesh from */
+  char          partitioner[2048]; /* The graph partitioner */
+  PetscLogEvent createMeshEvent;
+} AppCtx;
+
+#undef __FUNCT__
+#define __FUNCT__ "ProcessOptions"
+PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options) {
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  options->debug           = 0;
+  options->dim             = 2;
+  options->interpolate     = PETSC_FALSE;
+  options->refinementLimit = 0.0;
+
+  ierr = MPI_Comm_size(comm, &options->numProcs);CHKERRQ(ierr);
+  ierr = MPI_Comm_rank(comm, &options->rank);CHKERRQ(ierr);
+  ierr = PetscOptionsBegin(comm, "", "Mesh Distribution Options", "DMMESH");CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-debug", "The debugging level", "ex1.c", options->debug, &options->debug, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-dim", "The topological mesh dimension", "ex1.c", options->dim, &options->dim, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsBool("-interpolate", "Generate intermediate mesh elements", "ex1.c", options->interpolate, &options->interpolate, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsReal("-refinement_limit", "The largest allowable cell volume", "ex1.c", options->refinementLimit, &options->refinementLimit, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscStrcpy(options->filename, "");CHKERRQ(ierr);
+  ierr = PetscOptionsString("-filename", "The input filename", "ex1.c", options->filename, options->filename, 2048, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscStrcpy(options->partitioner, "chaco");CHKERRQ(ierr);
+  ierr = PetscOptionsString("-partitioner", "The graph partitioner", "ex1.c", options->partitioner, options->partitioner, 2048, PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();
+
+  ierr = PetscLogEventRegister("CreateMesh",    DM_CLASSID,   &options->createMeshEvent);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+};
+
+#undef __FUNCT__
+#define __FUNCT__ "CreateMesh"
+PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
+{
+  PetscInt       dim             = user->dim;
+  PetscBool      interpolate     = user->interpolate;
+  PetscReal      refinementLimit = user->refinementLimit;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscLogEventBegin(user->createMeshEvent,0,0,0,0);CHKERRQ(ierr);
+  ierr = DMMeshCreateBoxMesh(comm, dim, interpolate, dm);CHKERRQ(ierr);
+  {
+    DM refinedMesh     = PETSC_NULL;
+    DM distributedMesh = PETSC_NULL;
+    const char *partitioner = user->partitioner;
+
+    /* Refine mesh using a volume constraint */
+    ierr = DMMeshRefine(*dm, refinementLimit, interpolate, &refinedMesh);CHKERRQ(ierr);
+    if (refinedMesh) {
+      ierr = DMDestroy(dm);CHKERRQ(ierr);
+      *dm  = refinedMesh;
+    }
+    /* Distribute mesh over processes */
+    ierr = DMMeshDistribute(*dm, partitioner, &distributedMesh);CHKERRQ(ierr);
+    if (distributedMesh) {
+      ierr = DMDestroy(dm);CHKERRQ(ierr);
+      *dm  = distributedMesh;
+    }
+  }
+  ierr = DMSetFromOptions(*dm);CHKERRQ(ierr);
+  ierr = PetscLogEventEnd(user->createMeshEvent,0,0,0,0);CHKERRQ(ierr);
+  user->dm = *dm;
+  PetscFunctionReturn(0);
+}
+
+typedef struct {
+  PetscInt rank;                /* Rank of owner */
+  PetscInt index;               /* Index of node on rank */
+} PetscBGNode;
+
+#undef __FUNCT__
+#define __FUNCT__ "DMMeshConvertOverlapToBG"
+PetscErrorCode DMMeshConvertOverlapToBG(DM dm, PetscBG *bg)
+{
+  ALE::Obj<PETSC_MESH_TYPE> mesh;
+  PetscInt      *local;
+  PetscBGNode   *remote;
+  PetscInt       numPoints;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscBGCreate(((PetscObject) dm)->comm, bg);CHKERRQ(ierr);
+  ierr = DMMeshGetMesh(dm, mesh);CHKERRQ(ierr);
+  {
+    /* The local points have degree 1
+         We use the recv overlap
+    */
+    ALE::Obj<PETSC_MESH_TYPE::recv_overlap_type> overlap = mesh->getRecvOverlap();
+
+    numPoints = overlap->getNumPoints();
+    ierr = PetscMalloc(numPoints * sizeof(PetscInt), &local);CHKERRQ(ierr);
+    ierr = PetscMalloc(numPoints * sizeof(PetscBGNode), &remote);CHKERRQ(ierr);
+    for(PetscInt r = 0, i = 0; r < overlap->getNumRanks(); ++r) {
+      const PETSC_MESH_TYPE::recv_overlap_type::supportSequence::iterator cBegin = overlap->supportBegin(r);
+      const PETSC_MESH_TYPE::recv_overlap_type::supportSequence::iterator cEnd   = overlap->supportEnd(r);
+      const PetscInt                                                      rank   = overlap->getRank(r);
+
+      for(PETSC_MESH_TYPE::recv_overlap_type::supportSequence::iterator c_iter = cBegin; c_iter != cEnd; ++c_iter, ++i) {
+        local[i]        = *c_iter;
+        remote[i].rank  = rank;
+        remote[i].index = c_iter.color();
+      }
+    }
+    ierr = PetscBGSetGraph(*bg, numPoints, local, PETSC_OWN_POINTER, remote, PETSC_OWN_POINTER);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc, char *argv[])
+{
+  MPI_Comm       comm;
+  DM             dm;
+  PetscBG        bg;
+  AppCtx         user;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscInitialize(&argc, &argv, (char *) 0, help);CHKERRQ(ierr);
+  comm = PETSC_COMM_WORLD;
+  ierr = ProcessOptions(comm, &user);CHKERRQ(ierr);
+  ierr = CreateMesh(comm, &user, &dm);CHKERRQ(ierr);
+  ierr = DMMeshConvertOverlapToBG(dm, &bg);CHKERRQ(ierr);
+  ierr = PetscFinalize();
+  PetscFunctionReturn(0);
+}
