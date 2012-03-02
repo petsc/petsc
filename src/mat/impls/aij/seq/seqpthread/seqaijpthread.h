62c42960dbe89666b4d11562a5e8422561368d50
Shri Abhyankar <abhyshr@mcs.anl.gov>
2012-03-02 17:17:04 -0600

    Added PetscThreadsLayout object to manage layout information for threads.
    Hg-commit: 67d46ac2c4a968ea3ac836a1334d33833262f001

--- src/mat/impls/aij/seq/seqpthread/seqaijpthread.h
+++ src/mat/impls/aij/seq/seqpthread/seqaijpthread.h
@@ -3,11 +3,11 @@
 #define __AIJPTHREAD_H
 
 typedef struct {
-  PetscInt   *rstart;       /* List of starting row number for each thread */
-  PetscInt   *nrows;        /* List of nrows for each thread */
+  PetscInt   *rstart;       /* starting row number for each thread */
+  PetscInt   *nrows;        /* nrows for each thread */
   PetscInt   nthreads;      /* Number of threads to use for matrix operations */
   PetscInt   *cpu_affinity; /* CPU affinity of threads */
-  PetscInt   *nz;           /* List of number of nonzeros for each thread */
+  PetscInt   *nz;           /* Number of nonzeros for each thread */
 }Mat_SeqAIJPThread;
 
 typedef struct {
