2961e1531ddf07ed27c8cca5c8da65ce9e2660e8
Jed Brown <jed@59A2.org>
2012-03-09 11:49:59 -0600

    Add DMDA SNES local Jacobian support, does not work for rediscretized coarse levels; removed old junk from example
    Hg-commit: c5e2e9b14c81fedb8c1dae1e0e3b686f5ee813ba

--- src/snes/utils/dmdasnes.c
+++ src/snes/utils/dmdasnes.c
@@ -1,4 +1,5 @@
 #include <petscdmda.h>          /*I "petscdmda.h" I*/
+#include <private/dmimpl.h>
 #include <private/snesimpl.h>   /*I "petscsnes.h" I*/
 
 /* This structure holds the user-provided DMDA callbacks */
@@ -51,6 +52,9 @@ static PetscErrorCode SNESComputeFunction_DMDA(SNES snes,Vec X,Vec F,void *ctx)
   void           *x,*f;
 
   PetscFunctionBegin;
+  PetscValidHeaderSpecific(snes,SNES_CLASSID,1);
+  PetscValidHeaderSpecific(X,VEC_CLASSID,2);
+  PetscValidHeaderSpecific(F,VEC_CLASSID,3);
   if (!dmdasnes->residuallocal) SETERRQ(((PetscObject)snes)->comm,PETSC_ERR_PLIB,"Corrupt context");
   ierr = SNESGetDM(snes,&dm);CHKERRQ(ierr);
   ierr = DMGetLocalVector(dm,&Xloc);CHKERRQ(ierr);
@@ -65,6 +69,75 @@ static PetscErrorCode SNESComputeFunction_DMDA(SNES snes,Vec X,Vec F,void *ctx)
   ierr = DMDAVecRestoreArray(dm,Xloc,&x);CHKERRQ(ierr);
   ierr = DMDAVecRestoreArray(dm,F,&f);CHKERRQ(ierr);
   ierr = DMRestoreLocalVector(dm,&Xloc);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "SNESComputeJacobian_DMDA"
+/*
+  This function should eventually replace:
+    DMComputeJacobian() and DMDAComputeJacobian1()
+ */
+static PetscErrorCode SNESComputeJacobian_DMDA(SNES snes,Vec X,Mat *A,Mat *B,MatStructure *mstr,void *ctx)
+{
+  PetscErrorCode ierr;
+  DM             dm;
+  DM_DA_SNES     *dmdasnes = (DM_DA_SNES*)ctx;
+  DMDALocalInfo  info;
+  Vec            Xloc;
+  void           *x;
+
+  PetscFunctionBegin;
+  if (!dmdasnes->residuallocal) SETERRQ(((PetscObject)snes)->comm,PETSC_ERR_PLIB,"Corrupt context");
+  ierr = SNESGetDM(snes,&dm);CHKERRQ(ierr);
+
+  if (dmdasnes->jacobianlocal) {
+    ierr = DMGetLocalVector(dm,&Xloc);CHKERRQ(ierr);
+    ierr = DMGlobalToLocalBegin(dm,X,INSERT_VALUES,Xloc);CHKERRQ(ierr);
+    ierr = DMGlobalToLocalEnd(dm,X,INSERT_VALUES,Xloc);CHKERRQ(ierr);
+    ierr = DMDAGetLocalInfo(dm,&info);CHKERRQ(ierr);
+    ierr = DMDAVecGetArray(dm,Xloc,&x);CHKERRQ(ierr);
+    CHKMEMQ;
+    ierr = (*dmdasnes->jacobianlocal)(&info,x,*A,*B,mstr,dmdasnes->jacobianlocalctx);CHKERRQ(ierr);
+    CHKMEMQ;
+    ierr = DMDAVecRestoreArray(dm,Xloc,&x);CHKERRQ(ierr);
+    ierr = DMRestoreLocalVector(dm,&Xloc);CHKERRQ(ierr);
+  } else {
+    MatFDColoring fdcoloring;
+    ierr = PetscObjectQuery((PetscObject)dm,"DMDASNES_FDCOLORING",(PetscObject*)&fdcoloring);CHKERRQ(ierr);
+    if (!fdcoloring) {
+      ISColoring     coloring;
+
+      ierr = DMCreateColoring(dm,dm->coloringtype,MATAIJ,&coloring);CHKERRQ(ierr);
+      ierr = MatFDColoringCreate(*B,coloring,&fdcoloring);CHKERRQ(ierr);
+      ierr = ISColoringDestroy(&coloring);CHKERRQ(ierr);
+      switch (dm->coloringtype) {
+      case IS_COLORING_GLOBAL:
+        ierr = MatFDColoringSetFunction(fdcoloring,(PetscErrorCode(*)(void))SNESComputeFunction_DMDA,dmdasnes);CHKERRQ(ierr);
+        break;
+      default: SETERRQ1(((PetscObject)snes)->comm,PETSC_ERR_SUP,"No support for coloring type '%s'",ISColoringTypes[dm->coloringtype]);
+      }
+      ierr = PetscObjectSetOptionsPrefix((PetscObject)fdcoloring,((PetscObject)dm)->prefix);CHKERRQ(ierr);
+      ierr = MatFDColoringSetFromOptions(fdcoloring);CHKERRQ(ierr);
+      ierr = PetscObjectCompose((PetscObject)dm,"DMDASNES_FDCOLORING",(PetscObject)fdcoloring);CHKERRQ(ierr);
+      ierr = PetscObjectDereference((PetscObject)fdcoloring);CHKERRQ(ierr);
+
+      /* The following breaks an ugly reference counting loop that deserves a paragraph. MatFDColoringApply() will call
+       * VecDuplicate() with the state Vec and store inside the MatFDColoring. This Vec will duplicate the Vec, but the
+       * MatFDColoring is composed with the DM. We dereference the DM here so that the reference count will eventually
+       * drop to 0. Note the code in DMDestroy() that exits early for a negative reference count. That code path will be
+       * taken when the PetscOList for the Vec inside MatFDColoring is destroyed.
+       */
+      ierr = PetscObjectDereference((PetscObject)dm);CHKERRQ(ierr);
+    }
+    *mstr = SAME_NONZERO_PATTERN;
+    ierr = MatFDColoringApply(*B,fdcoloring,X,mstr,snes);CHKERRQ(ierr);
+  }
+  /* This will be redundant if the user called both, but it's too common to forget. */
+  if (*A != *B) {
+    ierr = MatAssemblyBegin(*A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(*A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
 
@@ -103,5 +176,46 @@ PetscErrorCode DMDASNESSetFunctionLocal(DM dm,PetscErrorCode (*func)(DMDALocalIn
   dmdasnes->residuallocal = func;
   dmdasnes->residuallocalctx = ctx;
   ierr = DMSNESSetFunction(dm,SNESComputeFunction_DMDA,dmdasnes);CHKERRQ(ierr);
+  if (!sdm->computejacobian) {  /* Call us for the Jacobian too, can be overridden by the user. */
+    ierr = DMSNESSetJacobian(dm,SNESComputeJacobian_DMDA,dmdasnes);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "DMDASNESSetJacobianLocal"
+/*@C
+   DMDASNESSetJacobianLocal - set a local residual evaluation function
+
+   Logically Collective
+
+   Input Arguments:
++  dm - DM to associate callback with
+.  func - local residual evaluation
+-  ctx - optional context for local residual evaluation
+
+   Calling sequence for func:
++  info - DMDALocalInfo defining the subdomain to evaluate the residual on
+.  x - dimensional pointer to state at which to evaluate residual
+.  f - dimensional pointer to residual, write the residual here
+-  ctx - optional context passed above
+
+   Level: beginner
+
+.seealso: DMSNESSetJacobian(), DMDASNESSetJacobian(), DMDACreate1d(), DMDACreate2d(), DMDACreate3d()
+@*/
+PetscErrorCode DMDASNESSetJacobianLocal(DM dm,PetscErrorCode (*func)(DMDALocalInfo*,void*,Mat,Mat,MatStructure*,void*),void *ctx)
+{
+  PetscErrorCode ierr;
+  SNESDM         sdm;
+  DM_DA_SNES     *dmdasnes;
+
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(dm,DM_CLASSID,1);
+  ierr = DMSNESGetContext(dm,&sdm);CHKERRQ(ierr);
+  ierr = DMDASNESGetContext(dm,sdm,&dmdasnes);CHKERRQ(ierr);
+  dmdasnes->jacobianlocal = func;
+  dmdasnes->jacobianlocalctx = ctx;
+  ierr = DMSNESSetJacobian(dm,SNESComputeJacobian_DMDA,dmdasnes);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
