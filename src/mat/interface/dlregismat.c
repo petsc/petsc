25017bab1a8fc87bd422787de26f142f963d1edb
Hong Zhang <hzhang@mcs.anl.gov>
2012-03-28 17:43:32 -0500

    shorten event names, because 'long names messing up alignment in -log_summary' as Jed complains.
    Hg-commit: 3912dfbfb18c6b2c4acb669d2596ea6fc2e6e2e4

--- src/mat/interface/dlregismat.c
+++ src/mat/interface/dlregismat.c
@@ -172,10 +172,10 @@ PetscErrorCode  MatInitializePackage(const char path[])
 
 
   /* these may be specific to MPIAIJ matrices */
-  ierr = PetscLogEventRegister("MatCreateMPIAIJSumSeqAIJNumeric",MAT_CLASSID,&MAT_Seqstompinum);
-  ierr = PetscLogEventRegister("MatCreateMPIAIJSumSeqAIJSymbolic",MAT_CLASSID,&MAT_Seqstompisym);
-  ierr = PetscLogEventRegister("MatCreateMPIAIJSumSeqAIJ",MAT_CLASSID,&MAT_Seqstompi);
-  ierr = PetscLogEventRegister("MatCreateMPIAIJConcatenateSeqAIJ",MAT_CLASSID,&MAT_Merge);
+  ierr = PetscLogEventRegister("MatMPISumSeqNumeric",MAT_CLASSID,&MAT_Seqstompinum);
+  ierr = PetscLogEventRegister("MatMPISumSeqSymbolic",MAT_CLASSID,&MAT_Seqstompisym);
+  ierr = PetscLogEventRegister("MatMPISumSeq",MAT_CLASSID,&MAT_Seqstompi);
+  ierr = PetscLogEventRegister("MatMPIConcateSeq",MAT_CLASSID,&MAT_Merge);
   ierr = PetscLogEventRegister("MatGetLocalMat",MAT_CLASSID,&MAT_Getlocalmat);
   ierr = PetscLogEventRegister("MatGetLocalMatCondensed",MAT_CLASSID,&MAT_Getlocalmatcondensed);
   ierr = PetscLogEventRegister("MatGetBrowsOfAcols",MAT_CLASSID,&MAT_GetBrowsOfAcols);
