0fd488f5fc9154c9a18e7281bfce1ead675b2e83
Shri Abhyankar <abhyshr@mcs.anl.gov>
2012-04-08 21:15:41 -0500

    Fixed typo and updated docs for PetscOptionsGetIntArray() and PetscOptionsIntArray()
    Hg-commit: 8e74aca2575bbd757b1e2fb0d6ce8cd0a4ae406c

--- src/sys/objects/aoptions.c
+++ src/sys/objects/aoptions.c
@@ -1276,9 +1276,9 @@ PetscErrorCode  PetscOptionsRealArray(const char opt[],const char text[],const c
    Notes: 
    The array can be passed as
    a comma seperated list:                                 0,1,2,3,4,5,6,7
-   a range (start:end+1):                                  0-8
-   a range with given increment (start-end+1:inc):         0-8:1
-   a combination of values and ranges seperated by commas: 0,1-4,4-8:1
+   a range (start-end+1):                                  0-8
+   a range with given increment (start-end+1:inc):         0-7:2
+   a combination of values and ranges seperated by commas: 0,1-8,8-15:2
 
    There must be no intervening spaces between the values.
 
