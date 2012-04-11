53508a3903df886420e80e6f00220398f7cc5b8b
Satish Balay <balay@mcs.anl.gov>
2012-04-10 22:13:15 -0500

    revert a913e4d88b2a d6ed175b07bc 23628f1ee9b3 [inline patch for hdf5]
    Hg-commit: 747bc45e47e095a2d1d746fd433a1f21816c46ca

--- config/packages/hdf5.py
+++ config/packages/hdf5.py
@@ -1,47 +1,6 @@
 import config.package
 import os
 
-testPatch = '''\
-Index: fortran/test/tH5Sselect.f90
-===================================================================
---- fortran/test/tH5Sselect.f90	2011-10-20 09:37:20.000000000 -0400
-+++ fortran/test/tH5Sselect.f90	2011-10-20 09:38:22.000000000 -0400
-@@ -1884,13 +1884,13 @@
-   CALL check("H5Soffset_simple_f", error, total_error)
- 
-   ! /* Set "regular" hyperslab selection */
--  start(1:2) = 2
-+  !start(1:2) = 2
-   stride(1:2) = 10
-   count(1:2) = 4
-   block(1:2) = 5
- 
--  CALL h5sselect_hyperslab_f(sid, H5S_SELECT_SET_F, start, &
--       count, error, stride, block)
-+  !CALL h5sselect_hyperslab_f(sid, H5S_SELECT_SET_F, start, &
-+  !     count, error, stride, block)
-   CALL check("h5sselect_hyperslab_f", error, total_error)
- 
-   !/* Get bounds for hyperslab selection */
-@@ -1931,13 +1931,13 @@
-   CALL check("H5Soffset_simple_f", error, total_error)
- 
-   ! /* Make "irregular" hyperslab selection */
--  start(1:2) = 20
-+  !start(1:2) = 20
-   stride(1:2) = 20
-   count(1:2) = 2
-   block(1:2) = 10
- 
--  CALL h5sselect_hyperslab_f(sid, H5S_SELECT_OR_F, start, &
--       count, error, stride, block)
-+  !CALL h5sselect_hyperslab_f(sid, H5S_SELECT_OR_F, start, &
-+  !     count, error, stride, block)
-   CALL check("h5sselect_hyperslab_f", error, total_error)
- 
-   !/* Get bounds for hyperslab selection */
-'''
-
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
@@ -86,18 +45,6 @@ class Configure(config.package.Package):
 
     if self.installNeeded('hdf5'):
       try:
-        self.logPrintBox('Patching HDF5; the stupid F90 test is broken for gfortran on the Mac')
-        try:
-          import patch,StringIO
-          oldDir = os.getcwd()
-          os.chdir(self.packageDir)
-          p = StringIO.StringIO(testPatch)
-          patcher = patch.PatchSet(p)
-          p.close()
-          patcher.apply()
-          os.chdir(oldDir)
-        except ImportError:
-          pass
         self.logPrintBox('Configuring HDF5; this may take several minutes')
         output1,err1,ret1  = config.package.Package.executeShellCommand('cd '+self.packageDir+' && ./configure '+args, timeout=900, log = self.framework.log)
       except RuntimeError, e:
