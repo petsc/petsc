31fe5ba55512ab6b0c199eb9524b54bc8715fdab
Matthew G Knepley <knepley@gmail.com>
2012-04-25 13:33:24 -0400

    Fixed declaration
    Hg-commit: a08b45d1534e06e2ffad6d22ea59a4847215ee9c

--- src/sys/threadcomm/interface/dlregisthreadcomm.c
+++ src/sys/threadcomm/interface/dlregisthreadcomm.c
@@ -3,31 +3,6 @@
 static PetscBool PetscThreadCommPackageInitialized = PETSC_FALSE;
 
 #undef __FUNCT__
-#define __FUNCT__ "PetscThreadCommInitializePackage"
-/*@C
-   PetscThreadCommInitializePackage - Initializes ThreadComm package
-
-   Logically collective
-
-   Input Parameter:
-.  path - The dynamic library path, or PETSC_NULL
- 
-   Level: developer
-
-.seealso: PetscThreadCommFinalizePackage()
-@*/
-PetscErrorCode PetscThreadCommInitializePackage(const char *path)
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBegin;
-  if(PetscThreadCommPackageInitialized) PetscFunctionReturn(0);
-  PetscThreadCommPackageInitialized = PETSC_TRUE;
-  ierr = PetscRegisterFinalize(PetscThreadCommFinalizePackage);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
 #define __FUNCT__ "PetscThreadCommFinalizePackage"
 /*@C
    PetscThreadCommFinalizePackage - Finalize PetscThreadComm package, called from PetscFinalize()
@@ -42,5 +17,30 @@ PetscErrorCode PetscThreadCommFinalizePackage(void)
 {
   PetscFunctionBegin;
   PetscThreadCommPackageInitialized = PETSC_FALSE;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "PetscThreadCommInitializePackage"
+/*@C
+   PetscThreadCommInitializePackage - Initializes ThreadComm package
+
+   Logically collective
+
+   Input Parameter:
+.  path - The dynamic library path, or PETSC_NULL
+
+   Level: developer
+
+.seealso: PetscThreadCommFinalizePackage()
+@*/
+PetscErrorCode PetscThreadCommInitializePackage(const char *path)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  if(PetscThreadCommPackageInitialized) PetscFunctionReturn(0);
+  PetscThreadCommPackageInitialized = PETSC_TRUE;
+  ierr = PetscRegisterFinalize(PetscThreadCommFinalizePackage);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
