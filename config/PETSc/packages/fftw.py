2d71514cc1dd1abd2e93ccda325c9cde64dfab9a
Satish Balay <balay@mcs.anl.gov>
2012-05-09 09:13:46 -0500

    fix url typo
    Hg-commit: fe6059b08aeff484c6b2005698f56e204cea8926

--- config/PETSc/packages/fftw.py
+++ config/PETSc/packages/fftw.py
@@ -4,7 +4,7 @@ class Configure(PETSc.package.NewPackage):
   def __init__(self, framework):
     PETSc.package.NewPackage.__init__(self, framework)
     # host locally as fftw.org url can expire after new release.
-    self.download  = ['http://mcs.anl.gov/pub/petsc/externalpackages/fftw-3.3.2.tar.gz','http://www.fftw.org/fftw-3.3.2.tar.gz']
+    self.download  = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/fftw-3.3.2.tar.gz','http://www.fftw.org/fftw-3.3.2.tar.gz']
     self.functions = ['fftw_malloc'] 
     self.includes  = ['fftw3-mpi.h']  
     self.liblist   = [['libfftw3_mpi.a','libfftw3.a']]
