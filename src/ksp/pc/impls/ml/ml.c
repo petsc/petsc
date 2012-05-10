79d04de19d8f2cc8cd0ecd55dc313f4ff099980d
Barry Smith <bsmith@mcs.anl.gov>
2012-05-09 22:14:06 -0500

    code formatting in ml.c
    Hg-commit: 6a6f03e312afa87e7e2f1a875f8356357f78c3e6

--- src/ksp/pc/impls/ml/ml.c
+++ src/ksp/pc/impls/ml/ml.c
@@ -46,16 +46,16 @@ typedef struct {
 
 /* Private context for the ML preconditioner */
 typedef struct {
-  ML             *ml_object;
-  ML_Aggregate   *agg_object;
-  GridCtx        *gridctx;
-  FineGridCtx    *PetscMLdata;
-  PetscInt       Nlevels,MaxNlevels,MaxCoarseSize,CoarsenScheme,EnergyMinimization;
-  PetscReal      Threshold,DampingFactor,EnergyMinimizationDropTol;
-  PetscBool      SpectralNormScheme_Anorm,BlockScaling,EnergyMinimizationCheap,Symmetrize,OldHierarchy,KeepAggInfo,Reusable;
-  PetscBool      reuse_interpolation;
+  ML                *ml_object;
+  ML_Aggregate      *agg_object;
+  GridCtx           *gridctx;
+  FineGridCtx       *PetscMLdata;
+  PetscInt          Nlevels,MaxNlevels,MaxCoarseSize,CoarsenScheme,EnergyMinimization;
+  PetscReal         Threshold,DampingFactor,EnergyMinimizationDropTol;
+  PetscBool         SpectralNormScheme_Anorm,BlockScaling,EnergyMinimizationCheap,Symmetrize,OldHierarchy,KeepAggInfo,Reusable;
+  PetscBool         reuse_interpolation;
   PCMLNullSpaceType nulltype;
-  PetscMPIInt    size; /* size of communicator for pc->pmat */
+  PetscMPIInt       size; /* size of communicator for pc->pmat */
 } PC_ML;
 
 #undef __FUNCT__  
@@ -67,7 +67,6 @@ static int PetscML_getrow(ML_Operator *ML_data, int N_requested_rows, int reques
   PetscScalar    *aa;
   FineGridCtx    *ml=(FineGridCtx*)ML_Get_MyGetrowData(ML_data);
   Mat_SeqAIJ     *a = (Mat_SeqAIJ*)ml->Aloc->data;
-
 
   ierr = MatGetSize(ml->Aloc,&m,PETSC_NULL); if (ierr) return(0);
   for (i = 0; i<N_requested_rows; i++) {
@@ -201,7 +200,7 @@ static PetscErrorCode MatMultAdd_ML(Mat A,Vec x,Vec w,Vec y)
   PetscFunctionReturn(0);
 }
 
-/* newtype is ignored because "ml" is not listed under Petsc MatType */
+/* newtype is ignored since only handles one case */
 #undef __FUNCT__  
 #define __FUNCT__ "MatConvert_MPIAIJ_ML"
 static PetscErrorCode MatConvert_MPIAIJ_ML(Mat A,MatType newtype,MatReuse scall,Mat *Aloc) 
@@ -265,9 +264,7 @@ static PetscErrorCode MatConvert_MPIAIJ_ML(Mat A,MatType newtype,MatReuse scall,
       ncols = bi[i+1] - bi[i];
       for (j=0; j<ncols; j++) *ca++ = *ba++; 
     }
-  } else {
-    SETERRQ1(((PetscObject)A)->comm,PETSC_ERR_ARG_WRONG,"Invalid MatReuse %d",(int)scall);
-  }
+  } else SETERRQ1(((PetscObject)A)->comm,PETSC_ERR_ARG_WRONG,"Invalid MatReuse %d",(int)scall);
   PetscFunctionReturn(0);
 }
 
@@ -540,7 +537,6 @@ PetscErrorCode PCSetUp_ML(PC pc)
   KSP             smoother;
   PC              subpc;
   PetscInt        mesh_level, old_mesh_level;
-
 
   PetscFunctionBegin;
   A = pc->pmat;
