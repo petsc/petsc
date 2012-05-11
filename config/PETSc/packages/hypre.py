5621cb7405942b046ea612d3feb16690a7c9f4f2
Satish Balay <balay@mcs.anl.gov>
2012-05-11 13:03:47 -0500

    'make distclean' requires config/Makefile.config to exist - and than it deletes this file in the clean process. So a hypre-rebuild after a broken hypre-build will give errors. Fix by always making sure the file exists before distclean is invoked.
    Hg-commit: f46421c5fe260a1fe4c53b04a18a98a9df961a1b

--- config/PETSc/packages/hypre.py
+++ config/PETSc/packages/hypre.py
@@ -98,7 +98,7 @@ class Configure(PETSc.package.NewPackage):
     if self.installNeeded('hypre'):
       try:
         self.logPrintBox('Configuring hypre; this may take several minutes')
-        output1,err1,ret1  = PETSc.package.NewPackage.executeShellCommand('cd '+os.path.join(self.packageDir,'src')+' && make distclean && ./configure '+args, timeout=900, log = self.framework.log)
+        output1,err1,ret1  = PETSc.package.NewPackage.executeShellCommand('cd '+os.path.join(self.packageDir,'src')+' && touch config/Makefile.config && make distclean && ./configure '+args, timeout=900, log = self.framework.log)
       except RuntimeError, e:
         raise RuntimeError('Error running configure on HYPRE: '+str(e))
       try:
