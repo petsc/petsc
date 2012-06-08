1bcc62fd601ddb57cc41a016a19e94b86793e7a7
Matthew G Knepley <knepley@gmail.com>
2012-06-08 19:41:13 -0400

    Bug fix for  MatGetSubMatrices_MPIAIJ_Local() when no submatrices are requested.
    Hg-commit: f9a0ff6a135338fc42b93235974eceef9984887f

--- src/mat/impls/aij/mpi/mpiov.c
+++ src/mat/impls/aij/mpi/mpiov.c
@@ -1189,8 +1189,10 @@ PetscErrorCode MatGetSubMatrices_MPIAIJ_Local(Mat C,PetscInt ismax,const IS isro
   /* Create lens which is required for MatCreate... */
   for (i=0,j=0; i<ismax; i++) { j += nrow[i]; }
   ierr    = PetscMalloc(ismax*sizeof(PetscInt*),&lens);CHKERRQ(ierr);
-  ierr    = PetscMalloc(j*sizeof(PetscInt),&lens[0]);CHKERRQ(ierr);
-  ierr    = PetscMemzero(lens[0],j*sizeof(PetscInt));CHKERRQ(ierr);
+  if (ismax) {
+    ierr    = PetscMalloc(j*sizeof(PetscInt),&lens[0]);CHKERRQ(ierr);
+    ierr    = PetscMemzero(lens[0],j*sizeof(PetscInt));CHKERRQ(ierr);
+  }
   for (i=1; i<ismax; i++) { lens[i] = lens[i-1] + nrow[i-1]; }
   
   /* Update lens from local data */
@@ -1237,8 +1239,10 @@ PetscErrorCode MatGetSubMatrices_MPIAIJ_Local(Mat C,PetscInt ismax,const IS isro
     }
 #else
   ierr    = PetscMalloc(ismax*sizeof(PetscInt*),&rmap);CHKERRQ(ierr);
-  ierr    = PetscMalloc(ismax*C->rmap->N*sizeof(PetscInt),&rmap[0]);CHKERRQ(ierr);
-  ierr    = PetscMemzero(rmap[0],ismax*C->rmap->N*sizeof(PetscInt));CHKERRQ(ierr);
+  if (ismax) {
+    ierr    = PetscMalloc(ismax*C->rmap->N*sizeof(PetscInt),&rmap[0]);CHKERRQ(ierr);
+    ierr    = PetscMemzero(rmap[0],ismax*C->rmap->N*sizeof(PetscInt));CHKERRQ(ierr);
+  }
   for (i=1; i<ismax; i++) { rmap[i] = rmap[i-1] + C->rmap->N;}
   for (i=0; i<ismax; i++) {
     rmap_i = rmap[i];
@@ -1500,7 +1504,7 @@ PetscErrorCode MatGetSubMatrices_MPIAIJ_Local(Mat C,PetscInt ismax,const IS isro
 #if defined (PETSC_USE_CTABLE)
   for (i=0; i<ismax; i++) {ierr = PetscTableDestroy((PetscTable*)&rmap[i]);CHKERRQ(ierr);}
 #else
-  ierr = PetscFree(rmap[0]);CHKERRQ(ierr);
+  if (ismax) {ierr = PetscFree(rmap[0]);CHKERRQ(ierr);}
 #endif
   ierr = PetscFree(rmap);CHKERRQ(ierr);
 
@@ -1514,7 +1518,7 @@ PetscErrorCode MatGetSubMatrices_MPIAIJ_Local(Mat C,PetscInt ismax,const IS isro
     }
   }
   ierr = PetscFree(cmap);CHKERRQ(ierr);
-  ierr = PetscFree(lens[0]);CHKERRQ(ierr);
+  if (ismax) {ierr = PetscFree(lens[0]);CHKERRQ(ierr);}
   ierr = PetscFree(lens);CHKERRQ(ierr);
 
   for (i=0; i<ismax; i++) {
