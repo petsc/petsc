ed36708ce3596f1a0f05ed6542a7d7dd55df83ce
Hong Zhang <hzhang@mcs.anl.gov>
2012-06-27 10:48:25 -0500

    add tests for elemental interface; trivial cleanup matelem.cxx
    Hg-commit: 07c61a609bed7f3a02c1f05a55846206239f6248

--- src/mat/examples/tests/ex38.c
+++ src/mat/examples/tests/ex38.c
@@ -19,6 +19,8 @@ int main(int argc,char **args)
   PetscInitialize(&argc,&args,(char *)0,help);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+
+  // Get local block or element size
   ierr = PetscOptionsGetInt(PETSC_NULL,"-m",&m,PETSC_NULL);CHKERRQ(ierr);
   n = m;
   ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
@@ -31,8 +33,10 @@ int main(int argc,char **args)
   ierr = PetscOptionsHasName(PETSC_NULL,"-row_oriented",&flg);CHKERRQ(ierr);
   if (flg) {ierr = MatSetOption(C,MAT_ROW_ORIENTED,PETSC_TRUE);CHKERRQ(ierr);}
   ierr = MatGetOwnershipIS(C,&isrows,&iscols);CHKERRQ(ierr);
-  if (0) {
+  ierr = PetscOptionsHasName(PETSC_NULL,"-Cexp_view_ownership",&flg);CHKERRQ(ierr);
+  if (flg) { // View ownership of explicit C
     IS tmp;
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Ownership of explicit C:\n");CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD,"Row index set:\n");CHKERRQ(ierr);
     ierr = ISOnComm(isrows,PETSC_COMM_WORLD,PETSC_USE_POINTER,&tmp);CHKERRQ(ierr);
     ierr = ISView(tmp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
@@ -42,6 +46,8 @@ int main(int argc,char **args)
     ierr = ISView(tmp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
     ierr = ISDestroy(&tmp);CHKERRQ(ierr);
   }
+
+  // Set local matrix entries
   ierr = ISGetLocalSize(isrows,&nrows);CHKERRQ(ierr);
   ierr = ISGetIndices(isrows,&rows);CHKERRQ(ierr);
   ierr = ISGetLocalSize(iscols,&ncols);CHKERRQ(ierr);
@@ -49,30 +55,47 @@ int main(int argc,char **args)
   ierr = PetscMalloc(nrows*ncols*sizeof *v,&v);CHKERRQ(ierr);
   for (i=0; i<nrows; i++) {
     for (j=0; j<ncols; j++) {
-      v[i*ncols+j] = (PetscReal)(rank*10000+100*rows[i]+cols[j]);
+      v[i*ncols+j] = (PetscReal)(rank); 
+      //v[i*ncols+j] = (PetscReal)(rank*10000+100*rows[i]+cols[j]); 
+      if (rank==-1) {ierr = PetscPrintf(PETSC_COMM_SELF,"[%d] set (%d, %d, %g)\n",rank,rows[i],cols[j],v[i*ncols+j]);CHKERRQ(ierr);}
     }
   }
   ierr = MatSetValues(C,nrows,rows,ncols,cols,v,INSERT_VALUES);CHKERRQ(ierr);
-  ierr = PetscFree(v);CHKERRQ(ierr);
   ierr = ISRestoreIndices(isrows,&rows);CHKERRQ(ierr);
   ierr = ISRestoreIndices(iscols,&cols);CHKERRQ(ierr);
   ierr = MatAssemblyBegin(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
-  /* Test MatView() */
-  ierr = MatView(C,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  // Set unowned matrix entries - not done yet
+  if (rank == size-1) { // select rank == 0!
+    PetscInt M,N;
+    ierr = MatGetSize(C,&M,&N);CHKERRQ(ierr);
+    printf("M %d, N %d\n",M,N);
+    i = M-1; j = N-1; //(i,j)-the entry
+    v[0] = 0.5;
+    ierr = MatSetValues(C,1,&i,1,&j,v,ADD_VALUES);CHKERRQ(ierr);
+  }
+  ierr = MatAssemblyBegin(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
+  // Test MatView() 
+  ierr = MatView(C,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
   ierr = MatComputeExplicitOperator(C,&Cexp);CHKERRQ(ierr);
   ierr = MatView(Cexp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  ierr = MatDestroy(&Cexp);CHKERRQ(ierr);
 
-  /* Test MatMult() */
+  // Test MatMult() 
+  ierr = MatMultEqual(C,Cexp,5,&flg);CHKERRQ(ierr);
+  if (!flg) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_ARG_NOTSAMETYPE,"C != Cexp. MatMultEqual() fails");
 
-  /* Test MatMultAdd() */
+  // Test MatMultAdd() 
+  ierr = MatMultAddEqual(C,Cexp,5,&flg);CHKERRQ(ierr);
+  if (!flg) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_ARG_NOTSAMETYPE,"C != Cexp. MatMultAddEqual() fails");
 
+  ierr = PetscFree(v);CHKERRQ(ierr);
   ierr = ISDestroy(&isrows);CHKERRQ(ierr);
   ierr = ISDestroy(&iscols);CHKERRQ(ierr);
   ierr = MatDestroy(&C);CHKERRQ(ierr);
+  ierr = MatDestroy(&Cexp);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return 0;
 }
