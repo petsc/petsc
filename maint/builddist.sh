af23807acfd844ccda343ac57691703fa56798b7
Jason Sarich <sarich@mcs.anl.gov>
2012-06-29 14:45:42 -0500

    make changes to build script - point to c2html and exclude experimental poundersb

--- maint/builddist.sh
+++ maint/builddist.sh
@@ -3,6 +3,7 @@ export BUILDDIR=$HOME/working/taobuild
 export VERSION=2.1-p0
 export REVISION=default
 export HERE=$PWD
+export C2HTML=c2html
 if [ ! -d "$BUILDDIR" ]
 then
     mkdir -p $BUILDDIR
@@ -33,11 +34,11 @@ echo "Building manpages..."
 make tao_allmanpages
 
 echo "Creating html from source files..."
-make tao_htmlpages
+make tao_htmlpages DOCTEXT=doctext MANPAGES=manpages
 
 echo "Building tarball $BUILDDIR/tao-$VERSION.tar.gz"
 cd $BUILDDIR
-tar czf tao-$VERSION.tar.gz --exclude-vcs --exclude="TODO" --exclude="oldtao" --exclude="nlstests" tao-$VERSION --exclude="sqpcon" --exclude="lm" --exclude="pounder" --exclude="taodm" --exclude="tex"
+tar czf tao-$VERSION.tar.gz --exclude-vcs --exclude="TODO" --exclude="oldtao" --exclude="nlstests" tao-$VERSION --exclude="sqpcon" --exclude="lm" --exclude="pounder" --exclude="taodm" --exclude="tex" --exclude="poundersb"
 
 echo "mv tao-$VERSION.tar.gz $HERE"
 mv tao-$VERSION.tar.gz $HERE
