629c3df2130cc58f6fdba934dee6578e3fb3474f
Dmitry Karpeev <karpeev@mcs.anl.gov>
2012-07-10 04:16:57 -0500

    An attempt at MatConvert_Nest_AIJ. Untested.  Unscalable due to ISAllGather. Relatively insensitive to Mat-type, however. Scalable implementation of _MPIAIJ must use the local off-diag column indices, hence becomes Mat-type-specific.
    Hg-commit: 5289fd118d481a7e74d17b744fd938b773fceed7

--- include/petsc-private/vecimpl.h
+++ include/petsc-private/vecimpl.h
@@ -106,6 +106,46 @@ PETSC_STATIC_INLINE PetscErrorCode PetscLayoutFindOwner(PetscLayout map,PetscInt
   PetscFunctionReturn(0);
 }
 
+#undef __FUNCT__
+#define __FUNCT__ "PetscLayoutFindOwnerIndex"
+/*@C
+     PetscLayoutFindOwnerIndex - Find the owning rank and the local index for a global index
+
+    Not Collective
+
+   Input Parameters:
++    map   - the layout
+-    idx   - global index to find the owner of
+
+   Output Parameter:
++    owner - the owning rank
+-    lidx  - local index used by the owner for idx 
+
+   Level: developer
+
+    Fortran Notes:
+      Not available from Fortran
+
+@*/
+PETSC_STATIC_INLINE PetscErrorCode PetscLayoutFindOwnerIndex(PetscLayout map,PetscInt idx,PetscInt *owner, PetscInt *lidx)
+{
+  PetscErrorCode ierr;
+  PetscMPIInt    lo = 0,hi,t;
+
+  PetscFunctionBegin;
+  if (!((map->n >= 0) && (map->N >= 0) && (map->range))) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_ARG_WRONGSTATE,"PetscLayoutSetUp() must be called first");
+  if (idx < 0 || idx > map->N) SETERRQ1(PETSC_COMM_SELF,PETSC_ERR_ARG_OUTOFRANGE,"Index %D is out of range",idx);
+  ierr = MPI_Comm_size(map->comm,&hi);CHKERRQ(ierr);
+  while (hi - lo > 1) {
+    t = lo + (hi - lo) / 2;
+    if (idx < map->range[t]) hi = t;
+    else                     lo = t;
+  }
+  *owner = lo;
+  *lidx  = idx-map->range[lo];
+  PetscFunctionReturn(0);
+}
+
 /* ----------------------------------------------------------------------------*/
 typedef struct _n_PetscUniformSection *PetscUniformSection;
 struct _n_PetscUniformSection {
