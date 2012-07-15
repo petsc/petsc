8122439e21ea03523f8d0d3f57b1deb03149ab6a
Jed Brown <jed@59A2.org>
2012-07-15 16:23:29 -0500

    Man pages for VecSetValue and VecSetValueLocal use PetscInt
    Hg-commit: fdd2b338ebe4006f5b1cccb6f5157cfc59f9ecfc

--- include/petscvec.h
+++ include/petscvec.h
@@ -272,7 +272,7 @@ PETSC_EXTERN PetscErrorCode VecStashGetInfo(Vec,PetscInt*,PetscInt*,PetscInt*,Pe
    VecSetValue - Set a single entry into a vector.
 
    Synopsis:
-   PetscErrorCode VecSetValue(Vec v,int row,PetscScalar value, InsertMode mode);
+   PetscErrorCode VecSetValue(Vec v,PetscInt row,PetscScalar value, InsertMode mode);
 
    Not Collective
 
@@ -403,7 +403,7 @@ PETSC_EXTERN PetscErrorCode VecSetValuesLocal(Vec,PetscInt,const PetscInt[],cons
    VecSetValueLocal - Set a single entry into a vector using the local numbering
 
    Synopsis:
-   PetscErrorCode VecSetValueLocal(Vec v,int row,PetscScalar value, InsertMode mode);
+   PetscErrorCode VecSetValueLocal(Vec v,PetscInt row,PetscScalar value, InsertMode mode);
 
    Not Collective
 
