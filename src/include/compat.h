c68ab0967f1c5f0e69ce2e50db8b40d6008449a8
Lisandro Dalcin <dalcinl@gmail.com>
2012-07-23 11:55:24 -0300

    Remove support for petsc-3.1

--- src/include/compat.h
+++ src/include/compat.h
@@ -8,12 +8,4 @@
 #include "compat/petsc-32.h"
 #endif
 
-#if PETSC_VERSION_(3,1,0)
-#include "compat/petsc-31.h"
-#endif
-
-#if PETSC_VERSION_(3,0,0)
-#include "compat/petsc-31.h"
-#endif
-
 #endif/*PETSC4PY_COMPAT_H*/
