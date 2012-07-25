cbceaf5d6588ae837fd026247f6b6bd38fbb6ece
Matthew G Knepley <knepley@gmail.com>
2012-07-25 14:42:03 -0500

    DMComplex: Added Fortran headers
    Hg-commit: d560f272ff1b6e7f4e28667ab8b55f0c7408979e

---
+++ include/finclude/petscdmcomplex.h
@@ -0,0 +1,4 @@
+!
+!  Include file for Fortran use of the DMComplex package in PETSc
+!
+#include "finclude/petscdmcomplexdef.h"
---
+++ include/finclude/petscdmcomplexdef.h
@@ -0,0 +1,10 @@
+!
+!
+!  Include file for Fortran use of the DMComplex package in PETSc
+!
+#if !defined (__PETSCDMCOMPLEXDEF_H)
+#define __PETSCDMCOMPLEXDEF_H
+
+#include "finclude/petscdmdef.h"
+
+#endif
