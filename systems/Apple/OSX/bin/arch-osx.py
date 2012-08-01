d0539d15bce6eb890e977ed684d7d77bc1e12b0b
Barry Smith <bsmith@mcs.anl.gov>
2012-08-01 17:40:48 -0500

    no longer use valgrind with apple framework to make things simplier
    Hg-commit: 33d0a4069143d094dd36b5bf3944f94ce30780d8

--- systems/Apple/OSX/bin/arch-osx.py
+++ systems/Apple/OSX/bin/arch-osx.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     '--download-mpich',
     '--with-fc=0',
     '--with-shared-libraries',
-    '-download-mpich-shared=0',
+    '--download-mpich-shared=0',
+    '--with-valgrind=0',            # valgrind is ok but then must also provide -I/opt/local/include to compiler
     'PETSC_ARCH=arch-osx',
   ]
   configure.petsc_configure(configure_options)
