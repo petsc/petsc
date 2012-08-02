85d61ca244c5f03fe1aee50d62f825497de5e900
Xuan Zhou <xzhou23@hawk.iit.edu>
2012-08-02 11:33:46 -0500

    work on MatGetFactor_Clique
    Hg-commit: b9a9c1bdd808c0fc1fe1b6ff132744ad280962a6

--- src/mat/impls/aij/mpi/clique/clique.cxx
+++ src/mat/impls/aij/mpi/clique/clique.cxx
@@ -4,6 +4,14 @@
 */
 
 #undef __FUNCT__
+#define __FUNCT__ "MatView_Clique"
+PetscErrorCode MatView_Clique(Mat A,PetscViewer viewer)
+{
+  PetscFunctionBegin;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
 #define __FUNCT__ "MatDestroy_Clique"
 PetscErrorCode MatDestroy_Clique(Mat A)
 {
@@ -56,6 +64,39 @@ PetscErrorCode MatGetFactor_aij_clique(Mat A,MatFactorType ftype,Mat *F)
 {
   PetscFunctionBegin;
   printf("MatGetFactor_seqaij_Clique ...\n");
+  Mat            B;
+  Mat_Clique     *Acliq;
+  PetscErrorCode ierr;
+  //PetscInt       indx,m=A->rmap->n,n=A->cmap->n;  
+  //PetscBool      flg;
+  //const char     *colperm[]={"NATURAL","MMD_ATA","MMD_AT_PLUS_A","COLAMD"}; /* MY_PERMC - not supported by the petsc interface yet */
+  //const char     *iterrefine[]={"NOREFINE", "SINGLE", "DOUBLE", "EXTRA"};
+  //const char     *rowperm[]={"NOROWPERM", "LargeDiag"}; /* MY_PERMC - not supported by the petsc interface yet */
+
+  PetscFunctionBegin;
+  ierr = MatCreate(((PetscObject)A)->comm,&B);CHKERRQ(ierr);
+  ierr = MatSetSizes(B,A->rmap->n,A->cmap->n,PETSC_DETERMINE,PETSC_DETERMINE);CHKERRQ(ierr);
+  ierr = MatSetType(B,((PetscObject)A)->type_name);CHKERRQ(ierr);
+  ierr = MatSetUp(B);CHKERRQ(ierr);
+  //ierr = MatSeqAIJSetPreallocation(B,0,PETSC_NULL);CHKERRQ(ierr);
+
+  if (ftype == MAT_FACTOR_CHOLESKY){
+    B->ops->choleskyfactorsymbolic  = MatCholeskyFactorSymbolic_Clique;
+  } else SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"Factor type not supported");
+
+  B->ops->destroy          = MatDestroy_Clique;
+  B->ops->view             = MatView_Clique;
+  B->factortype            = ftype; 
+  B->assembled             = PETSC_TRUE;  /* required by -ksp_view */
+  //B->preallocated          = PETSC_TRUE;
+  
+  ierr = PetscNewLog(B,Mat_Clique,&Acliq);CHKERRQ(ierr);
+  
+  /*if (ftype == MAT_FACTOR_LU){
+    lu->options.Equil = NO;
+    }*/
+  B->spptr = Acliq;
+  *F = B;
   PetscFunctionReturn(0);
 }
 EXTERN_C_END
--- src/mat/impls/aij/mpi/clique/matcliqueimpl.h
+++ src/mat/impls/aij/mpi/clique/matcliqueimpl.h
@@ -2,6 +2,12 @@
 #include <clique.hpp>
 #include <petsc-private/matimpl.h>
 
+#if defined (PETSC_USE_COMPLEX)
+typedef cliq::Complex<PetscReal> PetscCliqScalar;
+#else
+typedef PetscScalar PetscCliqScalar;
+#endif
+
 typedef struct {
- 
+  cliq::DistSparseMatrix<PetscCliqScalar> *cmat;
 } Mat_Clique;
