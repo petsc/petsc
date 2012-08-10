62ab002c5fe639f67fa5e617049a9d33bcc2db93
Satish Balay <balay@mcs.anl.gov>
2012-08-10 18:27:09 -0500

    fix PetscGetNCores() on freebsd
    Hg-commit: 4db8917f4fdecffb6f7f28fa2d9bc5c74cf0e56f

--- src/sys/threadcomm/interface/threadcomm.c
+++ src/sys/threadcomm/interface/threadcomm.c
@@ -37,7 +37,10 @@ PetscErrorCode PetscGetNCores(PetscInt *ncores)
     {
       PetscErrorCode ierr;
       size_t         len = sizeof(N_CORES);
-      ierr = sysctlbyname("hw.activecpu",&N_CORES,&len,NULL,0);CHKERRQ(ierr);
+      ierr = sysctlbyname("hw.activecpu",&N_CORES,&len,NULL,0); /* osx preferes activecpu over ncpu */
+      if (ierr) { /* freebsd check ncpu */
+        ierr = sysctlbyname("hw.ncpu",&N_CORES,&len,NULL,0); CHKERRQ(ierr);
+      }
     }
 #elif defined(PETSC_HAVE_WINDOWS_H)   /* Windows */
     {
