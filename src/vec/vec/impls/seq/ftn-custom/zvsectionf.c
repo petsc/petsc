524ec3c87665c85ee63e74b125f0bbef9c3e5363
Matthew G Knepley <knepley@gmail.com>
2012-08-27 13:17:10 -0500

    PetscSection; Added Fortran test for PetscSection - Added Fortran binding for PetscSectionView()
    Hg-commit: b6fe82991deee4a0d3f9a20654bc7750a6b1fe0f

--- src/vec/vec/impls/seq/ftn-custom/zvsectionf.c
+++ src/vec/vec/impls/seq/ftn-custom/zvsectionf.c
@@ -1,14 +1,24 @@
 #include <petsc-private/fortranimpl.h>
 #include <petscvec.h>
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define petscsectionview_            PETSCSECTIONVIEW
 #define petscsectiongetfieldname_    PETSCSECTIONGETFIELDNAME
 #define petscsectionsetfieldname_    PETSCSECTIONSETFIELDNAME
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define petscsectionview_            petscsectionview
 #define petscsectiongetfieldname_    petscsectiongetfieldname
 #define petscsectionsetfieldname_    petscsectionsetfieldname
 #endif
 
 EXTERN_C_BEGIN
+
+void PETSC_STDCALL petscsectionview_(PetscSection *s, PetscViewer *vin, PetscErrorCode *ierr)
+{
+  PetscViewer v;
+
+  PetscPatchDefaultViewers_Fortran(vin, v);
+  *ierr = PetscSectionView(*s, v);
+}
 
 void PETSC_STDCALL petscsectiongetfieldname_(PetscSection *s, PetscInt *field, CHAR name PETSC_MIXED_LEN(len), PetscErrorCode *ierr PETSC_END_LEN(len))
 {
