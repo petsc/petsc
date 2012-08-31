0a4141d1bf10cf1460b38d42f8506daa07af4e99
Hong Zhang <hzhang@mcs.anl.gov>
2012-08-31 15:25:02 -0500

    cleanup clique.cxx; mem leak in MatSolve_Clique() not fixed yet - wait for Jack
    Hg-commit: 0799b96c938b01fc81dd0be64232e47e42e5b92e

--- src/mat/impls/aij/mpi/clique/matcliqueimpl.h
+++ src/mat/impls/aij/mpi/clique/matcliqueimpl.h
@@ -11,16 +11,20 @@ typedef PetscScalar PetscCliqScalar;
 #endif
 
 typedef struct {
-  cliq::DistSparseMatrix<PetscCliqScalar> *cmat;  /* Clique sparse matrix */
   MatStructure   matstruc;
   PetscBool      CleanUpClique;  /* Boolean indicating if we call Clique clean step */
   MPI_Comm       cliq_comm;      /* Clique MPI communicator                         */
   PetscInt       cutoff;         /* maximum size of leaf node */
   PetscInt       numDistSeps;    /* number of distributed separators to try */
   PetscInt       numSeqSeps;     /* number of sequential separators to try */
-  cliq::DistMap  *inverseMap;
-  cliq::DistSymmInfo *info;
+
+  cliq::DistSparseMatrix<PetscCliqScalar>  *cmat;  /* Clique sparse matrix */
+  cliq::DistMap                            *inverseMap;
+  cliq::DistSymmInfo                       *info;
   cliq::DistSymmFrontTree<PetscCliqScalar> *frontTree;
+  cliq::DistVector<PetscCliqScalar>        *rhs;
+  cliq::DistNodalVector<PetscCliqScalar>   *xNodal;
+
   PetscErrorCode (*Destroy)(Mat);
 } Mat_Clique;
 
