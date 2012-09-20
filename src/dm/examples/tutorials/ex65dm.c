1aee4dce6c55b2c2a24e06eb4789929f4b4bc4c8
Barry Smith <bsmith@mcs.anl.gov>
2012-09-20 17:08:11 -0500

    removed duplicative snes examples and moved a test example to better directory
    Hg-commit: 1582405dbc3335aa1a5de92894f01967a91f9601

--- src/dm/examples/tutorials/ex65dm.c
+++ src/dm/examples/tutorials/ex65dm.c
@@ -19,7 +19,6 @@ int main(int argc, char **argv)
   DM                  daf,dac1,dac2,dac3,dac4,daf1,daf2,daf3,daf4;
   Vec                 scaling_p1,scaling_p2,scaling_p3,scaling_p4;
   Mat                 interp_p1,interp_p2,interp_p3,interp_p4,interp_m1,interp_m2,interp_m3,interp_m4;
-  PetscInt            i;
   
   PetscInitialize(&argc,&argv, (char*)0, help);
   ierr = DMDACreate2d(PETSC_COMM_WORLD,DMDA_BOUNDARY_PERIODIC,DMDA_BOUNDARY_PERIODIC,DMDA_STENCIL_BOX,1024,1024,PETSC_DECIDE,PETSC_DECIDE, 1, 1,PETSC_NULL,PETSC_NULL,&daf);CHKERRQ(ierr);
