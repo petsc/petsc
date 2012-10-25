a24ee22a1c1541e6da7b46a02e4a5dcfde089cad
Satish Balay <balay@mcs.anl.gov>
2012-10-25 16:23:22 -0500

    MPIU_C_COMPLEX etc should be conditional on PETSC_USE_COMPLEX [fixes msmpi build]
    Hg-commit: 3e5bdcc83cd970452d0648da9955f789cc574198

--- src/sys/objects/pinit.c
+++ src/sys/objects/pinit.c
@@ -702,11 +702,12 @@ PetscErrorCode  PetscInitialize(int *argc,char ***args,const char file[],const c
      shared libraries the constructors for global variables
      are not called; at least on IRIX.
   */
+#if defined(PETSC_USE_COMPLEX)
   {
-#if defined(PETSC_CLANGUAGE_CXX) && defined(PETSC_HAVE_CXX_COMPLEX)
+#if defined(PETSC_CLANGUAGE_CXX)
     PetscComplex ic(0.0,1.0);
-    PETSC_i = ic; 
-#elif defined(PETSC_CLANGUAGE_C) && defined(PETSC_HAVE_C99_COMPLEX)
+    PETSC_i = ic;
+#elif defined(PETSC_CLANGUAGE_C)
     PETSC_i = _Complex_I;
 #endif
   }
@@ -717,6 +718,7 @@ PetscErrorCode  PetscInitialize(int *argc,char ***args,const char file[],const c
   ierr = MPI_Type_contiguous(2,MPI_FLOAT,&MPIU_C_COMPLEX);CHKERRQ(ierr);
   ierr = MPI_Type_commit(&MPIU_C_COMPLEX);CHKERRQ(ierr);
 #endif
+#endif /* PETSC_USE_COMPLEX */
 
   /*
      Create the PETSc MPI reduction operator that sums of the first
