f390bd6e3b758d23bae39dd3e4a226e1748988e3
Barry Smith <bsmith@mcs.anl.gov>
2012-11-06 19:25:25 -0600

    added simple power model (buggy)
    Hg-commit: b3f588ea6f28a3a3596b51ba23b48fb1fa062b8b

---
+++ src/ts/examples/tutorials/power_grid/ex1.c
@@ -0,0 +1,181 @@
+
+static char help[] = "Basic equation for generator stability analysis.\n";
+
+/*F
+
+\begin{eqnarray}
+                 \frac{2 H}{\omega_s}\frac{d \omega}{dt} & = & P_m - \frac{EV}{X} \sin(\theta) \\
+                 \frac{d \theta}{dt} = \omega - \omega_s
+\end{eqnarray}
+
+F*/
+
+/*
+   Include "petscts.h" so that we can use TS solvers.  Note that this
+   file automatically includes:
+     petscsys.h       - base PETSc routines   petscvec.h - vectors
+     petscmat.h - matrices
+     petscis.h     - index sets            petscksp.h - Krylov subspace methods
+     petscviewer.h - viewers               petscpc.h  - preconditioners
+     petscksp.h   - linear solvers
+*/
+#include <petscts.h>
+
+typedef struct {
+  PetscScalar H,omega_s,E,V,X;
+  PetscRandom rand;
+} AppCtx;
+
+#undef __FUNCT__
+#define __FUNCT__ "IFunction"
+/*
+     Defines the ODE passed to the ODE solver
+*/
+static PetscErrorCode IFunction(TS ts,PetscReal t,Vec U,Vec Udot,Vec F,AppCtx *ctx)
+{
+  PetscErrorCode ierr;
+  PetscScalar    *u,*udot,*f,r;
+  static PetscScalar R = .4;
+
+  PetscFunctionBegin;
+  ierr = PetscRandomGetValue(ctx->rand,&r);CHKERRQ(ierr);
+  if (r > .9) R = .5;
+  if (r < .1) R = .4;
+  R = .4;
+  /*  The next three lines allow us to access the entries of the vectors directly */
+  ierr = VecGetArray(U,&u);CHKERRQ(ierr);
+  ierr = VecGetArray(Udot,&udot);CHKERRQ(ierr);
+  ierr = VecGetArray(F,&f);CHKERRQ(ierr);
+  f[0] = 2.0*ctx->H*udot[0]/ctx->omega_s + ctx->E*ctx->V*PetscSinScalar(u[1])/ctx->X - R;
+  f[1] = udot[1] - u[0] + ctx->omega_s;
+
+  ierr = VecRestoreArray(U,&u);CHKERRQ(ierr);
+  ierr = VecRestoreArray(Udot,&udot);CHKERRQ(ierr);
+  ierr = VecRestoreArray(F,&f);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "IJacobian"
+/*
+     Defines the Jacobian of the ODE passed to the ODE solver. See TSSetIJacobian() for the meaning of a and the Jacobian.
+*/
+static PetscErrorCode IJacobian(TS ts,PetscReal t,Vec U,Vec Udot,PetscReal a,Mat *A,Mat *B,MatStructure *flag,AppCtx *ctx)
+{
+  PetscErrorCode ierr;
+  PetscInt       rowcol[] = {0,1};
+  PetscScalar    *u,*udot,J[2][2];
+
+  PetscFunctionBegin;
+  ierr    = VecGetArray(U,&u);CHKERRQ(ierr);
+  ierr    = VecGetArray(Udot,&udot);CHKERRQ(ierr);
+  J[0][0] = 2.0*ctx->H*a/ctx->omega_s;   J[0][1] = -ctx->E*ctx->V*PetscCosScalar(u[1])/ctx->X;
+  J[1][0] = -1.0;                        J[1][1] = a;
+  ierr    = MatSetValues(*B,2,rowcol,2,rowcol,&J[0][0],INSERT_VALUES);CHKERRQ(ierr);
+  ierr    = VecRestoreArray(U,&u);CHKERRQ(ierr);
+  ierr    = VecRestoreArray(Udot,&udot);CHKERRQ(ierr);
+
+  ierr = MatAssemblyBegin(*A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(*A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  if (*A != *B) {
+    ierr = MatAssemblyBegin(*B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(*B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  }
+  *flag = SAME_NONZERO_PATTERN;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  TS             ts;            /* ODE integrator */
+  Vec            U;             /* solution will be stored here */
+  Mat            A;             /* Jacobian matrix */
+  PetscErrorCode ierr;
+  PetscMPIInt    size;
+  PetscInt       n = 2;
+  AppCtx         ctx;
+  PetscScalar    *u;
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Initialize program
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+  if (size > 1) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Only for sequential runs");
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    Create necessary matrix and vectors
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,n,n,PETSC_DETERMINE,PETSC_DETERMINE);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatSetUp(A);CHKERRQ(ierr);
+
+  ierr = MatGetVecs(A,&U,PETSC_NULL);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    Set runtime options
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = PetscOptionsBegin(PETSC_COMM_WORLD,PETSC_NULL,"Reaction options","");CHKERRQ(ierr);
+  {
+    ctx.omega_s  = 1.0;
+    ierr        = PetscOptionsScalar("-omega_s","","",ctx.omega_s,&ctx.omega_s,PETSC_NULL);CHKERRQ(ierr);
+    ctx.H        = 1.0;
+    ierr        = PetscOptionsScalar("-H","","",ctx.H,&ctx.H,PETSC_NULL);CHKERRQ(ierr);
+    ctx.E        = 1.0;
+    ierr        = PetscOptionsScalar("-E","","",ctx.E,&ctx.E,PETSC_NULL);CHKERRQ(ierr);
+    ctx.V        = 1.0;
+    ierr        = PetscOptionsScalar("-V","","",ctx.V,&ctx.V,PETSC_NULL);CHKERRQ(ierr);
+    ctx.X        = 1.0;
+    ierr        = PetscOptionsScalar("-X","","",ctx.X,&ctx.X,PETSC_NULL);CHKERRQ(ierr);
+
+    ierr = VecGetArray(U,&u);CHKERRQ(ierr);
+    u[0] = 1;
+    u[1] = .7;
+    ierr = VecRestoreArray(U,&u);CHKERRQ(ierr);
+    ierr = PetscOptionsVec("-initial","Initial values","",U,PETSC_NULL);CHKERRQ(ierr);
+  }
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
+  
+  ierr = PetscRandomCreate(PETSC_COMM_WORLD,&ctx.rand);CHKERRQ(ierr);
+  ierr = PetscRandomSetFromOptions(ctx.rand);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Create timestepping solver context
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
+  ierr = TSSetProblemType(ts,TS_NONLINEAR);CHKERRQ(ierr);
+  ierr = TSSetType(ts,TSROSW);CHKERRQ(ierr);
+  ierr = TSSetIFunction(ts,PETSC_NULL,(TSIFunction) IFunction,&ctx);CHKERRQ(ierr);
+  ierr = TSSetIJacobian(ts,A,A,(TSIJacobian)IJacobian,&ctx);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Set initial conditions
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = TSSetSolution(ts,U);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Set solver options
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = TSSetDuration(ts,100000,2000.0);CHKERRQ(ierr);
+  ierr = TSSetInitialTimeStep(ts,0.0,.001);CHKERRQ(ierr);
+  ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Solve nonlinear system
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = TSSolve(ts,U,PETSC_IGNORE);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     Free work space.  All PETSc objects should be destroyed when they are no longer needed.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = VecDestroy(&U);CHKERRQ(ierr);
+  ierr = TSDestroy(&ts);CHKERRQ(ierr);
+  ierr = PetscRandomDestroy(&ctx.rand);CHKERRQ(ierr);
+
+  ierr = PetscFinalize();
+  return(0);
+}
