69a05b1569878289a3871bf0c96d3be9e498dc03
Jed Brown <jed@59A2.org>
2012-11-22 00:26:01 +0100

    exodus reader: temporarily disable cell set porting due to parallel problems, see 247c8417942bf4e266857ca300
    Hg-commit: 801d52f072c4cd90eeb1e4d483d4127b5dde23cc

--- src/dm/impls/complex/complexexodusii.c
+++ src/dm/impls/complex/complexexodusii.c
@@ -116,10 +116,11 @@ PetscErrorCode DMComplexCreateExodus(MPI_Comm comm, PetscInt exoid, PetscBool in
     default:
       SETERRQ1(comm, PETSC_ERR_ARG_OUTOFRANGE, "No mesh interpolation support for dimension %D", dim);
     }
-    /* Maintain Cell Sets label */
-    ((DM_Complex *) (*dm)->data)->labels->next->next = ((DM_Complex *)   idm->data)->labels;
-    ((DM_Complex *)   idm->data)->labels             = ((DM_Complex *) (*dm)->data)->labels->next;
-    ((DM_Complex *) (*dm)->data)->labels->next       = PETSC_NULL;
+    if (0) {    /* Maintain Cell Sets label. Only valid on rank 0, but other code depends on collective results so skip for now. */
+      ((DM_Complex *) (*dm)->data)->labels->next->next = ((DM_Complex *)   idm->data)->labels;
+      ((DM_Complex *)   idm->data)->labels             = ((DM_Complex *) (*dm)->data)->labels->next;
+      ((DM_Complex *) (*dm)->data)->labels->next       = PETSC_NULL;
+    }
     ierr = DMDestroy(dm);CHKERRQ(ierr);
     *dm  = idm;
   }
