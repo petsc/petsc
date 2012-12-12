719d7bfe3f90a83c6e339603ea9efbc0452a9f6c
Barry Smith <bsmith@mcs.anl.gov>
2012-12-12 10:23:50 -0600

    cannot use class as a variable name :-(
    Hg-commit: e0c94f9842fb21b027f357dea04c9488c8f2f2f0

--- src/sys/objects/inherit.c
+++ src/sys/objects/inherit.c
@@ -195,7 +195,7 @@ PetscErrorCode  PetscObjectsDump(FILE *fd)
         {
 #if defined(PETSC_USE_DEBUG)
         PetscStack *stack;
-        char        *create,*class;
+        char        *create,*rclass;
 
         /* if the PETSc function the user calls is not a create then this object was NOT directly created by them */
         ierr = PetscMallocGetStack(h,&stack);CHKERRQ(ierr);
@@ -206,10 +206,10 @@ PetscErrorCode  PetscObjectsDump(FILE *fd)
         if (!create) {
           ierr = PetscStrstr(stack->function[k],"Get",&create);CHKERRQ(ierr);
         }
-        ierr = PetscStrstr(stack->function[k],h->class_name,&class);CHKERRQ(ierr);
+        ierr = PetscStrstr(stack->function[k],h->class_name,&rclass);CHKERRQ(ierr);
 
         if (!create) continue;
-        if (!class) continue;
+        if (!rclass) continue;
 #endif
 
         ierr = PetscFPrintf(PETSC_COMM_WORLD,fd,"[%d] %s %s %s\n",PetscGlobalRank,h->class_name,h->type_name,h->name);CHKERRQ(ierr);
