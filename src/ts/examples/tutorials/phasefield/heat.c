ba72ae17d64d59f7d62ef8f1d91937ce5181228d
Hong Zhang <hzhang@mcs.anl.gov>
2012-12-14 10:39:15 -0600

    update TSSetExactFinalTime(ts,PETSC_TRUE) -> TSSetExactFinalTime(ts,TS_EXACTFINALTIME_INTERPOLATE); biharmonic.c needs to be fixed for complex build.
    Hg-commit: 3a11f8b26d7da5835244e03ee1b033e1fa5ed983

--- src/ts/examples/tutorials/phasefield/heat.c
+++ src/ts/examples/tutorials/phasefield/heat.c
@@ -98,7 +98,7 @@ int main(int argc,char **argv)
   ierr = FormInitialSolution(da,x);CHKERRQ(ierr);
   ierr = TSSetInitialTimeStep(ts,0.0,dt);CHKERRQ(ierr);
   ierr = TSSetDuration(ts,maxsteps,.02);CHKERRQ(ierr);
-  ierr = TSSetExactFinalTime(ts,PETSC_TRUE);CHKERRQ(ierr);
+  ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_INTERPOLATE);CHKERRQ(ierr);
   ierr = TSSetSolution(ts,x);CHKERRQ(ierr);
 
 
@@ -215,9 +215,9 @@ PetscErrorCode FormInitialSolution(DM da,Vec U)
 {
   PetscErrorCode    ierr;
   PetscInt          i,xs,xm,Mx,scale,N;
-  PetscScalar       *u,r;
+  PetscScalar       *u;
   const PetscScalar *f;
-  PetscReal         hx,x;
+  PetscReal         hx,x,r;
   Vec               finesolution;
   PetscViewer       viewer;
   PetscBool         flg;
