bf044a61584637e285a80b22689c9cf3bccd8997
Satish Balay <balay@mcs.anl.gov>
2012-12-20 15:58:32 -0600

    update to mpich-3.0.1
    Hg-commit: 485dfd047509e5a52ce4f242ce9c20fab31309d9

--- config/packages/MPI.py
+++ config/packages/MPI.py
@@ -12,8 +12,8 @@ class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
     self.download_openmpi   = ['http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.gz']
-    self.download_mpich     = ['http://www.mpich.org/static/tarballs/3.0/mpich-3.0.tar.gz',
-                               'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.0.tar.gz']
+    self.download_mpich     = ['http://www.mpich.org/static/tarballs/3.0.1/mpich-3.0.1.tar.gz',
+                               'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.0.1.tar.gz']
     self.download           = ['redefine']
     self.functions          = ['MPI_Init', 'MPI_Comm_create']
     self.includes           = ['mpi.h']
