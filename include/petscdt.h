37045ce4b26a202cc7a954b115583b9ceffe94d5
Jed Brown <jed@59A2.org>
2012-12-24 03:08:16 -0600

    Add discretization tools: Legendre polynomials and Gauss quadratures
    Hg-commit: 784baf9728c4b138388b0998be51cc7f10a414ee

---
+++ include/petscdt.h
@@ -0,0 +1,12 @@
+/*
+  Common tools for constructing discretizations
+*/
+#if !defined(__PETSCDT_H)
+#define __PETSCDT_H
+
+#include <petscsys.h>
+
+PETSC_EXTERN PetscErrorCode PetscDTLegendreEval(PetscInt,const PetscReal*,PetscInt,const PetscInt*,PetscReal*,PetscReal*,PetscReal*);
+PETSC_EXTERN PetscErrorCode PetscDTGaussQuadrature(PetscInt,PetscReal,PetscReal,PetscReal*,PetscReal*);
+
+#endif
---
+++ src/dm/dt/interface/dt.c
@@ -0,0 +1,132 @@
+/* Discretization tools */
+
+#include <petscdt.h>            /*I "petscdt.h" I*/
+#include <petscblaslapack.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "PetscDTLegendreEval"
+/*@
+   PetscDTLegendreEval - evaluate Legendre polynomial at points
+
+   Not Collective
+
+   Input Arguments:
++  npoints - number of spatial points to evaluate at
+.  points - array of locations to evaluate at
+.  ndegree - number of basis degrees to evaluate
+-  degrees - sorted array of degrees to evaluate
+
+   Output Arguments:
++  B - row-oriented basis evaluation matrix B[point*ndegree + degree] (dimension npoints*ndegrees, allocated by caller) (or PETSC_NULL)
+.  D - row-oriented derivative evaluation matrix (or PETSC_NULL)
+-  D2 - row-oriented second derivative evaluation matrix (or PETSC_NULL)
+
+   Level: intermediate
+
+.seealso: PetscDTGaussQuadrature()
+@*/
+PetscErrorCode PetscDTLegendreEval(PetscInt npoints,const PetscReal *points,PetscInt ndegree,const PetscInt *degrees,PetscReal *B,PetscReal *D,PetscReal *D2)
+{
+  PetscInt i,maxdegree;
+
+  PetscFunctionBegin;
+  if (!npoints || !ndegree) PetscFunctionReturn(0);
+  maxdegree = degrees[ndegree-1];
+  for (i=0; i<npoints; i++) {
+    PetscReal pm1,pm2,pd1,pd2,pdd1,pdd2,x;
+    PetscInt j,k;
+    x = points[i];
+    pm2 = 0;
+    pm1 = 1;
+    pd2 = 0;
+    pd1 = 0;
+    pdd2 = 0;
+    pdd1 = 0;
+    k = 0;
+    if (degrees[k] == 0) {
+      if (B) B[i*ndegree+k] = pm1;
+      if (D) D[i*ndegree+k] = pd1;
+      if (D2) D2[i*ndegree+k] = pdd1;
+      k++;
+    }
+    for (j=1; j<=maxdegree; j++,k++) {
+      PetscReal p,d,dd;
+      p = ((2*j-1)*x*pm1 - (j-1)*pm2)/j;
+      d = pd2 + (2*j-1)*pm1;
+      dd = pdd2 + (2*j-1)*pd1;
+      pm2 = pm1;
+      pm1 = p;
+      pd2 = pd1;
+      pd1 = d;
+      pdd2 = pdd1;
+      pdd1 = dd;
+      if (degrees[k] == j) {
+        if (B) B[i*ndegree+k] = p;
+        if (D) D[i*ndegree+k] = d;
+        if (D2) D2[i*ndegree+k] = dd;
+      }
+    }
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "PetscDTGaussQuadrature"
+/*@
+   PetscDTGaussQuadrature - create Gauss quadrature
+
+   Not Collective
+
+   Input Arguments:
++  npoints - number of points
+.  a - left end of interval (often-1)
+-  b - right end of interval (often +1)
+
+   Output Arguments:
++  x - quadrature points
+-  w - quadrature weights
+
+   Level: intermediate
+
+   References:
+   Golub and Welsch, Calculation of Quadrature Rules, Math. Comp. 23(106), 221--230, 1969.
+
+.seealso: PetscDTLegendreEval()
+@*/
+PetscErrorCode PetscDTGaussQuadrature(PetscInt npoints,PetscReal a,PetscReal b,PetscReal *x,PetscReal *w)
+{
+  PetscErrorCode ierr;
+  PetscInt i;
+  PetscReal *work;
+  PetscScalar *Z;
+#if defined(PETSC_HAVE_COMPLEX)
+  char compz[] = "I";           /* compute the eigenvectors of the tridiagonal matrix directly */
+#else
+  char compz[] = "V";
+#endif
+  PetscBLASInt N,LDZ,info;
+
+  PetscFunctionBegin;
+  /* Set up the Golub-Welsch system */
+  for (i=0; i<npoints; i++) {
+    x[i] = 0;                   /* diagonal is 0 */
+    if (i) w[i-1] = 0.5 / PetscSqrtReal(1 - 1./PetscSqr(2*i));
+  }
+  ierr = PetscRealView(npoints-1,w,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  ierr = PetscMalloc2(npoints*npoints,PetscScalar,&Z,PetscMax(1,2*npoints-2),PetscReal,&work);CHKERRQ(ierr);
+  N = PetscBLASIntCast(npoints);
+  LDZ = N;
+  ierr = PetscFPTrapPush(PETSC_FP_TRAP_OFF);CHKERRQ(ierr);
+  LAPACKstev_(compz,&N,x,w,Z,&LDZ,work,&info);
+  ierr = PetscFPTrapPop();CHKERRQ(ierr);
+  if (info) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_PLIB,"xSTEV/xSTEQR error");
+
+  for (i=0; i<(npoints+1)/2; i++) {
+    PetscReal y = 0.5 * (-x[i] + x[npoints-i-1]); /* enforces symmetry */
+    x[i] = (a+b)/2 - y*(b-a)/2;
+    x[npoints-i-1] = (a+b)/2 + y*(b-a)/2;
+    w[i] = w[npoints-1-i] = (b-a)*PetscSqr(0.5*PetscAbsScalar(Z[i*npoints] + Z[(npoints-i-1)*npoints]));
+  }
+  ierr = PetscFree2(Z,work);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
