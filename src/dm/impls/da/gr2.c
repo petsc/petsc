db3019b158da59aa391517549babca7aac37f5ed
Barry Smith <bsmith@mcs.anl.gov>
2012-12-28 22:49:40 -0600

    used PetscClipInterval() instead of non-standard stupid macro
    Hg-commit: 8d6b62e9d51361ebc0d67885c7f0299677ce0cc2

--- src/dm/impls/da/gr2.c
+++ src/dm/impls/da/gr2.c
@@ -17,8 +17,6 @@ typedef struct {
   const char   *name0,*name1;
 } ZoomCtx;
 
-#define RestrictBetween(x,min,max) ((PetscRealPart(x) > max) ? max : ((PetscRealPart(x) < min) ? min : PetscRealPart(x)))
-
 /*
        This does the drawing for one particular field
     in one particular set of coordinates. It is a callback
@@ -48,10 +46,10 @@ PetscErrorCode VecView_MPI_Draw_DA2d_Zoom(PetscDraw draw,void *ctx)
   /* PetscDraw the contour plot patch */
   for (j=0; j<n-1; j++) {
     for (i=0; i<m-1; i++) {
-      id = i+j*m;    x1 = PetscRealPart(xy[2*id]);y_1 = PetscRealPart(xy[2*id+1]);c1 = (int)(PETSC_DRAW_BASIC_COLORS+s*(RestrictBetween(v[k+step*id],min,max)-min));
-      id = i+j*m+1;  x2 = PetscRealPart(xy[2*id]);y2  = y_1;                      c2 = (int)(PETSC_DRAW_BASIC_COLORS+s*(RestrictBetween(v[k+step*id],min,max)-min));
-      id = i+j*m+1+m;x3 = x2;                     y3  = PetscRealPart(xy[2*id+1]);c3 = (int)(PETSC_DRAW_BASIC_COLORS+s*(RestrictBetween(v[k+step*id],min,max)-min));
-      id = i+j*m+m;  x4 = x1;                     y4  = y3;                       c4 = (int)(PETSC_DRAW_BASIC_COLORS+s*(RestrictBetween(v[k+step*id],min,max)-min));
+      id = i+j*m;    x1 = PetscRealPart(xy[2*id]);y_1 = PetscRealPart(xy[2*id+1]);c1 = (int)(PETSC_DRAW_BASIC_COLORS+s*(PetscClipInterval(PetscRealPart(v[k+step*id]),min,max)-min));
+      id = i+j*m+1;  x2 = PetscRealPart(xy[2*id]);y2  = y_1;                      c2 = (int)(PETSC_DRAW_BASIC_COLORS+s*(PetscClipInterval(PetscRealPart(v[k+step*id]),min,max)-min));
+      id = i+j*m+1+m;x3 = x2;                     y3  = PetscRealPart(xy[2*id+1]);c3 = (int)(PETSC_DRAW_BASIC_COLORS+s*(PetscClipInterval(PetscRealPart(v[k+step*id]),min,max)-min));
+      id = i+j*m+m;  x4 = x1;                     y4  = y3;                       c4 = (int)(PETSC_DRAW_BASIC_COLORS+s*(PetscClipInterval(PetscRealPart(v[k+step*id]),min,max)-min));
 
       ierr = PetscDrawTriangle(draw,x1,y_1,x2,y2,x3,y3,c1,c2,c3);CHKERRQ(ierr);
       ierr = PetscDrawTriangle(draw,x1,y_1,x3,y3,x4,y4,c1,c3,c4);CHKERRQ(ierr);
