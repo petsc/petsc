d90f3eca1152100ba478496cb07a60737f39a76b
Barry Smith <bsmith@mcs.anl.gov>
2013-01-10 18:06:05 -0600

    added VecGet/RestoreArrayReadF90()
    Hg-commit: f7228d7c208260cf5d1103f0f5c5e26ce277c97c

--- src/vec/vec/interface/f90-custom/zvectorf90.c
+++ src/vec/vec/interface/f90-custom/zvectorf90.c
@@ -5,6 +5,8 @@
 #ifdef PETSC_HAVE_FORTRAN_CAPS
 #define vecgetarrayf90_            VECGETARRAYF90
 #define vecrestorearrayf90_        VECRESTOREARRAYF90
+#define vecgetarrayreadf90_        VECGETARRAYFREAD90
+#define vecrestorearrayreadf90_    VECRESTOREARRAYREADF90
 #define vecduplicatevecsf90_       VECDUPLICATEVECSF90
 #define vecdestroyvecsf90_         VECDESTROYVECSF90
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
@@ -34,6 +36,26 @@ void PETSC_STDCALL vecrestorearrayf90_(Vec *x,F90Array1d *ptr,int *__ierr PETSC_
   *__ierr = F90Array1dAccess(ptr,PETSC_SCALAR,(void**)&fa PETSC_F90_2PTR_PARAM(ptrd));if (*__ierr) return;
   *__ierr = F90Array1dDestroy(ptr,PETSC_SCALAR PETSC_F90_2PTR_PARAM(ptrd));if (*__ierr) return;
   *__ierr = VecRestoreArray(*x,&fa);
+}
+
+void PETSC_STDCALL vecgetarrayreadf90_(Vec *x,F90Array1d *ptr,int *__ierr PETSC_F90_2PTR_PROTO(ptrd))
+{
+  const PetscScalar *fa;
+  PetscInt     len;
+  if (!ptr) {
+    *__ierr = PetscError(((PetscObject)*x)->comm,__LINE__,PETSC_FUNCTION_NAME,__FILE__,__SDIR__,PETSC_ERR_ARG_BADPTR,PETSC_ERROR_INITIAL,"ptr==NULL, maybe #include <finclude/petscvec.h90> is missing?");
+    return;
+  }
+  *__ierr = VecGetArrayRead(*x,&fa);      if (*__ierr) return;
+  *__ierr = VecGetLocalSize(*x,&len); if (*__ierr) return;
+  *__ierr = F90Array1dCreate((PetscScalar*)fa,PETSC_SCALAR,1,len,ptr PETSC_F90_2PTR_PARAM(ptrd));
+}
+void PETSC_STDCALL vecrestorearrayreadf90_(Vec *x,F90Array1d *ptr,int *__ierr PETSC_F90_2PTR_PROTO(ptrd))
+{
+  const PetscScalar *fa;
+  *__ierr = F90Array1dAccess(ptr,PETSC_SCALAR,(void**)&fa PETSC_F90_2PTR_PARAM(ptrd));if (*__ierr) return;
+  *__ierr = F90Array1dDestroy(ptr,PETSC_SCALAR PETSC_F90_2PTR_PARAM(ptrd));if (*__ierr) return;
+  *__ierr = VecRestoreArrayRead(*x,&fa);
 }
 
 void PETSC_STDCALL vecduplicatevecsf90_(Vec *v,int *m,F90Array1d *ptr,int *__ierr PETSC_F90_2PTR_PROTO(ptrd))
