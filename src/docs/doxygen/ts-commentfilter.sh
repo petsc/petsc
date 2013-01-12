3055b7b4e13013c3e1551907460be5f55611fbdd
Karl Rupp <rupp@mcs.anl.gov>
2013-01-12 15:08:17 -0600

    Added files for generating Doxygen documentation.
    Hg-commit: ae4ae2e1b51a1911a2434236b4d60c563b4c0490

---
+++ src/docs/doxygen/ts-commentfilter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##### The following commands do the following:
+# Replace leading '/*M' and the like with '/**'
+# Same for trailing 'M*/'
+# Replace .seealso: by \see    
+# Replace 'Level: beginner' by '\addtogroup beginner \n Level: beginner'              (Note: [ \t] is more portable than \s)
+# Replace 'Level: intermediate' by '\addtogroup intermediate \n Level: intermediate'  (Note: [ \t] is more portable than \s)
+# Replace 'Level: advanced' by '\addtogroup advanced \n Level: advanced'              (Note: [ \t] is more portable than \s)
+# Replace 'Level: developer' by '\addtogroup developer \n Level: developer'           (Note: [ \t] is more portable than \s)
+# Replace '+ . . . -' enumeration list on left border for function parameters with @param
+  sed -e 's/\/\*[EJMS@]/\/\*\*/g' $1 \
+| sed -e 's/[EJMS@]\*\//\*\//g' \
+| sed -e 's/\.seealso:/\\see/g' \
+| sed -e 's/Level:[ \t][ \t]*beginner/\\ingroup ts-class-beginner\n Level: beginner/g' \
+| sed -e 's/Level:[ \t][ \t]*intermediate/\\ingroup ts-class-intermediate\n Level: intermediate/g' \
+| sed -e 's/Level:[ \t][ \t]*advanced/\\ingroup ts-class-advanced\n Level: advanced/g' \
+| sed -e 's/Level:[ \t][ \t]*developer/\\ingroup ts-class-developer\n Level: developer/g' \
+| sed -e 's/^+ /\\param /g' | sed -e 's/^\. /\\param /g' | sed -e 's/^- /\\param /g' 
+
