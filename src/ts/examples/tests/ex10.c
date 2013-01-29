aaed86047ed6fab9a7a04d4257e3f7624fac7907
Barry Smith <bsmith@mcs.anl.gov>
2013-01-29 13:29:26 -0600

    completed simple example of TSDAESimple wrapper object
    Hg-commit: 514b6b01ba7deb7ad609477204c6e8bb411b85cf

--- src/ts/examples/tests/ex10.c
+++ src/ts/examples/tests/ex10.c
@@ -1,50 +1,17 @@
-static char help[] = "Solves Simple DAEs \n";
+static char help[] = "Simple wrapper object to solve DAE of the form:\n\
+                             \\dot{U} = f(U,V)\n\
+                             F(U,V) = 0\n\n";
 
 #include <petscts.h>
-
-/*
-        \dot{U} = f(U,V)
-        F(U,V)  = 0
-
-
-*/
-
-
-#undef __FUNCT__
-#define __FUNCT__ "f"
-/*
-   f(U,V) = U + V
-
-*/
-PetscErrorCode f(PetscReal t,Vec U,Vec V,Vec F,void *ctx)
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBeginUser;
-  ierr = VecWAXPY(F,1.0,U,V);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "F"
-/*
-   F(U,V) = U - V
-
-*/
-PetscErrorCode F(PetscReal t,Vec U,Vec V,Vec F,void *ctx)
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBeginUser;
-  ierr = VecWAXPY(F,-1.0,V,U);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
 
 /* ----------------------------------------------------------------------------*/
 
 typedef struct _p_TSDAESimple *TSDAESimple;
 struct _p_TSDAESimple {
   MPI_Comm       comm;
+  PetscErrorCode (*setfromoptions)(TSDAESimple);
+  PetscErrorCode (*solve)(TSDAESimple,Vec);
+  PetscErrorCode (*destroy)(TSDAESimple);
   Vec            U,V;
   PetscErrorCode (*f)(PetscReal,Vec,Vec,Vec,void*);
   PetscErrorCode (*F)(PetscReal,Vec,Vec,Vec,void*);
@@ -99,6 +66,7 @@ PetscErrorCode TSDAESimpleDestroy(TSDAESimple *tsdae)
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
+  ierr = (*(*tsdae)->destroy)(*tsdae);CHKERRQ(ierr);
   ierr = VecDestroy(&(*tsdae)->U);CHKERRQ(ierr);
   ierr = VecDestroy(&(*tsdae)->V);CHKERRQ(ierr);
   ierr = PetscFree(*tsdae);CHKERRQ(ierr);
@@ -112,6 +80,7 @@ PetscErrorCode TSDAESimpleSolve(TSDAESimple tsdae,Vec Usolution)
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
+  ierr = (*tsdae->solve)(tsdae,Usolution);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -122,13 +91,14 @@ PetscErrorCode TSDAESimpleSetFromOptions(TSDAESimple tsdae)
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
+  ierr = (*tsdae->setfromoptions)(tsdae);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
 /* ----------------------------------------------------------------------------*/
 /*
       Integrates the system by integrating the reduced ODE system and solving the
-   algebraic constraints at each stage with a seperate SNES solve.
+   algebraic constraints at each stage with a separate SNES solve.
 */
 
 typedef struct {
@@ -177,30 +147,6 @@ PetscErrorCode TSDAESimple_Reduced_SNESFunction(SNES snes,Vec V,Vec F,void *actx
   PetscFunctionReturn(0);
 }
 
-#undef __FUNCT__
-#define __FUNCT__ "TSDAESimpleSetUp_Reduced"
-PetscErrorCode TSDAESimpleSetUp_Reduced(TSDAESimple tsdae)
-{
-  PetscErrorCode      ierr;
-  TSDAESimple_Reduced *red;
-  Vec                 tsrhs;
-
-  PetscFunctionBegin;
-  ierr = PetscMalloc(sizeof(TSDAESimple_Reduced),&red);CHKERRQ(ierr);
-  tsdae->data = red;
-
-  ierr = TSCreate(tsdae->comm,&red->ts);CHKERRQ(ierr);
-  ierr = TSSetProblemType(red->ts,TS_NONLINEAR);CHKERRQ(ierr);
-  ierr = TSSetType(red->ts,TSEULER);CHKERRQ(ierr);
-  ierr = VecDuplicate(tsdae->U,&tsrhs);CHKERRQ(ierr);
-  ierr = TSSetRHSFunction(red->ts,tsrhs,TSDAESimple_Reduced_TSFunction,tsdae);CHKERRQ(ierr);
-  ierr = VecDestroy(&tsrhs);CHKERRQ(ierr);
-
-  ierr = SNESCreate(tsdae->comm,&red->snes);CHKERRQ(ierr);
-  ierr = SNESSetFunction(red->snes,PETSC_NULL,TSDAESimple_Reduced_SNESFunction,tsdae);CHKERRQ(ierr);
-  ierr = SNESSetJacobian(red->snes,PETSC_NULL,PETSC_NULL,SNESDefaultComputeJacobian,tsdae);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
 
 #undef __FUNCT__
 #define __FUNCT__ "TSDAESimpleSolve_Reduced"
@@ -241,7 +187,227 @@ PetscErrorCode TSDAESimpleDestroy_Reduced(TSDAESimple tsdae)
   PetscFunctionReturn(0);
 }
 
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimpleSetUp_Reduced"
+PetscErrorCode TSDAESimpleSetUp_Reduced(TSDAESimple tsdae)
+{
+  PetscErrorCode      ierr;
+  TSDAESimple_Reduced *red;
+  Vec                 tsrhs;
+
+  PetscFunctionBegin;
+  ierr = PetscMalloc(sizeof(TSDAESimple_Reduced),&red);CHKERRQ(ierr);
+  tsdae->data = red;
+
+  tsdae->setfromoptions = TSDAESimpleSetFromOptions_Reduced;
+  tsdae->solve          = TSDAESimpleSolve_Reduced;
+  tsdae->destroy        = TSDAESimpleDestroy_Reduced;
+
+  ierr = TSCreate(tsdae->comm,&red->ts);CHKERRQ(ierr);
+  ierr = TSSetProblemType(red->ts,TS_NONLINEAR);CHKERRQ(ierr);
+  ierr = TSSetType(red->ts,TSEULER);CHKERRQ(ierr);
+  ierr = VecDuplicate(tsdae->U,&tsrhs);CHKERRQ(ierr);
+  ierr = TSSetRHSFunction(red->ts,tsrhs,TSDAESimple_Reduced_TSFunction,tsdae);CHKERRQ(ierr);
+  ierr = VecDestroy(&tsrhs);CHKERRQ(ierr);
+
+  ierr = SNESCreate(tsdae->comm,&red->snes);CHKERRQ(ierr);
+  ierr = SNESSetOptionsPrefix(red->snes,"tsdaesimple_");CHKERRQ(ierr);
+  ierr = SNESSetFunction(red->snes,PETSC_NULL,TSDAESimple_Reduced_SNESFunction,tsdae);CHKERRQ(ierr);
+  ierr = SNESSetJacobian(red->snes,PETSC_NULL,PETSC_NULL,SNESDefaultComputeJacobian,tsdae);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
 /* ----------------------------------------------------------------------------*/
+
+/*
+      Integrates the system by integrating directly the entire DAE system
+*/
+
+typedef struct {
+  TS         ts;
+  Vec        UV,UF,VF;
+  VecScatter scatterU,scatterV;
+} TSDAESimple_Full;
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimple_Full_TSRHSFunction"
+/*
+   Defines the RHS function that is passed to the time-integrator.
+
+   f(U,V)
+   0
+
+*/
+PetscErrorCode TSDAESimple_Full_TSRHSFunction(TS ts,PetscReal t,Vec UV,Vec F,void *actx)
+{
+  TSDAESimple      tsdae = (TSDAESimple)actx;
+  TSDAESimple_Full *full = (TSDAESimple_Full *)tsdae->data;
+  PetscErrorCode   ierr;
+
+  PetscFunctionBeginUser;
+  ierr = VecSet(F,0.0);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterU,UV,tsdae->U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterU,UV,tsdae->U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterV,UV,tsdae->V,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterV,UV,tsdae->V,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = (*tsdae->f)(t,tsdae->U,tsdae->V,full->UF,tsdae->fctx);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterU,full->UF,F,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterU,full->UF,F,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimple_Full_TSIFunction"
+/*
+   Defines the nonlinear function that is passed to the nonlinear solver
+
+   \dot{U}
+   F(U,V)
+
+*/
+PetscErrorCode TSDAESimple_Full_TSIFunction(TS ts,PetscReal t,Vec UV,Vec UVdot,Vec F,void *actx)
+{
+  TSDAESimple       tsdae = (TSDAESimple)actx;
+  TSDAESimple_Full *full = (TSDAESimple_Full *)tsdae->data;
+  PetscErrorCode    ierr;
+
+  PetscFunctionBeginUser;
+  ierr = VecCopy(UVdot,F);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterU,UV,tsdae->U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterU,UV,tsdae->U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterV,UV,tsdae->V,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterV,UV,tsdae->V,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = (*tsdae->F)(t,tsdae->U,tsdae->V,full->VF,tsdae->Fctx);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterV,full->VF,F,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterV,full->VF,F,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimpleSolve_Full"
+PetscErrorCode TSDAESimpleSolve_Full(TSDAESimple tsdae,Vec U)
+{
+  PetscErrorCode   ierr;
+  TSDAESimple_Full *full = (TSDAESimple_Full *)tsdae->data;
+
+  PetscFunctionBegin;
+  ierr = VecSet(full->UV,1.0);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterU,U,full->UV,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterU,U,full->UV,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
+  ierr = TSSolve(full->ts,full->UV);CHKERRQ(ierr);
+  ierr = VecScatterBegin(full->scatterU,full->UV,U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  ierr = VecScatterEnd(full->scatterU,full->UV,U,INSERT_VALUES,SCATTER_REVERSE);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimpleSetFromOptions_Full"
+PetscErrorCode TSDAESimpleSetFromOptions_Full(TSDAESimple tsdae)
+{
+  PetscErrorCode   ierr;
+  TSDAESimple_Full *full = (TSDAESimple_Full *)tsdae->data;
+
+  PetscFunctionBegin;
+  ierr = TSSetFromOptions(full->ts);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimpleDestroy_Full"
+PetscErrorCode TSDAESimpleDestroy_Full(TSDAESimple tsdae)
+{
+  PetscErrorCode   ierr;
+  TSDAESimple_Full *full = (TSDAESimple_Full *)tsdae->data;
+
+  PetscFunctionBegin;
+  ierr = TSDestroy(&full->ts);CHKERRQ(ierr);
+  ierr = VecDestroy(&full->UV);CHKERRQ(ierr);
+  ierr = VecDestroy(&full->UF);CHKERRQ(ierr);
+  ierr = VecDestroy(&full->VF);CHKERRQ(ierr);
+  ierr = VecScatterDestroy(&full->scatterU);CHKERRQ(ierr);
+  ierr = VecScatterDestroy(&full->scatterV);CHKERRQ(ierr);
+  ierr = PetscFree(full);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "TSDAESimpleSetUp_Full"
+PetscErrorCode TSDAESimpleSetUp_Full(TSDAESimple tsdae)
+{
+  PetscErrorCode   ierr;
+  TSDAESimple_Full *full;
+  Vec              tsrhs;
+  PetscInt         nU,nV,UVstart;
+  IS               is;
+
+  PetscFunctionBegin;
+  ierr = PetscMalloc(sizeof(TSDAESimple_Full),&full);CHKERRQ(ierr);
+  tsdae->data = full;
+
+  tsdae->setfromoptions = TSDAESimpleSetFromOptions_Full;
+  tsdae->solve          = TSDAESimpleSolve_Full;
+  tsdae->destroy        = TSDAESimpleDestroy_Full;
+
+  ierr = TSCreate(tsdae->comm,&full->ts);CHKERRQ(ierr);
+  ierr = TSSetProblemType(full->ts,TS_NONLINEAR);CHKERRQ(ierr);
+  ierr = TSSetType(full->ts,TSROSW);CHKERRQ(ierr);
+  ierr = VecDuplicate(tsdae->U,&full->UF);CHKERRQ(ierr);
+  ierr = VecDuplicate(tsdae->V,&full->VF);CHKERRQ(ierr);
+
+  ierr = VecGetLocalSize(tsdae->U,&nU);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(tsdae->V,&nV);CHKERRQ(ierr);
+  ierr = VecCreateMPI(tsdae->comm,nU+nV,PETSC_DETERMINE,&tsrhs);CHKERRQ(ierr);
+  ierr = VecDuplicate(tsrhs,&full->UV);CHKERRQ(ierr);
+
+  ierr = VecGetOwnershipRange(tsrhs,&UVstart,PETSC_NULL);CHKERRQ(ierr);
+  ierr = ISCreateStride(tsdae->comm,nU,UVstart,1,&is);CHKERRQ(ierr);
+  ierr = VecScatterCreate(tsdae->U,PETSC_NULL,tsrhs,is,&full->scatterU);CHKERRQ(ierr);
+  ierr = ISDestroy(&is);CHKERRQ(ierr);
+  ierr = ISCreateStride(tsdae->comm,nV,UVstart+nU,1,&is);CHKERRQ(ierr);
+  ierr = VecScatterCreate(tsdae->V,PETSC_NULL,tsrhs,is,&full->scatterV);CHKERRQ(ierr);
+  ierr = ISDestroy(&is);CHKERRQ(ierr);
+
+  ierr = TSSetRHSFunction(full->ts,tsrhs,TSDAESimple_Full_TSRHSFunction,tsdae);CHKERRQ(ierr);
+  ierr = TSSetIFunction(full->ts,PETSC_NULL,TSDAESimple_Full_TSIFunction,tsdae);CHKERRQ(ierr);
+  ierr = VecDestroy(&tsrhs);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
+/* ----------------------------------------------------------------------------*/
+
+
+#undef __FUNCT__
+#define __FUNCT__ "f"
+/*
+   Simple example:   f(U,V) = U + V
+
+*/
+PetscErrorCode f(PetscReal t,Vec U,Vec V,Vec F,void *ctx)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBeginUser;
+  ierr = VecWAXPY(F,1.0,U,V);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "F"
+/*
+   Simple example: F(U,V) = U - V
+
+*/
+PetscErrorCode F(PetscReal t,Vec U,Vec V,Vec F,void *ctx)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBeginUser;
+  ierr = VecWAXPY(F,-1.0,V,U);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
@@ -253,26 +419,25 @@ int main(int argc,char **argv)
 
   PetscInitialize(&argc,&argv,(char *)0,help);
   ierr = TSDAESimpleCreate(PETSC_COMM_WORLD,&tsdae);CHKERRQ(ierr);
-  ierr = TSDAESimpleSetFromOptions(tsdae);CHKERRQ(ierr);
 
   ierr = VecCreateMPI(PETSC_COMM_WORLD,1,PETSC_DETERMINE,&U);CHKERRQ(ierr);
-  ierr = VecDuplicate(U,&Usolution);CHKERRQ(ierr);
   ierr = VecCreateMPI(PETSC_COMM_WORLD,1,PETSC_DETERMINE,&V);CHKERRQ(ierr);
   ierr = TSDAESimpleSetRHSFunction(tsdae,U,f,PETSC_NULL);CHKERRQ(ierr);
   ierr = TSDAESimpleSetIFunction(tsdae,V,F,PETSC_NULL);CHKERRQ(ierr);
 
+  ierr = VecDuplicate(U,&Usolution);CHKERRQ(ierr);
   ierr = VecSet(Usolution,1.0);CHKERRQ(ierr);
-  ierr = TSDAESimpleSolve(tsdae,Usolution);CHKERRQ(ierr);
 
+  /*  ierr = TSDAESimpleSetUp_Full(tsdae);CHKERRQ(ierr); */
   ierr = TSDAESimpleSetUp_Reduced(tsdae);CHKERRQ(ierr);
-  ierr = TSDAESimpleSetFromOptions_Reduced(tsdae);CHKERRQ(ierr);
-  ierr = TSDAESimpleSolve_Reduced(tsdae,Usolution);CHKERRQ(ierr);
-  ierr = TSDAESimpleDestroy_Reduced(tsdae);CHKERRQ(ierr);
+
+  ierr = TSDAESimpleSetFromOptions(tsdae);CHKERRQ(ierr);
+  ierr = TSDAESimpleSolve(tsdae,Usolution);CHKERRQ(ierr);
+  ierr = TSDAESimpleDestroy(&tsdae);CHKERRQ(ierr);
 
   ierr = VecDestroy(&U);CHKERRQ(ierr);
   ierr = VecDestroy(&Usolution);CHKERRQ(ierr);
   ierr = VecDestroy(&V);CHKERRQ(ierr);
-  ierr = TSDAESimpleDestroy(&tsdae);CHKERRQ(ierr);
   PetscFinalize();
   return 0;
 }
