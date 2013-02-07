90cf3547a23a64a74f5ad902dc7c5026b3641211
Peter Brune <brune@mcs.anl.gov>
2013-02-07 11:20:05 -0600

    Added logging of the inner preconditioner solve to SNESNRICHARDSON
    Hg-commit: 026f78195ac03c76c30b75e330c9b2ea4fbe7ac8

--- src/snes/impls/richardson/snesrichardson.c
+++ src/snes/impls/richardson/snesrichardson.c
@@ -167,7 +167,9 @@ PetscErrorCode SNESSolve_NRichardson(SNES snes)
       ierr = VecCopy(X,Y);CHKERRQ(ierr);
       ierr = SNESSetInitialFunction(snes->pc, F);CHKERRQ(ierr);
       ierr = SNESSetInitialFunctionNorm(snes->pc, fnorm);CHKERRQ(ierr);
+      ierr = PetscLogEventBegin(SNES_NPCSolve,snes->pc,Y,snes->vec_rhs,0);CHKERRQ(ierr);
       ierr = SNESSolve(snes->pc, snes->vec_rhs, Y);CHKERRQ(ierr);
+      ierr = PetscLogEventEnd(SNES_NPCSolve,snes->pc,Y,snes->vec_rhs,0);CHKERRQ(ierr);
       ierr = SNESGetConvergedReason(snes->pc,&reason);CHKERRQ(ierr);
       if (reason < 0  && reason != SNES_DIVERGED_MAX_IT) {
         snes->reason = SNES_DIVERGED_INNER;
