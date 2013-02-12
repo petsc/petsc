5847835f01a83a65f10e398c0972b3c7a1e1c5f4
Jed Brown <jed@59A2.org>
2013-02-12 11:44:52 -0600

    Make fieldsplit only attempt to set vector block size when jac->bs > 0
    This fixes failing fieldpslit tests.
    
    Original code introduced in 398f06830ac9d0920f8d4e4d0bac972857d520d4,
    but did not use VecSetBlockSize, so the block size was not
    validated. Barry replies:
    
        I think this is to handle the case where the vectors were obtained
        without the appropriate block size so the routines called below
        cannot be work properly. Perhaps the code should be changed so that
        the x and y bs are only set if the jac->bs is a legitimate value and
        not when it is -1.
    
    Hg-commit: 1b382c0c8a0048fa000c4641e30980e1ca800f19

--- src/ksp/pc/impls/fieldsplit/fieldsplit.c
+++ src/ksp/pc/impls/fieldsplit/fieldsplit.c
@@ -831,8 +831,10 @@ static PetscErrorCode PCApply_FieldSplit(PC pc,Vec x,Vec y)
   PetscInt          cnt,bs;
 
   PetscFunctionBegin;
-  ierr = VecSetBlockSize(x,jac->bs);CHKERRQ(ierr);
-  ierr = VecSetBlockSize(y,jac->bs);CHKERRQ(ierr);
+  if (jac->bs > 0) {
+    ierr = VecSetBlockSize(x,jac->bs);CHKERRQ(ierr);
+    ierr = VecSetBlockSize(y,jac->bs);CHKERRQ(ierr);
+  }
   CHKMEMQ;
   if (jac->type == PC_COMPOSITE_ADDITIVE) {
     if (jac->defaultsplit) {
