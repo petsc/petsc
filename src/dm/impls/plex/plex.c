a57dd577347cf357bc3142e2987c5ffed089475f
Matthew G Knepley <knepley@gmail.com>
2013-03-04 20:26:37 -0500

    DMPlex: Default ASCII output now has information on labels
    Hg-commit: 6c53548603d714d1424d0883fcc36b4b018a6f18

--- src/dm/impls/plex/plex.c
+++ src/dm/impls/plex/plex.c
@@ -278,9 +278,10 @@ PetscErrorCode DMPlexView_Ascii(DM dm, PetscViewer viewer)
     PetscInt   *sizes;
     PetscInt    locDepth, depth, dim, d;
     PetscInt    pStart, pEnd, p;
+    PetscInt    numLabels, l;
     PetscMPIInt size;
 
-    ierr = PetscObjectGetComm((PetscObject)dm,&comm);CHKERRQ(ierr);  
+    ierr = PetscObjectGetComm((PetscObject)dm,&comm);CHKERRQ(ierr);
     ierr = MPI_Comm_size(comm, &size);CHKERRQ(ierr);
     ierr = DMPlexGetDimension(dm, &dim);CHKERRQ(ierr);
     ierr = PetscViewerASCIIPrintf(viewer, "Mesh in %D dimensions:\n", dim);CHKERRQ(ierr);
@@ -311,6 +312,31 @@ PetscErrorCode DMPlexView_Ascii(DM dm, PetscViewer viewer)
       }
     }
     ierr = PetscFree(sizes);CHKERRQ(ierr);
+    ierr = DMPlexGetNumLabels(dm, &numLabels);CHKERRQ(ierr);
+    if (numLabels) {ierr = PetscViewerASCIIPrintf(viewer, "Labels:\n");CHKERRQ(ierr);}
+    for (l = 0; l < numLabels; ++l) {
+      DMLabel         label;
+      const char     *name;
+      IS              valueIS;
+      const PetscInt *values;
+      PetscInt        numValues, v;
+
+      ierr = DMPlexGetLabelName(dm, l, &name);CHKERRQ(ierr);
+      ierr = DMPlexGetLabel(dm, name, &label);CHKERRQ(ierr);
+      ierr = DMLabelGetNumValues(label, &numValues);CHKERRQ(ierr);
+      ierr = PetscViewerASCIIPrintf(viewer, "  %s: %d strata of sizes (", name, numValues);CHKERRQ(ierr);
+      ierr = DMLabelGetValueIS(label, &valueIS);CHKERRQ(ierr);
+      ierr = ISGetIndices(valueIS, &values);CHKERRQ(ierr);
+      for (v = 0; v < numValues; ++v) {
+        PetscInt size;
+
+        ierr = DMLabelGetStratumSize(label, values[v], &size);CHKERRQ(ierr);
+        if (v > 0) {ierr = PetscViewerASCIIPrintf(viewer, ", ");CHKERRQ(ierr);}
+        ierr = PetscViewerASCIIPrintf(viewer, "%d", size);CHKERRQ(ierr);
+      }
+      ierr = PetscViewerASCIIPrintf(viewer, ")\n");CHKERRQ(ierr);
+      ierr = ISRestoreIndices(valueIS, &values);CHKERRQ(ierr);
+    }
   }
   PetscFunctionReturn(0);
 }
