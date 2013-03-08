71cb9d2a3cc4017abfa234f2b20ca49dc58f832d
Satish Balay <balay@mcs.anl.gov>
2013-03-08 13:37:57 -0600

    update breabdoard configures [for cuda5/ubuntu 12.05] and switch nightly builds from cg to bb
    Hg-commit: b06d6a053cb7cd522c730b19e99baecef185f115

--- config/examples/arch-cuda-double.py
+++ config/examples/arch-cuda-double.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
-# This test build is with Cuda 4.1, with default thrust, and cusp-v0.3.1 separately installed.
+# This test build is with Cuda 5.0, with default thrust, and cusp-v0.3.1 separately installed.
+# [using default mpich from ubuntu 12.04]
 # Also enable txpetscgpu.
 #
 if __name__ == '__main__':
@@ -9,17 +10,12 @@ if __name__ == '__main__':
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
-    '--with-cc=gcc',
-    '--with-cxx=g++',
-    '--download-mpich=1',
     '--with-cuda=1',
     '--with-cusp=1',
-    '-with-cusp-dir=/usr/local/cusp-v0.3.1',
+    '-with-cusp-dir=/home/balay/soft/cusp-v0.3.1',
     '--with-thrust=1',
     '--download-txpetscgpu=1',
-    'PETSC_ARCH=arch-cuda-double',
     '--with-precision=double',
-    '--with-fc=0',
     '--with-clanguage=c',
     '--with-cuda-arch=sm_13'
 
