dcc04cf162cd8ce415ddd6f7c3139894631885b4
Karl Rupp <rupp@iue.tuwien.ac.at>
2013-03-18 17:54:15 -0500

    ViennaCL: Added package detector for OpenCL, added OpenCL-dependency for ViennaCL.
    Note that ViennaCL technically does not depend on OpenCL, yet that is its primary use within PETSc for now.
    OpenCL header detection on Apple (Mac OS) needs clarification, as it is located in OpenCL/ instead of CL/.

--- config/PETSc/packages/viennacl.py
+++ config/PETSc/packages/viennacl.py
@@ -1,9 +1,9 @@
-from __future__ import generators
-import config.package
+import PETSc.package
+import os
 
-class Configure(config.package.Package):
+class Configure(PETSc.package.NewPackage):
   def __init__(self, framework):
-    config.package.Package.__init__(self, framework)
+    PETSc.package.NewPackage.__init__(self, framework)
     self.download        = ['http://downloads.sourceforge.net/project/viennacl/1.4.x/ViennaCL-1.4.1.tar.gz']
     self.downloadfilename = str('ViennaCL-1.4.1')
     self.includes        = ['viennacl/forwards.h']
@@ -12,6 +12,13 @@ class Configure(config.package.Package):
     self.archIndependent = 1
     self.worksonWindows  = 1
     self.downloadonWindows = 1
+    self.complex          = 0
+    return
+
+  def setupDependencies(self, framework):
+    PETSc.package.NewPackage.setupDependencies(self, framework)
+    self.opencl  = framework.require('PETSc.packages.opencl',self)
+    self.deps = [self.opencl]
     return
 
   def Install(self):
@@ -31,3 +38,4 @@ class Configure(config.package.Package):
   def getSearchDirectories(self):
     yield ''
     return
+
