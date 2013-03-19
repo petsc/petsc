1cf68315abf30cefb80d4279f620e6c11acae3b7
Blaise Bourdin <bourdin@lsu.edu>
2013-03-18 21:00:08 -0500

    PetscOptions: Added tests for PetscOptionsInsertFileYAML

---
+++ src/sys/examples/tutorials/ex10f90.F90
@@ -0,0 +1,19 @@
+      program ex10f90
+#include "finclude/petscdef.h"
+      use petsc
+      implicit none
+
+      PetscErrorCode                            :: ierr
+      Character(len=256)                        :: filename
+      PetscBool                                 :: flg
+      
+      Call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
+      call PetscOptionsGetString(PETSC_NULL_CHARACTER,"-f",filename,flg,ierr)
+      if (flg) then
+         call PetscOptionsInsertFileYAML(PETSC_COMM_WORLD,filename,PETSC_TRUE,ierr);
+      end if
+      call PetscOptionsView(PETSC_VIEWER_STDOUT_WORLD,ierr)
+      Call PetscFinalize(ierr)
+      end program ex10f90
+
+
