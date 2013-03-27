ec5d6292f9812af34ea1f3ccedff50a840aa4f5c
Satish Balay <balay@mcs.anl.gov>
2013-03-26 22:14:40 -0500

    nightlybuilds: revert path change to mpich1

--- config/examples/arch-linux-mpich1.py
+++ config/examples/arch-linux-mpich1.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
-    '--with-mpi-dir=/home/petsc/soft/linux-Ubuntu_12.04-x86_64/mpich-1.2.7p1', #intel
+    '--with-mpi-dir=/sandbox/petsc/software/mpich-1.2.7p1', #intel
     '--with-cxx=0',
     '--with-shared-libraries=0',
   ]
