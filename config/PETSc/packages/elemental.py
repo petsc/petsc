14cfba9203d7ea4d9b88ec5c4fb7d43b20e67ac6
Jed Brown <jed@59A2.org>
2013-04-01 10:43:12 -0500

    Elemental: no support for 64-bit indices so requires32bitint=1

--- config/PETSc/packages/elemental.py
+++ config/PETSc/packages/elemental.py
@@ -8,7 +8,7 @@ class Configure(PETSc.package.NewPackage):
     self.liblist    = [['libelemental.a','libplcg.a','libpmrrr.a']]
     self.includes   = ['elemental.hpp']
     self.cxx              = 1
-    self.requires32bitint = 0
+    self.requires32bitint = 1
     self.complex          = 1
     self.worksonWindows   = 0
     self.downloadonWindows= 0
