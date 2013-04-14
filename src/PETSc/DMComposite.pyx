d58ee3c01c44f58dc218e76ebbff95dc41929e27
Lisandro Dalcin <dalcinl@gmail.com>
2013-04-14 10:45:18 +0300

    Enhancements to DMComposite

--- src/PETSc/DMComposite.pyx
+++ src/PETSc/DMComposite.pyx
@@ -1,25 +1,48 @@
 # --------------------------------------------------------------------
 
 cdef class DMComposite(DM):
-    def create(self, comm=None, subdms=[]):
+
+    def create(self, comm=None):
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
-        cdef PetscDM newdm
+        cdef PetscDM newdm = NULL
         CHKERR( DMCompositeCreate(ccomm, &newdm) )
         PetscCLEAR(self.obj); self.dm = newdm
         return self
 
-    def addDM(self, DM dm not None):
+    def addDM(self, DM dm not None, *args):
         """Add DM to composite"""
         CHKERR( DMCompositeAddDM(self.dm, dm.dm) )
+        cdef object item
+        for item in args:
+            dm = <DM?> item
+            CHKERR( DMCompositeAddDM(self.dm, dm.dm) )
 
-    def getNumberDM(self):
-        cdef PetscInt n
+    def getNumber(self):
+        """Get number of sub-DMs contained in the DMComposite"""
+        cdef PetscInt n = 0
         CHKERR( DMCompositeGetNumberDM(self.dm, &n) )
         return toInt(n)
+    getNumberDM = getNumber
+
+    def getEntries(self):
+        """Get tuple of sub-DMs contained in the DMComposite"""
+        cdef PetscInt n = 0
+        CHKERR( DMCompositeGetNumberDM(self.dm, &n) )
+        cdef PetscDM *cdms = NULL
+        cdef object cdms_mem = oarray_p(empty_p(n), NULL, <void**>&cdms)
+        CHKERR( DMCompositeGetEntriesArray(self.dm, cdms) )
+        cdef DM entry = None
+        cdef list entries = []
+        for i from 0 <= i < n:
+            entry = subtype_DM(cdms[i])()
+            entry.dm = cdms[i]
+            PetscINCREF(entry.obj)
+            entries.append(entry)
+        return tuple(entries)
 
     def scatter(self, Vec gvec not None, lvecs):
         """Scatter coupled global vector into split local vectors"""
-        cdef PetscInt n
+        cdef PetscInt n = 0
         CHKERR( DMCompositeGetNumberDM(self.dm, &n) )
         cdef PetscVec *clvecs = NULL
         cdef object clvecs_mem = oarray_p(empty_p(n), NULL, <void**>&clvecs)
@@ -30,29 +53,13 @@ cdef class DMComposite(DM):
     def gather(self, Vec gvec not None, imode, lvecs):
         """Gather split local vectors into coupled global vector"""
         cdef PetscInsertMode cimode = insertmode(imode)
-        cdef PetscInt n
+        cdef PetscInt n = 0
         CHKERR( DMCompositeGetNumberDM(self.dm, &n) )
         cdef PetscVec *clvecs = NULL
         cdef object clvecs_mem = oarray_p(empty_p(n), NULL, <void**>&clvecs)
         for i from 0 <= i < n:
             clvecs[i] = (<Vec?>lvecs[i]).vec
         CHKERR( DMCompositeGatherArray(self.dm, gvec.vec, cimode, clvecs) )
-
-    def getEntries(self):
-        """Get tuple of sub-DMs contained in the DMComposite"""
-        cdef PetscInt n
-        CHKERR( DMCompositeGetNumberDM(self.dm, &n) )
-        cdef PetscDM *cdms
-        cdef object cdms_mem = oarray_p(empty_p(n), NULL, <void**>&cdms)
-        CHKERR( DMCompositeGetEntriesArray(self.dm, cdms) )
-        cdef DM ent = None
-        cdef list entries = []
-        for i from 0 <= i < n:
-            ent = subtype_DM(cdms[i])()
-            ent.dm = cdms[i]
-            PetscINCREF(ent.obj)
-            entries.append(ent)
-        return tuple(entries)
 
     def getAccess(self, Vec gvec not None, locs):
         """Get access to specified parts of global vector.
