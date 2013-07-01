e87d3dc4639f8572ae22d2fec3466958e574cde1
Satish Balay <balay@mcs.anl.gov>
2013-07-01 18:41:29 -0500

    autoreconf: add in proper test [in programs.py] to check if autoreconf is functional

--- config/BuildSystem/config/packages/MOAB.py
+++ config/BuildSystem/config/packages/MOAB.py
@@ -28,22 +28,17 @@ class Configure(config.package.GNUPackage):
     return
 
   def gitPreReqCheck(self):
-    '''Check for 'autoreconf' required by the git version to generate configure'''
-    if self.getExecutable('autoreconf', getFullPath = 1, resultName = 'autoreconf', setMakeMacro = 0):
-      return 1
-    else:
-      return 0
+    return self.programs.autoreconf_flg
 
   def gitPreInstallCheck(self):
     '''check for git repo - and then regenerate configure'''
     import os
     if os.path.isdir(os.path.join(self.packageDir,'.git')):
-      #redo autoreconf check as gitPreReqCheck() is not always invoked early-on
-      if not self.gitPreReqCheck():
-        raise RuntimeError('autoreconf required for git ' + self.PACKAGE+' not found! Try removing :',self.packageDir)
+      if not self.programs.autoreconf_flg:
+        raise RuntimeError('autoreconf required for git ' + self.PACKAGE+' not found (or broken)! Try removing :',self.packageDir)
       try:
         self.logPrintBox('Running autoreconf on ' +self.PACKAGE+'; this may take several minutes')
-        output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+' && '+self.autoreconf + ' -fi', timeout=200, log = self.framework.log)
+        output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+' && '+self.programs.autoreconf + ' -fi', timeout=200, log = self.framework.log)
       except RuntimeError, e:
         raise RuntimeError('Error running autoreconf on ' + self.PACKAGE+': '+str(e))
     return
