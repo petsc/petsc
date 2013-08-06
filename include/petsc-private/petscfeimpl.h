dbe77d9e958e01cd385d1449e9753a8b8b90c2af
Matthew G. Knepley <knepley@gmail.com>
2013-08-05 20:22:12 -0500

    PetscFE: This begins explicit FEM support in PETSc - PetscSpace: A linear space, like P_1, the P from Ciarlet - PetscDualSpace: The dual space to a PetscSpace, P' and K from Ciarlet, so it takes a reference element DM, uses PetscQuadrature for functionals - PetscFE: Combines PetscSpace, PetscDualSpace, PetscQuadrature to define an element, no implementations yet

---
+++ include/petsc-private/petscfeimpl.h
@@ -0,0 +1,69 @@
+#if !defined(_PETSCFEIMPL_H)
+#define _PETSCFEIMPL_H
+
+#include <petscfe.h>
+#include <petsc-private/petscimpl.h>
+
+typedef struct _PetscSpaceOps *PetscSpaceOps;
+struct _PetscSpaceOps {
+  PetscErrorCode (*setfromoptions)(PetscSpace);
+  PetscErrorCode (*setup)(PetscSpace);
+  PetscErrorCode (*view)(PetscSpace,PetscViewer);
+  PetscErrorCode (*destroy)(PetscSpace);
+
+  PetscErrorCode (*getdimension)(PetscSpace,PetscInt*);
+  PetscErrorCode (*evaluate)(PetscSpace,PetscInt,const PetscReal*,PetscReal*,PetscReal*,PetscReal*);
+};
+
+struct _p_PetscSpace {
+  PETSCHEADER(struct _PetscSpaceOps);
+  void    *data;  /* Implementation object */
+  PetscInt order; /* The approximation order of the space */
+  DM       dm;    /* Shell to use for temp allocation */
+};
+
+typedef struct {
+  PetscInt   numVariables; /* The number of variables in the space, e.g. x and y */
+  PetscBool  symmetric;    /* Use only symmetric polynomials */
+  PetscInt  *degrees;      /* Degrees of single variable which we need to compute */
+  PetscReal *A;            /* The assembly matrix A from 1D values to final polynomials */
+} PetscSpace_Poly;
+
+typedef struct _PetscDualSpaceOps *PetscDualSpaceOps;
+struct _PetscDualSpaceOps {
+  PetscErrorCode (*setfromoptions)(PetscDualSpace);
+  PetscErrorCode (*setup)(PetscDualSpace);
+  PetscErrorCode (*view)(PetscDualSpace,PetscViewer);
+  PetscErrorCode (*destroy)(PetscDualSpace);
+
+  PetscErrorCode (*getdimension)(PetscDualSpace,PetscInt*);
+};
+
+struct _p_PetscDualSpace {
+  PETSCHEADER(struct _PetscDualSpaceOps);
+  void            *data;       /* Implementation object */
+  DM               dm;         /* The integration region K */
+  PetscInt         order;      /* The approximation order of the space */
+  PetscQuadrature *functional; /* The basis of functionals for this space */
+};
+
+typedef struct {
+  PetscInt cellType;
+} PetscDualSpace_Lag;
+
+typedef struct _PetscFEOps *PetscFEOps;
+struct _PetscFEOps {
+  PetscErrorCode (*setfromoptions)(PetscFE);
+  PetscErrorCode (*setup)(PetscFE);
+  PetscErrorCode (*view)(PetscFE,PetscViewer);
+  PetscErrorCode (*destroy)(PetscFE);
+};
+
+struct _p_PetscFE {
+  PETSCHEADER(struct _PetscFEOps);
+  void          *data;       /* Implementation object */
+  PetscSpace     basisSpace; /* The basis space P */
+  PetscDualSpace dualSpace;  /* The dual space P' */
+};
+
+#endif
---
+++ include/petscfe.h
@@ -0,0 +1,60 @@
+/*
+      Objects which encapsulate finite element spaces and operations
+*/
+#if !defined(__PETSCFE_H)
+#define __PETSCFE_H
+#include <petscdm.h>
+#include <petscfetypes.h>
+
+PETSC_EXTERN PetscErrorCode PetscFEInitializePackage(void);
+
+PETSC_EXTERN PetscClassId PETSCSPACE_CLASSID;
+
+/*J
+  PetscSpaceType - String with the name of a PETSc linear space
+
+  Level: beginner
+
+.seealso: PetscSpaceSetType(), PetscSpace
+J*/
+typedef const char *PetscSpaceType;
+#define PETSCSPACEPOLYNOMIAL "poly"
+
+PETSC_EXTERN PetscFunctionList PetscSpaceList;
+PETSC_EXTERN PetscBool         PetscSpaceRegisterAllCalled;
+PETSC_EXTERN PetscErrorCode PetscSpaceCreate(MPI_Comm, PetscSpace *);
+PETSC_EXTERN PetscErrorCode PetscSpaceSetType(PetscSpace, PetscSpaceType);
+PETSC_EXTERN PetscErrorCode PetscSpaceGetType(PetscSpace, PetscSpaceType *);
+PETSC_EXTERN PetscErrorCode PetscSpaceSetFromOptions(PetscSpace);
+PETSC_EXTERN PetscErrorCode PetscSpaceRegister(const char [], PetscErrorCode (*)(PetscSpace));
+PETSC_EXTERN PetscErrorCode PetscSpaceRegisterAll(void);
+PETSC_EXTERN PetscErrorCode PetscSpaceRegisterDestroy(void);
+
+PETSC_EXTERN PetscClassId PETSCDUALSPACE_CLASSID;
+
+/*J
+  PetscDualSpaceType - String with the name of a PETSc dual space
+
+  Level: beginner
+
+.seealso: PetscDualSpaceSetType(), PetscDualSpace
+J*/
+typedef const char *PetscDualSpaceType;
+#define PETSCDUALSPACELAGRANGE "lagrange"
+
+PETSC_EXTERN PetscFunctionList PetscDualSpaceList;
+PETSC_EXTERN PetscBool         PetscDualSpaceRegisterAllCalled;
+PETSC_EXTERN PetscErrorCode PetscDualSpaceCreate(MPI_Comm, PetscDualSpace *);
+PETSC_EXTERN PetscErrorCode PetscDualSpaceSetType(PetscDualSpace, PetscDualSpaceType);
+PETSC_EXTERN PetscErrorCode PetscDualSpaceGetType(PetscDualSpace, PetscDualSpaceType *);
+PETSC_EXTERN PetscErrorCode PetscDualSpaceSetFromOptions(PetscDualSpace);
+PETSC_EXTERN PetscErrorCode PetscDualSpaceRegister(const char [], PetscErrorCode (*)(PetscDualSpace));
+PETSC_EXTERN PetscErrorCode PetscDualSpaceRegisterAll(void);
+PETSC_EXTERN PetscErrorCode PetscDualSpaceRegisterDestroy(void);
+
+PETSC_EXTERN PetscErrorCode PetscDualSpaceGetDimension(PetscDualSpace, PetscInt *);
+
+PETSC_EXTERN PetscClassId PETSCFE_CLASSID;
+
+PetscErrorCode PetscFEGetTabulation(PetscFE, PetscInt, const PetscReal [], PetscReal **, PetscReal **, PetscReal **);
+#endif
