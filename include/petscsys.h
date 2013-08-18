708d824c4cb53d42a2c9b87012eab7d009d93c07
Jed Brown <jedbrown@mcs.anl.gov>
2013-08-18 17:53:06 -0500

    Sys Subcomm: subsize field is used as PetscMPIInt [fixes 64-bit-indices]

--- include/petscsys.h
+++ include/petscsys.h
@@ -2292,7 +2292,7 @@ struct _n_PetscSubcomm {
   MPI_Comm   comm;        /* this communicator */
   PetscInt   n;           /* num of subcommunicators under the parent communicator */
   PetscInt   color;       /* color of processors belong to this communicator */
-  PetscInt   *subsize;    /* size of subcommunicator[color] */
+  PetscMPIInt *subsize;    /* size of subcommunicator[color] */
   PetscInt   type;
 };
 
