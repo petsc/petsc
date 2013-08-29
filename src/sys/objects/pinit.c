528d955991446b55c6f12db32fb17c6fe49c7b74
Barry Smith <bsmith@mcs.anl.gov>
2013-08-28 22:09:21 -0500

    added hyper link to PETSc for default PETSc SAWs introductor page

--- src/sys/objects/pinit.c
+++ src/sys/objects/pinit.c
@@ -840,7 +840,7 @@ PetscErrorCode  PetscInitialize(int *argc,char ***args,const char file[],const c
     }
 
     ierr = PetscSNPrintf(intro,4024,"<body>\n"
-                                    "<center><h2> PETSc Application Web server powered by <a href=\"https://bitbucket.org/saws/saws\">SAWs</a> </h2></center>\n"
+                                    "<center><h2> <a href=\"http://www.mcs.anl.gov/petsc\">PETSc</a> Application Web server powered by <a href=\"https://bitbucket.org/saws/saws\">SAWs</a> </h2></center>\n"
                                     "%s"
                                     "<center>This is the default PETSc application dashboard, from it you can access any published PETSc objects or logging data</center><br>\n",appline);
     ierr = SAWs_Set_Default_Body(0,intro);
