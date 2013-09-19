0f9b5ddf046682889c39eb42ac9cbf0a89dfa2a3
Jason Sarich <sarich@mcs.anl.gov>
2013-09-19 10:58:13 -0500

    version name is now 2.2.0

--- include/tao_version.h
+++ include/tao_version.h
@@ -2,11 +2,11 @@
 #define __TAO_VERSION_H
 
 /* ========================================================================== */
-#define TAO_VERSION_RELEASE  0 /*  1 if official release */
+#define TAO_VERSION_RELEASE  1 /*  1 if official release */
 /* 
    Current TAO version number and release date
 */
-#define TAO_VERSION_NUMBER "TAO Version 2.2"
+#define TAO_VERSION_NUMBER "TAO Version 2.2.0"
 #define TAO_VERSION_MAJOR    2
 #define TAO_VERSION_MINOR    2
 #define TAO_VERSION_SUBMINOR 0
@@ -15,7 +15,7 @@
     ((TAO_VERSION_MAJOR == (MAJOR)) &&       \
     (TAO_VERSION_MINOR == (MINOR)) &&       \
      (TAO_VERSION_SUBMINOR == (SUBMINOR)))
-#define TAO_VERSION_DATE     "Jun 12, 2013"
+#define TAO_VERSION_DATE     "Sept 20, 2013"
 #define TAO_AUTHOR_INFO      "The TAO Team:\
  Todd Munson, Jason Sarich, Stefan Wild\n\
 Bug reports, questions: tao-comments@mcs.anl.gov\n\
