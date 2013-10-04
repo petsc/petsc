e0a58c106f2ad02e7ad493540c7731d0a977060b
Matthew G. Knepley <knepley@gmail.com>
2013-10-04 03:53:46 -0500

    Mat: Fixed declaration

--- include/petscmat.h
+++ include/petscmat.h
@@ -1639,6 +1639,6 @@ PETSC_EXTERN PetscErrorCode MatNestSetSubMats(Mat,PetscInt,const IS[],PetscInt,c
 PETSC_EXTERN PetscErrorCode MatNestSetSubMat(Mat,PetscInt,PetscInt,Mat);
 
 PETSC_EXTERN PetscErrorCode MatChop(Mat,PetscReal);
-PETSC_EXTERN PetscErrorCode MatCalcBandwidth(Mat,PetscReal,PetscInt*);
+PETSC_EXTERN PetscErrorCode MatComputeBandwidth(Mat,PetscReal,PetscInt*);
 
 #endif
