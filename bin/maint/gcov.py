petsc/petsc

commit ce0b2093728594b0bb094d50d4d330838375f99d

massive cleanup of help messages for ./configure
Hg-commit: 69a88c545f9fb1d5ebfffe50990e5e3b38e28ae8

bin/maint/gcov.py
@@ -387,7 +387,7 @@ def make_htmlpage(LOC,tarballs):
 
 def main():
 
-    USER = os.environ['USER']
+    USER = os.environ>['USER']
     gcov_dir = "/tmp/gcov-"+USER
 
     if (sys.argv[1] == "-run_gcov"):

config/PETSc/Configure.py
@@ -21,11 +21,11 @@ class Configure(config.base.Configure):
 
   def setupHelp(self, help):
     import nargs
-    help.addArgument('PETSc',  '-prefix=<path>',                  nargs.Arg(None, '', 'Specifiy location to install PETSc (eg. /usr/local)'))
+    help.addArgument('PETSc',  '-prefix=<dir>',                  nargs.Arg(None, '', 'Specifiy location to install PETSc (eg. /usr/local)'))
     help.addArgument('Windows','-with-windows-graphics=<bool>',   nargs.ArgBool(None, 1,'Enable check for Windows Graphics'))
     help.addArgument('PETSc', '-with-default-arch=<bool>',        nargs.ArgBool(None, 1, 'Allow using the last configured arch without setting PETSC_ARCH'))
     help.addArgument('PETSc','-with-single-library=<bool>',       nargs.ArgBool(None, 1,'Put all PETSc code into the single -lpetsc library'))
-    help.addArgument('PETSc', '-with-iphone=<no,yes>',            nargs.ArgDownload(None, 0, 'Build an iPhone version of PETSc'))    
+    help.addArgument('PETSc', '-with-iphone=<bool>',              nargs.ArgBool(None, 0, 'Build an iPhone version of PETSc'))    
     return
 
   def setupDependencies(self, framework):

config/PETSc/packages/ADIC.py
@@ -17,7 +17,7 @@ class Configure(config.base.Configure):
   def setupHelp(self, help):
     import nargs
     help.addArgument('ADIC', '-with-adic=<bool>',      nargs.ArgBool(None, 0, 'Activate ADIC'))
-    help.addArgument('ADIC', '-with-adic-path=<path>', nargs.Arg(None, None, 'Full path of adic executable'))    
+    help.addArgument('ADIC', '-with-adic-path=<dir>', nargs.Arg(None, None, 'Full path of adic executable'))    
     return
 
   def generateADIC(self):

config/PETSc/petsc.py
@@ -59,7 +59,7 @@ class Configure(config.base.Configure):
     help.addArgument('PETSc', '-with-petsc-dir=<root dir>',        nargs.ArgDir(None, None, 'Specify the root directory of the PETSc installation'))
     help.addArgument('PETSc', '-with-petsc-arch=<arch>',           nargs.Arg(None, None, 'Specify PETSC_ARCH'))
     # Construction options
-    help.addArgument('PETSc', '-download-petsc=<no,yes>',          nargs.ArgBool(None, 0, 'Install PETSc'))
+    help.addArgument('PETSc', '-download-petsc=<bool>',          nargs.ArgBool(None, 0, 'Install PETSc'))
     # Testing options
     help.addArgument('PETSc', '-with-petsc-shared=<bool>',         nargs.ArgBool(None, 1, 'Require that the PETSc library be shared'))
     return

config/PETSc/utilities/CMake.py
@@ -15,7 +15,7 @@ class Configure(config.base.Configure):
     
   def setupHelp(self, help):
     import nargs
-    help.addArgument('PETSc', '-with-cmake=<cmakename>', nargs.Arg(None, 'cmake', 'Specify cmake'))
+    help.addArgument('PETSc', '-with-cmake=<prog>', nargs.Arg(None, 'cmake', 'Specify cmake'))
     return
 
   def configureCMake(self):

config/PETSc/utilities/Make.py
@@ -15,7 +15,7 @@ class Configure(config.base.Configure):
     
   def setupHelp(self, help):
     import nargs
-    help.addArgument('PETSc', '-with-make=<makename>', nargs.Arg(None, 'make', 'Specify make'))
+    help.addArgument('PETSc', '-with-make=<prog>', nargs.Arg(None, 'make', 'Specify make'))
     return
 
   def configureMake(self):

config/PETSc/utilities/arch.py
@@ -18,8 +18,8 @@ class Configure(config.base.Configure):
   
   def setupHelp(self, help):
     import nargs
-    help.addArgument('PETSc', '-PETSC_ARCH',     nargs.Arg(None, None, 'The configuration name'))
-    help.addArgument('PETSc', '-with-petsc-arch',nargs.Arg(None, None, 'The configuration name'))
+    help.addArgument('PETSc', '-PETSC_ARCH=<string>',     nargs.Arg(None, None, 'The configuration name'))
+    help.addArgument('PETSc', '-with-petsc-arch=<string>',nargs.Arg(None, None, 'The configuration name'))
     return
 
   def setupDependencies(self, framework):

config/PETSc/utilities/cacheDetails.py
@@ -28,9 +28,9 @@ class Configure(config.base.Configure):
     self.substPrefix  = 'PETSC'
     self.updated      = 0
     self.strmsg       = ''
-    self.attrs        = [CacheAttribute('level1-dcache-size', 'size', 'Size in bytes of Level 1 data cache', 32768, 16),
-                         CacheAttribute('level1-dcache-linesize', 'size', 'Size in bytes of each line of the Level 1 data cache', 32, 16),
-                         CacheAttribute('level1-dcache-assoc', 'ways', 'Associativity of the Level 1 data cache, 0 for full associative', 2, 0)]
+    self.attrs        = [CacheAttribute('level1-dcache-size', 'int', 'Size in bytes of Level 1 data cache', 32768, 16),
+                         CacheAttribute('level1-dcache-linesize', 'int', 'Size in bytes of each line of the Level 1 data cache', 32, 16),
+                         CacheAttribute('level1-dcache-assoc', 'int', 'Associativity of the Level 1 data cache, 0 for full associative', 2, 0)]
     self.method       = None
     return
 

commit f76cdb00f3cf50a6c0603e3fbd7c06620fc062a4

gcov: fix typo introduced in ce0b2093

bin/maint/gcov.py
@@ -387,7 +387,7 @@ def make_htmlpage(LOC,tarballs):
 
 def main():
 
-    USER = os.environ>['USER']
+    USER = os.environ['USER']
     gcov_dir = "/tmp/gcov-"+USER
 
     if (sys.argv[1] == "-run_gcov"):