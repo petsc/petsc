99f3bb647b972bb07c23bcb883511e03c4a5e354
Matthew G. Knepley <knepley@gmail.com>
2013-10-28 16:41:10 -0500

    DMPlex ex4: Reworked 2D simplex hybrid test

--- src/dm/impls/plex/examples/tests/ex4.c
+++ src/dm/impls/plex/examples/tests/ex4.c
@@ -105,142 +105,59 @@ PetscErrorCode CreateSimplex_2D(MPI_Comm comm, DM *dm)
       \ |      | /
         4--15--7
 */
-PetscErrorCode CreateSimplexHybrid_2D(MPI_Comm comm, DM dm)
+PetscErrorCode CreateSimplexHybrid_2D(MPI_Comm comm, PetscInt testNum, DM *dm)
 {
-  Vec            coordinates;
-  PetscSection   coordSection;
-  PetscScalar    *coords;
-  PetscInt       numVertices = 0, numEdges = 0, numCells = 0, cMax = PETSC_DETERMINE, fMax = PETSC_DETERMINE;
-  PetscInt       firstVertex, firstEdge, coordSize;
-  PetscInt       v, e;
+  DM             idm, hdm;
+  DMLabel        faultLabel, hybridLabel;
+  PetscInt       p;
   PetscMPIInt    rank;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
   ierr = MPI_Comm_rank(comm, &rank);CHKERRQ(ierr);
   if (!rank) {
-    numVertices = 3 + 3;
-    numEdges    = 6 + 2;
-    numCells    = 3;
-    cMax        = 2;
-    fMax        = 15;
-  }
-  firstVertex = numCells;
-  firstEdge   = numCells + numVertices;
-  ierr        = DMPlexSetChart(dm, 0, numCells+numEdges+numVertices);CHKERRQ(ierr);
-  if (numCells) {
-    ierr = DMPlexSetConeSize(dm, 0, 3);CHKERRQ(ierr);
-    ierr = DMPlexSetConeSize(dm, 1, 3);CHKERRQ(ierr);
-    ierr = DMPlexSetConeSize(dm, 2, 4);CHKERRQ(ierr);
-  }
-  for (e = firstEdge; e < firstEdge+numEdges; ++e) {
-    ierr = DMPlexSetConeSize(dm, e, 2);CHKERRQ(ierr);
-  }
-  ierr = DMSetUp(dm);CHKERRQ(ierr); /* Allocate space for cones */
-  ierr = DMPlexSetHybridBounds(dm, cMax, PETSC_DETERMINE, fMax, PETSC_DETERMINE);CHKERRQ(ierr); /* Indicate a hybrid mesh */
-  /* Build cells */
-  if (numCells > 0) {
-    const PetscInt cone[3] = {9, 10, 11};
-    const PetscInt ornt[3] = {0,  0,  0};
-    const PetscInt cell    = 0;
+    switch (testNum) {
+    case 0:
+    {
+      PetscInt    numPoints[2]        = {4, 2};
+      PetscInt    coneSize[6]         = {3, 3, 0, 0, 0, 0};
+      PetscInt    cones[6]            = {2, 3, 4,  5, 4, 3};
+      PetscInt    coneOrientations[6] = {0, 0, 0,  0, 0, 0};
+      PetscScalar vertexCoords[8]     = {-1.0, -0.5,  0.0, -0.5,  0.0, 0.5,  1.0, 0.5};
+      PetscInt    faultPoints[2]      = {3, 4};
 
-    ierr = DMPlexSetCone(dm, cell, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetConeOrientation(dm, cell, ornt);CHKERRQ(ierr);
-  }
-  if (numCells > 1) {
-    const PetscInt cone[3] = {12, 14, 13};
-    const PetscInt ornt[3] = { 0, -2,  0};
-    const PetscInt cell    = 1;
+      ierr = DMPlexCreateFromDAG(*dm, 1, numPoints, coneSize, cones, coneOrientations, vertexCoords);CHKERRQ(ierr);
+      for(p = 0; p < 2; ++p) {ierr = DMPlexSetLabelValue(*dm, "fault", faultPoints[p], 1);CHKERRQ(ierr);}
+    }
+    break;
+    break;
+    default: SETERRQ1(comm, PETSC_ERR_ARG_OUTOFRANGE, "No test mesh %d", testNum);
+    }
+    ierr = DMPlexCheckSymmetry(*dm);CHKERRQ(ierr);
+    ierr = DMPlexInterpolate(*dm, &idm);CHKERRQ(ierr);
+    ierr = DMPlexCopyCoordinates(*dm, idm);CHKERRQ(ierr);
+    ierr = PetscObjectSetOptionsPrefix((PetscObject) idm, "in_");CHKERRQ(ierr);
+    ierr = DMSetFromOptions(idm);CHKERRQ(ierr);
+    ierr = DMPlexCheckSymmetry(idm);CHKERRQ(ierr);
+    ierr = DMPlexGetLabel(*dm, "fault", &faultLabel);CHKERRQ(ierr);
+    ierr = DMPlexCreateHybridMesh(idm, faultLabel, &hybridLabel, &hdm);CHKERRQ(ierr);
+    ierr = DMLabelDestroy(&hybridLabel);CHKERRQ(ierr);
+    ierr = DMDestroy(&idm);CHKERRQ(ierr);
+    ierr = DMDestroy(dm);CHKERRQ(ierr);
+    *dm  = hdm;
+  } else {
+    PetscInt numPoints[2] = {0, 0};
 
-    ierr = DMPlexSetCone(dm, cell, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetConeOrientation(dm, cell, ornt);CHKERRQ(ierr);
+    ierr = DMPlexCreateFromDAG(*dm, 1, numPoints, NULL, NULL, NULL, NULL);CHKERRQ(ierr);
+    ierr = DMPlexInterpolate(*dm, &idm);CHKERRQ(ierr);
+    ierr = DMPlexCopyCoordinates(*dm, idm);CHKERRQ(ierr);
+    ierr = PetscObjectSetOptionsPrefix((PetscObject) idm, "in_");CHKERRQ(ierr);
+    ierr = DMSetFromOptions(idm);CHKERRQ(ierr);
+    ierr = DMPlexCreateHybridMesh(idm, NULL, NULL, &hdm);CHKERRQ(ierr);
+    ierr = DMDestroy(&idm);CHKERRQ(ierr);
+    ierr = DMDestroy(dm);CHKERRQ(ierr);
+    *dm  = hdm;
   }
-  if (numCells > 2) {
-    const PetscInt cone[4] = {10, 14, 15, 16};
-    const PetscInt ornt[4] = { 0,  0,  0,  0};
-    const PetscInt cell    = 2;
-
-    ierr = DMPlexSetCone(dm, cell, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetConeOrientation(dm, cell, ornt);CHKERRQ(ierr);
-  }
-  /* Build edges*/
-  if (numEdges > 0) {
-    const PetscInt cone[2] = {3, 4};
-    const PetscInt edge    = 9;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetLabelValue(dm, "marker", edge, 1);CHKERRQ(ierr);
-  }
-  if (numEdges > 1) {
-    const PetscInt cone[2] = {4, 5};
-    const PetscInt edge    = 10;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-  }
-  if (numEdges > 2) {
-    const PetscInt cone[2] = {5, 3};
-    const PetscInt edge    = 11;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetLabelValue(dm, "marker", edge, 1);CHKERRQ(ierr);
-  }
-  if (numEdges > 3) {
-    const PetscInt cone[2] = {6, 8};
-    const PetscInt edge    = 12;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetLabelValue(dm, "marker", edge, 1);CHKERRQ(ierr);
-  }
-  if (numEdges > 4) {
-    const PetscInt cone[2] = {7, 6};
-    const PetscInt edge    = 13;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-    ierr = DMPlexSetLabelValue(dm, "marker", edge, 1);CHKERRQ(ierr);
-  }
-  if (numEdges > 5) {
-    const PetscInt cone[2] = {7, 8};
-    const PetscInt edge    = 14;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-  }
-  if (numEdges > 6) {
-    const PetscInt cone[2] = {4, 7};
-    const PetscInt edge    = 15;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-  }
-  if (numEdges > 7) {
-    const PetscInt cone[2] = {5, 8};
-    const PetscInt edge    = 16;
-
-    ierr = DMPlexSetCone(dm, edge, cone);CHKERRQ(ierr);
-  }
-  ierr = DMPlexSymmetrize(dm);CHKERRQ(ierr);
-  ierr = DMPlexStratify(dm);CHKERRQ(ierr);
-  /* Build coordinates */
-  ierr = DMPlexGetCoordinateSection(dm, &coordSection);CHKERRQ(ierr);
-  ierr = PetscSectionSetChart(coordSection, firstVertex, firstVertex+numVertices);CHKERRQ(ierr);
-  for (v = firstVertex; v < firstVertex+numVertices; ++v) {
-    ierr = PetscSectionSetDof(coordSection, v, 2);CHKERRQ(ierr);
-  }
-  ierr = PetscSectionSetUp(coordSection);CHKERRQ(ierr);
-  ierr = PetscSectionGetStorageSize(coordSection, &coordSize);CHKERRQ(ierr);
-  ierr = VecCreate(PetscObjectComm((PetscObject)dm), &coordinates);CHKERRQ(ierr);
-  ierr = VecSetSizes(coordinates, coordSize, PETSC_DETERMINE);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(coordinates);CHKERRQ(ierr);
-  ierr = VecGetArray(coordinates, &coords);CHKERRQ(ierr);
-  if (numVertices) {
-    coords[0]  = -0.5; coords[1]  = 0.5;
-    coords[2]  = -0.2; coords[3]  = 0.0;
-    coords[4]  = -0.2; coords[5]  = 1.0;
-    coords[6]  =  0.5; coords[7]  = 0.5;
-    coords[8]  =  0.2; coords[9]  = 0.0;
-    coords[10] =  0.2; coords[11] = 1.0;
-  }
-  ierr = VecRestoreArray(coordinates, &coords);CHKERRQ(ierr);
-  ierr = DMSetCoordinatesLocal(dm, coordinates);CHKERRQ(ierr);
-  ierr = VecDestroy(&coordinates);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -586,7 +503,7 @@ PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
   case 2:
     if (cellSimplex) {
       if (cellHybrid) {
-        ierr = CreateSimplexHybrid_2D(comm, *dm);CHKERRQ(ierr);
+        ierr = CreateSimplexHybrid_2D(comm, user->testNum, dm);CHKERRQ(ierr);
       } else {
         ierr = CreateSimplex_2D(comm, dm);CHKERRQ(ierr);
       }
