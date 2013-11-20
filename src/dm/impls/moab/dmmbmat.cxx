e9df842dfb0fa20d45d430c7506ee958e3009e52
Vijay Mahadevan <vijay.m@gmail.com>
2013-11-20 11:15:26 -0600

    Fix for the NNZ pattern computation when user asks for more layers of ghosted entities. The DMMoab itself does not care about these extra level of DOFs and only needs the interface(d837989). No need to override the user options from command line(b49477c4).

--- src/dm/impls/moab/dmmbmat.cxx
+++ src/dm/impls/moab/dmmbmat.cxx
@@ -2,6 +2,7 @@
 #include <petsc-private/vecimpl.h> /*I  "petscdm.h"   I*/
 
 #include <petscdmmoab.h>
+#include <MBTagConventions.hpp>
 
 static PetscErrorCode DMMoab_Compute_NNZ_From_Connectivity(DM,PetscInt*,PetscInt*,PetscInt*,PetscInt*,PetscBool);
 
@@ -10,19 +11,16 @@ static PetscErrorCode DMMoab_Compute_NNZ_From_Connectivity(DM,PetscInt*,PetscInt
 PetscErrorCode DMCreateMatrix_Moab(DM dm, MatType mtype,Mat *J)
 {
   PetscErrorCode  ierr;
-  ISLocalToGlobalMapping ltog;
-  MatType         mltype;
-  PetscInt        i,nloc,count,dof,innz,ionz,nsize;
-  DM_Moab         *dmmoab = (DM_Moab*)dm->data;
-  moab::Range     *range=dmmoab->vowned;
-  PetscInt        *nnz,*onz;
+  ISLocalToGlobalMapping ltogb;
+  PetscInt        innz,ionz,nlsiz;
+  DM_Moab         *dmmoab=(DM_Moab*)dm->data;
+  PetscInt        *nnz=0,*onz=0;
   char            *tmp=0;
   moab::ErrorCode merr;
 
   PetscFunctionBegin;
   PetscValidHeaderSpecific(dm,DM_CLASSID,1);
   PetscValidPointer(J,3);
-  nloc = dmmoab->vowned->size() * dmmoab->bs;
 
   /* create the Matrix and set its type as specified by user */
   ierr = MatCreate(dmmoab->pcomm->comm(), J);CHKERRQ(ierr);
@@ -36,25 +34,26 @@ PetscErrorCode DMCreateMatrix_Moab(DM dm, MatType mtype,Mat *J)
   nsize = (tmp ? dmmoab->nloc:dmmoab->nloc*dmmoab->bs);
 
   /* allocate the nnz, onz arrays based on block size and local nodes */
-  ierr = PetscMalloc(nsize*sizeof(PetscInt),&nnz);CHKERRQ(ierr);
-  ierr = PetscMemzero(nnz,sizeof(PetscInt)*nsize);CHKERRQ(ierr);
-  ierr = PetscMalloc(nsize*sizeof(PetscInt),&onz);CHKERRQ(ierr);
-  ierr = PetscMemzero(onz,sizeof(PetscInt)*nsize);CHKERRQ(ierr);
+  ierr = PetscMalloc((nlsiz)*sizeof(PetscInt),&nnz);CHKERRQ(ierr);
+  ierr = PetscMemzero(nnz,sizeof(PetscInt)*(nlsiz));CHKERRQ(ierr);
+  ierr = PetscMalloc(nlsiz*sizeof(PetscInt),&onz);CHKERRQ(ierr);
+  ierr = PetscMemzero(onz,sizeof(PetscInt)*nlsiz);CHKERRQ(ierr);
 
   /* compute the nonzero pattern based on MOAB connectivity data for local elements */
   ierr = DMMoab_Compute_NNZ_From_Connectivity(dm,&innz,nnz,&ionz,onz,(tmp?PETSC_TRUE:PETSC_FALSE));CHKERRQ(ierr);
 
-  if (dmmoab->bs > 1 && tmp) {
-    /* Block matrix created, now set local to global mapping */
-    ierr = ISLocalToGlobalMappingCreate(PETSC_COMM_SELF,range->size(),dmmoab->gsindices,PETSC_COPY_VALUES,&ltog);CHKERRQ(ierr);
-    ierr = MatSetLocalToGlobalMappingBlock(*J,ltog,ltog);CHKERRQ(ierr);
+  /* create the Matrix and set its type as specified by user */
+  ierr = MatCreate(dmmoab->pcomm->comm(), J);CHKERRQ(ierr);
+  ierr = MatSetSizes(*J, dmmoab->nloc*dmmoab->nfields, dmmoab->nloc*dmmoab->nfields, PETSC_DETERMINE, PETSC_DETERMINE);CHKERRQ(ierr);
+  ierr = MatSetBlockSize(*J, dmmoab->bs);CHKERRQ(ierr);
+  ierr = MatSetType(*J, mtype);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(*J);CHKERRQ(ierr);
 
-    /* Clean up */
-    ierr = ISLocalToGlobalMappingDestroy(&ltog);CHKERRQ(ierr);
-    
-  } else {
-    ierr = MatSetBlockSize(*J, dmmoab->bs);CHKERRQ(ierr);
-  }
+  if (!dmmoab->ltog_map) SETERRQ(dmmoab->pcomm->comm(), PETSC_ERR_ORDER, "Cannot create a DMMoab Mat without calling DMSetUp first.");
+  ierr = MatSetLocalToGlobalMapping(*J,dmmoab->ltog_map,dmmoab->ltog_map);CHKERRQ(ierr);
+  ierr = ISLocalToGlobalMappingBlock(dmmoab->ltog_map,dmmoab->bs,&ltogb);
+  ierr = MatSetLocalToGlobalMappingBlock(*J,ltogb,ltogb);CHKERRQ(ierr);
+  ierr = ISLocalToGlobalMappingDestroy(&ltogb);CHKERRQ(ierr);
 
   /* set preallocation based on different supported Mat types */
   ierr = MatSeqAIJSetPreallocation(*J, innz, nnz);CHKERRQ(ierr);
@@ -73,46 +72,34 @@ PetscErrorCode DMCreateMatrix_Moab(DM dm, MatType mtype,Mat *J)
 #define __FUNCT__ "DMMoab_Compute_NNZ_From_Connectivity"
 PetscErrorCode DMMoab_Compute_NNZ_From_Connectivity(DM dm,PetscInt* innz,PetscInt* nnz,PetscInt* ionz,PetscInt* onz,PetscBool isbaij)
 {
-  PetscErrorCode  ierr;
-  PetscInt        i,j,f,nloc,vpere,bs,nsize,nghost_found;
+  PetscInt        i,f,nloc,vpere,bs,nsize,ivtx,n_nnz,n_onz;
   DM_Moab         *dmmoab = (DM_Moab*)dm->data;
   const moab::EntityHandle *connect;
-  moab::Range     *vowned=dmmoab->vowned,*elocal=dmmoab->elocal,*eghost=dmmoab->eghost,adjs,visited;
-  moab::Range::iterator iter;
-  PetscInt        dof,doff,ndofs;
-  moab::Tag       id_tag;
+  moab::Range     adjs,found,allvlocal,allvghost;
+  moab::Range::iterator iter,jter;
+  std::vector<moab::EntityHandle> storage;
+  moab::EntityHandle vtx;
   moab::ErrorCode merr;
-  PetscSection section;
 
   PetscFunctionBegin;
   bs = dmmoab->bs;
   nloc = dmmoab->nloc;
   nsize = (isbaij ? nloc:nloc*bs);
 
-  ierr = DMMoabGetLocalToGlobalTag(dm,&id_tag);CHKERRQ(ierr);
+  /* find the truly user-expected layer of ghosted entities to decipher NNZ pattern */
+  merr = dmmoab->mbiface->get_entities_by_type(dmmoab->fileset,moab::MBVERTEX,allvlocal,true);MBERRNM(merr);
+  merr = dmmoab->pcomm->filter_pstatus(allvlocal,PSTATUS_NOT_OWNED,PSTATUS_NOT,-1,&adjs);MBERRNM(merr);
+  allvghost = moab::subtract(allvlocal, adjs);
 
-  ierr = DMGetDefaultSection(dm, &section);CHKERRQ(ierr);
-  
-  for(iter = elocal->begin(),i=0; iter != elocal->end(); iter++,i++) {
-    /* Get connectivity information in canonical ordering for the local element */
-    merr = dmmoab->mbiface->get_connectivity((*iter),connect,vpere);MBERRNM(merr);
-    
-    nghost_found=0;
-    /* loop over vertices and update the number of connectivity */
-    for (j=0;j<vpere;j++) {
-      moab::Range::const_iterator giter = dmmoab->vghost->find(connect[j]);
-      if (giter != dmmoab->vghost->end()) nghost_found++;
-    }
+  /* loop over vertices and update the number of connectivity */
+  for (j=0;j<vpere;j++) {
 
-    /* loop over vertices and update the number of connectivity */
-    for (j=0;j<vpere;j++) {
-
-      /*
-      moab::Range::const_iterator giter = visited.find(connect[j]);
-      if (giter != visited.end()) continue;
-
-      visited.insert(connect[j]);
-      */
+    vtx = *iter;
+    adjs.clear();
+    /* Get adjacency information for current vertex - i.e., all elements of dimension (dim) that connects
+       to the current vertex. We can then decipher if a vertex is ghosted or not and compute the 
+       non-zero pattern accordingly. */
+    merr = dmmoab->mbiface->get_adjacencies(&vtx,1,dmmoab->dim,false,adjs,moab::Interface::INTERSECT);
 
     /* loop over vertices and update the number of connectivity */
     for(jter = adjs.begin(); jter != adjs.end(); jter++) {
@@ -122,13 +109,11 @@ PetscErrorCode DMMoab_Compute_NNZ_From_Connectivity(DM dm,PetscInt* innz,PetscIn
 
       for (i=0; i<vpere; ++i) {
         if (connect[i] == vtx || found.find(connect[i]) != found.end()) continue;
-        if (dmmoab->vghost->find(connect[i]) != dmmoab->vghost->end()) n_onz++;
+        if (allvghost.find(connect[i]) != allvghost.end()) n_onz++;
         else n_nnz++;
         found.insert(connect[i]);
       }
     }
-    
-    merr = dmmoab->mbiface->tag_get_data(id_tag,&vtx,1,&gid);MBERRNM(merr);
  
     if (isbaij) {
       nnz[ivtx]=n_nnz;      /* leave out self to avoid repeats -> node shared by multiple elements */
@@ -142,19 +127,19 @@ PetscErrorCode DMMoab_Compute_NNZ_From_Connectivity(DM dm,PetscInt* innz,PetscIn
     }
   }
 
-  visited.clear();
-
   if (innz) *innz=0;
   if (ionz) *ionz=0;
   for (i=0;i<nsize;i++) {
     nnz[i]+=1;  /* self count the node */
+    /* check if we got overzealous */
+    nnz[i]=(nnz[i]>dmmoab->nloc ? dmmoab->nloc:nnz[i]);
     if (!isbaij) {
       nnz[i]*=bs;
       onz[i]*=bs;
     }
-
-    if (innz && nnz[i]>*innz) *innz=nnz[i];
-    if (ionz && onz[i]>*ionz) *ionz=onz[i];
+    
+    if (innz && (nnz[i]>*innz)) *innz=nnz[i];
+    if ((ionz && onz) && (onz[i]>*ionz)) *ionz=onz[i];
   }
   PetscFunctionReturn(0);
 }
