14e9e3bfa36c987e8af92b0641607f5053b3015e
Vijay Mahadevan <vijay.m@gmail.com>
2013-11-20 01:22:08 -0600

    Add a separate DMMoab private header to store internal data structure along with the description of a MOABVec that transparently interacts with contiguous tag data defined on the mesh

---
+++ include/petsc-private/dmmbimpl.h
@@ -0,0 +1,48 @@
+#if !defined(_MOABIMPL_H)
+#define _MOABIMPL_H
+
+#include <petscmat.h>       /*I      "petscmat.h"          I*/
+#include <petscdmmoab.h> /*I      "petscdmplex.h"    I*/
+#include "petsc-private/dmimpl.h"
+
+/* PETSC_EXTERN PetscLogEvent DMMOAB_Read, DMMOAB_Write; */
+
+/* This is an integer map, in addition it is also a container class
+   Design points:
+     - Low storage is the most important design point
+     - We want flexible insertion and deletion
+     - We can live with O(log) query, but we need O(1) iteration over strata
+*/
+typedef struct {
+  moab::Interface    *mbiface;
+  moab::ParallelComm *pcomm;
+  moab::Range        *tag_range; /* entities to which this tag applies */
+  moab::Tag           tag;
+  PetscInt            tag_size;
+  PetscBool           new_tag;
+  PetscBool           is_global_vec;
+} Vec_MOAB;
+
+
+typedef struct {
+  PetscInt                bs;                     /* Number of degrees of freedom on each entity, aka tag size in moab */
+  PetscInt                dim;
+  PetscInt                n,nloc,nghost;           /* Number of global, local only and shared degrees of freedom for current partition */
+  PetscInt                nele,neleloc;           /* Number of global, local only and shared degrees of freedom for current partition */
+  PetscBool               icreatedinstance;       /* true if DM created moab instance internally, will destroy instance in DMDestroy */
+  moab::ParallelComm      *pcomm;
+  moab::Interface         *mbiface;
+  moab::Tag               ltog_tag;               /* moab supports "global id" tags, which are usually local to global numbering */
+  ISLocalToGlobalMapping  ltog_map;
+  VecScatter              ltog_sendrecv;
+  moab::Range             *vlocal, *vowned, *vghost;
+  moab::Range             *elocal, *eghost;
+} DM_Moab;
+
+
+PETSC_EXTERN PetscErrorCode DMCreateGlobalVector_Moab(DM,Vec *);
+PETSC_EXTERN PetscErrorCode DMCreateLocalVector_Moab(DM,Vec *);
+PETSC_EXTERN PetscErrorCode DMCreateMatrix_Moab(DM dm, MatType mtype,Mat *J);
+
+#endif /* _MOABIMPL_H */
+
