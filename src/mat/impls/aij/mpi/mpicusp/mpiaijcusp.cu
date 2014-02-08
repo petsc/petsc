c5483a7ad0c08f8ca23bb12da38e10cded2ce8b7
Barry Smith <bsmith@mcs.anl.gov>
2014-02-08 10:28:33 -0600

    fix for change to include files with <>

--- src/mat/impls/aij/mpi/mpicusp/mpiaijcusp.cu
+++ src/mat/impls/aij/mpi/mpicusp/mpiaijcusp.cu
@@ -2,7 +2,7 @@
 PETSC_CUDA_EXTERN_C_BEGIN
 #include <../src/mat/impls/aij/mpi/mpiaij.h>   /*I "petscmat.h" I*/
 PETSC_CUDA_EXTERN_C_END
-#include <mpicuspmatimpl.h>
+#include <../src/mat/impls/aij/mpi/mpicusp/mpicuspmatimpl.h>
 
 #undef __FUNCT__
 #define __FUNCT__ "MatMPIAIJSetPreallocation_MPIAIJCUSP"
