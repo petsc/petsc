37f9d51e0e1aac790f76993bea64de45d0efe3f6
Satish Balay <balay@mcs.anl.gov>
2014-02-20 11:47:54 -0600

    elemental: update to elemental-0.83.tgz

--- config/PETSc/packages/elemental.py
+++ config/PETSc/packages/elemental.py
@@ -3,10 +3,10 @@ import PETSc.package
 class Configure(PETSc.package.NewPackage):
   def __init__(self, framework):
     PETSc.package.NewPackage.__init__(self, framework)
-    self.gitcommit  = '89e7ddc52db68dc5a3f2635733293d7349dbd518'
+    self.gitcommit  = '8af5a5ccdcc57a8fa1a22398010bc5858ac8e8f1'
     self.giturls    = ['https://github.com/elemental/Elemental']
-    self.download   = ['http://libelemental.org/pub/releases/elemental-0.81.tgz',
-                       'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/elemental-0.81.tgz']
+    self.download   = ['http://libelemental.org/pub/releases/elemental-0.83.tgz',
+                       'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/elemental-0.83.tgz']
     self.liblist    = [['libelemental.a','libpmrrr.a']]
     self.includes   = ['elemental.hpp']
     self.cxx              = 1
