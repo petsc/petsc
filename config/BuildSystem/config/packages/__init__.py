44cd52722c4da7884235941c753194f233b6512c
Michael Lange <michael.lange@imperial.ac.uk>
2014-02-26 12:58:17 +0000

    DMPlex: Add DMPlexCreateCGNSFromFile() wrapper

--- config/BuildSystem/config/packages/__init__.py
+++ config/BuildSystem/config/packages/__init__.py
@@ -1,1 +1,1 @@
-all = ['make','f2cblaslapack','BlasLapack', 'CHOLMOD', 'exodusii', 'scientificpython', 'fiat', 'MOAB', 'MPI', 'netcdf', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust', 'hdf5', 'netcdf-cxx']
+all = ['make','f2cblaslapack','BlasLapack', 'CHOLMOD', 'exodusii', 'scientificpython', 'fiat', 'MOAB', 'MPI', 'netcdf', 'PETSc', 'UMFPACK', 'boost', 'cusp', 'thrust', 'hdf5', 'netcdf-cxx', 'cgns']
