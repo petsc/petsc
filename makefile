petsc/petsc

commit 5fabf8590ff44484ab83a66709e4e483d353a2f5

run streams test will alltests

makefile
@@ -246,6 +246,9 @@ newall:
 
 streams:
 	cd src/benchmarks/streams; ${OMAKE} streams
+
+stream:
+	cd src/benchmarks/streams; ${OMAKE} stream
 # ------------------------------------------------------------------
 #
 # All remaining actions are intended for PETSc developers only.
@@ -307,7 +310,7 @@ alldoc1: chk_loc deletemanualpages chk_concepts_dir
 # Builds .html versions of the source
 # html overwrites some stuff created by update-docs - hence this is done later.
 alldoc2: chk_loc
-	-${OMAKE} ACTION=html PETSC_DIR=${PETSC_DIR} alltree LOC=${LOC}
+	-${OMAKE} ACTION=html PETSC_DIR=${PETSC_DIR} alltee LOC=${LOC}
 	-${PYTHON} bin/maint/update-docs.py ${PETSC_DIR} ${LOC}
 #
 # Builds HTML versions of Matlab scripts

src/benchmarks/streams/makefile
@@ -35,7 +35,7 @@ PthreadVersion: PthreadVersion.o  chkopts
 	-@${CLINKER} -o PthreadVersion PthreadVersion.o ${PETSC_LIB}
 	@${RM} -f PthreadVersion.o
 
-streams:  MPIVersion
+stream:  MPIVersion
 	@if [ "${NPMAX}foo" = "foo" ]; then echo "---------"; echo " Run with make streams NPMAX=<integer number of MPI processes to use>"; exit 1 ; fi
 	-@printf "" > scaling.log
 	-@for (( i=1; i<=${NPMAX}; i++ )) do \
@@ -43,7 +43,8 @@ streams:  MPIVersion
         done
 	-@echo "------------------------------------------------"
 	-@./process.py createfile
-	-@./process.py
 
+streams: stream
+	-@./process.py
 
 

conf/test
@@ -195,6 +195,7 @@ TESTMODE = testexamples
 alltests:
 	-@${RM} ${PETSC_ARCH}/conf/alltests.log alltests.log
 	-@ln -s ${PETSC_ARCH}/conf/alltests.log alltests.log
+	-@if [ "${PETSC_WITH_BATCH}" == "" ]; then make PETSC_ARCH=${PETSC_ARCH} PETSC_DIR=${PETSC_DIR} stream NPMAX=4 | tee ${PETSC_ARCH}/conf/alltests.log ;fi
 	-@${OMAKE} alltest PETSC_ARCH=${PETSC_ARCH} PETSC_DIR=${PETSC_DIR} MPIEXEC="${MPIEXEC}" DATAFILESPATH=${DATAFILESPATH} 2>&1 | tee ${PETSC_ARCH}/conf/alltests.log
 
 alltest:

commit 7070facac7f8bfe11dd3d0ee24b7f423449f95d9

html: fix typo introduced in 5fabf8590 - that prevented html doc generation

makefile
@@ -314,7 +314,7 @@ alldoc1: chk_loc deletemanualpages chk_concepts_dir
 # Builds .html versions of the source
 # html overwrites some stuff created by update-docs - hence this is done later.
 alldoc2: chk_loc
-	-${OMAKE} ACTION=html PETSC_DIR=${PETSC_DIR} alltee LOC=${LOC}
+	-${OMAKE} ACTION=html PETSC_DIR=${PETSC_DIR} alltree LOC=${LOC}
 	-${PYTHON} bin/maint/update-docs.py ${PETSC_DIR} ${LOC}
 #
 # Builds HTML versions of Matlab scripts