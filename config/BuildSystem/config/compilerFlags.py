808f76021818070b9111b583054cf1da0e1d5802
Barry Smith <bsmith@mcs.anl.gov>
2014-04-06 22:58:41 -0500

    barry is an idiot who doesn't have a clue as to what he is doing (and git's unfriendlyness doesn't help matters)

--- config/BuildSystem/config/compilerFlags.py
+++ config/BuildSystem/config/compilerFlags.py
@@ -87,7 +87,7 @@ class Configure(config.base.Configure):
       try:
         self.rejected[language] = []
         for bopt in bopts:
-g          if not bopt == '' and self.getOptionalFlagsName(language) in self.framework.argDB:
+          if not bopt == '' and self.getOptionalFlagsName(language) in self.framework.argDB:
             # treat user supplied options as single option - as it could include options separated by spaces '-tp k8-64'
             flags = [self.framework.argDB[self.getOptionalFlagsName(language)]]
           elif bopt == '' and self.getCompilerFlagsName(language) in self.framework.argDB:
