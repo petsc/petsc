859a8de3bb27aab35f22d86bc50015c35ecb6944
Peter Brune <brune@mcs.anl.gov>
2014-04-10 18:37:37 -0500

    MatColoring: Remove MatColoringMIS

--- src/mat/color/interface/matcoloringregi.c
+++ src/mat/color/interface/matcoloringregi.c
@@ -2,7 +2,6 @@
 #include <petscmat.h>
 
 PETSC_EXTERN PetscErrorCode MatColoringCreate_JP(MatColoring);
-PETSC_EXTERN PetscErrorCode MatColoringCreate_MIS(MatColoring);
 PETSC_EXTERN PetscErrorCode MatColoringCreate_Greedy(MatColoring);
 PETSC_EXTERN PetscErrorCode MatColoringCreate_Natural(MatColoring);
 PETSC_EXTERN PetscErrorCode MatColoringCreate_SL(MatColoring);
@@ -34,7 +33,6 @@ PetscErrorCode  MatColoringRegisterAll(void)
   PetscFunctionBegin;
   MatColoringRegisterAllCalled = PETSC_TRUE;
   ierr = MatColoringRegister(MATCOLORINGJP,MatColoringCreate_JP);CHKERRQ(ierr);
-  ierr = MatColoringRegister(MATCOLORINGMIS,MatColoringCreate_MIS);CHKERRQ(ierr);
   ierr = MatColoringRegister(MATCOLORINGGREEDY,MatColoringCreate_Greedy);CHKERRQ(ierr);
   ierr = MatColoringRegister(MATCOLORINGNATURAL,MatColoringCreate_Natural);CHKERRQ(ierr);
   ierr = MatColoringRegister(MATCOLORINGSL,MatColoringCreate_SL);CHKERRQ(ierr);
