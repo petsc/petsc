fd9961978eb97d9d2e825c9b5eff87d3d6d8cac9
Matthew G. Knepley <knepley@gmail.com>
2014-04-10 22:24:09 -0500

    PetscSection: Better check when creating global section

--- src/vec/is/utils/vsectionis.c
+++ src/vec/is/utils/vsectionis.c
@@ -977,7 +977,11 @@ PetscErrorCode PetscSectionCreateGlobalSection(PetscSection s, PetscSF sf, Petsc
     ierr = PetscSFBcastBegin(sf, MPIU_INT, neg, recv);CHKERRQ(ierr);
     ierr = PetscSFBcastEnd(sf, MPIU_INT, neg, recv);CHKERRQ(ierr);
     for (p = pStart; p < pEnd; ++p) {
-      if (recv[p] < 0) (*gsection)->atlasDof[p-pStart] = recv[p];
+      if (recv[p] < 0) {
+        (*gsection)->atlasDof[p-pStart] = recv[p];
+        ierr = PetscSectionGetDof(s, p, &dof);CHKERRQ(ierr);
+        if (-(recv[p]+1) != dof) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Global dof %d for point %d is not the unconstrained %d", -(recv[p]+1), p, dof);
+      }
     }
   }
   /* Calculate new sizes, get proccess offset, and calculate point offsets */
