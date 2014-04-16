0c8c14c502e79aaeb7ec26e1cd1e476322d9010e
Barry Smith <bsmith@mcs.anl.gov>
2014-04-16 13:56:11 -0500

    double http:// in message
    Reported-by: "Jose E. Roman" <jroman@dsic.upv.es>

--- src/sys/error/errtrace.c
+++ src/sys/error/errtrace.c
@@ -221,7 +221,7 @@ PetscErrorCode  PetscTraceBackErrorHandler(MPI_Comm comm,int line,const char *fu
         if (text) (*PetscErrorPrintf)("%s\n",text);
       }
       if (mess) (*PetscErrorPrintf)("%s\n",mess);
-      (*PetscErrorPrintf)("See http://http://www.mcs.anl.gov/petsc/documentation/faq.html for trouble shooting.\n");
+      (*PetscErrorPrintf)("See http://www.mcs.anl.gov/petsc/documentation/faq.html for trouble shooting.\n");
       (*PetscErrorPrintf)("%s\n",version);
       if (PetscErrorPrintfInitializeCalled) (*PetscErrorPrintf)("%s on a %s named %s by %s %s\n",pname,arch,hostname,username,date);
       (*PetscErrorPrintf)("Configure options %s\n",petscconfigureoptions);
