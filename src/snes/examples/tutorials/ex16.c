ca88a98c66147bb420518223d8a496fd63178d5b
Barry Smith <bsmith@mcs.anl.gov>
2014-04-18 18:31:32 -0500

    need to cast floating point to (double) in printf() statements since could be quad precision

--- src/snes/examples/tutorials/ex16.c
+++ src/snes/examples/tutorials/ex16.c
@@ -164,7 +164,7 @@ int main(int argc,char **argv)
   ierr = InitialGuess(da,&user,x);CHKERRQ(ierr);
   ierr = FormRHS(da,&user,b);CHKERRQ(ierr);
 
-  ierr = PetscPrintf(comm,"lambda: %f mu: %f\n",user.lambda,user.mu);CHKERRQ(ierr);
+  ierr = PetscPrintf(comm,"lambda: %f mu: %f\n",(double)user.lambda,(double)user.mu);CHKERRQ(ierr);
 
   /* show a cross-section of the initial state */
   if (viewline) {
@@ -1057,7 +1057,7 @@ PetscErrorCode DisplayLine(SNES snes,Vec X)
     if (rank == r) {
       if (j >= ys && j < ys+ym && k >= zs && k < zs+zm) {
         for (i=xs; i<xs+xm; i++) {
-          PetscPrintf(PETSC_COMM_SELF,"%d %d %d: %f %f %f\n",i,0,0,PetscRealPart(c[k][j][i][0] + x[k][j][i][0]),PetscRealPart(c[k][j][i][1] + x[k][j][i][1]),PetscRealPart(c[k][j][i][2] + x[k][j][i][2]));
+          ierr = PetscPrintf(PETSC_COMM_SELF,"%D %d %d: %f %f %f\n",i,0,0,(double)PetscRealPart(c[k][j][i][0] + x[k][j][i][0]),(double)PetscRealPart(c[k][j][i][1] + x[k][j][i][1]),(double)PetscRealPart(c[k][j][i][2] + x[k][j][i][2]));CHKERRQ(ierr);
         }
       }
     }
