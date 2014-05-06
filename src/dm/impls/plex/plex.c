9a4fe19dfa62737c406150298762fa5e60d2bf1a
Matthew G. Knepley <knepley@gmail.com>
2014-05-05 20:49:31 -0500

    DMPlex: Fixed stupid typo

--- src/dm/impls/plex/plex.c
+++ src/dm/impls/plex/plex.c
@@ -3056,7 +3056,7 @@ PetscErrorCode DMPlexOrient(DM dm)
         ierr = MatSetUp(G);CHKERRQ(ierr);
         for (p = 0; p < numProcs; ++p) {
           for (n = 0; n < recvcounts[p]; ++n) {
-            const PetscInt    r = proc;
+            const PetscInt    r = p;
             const PetscInt    q = adj[displs[p]+n];
             const PetscScalar o = val[displs[p]+n] ? 1.0 : 0.0;
 
