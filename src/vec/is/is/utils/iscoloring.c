petsc/petsc

commit 98c3331e5c81c4bfa5036a9b6bc521ec2d439166

Insure that all ASCII viewers for PETSc objects call PetscObjectPrintClassNamePrefixType()

src/vec/is/is/utils/iscoloring.c
@@ -130,6 +130,8 @@ PetscErrorCode  ISColoringView(ISColoring iscoloring,PetscViewer viewer)
   if (iascii) {
     MPI_Comm    comm;
     PetscMPIInt rank;
+
+    ierr = PetscObjectPrintClassNamePrefixType((PetscObject)iscoloring,viewer);CHKERRQ(ierr);
     ierr = PetscObjectGetComm((PetscObject)viewer,&comm);CHKERRQ(ierr);
     ierr = MPI_Comm_rank(comm,&rank);CHKERRQ(ierr);
     ierr = PetscViewerASCIISynchronizedAllow(viewer,PETSC_TRUE);CHKERRQ(ierr);

src/dm/dt/interface/dt.c
@@ -111,6 +111,7 @@ PetscErrorCode PetscQuadratureView(PetscQuadrature quad, PetscViewer viewer)
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)quad,viewer);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(viewer, "Quadrature on %d points\n  (", quad->numPoints);CHKERRQ(ierr);
   for (q = 0; q < quad->numPoints; ++q) {
     for (d = 0; d < quad->dim; ++d) {

src/dm/interface/dm.c
@@ -624,6 +624,7 @@ PetscErrorCode  DMView(DM dm,PetscViewer v)
   if (!v) {
     ierr = PetscViewerASCIIGetStdout(PetscObjectComm((PetscObject)dm),&v);CHKERRQ(ierr);
   }
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)dm,v);CHKERRQ(ierr);
   ierr = PetscObjectTypeCompare((PetscObject)v,PETSCVIEWERBINARY,&isbinary);CHKERRQ(ierr);
   if (isbinary) {
     PetscInt classid = DM_FILE_CLASSID;

src/mat/coarsen/coarsen.c
@@ -312,10 +312,7 @@ PetscErrorCode  MatCoarsenView(MatCoarsen agg,PetscViewer viewer)
   PetscCheckSameComm(agg,1,viewer,2);
 
   ierr = PetscObjectTypeCompare((PetscObject)viewer,PETSCVIEWERASCII,&iascii);CHKERRQ(ierr);
-  if (iascii) {
-    ierr = PetscObjectPrintClassNamePrefixType((PetscObject)agg,viewer);CHKERRQ(ierr);
-  } else SETERRQ1(PETSC_COMM_SELF,PETSC_ERR_SUP,"Viewer type %s not supported for this MatCoarsen",((PetscObject)viewer)->type_name);
-
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)agg,viewer);CHKERRQ(ierr);
   if (agg->ops->view) {
     ierr = PetscViewerASCIIPushTab(viewer);CHKERRQ(ierr);
     ierr = (*agg->ops->view)(agg,viewer);CHKERRQ(ierr);

src/mat/interface/matrix.c
@@ -841,8 +841,8 @@ PetscErrorCode  MatView(Mat mat,PetscViewer viewer)
   if (iascii) {
     if (!mat->assembled) SETERRQ(PetscObjectComm((PetscObject)mat),PETSC_ERR_ORDER,"Must call MatAssemblyBegin/End() before viewing matrix");
     ierr = PetscViewerGetFormat(viewer,&format);CHKERRQ(ierr);
+    ierr = PetscObjectPrintClassNamePrefixType((PetscObject)mat,viewer);CHKERRQ(ierr);
     if (format == PETSC_VIEWER_ASCII_INFO || format == PETSC_VIEWER_ASCII_INFO_DETAIL) {
-      ierr = PetscObjectPrintClassNamePrefixType((PetscObject)mat,viewer);CHKERRQ(ierr);
       ierr = PetscViewerASCIIPushTab(viewer);CHKERRQ(ierr);
       ierr = MatGetSize(mat,&rows,&cols);CHKERRQ(ierr);
       ierr = MatGetBlockSize(mat,&bs);CHKERRQ(ierr);

src/sys/classes/draw/interface/drawreg.c
@@ -58,6 +58,7 @@ PetscErrorCode  PetscDrawView(PetscDraw indraw,PetscViewer viewer)
   PetscValidHeaderSpecific(viewer,PETSC_VIEWER_CLASSID,2);
   PetscCheckSameComm(indraw,1,viewer,2);
 
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)indraw,viewer);CHKERRQ(ierr);
   ierr = PetscObjectTypeCompare((PetscObject)viewer,PETSCVIEWERDRAW,&isdraw);CHKERRQ(ierr);
 #if defined(PETSC_HAVE_SAWS)
   ierr = PetscObjectTypeCompare((PetscObject)viewer,PETSCVIEWERSAWS,&isams);CHKERRQ(ierr);

src/sys/classes/draw/utils/hists.c
@@ -421,6 +421,7 @@ PetscErrorCode  PetscDrawHGView(PetscDrawHG hist,PetscViewer viewer)
   if (!viewer){
     ierr = PetscViewerASCIIGetStdout(PetscObjectComm((PetscObject)hist),&viewer);CHKERRQ(ierr);
   }
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)hist,viewer);CHKERRQ(ierr);
   xmax      = hist->xmax;
   xmin      = hist->xmin;
   numValues = hist->numValues;

src/sys/classes/draw/utils/lgc.c
@@ -554,6 +554,7 @@ PetscErrorCode  PetscDrawLGView(PetscDrawLG lg,PetscViewer viewer)
   if (!viewer){
     ierr = PetscViewerASCIIGetStdout(PetscObjectComm((PetscObject)lg),&viewer);CHKERRQ(ierr);
   }
+  ierr = PetscObjectPrintClassNamePrefixType((PetscObject)lg,viewer);CHKERRQ(ierr);
   for (i = 0; i < dim; i++) {
     ierr = PetscViewerASCIIPrintf(viewer, "Line %D>\n", i);CHKERRQ(ierr);
     for (j = 0; j < nopts; j++) {

commit 6658969be2f1b98a9d422051441b68a28f8be09f

ISColoring: Fix ISColoringView
Bug introduced in 98c3331 making all PetscObjects call
PetscObjectPrintClassNamePrefixType() during their ASCII viewer.
However, ISColoring is not a PetscObject.  ISColoringView now prints
a fascimile of "ClassNamePrefixType" output.

src/vec/is/is/utils/iscoloring.c
@@ -129,11 +129,12 @@ PetscErrorCode  ISColoringView(ISColoring iscoloring,PetscViewer viewer)
   ierr = PetscObjectTypeCompare((PetscObject)viewer,PETSCVIEWERASCII,&iascii);CHKERRQ(ierr);
   if (iascii) {
     MPI_Comm    comm;
-    PetscMPIInt rank;
+    PetscMPIInt size,rank;
 
-    ierr = PetscObjectPrintClassNamePrefixType((PetscObject)iscoloring,viewer);CHKERRQ(ierr);
     ierr = PetscObjectGetComm((PetscObject)viewer,&comm);CHKERRQ(ierr);
+    ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
     ierr = MPI_Comm_rank(comm,&rank);CHKERRQ(ierr);
+    ierr = PetscViewerASCIIPrintf(viewer,"ISColoring Object: %d MPI processes\n",size);CHKERRQ(ierr);
     ierr = PetscViewerASCIISynchronizedAllow(viewer,PETSC_TRUE);CHKERRQ(ierr);
     ierr = PetscViewerASCIISynchronizedPrintf(viewer,"[%d] Number of colors %d\n",rank,iscoloring->n);CHKERRQ(ierr);
     ierr = PetscViewerFlush(viewer);CHKERRQ(ierr);