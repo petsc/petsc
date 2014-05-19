4d06e3008b3ddd55e8ae7cd0cc281cbd2ace4ecc
Surtai Han <surtai.han@gmail.com>
2014-05-19 14:45:15 -0500

    reorganize prefix parsing code

--- saws/js/fetchSawsData.js
+++ saws/js/fetchSawsData.js
@@ -1,9 +1,9 @@
-//modified from PETSc.getAndDisplayDirectory
+//GetAndDisplayDirectory: modified from PETSc.getAndDisplayDirectory
+//------------------------------------------
 SAWsGetAndDisplayDirectory = function(names,divEntry){
     jQuery(divEntry).html(""); //clears divEntry
     SAWs.getDirectory(names,SAWsDisplayDirectory,divEntry);
 }
-
 
 
 //DisplayDirectory: modified from PETSc.displayDirectory
@@ -32,82 +32,18 @@ SAWsDisplayDirectory = function(sub,divEntry)
             $("#o-1").append("<br><b style='margin-left:20px;' title=\"Preconditioner\" id=\"pcList-1"+SAWs_prefix+"text\">-"+SAWs_prefix+"pc_type &nbsp; &nbsp;</b><select class=\"pcLists\" id=\"pcList-1"+SAWs_prefix+"\"></select>");
             populatePcList("pcList-1"+SAWs_prefix,SAWs_alternatives,SAWs_pcVal);
 
-            //parse through prefix: first determine what fieldsplit level we are working with, then determine what endtag we are working with
+            //parse through prefix
 
-            var fieldsplit="0";
-
-            if(SAWs_prefix.indexOf("fieldsplit_")!=-1) {//still contains at least 1 fieldsplit (assume max of 1 fieldsplit for now...need more test cases)
-
-                var closest=SAWs_prefix.length;//the furthest a keyword could possibly be
-                //find index of next keyword (pc, ksp, sub, smoothing, coarse)
-                var keywords=["pc","ksp","sub","smoothing","coarse","redundant"];
-                var loc="";
-                for(var i=0; i<keywords.length; i++) {
-                    loc=SAWs_prefix.indexOf(keywords[i]);
-                    if(loc < closest && loc != -1)
-                        closest=loc;
-                }
-
-                var theword = SAWs_prefix.substring(11,closest-1);//omit the first and last underscore
-
-                if(theword != currentFieldsplitWord) {// new fieldsplit
-                    currentFieldsplitNumber++;
-                    currentFieldsplitWord=theword;
-                    var writeLoc = sawsInfo.length;
-                    sawsInfo[writeLoc]      = new Object();
-                    sawsInfo[writeLoc].data = new Array();
-                    sawsInfo[writeLoc].id   = fieldsplit + currentFieldsplitNumber.toString();
-                }
-                if(currentFieldsplitNumber != -1)
-                    fieldsplit = fieldsplit + currentFieldsplitNumber.toString();
-            }
-
+            var fieldsplit = parsePrefixForFieldsplit(SAWs_prefix);
             var index = getSawsIndex(fieldsplit);
 
-            var endtag="";
-            while(SAWs_prefix!="") {//parse the entire prefix
-                var indexFirstUnderscore=SAWs_prefix.indexOf("_");
-                var chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//dont include the underscore
-
-                if(chunk=="mg") {//mg_
-                    indexFirstUnderscore=SAWs_prefix.indexOf("_",3); //this will actually be the index of the second underscore now since mg prefix has underscore in itself
-                    chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
-                }
-
-                if(chunk=="mg_levels") {//need to include yet another underscore
-                    indexFirstUnderscore=SAWs_prefix.indexOf("_",10); //this will actually be the index of the third underscore
-                    chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
-                }
-
-                SAWs_prefix=SAWs_prefix.substring(indexFirstUnderscore+1, SAWs_prefix.length);//dont include the underscore
-
-                if(chunk=="mg_coarse" && SAWs_prefix=="")//new mg coarse
-                    mgLevelLocation=endtag+"0";
-
-                if(chunk=="ksp" || chunk=="sub" || chunk=="mg_coarse" || chunk=="redundant")
-                    endtag+="0";
-                else if(chunk.substring(0,10)=="mg_levels_")
-                    endtag+=chunk.substring(10,11);//can only be 1 character long for now
-
-                if(chunk.substring(0,10)=="mg_levels_" && SAWs_prefix=="")//new mg levels. it's okay to assume memory was already allocated b/c levels is after coarse
-                    sawsInfo[index].data[getSawsDataIndex(index, mgLevelLocation)].mg_levels=parseInt(chunk.substring(10,11))+1;
-            }
+            var endtag = parsePrefixForEndtag(SAWs_prefix, index);
 
             if(sawsInfo.length == 0) //special case. manually start off first one
                 index = 0;
 
-            if(sawsInfo[index] == undefined) {
-                sawsInfo[index]=new Object();
-                sawsInfo[index].id = fieldsplit;
-            }
-            if(sawsInfo[index].data == undefined)//allocate new mem if needed
-                sawsInfo[index].data=new Array();
-            //search if it has already been created
-            if(getSawsDataIndex(index,endtag) == -1) {//doesn't already exist so allocate new memory
-                var writeLoc=sawsInfo[index].data.length;
-                sawsInfo[index].data[writeLoc]=new Object();
-                sawsInfo[index].data[writeLoc].endtag=endtag;
-            }
+            allocateMemory(fieldsplit, endtag, index);
+
             var index2=getSawsDataIndex(index,endtag);
             sawsInfo[index].data[index2].pc=SAWs_pcVal;
 
@@ -141,80 +77,39 @@ SAWsDisplayDirectory = function(sub,divEntry)
             populateKspList("kspList-1"+SAWs_prefix,SAWs_alternatives,SAWs_kspVal);
 
             //parse through prefix...
-            //first determine what fieldsplit level we are working with, then determine what endtag we are working with
 
-            var fieldsplit="0";
+            var fieldsplit = parsePrefixForFieldsplit(SAWs_prefix);
+            var index = getSawsIndex(fieldsplit);
 
-            if(SAWs_prefix.indexOf("fieldsplit_")!=-1) {//still contains at least 1 fieldsplit (assume max of 1 fieldsplit for now...need more test cases)
-
-                var closest=SAWs_prefix.length;//the furthest a keyword could possibly be
-                //find index of next keyword (pc, ksp, sub, smoothing, coarse)
-                var keywords=["pc","ksp","sub","smoothing","coarse","redundant"];
-                var loc="";
-                for(var i=0; i<keywords.length; i++) {
-                    loc=SAWs_prefix.indexOf(keywords[i]);
-                    if(loc < closest && loc != -1)
-                        closest=loc;
-                }
-
-                var theword = SAWs_prefix.substring(11,closest-1);//omit the first and last underscore
-
-                if(theword != currentFieldsplitWord) {// new fieldsplit
-                    currentFieldsplitNumber++;
-                    currentFieldsplitWord=theword;
-                    var writeLoc = sawsInfo.length;
-                    sawsInfo[writeLoc]      = new Object();
-                    sawsInfo[writeLoc].data = new Array();
-                    sawsInfo[writeLoc].id   = fieldsplit + currentFieldsplitNumber.toString();
-                }
-                if(currentFieldsplitNumber != -1)
-                    fieldsplit = fieldsplit + currentFieldsplitNumber.toString();
-            }
-
-            var index=getSawsIndex(fieldsplit);
-
-            var endtag="";
-
-            while(SAWs_prefix!="") {//parse the entire prefix
-                var indexFirstUnderscore=SAWs_prefix.indexOf("_");
-                var chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//dont include the underscore
-
-                if(chunk=="mg") {//mg_
-                    indexFirstUnderscore=SAWs_prefix.indexOf("_",3); //this will actually be the index of the second underscore now since mg prefix has underscore in itself
-                    chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
-                }
-
-                if(chunk=="mg_levels") {//need to include yet another underscore
-                    indexFirstUnderscore=SAWs_prefix.indexOf("_",10); //this will actually be the index of the third underscore
-                    chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
-                }
-
-                SAWs_prefix=SAWs_prefix.substring(indexFirstUnderscore+1, SAWs_prefix.length);//dont include the underscore
-                if(chunk=="ksp" || chunk=="sub" || chunk=="mg_coarse" || chunk=="redundant")
-                    endtag+="0";
-                else if(chunk.substring(0,10)=="mg_levels_")
-                    endtag+=chunk.substring(10,11);//can only be 1 character long for now
-            }
+            var endtag = parsePrefixForEndtag(SAWs_prefix, index);
 
             if(index == -1)
                 index = 0;
 
-            if(sawsInfo[index] == undefined) {
-                sawsInfo[index]=new Object();
-                sawsInfo[index].id = fieldsplit;
-            }
-            if(sawsInfo[index].data == undefined)//allocate new mem if needed
-                sawsInfo[index].data=new Array();
-            //search if it has already been created
-            if(getSawsDataIndex(index,endtag) == -1) {//doesn't already exist so allocate new memory
-                var writeLoc=sawsInfo[index].data.length;
-                sawsInfo[index].data[writeLoc]=new Object();
-                sawsInfo[index].data[writeLoc].endtag=endtag;
-            }
+            allocateMemory(fieldsplit, endtag, index);
+
             var index2=getSawsDataIndex(index,endtag);
             sawsInfo[index].data[index2].ksp=SAWs_kspVal;
         }
     }
 
     SAWs.displayDirectoryRecursive(sub.directories,divEntry,0,"");//this function is not in SAWs API ?
+}
+
+
+//this function allocates memory in sawsInfo if needed to fit the new data from SAWs
+function allocateMemory(fieldsplit, endtag, index) {
+
+    if(sawsInfo[index] == undefined) {
+        sawsInfo[index]=new Object();
+        sawsInfo[index].id = fieldsplit;
+    }
+    if(sawsInfo[index].data == undefined)//allocate new mem if needed
+        sawsInfo[index].data=new Array();
+    //search if it has already been created
+    if(getSawsDataIndex(index,endtag) == -1) {//doesn't already exist so allocate new memory
+        var writeLoc=sawsInfo[index].data.length;
+        sawsInfo[index].data[writeLoc]=new Object();
+        sawsInfo[index].data[writeLoc].endtag=endtag;
+    }
 }
--- saws/js/parsePrefix.js
+++ saws/js/parsePrefix.js
@@ -1,7 +1,67 @@
-function parsePrefixForFieldsplit() {
+function parsePrefixForFieldsplit(SAWs_prefix) {
+
+    var fieldsplit="0";
+
+    if(SAWs_prefix.indexOf("fieldsplit_")!=-1) {//still contains at least 1 fieldsplit (assume max of 1 fieldsplit for now)
+
+        var closest=SAWs_prefix.length;//the furthest a keyword could possibly be
+        //find index of next keyword (pc, ksp, sub, smoothing, coarse)
+        var keywords=["pc","ksp","sub","smoothing","coarse","redundant"];
+        var loc="";
+        for(var i=0; i<keywords.length; i++) {
+            loc=SAWs_prefix.indexOf(keywords[i]);
+            if(loc < closest && loc != -1)
+                closest=loc;
+        }
+
+        var theword = SAWs_prefix.substring(11,closest-1);//omit the first and last underscore
+
+        if(theword != currentFieldsplitWord) {// new fieldsplit
+            currentFieldsplitNumber++;
+            currentFieldsplitWord=theword;
+            var writeLoc = sawsInfo.length;
+            sawsInfo[writeLoc]      = new Object();
+            sawsInfo[writeLoc].data = new Array();
+            sawsInfo[writeLoc].id   = fieldsplit + currentFieldsplitNumber.toString();
+        }
+        if(currentFieldsplitNumber != -1)
+            fieldsplit = fieldsplit + currentFieldsplitNumber.toString();
+    }
+
+    return fieldsplit;
 
 }
 
-function parsePrefixForEndtag() {
+function parsePrefixForEndtag(SAWs_prefix, index) {
 
+    var endtag="";
+    while(SAWs_prefix!="") {//parse the entire prefix
+        var indexFirstUnderscore=SAWs_prefix.indexOf("_");
+        var chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//dont include the underscore
+
+        if(chunk=="mg") {//mg_
+            indexFirstUnderscore=SAWs_prefix.indexOf("_",3); //this will actually be the index of the second underscore now since mg prefix has underscore in itself
+            chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
+        }
+
+        if(chunk=="mg_levels") {//need to include yet another underscore
+            indexFirstUnderscore=SAWs_prefix.indexOf("_",10); //this will actually be the index of the third underscore
+            chunk=SAWs_prefix.substring(0,indexFirstUnderscore);//updated chunk
+        }
+
+        SAWs_prefix=SAWs_prefix.substring(indexFirstUnderscore+1, SAWs_prefix.length);//dont include the underscore
+
+        if(chunk=="mg_coarse" && SAWs_prefix=="")//new mg coarse
+            mgLevelLocation=endtag+"0";
+
+        if(chunk=="ksp" || chunk=="sub" || chunk=="mg_coarse" || chunk=="redundant")
+            endtag+="0";
+        else if(chunk.substring(0,10)=="mg_levels_")
+            endtag+=chunk.substring(10,11);//can only be 1 character long for now
+
+        if(chunk.substring(0,10)=="mg_levels_" && SAWs_prefix=="")//new mg levels. it's okay to assume memory was already allocated b/c levels is after coarse
+            sawsInfo[index].data[getSawsDataIndex(index, mgLevelLocation)].mg_levels=parseInt(chunk.substring(10,11))+1;
+    }
+
+    return endtag;
 }
