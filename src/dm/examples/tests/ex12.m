2c9ec6dfe874b911fa49ef7e759d29a8430d6aff
Barry Smith <bsmith@mcs.anl.gov>
2014-06-20 20:11:59 -0500

    make PETSc MATLAB socket access code store generated .mex files in PETSC_ARCH/lib/petsc/matlab instead of bin/matlab

--- src/dm/examples/tests/ex12.m
+++ src/dm/examples/tests/ex12.m
@@ -5,8 +5,8 @@ function ex12(np,opt)
 %
 % Run with option -on_error_attach_debugger to debug
 %
-%  Requires the Matlab mex routines in ${PETSC_DIR}/bin/matlab.
-%  Make sure that ${PETSC_DIR}/bin/matlab is in your Matlab PATH.
+  %  Requires the Matlab mex routines in ${PETSC_DIR}/${PETSC_ARCH}/lib/matlab.
+%  Make sure that ${PETSC_DIR}/bin/matlab and ${PETSC_DIR}/${PETSC_ARCH}/lib/matlab is in your Matlab PATH.
 %
 if (nargin < 1)
   np = 1;
--- src/sys/classes/viewer/impls/matlab/vmatlab.c
+++ src/sys/classes/viewer/impls/matlab/vmatlab.c
@@ -9,7 +9,7 @@
    Level: intermediate
 
        Note: Currently can only save PETSc vectors to .mat files, not matrices (use the PETSCVIEWERBINARY and
-             ${PETSC_DIR}/bin/matlab/PetscBinaryRead.m to read matrices into matlab).
+             ${PETSC_DIR}/bin/matlab/PetscBinaryRead.m to read matrices into MATLAB).
 
              For parallel vectors obtained with DMCreateGlobalVector() or DMGetGlobalVector() the vectors are saved to
              the .mat file in natural ordering. You can use DMView() to save the DMDA information to the .mat file
