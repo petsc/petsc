a3fb1227e98c1cc5cec2d51ce6cbf5ed353dd226
Surtai Han <surtai.han@gmail.com>
2014-07-08 17:47:04 -0500

    progress on getCmdOptions

--- saws/js/getCmdOptions.js
+++ saws/js/getCmdOptions.js
@@ -1,3 +1,82 @@
+//this function generates the command-line options for the given solver
+//thus, passing in "0" (root solver) would generate the full command-line options
+//important: use option='newline' to display each option on a new line. use option='space' to put spaces in between the options (as if we were using the terminal). option is space by default.
+function getCmdOptions(endtag,prefix,option)
+{
+    if(prefix == undefined)
+        prefix = "";
+
+    var endl = "";
+    if(option == "newline")
+        endl = "\n";
+    else if(option == "space")
+        endl = " ";
+    else
+        endl = " ";// use space by default
+
+    var ret   = "";
+    var index = getIndex(matInfo,endtag);
+    if(index == -1)
+        return "";
+
+    ret += prefix + "pc_type " + matInfo[index].pc_type + endl;
+    ret += prefix + "ksp_type " + matInfo[index].ksp_type + endl;
+
+    var pc_type = matInfo[index].pc_type;
+
+    if(pc_type == "mg") { //add extra info related to mg
+        ret += prefix + "pc_mg_type " + matInfo[index].pc_mg_type + endl;
+        ret += prefix + "pc_mg_levels " + matInfo[index].pc_mg_levels + endl;
+    }
+    else if(pc_type == "fieldsplit") {
+        ret += prefix + "pc_fieldsplit_type " + matInfo[index].pc_fieldsplit_type + endl;
+        ret += prefix + "pc_fieldsplit_blocks " + matInfo[index].pc_fieldsplit_blocks + endl;
+    }
+    else if(pc_type == "bjacobi") {
+        ret += prefix + "pc_bjacobi_blocks " + matInfo[index].pc_bjacobi_blocks + endl;
+    }
+    else if(pc_type == "asm") {
+        ret += prefix + "pc_asm_blocks " + matInfo[index].pc_asm_blocks + endl;
+        ret += prefix + "pc_asm_overlap " + matInfo[index].pc_asm_overlap + endl;
+    }
+    else if(pc_type == "redundant") {
+        ret += prefix + "pc_redundant_number " + matInfo[index].pc_redundant_number + endl;
+    }
+
+
+    //then recursively handle all the children
+    var numChildren = getNumChildren(matInfo,endtag);
+
+    //handle children recursively
+    for(var i=0; i<numChildren; i++) {
+        var childEndtag = endtag + "_" + i;
+        var childPrefix  = "";
+
+        //first determine appropriate prefix
+        if(pc_type == "mg")
+            childPrefix = "mg_levels_" + i + "_";
+        else if(pc_type == "fieldsplit")
+            childPrefix = "fieldsplit_" + i + "_";
+        else if(pc_type == "bjacobi")
+            childPrefix = "sub_";
+        else if(pc_type == "asm")
+            childPrefix = "sub_";
+        else if(pc_type == "redundant")
+            childPrefix = "redundant_";
+        else if(pc_type == "ksp")
+            childPrefix = "sub_";
+
+        ret += getCmdOptions(childEndtag,childPrefix); //recursive call
+    }
+
+    return ret;
+}
+
+
+
+
+
+
 /*
   pcGetDetailedInfo - get detailed information from the pclists
   input:
@@ -8,6 +87,7 @@
     matInfo.string
     matInfo.stringshort
 */
+/*
 function pcGetDetailedInfo(pcListID, prefix,recursionCounter,matInfo)
 {
     var pcSelectedValue=$("#"+pcListID).val();
@@ -145,7 +225,7 @@ function pcGetDetailedInfo(pcListID, prefix,recursionCounter,matInfo)
             pcGetDetailedInfo(childID,prefix,recursionCounter,matInfo);
 	}
     }
-}
+}*/
 
 
 
@@ -158,7 +238,7 @@ function pcGetDetailedInfo(pcListID, prefix,recursionCounter,matInfo)
     matInfo[].string stores collected solver options
     matInfo[].stringshort
 */
-function solverGetOptions(matInfo)
+/*function solverGetOptions(matInfo)
 {
     var prefix,kspSelectedValue,pcSelectedValue,level;
 
@@ -199,4 +279,4 @@ function solverGetOptions(matInfo)
             pcGetDetailedInfo("pcList"+ matInfo[i].id,prefix,matInfo[i].id,matInfo);
 	}
     }
-}
+}*/
