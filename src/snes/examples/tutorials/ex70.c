35a2f1ba208ddbb74d657014264e2a9c06170dbe
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-07-18 12:53:12 +0200

    SNES ex70: Updated usage instructions.
    Reported-by: Christiaan Klaij <C.Klaij@marin.nl> via petsc-users

--- src/snes/examples/tutorials/ex70.c
+++ src/snes/examples/tutorials/ex70.c
@@ -16,35 +16,39 @@ static char help[] = "Poiseuille flow problem. Viscous, laminar flow in a 2D cha
 /* Discretized with the cell-centered finite-volume method on a                */
 /* Cartesian grid with co-located variables. Variables ordered as              */
 /* [u1...uN v1...vN p1...pN]^T. Matrix [A00 A01; A10, A11] solved with         */
-/* PCFIELDSPLIT.                                                               */
+/* PCFIELDSPLIT. Lower factorization is used to mimick the Semi-Implicit       */
+/* Method for Pressure Linked Equations (SIMPLE) used as preconditioner        */
+/* instead of solver.                                                          */
 /*                                                                             */
 /* Disclaimer: does not contain the pressure-weighed interpolation             */
 /* method needed to suppress spurious pressure modes in real-life              */
 /* problems.                                                                   */
 /*                                                                             */
-/* usage:                                                                      */
+/* Usage:                                                                      */
 /*                                                                             */
-/* mpiexec -n 2 ./stokes -nx 32 -ny 48                                         */
+/* mpiexec -n 2 ./ex70 -nx 32 -ny 48 -ksp_type fgmres -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_schur_fact_type lower -fieldsplit_1_pc_type none */
 /*                                                                             */
-/*   Runs with PETSc defaults on 32x48 grid, no PC for the Schur               */
-/*   complement because A11 is zero.                                           */
+/*   Runs with PCFIELDSPLIT on 32x48 grid, no PC for the Schur                 */
+/*   complement because A11 is zero. FGMRES is needed because                  */
+/*   PCFIELDSPLIT is a variable preconditioner.                                */
 /*                                                                             */
-/* mpiexec -n 2 ./stokes -nx 32 -ny 48 -fieldsplit_1_user_pc                   */
+/* mpiexec -n 2 ./ex70 -nx 32 -ny 48 -ksp_type fgmres -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_schur_fact_type lower -user_pc */
 /*                                                                             */
 /*   Same as above but with user defined PC for the true Schur                 */
 /*   complement. PC based on the SIMPLE-type approximation (inverse of         */
 /*   A00 approximated by inverse of its diagonal).                             */
 /*                                                                             */
-/* mpiexec -n 2 ./stokes -nx 32 -ny 48 -fieldsplit_1_user_ksp                  */
+/* mpiexec -n 2 ./ex70 -nx 32 -ny 48 -ksp_type fgmres -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_schur_fact_type lower -user_ksp */
 /*                                                                             */
 /*   Replace the true Schur complement with a user defined Schur               */
 /*   complement based on the SIMPLE-type approximation. Same matrix is         */
 /*   used as PC.                                                               */
 /*                                                                             */
-/* mpiexec -n 2 ./stokes -nx 32 -ny 48 -fieldsplit_1_user_ksp -fieldsplit_1_ksp_rtol 0.01 -fieldsplit_0_ksp_rtol 0.01 */
+/* mpiexec -n 2 ./ex70 -nx 32 -ny 48 -ksp_type fgmres -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_schur_fact_type lower -fieldsplit_0_ksp_type gmres -fieldsplit_0_pc_type bjacobi -fieldsplit_1_pc_type jacobi -fieldsplit_1_inner_ksp_type preonly -fieldsplit_1_inner_pc_type jacobi -fieldsplit_1_upper_ksp_type preonly -fieldsplit_1_upper_pc_type jacobi */
 /*                                                                             */
-/*   SIMPLE-type approximations are crude, there's no benefit in               */
-/*   solving the subsystems in the preconditioner very accurately.             */
+/*   Out-of-the-box SIMPLE-type preconditioning. The major advantage           */
+/*   is that the user neither needs to provide the approximation of            */
+/*   the Schur complement, nor the corresponding preconditioner.               */
 /*                                                                             */
 /*---------------------------------------------------------------------------- */
 
