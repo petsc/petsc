51cb09fe7aa655118d9d6c6667d4a79a20517543
Stefano Zampini <stefano.zampini@gmail.com>
2014-08-24 10:36:47 +0300

    PCBDDC: using MatIsSymmetricKnown for symmetry checking will never break codes

--- src/ksp/pc/impls/bddc/bddcprivate.c
+++ src/ksp/pc/impls/bddc/bddcprivate.c
@@ -532,7 +532,13 @@ PetscErrorCode PCBDDCSetUpCorrection(PC pc, PetscScalar **coarse_submat_vals_n)
   }
 
   /* compute other basis functions for non-symmetric problems */
-  ierr = MatIsSymmetric(pc->pmat,0.,&pcbddc->issym);CHKERRQ(ierr);
+  /*ierr = MatIsSymmetric(pc->pmat,0.,&pcbddc->issym);CHKERRQ(ierr);*/
+  { /* this is a temporary workaround since seqbaij matrices does not have support for symmetry checking */
+    PetscBool setsym;
+    ierr = MatIsSymmetricKnown(pc->pmat,&setsym,&pcbddc->issym);CHKERRQ(ierr);
+    if (!setsym) pcbddc->issym = PETSC_FALSE;
+  }
+
   if (!pcbddc->issym) {
     if (!pcbddc->coarse_psi_B) {
       ierr = MatCreate(PETSC_COMM_SELF,&pcbddc->coarse_psi_B);CHKERRQ(ierr);
