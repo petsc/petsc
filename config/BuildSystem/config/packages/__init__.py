47c09f671394d1bc3527a643da9c66caa84ff992
Matthew G. Knepley <knepley@gmail.com>
2014-09-04 16:03:54 -0500

    Configure: No longer use __init__.py to store the list of packages, read them from the filesystem

--- config/BuildSystem/config/packages/__init__.py
+++ config/BuildSystem/config/packages/__init__.py
@@ -1,1 +1,1 @@
-all = ['make','fblaslapack','f2cblaslapack','BlasLapack','exodusii', 'scientificpython', 'fiat', 'MOAB', 'MPI', 'netcdf', 'PETSc','boost', 'cusp', 'thrust', 'hdf5', 'netcdf-cxx', 'cgns']
+all = []
