023ee66dd61aab9a75796ef77af82dc44a56fcaa
Barry Smith <bsmith@mcs.anl.gov>
2014-09-08 19:45:02 -0500

    updated fftw and hypre to use GNUPackage changed mkl_parisodo.py to not not need PETScPackage

--- config/PETSc/packages/fftw.py
+++ config/PETSc/packages/fftw.py
@@ -1,8 +1,8 @@
-import PETSc.package
+import config.package
 
-class Configure(PETSc.package.NewPackage):
+class Configure(config.package.GNUPackage):
   def __init__(self, framework):
-    PETSc.package.NewPackage.__init__(self, framework)
+    config.package.GNUPackage.__init__(self, framework)
     # host locally as fftw.org url can expire after new release.
     self.download  = ['http://www.fftw.org/fftw-3.3.3.tar.gz',
                       'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/fftw-3.3.3.tar.gz']
@@ -13,60 +13,17 @@ class Configure(PETSc.package.NewPackage):
     return
 
   def setupDependencies(self, framework):
-    PETSc.package.NewPackage.setupDependencies(self, framework)
-    self.mpi             = framework.require('config.packages.MPI',self)
+    config.package.GNUPackage.setupDependencies(self, framework)
+    self.mpi  = framework.require('config.packages.MPI',self)
     self.deps = [self.mpi]
     return
 
-  def Install(self):
-    import os
-
-    args = ['--prefix='+self.installDir]
-    args.append('--libdir='+os.path.join(self.installDir,self.libdir))
+  def formGNUConfigureArgs(self):
+    args = config.package.GNUPackage.formGNUConfigureArgs(self)
     self.framework.pushLanguage('C')
-    ccompiler=self.framework.getCompiler()
-    args.append('CC="'+self.framework.getCompiler()+'"')
     args.append('MPICC="'+self.framework.getCompiler()+'"')
-    args.append('CFLAGS="'+self.framework.getCompilerFlags()+'"')
     self.framework.popLanguage()
-    if hasattr(self.compilers, 'CXX'):
-      self.framework.pushLanguage('Cxx')
-      args.append('CXX="'+self.framework.getCompiler()+'"')
-      args.append('CXXFLAGS="'+self.framework.getCompilerFlags()+'"')
-      self.framework.popLanguage()
-     # else error?
-    if hasattr(self.compilers, 'FC'):
-      self.framework.pushLanguage('FC')
-      args.append('F77="'+self.framework.getCompiler()+'"')
-      args.append('FFLAGS="'+self.framework.getCompilerFlags()+'"')
-      self.framework.popLanguage()
-     #else error?
-
-    # MPI args need fixing
     args.append('--enable-mpi')
     if self.mpi.lib:
       args.append('LIBS="'+self.libraries.toStringNoDupes(self.mpi.lib)+'"')
-
-    # Also build the single precision library
-    #   in order to do this, I will have to build two separate versions with different flags (thank you crap GNU buildsystem)
-    # args.append('--enable-single')
-
-    args = ' '.join(args)
-    fd = file(os.path.join(self.packageDir,'fftw'), 'w')
-    fd.write(args)
-    fd.close()
-
-    if self.installNeeded('fftw'):
-      try:
-        self.logPrintBox('Configuring FFTW; this may take several minutes')
-        output1,err1,ret1  = PETSc.package.NewPackage.executeShellCommand('cd '+self.packageDir+' && ./configure '+args, timeout=900, log = self.framework.log)
-      except RuntimeError, e:
-        raise RuntimeError('Error running configure on FFTW: '+str(e))
-      try:
-        self.logPrintBox('Compiling and install FFTW; this may take several minutes')
-        self.installDirProvider.printSudoPasswordMessage()
-        output2,err2,ret2  = PETSc.package.NewPackage.executeShellCommand('cd '+self.packageDir+' && make && '+self.installSudo+'make install', timeout=2500, log = self.framework.log)
-      except RuntimeError, e:
-        raise RuntimeError('Error running make on FFTW: '+str(e))
-      self.postInstall(output1+err1+output2+err2,'fftw')
-    return self.installDir
+    return args
