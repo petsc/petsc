aa5d86ddfae0047417d389bb2e0735a42e08182d
Barry Smith <bsmith@mcs.anl.gov>
2014-09-09 12:28:36 -0500

    remove PETSc.package.NewPackage

--- config/PETSc/packages/threadcomm.py
+++ config/PETSc/packages/threadcomm.py
@@ -1,16 +1,16 @@
-import PETSc.package
+import config.package
 import os
 
 # This is a temporary file for defining the flag PETSC_THREADCOMM_ACTIVE.
 # It should be deleted when the flag is removed from the code.
 
-class Configure(PETSc.package.NewPackage):
+class Configure(config.package.Package):
   def __init__(self, framework):
-    PETSc.package.NewPackage.__init__(self, framework)
+    config.package.Package.__init__(self, framework)
     return
 
   def setupDependencies(self, framework):
-    PETSc.package.NewPackage.setupDependencies(self, framework)
+    config.package.Package.setupDependencies(self, framework)
     self.deps    = []
     return
 
