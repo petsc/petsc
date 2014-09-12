1f738947e7257e585b983ee84167bb4b3b3a368e
Dominic Meiser <dmeiser@txcorp.com>
2014-09-12 15:49:09 -0600

    Use cuda device architecture sm_20 by default.
    This is needed because nvcc in cuda 6.5 generates a deprecation warning
    with older compute architectures which makes the nvcc tests fail.

--- config/BuildSystem/config/packages/cuda.py
+++ config/BuildSystem/config/packages/cuda.py
@@ -33,7 +33,7 @@ class Configure(config.package.Package):
   def setupHelp(self, help):
     import nargs
     config.package.Package.setupHelp(self, help)
-    help.addArgument('CUDA', '-with-cuda-arch=<arch>', nargs.Arg(None, None, 'Target architecture for nvcc, e.g. sm_13'))
+    help.addArgument('CUDA', '-with-cuda-arch=<arch>', nargs.Arg(None, None, 'Target architecture for nvcc, e.g. sm_20'))
     return
 
   def setupDependencies(self, framework):
@@ -99,15 +99,13 @@ class Configure(config.package.Package):
 #Not setting -arch if with-cuda-arch is not specified uses nvcc default architecture
       if 'with-cuda-arch' in self.framework.argDB:
         if not self.framework.argDB['with-cuda-arch'] in ['compute_10', 'compute_11', 'compute_12', 'compute_13', 'compute_20', 'compute_21', 'compute_30', 'compute_35', 'compute_50', 'sm_10', 'sm_11', 'sm_12', 'sm_13', 'sm_20', 'sm_21', 'sm_30', 'sm_35', 'sm_50']:
-          raise RuntimeError('CUDA Error: specified CUDA architecture invalid.  Example of valid architecture: \'-with-cuda-arch=sm_13\'')
+          raise RuntimeError('CUDA Error: specified CUDA architecture invalid.  Example of valid architecture: \'-with-cuda-arch=sm_20\'')
         else:
           self.cudaArch = '-arch='+ self.framework.argDB['with-cuda-arch']
-      elif self.scalartypes.precision == 'double':
-        #default to sm_13 for double precision
-        self.cudaArch = '-arch=sm_13'
-      elif self.scalartypes.precision == 'single':
-        #default to sm_10 for single precision
-        self.cudaArch = '-arch=sm_10'
+      else :
+        # default to sm_20 because cuda 6.5 emits deprecation warning for
+        # earlier architectures
+        self.cudaArch = '-arch=sm_20'
       if self.cudaArch:
         self.setCompilers.addCompilerFlag(self.cudaArch)
       self.setCompilers.popLanguage()
