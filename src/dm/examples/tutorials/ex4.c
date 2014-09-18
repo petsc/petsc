5b547c6678e6e8eda0c0def7cd35246856ed61cb
Barry Smith <bsmith@mcs.anl.gov>
2014-09-18 13:12:32 -0500

    VecDestroy() not called in an example
    Reported-by: Torquil Mcdonald Sorensen <torquil@gmail.com>

--- src/dm/examples/tutorials/ex4.c
+++ src/dm/examples/tutorials/ex4.c
@@ -58,6 +58,7 @@ int main(int argc,char **argv)
      Free work space.  All PETSc objects should be destroyed when they
      are no longer needed.
   */
+  ierr = VecDestroy(&u);CHKERRQ(ierr);
   ierr = PFDestroy(&pf);CHKERRQ(ierr);
   ierr = DMDestroy(&da);CHKERRQ(ierr);
   ierr = PetscFinalize();
