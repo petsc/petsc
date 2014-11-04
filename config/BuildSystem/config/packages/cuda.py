9659392f62abb0111e38d8418431274ef4f70bda
Satish Balay <balay@mcs.anl.gov>
2014-11-04 07:43:02 -0600

    cuda: fix version (computation)

--- config/BuildSystem/config/packages/cuda.py
+++ config/BuildSystem/config/packages/cuda.py
@@ -12,7 +12,7 @@ class Configure(config.package.Package):
     self.cxx              = 0
     self.cudaArch      = ''
     self.CUDAVersion   = '4200' # Minimal cuda version is 4.2
-    self.CUDAVersionStr = str(int(self.CUDAVersion)/1000) + '.' + str(int(self.CUDAVersion)%100)
+    self.CUDAVersionStr = str(int(self.CUDAVersion)/1000) + '.' + str(int(self.CUDAVersion)/100%10)
     return
 
   def __str__(self):
