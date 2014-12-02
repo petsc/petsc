a5ca81901c1a99d2bd95e20d2ff32332c04ca1e8
Barry Smith <bsmith@mcs.anl.gov>
2014-12-02 13:25:27 -0600

    Fix for docs
    Reported-by: Michael Welland <mwelland@anl.gov>

--- src/ksp/pc/impls/gamg/gamg.c
+++ src/ksp/pc/impls/gamg/gamg.c
@@ -1378,7 +1378,7 @@ PetscErrorCode PCSetFromOptions_GAMG(PC pc)
 
    Options Database Keys:
    Multigrid options(inherited)
-+  -pc_mg_cycles <1>: 1 for V cycle, 2 for W-cycle (PCMGSetCycleType)
++  -pc_mg_cycles <v>: v or w (PCMGSetCycleType)
 .  -pc_mg_smoothup <1>: Number of post-smoothing steps (PCMGSetNumberSmoothUp)
 .  -pc_mg_smoothdown <1>: Number of pre-smoothing steps (PCMGSetNumberSmoothDown)
 -  -pc_mg_type <multiplicative>: (one of) additive multiplicative full kascade
