13230571db985326fdf4059f340da483449c97a1
Michael Lange <michael.lange@imperial.ac.uk>
2014-12-02 12:23:48 +0000

    Sys: Add header entry for PetscLogEventGetPerfInfo

--- include/petsclog.h
+++ include/petsclog.h
@@ -257,6 +257,7 @@ PETSC_EXTERN PetscErrorCode PetscLogEventSetActiveAll(PetscLogEvent, PetscBool )
 PETSC_EXTERN PetscErrorCode PetscLogEventActivateClass(PetscClassId);
 PETSC_EXTERN PetscErrorCode PetscLogEventDeactivateClass(PetscClassId);
 PETSC_EXTERN PetscErrorCode PetscLogEventGetId(const char[],PetscLogEvent*);
+PETSC_EXTERN PetscErrorCode PetscLogEventGetPerfInfo(int, PetscLogEvent, PetscEventPerfInfo *);
 
 /* Global counters */
 PETSC_EXTERN PetscLogDouble petsc_irecv_ct;
