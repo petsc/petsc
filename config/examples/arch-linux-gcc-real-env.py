f08ee00aad49e3323ac94ac24ecf6a5c140bc79a
Jason Sarich <sarich@mcs.anl.gov>
2014-12-05 16:11:32 -0600

    make real commit this time

--- config/examples/arch-linux-gcc-real-env.py
+++ config/examples/arch-linux-gcc-real-env.py
@@ -3,21 +3,18 @@ if __name__ == '__main__':
   import sys
   import os
   os.environ['CC'] = 'gcc'
-  os.environ['CFLAGS']='-I/home/sarich'
-  os.environ['FC']='gfortran'
-  os.environ['F77FLAGS']='-g'
-  os.environ['F90FLAGS']='-g'
+  #os.environ['FC'] = 'gfortran'
 
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
-    '--download-fblaslapack=1',
-    '--with-cc=gcc',
+    '--with-mpi=0',
     '--with-clanguage=c',
-    '--with-shared-libraries=1',
-    '--with-python=1',
-    '--PETSC_ARCH=arch-linux-gcc-real-env',
+    '--enable-shared',
+    '--enable-fortran=0',
+    '--with-environment-variables',
+    '--PETSC_ARCH=arch-linux-gcc-real-env'
     ]
-#    '--with-environment-variables',
+
 
   configure.petsc_configure(configure_options)
