523f37fc9117dd5fa1aa02add422425c9dfa13a5
Matthew G. Knepley <knepley@gmail.com>
2014-12-23 12:46:50 -0600

    PetscFV: Moved struct defintions to type header

--- include/petscfvtypes.h
+++ include/petscfvtypes.h
@@ -23,4 +23,16 @@ typedef struct _p_PetscLimiter *PetscLimiter;
 S*/
 typedef struct _p_PetscFV *PetscFV;
 
+/* Assuming dim <= 3 */
+typedef struct {
+  PetscReal   normal[3];   /* Area-scaled normals */
+  PetscReal   centroid[3]; /* Location of centroid (quadrature point) */
+  PetscScalar grad[2][3];  /* Face contribution to gradient in left and right cell */
+} PetscFVFaceGeom;
+
+typedef struct {
+  PetscReal centroid[3];
+  PetscReal volume;
+} PetscFVCellGeom;
+
 #endif
