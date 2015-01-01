6c17a5edb14b105485e24158fe37df094ec46ae0
Barry Smith <bsmith@mcs.anl.gov>
2015-01-01 14:37:54 -0600

    added test for PETSC_VIEWER_ASCII_MATRIXMARKET minor formating of code/comments

---
+++ src/mat/examples/tests/ex173.c
@@ -0,0 +1,38 @@
+
+static char help[] = "Test MatrixMarket outputing.\n\n";
+
+
+/*
+  Include "petscmat.h" so that we can use matrices.
+  automatically includes:
+     petscsys.h    - base PETSc routines   petscvec.h    - vectors
+     petscmat.h    - matrices
+     petscis.h     - index sets            petscviewer.h - viewers
+*/
+#include <petscmat.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args)
+{
+  Mat            A;
+  PetscViewer    fd;                        /* viewer */
+  char           file[PETSC_MAX_PATH_LEN];  /* input file name */
+  PetscErrorCode ierr;
+  PetscBool      flg;
+ 
+  PetscInitialize(&argc,&args,(char*)0,help);
+
+  ierr = PetscOptionsGetString(NULL,"-f0",file,PETSC_MAX_PATH_LEN,&flg);CHKERRQ(ierr);
+  if (!flg) SETERRQ(PETSC_COMM_WORLD,1,"Must indicate binary file with the -f0 option");
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,file,FILE_MODE_READ,&fd);CHKERRQ(ierr);
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatLoad(A,fd);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(&fd);CHKERRQ(ierr);
+
+  ierr = PetscViewerSetFormat(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_ASCII_MATRIXMARKET);CHKERRQ(ierr);
+  ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = PetscFinalize();
+  return 0;
+}
