d0685a901f92cfc903303c612e6f3da9cb048f0e
Jed Brown <jed@jedbrown.org>
2015-02-05 16:05:05 -0700

    TS: man page spelling and links

--- src/ts/impls/rosw/rosw.c
+++ src/ts/impls/rosw/rosw.c
@@ -1652,6 +1652,8 @@ PetscErrorCode  TSRosWSetRecomputeJacobian_RosW(TS ts,PetscBool flg)
   Notes:
   This method currently only works with autonomous ODE and DAE.
 
+  Consider trying TSARKIMEX if the stiff part is strongly nonlinear.
+
   Developer notes:
   Rosenbrock-W methods are typically specified for autonomous ODE
 
@@ -1698,7 +1700,7 @@ $  g(u_0 + sum_j a_ij y_j + y_i, ydot_i) = 0
 
   Level: beginner
 
-.seealso:  TSCreate(), TS, TSSetType(), TSRosWSetType(), TSRosWRegister(), TSROSW2M, TSROSW2P, TSROSWRA3PW, TSROSWRA34PW2, TSROSWRODAS3,
+.seealso:  TSCreate(), TS, TSSetType(), TSRosWSetType(), TSRosWRegister(), TSROSWTHETA1, TSROSWTHETA2, TSROSW2M, TSROSW2P, TSROSWRA3PW, TSROSWRA34PW2, TSROSWRODAS3,
            TSROSWSANDU3, TSROSWASSP3P3S1C, TSROSWLASSP3P4S2C, TSROSWLLSSP3P4S2C, TSROSWGRK4T, TSROSWSHAMP4, TSROSWVELDD4, TSROSW4L
 M*/
 #undef __FUNCT__
