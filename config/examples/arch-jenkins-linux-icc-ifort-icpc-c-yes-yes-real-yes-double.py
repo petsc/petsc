05e9f9937bc2b1964680dd063e017be1e82d28ef
Jason Sarich <sarich@mcs.anl.gov>
2015-02-18 15:37:32 -0600

    fix jenkins intel example arch

--- config/examples/arch-jenkins-linux-icc-ifort-icpc-c-yes-yes-real-yes-double.py
+++ config/examples/arch-jenkins-linux-icc-ifort-icpc-c-yes-yes-real-yes-double.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
-    '--with-cc=/home/petsc/soft/linux-Ubuntu_12.04-x86/mpich-3.1.3-intel/bin/mpicc',
-    '--with-fc=/home/petsc/soft/linux-Ubuntu_12.04-x86/mpich-3.1.3-intel/bin/mpif90',
-    '--with-cxx=/home/petsc/soft/linux-Ubuntu_12.04-x86/mpich-3.1.3-intel/bin/mpicxx',
+    '--with-cc=/home/petsc/soft/linux-Ubuntu_12.04-x86_64/mpich-3.1.3-intel/bin/mpicc',
+    '--with-fc=/home/petsc/soft/linux-Ubuntu_12.04-x86_64/mpich-3.1.3-intel/bin/mpif90',
+    '--with-cxx=/home/petsc/soft/linux-Ubuntu_12.04-x86_64/mpich-3.1.3-intel/bin/mpicxx',
     '--with-blas-lapack-dir=/soft/com/packages/intel/13/update5/mkl',
     '--with-clanguage=c',
     '--with-shared-libraries=yes',
