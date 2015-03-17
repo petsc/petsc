7fe365778230fe706426b27d82c3bfeb85f76fa5
Barry Smith <bsmith@mcs.anl.gov>
2015-03-17 14:01:51 -0500

    update petsc4py to latest version since previous version did not work with new PETSc install directory structure MatConvert_SeqAIJ_SeqBAIJ does not work with block size > 1 so error out instead of confusing user with incorrect matrix

--- config/BuildSystem/config/packages/petsc4py.py
+++ config/BuildSystem/config/packages/petsc4py.py
@@ -4,7 +4,7 @@ class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
     self.giturls           = ['git@bitbucket.org:petsc/petsc4py.git']
-    self.gitcommit         = 'cde2915'
+    self.gitcommit         = 'master'
     self.download          = ['http://ftp.mcs.anl.gov/pub/petsc/externalpackages/petsc4py-418f86b7e1b8.tar.gz']
     self.functions         = []
     self.includes          = []
