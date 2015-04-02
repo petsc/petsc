a02bda8e9177763e3a7baa8b1d4a3ca70a8d6433
Barry Smith <bsmith@mcs.anl.gov>
2015-04-02 17:40:17 -0500

    Resetting of inode information for SeqAIJ matrices did not always take place when the matrix structured changed. Updated code to use nonzerostatus of matrix to track when inode information needed to be updated.

--- src/mat/examples/tests/ex182.c
+++ src/mat/examples/tests/ex182.c
@@ -7,11 +7,16 @@ static char help[] = "Tests using MatShift() to create a constant diagonal matri
 #define __FUNCT__ "main"
 int main(int argc,char **argv)
 {
-  Mat            A;
+  Mat            A,F;
+  MatFactorInfo  info;
   PetscErrorCode ierr;
   PetscInt       m = 10;
+  IS             perm;
+  PetscMPIInt    size;
+  PetscBool      issbaij;
 
   PetscInitialize(&argc,&argv,(char*) 0,help);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
 
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
   ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,m,m);CHKERRQ(ierr);
@@ -21,6 +26,17 @@ int main(int argc,char **argv)
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
   ierr = MatShift(A,1.0);CHKERRQ(ierr);
+
+  ierr = PetscObjectTypeCompare((PetscObject)A,MATSEQSBAIJ,&issbaij);CHKERRQ(ierr);
+  if (size == 1 && !issbaij) {
+    ierr = MatGetFactor(A,MATSOLVERPETSC,MAT_FACTOR_LU,&F);CHKERRQ(ierr);
+    ierr = MatFactorInfoInitialize(&info);CHKERRQ(ierr);
+    ierr = ISCreateStride(PETSC_COMM_SELF,m,0,1,&perm);CHKERRQ(ierr);
+    ierr = MatLUFactorSymbolic(F,A,perm,perm,&info);CHKERRQ(ierr);
+    ierr = MatLUFactorNumeric(F,A,&info);CHKERRQ(ierr);
+    ierr = MatDestroy(&F);CHKERRQ(ierr);
+    ierr = ISDestroy(&perm);CHKERRQ(ierr);
+  }
   ierr = MatDestroy(&A);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return 0;
