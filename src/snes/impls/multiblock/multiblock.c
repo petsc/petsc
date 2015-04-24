c579b30025f7481884435f5d6baf5e954044f830
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2015-04-24 12:43:23 +0200

    If the user sets bounds and calls a solver that does not support them, throw an error.

--- src/snes/impls/multiblock/multiblock.c
+++ src/snes/impls/multiblock/multiblock.c
@@ -512,6 +512,11 @@ PetscErrorCode SNESSolve_Multiblock(SNES snes)
   PetscErrorCode  ierr;
 
   PetscFunctionBegin;
+
+  if (snes->xl || snes->xu || snes->ops->computevariablebounds) {
+    SETERRQ1(PetscObjectComm((PetscObject)snes),PETSC_ERR_ARG_WRONGSTATE, "SNES solver %s does not support bounds", ((PetscObject)snes)->type_name);
+  }
+
   snes->reason = SNES_CONVERGED_ITERATING;
 
   maxits = snes->max_its;        /* maximum number of iterations */
--- src/snes/impls/tr/tr.c
+++ src/snes/impls/tr/tr.c
@@ -97,6 +97,11 @@ static PetscErrorCode SNESSolve_NEWTONTR(SNES snes)
   PetscBool           domainerror;
 
   PetscFunctionBegin;
+
+  if (snes->xl || snes->xu || snes->ops->computevariablebounds) {
+    SETERRQ1(PetscObjectComm((PetscObject)snes),PETSC_ERR_ARG_WRONGSTATE, "SNES solver %s does not support bounds", ((PetscObject)snes)->type_name);
+  }
+
   maxits = snes->max_its;               /* maximum number of iterations */
   X      = snes->vec_sol;               /* solution vector */
   F      = snes->vec_func;              /* residual vector */
