eadd315233345ce232ac2100f68176a5c6b02581
Fande Kong <fdkong.jd@gmail.com>
2015-05-04 16:47:52 -0600

    created an example to test hierarchical partitioning

---
+++ src/mat/examples/tests/ex82.c
@@ -0,0 +1,84 @@
+
+static char help[] = "Partition tiny grid using hierarchical partitioning.\n\n";
+
+/*T
+   Concepts: partitioning
+   Processors: 4
+T*/
+
+/*
+  Include "petscmat.h" so that we can use matrices.  Note that this file
+  automatically includes:
+     petscsys.h       - base PETSc routines   petscvec.h - vectors
+     petscmat.h - matrices
+     petscis.h     - index sets
+     petscviewer.h - viewers
+*/
+#include <petscmat.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args)
+{
+  Mat             A;
+  PetscErrorCode  ierr;
+  PetscMPIInt     rank,size;
+  PetscInt        *ia,*ja;
+  MatPartitioning part;
+  IS              is,isn;
+
+  PetscInitialize(&argc,&args,(char*)0,help);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+  if (size != 4) SETERRQ(PETSC_COMM_WORLD,1,"Must run with 4 processors");
+  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
+
+  ierr = PetscMalloc1(5,&ia);CHKERRQ(ierr);
+  ierr = PetscMalloc1(16,&ja);CHKERRQ(ierr);
+  if (!rank) {
+    ja[0] = 1; ja[1] = 4; ja[2] = 0; ja[3] = 2; ja[4] = 5; ja[5] = 1; ja[6] = 3; ja[7] = 6;
+    ja[8] = 2; ja[9] = 7;
+    ia[0] = 0; ia[1] = 2; ia[2] = 5; ia[3] = 8; ia[4] = 10;
+  } else if (rank == 1) {
+    ja[0] = 0; ja[1] = 5; ja[2] = 8; ja[3] = 1; ja[4] = 4; ja[5] = 6; ja[6] = 9; ja[7] = 2;
+    ja[8] = 5; ja[9] = 7; ja[10] = 10; ja[11] = 3; ja[12] = 6; ja[13] = 11;
+    ia[0] = 0; ia[1] = 3; ia[2] = 7; ia[3] = 11; ia[4] = 14;
+  } else if (rank == 2) {
+    ja[0] = 4; ja[1] = 9; ja[2] = 12; ja[3] = 5; ja[4] = 8; ja[5] = 10; ja[6] = 13; ja[7] = 6;
+    ja[8] = 9; ja[9] = 11; ja[10] = 14; ja[11] = 7; ja[12] = 10; ja[13] = 15;
+    ia[0] = 0; ia[1] = 3; ia[2] = 7; ia[3] = 11; ia[4] = 14;
+  } else {
+    ja[0] = 8; ja[1] = 13; ja[2] = 9; ja[3] = 12; ja[4] = 14; ja[5] = 10; ja[6] = 13; ja[7] = 15;
+    ja[8] = 11; ja[9] = 14;
+    ia[0] = 0; ia[1] = 2; ia[2] = 5; ia[3] = 8; ia[4] = 10;
+  }
+
+  ierr = MatCreateMPIAdj(PETSC_COMM_WORLD,4,16,ia,ja,NULL,&A);CHKERRQ(ierr);
+  ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+  /*
+       Partition the graph of the matrix
+  */
+  ierr = MatPartitioningCreate(PETSC_COMM_WORLD,&part);CHKERRQ(ierr);
+  ierr = MatPartitioningSetAdjacency(part,A);CHKERRQ(ierr);
+  ierr = MatPartitioningSetFromOptions(part);CHKERRQ(ierr);
+  /* get new processor owner number of each vertex */
+  ierr = MatPartitioningApply(part,&is);CHKERRQ(ierr);
+  /* get new global number of each old global number */
+  ierr = ISPartitioningToNumbering(is,&isn);CHKERRQ(ierr);
+  ierr = ISView(isn,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = ISDestroy(&is);CHKERRQ(ierr);
+
+  ierr = ISDestroy(&isn);CHKERRQ(ierr);
+  ierr = MatPartitioningDestroy(&part);CHKERRQ(ierr);
+
+  /*
+       Free work space.  All PETSc objects should be destroyed when they
+       are no longer needed.
+  */
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+
+
+  ierr = PetscFinalize();
+  return 0;
+}
+
