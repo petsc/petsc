6bda7c79e171cdfb4345d4142fe83df622e27509
Matthew G. Knepley <knepley@gmail.com>
2015-05-28 08:12:15 -0500

    Configure: Added Pragmatic package

---
+++ config/BuildSystem/config/packages/pragmatic.py
@@ -0,0 +1,38 @@
+import config.package
+
+class Configure(config.package.CMakePackage):
+  def __init__(self, framework):
+    import os
+    config.package.CMakePackage.__init__(self, framework)
+    self.download          = ['GitOnly']
+    self.gitcommit         = 'f7b05b034ba14a869d08033c0ae361ba7d3bdb55'
+    self.giturls           = ['https://github.com/pefarrell/pragmatic.git']
+    self.functions         = ['pragmatic_2d_init']
+    self.includes          = ['pragmatic.h']
+    self.liblist           = [['libpragmatic.a']]
+    self.needsMath         = 1
+    self.includedir        = os.path.join('include', 'pragmatic')
+    return
+
+  def setupDependencies(self, framework):
+    config.package.CMakePackage.setupDependencies(self, framework)
+    self.compilerFlags   = framework.require('config.compilerFlags', self)
+    self.sharedLibraries = framework.require('PETSc.options.sharedLibraries', self)
+    self.scalartypes     = framework.require('PETSc.options.scalarTypes',self)
+    self.indexTypes      = framework.require('PETSc.options.indexTypes', self)
+    self.metis           = framework.require('config.packages.metis', self)
+    return
+
+  def formCMakeConfigureArgs(self):
+    if not self.cmake.found:
+      raise RuntimeError('CMake > 2.5 is needed to build Pragmatic')
+
+    args = config.package.CMakePackage.formCMakeConfigureArgs(self)
+    args.append('-DMETIS_DIR='+self.metis.getInstallDir())
+    if self.indexTypes.integerSize == 64:
+      raise RuntimeError('Pragmatic cannot be built with 64-bit integers')
+    if self.scalartypes.precision == 'single':
+      raise RuntimeError('Pragmatic cannot be built with single precision')
+    elif self.scalartypes.precision == 'quad':
+      raise RuntimeError('Pragmatic cannot be built with quad precision')
+    return args
