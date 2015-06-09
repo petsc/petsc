7ad1098541f8a490cd032a2f4153a5aee266e4b3
Matthew G. Knepley <knepley@gmail.com>
2015-06-09 06:49:56 -0400

    Configure: Do not log directly to the framework

--- config/PETSc/options/scalarTypes.py
+++ config/PETSc/options/scalarTypes.py
@@ -46,7 +46,7 @@ class Configure(config.base.Configure):
     elif not self.scalartype == 'real':
       raise RuntimeError('--with-scalar-type must be real or complex')
     self.addDefine('USE_SCALAR_'+self.scalartype.upper(), '1')
-    self.framework.logPrint('Scalar type is '+str(self.scalartype))
+    self.logPrint('Scalar type is '+str(self.scalartype))
     # On apple isinf() and isnan() do not work when <complex> is included
     self.pushLanguage(self.languages.clanguage)
     if self.scalartype == 'complex' and self.languages.clanguage == 'Cxx':
@@ -91,7 +91,7 @@ class Configure(config.base.Configure):
         raise RuntimeError('quadmath support not found. --with-precision=__float128 works with gcc-4.6 and newer compilers.')
     else:
       raise RuntimeError('--with-precision must be single, double,__float128')
-    self.framework.logPrint('Precision is '+str(self.precision))
+    self.logPrint('Precision is '+str(self.precision))
     return
 
   def configure(self):
