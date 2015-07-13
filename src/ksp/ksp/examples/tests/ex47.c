53935eaf8623c72f022d22de1c65b96f37f95d4e
Barry Smith <bsmith@mcs.anl.gov>
2015-07-13 14:31:39 -0500

    Need to dely attachment of null spaces from IS to matrices inside fieldsplit until after the matrices are available
    Reported-by: Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>

---
+++ src/ksp/ksp/examples/tests/ex47.c
@@ -0,0 +1,53 @@
+/*
+    Tests attaching null space to IS for fieldsplit preconditioner
+*/
+#include <petsc.h>
+
+int main(int argc, char **argv)
+{
+  PetscErrorCode ierr;
+  Mat            A;
+  KSP            ksp;
+  PC             pc;
+  IS             zero, one;
+  MatNullSpace   nullsp;
+  Vec            x, b;
+  MPI_Comm       comm;
+
+  PetscInitialize(&argc, &argv, NULL, NULL);
+
+  comm = PETSC_COMM_WORLD;
+
+  ierr = MatCreate(comm, &A);CHKERRQ(ierr);
+  ierr = MatSetSizes(A, 4, 4, PETSC_DECIDE, PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = MatSetUp(A);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatCreateVecs(A, &x, &b);CHKERRQ(ierr);
+  ierr = VecSet(x, 2.0);CHKERRQ(ierr);
+  ierr = VecSet(b, 12.0);CHKERRQ(ierr);
+  ierr = MatDiagonalSet(A, x, INSERT_VALUES);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = ISCreateStride(comm, 2, 0, 1, &zero);CHKERRQ(ierr);
+  ierr = ISCreateStride(comm, 2, 2, 1, &one);CHKERRQ(ierr);
+  ierr = MatNullSpaceCreate(comm, PETSC_TRUE, 0, NULL, &nullsp);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)zero, "nullspace",(PetscObject)nullsp);CHKERRQ(ierr);
+  ierr = KSPCreate(comm, &ksp);CHKERRQ(ierr);
+  ierr = KSPSetOperators(ksp, A, A);CHKERRQ(ierr);
+  ierr = KSPSetUp(ksp);CHKERRQ(ierr);
+  ierr = KSPGetPC(ksp, &pc);CHKERRQ(ierr);
+  ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
+  ierr = PCFieldSplitSetIS(pc, "0", zero);
+  ierr = PCFieldSplitSetIS(pc, "1", one);
+  ierr = KSPSolve(ksp, b, x);CHKERRQ(ierr);
+  ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
+  ierr = MatNullSpaceDestroy(&nullsp);CHKERRQ(ierr);
+  ierr = ISDestroy(&zero);CHKERRQ(ierr);
+  ierr = ISDestroy(&one);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  ierr = VecDestroy(&b);CHKERRQ(ierr);
+
+  PetscFinalize();
+  return 0;
+}
