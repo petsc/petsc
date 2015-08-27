383e01f8df58866ef5b3383f280dc3ce2f891851
Barry Smith <bsmith@mcs.anl.gov>
2015-08-27 16:57:20 -0500

    add specific link test for concurrencykit

--- config/BuildSystem/config/packages/concurrencykit.py
+++ config/BuildSystem/config/packages/concurrencykit.py
@@ -17,3 +17,22 @@ class Configure(config.package.GNUPackage):
     rejects = ['--disable-cxx','--disable-fortran', '--disable-fc','--disable-f77','--disable-f90']
     self.logPrint('MPICH is rejecting configure arguments '+str(rejects))
     return [arg for arg in args if not arg in rejects]
+
+  def checkForCorrectness(self):
+    include = '#include <ck_spinlock.h>'
+    body    = 'ck_spinlock_t ck_spinlock; ck_spinlock_init(&ck_spinlock);ck_spinlock_lock(&ck_spinlock);ck_spinlock_unlock(&ck_spinlock);'
+    oldFlags = self.compilers.CPPFLAGS
+    oldLibs  = self.compilers.LIBS
+    self.compilers.CPPFLAGS += ' '+self.headers.toString(self.include)
+    self.compilers.LIBS = self.libraries.toString(self.lib)+' '+self.compilers.LIBS
+    if not self.checkLink(include, body):
+      raise RuntimeError('Concurrencykit cannot be used')
+    self.compilers.CPPFLAGS = oldFlags
+    self.compilers.LIBS = oldLibs
+
+  def configureLibrary(self):
+    '''Calls the regular package configureLibrary and then does an additional test needed'''
+    if 'with-'+self.package+'-shared' in self.argDB:
+      self.argDB['with-'+self.package] = 1
+    config.package.Package.configureLibrary(self)
+    self.executeTest(self.checkForCorrectness)
