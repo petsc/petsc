404c08d0a9d62fefba0a0a3b4b8ead5274955a1b
Matthew G. Knepley <knepley@gmail.com>
2015-08-27 09:41:21 -0500

    DMLabel+Doc: Adding function documentation

--- src/dm/impls/plex/plexlabel.c
+++ src/dm/impls/plex/plexlabel.c
@@ -3,6 +3,19 @@
 
 #undef __FUNCT__
 #define __FUNCT__ "DMLabelCreate"
+/*@C
+  DMLabelCreate - Create a DMLabel object, which is a multimap
+
+  Input parameter:
+. name - The label name
+
+  Output parameter:
+. label - The DMLabel
+
+  Level: beginner
+
+.seealso: DMLabelDestroy()
+@*/
 PetscErrorCode DMLabelCreate(const char name[], DMLabel *label)
 {
   PetscErrorCode ierr;
@@ -26,6 +39,20 @@ PetscErrorCode DMLabelCreate(const char name[], DMLabel *label)
 
 #undef __FUNCT__
 #define __FUNCT__ "DMLabelMakeValid_Private"
+/*
+  DMLabelMakeValid_Private - Transfer stratum data from the hash format to the sorted list format
+
+  Input parameter:
++ label - The DMLabel
+- v - The stratum value
+
+  Output parameter:
+. label - The DMLabel with stratum in sorted list format
+
+  Level: developer
+
+.seealso: DMLabelCreate()
+*/
 static PetscErrorCode DMLabelMakeValid_Private(DMLabel label, PetscInt v)
 {
   PetscInt       off;
@@ -58,6 +85,19 @@ static PetscErrorCode DMLabelMakeValid_Private(DMLabel label, PetscInt v)
 
 #undef __FUNCT__
 #define __FUNCT__ "DMLabelMakeAllValid_Private"
+/*
+  DMLabelMakeAllValid_Private - Transfer all strata from the hash format to the sorted list format
+
+  Input parameter:
+. label - The DMLabel
+
+  Output parameter:
+. label - The DMLabel with all strata in sorted list format
+
+  Level: developer
+
+.seealso: DMLabelCreate()
+*/
 static PetscErrorCode DMLabelMakeAllValid_Private(DMLabel label)
 {
   PetscInt       v;
@@ -72,6 +112,20 @@ static PetscErrorCode DMLabelMakeAllValid_Private(DMLabel label)
 
 #undef __FUNCT__
 #define __FUNCT__ "DMLabelMakeInvalid_Private"
+/*
+  DMLabelMakeInvalid_Private - Transfer stratum data from the sorted list format to the hash format
+
+  Input parameter:
++ label - The DMLabel
+- v - The stratum value
+
+  Output parameter:
+. label - The DMLabel with stratum in hash format
+
+  Level: developer
+
+.seealso: DMLabelCreate()
+*/
 static PetscErrorCode DMLabelMakeInvalid_Private(DMLabel label, PetscInt v)
 {
   PETSC_UNUSED PetscHashIIter ret, iter;
@@ -131,6 +185,19 @@ static PetscErrorCode DMLabelAddStratum_Private(DMLabel label, PetscInt value)
 
 #undef __FUNCT__
 #define __FUNCT__ "DMLabelGetName"
+/*@C
+  DMLabelGetName - Return the name of a DMLabel object
+
+  Input parameter:
+. label - The DMLabel
+
+  Output parameter:
+. name - The label name
+
+  Level: beginner
+
+.seealso: DMLabelCreate()
+@*/
 PetscErrorCode DMLabelGetName(DMLabel label, const char **name)
 {
   PetscFunctionBegin;
