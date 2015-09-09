0f2356a4cf824482aba9a9f0d82629e8b42853a7
Barry Smith <bsmith@mcs.anl.gov>
2015-09-08 22:13:03 -0500

    decrease time from 10 to 1 for adj and opt tests so they run faster and tests don't take too long

--- src/ts/examples/tutorials/power_grid/ex3adj.c
+++ src/ts/examples/tutorials/power_grid/ex3adj.c
@@ -325,7 +325,7 @@ int main(int argc,char **argv)
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Set solver options
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  ierr = TSSetDuration(ts,PETSC_DEFAULT,10.0);CHKERRQ(ierr);
+  ierr = TSSetDuration(ts,PETSC_DEFAULT,1.0);CHKERRQ(ierr);
   ierr = TSSetInitialTimeStep(ts,0.0,.005);CHKERRQ(ierr);
   ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
 
