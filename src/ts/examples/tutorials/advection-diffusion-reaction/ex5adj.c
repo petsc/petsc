15457990ec274cd544db716dc65279851856af6e
Hong Zhang <hongzhang@anl.gov>
2015-09-15 15:59:43 -0500

    deleted commented code

--- src/ts/examples/tutorials/advection-diffusion-reaction/ex5adj.c
+++ src/ts/examples/tutorials/advection-diffusion-reaction/ex5adj.c
@@ -155,9 +155,7 @@ int main(int argc,char **argv)
   ierr = TSSetCostGradients(ts,1,lambda,NULL);CHKERRQ(ierr);
 
   ierr = TSAdjointSolve(ts);CHKERRQ(ierr);
-  /*
-  ierr = VecView(lambda[0],PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  */
+
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Free work space.  All PETSc objects should be destroyed when they
      are no longer needed.
