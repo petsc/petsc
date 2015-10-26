3248e36b422a33e476d1b708110ee0872b55b32a
Barry Smith <bsmith@mcs.anl.gov>
2015-10-26 16:09:49 -0500

    turn off absurdly tight tolerances for test example so as to produce similar results in single, double, and quad note this seems to break the single run

--- src/ts/examples/tutorials/power_grid/ex9opt.c
+++ src/ts/examples/tutorials/power_grid/ex9opt.c
@@ -275,7 +275,7 @@ int main(int argc,char **argv)
     ierr = PCSetType(pc,PCNONE);CHKERRQ(ierr);
   }
 
-  ierr = TaoSetTolerances(tao,1e-15,1e-15,1e-15,1e-15,1e-15);
+  /* ierr = TaoSetTolerances(tao,1e-15,1e-15,1e-15,1e-15,1e-15); */
   /* SOLVE THE APPLICATION */
   ierr = TaoSolve(tao); CHKERRQ(ierr);
 
