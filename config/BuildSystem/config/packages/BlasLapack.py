8a200607979bd8d9867f42e992e9ea11b2fd5017
Jose E. Roman <jroman@dsic.upv.es>
2015-12-02 13:31:50 +0100

    configure: add a few checks for missing Lapack subroutines

--- config/BuildSystem/config/packages/BlasLapack.py
+++ config/BuildSystem/config/packages/BlasLapack.py
@@ -443,7 +443,7 @@ class Configure(config.package.Package):
     '''Check for missing LAPACK routines'''
     if self.foundLapack:
       mangleFunc = hasattr(self.compilers, 'FC') and not self.f2c
-    routines = ['trsen','gerfs','gges','tgsen','gesvd','getrf','getrs','geev','gelss','syev','syevx','sygv','sygvx','potrf','potrs','stebz','pttrf','pttrs','stein','orgqr','geqrf','gesv','hseqr','steqr']
+    routines = ['trsen','trtrs','gerfs','gges','tgsen','gesvd','getrf','getrs','geev','gelss','syev','syevx','sygv','sygvx','potrf','potrs','stebz','pttrf','pttrs','stein','orgqr','ormqr','geqrf','gesv','hseqr','steqr','hgeqz']
     self.libraries.saveLog()
     oldLibs = self.compilers.LIBS
     found, missing = self.libraries.checkClassify(self.lapackLibrary, map(self.mangleBlas,routines), otherLibs = self.getOtherLibs(), fortranMangle = mangleFunc)
