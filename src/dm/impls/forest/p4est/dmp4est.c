e0b3996a778bc487aee0b7c9537885a5d53a3210
Toby Isaac <tisaac@uchicago.edu>
2015-12-09 13:48:09 -0600

    DMP4est: fix P4estFaceToPetscFace
    Was not the inverse permutation of PetscFaceToP4estFace before, now it
    is.

--- src/dm/impls/forest/p4est/dmp4est.c
+++ src/dm/impls/forest/p4est/dmp4est.c
@@ -1,7 +1,7 @@
 #include <petscsys.h>
 
 static const PetscInt PetscFaceToP4estFace[4] = {2, 1, 3, 0};
-static const PetscInt P4estFaceToPetscFace[4] = {3, 1, 2, 0};
+static const PetscInt P4estFaceToPetscFace[4] = {3, 1, 0, 2};
 static const PetscInt P4estFaceToPetscOrnt[4] = {-2, 0, 0, -2};
 static const PetscInt PetscVertToP4estVert[4] = {0, 1, 3, 2};
 static const PetscInt P4estVertToPetscVert[4] = {0, 1, 3, 2};
