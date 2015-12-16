fdb79d078046a07e1f565bfaee6885b6b637175c
Barry Smith <bsmith@mcs.anl.gov>
2015-12-16 15:38:04 -0600

    Print out more detailed information on shared memory communicator used for VecScatter
    Funded-by: P-ECP
    Time: 0 hours

--- src/ksp/ksp/examples/tests/benchmarkscatters/ex2.c
+++ src/ksp/ksp/examples/tests/benchmarkscatters/ex2.c
@@ -15,19 +15,23 @@ int main(int argc,char **args)
   PetscErrorCode  ierr;
   PetscCommShared scomm;
   MPI_Comm        comm;
-  PetscMPIInt     lrank,rank;
+  PetscMPIInt     lrank,rank,size,i;
   Vec             x,y;
   VecScatter      vscat;
   IS              is;
-
+  PetscViewer     singleton;
+  
   PetscInitialize(&argc,&args,(char*)0,help);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   ierr = PetscCommDuplicate(PETSC_COMM_WORLD,&comm,NULL);CHKERRQ(ierr);
   ierr = PetscCommSharedGet(comm,&scomm);CHKERRQ(ierr);
   ierr = PetscCommSharedGet(comm,&scomm);CHKERRQ(ierr);
 
-  ierr = PetscCommSharedGlobalToLocal(scomm,1,&lrank);CHKERRQ(ierr);
-  ierr = PetscSynchronizedPrintf(PETSC_COMM_WORLD,"[%d] Global rank %d shared memory comm rank %d\n",rank,1,lrank);CHKERRQ(ierr);
+  for (i=0; i<size; i++) {
+    ierr = PetscCommSharedGlobalToLocal(scomm,i,&lrank);CHKERRQ(ierr);
+    ierr = PetscSynchronizedPrintf(PETSC_COMM_WORLD,"[%d] Global rank %d shared memory comm rank %d\n",rank,i,lrank);CHKERRQ(ierr);
+  }
   ierr = PetscSynchronizedFlush(PETSC_COMM_WORLD,stdout);CHKERRQ(ierr);
   ierr = PetscCommDestroy(&comm);CHKERRQ(ierr);
 
@@ -35,6 +39,8 @@ int main(int argc,char **args)
   ierr = VecSetValue(x,rank,(PetscScalar)rank,INSERT_VALUES);CHKERRQ(ierr);
   ierr = VecAssemblyBegin(x);CHKERRQ(ierr);
   ierr = VecAssemblyEnd(x);CHKERRQ(ierr);
+  ierr = VecView(x,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  
   ierr = VecCreateSeq(PETSC_COMM_SELF,3,&y);CHKERRQ(ierr);
   ierr = ISCreateStride(PETSC_COMM_SELF,3,0,1,&is);CHKERRQ(ierr);
   ierr = ISToGeneral(is);CHKERRQ(ierr);
@@ -42,7 +48,10 @@ int main(int argc,char **args)
   ierr = VecScatterBegin(vscat,x,y,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
   ierr = VecScatterEnd(vscat,x,y,INSERT_VALUES,SCATTER_FORWARD);CHKERRQ(ierr);
   ierr = VecScatterDestroy(&vscat);CHKERRQ(ierr);
-  ierr = VecView(y,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
+  ierr = PetscViewerGetSubViewer(PETSC_VIEWER_STDOUT_WORLD,PETSC_COMM_SELF,&singleton);CHKERRQ(ierr);
+  ierr = VecView(y,singleton);CHKERRQ(ierr);
+  ierr = PetscViewerRestoreSubViewer(PETSC_VIEWER_STDOUT_WORLD,PETSC_COMM_SELF,&singleton);CHKERRQ(ierr);
+  
   ierr = ISDestroy(&is);CHKERRQ(ierr);
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&y);CHKERRQ(ierr);
