aff9420f7476d2ffa14991ef87d264f86287f503
Toby Isaac <tisaac@uchicago.edu>
2015-12-18 13:51:58 -0800

    DMP4est: added serial 2d regression tests to builder.py

--- config/builder.py
+++ config/builder.py
@@ -407,6 +407,9 @@ regressionParameters = {'src/dm/impls/patch/examples/tests/ex1': [{'numProcs': 1
                                                                {'num': 'tet_conv_p1_r3', 'numProcs': 1, 'args': '-run_type full -dim 3 -dm_refine 3 -bc_type dirichlet -interpolate 1 -petscspace_order 1 -dm_view -snes_converged_reason -pc_type lu'},
                                                                {'num': 'tet_conv_p2_r0', 'numProcs': 1, 'args': '-run_type full -dim 3 -dm_refine 0 -bc_type dirichlet -interpolate 1 -petscspace_order 2 -dm_view -snes_converged_reason -pc_type lu'},
                                                                {'num': 'tet_conv_p2_r2', 'numProcs': 1, 'args': '-run_type full -dim 3 -dm_refine 2 -bc_type dirichlet -interpolate 1 -petscspace_order 2 -dm_view -snes_converged_reason -pc_type lu'},
+                                                               # tensor
+                                                               {'num': 'tensor_plex_2d', 'numProcs': 1, 'args': '-run_type test -refinement_limit 0.0 -simplex 0 -bc_type dirichlet -petscspace_order 1 -petscspace_poly_tensor -dm_refine_hierarchy 2'},
+                                                               {'num': 'tensor_p4est_2d', 'numProcs': 1, 'args': '-run_type test -refinement_limit 0.0 -simplex 0 -bc_type dirichlet -petscspace_order 1 -petscspace_poly_tensor -dm_refine_hierarchy 2 -dm_plex_convert_type p4est'},
                                                                ],
                         'src/snes/examples/tutorials/ex33':   [{'numProcs': 1, 'args': '-snes_converged_reason -snes_monitor_short'}],
                         'src/snes/examples/tutorials/ex36':   [# 2D serial P2/P1 tests 0-1
