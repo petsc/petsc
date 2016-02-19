c93913ab74b1bccabefb25b7a3c8ccd8d2ff6f59
Satish Balay <balay@mcs.anl.gov>
2016-02-19 16:42:08 -0600

    TSRK: add TSRKSetType() fortran interface
    Reported-by: Dietzsch Felix <Felix.Dietzsch@vtc.tu-freiberg.de>

---
+++ src/ts/impls/explicit/rk/ftn-custom/zrkf.c
@@ -0,0 +1,18 @@
+#include <petsc/private/fortranimpl.h>
+#include <petscts.h>
+#include <../src/sys/f90-src/f90impl.h>
+
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define tsrksettype_                     TSRKSETTYPE
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define tsrksettype_                     tsrksettype
+#endif
+
+PETSC_EXTERN void PETSC_STDCALL tsrksettype_(TS *ts,CHAR type PETSC_MIXED_LEN(len),PetscErrorCode *ierr PETSC_END_LEN(len))
+{
+  char *t;
+
+  FIXCHAR(type,len,t);
+  *ierr = TSRKSetType(*ts,t);
+  FREECHAR(type,t);
+}
