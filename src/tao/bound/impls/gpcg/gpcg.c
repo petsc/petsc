53b1470a97e3aa15ce631049cebf46620393f1b2
Tianyi Li <tianyi-externe.li@edf.fr>
2016-03-01 16:13:09 +0100

    fix a typo in defining tolerances

--- src/tao/bound/impls/gpcg/gpcg.c
+++ src/tao/bound/impls/gpcg/gpcg.c
@@ -349,10 +349,10 @@ PETSC_EXTERN PetscErrorCode TaoCreate_GPCG(Tao tao)
   if (!tao->max_it_changed) tao->max_it=500;
   if (!tao->max_funcs_changed) tao->max_funcs = 100000;
 #if defined(PETSC_USE_REAL_SINGLE)
-  if (!tao->gatol_changed) tao->grtol=1e-6;
+  if (!tao->gatol_changed) tao->gatol=1e-6;
   if (!tao->grtol_changed) tao->grtol=1e-6;
 #else
-  if (!tao->gatol_changed) tao->grtol=1e-12;
+  if (!tao->gatol_changed) tao->gatol=1e-12;
   if (!tao->grtol_changed) tao->grtol=1e-12;
 #endif
 
