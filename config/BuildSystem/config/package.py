2061b0d76caedae37c24e2ba43de492277d32e11
Matthew G. Knepley <knepley@gmail.com>
2016-03-02 13:39:03 -0600

    Configure: I really hate exceptions - We should rewrite this to eliminate all exceptions in favor of return codes

--- config/BuildSystem/config/package.py
+++ config/BuildSystem/config/package.py
@@ -591,15 +591,12 @@ class Package(config.base.Configure):
       try:
         retriever.genericRetrieve(url, self.externalPackagesDir, self.package)
         self.logWrite(retriever.restoreLog())
-        try:
-          pkgdir = self.getDir()
-        except RuntimeError, e:
-          self.logPrint('ERROR: '+str(e))
-          err += str(e)
-          pkgdir = None
+        retriever.saveLog()
+        pkgdir = self.getDir()
         if not pkgdir:
-          raise RuntimeError('Unable to download '+self.PACKAGE+'\n'+err)
+          raise RuntimeError('Failed to download '+self.PACKAGE)
         self.framework.actions.addArgument(self.PACKAGE, 'Download', 'Downloaded '+self.PACKAGE+' into '+pkgdir)
+        retriever.restoreLog()
         return pkgdir
       except RuntimeError, e:
         self.logPrint('ERROR: '+str(e))
