57d6a9ace535c1e55e64f10bf98da76b73f5b91c
Barry Smith <bsmith@mcs.anl.gov>
2016-03-05 15:49:33 -0600

    Have ./configure return a nonzero exit code when --with-packages-dir detects missing packages so that calling scripts know this is occurred simplify some code
    Funded-by: IDEAS
    Project: xSDK
    Time: .1 hour

--- config/BuildSystem/help.py
+++ config/BuildSystem/help.py
@@ -165,25 +165,22 @@ class Help(Info):
           missing = 1
     if missing:
       print 'Download the following packages to '+pkgdir+' \n'
-    missing = 0
-    for i in self.argDB.dlist.keys():
-      if not nargs.Arg.findArgument('download-'+i, self.clArgs) == None and not nargs.Arg.findArgument('download-'+i, self.clArgs) == '0':
-        dlist = self.argDB.dlist[i]
-        found = 0
-        for k in range(0,len(dlist)):
-          fd = os.path.join(pkgdir,os.path.basename(dlist[k]))
-          if os.path.isdir(fd) or os.path.isfile(fd):
-            found = 1
-            for k in range(0,len(self.clArgs)):
-              if self.clArgs[k].startswith('--download-'+i):
-                self.clArgs[k] = 'download-'+i+'='+fd
-                self.argDB.insertArgs([self.clArgs[k]])
-            break
-        if not found:
-          print i + ' ' + str(self.argDB.dlist[i])
-          missing = 1
-    if missing:
+      for i in self.argDB.dlist.keys():
+        if not nargs.Arg.findArgument('download-'+i, self.clArgs) == None and not nargs.Arg.findArgument('download-'+i, self.clArgs) == '0':
+          dlist = self.argDB.dlist[i]
+          found = 0
+          for k in range(0,len(dlist)):
+            fd = os.path.join(pkgdir,os.path.basename(dlist[k]))
+            if os.path.isdir(fd) or os.path.isfile(fd):
+              found = 1
+              for k in range(0,len(self.clArgs)):
+                if self.clArgs[k].startswith('--download-'+i):
+                  self.clArgs[k] = 'download-'+i+'='+fd
+                  self.argDB.insertArgs([self.clArgs[k]])
+              break
+          if not found:
+            print i + ' ' + str(self.argDB.dlist[i])
       print '\nThen run the script again\n'
-      sys.exit()
+      sys.exit(1)
 
 
