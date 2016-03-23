ea3ed5f73437799ea7e83e3c9f9f3a9b1bcec426
Dave May <dave.mayhem23@gmail.com>
2016-03-23 14:18:18 +0100

    DMSwarm example: * Added vector creation tests

--- src/dm/examples/tutorials/swarm_ex1.c
+++ src/dm/examples/tutorials/swarm_ex1.c
@@ -10,6 +10,7 @@ int main(int argc,char **argv)
 {
   DM dms;
   PetscErrorCode ierr;
+  Vec x;
   
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr);
 
@@ -19,11 +20,29 @@ int main(int argc,char **argv)
   ierr = DMSwarmInitializeFieldRegister(dms);CHKERRQ(ierr);
   
   ierr = DMSwarmRegisterPetscDatatypeField(dms,"viscosity",1,PETSC_REAL);CHKERRQ(ierr);
+  ierr = DMSwarmRegisterPetscDatatypeField(dms,"strain",1,PETSC_REAL);CHKERRQ(ierr);
   
   ierr = DMSwarmFinalizeFieldRegister(dms);CHKERRQ(ierr);
   
   ierr = DMSwarmSetLocalSizes(dms,20,4);CHKERRQ(ierr);
   ierr = DMView(dms,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  
+  {
+    PetscReal *array;
+    ierr = DMSwarmGetField(dms,"viscosity",NULL,NULL,&array);CHKERRQ(ierr);
+    array[0] = 11.1;
+    array[3] = 33.3;
+    ierr = DMSwarmRestoreField(dms,"viscosity",NULL,NULL,&array);CHKERRQ(ierr);
+  }
+  
+  ierr = DMSwarmCreateGlobalVectorFromField(dms,"viscosity",&x);CHKERRQ(ierr);
+  ierr = VecView(x,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = DMSwarmDestroyGlobalVectorFromField(dms,"viscosity",&x);CHKERRQ(ierr);
+  
+  ierr = DMSwarmVectorDefineField(dms,"strain");CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(dms,&x);CHKERRQ(ierr);
+  ierr = VecView(x,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = VecDestroy(&x);CHKERRQ(ierr);
   
   ierr = DMDestroy(&dms);CHKERRQ(ierr);
   
