7ba0d4e187619ccccf958cfdade0eb5ad4f5b1dc
Barry Smith <bsmith@mcs.anl.gov>
2016-04-21 21:13:47 -0500

    Changed Trilinos and xSDKTrilinos hyperlinks to the release version tags and tarballs.
    Funded-by: IDEAS
    Project: xSDK
    Reported-by: James M. Willenbring <jmwille@sandia.gov>

--- config/BuildSystem/config/packages/xSDKTrilinos.py
+++ config/BuildSystem/config/packages/xSDKTrilinos.py
@@ -4,8 +4,8 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit         = 'origin/master'
-    self.download          = ['git://https://github.com/trilinos/xSDKTrilinos.git']
+    self.gitcommit         = 'trilinos-release-12-6-2'
+    self.download          = ['https://github.com/trilinos/xSDKTrilinos/archive/trilinos-release-12-6-2.tar.gz','git://https://github.com/trilinos/xSDKTrilinos.git']
     self.downloaddirname   = 'xSDKTrilinos'
     self.includes          = []
     self.functions         = []
