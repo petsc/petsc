afbfbe643b2fba25f1fa96f1d8c08d33b151c7c3
Satish Balay <balay@mcs.anl.gov>
2016-05-06 15:31:59 -0500

    petsc4py: use snapshot with BSTRM update

--- config/BuildSystem/config/packages/petsc4py.py
+++ config/BuildSystem/config/packages/petsc4py.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit         = '3.7.0'
+    self.gitcommit         = '7f892f7' #3.7+
     self.download          = ['git://https://bitbucket.org/petsc/petsc4py','https://bitbucket.org/petsc/petsc4py/downloads/petsc4py-'+self.gitcommit+'.tar.gz']
     self.functions         = []
     self.includes          = []
