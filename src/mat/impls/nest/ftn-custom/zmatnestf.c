ffa9b3b1b4df436c9af82c61ab815cba07b7b4bc
Vincent Le Chenadec <vlc@Vincents-MacBook-Pro.local>
2016-05-15 17:15:07 +0200

    Custom fortran wrapper for MatNestGetSubMats added

--- src/mat/impls/nest/ftn-custom/zmatnestf.c
+++ src/mat/impls/nest/ftn-custom/zmatnestf.c
@@ -4,9 +4,11 @@
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
 #define matcreatenest_                   MATCREATENEST
 #define matnestgetiss_                   MATNESTGETISS
+#define matnestgetsubmats_               MATNESTGETSUBMATS
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define matcreatenest_                   matcreatenest
 #define matnestgetiss_                   matnestgetiss
+#define matnestgetsubmats_               matnestgetsubmats
 #endif
 
 PETSC_EXTERN void PETSC_STDCALL matcreatenest_(MPI_Fint *comm,PetscInt *nr,IS is_row[],PetscInt *nc,IS is_col[],Mat a[],Mat *B,int *ierr)
@@ -21,4 +23,16 @@ PETSC_EXTERN void PETSC_STDCALL  matnestgetiss_(Mat *A,IS rows[],IS cols[], int 
   CHKFORTRANNULLOBJECT(rows);
   CHKFORTRANNULLOBJECT(cols);
   *ierr = MatNestGetISs(*A,rows,cols);
+}
+
+PETSC_EXTERN void PETSC_STDCALL matnestgetsubmats_(Mat *A,PetscInt *M,PetscInt *N,Mat *sub,int *ierr)
+{
+  Mat **mat;
+  PetscInt i,j;
+  *ierr = MatNestGetSubMats(*A,M,N,&mat);
+  for (i=0; i<(*M);i++){
+    for (j=0;j<(*N);j++){
+      sub[j + (*N) * i] = mat[i][j];
+    }
+  }
 }
