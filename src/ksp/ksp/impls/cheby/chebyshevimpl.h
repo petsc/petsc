b65fd476815669c287a3b9df77324278b7978960
Mark Adams <cal2princeton@yahoo.com>
2016-05-19 13:14:27 -0400

    changed Cheby random to noisy
    added snes ex56, Plex with 3D linear elasticity

--- src/ksp/ksp/impls/cheby/chebyshevimpl.h
+++ src/ksp/ksp/impls/cheby/chebyshevimpl.h
@@ -12,8 +12,7 @@ typedef struct {
   KSP              kspest;       /* KSP used to estimate eigenvalues */
   PetscReal        tform[4];     /* transform from Krylov estimates to Chebyshev bounds */
   PetscInt         eststeps;     /* number of kspest steps in KSP used to estimate eigenvalues */
-  PetscBool        userandom;    /* use random right hand side vector to estimate eigenvalues */
-  PetscRandom      random;
+  PetscBool        usenoisy;    /* use noisy right hand side vector to estimate eigenvalues */
   /* For tracking when to update the eigenvalue estimates */
   PetscObjectId    amatid,    pmatid;
   PetscObjectState amatstate, pmatstate;
