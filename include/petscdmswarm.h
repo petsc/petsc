934315b857560e2e25b72deadc307dba6a67aadb
Matthew G. Knepley <knepley@gmail.com>
2016-06-11 10:10:22 +0100

    Swarm: Fixed prototypes

--- include/petscdmswarm.h
+++ include/petscdmswarm.h
@@ -31,38 +31,37 @@ PETSC_EXTERN const char DMSwarmField_rank[];
 PETSC_EXTERN const char DMSwarmPICField_coor[];
 
 
-PETSC_EXTERN PetscErrorCode DMSwarmCreateGlobalVectorFromField(DM dm,const char fieldname[],Vec *vec);
-PETSC_EXTERN PetscErrorCode DMSwarmDestroyGlobalVectorFromField(DM dm,const char fieldname[],Vec *vec);
-PETSC_EXTERN PetscErrorCode DMSwarmCreateGlobalVector(DM, const char[], Vec *);
-
-PETSC_EXTERN PetscErrorCode DMSwarmInitializeFieldRegister(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmFinalizeFieldRegister(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmSetLocalSizes(DM dm,PetscInt nlocal,PetscInt buffer);
-PETSC_EXTERN PetscErrorCode DMSwarmRegisterPetscDatatypeField(DM dm,const char fieldname[],PetscInt blocksize,PetscDataType type);
-PETSC_EXTERN PetscErrorCode DMSwarmRegisterUserStructField(DM dm,const char fieldname[],size_t size);
-PETSC_EXTERN PetscErrorCode DMSwarmRegisterUserDatatypeField(DM dm,const char fieldname[],size_t size,PetscInt blocksize);
-PETSC_EXTERN PetscErrorCode DMSwarmGetField(DM dm,const char fieldname[],PetscInt *blocksize,PetscDataType *type,void **data);
-PETSC_EXTERN PetscErrorCode DMSwarmRestoreField(DM dm,const char fieldname[],PetscInt *blocksize,PetscDataType *type,void **data);
-
-PETSC_EXTERN PetscErrorCode DMSwarmVectorDefineField(DM dm,const char fieldname[]);
-
-PETSC_EXTERN PetscErrorCode DMSwarmAddPoint(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmAddNPoints(DM dm,PetscInt npoints);
-PETSC_EXTERN PetscErrorCode DMSwarmRemovePoint(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmRemovePointAtIndex(DM dm,PetscInt idx);
-
-PETSC_EXTERN PetscErrorCode DMSwarmGetLocalSize(DM dm,PetscInt *nlocal);
-PETSC_EXTERN PetscErrorCode DMSwarmGetSize(DM dm,PetscInt *n);
-PETSC_EXTERN PetscErrorCode DMSwarmMigrate(DM dm,PetscBool remove_sent_points);
-
-PETSC_EXTERN PetscErrorCode DMSwarmCollectViewCreate(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmCollectViewDestroy(DM dm);
-PETSC_EXTERN PetscErrorCode DMSwarmSetCellDM(DM dm,DM dmcell);
-PETSC_EXTERN PetscErrorCode DMSwarmGetCellDM(DM dm,DM *dmcell);
-
-PETSC_EXTERN PetscErrorCode DMSwarmSetType(DM dm,DMSwarmType stype);
+PETSC_EXTERN PetscErrorCode DMSwarmCreateGlobalVectorFromField(DM,const char[],Vec*);
+PETSC_EXTERN PetscErrorCode DMSwarmDestroyGlobalVectorFromField(DM,const char[],Vec*);
 PETSC_EXTERN PetscErrorCode DMSwarmCreateLocalVectorFromField(DM,const char[],Vec*);
 PETSC_EXTERN PetscErrorCode DMSwarmDestroyLocalVectorFromField(DM,const char[],Vec*);
+
+PETSC_EXTERN PetscErrorCode DMSwarmInitializeFieldRegister(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmFinalizeFieldRegister(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmSetLocalSizes(DM,PetscInt,PetscInt);
+PETSC_EXTERN PetscErrorCode DMSwarmRegisterPetscDatatypeField(DM,const char[],PetscInt,PetscDataType);
+PETSC_EXTERN PetscErrorCode DMSwarmRegisterUserStructField(DM,const char[],size_t);
+PETSC_EXTERN PetscErrorCode DMSwarmRegisterUserDatatypeField(DM,const char[],size_t,PetscInt);
+PETSC_EXTERN PetscErrorCode DMSwarmGetField(DM,const char[],PetscInt*,PetscDataType*,void**);
+PETSC_EXTERN PetscErrorCode DMSwarmRestoreField(DM,const char[],PetscInt*,PetscDataType*,void**);
+
+PETSC_EXTERN PetscErrorCode DMSwarmVectorDefineField(DM,const char[]);
+
+PETSC_EXTERN PetscErrorCode DMSwarmAddPoint(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmAddNPoints(DM,PetscInt);
+PETSC_EXTERN PetscErrorCode DMSwarmRemovePoint(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmRemovePointAtIndex(DM,PetscInt);
+
+PETSC_EXTERN PetscErrorCode DMSwarmGetLocalSize(DM,PetscInt*);
+PETSC_EXTERN PetscErrorCode DMSwarmGetSize(DM,PetscInt*);
+PETSC_EXTERN PetscErrorCode DMSwarmMigrate(DM,PetscBool);
+
+PETSC_EXTERN PetscErrorCode DMSwarmCollectViewCreate(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmCollectViewDestroy(DM);
+PETSC_EXTERN PetscErrorCode DMSwarmSetCellDM(DM,DM);
+PETSC_EXTERN PetscErrorCode DMSwarmGetCellDM(DM,DM*);
+
+PETSC_EXTERN PetscErrorCode DMSwarmSetType(DM,DMSwarmType);
 
 #endif
 
