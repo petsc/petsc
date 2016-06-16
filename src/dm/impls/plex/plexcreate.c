2f356fac0af275bc76cdd9597a42ebd782721208
Matthew G. Knepley <knepley@gmail.com>
2016-06-16 15:49:14 -0500

    Plex: Fixed type for GetNeighbors

--- src/dm/impls/plex/plexcreate.c
+++ src/dm/impls/plex/plexcreate.c
@@ -1306,14 +1306,14 @@ static PetscErrorCode DMGetDimPoints_Plex(DM dm, PetscInt dim, PetscInt *pStart,
 
 #undef __FUNCT__
 #define __FUNCT__ "DMGetNeighors_Plex"
-static PetscErrorCode DMGetNeighors_Plex(DM dm, PetscInt *nranks, const PetscInt *ranks[])
+static PetscErrorCode DMGetNeighors_Plex(DM dm, PetscInt *nranks, const PetscMPIInt *ranks[])
 {
+  PetscSF        sf;
   PetscErrorCode ierr;
-  PetscSF sf;
+
   PetscFunctionBegin;
-  
-  ierr = DMGetPointSF(dm,&sf);CHKERRQ(ierr);
-  ierr = PetscSFGetRanks(sf,nranks,ranks,NULL,NULL,NULL);CHKERRQ(ierr);
+  ierr = DMGetPointSF(dm, &sf);CHKERRQ(ierr);
+  ierr = PetscSFGetRanks(sf, nranks, ranks, NULL, NULL, NULL);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
