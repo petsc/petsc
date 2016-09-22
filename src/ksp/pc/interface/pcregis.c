075987266ca0d0e8d09d008387b2c5b96849a557
Karl Rupp <rupp@iue.tuwien.ac.at>
2016-09-22 14:25:45 +0200

    ViennaCL: Added smoothed aggregation preconditioner (saviennacl) similar to sacusp.
    Requires commit 6e09ddc from ViennaCL repository. Release 1.7.1 is insufficient.

--- src/ksp/pc/interface/pcregis.c
+++ src/ksp/pc/interface/pcregis.c
@@ -51,6 +51,9 @@ PETSC_EXTERN PetscErrorCode PCCreate_SACUSPPoly(PC);
 PETSC_EXTERN PetscErrorCode PCCreate_BiCGStabCUSP(PC);
 PETSC_EXTERN PetscErrorCode PCCreate_AINVCUSP(PC);
 #endif
+#if defined(PETSC_HAVE_VIENNACL)
+PETSC_EXTERN PetscErrorCode PCCreate_SAVIENNACL(PC);
+#endif
 #if defined(PETSC_HAVE_PARMS)
 PETSC_EXTERN PetscErrorCode PCCreate_PARMS(PC);
 #endif
@@ -128,6 +131,9 @@ PetscErrorCode  PCRegisterAll(void)
   ierr = PCRegister(PCAINVCUSP     ,PCCreate_AINVCUSP);CHKERRQ(ierr);
   ierr = PCRegister(PCBICGSTABCUSP ,PCCreate_BiCGStabCUSP);CHKERRQ(ierr);
   ierr = PCRegister(PCSACUSPPOLY   ,PCCreate_SACUSPPoly);CHKERRQ(ierr);
+#endif
+#if defined(PETSC_HAVE_VIENNACL)
+  ierr = PCRegister(PCSAVIENNACL   ,PCCreate_SAVIENNACL);CHKERRQ(ierr);
 #endif
 #if defined(PETSC_HAVE_PARMS)
   ierr = PCRegister(PCPARMS        ,PCCreate_PARMS);CHKERRQ(ierr);
