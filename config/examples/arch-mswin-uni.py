db90e4faedf23b14c8e61f4ecbb0a936755cae95
Satish Balay <balay@mcs.anl.gov>
2016-09-26 17:13:55 -0500

    win32: set '-MD' as default for dll build.
    This fixes error with conflicting'stdout' usage in main vs petsc library - in src/mat/examples/tests/ex21.c

--- config/examples/arch-mswin-uni.py
+++ config/examples/arch-mswin-uni.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
-    '--with-blas-lapack-lib=-L/home/petsc/soft/f2cblaslapack libf2clapack.lib libf2cblas.lib',
+    '--download-f2cblaslapack',
     '--with-cc=win32fe cl',
     '--with-cxx=0',
     '--with-fc=0',
