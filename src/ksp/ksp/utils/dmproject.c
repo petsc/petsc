030bf5098787785becbd7a38ba50c5e93635df12
Matthew G. Knepley <knepley@gmail.com>
2016-10-05 17:51:09 -0500

    DM: If U == X on input to projection, make loci == locX - We use locU == locX to determine whether to apply bc to locX

--- src/ksp/ksp/utils/dmproject.c
+++ src/ksp/ksp/utils/dmproject.c
@@ -200,7 +200,9 @@ PetscErrorCode DMProjectField(DM dm, PetscReal time, Vec U,
   PetscFunctionBegin;
   PetscValidHeaderSpecific(dm, DM_CLASSID, 1);
   ierr = DMGetLocalVector(dm, &localX);CHKERRQ(ierr);
-  ierr = DMGetLocalVector(dm, &localU);CHKERRQ(ierr);
+  /* We currently check whether locU == locX to see if we need to apply BC */
+  if (U != X) {ierr = DMGetLocalVector(dm, &localU);CHKERRQ(ierr);}
+  else        {localU = localX;}
   ierr = DMGlobalToLocalBegin(dm, U, INSERT_VALUES, localU);CHKERRQ(ierr);
   ierr = DMGlobalToLocalEnd(dm, U, INSERT_VALUES, localU);CHKERRQ(ierr);
   ierr = DMProjectFieldLocal(dm, time, localU, funcs, mode, localX);CHKERRQ(ierr);
@@ -215,7 +217,7 @@ PetscErrorCode DMProjectField(DM dm, PetscReal time, Vec U,
     }
   }
   ierr = DMRestoreLocalVector(dm, &localX);CHKERRQ(ierr);
-  ierr = DMRestoreLocalVector(dm, &localU);CHKERRQ(ierr);
+  if (U != X) {ierr = DMRestoreLocalVector(dm, &localU);CHKERRQ(ierr);}
   PetscFunctionReturn(0);
 }
 
