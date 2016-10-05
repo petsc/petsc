7fc5cb51c207b05dca0708de826e89033c0391a8
Satish Balay <balay@mcs.anl.gov>
2016-10-05 10:56:19 -0500

    xsdk: update tests to use --download-xsdk. And also switch 'latest' test to xsdk

---
+++ config/examples/arch-linux-xsdk-latest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+configure_options = [
+  '--download-xsdk',
+  '--download-fblaslapack=1',
+  '--download-mpich=1',
+  '--download-cmake=1',
+  '--with-clanguage=C++',
+  '--with-debugging=0',
+  '--with-shared-libraries=1',
+  #prereq packages
+  '--download-triangle=1', # for hdf5 tests
+  '--download-chaco=1', # for hdf5 tests
+  #test latest snapshot
+  '--download-hypre-commit=origin/master',
+  '--download-alquimia-commit=origin/master',
+  '--download-mstk-commit=origin/master',
+  '--download-superlu_dist-commit=origin/master',
+  '--download-hypre-commit=origin/master',
+  '--download-trilinos-commit=origin/master',
+  '--download-xsdktrilinos-commit=origin/master',
+
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
