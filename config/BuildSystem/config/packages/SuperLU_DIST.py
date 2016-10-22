e262d011fb64d87aeabfb255de01b02a5a3f5481
Satish Balay <balay@mcs.anl.gov>
2016-10-21 23:15:29 -0500

    superlu_dist: 64-bit-indices fix https://github.com/xiaoyeli/superlu_dist/issues/4

--- config/BuildSystem/config/packages/SuperLU_DIST.py
+++ config/BuildSystem/config/packages/SuperLU_DIST.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit         = 'd3a08bd' #maint/v5.1.1+
+    self.gitcommit         = 'v5.1.2'
     self.download         = ['git://https://github.com/xiaoyeli/superlu_dist','https://github.com/xiaoyeli/superlu_dist/archive/'+self.gitcommit+'.tar.gz']
     self.downloaddirname  = 'superlu_dist'
     self.functions        = ['set_default_options_dist']
