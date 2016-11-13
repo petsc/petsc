ffbfca0e85b382dc535f2376688d6f96b3e13b80
Satish Balay <balay@mcs.anl.gov>
2016-11-13 17:56:52 -0600

    test: fix arch-pardiso to work on the new ubuntu 14.04 build with ifort-16

--- config/examples/arch-pardiso.py
+++ config/examples/arch-pardiso.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     'FC=ifort',
     '--with-blas-lapack-dir='+os.environ['MKL_HOME'],
     '--with-mkl_pardiso-dir='+os.environ['MKL_HOME'],
-    '--download-mpich=1',
+    # using mpich-3.2 as default mpich-3.1.3 does not build with ifort-16
+    '--download-mpich=http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.2.tar.gz',
   ]
   configure.petsc_configure(configure_options)
