9d9032ef551a49a0e3ad07899f42c75d20718208
Jose E. Roman <jroman@dsic.upv.es>
2016-11-16 18:48:16 +0100

    MatLRC: check arguments of MatCreateLRC including dimensions

--- src/mat/impls/lrc/lrc.c
+++ src/mat/impls/lrc/lrc.c
@@ -3,7 +3,7 @@
 
 typedef struct {
   Mat         A,U,V;
-  Vec         work1,work2; /* Sequential (big) vectors that hold partial products */
+  Vec         work1,work2; /* Sequential vectors that hold partial products */
   PetscMPIInt nwork;       /* length of work vectors */
 } Mat_LRC;
 
@@ -62,37 +62,50 @@ PetscErrorCode MatDestroy_LRC(Mat N)
 #undef __FUNCT__
 #define __FUNCT__ "MatCreateLRC"
 /*@
-      MatCreateLRC - Creates a new matrix object that behaves like A + U*V'
+   MatCreateLRC - Creates a new matrix object that behaves like A + U*V'
 
    Collective on Mat
 
-   Input Parameter:
-+   A  - the (sparse) matrix
--   U. V - two dense rectangular (tall and skinny) matrices
+   Input Parameters:
++  A  - the (sparse) matrix
+-  U, V - two dense rectangular (tall and skinny) matrices
 
    Output Parameter:
-.   N - the matrix that represents A + U*V'
+.  N - the matrix that represents A + U*V'
+
+   Notes:
+   The matrix A + U*V' is not formed! Rather the new matrix
+   object performs the matrix-vector product by first multiplying by
+   A and then adding the other term.
 
    Level: intermediate
-
-   Notes: The matrix A + U*V' is not formed! Rather the new matrix
-          object performs the matrix-vector product by first multiplying by
-          A and then adding the other term
 @*/
-PetscErrorCode  MatCreateLRC(Mat A,Mat U, Mat V,Mat *N)
+PetscErrorCode MatCreateLRC(Mat A,Mat U,Mat V,Mat *N)
 {
   PetscErrorCode ierr;
-  PetscInt       m,n;
+  PetscInt       m,n,k,m1,n1,k1;
   Mat_LRC        *Na;
 
   PetscFunctionBegin;
-  ierr = MatGetLocalSize(A,&m,&n);CHKERRQ(ierr);
+  PetscValidHeaderSpecific(A,MAT_CLASSID,1);
+  PetscValidHeaderSpecific(U,MAT_CLASSID,2);
+  PetscValidHeaderSpecific(V,MAT_CLASSID,3);
+
+  ierr = MatGetSize(U,NULL,&k);CHKERRQ(ierr);
+  ierr = MatGetSize(V,NULL,&k1);CHKERRQ(ierr);
+  if (k!=k1) SETERRQ(PetscObjectComm((PetscObject)U),PETSC_ERR_ARG_INCOMP,"U and V have different number of columns");
+  ierr = MatGetLocalSize(U,&m,NULL);CHKERRQ(ierr);
+  ierr = MatGetLocalSize(V,&n,NULL);CHKERRQ(ierr);
+  ierr = MatGetLocalSize(A,&m1,&n1);CHKERRQ(ierr);
+  if (m!=m1) SETERRQ(PetscObjectComm((PetscObject)U),PETSC_ERR_ARG_INCOMP,"Local dimensions of U and A do not match");
+  if (n!=n1) SETERRQ(PetscObjectComm((PetscObject)V),PETSC_ERR_ARG_INCOMP,"Local dimensions of V and A do not match");
+
   ierr = MatCreate(PetscObjectComm((PetscObject)A),N);CHKERRQ(ierr);
-  ierr = MatSetSizes(*N,n,n,PETSC_DECIDE,PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = MatSetSizes(*N,m,n,PETSC_DECIDE,PETSC_DECIDE);CHKERRQ(ierr);
   ierr = PetscObjectChangeTypeName((PetscObject)*N,MATLRC);CHKERRQ(ierr);
 
   ierr       = PetscNewLog(*N,&Na);CHKERRQ(ierr);
-  (*N)->data = (void*) Na;
+  (*N)->data = (void*)Na;
   Na->A      = A;
 
   ierr = MatDenseGetLocalMatrix(U,&Na->U);CHKERRQ(ierr);
@@ -108,10 +121,10 @@ PetscErrorCode  MatCreateLRC(Mat A,Mat U, Mat V,Mat *N)
   (*N)->ops->destroy = MatDestroy_LRC;
   (*N)->ops->mult    = MatMult_LRC;
   (*N)->assembled    = PETSC_TRUE;
-  (*N)->cmap->N      = A->cmap->N;
-  (*N)->rmap->N      = A->cmap->N;
-  (*N)->cmap->n      = A->cmap->n;
-  (*N)->rmap->n      = A->cmap->n;
+  (*N)->cmap->N      = V->rmap->N;
+  (*N)->rmap->N      = U->rmap->N;
+  (*N)->cmap->n      = V->rmap->n;
+  (*N)->rmap->n      = U->rmap->n;
   PetscFunctionReturn(0);
 }
 
