0eef0fda4725ef416cad9457aef153c2dbe1600b
Barry Smith <bsmith@mcs.anl.gov>
2016-11-16 11:25:51 -0600

    Fix incorrect checking of file existence.
    Reported-by: Jose E. Roman <jroman@dsic.upv.es>

--- config/install.py
+++ config/install.py
@@ -329,7 +329,7 @@ for dir in dirs:
 
   def installShare(self):
     self.copies.extend(self.copytree(self.rootShareDir, self.destShareDir))
-    if os.path.join(self.destShareDir,'petsc','examples'):
+    if os.path.exists(os.path.join(self.destShareDir,'petsc','examples')):
       shutil.rmtree(os.path.join(self.destShareDir,'petsc','examples'))
     os.mkdir(os.path.join(self.destShareDir,'petsc','examples'))
     self.copyExamples(self.rootDir,os.path.join(self.destShareDir,'petsc','examples'))
