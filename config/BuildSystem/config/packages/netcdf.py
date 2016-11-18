ec72076eb2a803164d4aeb5809971e2d78545e64
Satish Balay <balay@mcs.anl.gov>
2016-11-18 09:17:42 -0600

    hdf5: update to version 1.8.18 netcdf: update to version 4.4.1
    Notes:
    
    * netcdf changed default of dynamic-loading option resluting in '-ldl' error. Hence using --disable-dynamic-loading option
    
    * Tried using hdf5-1.10.0-patch1 - and it has a few issues.
    - Does not work with ubuntu-12.04 default compilers. [fix is in git]
    - requires gnumake for 'fortran/test/Makefile' as it uses .NOTPARALLEL directive [so has issues with bsdmake]
    - requires F2003 compiler for fortran interface [so has issues on opensolaris]

--- config/BuildSystem/config/packages/netcdf.py
+++ config/BuildSystem/config/packages/netcdf.py
@@ -4,8 +4,8 @@ import os
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.download  = ['ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.2.tar.gz',
-                      'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/netcdf-4.3.2.tar.gz']
+    self.download        = ['ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.1.tar.gz',
+                            'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/netcdf-4.4.1.tar.gz']
     self.functions       = ['nccreate']
     self.includes        = ['netcdf.h']
     self.liblist         = [['libnetcdf.a']]
@@ -26,5 +26,6 @@ class Configure(config.package.GNUPackage):
     args.append('LIBS="'+self.libraries.toString(self.hdf5.dlib)+' '+self.compilers.LIBS+'"')
     args.append('--enable-netcdf-4')
     args.append('--disable-dap')
+    args.append('--disable-dynamic-loading') #This was disabled in v4.3.2 - but enabled in subsequent versions - giving config errors on freebsd (wrt -ldl)
     args.append('--disable-hdf4')
     return args
