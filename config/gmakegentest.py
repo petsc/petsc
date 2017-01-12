65ea9442e1277dba453ba946989c4de03cb48eda
Jed Brown <jed@jedbrown.org>
2017-01-11 21:24:09 -0600

    gtest: ensure that test execution completes before removing executables

--- config/gmakegentest.py
+++ config/gmakegentest.py
@@ -666,7 +666,7 @@ class generateExamples(Petsc):
           basedir=os.path.dirname(ftest)
           testdeps.append(self.nameSpace(test,basedir))
         fd.write("test-"+pkg+"."+lang+" := "+' '.join(testdeps)+"\n")
-        fd.write('test-%s-%s : $(test-%s.%s)\n' % (pkg, lang, pkg, lang))
+        fd.write('test-%s.%s : $(test-%s.%s)\n' % (pkg, lang, pkg, lang))
 
         # test targets
         for ftest in self.tests[pkg][lang]:
