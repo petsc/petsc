00f954742a5d1f2b347d71796d964f5375a7b837
Satish Balay <balay@mcs.anl.gov>
2017-03-23 19:17:11 -0500

    zlib: enable static build with --with-shared-libraries=0 option - add -download-zlib-static option for use with --with-shared-libraries=1 - useful when libz.so build fails - but libpetsc.so would work
    Reported-by: Mark Adams <mfadams@lbl.gov>

--- config/BuildSystem/config/packages/zlib.py
+++ config/BuildSystem/config/packages/zlib.py
@@ -9,6 +9,11 @@ class Configure(config.package.Package):
     self.liblist   = [['libz.a'],['zlib.lib']]
     return
 
+  def setupHelp(self, help):
+    import nargs
+    config.package.Package.setupHelp(self, help)
+    help.addArgument('ZLIB', '-download-zlib-static=<bool>',                 nargs.ArgBool(None, 0, 'Build libz as a static library'))
+
   def Install(self):
     import os
 
@@ -20,9 +25,15 @@ class Configure(config.package.Package):
     self.framework.popLanguage()
     args=' '.join(args)
 
+    cargs=[]
+    if (not self.checkSharedLibrariesEnabled()) or self.argDB['download-zlib-static']:
+      cargs.append('--static')
+    cargs=' '.join(cargs)
+
     conffile = os.path.join(self.packageDir,self.package+'.petscconf')
     fd = file(conffile, 'w')
-    fd.write(args)
+    fd.write('args: '+args+'\n')
+    fd.write('cargs: '+cargs+'\n')
     fd.close()
 
     if not self.installNeeded(conffile): return self.installDir
@@ -30,7 +41,7 @@ class Configure(config.package.Package):
     self.logPrintBox('Building and installing zlib, this may take many minutes')
     self.installDirProvider.printSudoPasswordMessage()
     try:
-      output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+' && ' + args + ' ./configure && '+self.make.make_jnp+' && '+self.installSudo+' ' +self.make.make+' install', timeout=600, log = self.log)
+      output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+' && ' + args + ' ./configure '+cargs+' && '+self.make.make_jnp+' && '+self.installSudo+' ' +self.make.make+' install', timeout=600, log = self.log)
     except RuntimeError, e:
       raise RuntimeError('Error building/install zlib files from '+os.path.join(self.packageDir, 'zlib')+' to '+self.packageDir)
     self.postInstall(output+err,conffile)
