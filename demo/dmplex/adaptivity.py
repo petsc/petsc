538a0d2e60486e6cd8f0af916aa0770169cddd9d
Lisandro Dalcin <dalcinl@gmail.com>
2017-04-13 10:04:17 +0300

    demo: Workaround too small grid in DMPlex adaptivity example

--- demo/dmplex/adaptivity.py
+++ demo/dmplex/adaptivity.py
@@ -6,7 +6,7 @@ import numpy as np
 OptDB = PETSc.Options()
 
 dim = OptDB.getInt('dim', 2)
-plex = PETSc.DMPlex().createBoxMesh(dim, 3)
+plex = PETSc.DMPlex().createBoxMesh(dim, 4)
 plex.view()
 
 dim = plex.getDimension()
