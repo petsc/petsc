1c287f868b87547f7872a682a7b0051d361920ef
Satish Balay <balay@mcs.anl.gov>
2017-04-25 17:16:51 -0500

    remove duplicate petscmemorysetgetmaximumusage_ introduced in dc37d89f2626b4ce22e9b823b8536ba0061a6977

--- src/sys/memory/ftn-custom/zmtrf.c
+++ src/sys/memory/ftn-custom/zmtrf.c
@@ -7,13 +7,11 @@
 #define petscmallocdumplog_            PETSCMALLOCDUMPLOG
 #define petscmallocvalidate_           PETSCMALLOCVALIDATE
 #define petscmemoryview_               PETSCMEMORYVIEW
-#define petscmemorysetgetmaximumusage_ PETSCMEMORYSETGETMAXIMUMUSAGE
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define petscmallocdump_               petscmallocdump
 #define petscmallocdumplog_            petscmallocdumplog
 #define petscmallocvalidate_           petscmallocvalidate
 #define petscmemoryview_               petscmemoryview
-#define petscmemorysetgetmaximumusage_ petscmemorysetgetmaximumusage
 #endif
 
 static PetscErrorCode PetscFixSlashN(const char *in, char **out)
@@ -43,11 +41,6 @@ PETSC_EXTERN void PETSC_STDCALL petscmallocdumplog_(PetscErrorCode *ierr)
 PETSC_EXTERN void PETSC_STDCALL petscmallocvalidate_(PetscErrorCode *ierr)
 {
   *ierr = PetscMallocValidate(0,"Unknown Fortran",0);
-}
-
-PETSC_EXTERN void PETSC_STDCALL petscmemorysetgetmaximumusage_(PetscErrorCode *ierr)
-{
-  *ierr = PetscMemorySetGetMaximumUsage();
 }
 
 PETSC_EXTERN void PETSC_STDCALL petscmemoryview_(PetscViewer *vin, char* message PETSC_MIXED_LEN(len), PetscErrorCode *ierr PETSC_END_LEN(len))
