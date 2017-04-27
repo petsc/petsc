2becd9dc3b70afefe001e0823eab9229dd4f1aab
Toby Isaac <tisaac@uchicago.edu>
2017-04-26 23:43:10 -0400

    PetscMath: check for math functions by function pointer resolvability
    I had previously tried to solve the issue of hidden symbols in math
    libraries being misused in c89.  Trying to set a pointer to the
    functio will fail if the symbol is hidden, so this is a better
    solution.  I have applied it to other c99 math functions (erf, tgamma)
    as well.
    
    Suggested-by: Jed Brown <jed@jedbrown.org>

--- include/petscmath.h
+++ include/petscmath.h
@@ -21,13 +21,6 @@
 
 */
 
-/* Some compilers still strange behavior with -std=c89, in that log2 is a
-   usable symbol but gives the wrong result unless the log2 prototype is given
-   again */
-#if defined(PETSC_HAVE_LOG2) && !defined(__cplusplus) && !defined(__STDC_VERSION__) && !defined(PETSC_HAVE_WINDOWS_COMPILERS)
-double log2(double);
-#endif
-
 #if defined(PETSC_USE_REAL_SINGLE)
 #define MPIU_REAL   MPI_FLOAT
 typedef float PetscReal;
