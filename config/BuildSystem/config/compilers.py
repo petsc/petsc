1caa10d8c1482a7c13998b1342ee7f9be25ab338
Satish Balay <balay@mcs.anl.gov>
2017-06-30 18:18:35 -0500

    configure: update checkFortran90FreeForm() to test lines with 255 char len (within cray ftn free-form limit)
    PGI limit is 264

--- config/BuildSystem/config/compilers.py
+++ config/BuildSystem/config/compilers.py
@@ -1144,7 +1144,7 @@ class Configure(config.base.Configure):
     '''Determine whether the Fortran compiler handles F90FreeForm
        We also require that the compiler handles lines longer than 132 characters'''
     self.pushLanguage('FC')
-    if self.checkLink(body = '      INTEGER, PARAMETER ::        int = SELECTED_INT_KIND(8);              INTEGER (KIND=int) :: ierr;       ierr                            = 1'):
+    if self.checkLink(body = '      INTEGER, PARAMETER ::        int = SELECTED_INT_KIND(8);              INTEGER (KIND=int) :: ierr;       ierr                            =                                                                                                               1'):
       self.addDefine('USING_F90FREEFORM', 1)
       self.fortranIsF90FreeForm = 1
       self.logPrint('Fortran compiler supports F90FreeForm')
