19532ea364b07ecd049071e360c10aa6fe9ed42f
Satish Balay <balay@mcs.anl.gov>
2017-07-14 15:46:54 -0500

    add fortran interface for PetscDataTypeGetSize()
    Reported-by: Blaise A Bourdin <bourdin@lsu.edu>

---
+++ src/sys/objects/ftn-custom/zptypef.c
@@ -0,0 +1,13 @@
+#include <petsc/private/fortranimpl.h>
+
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define petscdatatypegetsize_        PETSCDATATYPEGETSIZE
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define petscdatatypegetsize_        petscdatatypegetsize
+#endif
+
+PETSC_EXTERN void PETSC_STDCALL  petscdatatypegetsize_(PetscDataType *ptype,size_t *size, PetscErrorCode *ierr )
+{
+  *ierr = PetscDataTypeGetSize(*ptype,size);
+}
+
