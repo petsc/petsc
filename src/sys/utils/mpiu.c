petsc/petsc

commit 2fe4ef4a9eb82e4d7c3a118a6d3c972f05c4238d

Free Petsc_Seq_Keyval if it has been created.
Commit-type: testing-fix,style-fix
Funded-by: ECP
Time:  .1   hours
Reported-by: nightly tests

Development Tools: Vim, Emacs, Eclipse

src/sys/utils/mpiu.c
@@ -51,7 +51,7 @@ PetscErrorCode PetscSequentialPhaseEnd_Private(MPI_Comm comm,int ng)
     The variable Petsc_Seq_keyval is used to indicate an MPI attribute that
   is attached to a communicator that manages the sequential phase code below.
 */
-static int Petsc_Seq_keyval = MPI_KEYVAL_INVALID;
+PetscMPIInt Petsc_Seq_keyval = MPI_KEYVAL_INVALID;
 
 /*@
    PetscSequentialPhaseBegin - Begins a sequential section of code.

include/petsc/private/petscimpl.h
@@ -774,6 +774,7 @@ PETSC_EXTERN PetscErrorCode PetscMonitorCompare(PetscErrorCode (*)(void),void *,
 PETSC_EXTERN PetscMPIInt Petsc_Counter_keyval;
 PETSC_EXTERN PetscMPIInt Petsc_InnerComm_keyval;
 PETSC_EXTERN PetscMPIInt Petsc_OuterComm_keyval;
+PETSC_EXTERN PetscMPIInt Petsc_Seq_keyval;
 
 /*
   PETSc communicators have this attribute, see

src/sys/classes/viewer/interface/dlregispetsc.c
@@ -20,31 +20,9 @@ PetscErrorCode  PetscSysFinalizePackage(void)
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
-  if (Petsc_Viewer_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_keyval);CHKERRQ(ierr);
+  if (Petsc_Seq_keyval != MPI_KEYVAL_INVALID) {
+    ierr = MPI_Keyval_free(&Petsc_Seq_keyval);CHKERRQ(ierr);
   }
-  if (Petsc_Viewer_Stdout_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_Stdout_keyval);CHKERRQ(ierr);
-  }
-  if (Petsc_Viewer_Stderr_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_Stderr_keyval);CHKERRQ(ierr);
-  }
-  if (Petsc_Viewer_Binary_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_Binary_keyval);CHKERRQ(ierr);
-  }
-  if (Petsc_Viewer_Draw_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_Draw_keyval);CHKERRQ(ierr);
-  }
-#if defined(PETSC_HAVE_HDF5)
-  if (Petsc_Viewer_HDF5_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_HDF5_keyval);CHKERRQ(ierr);
-  }
-#endif
-#if defined(PETSC_USE_SOCKETVIEWER)
-  if (Petsc_Viewer_Socket_keyval != MPI_KEYVAL_INVALID) {
-    ierr = MPI_Keyval_free(&Petsc_Viewer_Socket_keyval);CHKERRQ(ierr);
-  }
-#endif
   PetscSysPackageInitialized = PETSC_FALSE;
   PetscFunctionReturn(0);
 }

commit 8ae1f6135c49756e521071b429598a4db23cdcb4

test: fix compile error on windows introduced by 2fe4ef4a9eb82e4d7c3a118a6d3c972f05c4238d
libpetsc.lib(dlregispetsc.o) : error LNK2019: unresolved external symbol Petsc_Seq_keyval referenced in function "int __cdecl PetscSysFinalizePackage(void)" (?PetscSysFinalizePackage@@YAHXZ)

src/sys/utils/mpiu.c
@@ -1,5 +1,6 @@
 
 #include <petscsys.h>        /*I  "petscsys.h"  I*/
+#include <petsc/private/petscimpl.h>
 /*
     Note that tag of 0 is ok because comm is a private communicator
   generated below just for these routines.