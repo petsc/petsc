69a62b6c6fea10e05e43d66e2c3a7c9bd3dab46e
Satish Balay <balay@mcs.anl.gov>
2017-08-02 16:19:18 -0500

    test: add a test on KNL

---
+++ config/examples/arch-linux-knl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+if __name__ == '__main__':
+  import sys
+  import os
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--with-debugging=0',
+    '--with-cc=mpiicc',
+    '--with-cxx=mpiicpc',
+    '--with-fc=mpiifort',
+    '--with-mpiexec=mpiexec.hydra',
+    'COPTFLAGS=-g -xMIC-AVX512 -O3 -mP2OPT_hpo_vec_remainder=F',
+    'CXXOPTFLAGS=-g -xMIC-AVX512 -O3 -mP2OPT_hpo_vec_remainder=F',
+    'FOPTFLAGS=-g -xMIC-AVX512 -O3 -mP2OPT_hpo_vec_remainder=F',
+    '--with-blas-lapack-dir='+os.environ['MKLROOT'],
+    '--download-metis=1',
+    '--download-parmetis=1',
+    '--download-superlu_dist=1',
+  ]
+  configure.petsc_configure(configure_options)
