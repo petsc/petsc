931e20c1502952db78ad53b398eaf61e937a7ad7
Toby Isaac <tisaac@cc.gatech.edu>
2017-09-06 11:20:47 -0400

    Start adding a test to demonstrate weaknesses of LCGs

---
+++ src/sys/classes/random/examples/tests/ex3.c
@@ -0,0 +1,7 @@
+
+static char help[] = "Run Birthday Spacing Tests for PetscRandom.\n\n"
+
+/* L'Ecuyer & Simard, 2001.
+ * "On the performance of birthday spacings tests with certain families of random number generators"
+ * https://doi.org/10.1016/S0378-4754(00)00253-6
+ */
