768de9af326686eb14c92288ba1957cddbc3ea54
Richard Tran Mills <rmills@rmills.org>
2017-09-21 08:33:39 -0700

    Added Fortran stub for PCGASMGetSubKSP().

---
+++ src/ksp/pc/impls/gasm/ftn-custom/zgasmf.c
@@ -0,0 +1,24 @@
+#include <petsc/private/fortranimpl.h>
+#include <petscksp.h>
+
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define pcgasmgetsubksp_            PCGASMGETSUBKSP
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define pcgasmgetsubksp_            pcgasmgetsubksp
+#endif
+
+PETSC_EXTERN void PETSC_STDCALL pcgasmgetsubksp_(PC *pc,PetscInt *n_local,PetscInt *first_local,KSP *ksp,PetscErrorCode *ierr)
+{
+  KSP      *tksp;
+  PetscInt i,nloc;
+  CHKFORTRANNULLINTEGER(n_local);
+  CHKFORTRANNULLINTEGER(first_local);
+  CHKFORTRANNULLOBJECT(ksp);
+  *ierr = PCGASMGetSubKSP(*pc,&nloc,first_local,&tksp);
+  if (n_local) *n_local = nloc;
+  if (ksp) {
+    for (i=0; i<nloc; i++) ksp[i] = tksp[i];
+  }
+}
+
+
