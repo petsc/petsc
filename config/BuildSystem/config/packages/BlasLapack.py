b8695a4a8c79604fe096e2290da1d8555759a29b
Karl Rupp <me@karlrupp.net>
2017-10-03 17:26:26 -0500

    BuildSystem: Updated list of LAPACK routines to check for.

--- config/BuildSystem/config/packages/BlasLapack.py
+++ config/BuildSystem/config/packages/BlasLapack.py
@@ -443,7 +443,10 @@ class Configure(config.package.Package):
     '''Check for missing LAPACK routines'''
     if self.foundLapack:
       mangleFunc = hasattr(self.compilers, 'FC') and not self.f2c
-    routines = ['trsen','trtrs','gerfs','gges','tgsen','gesvd','getrf','getrs','geev','gelss','syev','syevx','sygv','sygvx','potrf','potrs','stebz','pttrf','pttrs','stein','orgqr','ormqr','geqrf','gesv','hseqr','steqr','hgeqz']
+    routines = ['geev','gels','gelss','geqrf','gerfs','gesv','gesvd','getrf','getri','getrs','gges',
+                'hetrf','hetri','hetrs','hgeqz','hseqr','ormqr','potrf','potri','potrs','pttrf','pttrs',
+                'stebz','stein','steqr','syev','syevx','sygvx','sytrf','sytri','sytrs',
+                'tgsen','trsen','trtrs','ungqr']
     self.libraries.saveLog()
     oldLibs = self.compilers.LIBS
     found, missing = self.libraries.checkClassify(self.lapackLibrary, map(self.mangleBlas,routines), otherLibs = self.getOtherLibs(), fortranMangle = mangleFunc)
