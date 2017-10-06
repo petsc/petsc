658dd75e7d98b3384b286072b00d4c1ba3e89364
Barry Smith <bsmith@mcs.anl.gov>
2017-10-06 13:23:17 +0200

    Fix test code to 1) have no C99/C++-ish constructs and 2) match PETSc style
    Commit-type: portability-fix,style-fix
    Reported-by: Karl Rupp <rupp@iue.tuwien.ac.at>

--- src/ksp/ksp/examples/tests/ex54.c
+++ src/ksp/ksp/examples/tests/ex54.c
@@ -1,18 +1,12 @@
+/*
+
+     Tests MPIDENSE matrix operations MatMultTranspose() with processes with no rows or columns
+*/
+
 #include <petscksp.h>
 
-
-// Mat Q:
-// 1.0000000000000000e+00 0.0000000000000000e+00 0.0000000000000000e+00 
-// 1.0000000000000000e+00 0.0000000000000000e+00 1.0000000000000000e+00 
-// 1.0000000000000000e+00 1.0000000000000000e+00 0.0000000000000000e+00 
-// 1.0000000000000000e+00 1.0000000000000000e+00 1.0000000000000000e+00
-
-// 1.0000000000000000e+00 2.0000000000000000e+00 0.0000000000000000e+00 
-// 1.0000000000000000e+00 2.0000000000000000e+00 1.0000000000000000e+00 
-// 1.0000000000000000e+00 3.0000000000000000e+00 0.0000000000000000e+00 
-// 1.0000000000000000e+00 3.0000000000000000e+00 1.0000000000000000e+00 
-
-PetscErrorCode fill(Mat m, Vec v) {
+PetscErrorCode fill(Mat m, Vec v)
+{
   PetscInt       idxn[3] = {0, 1, 2};
   PetscInt       localRows = 0;
   PetscMPIInt    rank,size;
@@ -32,40 +26,33 @@ PetscErrorCode fill(Mat m, Vec v) {
   ierr = MatSetUp(m);CHKERRQ(ierr);
 
   if (size == 1) {
-    localRows = 8;
-
-
-    PetscInt idxm1[4] = {0, 1, 2, 3};
+    PetscInt    idxm1[4] = {0, 1, 2, 3};
     PetscScalar values1[12] = {1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1};
+    PetscInt    idxm2[4] = {4, 5, 6, 7};
+    PetscScalar values2[12] = {1, 2, 0, 1, 2, 1, 1, 3, 0, 1, 3, 1};
 
     ierr = MatSetValues(m, 4, idxm1, 3, idxn, values1, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 0, 1.1, INSERT_VALUES); VecSetValue(v, 1, 2.5, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 2, 3, INSERT_VALUES); VecSetValue(v, 3, 4, INSERT_VALUES);CHKERRQ(ierr);
 
-    PetscInt idxm2[4] = {4, 5, 6, 7};
-    PetscScalar values2[12] = {1, 2, 0, 1, 2, 1, 1, 3, 0, 1, 3, 1};
-
     ierr = MatSetValues(m, 4, idxm2, 3, idxn, values2, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 4, 5, INSERT_VALUES); VecSetValue(v, 5, 6, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 6, 7, INSERT_VALUES); VecSetValue(v, 7, 8, INSERT_VALUES);CHKERRQ(ierr);
-
-  }else if (rank == 1) {
-    PetscInt idxm[4] = {0, 1, 2, 3};
+  } else if (rank == 1) {
+    PetscInt    idxm[4] = {0, 1, 2, 3};
     PetscScalar values[12] = {1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1};
 
     ierr = MatSetValues(m, 4, idxm, 3, idxn, values, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 0, 1.1, INSERT_VALUES); VecSetValue(v, 1, 2.5, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 2, 3, INSERT_VALUES); VecSetValue(v, 3, 4, INSERT_VALUES);CHKERRQ(ierr);
-  }
-  else if (rank == 2) {
-    PetscInt idxm[4] = {4, 5, 6, 7};
+  } else if (rank == 2) {
+    PetscInt    idxm[4] = {4, 5, 6, 7};
     PetscScalar values[12] = {1, 2, 0, 1, 2, 1, 1, 3, 0, 1, 3, 1};
 
     ierr = MatSetValues(m, 4, idxm, 3, idxn, values, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 4, 5, INSERT_VALUES); VecSetValue(v, 5, 6, INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(v, 6, 7, INSERT_VALUES); VecSetValue(v, 7, 8, INSERT_VALUES);CHKERRQ(ierr);
   }
-
   ierr = MatAssemblyBegin(m, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(m, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = VecAssemblyBegin(v);CHKERRQ(ierr);
@@ -80,27 +67,14 @@ int main(int argc, char** argv)
   Vec            v, a;
   KSP            QRsolver;
   PC             pc;
-  PetscViewer    viewerQ, viewerV;
-  PetscBool      load;
   PetscErrorCode ierr;
 
-  ierr = PetscInitialize(&argc, &argv, NULL, NULL);CHKERRQ(ierr);
+  ierr = PetscInitialize(&argc, &argv, NULL, NULL);if (ierr) return ierr;
 
   ierr = VecCreate(PETSC_COMM_WORLD, &v);CHKERRQ(ierr);
   ierr = MatCreate(PETSC_COMM_WORLD, &Q);CHKERRQ(ierr);
   ierr = MatSetType(Q, MATDENSE);CHKERRQ(ierr);
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD, "matrixQ", FILE_MODE_READ, &viewerQ);CHKERRQ(ierr);
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD, "in", FILE_MODE_READ, &viewerV);CHKERRQ(ierr);
-
-  ierr = PetscOptionsHasName(NULL, NULL, "-load", &load);CHKERRQ(ierr);
-  if (load) {
-    ierr = MatLoad(Q, viewerQ);CHKERRQ(ierr);
-    ierr = VecLoad(v, viewerV);CHKERRQ(ierr);
-  } else {
-    ierr = fill(Q, v);CHKERRQ(ierr);
-  }
-  ierr = PetscViewerDestroy(&viewerQ);CHKERRQ(ierr);
-  ierr = PetscViewerDestroy(&viewerV);CHKERRQ(ierr);
+  ierr = fill(Q, v);CHKERRQ(ierr);
 
   ierr = MatCreateVecs(Q, &a, NULL);CHKERRQ(ierr);
   ierr = KSPCreate(PETSC_COMM_WORLD, &QRsolver);CHKERRQ(ierr);
