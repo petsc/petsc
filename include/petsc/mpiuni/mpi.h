6f8ba5c8eaa3df06fc35c6211e74915992ef9979
Barry Smith <bsmith@mcs.anl.gov>
2017-10-24 18:45:27 -0500

    add most basic Fortran datatypes to C include file so that PetscMPIFortranDatatypeToC() works with MPI uni
    Commit-type: bug-fix
    Reported-by: travis

--- include/petsc/mpiuni/mpi.h
+++ include/petsc/mpiuni/mpi.h
@@ -236,6 +236,11 @@ typedef int MPI_Datatype;
 #define MPI_SHORT_INT          (13 << 20 | 1 << 8 | (int)(sizeof(short) + sizeof(int)))
 #define MPI_2INT               (14 << 20 | 1 << 8 | (int)(2*sizeof(int)))
 
+/* Fortran datatypes; Jed Brown says they should be defined here */
+#define MPI_INTEGER MPI_INT
+#define MPI_DOUBLE_PRECISION MPI_DOUBLE
+#define MPI_COMPLEX16 MPI_C_DOUBLE_COMPLEX
+
 #define MPI_ORDER_C            0
 #define MPI_ORDER_FORTRAN      1
 
