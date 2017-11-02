75b4073dc30b4dcec907764fef9d3f4c10d4d3ee
Pieter Ghysels <pghysels@lbl.gov>
2017-11-02 14:18:06 -0700

    Add extra download link, not requiring git.

--- config/BuildSystem/config/packages/strumpack.py
+++ config/BuildSystem/config/packages/strumpack.py
@@ -5,7 +5,7 @@ class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
     self.gitcommit        = 'v2.1.0'
-    self.download         = ['git://https://github.com/pghysels/STRUMPACK']
+    self.download         = ['git://https://github.com/pghysels/STRUMPACK','http://portal.nersc.gov/project/sparse/strumpack/strumpack-2.1.0.tar.gz']
     self.functions        = ['STRUMPACK_init']
     self.includes         = ['StrumpackSparseSolver.h']
     self.liblist          = [['libstrumpack.a']]
