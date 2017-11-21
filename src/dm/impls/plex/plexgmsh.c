da83f57b7d7a8ce0f6c53f34cec94a0034ada17c
Lisandro Dalcin <dalcinl@gmail.com>
2017-11-21 20:36:32 +0300

    DMPlex: Fix Gmsh reader to discard optional transforms in periodic section

--- src/dm/impls/plex/plexgmsh.c
+++ src/dm/impls/plex/plexgmsh.c
@@ -323,19 +323,20 @@ PetscErrorCode DMPlexCreateGmsh(MPI_Comm comm, PetscViewer viewer, PetscBool int
       snum = sscanf(line, "%d", &numPeriodic);
       if (snum != 1) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "File is not a valid Gmsh file");
       for (i = 0; i < numPeriodic; i++) {
-        int j, edim = -1, slaveTag = -1, masterTag = -1, nNodes;
+        int j, edim = -1, slaveTag = -1, masterTag = -1, nNodes, slaveNode, masterNode;
 
         ierr = PetscViewerRead(viewer, line, 3, NULL, PETSC_STRING);CHKERRQ(ierr);
         snum = sscanf(line, "%d %d %d", &edim, &slaveTag, &masterTag); /* slaveTag and masterTag are unused */
         if (snum != 3) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "File is not a valid Gmsh file");
-        /* Type of tranformation, discarded */
-        ierr = PetscViewerRead(viewer, line, -PETSC_MAX_PATH_LEN, NULL, PETSC_STRING);CHKERRQ(ierr);
         ierr = PetscViewerRead(viewer, line, 1, NULL, PETSC_STRING);CHKERRQ(ierr);
         snum = sscanf(line, "%d", &nNodes);
-        if (snum != 1) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "File is not a valid Gmsh file");
+        if (snum != 1) { /* discard tranformation and try again */
+          ierr = PetscViewerRead(viewer, line, -PETSC_MAX_PATH_LEN, NULL, PETSC_STRING);CHKERRQ(ierr);
+          ierr = PetscViewerRead(viewer, line, 1, NULL, PETSC_STRING);CHKERRQ(ierr);
+          snum = sscanf(line, "%d", &nNodes);
+          if (snum != 1) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "File is not a valid Gmsh file");
+        }
         for (j = 0; j < nNodes; j++) {
-          int slaveNode, masterNode;
-
           ierr = PetscViewerRead(viewer, line, 2, NULL, PETSC_STRING);CHKERRQ(ierr);
           snum = sscanf(line, "%d %d", &slaveNode, &masterNode);
           if (snum != 2) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "File is not a valid Gmsh file");
