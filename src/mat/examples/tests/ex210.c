petsc/petsc

commit 165cd838033bf79256338c57fa4a1fc5d170f14c

Actually provide the local to global mapping and fix problems with MATNEST
Note that src/mat/examples/tests/ runex23_2 23_3 23_4 are broken, I think the code
was always broken but the addition of the local to global mapping in the MatConvert()
exposed the bugs

Updated by Hong: MATNEST has been fixed in a separate branch, and ex23 is not broken anymore.

Commit-type: bug-fix
Funded-by: ECP
Project: Ellpack format for KNL
Time:   .2  hours

src/mat/impls/nest/matnest.c
@@ -1149,7 +1149,7 @@ static PetscErrorCode MatNestCreateAggregateL2G_Private(Mat A,PetscInt n,const I
   }
   if (flg) {
     ierr = PetscMalloc1(m,&ix);CHKERRQ(ierr);
-    for (i=0,n=0; i<n; i++) {
+    for (i=0,m=0; i<n; i++) {
       ISLocalToGlobalMapping smap = NULL;
       VecScatter             scat;
       IS                     isreq;
@@ -1158,7 +1158,7 @@ static PetscErrorCode MatNestCreateAggregateL2G_Private(Mat A,PetscInt n,const I
       Mat sub;
 
       if (sizeof(*x) != sizeof(PetscScalar)) SETERRQ(PetscObjectComm((PetscObject)A),PETSC_ERR_SUP,"No support when scalars smaller than integers");
-      if (colflg) {
+      if (!colflg) {
         ierr = MatNestFindNonzeroSubMatRow(A,i,&sub);CHKERRQ(ierr);
       } else {
         ierr = MatNestFindNonzeroSubMatCol(A,i,&sub);CHKERRQ(ierr);
@@ -1171,6 +1171,7 @@ static PetscErrorCode MatNestCreateAggregateL2G_Private(Mat A,PetscInt n,const I
       }
       for (j=0; j<mi; j++) ix[m+j] = j;
       if (smap) {ierr = ISLocalToGlobalMappingApply(smap,mi,ix+m,ix+m);CHKERRQ(ierr);}
+
       /*
         Now we need to extract the monolithic global indices that correspond to the given split global indices.
         In many/most cases, we only want MatGetLocalSubMatrix() to work, in which case we only need to know the size of the local spaces.

commit 191fd14bb204acae2656edeac823bd50b9fb2422

165cd838 broke MatCreateNest for us, because it assumes that the row and col local to global maps are the same.
Commit-type: bug-fix
Reported-by: Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>

src/mat/impls/nest/matnest.c
@@ -1163,7 +1163,13 @@ static PetscErrorCode MatNestCreateAggregateL2G_Private(Mat A,PetscInt n,const I
       } else {
         ierr = MatNestFindNonzeroSubMatCol(A,i,&sub);CHKERRQ(ierr);
       }
-      if (sub) {ierr = MatGetLocalToGlobalMapping(sub,&smap,NULL);CHKERRQ(ierr);}
+      if (sub) {
+        if (!colflg) {
+          ierr = MatGetLocalToGlobalMapping(sub,&smap,NULL);CHKERRQ(ierr);
+        } else {
+          ierr = MatGetLocalToGlobalMapping(sub,NULL,&smap);CHKERRQ(ierr);
+        }
+      }
       if (islocal[i]) {
         ierr = ISGetSize(islocal[i],&mi);CHKERRQ(ierr);
       } else {

src/mat/examples/tests/ex210.c
@@ -1,0 +1,50 @@
+static char help[] = "Test MatCreateNest with block sizes.\n";
+
+#include <petscmat.h>
+
+int main(int argc, char **argv)
+{
+ Mat                    A, B, C, mats[2];
+ ISLocalToGlobalMapping cmap, rmap;
+ const PetscInt         indices[1] = {0};
+ PetscMPIInt            size;
+ PetscErrorCode         ierr;
+
+ ierr = PetscInitialize(&argc, &argv, NULL, help); if (ierr) return ierr;
+ ierr = MPI_Comm_size(PETSC_COMM_WORLD, &size);CHKERRQ(ierr);
+ if (size > 1) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_ARG_WRONG,"Only coded for one process");
+ ierr = MatCreate(PETSC_COMM_WORLD, &A);CHKERRQ(ierr);
+ ierr = MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, 1, 2);CHKERRQ(ierr);
+ ierr = MatSetBlockSizes(A, 1, 2);CHKERRQ(ierr);
+ ierr = MatSetType(A,MATAIJ);CHKERRQ(ierr);
+ ierr = ISLocalToGlobalMappingCreate(PETSC_COMM_WORLD, 2, 1, indices,PETSC_COPY_VALUES, &cmap);CHKERRQ(ierr);
+ ierr = ISLocalToGlobalMappingCreate(PETSC_COMM_WORLD, 1, 1, indices,PETSC_COPY_VALUES, &rmap);CHKERRQ(ierr);
+ ierr = MatSetLocalToGlobalMapping(A, rmap, cmap);CHKERRQ(ierr);
+ ierr = MatCreate(PETSC_COMM_WORLD, &B);CHKERRQ(ierr);
+ ierr = MatSetSizes(B, PETSC_DECIDE, PETSC_DECIDE, 1, 1);CHKERRQ(ierr);
+ ierr = MatSetBlockSizes(A, 1, 1);CHKERRQ(ierr);
+ ierr = MatSetType(B,MATAIJ);CHKERRQ(ierr);
+ ierr = MatSetLocalToGlobalMapping(B, rmap, rmap);CHKERRQ(ierr);
+ ierr = ISLocalToGlobalMappingDestroy(&rmap);CHKERRQ(ierr);
+ ierr = ISLocalToGlobalMappingDestroy(&cmap);CHKERRQ(ierr);
+ ierr = MatSetUp(A);CHKERRQ(ierr);
+ ierr = MatSetUp(B);CHKERRQ(ierr);
+ mats[0] = A;
+ mats[1] = B;
+ ierr = MatCreateNest(PETSC_COMM_WORLD, 1, NULL, 2, NULL, mats,&C);CHKERRQ(ierr);
+ ierr = MatSetUp(C);CHKERRQ(ierr);
+ ierr = MatAssemblyBegin(C, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+ ierr = MatAssemblyEnd(C, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+ ierr = MatView(C, NULL);CHKERRQ(ierr);
+ ierr = MatDestroy(&C);CHKERRQ(ierr);
+ ierr = MatDestroy(&B);CHKERRQ(ierr);
+ ierr = MatDestroy(&A);CHKERRQ(ierr);
+ ierr = PetscFinalize();
+ return ierr;
+}
+
+/*TEST
+
+    test:
+
+TEST*/

src/mat/examples/tests/makefile
@@ -23,7 +23,7 @@ EXAMPLESC	 = ex1.c ex2.c ex3.c ex4.c ex5.c ex6.c ex7.c ex8.c ex9.c \
                 ex143.c ex144.c ex145.c ex146.c ex147.c ex148.c ex149.c \
                 ex150.c ex151.c ex152.c ex153.c ex155.c ex157.c ex158.c ex159.c ex164.c ex169.c ex171.c ex172.c ex173.c ex174.cxx ex175.c ex180.c \
                 ex181.c ex182.c ex183.c ex300.c ex190.c ex191.c ex192.c ex193.c ex194.c ex195.c ex197.c ex198.c ex199.c ex200.c \
-                ex202.c ex203.c ex205.c ex206.c ex207.c ex208.c ex209.c
+                ex202.c ex203.c ex205.c ex206.c ex207.c ex208.c ex209.c ex210.c
 
 EXAMPLESF	 = ex16f90.F90 ex36f.F ex58f.F ex63f.F ex67f.F ex79f.F90 ex85f.F ex105f.F ex120f.F ex126f.F ex171f.F ex196f90.F90 ex201f.F
 
@@ -816,6 +816,10 @@ ex208: ex208.o chkopts
 ex209: ex209.o chkopts
 	-${CLINKER} -o ex209 ex209.o ${PETSC_MAT_LIB}
 	${RM} ex209.o
+
+ex210: ex210.o chkopts
+	-${CLINKER} -o ex210 ex210.o ${PETSC_MAT_LIB}
+	${RM} ex210.o
 
 #-----------------------------------------------------------------------------
 NPROCS    = 1 3

src/mat/examples/tests/output/ex210_1.out
@@ -1,0 +1,7 @@
+Mat Object: 1 MPI processes
+  type: nest
+  Matrix object: 
+    type=nest, rows=1, cols=2 
+    MatNest structure: 
+    (0,0) : type=seqaij, rows=1, cols=2 
+    (0,1) : type=seqaij, rows=1, cols=1 