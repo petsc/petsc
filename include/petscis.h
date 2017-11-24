9e436492a5f3dfdbfdb4be14586e1a8ebf1e4e34
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2017-11-24 17:20:37 +0100

    PetscSectionCompare implemented

--- include/petscis.h
+++ include/petscis.h
@@ -307,6 +307,7 @@ PETSC_EXTERN PetscClassId PETSC_SECTION_CLASSID;
 PETSC_EXTERN PetscErrorCode PetscSectionCreate(MPI_Comm,PetscSection*);
 PETSC_EXTERN PetscErrorCode PetscSectionClone(PetscSection, PetscSection*);
 PETSC_EXTERN PetscErrorCode PetscSectionCopy(PetscSection, PetscSection);
+PETSC_EXTERN PetscErrorCode PetscSectionCompare(PetscSection, PetscSection, PetscBool*);
 PETSC_EXTERN PetscErrorCode PetscSectionGetNumFields(PetscSection, PetscInt *);
 PETSC_EXTERN PetscErrorCode PetscSectionSetNumFields(PetscSection, PetscInt);
 PETSC_EXTERN PetscErrorCode PetscSectionGetFieldName(PetscSection, PetscInt, const char *[]);
