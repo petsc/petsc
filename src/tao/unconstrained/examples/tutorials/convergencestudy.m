59c6c28c994caeca3ade60471b84a952e2d45834
Barry Smith <bsmith@mcs.anl.gov>
2017-11-27 17:44:45 -0600

    Modify convergencestudy.m to match font and line style of other figures

--- src/tao/unconstrained/examples/tutorials/convergencestudy.m
+++ src/tao/unconstrained/examples/tutorials/convergencestudy.m
@@ -1,7 +1,14 @@
+fontsize_labels = 14;
+fontsize_grid   = 12;
+fontname = 'Times';
 
 h = zeros(5,1);
 e = zeros(5,1);
 hold off
+clf
+set(gca,'FontName',fontname)
+set(gca,'FontSize',fontsize_grid)
+set(gca,'FontSize',fontsize_labels)
 for i=1:5
   E = 8*2^i;
   syscommand = compose('./spectraladjointassimilation  -ts_adapt_dt_max 3.e-3 -E %d -N 2 -ncoeff 5  -a .1 -tao_grtol 1.e-12  -tao_gatol 1.e-12  -tao_max_it 7',[E])
@@ -12,27 +19,33 @@ for i=1:5
   e(i) = E;
   if (mod(i,2) == 1)
     yyaxis left
-    semilogy(history(:,1),history(:,2));
+    semilogy(history(:,1),history(:,2),'Markersize',6,'LineWidth',2);
     ylabel('Continuum Error');
     yyaxis right
-    semilogy(history(:,1),history(:,3));
+    semilogy(history(:,1),history(:,3),'Markersize',6,'LineWidth',2);
     ylabel('Objective function');
     hold on
   end
 end
 legend('16 elements','64 elements','256 elements','16 elements','64 elements','256 elements')
 xlabel('Iteration')
-print('convergencestudy-h','-dpdf');
+print('convergencestudy-h','-depsc');
 
 hold off
 clf
-loglog(e,h,'-+');
+set(gca,'FontName',fontname)
+set(gca,'FontSize',fontsize_grid)
+set(gca,'FontSize',fontsize_labels)
+loglog(e,h,'-+','Markersize',6,'LineWidth',2);
 xlabel('1/h');
 ylabel('Continuum Error');
-print('convergencestudy-h-2','-dpdf');
+print('convergencestudy-h-2','-depsc');
 
 
 clf
+set(gca,'FontName',fontname)
+set(gca,'FontSize',fontsize_grid)
+set(gca,'FontSize',fontsize_labels)
 h = zeros(4,1);
 e = zeros(4,1);
 for i=1:4
@@ -45,20 +58,23 @@ history
   [m,n] = size(history);
   h(i) = history(m,2)
   yyaxis left
-  semilogy(history(:,1),history(:,2));
+  semilogy(history(:,1),history(:,2),'Markersize',6,'LineWidth',2);
   ylabel('Continuum Error');
   yyaxis right
-  semilogy(history(:,1),history(:,3));
+  semilogy(history(:,1),history(:,3),'Markersize',6,'LineWidth',2);
   ylabel('Objective function');
   hold on
 end
 legend('Order 4','Order 7','Order 10','Order 13','Order 4','Order 7','Order 10','Order 13')
 xlabel('Iteration')
-print('convergencestudy-p','-dpdf');
+print('convergencestudy-p','-depsc');
 
 hold off
 clf
-loglog(e,h,'-+');
+set(gca,'FontName',fontname)
+set(gca,'FontSize',fontsize_grid)
+set(gca,'FontSize',fontsize_labels)
+loglog(e,h,'-+','Markersize',6,'LineWidth',2);
 xlabel('Polynomial order');
 ylabel('Continuum Error');
-print('convergencestudy-p-2','-dpdf');
+print('convergencestudy-p-2','-depsc');
