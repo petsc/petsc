291493fa95b0cf8deab175d795f0aa945aaf2a83
Hong Zhang <hongzhang@anl.gov>
2017-11-29 15:40:41 -0600

    Fix the warning caught by nightly test
    Funded-by:
    Project: PETSc for Theta
    Time: 0.1 hours
    Reported-by:
    Thanks-to:

--- src/mat/impls/sell/seq/sell.h
+++ src/mat/impls/sell/seq/sell.h
@@ -36,7 +36,7 @@ PetscBool   pivotinblocks;     /* pivot inside factorization of each diagonal bl
 Mat         parent;            /* set if this matrix was formed with MatDuplicate(...,MAT_SHARE_NONZERO_PATTERN,....);
 means that this shares some data structures with the parent including diag, ilen, imax, i, j */ \
 PetscInt    *sliidx;           /* slice index */ \
-PetscInt    totalslices;       /* total number of slices */ \
+PetscInt    totalslices       /* total number of slices */ \
 
 typedef struct {
   SEQSELLHEADER(MatScalar);
