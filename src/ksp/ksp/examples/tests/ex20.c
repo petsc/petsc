216bba129553a9d342a598a64e2b7a4467a0e5f7
Satish Balay <balay@mcs.anl.gov>
2017-11-30 22:50:31 -0600

    test: more 64bit-idx fixes

--- src/ksp/ksp/examples/tests/ex20.c
+++ src/ksp/ksp/examples/tests/ex20.c
@@ -17,14 +17,16 @@ int FormElementStiffness(PetscReal H,PetscScalar *Ke)
 
 int main(int argc,char **args)
 {
-  Mat          C;
-  int          i,m = 5,rank,size,N,start,end,M;
-  int          ierr,idx[4];
-  PetscScalar  Ke[16];
-  PetscReal    h;
-  Vec          u,b;
-  KSP          ksp;
-  MatNullSpace nullsp;
+  Mat            C;
+  PetscInt       i,m = 5,N,start,end,M;
+  PetscMPIInt    rank,size;
+  PetscInt       idx[4];
+  PetscErrorCode ierr;
+  PetscScalar    Ke[16];
+  PetscReal      h;
+  Vec            u,b;
+  KSP            ksp;
+  MatNullSpace   nullsp;
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-m",&m,NULL);CHKERRQ(ierr);
