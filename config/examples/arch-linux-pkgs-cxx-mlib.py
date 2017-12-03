1951a3a95db49bfe714855315527fe74aa3d4cf1
Stefano Zampini <stefano.zampini@gmail.com>
2017-12-03 10:41:25 +0300

    Add CTETGEN to testing suites

--- config/examples/arch-linux-pkgs-cxx-mlib.py
+++ config/examples/arch-linux-pkgs-cxx-mlib.py
@@ -17,6 +17,7 @@ configure_options = [
   '--download-scalapack=1',
   '--download-mumps=1',
   '--download-parms=1',
+  '--download-ctetgen=1',
   #'--download-elemental=1',
   #'--with-cxx-dialect=C++11', hdf5 conflicts with C++11
   '--download-spai=1',
--- config/examples/arch-osx-10.6-cxx-pkgs-opt.py
+++ config/examples/arch-osx-10.6-cxx-pkgs-opt.py
@@ -36,6 +36,7 @@ configure_options = [
   '--download-moab=1',
   '--download-saws',
   '--download-revolve=1',
+  '--download-ctetgen=1',
   ]
 
 if __name__ == '__main__':
