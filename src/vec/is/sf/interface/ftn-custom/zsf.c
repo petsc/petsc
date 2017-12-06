7f139299d27ee13a3a9465b4301f204a75546a06
Barry Smith <bsmith@mcs.anl.gov>
2017-12-05 20:09:11 -0600

    Fix petscsfgetgraph_ to work for all Fortran compilers by directly using array of PetscSFNodes
    Commit-type: bug-fix, portability-fix, testing-fix
    Reported-by: nightly tests

--- src/vec/is/sf/interface/ftn-custom/zsf.c
+++ src/vec/is/sf/interface/ftn-custom/zsf.c
@@ -2,16 +2,20 @@
 #include <petsc/private/sfimpl.h>
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
-#define petscsfview_       PETSCSFVIEW
-#define petscsfgetgraph_   PETSCSFGETGRAPH
-#define petscsfbcastbegin_ PETSCSFBCASTBEGIN
-#define petscsfbcastend_   PETSCSFBCASTEND
+#define petscsfview_          PETSCSFVIEW
+#define petscsfgetgraph_      PETSCSFGETGRAPH
+#define petscsfbcastbegin_    PETSCSFBCASTBEGIN
+#define petscsfbcastend_      PETSCSFBCASTEND
+#define f90arraysfnodecreate_ F90ARRAYSFNODECREATE
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
-#define petscsfgetgraph_   petscsfgetgraph
-#define petscsfview_       petscsfview
-#define petscsfbcastbegin_ petscsfbcastbegin
-#define petscsfbcastend_   petscsfbcastend
+#define petscsfgetgraph_      petscsfgetgraph
+#define petscsfview_          petscsfview
+#define petscsfbcastbegin_    petscsfbcastbegin
+#define petscsfbcastend_      petscsfbcastend
+#define f90arraysfnodecreate_ f90arraysfnodecreate
 #endif
+
+PETSC_EXTERN void PETSC_STDCALL f90arraysfnodecreate_(const PetscInt *,PetscInt *,void * PETSC_F90_2PTR_PROTO_NOVAR);
 
 PETSC_EXTERN void PETSC_STDCALL petscsfview_(PetscSF *sf, PetscViewer *vin, PetscErrorCode *ierr)
 {
@@ -21,6 +25,7 @@ PETSC_EXTERN void PETSC_STDCALL petscsfview_(PetscSF *sf, PetscViewer *vin, Pets
   *ierr = PetscSFView(*sf, v);
 }
 
+
 PETSC_EXTERN void PETSC_STDCALL  petscsfgetgraph_(PetscSF *sf,PetscInt *nroots,PetscInt *nleaves, F90Array1d  *ailocal, F90Array1d  *airemote, int *ierr PETSC_F90_2PTR_PROTO(pilocal) PETSC_F90_2PTR_PROTO(piremote))
 {
   const PetscInt    *ilocal;
@@ -28,8 +33,8 @@ PETSC_EXTERN void PETSC_STDCALL  petscsfgetgraph_(PetscSF *sf,PetscInt *nroots,P
 
   *ierr = PetscSFGetGraph(*sf,nroots,nleaves,&ilocal,&iremote);if (*ierr) return;
   *ierr = F90Array1dCreate((void*)ilocal,MPIU_INT,1,*nleaves, ailocal PETSC_F90_2PTR_PARAM(pilocal));
-  /* this is iffy, since airemote is actually an array of type(PetscSFNode) not an array of PetscInt; works with gfortran, needs testing with all other compilers */
-  *ierr = F90Array1dCreate((void*)iremote,MPIU_INT,1,2*(*nleaves), airemote PETSC_F90_2PTR_PARAM(pilocal));
+  /* this creates a memory leak */
+  f90arraysfnodecreate_((PetscInt*)iremote,nleaves, airemote PETSC_F90_2PTR_PARAM(pairemote));
 }
 
 PETSC_EXTERN void PETSC_STDCALL petscsfbcastbegin_(PetscSF *sf, MPI_Fint *unit,F90Array1d *rptr, F90Array1d *lptr, int *ierr PETSC_F90_2PTR_PROTO(rptrd) PETSC_F90_2PTR_PROTO(lptrd))
