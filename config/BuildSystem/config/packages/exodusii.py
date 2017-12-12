40e244fa748d583508e8da43a227b638c1968ab6
Blaise Bourdin <bourdin@lsu.edu>
2017-12-12 10:08:07 -0500

    BuildSystem: Bump ExodusII version

--- config/BuildSystem/config/packages/exodusii.py
+++ config/BuildSystem/config/packages/exodusii.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit         = '3bb02f49dab4ee827cfb538173daf11eabc5815f'
+    self.gitcommit         = '840b0e38a7390ecd53614f68b9b5def6b4a3fbd2'
     self.download          = ['git://https://github.com/gsjaardema/seacas.git','https://github.com/gsjaardema/seacas/archive/'+self.gitcommit+'.tar.gz']
     self.downloaddirnames  = ['seacas']
     self.functions         = ['ex_close']
