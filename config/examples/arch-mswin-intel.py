153d50c25af349deb3babdeb9977d7ea320a8fde
Satish Balay <balay@mcs.anl.gov>
2017-12-14 14:45:55 -0600

    win: workarround intel compiler warning (bug?)
    C:\cygwin64\home\petsc\PETSC~2.NEX\src\snes\examples\tests\ex4.c(152): warning #264: floating-point value does not fit in required floating-point type
        if (cnt++ == infatcount) *f = INFINITY;
                                      ^

--- config/examples/arch-mswin-intel.py
+++ config/examples/arch-mswin-intel.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     '--with-cc=win32fe icl',
     '--with-cxx=win32fe icl',
     '--with-fc=win32fe ifort',
+    'CFLAGS=-MT -wd264', # using -wd264 to suppress warnings from icl from using INFINITY in src/snes/examples/tests/ex4.c
     '--with-mpi-include=[/cygdrive/c/PROGRA~2/MICROS~2/MPI/Include/,/cygdrive/c/PROGRA~2/MICROS~2/MPI/Include/x64]',
     '--with-mpi-lib=[/cygdrive/c/PROGRA~2/MICROS~2/MPI/lib/x64/msmpifec.lib,/cygdrive/c/PROGRA~2/MICROS~2/MPI/lib/x64/msmpi.lib]',
     '--with-mpiexec=/cygdrive/c/PROGRA~1/MICROS~2/Bin/mpiexec',
