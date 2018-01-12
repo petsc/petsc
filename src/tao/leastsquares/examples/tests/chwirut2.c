cbba38c36f5e6c155b5b8942adfb6067d31267cc
Barry Smith <bsmith@mcs.anl.gov>
2018-01-12 09:47:09 -0600

    don't compile example with single or complex
    Commit-type: portability-fix, testing-fix
    Reported-by: nightly tests

--- src/tao/leastsquares/examples/tests/chwirut2.c
+++ src/tao/leastsquares/examples/tests/chwirut2.c
@@ -432,17 +432,21 @@ PetscErrorCode InitializeData(AppCtx *user)
 
      test:
        args:  -tao_smonitor -tao_max_it 100 -tao_type pounders -tao_pounders_delta 0.05
+       requires: !single !complex
 
      test:
        suffix: 2
        args: -tao_smonitor -tao_max_it 100 -wtype 1 -tao_type pounders -tao_pounders_delta 0.05
+       requires: !single !complex
 
      test:
        suffix: 3
        args: -tao_smonitor -tao_max_it 100 -wtype 2 -tao_type pounders -tao_pounders_delta 0.05
+       requires: !single !complex
 
      test:
        suffix: 4
        args: -tao_smonitor -tao_max_it 100 -tao_type pounders -tao_pounders_delta 0.05 -pounders_subsolver_tao_type blmvm
+       requires: !single !complex
 
  TEST*/
