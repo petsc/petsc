e68ccae8c211ef21b0d8e1e4de86b470ac8cefa3
Barry Smith <bsmith@mcs.anl.gov>
2018-01-13 08:34:02 -0600

    Cleanup Radau5 code, add error checking, documentation
    Commit-type: style-fix, documentation

--- src/ts/impls/implicit/radau5/radau5.c
+++ src/ts/impls/implicit/radau5/radau5.c
@@ -8,107 +8,69 @@ typedef struct {
   Vec work,workf;
 } TS_Radau5;
 
-#ifdef foo
-/*
-        TSFunction_Radau5 - routine that we provide to Radau5 that applies the right hand side.
-*/
-int TSFunction_Radau5(realtype t,N_Vector y,N_Vector ydot,void *ctx)
+void FVPOL(int *N,double *X,double *Y,double *F,double *RPAR,void *IPAR)
 {
-  TS             ts = (TS) ctx;
+  TS             ts = (TS) IPAR;
+  TS_Radau5      *cvode = (TS_Radau5*)ts->data;
   DM             dm;
   DMTS           tsdm;
   TSIFunction    ifunction;
-  MPI_Comm       comm;
-  TS_Radau5    *cvode = (TS_Radau5*)ts->data;
-  Vec            yy     = cvode->w1,yyd = cvode->w2,yydot = cvode->ydot;
-  PetscScalar    *y_data,*ydot_data;
   PetscErrorCode ierr;
 
-  PetscFunctionBegin;
-  ierr = PetscObjectGetComm((PetscObject)ts,&comm);CHKERRQ(ierr);
-  /* Make the PETSc work vectors yy and yyd point to the arrays in the RADAU5 vectors y and ydot respectively*/
-  y_data    = (PetscScalar*) N_VGetArrayPointer(y);
-  ydot_data = (PetscScalar*) N_VGetArrayPointer(ydot);
-  ierr      = VecPlaceArray(yy,y_data);CHKERRABORT(comm,ierr);
-  ierr      = VecPlaceArray(yyd,ydot_data);CHKERRABORT(comm,ierr);
+  ierr = VecPlaceArray(cvode->work,Y);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecPlaceArray(cvode->workf,F);CHKERRABORT(PETSC_COMM_SELF,ierr);
 
   /* Now compute the right hand side function, via IFunction unless only the more efficient RHSFunction is set */
-  ierr = TSGetDM(ts,&dm);CHKERRQ(ierr);
-  ierr = DMGetDMTS(dm,&tsdm);CHKERRQ(ierr);
-  ierr = DMTSGetIFunction(dm,&ifunction,NULL);CHKERRQ(ierr);
+  ierr = TSGetDM(ts,&dm);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = DMGetDMTS(dm,&tsdm);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = DMTSGetIFunction(dm,&ifunction,NULL);CHKERRABORT(PETSC_COMM_SELF,ierr);
   if (!ifunction) {
-    ierr = TSComputeRHSFunction(ts,t,yy,yyd);CHKERRQ(ierr);
-  } else {                      /* If rhsfunction is also set, this computes both parts and shifts them to the right */
-    ierr = VecZeroEntries(yydot);CHKERRQ(ierr);
-    ierr = TSComputeIFunction(ts,t,yy,yydot,yyd,PETSC_FALSE);CHKERRABORT(comm,ierr);
-    ierr = VecScale(yyd,-1.);CHKERRQ(ierr);
-  }
-  ierr = VecResetArray(yy);CHKERRABORT(comm,ierr);
-  ierr = VecResetArray(yyd);CHKERRABORT(comm,ierr);
-  PetscFunctionReturn(0);
-}
-#endif
-
-void FVPOL(int *N,double *X,double *Y,double *F,double *RPAR,void *IPAR)
-{
-  TS          ts = (TS) IPAR;
-  TS_Radau5   *cvode = (TS_Radau5*)ts->data;
-  DM          dm;
-  DMTS        tsdm;
-  TSIFunction ifunction;
-
-  VecPlaceArray(cvode->work,Y);
-  VecPlaceArray(cvode->workf,F);
-
-  /* Now compute the right hand side function, via IFunction unless only the more efficient RHSFunction is set */
-  TSGetDM(ts,&dm);
-  DMGetDMTS(dm,&tsdm);
-  DMTSGetIFunction(dm,&ifunction,NULL);
-  if (!ifunction) {
-    TSComputeRHSFunction(ts,*X,cvode->work,cvode->workf);
-  } else {                      /* If rhsfunction is also set, this computes both parts and shifts them to the right */
+    ierr = TSComputeRHSFunction(ts,*X,cvode->work,cvode->workf);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  } else {       /* If rhsfunction is also set, this computes both parts and scale them to the right hand side */
     Vec yydot;
 
-    VecDuplicate(cvode->work,&yydot);
-    VecZeroEntries(yydot);
-    TSComputeIFunction(ts,*X,cvode->work,yydot,cvode->workf,PETSC_FALSE);
-    VecScale(cvode->workf,-1.);
-    VecDestroy(&yydot);
+    ierr = VecDuplicate(cvode->work,&yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
+    ierr = VecZeroEntries(yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
+    ierr = TSComputeIFunction(ts,*X,cvode->work,yydot,cvode->workf,PETSC_FALSE);CHKERRABORT(PETSC_COMM_SELF,ierr);
+    ierr = VecScale(cvode->workf,-1.);CHKERRABORT(PETSC_COMM_SELF,ierr);
+    ierr = VecDestroy(&yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
   }
 
-  VecResetArray(cvode->work);
-  VecResetArray(cvode->workf);
+  ierr = VecResetArray(cvode->work);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecResetArray(cvode->workf);CHKERRABORT(PETSC_COMM_SELF,ierr);
 }
 
 void JVPOL(PetscInt *N,PetscScalar *X,PetscScalar *Y,PetscScalar *DFY,int *LDFY,PetscScalar *RPAR,void *IPAR)
 {
-  TS          ts = (TS) IPAR;
-  TS_Radau5   *cvode = (TS_Radau5*)ts->data;
-  Vec         yydot;
-  Mat         mat;
-  PetscInt    n;
+  TS             ts = (TS) IPAR;
+  TS_Radau5      *cvode = (TS_Radau5*)ts->data;
+  Vec            yydot;
+  Mat            mat;
+  PetscInt       n;
+  PetscErrorCode ierr;
 
-  VecPlaceArray(cvode->work,Y);
-  VecDuplicate(cvode->work,&yydot);
-  VecGetSize(yydot,&n);
-  MatCreateSeqDense(PETSC_COMM_SELF,n,n,DFY,&mat);
-  VecZeroEntries(yydot);
-  TSComputeIJacobian(ts,*X,cvode->work,yydot,0,mat,mat,PETSC_FALSE);
-  MatScale(mat,-1.0);
-  MatDestroy(&mat);
-  VecDestroy(&yydot);
-  VecResetArray(cvode->work);
+  ierr = VecPlaceArray(cvode->work,Y);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecDuplicate(cvode->work,&yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecGetSize(yydot,&n);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = MatCreateSeqDense(PETSC_COMM_SELF,n,n,DFY,&mat);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecZeroEntries(yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = TSComputeIJacobian(ts,*X,cvode->work,yydot,0,mat,mat,PETSC_FALSE);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = MatScale(mat,-1.0);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = MatDestroy(&mat);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecDestroy(&yydot);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecResetArray(cvode->work);CHKERRABORT(PETSC_COMM_SELF,ierr);
 }
 
 void SOLOUT(int *NR,double *XOLD,double *X, double *Y,double *CONT,double *LRC,int *N,double *RPAR,void *IPAR,int *IRTRN)
 {
-  TS        ts = (TS) IPAR;
-  TS_Radau5 *cvode = (TS_Radau5*)ts->data;
+  TS             ts = (TS) IPAR;
+  TS_Radau5      *cvode = (TS_Radau5*)ts->data;
+  PetscErrorCode ierr;
 
-  VecPlaceArray(cvode->work,Y);
+  ierr = VecPlaceArray(cvode->work,Y);CHKERRABORT(PETSC_COMM_SELF,ierr);
   ts->time_step = *X - *XOLD;
-  TSMonitor(ts,*NR-1,*X,cvode->work);
-  VecResetArray(cvode->work);
+  ierr = TSMonitor(ts,*NR-1,*X,cvode->work);CHKERRABORT(PETSC_COMM_SELF,ierr);
+  ierr = VecResetArray(cvode->work);CHKERRABORT(PETSC_COMM_SELF,ierr);
 }
 
 
@@ -155,11 +117,9 @@ PetscErrorCode TSSolve_Radau5(TS ts)
   /* C --- INITIAL STEP SIZE */
   H = ts->time_step;
 
-  /* output MUJAC MLMAS IDID */
+  /* output MUJAC MLMAS IDID; currently all ignored */
 
-  CHKMEMQ;
   radau5_(&ND,FVPOL,&X,Y,&XEND,&H,&RTOL,&ATOL,&ITOL,JVPOL,&IJAC,&MLJAC,&MUJAC,FVPOL,&IMAS,&MLMAS,&MUMAS,SOLOUT,&IOUT,WORK,&LWORK,IWORK,&LIWORK,&RPAR,(void*)ts,&IDID);
-  CHKMEMQ;
 
   ierr = PetscFree(WORK);CHKERRQ(ierr);
   ierr = PetscFree(IWORK);CHKERRQ(ierr);
@@ -178,34 +138,17 @@ PetscErrorCode TSDestroy_Radau5(TS ts)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode TSSetFromOptions_Radau5(PetscOptionItems *PetscOptionsObject,TS ts)
-{
-  TS_Radau5      *cvode = (TS_Radau5*)ts->data;
-  PetscErrorCode ierr;
-
-  PetscFunctionBegin;
-  ierr = PetscOptionsHead(PetscOptionsObject,"RADAU5 ODE solver options");CHKERRQ(ierr);
-  ierr = PetscOptionsTail();CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-PetscErrorCode TSView_Radau5(TS ts,PetscViewer viewer)
-{
-  TS_Radau5      *cvode = (TS_Radau5*)ts->data;
-  PetscErrorCode ierr;
-
-  PetscFunctionBegin;
-  PetscFunctionReturn(0);
-}
-
 /*MC
       TSRADAU5 - ODE solver using the RADAU5 package
 
-    Notes: This uses its own nonlinear solver and Krylov method so PETSc SNES and KSP options do not apply
+    Notes: This uses its own nonlinear solver and dense matrix direct solver so PETSc SNES and KSP options do not apply.
+           Uses its own time-step adaptivity (but uses the TS rtol and atol, and initial timestep)
+           Uses its own memory for the dense matrix storage and factorization
+           Can only handle ODEs of the form \cdot{u} = -F(t,u) + G(t,u)
 
     Level: beginner
 
-.seealso:  TSCreate(), TS, TSSetType(), TSSetExactFinalTime()
+.seealso:  TSCreate(), TS, TSSetType()
 
 M*/
 PETSC_EXTERN PetscErrorCode TSCreate_Radau5(TS ts)
@@ -215,9 +158,7 @@ PETSC_EXTERN PetscErrorCode TSCreate_Radau5(TS ts)
 
   PetscFunctionBegin;
   ts->ops->destroy        = TSDestroy_Radau5;
-  ts->ops->view           = TSView_Radau5;
   ts->ops->solve          = TSSolve_Radau5;
-  ts->ops->setfromoptions = TSSetFromOptions_Radau5;
   ts->default_adapt_type  = TSADAPTNONE;
 
   ierr = PetscNewLog(ts,&cvode);CHKERRQ(ierr);
