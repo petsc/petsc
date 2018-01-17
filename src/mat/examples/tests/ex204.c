0931adeede478c3bbae5f80a72b85412a42235d4
Karl Rupp <me@karlrupp.net>
2018-01-17 06:52:51 +0100

    ViennaCL: Fixed incorrect object used in one of the tests.
    Commit-type: bug-fix,testing-fix

--- src/mat/examples/tests/ex204.c
+++ src/mat/examples/tests/ex204.c
@@ -82,7 +82,7 @@ int main(int argc,char **args)
 
     ierr = VecDuplicate(r_vcl,&d_vcl);CHKERRQ(ierr);
     ierr = VecCopy(r_vcl,d_vcl);CHKERRQ(ierr);
-    ierr = VecAXPY(d_vcl,-1.0,r);
+    ierr = VecAXPY(d_vcl,-1.0,r_vcl);
     ierr = VecNorm(d_vcl,NORM_INFINITY,&dnorm);CHKERRQ(ierr);
     if (dnorm > tol) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_INCOMP,"Sequential CPU and MPI ViennaCL vector results incompatible with inf norm greater than tolerance of %g",tol);
 
@@ -134,7 +134,7 @@ int main(int argc,char **args)
 
     ierr = VecDuplicate(r_vcl,&d_vcl);CHKERRQ(ierr);
     ierr = VecCopy(r_vcl,d_vcl);CHKERRQ(ierr);
-    ierr = VecAXPY(d_vcl,-1.0,r);
+    ierr = VecAXPY(d_vcl,-1.0,r_vcl);
     ierr = VecNorm(d_vcl,NORM_INFINITY,&dnorm);CHKERRQ(ierr);
     if (dnorm > tol) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_INCOMP,"MPI CPU and MPI ViennaCL Vector results incompatible with inf norm greater than tolerance of %g",tol);
 
@@ -184,7 +184,7 @@ int main(int argc,char **args)
 
     ierr = VecDuplicate(r_vcl,&d_vcl);CHKERRQ(ierr);
     ierr = VecCopy(r_vcl,d_vcl);CHKERRQ(ierr);
-    ierr = VecAXPY(d_vcl,-1.0,r);
+    ierr = VecAXPY(d_vcl,-1.0,r_vcl);
     ierr = VecNorm(d_vcl,NORM_INFINITY,&dnorm);CHKERRQ(ierr);
     if (dnorm > tol) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_INCOMP,"MPI CPU and MPI ViennaCL Vector results incompatible with inf norm greater than tolerance of %g",tol);
 
@@ -236,7 +236,7 @@ int main(int argc,char **args)
 
     ierr = VecDuplicate(r_vcl,&d_vcl);CHKERRQ(ierr);
     ierr = VecCopy(r_vcl,d_vcl);CHKERRQ(ierr);
-    ierr = VecAXPY(d_vcl,-1.0,r);CHKERRQ(ierr);
+    ierr = VecAXPY(d_vcl,-1.0,r_vcl);CHKERRQ(ierr);
     ierr = VecNorm(d_vcl,NORM_INFINITY,&dnorm);CHKERRQ(ierr);
     if (dnorm > tol) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_INCOMP,"MPI CPU and MPI ViennaCL Vector results incompatible with inf norm greater than tolerance of %g",tol);
 
