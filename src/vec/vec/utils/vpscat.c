c07a006ded7da4430d063d3f5dd0f9bf259ef992
Barry Smith <bsmith@mcs.anl.gov>
2018-01-29 09:21:26 -0600

    Cannot PetscIntMultError() to MPI int
    Commit-type: bug-fix, portability-fix
    Reported-by: nightly tests

--- src/vec/vec/utils/vpscat.c
+++ src/vec/vec/utils/vpscat.c
@@ -2559,12 +2559,14 @@ PetscErrorCode VecScatterCreateCommon_PtoS(VecScatter_MPI_General *from,VecScatt
 #endif
 #if defined(PETSC_HAVE_MPI_WIN_CREATE)
   } else if (to->use_window) {
-    PetscMPIInt temptag,winsize = 0;
+    PetscMPIInt temptag,winsize;
+    PetscInt    iwinsize = 0;
     MPI_Request *request;
     MPI_Status  *status;
 
     ierr = PetscObjectGetNewTag((PetscObject)ctx,&temptag);CHKERRQ(ierr);
-    ierr = PetscIntMultError((to->n ? to->starts[to->n] : 0),bs*sizeof(PetscScalar),&winsize);CHKERRQ(ierr);
+    ierr = PetscIntMultError((to->n ? to->starts[to->n] : 0),bs*sizeof(PetscScalar),&iwinsize);CHKERRQ(ierr);
+    ierr = PetscMPIIntCast(iwinsize,&winsize);CHKERRQ(ierr);
     ierr = MPI_Win_create(to->values ? to->values : MPI_BOTTOM,winsize,sizeof(PetscScalar),MPI_INFO_NULL,comm,&to->window);CHKERRQ(ierr);
     ierr = PetscMalloc1(to->n,&to->winstarts);CHKERRQ(ierr);
     ierr = PetscMalloc2(to->n,&request,to->n,&status);CHKERRQ(ierr);
