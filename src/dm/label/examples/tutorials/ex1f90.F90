c3f8afdfff3ac8b33bfa1bad670bd14048d6a60d
Satish Balay <balay@mcs.anl.gov>
2018-02-02 10:26:59 -0600

    fortran: use variables for PetscInt const values

--- src/dm/label/examples/tutorials/ex1f90.F90
+++ src/dm/label/examples/tutorials/ex1f90.F90
@@ -10,6 +10,8 @@ program  ex1f90
   PetscBool           :: interpolate = PETSC_FALSE
   PetscBool           :: flg
   PetscErrorCode      :: ierr
+  PetscInt            :: izero
+  izero = 0
 
   call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
     if (ierr .ne. 0) then
@@ -20,7 +22,7 @@ program  ex1f90
   call PetscOptionsGetBool(PETSC_NULL_OPTIONS,PETSC_NULL_CHARACTER,"-interpolate",interpolate,flg,ierr);CHKERRA(ierr)
 
   call DMPlexCreateFromFile(PETSC_COMM_WORLD,filename,interpolate,dm,ierr);CHKERRA(ierr);
-  call DMPlexDistribute(dm,0,PETSC_NULL_SF,dmDist,ierr);CHKERRA(ierr)
+  call DMPlexDistribute(dm,izero,PETSC_NULL_SF,dmDist,ierr);CHKERRA(ierr)
   if (dmDist%v /= -1) then
     call DMDestroy(dm,ierr);CHKERRA(ierr)
     dm%v = dmDist%v
