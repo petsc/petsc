078b4db875abf23e7cee790f2bbc56545c60e62b
Satish Balay <balay@mcs.anl.gov>
2018-02-05 16:17:05 -0600

    mpich: update to version 3.3b1. This should fix build issues on Mac(static)

--- config/BuildSystem/config/packages/MPICH.py
+++ config/BuildSystem/config/packages/MPICH.py
@@ -4,8 +4,8 @@ import os
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.download         = ['http://www.mpich.org/static/downloads/3.3a3/mpich-3.3a3.tar.gz',
-                             'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.3a3.tar.gz']
+    self.download         = ['http://www.mpich.org/static/downloads/3.3b1/mpich-3.3b1.tar.gz',
+                             'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.3b1.tar.gz']
     self.download_31      = ['http://www.mpich.org/static/downloads/3.1/mpich-3.1.tar.gz',
                              'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.1.tar.gz']
     self.download_33a2    = ['http://www.mpich.org/static/downloads/3.3a2/mpich-3.3a2.tar.gz',
@@ -61,7 +61,7 @@ class Configure(config.package.GNUPackage):
   def configure(self):
     if config.setCompilers.Configure.isCygwin(self.log) and config.setCompilers.Configure.isGNU(self.setCompilers.CC, self.log):
       self.download = self.download_31
-    elif (self.setCompilers.isDarwin(self.log) and not self.sharedLibraries.useShared) or config.setCompilers.Configure.isSolaris(self.log):
+    elif config.setCompilers.Configure.isSolaris(self.log):
       self.download = self.download_33a2
     return config.package.Package.configure(self)
 
