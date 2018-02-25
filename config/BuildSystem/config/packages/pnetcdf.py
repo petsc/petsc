f44206646624860e7f7c3bee4c9ee33caf156d2e
Blaise Bourdin <bourdin@lsu.edu>
2018-02-25 16:25:19 -0600

    BuildSystem: bug fixes in pnetcdf
       updated to 1.9 from 1.9RC1
       fixed a bug in configure script

--- config/BuildSystem/config/packages/pnetcdf.py
+++ config/BuildSystem/config/packages/pnetcdf.py
@@ -4,12 +4,12 @@ import os
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.download         = ['http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-1.9.0.pre1.tar.gz',
-                             'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/parallel-netcdf-1.9.0.pre1.tar.gz']
+    self.download         = ['http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-1.9.0.tar.gz',
+                             'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/parallel-netcdf-1.9.0.tar.gz']
     self.functions        = ['ncmpi_create']
     self.includes         = ['pnetcdf.h']
     self.liblist          = [['libpnetcdf.a']]
-    self.downloaddirnames = ['parallel-netcdf-1.9.0.pre1']
+    self.downloaddirnames = ['parallel-netcdf-1.9.0']
     return
 
   def setupDependencies(self, framework):
@@ -21,6 +21,6 @@ class Configure(config.package.GNUPackage):
 
   def formGNUConfigureArgs(self):
     args = config.package.GNUPackage.formGNUConfigureArgs(self)
-    args.append('--with-mpi="'+os.path.dirname(os.path.dirname(self.mpi.mpiexec))+'"')
+    args.append('--with-mpi="'+self.mpi.directory+'"')
     args.append('LIBS="'+self.libraries.toStringNoDupes(self.flibs.lib)+'"')
     return args
