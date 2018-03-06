131f56ebf150415cd61328adf953cec201f7cb72
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2018-03-06 18:04:42 +0000

    Explicitly convert comparison to PetscBool

--- src/dm/impls/da/dacreate.c
+++ src/dm/impls/da/dacreate.c
@@ -304,7 +304,7 @@ static PetscErrorCode DMHasCreateInjection_DA(DM dm, PetscBool *flg)
   PetscFunctionBegin;
   PetscValidHeaderSpecific(dm,DM_CLASSID,1);
   PetscValidPointer(flg,2);
-  *flg = da->interptype == DMDA_Q1;
+  *flg = da->interptype == DMDA_Q1 ? PETSC_TRUE : PETSC_FALSE;
   PetscFunctionReturn(0);
 }
 
