262aaa3862c0011b011bbbbfd04e01f2402a55f4
Satish Balay <balay@mcs.anl.gov>
2018-03-09 20:06:25 -0600

    cusp: fix typo, boost -> cusp

--- config/BuildSystem/config/packages/cusp.py
+++ config/BuildSystem/config/packages/cusp.py
@@ -25,7 +25,7 @@ class Configure(config.package.Package):
   def Install(self):
     import shutil
     import os
-    self.log.write('boostDir = '+self.packageDir+' installDir '+self.installDir+'\n')
+    self.log.write('cuspDir = '+self.packageDir+' installDir '+self.installDir+'\n')
     srcdir = os.path.join(self.packageDir,'cusp')
     destdir = os.path.join(self.installDir,'include','cusp')
     if self.installSudo:
@@ -33,13 +33,13 @@ class Configure(config.package.Package):
       try:
         output,err,ret  = config.base.Configure.executeShellCommand(self.installSudo+'mkdir -p '+destdir+' && '+self.installSudo+'rm -rf '+destdir+'  && '+self.installSudo+'cp -rf '+srcdir+' '+destdir, timeout=6000, log = self.log)
       except RuntimeError, e:
-        raise RuntimeError('Error copying Boost files from '+os.path.join(self.packageDir, 'Boost')+' to '+packageDir)
+        raise RuntimeError('Error copying Cusp files from '+os.path.join(self.packageDir, 'Cusp')+' to '+packageDir)
     else:
       try:
         if os.path.isdir(destdir): shutil.rmtree(destdir)
         shutil.copytree(srcdir,destdir)
       except RuntimeError,e:
-        raise RuntimeError('Error installing Boost include files: '+str(e))
+        raise RuntimeError('Error installing Cusp include files: '+str(e))
     return self.installDir
 
   def getSearchDirectories(self):
