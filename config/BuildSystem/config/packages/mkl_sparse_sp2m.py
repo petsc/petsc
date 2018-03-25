57477262769ecd1dc685e638f09bf9f613fa6675
Richard Tran Mills <rmills@rmills.org>
2018-03-24 17:54:45 -0700

    Added test for mkl_sparse_sp2m().

---
+++ config/BuildSystem/config/packages/mkl_sparse_sp2m.py
@@ -0,0 +1,19 @@
+import config.package
+import os
+
+class Configure(config.package.Package):
+  def __init__(self, framework):
+    config.package.Package.__init__(self, framework)
+    self.includes         = ['mkl.h','mkl_spblas.h']
+    self.functions        = ['mkl_sparse_sp2m']
+    self.liblist          = [[]] # use MKL detected by BlasLapack.py
+    self.precisions       = ['single','double']
+    self.lookforbydefault = 1
+    self.requires32bitint = 1
+    return
+
+  def setupDependencies(self, framework):
+    config.package.Package.setupDependencies(self, framework)
+    self.blasLapack = framework.require('config.packages.BlasLapack',self)
+    self.deps       = [self.blasLapack]
+    return
