4837962931f60bbab8185cce320734f3815113c9
Hong Zhang <hzhang@mcs.anl.gov>
2018-03-28 14:47:23 -0500

    add missing CHKERRQ

--- src/vec/vscat/examples/ex3.c
+++ src/vec/vscat/examples/ex3.c
@@ -57,7 +57,7 @@ int main(int argc,char **argv)
   }
 
   if (rank == 10) {
-    ierr = PetscPrintf(PETSC_COMM_SELF,"\n[%d] isx:\n",rank);
+    ierr = PetscPrintf(PETSC_COMM_SELF,"\n[%d] isx:\n",rank);CHKERRQ(ierr);
     ierr = ISView(isx,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
   }
 
