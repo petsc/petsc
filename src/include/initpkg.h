0e9d8ac39ea7468a7949224056c21189cabc2ed5
Lisandro Dalcin <dalcinl@gmail.com>
2018-04-01 22:59:35 +0300

    PetscInitializePackageAll: Add/reorder entries

--- src/include/initpkg.h
+++ src/include/initpkg.h
@@ -5,20 +5,22 @@ static PetscErrorCode PetscInitializePackageAll(void)
   PetscErrorCode ierr;
   PetscFunctionBegin;
   ierr = PetscSysInitializePackage();CHKERRQ(ierr);
+  ierr = PetscDrawInitializePackage();CHKERRQ(ierr);
   ierr = PetscViewerInitializePackage();CHKERRQ(ierr);
   ierr = PetscRandomInitializePackage();CHKERRQ(ierr);
   ierr = ISInitializePackage();CHKERRQ(ierr);
-  ierr = VecInitializePackage();CHKERRQ(ierr);
+  ierr = AOInitializePackage();CHKERRQ(ierr);
   ierr = PFInitializePackage();CHKERRQ(ierr);
+  ierr = PetscSFInitializePackage();CHKERRQ(ierr);
+  ierr = VecInitializePackage();CHKERRQ(ierr);
+  ierr = VecScatterInitializePackage();CHKERRQ(ierr);
   ierr = MatInitializePackage();CHKERRQ(ierr);
   ierr = PCInitializePackage();CHKERRQ(ierr);
   ierr = KSPInitializePackage();CHKERRQ(ierr);
   ierr = SNESInitializePackage();CHKERRQ(ierr);
-  ierr = TSInitializePackage();CHKERRQ(ierr);
   ierr = TaoInitializePackage();CHKERRQ(ierr);
-  ierr = AOInitializePackage();CHKERRQ(ierr);
+  ierr = TSInitializePackage();CHKERRQ(ierr);
   ierr = DMInitializePackage();CHKERRQ(ierr);
-  ierr = PetscSFInitializePackage();CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
