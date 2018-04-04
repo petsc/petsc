a9b9f03ac70677b6dcf7ed9fd458812778f451c6
Hong Zhang <hzhang@mcs.anl.gov>
2018-04-04 12:57:42 -0500

    cleanup ex24.c

--- src/mat/examples/tests/ex24.c
+++ src/mat/examples/tests/ex24.c
@@ -1,87 +1,21 @@
 
-static char help[] = "Tests the different MatColoring implementatons and ISColoringTestValid().\n\n";
+static char help[] = "Tests the different MatColoring implementatons and ISColoringTestValid() \n\
+                      Modifed from the code contributed by Ali Berk Kahraman. \n\n";
 #include <petscmat.h>
 
-#if 0
-#include <petscbt.h>
-
-PetscErrorCode MatColoringVarify(Mat A,ISColoring iscoloring)
+PetscErrorCode FormJacobian(Mat A,void *ctx)
 {
   PetscErrorCode ierr;
-  PetscInt       nn,c,i,j,M,N,nc,nnz,col,row;
-  const PetscInt *cia,*cja;
-  IS             *isis;
-  Mat            Aseq = A;
-  MPI_Comm       comm;
-  PetscMPIInt    size,rank;
-  PetscBool      done;
-  PetscBT        table;
-  const PetscInt *cols;
-
-  PetscFunctionBegin;
-  ierr = ISColoringGetIS(iscoloring,&nn,&isis);CHKERRQ(ierr);
-
-  ierr = PetscObjectGetComm((PetscObject)A,&comm);CHKERRQ(ierr);
-  ierr = MPI_Comm_rank(comm,&rank);CHKERRQ(ierr);
-  ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
-  if (size > 1) {
-    /* create a sequential iscoloring on all processors */
-    ierr = MatGetSeqNonzeroStructure(A,&Aseq);CHKERRQ(ierr);
-  }
-  ierr = MatGetSize(Aseq,&M,NULL);CHKERRQ(ierr);
-  printf("MatColoringVarify...nn %d, M %d\n",nn,M);
-
-  ierr = MatGetColumnIJ(Aseq,0,PETSC_FALSE,PETSC_TRUE,&N,&cia,&cja,&done);CHKERRQ(ierr);
-  if (!done) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"Ordering requires IJ");
-
-  ierr = PetscBTCreate(M,&table);CHKERRQ(ierr);
-  for (c=0; c<nn; c++) { /* for each color */
-    ierr = ISGetSize(isis[c],&nc);CHKERRQ(ierr);
-    if (nc <= 1) continue;
-
-    ierr = PetscBTMemzero(M,table);CHKERRQ(ierr);
-    //printf("\n %d ---- isis\n",c);
-
-    ierr = ISGetIndices(isis[c],&cols);CHKERRQ(ierr);
-    for (j=0; j<nc; j++) {
-      //PetscInt nnz,col,row;
-      col = cols[j];
-      nnz = cia[col+1] - cia[col];
-      //printf("    col %d, nnz %d, rows:\n",col,nnz);
-      for (i=0; i<nnz; i++) {
-        row = cja[cia[col]+i];
-        //printf(" %d,",row);
-        if (PetscBTLookupSet(table,row)) {
-          SETERRQ3(PETSC_COMM_SELF,PETSC_ERR_ARG_WRONG,"color %D, col %D: row %D already in this color",c,col,row);
-        }
-      }
-      //printf(" \n");
-    }
-    ierr = ISRestoreIndices(isis[c],&cols);CHKERRQ(ierr);
-  }
-  ierr = PetscBTDestroy(&table);CHKERRQ(ierr);
-
-  if (size > 1) {
-    ierr = MatDestroy(&Aseq);CHKERRQ(ierr);
-  }
-  ierr = MatRestoreColumnIJ(Aseq,1,PETSC_FALSE,PETSC_TRUE,NULL,&cia,&cja,&done);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-#endif
-
-PetscErrorCode FormJacobian(Vec input,Mat A,Mat B,void *ctx)
-{
-  PetscErrorCode ierr;
-  PetscInt       vecsize,ownbegin,ownend,i,j;
+  PetscInt       M,ownbegin,ownend,i,j;
   PetscScalar    dummy=0.0;
 
   PetscFunctionBeginUser;
-  ierr = VecGetSize(input,&vecsize);CHKERRQ(ierr);
-  ierr = VecGetOwnershipRange(input,&ownbegin,&ownend);CHKERRQ(ierr);
+  ierr = MatGetSize(A,&M,NULL);CHKERRQ(ierr);
+  ierr = MatGetOwnershipRange(A,&ownbegin,&ownend);CHKERRQ(ierr);
 
   for (i=ownbegin; i<ownend; i++) {
-    for(j=i-3;j<i+3;j++) {
-      if(j<vecsize){
+    for(j=i-3; j<i+3; j++) {
+      if (j >= 0 && j < M) {
         ierr = MatSetValues(A,1,&i,1,&j,&dummy,INSERT_VALUES);CHKERRQ(ierr);
       }
     }
@@ -114,7 +48,7 @@ int main(int argc, char *argv[])
   ierr = VecSetFromOptions(solution);CHKERRQ(ierr);
   ierr = VecDuplicate(solution,&residual);CHKERRQ(ierr);
 
-  ierr = FormJacobian(solution,J,J,NULL);CHKERRQ(ierr);
+  ierr = FormJacobian(J,NULL);CHKERRQ(ierr);
   ierr = MatView(J,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   /*
