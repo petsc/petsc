479ac237a8f9c094fb37edf66b71f257c7d40d36
Hong Zhang <hongzhang@anl.gov>
2018-05-06 14:37:02 -0500

    Change to BNTR method for eptorsion1
    Using NTR gives an error because of missing MATOP_GET_DIAGONAL operation
    
    Funded-by:
    Project: FASTMATH adjoint
    Time: 0.1 hours
    Reported-by:
    Thanks-to:

--- src/tao/unconstrained/examples/tutorials/eptorsion1.c
+++ src/tao/unconstrained/examples/tutorials/eptorsion1.c
@@ -606,6 +606,6 @@ PetscErrorCode HessianProduct(void *ptr,Vec svec,Vec y)
 
    test:
       suffix: 4
-      args: -tao_smonitor -tao_type ntr -tao_gatol 1.e-4 -my_tao_mf -tao_test_hessian
+      args: -tao_smonitor -tao_type bntr -tao_gatol 1.e-4 -my_tao_mf -tao_test_hessian
 
 TEST*/
