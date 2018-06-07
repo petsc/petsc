4ceaa6fead1c6ebb723346c52163172f41333687
Patrick Sanan <patrick.sanan@gmail.com>
2018-06-07 17:03:59 +0200

    petscimpl.h: namespace REDUCE_XXX CPP macros and move to an enum

--- include/petsc/private/petscimpl.h
+++ include/petsc/private/petscimpl.h
@@ -808,9 +808,7 @@ typedef enum {PETSC_OFFLOAD_UNALLOCATED,PETSC_OFFLOAD_GPU,PETSC_OFFLOAD_CPU,PETS
 
 typedef enum {STATE_BEGIN, STATE_PENDING, STATE_END} SRState;
 
-#define REDUCE_SUM  0
-#define REDUCE_MAX  1
-#define REDUCE_MIN  2
+typedef enum {PETSC_SR_REDUCE_SUM=0,PETSC_SR_REDUCE_MAX=1,PETSC_SR_REDUCE_MIN=2} PetscSRReductionType;
 
 typedef struct {
   MPI_Comm    comm;
