5d16cefbd2183831c68f7c0ba68e3931f4b910e7
Satish Balay <balay@mcs.anl.gov>
2018-06-13 10:35:32 +0530

    test: Silence one more warning from Clang Static Analyzer
    copy fix from e9072f5865727a63ead1eece06a5a64da9f8f942

--- src/sys/examples/tests/ex43.c
+++ src/sys/examples/tests/ex43.c
@@ -19,6 +19,9 @@ int main(int argc, char **argv)
   ierr = PetscOptionsGetInt(NULL,NULL,"-N",&N,NULL);CHKERRQ(ierr);
   ierr = PetscHSetIJCreate(&table);CHKERRQ(ierr);
 
+  /* The following line silences warnings from Clang Static Analyzer */
+  ierr = PetscHSetIJResize(table,0);CHKERRQ(ierr);
+
   ierr = PetscTimeSubtract(&t_add);CHKERRQ(ierr);
   for (i = 0; i < N; ++i) {
     for (j = 0; j < N; ++j) {
