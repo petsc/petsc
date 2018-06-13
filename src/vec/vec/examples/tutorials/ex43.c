d48f17134fae3703bee80c4958e1a7d522bf5496
Satish Balay <balay@mcs.anl.gov>
2018-06-13 10:12:28 +0530

    fix function name mangling to be call-able form fortran
    Fixes:
    
    arch-freebsd-cxx-cmplx-pkgs-dbg/tests/vec/vec/examples/tutorials/ex43f.o: In function `MAIN__':
    /usr/home/balay/petsc.next/src/vec/vec/examples/tutorials/ex43f.F90:48: undefined reference to `fillupvector'

--- src/vec/vec/examples/tutorials/ex43.c
+++ src/vec/vec/examples/tutorials/ex43.c
@@ -1,7 +1,12 @@
 
 #include <petscvec.h>
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+#define fillupvector_            FILLUPVECTOR
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
+#define fillupvector_            fillupvector
+#endif
 
-void fillupvector(Vec *v,PetscErrorCode *ierr)
+PETSC_EXTERN void fillupvector(Vec *v,PetscErrorCode *ierr)
 {
   *ierr = VecSet(*v,1.0);
   return;
