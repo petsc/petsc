07f6e9dcf8d2f7a1504ea5bcd70c27306abcd6be
Marek Pecha <pecha.mari@gmail.com>
2018-06-13 22:25:47 +0200

    MatLoad_AIJ_HDF5 adapted from PERMON

--- src/mat/impls/aij/seq/aijhdf5.c
+++ src/mat/impls/aij/seq/aijhdf5.c
@@ -1,13 +1,122 @@
 
 #include <../src/mat/impls/aij/seq/aij.h>
+#include <petscviewerhdf5.h>
 
 #if defined(PETSC_HAVE_HDF5)
 PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
 {
-  /*PetscErrorCode ierr;*/
+  hid_t           file_id,group_matrix_id,dset_id,dspace_id,mspace_id,dtype;
+  hsize_t         h5_count_i,h5_offset_i,h5_count_data,h5_offset_data;
+
+  PetscInt        count_i,offset_i,count_data,offset_data,offset_i_end;
+  PetscInt        *i = NULL,*j = NULL;
+  PetscReal       *a = NULL;
+  const char      *a_name,*i_name,*j_name,*mat_name,*c_name;
+  PetscInt        p,m,M,N;
+
+  PetscErrorCode  ierr;
+  MPI_Comm        comm;
 
   PetscFunctionBegin;
-  SETERRQ(PetscObjectComm((PetscObject)mat),PETSC_ERR_SUP,"MatLoad_MPIAIJ_HDF5 is not implemented yet!");
+  ierr = PetscObjectGetComm((PetscObject)mat,&comm);CHKERRQ(ierr);
+  ierr = PetscObjectGetName((PetscObject)mat,&mat_name);CHKERRQ(ierr);
+  ierr = PetscViewerHDF5GetAIJNames(viewer,&i_name,&j_name,&a_name,&c_name);CHKERRQ(ierr);
+
+  ierr = PetscViewerHDF5GetFileId(viewer,&file_id);CHKERRQ(ierr);
+  ierr = PetscViewerHDF5PushGroup(viewer,mat_name);CHKERRQ(ierr);
+  ierr = PetscViewerHDF5OpenGroup(viewer,&file_id,&group_matrix_id);CHKERRQ(ierr);
+
+  ierr = PetscViewerHDF5ReadAttribute(viewer,mat_name,c_name,PETSC_INT,&M);
+  N = M;
+  m = PETSC_DECIDE;
+  ierr = PetscSplitOwnership(comm,&m,&M);CHKERRQ(ierr);
+  ierr = MPI_Scan(&m,&offset_i_end,1,MPIU_INT,MPI_SUM,comm);CHKERRQ(ierr);
+
+  if (m > 0) {
+    /* Read array i (array of row indices) */
+    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,i_name,H5P_DEFAULT));
+    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
+
+    ierr = PetscMalloc((m + 1) * sizeof(PetscInt),&i);CHKERRQ(ierr);
+
+    /* Determine offset and count of elements for reading local part of array i */
+    offset_i = offset_i_end - m;
+    ierr = PetscHDF5IntCast(offset_i,&h5_offset_i);CHKERRQ(ierr);
+    count_i = m + 1;
+    ierr = PetscHDF5IntCast(count_i,&h5_count_i);CHKERRQ(ierr);
+
+    /* Read local part of array i */
+    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_i,NULL));
+    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_i,NULL,&h5_count_i,NULL));
+    ierr = PetscDataTypeToHDF5DataType(PETSC_INT,&dtype);CHKERRQ(ierr);
+    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,i));
+
+    PetscStackCallHDF5(H5Sclose,(mspace_id));
+    PetscStackCallHDF5(H5Sclose,(dspace_id));
+    PetscStackCallHDF5(H5Dclose,(dset_id));
+
+    /* Determine offset and count of elements for reading local part of array data (a, j) */
+    offset_data = i[0];
+    ierr = PetscHDF5IntCast(offset_data,&h5_offset_data);CHKERRQ(ierr);
+    count_data = i[count_i - 1] - i[0];
+    ierr = PetscHDF5IntCast(count_data,&h5_count_data);CHKERRQ(ierr);
+
+    /* Read array j (array of column indices) */
+    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,j_name,H5P_DEFAULT));
+    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
+
+    ierr = PetscMalloc(count_data * sizeof(PetscInt),&j);CHKERRQ(ierr);
+
+    /* Read local part of array j */
+    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_data,NULL));
+    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_data,NULL,&h5_count_data,NULL));
+    ierr = PetscDataTypeToHDF5DataType(PETSC_INT,&dtype);CHKERRQ(ierr);
+    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,j));
+
+    PetscStackCallHDF5(H5Sclose,(mspace_id));
+    PetscStackCallHDF5(H5Sclose,(dspace_id));
+    PetscStackCallHDF5(H5Dclose,(dset_id));
+
+    /* Read array a (array of values) */
+    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,a_name,H5P_DEFAULT));
+    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
+
+    ierr = PetscMalloc(count_data * sizeof(PetscReal),&a);CHKERRQ(ierr);
+
+    /* Read local part of array a */
+    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_data,NULL));
+    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_data,NULL,&h5_count_data,NULL));
+    ierr = PetscDataTypeToHDF5DataType(PETSC_REAL,&dtype);CHKERRQ(ierr);
+    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,a));
+
+    PetscStackCallHDF5(H5Sclose,(mspace_id));
+    PetscStackCallHDF5(H5Sclose,(dspace_id));
+    PetscStackCallHDF5(H5Dclose,(dset_id));
+  } else {
+    a = NULL;
+    j = NULL;
+    ierr = PetscMalloc(sizeof(PetscInt),&i);CHKERRQ(ierr);
+    i[0] = 0;
+  }
+
+  /* close group */
+  PetscStackCallHDF5(H5Gclose,(group_matrix_id));
+
+  /* Converting global to local indexing of rows */
+  for (p=1; p<count_i; ++p) i[p] -= i[0];
+  i[0] = 0;
+
+  /* create matrix */
+  ierr = MatCreateMPIAIJWithArrays(comm,m,PETSC_DETERMINE,M,N,i,j,a,&mat);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(mat,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(mat,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  ierr = PetscFree(i);CHKERRQ(ierr);
+  if (j) {
+    ierr = PetscFree(j);CHKERRQ(ierr);
+  } if (a) {
+    ierr = PetscFree(a);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
 #endif
