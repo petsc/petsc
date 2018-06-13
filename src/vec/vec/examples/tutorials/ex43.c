f97e436bc749a7fe3933c978320a9f48dafcc0b5
Satish Balay <balay@mcs.anl.gov>
2018-06-13 21:29:11 +0530

    revert unneeded fix in d48f17134fae3703bee80c4958e1a7d522bf5496 - the primary fix was to add PETSC_EXTERN

--- src/vec/vec/examples/tutorials/ex43.c
+++ src/vec/vec/examples/tutorials/ex43.c
@@ -1,10 +1,5 @@
 
 #include <petscvec.h>
-#if defined(PETSC_HAVE_FORTRAN_CAPS)
-#define fillupvector_            FILLUPVECTOR
-#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
-#define fillupvector_            fillupvector
-#endif
 
 PETSC_EXTERN void fillupvector(Vec *v,PetscErrorCode *ierr)
 {
