34e9d0e7c557ee972c049d43d341538b6ff181b5
Matthew G. Knepley <knepley@gmail.com>
2018-06-27 09:37:10 -0500

    Configure+p4est: Update to Toby's fix for the p4est link

--- config/BuildSystem/config/packages/p4est.py
+++ config/BuildSystem/config/packages/p4est.py
@@ -3,8 +3,8 @@ import config.package
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.gitcommit         = 'v2.0'
-    self.download          = ['git://https://github.com/cburstedde/p4est','https://github.com/p4est/p4est.github.io/raw/master/release/p4est-2.0.tar.gz']
+    self.gitcommit         = '05de094256aa783685075af3a38c4d29d0c8daa5'
+    self.download          = ['git://https://github.com/tisaac/p4est','https://github.com/p4est/p4est.github.io/raw/master/release/p4est-2.0.tar.gz']
     self.functions         = ['p4est_init']
     self.includes          = ['p4est_bits.h']
     self.liblist           = [['libp4est.a', 'libsc.a']]
