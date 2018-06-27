ff54fdf891d05725cc54ef43d84dd969e8f43227
Barry Smith <bsmith@mcs.anl.gov>
2018-06-27 13:19:09 -0500

    Switch CoDiPack download to master since adblaslapack doesn't build with v1.6 on Apple
    Commit-type: portability-fix
    Reported-by: Michel Schanen <mschanen@anl.gov>

--- config/BuildSystem/config/packages/CoDiPack.py
+++ config/BuildSystem/config/packages/CoDiPack.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit     = 'v1.6'
+    self.gitcommit     = 'master'
     self.download      = ['git://https://github.com/SciCompKL/CoDiPack.git']
     self.includes      = ['adjointInterface.hpp']
     self.liblist       = []
