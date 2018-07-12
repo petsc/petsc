25df13449fd0cb8c70fb77d93be6a7c9008e66c5
Hong Zhang <hongzhang@anl.gov>
2018-07-12 10:09:36 -0700

    Improve the two van der pol examples
    Now ex16 and ex20 use the same formulation with improved initial values to reduce the errors of the first few time steps.
    - ex16 intends to demonstrate IMEX
    - ex20 intends to demonstrate explicit or implicit TS interface
    
    Funded-by:
    Project: FASTMATH adjoint
    Time: 1.5 hours
    Reported-by:
    Thanks-to:

--- src/ts/examples/tutorials/ex20.c
+++ src/ts/examples/tutorials/ex20.c
@@ -11,64 +11,15 @@ Input parameters include:\n";
 
    This program solves the van der Pol DAE ODE equivalent
        y' = z                 (1)
-       z' = mu[(1-y^2)z-y]
+       z' = \mu ((1-y^2)z-y)
    on the domain 0 <= x <= 1, with the boundary conditions
-       y(0) = 2, y'(0) = -6.666665432100101e-01,
+       y(0) = 2, y'(0) = −2/3 +10/(81*\mu) − 292/(2187*\mu^2),
    and
-       mu = 10^6.
-   This is a nonlinear equation.
+       \mu = 10^6 ( y'(0) ~ -0.6666665432100101).
+   This is a nonlinear equation. The well prepared initial condition gives errors that are not dominated by the first few steps of the method when \mu is large.
 
    Notes:
-   This code demonstrates the TS solver interface to a variant of
-   linear problems, u_t = f(u,t), namely turning (1) into a system of
-   first order differential equations,
-
-   [ y' ] = [          z          ]
-   [ z' ]   [     mu[(1-y^2)z-y]  ]
-
-   which then we can write as a vector equation
-
-   [ u_1' ] = [      u_2              ]  (2)
-   [ u_2' ]   [ mu[(1-u_1^2)u_2-u_1]  ]
-
-   which is now in the desired form of u_t = f(u,t). One way that we
-   can split f(u,t) in (2) is to split by component,
-
-   [ u_1' ] = [  u_2 ] + [       0              ]
-   [ u_2' ]   [  0   ]   [ mu[(1-u_1^2)u_2-u_1] ]
-
-   where
-
-   [ F(u,t) ] = [  u_2 ]
-                [  0   ]
-
-   and
-
-   [ G(u',u,t) ] = [ u_1' ] - [            0         ]
-                   [ u_2' ]   [ mu[(1-u_1^2)u_2-u_1] ]
-
-   Using the definition of the Jacobian of G (from the PETSc user manual),
-   in the equation G(u',u,t) = F(u,t),
-
-              dG   dG
-   J(G) = a * -- - --
-              du'  du
-
-   where d is the partial derivative. In this example,
-
-   dG   [ 1 ; 0 ]
-   -- = [       ]
-   du'  [ 0 ; 1 ]
-
-   dG   [ 0                       ;         0         ]
-   -- = [                                             ]
-   du   [ -mu*(1.0 + 2.0*u_1*u_2) ; mu*(1-u_1*u_1)    ]
-
-   Hence,
-
-          [      a                 ;         0          ]
-   J(G) = [                                             ]
-          [ mu*(1.0 + 2.0*u_1*u_2) ; a - mu*(1-u_1*u_1) ]
+   This code demonstrates the TS solver interface to an ODE -- RHSFunction for explicit form and IFunction for implicit form.
 
   ------------------------------------------------------------------------- */
 
@@ -77,7 +28,6 @@ Input parameters include:\n";
 typedef struct _n_User *User;
 struct _n_User {
   PetscReal mu;
-  PetscBool imex;
   PetscReal next_output;
 };
 
@@ -94,12 +44,13 @@ static PetscErrorCode RHSFunction(TS ts,PetscReal t,Vec X,Vec F,void *ctx)
   PetscFunctionBeginUser;
   ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
   ierr = VecGetArray(F,&f);CHKERRQ(ierr);
-  f[0] = (user->imex ? x[1] : 0.0);
-  f[1] = 0.0;
+  f[0] = x[1];
+  f[1] = user->mu*(1.-x[0]*x[0])*x[1]-x[0];
   ierr = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);
   ierr = VecRestoreArray(F,&f);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
+
 
 static PetscErrorCode IFunction(TS ts,PetscReal t,Vec X,Vec Xdot,Vec F,void *ctx)
 {
@@ -112,7 +63,7 @@ static PetscErrorCode IFunction(TS ts,PetscReal t,Vec X,Vec Xdot,Vec F,void *ctx
   ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
   ierr = VecGetArrayRead(Xdot,&xdot);CHKERRQ(ierr);
   ierr = VecGetArray(F,&f);CHKERRQ(ierr);
-  f[0] = xdot[0] - (user->imex ? 0 : x[1]);
+  f[0] = xdot[0] - x[1];
   f[1] = xdot[1] - user->mu*((1.0-x[0]*x[0])*x[1] - x[0]);
   ierr = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);
   ierr = VecRestoreArrayRead(Xdot,&xdot);CHKERRQ(ierr);
@@ -130,8 +81,8 @@ static PetscErrorCode IJacobian(TS ts,PetscReal t,Vec X,Vec Xdot,PetscReal a,Mat
 
   PetscFunctionBeginUser;
   ierr    = VecGetArrayRead(X,&x);CHKERRQ(ierr);
-  J[0][0] = a;     J[0][1] = (user->imex ? 0 : -1.0);
-  J[1][0] = user->mu*(1.0 + 2.0*x[0]*x[1]);   J[1][1] = a - user->mu*(1.0-x[0]*x[0]);
+  J[0][0] = a;     J[0][1] = -1.0;
+  J[1][0] = user->mu*(2.0*x[0]*x[1] + 1.0);   J[1][1] = a - user->mu*(1.0-x[0]*x[0]);
   ierr    = MatSetValues(B,2,rowcol,2,rowcol,&J[0][0],INSERT_VALUES);CHKERRQ(ierr);
   ierr    = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);
 
@@ -140,25 +91,6 @@ static PetscErrorCode IJacobian(TS ts,PetscReal t,Vec X,Vec Xdot,PetscReal a,Mat
   if (A != B) {
     ierr = MatAssemblyBegin(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
     ierr = MatAssemblyEnd(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  }
-  PetscFunctionReturn(0);
-}
-
-/* This is an example of registering an user-provided ARKIMEX scheme */
-static PetscErrorCode RegisterMyARK2(void)
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBeginUser;
-  {
-    const PetscReal
-      A[3][3] = {{0,0,0},
-                 {0.41421356237309504880,0,0},
-                 {0.75,0.25,0}},
-      At[3][3] = {{0,0,0},
-                  {0.12132034355964257320,0.29289321881345247560,0},
-                  {0.20710678118654752440,0.50000000000000000000,0.29289321881345247560}};
-    ierr = TSARKIMEXRegister("myark2",2,3,&At[0][0],NULL,NULL,&A[0][0],NULL,NULL,NULL,NULL,0,NULL,NULL);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
@@ -196,8 +128,8 @@ int main(int argc,char **argv)
   Vec            x;             /* solution, residual vectors */
   Mat            A;             /* Jacobian matrix */
   PetscInt       steps;
-  PetscReal      ftime   = 0.5;
-  PetscBool      monitor = PETSC_FALSE;
+  PetscReal      ftime = 0.5;
+  PetscBool      monitor = PETSC_FALSE,implicitform = PETSC_TRUE;
   PetscScalar    *x_ptr;
   PetscMPIInt    size;
   struct _n_User user;
@@ -210,17 +142,13 @@ int main(int argc,char **argv)
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   if (size != 1) SETERRQ(PETSC_COMM_SELF,1,"This is a uniprocessor example only!");
 
-  /* Register user-specified ARKIMEX method */
-  ierr = RegisterMyARK2();CHKERRQ(ierr);
-
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     Set runtime options
     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  user.imex        = PETSC_TRUE;
   user.next_output = 0.0;
-  user.mu          = 1.0e6;
-  ierr = PetscOptionsGetBool(NULL,NULL,"-imex",&user.imex,NULL);CHKERRQ(ierr);
+  user.mu          = 1.0e3;
   ierr = PetscOptionsGetBool(NULL,NULL,"-monitor",&monitor,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-implicitform",&implicitform,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,NULL,"Physical parameters",NULL);CHKERRQ(ierr);
   ierr = PetscOptionsReal("-mu","Stiffness parameter","<1.0e6>",user.mu,&user.mu,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
@@ -239,12 +167,16 @@ int main(int argc,char **argv)
      Create timestepping solver context
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
-  ierr = TSSetType(ts,TSBEULER);CHKERRQ(ierr);
-  ierr = TSSetRHSFunction(ts,NULL,RHSFunction,&user);CHKERRQ(ierr);
-  ierr = TSSetIFunction(ts,NULL,IFunction,&user);CHKERRQ(ierr);
-  ierr = TSSetIJacobian(ts,A,A,IJacobian,&user);CHKERRQ(ierr);
-
+  if (implicitform) {
+    ierr = TSSetIFunction(ts,NULL,IFunction,&user);CHKERRQ(ierr);
+    ierr = TSSetIJacobian(ts,A,A,IJacobian,&user);CHKERRQ(ierr);
+    ierr = TSSetType(ts,TSBEULER);CHKERRQ(ierr);
+  } else {
+    ierr = TSSetRHSFunction(ts,NULL,RHSFunction,&user);CHKERRQ(ierr);
+    ierr = TSSetType(ts,TSRK);CHKERRQ(ierr);
+  }
   ierr = TSSetMaxTime(ts,ftime);CHKERRQ(ierr);
+  ierr = TSSetTimeStep(ts,0.001);CHKERRQ(ierr);
   ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_STEPOVER);CHKERRQ(ierr);
   if (monitor) {
     ierr = TSMonitorSet(ts,Monitor,&user,NULL);CHKERRQ(ierr);
@@ -254,9 +186,9 @@ int main(int argc,char **argv)
      Set initial conditions
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = VecGetArray(x,&x_ptr);CHKERRQ(ierr);
-  x_ptr[0] = 2.0;   x_ptr[1] = -6.666665432100101e-01;
+  x_ptr[0] = 2.0;
+  x_ptr[1] = -2.0/3.0 + 10.0/(81.0*user.mu) - 292.0/(2187.0*user.mu*user.mu);
   ierr = VecRestoreArray(x,&x_ptr);CHKERRQ(ierr);
-  ierr = TSSetTimeStep(ts,.001);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Set runtime options
@@ -288,5 +220,6 @@ int main(int argc,char **argv)
 
     test:
       requires: !single
+      args: -mu 1e6
 
 TEST*/
