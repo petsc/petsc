16b7389b031c1c177dc6ccb5ad39e24f83f17432
Karl Rupp <me@karlrupp.net>
2018-07-25 16:09:14 -0500

    Jenkins: Added arch files for FreeBSD, Linux, Windows.

---
+++ config/examples/arch-jenkins-freebsd-cxx-cmplx-64idx-dbg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+configure_options = [
+  '--with-cc=/home/petsc/soft/mpich-3.3b1/bin/mpicc',
+  '--with-fc=/home/petsc/soft/mpich-3.3b1/bin/mpif90',
+  '--with-cxx=/home/petsc/soft/mpich-3.3b1/bin/mpicxx',
+  '--with-clanguage=cxx',
+  '--with-scalar-type=complex',
+  '--with-cxx-dialect=C++11',
+  '--with-64-bit-indices=1',
+  '--with-debugging=1',
+  'DATAFILESPATH=/home/petsc/datafiles',
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
