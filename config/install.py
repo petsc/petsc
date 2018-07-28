petsc/petsc

commit ad246c4de299cad1fd22430e8f23e651a7bf5d2b

install: remove usage of os.relpath() - appears to break with some installs
Reported-by: Brad Aagaard <baagaard@usgs.gov>

config/install.py
@@ -190,10 +190,9 @@ class Installer(script.Script):
   def copyExamples(self, src, dst):
     """copy the examples directories
     """
-    top=os.path.relpath(src,os.path.abspath(os.curdir))
-    for root, dirs, files in os.walk(top, topdown=False):
-        if not os.path.basename(root) == "examples": continue
-        self.copies.extend(self.copytree(root, os.path.join(dst,root)))
+    for root, dirs, files in os.walk(src, topdown=False):
+      if not os.path.basename(root) == "examples": continue
+      self.copies.extend(self.copytree(root, root.replace(src,dst)))
     return
 
   def copytree(self, src, dst, symlinks = False, copyFunc = shutil.copy2, exclude = [], exclude_ext= ['.DSYM','.o','.pyc'], recurse = 1):

commit dccacece9fe1ca01e87cdf5784a51f8902aa8e87

install: fix 'make alltests' with prefix install
This regression started with ad246c4de299cad1fd22430e8f23e651a7bf5d2b

This change caused examples to be copied to
PREFIX/share/petsc/examples/[sys,ksp] instead of PREFIX/share/petsc/examples/src/[sys,ksp]

This fix copies the example files to the required 'examples/src' location

Reported-by: Jose E. Roman <jroman@dsic.upv.es>

config/install.py
@@ -335,7 +335,7 @@ for file in files:
       shutil.rmtree(examplesdir)
     os.mkdir(examplesdir)
     os.mkdir(os.path.join(examplesdir,'src'))
-    self.copyExamples(self.rootSrcDir,examplesdir)
+    self.copyExamples(self.rootSrcDir,os.path.join(examplesdir,'src'))
     self.copyConfig(self.rootDir,examplesdir)
     self.fixExamplesMakefile(os.path.join(examplesdir,'gmakefile.test'))
     return