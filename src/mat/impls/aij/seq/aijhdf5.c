3b99b35f63a96617c705d355d0dc2b05e0a65818
Marek Pecha <pecha.mari@gmail.com>
2018-08-16 13:58:05 +0200

    MatLoad_AIJ_HDF5: load indices as IS and values as Vec

--- src/mat/impls/aij/seq/aijhdf5.c
+++ src/mat/impls/aij/seq/aijhdf5.c
@@ -1,16 +1,19 @@
 
 #include <../src/mat/impls/aij/seq/aij.h>
+#include "../../../../vec/is/is/impls/general/general.h"
+#include "../../../../vec/vec/impls/mpi/pvecimpl.h"
 #include <petscviewerhdf5.h>
 
 #if defined(PETSC_HAVE_HDF5)
 PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
 {
-  hid_t           file_id,group_matrix_id,dset_id,dspace_id,mspace_id,dtype;
-  hsize_t         h5_count_i,h5_offset_i,h5_count_data,h5_offset_data,h5_dims[4];
+  PetscMPIInt     rank,size;
+  hid_t           file_id,group_matrix_id,dset_id,dspace_id;
+  hsize_t         h5_dims[4];
 
-  PetscInt        count_i,offset_i,count_data,offset_data;
+  PetscInt        count_data;
   PetscInt        *i = NULL,*j = NULL;
-  PetscReal       *a = NULL;
+  PetscScalar     *a = NULL;
   const char      *a_name,*i_name,*j_name,*mat_name,*c_name;
   int             rdim;
   PetscInt        p,m,M,N;
@@ -20,8 +23,13 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   PetscErrorCode  ierr;
   MPI_Comm        comm;
 
+  IS              is_i,is_j;
+  Vec             vec_a;
+
   PetscFunctionBegin;
   ierr = PetscObjectGetComm((PetscObject)mat,&comm);CHKERRQ(ierr);
+  ierr = MPI_Comm_rank(comm,&rank);CHKERRQ(ierr);
+  ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
   ierr = PetscObjectGetName((PetscObject)mat,&mat_name);CHKERRQ(ierr);
   ierr = PetscViewerHDF5GetAIJNames(viewer,&i_name,&j_name,&a_name,&c_name);CHKERRQ(ierr);
 
@@ -34,7 +42,6 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
 
   ierr = PetscViewerHDF5PushGroup(viewer,mat_name);CHKERRQ(ierr);
   ierr = PetscViewerHDF5OpenGroup(viewer,&file_id,&group_matrix_id);CHKERRQ(ierr);
-  ierr = PetscViewerHDF5PopGroup(viewer);CHKERRQ(ierr);
 
   ierr = PetscViewerHDF5ReadAttribute(viewer,mat_name,c_name,PETSC_INT,&N);CHKERRQ(ierr);
 
@@ -54,80 +61,57 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   /* determine ownership of all (block) rows and columns */
   ierr = PetscLayoutSetUp(mat->rmap);CHKERRQ(ierr);
   ierr = PetscLayoutSetUp(mat->cmap);CHKERRQ(ierr);
-  m = mat->rmap->n;
-  offset_i = mat->rmap->rstart;
-  count_i = m + 1;
+  m = (rank == size - 1) ? mat->rmap->n + 1 : mat->rmap->n;
+  M = M + 1;
 
   if (m > 0) {
     /* Read array i (array of row indices) */
-    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,i_name,H5P_DEFAULT));
-    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
+    ierr = ISCreate(comm,&is_i);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)is_i,i_name);CHKERRQ(ierr);
+    ierr = PetscLayoutSetLocalSize(is_i->map,m);CHKERRQ(ierr);
+    ierr = PetscLayoutSetSize(is_i->map,M);CHKERRQ(ierr);
+    ierr = ISLoad(is_i,viewer);CHKERRQ(ierr);
+    i    = ((IS_General*)is_i->data)->idx;
 
-    ierr = PetscMalloc((m + 1) * sizeof(PetscInt),&i);CHKERRQ(ierr);
+    /* Determine offset and count of elements for reading local part of array data*/
+    if (rank > 0) {
+      ierr = MPI_Isend(i,1,MPIU_INT,rank - 1,0,comm,NULL);CHKERRQ(ierr);
+    }
 
-    /* Determine offset and count of elements for reading local part of array i */
-    ierr = PetscHDF5IntCast(offset_i,&h5_offset_i);CHKERRQ(ierr);
-    ierr = PetscHDF5IntCast(count_i,&h5_count_i);CHKERRQ(ierr);
-
-    /* Read local part of array i */
-    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_i,NULL));
-    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_i,NULL,&h5_count_i,NULL));
-    ierr = PetscDataTypeToHDF5DataType(PETSC_INT,&dtype);CHKERRQ(ierr);
-    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,i));
-
-    PetscStackCallHDF5(H5Sclose,(mspace_id));
-    PetscStackCallHDF5(H5Sclose,(dspace_id));
-    PetscStackCallHDF5(H5Dclose,(dset_id));
-
-    /* Determine offset and count of elements for reading local part of array data (a, j) */
-    offset_data = i[0];
-    ierr = PetscHDF5IntCast(offset_data,&h5_offset_data);CHKERRQ(ierr);
-    count_data = i[count_i - 1] - i[0];
-    ierr = PetscHDF5IntCast(count_data,&h5_count_data);CHKERRQ(ierr);
+    if (rank < size - 1) {
+      ierr = MPI_Irecv(&count_data,1,MPIU_INT,rank+1,0,comm,NULL);CHKERRQ(ierr);
+      count_data = count_data - i[0];
+    } else {
+      count_data = i[m-1] - i[0];
+    }
 
     /* Read array j (array of column indices) */
-    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,j_name,H5P_DEFAULT));
-    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
-
-    ierr = PetscMalloc(count_data * sizeof(PetscInt),&j);CHKERRQ(ierr);
-
-    /* Read local part of array j */
-    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_data,NULL));
-    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_data,NULL,&h5_count_data,NULL));
-    ierr = PetscDataTypeToHDF5DataType(PETSC_INT,&dtype);CHKERRQ(ierr);
-    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,j));
-
-    PetscStackCallHDF5(H5Sclose,(mspace_id));
-    PetscStackCallHDF5(H5Sclose,(dspace_id));
-    PetscStackCallHDF5(H5Dclose,(dset_id));
+    ierr = ISCreate(comm,&is_j);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)is_j,j_name);CHKERRQ(ierr);
+    ierr = PetscLayoutSetLocalSize(is_j->map,count_data);CHKERRQ(ierr);
+    ierr = PetscLayoutSetUp(is_j->map);CHKERRQ(ierr);
+    ierr = ISLoad(is_j,viewer);CHKERRQ(ierr);
+    j    = ((IS_General*)is_j->data)->idx;
 
     /* Read array a (array of values) */
-    PetscStackCallHDF5Return(dset_id,H5Dopen,(group_matrix_id,a_name,H5P_DEFAULT));
-    PetscStackCallHDF5Return(dspace_id,H5Dget_space,(dset_id));
+    ierr = VecCreate(comm,&vec_a);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)vec_a,a_name);CHKERRQ(ierr);
+    ierr = VecSetSizes(vec_a,count_data,PETSC_DECIDE);CHKERRQ(ierr);
+    ierr = VecLoad(vec_a,viewer);CHKERRQ(ierr);
+    a    = ((Vec_MPI*)vec_a->data)->array;
 
-    ierr = PetscMalloc(count_data * sizeof(PetscReal),&a);CHKERRQ(ierr);
-
-    /* Read local part of array a */
-    PetscStackCallHDF5Return(mspace_id,H5Screate_simple,(1,&h5_count_data,NULL));
-    PetscStackCallHDF5(H5Sselect_hyperslab,(dspace_id,H5S_SELECT_SET,&h5_offset_data,NULL,&h5_count_data,NULL));
-    ierr = PetscDataTypeToHDF5DataType(PETSC_REAL,&dtype);CHKERRQ(ierr);
-    PetscStackCallHDF5(H5Dread,(dset_id,dtype,mspace_id,dspace_id,H5P_DEFAULT,a));
-
-    PetscStackCallHDF5(H5Sclose,(mspace_id));
-    PetscStackCallHDF5(H5Sclose,(dspace_id));
-    PetscStackCallHDF5(H5Dclose,(dset_id));
   } else {
     a = NULL;
     j = NULL;
-    ierr = PetscMalloc(sizeof(PetscInt),&i);CHKERRQ(ierr);
-    i[0] = 0;
+    i = 0;
   }
 
   /* close group */
   PetscStackCallHDF5(H5Gclose,(group_matrix_id));
+  ierr = PetscViewerHDF5PopGroup(viewer);CHKERRQ(ierr);
 
   /* Converting global to local indexing of rows */
-  for (p=1; p<count_i; ++p) i[p] -= i[0];
+  for (p=1; p<m; ++p) i[p] -= i[0];
   i[0] = 0;
 
   /* populate matrix */
@@ -141,9 +125,9 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   ierr = MatMPIBAIJSetPreallocationCSR(mat,bs,i,j,a);CHKERRQ(ierr);
   */
 
-  ierr = PetscFree(i);CHKERRQ(ierr);
-  ierr = PetscFree(j);CHKERRQ(ierr);
-  ierr = PetscFree(a);CHKERRQ(ierr);
+  ierr = ISDestroy(&is_i);CHKERRQ(ierr);
+  ierr = ISDestroy(&is_j);CHKERRQ(ierr);
+  ierr = VecDestroy(&vec_a);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 #endif
