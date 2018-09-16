a76c4c5d9678040c6ca3c6fbb3cfdce495a55e52
Lisandro Dalcin <dalcinl@gmail.com>
2018-09-16 11:43:50 +0300

    Bump version number to 3.10.0

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ communication.
 """
 
 __author__    = 'Lisandro Dalcin'
-__version__   = '3.9.1'
+__version__   = '3.10.0'
 __credits__   = 'PETSc Team <petsc-maint@mcs.anl.gov>'
 
 # --------------------------------------------------------------------
