c744bbe1af6740c35e4edd157404e34d647ceb41
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-10-02 17:25:06 +0200

    DMPlexOrientPointSF_Internal: moved some vars from inner blocks to main block

--- src/dm/impls/plex/plexinterpolate.c
+++ src/dm/impls/plex/plexinterpolate.c
@@ -663,6 +663,8 @@ static PetscErrorCode DMPlexOrientPointSF_Internal(DM dm, PetscSF sf)
   const PetscInt    *locals;
   const PetscSFNode *remotes;
   PetscInt           nroots, nleaves, p;
+  const PetscInt    *cone;
+  PetscInt           coneSize, ind0, ind1;
   MPI_Comm           comm;
   PetscMPIInt        rank;
   PetscInt           debug = 0;
@@ -671,15 +673,11 @@ static PetscErrorCode DMPlexOrientPointSF_Internal(DM dm, PetscSF sf)
   PetscFunctionBegin;
   ierr = PetscSFGetGraph(sf, &nroots, &nleaves, &locals, &remotes);CHKERRQ(ierr);
   if (nroots < 0) PetscFunctionReturn(0);
-  /* If a point p is on the interface, then all its cone points must be also on interface  */
   ierr = PetscMalloc2(nroots, &roots, nroots, &leaves);CHKERRQ(ierr);
   ierr = PetscObjectGetComm((PetscObject) dm, &comm);CHKERRQ(ierr);
   ierr = MPI_Comm_rank(comm, &rank);CHKERRQ(ierr);
   if (debug && rank == 0) {ierr = PetscSynchronizedPrintf(comm, "Roots\n");CHKERRQ(ierr);}
   for (p = 0; p < nroots; ++p) {
-    const PetscInt *cone;
-    PetscInt        coneSize, ind0, ind1;
-
     ierr = DMPlexGetConeSize(dm, p, &coneSize);CHKERRQ(ierr);
     ierr = DMPlexGetCone(dm, p, &cone);CHKERRQ(ierr);
     if (coneSize  > 1) {
@@ -704,17 +702,12 @@ static PetscErrorCode DMPlexOrientPointSF_Internal(DM dm, PetscSF sf)
   if (debug) {ierr = PetscSynchronizedFlush(comm, NULL);CHKERRQ(ierr);}
   if (debug && rank == 0) {ierr = PetscSynchronizedPrintf(comm, "Referred leaves\n");CHKERRQ(ierr);}
   for (p = 0; p < nroots; ++p) {
-    const PetscInt *cone;
-    PetscInt        coneSize;
-
     if (leaves[p][0] < 0) continue;
     if (debug) {ierr = PetscSynchronizedPrintf(comm, "[%d]  %D: %D %D\n", rank, p, leaves[p][0], leaves[p][1]);CHKERRQ(ierr);}
 
     ierr = DMPlexGetConeSize(dm, p, &coneSize);CHKERRQ(ierr);
     ierr = DMPlexGetCone(dm, p, &cone);CHKERRQ(ierr);
     if (coneSize > 1) {
-      PetscInt ind0, ind1;
-
       ierr = PetscFindInt(cone[0], nleaves, locals, &ind0);CHKERRQ(ierr);
       ierr = PetscFindInt(cone[1], nleaves, locals, &ind1);CHKERRQ(ierr);
       if (ind0 < 0) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "Point SF contains %D but is missing cone point %D = %D", p, 0, cone[0]);
