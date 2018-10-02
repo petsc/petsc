6fce1ffafb5251c59f9dd98e687c451a80d43c76
Martin Diehl <m.diehl@mpie.de>
2018-10-02 19:27:06 +0200

    interfaces also for DMDAgetGhostCorners

--- src/dm/impls/da/ftn-custom/zdaghostf.c
+++ src/dm/impls/da/ftn-custom/zdaghostf.c
@@ -2,9 +2,13 @@
 #include <petsc/private/dmdaimpl.h>
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
-#define dmdagetghostcorners_           DMDAGETGHOSTCORNERS
+#define dmdagetghostcorners000000_     DMDAGETGHOSTCORNERS000000
+#define dmdagetghostcorners001001_     DMDAGETGHOSTCORNERS001001
+#define dmdagetghostcorners011011_     DMDAGETGHOSTCORNERS011011
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
-#define dmdagetghostcorners_           dmdagetghostcorners
+#define dmdagetghostcorners000000_     dmdagetghostcorners000000
+#define dmdagetghostcorners001001_     dmdagetghostcorners001001
+#define dmdagetghostcorners011011_     dmdagetghostcorners011011
 #endif
 
 PETSC_EXTERN void PETSC_STDCALL  dmdagetghostcorners_(DM *da,PetscInt *x,PetscInt *y,PetscInt *z,PetscInt *m,PetscInt *n,PetscInt *p, int *ierr )
@@ -15,4 +19,19 @@ PETSC_EXTERN void PETSC_STDCALL  dmdagetghostcorners_(DM *da,PetscInt *x,PetscIn
   CHKFORTRANNULLINTEGER(p);
 
   *ierr = DMDAGetGhostCorners(*da,x,y,z,m,n,p);
+}
+
+PETSC_EXTERN void PETSC_STDCALL  dmdagetghostcorners000000_(DM *da,PetscInt *x,PetscInt *y,PetscInt *z,PetscInt *m,PetscInt *n,PetscInt *p, int *ierr )
+{
+  dmdagetghostcorners_(da,x,y,z,m,n,p,ierr);
+}
+
+PETSC_EXTERN void PETSC_STDCALL  dmdagetghostcorners001001_(DM *da,PetscInt *x,PetscInt *y,PetscInt *z,PetscInt *m,PetscInt *n,PetscInt *p, int *ierr )
+{
+  dmdagetghostcorners_(da,x,y,z,m,n,p,ierr);
+}
+
+PETSC_EXTERN void PETSC_STDCALL  dmdagetghostcorners011011_(DM *da,PetscInt *x,PetscInt *y,PetscInt *z,PetscInt *m,PetscInt *n,PetscInt *p, int *ierr )
+{
+  dmdagetghostcorners_(da,x,y,z,m,n,p,ierr);
 }
