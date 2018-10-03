6dc21c250422d0f29984dbbfd0625e44819bf3b1
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-10-03 16:55:37 +0200

    DMPlexOrientPointSF_Internal: newcone -> cone

--- src/dm/impls/plex/plexinterpolate.c
+++ src/dm/impls/plex/plexinterpolate.c
@@ -725,14 +725,12 @@ static PetscErrorCode DMPlexOrientPointSF_Internal(DM dm, PetscSF sf)
     if (debug) {ierr = PetscSynchronizedPrintf(comm, "[%d]  %D: cone=[%D %D] leaves=[%D %D] roots=[%D %D] leavesRanks=[%D %D] rootsRanks=[%D %D]\n", rank, p, cone[0], cone[1], leaves[p][0], leaves[p][1], roots[p][0], roots[p][1], leavesRanks[p][0], leavesRanks[p][1], rootsRanks[p][0], rootsRanks[p][1]);CHKERRQ(ierr);}
     {
       if ((leaves[p][0] != roots[p][0]) || (leaves[p][1] != roots[p][1]) || (leavesRanks[p][0] != rootsRanks[p][0]) || (leavesRanks[p][0] != rootsRanks[p][0])) {
-        const PetscInt *newcone;
-
         /* TODO Generalize this to correct an arbitrary orientation */
         ierr = DMPlexReverseCell(dm, p);CHKERRQ(ierr);
         if (debug) {ierr = PetscSynchronizedPrintf(comm, "[%d]  reversed point %D\n", rank, p);CHKERRQ(ierr);}
-        ierr = DMPlexGetCone(dm, p, &newcone);CHKERRQ(ierr);
-        ierr = PetscFindInt(newcone[0], nleaves, locals, &ind0);CHKERRQ(ierr);
-        ierr = PetscFindInt(newcone[1], nleaves, locals, &ind1);CHKERRQ(ierr);
+        ierr = DMPlexGetCone(dm, p, &cone);CHKERRQ(ierr);
+        ierr = PetscFindInt(cone[0], nleaves, locals, &ind0);CHKERRQ(ierr);
+        ierr = PetscFindInt(cone[1], nleaves, locals, &ind1);CHKERRQ(ierr);
         if (ind0 < 0) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "Point SF contains %D but is missing cone point %D = %D", p, 0, cone[0]);
         if (ind1 < 0) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "Point SF contains %D but is missing cone point %D = %D", p, 1, cone[1]);
         if (leaves[p][0]  != remotes[ind0].index)
