94b451ac865b2325eed00305b98499871e819e1e
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-10-23 15:16:01 +0200

    MatLoad_AIJ_HDF5: Create i with proper i[m] (number of all local nonzeros).
    Replace count_data with i[m].
    It currently works only if there are no multiple ranks with 0 rows.

--- src/mat/impls/aij/seq/aijhdf5.c
+++ src/mat/impls/aij/seq/aijhdf5.c
@@ -11,7 +11,7 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   hid_t           file_id,group_matrix_id,dset_id,dspace_id;
   hsize_t         h5_dims[4];
 
-  PetscInt        count_data;
+  const PetscInt  *i_glob = NULL;
   PetscInt        *i = NULL;
   const PetscInt  *j = NULL;
   const PetscScalar *a = NULL;
@@ -63,37 +63,42 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   mat->cmap->N = N;
   ierr = PetscLayoutSetUp(mat->rmap);CHKERRQ(ierr);
   ierr = PetscLayoutSetUp(mat->cmap);CHKERRQ(ierr);
-  m = (rank == size - 1) ? mat->rmap->n + 1 : mat->rmap->n;
-  M = M + 1;
+  m = mat->rmap->n;
 
   /* Read array i (array of row indices) */
+  ierr = PetscMalloc1(m+1, &i);CHKERRQ(ierr); /* allocate i with one more position for local number of nonzeros on each rank */
+  if (rank == size-1) m++; /* in the loaded array i_glob, only the last rank has one more position with the global number of nonzeros */
+  M++;
   ierr = ISCreate(comm,&is_i);CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject)is_i,i_name);CHKERRQ(ierr);
   ierr = PetscLayoutSetLocalSize(is_i->map,m);CHKERRQ(ierr);
   ierr = PetscLayoutSetSize(is_i->map,M);CHKERRQ(ierr);
   ierr = ISLoad(is_i,viewer);CHKERRQ(ierr);
-  ierr = ISGetIndices(is_i,(const PetscInt**)&i);CHKERRQ(ierr);  /* we cheat here as we know it's ISGENERAL and need to modify the array */
+  ierr = ISGetIndices(is_i,&i_glob);CHKERRQ(ierr);
+  ierr = PetscMemcpy(i,i_glob,m*sizeof(PetscInt));CHKERRQ(ierr);
+
+  /* Reset m and M to the matrix sizes */
+  m = mat->rmap->n;
+  M--;
 
   /* Determine offset and count of elements for reading local part of array data*/
   if (rank > 0) {
-    ierr = MPI_Isend(i,1,MPIU_INT,rank-1,0,comm,&sreq);CHKERRQ(ierr);
+    ierr = MPI_Isend(&i[0],1,MPIU_INT,rank-1,0,comm,&sreq);CHKERRQ(ierr);
   }
-
   if (rank < size - 1) {
-    ierr = MPI_Irecv(&count_data,1,MPIU_INT,rank+1,0,comm,&rreq);CHKERRQ(ierr);
+    ierr = MPI_Irecv(&i[m],1,MPIU_INT,rank+1,0,comm,&rreq);CHKERRQ(ierr);
   }
   if (sreq) ierr = MPI_Wait(&sreq,MPI_STATUS_IGNORE);CHKERRQ(ierr);
   if (rreq) ierr = MPI_Wait(&rreq,MPI_STATUS_IGNORE);CHKERRQ(ierr);
-  if (rank < size - 1) {
-    count_data = count_data - i[0];
-  } else {
-    count_data = i[m-1] - i[0];
-  }
+
+  /* Convert global to local indexing of rows */
+  for (p=1; p<m+1; ++p) i[p] -= i[0];
+  i[0] = 0;
 
   /* Read array j (array of column indices) */
   ierr = ISCreate(comm,&is_j);CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject)is_j,j_name);CHKERRQ(ierr);
-  ierr = PetscLayoutSetLocalSize(is_j->map,count_data);CHKERRQ(ierr);
+  ierr = PetscLayoutSetLocalSize(is_j->map,i[m]);CHKERRQ(ierr);
   ierr = PetscLayoutSetUp(is_j->map);CHKERRQ(ierr);
   ierr = ISLoad(is_j,viewer);CHKERRQ(ierr);
   ierr = ISGetIndices(is_j,&j);CHKERRQ(ierr);
@@ -101,17 +106,13 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   /* Read array a (array of values) */
   ierr = VecCreate(comm,&vec_a);CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject)vec_a,a_name);CHKERRQ(ierr);
-  ierr = VecSetSizes(vec_a,count_data,PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = VecSetSizes(vec_a,i[m],PETSC_DECIDE);CHKERRQ(ierr);
   ierr = VecLoad(vec_a,viewer);CHKERRQ(ierr);
   ierr = VecGetArrayRead(vec_a,&a);CHKERRQ(ierr);
 
   /* close group */
   PetscStackCallHDF5(H5Gclose,(group_matrix_id));
   ierr = PetscViewerHDF5PopGroup(viewer);CHKERRQ(ierr);
-
-  /* Convert global to local indexing of rows */
-  for (p=1; p<m; ++p) i[p] -= i[0];
-  i[0] = 0;
 
   /* populate matrix */
   if (!((PetscObject)mat)->type_name) {
@@ -124,7 +125,8 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   ierr = MatMPIBAIJSetPreallocationCSR(mat,bs,i,j,a);CHKERRQ(ierr);
   */
 
-  ierr = ISRestoreIndices(is_i,(const PetscInt**)&i);CHKERRQ(ierr);
+  ierr = PetscFree(i);CHKERRQ(ierr);
+  ierr = ISRestoreIndices(is_i,&i_glob);CHKERRQ(ierr);
   ierr = ISRestoreIndices(is_j,&j);CHKERRQ(ierr);
   ierr = VecRestoreArrayRead(vec_a,&a);CHKERRQ(ierr);
   ierr = ISDestroy(&is_i);CHKERRQ(ierr);
