5efdb6ea3b93b30e78da96db6971ae982ee6c13b
Matthew G. Knepley <knepley@gmail.com>
2018-11-01 16:48:48 -0400

    SNES ex17: Fixed 3D linear problems and shortened some tests - Updated test output

--- src/snes/examples/tutorials/ex17.c
+++ src/snes/examples/tutorials/ex17.c
@@ -471,7 +471,7 @@ int main(int argc, char **argv)
   test:
     suffix: 3d_p1_quad_vlap
     requires: ctetgen
-    args: -dim 3 -displacement_petscspace_degree 1 -dm_refine 0 -convest_num_refine 3 -snes_convergence_estimate
+    args: -dim 3 -cells 2,2,2 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_p2_quad_vlap
     requires: ctetgen
@@ -482,7 +482,7 @@ int main(int argc, char **argv)
     args: -dim 3 -displacement_petscspace_degree 3 -dm_refine 0 -dmsnes_check
   test:
     suffix: 3d_q1_quad_vlap
-    args: -dim 3 -simplex 0 -displacement_petscspace_degree 1 -dm_refine 0 -convest_num_refine 3 -snes_convergence_estimate
+    args: -dim 3 -cells 2,2,2 -simplex 0 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_q2_quad_vlap
     args: -dim 3 -simplex 0 -displacement_petscspace_degree 2 -dm_refine 1 -dmsnes_check
@@ -492,7 +492,7 @@ int main(int argc, char **argv)
   test:
     suffix: 3d_p1_quad_elas
     requires: ctetgen
-    args: -sol_type elas_quad -dim 3 -displacement_petscspace_degree 1 -dm_refine 0 -convest_num_refine 3 -snes_convergence_estimate
+    args: -sol_type elas_quad -dim 3 -cells 2,2,2 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_p2_quad_elas
     requires: ctetgen
@@ -503,7 +503,7 @@ int main(int argc, char **argv)
     args: -sol_type elas_quad -dim 3 -displacement_petscspace_degree 3 -dm_refine 0 -dmsnes_check
   test:
     suffix: 3d_q1_quad_elas
-    args: -sol_type elas_quad -dim 3 -simplex 0 -displacement_petscspace_degree 1 -dm_refine 0 -convest_num_refine 3 -snes_convergence_estimate
+    args: -sol_type elas_quad -dim 3 -cells 2,2,2 -simplex 0 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_q2_quad_elas
     args: -sol_type elas_quad -dim 3 -simplex 0 -displacement_petscspace_degree 2 -dm_refine 1 -dmsnes_check
@@ -557,7 +557,7 @@ int main(int argc, char **argv)
   test:
     suffix: 3d_p1_trig_vlap
     requires: ctetgen
-    args: -sol_type vlap_trig -dim 3 -displacement_petscspace_degree 1 -dm_refine 1 -convest_num_refine 2 -snes_convergence_estimate
+    args: -sol_type vlap_trig -dim 3 -cells 2,2,2 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_p2_trig_vlap
     requires: ctetgen
@@ -568,7 +568,7 @@ int main(int argc, char **argv)
     args: -sol_type vlap_trig -dim 3 -displacement_petscspace_degree 3 -dm_refine 0 -convest_num_refine 1 -snes_convergence_estimate
   test:
     suffix: 3d_q1_trig_vlap
-    args: -sol_type vlap_trig -dim 3 -simplex 0 -displacement_petscspace_degree 1 -dm_refine 1 -convest_num_refine 2 -snes_convergence_estimate
+    args: -sol_type vlap_trig -dim 3 -cells 2,2,2 -simplex 0 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_q2_trig_vlap
     args: -sol_type vlap_trig -dim 3 -simplex 0 -displacement_petscspace_degree 2 -dm_refine 0 -convest_num_refine 1 -snes_convergence_estimate
@@ -578,7 +578,7 @@ int main(int argc, char **argv)
   test:
     suffix: 3d_p1_trig_elas
     requires: ctetgen
-    args: -sol_type elas_trig -dim 3 -displacement_petscspace_degree 1 -dm_refine 1 -convest_num_refine 2 -snes_convergence_estimate
+    args: -sol_type elas_trig -dim 3 -cells 2,2,2 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_p2_trig_elas
     requires: ctetgen
@@ -589,7 +589,7 @@ int main(int argc, char **argv)
     args: -sol_type elas_trig -dim 3 -displacement_petscspace_degree 3 -dm_refine 0 -convest_num_refine 1 -snes_convergence_estimate
   test:
     suffix: 3d_q1_trig_elas
-    args: -sol_type elas_trig -dim 3 -simplex 0 -displacement_petscspace_degree 1 -dm_refine 0 -convest_num_refine 2 -snes_convergence_estimate
+    args: -sol_type elas_trig -dim 3 -cells 2,2,2 -simplex 0 -displacement_petscspace_degree 1 -convest_num_refine 2 -snes_convergence_estimate
   test:
     suffix: 3d_q2_trig_elas
     args: -sol_type elas_trig -dim 3 -simplex 0 -displacement_petscspace_degree 2 -dm_refine 0 -convest_num_refine 1 -snes_convergence_estimate
