41e54440545de7f26e98f0528bfcafacc4aa81ad
jwallwork23 <j.wallwork16@imperial.ac.uk>
2018-11-20 14:47:30 -0600

    First attempt at install script for ADOL-C

--- config/BuildSystem/config/packages/ADOLC.py
+++ config/BuildSystem/config/packages/ADOLC.py
@@ -3,29 +3,22 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.version    = 'ADOL-C-2.6.0.tgz'
-    self.download   = ['https://www.coin-or.org/download/source/ADOL-C/'+self.version+'.tar.gz']
-    self.functions  = []
-    self.includes   = ['adolc.h']
-    self.liblist    = [['libadolc.la']]
-    self.cxx        = 1
-    self.precisions = ['double']
-    self.complex    = 0
+    self.version          = 'ADOL-C-2.6.0'
+    self.download         = ['https://www.coin-or.org/download/source/ADOL-C/'+self.version+'.tar.gz']
+    self.includes         = ['adolc.h']
+    self.liblist          = [['libadolc.la']]
+    self.functions        = []  # TODO: Include function for testing
+    self.lookforbydefault = 0
+    self.cxx              = 1
+    self.precisions       = ['double']
+    self.complex          = 0
     return
 
   def setupDependencies(self, framework):
     config.package.Package.setupDependencies(self, framework)
-    self.ADOLC = framework.require('config.packages.ADOLC', self)
-    self.deps  = [self.ADOLC]
     return
 
-  def Install(self):
-    import os
-
-    self.framework.pushLanguage('Cxx')
-
-    makeam = os.path.join(self.packageDir, 'Makefile.am')
-
-    g = open(makeam, 'w')
-    g
-#TODO
+  def formGNUConfigureArgs(self):
+    args = config.package.GNUPackage.formGNUConfigureArgs(self)
+    args.append('--with-docexa')
+    return args
