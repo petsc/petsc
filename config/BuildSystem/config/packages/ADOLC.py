09809567e426c3874e3a5abf4757f22f5c76e572
jwallwork23 <j.wallwork16@imperial.ac.uk>
2018-11-20 17:13:47 -0600

    ADOL-C download and install working

--- config/BuildSystem/config/packages/ADOLC.py
+++ config/BuildSystem/config/packages/ADOLC.py
@@ -1,29 +1,29 @@
 import config.package
 
-class Configure(config.package.Package):
+class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.version          = '2.6.0'
-    self.download         = ['https://www.coin-or.org/download/source/ADOL-C/ADOL-C-'+self.version+'.tgz']
-    self.includes         = ['adolc.h', 'adalloc.h']
+#    self.version          = '2.6.0'
+    self.download         = ['https://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.6.0.tgz']
+    self.includes         = ['adolc/adolc.h']
     self.liblist          = [['libadolc.la']]
-    self.functions        = ['myalloc2']
-    self.lookforbydefault = 0
+    self.functions        = []
     self.cxx              = 1
-    self.requirescxx11    = 1
-    self.precisions       = ['double']
-    self.complex          = 0
+#    self.requirescxx11    = 1
+#    self.precisions       = ['double']
+#    self.complex          = 0
 #   self.name             = 'ADOL-C'
 #   self.package          = 'adolc'
 #   self.PACKAGE          = 'ADOLC'
 #   self.downloadname     = 'ADOL-C'
-    self.downloaddirnames = ['ADOL-C-'+self.version]
+    self.downloaddirnames = ['ADOL-C-2.6.0']
     return
 
   def setupDependencies(self, framework):
     config.package.Package.setupDependencies(self, framework)
 #    self.colpack = framework.require('config.packages.ColPack',self) # TODO
 #    self.deps    = [self.colpack] # TODO
+    self.deps = []
     return
 
 #  def Install(self):
@@ -32,6 +32,7 @@ class Configure(config.package.Package):
 
   def formGNUConfigureArgs(self):
     args = config.package.GNUPackage.formGNUConfigureArgs(self)
-    args.append('--enable-sparse')
+    args.append('--enable-docexa')
+#    args.append('--enable-sparse')
 #    args.append('--with-colpack-dir=$COLPACK_HOME') # TODO
     return args
