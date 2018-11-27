b410bcea547c347a6f59939f13798c913532057b
Stefano Zampini <stefano.zampini@gmail.com>
2018-11-27 11:54:22 +0300

    configure GLVis: Update commit

--- config/BuildSystem/config/packages/GLVis.py
+++ config/BuildSystem/config/packages/GLVis.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.gitcommit              = 'fa3aeaa285589bd788f9d21679d0537381184f75'
+    self.gitcommit              = 'bca608d856fe8183f9f7bf5e57d493af0b02f616'
     self.download               = ['git://https://github.com/stefanozampini/glvis.git']
     self.linkedbypetsc          = 0
     self.downloadonWindows      = 1
