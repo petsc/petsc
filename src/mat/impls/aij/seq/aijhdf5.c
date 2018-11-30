6123c46a119e5ec901b6a098686202a5cac55150
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-11-30 15:51:41 +0100

    MatLoad_AIJ_HDF5: improve comments

--- src/mat/impls/aij/seq/aijhdf5.c
+++ src/mat/impls/aij/seq/aijhdf5.c
@@ -81,17 +81,17 @@ PetscErrorCode MatLoad_AIJ_HDF5(Mat mat, PetscViewer viewer)
   m = mat->rmap->n;
   M--;
 
-  /* Determine offset and count of elements for reading local part of array data */
   /* Create PetscLayout for j and a vectors; construct ranges first */
   ierr = PetscLayoutCreate(comm,&jmap);CHKERRQ(ierr);
   ierr = PetscCalloc1(size+1, &jmap->range);CHKERRQ(ierr);
   ierr = MPI_Allgather(&i[0], 1, MPIU_INT, jmap->range, 1, MPIU_INT, comm);CHKERRQ(ierr);
+  /* Last rank has global number of nonzeros (= length of j and a arrays) in i[m] (last i entry) so broadcast it */
   jmap->range[size] = i[m];
   ierr = MPI_Bcast(&jmap->range[size], 1, MPIU_INT, size-1, comm);CHKERRQ(ierr);
   for (p=size-1; p>0; p--) {
     if (!jmap->range[p]) jmap->range[p] = jmap->range[p+1]; /* for ranks with 0 rows, take the value from the next processor */
   }
-  i[m] = jmap->range[rank+1];
+  i[m] = jmap->range[rank+1]; /* i[m] (last i entry) is equal to next rank's offset */
   /* Deduce rstart, rend, n and N from the ranges */
   ierr = PetscLayoutSetUp(jmap);CHKERRQ(ierr);
 
