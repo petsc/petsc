petsc/petsc

commit f20c2d659b168217cb455989f28afe052d71a64c

python: use sort(key=) instead of sort with cmp function
For Python-3, but also cleaner.

lib/petsc/bin/maint/lex.py
@@ -454,10 +454,10 @@ def lex(module=None,debug=0,optimize=0):
             error = 1
 
     # Sort the functions by line number
-    fsymbols.sort(lambda x,y: cmp(x.__code__.co_firstlineno,y.__code__.co_firstlineno))
+    fsymbols.sort(key=lambda x: x.__code__.co_firstlineno)
 
     # Sort the strings by regular expression length
-    ssymbols.sort(lambda x,y: (len(x[1]) < len(y[1])) - (len(x[1]) > len(y[1])))
+    ssymbols.sort(key=lambda x: len(x[1]))
 
     # Check for non-empty symbols
     if len(fsymbols) == 0 and len(ssymbols) == 0:

config/BuildSystem/config/setsOrdered.py
@@ -100,7 +100,7 @@ class BaseSet(object):
                 items.extend([(key, v) for v in value])
             else:
                 items.append((key, value))
-        items.sort(lambda a, b: a[1].__cmp__(b[1]))
+        items.sort(key=lambda a: a[1])
         return [i[0] for i in items]
 
     # Standard protocols: __len__, __repr__, __str__, __iter__

config/BuildSystem/help.py
@@ -62,7 +62,7 @@ class Info(logger.Logger):
     (nameLen, descLen) = self.getTextSizes()
     format = '  %-'+str(nameLen)+'s: %s\n'
     items  = self.sections.items()
-    items.sort(lambda a, b: a[1][0].__cmp__(b[1][0]))
+    items.sort(key=lambda a: a[1][0])
     for section, names in items:
       f.write(section+':\n')
       for name in names[1]:
@@ -128,7 +128,7 @@ class Help(Info):
     format    = '  -%s\n       %s\n'
     formatDef = '  -%s\n       %s  current: %s\n'
     items = self.sections.items()
-    items.sort(lambda a, b: a[1][0].__cmp__(b[1][0]))
+    items.sort(key=lambda a: a[1][0])
     for section, names in items:
       if sections and not section.lower() in sections: continue
       f.write(section+':\n')

commit 3b5b5b43c1b783ebd0abc676d1f7747efe38b8ac

Commit f20c2d659b168217cb455989f28afe052d71a64c broke lex.py so the users manual was no longer being built. There reverted the changes to lex.py
Commit-type: bug-fix

lib/petsc/bin/maint/lex.py
@@ -454,10 +454,10 @@ def lex(module=None,debug=0,optimize=0):
             error = 1
 
     # Sort the functions by line number
-    fsymbols.sort(key=lambda x: x.__code__.co_firstlineno)
+    fsymbols.sort(lambda x,y: cmp(x.__code__.co_firstlineno,y.__code__.co_firstlineno))
 
     # Sort the strings by regular expression length
-    ssymbols.sort(key=lambda x: len(x[1]))
+    ssymbols.sort(lambda x,y: (len(x[1]) < len(y[1])) - (len(x[1]) > len(y[1])))
 
     # Check for non-empty symbols
     if len(fsymbols) == 0 and len(ssymbols) == 0: