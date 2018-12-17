ecb828f358935bc2edb1610d09b391d57eb4c79e
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-12-17 15:20:14 +0100

    vec_vec_tests-ex48: test group/dataset existence queries

--- src/vec/vec/examples/tests/ex48.c
+++ src/vec/vec/examples/tests/ex48.c
@@ -11,7 +11,6 @@ static char help[] = "Tests HDF5 attribute I/O.\n\n";
 int main(int argc,char **argv)
 {
   PetscViewer    viewer;
-  Vec            x;
   PetscBool      has;
   PetscInt       a,p,s,n=5;
 #define np 4
@@ -19,9 +18,10 @@ int main(int argc,char **argv)
 #define na 6
   const char     attr[na][128]  = {"integer", "real",     "boolean0", "boolean1", "string",     "nonExistingAttribute"};
   PetscDataType  dts[na]        = {PETSC_INT, PETSC_REAL, PETSC_BOOL, PETSC_BOOL, PETSC_STRING, PETSC_INT};
-#define ns 2
-  const char     psufs[ns][128] = {"", "/x"}; /* test group and dataset (vector) attributes */
-  const char     vecname[]      = "x";
+#define ns 3
+  const char     names[ns][128] = {"", "x", "nonExistingVec"}; /* test group and dataset (vector) attributes */
+  Vec            vecs[ns];
+  const char     *name,*group;
   char           buf[PETSC_MAX_PATH_LEN];
   PetscBool      boolean0       = PETSC_FALSE;
   PetscBool      boolean1       = PETSC_TRUE;
@@ -36,29 +36,35 @@ int main(int argc,char **argv)
 
   PetscFunctionBegin;
   ierr = PetscInitialize(&argc, &argv, (char*) 0, help);if (ierr) return ierr;
-  ierr = PetscPrintf(PETSC_COMM_WORLD, "na np %D %D\n", na,np);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "na np ns %D %D %D\n", na, np, ns);CHKERRQ(ierr);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);
   ierr = PetscOptionsGetInt(NULL,NULL, "-n", &n, NULL);CHKERRQ(ierr);
+  ierr = PetscMemzero(vecs, ns*sizeof(Vec));CHKERRQ(ierr);
 
-  /* create & initialize vector x */
-  ierr = VecCreate(PETSC_COMM_WORLD, &x);CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject)x, vecname);CHKERRQ(ierr);
-  ierr = VecSetSizes(x, n, PETSC_DECIDE);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(x);CHKERRQ(ierr);
-  ierr = VecSetRandom(x,NULL);CHKERRQ(ierr);
+  /* create & initialize vector vecs[1] "x" */
+  ierr = VecCreate(PETSC_COMM_WORLD, &vecs[1]);CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject)vecs[1], names[1]);CHKERRQ(ierr);
+  ierr = VecSetSizes(vecs[1], n, PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(vecs[1]);CHKERRQ(ierr);
+  ierr = VecSetRandom(vecs[1],NULL);CHKERRQ(ierr);
+
+  /* create & initialize vector vecs[2] "nonExistingVec" */
+  ierr = VecDuplicate(vecs[1], &vecs[2]);CHKERRQ(ierr);
+  ierr = VecCopy(vecs[1], vecs[2]);CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject)vecs[2], names[2]);CHKERRQ(ierr);
 
   ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, DF, FILE_MODE_WRITE, &viewer);CHKERRQ(ierr);
 
   for (p=0; p<np-1; p++) {
     ierr = PetscViewerHDF5PushGroup(viewer, path[p]);CHKERRQ(ierr);
-    ierr = VecView(x, viewer);CHKERRQ(ierr);
+    ierr = VecView(vecs[1], viewer);CHKERRQ(ierr);
     ierr = PetscViewerHDF5PopGroup(viewer);CHKERRQ(ierr);
   }
 
   /* test attribute writing */
-  for (s=0; s<ns; s++) for (p=0; p<np-1; p++) {
+  for (p=0; p<np-1; p++) for (s=0; s<ns-1; s++) {
     a = 0;
-    ierr = PetscSNPrintf(buf, sizeof(buf), "%s%s", path[p], psufs[s]);CHKERRQ(ierr);
+    ierr = PetscSNPrintf(buf, sizeof(buf), "%s/%s", path[p], names[s]);CHKERRQ(ierr);
     ierr = PetscViewerHDF5WriteAttribute(viewer, buf, attr[a], dts[a], &integer);CHKERRQ(ierr);  a++;
     ierr = PetscViewerHDF5WriteAttribute(viewer, buf, attr[a], dts[a], &real);CHKERRQ(ierr);     a++;
     ierr = PetscViewerHDF5WriteAttribute(viewer, buf, attr[a], dts[a], &boolean0);CHKERRQ(ierr); a++;
@@ -70,9 +76,31 @@ int main(int argc,char **argv)
   ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, DF, FILE_MODE_READ, &viewer);CHKERRQ(ierr);
 
+  /* test group/dataset querying */
+  for (p=0; p<np; p++) {
+    ierr = PetscViewerHDF5PushGroup(viewer, path[p]);CHKERRQ(ierr);
+    ierr = PetscViewerHDF5GetGroup(viewer, &group);CHKERRQ(ierr);
+    ierr = PetscStrcmp(path[p], group, &has);CHKERRQ(ierr);
+    if (!has) SETERRQ2(PETSC_COMM_WORLD, PETSC_ERR_PLIB, "current group %s not equal to anticipated %s", group, path[p]);
+    ierr = PetscViewerHDF5HasGroup(viewer, &has);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD, "Has %s group? %D\n", path[p], has);CHKERRQ(ierr);
+
+    for (s=0; s<ns; s++) {
+      if (vecs[s]) {
+        ierr = PetscObjectGetName((PetscObject)vecs[s], &name);CHKERRQ(ierr);
+        ierr = PetscStrcmp(names[s], name, &has);CHKERRQ(ierr);
+        if (!has) SETERRQ2(PETSC_COMM_WORLD, PETSC_ERR_PLIB, "current object name %s not equal to anticipated %s", name, names[s]);
+        ierr = PetscViewerHDF5HasObject(viewer, (PetscObject)vecs[s], &has);CHKERRQ(ierr);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "-   %s/%s dataset? %D\n", group, name, has);CHKERRQ(ierr);
+      }
+    }
+
+    ierr = PetscViewerHDF5PopGroup(viewer);CHKERRQ(ierr);
+  }
+
   /* test attribute querying including nonexisting attributes */
-  for (s=0; s<ns; s++) for (p=0; p<np; p++)  {
-    ierr = PetscSNPrintf(buf, sizeof(buf), "%s%s", path[p], psufs[s]);CHKERRQ(ierr);
+  for (p=0; p<np; p++) for (s=0; s<ns; s++) {
+    ierr = PetscSNPrintf(buf, sizeof(buf), "%s/%s", path[p], names[s]);CHKERRQ(ierr);
     for (a=0; a<na; a++) {
       ierr = PetscViewerHDF5HasAttribute(viewer, buf, attr[a], &has);CHKERRQ(ierr);
       ierr = PetscPrintf(PETSC_COMM_WORLD, "Has %s/%s? %D\n", buf, attr[a], has);CHKERRQ(ierr);
@@ -80,13 +108,13 @@ int main(int argc,char **argv)
   }
 
   /* test attribute reading */
-  for (s=0; s<ns; s++) for (p=0; p<np-1; p++) {
+  for (p=0; p<np-1; p++) for (s=0; s<ns-1; s++) {
     integer = -1;
     real = -1.0;
     boolean0 = -1;
     boolean1 = -1;
     a = 0;
-    ierr = PetscSNPrintf(buf, sizeof(buf), "%s%s", path[p], psufs[s]);CHKERRQ(ierr);
+    ierr = PetscSNPrintf(buf, sizeof(buf), "%s/%s", path[p], names[s]);CHKERRQ(ierr);
     ierr = PetscViewerHDF5ReadAttribute(viewer, buf, attr[a], dts[a], &integer);CHKERRQ(ierr);  a++;
     ierr = PetscViewerHDF5ReadAttribute(viewer, buf, attr[a], dts[a], &real);CHKERRQ(ierr);     a++;
     ierr = PetscViewerHDF5ReadAttribute(viewer, buf, attr[a], dts[a], &boolean0);CHKERRQ(ierr); a++;
@@ -110,7 +138,8 @@ int main(int argc,char **argv)
     if (a != na-1) SETERRQ2(PETSC_COMM_WORLD, PETSC_ERR_ARG_OUTOFRANGE, "a != na-1, %D != %D", a, na-1);
   }
 
-  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  ierr = VecDestroy(&vecs[1]);CHKERRQ(ierr);
+  ierr = VecDestroy(&vecs[2]);CHKERRQ(ierr);
   ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
