f0e385c99032cbd0ccef7ac2ac1368dbf95df929
Satish Balay <balay@mcs.anl.gov>
2019-01-21 14:40:45 -0600

    petsc4py: update to snapshot with  DMSetDS -> DMCopyDisc change

--- config/BuildSystem/config/packages/petsc4py.py
+++ config/BuildSystem/config/packages/petsc4py.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '41c5af0' # master (Jan 3 2019)
+    self.gitcommit              = 'a72aa7a' # master (Jan 21 2019)
     self.download               = ['git://https://bitbucket.org/petsc/petsc4py','https://bitbucket.org/petsc/petsc4py/get/'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
