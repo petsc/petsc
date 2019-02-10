89ccb272901845e027463e79793c2a555b726b1e
Jed Brown <jed@jedbrown.org>
2019-02-09 20:53:46 -0700

    PetscCheckPointer: add test

---
+++ src/sys/error/examples/tests/ex2.c
@@ -0,0 +1,28 @@
+static char help[] = "Tests checking pointers.\n\n";
+
+#include <petscsys.h>
+#include <petsc/private/petscimpl.h>
+
+int main(int argc, char *args[])
+{
+  PetscErrorCode ierr;
+  PetscInt *ptr,*ptr2;
+
+  ierr = PetscInitialize(&argc, &args, (char*) 0, help);
+  if (ierr) return ierr;
+  ierr = PetscMalloc(1024 * 1024 * 8,&ptr);CHKERRQ(ierr); /* Almost certainly larger than MMAP_THRESHOLD (128 KiB by default) */
+  if (!PetscCheckPointer(ptr,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook valid pointer %p for invalid pointer\n",(void*)ptr);CHKERRQ(ierr);}
+  ptr2 = ptr;
+  ierr = PetscFree(ptr);CHKERRQ(ierr);
+  if (PetscCheckPointer(ptr,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook NULL pointer for valid pointer\n");CHKERRQ(ierr);}
+  if (PetscCheckPointer(ptr2,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook invalid pointer %p for valid pointer\n",(void*)ptr2);CHKERRQ(ierr);}
+  ierr = PetscFinalize();
+  return ierr;
+}
+
+/*TEST
+
+   test:
+     args: -check_pointer_intensity 1
+
+TEST*/
