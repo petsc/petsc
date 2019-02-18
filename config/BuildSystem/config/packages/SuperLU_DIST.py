18f6f3ad8c1b53463d2a90fac4635492999f06ed
Jed Brown <jed@jedbrown.org>
2019-02-18 13:45:40 -0700

    SuperLU_DIST: update after bug fix for MPI_Comm_get_attr

--- config/BuildSystem/config/packages/SuperLU_DIST.py
+++ config/BuildSystem/config/packages/SuperLU_DIST.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit         = 'v6.1.0'
+    self.gitcommit         = 'v6.1.1'
     self.download         = ['git://https://github.com/xiaoyeli/superlu_dist','https://github.com/xiaoyeli/superlu_dist/archive/'+self.gitcommit+'.tar.gz']
     self.downloaddirnames = ['SuperLU_DIST','superlu_dist']
     self.functions        = ['set_default_options_dist']
