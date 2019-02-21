75eef61807b47864ef46280baf42f6e57aa3b1a3
Jed Brown <jed@jedbrown.org>
2019-02-21 00:08:15 -0700

    PetscCheckPointer ex2: allow success via alt output when free'd memory still accessible

--- src/sys/error/examples/tests/ex2.c
+++ src/sys/error/examples/tests/ex2.c
@@ -14,10 +14,17 @@ int main(int argc, char *args[])
   if (!PETSC_RUNNING_ON_VALGRIND) {                         /* PetscCheckPointer always returns TRUE when running on Valgrind */
     ierr = PetscMalloc(1024 * 1024 * 8,&ptr);CHKERRQ(ierr); /* Almost certainly larger than MMAP_THRESHOLD (128 KiB by default) */
     if (!PetscCheckPointer(ptr,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook valid pointer %p for invalid pointer\n",(void*)ptr);CHKERRQ(ierr);}
+    ptr[0] = 0x12345678;
     ptr2 = ptr;
     ierr = PetscFree(ptr);CHKERRQ(ierr);
     if (PetscCheckPointer(ptr,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook NULL pointer for valid pointer\n");CHKERRQ(ierr);}
-    if (PetscCheckPointer(ptr2,PETSC_INT)) {ierr = PetscPrintf(PETSC_COMM_SELF,"Mistook invalid pointer %p for valid pointer\n",(void*)ptr2);CHKERRQ(ierr);}
+    if (PetscCheckPointer(ptr2,PETSC_INT)) {
+      if (*(volatile PetscInt*)ptr2 == 0x12345678) {
+        ierr = PetscPrintf(PETSC_COMM_SELF,"Free'd pointer is still accessible\n");CHKERRQ(ierr);
+      } else {
+        ierr = PetscPrintf(PETSC_COMM_SELF,"Free'd pointer is still accessible, but contains corrupt data\n");CHKERRQ(ierr);
+      }
+    }
   }
   ierr = PetscFinalize();
   return ierr;
