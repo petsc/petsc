fe6faf198138896044c7f0fed7bb3b42de1cdcec
Junchao Zhang <jczhang@mcs.anl.gov>
2019-02-25 11:37:14 -0600

    dos2unix format convert

--- src/mat/examples/tests/ex72mmio.h
+++ src/mat/examples/tests/ex72mmio.h
@@ -30,47 +30,46 @@ int mm_write_mtx_array_size(FILE *f, int M, int N);
 
 /********************* MM_typecode query fucntions ***************************/
 
-#define mm_is_matrix(typecode)	((typecode)[0]=='M')
+#define mm_is_matrix(typecode)    ((typecode)[0]=='M')
 
-#define mm_is_sparse(typecode)	((typecode)[1]=='C')
+#define mm_is_sparse(typecode)    ((typecode)[1]=='C')
 #define mm_is_coordinate(typecode)((typecode)[1]=='C')
-#define mm_is_dense(typecode)	((typecode)[1]=='A')
-#define mm_is_array(typecode)	((typecode)[1]=='A')
+#define mm_is_dense(typecode)    ((typecode)[1]=='A')
+#define mm_is_array(typecode)    ((typecode)[1]=='A')
 
-#define mm_is_complex(typecode)	((typecode)[2]=='C')
-#define mm_is_real(typecode)		((typecode)[2]=='R')
-#define mm_is_pattern(typecode)	((typecode)[2]=='P')
+#define mm_is_complex(typecode)    ((typecode)[2]=='C')
+#define mm_is_real(typecode)        ((typecode)[2]=='R')
+#define mm_is_pattern(typecode)    ((typecode)[2]=='P')
 #define mm_is_integer(typecode) ((typecode)[2]=='I')
 
 #define mm_is_symmetric(typecode)((typecode)[3]=='S')
-#define mm_is_general(typecode)	((typecode)[3]=='G')
-#define mm_is_skew(typecode)	((typecode)[3]=='K')
+#define mm_is_general(typecode)    ((typecode)[3]=='G')
+#define mm_is_skew(typecode)    ((typecode)[3]=='K')
 #define mm_is_hermitian(typecode)((typecode)[3]=='H')
 
-int mm_is_valid(MM_typecode matcode);		/* too complex for a macro */
+int mm_is_valid(MM_typecode matcode);        /* too complex for a macro */
 
 
 /********************* MM_typecode modify fucntions ***************************/
 
-#define mm_set_matrix(typecode)	((*typecode)[0]='M')
-#define mm_set_coordinate(typecode)	((*typecode)[1]='C')
-#define mm_set_array(typecode)	((*typecode)[1]='A')
-#define mm_set_dense(typecode)	mm_set_array(typecode)
-#define mm_set_sparse(typecode)	mm_set_coordinate(typecode)
+#define mm_set_matrix(typecode)    ((*typecode)[0]='M')
+#define mm_set_coordinate(typecode)    ((*typecode)[1]='C')
+#define mm_set_array(typecode)    ((*typecode)[1]='A')
+#define mm_set_dense(typecode)    mm_set_array(typecode)
+#define mm_set_sparse(typecode)    mm_set_coordinate(typecode)
 
 #define mm_set_complex(typecode)((*typecode)[2]='C')
-#define mm_set_real(typecode)	((*typecode)[2]='R')
+#define mm_set_real(typecode)    ((*typecode)[2]='R')
 #define mm_set_pattern(typecode)((*typecode)[2]='P')
 #define mm_set_integer(typecode)((*typecode)[2]='I')
 
 
 #define mm_set_symmetric(typecode)((*typecode)[3]='S')
 #define mm_set_general(typecode)((*typecode)[3]='G')
-#define mm_set_skew(typecode)	((*typecode)[3]='K')
+#define mm_set_skew(typecode)    ((*typecode)[3]='K')
 #define mm_set_hermitian(typecode)((*typecode)[3]='H')
 
-#define mm_clear_typecode(typecode) ((*typecode)[0]=(*typecode)[1]= \
-									(*typecode)[2]=' ',(*typecode)[3]='G')
+#define mm_clear_typecode(typecode) ((*typecode)[0]=(*typecode)[1]=(*typecode)[2]=' ',(*typecode)[3]='G')
 
 #define mm_initialize_typecode(typecode) mm_clear_typecode(typecode)
 
@@ -78,43 +77,43 @@ int mm_is_valid(MM_typecode matcode);		/* too complex for a macro */
 /********************* Matrix Market error codes ***************************/
 
 
-#define MM_COULD_NOT_READ_FILE	11
-#define MM_PREMATURE_EOF		12
-#define MM_NOT_MTX				13
-#define MM_NO_HEADER			14
-#define MM_UNSUPPORTED_TYPE		15
-#define MM_LINE_TOO_LONG		16
-#define MM_COULD_NOT_WRITE_FILE	17
+#define MM_COULD_NOT_READ_FILE    11
+#define MM_PREMATURE_EOF          12
+#define MM_NOT_MTX                13
+#define MM_NO_HEADER              14
+#define MM_UNSUPPORTED_TYPE       15
+#define MM_LINE_TOO_LONG          16
+#define MM_COULD_NOT_WRITE_FILE   17
 
 
 /******************** Matrix Market internal definitions ********************
 
    MM_matrix_typecode: 4-character sequence
 
-				    ojbect 		sparse/   	data        storage
-						  		dense     	type        scheme
+                    ojbect      sparse/     data        storage
+                                dense       type        scheme
 
-   string position:	 [0]        [1]			[2]         [3]
+   string position:  [0]        [1]         [2]         [3]
 
-   Matrix typecode:  M(atrix)  C(oord)		R(eal)   	G(eneral)
-						        A(array)	C(omplex)   H(ermitian)
-											P(attern)   S(ymmetric)
-								    		I(nteger)	K(kew)
+   Matrix typecode:  M(atrix)   C(oord)     R(eal)      G(eneral)
+                                A(array)    C(omplex)   H(ermitian)
+                                            P(attern)   S(ymmetric)
+                                            I(nteger)   K(kew)
 
  ***********************************************************************/
 
-#define MM_MTX_STR		"matrix"
-#define MM_ARRAY_STR	"array"
-#define MM_DENSE_STR	"array"
+#define MM_MTX_STR        "matrix"
+#define MM_ARRAY_STR    "array"
+#define MM_DENSE_STR    "array"
 #define MM_COORDINATE_STR "coordinate"
-#define MM_SPARSE_STR	"coordinate"
-#define MM_COMPLEX_STR	"complex"
-#define MM_REAL_STR		"real"
-#define MM_INT_STR		"integer"
+#define MM_SPARSE_STR    "coordinate"
+#define MM_COMPLEX_STR    "complex"
+#define MM_REAL_STR        "real"
+#define MM_INT_STR        "integer"
 #define MM_GENERAL_STR  "general"
-#define MM_SYMM_STR		"symmetric"
-#define MM_HERM_STR		"hermitian"
-#define MM_SKEW_STR		"skew-symmetric"
+#define MM_SYMM_STR        "symmetric"
+#define MM_HERM_STR        "hermitian"
+#define MM_SKEW_STR        "skew-symmetric"
 #define MM_PATTERN_STR  "pattern"
 
 
