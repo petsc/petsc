75fe3c32bc4d43b8babf78ac6e8aee5fcc30fc8b
Barry Smith <bsmith@mcs.anl.gov>
2019-04-16 22:01:35 -0500

    chunksize could overflow and become negative; thus preventing the correct setting of chunkDim thus resulting in errors from HDF5.
    Commit-type: bug-fix
    Reported-by: Sajid Ali <sajidsyed2021@u.northwestern.edu>

--- src/vec/vec/impls/mpi/pdvec.c
+++ src/vec/vec/impls/mpi/pdvec.c
@@ -709,7 +709,7 @@ PetscErrorCode VecView_MPI_HDF5(Vec xin, PetscViewer viewer)
   hsize_t           maxDims[4], dims[4], chunkDims[4], count[4],offset[4];
   PetscInt          timestep;
   PetscInt          low;
-  PetscInt          chunksize;
+  PetscInt64        chunksize;
   const PetscScalar *x;
   const char        *vecname;
   PetscErrorCode    ierr;
