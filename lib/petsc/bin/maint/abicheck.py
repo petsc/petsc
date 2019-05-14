fecbc081bce81aa26982d94ac7fb180ba8ee551c
Junchao Zhang <jczhang@mcs.anl.gov>
2019-05-14 15:24:06 -0500

    Add a wrapper script to check abi/api for petsc

---
+++ lib/petsc/bin/maint/abicheck.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+#
+#
+# Check ABI/API compatibility of two PETSc versions (the old and the new). Old and new PETSc should have already been built using GCC and with -g
+#
+# Usage:
+#   ./abicheck.py -old_dir  <old PETSC_DIR>
+#                 -old_arch <old PETSC_ARCH>
+#                 -new_dir  <new PETSC_DIR>
+#                 -new_arch <new PETSC_ARCH>
+#                 -libs     <a list of space separated library names>. Names can be petsc, petscvec, vec, ksp, etc.
+#
+
+from __future__ import print_function
+import os, sys
+import subprocess
+import argparse
+
+def gen_xml_desc(petsc_dir, petsc_arch, libs, xmlfile):
+  """ Generate an ABI descriptor file for the given PETSC_DIR and PETSC_ARCH"""
+
+  # Get branch and hash of the source used to build the library. We use them as an informative version number of the library
+  petscconf = os.path.join(petsc_dir, petsc_arch, 'include', 'petscconf.h')
+
+  branch = subprocess.check_output(['grep', '^#define PETSC_VERSION_BRANCH_GIT', petscconf])
+  branch = branch.split("\"")[1]
+  hash   = subprocess.check_output(['grep', '^#define PETSC_VERSION_GIT', petscconf])
+  hash   = hash.split("\"")[1]
+
+  petsclibs = ''
+  petscheaders = ''
+
+  for name in libs:
+    name = name.lower()
+    if not name.startswith('petsc'): # one can input -libs petsc, or libs petscsys petscvec, or -libs sys vec
+      name = 'petsc'+name
+    libname       = 'lib'+name
+    libname_so    = os.path.join(petsc_dir, petsc_arch, 'lib', libname+'.so')
+    libname_dylib = os.path.join(petsc_dir, petsc_arch, 'lib', libname+'.dylib')
+    libname_a     = os.path.join(petsc_dir, petsc_arch, 'lib', libname+'.a')
+    lib = ''
+    if os.path.isfile(libname_so):
+      lib = libname_so
+    elif os.path.isfile(libname_dylib):
+      lib = libname_dylib
+    elif os.path.isfile(libname_a):
+      lib = libname_a
+
+    if lib == '':
+      raise RuntimeError('Could not find library %s for PETSC_DIR=%s PETSC_ARCH=%s. Please configure and build it before doing ABI/API checking\n'% (name,petsc_dir,petsc_arch))
+    petsclibs += lib+'\n'
+
+    header = os.path.join(petsc_dir,'include',name+'.h')
+    if not os.path.isfile(header):
+      raise RuntimeError('Could not find the header file %s\n'% (header))
+    petscheaders += header+'\n'
+
+  with open(xmlfile, "w") as file:
+    file.write("<version>\n")
+    file.write(branch + ' (' + hash +')')
+    file.write("</version>\n\n")
+
+    file.write("<headers>\n")
+    file.write(petscheaders)
+    file.write("\n")
+    file.write("</headers>\n\n")
+
+    file.write("<libs>\n")
+    file.write(petsclibs)
+    file.write("\n")
+    file.write("</libs>\n\n")
+
+    file.write('<include_paths>\n')
+    file.write(os.path.join(petsc_dir,'include'))
+    file.write("\n")
+    file.write(os.path.join(petsc_dir,petsc_arch,'include'))
+    file.write("\n")
+    file.write("</include_paths>\n")
+
+def run_abi_checker(petsc_dir, petsc_arch, abi_dir,name, oldxml, newxml):
+  retval         = 0
+  petscvariables = os.path.join(petsc_dir, petsc_arch, 'lib', 'petsc','conf', 'petscvariables')
+  ccstring       = subprocess.check_output(['grep', '^CC =', petscvariables])
+  cc             = ccstring.split('=')[1].strip()
+  reportfile     = os.path.join(abi_dir,'report.html')
+  abichecker     = os.path.join(petsc_dir, 'lib', 'petsc', 'bin', 'maint', 'abi-compliance-checker', 'abi-compliance-checker.pl')
+
+  retval = subprocess.call([abichecker, '-l', 'petsc', '--lang=C', '-old', oldxml, '-new', newxml, '--gcc-path', cc, '--report-path', reportfile])
+  return retval
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-old_dir",  help="Old PETSC_DIR, which defines the PETSc library to compare with", required=True)
+  parser.add_argument("-old_arch", help="Old PETSC_ARCH, which defines the PETSc library to compare with", required=True)
+  parser.add_argument("-new_dir",  help="New PETSC_DIR", required=True)
+  parser.add_argument("-new_arch", help="New PETSC_ARCH", required=True)
+  parser.add_argument("-libs",     help="PETSc library names, e.g., petsc", nargs='+', required=True)
+
+  args     = parser.parse_args()
+  old_dir  = args.old_dir
+  old_arch = args.old_arch
+  new_dir  = args.new_dir
+  new_arch = args.new_arch
+  libs     = args.libs
+
+  # We generate report under new_dir
+  abi_dir = os.path.join(new_dir, new_arch, 'abi')
+  if not os.path.isdir(abi_dir):
+    os.makedirs (abi_dir)
+
+  print("\nChecking library %s ..." %(libs))
+  oldxml = os.path.join(abi_dir, 'old.xml')
+  newxml = os.path.join(abi_dir, 'new.xml')
+  gen_xml_desc(old_dir,old_arch, libs, oldxml)
+  gen_xml_desc(new_dir,new_arch, libs, newxml)
+  ierr = run_abi_checker(new_dir, new_arch, abi_dir, libs, oldxml, newxml)
+
+  print("=========================================================================================")
+  if (ierr):
+    print("Error: ABI/API compatibility check failed. Open the compatibility report(s) with a browser to see details.")
+  else:
+    print("ABI/API of the two versions are compatible.")
+
+if __name__ == '__main__':
+    main()
