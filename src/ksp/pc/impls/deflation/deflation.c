409a357bae74e46200d26aa0a2f21a6fc06036bf
Jakub Kruzik <jakub.kruzik@gmail.com>
2019-05-17 11:04:15 +0200

    add SetUP

--- src/ksp/pc/impls/deflation/deflation.c
+++ src/ksp/pc/impls/deflation/deflation.c
@@ -65,9 +65,10 @@ typedef struct {
   PetscInt  reductionfact;
   Mat       W,Wt,AW,WtAW;    /* deflation space, coarse problem mats */
   KSP       WtAWinv;         /* deflation coarse problem */
+  KSPType   ksptype;
   Vec       *work;
 
-  PCDEFLATIONSpaceType spacetype;
+  PCDeflationSpaceType spacetype;
   PetscInt             spacesize;
   PetscInt             nestedlvl;
   PetscInt             maxnestedlvl;
@@ -221,38 +222,165 @@ PetscErrorCode PCDeflationSetSpace(PC pc,Mat W,PetscBool transpose)
 */
 static PetscErrorCode PCSetUp_Deflation(PC pc)
 {
-  PC_Deflation      *jac = (PC_Deflation*)pc->data;
-  Vec            diag,diagsqrt;
-  PetscErrorCode ierr;
-  PetscInt       n,i;
-  PetscScalar    *x;
-  PetscBool      zeroflag = PETSC_FALSE;
+  PC_Deflation     *def = (PC_Deflation*)pc->data;
+  KSP              innerksp;
+  PC               pcinner;
+  Mat              Amat,nextDef=NULL,*mats;
+  PetscInt         i,m,red,size,commsize;
+  PetscBool        match,flgspd,transp=PETSC_FALSE;
+  MatCompositeType ctype;
+  MPI_Comm         comm;
+  const char       *prefix;
+  PetscErrorCode   ierr;
 
   PetscFunctionBegin;
-  /*
-       For most preconditioners the code would begin here something like
-
-  if (pc->setupcalled == 0) { allocate space the first time this is ever called
-    ierr = MatCreateVecs(pc->mat,&jac->diag);CHKERRQ(ierr);
-    PetscLogObjectParent((PetscObject)pc,(PetscObject)jac->diag);
+  if (pc->setupcalled) PetscFunctionReturn(0);
+  ierr = PetscObjectGetComm((PetscObject)pc,&comm);CHKERRQ(ierr);
+  if (def->W || def->Wt) {
+    def->spacetype = PC_DEFLATION_SPACE_USER;
+  } else {
+    //ierr = KSPDCGComputeDeflationSpace(ksp);CHKERRQ(ierr);
   }
 
-    But for this preconditioner we want to support use of both the matrix' diagonal
-    elements (for left or right preconditioning) and square root of diagonal elements
-    (for symmetric preconditioning).  Hence we do not allocate space here, since we
-    don't know at this point which will be needed (diag and/or diagsqrt) until the user
-    applies the preconditioner, and we don't want to allocate BOTH unless we need
-    them both.  Thus, the diag and diagsqrt are allocated in PCSetUp_Deflation_NonSymmetric()
-    and PCSetUp_Deflation_Symmetric(), respectively.
-  */
-
-  /*
-    Here we set up the preconditioner; that is, we copy the diagonal values from
-    the matrix and put them into a format to make them quick to apply as a preconditioner.
-  */
-  if (zeroflag) {
-    ierr = PetscInfo(pc,"Zero detected in diagonal of matrix, using 1 at those locations\n");CHKERRQ(ierr);
+  /* nested deflation */
+  if (def->W) {
+    ierr = PetscObjectTypeCompare((PetscObject)def->W,MATCOMPOSITE,&match);CHKERRQ(ierr);
+    if (match) {
+      ierr = MatCompositeGetType(def->W,&ctype);CHKERRQ(ierr);
+      ierr = MatCompositeGetNumberMat(def->W,&size);CHKERRQ(ierr);
+    }
+  } else {
+    ierr = MatCreateTranspose(def->Wt,&def->W);CHKERRQ(ierr);
+    ierr = PetscObjectTypeCompare((PetscObject)def->Wt,MATCOMPOSITE,&match);CHKERRQ(ierr);
+    if (match) {
+      ierr = MatCompositeGetType(def->Wt,&ctype);CHKERRQ(ierr);
+      ierr = MatCompositeGetNumberMat(def->Wt,&size);CHKERRQ(ierr);
+    }
+    transp = PETSC_TRUE;
   }
+  if (match && ctype == MAT_COMPOSITE_MULTIPLICATIVE) {
+    ierr = PetscMalloc1(size,&mats);CHKERRQ(ierr);
+    if (!transp) {
+      for (i=0; i<size; i++) {
+        ierr = MatCompositeGetMat(def->W,i,&mats[i]);CHKERRQ(ierr);
+        //ierr = PetscObjectReference((PetscObject)mats[i]);CHKERRQ(ierr);
+      }
+      if (def->nestedlvl < def->maxnestedlvl) {
+        size -= 1;
+        ierr = MatDestroy(&def->W);CHKERRQ(ierr);
+        def->W = mats[size];
+        ierr = PetscObjectReference((PetscObject)mats[size]);CHKERRQ(ierr);
+        if (size > 1) {
+          ierr = MatCreateComposite(comm,size,mats,&nextDef);CHKERRQ(ierr);
+          ierr = MatCompositeSetType(nextDef,MAT_COMPOSITE_MULTIPLICATIVE);CHKERRQ(ierr);
+        } else {
+          nextDef = mats[0];
+          ierr = PetscObjectReference((PetscObject)mats[0]);CHKERRQ(ierr);
+        }
+      } else {
+        /* ierr = MatCompositeSetMergeType(def->W,MAT_COMPOSITE_MERGE_LEFT);CHKERRQ(ierr); */
+        ierr = MatCompositeMerge(def->W);CHKERRQ(ierr);
+      }
+    }
+    ierr = PetscFree(mats);CHKERRQ(ierr);
+  }
+
+  /* setup coarse problem */
+  if (!def->WtAWinv) {
+    ierr = MatGetSize(def->W,NULL,&m);CHKERRQ(ierr); /* TODO works for W MatTranspose? */
+    if (!def->WtAW) {
+      ierr = PCGetOperators(pc,&Amat,NULL);CHKERRQ(ierr); /* using Amat! */
+      /* TODO add implicit product version ? */
+      if (!def->AW) {
+        ierr = MatPtAP(Amat,def->W,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&def->WtAW);CHKERRQ(ierr);
+      } else {
+        ierr = MatTransposeMatMult(def->W,def->AW,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&def->WtAW);CHKERRQ(ierr);
+      }
+      /* TODO create MatInheritOption(Mat,MatOption) */
+      ierr = MatGetOption(Amat,MAT_SPD,&flgspd);CHKERRQ(ierr);
+      ierr = MatSetOption(def->WtAW,MAT_SPD,flgspd);CHKERRQ(ierr);
+#if defined(PETSC_USE_DEBUG)
+      /* Check WtAW is not sigular */
+      PetscReal *norms;
+      ierr = PetscMalloc1(m,&norms);CHKERRQ(ierr);
+      ierr = MatGetColumnNorms(def->WtAW,NORM_INFINITY,norms);CHKERRQ(ierr);
+      for (i=0; i<m; i++) {
+        if (norms[i] < 100*PETSC_MACHINE_EPSILON) {
+          SETERRQ1(comm,PETSC_ERR_SUP,"Column %D of W is in kernel of A.",i);
+        }
+      }
+      ierr = PetscFree(norms);CHKERRQ(ierr);
+#endif
+    } else {
+      ierr = MatGetOption(def->WtAW,MAT_SPD,&flgspd);CHKERRQ(ierr);
+    }
+    /* TODO use MATINV */
+    ierr = KSPCreate(comm,&def->WtAWinv);CHKERRQ(ierr);
+    ierr = KSPSetOperators(def->WtAWinv,def->WtAW,def->WtAW);CHKERRQ(ierr);
+    ierr = KSPSetType(def->WtAWinv,KSPPREONLY);CHKERRQ(ierr);
+    ierr = KSPGetPC(def->WtAWinv,&pcinner);CHKERRQ(ierr);
+    ierr = PCSetType(pcinner,PCTELESCOPE);CHKERRQ(ierr);
+    /* Reduction factor choice */
+    red = def->reductionfact;
+    if (red < 0) {
+      ierr = MPI_Comm_size(comm,&commsize);CHKERRQ(ierr);
+      red  = ceil((float)commsize/ceil((float)m/commsize));
+      ierr = PetscObjectTypeCompareAny((PetscObject)(def->WtAW),&match,MATSEQDENSE,MATMPIDENSE,MATDENSE,"");CHKERRQ(ierr);
+      if (match) red = commsize;
+      ierr = PetscInfo1(pc,"Auto choosing reduction factor %D\n",red);CHKERRQ(ierr); /* TODO add level? */
+    }
+    ierr = PCTelescopeSetReductionFactor(pcinner,red);CHKERRQ(ierr);
+    ierr = PCTelescopeGetKSP(pcinner,&innerksp);CHKERRQ(ierr);
+    ierr = KSPGetPC(innerksp,&pcinner);CHKERRQ(ierr);
+    /* Setup KSP and PC */
+    if (nextDef) { /* next level for multilevel deflation */
+      /* set default KSPtype */
+      if (!def->ksptype) {
+        def->ksptype = KSPFGMRES;
+        if (flgspd) { /* SPD system */
+          def->ksptype = KSPFCG;
+        }
+      }
+      ierr = KSPSetType(innerksp,def->ksptype);CHKERRQ(ierr); /* TODO iherit from KSP */
+      ierr = PCSetType(pcinner,PCDEFLATION);CHKERRQ(ierr); /* TODO create coarse preconditinoner M_c = WtMW ? */
+      ierr = PCDeflationSetSpace(pcinner,nextDef,transp);CHKERRQ(ierr);
+      ierr = PCDeflationSetNestLvl_Deflation(pcinner,def->nestedlvl+1,def->maxnestedlvl);CHKERRQ(ierr);
+      /* inherit options TODO if not set */
+      ((PC_Deflation*)(pcinner))->ksptype = def->ksptype;
+      ((PC_Deflation*)(pcinner))->correct = def->correct;
+      ((PC_Deflation*)(pcinner))->adaptiveconv = def->adaptiveconv;
+      ((PC_Deflation*)(pcinner))->adaptiveconst = def->adaptiveconst;
+      ierr = MatDestroy(&nextDef);CHKERRQ(ierr);
+    } else { /* the last level */
+      ierr = KSPSetType(innerksp,KSPPREONLY);CHKERRQ(ierr);
+      /* TODO Cholesky if flgspd? */
+      ierr = PCSetType(pc,PCLU);CHKERRQ(ierr);
+      //TODO remove explicit matSolverPackage
+      if (commsize == red) {
+        ierr = PCFactorSetMatSolverType(pc,MATSOLVERSUPERLU);CHKERRQ(ierr);
+      } else {
+        ierr = PCFactorSetMatSolverType(pc,MATSOLVERSUPERLU_DIST);CHKERRQ(ierr);
+      }
+    }
+    /* TODO use def_[lvl]_ if lvl > 0? */
+    ierr = PCGetOptionsPrefix(pc,&prefix);CHKERRQ(ierr);
+    if (prefix) {
+      ierr = KSPSetOptionsPrefix(innerksp,prefix);CHKERRQ(ierr);
+      ierr = KSPAppendOptionsPrefix(innerksp,"def_");CHKERRQ(ierr);
+    } else {
+      ierr = KSPSetOptionsPrefix(innerksp,"def_");CHKERRQ(ierr);
+    }
+    /* TODO: check if WtAWinv is KSP and move following from this if */
+    ierr = KSPSetFromOptions(def->WtAWinv);CHKERRQ(ierr);
+    //if (def->adaptiveconv) {
+    //  PetscReal *rnorm;
+    //  PetscNew(&rnorm);
+    //  ierr = KSPSetConvergenceTest(def->WtAWinv,KSPDCGConvergedAdaptive_DCG,rnorm,NULL);CHKERRQ(ierr);
+    //}
+    ierr = KSPSetUp(def->WtAWinv);CHKERRQ(ierr);
+  }
+
+  ierr = KSPCreateVecs(def->WtAWinv,2,&def->work,0,NULL);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 /* -------------------------------------------------------------------------- */
@@ -336,8 +464,6 @@ static PetscErrorCode PCSetFromOptions_Deflation(PetscOptionItems *PetscOptionsO
 -    -pc_jacobi_abs - use the absolute value of the diagonal entry
 
    Level: beginner
-
-  Concepts: Deflation, preconditioners
 
   Notes:
     todo
