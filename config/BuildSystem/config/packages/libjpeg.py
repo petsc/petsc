a522e665b362190f21a66e2df4ab7444b28593a5
Satish Balay <balay@mcs.anl.gov>
2019-06-08 22:30:19 -0500

    libjpeg: update to version v9c [and add backup URL]

--- config/BuildSystem/config/packages/libjpeg.py
+++ config/BuildSystem/config/packages/libjpeg.py
@@ -3,7 +3,9 @@ import config.package
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.download         = ['http://www.ijg.org/files/jpegsrc.v9b.tar.gz']
+    self.download         = ['http://www.ijg.org/files/jpegsrc.v9c.tar.gz',
+                             'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/jpegsrc.v9c.tar.gz']
+    self.downloaddirnames  = ['jpeg']
     self.includes         = ['jpeglib.h']
     self.liblist          = [['libjpeg.a']]
     self.functions        = ['jpeg_destroy_compress']
