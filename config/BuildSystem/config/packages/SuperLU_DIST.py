a7b55187c168a3c94455badbf0c9f954c0a5c4b9
Barry Smith <bsmith@mcs.anl.gov>
2019-06-11 11:30:28 -0500

    SuperLU_DIST: update to version 6.3.0
    SuperLU_DIST.py GPU code currently requires OpenMP
    
    Remove -download-superlu_dist-gpu option and use --with-cuda=1
    
    Keep backward compatibility with older superlu_dist versions.

--- config/BuildSystem/config/packages/SuperLU_DIST.py
+++ config/BuildSystem/config/packages/SuperLU_DIST.py
@@ -4,9 +4,10 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.version          = '6.2.0'
+    self.minversion       = '6.1.1'
+    self.version          = '6.3.0'
     self.versionname      = 'SUPERLU_DIST_MAJOR_VERSION.SUPERLU_DIST_MINOR_VERSION.SUPERLU_DIST_PATCH_VERSION'
-    self.gitcommit         = 'v'+self.version
+    self.gitcommit        = 'v'+self.version
     self.download         = ['git://https://github.com/xiaoyeli/superlu_dist','https://github.com/xiaoyeli/superlu_dist/archive/'+self.gitcommit+'.tar.gz']
     self.downloaddirnames = ['SuperLU_DIST','superlu_dist']
     self.functions        = ['set_default_options_dist']
@@ -23,31 +24,30 @@ class Configure(config.package.CMakePackage):
     self.requirescxx11    = 1
     return
 
-  def setupHelp(self, help):
-    import nargs
-    config.package.CMakePackage.setupHelp(self, help)
-    help.addArgument('SUPERLU_DIST', '-download-superlu_dist-gpu=<bool>',    nargs.ArgBool(None, 0, 'Install Superlu_DIST to use GPUs'))
-
   def setupDependencies(self, framework):
     config.package.CMakePackage.setupDependencies(self, framework)
     self.blasLapack     = framework.require('config.packages.BlasLapack',self)
     self.parmetis       = framework.require('config.packages.parmetis',self)
     self.mpi            = framework.require('config.packages.MPI',self)
-    self.odeps          = [self.parmetis]
-    if self.framework.argDB['download-superlu_dist-gpu']:
-      self.cuda           = framework.require('config.packages.cuda',self)
-      self.openmp         = framework.require('config.packages.openmp',self)
-      self.deps           = [self.mpi,self.blasLapack,self.cuda,self.openmp]
-    else:
-      self.deps           = [self.mpi,self.blasLapack]
+    self.cuda           = framework.require('config.packages.cuda',self)
+    self.openmp         = framework.require('config.packages.openmp',self)
+    self.odeps          = [self.parmetis,self.cuda,self.openmp]
+    self.deps           = [self.mpi,self.blasLapack]
     return
 
   def formCMakeConfigureArgs(self):
     args = config.package.CMakePackage.formCMakeConfigureArgs(self)
-    if not self.framework.argDB['download-superlu_dist-gpu']:
-      args.append('-DCMAKE_DISABLE_FIND_PACKAGE_OpenMP=TRUE')
-    else:
+    if self.openmp.found:
       self.usesopenmp = 'yes'
+    else:
+      args.append('-DCMAKE_DISABLE_FIND_PACKAGE_OpenMP=TRUE')
+    if self.cuda.found:
+      if not self.openmp.found:
+        raise RuntimeError('SuperLU_DIST GPU code currently requires OpenMP. Use --with-openmp=1')
+      # SuperLU_DIST CMake doesn't know about GPU builds
+      for place,item in enumerate(args):
+        if item.find('CMAKE_C_FLAGS') >= 0:
+          args[place]=item[:-1]+' -DGPU_ACC '+self.headers.toString(self.cuda.include)+'"'
     args.append('-DUSE_XSDK_DEFAULTS=YES')
     args.append('-DTPL_BLAS_LIBRARIES="'+self.libraries.toString(self.blasLapack.dlib)+'"')
     args.append('-DTPL_LAPACK_LIBRARIES="'+self.libraries.toString(self.blasLapack.dlib)+'"')
@@ -87,16 +87,6 @@ class Configure(config.package.CMakePackage):
         args[place]=item[:-1]+' '+mangledef+'"'
 
     return args
-
-
- #   if self.framework.argDB['download-superlu_dist-gpu']:
- #     g.write('ACC          = GPU\n')
- #     g.write('CUDAFLAGS    = -DGPU_ACC '+self.headers.toString(self.cuda.include)+'\n')
- #     g.write('CUDALIB      = '+self.libraries.toString(self.cuda.lib)+'\n')
- #   else:
- #     g.write('ACC          = \n')
- #     g.write('CUDAFLAGS    = \n')
- #     g.write('CUDALIB      = \n')
 
 
 
