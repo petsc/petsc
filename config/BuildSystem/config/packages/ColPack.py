6e0d639c278d460e209c024789392c326ac90e92
Barry Smith <bsmith@mcs.anl.gov>
2019-07-04 11:05:34 -0500

    Several GNU package installers were initializing with the basic package installer
    Commit-type: bug-fix
    Thanks-to: Joe Wallwork <j.wallwork16@imperial.ac.uk>

--- config/BuildSystem/config/packages/ColPack.py
+++ config/BuildSystem/config/packages/ColPack.py
@@ -2,7 +2,7 @@ import config.package
 
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
-    config.package.Package.__init__(self, framework)
+    config.package.GNUPackage.__init__(self, framework)
     self.gitcommit     = 'master'
     self.download      = ['git://https://github.com/CSCsw/ColPack.git']
     self.includes      = ['ColPack/ColPackHeaders.h']
