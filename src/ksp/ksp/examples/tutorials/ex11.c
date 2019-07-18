6da919c7708d5aa575ff81f858c46cf242dd1a56
Jakub Kruzik <jakub.kruzik@gmail.com>
2019-07-18 14:13:45 +0200

    require superlu_dist for parallel LU

--- src/ksp/ksp/examples/tutorials/ex11.c
+++ src/ksp/ksp/examples/tutorials/ex11.c
@@ -233,11 +233,13 @@ int main(int argc,char **args)
       args: -n 6 -norandom -pc_type deflation -ksp_monitor_short
       test:
       test:
+        requires: superlu_dist
         nsize: 3
         args: -pc_deflation_compute_space {{db2 aggregation}}
 
       test:
         suffix: pc_deflation_init_only-0
+        requires: superlu_dist
         nsize: 4
         args: -ksp_type fgmres -pc_deflation_compute_space db4 -pc_deflation_compute_space_size 2 -pc_deflation_levels 2 -deflation_ksp_max_it 10
         #TODO remove suffix and next test when this works
@@ -246,6 +248,7 @@ int main(int argc,char **args)
 
       test:
         suffix: pc_deflation_init_only-1
+        requires: superlu_dist
         nsize: 4
         args: -ksp_type fgmres -pc_deflation_compute_space db4 -pc_deflation_compute_space_size 2 -pc_deflation_levels 2 -deflation_ksp_max_it 10
         args: -pc_deflation_init_only 1
