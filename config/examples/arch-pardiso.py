f07a850f2777ecbdb1adea77a6f649f7c7cce56b
Junchao Zhang <jczhang@mcs.anl.gov>
2019-07-20 16:14:12 -0500

    Upgrade mpich to use the default

--- config/examples/arch-pardiso.py
+++ config/examples/arch-pardiso.py
@@ -14,8 +14,7 @@ if __name__ == '__main__':
     '--with-blaslapack-dir='+os.environ['MKL_HOME'],
     '--with-mkl_pardiso-dir='+os.environ['MKL_HOME'],
     '--with-mkl_sparse_optimize=0',
-    # using mpich-3.2 as default mpich-3.1.3 does not build with ifort-16
-    '--download-mpich=http://ftp.mcs.anl.gov/pub/petsc/externalpackages/mpich-3.2.tar.gz',
+    '--download-mpich=1',
     '--download-triangle=1',
     '--download-ctetgen=1',
     '--download-p4est=1',
