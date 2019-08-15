petsc/petsc

commit 4fa2624631cd4dd3fb87acf6c5b4ca00f56a7287

Plex: Fix L2G map for MatIS

src/dm/impls/plex/plex.c
@@ -976,8 +976,12 @@ PetscErrorCode DMCreateMatrix_Plex(DM dm, Mat *J)
     ierr = MPIU_Allreduce(&bsLocal, &bsMin, 1, MPIU_INT, MPI_MIN, PetscObjectComm((PetscObject)dm));CHKERRQ(ierr);
     if (bsMin != bsMax) {bs = 1;}
     else                {bs = bsMax;}
+    bs   = bs < 0 ? 1 : bs;
     if (isMatIS) {
-      ierr = ISLocalToGlobalMappingCreate(PetscObjectComm((PetscObject)dm), bs < 0 ? 1 : bs, lsize, ltogidx, PETSC_OWN_POINTER, &ltog);CHKERRQ(ierr);
+      PetscInt l;
+      /* Must reduce indices by blocksize */
+      if (bs > 1) for (l = 0; l < lsize; ++l) ltogidx[l] /= bs;
+      ierr = ISLocalToGlobalMappingCreate(PetscObjectComm((PetscObject)dm), bs, lsize, ltogidx, PETSC_OWN_POINTER, &ltog);CHKERRQ(ierr);
     }
     ierr = MatSetLocalToGlobalMapping(*J,ltog,ltog);CHKERRQ(ierr);
     if (isMatIS) {

commit 6fd5c86abdfcf2f64d32b3f938174814d99ed55a

DMPLEX: fix bug for bs > 1 introduced in 4fa2624631c

src/dm/impls/plex/plex.c
@@ -1440,11 +1440,12 @@ PetscErrorCode DMCreateMatrix_Plex(DM dm, Mat *J)
     ierr = PetscGlobalMinMaxInt(PetscObjectComm((PetscObject) dm), bsLocal, bsMinMax);CHKERRQ(ierr);
     if (bsMinMax[0] != bsMinMax[1]) {bs = 1;}
     else                            {bs = bsMinMax[0];}
-    bs = bs < 0 ? 1 : bs;
-    if (isMatIS) {
+    bs = PetscMax(1,bs);
+    if (isMatIS) { /* Must reduce indices by blocksize */
       PetscInt l;
-      /* Must reduce indices by blocksize */
-      if (bs > 1) for (l = 0; l < lsize; ++l) ltogidx[l] /= bs;
+
+      lsize = lsize/bs;
+      if (bs > 1) for (l = 0; l < lsize; ++l) ltogidx[l] = ltogidx[l*bs]/bs;
       ierr = ISLocalToGlobalMappingCreate(PetscObjectComm((PetscObject)dm), bs, lsize, ltogidx, PETSC_OWN_POINTER, &ltog);CHKERRQ(ierr);
     }
     ierr = MatSetLocalToGlobalMapping(*J,ltog,ltog);CHKERRQ(ierr);