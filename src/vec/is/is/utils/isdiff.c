aa3c2e73612e0b14120db38997f15c7351c67acc
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2019-08-18 22:45:17 +0200

    Misleading text and wrong indentation.

--- src/vec/is/is/utils/isdiff.c
+++ src/vec/is/is/utils/isdiff.c
@@ -739,20 +739,20 @@ PetscErrorCode ISEmbed(IS a, IS b, PetscBool drop, IS *c)
 
   Input arguments:
 + f      -  IS to sort
-- always -  build the permutation even when f's indices are nondecreasin.
+- always -  build the permutation even when f's indices are nondecreasing.
 
   Output argument:
-. h    -  permutation or NULL, if f is nondecreasing and always == PETSC_TRUE.
+. h    -  permutation or NULL, if f is nondecreasing and always == PETSC_FALSE.
 
 
   Note: Indices in f are unchanged. f[h[i]] is the i-th smallest f index.
         If always == PETSC_FALSE, an extra check is peformed to see whether
-	the f indices are nondecreasing. h is built on PETSC_COMM_SELF, since
-	the permutation has a local meaning only.
+        the f indices are nondecreasing. h is built on PETSC_COMM_SELF, since
+        the permutation has a local meaning only.
 
   Level: advanced
 
-.seealso ISLocalToGlobalMapping, ISSort(), PetscIntSortWithPermutation()
+.seealso ISLocalToGlobalMapping, ISSort()
  @*/
 PetscErrorCode ISSortPermutation(IS f,PetscBool always,IS *h)
 {
@@ -771,8 +771,8 @@ PetscErrorCode ISSortPermutation(IS f,PetscBool always,IS *h)
     isincreasing = PETSC_TRUE;
     for (i = 1; i < fsize; ++i) {
       if (findices[i] <= findices[i-1]) {
-	isincreasing = PETSC_FALSE;
-	break;
+        isincreasing = PETSC_FALSE;
+        break;
       }
     }
     if (isincreasing) {
