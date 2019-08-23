3e0a28c5c4d087b8062e48debfba00969ccf6e8b
Satish Balay <balay@mcs.anl.gov>
2019-08-23 10:00:59 -0500

    convert mswin examples to glci format

--- config/examples/arch-ci-mswin-opt-impi.py
+++ config/examples/arch-ci-mswin-opt-impi.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
+
+import os
+petsc_hash_pkgs=os.path.join(os.getenv('HOME'),'petsc-hash-pkgs')
+if not os.path.isdir(petsc_hash_pkgs): os.mkdir(petsc_hash_pkgs)
+
 if __name__ == '__main__':
   import sys
   import os
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
+    '--package-prefix-hash='+petsc_hash_pkgs,
     '--with-debugging=0',
     '--with-blaslapack-lib=-L/cygdrive/c/PROGRA~2/INTELS~1/COMPIL~2/windows/mkl/lib/intel64 mkl_intel_lp64_dll.lib mkl_sequential_dll.lib mkl_core_dll.lib',
     '--with-cc=win32fe cl',
