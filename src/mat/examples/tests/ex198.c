336546cc1bb119545d85aed916f078aebe6cdd22
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2019-09-05 14:19:35 +0000

    Update ex198.c: fixed typo datafilespah -> datafilespath

--- src/mat/examples/tests/ex198.c
+++ src/mat/examples/tests/ex198.c
@@ -71,7 +71,7 @@ int main(int argc,char **args)
    test:
       suffix: 2
       nsize: 3
-      requires: datafilespah !complex double !define(PETSC_USE_64BIT_INDICES)
+      requires: datafilespath !complex double !define(PETSC_USE_64BIT_INDICES)
       args: -fA ${DATAFILESPATH}/matrices/matmatmatmult/A.bin -fB ${DATAFILESPATH}/matrices/matmatmatmult/B.bin -fC ${DATAFILESPATH}/matrices/matmatmatmult/C.bin
       output_file: output/ex198.out
 
