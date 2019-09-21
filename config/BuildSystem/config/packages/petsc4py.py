5ec2ac8aa48eff5197b6573414562e4cec75ef91
Satish Balay <balay@mcs.anl.gov>
2019-09-21 17:03:31 -0500

    petsc4py: update to snapshot with corresponding changes

--- config/BuildSystem/config/packages/petsc4py.py
+++ config/BuildSystem/config/packages/petsc4py.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = 'b6c0547' #master sep-21-2019
+    self.gitcommit              = 'c654db8' #master sep-21-2019
     self.download               = ['git://https://bitbucket.org/petsc/petsc4py','https://bitbucket.org/petsc/petsc4py/get/'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
