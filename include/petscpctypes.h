f73ac4390e763bc4e3d4297e73c18f045c62763c
Mark Adams <mfadams@lbl.gov>
2019-09-23 19:08:48 -0400

    added a seealso for enum

--- include/petscpctypes.h
+++ include/petscpctypes.h
@@ -373,6 +373,7 @@ typedef enum {PC_NOERROR,PC_FACTOR_STRUCT_ZEROPIVOT,PC_FACTOR_NUMERIC_ZEROPIVOT,
 
     Level: intermediate
 
+.seealso: PCGAMGSetCoarseGridLayoutType()
     Any additions/changes here MUST also be made in include/petsc/finclude/petscpc.h
 E*/
 typedef enum {PCGAMG_LAYOUT_COMPACT=0,PCGAMG_LAYOUT_SPREAD} PCGAMGLayoutType;
