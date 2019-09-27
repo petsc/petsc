d5d3fe651580ab184c2945005109235830a1b6a4
Satish Balay <balay@mcs.anl.gov>
2019-09-27 00:37:41 -0500

    ci: use package-prefix-hash with cuda tests

--- config/examples/arch-ci-linux-c-exodus-dbg.py
+++ config/examples/arch-ci-linux-c-exodus-dbg.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
-#
+
+import os
+petsc_hash_pkgs=os.path.join(os.getenv('HOME'),'petsc-hash-pkgs')
+if not os.path.isdir(petsc_hash_pkgs): os.mkdir(petsc_hash_pkgs)
+
 if __name__ == '__main__':
   import sys
   import os
   sys.path.insert(0, os.path.abspath('config'))
   import configure
   configure_options = [
+    '--package-prefix-hash='+petsc_hash_pkgs,
     'COPTFLAGS=-g -O',
     'FOPTFLAGS=-g -O',
     'CXXOPTFLAGS=-g -O',
