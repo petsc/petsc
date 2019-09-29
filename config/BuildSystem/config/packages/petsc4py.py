7853793db5f7bae8c208cd7e1234ad2e56e3655d
Satish Balay <balay@mcs.anl.gov>
2019-09-29 11:33:28 -0500

    slepc, hpddm, petsc4py: update to use latest master for self.gitcommit [slepc, hpddm are tested with  self.gitcommitmaster anyway]

--- config/BuildSystem/config/packages/petsc4py.py
+++ config/BuildSystem/config/packages/petsc4py.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = 'c654db8' #master sep-21-2019
+    self.gitcommit              = '921803d' #master sep-29-2019
     self.download               = ['git://https://bitbucket.org/petsc/petsc4py','https://bitbucket.org/petsc/petsc4py/get/'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
