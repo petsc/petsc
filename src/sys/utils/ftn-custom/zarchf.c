7e4e2a2d5b77d05327773006136c5762506227f3
Barry Smith <bsmith@mcs.anl.gov>
2019-10-29 09:44:27 -0500

    Fix for calling PetscBarrier() from Fortran
    Commit-type: bug-fix
    Reported-by: Victor Eijkhout <eijkhout@tacc.utexas.edu>

--- src/sys/utils/ftn-custom/zarchf.c
+++ src/sys/utils/ftn-custom/zarchf.c
@@ -1,9 +1,11 @@
 #include <petsc/private/fortranimpl.h>
-
+#include "petscsys.h"
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
 #define petscgetarchtype_                  PETSCGETARCHTYPE
+#define petscbarrier_                      PETSCBARRIER
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
 #define petscgetarchtype_                  petscgetarchtype
+#define petscbarrier_                      petscbarrier
 #endif
 
 PETSC_EXTERN void PETSC_STDCALL petscgetarchtype_(char* str PETSC_MIXED_LEN(len),PetscErrorCode *ierr PETSC_END_LEN(len))
@@ -14,6 +16,9 @@ PETSC_EXTERN void PETSC_STDCALL petscgetarchtype_(char* str PETSC_MIXED_LEN(len)
   tlen  = len; /* int to size_t */
   *ierr = PetscGetArchType(tstr,tlen);
   FIXRETURNCHAR(PETSC_TRUE,str,len);
+}
 
+PETSC_EXTERN void PETSC_STDCALL  petscbarrier_(PetscObject *obj, int *ierr){
+  *ierr = PetscBarrier(*obj);
 }
 
