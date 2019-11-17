f2ebaf19a3dac4ef609ecff5eb782b60c1497a9b
Stefano Zampini <stefano.zampini@gmail.com>
2019-11-17 12:55:14 +0300

    suitesparse: Update to github releases
    add support for METIS based reorderings

--- config/BuildSystem/config/packages/SuiteSparse.py
+++ config/BuildSystem/config/packages/SuiteSparse.py
@@ -3,36 +3,38 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self,framework)
-    self.version           = '5.7.1'
-    self.versionname       = 'UMFPACK_MAIN_VERSION.UMFPACK_SUB_VERSION.UMFPACK_SUBSUB_VERSION'
-    self.versioninclude    = 'umfpack.h'
-    #  Note that there is not SuitSparse version number in the code, the only version information is for UMFPACK
-    self.download          = ['http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.3.tar.gz',
-                              'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/SuiteSparse-4.4.3.tar.gz']
+    self.version           = '5.6.0'
+    self.versioninclude    = 'SuiteSparse_config.h'
+    self.versionname       = 'SUITESPARSE_MAIN_VERSION.SUITESPARSE_SUB_VERSION.SUITESPARSE_SUBSUB_VERSION'
+    self.gitcommit         = 'v'+self.version
+    self.download          = ['git://https://github.com/DrTimothyAldenDavis/SuiteSparse','https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/'+self.gitcommit+'.tar.gz']
     self.liblist           = [['libumfpack.a','libklu.a','libcholmod.a','libbtf.a','libccolamd.a','libcolamd.a','libcamd.a','libamd.a','libsuitesparseconfig.a'],
                              ['libumfpack.a','libklu.a','libcholmod.a','libbtf.a','libccolamd.a','libcolamd.a','libcamd.a','libamd.a','libsuitesparseconfig.a','librt.a'],
                              ['libumfpack.a','libklu.a','libcholmod.a','libbtf.a','libccolamd.a','libcolamd.a','libcamd.a','libamd.a','libmetis.a','libsuitesparseconfig.a'],
                              ['libumfpack.a','libklu.a','libcholmod.a','libbtf.a','libccolamd.a','libcolamd.a','libcamd.a','libamd.a','libmetis.a','libsuitesparseconfig.a','librt.a']]
-    self.functions        = ['umfpack_dl_wsolve','cholmod_l_solve','klu_l_solve']
-    self.includes         = ['umfpack.h','cholmod.h','klu.h']
-    self.hastests         = 1
-    self.hastestsdatafiles= 1
-    self.precisions       = ['double']
+    self.functions         = ['umfpack_dl_wsolve','cholmod_l_solve','klu_l_solve']
+    self.includes          = ['umfpack.h','cholmod.h','klu.h']
+    self.hastests          = 1
+    self.hastestsdatafiles = 1
+    self.precisions        = ['double']
     return
 
   def setupHelp(self, help):
     import nargs
     config.package.Package.setupHelp(self, help)
-    help.addArgument('SUITESPARSE', '-download-suitesparse-gpu=<bool>',    nargs.ArgBool(None, 0, 'Install SuiteSparse to use GPUs'))
-    
+    # This is set to 1 since CHOLMOD is broken with GPU support (does not even compile with icc on my workstation)
+    # see https://github.com/DrTimothyAldenDavis/SuiteSparse/issues/5
+    help.addArgument('SUITESPARSE', '-download-suitesparse-disablegpu=<bool>',    nargs.ArgBool(None, 1, 'Force disabling SuiteSparse/CHOLMOD use of GPUs'))
+
   def setupDependencies(self, framework):
     config.package.Package.setupDependencies(self, framework)
     self.blasLapack = framework.require('config.packages.BlasLapack',self)
     self.mathlib    = framework.require('config.packages.mathlib',self)
     self.deps       = [self.blasLapack,self.mathlib]
-    if self.argDB['download-suitesparse-gpu']:
-      self.cuda       = framework.require('config.packages.cuda',self)
-      self.deps.append(self.cuda)
+    self.cuda       = framework.require('config.packages.cuda',self)
+    self.openmp     = framework.require('config.packages.openmp',self)
+    self.metis      = framework.require('config.packages.metis',self)
+    self.odeps      = [self.openmp,self.cuda,self.metis]
     return
 
   def Install(self):
@@ -41,36 +43,62 @@ class Configure(config.package.Package):
     if not self.make.haveGNUMake:
       raise RuntimeError('SuiteSparse buildtools require GNUMake. Use --with-make=gmake or --download-make')
 
-    mkfile = 'SuiteSparse_config/SuiteSparse_config.mk'
-    g = open(os.path.join(self.packageDir, mkfile), 'w')
+    # Use CHOLMOD_OMP_NUM_THREADS to control the number of threads
+    if self.openmp.found:
+      self.usesopenmp = 'yes'
+
+    # From v4.5.0, SuiteSparse_config/SuiteSparse_config.mk is not modifiable anymore. Instead, we must override make variables
+    args=[]
+
     self.setCompilers.pushLanguage('C')
-    g.write('CC           = '+self.setCompilers.getCompiler()+'\n')
-    g.write('CF           = '+self.removeWarningFlags(self.setCompilers.getCompilerFlags())+'\n')
+    args.append('CC="'+self.setCompilers.getCompiler()+'"')
+    cflags=self.removeWarningFlags(self.setCompilers.getCompilerFlags())
+    ldflags=self.setCompilers.getDynamicLinkerFlags()
+    ldflags+=self.setCompilers.LDFLAGS
+    # SuiteSparse 5.6.0 makefile has a bug in how it treats LDFLAGS (not using the override directive)
+    ldflags+=" -L\$(INSTALL_LIB)"
     self.setCompilers.popLanguage()
-    g.write('MAKE         ='+self.make.make+'\n')
-    g.write('RANLIB       = '+self.setCompilers.RANLIB+'\n')
-    g.write('ARCHIVE      = '+self.setCompilers.AR+' '+self.setCompilers.AR_FLAGS+'\n')
-    g.write('RM           = '+self.programs.RM+'\n')
-    g.write('MV           = '+self.programs.mv+'\n')
-    g.write('CP           = '+self.programs.cp+'\n')
-    g.write('CLEAN             = *.o *.obj *.ln *.bb *.bbg *.da *.tcov *.gcov gmon.out *.bak *.d\n')
-    g.write('INSTALL_LIB       = ' + self.libDir + '\n')
-    g.write('INSTALL_INCLUDE   = ' + self.includeDir + '\n')
+
+    # CHOLMOD may build the shared library with CXX
+    self.setCompilers.pushLanguage('Cxx')
+    args.append('CXX="'+self.setCompilers.getCompiler()+'"')
+    self.setCompilers.popLanguage()
+
+    args.append('MAKE="'+self.make.make+'"')
+    args.append('RANLIB="'+self.setCompilers.RANLIB+'"')
+    args.append('ARCHIVE="'+self.setCompilers.AR+' '+self.setCompilers.AR_FLAGS+'"')
+    args.append('RM="'+self.programs.RM+'"')
+    args.append('MV="'+self.programs.mv+'"')
+    args.append('CP="'+self.programs.cp+'"')
+    args.append('LDFLAGS="'+ldflags+'"')
+    args.append('INSTALL_LIB='+self.libDir)
+    args.append('INSTALL_INCLUDE='+self.includeDir)
+    args.append('INSTALL_DOC='+self.installDir+'/share/doc/suitesparse')
+    args.append('BLAS="'+self.libraries.toString(self.blasLapack.dlib)+'"')
+    args.append('LAPACK="'+self.libraries.toString(self.blasLapack.dlib)+'"')
     if self.blasLapack.mangling == 'underscore':
       flg = ''
     elif self.blasLapack.mangling == 'caps':
       flg = '-DBLAS_CAPS_DOES_NOT_WORK'
     else:
       flg = '-DBLAS_NO_UNDERSCORE'
-    g.write('UMFPACK_CONFIG    = '+flg+'\n')
-    if self.argDB['download-suitesparse-gpu']:
-      if self.defaultIndexSize == 32:
-        raise RuntimeError('SuiteSparse only uses GPUs with --with-64-bit-indices')
-      if not hasattr(self.compilers, 'CUDAC'):
-        raise RuntimeError('Run with --with-cuda to use allow SuiteSparse to compile using CUDA')
-      # code taken from cuda.py
+    args.append('UMFPACK_CONFIG='+flg)
+
+    if self.metis.found:
+      # '-I' is added automatically inside SuiteSparse_config.mk
+      metisinc = self.headers.toString(self.metis.include).replace('-I','',1)
+      args.append('MY_METIS_INC="'+metisinc+'"')
+      args.append('MY_METIS_LIB="'+self.libraries.toString(self.metis.dlib)+'"')
+    else:
+      flg+=' -DNPARTITION'
+
+    # CUDA support for 64bit indices installations only
+    if self.cuda.found and self.defaultIndexSize == 64 and not self.argDB['download-suitesparse-disablegpu']:
+      self.logPrintBox('SuiteSparse: Enabling support for CHOLMOD on GPUs (it can be disabled with --download-suitesparse-disablegpu=1)')
+      args.append('CF="'+cflags+' -D_GNU_SOURCE"') # The GPU code branches use feenableexcept including fenv.h only
       self.pushLanguage('CUDA')
       petscNvcc = self.getCompiler()
+      cudaFlags = self.getCompilerFlags()
       self.popLanguage()
       self.getExecutable(petscNvcc,getFullPath=1,resultName='systemNvcc')
       if hasattr(self,'systemNvcc'):
@@ -78,46 +106,50 @@ class Configure(config.package.Package):
         cudaDir = os.path.split(nvccDir)[0]
       else:
         raise RuntimeError('Unable to locate CUDA NVCC compiler')
-      g.write('CUDA_ROOT     = '+cudaDir+'\n')
-      g.write('GPU_BLAS_PATH = $(CUDA_ROOT)\n')
-      g.write('GPU_CONFIG    = -I$(CUDA_ROOT)/include -DGPU_BLAS\n')
-# GPU_CONFIG    = -I$(CUDA_ROOT)/include -DGPU_BLAS -DCHOLMOD_OMP_NUM_THREADS=10
-      g.write('CUDA_PATH     = $(CUDA_ROOT)\n')
-      g.write('CUDART_LIB    = $(CUDA_ROOT)/lib64/libcudart.so\n')
-      g.write('CUBLAS_LIB    = $(CUDA_ROOT)/lib64/libcublas.so\n')
-      g.write('CUDA_INC_PATH = $(CUDA_ROOT)/include/\n')
-      g.write('NV20          = -arch=sm_20 -Xcompiler -fPIC\n')
-      g.write('NV30          = -arch=sm_30 -Xcompiler -fPIC\n')
-      g.write('NV35          = -arch=sm_35 -Xcompiler -fPIC\n')
-      g.write('NVCC          = $(CUDA_ROOT)/bin/nvcc\n')
-      g.write('NVCCFLAGS     = -O3 -gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30 -gencode=arch=compute_35,code=sm_35 -Xcompiler -fPIC\n')
-      g.write('CHOLMOD_CONFIG    = '+flg+' -DNPARTITION $(GPU_CONFIG)\n')
+      args.append('CUDA_ROOT='+cudaDir)
+      args.append('GPU_BLAS_PATH='+cudaDir)
+      args.append('CUDA_PATH='+cudaDir)
+      args.append('CUDART_LIB='+cudaDir+'/lib64/libcudart.so')
+      args.append('CUBLAS_LIB='+cudaDir+'/lib64/libcublas.so')
+      args.append('CUDA_INC_PATH='+cudaDir+'/include')
+      args.append('NVCCFLAGS="'+cudaFlags+' -Xcompiler -fPIC"')
+      args.append('CHOLMOD_CONFIG="'+flg+' -DGPU_BLAS"')
       self.addDefine('USE_SUITESPARSE_GPU',1)
     else:
-      g.write('CHOLMOD_CONFIG    = '+flg+' -DNPARTITION\n')
-    g.close()
+      if self.cuda.found and not self.argDB['download-suitesparse-disablegpu']:
+        self.logPrintBox('SuiteSparse: Cannot enable support for GPUs. SuiteSparse only uses GPUs with --with-64-bit-indices')
+      args.append('CF="'+cflags+'"')
+      args.append('CHOLMOD_CONFIG="'+flg+'"')
+      args.append('CUDA=no')
 
-    if self.installNeeded(mkfile):
+    args = ' '.join(args)
+    conffile = os.path.join(self.packageDir,self.package+'.petscconf')
+    fd = open(conffile, 'w')
+    fd.write(args)
+    fd.close()
+
+    if self.installNeeded(conffile):
       try:
         self.logPrintBox('Compiling and installing SuiteSparse; this may take several minutes')
         self.installDirProvider.printSudoPasswordMessage()
-        # SuiteSparse install does not create missing directories, hence we need to create them first 
+        makewithargs=self.make.make+' '+args
+        # SuiteSparse install may not create missing directories, hence we need to create them first
         output,err,ret = config.package.Package.executeShellCommand(self.installSudo+'mkdir -p '+os.path.join(self.installDir,'lib'), timeout=2500, log=self.log)
         output,err,ret = config.package.Package.executeShellCommand(self.installSudo+'mkdir -p '+os.path.join(self.installDir,'include'), timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/SuiteSparse_config && '+self.make.make+' && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/AMD && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/COLAMD && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/BTF && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CAMD && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CCOLAMD && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CHOLMOD && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/UMFPACK && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
-        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/KLU && '+self.make.make+' library && '+self.installSudo+self.make.make+' install && '+self.make.make+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/SuiteSparse_config && '+makewithargs+' clean && '+makewithargs+' && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/AMD                && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/COLAMD             && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/BTF                && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CAMD               && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CCOLAMD            && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/CHOLMOD            && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/UMFPACK            && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
+        output,err,ret = config.package.Package.executeShellCommand('cd '+self.packageDir+'/KLU                && '+makewithargs+' clean && '+makewithargs+' library && '+self.installSudo+makewithargs+' install && '+makewithargs+' clean', timeout=2500, log=self.log)
 
         self.addDefine('HAVE_SUITESPARSE',1)
       except RuntimeError as e:
         raise RuntimeError('Error running make on SuiteSparse: '+str(e))
-      self.postInstall(output+err, mkfile)
+      self.postInstall(output+err, conffile)
     return self.installDir
 
   def consistencyChecks(self):
