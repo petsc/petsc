ab3c0519d1f340455e8112c7edb23ef80f6e560e
Stefano Zampini <stefano.zampini@gmail.com>
2019-12-10 15:43:17 +0300

    PETSCSF: Added window tests

--- src/vec/is/sf/examples/tests/ex1.c
+++ src/vec/is/sf/examples/tests/ex1.c
@@ -66,8 +66,10 @@ int main(int argc,char **argv)
   PetscSF        sf,sfDup,sfInv,sfEmbed,sfA,sfB,sfBA;
   const PetscInt *degree;
   PetscErrorCode ierr;
+  char           sftype[64] = PETSCSFBASIC;
 
   ierr = PetscInitialize(&argc,&argv,NULL,help);if (ierr) return ierr;
+  ierr = PetscOptionsGetString(NULL,NULL,"-user_sf_type",sftype,sizeof(sftype),NULL);CHKERRQ(ierr);
 
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
@@ -81,13 +83,13 @@ int main(int argc,char **argv)
 
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
   ierr = PetscSFReset(sf);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
@@ -101,7 +103,7 @@ int main(int argc,char **argv)
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sf);CHKERRQ(ierr);
   ierr = PetscSFReset(sf);CHKERRQ(ierr);
@@ -138,13 +140,13 @@ int main(int argc,char **argv)
 
   /* Test set type then view (no graph set) */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFView(sf,NULL);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
   /* Test set type then graph then view */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
   ierr = PetscSFView(sf,NULL);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
@@ -152,22 +154,28 @@ int main(int argc,char **argv)
   /* Test set graph then type */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
-  ierr = PetscSFSetType(sf,PETSCSFBASIC);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sf);CHKERRQ(ierr);
   ierr = PetscSFReset(sf);CHKERRQ(ierr);
   ierr = CheckGraphNotSet(sf);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
-  /* Test Bcast */
+  /* Test Bcast (we call setfromoptions) */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
   ierr = PetscSFBcastBegin(sf,MPI_INT,NULL,NULL);CHKERRQ(ierr);
   ierr = PetscSFBcastEnd  (sf,MPI_INT,NULL,NULL);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
+  /* From now on we also call SetFromOptions */
+
   /* Test Reduce */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFReduceBegin(sf,MPI_INT,NULL,NULL,MPIU_REPLACE);CHKERRQ(ierr);
   ierr = PetscSFReduceEnd  (sf,MPI_INT,NULL,NULL,MPIU_REPLACE);CHKERRQ(ierr);
   ierr = PetscSFReduceBegin(sf,MPI_INT,NULL,NULL,MPI_SUM);CHKERRQ(ierr);
@@ -176,21 +184,27 @@ int main(int argc,char **argv)
 
   /* Test FetchAndOp */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFFetchAndOpBegin(sf,MPI_INT,NULL,NULL,NULL,MPI_SUM);CHKERRQ(ierr);
   ierr = PetscSFFetchAndOpEnd  (sf,MPI_INT,NULL,NULL,NULL,MPI_SUM);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
   /* Test ComputeDegree */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_COPY_VALUES,NULL,PETSC_COPY_VALUES);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFComputeDegreeBegin(sf,&degree);CHKERRQ(ierr);
   ierr = PetscSFComputeDegreeEnd(sf,&degree);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sf);CHKERRQ(ierr);
 
   /* Test PetscSFDuplicate() */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFDuplicate(sf,PETSCSF_DUPLICATE_GRAPH,&sfDup);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sfDup);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sfDup);CHKERRQ(ierr);
@@ -198,7 +212,9 @@ int main(int argc,char **argv)
 
   /* Test PetscSFCreateInverseSF() */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFCreateInverseSF(sf,&sfInv);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sfInv);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sfInv);CHKERRQ(ierr);
@@ -206,7 +222,9 @@ int main(int argc,char **argv)
 
   /* Test PetscSFCreateEmbeddedSF() */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFCreateEmbeddedSF(sf,0,NULL,&sfEmbed);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sfEmbed);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sfEmbed);CHKERRQ(ierr);
@@ -214,7 +232,9 @@ int main(int argc,char **argv)
 
   /* Test PetscSFCreateEmbeddedLeafSF() */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sf);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sf,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sf,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
+  ierr = PetscSFSetFromOptions(sf);CHKERRQ(ierr);
   ierr = PetscSFCreateEmbeddedLeafSF(sf,0,NULL,&sfEmbed);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sfEmbed);CHKERRQ(ierr);
   ierr = PetscSFDestroy(&sfEmbed);CHKERRQ(ierr);
@@ -222,8 +242,10 @@ int main(int argc,char **argv)
 
   /* Test PetscSFCompose() */
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sfA);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sfA,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sfA,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
   ierr = PetscSFCreate(PETSC_COMM_WORLD,&sfB);CHKERRQ(ierr);
+  ierr = PetscSFSetType(sfB,sftype);CHKERRQ(ierr);
   ierr = PetscSFSetGraph(sfB,0,0,NULL,PETSC_USE_POINTER,NULL,PETSC_USE_POINTER);CHKERRQ(ierr);
   ierr = PetscSFCompose(sfA,sfB,&sfBA);CHKERRQ(ierr);
   ierr = CheckGraphEmpty(sfBA);CHKERRQ(ierr);
@@ -248,5 +270,18 @@ int main(int argc,char **argv)
    test:
       suffix: basic_3
       nsize: 3
+
+   test:
+      suffix: window
+      args: -user_sf_type window -sf_type window -sf_window_flavor {{create dynamic allocate}} -sf_window_sync {{fence active lock}}
+      nsize: {{1 2 3}separate output}
+      requires: define(PETSC_HAVE_MPI_ONE_SIDED)
+
+   # The nightly test suite with MPICH uses ch3:sock, which is broken when winsize == 0 in some of the processes
+   test:
+      suffix: window_shared
+      args: -user_sf_type window -sf_type window -sf_window_flavor shared -sf_window_sync {{fence active lock}}
+      nsize: {{1 2 3}separate output}
+      requires: define(PETSC_HAVE_MPI_PROCESS_SHARED_MEMORY) !define(PETSC_HAVE_MPICH_NUMVERSION) define(PETSC_HAVE_MPI_ONE_SIDED)
 
 TEST*/
