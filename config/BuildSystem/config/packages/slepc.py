fb68c966b8f74362e1ad99e9289a6e4bc59a157d
Jose E. Roman <jroman@dsic.upv.es>
2019-12-13 15:29:16 +0100

    Update slepc.py to point to a version that fixes 'cxx' replacement
    Fixes Issue #514

--- config/BuildSystem/config/packages/slepc.py
+++ config/BuildSystem/config/packages/slepc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = 'e63a3db' #master nov-04-2019
+    self.gitcommit              = 'bf89b9d' #master dec-13-2019
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
