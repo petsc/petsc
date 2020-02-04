66af783f5f299f48d422648009a9cb87d5945fd0
Satish Balay <balay@mcs.anl.gov>
2020-02-04 10:40:25 -0600

    doc: fixes [and fix MatCoarsenViewFromOptions ftn interface that wasn't tested before]

--- src/mat/coarsen/ftn-custom/zcoarsenf.c
+++ src/mat/coarsen/ftn-custom/zcoarsenf.c
@@ -1,6 +1,5 @@
 #include <petsc/private/fortranimpl.h>
-#include <petscmat.h>
-#include <petscviewer.h>
+#include <petscmatcoarsen.h>
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
 #define matcoarsenviewfromoptions_  MATCOARSENVIEWFROMOPTIONS
@@ -8,11 +7,11 @@
 #define matcoarsenviewfromoptions_  matcoarsenviewfromoptions
 #endif
 
-PETSC_EXTERN void PETSC_STDCALL matcoarsenviewfromoptions_(Mat *ao,PetscObject obj,char* type PETSC_MIXED_LEN(len),PetscErrorCode *ierr PETSC_END_LEN(len))
+PETSC_EXTERN void PETSC_STDCALL matcoarsenviewfromoptions_(MatCoarsen *a,PetscObject obj,char* type PETSC_MIXED_LEN(len),PetscErrorCode *ierr PETSC_END_LEN(len))
 {
   char *t;
 
   FIXCHAR(type,len,t);
-  *ierr = MatCoarsenViewFromOptions(*ao,obj,t);if (*ierr) return;
+  *ierr = MatCoarsenViewFromOptions(*a,obj,t);if (*ierr) return;
   FREECHAR(type,t);
 }
