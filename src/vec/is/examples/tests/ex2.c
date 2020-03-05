22a8f86c5e481f60ff98917e6874298d26cbcd3b
Lisandro Dalcin <dalcinl@gmail.com>
2020-03-05 16:22:00 +0300

    PETSCVIEWERBINARY: Various fixes and enhancements
    * MPI-IO support for FILE_MODE_APPEND (write at end of file)
    * PetscViewerBinaryWriteReadMPIIO() now does IO only in rank zero
    * PetscViewerBinaryAddMPIIOOffset() is now logically collective
    * Fix PetscViewerSetFromOptions() to print current option values
    * Add missing error checking in set/get routines

--- src/vec/is/examples/tests/ex2.c
+++ src/vec/is/examples/tests/ex2.c
@@ -1,4 +1,3 @@
-
 static char help[]= "Tests ISView() and ISLoad() \n\n";
 
 #include <petscis.h>
@@ -7,30 +6,59 @@ static char help[]= "Tests ISView() and ISLoad() \n\n";
 int main(int argc,char **argv)
 {
   PetscErrorCode         ierr;
-  PetscInt               n = 3,ix[3][3] = {{3,5,4},{1,7,9},{0,2,8}};
-  IS                     isx,il;
+  PetscInt               n = 3, j, i;
+  PetscInt               ix[3][3][3] = {{{3,5,4},{1,7,9},{0,2,8}},
+                                        {{0,2,8},{3,5,4},{1,7,9}},
+                                        {{1,7,9},{0,2,8},{3,5,4}}};
+  IS                     isx[3],il;
   PetscMPIInt            size,rank;
   PetscViewer            vx,vl;
   PetscBool              equal;
 
-  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
+  ierr = PetscInitialize(&argc,&argv,NULL,help);if (ierr) return ierr;
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   if (size > 3) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_ARG_SIZ,"Example only works with up to three processes");
-  ierr = ISCreateGeneral(PETSC_COMM_WORLD,n,ix[rank],PETSC_COPY_VALUES,&isx);CHKERRQ(ierr);
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_WRITE,&vx);CHKERRQ(ierr);
-  ierr = ISView(isx,vx);CHKERRQ(ierr);
-  ierr = PetscViewerDestroy(&vx);CHKERRQ(ierr);
 
-  ierr = ISCreate(PETSC_COMM_WORLD,&il);CHKERRQ(ierr);
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_READ,&vl);CHKERRQ(ierr);
-  ierr = ISLoad(il,vl);CHKERRQ(ierr);
-  ierr = PetscViewerDestroy(&vl);CHKERRQ(ierr);
+  for (i = 0; i < 3; i++) {
+    ierr = ISCreateGeneral(PETSC_COMM_WORLD,n,ix[i][rank],PETSC_COPY_VALUES,&isx[i]);CHKERRQ(ierr);
+  }
 
-  ierr = ISEqual(il,isx,&equal);CHKERRQ(ierr);
-  if (!equal) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_PLIB,"Index set loaded from file does not match index set to file");
-  ierr = ISDestroy(&il);CHKERRQ(ierr);
-  ierr = ISDestroy(&isx);CHKERRQ(ierr);
+  for (j = 0; j < 3; j++) {
+
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_WRITE,&vx);CHKERRQ(ierr);
+    ierr = ISView(isx[0],vx);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&vx);CHKERRQ(ierr);
+
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_READ,&vl);CHKERRQ(ierr);
+    ierr = ISCreate(PETSC_COMM_WORLD,&il);CHKERRQ(ierr);
+    ierr = ISLoad(il,vl);CHKERRQ(ierr);
+    ierr = ISEqual(il,isx[0],&equal);CHKERRQ(ierr);
+    if (!equal) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_PLIB,"Iteration %D - Index set loaded from file does not match",j);
+    ierr = ISDestroy(&il);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&vl);CHKERRQ(ierr);
+
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_APPEND,&vx);CHKERRQ(ierr);
+    ierr = ISView(isx[1],vx);CHKERRQ(ierr);
+    ierr = ISView(isx[2],vx);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&vx);CHKERRQ(ierr);
+
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"testfile",FILE_MODE_READ,&vl);CHKERRQ(ierr);
+    for (i = 0; i < 3; i++) {
+      ierr = ISCreate(PETSC_COMM_WORLD,&il);CHKERRQ(ierr);
+      ierr = ISLoad(il,vl);CHKERRQ(ierr);
+      ierr = ISEqual(il,isx[i],&equal);CHKERRQ(ierr);
+      if (!equal) SETERRQ2(PETSC_COMM_WORLD,PETSC_ERR_PLIB,"Iteration %D - Index set %D loaded from file does not match",j,i);
+      ierr = ISDestroy(&il);CHKERRQ(ierr);
+    }
+    ierr = PetscViewerDestroy(&vl);CHKERRQ(ierr);
+
+  }
+
+  for (i = 0; i < 3; i++) {
+    ierr = ISDestroy(&isx[i]);CHKERRQ(ierr);
+  }
+
   ierr = PetscFinalize();
   return ierr;
 }
@@ -39,15 +67,17 @@ int main(int argc,char **argv)
 
    test:
 
-   test:
-      suffix: 2
-      nsize: 2
+   testset:
       output_file: output/ex2_1.out
-
-   test:
-      suffix: 3
-      nsize: 3
-      output_file: output/ex2_1.out
+      test:
+        suffix: 1
+        nsize: 1
+      test:
+        suffix: 2
+        nsize: 2
+      test:
+        suffix: 3
+        nsize: 3
 
    testset:
       requires: mpiio
