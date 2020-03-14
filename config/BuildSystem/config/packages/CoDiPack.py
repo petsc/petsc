b1ef6821e2a89400390402127645a2038b1ebdbb
Jed Brown <jed@jedbrown.org>
2020-03-14 15:21:44 -0600

    config: upgrade CoDiPack to v1.9.1 (2020-01-13)

--- config/BuildSystem/config/packages/CoDiPack.py
+++ config/BuildSystem/config/packages/CoDiPack.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit     = '388077c' #master oct-01-2019
+    self.gitcommit     = 'v1.9.1' # 2020-01-13
     self.download      = ['git://https://github.com/SciCompKL/CoDiPack.git']
     self.includes      = ['codi/adjointInterface.hpp']
     self.liblist       = []
