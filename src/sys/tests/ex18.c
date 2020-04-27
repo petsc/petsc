123263cfed472c1465eaf5cf61dd1541bbcd0e7a
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2020-04-27 23:32:32 +0200

    fix src/sys/tests/ex18.c and add test

--- src/sys/tests/ex18.c
+++ src/sys/tests/ex18.c
@@ -10,7 +10,14 @@ int main(int argc,char **argv)
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
   ierr = PetscContainerCreate(PETSC_COMM_SELF,&container);CHKERRQ(ierr);
-  ierr = PetscContainerDestroy(container);CHKERRQ(ierr);
+  ierr = PetscContainerDestroy(&container);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
 }
+
+
+/*TEST
+
+   test:
+
+TEST*/
