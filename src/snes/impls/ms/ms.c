9ce202e011f2eb5cfa64710b2cc03044437a6e9e
Lisandro Dalcin <dalcinl@gmail.com>
2020-05-01 16:46:37 +0300

    SNESMS: Extend SNESMSRegister() to support methods in basic form

--- src/snes/impls/ms/ms.c
+++ src/snes/impls/ms/ms.c
@@ -46,64 +46,49 @@ PetscErrorCode SNESMSRegisterAll(void)
   SNESMSRegisterAllCalled = PETSC_TRUE;
 
   {
-    PetscReal gamma[3][1] = {{1.0},{0.0},{0.0}};
-    PetscReal delta[1]    = {0.0};
-    PetscReal betasub[1]  = {1.0};
-    ierr = SNESMSRegister(SNESMSEULER,1,3,1.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[1] = {1.0};
+    ierr = SNESMSRegister(SNESMSEULER,1,1,1.0,NULL,NULL,alpha);CHKERRQ(ierr);
   }
 
   {
     PetscReal gamma[3][6] = {
-      {0.0000000000000000E+00,  -7.0304722367110606E-01, -1.9836719667506464E-01, -1.6023843981863788E+00, 9.4483822882855284E-02,  -1.4204296130641869E-01},
+      {0.0000000000000000E+00, -7.0304722367110606E-01, -1.9836719667506464E-01, -1.6023843981863788E+00,  9.4483822882855284E-02, -1.4204296130641869E-01},
       {1.0000000000000000E+00,  1.1111025767083920E+00,  5.6150921583923230E-01,  7.4151723494934041E-01,  3.1714538168600587E-01,  4.6479276238548706E-01},
-      {0.0000000000000000E+00,  0.0000000000000000E+00,  0.0000000000000000E+00,  6.7968174970583317E-01,  -4.1755042846051737E-03, -1.9115668129923846E-01}
+      {0.0000000000000000E+00,  0.0000000000000000E+00,  0.0000000000000000E+00,  6.7968174970583317E-01, -4.1755042846051737E-03, -1.9115668129923846E-01}
     };
     PetscReal delta[6]   = {1.0000000000000000E+00, 5.3275427433201750E-01, 6.0143544663985238E-01, 4.5874077053842177E-01, 2.7544386906104651E-01, 0.0000000000000000E+00};
     PetscReal betasub[6] = {8.4753115429481929E-01, 7.4018896368655618E-01, 6.5963574086583309E-03, 4.6747795645517759E-01, 1.3314545813643919E-01, 5.3260800028018784E-01};
     ierr = SNESMSRegister(SNESMSM62,6,3,1.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
   }
-  {
-    PetscReal gamma[3][4] = {{0,0,0,0},{0,0,0,0},{1,1,1,1}};
-    PetscReal delta[4]    = {0,0,0,0};
-    PetscReal betasub[4]  = {0.25, 0.5, 0.55, 1.0};
-    ierr = SNESMSRegister(SNESMSJAMESON83,4,3,1.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+
+  { /* Jameson (1983) */
+    PetscReal alpha[4] = {0.25, 0.5, 0.55, 1.0};
+    ierr = SNESMSRegister(SNESMSJAMESON83,4,1,1.0,NULL,NULL,alpha);CHKERRQ(ierr);
   }
 
   { /* Van Leer, Tai, and Powell (1989) 1 stage, order 1 */
-    PetscReal gamma[3][1] = {{0},{0},{1}};
-    PetscReal delta[1]    = {0};
-    PetscReal betasub[1]  = {1.0};
-    ierr = SNESMSRegister(SNESMSVLTP11,1,3,0.5,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[1]  = {1.0};
+    ierr = SNESMSRegister(SNESMSVLTP11,1,1,0.5,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   { /* Van Leer, Tai, and Powell (1989) 2 stage, order 1 */
-    PetscReal gamma[3][2] = {{0,0},{0,0},{1,1}};
-    PetscReal delta[2]    = {0,0};
-    PetscReal betasub[2]  = {0.3333,1.0};
-    ierr = SNESMSRegister(SNESMSVLTP21,2,3,1.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[2] = {0.3333, 1.0};
+    ierr = SNESMSRegister(SNESMSVLTP21,2,1,1.0,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   { /* Van Leer, Tai, and Powell (1989) 3 stage, order 1 */
-    PetscReal gamma[3][3] = {{0,0,0},{0,0,0},{1,1,1}};
-    PetscReal delta[3]    = {0,0,0};
-    PetscReal betasub[3]  = {0.1481,0.4000,1.0};
-    ierr = SNESMSRegister(SNESMSVLTP31,3,3,1.5,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[3] = {0.1481, 0.4000, 1.0};
+    ierr = SNESMSRegister(SNESMSVLTP31,3,1,1.5,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   { /* Van Leer, Tai, and Powell (1989) 4 stage, order 1 */
-    PetscReal gamma[3][4] = {{0,0,0,0},{0,0,0,0},{1,1,1,1}};
-    PetscReal delta[4]    = {0,0,0,0};
-    PetscReal betasub[4]  = {0.0833,0.2069,0.4265,1.0};
-    ierr = SNESMSRegister(SNESMSVLTP41,4,3,2.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[4] = {0.0833, 0.2069, 0.4265, 1.0};
+    ierr = SNESMSRegister(SNESMSVLTP41,4,1,2.0,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   { /* Van Leer, Tai, and Powell (1989) 5 stage, order 1 */
-    PetscReal gamma[3][5] = {{0,0,0,0,0},{0,0,0,0,0},{1,1,1,1,1}};
-    PetscReal delta[5]    = {0,0,0,0,0};
-    PetscReal betasub[5]  = {0.0533,0.1263,0.2375,0.4414,1.0};
-    ierr = SNESMSRegister(SNESMSVLTP51,5,3,2.5,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[5] = {0.0533, 0.1263, 0.2375, 0.4414,1.0};
+    ierr = SNESMSRegister(SNESMSVLTP51,5,1,2.5,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   { /* Van Leer, Tai, and Powell (1989) 6 stage, order 1 */
-    PetscReal gamma[3][6] = {{0,0,0,0,0,0},{0,0,0,0,0,0},{1,1,1,1,1,1}};
-    PetscReal delta[6]    = {0,0,0,0,0,0};
-    PetscReal betasub[6]  = {0.0370,0.0851,0.1521,0.2562,0.4512,1.0};
-    ierr = SNESMSRegister(SNESMSVLTP61,6,3,3.0,&gamma[0][0],delta,betasub);CHKERRQ(ierr);
+    PetscReal alpha[6] = {0.0370, 0.0851, 0.1521, 0.2562, 0.4512, 1.0};
+    ierr = SNESMSRegister(SNESMSVLTP61,6,1,3.0,NULL,NULL,alpha);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
@@ -127,8 +112,10 @@ PetscErrorCode SNESMSRegisterDestroy(void)
     SNESMSTableau t = &link->tab;
     SNESMSTableauList = link->next;
 
-    ierr = PetscFree3(t->gamma,t->delta,t->betasub);CHKERRQ(ierr);
     ierr = PetscFree(t->name);CHKERRQ(ierr);
+    ierr = PetscFree(t->gamma);CHKERRQ(ierr);
+    ierr = PetscFree(t->delta);CHKERRQ(ierr);
+    ierr = PetscFree(t->betasub);CHKERRQ(ierr);
     ierr = PetscFree(link);CHKERRQ(ierr);
   }
   SNESMSRegisterAllCalled = PETSC_FALSE;
@@ -184,12 +171,22 @@ PetscErrorCode SNESMSFinalizePackage(void)
 +  name - identifier for method
 .  nstages - number of stages
 .  nregisters - number of registers used by low-storage implementation
+.  stability - scaled stability region
 .  gamma - coefficients, see Ketcheson's paper
 .  delta - coefficients, see Ketcheson's paper
 -  betasub - subdiagonal of Shu-Osher form
 
    Notes:
    The notation is described in Ketcheson (2010) Runge-Kutta methods with minimum storage implementations.
+
+   Many multistage schemes are of the form
+   $ X_0 = X^{(n)}
+   $ X_k = X_0 + \alpha_k * F(X_{k-1}), k = 1,\ldots,s
+   $ X^{(n+1)} = X_s
+   These methods can be registered with
+.vb
+   SNESMSRegister("name",s,1,stability,NULL,NULL,alpha);
+.ve
 
    Level: advanced
 
@@ -204,9 +201,13 @@ PetscErrorCode SNESMSRegister(SNESMSType name,PetscInt nstages,PetscInt nregiste
   PetscFunctionBegin;
   PetscValidCharPointer(name,1);
   if (nstages < 1) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_ARG_OUTOFRANGE,"Must have at least one stage");
-  if (nregisters != 3) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"Only support for methods written in 3-register form");
-  PetscValidRealPointer(gamma,4);
-  PetscValidRealPointer(delta,5);
+  if (gamma || delta) {
+    if (nregisters != 3) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"Only support for methods written in 3-register form");
+    PetscValidRealPointer(gamma,4);
+    PetscValidRealPointer(delta,5);
+  } else {
+    if (nregisters != 1) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"Only support for methods written in 1-register form");
+  }
   PetscValidRealPointer(betasub,6);
 
   ierr          = SNESMSInitializePackage();CHKERRQ(ierr);
@@ -217,9 +218,13 @@ PetscErrorCode SNESMSRegister(SNESMSType name,PetscInt nstages,PetscInt nregiste
   t->nregisters = nregisters;
   t->stability  = stability;
 
-  ierr = PetscMalloc3(nstages*nregisters,&t->gamma,nstages,&t->delta,nstages,&t->betasub);CHKERRQ(ierr);
-  ierr = PetscArraycpy(t->gamma,gamma,nstages*nregisters);CHKERRQ(ierr);
-  ierr = PetscArraycpy(t->delta,delta,nstages);CHKERRQ(ierr);
+  if (gamma && delta) {
+    ierr = PetscMalloc1(nstages*nregisters,&t->gamma);CHKERRQ(ierr);
+    ierr = PetscMalloc1(nstages,&t->delta);CHKERRQ(ierr);
+    ierr = PetscArraycpy(t->gamma,gamma,nstages*nregisters);CHKERRQ(ierr);
+    ierr = PetscArraycpy(t->delta,delta,nstages);CHKERRQ(ierr);
+  }
+  ierr = PetscMalloc1(nstages,&t->betasub);CHKERRQ(ierr);
   ierr = PetscArraycpy(t->betasub,betasub,nstages);CHKERRQ(ierr);
 
   link->next        = SNESMSTableauList;
@@ -239,7 +244,6 @@ static PetscErrorCode SNESMSStep_3Sstar(SNES snes,Vec X,Vec F)
   const PetscReal *gamma = t->gamma,*delta = t->delta,*betasub = t->betasub;
   Vec             S1,S2,S3,Y;
   PetscInt        i,nstages = t->nstages;
-
 
   PetscFunctionBegin;
   Y    = snes->work[0];
@@ -265,6 +269,46 @@ static PetscErrorCode SNESMSStep_3Sstar(SNES snes,Vec X,Vec F)
     }
     ierr = KSPSolve(snes->ksp,F,Y);CHKERRQ(ierr);
     ierr = VecMAXPY(S1,4,scoeff,Ss);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+/*
+  X - initial state, updated in-place.
+  F - residual, computed at the initial X on input
+*/
+static PetscErrorCode SNESMSStep_Basic(SNES snes,Vec X,Vec F)
+{
+  PetscErrorCode  ierr;
+  SNES_MS         *ms    = (SNES_MS*)snes->data;
+  SNESMSTableau   tab    = ms->tableau;
+  const PetscReal *alpha = tab->betasub, h = ms->damping;
+  PetscInt        i,nstages = tab->nstages;
+  Vec             X0 = snes->work[0];
+
+  PetscFunctionBegin;
+  ierr = VecCopy(X,X0);CHKERRQ(ierr);
+  for (i = 0; i < nstages; i++) {
+    if (i > 0) {
+      ierr = SNESComputeFunction(snes,X,F);CHKERRQ(ierr);
+    }
+    ierr = KSPSolve(snes->ksp,F,X);CHKERRQ(ierr);
+    ierr = VecAYPX(X,-alpha[i]*h,X0);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode SNESMSStep_Step(SNES snes,Vec X,Vec F)
+{
+  SNES_MS        *ms = (SNES_MS*)snes->data;
+  SNESMSTableau  tab = ms->tableau;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  if (tab->gamma && tab->delta) {
+    ierr = SNESMSStep_3Sstar(snes,X,F);CHKERRQ(ierr);
+  } else {
+    ierr = SNESMSStep_Basic(snes,X,F);CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
@@ -295,7 +339,6 @@ static PetscErrorCode SNESMSStep_Norms(SNES snes,PetscInt iter,Vec F)
   }
   PetscFunctionReturn(0);
 }
-
 
 static PetscErrorCode SNESSolve_MS(SNES snes)
 {
@@ -335,7 +378,7 @@ static PetscErrorCode SNESSolve_MS(SNES snes)
       ierr = KSPSetOperators(snes->ksp,snes->jacobian,snes->jacobian_pre);CHKERRQ(ierr);
     }
 
-    ierr = SNESMSStep_3Sstar(snes,X,F);CHKERRQ(ierr);
+    ierr = SNESMSStep_Step(snes,X,F);CHKERRQ(ierr);
 
     if (i < snes->max_its-1 || ms->norms) {
       ierr = SNESComputeFunction(snes,X,F);CHKERRQ(ierr);
@@ -350,10 +393,13 @@ static PetscErrorCode SNESSolve_MS(SNES snes)
 
 static PetscErrorCode SNESSetUp_MS(SNES snes)
 {
+  SNES_MS        *ms   = (SNES_MS*)snes->data;
+  SNESMSTableau  tab   = ms->tableau;
+  PetscInt       nwork = tab->nregisters;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
-  ierr = SNESSetWorkVecs(snes,3);CHKERRQ(ierr);
+  ierr = SNESSetWorkVecs(snes,nwork);CHKERRQ(ierr);
   ierr = SNESSetUpMatrices(snes);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
@@ -638,4 +684,3 @@ PETSC_EXTERN PetscErrorCode SNESCreate_MS(SNES snes)
   ierr = SNESMSSetType(snes,SNESMSDefault);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
-
