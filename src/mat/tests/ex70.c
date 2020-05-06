456288a8fa8a79fdbaeb1744199f1ef702eb3e1a
Stefano Zampini <stefano.zampini@gmail.com>
2020-05-06 12:31:46 +0300

    Mat ex70: add tests
    MatDenseGetColumnVec and friends
    MATSHELL
    CUDA

--- src/mat/tests/ex70.c
+++ src/mat/tests/ex70.c
@@ -20,19 +20,21 @@ static PetscErrorCode CheckLocal(Mat A, Mat B, PetscScalar *a, PetscScalar *b)
     wB   = (PetscBool)(b != Bb);
     ierr = MatDenseRestoreArrayRead(B,&Bb);CHKERRQ(ierr);
   }
-  if (wA || wB) SETERRQ2(PETSC_COMM_SELF,PETSC_ERR_PLIB,"Wrong array in A Mat %d, Wrong array in B Mat %d",wA,wB);
+  if (wA || wB) SETERRQ2(PETSC_COMM_SELF,PETSC_ERR_PLIB,"Wrong array in first Mat? %d, Wrong array in second Mat? %d",wA,wB);
   PetscFunctionReturn(0);
 }
 
 int main(int argc,char **args)
 {
-  Mat            X,B,A,T;
-  PetscInt       m,n,M = 10,N = 10,K = 5, ldx = 0, ldb = 0;
+  Mat            X,B,A,T,T2;
+  PetscInt       m,n,k,M = 10,N = 10,K = 5, ldx = 3, ldb = 5;
   const char     *deft = MATAIJ;
   char           mattype[256];
   PetscBool      flg,symm = PETSC_FALSE,testtt = PETSC_TRUE, testnest = PETSC_TRUE, testtranspose = PETSC_TRUE, testcircular = PETSC_FALSE, local = PETSC_TRUE;
+  PetscBool      xgpu = PETSC_FALSE,bgpu = PETSC_FALSE;
   PetscScalar    *dataX = NULL,*dataB = NULL;
   PetscScalar    *aX,*aB;
+  PetscReal      err;
   PetscErrorCode ierr;
 
   ierr = PetscInitialize(&argc,&args,NULL,help);if (ierr) return ierr;
@@ -47,6 +49,8 @@ int main(int argc,char **args)
   ierr = PetscOptionsGetBool(NULL,NULL,"-testnest",&testnest,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-testtt",&testtt,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-testcircular",&testcircular,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-xgpu",&xgpu,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-bgpu",&bgpu,NULL);CHKERRQ(ierr);
 #if defined(PETSC_USE_COMPLEX)
   testtranspose = PETSC_FALSE;
   testtt = PETSC_FALSE;
@@ -66,12 +70,14 @@ int main(int argc,char **args)
   }
   ierr = MatViewFromOptions(A,NULL,"-A_init_view");CHKERRQ(ierr);
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,"","","");CHKERRQ(ierr);
-  ierr = PetscOptionsFList("-mat_type","Matrix type","MatSetType",MatList,deft,mattype,256,&flg);CHKERRQ(ierr);
+  ierr = PetscOptionsFList("-A_mat_type","Matrix type","MatSetType",MatList,deft,mattype,256,&flg);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
   if (flg) {
     Mat A2;
 
-    ierr = MatConvert(A,mattype,MAT_INITIAL_MATRIX,&A2);CHKERRQ(ierr);
+    /* MATSEQAIJCUSPARSE does not support MAT_INITIAL_MATRIX */
+    ierr = MatDuplicate(A,MAT_COPY_VALUES,&A2);CHKERRQ(ierr);
+    ierr = MatConvert(A,mattype,MAT_INPLACE_MATRIX,&A);CHKERRQ(ierr);
     ierr = MatMultEqual(A,A2,10,&flg);CHKERRQ(ierr);
     if (!flg) {
       Mat AE,A2E;
@@ -86,8 +92,7 @@ int main(int argc,char **args)
       ierr = MatDestroy(&A2E);CHKERRQ(ierr);
       ierr = MatDestroy(&AE);CHKERRQ(ierr);
     }
-    ierr = MatDestroy(&A);CHKERRQ(ierr);
-    A = A2;
+    ierr = MatDestroy(&A2);CHKERRQ(ierr);
   }
   ierr = MatViewFromOptions(A,NULL,"-A_view");CHKERRQ(ierr);
 
@@ -111,6 +116,16 @@ int main(int argc,char **args)
     dataB = aB;
   }
   ierr = MatSetRandom(B,NULL);CHKERRQ(ierr);
+  /* convert to CUDA if needed */
+  if (bgpu) {
+    ierr = MatConvert(B,MATDENSECUDA,MAT_INPLACE_MATRIX,&B);CHKERRQ(ierr);
+  }
+  if (xgpu) {
+    ierr = MatAssemblyBegin(X,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(X,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatConvert(X,MATDENSECUDA,MAT_INPLACE_MATRIX,&X);CHKERRQ(ierr);
+  }
+  ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
 
   /* Test reusing a previously allocated dense buffer */
   ierr = MatMatMult(A,B,MAT_REUSE_MATRIX,PETSC_DEFAULT,&X);CHKERRQ(ierr);
@@ -124,10 +139,63 @@ int main(int argc,char **args)
     ierr = MatDestroy(&T);CHKERRQ(ierr);
   }
 
+  /* Test MatDenseGetColumnVec and friends */
+  ierr = MatMatMult(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+  ierr = MatDuplicate(T,MAT_DO_NOT_COPY_VALUES,&T2);CHKERRQ(ierr);
+  for (k=0;k<K;k++) {
+    Vec Xv,Tv,T2v;
+
+    ierr = MatDenseGetColumnVecRead(X,k,&Xv);CHKERRQ(ierr);
+    ierr = MatDenseGetColumnVec(T,k,&Tv);CHKERRQ(ierr);
+    ierr = MatDenseGetColumnVecWrite(T2,k,&T2v);CHKERRQ(ierr);
+    ierr = VecCopy(Xv,T2v);CHKERRQ(ierr);
+    ierr = VecAXPY(Tv,-1.,Xv);CHKERRQ(ierr);
+    ierr = MatDenseRestoreColumnVecRead(X,k,&Xv);CHKERRQ(ierr);
+    ierr = MatDenseRestoreColumnVec(T,k,&Tv);CHKERRQ(ierr);
+    ierr = MatDenseRestoreColumnVecWrite(T2,k,&T2v);CHKERRQ(ierr);
+  }
+  ierr = MatNorm(T,NORM_FROBENIUS,&err);CHKERRQ(ierr);
+  if (err > PETSC_SMALL) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with MatDenseGetColumnVec\n");CHKERRQ(ierr);
+    ierr = MatView(T,NULL);CHKERRQ(ierr);
+  }
+  ierr = MatAXPY(T2,-1.,X,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+  ierr = MatNorm(T2,NORM_FROBENIUS,&err);CHKERRQ(ierr);
+  if (err > PETSC_SMALL) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with MatDenseGetColumnVecWrite\n");CHKERRQ(ierr);
+    ierr = MatView(T2,NULL);CHKERRQ(ierr);
+  }
+  ierr = MatDestroy(&T);CHKERRQ(ierr);
+  ierr = MatDestroy(&T2);CHKERRQ(ierr);
+
+  /* Test with MatShell */
+  ierr = MatConvert(A,MATSHELL,MAT_INITIAL_MATRIX,&T2);CHKERRQ(ierr);
+  ierr = MatMatMult(T2,B,MAT_REUSE_MATRIX,PETSC_DEFAULT,&X);CHKERRQ(ierr);
+  ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
+  ierr = MatMatMultEqual(T2,B,X,10,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with reusage (MATSHELL)\n");CHKERRQ(ierr);
+    ierr = MatMatMult(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+    ierr = MatAXPY(T,-1.0,X,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+    ierr = MatView(T,NULL);CHKERRQ(ierr);
+    ierr = MatDestroy(&T);CHKERRQ(ierr);
+  }
+  ierr = MatTransposeMatMult(T2,X,MAT_REUSE_MATRIX,PETSC_DEFAULT,&B);CHKERRQ(ierr);
+  ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
+  ierr = MatTransposeMatMultEqual(T2,X,B,10,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with reusage (MatTranspose, MATSHELL)\n");CHKERRQ(ierr);
+  }
+  ierr = MatDestroy(&T2);CHKERRQ(ierr);
+
   if (testnest) { /* test with MatNest */
-    Mat NA;
+    Mat        NA;
+    const char *vtype;
 
     ierr = MatCreateNest(PETSC_COMM_WORLD,1,NULL,1,NULL,&A,&NA);CHKERRQ(ierr);
+    /* needed to test against CUSPARSE matrices */
+    ierr = MatGetVecType(A,&vtype);CHKERRQ(ierr);
+    ierr = MatSetVecType(NA,vtype);CHKERRQ(ierr);
     ierr = MatViewFromOptions(NA,NULL,"-NA_view");CHKERRQ(ierr);
     ierr = MatMatMult(NA,B,MAT_REUSE_MATRIX,PETSC_DEFAULT,&X);CHKERRQ(ierr);
     ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
@@ -209,9 +277,29 @@ int main(int argc,char **args)
 
   test:
     output_file: output/ex70_1.out
+    requires: cuda
+    suffix: 1_cuda
+    args: -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{seqaijcusparse seqaij}} -testnest 0
+
+  test:
+    TODO: VecGetSubVector seems broken with CUDA
+    output_file: output/ex70_1.out
+    requires: cuda
+    suffix: 1_cuda_broken
+    args: -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type seqaijcusparse -testnest
+
+  test:
+    output_file: output/ex70_1.out
     nsize: 2
     suffix: 1_par
     args: -testtranspose 0 -local {{0 1}}
+
+  test:
+    output_file: output/ex70_1.out
+    requires: cuda
+    nsize: 2
+    suffix: 1_par_cuda
+    args: -testtranspose 0 -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{mpiaijcusparse mpiaij}} -testnest 0
 
   test:
     TODO: MPIAIJ x MPIDENSE broken for MatTransposeMatMult
@@ -227,10 +315,24 @@ int main(int argc,char **args)
     args: -M {{7 11}} -N {{12 9}} -K {{1 3}} -local {{0 1}}
 
   test:
+    requires: cuda
+    output_file: output/ex70_1.out
+    suffix: 2_cuda
+    nsize: 1
+    args: -M 7 -N 9 -K 2 -local {{0 1}} -testnest 0 -A_mat_type {{seqdensecuda seqdense}} -xgpu {{0 1}} -bgpu {{0 1}}
+
+  test:
     output_file: output/ex70_1.out
     suffix: 2_par
     nsize: 2
     args: -M {{7 11}} -N {{12 9}} -K {{1 3}} -local {{0 1}} -testcircular 0
+
+  test:
+    requires: cuda
+    output_file: output/ex70_1.out
+    suffix: 2_par_cuda
+    nsize: 2
+    args: -M 11 -N 9 -K 1 -local {{0 1}} -testcircular 0 -A_mat_type mpiaijcusparse -xgpu -bgpu -testnest 0
 
   test:
     TODO: MatTransposeMatMultSymbolic_SeqAIJ_SeqDense plays with the destroy routine
@@ -242,8 +344,8 @@ int main(int argc,char **args)
   test:
     output_file: output/ex70_1.out
     suffix: 3
-    nsize: 1
-    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -mat_type sbaij -symm -testtranspose 0
+    nsize: {{1 3}}
+    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type sbaij -symm -testtranspose 0
 
   test:
     output_file: output/ex70_1.out
@@ -281,12 +383,12 @@ int main(int argc,char **args)
     output_file: output/ex70_1.out
     suffix: 7
     nsize: 1
-    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -mat_type dense -testnest 0 -testcircular
+    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type dense -testnest 0 -testcircular
 
   test:
     TODO: NEST x DENSE with dense nested matrices seems broken in this case
     output_file: output/ex70_1.out
     suffix: 7_broken
     nsize: 1
-    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -mat_type dense -testnest -testcircular
+    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type dense -testnest -testcircular
 TEST*/
