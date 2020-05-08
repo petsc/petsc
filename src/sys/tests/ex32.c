a87ad0e9353f885899e364e9a8ddc629c4bfa172
Stefano Zampini <stefano.zampini@gmail.com>
2020-05-08 11:20:42 +0300

    PETSCSYS tests: fix for cuda builds

--- src/sys/tests/ex32.c
+++ src/sys/tests/ex32.c
@@ -20,6 +20,6 @@ int main(int argc,char **argv)
 /*TEST
 
    test:
-      args: -skip_petscrc -options_left 0
+      args: -skip_petscrc -options_left 0 -use_gpu_aware_mpi 0
       filter: egrep -v "(malloc|saws_port_auto_select|vecscatter_mpi1|error_output_stdout|check_pointer_intensity|cuda_initialize|nox|use_gpu_aware_mpi)"
 TEST*/
