750a5874f4be12214f83bfcbe6f0a25b9e16fdea
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2020-06-26 17:36:23 +0200

    [PC + KSP]HPDDM: multilevel + multiple RHS solve

--- src/ksp/ksp/tutorials/ex76.c
+++ src/ksp/ksp/tutorials/ex76.c
@@ -5,12 +5,13 @@ static char help[] = "Solves a linear system using PCHPDDM.\n\n";
 int main(int argc,char **args)
 {
   Vec            x,b;        /* computed solution and RHS */
-  Mat            A,aux;      /* linear system matrix */
+  Mat            A,aux,X,B;  /* linear system matrix */
   KSP            ksp;        /* linear solver context */
   PC             pc;
   IS             is,sizes;
   const PetscInt *idx;
   PetscMPIInt    rank,size;
+  PetscInt       m,N = 1;
   const char     *deft = MATAIJ;
   PetscViewer    viewer;
   char           dir[PETSC_MAX_PATH_LEN],name[PETSC_MAX_PATH_LEN],type[256];
@@ -20,6 +21,7 @@ int main(int argc,char **args)
   ierr = PetscInitialize(&argc,&args,NULL,help);if (ierr) return ierr;
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   if (size != 4) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_USER,"This example requires 4 processes");
+  ierr = PetscOptionsGetInt(NULL,NULL,"-rhs",&N,NULL);CHKERRQ(ierr);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
   ierr = MatCreate(PETSC_COMM_SELF,&aux);CHKERRQ(ierr);
@@ -74,8 +76,41 @@ int main(int argc,char **args)
   ierr = MatCreateVecs(A,&x,&b);CHKERRQ(ierr);
   ierr = VecSet(b,1.0);CHKERRQ(ierr);
   ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(x,&m);CHKERRQ(ierr);
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&b);CHKERRQ(ierr);
+  if (N > 1) {
+    KSPType type;
+    ierr = PetscOptionsClearValue(NULL,"-ksp_converged_reason");CHKERRQ(ierr);
+    ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
+    ierr = MatCreateDense(PETSC_COMM_WORLD,m,PETSC_DECIDE,PETSC_DECIDE,N,NULL,&B);CHKERRQ(ierr);
+    ierr = MatCreateDense(PETSC_COMM_WORLD,m,PETSC_DECIDE,PETSC_DECIDE,N,NULL,&X);CHKERRQ(ierr);
+    ierr = MatSetRandom(B,NULL);CHKERRQ(ierr);
+    /* this is algorithmically optimal in the sense that blocks of vectors are coarsened or interpolated using matrix--matrix operations */
+    /* PCHPDDM however heavily relies on MPI[S]BAIJ format for which there is no efficient MatProduct implementation */
+    ierr = KSPMatSolve(ksp,B,X);CHKERRQ(ierr);
+    ierr = KSPGetType(ksp,&type);
+    ierr = PetscStrcmp(type,KSPHPDDM,&flg);CHKERRQ(ierr);
+#if defined(PETSC_HAVE_HPDDM)
+    if (flg) {
+      PetscReal    norm;
+      KSPHPDDMType type;
+      ierr = KSPHPDDMGetType(ksp,&type);
+      if (type == KSP_HPDDM_TYPE_PREONLY || type == KSP_HPDDM_TYPE_CG || type == KSP_HPDDM_TYPE_GMRES || type == KSP_HPDDM_TYPE_GCRODR) {
+        Mat C;
+        ierr = MatDuplicate(X,MAT_DO_NOT_COPY_VALUES,&C);
+        ierr = KSPSetMatSolveBlockSize(ksp,1);
+        ierr = KSPMatSolve(ksp,B,C);CHKERRQ(ierr);
+        ierr = MatAYPX(C,-1.0,X,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+        ierr = MatNorm(C,NORM_INFINITY,&norm);CHKERRQ(ierr);
+        ierr = MatDestroy(&C);CHKERRQ(ierr);
+        if (norm > 100*PETSC_MACHINE_EPSILON) SETERRQ2(PetscObjectComm((PetscObject)pc),PETSC_ERR_PLIB,"KSPMatSolve() and KSPSolve() difference has nonzero norm %g with pseudo-block KSPHPDDMType %s",(double)norm,KSPHPDDMTypes[type]);
+      }
+    }
+#endif
+    ierr = MatDestroy(&X);CHKERRQ(ierr);
+    ierr = MatDestroy(&B);CHKERRQ(ierr);
+  }
   ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
   ierr = MatDestroy(&A);CHKERRQ(ierr);
   ierr = PetscFinalize();
@@ -107,5 +142,12 @@ int main(int argc,char **args)
       output_file: output/ex76_fgmres_geneo_20_p_2.out
       nsize: 4
       args: -ksp_converged_reason -pc_type hpddm -pc_hpddm_levels_1_sub_pc_type cholesky -pc_hpddm_levels_1_eps_nev 20 -pc_hpddm_levels_2_p 2 -pc_hpddm_levels_2_mat_type {{baij sbaij}shared output} -pc_hpddm_levels_2_eps_nev {{5 20}shared output} -pc_hpddm_levels_2_sub_pc_type cholesky -pc_hpddm_levels_2_ksp_type gmres -ksp_type fgmres -pc_hpddm_coarse_mat_type {{baij sbaij}shared output} -mat_type {{aij sbaij}shared output} -load_dir ${DATAFILESPATH}/matrices/hpddm/GENEO
+   # PCHPDDM + KSPHPDDM test to exercise multilevel + multiple RHS in one go
+   test:
+      requires: hpddm slepc datafilespath double !complex !define(PETSC_USE_64BIT_INDICES)
+      suffix: fgmres_geneo_20_p_2_geneo_rhs
+      output_file: output/ex76_fgmres_geneo_20_p_2.out
+      nsize: 4
+      args: -ksp_converged_reason -pc_type hpddm -pc_hpddm_levels_1_sub_pc_type cholesky -pc_hpddm_levels_1_eps_nev 20 -pc_hpddm_levels_2_p 2 -pc_hpddm_levels_2_mat_type baij -pc_hpddm_levels_2_eps_nev 5 -pc_hpddm_levels_2_sub_pc_type cholesky -pc_hpddm_levels_2_ksp_max_it 10 -pc_hpddm_levels_2_ksp_type hpddm -pc_hpddm_levels_2_ksp_hpddm_type gmres -ksp_type hpddm -ksp_hpddm_variant flexible -pc_hpddm_coarse_mat_type baij -mat_type aij -load_dir ${DATAFILESPATH}/matrices/hpddm/GENEO -rhs 4
 
 TEST*/
