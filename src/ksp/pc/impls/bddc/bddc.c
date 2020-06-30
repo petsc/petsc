819759106a4039773f5fdbcb60133805faec8c2a
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2020-06-30 09:00:46 +0000

    PCBDDC: remove irrelevant comment

--- src/ksp/pc/impls/bddc/bddc.c
+++ src/ksp/pc/impls/bddc/bddc.c
@@ -2944,7 +2944,6 @@ PetscErrorCode PCBDDCCreateFETIDPOperators(PC pc, PetscBool fully_redundant, con
    Adaptive selection of primal constraints [4] is supported for SPD systems with high-contrast in the coefficients if MUMPS or MKL_PARDISO are present. Future versions of the code will also consider using PASTIX.
 
    An experimental interface to the FETI-DP method is available. FETI-DP operators could be created using PCBDDCCreateFETIDPOperators(). A stand-alone class for the FETI-DP method will be provided in the next releases.
-   Deluxe scaling is not supported yet for FETI-DP.
 
    Options Database Keys (some of them, run with -help for a complete list):
 
