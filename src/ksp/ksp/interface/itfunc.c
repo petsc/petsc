b5c4cf30c1d039406a5e54012fc98d58cd264d85
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2020-07-03 13:37:29 +0200

    KSPMatSolve: MatZeroEntries(output)

--- src/ksp/ksp/interface/itfunc.c
+++ src/ksp/ksp/interface/itfunc.c
@@ -997,6 +997,9 @@ PetscErrorCode KSPMatSolve(KSP ksp, Mat B, Mat X)
   if (!match) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Provided block of solutions not stored in a dense Mat");
   ierr = KSPSetUp(ksp);CHKERRQ(ierr);
   if (ksp->ops->matsolve) {
+    if (ksp->guess_zero) {
+      ierr = MatZeroEntries(X);CHKERRQ(ierr);
+    }
     ierr = PetscLogEventBegin(KSP_MatSolve, ksp, B, X, 0);CHKERRQ(ierr);
     ierr = KSPGetMatSolveBlockSize(ksp, &Bbn);CHKERRQ(ierr);
     /* by default, do a single solve with all columns */
