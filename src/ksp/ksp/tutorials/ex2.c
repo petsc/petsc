10582eeaa9b3efe3f6cf39ea8a1cfd7a829c4aa6
Jed Brown <jed@jedbrown.org>
2020-07-17 08:11:21 -0600

    Fix doc inconsistencies
    Commit-type: documentation
    Reported-by: Jan Busa Sr. <busaj@jinr.ru>

--- src/ksp/ksp/tutorials/ex2.c
+++ src/ksp/ksp/tutorials/ex2.c
@@ -3,7 +3,7 @@ static char help[] = "Solves a linear system in parallel with KSP.\n\
 Input parameters include:\n\
   -view_exact_sol   : write exact solution vector to stdout\n\
   -m <mesh_x>       : number of mesh points in x-direction\n\
-  -n <mesh_n>       : number of mesh points in y-direction\n\n";
+  -n <mesh_y>       : number of mesh points in y-direction\n\n";
 
 /*T
    Concepts: KSP^basic parallel example;
