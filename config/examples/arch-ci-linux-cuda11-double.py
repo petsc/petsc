5d5bcd6a51068fa059d5349ad18d47151f20595d
Junchao Zhang <jczhang@mcs.anl.gov>
2020-09-17 21:37:06 -0500

    Open cuda-11 testing for double and complex

---
+++ config/examples/arch-ci-linux-cuda11-double.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+petsc_hash_pkgs=os.path.join(os.getenv('HOME'),'petsc-hash-pkgs')
+
+if __name__ == '__main__':
+  import sys
+  import os
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--package-prefix-hash='+petsc_hash_pkgs,
+    '--with-make-test-np=2',
+    'COPTFLAGS=-g -O',
+    'FOPTFLAGS=-g -O',
+    'CXXOPTFLAGS=-g -O',
+    '--with-cuda-dir=/usr/local/cuda-11.0',
+  ]
+
+  configure.petsc_configure(configure_options)
