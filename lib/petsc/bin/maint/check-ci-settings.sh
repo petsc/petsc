a11e88d72f610ee5b2d20f510295bd0314e72eb1
Satish Balay <balay@mcs.anl.gov>
2020-09-25 09:04:36 -0500

    gcov: use appropriate maint/master destination branch
    'make mergegcov' gets the target branch using check-merge-branch.sh script and passes it to gcov.py
    
    And fix gcov.py [merge] - when diff is empty

--- lib/petsc/bin/maint/check-ci-settings.sh
+++ lib/petsc/bin/maint/check-ci-settings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+if [ ! -z ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME+x} ]; then
+  echo Skipping as this is MR CI for ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME} branch
+  exit 0
+fi
+
 git fetch --unshallow --no-tags origin +maint:remotes/origin/maint +master:remotes/origin/master HEAD
 base_maint=$(git merge-base --octopus origin/maint origin/master HEAD)
 base_master=$(git merge-base origin/master HEAD)
