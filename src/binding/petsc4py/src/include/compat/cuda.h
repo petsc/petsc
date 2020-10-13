b12b66d538943754d9484852c7c516652c02e684
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-10-12 21:09:13 -0500

    exposes some more GPU related functions in petsc4py
    - defined bindToCPU()
    - defined create{CUDA|ViennaCL}WithArrays()
    - defined getCL{Context|Queue|Mem}Handle()
    - defined getOffloadMask()

---
+++ src/binding/petsc4py/src/include/compat/cuda.h
@@ -0,0 +1,18 @@
+#ifndef PETSC4PY_COMPAT_CUDA_H
+#define PETSC4PY_COMPAT_CUDA_H
+
+#if !defined(PETSC_HAVE_CUDA)
+
+#define PetscCUDAError do { \
+    PetscFunctionBegin; \
+    SETERRQ1(PETSC_COMM_SELF,PETSC_ERR_SUP,"%s() requires CUDA",PETSC_FUNCTION_NAME); \
+    PetscFunctionReturn(PETSC_ERR_SUP);} while (0)
+
+PetscErrorCode  VecCreateSeqCUDAWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,const PetscScalar cpuarray[],const PetscScalar gpuarray[],Vec *V) {PetscCUDAError;}
+PetscErrorCode VecCreateMPICUDAWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,PetscInt N,const PetscScalar cpuarray[],const PetscScalar gpuarray[],Vec *vv) {PetscCUDAError;}
+
+#undef PetscCUDAError
+
+#endif
+
+#endif/*PETSC4PY_COMPAT_CUDA_H*/
