103d0f9dcf4bff221b70e26d2a9c9e38063ec3e4
Pierre Jolivet <pierre@joliv.et>
2021-02-03 08:37:14 +0100

    Bump HPDDM

--- config/BuildSystem/config/packages/hpddm.py
+++ config/BuildSystem/config/packages/hpddm.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self,framework):
     config.package.Package.__init__(self,framework)
-    self.gitcommit              = '00cf77c73f63e2fa6527613fd6f0769845b283af' # master jan-20-2021
+    self.gitcommit              = 'b0d709f9a09d0a9fd47d996fd120bca504501623' # master feb-22-2021
     self.download               = ['git://https://github.com/hpddm/hpddm','https://github.com/hpddm/hpddm/archive/'+self.gitcommit+'.tar.gz']
     self.minversion             = '2.0.8'
     self.versionname            = 'HPDDM_VERSION'
