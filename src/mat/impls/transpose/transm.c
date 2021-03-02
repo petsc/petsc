ff83db7b6187e6e76e718e6806a94af28628dfd2
Pierre Jolivet <pierre@joliv.et>
2021-03-02 22:04:46 +0100

    Fix [h]transm.c

--- src/mat/impls/transpose/transm.c
+++ src/mat/impls/transpose/transm.c
@@ -236,8 +236,13 @@ PetscErrorCode MatConvert_Transpose(Mat A,MatType newtype,MatReuse reuse,Mat *ne
 
   PetscFunctionBegin;
   ierr = MatTranspose(Aa->A,MAT_INITIAL_MATRIX,&B);CHKERRQ(ierr);
-  ierr = MatConvert(B,newtype,reuse,newmat);CHKERRQ(ierr);
-  ierr = MatDestroy(&B);CHKERRQ(ierr);
+  if (reuse != MAT_INPLACE_MATRIX) {
+    ierr = MatConvert(B,newtype,reuse,newmat);CHKERRQ(ierr);
+    ierr = MatDestroy(&B);CHKERRQ(ierr);
+  } else {
+    ierr = MatConvert(B,newtype,MAT_INPLACE_MATRIX,&B);CHKERRQ(ierr);
+    ierr = MatHeaderReplace(A,&B);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
 
