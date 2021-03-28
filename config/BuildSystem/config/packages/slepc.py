708ec4202554215c6381d9306484b3e36e09bd7f
Satish Balay <balay@mcs.anl.gov>
2021-03-28 12:56:24 -0500

    slepc: update to latest snapshot

--- config/BuildSystem/config/packages/slepc.py
+++ config/BuildSystem/config/packages/slepc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = 'bc797919109d857066e74cb7061a882f1565a372' # main mar-22-2021
+    self.gitcommit              = '36355849444d1e7f855f863bb249f6241dde0898' # main mar-30-2021
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
