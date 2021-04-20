9e2fe13843cc1957dbe26e9163e43e9766df8148
Toby Isaac <tisaac@cc.gatech.edu>
2021-04-20 16:42:04 -0400

    Add PetscVFPrintf_PythonStd, PETSc._push_python_vfprintf() and PETSc._pop_python_vfprintf()
    Added test of redirected python stdout
    
    Move test into petsc4pytest and use push/pop to avoid losing info about past vfprintf functions
    
    Use file pointer in cython thanks to Lisandro's guidance
    
    Apply 2 suggestion(s) to 1 file(s)
    
    Apply 1 suggestion(s) to 1 file(s)
    
    Add stderr control to python vfprintf, rename PETSc._push_python_stdout() to PETSc._push_python_vfprintf(), and add assertions for good state
    
    Undo vfprintf stack in finalize and handle -error_output_stdout
    
    Strip trailing nul bytes out of strings before decoding

---
+++ src/binding/petsc4py/test/test_stdout.py
@@ -0,0 +1,57 @@
+import unittest
+
+# --------------------------------------------------------------------
+
+
+class TestStdout(unittest.TestCase):
+    def testStdoutRedirect(self):
+        from io import StringIO
+        import sys
+        prevstdout = sys.stdout
+        prevstderr = sys.stderr
+        sys.stdout = StringIO()
+        sys.stderr = StringIO()
+
+        import numpy as np
+        from petsc4py import PETSc
+
+        if not (__name__ == '__main__'):
+            PETSc._push_python_vfprintf()
+
+        a = np.array([0.,0.,0.])
+        a_vec = PETSc.Vec().createWithArray(a,comm=PETSc.COMM_SELF)
+        a_vec.view()
+        v = PETSc.Viewer.STDERR(PETSc.COMM_SELF)
+        v.printfASCII("Error message")
+
+        newstdout = sys.stdout
+        newstderr = sys.stderr
+        sys.stdout = prevstdout
+        sys.stderr = prevstderr
+
+        output = newstdout.getvalue()
+        error = newstderr.getvalue()
+        if not (__name__ == '__main__'):
+            PETSc._pop_python_vfprintf()
+        stdoutshouldbe = \
+"""Vec Object: 1 MPI processes
+  type: seq
+0.
+0.
+0.
+"""
+        stderrshouldbe = "Error message"
+        if PETSc._stdout_is_stderr():
+            stdoutshouldbe = stdoutshouldbe + stderrshouldbe
+            stderrshouldbe = ""
+        self.assertEqual(output,stdoutshouldbe)
+        self.assertEqual(error,stderrshouldbe)
+
+
+
+# --------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
+
+# --------------------------------------------------------------------
