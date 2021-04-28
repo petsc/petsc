4add49896cb01c4074556f9bfc4a2c4641e76e1f
Junchao Zhang <jczhang@mcs.anl.gov>
2021-04-27 22:43:42 -0500

    Packages: upgrade Kokkos to avoid a bug
    Kokkos nvcc_wrapper wrongly parsed `-gencode arch=compute_75,code=sm_75`. The new commit fixed this issue.

--- config/BuildSystem/config/packages/kokkos-kernels.py
+++ config/BuildSystem/config/packages/kokkos-kernels.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = '3.2.00'
+    self.gitcommit        = '54e4213ca028163a76639d23b1204b213203bc79' # origin/develop of 2021-04-28
     self.versionname      = 'KOKKOS_KERNELS_VERSION'  # It looks kokkos-kernels does not yet have a macro for version number
     self.download         = ['git://https://github.com/kokkos/kokkos-kernels.git']
     #TODO: See comments in kokkos.py
