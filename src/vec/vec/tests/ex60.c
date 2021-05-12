Jnydhruva/CSE548

Vec ex60: added tests for VecReciprocal

src/vec/vec/tests/ex60.c
@@ -1,4 +1,4 @@
-static char help[] = "Tests VecPlaceArray().\n\n";
+static char help[] = "Tests VecPlaceArray() and VecReciprocal().\n\n";
 
 #include <petscvec.h>
 
@@ -6,44 +6,59 @@ int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
   PetscInt       n=5,bs;
-  PetscBool      cuda;
-  Vec            x,x1,x2;
+  PetscBool      iscuda,iskokkos;
+  Vec            x,x1,x2,x3;
   PetscScalar    *px;
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
 
   /* create vector of length 2*n */
   ierr = VecCreate(PETSC_COMM_SELF,&x);CHKERRQ(ierr);
-  ierr = VecSetSizes(x,2*n,PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = VecSetSizes(x,3*n,PETSC_DECIDE);CHKERRQ(ierr);
   ierr = VecSetFromOptions(x);CHKERRQ(ierr);
 
   /* create two vectors of length n without array */
-  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQCUDA,&cuda);CHKERRQ(ierr);
+  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQCUDA,&iscuda);CHKERRQ(ierr);
+  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQKOKKOS,&iskokkos);CHKERRQ(ierr);
   ierr = VecGetBlockSize(x,&bs);CHKERRQ(ierr);
-  if (cuda) {
+  if (iscuda) {
 #if defined(PETSC_HAVE_CUDA)
     ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
     ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
+#endif
+  } else if (iskokkos) {
+#if defined(PETSC_HAVE_KOKKOS)
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
 #endif
   } else {
     ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
     ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
   }
 
   ierr = VecGetArrayWrite(x,&px);CHKERRQ(ierr);
   ierr = VecPlaceArray(x1,px);CHKERRQ(ierr);
   ierr = VecPlaceArray(x2,px+n);CHKERRQ(ierr);
+  ierr = VecPlaceArray(x3,px+2*n);CHKERRQ(ierr);
   ierr = VecSet(x1,1.0);CHKERRQ(ierr);
-  ierr = VecSet(x2,2.0);CHKERRQ(ierr);
+  ierr = VecSet(x2,0.0);CHKERRQ(ierr);
+  ierr = VecSet(x3,2.0);CHKERRQ(ierr);
   ierr = VecResetArray(x1);CHKERRQ(ierr);
   ierr = VecResetArray(x2);CHKERRQ(ierr);
+  ierr = VecResetArray(x3);CHKERRQ(ierr);
   ierr = VecRestoreArrayWrite(x,&px);CHKERRQ(ierr);
+  ierr = VecDestroy(&x1);CHKERRQ(ierr);
+  ierr = VecDestroy(&x2);CHKERRQ(ierr);
+  ierr = VecDestroy(&x3);CHKERRQ(ierr);
 
+  ierr = VecView(x,NULL);CHKERRQ(ierr);
+  ierr = VecReciprocal(x);CHKERRQ(ierr);
   ierr = VecView(x,NULL);CHKERRQ(ierr);
 
   ierr = VecDestroy(&x);CHKERRQ(ierr);
-  ierr = VecDestroy(&x1);CHKERRQ(ierr);
-  ierr = VecDestroy(&x2);CHKERRQ(ierr);
 
   ierr = PetscFinalize();
   return ierr;
@@ -53,6 +68,7 @@ int main(int argc,char **argv)
 
      testset:
        output_file: output/ex60_1.out
+       diff_args: -j
        test:
          suffix: 1
        test:
@@ -60,5 +76,11 @@ int main(int argc,char **argv)
          args: -vec_type cuda
          filter: sed -e 's/seqcuda/seq/'
          requires: cuda
+       test:
+         TODO: broken
+         suffix: 1_kokkos
+         args: -vec_type kokkos
+         filter: sed -e 's/seqkokkos/seq/'
+         requires: kokkos_kernels
 
 TEST*/

petsc/petsc

src/vec/vec/tests/ex60.c
@@ -1,4 +1,4 @@
-static char help[] = "Tests VecPlaceArray().\n\n";
+static char help[] = "Tests VecPlaceArray() and VecReciprocal().\n\n";
 
 #include <petscvec.h>
 
@@ -6,44 +6,59 @@ int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
   PetscInt       n=5,bs;
-  PetscBool      cuda;
-  Vec            x,x1,x2;
+  PetscBool      iscuda,iskokkos;
+  Vec            x,x1,x2,x3;
   PetscScalar    *px;
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
 
   /* create vector of length 2*n */
   ierr = VecCreate(PETSC_COMM_SELF,&x);CHKERRQ(ierr);
-  ierr = VecSetSizes(x,2*n,PETSC_DECIDE);CHKERRQ(ierr);
+  ierr = VecSetSizes(x,3*n,PETSC_DECIDE);CHKERRQ(ierr);
   ierr = VecSetFromOptions(x);CHKERRQ(ierr);
 
   /* create two vectors of length n without array */
-  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQCUDA,&cuda);CHKERRQ(ierr);
+  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQCUDA,&iscuda);CHKERRQ(ierr);
+  ierr = PetscObjectTypeCompare((PetscObject)x,VECSEQKOKKOS,&iskokkos);CHKERRQ(ierr);
   ierr = VecGetBlockSize(x,&bs);CHKERRQ(ierr);
-  if (cuda) {
+  if (iscuda) {
 #if defined(PETSC_HAVE_CUDA)
     ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
     ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqCUDAWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
+#endif
+  } else if (iskokkos) {
+#if defined(PETSC_HAVE_KOKKOS_KERNELS)
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqKokkosWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
 #endif
   } else {
     ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x1);CHKERRQ(ierr);
     ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x2);CHKERRQ(ierr);
+    ierr = VecCreateSeqWithArray(PETSC_COMM_SELF,bs,n,NULL,&x3);CHKERRQ(ierr);
   }
 
   ierr = VecGetArrayWrite(x,&px);CHKERRQ(ierr);
   ierr = VecPlaceArray(x1,px);CHKERRQ(ierr);
   ierr = VecPlaceArray(x2,px+n);CHKERRQ(ierr);
+  ierr = VecPlaceArray(x3,px+2*n);CHKERRQ(ierr);
   ierr = VecSet(x1,1.0);CHKERRQ(ierr);
-  ierr = VecSet(x2,2.0);CHKERRQ(ierr);
+  ierr = VecSet(x2,0.0);CHKERRQ(ierr);
+  ierr = VecSet(x3,2.0);CHKERRQ(ierr);
   ierr = VecResetArray(x1);CHKERRQ(ierr);
   ierr = VecResetArray(x2);CHKERRQ(ierr);
+  ierr = VecResetArray(x3);CHKERRQ(ierr);
   ierr = VecRestoreArrayWrite(x,&px);CHKERRQ(ierr);
+  ierr = VecDestroy(&x1);CHKERRQ(ierr);
+  ierr = VecDestroy(&x2);CHKERRQ(ierr);
+  ierr = VecDestroy(&x3);CHKERRQ(ierr);
 
+  ierr = VecView(x,NULL);CHKERRQ(ierr);
+  ierr = VecReciprocal(x);CHKERRQ(ierr);
   ierr = VecView(x,NULL);CHKERRQ(ierr);
 
   ierr = VecDestroy(&x);CHKERRQ(ierr);
-  ierr = VecDestroy(&x1);CHKERRQ(ierr);
-  ierr = VecDestroy(&x2);CHKERRQ(ierr);
 
   ierr = PetscFinalize();
   return ierr;
@@ -53,6 +68,7 @@ int main(int argc,char **argv)
 
      testset:
        output_file: output/ex60_1.out
+       diff_args: -j
        test:
          suffix: 1
        test:
@@ -60,5 +76,11 @@ int main(int argc,char **argv)
          args: -vec_type cuda
          filter: sed -e 's/seqcuda/seq/'
          requires: cuda
+       test:
+         TODO: broken
+         suffix: 1_kokkos
+         args: -vec_type kokkos
+         filter: sed -e 's/seqkokkos/seq/'
+         requires: kokkos_kernels
 
 TEST*/