23eca7e2fb63a6c96a6a4a85652940f2224d9a59
Stefano Zampini <stefano.zampini@gmail.com>
2021-05-13 13:48:59 +0300

    Sys ex54: fix egrep pattern for machines including the fullpath in the program name

--- src/sys/tests/ex54.c
+++ src/sys/tests/ex54.c
@@ -26,7 +26,8 @@ int main(int argc,char **argv)
         suffix: 1
         localrunfiles: ex54options ex54options_1a_wrong ex54options_1b_wrong ex54options_1c_wrong ex54options_1d_wrong ex54options_1e_wrong ex54options_1f_wrong ex54options_1g_wrong
         args: -options_file {{ex54options_1a_wrong ex54options_1b_wrong ex54options_1c_wrong ex54options_1d_wrong ex54options_1e_wrong ex54options_1f_wrong ex54options_1g_wrong}separate output}
-        filter: Error: egrep -v "(malloc|nox|display|saws_port|vecscatter|check_pointer_intensity|cuda_initialize|error_output_stdout|use_gpu_aware_mpi|checkstack)" | egrep "ex54options"
+        # Some machines use the fullpath in the program name, so filter with " ex54options" and not "ex54options"
+        filter: Error: egrep -v "(malloc|nox|display|saws_port|vecscatter|check_pointer_intensity|cuda_initialize|error_output_stdout|use_gpu_aware_mpi|checkstack)" | egrep " ex54options"
       test:
         suffix: 1_options_file-ex54options_1h
         localrunfiles: ex54options ex54options_1h
