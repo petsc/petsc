51664b9698931a1661dbc6765f37afa3b7644b4b
Satish Balay <balay@mcs.anl.gov>
2021-06-02 10:10:41 -0500

    gitcommit: follow-up to 3c43290cbaa09cbcaf486ab9c4f7393eb19a428c for changes in main

--- config/BuildSystem/config/packages/kokkos.py
+++ config/BuildSystem/config/packages/kokkos.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = 'd4ed86ffb1b156faaa0f936ce839cfd6d3282478' # origin/develop of 2021-04-28
+    self.gitcommit        = 'd4ed86ffb1b156faaa0f936ce839cfd6d3282478' # develop of 2021-04-28
     self.versionname      = 'KOKKOS_VERSION'
     self.download         = ['git://https://github.com/kokkos/kokkos.git']
     self.downloaddirnames = ['kokkos']
