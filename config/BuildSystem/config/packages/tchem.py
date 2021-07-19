bd438bd60ac81ba5f8268e9f3d90e37345858989
Jed Brown <jed@jedbrown.org>
2021-07-19 15:18:19 -0600

    config tchem: fix configure and install
    This previously tried to add a list to a string and was not
    sudo-aware for installation. Now we do the globbing in Python and always
    use lists instead of string concatenation.
    
    Reported-by: David Salac <davidsal@buffalo.edu>

--- config/BuildSystem/config/packages/tchem.py
+++ config/BuildSystem/config/packages/tchem.py
@@ -18,7 +18,7 @@ class Configure(config.package.Package):
     return
 
   def Install(self):
-    import os
+    import os, glob
 
     libDir         = os.path.join(self.installDir, 'lib')
     includeDir     = os.path.join(self.installDir, 'include')
@@ -26,19 +26,18 @@ class Configure(config.package.Package):
 
     args = []
     self.pushLanguage('C')
-    args.append('CC="'+self.getCompiler()+'"')
-    args.append('CFLAGS="'+self.updatePackageCFlags(self.getCompilerFlags())+'"')
+    args.append('CC='+self.getCompiler())
+    args.append('CFLAGS='+self.updatePackageCFlags(self.getCompilerFlags()))
     self.popLanguage()
     if hasattr(self.compilers, 'CXX'):
       self.pushLanguage('Cxx')
-      args.append('CXX="'+self.getCompiler()+'"')
-      args.append('CXXFLAGS="'+self.updatePackageCxxFlags(self.getCompilerFlags())+'"')
+      args.append('CXX='+self.getCompiler())
+      args.append('CXXFLAGS='+self.updatePackageCxxFlags(self.getCompilerFlags()))
       self.popLanguage()
-    args = ' '.join(args)
 
     conffile = os.path.join(self.packageDir, self.package)
     fd = open(conffile, 'w')
-    fd.write(args)
+    fd.write(' '.join(args))
     fd.close()
 
     if self.installNeeded(conffile):
@@ -49,8 +48,13 @@ class Configure(config.package.Package):
         raise RuntimeError('Error running configure on TChem: '+str(e))
       try:
         self.logPrintBox('Compiling TChem; this may take several minutes')
-        output2,err2,ret2  = config.package.Package.executeShellCommand('cd '+self.packageDir+' && make && cp include/TC_*.h %(includeDir)s && cp lib/libtchem* %(libDir)s' % dict(includeDir=includeDir,libDir=libDir), timeout=500, log = self.log)
-        output2,err2,ret2  = config.package.Package.executeShellCommand('cd '+self.packageDir+' && cp data/periodictable.dat  %(shareDir)s' % dict(shareDir=shareDir) , timeout=60, log = self.log)
+        output2,err2,ret2  = config.package.Package.executeShellCommand(['make'], cwd=self.packageDir, timeout=500, log = self.log)
+        output2,err2,ret2  = config.package.Package.executeShellCommandSeq([
+          self.withSudo('mkdir', '-p', includeDir, libDir, shareDir),
+          self.withSudo('cp', *(glob.glob(os.path.join(self.packageDir, 'include', 'TC_*')) + [includeDir])),
+          self.withSudo('cp', *(glob.glob(os.path.join(self.packageDir, 'lib', 'libtchem*')) + [libDir])),
+          self.withSudo('cp', os.path.join(self.packageDir, 'data', 'periodictable.dat'), shareDir),
+          ], timeout=500, log = self.log)
       except RuntimeError as e:
         raise RuntimeError('Error running make on TChem: '+str(e))
       self.postInstall(output1+err1+output2+err2,'tchem')
