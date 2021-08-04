dcc276dd77ac263b0ecaf95b5c894054dde35e1f
Pierre Jolivet <pierre@joliv.et>
2021-08-04 10:18:45 +0200

    snes_tutorials-ex20_2d_p1_gmg_vcycle_cr: broken

--- src/snes/tutorials/ex20.c
+++ src/snes/tutorials/ex20.c
@@ -198,6 +198,8 @@ int main(int argc, char **argv)
 
   test:
     suffix: 2d_p1_gmg_vcycle_cr
+    TODO: broken
+    # cannot MatShift() a MATNORMAL until this MatType inherits from MATSHELL, cf. https://gitlab.com/petsc/petsc/-/issues/972
     requires: triangle
     args: -potential_petscspace_degree 1 -dm_plex_box_faces 2,2 -dm_refine_hierarchy 3 \
           -ksp_rtol 5e-10 -pc_type mg  -pc_mg_adapt_cr \
