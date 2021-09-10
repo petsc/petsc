dd1b8ef0679c2612f7773531a90a65051bb44040
Satish Balay <balay@mcs.anl.gov>
2021-09-10 16:07:59 -0500

    doublelinecheck: fix incorrect processing. said: Found double blank line 11 src/dm/impls/forest/p4est/dmp8est.c when it should have said: Found double blank line 278 include/petscvec.h
    Reported-by: Richard Tran Mills <rtmills@anl.gov>

--- lib/petsc/bin/maint/doublelinecheck.py
+++ lib/petsc/bin/maint/doublelinecheck.py
@@ -1,13 +1,13 @@
 import sys
 
-found = 0
+prevf   = ''
 prevnum = -1
 fd = open(sys.argv[1])
 for a in fd.readlines():
-  f   = a[0:a.find(':')]
-  num = int(a[a.find(':')+1:-2])
-  if num == prevnum+1: found += 1
-  if found == 2:
+  aa = a.split(':')
+  f = aa[0]
+  num = int(aa[1])
+  if f == prevf and num == prevnum+1:
     print('Found double blank line '+str(num)+' '+f)
-    found = 0
+  prevf = f
   prevnum = num
