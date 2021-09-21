3b9338fae9ddcdbe28fd52f3508194391ca36a23
Pierre Jolivet <pierre@joliv.et>
2021-09-21 14:18:15 +0200

    MatHtool: update htool.py and new compressor

--- config/BuildSystem/config/packages/htool.py
+++ config/BuildSystem/config/packages/htool.py
@@ -3,9 +3,9 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self,framework):
     config.package.Package.__init__(self,framework)
-    self.gitcommit              = '9c004f24326c454eb34df5d155f145c7f902d573' # main aug-03-2021
+    self.gitcommit              = 'f0a1542a8c408e1da02478a9843bb12815a680ae' # main sep-21-2021
     self.download               = ['git://https://github.com/htool-ddm/htool','https://github.com/htool-ddm/htool/archive/'+self.gitcommit+'.tar.gz']
-    self.minversion             = '0.5.0'
+    self.minversion             = '0.7.0'
     self.versionname            = 'HTOOL_VERSION'
     self.versioninclude         = 'htool/misc/define.hpp'
     self.minCxxVersion          = 'c++11'
@@ -30,14 +30,19 @@ class Configure(config.package.Package):
     return
 
   def Install(self):
+    import shutil
     import os
     incDir = os.path.join(self.installDir,self.includedir)
     self.include = [incDir]
     if not hasattr(self.framework,'packages'):
       self.framework.packages = []
     self.framework.packages.append(self)
-    cpstr = 'mkdir -p '+incDir+' && cp -r '+os.path.join(self.packageDir,'include','*')+' '+incDir
-    self.logPrintBox('Copying Htool; this may take several seconds')
-    output,err,ret = config.package.Package.executeShellCommand(cpstr,timeout=100,log=self.log)
-    self.log.write(output+err)
+    srcdir = os.path.join(self.packageDir,'include','htool')
+    destdir = os.path.join(incDir,'htool')
+    try:
+      self.logPrintBox('Copying Htool; this may take several seconds')
+      if os.path.isdir(destdir): shutil.rmtree(destdir)
+      shutil.copytree(srcdir,destdir)
+    except RuntimeError as e:
+      raise RuntimeError('Error copying Htool: '+str(e))
     return self.installDir
