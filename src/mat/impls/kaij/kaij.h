1556b0b06104edf0d9cbe55f94e5044d75e1111b
Richard Tran Mills <rmills@rmills.org>
2021-09-21 14:44:19 -0700

    Update comments on Mat_MPIKAIJ struct to better explain differences between sequential and parallel KAIJ cases.

--- src/mat/impls/kaij/kaij.h
+++ src/mat/impls/kaij/kaij.h
@@ -21,8 +21,8 @@ typedef struct {
 
 typedef struct {
   KAIJHEADER
-  Mat              OAIJ;  /* sequential KAIJ matrix that corresponds to off-diagonal matrix entries */
-  Mat              A;
+  Mat              OAIJ;  /* sequential KAIJ matrix that corresponds to off-diagonal matrix entries (diagonal entries are stored in 'AIJ') */
+  Mat              A;     /* AIJ matrix describing the blockwise action of the KAIJ matrix; compare with struct member 'AIJ' in sequential case */
   VecScatter       ctx;   /* update ghost points for parallel case */
   Vec              w;     /* work space for ghost values for parallel case */
   PetscObjectState state; /* state of the matrix A when AIJ and OIJ were last updated */
