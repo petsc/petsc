2a994da64846481b58c070aff03aca49619c020d
Stefano Zampini <stefano.zampini@gmail.com>
2021-09-27 23:00:25 +0300

    H2OPUS: update branch

--- config/BuildSystem/config/packages/h2opus.py
+++ config/BuildSystem/config/packages/h2opus.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = 'dea033b'
+    self.gitcommit              = 'b30a056' # Sep 27 2021, fix macosx install
     self.download               = ['git://https://github.com/ecrc/h2opus']
     self.precisions             = ['single','double']
     self.skippackagewithoptions = 1
