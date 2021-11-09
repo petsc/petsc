d0d779802edd6e333eef4b05a2ff93ccbf659448
Junchao Zhang <jczhang@mcs.anl.gov>
2021-11-09 10:53:04 -0600

    sfhip: guard some atomics with HIP_VERSION

--- src/vec/is/sf/impls/basic/hip/sfhip.hip.cpp
+++ src/vec/is/sf/impls/basic/hip/sfhip.hip.cpp
@@ -174,7 +174,9 @@ template<typename Type> struct Maxloc {
 
   See Cuda version
 */
+#if PETSC_PKG_HIP_VERSION_LT(4,4,0)
 __device__ static double atomicExch(double* address,double val) {return __longlong_as_double(atomicExch((ullint*)address,__double_as_longlong(val)));}
+#endif
 
 __device__ static llint atomicExch(llint* address,llint val) {return (llint)(atomicExch((ullint*)address,(ullint)val));}
 
@@ -297,7 +299,7 @@ template<typename Type> struct AtomicMult {__device__ Type operator() (Type& x,T
 
   See CUDA version for comments.
  */
-
+#if PETSC_PKG_HIP_VERSION_LT(4,4,0)
 #if defined(PETSC_USE_REAL_DOUBLE)
 __device__ static double atomicMin(double* address, double val)
 {
@@ -342,6 +344,7 @@ __device__ static float atomicMax(float* address,float val)
   } while (assumed != old);
   return __int_as_float(old);
 }
+#endif
 #endif
 
 /* As of ROCm 3.10 llint atomicMin/Max(llint*, llint) is not supported */
