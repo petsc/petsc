62f8be82234c436a848ab245fef4462042c3d8c4
Jacob Faibussowitsch <jacob.fai@gmail.com>
2021-11-15 10:32:25 -0600

    update to upstream hpddm format fixes for new attribute format

--- config/BuildSystem/config/packages/hpddm.py
+++ config/BuildSystem/config/packages/hpddm.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self,framework):
     config.package.Package.__init__(self,framework)
-    self.gitcommit              = '5fd4ea2cec52b491874ccd3907b53f92f43e5093' # main nov-03-2021
+    self.gitcommit              = 'f365bcb5cd3b82169b5548156b79ff6845ef6201' # format nov-05-2021
     self.download               = ['git://https://github.com/hpddm/hpddm','https://github.com/hpddm/hpddm/archive/'+self.gitcommit+'.tar.gz']
     self.minversion             = '2.0.8'
     self.versionname            = 'HPDDM_VERSION'
