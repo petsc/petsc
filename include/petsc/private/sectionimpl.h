85daa821502cba01871fde9b7752cacb5e1c5e81
Jacob Faibussowitsch <jacob.fai@gmail.com>
2021-11-18 10:46:12 -0600

    Change to PetscInt_FMT in the vec headers

--- include/petsc/private/sectionimpl.h
+++ include/petsc/private/sectionimpl.h
@@ -84,4 +84,19 @@ PETSC_EXTERN PetscErrorCode ISIntersect_Caching_Internal(IS, IS, IS *);
 PETSC_INTERN PetscErrorCode PetscSectionView_HDF5_Internal(PetscSection, PetscViewer);
 PETSC_INTERN PetscErrorCode PetscSectionLoad_HDF5_Internal(PetscSection, PetscViewer);
 #endif
-#endif
+
+#if defined(PETSC_CLANG_STATIC_ANALYZER)
+#  define PetscSectionCheckValidField(x,y)
+#  define PetscSectionCheckValidFieldComponent(x,y)
+#else
+#  define PetscSectionCheckValid_(description,item,nitems) do {         \
+    if (PetscUnlikely(((item) < 0) || ((item) >= (nitems)))) SETERRQ2(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE,"Invalid " description " %" PetscInt_FMT "; not in [0, %" PetscInt_FMT ")", (item), (nitems)); \
+  } while (0)
+
+#  define PetscSectionCheckValidFieldComponent(comp,nfieldcomp) PetscSectionCheckValid_("section field component",comp,nfieldcomp)
+
+#  define PetscSectionCheckValidField(field,nfields)            PetscSectionCheckValid_("field number",field,nfields)
+
+#endif /* PETSC_CLANG_STATIC_ANALYZER */
+
+#endif /* PETSCSECTIONIMPL_H */
--- include/petscvec.h
+++ include/petscvec.h
@@ -622,7 +622,7 @@ PETSC_STATIC_INLINE PetscErrorCode VecSetErrorIfLocked(Vec x,PetscInt arg)
 
   PetscFunctionBegin;
   ierr = VecLockGet(x,&state);CHKERRQ(ierr);
-  if (state != 0) SETERRQ1(PETSC_COMM_SELF,PETSC_ERR_ARG_WRONGSTATE," Vec is already locked for read-only or read/write access, argument # %d",arg);
+  if (PetscUnlikely(state != 0)) SETERRQ1(PETSC_COMM_SELF,PETSC_ERR_ARG_WRONGSTATE," Vec is already locked for read-only or read/write access, argument # %" PetscInt_FMT,arg);
   PetscFunctionReturn(0);
 }
 /* The three are deprecated */
