21daafab1052c4a35ee7826c70456c985e128af9
Junchao Zhang <jczhang@mcs.anl.gov>
2022-01-26 16:23:25 -0500

    Packages: upgrade Kokkos and KK to their latest develop snapshot

--- config/BuildSystem/config/packages/kokkos-kernels.py
+++ config/BuildSystem/config/packages/kokkos-kernels.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = '6c786cd6900b977e7b3b19a1f88c0c433a49cbcc' # develop of 2021-12-20
+    self.gitcommit        = 'bcc505d321da561889285184a085220da8920515' # develop of 2022-01-25
     self.versionname      = 'KOKKOS_KERNELS_VERSION'  # It looks kokkos-kernels does not yet have a macro for version number
     self.download         = ['git://https://github.com/kokkos/kokkos-kernels.git']
     self.includes         = ['KokkosBlas.hpp','KokkosSparse_CrsMatrix.hpp']
