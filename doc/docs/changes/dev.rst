a0ef30120f17f79cac9d72ca3bf4d9cb164d13d7
David Wells <drwells@email.unc.edu>
2022-01-28 11:34:21 -0500

    Add a new initial guess routine, based on Fischer's work.

--- doc/docs/changes/dev.rst
+++ doc/docs/changes/dev.rst
@@ -120,6 +120,7 @@ Changes: Development
 - Outer most ``KSPSolve()`` will error if KSP_DIVERGED_ITS and ```KSPSetErrorIfNotConverged()`` is used
 - Add ``KSPQMRCGS`` to support qmrcgstab with right preconditioning
 - Add ``KSPGuessSetTolerance()``
+- Add a new model option to ``KSPGuessFischerSetModel()``
 
 .. rubric:: SNES:
 
--- src/ksp/ksp/guess/impls/fischer/fischer.c
+++ src/ksp/ksp/guess/impls/fischer/fischer.c
@@ -1,4 +1,5 @@
 #include <petsc/private/kspimpl.h> /*I "petscksp.h" I*/
+#include <petscblaslapack.h>
 
 typedef struct {
   PetscInt    method;   /* 1 or 2 */
@@ -7,9 +8,10 @@ typedef struct {
   PetscBool   monitor;
   PetscScalar *alpha;   /* */
   Vec         *xtilde;  /* Saved x vectors */
-  Vec         *btilde;  /* Saved b vectors, method 1 */
+  Vec         *btilde;  /* Saved b vectors, methods 1 and 3 */
   Vec         Ax;       /* method 2 */
   Vec         guess;
+  PetscScalar *corr;    /* correlation matrix in column-major format, method 3 */
   PetscReal   tol;      /* tolerance for determining rank, method 3 */
 } KSPGuessFischer;
 
@@ -39,6 +41,10 @@ static PetscErrorCode KSPGuessReset_Fischer(KSPGuess guess)
     ierr = VecDestroy(&itg->guess);CHKERRQ(ierr);
     ierr = VecDestroy(&itg->Ax);CHKERRQ(ierr);
   }
+  if(itg->corr) {
+    ierr = PetscMemzero(itg->corr,sizeof(*itg->corr)*itg->maxl*itg->maxl);CHKERRQ(ierr);
+  }
+
   PetscFunctionReturn(0);
 }
 
@@ -56,7 +62,7 @@ static PetscErrorCode KSPGuessSetUp_Fischer(KSPGuess guess)
     ierr = KSPCreateVecs(guess->ksp,itg->maxl,&itg->xtilde,0,NULL);CHKERRQ(ierr);
     ierr = PetscLogObjectParents(guess,itg->maxl,itg->xtilde);CHKERRQ(ierr);
   }
-  if (!itg->btilde && itg->method == 1) {
+  if (!itg->btilde && (itg->method == 1 || itg->method == 3)) {
     ierr = KSPCreateVecs(guess->ksp,itg->maxl,&itg->btilde,0,NULL);CHKERRQ(ierr);
     ierr = PetscLogObjectParents(guess,itg->maxl,itg->btilde);CHKERRQ(ierr);
   }
@@ -64,9 +70,13 @@ static PetscErrorCode KSPGuessSetUp_Fischer(KSPGuess guess)
     ierr = VecDuplicate(itg->xtilde[0],&itg->Ax);CHKERRQ(ierr);
     ierr = PetscLogObjectParent((PetscObject)guess,(PetscObject)itg->Ax);CHKERRQ(ierr);
   }
-  if (!itg->guess) {
+  if (!itg->guess && (itg->method == 1 || itg->method == 2)) {
     ierr = VecDuplicate(itg->xtilde[0],&itg->guess);CHKERRQ(ierr);
     ierr = PetscLogObjectParent((PetscObject)guess,(PetscObject)itg->guess);CHKERRQ(ierr);
+  }
+  if (!itg->corr && itg->method == 3) {
+    ierr = PetscCalloc1(itg->maxl*itg->maxl,&itg->corr);CHKERRQ(ierr);
+    ierr = PetscLogObjectMemory((PetscObject)guess,itg->maxl*itg->maxl*sizeof(PetscScalar));CHKERRQ(ierr);
   }
   PetscFunctionReturn(0);
 }
@@ -82,6 +92,7 @@ static PetscErrorCode KSPGuessDestroy_Fischer(KSPGuess guess)
   ierr = VecDestroyVecs(itg->maxl,&itg->xtilde);CHKERRQ(ierr);
   ierr = VecDestroy(&itg->guess);CHKERRQ(ierr);
   ierr = VecDestroy(&itg->Ax);CHKERRQ(ierr);
+  ierr = PetscFree(itg->corr);CHKERRQ(ierr);
   ierr = PetscFree(itg);CHKERRQ(ierr);
   ierr = PetscObjectComposeFunction((PetscObject)guess,"KSPGuessFischerSetModel_C",NULL);CHKERRQ(ierr);
   PetscFunctionReturn(0);
@@ -209,6 +220,152 @@ static PetscErrorCode KSPGuessUpdate_Fischer_2(KSPGuess guess, Vec b, Vec x)
   PetscFunctionReturn(0);
 }
 
+/*
+  Rather than the standard algorithm implemented in 2, we treat the provided x and b vectors to be spanning sets (not necessarily linearly independent) and use them to compute a windowed correlation matrix. Since the correlation matrix may be singular we solve it with the pseudoinverse, provided by SYEV/HEEV.
+*/
+static PetscErrorCode KSPGuessFormGuess_Fischer_3(KSPGuess guess, Vec b, Vec x)
+{
+  KSPGuessFischer *itg = (KSPGuessFischer*)guess->data;
+  PetscErrorCode  ierr;
+  PetscInt        i,j,m;
+  PetscReal       *s_values;
+  PetscScalar     *corr,*work,*b_coefs,*scratch_vec,zero=0.0,one=1.0;
+  PetscBLASInt    blas_m,blas_info,blas_rank=0,blas_lwork,blas_one = 1;
+#if defined(PETSC_USE_COMPLEX)
+  PetscReal       *rwork;
+#endif
+
+  /* project provided b onto space of stored btildes */
+  PetscFunctionBegin;
+  ierr = VecSet(x,0.0);CHKERRQ(ierr);
+  m = itg->curl;
+  if (m > 0) {
+    ierr = PetscBLASIntCast(m,&blas_m);CHKERRQ(ierr);
+    blas_lwork = (/* assume a block size of m */blas_m+2)*blas_m;
+#if defined(PETSC_USE_COMPLEX)
+    ierr = PetscCalloc6(m*m,&corr,m,&s_values,blas_lwork,&work,3*m-2,&rwork,m,&b_coefs,m,&scratch_vec);CHKERRQ(ierr);
+#else
+    ierr = PetscCalloc5(m*m,&corr,m,&s_values,blas_lwork,&work,m,&b_coefs,m,&scratch_vec);CHKERRQ(ierr);
+#endif
+    ierr = VecMDot(b,itg->curl,itg->btilde,b_coefs);CHKERRQ(ierr);
+    for (j=0;j<m;++j) {
+      for (i=0;i<m;++i) {
+        corr[m*j+i] = itg->corr[(itg->maxl)*j+i];
+      }
+    }
+    ierr = PetscFPTrapPush(PETSC_FP_TRAP_OFF);CHKERRQ(ierr);
+    PetscReal max_s_value = 0.0;
+#if defined(PETSC_USE_COMPLEX)
+    PetscStackCallBLAS("LAPACKheev", LAPACKheev_("V", "L", &blas_m, corr, &blas_m, s_values, work, &blas_lwork, rwork, &blas_info));
+#else
+    PetscStackCallBLAS(
+      "LAPACKsyev", LAPACKsyev_("V", "L", &blas_m, corr, &blas_m, s_values, work, &blas_lwork, &blas_info));
+#endif
+
+    if (blas_info == 0) {
+      /* make corr store singular vectors and s_values store singular values */
+      for (j=0; j<m; ++j) {
+        if (s_values[j] < 0.0) {
+          s_values[j] = PetscAbsReal(s_values[j]);
+          for (i=0; i<m; ++i) {
+            corr[m*j + i] *= -1.0;
+          }
+        }
+        max_s_value = PetscMax(max_s_value, s_values[j]);
+      }
+
+      /* manually apply the action of the pseudoinverse */
+      PetscStackCallBLAS(
+        "BLASgemv", BLASgemv_(
+          "T", &blas_m, &blas_m, &one, corr, &blas_m, b_coefs, &blas_one, &zero, scratch_vec, &blas_one));
+      for (j=0; j<m; ++j) {
+        if (s_values[j] > itg->tol*max_s_value) {
+          scratch_vec[j] /= s_values[j];
+          blas_rank += 1;
+        } else {
+          scratch_vec[j] = 0.0;
+        }
+      }
+      PetscStackCallBLAS("BLASgemv", BLASgemv_("N", &blas_m, &blas_m, &one, corr, &blas_m, scratch_vec, &blas_one, &zero, itg->alpha, &blas_one));
+
+    } else {
+      ierr = PetscInfo(guess, "Warning eigenvalue solver failed with error code %d - setting initial guess to zero\n", (int)blas_info);CHKERRQ(ierr);
+      ierr = PetscMemzero(itg->alpha,sizeof(*itg->alpha)*itg->maxl);CHKERRQ(ierr);
+    }
+    ierr = PetscFPTrapPop();CHKERRQ(ierr);
+
+    if (itg->monitor && blas_info == 0) {
+      ierr = PetscPrintf(((PetscObject)guess)->comm,"KSPFischerGuess correlation rank = %d\n",(int)blas_rank);CHKERRQ(ierr);
+      ierr = PetscPrintf(((PetscObject)guess)->comm,"KSPFischerGuess singular values = ");CHKERRQ(ierr);
+      for (i=0; i<itg->curl; i++) {
+        ierr = PetscPrintf(((PetscObject)guess)->comm," %g",(double)s_values[i]);CHKERRQ(ierr);
+      }
+      ierr = PetscPrintf(((PetscObject)guess)->comm,"\n");CHKERRQ(ierr);
+
+      ierr = PetscPrintf(((PetscObject)guess)->comm,"KSPFischerGuess alphas =");CHKERRQ(ierr);
+      for (i=0; i<itg->curl; i++) {
+        ierr = PetscPrintf(((PetscObject)guess)->comm," %g",(double)PetscAbsScalar(itg->alpha[i]));CHKERRQ(ierr);
+      }
+      ierr = PetscPrintf(((PetscObject)guess)->comm,"\n");CHKERRQ(ierr);
+    }
+    /* Form the initial guess by using b's projection coefficients with the xs */
+    ierr = VecMAXPY(x,itg->curl,itg->alpha,itg->xtilde);CHKERRQ(ierr);
+#if defined(PETSC_USE_COMPLEX)
+    ierr = PetscFree6(corr, s_values, work, rwork, b_coefs, scratch_vec);CHKERRQ(ierr);
+#else
+    ierr = PetscFree5(corr, s_values, work, b_coefs, scratch_vec);CHKERRQ(ierr);
+#endif
+  }
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode KSPGuessUpdate_Fischer_3(KSPGuess guess, Vec b, Vec x)
+{
+  KSPGuessFischer *itg = (KSPGuessFischer*)guess->data;
+  PetscErrorCode  ierr;
+  PetscInt        i,j;
+  Vec             oldest;
+
+  PetscFunctionBegin;
+  if (itg->curl == itg->maxl) {
+    /* we have the maximum number of vectors so rotate: oldest vector is at index 0 */
+    oldest = itg->xtilde[0];
+    for (i=1;i<itg->curl;++i) {
+      itg->xtilde[i-1] = itg->xtilde[i];
+    }
+    itg->xtilde[itg->curl-1] = oldest;
+    ierr = VecCopy(x,itg->xtilde[itg->curl-1]);CHKERRQ(ierr);
+
+    oldest = itg->btilde[0];
+    for (i=1;i<itg->curl;++i) {
+      itg->btilde[i-1] = itg->btilde[i];
+    }
+    itg->btilde[itg->curl-1] = oldest;
+    ierr = VecCopy(b,itg->btilde[itg->curl-1]);CHKERRQ(ierr);
+    /* shift correlation matrix up and left */
+    for (j=1; j<itg->maxl; ++j) {
+      for (i=1; i<itg->maxl; ++i) {
+        itg->corr[(j-1)*itg->maxl+i-1]=itg->corr[j*itg->maxl+i];
+      }
+    }
+  } else {
+    /* append new vectors */
+    ierr = VecCopy(x,itg->xtilde[itg->curl]);CHKERRQ(ierr);
+    ierr = VecCopy(b,itg->btilde[itg->curl]);CHKERRQ(ierr);
+    itg->curl++;
+  }
+
+  /* Populate new column of the correlation matrix and then copy it into the
+   * row. itg->maxl is the allocated length per column: itg->curl is the actual
+   * column length.
+   */
+  ierr = VecMDot(b,itg->curl,itg->btilde,itg->corr+(itg->curl-1)*itg->maxl);CHKERRQ(ierr);
+  for (i=0;i<itg->curl;++i) {
+    itg->corr[i*itg->maxl+itg->curl-1] = itg->corr[(itg->curl-1)*itg->maxl+i];
+  }
+  PetscFunctionReturn(0);
+}
+
 static PetscErrorCode KSPGuessSetFromOptions_Fischer(KSPGuess guess)
 {
   KSPGuessFischer *ITG = (KSPGuessFischer *)guess->data;
@@ -254,13 +411,13 @@ static PetscErrorCode KSPGuessView_Fischer(KSPGuess guess,PetscViewer viewer)
 }
 
 /*@
-   KSPGuessFischerSetModel - Use the Paul Fischer algorithm
+   KSPGuessFischerSetModel - Use the Paul Fischer algorithm or its variants
 
    Logically Collective on guess
 
    Input Parameters:
 +  guess - the initial guess context
-.  model - use model 1, model 2 or any other number to turn it off
+.  model - use model 1, model 2, model 3, or any other number to turn it off
 -  size  - size of subspace used to generate initial guess
 
     Options Database:
@@ -293,6 +450,9 @@ static PetscErrorCode KSPGuessFischerSetModel_Fischer(KSPGuess guess,PetscInt mo
   } else if (model == 2) {
     guess->ops->update    = KSPGuessUpdate_Fischer_2;
     guess->ops->formguess = KSPGuessFormGuess_Fischer_2;
+  } else if (model == 3) {
+    guess->ops->update    = KSPGuessUpdate_Fischer_3;
+    guess->ops->formguess = KSPGuessFormGuess_Fischer_3;
   } else {
     guess->ops->update    = NULL;
     guess->ops->formguess = NULL;
@@ -312,14 +472,14 @@ static PetscErrorCode KSPGuessFischerSetModel_Fischer(KSPGuess guess,PetscInt mo
 }
 
 /*
-    KSPGUESSFISCHER - Implements Paul Fischer's initial guess algorithm Method 1 and 2 for situations where
+    KSPGUESSFISCHER - Implements Paul Fischer's two initial guess algorithms and a nonorthogonalizing variant for situations where
     a linear system is solved repeatedly
 
   References:
 .   1. -   https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19940020363_1994020363.pdf
 
    Notes:
-    the algorithm is different from the paper because we do not CHANGE the right hand side of the new
+    the algorithm is different from Fischer's paper because we do not CHANGE the right hand side of the new
     problem and solve the problem with an initial guess of zero, rather we solve the original problem
     with a nonzero initial guess (this is done so that the linear solver convergence tests are based on
     the original RHS). We use the xtilde = x - xguess as the new direction so that it is not
@@ -330,10 +490,20 @@ static PetscErrorCode KSPGuessFischerSetModel_Fischer(KSPGuess guess,PetscInt mo
     KSPGetGuess(ksp,&guess);
     KSPGuessSetType(guess,KSPGUESSFISCHER);
     KSPGuessFischerSetModel(guess,model,basis);
+    KSPGuessSetTolerance(guess,PETSC_MACHINE_EPSILON);
+
+    The default tolerance (which is only used in Method 3) is 32*PETSC_MACHINE_EPSILON. This value was chosen
+    empirically by trying a range of tolerances and picking the one that lowered the solver iteration count the most
+    with five vectors.
 
     Method 2 is only for positive definite matrices, since it uses the A norm.
 
-    Developer note: the option -ksp_fischer_guess <int,int> is still available for backward compatibility
+    Method 3 is not in the original paper. It is the same as the first two methods except that it
+    does not orthogonalize the input vectors or use A at all. This choice is faster but provides a
+    less effective initial guess for large (about 10) numbers of stored vectors.
+
+    Developer note:
+      The option -ksp_fischer_guess <int,int> is still available for backward compatibility
 
     Level: intermediate
 
@@ -347,7 +517,7 @@ PetscErrorCode KSPGuessCreate_Fischer(KSPGuess guess)
   ierr = PetscNewLog(guess,&fischer);CHKERRQ(ierr);
   fischer->method = 1;  /* defaults to method 1 */
   fischer->maxl   = 10;
-  fischer->tol    = PETSC_MACHINE_EPSILON;
+  fischer->tol    = 32.0*PETSC_MACHINE_EPSILON;
   guess->data     = fischer;
 
   guess->ops->setfromoptions = KSPGuessSetFromOptions_Fischer;
