d751fb92f56777f4a566f5e5fedf76f657538c08
Stefano Zampini <stefano.zampini@gmail.com>
2022-01-30 15:44:36 +0300

    MATLRC: add support for GPUs and transpose operation

--- src/mat/tests/ex102.c
+++ src/mat/tests/ex102.c
@@ -12,79 +12,56 @@ T*/
 int main(int argc,char **args)
 {
   Vec            x,b,c=NULL;
-  Mat            A,U,V,LR;
-  PetscInt       i,j,Ii,J,Istart,Iend,m = 8,n = 7,rstart,rend;
+  Mat            A,U,V,LR,X,LRe;
+  PetscInt       M = 5, N = 7;
   PetscErrorCode ierr;
   PetscBool      flg;
-  PetscScalar    *u,a;
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
-  ierr = PetscOptionsGetInt(NULL,NULL,"-m",&m,NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(NULL,NULL,"-m",&M,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&N,NULL);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Create the sparse matrix
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
-  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,m*n,m*n);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,M,N);CHKERRQ(ierr);
+  ierr = MatSetOptionsPrefix(A,"A_");CHKERRQ(ierr);
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatSeqAIJSetPreallocation(A,5,NULL);CHKERRQ(ierr);
+  ierr = MatMPIAIJSetPreallocation(A,5,NULL,5,NULL);CHKERRQ(ierr);
   ierr = MatSetUp(A);CHKERRQ(ierr);
-  ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
-  for (Ii=Istart; Ii<Iend; Ii++) {
-    a = -1.0; i = Ii/n; j = Ii - i*n;
-    if (i>0)   {J = Ii - n; ierr = MatSetValues(A,1,&Ii,1,&J,&a,INSERT_VALUES);CHKERRQ(ierr);}
-    if (i<m-1) {J = Ii + n; ierr = MatSetValues(A,1,&Ii,1,&J,&a,INSERT_VALUES);CHKERRQ(ierr);}
-    if (j>0)   {J = Ii - 1; ierr = MatSetValues(A,1,&Ii,1,&J,&a,INSERT_VALUES);CHKERRQ(ierr);}
-    if (j<n-1) {J = Ii + 1; ierr = MatSetValues(A,1,&Ii,1,&J,&a,INSERT_VALUES);CHKERRQ(ierr);}
-    a = 4.0; ierr = MatSetValues(A,1,&Ii,1,&Ii,&a,INSERT_VALUES);CHKERRQ(ierr);
-  }
-  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatSetRandom(A,NULL);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Create the dense matrices
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = MatCreate(PETSC_COMM_WORLD,&U);CHKERRQ(ierr);
-  ierr = MatSetSizes(U,PETSC_DECIDE,PETSC_DECIDE,m*n,3);CHKERRQ(ierr);
+  ierr = MatSetSizes(U,PETSC_DECIDE,PETSC_DECIDE,M,3);CHKERRQ(ierr);
   ierr = MatSetType(U,MATDENSE);CHKERRQ(ierr);
+  ierr = MatSetOptionsPrefix(U,"U_");CHKERRQ(ierr);
+  ierr = MatSetFromOptions(U);CHKERRQ(ierr);
   ierr = MatSetUp(U);CHKERRQ(ierr);
-  ierr = MatGetOwnershipRange(U,&rstart,&rend);CHKERRQ(ierr);
-  ierr = MatDenseGetArray(U,&u);CHKERRQ(ierr);
-  for (i=rstart; i<rend; i++) {
-    u[i-rstart]          = (PetscReal)i;
-    u[i+rend-2*rstart]   = (PetscReal)1000*i;
-    u[i+2*rend-3*rstart] = (PetscReal)100000*i;
-  }
-  ierr = MatDenseRestoreArray(U,&u);CHKERRQ(ierr);
-  ierr = MatAssemblyBegin(U,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(U,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatSetRandom(U,NULL);CHKERRQ(ierr);
 
   ierr = MatCreate(PETSC_COMM_WORLD,&V);CHKERRQ(ierr);
-  ierr = MatSetSizes(V,PETSC_DECIDE,PETSC_DECIDE,m*n,3);CHKERRQ(ierr);
+  ierr = MatSetSizes(V,PETSC_DECIDE,PETSC_DECIDE,N,3);CHKERRQ(ierr);
   ierr = MatSetType(V,MATDENSE);CHKERRQ(ierr);
+  ierr = MatSetOptionsPrefix(V,"V_");CHKERRQ(ierr);
+  ierr = MatSetFromOptions(V);CHKERRQ(ierr);
   ierr = MatSetUp(V);CHKERRQ(ierr);
-  ierr = MatGetOwnershipRange(U,&rstart,&rend);CHKERRQ(ierr);
-  ierr = MatDenseGetArray(V,&u);CHKERRQ(ierr);
-  for (i=rstart; i<rend; i++) {
-    u[i-rstart]          = (PetscReal)i;
-    u[i+rend-2*rstart]   = (PetscReal)1.2*i;
-    u[i+2*rend-3*rstart] = (PetscReal)1.67*i+2;
-  }
-  ierr = MatDenseRestoreArray(V,&u);CHKERRQ(ierr);
-  ierr = MatAssemblyBegin(V,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(V,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatSetRandom(V,NULL);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Create a vector to hold the diagonal of C
+         A sequential vector can be created as well on each process
+         It is user responsibility to ensure the data in the vector
+         is consistent across processors
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = PetscOptionsHasName(NULL,NULL,"-use_c",&flg);CHKERRQ(ierr);
   if (flg) {
-    ierr = VecCreateSeq(PETSC_COMM_SELF,3,&c);CHKERRQ(ierr);
-    ierr = VecGetArray(c,&u);CHKERRQ(ierr);
-    u[0] = 2.0;
-    u[1] = -1.0;
-    u[2] = 1.0;
-    ierr = VecRestoreArray(c,&u);CHKERRQ(ierr);
+    ierr = VecCreateMPI(PETSC_COMM_WORLD,PETSC_DECIDE,3,&c);CHKERRQ(ierr);
+    ierr = VecSetRandom(c,NULL);CHKERRQ(ierr);
   }
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -94,33 +71,45 @@ int main(int argc,char **args)
   if (flg) {
     /* create a low-rank matrix, with no A-matrix */
     ierr = MatCreateLRC(NULL,U,c,V,&LR);CHKERRQ(ierr);
+    ierr = MatDestroy(&A);CHKERRQ(ierr);
   } else {
     ierr = MatCreateLRC(A,U,c,V,&LR);CHKERRQ(ierr);
   }
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         Create the low rank correction matrix explicitly to check for
+         correctness
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = MatHermitianTranspose(V,MAT_INITIAL_MATRIX,&X);CHKERRQ(ierr);
+  ierr = MatDiagonalScale(X,c,NULL);CHKERRQ(ierr);
+  ierr = MatMatMult(U,X,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&LRe);CHKERRQ(ierr);
+  ierr = MatDestroy(&X);CHKERRQ(ierr);
+  if (A) {
+    ierr = MatAYPX(LRe,1.0,A,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Create test vectors
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);
-  ierr = VecSetSizes(x,PETSC_DECIDE,m*n);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(x);CHKERRQ(ierr);
-  ierr = VecDuplicate(x,&b);CHKERRQ(ierr);
-  ierr = VecGetOwnershipRange(x,&rstart,&rend);CHKERRQ(ierr);
-  ierr = VecGetArray(x,&u);CHKERRQ(ierr);
-  for (i=rstart; i<rend; i++) u[i-rstart] = (PetscScalar)i;
-  ierr = VecRestoreArray(x,&u);CHKERRQ(ierr);
-
+  ierr = MatCreateVecs(LR,&x,&b);CHKERRQ(ierr);
+  ierr = VecSetRandom(x,NULL);CHKERRQ(ierr);
   ierr = MatMult(LR,x,b);CHKERRQ(ierr);
-  /*
-     View the product if desired
-  */
-  ierr = PetscOptionsHasName(NULL,NULL,"-view_product",&flg);CHKERRQ(ierr);
-  if (flg) {ierr = VecView(b,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);}
-
+  ierr = MatMultTranspose(LR,b,x);CHKERRQ(ierr);
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&b);CHKERRQ(ierr);
-  /* you can destroy the matrices in any order you like */
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         Check correctness
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = MatMultEqual(LR,LRe,10,&flg);CHKERRQ(ierr);
+  if (!flg) { ierr = PetscPrintf(PETSC_COMM_WORLD,"Error in MatMult\n");CHKERRQ(ierr); }
+#if !defined(PETSC_USE_COMPLEX)
+  ierr = MatMultHermitianTransposeEqual(LR,LRe,10,&flg);CHKERRQ(ierr);
+  if (!flg) { ierr = PetscPrintf(PETSC_COMM_WORLD,"Error in MatMultTranspose\n");CHKERRQ(ierr); }
+#endif
+
   ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = MatDestroy(&LRe);CHKERRQ(ierr);
   ierr = MatDestroy(&U);CHKERRQ(ierr);
   ierr = MatDestroy(&V);CHKERRQ(ierr);
   ierr = VecDestroy(&c);CHKERRQ(ierr);
@@ -138,19 +127,15 @@ int main(int argc,char **args)
 
 /*TEST
 
-   test:
-      suffix: 1
-      nsize: 2
-      args: -view_product
-
-   test:
-      suffix: 2
-      nsize: 2
-      args: -low_rank -view_product
-
-   test:
-      suffix: 3
-      nsize: 2
-      args: -use_c -view_product
+   testset:
+      output_file: output/ex102_1.out
+      nsize: {{1 2}}
+      args: -low_rank {{0 1}} -use_c {{0 1}}
+      test:
+        suffix: standard
+      test:
+        suffix: cuda
+        requires: cuda
+        args: -A_mat_type aijcusparse -U_mat_type densecuda -V_mat_type densecuda
 
 TEST*/
