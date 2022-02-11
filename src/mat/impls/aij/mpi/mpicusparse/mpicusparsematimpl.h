219fbbafcab96f1b7f27d5f571f31c0ed2202c25
Junchao Zhang <jczhang@mcs.anl.gov>
2022-02-11 14:41:46 -0600

    Mat: support extended COO with mataijcusparse

--- src/mat/impls/aij/mpi/mpicusparse/mpicusparsematimpl.h
+++ src/mat/impls/aij/mpi/mpicusparse/mpicusparsematimpl.h
@@ -4,7 +4,7 @@
 #include <cusparse_v2.h>
 #include <petsc/private/cudavecimpl.h>
 
-typedef struct {
+struct Mat_MPIAIJCUSPARSE {
   /* The following are used by GPU capabilities to store matrix storage formats on the device */
   MatCUSPARSEStorageFormat   diagGPUMatFormat;
   MatCUSPARSEStorageFormat   offdiagGPUMatFormat;
@@ -14,7 +14,26 @@ typedef struct {
   PetscInt                   coo_nd,coo_no; /* number of nonzero entries in coo for the diag/offdiag part */
   THRUSTINTARRAY             *coo_p; /* the permutation array that partitions the coo array into diag/offdiag parts */
   THRUSTARRAY                *coo_pw; /* the work array that stores the partitioned coo scalar values */
-} Mat_MPIAIJCUSPARSE;
+
+  /* Extended COO stuff */
+  PetscCount  *Aimap1_d,*Ajmap1_d,*Aperm1_d; /* Local entries to diag */
+  PetscCount  *Bimap1_d,*Bjmap1_d,*Bperm1_d; /* Local entries to offdiag */
+  PetscCount  *Aimap2_d,*Ajmap2_d,*Aperm2_d; /* Remote entries to diag */
+  PetscCount  *Bimap2_d,*Bjmap2_d,*Bperm2_d; /* Remote entries to offdiag */
+  PetscCount  *Cperm1_d; /* Permutation to fill send buffer. 'C' for communication */
+  PetscScalar *sendbuf_d,*recvbuf_d; /* Buffers for remote values in MatSetValuesCOO() */
+  PetscBool   use_extended_coo;
+
+  Mat_MPIAIJCUSPARSE() {
+    diagGPUMatFormat    = MAT_CUSPARSE_CSR;
+    offdiagGPUMatFormat = MAT_CUSPARSE_CSR;
+    coo_p               = NULL;
+    coo_pw              = NULL;
+    stream              = 0;
+    deviceMat           = NULL;
+    use_extended_coo    = PETSC_FALSE;
+  }
+};
 
 PETSC_INTERN PetscErrorCode MatCUSPARSESetStream(Mat, const cudaStream_t stream);
 PETSC_INTERN PetscErrorCode MatCUSPARSESetHandle(Mat, const cusparseHandle_t handle);
