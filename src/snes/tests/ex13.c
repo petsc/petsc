2e776fa087343bfb9b2acf147d05dcaf10b8fee9
Mark Adams <524115-markadams4@users.noreply.gitlab.com>
2022-03-26 14:29:07 -0400

    fix format and update ex13

--- src/snes/tests/ex13.c
+++ src/snes/tests/ex13.c
@@ -65,7 +65,7 @@ static void f0_strong_u(PetscInt dim, PetscInt Nf, PetscInt NfAux,
 
 static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
 {
-  PetscErrorCode ierr;
+   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
   options->nit    = 10;
@@ -123,7 +123,7 @@ static PetscErrorCode SetupDiscretization(DM dm, const char name[], PetscErrorCo
   PetscCall(DMGetDimension(dm, &dim));
   PetscCall(DMPlexGetHeightStratum(dm, 0, &cStart, NULL));
   PetscCall(DMPlexGetCellType(dm, cStart, &ct));
-  simplex = DMPolytopeTypeGetNumVertices(ct) == DMPolytopeTypeGetDim(ct)+1 ? PETSC_TRUE : PETSC_FALSE;
+  simplex = DMPolytopeTypeGetNumVertices(ct) == DMPolytopeTypeGetDim(ct)+1 ? PETSC_TRUE : PETSC_FALSE; // false
   /* Create finite element */
   PetscCall(PetscSNPrintf(prefix, PETSC_MAX_PATH_LEN, "%s_", name));
   PetscCall(PetscFECreateDefault(PETSC_COMM_SELF, dim, 1, simplex, name ? prefix : NULL, -1, &fe));
@@ -147,53 +147,44 @@ int main(int argc, char **argv)
   SNES           snes; /* Nonlinear solver */
   Vec            u;    /* Solutions */
   AppCtx         user; /* User-defined work context */
+  PetscLogDouble time;
+  Mat            Amat;
 
   PetscCall(PetscInitialize(&argc, &argv, NULL,help));
   PetscCall(ProcessOptions(PETSC_COMM_WORLD, &user));
-  /* Primal system */
+  /* system */
   PetscCall(SNESCreate(PETSC_COMM_WORLD, &snes));
   PetscCall(CreateMesh(PETSC_COMM_WORLD, &user, &dm));
   PetscCall(SNESSetDM(snes, dm));
   PetscCall(SetupDiscretization(dm, "potential", SetupPrimalProblem, &user));
   PetscCall(DMCreateGlobalVector(dm, &u));
-  PetscCall(VecSet(u, 0.0));
+  PetscCall(SNESSetFromOptions(snes));
   PetscCall(PetscObjectSetName((PetscObject) u, "potential"));
   PetscCall(DMPlexSetSNESLocalFEM(dm, &user, &user, &user));
-  PetscCall(SNESSetFromOptions(snes));
   PetscCall(DMSNESCheckFromOptions(snes, u));
+  PetscCall(PetscTime(&time));
+  PetscCall(SNESSetUp(snes));
+  PetscCall(SNESGetJacobian(snes, &Amat, NULL, NULL, NULL));
+  PetscCall(MatSetOption(Amat,MAT_SPD,PETSC_TRUE));
   PetscCall(SNESSolve(snes, NULL, u));
+  PetscCall(PetscTimeSubtract(&time));
+  // PetscCall(PetscPrintf(PETSC_COMM_WORLD,"First Solve time: %g\n",-time));
   /* Benchmark system */
   if (user.nit) {
+    Vec            b;
+    PetscInt       i;
 #if defined(PETSC_USE_LOG)
-    PetscLogStage kspstage,pcstage;
+    PetscLogStage  kspstage;
 #endif
-    KSP       ksp;
-    PC        pc;
-    Vec       b;
-    PetscInt  i;
-    PetscLogDouble time;
-    PetscCall(PetscOptionsClearValue(NULL,"-ksp_monitor"));
-    PetscCall(PetscOptionsClearValue(NULL,"-ksp_view"));
-    PetscCall(SNESGetKSP(snes, &ksp));
-    PetscCall(SNESGetSolution(snes, &u));
-    PetscCall(KSPSetFromOptions(ksp));
-    PetscCall(VecSet(u, 0.0));
-    PetscCall(SNESGetFunction(snes, &b, NULL, NULL));
-    PetscCall(KSPGetPC(ksp, &pc));
-    PetscCall(PetscLogStageRegister("PCSetUp", &pcstage));
-    PetscCall(PetscLogStagePush(pcstage));
-    PetscCall(PCSetUp(pc));
-    PetscCall(PetscLogStagePop());
-    PetscCall(PetscLogStageRegister("KSP Solve only", &kspstage));
-    PetscCall(PetscTime(&time));
+    PetscCall(PetscLogStageRegister("Solve only", &kspstage));
     PetscCall(PetscLogStagePush(kspstage));
+    PetscCall(SNESGetSolution(snes, &u));
+    PetscCall(SNESGetFunction(snes, &b, NULL, NULL));
     for (i=0;i<user.nit;i++) {
       PetscCall(VecZeroEntries(u));
-      PetscCall(KSPSolve(ksp, b, u));
+      PetscCall(SNESSolve(snes, NULL, u));
     }
     PetscCall(PetscLogStagePop());
-    PetscCall(PetscTimeSubtract(&time));
-    // ierr = PetscPrintf(PETSC_COMM_WORLD,"Solve time: %g\n",-time); // breaks CI
   }
   PetscCall(SNESGetSolution(snes, &u));
   PetscCall(VecViewFromOptions(u, NULL, "-potential_view"));
@@ -216,17 +207,46 @@ int main(int argc, char **argv)
   test:
     suffix: bench
     nsize: 4
-    args: -dm_plex_dim 3 -dm_plex_simplex 0 -dm_plex_box_faces 2,2,8 -dm_refine 1 \
-          -petscpartitioner_type simple -petscpartitioner_simple_process_grid 1,1,2 -petscpartitioner_simple_node_grid 1,1,2 \
-          -potential_petscspace_degree 2 -ksp_type cg -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -benchmark_it 1 -dm_view -snes_rtol 1.e-4
+    args: -dm_plex_dim 3 -dm_plex_simplex 0 -dm_plex_box_faces 2,2,1 -dm_refine 2 -dm_view -ksp_monitor \
+       -benchmark_it 1 \
+       -dm_plex_box_upper 2,2,1 \
+       -dm_plex_box_lower 0,0,0 \
+       -dm_plex_dim 3 \
+       -ksp_converged_reason \
+       -ksp_norm_type unpreconditioned \
+       -ksp_rtol 1.e-6 \
+       -ksp_type cg \
+       -mg_levels_ksp_chebyshev_esteig 0,0.2,0,1.1 \
+       -mg_levels_ksp_max_it 1 \
+       -mg_levels_ksp_type chebyshev \
+       -mg_levels_pc_type jacobi \
+       -pc_gamg_coarse_eq_limit 200 \
+       -pc_gamg_coarse_grid_layout_type compact \
+       -pc_gamg_esteig_ksp_max_it 5 \
+       -pc_gamg_process_eq_limit 200 \
+       -pc_gamg_repartition false \
+       -pc_gamg_reuse_interpolation true \
+       -pc_gamg_square_graph 0 \
+       -pc_gamg_threshold 0.001 -pc_gamg_threshold_scale .5\
+       -pc_gamg_type agg \
+       -pc_type gamg \
+       -petscpartitioner_simple_node_grid 1,2,1 \
+       -petscpartitioner_simple_process_grid 2,1,1 \
+       -petscpartitioner_type simple \
+       -potential_petscspace_degree 2 \
+       -snes_lag_jacobian -2 \
+       -snes_max_it 1 \
+       -snes_rtol 1.e-8 \
+       -snes_type ksponly \
+       -use_gpu_aware_mpi true
 
   test:
     suffix: comparison
     nsize: 4
     args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
       -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_monitor_short -snes_type ksponly -dm_view -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -snes_converged_reason -ksp_converged_reason -snes_rtol 1.e-4
+      -dm_plex_simplex 0 -snes_type ksponly -dm_view -ksp_type cg -ksp_norm_type unpreconditioned -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
+      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4
 
   test:
     suffix: cuda
@@ -235,8 +255,8 @@ int main(int argc, char **argv)
     output_file: output/ex13_comparison.out
     args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
       -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_monitor_short -snes_type ksponly -dm_view -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -snes_converged_reason -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijcusparse -dm_vec_type cuda
+      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
+      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijcusparse -dm_vec_type cuda
 
   test:
     suffix: kokkos_comp
@@ -245,16 +265,16 @@ int main(int argc, char **argv)
     output_file: output/ex13_comparison.out
     args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
       -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_monitor_short -snes_type ksponly -dm_view -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -snes_converged_reason -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijkokkos -dm_vec_type kokkos
+      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
+      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijkokkos -dm_vec_type kokkos
 
   test:
     nsize: 4
     requires: !sycl kokkos_kernels
     suffix: kokkos
     args: -dm_plex_dim 2 -dm_plex_box_faces 2,8 -petscpartitioner_type simple -petscpartitioner_simple_process_grid 2,1 \
-          -petscpartitioner_simple_node_grid 2,1 -dm_plex_simplex 0 -potential_petscspace_degree 1 -dm_refine 1 -ksp_type cg -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -ksp_norm_type unpreconditioned \
-          -pc_gamg_esteig_ksp_type cg -ksp_converged_reason -snes_monitor_short -snes_rtol 1.e-4 -dm_view -dm_mat_type aijkokkos -dm_vec_type kokkos
+          -petscpartitioner_simple_node_grid 2,1 -dm_plex_simplex 0 -potential_petscspace_degree 1 -dm_refine 1 -ksp_type cg -pc_type gamg  -ksp_norm_type unpreconditioned \
+          -pc_gamg_esteig_ksp_type cg -ksp_converged_reason -snes_rtol 1.e-4 -dm_view -dm_mat_type aijkokkos -dm_vec_type kokkos
 
   test:
     suffix: aijmkl_comp
@@ -263,17 +283,17 @@ int main(int argc, char **argv)
     output_file: output/ex13_comparison.out
     args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
       -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_monitor_short -snes_type ksponly -dm_view -pc_type gamg -pc_gamg_esteig_ksp_type cg -pc_gamg_esteig_ksp_max_it 10 -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -snes_converged_reason -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijmkl
+      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
+      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijmkl
 
   test:
     suffix: aijmkl_seq
     nsize: 1
     requires: mkl_sparse
     TODO: broken (INDEFINITE PC)
-    args: -dm_plex_dim 3 -dm_plex_box_faces 4,4,4 -dm_refine 1 -petscpartitioner_type simple -potential_petscspace_degree 1 -dm_plex_simplex 0 -snes_monitor_short \
+    args: -dm_plex_dim 3 -dm_plex_box_faces 4,4,4 -dm_refine 1 -petscpartitioner_type simple -potential_petscspace_degree 1 -dm_plex_simplex 0 \
           -snes_type ksponly -dm_view -pc_type gamg -pc_gamg_sym_graph 0 -pc_gamg_threshold -1 -pc_gamg_square_graph 10 -pc_gamg_process_eq_limit 400 \
-          -pc_gamg_reuse_interpolation -pc_gamg_coarse_eq_limit 10 -pc_gamg_esteig_ksp_type cg -ksp_type cg -ksp_norm_type unpreconditioned -snes_converged_reason \
+          -pc_gamg_reuse_interpolation -pc_gamg_coarse_eq_limit 10 -pc_gamg_esteig_ksp_type cg -ksp_type cg -ksp_norm_type unpreconditioned \
           -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijmkl -dm_vec_type standard
 
 TEST*/
