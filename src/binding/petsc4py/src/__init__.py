3bd2ae8f4a7b05db84deaa76805d796209d931fd
Satish Balay <balay@mcs.anl.gov>
2022-03-30 19:13:58 -0500

    Increase patchlevel to 3.16.6

--- src/binding/petsc4py/src/__init__.py
+++ src/binding/petsc4py/src/__init__.py
@@ -21,7 +21,7 @@ communication.
 """
 
 __author__    = 'Lisandro Dalcin'
-__version__   = '3.16.5'
+__version__   = '3.16.6'
 __credits__   = 'PETSc Team <petsc-maint@mcs.anl.gov>'
 
 # --------------------------------------------------------------------
