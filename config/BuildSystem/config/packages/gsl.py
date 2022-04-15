d9c3207179c47449b36a19735e8f241f18c35a68
Barry Smith <bsmith@mcs.anl.gov>
2022-04-15 08:57:10 -0400

    Update GSL download
    /spend 5m

--- config/BuildSystem/config/packages/gsl.py
+++ config/BuildSystem/config/packages/gsl.py
@@ -5,7 +5,7 @@ class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
     self.versionname       = "GSL_MAJOR_VERSION.GSL_MINOR_VERSION"
-    self.download          = ['ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz']
+    self.download          = ['ftp://ftp.gnu.org/gnu/gsl/gsl-2.7.1.tar.gz']
     self.functions         = ['gsl_sf_hermite_zero']
     self.includes          = ['gsl/gsl_version.h']
     self.liblist           = [['libgsl.a','libgslcblas.a']]
