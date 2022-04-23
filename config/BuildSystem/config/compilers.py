61715dfe6f2767c1432c54c1f158017495aefdd5
Satish Balay <balay@mcs.anl.gov>
2022-04-23 18:50:52 -0500

    fix warning with intel compiler build
    ld: skipping incompatible /usr/lib/i386-linux-gnu/libgcc_s.so.1 when searching for libgcc_s.so.1

--- config/BuildSystem/config/compilers.py
+++ config/BuildSystem/config/compilers.py
@@ -35,7 +35,7 @@ class Configure(config.base.Configure):
     if len(self.skipdefaultpaths):
       return self.skipdefaultpaths
     else:
-      self.skipdefaultpaths = ['/usr/lib','/lib','/usr/lib64','/lib64','/usr/lib/x86_64-linux-gnu','/lib/x86_64-linux-gnu']
+      self.skipdefaultpaths = ['/usr/lib','/lib','/usr/lib64','/lib64','/usr/lib/x86_64-linux-gnu','/lib/x86_64-linux-gnu','/usr/lib/i386-linux-gnu']
       conda_sysrt = os.getenv('CONDA_BUILD_SYSROOT')
       if conda_sysrt:
         conda_sysrt = os.path.abspath(conda_sysrt)
