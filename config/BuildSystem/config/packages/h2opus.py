1a01f780e83292cc84062ee99ba97275fef9a0d5
Stefano Zampini <stefano.zampini@gmail.com>
2022-05-03 15:31:34 +0300

    H2OPUS: Update gitcommit

--- config/BuildSystem/config/packages/h2opus.py
+++ config/BuildSystem/config/packages/h2opus.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '325a72a' # Feb 7 2022
+    self.gitcommit              = 'c75d74cc96d728c11b7bf0f291ba71dc369a89f4' # Thu Apr 28, 2022
     self.download               = ['git://https://github.com/ecrc/h2opus']
     self.precisions             = ['single','double']
     self.skippackagewithoptions = 1
