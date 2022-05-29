221c8201310b89c2c9568e582949180bda10c2bc
Lawrence Mitchell <lawrence@wence.uk>
2022-05-29 16:30:52 +0100

    Remove style-incorrect viewer error check

--- src/dm/impls/swarm/swarm.c
+++ src/dm/impls/swarm/swarm.c
@@ -1648,7 +1648,6 @@ PetscErrorCode DMView_Swarm(DM dm, PetscViewer viewer)
       default: PetscCall(DMView_Swarm_Ascii(dm, viewer));
     }
   } else {
-    PetscCheck(!ibinary,PetscObjectComm((PetscObject)dm),PETSC_ERR_SUP,"NO Binary support");
 #if defined(PETSC_HAVE_HDF5)
     if (ishdf5) {
       PetscCall(DMSwarmView_HDF5(dm, viewer));
