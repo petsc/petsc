e39dcfda482adc70ee69f1157543c4437b610237
Junchao Zhang <jczhang@mcs.anl.gov>
2022-06-28 10:36:02 -0500

    Packages: upgrade Kokkos and KK to 3.6.01

--- config/BuildSystem/config/packages/kokkos.py
+++ config/BuildSystem/config/packages/kokkos.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = '3.6.00'
+    self.gitcommit        = '3.6.01'
     self.minversion       = '3.5.00'
     self.versionname      = 'KOKKOS_VERSION'
     self.download         = ['git://https://github.com/kokkos/kokkos.git']
