86081d6ed52b868795172ba1e877cb918bc39543
Mark Adams <524115-markadams4@users.noreply.gitlab.com>
2022-07-14 15:32:04 +0000

    cleanup ex13 tests, switch GMRES to cg - in an attempt to fix this error
    However there appears to be a non-deterministic bug in parallel MKL, in this older version
    
    *** Error in `../ex13': free(): invalid next size (normal): 0x0000000000e210d0 ***
    ======= Backtrace: =========
    /lib64/libc.so.6(+0x81329)[0x2b3c7c755329]
    /homes/intel/18u2/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64_lin/libmkl_core.so(mkl_serv_free+0x2c4)[0x2b3c725d6504]
    /homes/intel/18u2/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64_lin/libmkl_sequential.so(mkl_sparse_d_inspector_gen_mv_i4+0x4af)[0x2b3c767c166f]
    /homes/intel/18u2/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64_lin/libmkl_sequential.so(mkl_sparse_d_optimize_csr_mv_i4+0xa5)[0x2b3c767c5515]
    /homes/intel/18u2/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64_lin/libmkl_avx512_mic.so(mkl_sparse_optimize_i4_avx512_mic+0x220)[0x2b3ca3476bc0]
    /home/petsc/builds/6i15ewNf/2/petsc/petsc/arch-ci-linux-knl/lib/libpetsc.so.3.017(+0x65e06a)[0x2b3c6f4f006a]
    /home/petsc/builds/6i15ewNf/2/petsc/petsc/arch-ci-linux-knl/lib/libpetsc.so.3.017(MatMultTranspose_SeqAIJMKL_SpMV2+0x110)[0x2b3c6f4f14d0]
    /home/petsc/builds/6i15ewNf/2/petsc/petsc/arch-ci-linux-knl/lib/libpetsc.so.3.017(MatMultTranspose_MPIAIJ+0x49)[0x2b3c6f3529e9]
    /home/petsc/builds/6i15ewNf/2/petsc/petsc/arch-ci-linux-knl/lib/libpetsc.so.3.017(MatMultTranspose+0x15e)[0x2b3c6f8c828e]
    /home/petsc/builds/6i15ewNf/2/petsc/petsc/arch-ci-linux-knl/lib/libpetsc.so.3.017(MatRestrict+0x7f)[0x2b3c6f8bcb4f]

--- src/snes/tests/ex13.c
+++ src/snes/tests/ex13.c
@@ -201,90 +201,41 @@ int main(int argc, char **argv)
   test:
     suffix: strong
     requires: triangle
-    args: -dm_plex_dim 2 -dm_refine 1 -benchmark_it 0 -dmsnes_check \
-          -potential_petscspace_degree 2 -dm_ds_jet_degree 2 -strong
+    args: -dm_plex_dim 2 -dm_refine 1 -benchmark_it 0 -dmsnes_check -potential_petscspace_degree 2 -dm_ds_jet_degree 2 -strong
 
   test:
     suffix: bench
     nsize: 4
     args: -dm_plex_dim 3 -dm_plex_simplex 0 -dm_plex_box_faces 2,2,1 -dm_refine 2 -dm_view -ksp_monitor \
-       -benchmark_it 1 \
-       -dm_plex_box_upper 2,2,1 \
-       -dm_plex_box_lower 0,0,0 \
-       -dm_plex_dim 3 \
-       -ksp_converged_reason \
-       -ksp_norm_type unpreconditioned \
-       -ksp_rtol 1.e-6 \
-       -ksp_type cg \
-       -mg_levels_ksp_chebyshev_esteig 0,0.2,0,1.1 \
-       -mg_levels_ksp_max_it 1 \
-       -mg_levels_ksp_type chebyshev \
-       -mg_levels_pc_type jacobi \
-       -pc_gamg_coarse_eq_limit 200 \
-       -pc_gamg_coarse_grid_layout_type compact \
-       -pc_gamg_esteig_ksp_max_it 5 \
-       -pc_gamg_process_eq_limit 200 \
-       -pc_gamg_repartition false \
-       -pc_gamg_reuse_interpolation true \
-       -pc_gamg_aggressive_coarsening 0 \
-       -pc_gamg_threshold 0.001 -pc_gamg_threshold_scale .5\
-       -pc_gamg_type agg \
-       -pc_type gamg \
-       -petscpartitioner_simple_node_grid 1,2,1 \
-       -petscpartitioner_simple_process_grid 2,1,1 \
-       -petscpartitioner_type simple \
-       -potential_petscspace_degree 2 \
-       -snes_lag_jacobian -2 \
-       -snes_max_it 1 \
-       -snes_rtol 1.e-8 \
-       -snes_type ksponly \
-       -use_gpu_aware_mpi true
+       -benchmark_it 1 -dm_plex_box_upper 2,2,1 -dm_plex_box_lower 0,0,0 -dm_plex_dim 3 -ksp_converged_reason \
+       -ksp_norm_type unpreconditioned -ksp_rtol 1.e-6 -ksp_type cg -mg_levels_ksp_chebyshev_esteig 0,0.2,0,1.1 \
+       -mg_levels_ksp_max_it 1 -mg_levels_ksp_type chebyshev  -mg_levels_pc_type jacobi -pc_gamg_coarse_eq_limit 200 \
+       -pc_gamg_coarse_grid_layout_type compact -pc_gamg_esteig_ksp_max_it 5 -pc_gamg_process_eq_limit 200 \
+       -pc_gamg_repartition false -pc_gamg_reuse_interpolation true -pc_gamg_aggressive_coarsening 0 -pc_gamg_threshold 0.001 -pc_gamg_threshold_scale .5 \
+       -pc_gamg_type agg -pc_type gamg -petscpartitioner_simple_node_grid 1,2,1 -petscpartitioner_simple_process_grid 2,1,1 \
+       -petscpartitioner_type simple -potential_petscspace_degree 2 -snes_lag_jacobian -2 -snes_max_it 1 -snes_rtol 1.e-8 -snes_type ksponly -use_gpu_aware_mpi true
 
-  test:
-    suffix: comparison
+  testset:
     nsize: 4
-    args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
-      -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_type ksponly -dm_view -ksp_type cg -ksp_norm_type unpreconditioned -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4
-
-  test:
-    suffix: cuda
-    nsize: 4
-    requires: cuda
     output_file: output/ex13_comparison.out
-    args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
-      -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijcusparse -dm_vec_type cuda
-
-  test:
-    suffix: kokkos_comp
-    nsize: 4
-    requires: !sycl kokkos_kernels
-    output_file: output/ex13_comparison.out
-    args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
-      -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijkokkos -dm_vec_type kokkos
-
-  test:
-    nsize: 4
-    requires: !sycl kokkos_kernels
-    suffix: kokkos
-    args: -dm_plex_dim 2 -dm_plex_box_faces 2,8 -petscpartitioner_type simple -petscpartitioner_simple_process_grid 2,1 \
-          -petscpartitioner_simple_node_grid 2,1 -dm_plex_simplex 0 -potential_petscspace_degree 1 -dm_refine 1 -ksp_type cg -pc_type gamg  -ksp_norm_type unpreconditioned \
-          -pc_gamg_esteig_ksp_type cg -ksp_converged_reason -snes_rtol 1.e-4 -dm_view -dm_mat_type aijkokkos -dm_vec_type kokkos
-
-  test:
-    suffix: aijmkl_comp
-    nsize: 4
-    requires: mkl_sparse
-    output_file: output/ex13_comparison.out
-    args: -dm_plex_dim 2 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
-      -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple \
-      -dm_plex_simplex 0 -snes_type ksponly -dm_view -pc_type gamg  -pc_gamg_process_eq_limit 400 -ksp_norm_type unpreconditioned \
-      -pc_gamg_coarse_eq_limit 10 -ksp_converged_reason -snes_rtol 1.e-4 -dm_mat_type aijmkl
+    args: -dm_plex_dim 2 -benchmark_it 10 -dm_plex_box_faces 4,4 -dm_refine 3 -petscpartitioner_simple_process_grid 2,2 \
+      -petscpartitioner_simple_node_grid 1,1 -potential_petscspace_degree 2 -petscpartitioner_type simple  \
+      -dm_plex_simplex 0 -snes_type ksponly -dm_view -ksp_type cg -pc_type gamg -pc_gamg_process_eq_limit 400 \
+      -ksp_norm_type unpreconditioned -ksp_converged_reason
+    test:
+      suffix: comparison
+    test:
+      suffix: cuda
+      requires: cuda
+      args: -dm_mat_type aijcusparse -dm_vec_type cuda
+    test:
+      suffix: kokkos
+      requires: !sycl kokkos_kernels
+      args: -dm_mat_type aijkokkos -dm_vec_type kokkos
+    test:
+      suffix: aijmkl_comp
+      requires: mkl_sparse
+      args: -dm_mat_type aijmkl
 
   test:
     suffix: aijmkl_seq
