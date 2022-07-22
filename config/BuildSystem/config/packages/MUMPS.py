12615522e33503cecc0ce640bcb8f650d40e212d
Pierre Jolivet <pierre@joliv.et>
2022-07-22 12:24:46 +0200

    configure: MUMPS 5.5.1

--- config/BuildSystem/config/packages/MUMPS.py
+++ config/BuildSystem/config/packages/MUMPS.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.version          = '5.5.0'
+    self.version          = '5.5.1'
     self.minversion       = '5.2.1'
     self.versionname      = 'MUMPS_VERSION'
     self.requiresversion  = 1
