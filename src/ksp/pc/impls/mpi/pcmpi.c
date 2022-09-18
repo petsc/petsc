dd0d27b1b907406c548b52a6eac4547f0c73d824
Barry Smith <bsmith@mcs.anl.gov>
2022-09-18 11:56:37 -0400

    PCMPI should pass block size to parallel matrix for the preconditioner to utilize
    Additional properties of the matrix should also be sent out automatically such as symmetry
    
    Commit-type: optimization
    /spend 10m

--- src/ksp/pc/impls/mpi/pcmpi.c
+++ src/ksp/pc/impls/mpi/pcmpi.c
@@ -125,7 +125,7 @@ static PetscErrorCode PCMPICreate(PC pc) {
 static PetscErrorCode PCMPISetMat(PC pc) {
   PC_MPI            *km = pc ? (PC_MPI *)pc->data : NULL;
   Mat                A;
-  PetscInt           N[2], n, *ia, *ja, j;
+  PetscInt           N[2], n, *ia, *ja, j, bs;
   Mat                sA;
   MPI_Comm           comm = PC_MPI_COMM_WORLD;
   KSP                ksp;
@@ -145,12 +145,15 @@ static PetscErrorCode PCMPISetMat(PC pc) {
     PCMPIMatCounts[size - 1]++;
     PetscCall(PCGetOperators(pc, &sA, &sA));
     PetscCall(MatGetSize(sA, &N[0], &N[1]));
+    PetscCall(MatGetBlockSize(sA, &bs));
+    /* need to broadcast symmetry flags etc if set */
   }
   PetscCallMPI(MPI_Bcast(N, 2, MPIU_INT, 0, comm));
+  PetscCallMPI(MPI_Bcast(&bs, 1, MPIU_INT, 0, comm));
 
   /* determine ownership ranges of matrix */
   PetscCall(PetscLayoutCreate(comm, &layout));
-  PetscCall(PetscLayoutSetBlockSize(layout, 1));
+  PetscCall(PetscLayoutSetBlockSize(layout, bs));
   PetscCall(PetscLayoutSetSize(layout, N[0]));
   PetscCall(PetscLayoutSetUp(layout));
   PetscCall(PetscLayoutGetLocalSize(layout, &n));
@@ -189,6 +192,7 @@ static PetscErrorCode PCMPISetMat(PC pc) {
   for (j = 1; j < n + 1; j++) ia[j] -= ia[0];
   ia[0] = 0;
   PetscCall(MatCreateMPIAIJWithArrays(comm, n, n, N[0], N[0], ia, ja, a, &A));
+  PetscCall(MatSetBlockSize(A, bs));
   PetscCall(MatSetOptionsPrefix(A, "mpi_"));
 
   PetscCall(PetscFree3(ia, ja, a));
