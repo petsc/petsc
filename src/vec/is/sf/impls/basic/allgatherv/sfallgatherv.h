661006243e0904bc232e219bc5c02c8d300d1d6d
Stefano Zampini <stefano.zampini@gmail.com>
2022-09-21 17:19:26 +0300

    PetscSF: optimize SFALLGATHERV for the one-to-all pattern
    this happens with ML models using data parallelism having replicated local parameters
    Add test

--- src/vec/is/sf/impls/basic/allgatherv/sfallgatherv.h
+++ src/vec/is/sf/impls/basic/allgatherv/sfallgatherv.h
@@ -8,6 +8,9 @@
 typedef struct {
   SFBASICHEADER;
   PetscMPIInt *displs, *recvcounts;
+  /* special treatment for one-to-all patterns detected at setup time */
+  PetscBool   bcast_pattern; /* bcast here means one-to-all; we might do MPI_Reduce with this pattern */
+  PetscMPIInt bcast_root;    /* the root rank in MPI_Bcast */
 } PetscSF_Allgatherv;
 
 PETSC_INTERN PetscErrorCode PetscSFSetUp_Allgather(PetscSF);
