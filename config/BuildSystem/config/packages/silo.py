ea2026cfb43596e86578e3f2e31ec95548aa011a
David Wells <drwells@vt.edu>
2023-02-19 21:21:46 +0000

    Bump silo to version 4.11
    The present link is dead - new releases are on GitHub.

--- config/BuildSystem/config/packages/silo.py
+++ config/BuildSystem/config/packages/silo.py
@@ -3,11 +3,11 @@ import config.package
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.download  = ['https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10/silo-4.10-bsd-smalltest.tar.gz']
+    self.version   = '4.11'
+    self.download  = ['https://github.com/LLNL/Silo/releases/download/v'+self.version+'/silo-'+self.version+'-bsd.tar.gz']
     self.functions = ['DBReadVar']
     self.includes  = ['silo.h']
     self.liblist   = [['libsilo.a']]
-    self.pkgname   = 'silo-4.10-bsd'
     self.buildLanguages    = ['Cxx']
     return
 
