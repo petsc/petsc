27f49a208b01d2e827ab9db411a2d16003fe9262
Barry Smith <bsmith@mcs.anl.gov>
2023-03-17 22:46:57 -0400

    Only makefiles in the test and tutorial directories need lib/petsc/conf/test
    Commit-type: housekeeping

--- lib/petsc/bin/maint/generatefortranstubs.py
+++ lib/petsc/bin/maint/generatefortranstubs.py
@@ -152,7 +152,6 @@ def FixDir(petscdir,dir,verbose):
     outbuf +=  libbase + '\n'
     outbuf +=  'include ${PETSC_DIR}/lib/petsc/conf/variables\n'
     outbuf +=  'include ${PETSC_DIR}/lib/petsc/conf/rules\n'
-    outbuf +=  'include ${PETSC_DIR}/lib/petsc/conf/test\n'
 
     ff = open(os.path.join(dir, 'makefile'), 'w')
     ff.write(outbuf)
