0503d4b72b94fd3bceb8450b8b75db2cfc0b8f5f
Satish Balay <balay@mcs.anl.gov>
2023-03-27 14:25:18 -0500

    slepc: update to latest (pre-3.19.0) snapshot

--- config/BuildSystem/config/packages/slepc.py
+++ config/BuildSystem/config/packages/slepc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '4e134e893b39e336d52e59922fc4ca52935d46c0' # main 11/03/2023
+    self.gitcommit              = '09991eb9b088b6f858e03e9cf9b0868b8d8eae75' # main (pre-3.19) mar 20, 2023
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
