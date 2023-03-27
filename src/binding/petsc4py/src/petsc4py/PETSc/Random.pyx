6ba42905e19725280ed786f3a2c85d3255475c21
Stefano Zampini <stefano.zampini@gmail.com>
2023-03-27 17:39:48 +0100

    random

--- src/binding/petsc4py/src/petsc4py/PETSc/Random.pyx
+++ src/binding/petsc4py/src/petsc4py/PETSc/Random.pyx
@@ -1,6 +1,7 @@
 # --------------------------------------------------------------------
 
 class RandomType(object):
+    """The random number generator type."""
     RAND      = S_(PETSCRAND)
     RAND48    = S_(PETSCRAND48)
     SPRNG     = S_(PETSCSPRNG)
@@ -10,71 +11,215 @@ class RandomType(object):
 # --------------------------------------------------------------------
 
 cdef class Random(Object):
+    """The random number generator object.
+
+    See Also
+    --------
+    petsc.PetscRandom
+
+    """
 
     Type = RandomType
 
-    def __cinit__(self):
+    def __cinit__(self) -> None:
         self.obj = <PetscObject*> &self.rnd
         self.rnd = NULL
 
-    def __call__(self):
+    def __call__(self) -> Scalar:
+        """Generate a scalar random number.
+
+        Not collective.
+
+        See Also
+        --------
+        petsc.PetscRandomGetValue
+
+        """
         return self.getValue()
 
-    def view(self, Viewer viewer=None):
+    def view(self, Viewer viewer=None) -> None:
+        """View a random number generator object.
+
+        Collective.
+
+        Parameters
+        ----------
+        viewer
+          A `Viewer` instance or `None` for the default viewer.
+
+        See Also
+        --------
+        petsc.PetscRandomView
+
+        """
         assert self.obj != NULL
         cdef PetscViewer vwr = NULL
         if viewer is not None: vwr = viewer.vwr
         CHKERR( PetscRandomView(self.rnd, vwr) )
 
-    def destroy(self):
+    def destroy(self) -> Self:
+        """Destroy the random number generator object.
+
+        Collective.
+
+        See Also
+        --------
+        petsc.PetscRandomDestroy
+
+        """
         CHKERR( PetscRandomDestroy(&self.rnd) )
         return self
 
-    def create(self, comm=None):
+    def create(self, comm: Comm | None = None) -> Self:
+        """Create a random number generator object.
+
+        Collective.
+
+        Parameters
+        ----------
+        comm
+          The communicator associated with the object. Defaults to `Sys.getDefaultComm`.
+
+        See Also
+        --------
+        Sys.getDefaultComm, petsc.PetscRandomCreate
+
+        """
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
         CHKERR( PetscRandomCreate(ccomm, &self.rnd) )
         return self
 
-    def setType(self, rnd_type):
+    def setType(self, rnd_type: Random.Type | str) -> None:
+        """Set the type of the random number generator object.
+
+        Collective.
+
+        Parameters
+        ----------
+        rnd_type
+          The type of the generator.
+
+        See Also
+        --------
+        getType, petsc.PetscRandomSetType
+
+        """
         cdef PetscRandomType cval = NULL
         rnd_type = str2bytes(rnd_type, &cval)
         CHKERR( PetscRandomSetType(self.rnd, cval) )
 
-    def getType(self):
+    def getType(self) -> str:
+        """Return the type of the random number generator object.
+
+        Not collective.
+
+        See Also
+        --------
+        setType, petsc.PetscRandomGetType
+
+        """
         cdef PetscRandomType cval = NULL
         CHKERR( PetscRandomGetType(self.rnd, &cval) )
         return bytes2str(cval)
 
-    def setFromOptions(self):
+    def setFromOptions(self) -> None:
+        """Configure the random number generator from the options database.
+
+        Collective.
+
+        See Also
+        --------
+        petsc_options, petsc.PetscRandomSetFromOptions
+
+        """
         CHKERR( PetscRandomSetFromOptions(self.rnd) )
 
-    def getValue(self):
+    def getValue(self) -> Scalar:
+        """Generate a scalar random number.
+
+        Not collective.
+
+        See Also
+        --------
+        petsc.PetscRandomGetValue
+
+        """
         cdef PetscScalar sval = 0
         CHKERR( PetscRandomGetValue(self.rnd, &sval) )
         return toScalar(sval)
 
-    def getValueReal(self):
+    def getValueReal(self) -> float:
+        """Generate a real random number.
+
+        Not collective.
+
+        See Also
+        --------
+        petsc.PetscRandomGetValueReal
+
+        """
         cdef PetscReal rval = 0
         CHKERR( PetscRandomGetValueReal(self.rnd, &rval) )
         return toReal(rval)
 
-    def getSeed(self):
+    def getSeed(self) -> int:
+        """Return the random number generator seed.
+
+        Not collective.
+
+        See Also
+        --------
+        setSeed, petsc.PetscRandomGetSeed
+
+        """
         cdef unsigned long seed = 0
         CHKERR( PetscRandomGetSeed(self.rnd, &seed) )
         return seed
 
-    def setSeed(self, seed=None):
+    def setSeed(self, seed: int | None = None) -> None:
+        """Set the seed of random number generator.
+
+        Not collective.
+
+        Parameters
+        ----------
+        seed
+          The value for the seed. If `None`, it only seeds the generator.
+
+        See Also
+        --------
+        getSeed, petsc.PetscRandomSetSeed, petsc.PetscRandomSeed
+
+        """
         if seed is not None:
             CHKERR( PetscRandomSetSeed(self.rnd, seed) )
         CHKERR( PetscRandomSeed(self.rnd) )
 
-    def getInterval(self):
+    def getInterval(self) -> tuple[Scalar, Scalar]:
+        """Return the interval containing the random numbers generated.
+
+        Not collective.
+
+        See Also
+        --------
+        setInterval, petsc.PetscRandomGetInterval
+
+        """
         cdef PetscScalar sval1 = 0
         cdef PetscScalar sval2 = 1
         CHKERR( PetscRandomGetInterval(self.rnd, &sval1, &sval2) )
         return (toScalar(sval1), toScalar(sval2))
 
-    def setInterval(self, interval):
+    def setInterval(self, interval: tuple[Scalar, Scalar]) -> None:
+        """Set the interval of the random number generator.
+
+        Not collective.
+
+        See Also
+        --------
+        getInterval, petsc.PetscRandomSetInterval
+
+        """
         cdef PetscScalar sval1 = 0
         cdef PetscScalar sval2 = 1
         low, high = interval
@@ -85,15 +230,17 @@ cdef class Random(Object):
     #
 
     property seed:
-        def __get__(self):
+        """The seed of the random number generator."""
+        def __get__(self) -> int:
             return self.getSeed()
-        def __set__(self, value):
+        def __set__(self, value: int | None) -> None:
             self.setSeed(value)
 
     property interval:
-        def __get__(self):
+        """The interval of the generated random numbers."""
+        def __get__(self) -> tuple[Scalar, Scalar]:
             return self.getInterval()
-        def __set__(self, value):
+        def __set__(self, value: tuple[Scalar, Scalar]):
             self.setInterval(value)
 
 # --------------------------------------------------------------------
