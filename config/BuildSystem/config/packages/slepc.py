24f010a8ffac22b4ea61966e283ad35824562727
Satish Balay <balay@mcs.anl.gov>
2023-04-04 08:00:42 -0500

    slepc: switch to main

--- config/BuildSystem/config/packages/slepc.py
+++ config/BuildSystem/config/packages/slepc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '09991eb9b088b6f858e03e9cf9b0868b8d8eae75' # main (pre-3.19) mar 20, 2023
+    self.gitcommit              = 'e438e49934b4d8ac38d5c001ee2b7d8e98360e4b' # main apr 2, 2023
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
