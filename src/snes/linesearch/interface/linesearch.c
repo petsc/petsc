51dbb4743480cffa8ff1aea6c2d5dbc193819d06
Barry Smith <bsmith@mcs.anl.gov>
2023-04-10 21:19:22 -0400

    Fix bug in handling of -snes_linesearch_maxstep.
    It should be relative to fnorm, not an absolute value
    
    Commit-type: bug-fix

--- src/snes/linesearch/interface/linesearch.c
+++ src/snes/linesearch/interface/linesearch.c
@@ -1069,7 +1069,7 @@ PetscErrorCode SNESLineSearchSetLambda(SNESLineSearch linesearch, PetscReal lamb
 .  linesearch - linesearch context
 
    Output Parameters:
-+  steptol - The minimum steplength
++  steptol - The minimum steplength, this is the value set by `SNESLineSearchGetTolerances()` scaled by the current norm of the function value
 .  maxstep - The maximum steplength
 .  rtol    - The relative tolerance for iterative line searches
 .  atol    - The absolute tolerance for iterative line searches
@@ -1094,7 +1094,7 @@ PetscErrorCode SNESLineSearchGetTolerances(SNESLineSearch linesearch, PetscReal 
   }
   if (maxstep) {
     PetscValidRealPointer(maxstep, 3);
-    *maxstep = linesearch->maxstep;
+    *maxstep = linesearch->maxstep * linesearch->fnorm;
   }
   if (rtol) {
     PetscValidRealPointer(rtol, 4);
