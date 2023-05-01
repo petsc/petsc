1b5bff3af59a4c14f97a1eb33b3495356242ce7d
Stefano Zampini <stefano.zampini@gmail.com>
2023-05-01 12:21:52 +0300

    MATHTOOL: update commit without  fpe

--- config/BuildSystem/config/packages/htool.py
+++ config/BuildSystem/config/packages/htool.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self,framework):
     config.package.Package.__init__(self,framework)
-    self.gitcommit              = 'feb9643673371553d8d20b7c5a1a117f5946b47d' # main apr-14-2023
+    self.gitcommit              = '34b0a74ea932258034dfb91fa8ee83664cf40a1d' # main may-02-2023
     self.download               = ['git://https://github.com/htool-ddm/htool','https://github.com/htool-ddm/htool/archive/'+self.gitcommit+'.tar.gz']
     self.minversion             = '0.8.0'
     self.versionname            = 'HTOOL_VERSION'
