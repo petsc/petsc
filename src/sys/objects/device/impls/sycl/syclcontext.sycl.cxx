08a0d18beaa5e851c8195495af12cfd86a127ec9
Jacob Faibussowitsch <jacob.fai@gmail.com>
2023-05-17 13:11:48 -0400

    Make hostcontext and syclcontext ops static, and do some minor cleanup on syclcontext

--- src/sys/objects/device/impls/sycl/syclcontext.sycl.cxx
+++ src/sys/objects/device/impls/sycl/syclcontext.sycl.cxx
@@ -15,62 +15,54 @@ namespace impl
 
 class DeviceContext {
 public:
-  struct PetscDeviceContext_IMPLS {
-    ::sycl::event event;
-    ::sycl::event begin; // timer-only
-    ::sycl::event end;   // timer-only
+  struct PetscDeviceContext_SYCL {
+    ::sycl::event event{};
+    ::sycl::event begin{}; // timer-only
+    ::sycl::event end{};   // timer-only
 #if PetscDefined(USE_DEBUG)
-    PetscBool timerInUse;
+    PetscBool timerInUse{};
 #endif
   };
 
 private:
-  static bool initialized_;
-
-  static PetscErrorCode finalize_() noexcept
-  {
-    PetscFunctionBegin;
-    initialized_ = false;
-    PetscFunctionReturn(PETSC_SUCCESS);
-  }
-
-  static PetscErrorCode initialize_(PetscInt id, DeviceContext *dci) noexcept
-  {
-    PetscFunctionBegin;
-    PetscCall(PetscDeviceCheckDeviceCount_Internal(id));
-    if (!initialized_) {
-      initialized_ = true;
-      PetscCall(PetscRegisterFinalize(finalize_));
-    }
-    PetscFunctionReturn(PETSC_SUCCESS);
-  }
+  PETSC_NODISCARD static PetscDeviceContext_SYCL *impls_cast_(PetscDeviceContext dctx) noexcept { return static_cast<PetscDeviceContext_SYCL *>(dctx->data); }
 
 public:
-  const struct _DeviceContextOps ops = {destroy, changeStreamType, setUp, query, waitForContext, synchronize, getBlasHandle, getSolverHandle, getStreamHandle, beginTimer, endTimer, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
-
-  // default constructor
-  DeviceContext() noexcept = default;
-
   // All of these functions MUST be static in order to be callable from C, otherwise they
   // get the implicit 'this' pointer tacked on
   static PetscErrorCode destroy(PetscDeviceContext dctx) noexcept
   {
     PetscFunctionBegin;
-    delete static_cast<PetscDeviceContext_IMPLS *>(dctx->data);
+    delete impls_cast_(dctx);
     dctx->data = nullptr;
     PetscFunctionReturn(PETSC_SUCCESS);
   };
-  static PetscErrorCode changeStreamType(PetscDeviceContext, PetscStreamType) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode setUp(PetscDeviceContext) noexcept { return PETSC_SUCCESS; }; // Nothing to setup
-  static PetscErrorCode query(PetscDeviceContext, PetscBool *) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode waitForContext(PetscDeviceContext, PetscDeviceContext) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode synchronize(PetscDeviceContext) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode getBlasHandle(PetscDeviceContext, void *) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode getSolverHandle(PetscDeviceContext, void *) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode getStreamHandle(PetscDeviceContext, void *) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode beginTimer(PetscDeviceContext) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
-  static PetscErrorCode endTimer(PetscDeviceContext, PetscLogDouble *) noexcept { SETERRQ(PETSC_COMM_SELF, PETSC_ERR_SUP, "Not implemented"); };
+
+  // clang-format off
+  static constexpr _DeviceContextOps ops = {
+    PetscDesignatedInitializer(destroy, nullptr),
+    PetscDesignatedInitializer(changestreamtype, nullptr),
+    PetscDesignatedInitializer(setup, nullptr),
+    PetscDesignatedInitializer(query, nullptr),
+    PetscDesignatedInitializer(waitforcontext, nullptr),
+    PetscDesignatedInitializer(synchronize, nullptr),
+    PetscDesignatedInitializer(getblashandle, nullptr),
+    PetscDesignatedInitializer(getsolverhandle, nullptr),
+    PetscDesignatedInitializer(getstreamhandle, nullptr),
+    PetscDesignatedInitializer(begintimer, nullptr),
+    PetscDesignatedInitializer(endtimer, nullptr),
+    PetscDesignatedInitializer(memalloc, nullptr),
+    PetscDesignatedInitializer(memfree, nullptr),
+    PetscDesignatedInitializer(memcopy, nullptr),
+    PetscDesignatedInitializer(memset, nullptr),
+    PetscDesignatedInitializer(createevent, nullptr),
+    PetscDesignatedInitializer(recordevent, nullptr),
+    PetscDesignatedInitializer(waitforevent, nullptr)
+  };
+  // clang-format on
 };
+
+constexpr _DeviceContextOps DeviceContext::ops;
 
 } // namespace impl
 
@@ -84,10 +76,10 @@ PetscErrorCode PetscDeviceContextCreate_SYCL(PetscDeviceContext dctx)
 {
   using namespace Petsc::device::sycl::impl;
 
-  static const DeviceContext syclctx;
+  static constexpr DeviceContext syclctx;
 
   PetscFunctionBegin;
-  dctx->data = new DeviceContext::PetscDeviceContext_IMPLS();
-  PetscCall(PetscMemcpy(dctx->ops, &syclctx.ops, sizeof(syclctx.ops)));
+  PetscCallCXX(dctx->data = new DeviceContext::PetscDeviceContext_SYCL{});
+  *(dctx->ops) = syclctx.ops;
   PetscFunctionReturn(PETSC_SUCCESS);
 }
