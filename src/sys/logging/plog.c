92d1ba04199d57b54f1de5c1e878c17c22b9f227
Barry Smith <bsmith@mcs.anl.gov>
2023-05-17 22:27:46 -0400

    Have -log_view_memory print in its docs that the memory sizes are obtained by summing over all MPI ranks

--- src/sys/logging/plog.c
+++ src/sys/logging/plog.c
@@ -1848,6 +1848,7 @@ PetscErrorCode PetscLogView_Default(PetscViewer viewer)
   PetscCall(PetscFPrintf(comm, fd, "      %%R - percent reductions in this phase\n"));
   PetscCall(PetscFPrintf(comm, fd, "   Total Mflop/s: 10e-6 * (sum of flop over all processors)/(max time over all processors)\n"));
   if (PetscLogMemory) {
+    PetscCall(PetscFPrintf(comm, fd, "   Memory usage is summed over all MPI processes, it is given in mega-bytes\n"));
     PetscCall(PetscFPrintf(comm, fd, "   Malloc Mbytes: Memory allocated and kept during event (sum over all calls to event). May be negative\n"));
     PetscCall(PetscFPrintf(comm, fd, "   EMalloc Mbytes: extra memory allocated during event and then freed (maximum over all calls to events). Never negative\n"));
     PetscCall(PetscFPrintf(comm, fd, "   MMalloc Mbytes: Increase in high water mark of allocated memory (sum over all calls to event). Never negative\n"));
