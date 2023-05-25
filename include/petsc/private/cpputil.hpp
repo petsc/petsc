418dbbe5c294b713971823b0eef04bc220e11f0c
Satish Balay <balay@mcs.anl.gov>
2023-05-25 08:10:04 +0530

    Revert "Revert "Merge branch 'jacobf/2023-05-17/misc-cxx-fixups/release' into 'release'""
    This reverts commit df799b48afdc2b9da2e7091d01d5c06978131d9d.

--- include/petsc/private/cpputil.hpp
+++ include/petsc/private/cpputil.hpp
@@ -3,15 +3,13 @@
 
 #include <petsc/private/petscimpl.h>
 
-#if defined(__cplusplus)
-  #include <petsc/private/cpp/macros.hpp>
-  #include <petsc/private/cpp/utility.hpp>
-  #include <petsc/private/cpp/type_traits.hpp>
-  #include <petsc/private/cpp/tuple.hpp>
-  #include <petsc/private/cpp/array.hpp>
-  #include <petsc/private/cpp/functional.hpp>
-  #include <petsc/private/cpp/register_finalize.hpp>
-  #include <petsc/private/cpp/crtp.hpp>
-#endif // __cplusplus
+#include <petsc/private/cpp/macros.hpp>
+#include <petsc/private/cpp/utility.hpp>
+#include <petsc/private/cpp/type_traits.hpp>
+#include <petsc/private/cpp/tuple.hpp>
+#include <petsc/private/cpp/array.hpp>
+#include <petsc/private/cpp/functional.hpp>
+#include <petsc/private/cpp/register_finalize.hpp>
+#include <petsc/private/cpp/crtp.hpp>
 
 #endif // PETSC_CPPUTIL_HPP
