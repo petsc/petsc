b1a4cc51b2879b8826b753d243417014b0228aa3
Pierre Jolivet <pierre@joliv.et>
2023-06-03 20:31:54 +0200

    configure: update CMake/[Par]Mmg/Htool

--- config/BuildSystem/config/packages/mmg.py
+++ config/BuildSystem/config/packages/mmg.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = '9bd0023fe5f4a7de55d2959dd8dd4bf5d6929a36' # develop dec-22-2022
+    self.gitcommit        = 'a940c305f68adb76a2f12e95e02ff02617031882' # develop june-6-2023
     self.download         = ['git://https://github.com/MmgTools/mmg.git','https://github.com/MmgTools/mmg/archive/'+self.gitcommit+'.tar.gz']
     self.versionname      = 'MMG_VERSION_RELEASE'
     self.includes         = ['mmg/libmmg.h']
