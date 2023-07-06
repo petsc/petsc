c04c6898996b44fea028873ca3145a09e69262c0
Stefano Zampini <stefano.zampini@gmail.com>
2023-07-06 19:18:52 +0200

    minor

--- src/ts/tests/ex2.c
+++ src/ts/tests/ex2.c
@@ -4,9 +4,9 @@
           Solves U_t=F(t,u)
           Where:
 
-                  [2*u1+u2
-          F(t,u)= [u1+2*u2+u3
-                  [   u2+2*u3
+                  [2*u1+u2   ]
+          F(t,u)= [u1+2*u2+u3]
+                  [   u2+2*u3]
        We can compare the solutions from euler, beuler and SUNDIALS to
        see what is the difference.
 
