ca85ed896d2b3f93fb97c10ef3c5bb1c77475ae8
Barry Smith <bsmith@mcs.anl.gov>
2023-07-08 17:51:52 -0400

    Sadly, isatty() is not a reliable way to determine if PetscAttachDebugger() will work
    Reported-by: Pierre Jolivet

--- src/sys/error/adebug.c
+++ src/sys/error/adebug.c
@@ -230,10 +230,7 @@ PetscErrorCode PetscAttachDebugger(void)
     ierr = (*PetscErrorPrintf)("PetscAttachDebugger: Cannot determine program name needed to attach debugger\n");
     return PETSC_ERR_SYS;
   }
-  if (PetscUnlikely(!isatty(fileno(stdin))) && !UseDebugTerminal) {
-    ierr = (*PetscErrorPrintf)("PetscAttachDebugger: stdin is not a tty, hence unable to attach debugger, see PetscAttachDebugger()\n");
-    return PETSC_ERR_SYS;
-  }
+  if (PetscUnlikely(!isatty(fileno(stdin))) && !UseDebugTerminal) { printf("If the debugger exits immediately or hangs, this indicates you cannot use PetscAttachDebugger() in this situation\n\n"); }
   child = (int)fork();
   if (PetscUnlikely(child < 0)) {
     ierr = (*PetscErrorPrintf)("PetscAttachDebugger: Error in fork() prior to attaching debugger\n");
