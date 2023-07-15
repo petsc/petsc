70009435183fa67966fdf74370ab10cffad02a8d
Pierre Jolivet <pierre@joliv.et>
2023-07-15 06:18:12 +0200

    PCHPDDM: fix wrong Fortran interface

---
+++ src/ksp/pc/impls/hpddm/ftn-custom/zpchpddmf.c
@@ -0,0 +1,17 @@
+#include <petsc/private/fortranimpl.h>
+
+#include <petscpc.h>
+#if defined(PETSC_HAVE_FORTRAN_CAPS)
+  #define pchpddmsetauxiliarymat_ PCHPDDMSETAUXILIARYMAT
+#elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE) && !defined(FORTRANDOUBLEUNDERSCORE)
+  #define pchpddmsetauxiliarymat_ pchpddmsetauxiliarymat
+#endif
+
+PETSC_EXTERN void pchpddmsetauxiliarymat_(PC *pc, IS *is, Mat *A, PetscErrorCode (*setup)(Mat, PetscReal, Vec, Vec, PetscReal, IS, void *), PETSC_UNUSED void *setup_ctx, PetscErrorCode *ierr)
+{
+  if ((PetscVoidFunction)setup != (PetscVoidFunction)PETSC_NULL_FUNCTION_Fortran) {
+    *ierr = PETSC_ERR_ARG_WRONG;
+    return;
+  }
+  *ierr = PCHPDDMSetAuxiliaryMat(*pc, *is, *A, NULL, NULL);
+}
