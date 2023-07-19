c41404d664ed8170cc1cfaac2ea7c3e5ec8330cf
Toby Isaac <toby.isaac@gmail.com>
2023-07-19 10:28:54 -0400

    Update macros to match PetscValidPointer becoming PetscAssertPointer

--- src/sys/tests/ex67.c
+++ src/sys/tests/ex67.c
@@ -2,7 +2,7 @@ const char help[] = "Test PetscAssertPointer type generics";
 
 #include <petsc/private/petscimpl.h>
 
-#define PETSC_TEST_VALID_POINTER_GENERICS_SINGLE(type, PETSC_TYPE, string) \
+#define PETSC_TEST_ASSERT_POINTER_GENERICS_SINGLE(type, PETSC_TYPE, string) \
   do { \
     type         *h = NULL; \
     PetscBool     same_string; \
@@ -14,31 +14,31 @@ const char help[] = "Test PetscAssertPointer type generics";
     PetscCall(PetscStrcmp(PetscAssertPointer_String(h), expected_string, &same_string)); \
     PetscCheck(same_string, PETSC_COMM_SELF, PETSC_ERR_PLIB, "[PetscAssertPointer_String(%s *h) = \"%s\"] != \"%s\"", expected_string, PetscAssertPointer_String(h), expected_string); \
   } while (0)
-#define PETSC_TEST_VALID_POINTER_GENERICS(type, PETSC_TYPE) \
-  PETSC_TEST_VALID_POINTER_GENERICS_SINGLE(type, PETSC_TYPE, PetscStringize(type)); \
-  PETSC_TEST_VALID_POINTER_GENERICS_SINGLE(const type, PETSC_TYPE, PetscStringize(type))
+#define PETSC_TEST_ASSERT_POINTER_GENERICS(type, PETSC_TYPE) \
+  PETSC_TEST_ASSERT_POINTER_GENERICS_SINGLE(type, PETSC_TYPE, PetscStringize(type)); \
+  PETSC_TEST_ASSERT_POINTER_GENERICS_SINGLE(const type, PETSC_TYPE, PetscStringize(type))
 
 int main(int argc, char **argv)
 {
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
 #if defined(PetscAssertPointer_PetscDataType) && (defined(__cplusplus) || (PETSC_C_VERSION >= 11))
   // clang-format off
-  PETSC_TEST_VALID_POINTER_GENERICS(          char, PETSC_CHAR   );
-  PETSC_TEST_VALID_POINTER_GENERICS(   signed char, PETSC_CHAR   );
-  PETSC_TEST_VALID_POINTER_GENERICS( unsigned char, PETSC_CHAR   );
-  PETSC_TEST_VALID_POINTER_GENERICS(         short, PETSC_SHORT  );
-  PETSC_TEST_VALID_POINTER_GENERICS(unsigned short, PETSC_SHORT  );
-  PETSC_TEST_VALID_POINTER_GENERICS(         float, PETSC_FLOAT  );
-  PETSC_TEST_VALID_POINTER_GENERICS(        double, PETSC_DOUBLE );
-  PETSC_TEST_VALID_POINTER_GENERICS(  PetscComplex, PETSC_COMPLEX);
-  PETSC_TEST_VALID_POINTER_GENERICS(       int32_t, PETSC_INT32  );
-  PETSC_TEST_VALID_POINTER_GENERICS(      uint32_t, PETSC_INT32  );
-  PETSC_TEST_VALID_POINTER_GENERICS(       int64_t, PETSC_INT64  );
-  PETSC_TEST_VALID_POINTER_GENERICS(      uint64_t, PETSC_INT64  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(          char, PETSC_CHAR   );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(   signed char, PETSC_CHAR   );
+  PETSC_TEST_ASSERT_POINTER_GENERICS( unsigned char, PETSC_CHAR   );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(         short, PETSC_SHORT  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(unsigned short, PETSC_SHORT  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(         float, PETSC_FLOAT  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(        double, PETSC_DOUBLE );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(  PetscComplex, PETSC_COMPLEX);
+  PETSC_TEST_ASSERT_POINTER_GENERICS(       int32_t, PETSC_INT32  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(      uint32_t, PETSC_INT32  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(       int64_t, PETSC_INT64  );
+  PETSC_TEST_ASSERT_POINTER_GENERICS(      uint64_t, PETSC_INT64  );
   // clang-format on
 #endif
 #if defined(PetscAssertPointer_PetscDataType) && defined(__cplusplus)
-  PETSC_TEST_VALID_POINTER_GENERICS(PetscBool, PETSC_BOOL);
+  PETSC_TEST_ASSERT_POINTER_GENERICS(PetscBool, PETSC_BOOL);
 #endif
   PetscCall(PetscFinalize());
   return 0;
