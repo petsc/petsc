bdd283b3145fd7a139b37a07f7c28f2b66798c3a
Barry Smith <bsmith@mcs.anl.gov>
2023-09-17 15:29:24 -0400

    Fix minor typos etc in some KSP docs

--- src/ksp/ksp/impls/preonly/preonly.c
+++ src/ksp/ksp/impls/preonly/preonly.c
@@ -78,6 +78,21 @@ static PetscErrorCode KSPMatSolve_PREONLY(KSP ksp, Mat B, Mat X)
 }
 
 /*MC
+     KSPNONE - An alias for `KSPPREONLY`
+
+   Options Database Key:
+.   -ksp_type none - use a single application of the preconditioner only
+
+   Level: beginner
+
+   Note:
+   See `KSPPREONLY` for more details
+
+.seealso: [](ch_ksp), `KSPCreate()`, `KSPSetType()`, `KSPType`, `KSPPREONLY`, `KSP`, `KSPRICHARDSON`, `KSPCHEBYSHEV`, `KSPGetPC()`, `KSPSetInitialGuessNonzero()`,
+          `PCREDISTRIBUTE`, `PCRedistributeGetKSP()`
+M*/
+
+/*MC
      KSPPREONLY - This implements a method that applies ONLY the preconditioner exactly once.
                   This may be used in inner iterations, where it is desired to
                   allow multiple iterations as well as the "0-iteration" case. It is
@@ -85,7 +100,7 @@ static PetscErrorCode KSPMatSolve_PREONLY(KSP ksp, Mat B, Mat X)
                   There is an alias of this with the name `KSPNONE`.
 
    Options Database Key:
-.   -ksp_type preonly - use preconditioner only
+.   -ksp_type preonly - use a single application of the preconditioner only
 
    Level: beginner
 
@@ -106,7 +121,7 @@ static PetscErrorCode KSPMatSolve_PREONLY(KSP ksp, Mat B, Mat X)
    This is so the users does not have to change `KSPNormType` options when they switch from other `KSP` methods to this one.
 
 .seealso: [](ch_ksp), `KSPCreate()`, `KSPSetType()`, `KSPType`, `KSP`, `KSPRICHARDSON`, `KSPCHEBYSHEV`, `KSPGetPC()`, `KSPSetInitialGuessNonzero()`,
-          `PCREDISTRIBUTE`, `PCRedistributedGetKSP()`
+          `PCREDISTRIBUTE`, `PCRedistributeGetKSP()`
 M*/
 
 PETSC_EXTERN PetscErrorCode KSPCreate_PREONLY(KSP ksp)
