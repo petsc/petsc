e471551a900dc5fab4aebf800ba579c1951cb35a
Pierre Jolivet <pierre@joliv.et>
2023-09-30 06:53:29 +0200

    configure: SLEPc 3.20.0

--- config/BuildSystem/config/packages/slepc.py
+++ config/BuildSystem/config/packages/slepc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '8cd5b338c8d505feaba64739b6f1374b14835c4a' # (pre-3.20.0) main sep-28-2023
+    self.gitcommit              = 'v3.20.0'
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
     self.includes               = []
