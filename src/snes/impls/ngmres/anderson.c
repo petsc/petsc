4936d80934666c0fe36796ce94b63b57bfb10049
Stefano Zampini <stefano.zampini@gmail.com>
2023-10-23 17:35:32 +0300

    SNESNGMRES: support objective functions
    Removed some PetscSqrtReal calls.
    I don't understand why PetscSqrtReal(norm) was used.
    The norm is already the result of a sqrt
    Some testing output has improved (probably it was a bug?)
    add tests for objective functions in TAO

--- src/snes/impls/ngmres/anderson.c
+++ src/snes/impls/ngmres/anderson.c
@@ -107,7 +107,7 @@ static PetscErrorCode SNESSolve_Anderson(SNES snes)
         PetscFunctionReturn(PETSC_SUCCESS);
       }
       PetscCall(SNESGetNPCFunction(snes, FM, &fMnorm));
-      if (ngmres->andersonBeta != 1.0) PetscCall(VecAXPBY(XM, (1.0 - ngmres->andersonBeta), ngmres->andersonBeta, X));
+      PetscCall(VecAXPBY(XM, 1.0 - ngmres->andersonBeta, ngmres->andersonBeta, X));
     } else {
       PetscCall(VecCopy(F, FM));
       PetscCall(VecCopy(X, XM));
@@ -119,7 +119,7 @@ static PetscErrorCode SNESSolve_Anderson(SNES snes)
     ivec = k_restart % ngmres->msize;
     if (ngmres->restart_type == SNES_NGMRES_RESTART_DIFFERENCE) {
       PetscCall(SNESNGMRESNorms_Private(snes, l, X, F, XM, FM, XA, FA, D, &dnorm, &dminnorm, NULL, NULL, NULL, &xnorm, &fAnorm, &ynorm));
-      PetscCall(SNESNGMRESSelectRestart_Private(snes, l, fMnorm, fnorm, dnorm, fminnorm, dminnorm, &selectRestart));
+      PetscCall(SNESNGMRESSelectRestart_Private(snes, l, snes->norm, fMnorm, fnorm, dnorm, fminnorm, dminnorm, &selectRestart));
       /* if the restart conditions persist for more than restart_it iterations, restart. */
       if (selectRestart) restart_count++;
       else restart_count = 0;
@@ -186,8 +186,8 @@ static PetscErrorCode SNESSolve_Anderson(SNES snes)
    Very similar to the `SNESNGMRES` algorithm.
 
    References:
-+  * -  D. G. Anderson. Iterative procedures for nonlinear integral equations.
-    J. Assoc. Comput. Mach., 12, 1965."
++  * -  D. G. Anderson. "Iterative procedures for nonlinear integral equations",
+    J. Assoc. Comput. Mach., 12, 1965
 -  * - Peter R. Brune, Matthew G. Knepley, Barry F. Smith, and Xuemin Tu,"Composing Scalable Nonlinear Algebraic Solvers",
    SIAM Review, 57(4), 2015
 
