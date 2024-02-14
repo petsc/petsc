a022890329e8eb6dd4bd521e07bc86f3eed0f938
Hong Zhang <hzhang@mcs.anl.gov>
2024-02-13 19:48:58 -0600

    fix error message when MatProductSymbolic() is not supported for the matrix types Reported-by: Hana Honnerová <hhornik@kma.zcu.cz> add 'bool setfromoptions_called' in Mat_Product for checking if MatProductSetFromOptions() is called

--- include/petsc/private/matimpl.h
+++ include/petsc/private/matimpl.h
@@ -441,6 +441,7 @@ typedef struct { /* used by MatProduct() */
   PetscBool      symbolic_used_the_fact_C_is_symmetric; /* MatMatMult(A,B,MAT_REUSE_MATRIX,..&C) is still legitimate), we need to redo symbolic! */
   PetscReal      fill;
   PetscBool      api_user; /* used to distinguish command line options and to indicate the matrix values are ready to be consumed at symbolic phase if needed */
+  PetscBool      setfromoptionscalled;
 
   /* Some products may display the information on the algorithm used */
   PetscErrorCode (*view)(Mat, PetscViewer);
