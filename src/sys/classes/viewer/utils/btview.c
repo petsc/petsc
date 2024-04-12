203d8000a098ebbf4c2af227db0fb4abf6106a92
Matthew G. Knepley <knepley@gmail.com>
2024-04-11 20:31:37 -0400

    BT: Fix for parallel viewing

--- src/sys/classes/viewer/utils/btview.c
+++ src/sys/classes/viewer/utils/btview.c
@@ -5,7 +5,6 @@
 PetscErrorCode PetscBTView(PetscInt m, const PetscBT bt, PetscViewer viewer)
 {
   PetscFunctionBegin;
-  if (m < 1) PetscFunctionReturn(PETSC_SUCCESS);
   if (!viewer) PetscCall(PetscViewerASCIIGetStdout(PETSC_COMM_SELF, &viewer));
   PetscCall(PetscViewerASCIIPushSynchronized(viewer));
   for (PetscInt i = 0; i < m; ++i) PetscCall(PetscViewerASCIISynchronizedPrintf(viewer, "%" PetscInt_FMT " %hhu\n", i, PetscBTLookup(bt, i)));
