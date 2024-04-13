6ba8a279b4090845d41307521b492df5769b59e7
Satish Balay <balay@mcs.anl.gov>
2024-04-13 13:58:32 -0500

    CI: fix build error in petsc4py-pip job [wrt building petsc4py wheel] by removing the "workaround for rpath linker option"
    Ref: https://github.com/mpi4py/mpi4py/commit/484e365a16efb799644cf6b2212622c62ec5941d
    
      File "/scratch/svcpetsc/glci-builds/5WySGRd5/0/petsc/petsc/src/binding/petsc4py/conf/confpetsc.py", line 208, in rpath_option
        if option.startswith('-R'):
    AttributeError: 'list' object has no attribute 'startswith'

--- src/binding/petsc4py/conf/confpetsc.py
+++ src/binding/petsc4py/conf/confpetsc.py
@@ -200,18 +200,6 @@ def get_config_vars(*names):
     values = fix_config_vars(names, values)
     return values
 
-from distutils.unixccompiler import UnixCCompiler
-rpath_option_orig = UnixCCompiler.runtime_library_dir_option
-def rpath_option(compiler, dir):
-    option = rpath_option_orig(compiler, dir)
-    if sys.platform[:5] == 'linux':
-        if option.startswith('-R'):
-            option =  option.replace('-R', '-Wl,-rpath,', 1)
-        elif option.startswith('-Wl,-R'):
-            option =  option.replace('-Wl,-R', '-Wl,-rpath,', 1)
-    return option
-UnixCCompiler.runtime_library_dir_option = rpath_option
-
 # --------------------------------------------------------------------
 
 class PetscConfig:
