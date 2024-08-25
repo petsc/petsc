6b5e11fbcb251781fc0b428412e13db733ff877a
James Wright <james@jameswright.xyz>
2024-08-24 18:35:54 -0600

    minor: Correct typo in glvis test name
    The file written is tris, not tets

--- src/ts/tutorials/ex11.c
+++ src/ts/tutorials/ex11.c
@@ -1867,7 +1867,7 @@ int initLinearWave(EulerNode *ux, const PetscReal gamma, const PetscReal coord[]
 
     # Test GLVis visualization of PetscFV fields
     test:
-      suffix: glvis_adv_2d_tet
+      suffix: glvis_adv_2d_tri
       args: -ufv_vtk_interval 0 -ufv_vtk_monitor 0 \
             -dm_plex_filename ${wPETSC_DIR}/share/petsc/datafiles/meshes/square_periodic.msh -dm_plex_gmsh_periodic 0 \
             -ts_monitor_solution glvis: -ts_max_steps 0
