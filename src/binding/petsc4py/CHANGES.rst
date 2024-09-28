5636f27c76828a080c3b842390fcec7bdce2ec1d
Satish Balay <balay@mcs.anl.gov>
2024-09-28 14:57:35 -0500

    petsc4py: set 3.22.0 release strings

--- src/binding/petsc4py/CHANGES.rst
+++ src/binding/petsc4py/CHANGES.rst
@@ -6,6 +6,11 @@ CHANGES: PETSc for Python
 :Contact: dalcinl@gmail.com
 
 
+Release 3.22.0
+==============
+
+- Update to PETSc 3.22 release.
+
 Release 3.21.0
 ==============
 
--- src/binding/petsc4py/src/petsc4py/__init__.py
+++ src/binding/petsc4py/src/petsc4py/__init__.py
@@ -16,7 +16,7 @@ communications.
 """
 
 __author__ = 'Lisandro Dalcin'
-__version__ = '3.21.6'
+__version__ = '3.22.0'
 __credits__ = 'PETSc Team <petsc-maint@mcs.anl.gov>'
 
 
