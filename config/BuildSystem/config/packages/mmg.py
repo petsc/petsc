a937d8f23bf2d49c9114f33b80c6efac8f1ebd2e
Pierre Jolivet <pierre@joliv.et>
2024-09-30 08:01:14 +0200

    configure: update CMake/hwloc/[Par]Mmg/SLEPc

--- config/BuildSystem/config/packages/mmg.py
+++ config/BuildSystem/config/packages/mmg.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = '2263f92c032a26672c2625230a0689e29e8d96c6' # develop july-22-2024
+    self.gitcommit        = 'bf6bcab2d8310ac73fdb4ede5e2513e9c08b8d2d' # develop aug-27-2024
     self.download         = ['git://https://github.com/MmgTools/mmg.git','https://github.com/MmgTools/mmg/archive/'+self.gitcommit+'.tar.gz']
     self.versionname      = 'MMG_VERSION_RELEASE'
     self.includes         = ['mmg/libmmg.h']
