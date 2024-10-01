731341e59ef9e9cfa98efcaf9a5a63ad0406ab95
Junchao Zhang <jczhang@anl.gov>
2024-09-30 21:54:32 -0500

    CUPM: fix hipblasDoubleComplex, which is replaced by hipDoubleComplex in newer hipBLAS

---
+++ config/examples/arch-ci-linux-hip-cmplx.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+petsc_hash_pkgs=os.path.join(os.getenv('HOME'),'petsc-hash-pkgs')
+
+if __name__ == '__main__':
+  import sys
+  import os
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--package-prefix-hash='+petsc_hash_pkgs,
+    #'--with-mpi-dir=/home/users/balay/soft/instinct/gcc-10.2.0/mpich-4.1',
+    #'--with-blaslapack-dir=/home/users/balay/soft/instinct/gcc-10.2.0/fblaslapack',
+    '--with-make-np=24',
+    '--with-make-test-np=8',
+    '--with-hipc=/opt/rocm-5.4.3/bin/hipcc',
+    '--with-hip-dir=/opt/rocm-5.4.3',
+    'COPTFLAGS=-g -O',
+    'FOPTFLAGS=-g -O',
+    'CXXOPTFLAGS=-g -O',
+    'HIPOPTFLAGS=-g -O',
+    '--with-cuda=0',
+    '--with-hip=1',
+    '--with-precision=double',
+    '--with-clanguage=c',
+    '--download-kokkos',
+    '--download-kokkos-kernels',
+    # '--download-hypre', # does not support complex on the GPU yet
+    # '--download-mfem',  # requires hypre
+    '--download-magma',
+    '--with-magma-fortran-bindings=0',
+    '--download-metis',
+    '--with-strict-petscerrorcode',
+    '--with-scalar-type=complex',
+    #'--with-coverage',
+  ]
+
+  configure.petsc_configure(configure_options)
