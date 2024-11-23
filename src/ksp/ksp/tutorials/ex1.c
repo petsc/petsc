279b6799eee79dfb70b33177e1474eed304ab281
Pierre Jolivet <pierre@joliv.et>
2024-11-23 08:32:27 +0100

    ksp_ksp_tutorials-ex1: fix -n 1 case

--- src/ksp/ksp/tutorials/ex1.c
+++ src/ksp/ksp/tutorials/ex1.c
@@ -71,16 +71,18 @@ int main(int argc, char **args)
     col[2] = i + 1;
     PetscCall(MatSetValues(A, 1, &i, 3, col, value, INSERT_VALUES));
   }
-  i      = n - 1;
-  col[0] = n - 2;
-  col[1] = n - 1;
-  PetscCall(MatSetValues(A, 1, &i, 2, col, value, INSERT_VALUES));
+  if (n > 1) {
+    i      = n - 1;
+    col[0] = n - 2;
+    col[1] = n - 1;
+    PetscCall(MatSetValues(A, 1, &i, 2, col, value, INSERT_VALUES));
+  }
   i        = 0;
   col[0]   = 0;
   col[1]   = 1;
   value[0] = 2.0;
   value[1] = -1.0;
-  PetscCall(MatSetValues(A, 1, &i, 2, col, value, INSERT_VALUES));
+  PetscCall(MatSetValues(A, 1, &i, n > 1 ? 2 : 1, col, value, INSERT_VALUES));
   PetscCall(MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY));
   PetscCall(MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY));
 
