fea1ad4e0ec7b78000d0540c11a65abd4eedc25d
Satish Balay <balay@mcs.anl.gov>
2025-03-27 21:06:08 -0500

    petsc, petsc4py fix warning triggered by setuptools v78

--- src/binding/petsc4py/setup.py
+++ src/binding/petsc4py/setup.py
@@ -70,7 +70,6 @@ tarball = F('{pyname}-%s.tar.gz' % get_version())
 download = '/'.join([pypiroot, pypislug, tarball])
 
 classifiers = """
-License :: OSI Approved :: BSD License
 Operating System :: POSIX
 Intended Audience :: Developers
 Intended Audience :: Science/Research
