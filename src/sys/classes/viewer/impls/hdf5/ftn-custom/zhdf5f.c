a968899dcf54030bc4915260052f186034d09103
Tapashree Pradhan <tapashreepradhan@Tapashrees-MacBook-Pro.local>
2025-04-08 14:59:57 +0200

    Fortran: Fix HDF5 attribute writing interface
    - Ensure real attribute writing routine is available unconditionally in real-only builds
    - Add new wrappers in zhdf5f.c for writing:
      - Integer attributes
      - Scalar (PetscScalar) attributes
      - Real attributes
    - Update petscviewer.h90 to expose the real version consistently
    - Adds a minimal test program (ex80f.F90) to verify functionality
    - Update test case to reflect proper names
    Fixes: #194
    
    Thanks-to: Martin Diehl
    
    add dependency for HDF5 in test
    
    fix the c stubs to remove garbage values
    
    update the test case to reflect proper names
    
    fix clang format

--- src/sys/classes/viewer/impls/hdf5/ftn-custom/zhdf5f.c
+++ src/sys/classes/viewer/impls/hdf5/ftn-custom/zhdf5f.c
@@ -2,9 +2,15 @@
 #include <petscviewerhdf5.h>
 
 #if defined(PETSC_HAVE_FORTRAN_CAPS)
-  #define petscviewerhdf5opengroup_ PETSCVIEWERHDF5OPENGROUP
+  #define petscviewerhdf5opengroup_            PETSCVIEWERHDF5OPENGROUP
+  #define petscviewerhdf5writeattributeint_    PETSCVIEWERHDF5WRITEATTRIBUTEINT
+  #define petscviewerhdf5writeattributescalar_ PETSCVIEWERHDF5WRITEATTRIBUTESCALAR
+  #define petscviewerhdf5writeattributereal_   PETSCVIEWERHDF5WRITEATTRIBUTEREAL
 #elif !defined(PETSC_HAVE_FORTRAN_UNDERSCORE)
-  #define petscviewerhdf5opengroup_ petscviewerhdf5opengroup
+  #define petscviewerhdf5opengroup_            petscviewerhdf5opengroup
+  #define petscviewerhdf5writeattributeint_    petscviewerhdf5writeattributeint
+  #define petscviewerhdf5writeattributescalar_ petscviewerhdf5writeattributescalar
+  #define petscviewerhdf5writeattributereal_   petscviewerhdf5writeattributereal
 #endif
 
 PETSC_EXTERN void petscviewerhdf5opengroup_(PetscViewer *viewer, char path[], hid_t *fileId, hid_t *groupId, int *ierr, PETSC_FORTRAN_CHARLEN_T len)
@@ -14,4 +20,40 @@ PETSC_EXTERN void petscviewerhdf5opengroup_(PetscViewer *viewer, char path[], hi
   FIXCHAR(path, len, c1);
   *ierr = PetscViewerHDF5OpenGroup(*viewer, c1, fileId, groupId);
   FREECHAR(path, c1);
+}
+
+PETSC_EXTERN void petscviewerhdf5writeattributeint_(PetscViewer *viewer, const char parent[], const char name[], PetscInt *value, int *ierr, PETSC_FORTRAN_CHARLEN_T len1, PETSC_FORTRAN_CHARLEN_T len2)
+{
+  char *c1;
+  char *c2;
+
+  FIXCHAR(parent, len1, c1);
+  FIXCHAR(name, len2, c2);
+  *ierr = PetscViewerHDF5WriteAttribute(*viewer, c1, c2, PETSC_INT, value);
+  FREECHAR(parent, c1);
+  FREECHAR(name, c2);
+}
+
+PETSC_EXTERN void petscviewerhdf5writeattributescalar_(PetscViewer *viewer, const char parent[], const char name[], PetscScalar *value, int *ierr, PETSC_FORTRAN_CHARLEN_T len1, PETSC_FORTRAN_CHARLEN_T len2)
+{
+  char *c1;
+  char *c2;
+
+  FIXCHAR(parent, len1, c1);
+  FIXCHAR(name, len2, c2);
+  *ierr = PetscViewerHDF5WriteAttribute(*viewer, c1, c2, PETSC_SCALAR, value);
+  FREECHAR(parent, c1);
+  FREECHAR(name, c2);
+}
+
+PETSC_EXTERN void petscviewerhdf5writeattributereal_(PetscViewer *viewer, const char parent[], const char name[], PetscReal *value, int *ierr, PETSC_FORTRAN_CHARLEN_T len1, PETSC_FORTRAN_CHARLEN_T len2)
+{
+  char *c1;
+  char *c2;
+
+  FIXCHAR(parent, len1, c1);
+  FIXCHAR(name, len2, c2);
+  *ierr = PetscViewerHDF5WriteAttribute(*viewer, c1, c2, PETSC_REAL, value);
+  FREECHAR(parent, c1);
+  FREECHAR(name, c2);
 }
