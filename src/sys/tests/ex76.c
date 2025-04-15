444bb3e782ca53663fa265312c4af9b146803a6e
Jed Brown <jed@jedbrown.org>
2025-04-15 16:01:00 -0600

    Replace egrep with grep -E
    On recent systems, egrep will print this on stderr:
    
    egrep: warning: egrep is obsolescent; using grep -E
    
    This has been fixed before (petsc/petsc!5674), but crept back in.

--- src/sys/tests/ex76.c
+++ src/sys/tests/ex76.c
@@ -17,6 +17,6 @@ int main(int argc, char **args)
    test:
      requires: !defined(PETSC_HAVE_MPI_LARGE_COUNT) defined(PETSC_HAVE_64_BIT_INDICES)
      args: -petsc_ci_portable_error_output -error_output_stdout
-     filter: grep -E "(PETSC ERROR)" | egrep "(Error Created|CreateError\(\)|main\(\))"
+     filter: grep -E "(PETSC ERROR)" | grep -E "(Error Created|CreateError\(\)|main\(\))"
 
 TEST*/
