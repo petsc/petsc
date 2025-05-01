90907b5b315d0933cea8c81b7e425a9e835c01bb
Barry Smith <bsmith@mcs.anl.gov>
2025-05-01 15:43:50 -0400

    Fix some TS manual pages

--- src/ts/interface/sensitivity/tssen.c
+++ src/ts/interface/sensitivity/tssen.c
@@ -8,7 +8,7 @@ PetscLogEvent TS_AdjointStep, TS_ForwardStep, TS_JacobianPEval;
 /* ------------------------ Sensitivity Context ---------------------------*/
 
 /*@C
-  TSSetRHSJacobianP - Sets the function that computes the Jacobian of G w.r.t. the parameters P where U_t = G(U,P,t), as well as the location to store the matrix.
+  TSSetRHSJacobianP - Sets the function that computes the Jacobian of $G$ w.r.t. the parameters $p$ where $U_t = G(U,p,t)$, as well as the location to store the matrix.
 
   Logically Collective
 
@@ -42,7 +42,7 @@ PetscErrorCode TSSetRHSJacobianP(TS ts, Mat Amat, TSRHSJacobianPFn *func, void *
 }
 
 /*@C
-  TSGetRHSJacobianP - Gets the function that computes the Jacobian of G w.r.t. the parameters P where U_t = G(U,P,t), as well as the location to store the matrix.
+  TSGetRHSJacobianP - Gets the function that computes the Jacobian of $G $ w.r.t. the parameters $p$ where $ U_t = G(U,p,t)$, as well as the location to store the matrix.
 
   Logically Collective
 
@@ -108,7 +108,7 @@ PetscErrorCode TSComputeRHSJacobianP(TS ts, PetscReal t, Vec U, Mat Amat)
 }
 
 /*@C
-  TSSetIJacobianP - Sets the function that computes the Jacobian of F w.r.t. the parameters P where F(Udot,U,t) = G(U,P,t), as well as the location to store the matrix.
+  TSSetIJacobianP - Sets the function that computes the Jacobian of $F$ w.r.t. the parameters $p$ where $F(Udot,U,p,t) = G(U,p,t)$, as well as the location to store the matrix.
 
   Logically Collective
 
@@ -123,14 +123,14 @@ PetscErrorCode TSComputeRHSJacobianP(TS ts, PetscReal t, Vec U, Mat Amat)
 . t     - current timestep
 . U     - input vector (current ODE solution)
 . Udot  - time derivative of state vector
-. shift - shift to apply, see note below
+. shift - shift to apply, see the note in `TSSetIJacobian()`
 . A     - output matrix
 - ctx   - [optional] user-defined function context
 
   Level: intermediate
 
   Note:
-  Amat has the same number of rows and the same row parallel layout as u, Amat has the same number of columns and parallel layout as p
+  `Amat` has the same number of rows and the same row parallel layout as `u`, `Amat` has the same number of columns and parallel layout as `p`
 
 .seealso: [](ch_ts), `TSSetRHSJacobianP()`, `TS`
 @*/
@@ -161,7 +161,7 @@ PetscErrorCode TSSetIJacobianP(TS ts, Mat Amat, PetscErrorCode (*func)(TS ts, Pe
 . U     - state vector
 . Udot  - time derivative of state vector
 . shift - shift to apply, see note below
-- imex  - flag indicates if the method is IMEX so that the RHSJacobianP should be kept separate
+- imex  - flag indicates if the method is IMEX so that the `RHSJacobianP` should be kept separate
 
   Output Parameter:
 . Amat - Jacobian matrix
