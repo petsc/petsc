737758cee214a2a77eb3db9a8fa22b9926ebcdcf
Daniel Otto de Mentock <d.mentock@mpie.de>
2025-05-01 22:35:07 +0200

    configure: update boost build
    - update to v1.88.0
    - change -boost-headers-only to -download-boost-headers-only
    - add -download-boost-bootstrap-arguments
    - set toolset via bootstrap.sh for gcc, intel-linux, clang
    - use working precompiled headers option for gcc and clang
    - set compiler via user-config.jam

--- config/BuildSystem/config/packages/boost.py
+++ config/BuildSystem/config/packages/boost.py
@@ -1,10 +1,12 @@
 from __future__ import generators
 import config.package
+import sysconfig
+from pathlib import Path
 
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.version           = '1.87.0'
+    self.version           = '1.88.0'
     self.download          = ['https://archives.boost.io/release/'+self.version+'/source/boost_'+self.version.replace('.','_')+'.tar.bz2',
                               'https://web.cels.anl.gov/projects/petsc/download/externalpackages/boost_'+self.version.replace('.','_')+'.tar.bz2']
     self.includes          = ['boost/multi_index_container.hpp']
@@ -12,42 +14,72 @@ class Configure(config.package.Package):
     self.buildLanguages    = ['Cxx']
     self.downloadonWindows = 1
     self.useddirectly      = 0
-    return
 
   def setupHelp(self, help):
     import nargs
     config.package.Package.setupHelp(self, help)
-    help.addArgument('BOOST', '-boost-headers-only=<bool>', nargs.ArgBool(None, 0, 'When true, do not build boost libraries, only install headers'))
+    help.addArgument('BOOST', '-download-boost-headers-only=<bool>', nargs.ArgBool(None, 0, 'When true, do not build Boost libraries, only install headers'))
+    help.addArgument('BOOST', '-download-boost-bootstrap-arguments=<string>', nargs.ArgString(None, 0, 'Additional arguments for bootstrap of Boost build'))
 
   def Install(self):
-    import shutil
-    import os
+    conffile = Path(self.packageDir) / (self.package + '.petscconf')
+    conffile.write_text(self.installDir)
 
-    conffile = os.path.join(self.packageDir,self.package+'.petscconf')
-    fd = open(conffile, 'w')
-    fd.write(self.installDir)
-    fd.close()
-    if not self.installNeeded(conffile): return self.installDir
+    if not self.installNeeded(str(conffile)):
+      return self.installDir
 
-    if self.framework.argDB['boost-headers-only']:
-       boostIncludeDir = os.path.join(os.path.join(self.installDir, self.includedir), 'boost')
-       self.logPrintBox('Configure option --boost-headers-only is ENABLED ... boost libraries will not be built')
-       self.logPrintBox('Installing boost headers, this should not take long')
-       try:
-         if os.path.lexists(boostIncludeDir): os.remove(boostIncludeDir)
-         output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+';' + 'ln -s $PWD/boost/ ' + boostIncludeDir, timeout=6000, log = self.log)
-       except RuntimeError as e:
-         raise RuntimeError('Error linking '+self.packageDir+' to '+ boostIncludeDir)
-       return self.installDir
+    if self.argDB['download-boost-headers-only']:
+      boostIncludeDir = Path(self.installDir) / self.includedir / 'boost'
+      self.logPrintBox('Configure option --boost-headers-only is ENABLED ... boost libraries will not be built')
+      self.logPrintBox('Installing boost headers, this should not take long')
+      try:
+        if boostIncludeDir.exists() or boostIncludeDir.is_symlink():
+          boostIncludeDir.unlink()
+        cmd = 'cd {} && ln -s $PWD/boost {}'.format(self.packageDir, boostIncludeDir)
+        config.base.Configure.executeShellCommand(cmd, timeout=6000, log=self.log)
+      except RuntimeError as e:
+        raise RuntimeError('Error linking Boost headers:\n'+str(e))
     else:
-       if not self.checkCompile('#include <bzlib.h>', ''):
-         raise RuntimeError('Boost requires bzlib.h. Please install it in default compiler search location.')
+      if not self.checkCompile('#include <bzlib.h>', ''):
+        raise RuntimeError('Boost requires bzlib.h. Please install it in default compiler search location.')
 
-       self.log.write('boostDir = '+self.packageDir+' installDir '+self.installDir+'\n')
-       self.logPrintBox('Building and installing boost; this may take many minutes')
-       try:
-         output,err,ret  = config.base.Configure.executeShellCommand('cd '+self.packageDir+'; ./bootstrap.sh --prefix='+self.installDir+'; ./b2 -j'+str(self.make.make_np)+'; ./b2 install', timeout=6000, log = self.log)
-       except RuntimeError as e:
-         raise RuntimeError('Error building/install Boost files from '+os.path.join(self.packageDir, 'Boost')+' to '+self.packageDir)
-       self.postInstall(output+err,conffile)
+      if not (Path(sysconfig.get_paths()['include']) / 'pyconfig.h').is_file():
+        raise RuntimeError('pyconfig.h missing: Boost requires python development version to be installed. (pythonX.x-dev)')
+
+      with self.Language('Cxx'):
+          cxx = self.getCompiler().lower()
+          cxxflags = self.getCompilerFlags()
+
+      if config.setCompilers.Configure.isGNU(cxx, self.log):
+        toolset = 'gcc'
+        pch = 'on'
+      elif config.setCompilers.Configure.isOneAPI(cxx, self.log) or config.setCompilers.Configure.isIntel(cxx, self.log):
+        toolset = 'intel-linux'
+        pch = 'off' # https://github.com/bfgroup/b2/issues/413
+      elif config.setCompilers.Configure.isClang(cxx, self.log):
+        toolset='clang'
+        pch='on'
+      else:
+        raise RuntimeError('Invalid CXX compiler specifield for boost: {}'.format(cxx))
+
+      self.logPrintBox('Building Boost with toolset "{}", compiler "{}"'.format(toolset, cxx))
+
+      jamfile = Path(self.packageDir) / 'user-config.jam'
+      jamfile.write_text('using {} : : {} : <cxxflags>"{}" ;\n'.format(toolset, cxx, cxxflags))
+
+      if 'download-boost-bootstrap-arguments' in self.argDB and self.argDB['download-boost-bootstrap-arguments']:
+        bootstrap_arguments = self.argDB['download-boost-bootstrap-arguments']
+      else:
+        bootstrap_arguments = ''
+
+      bootstrap_cmd = 'cd {} && ./bootstrap.sh --with-toolset={} --prefix={}'.format(self.packageDir, toolset, self.installDir)
+      out, err, ret = config.base.Configure.executeShellCommand(bootstrap_cmd, timeout=6000, log=self.log)
+
+      build_cmd = 'cd {} && ./b2 toolset={} {} pch={} -j{}'.format(self.packageDir, toolset, bootstrap_arguments, pch, self.make.make_np)
+      out, err, ret = config.base.Configure.executeShellCommand(build_cmd, timeout=6000, log=self.log)
+
+      install_cmd = 'cd {} && ./b2 toolset={} {} pch={} -j{} install'.format(self.packageDir, toolset, bootstrap_arguments, pch, self.make.make_np)
+      out, err, ret = config.base.Configure.executeShellCommand(install_cmd, timeout=6000, log=self.log)
+
+      self.postInstall(out + err, str(conffile))
     return self.installDir
