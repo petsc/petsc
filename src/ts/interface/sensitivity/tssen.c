3beb8511b5c2af4e2554bd30717063f65f0f458c
Barry Smith <bsmith@mcs.anl.gov>
2025-05-01 17:54:00 -0400

    add missing C function TSGetIJacobianP()

--- src/ts/interface/sensitivity/tssen.c
+++ src/ts/interface/sensitivity/tssen.c
@@ -150,6 +150,46 @@ PetscErrorCode TSSetIJacobianP(TS ts, Mat Amat, PetscErrorCode (*func)(TS ts, Pe
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
+/*@C
+  TSGetIJacobianP - Gets the function that computes the Jacobian of $ F$ w.r.t. the parameters $p$ where $F(Udot,U,p,t) = G(U,p,t) $, as well as the location to store the matrix.
+
+  Logically Collective
+
+  Input Parameter:
+. ts - `TS` context obtained from `TSCreate()`
+
+  Output Parameters:
++ Amat - JacobianP matrix
+. func - the function that computes the JacobianP
+- ctx  - [optional] user-defined function context
+
+  Calling sequence of `func`:
++ ts    - the `TS` context
+. t     - current timestep
+. U     - input vector (current ODE solution)
+. Udot  - time derivative of state vector
+. shift - shift to apply, see the note in `TSSetIJacobian()`
+. A     - output matrix
+- ctx   - [optional] user-defined function context
+
+  Level: intermediate
+
+  Note:
+  `Amat` has the same number of rows and the same row parallel layout as `u`, `Amat` has the same number of columns and parallel layout as `p`
+
+.seealso: [](ch_ts), `TSSetRHSJacobianP()`, `TS`, `TSSetIJacobianP()`, `TSGetRHSJacobianP()`
+@*/
+PetscErrorCode TSGetIJacobianP(TS ts, Mat *Amat, PetscErrorCode (**func)(TS ts, PetscReal t, Vec U, Vec Udot, PetscReal shift, Mat A, void *ctx), void **ctx)
+{
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(ts, TS_CLASSID, 1);
+
+  if (func) *func = ts->ijacobianp;
+  if (ctx) *ctx = ts->ijacobianpctx;
+  if (Amat) *Amat = ts->Jacp;
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
 /*@
   TSComputeIJacobianP - Runs the user-defined IJacobianP function.
 
