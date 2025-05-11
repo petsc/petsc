7813f1e2699829593ca107d33833e4c2c32d98f9
Junchao Zhang <jczhang@anl.gov>
2025-05-10 21:13:34 -0500

    sycl: use sycl::cpu_selector_v as sycl::host_selector() is removed

--- src/sys/objects/device/impls/sycl/sycldevice.sycl.cxx
+++ src/sys/objects/device/impls/sycl/sycldevice.sycl.cxx
@@ -92,7 +92,7 @@ private:
   static ::sycl::device chooseSYCLDevice_(int id)
   {
     if (id == PETSC_SYCL_DEVICE_HOST) {
-      return ::sycl::device(::sycl::host_selector());
+      return ::sycl::device(::sycl::cpu_selector_v);
     } else {
       return ::sycl::device::get_devices(::sycl::info::device_type::gpu)[id];
     }
