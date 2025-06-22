208c597f89a15781a397de7c77fa3322270b8cdc
Jose E. Roman <jroman@dsic.upv.es>
2025-06-22 12:29:00 +0200

    petsc4py: prevent warning about absolute paths in manifest

--- src/binding/petsc4py/conf/confpetsc.py
+++ src/binding/petsc4py/conf/confpetsc.py
@@ -753,6 +753,13 @@ class build_ext(_build_ext):
                 outputs.append(outfile)
         return list(set(outputs))
 
+    def get_source_files(self):
+        orig = log.set_threshold(log.WARN)
+        try:
+            return super().get_source_files()
+        finally:
+            log.set_threshold(orig)
+
 
 class install(_install):
     def initialize_options(self):
