f0c227fb9e7ece2f21a83203a567dc40969a87c2
Junchao Zhang <jczhang@anl.gov>
2025-07-03 16:14:13 -0500

    test: enhance the test for MatCreateMPIAIJWithSeqAIJ

--- src/mat/tests/ex303.c
+++ src/mat/tests/ex303.c
@@ -4,119 +4,103 @@ static char help[] = "Testing MatCreateMPIAIJWithSeqAIJ().\n\n";
 
 int main(int argc, char **argv)
 {
-  Mat             A, B;
-  PetscInt        i, j, column, M, N, m, n;
-  PetscInt       *oi, *oj, nd;
-  const PetscInt *garray;
-  PetscInt       *garray_h;
-  PetscScalar     value;
-  PetscScalar    *oa;
-  PetscRandom     rctx;
-  PetscBool       equal, done;
-  Mat             AA, AB;
-  PetscMPIInt     size, rank;
-  MatType         mat_type;
+  Mat                mat, mat2;
+  Mat                A, B;   // diag, offdiag of mat
+  Mat                A2, B2; // diag, offdiag of mat2
+  PetscInt           M, N, m = 5, n = 6, d_nz = 3, o_nz = 4;
+  PetscInt          *bi, *bj, nd;
+  const PetscScalar *ba;
+  const PetscInt    *garray;
+  PetscInt          *garray_h;
+  PetscBool          equal, done;
+  PetscMPIInt        size;
+  char               mat_type[PETSC_MAX_PATH_LEN];
+  PetscBool          flg;
 
   PetscFunctionBeginUser;
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
   PetscCallMPI(MPI_Comm_size(PETSC_COMM_WORLD, &size));
   PetscCheck(size > 1, PETSC_COMM_WORLD, PETSC_ERR_WRONG_MPI_SIZE, "Must run with 2 or more processes");
-  PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
 
-  /* Create a mpiaij matrix for checking */
-  PetscCall(MatCreateAIJ(PETSC_COMM_WORLD, 5, 5, PETSC_DECIDE, PETSC_DECIDE, 0, NULL, 0, NULL, &A));
-  PetscCall(MatSetFromOptions(A));
-  PetscCall(MatSetOption(A, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_FALSE));
-  PetscCall(MatSetUp(A));
-  PetscCall(PetscRandomCreate(PETSC_COMM_WORLD, &rctx));
-  PetscCall(PetscRandomSetFromOptions(rctx));
+  // Create a MATMPIAIJ matrix for checking against
+  PetscCall(MatCreateAIJ(PETSC_COMM_WORLD, m, n, PETSC_DECIDE, PETSC_DECIDE, d_nz, NULL, o_nz, NULL, &mat));
+  PetscCall(MatSetRandom(mat, NULL));
+  PetscCall(MatGetSize(mat, &M, &N));
+  PetscCall(MatGetLocalSize(mat, &m, &n));
+  PetscCall(MatMPIAIJGetSeqAIJ(mat, &A, &B, &garray));
 
-  for (i = 5 * rank; i < 5 * rank + 5; i++) {
-    for (j = 0; j < 5 * size; j++) {
-      PetscCall(PetscRandomGetValue(rctx, &value));
-      column = (PetscInt)(5 * size * PetscRealPart(value));
-      PetscCall(PetscRandomGetValue(rctx, &value));
-      PetscCall(MatSetValues(A, 1, &i, 1, &column, &value, INSERT_VALUES));
-    }
-  }
-  PetscCall(MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY));
-  PetscCall(MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY));
-  PetscCall(MatGetSize(A, &M, &N));
-  PetscCall(MatGetLocalSize(A, &m, &n));
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-mat_type", mat_type, sizeof(mat_type), &flg));
+  if (!flg) PetscCall(PetscStrncpy(mat_type, MATSEQAIJ, sizeof(mat_type))); // Default to MATAIJ
+  PetscCall(MatConvert(A, mat_type, MAT_INITIAL_MATRIX, &A2));              // Copy A, B to A2, B2 but in the given mat_type
+  PetscCall(MatConvert(B, mat_type, MAT_INITIAL_MATRIX, &B2));
 
-  PetscCall(MatMPIAIJGetSeqAIJ(A, &AA, &AB, &garray));
-
-  Mat output_mat_local, output_mat_nonlocal, output_mat_local_copy, output_mat_nonlocal_copy;
-
-  PetscCall(MatConvert(AA, MATSAME, MAT_INITIAL_MATRIX, &output_mat_local));
-  PetscCall(MatConvert(AB, MATSAME, MAT_INITIAL_MATRIX, &output_mat_nonlocal));
-  PetscCall(MatConvert(AA, MATSAME, MAT_INITIAL_MATRIX, &output_mat_local_copy));
-
-  // The garray passed in has to be on the host, but it can be created
-  // on device and copied to the host
-  // We're just going to copy the existing host values here
+  // The garray passed in has to be on the host, but it can be created on device and copied to the host.
+  // We're just going to copy the existing host values here.
   PetscInt nonlocalCols;
-  PetscCall(MatGetLocalSize(AB, NULL, &nonlocalCols));
+  PetscCall(MatGetLocalSize(B, NULL, &nonlocalCols));
   PetscCall(PetscMalloc1(nonlocalCols, &garray_h));
   for (int i = 0; i < nonlocalCols; i++) { garray_h[i] = garray[i]; }
 
-  // Build our MPI matrix
-  // If we provide garray and output_mat_nonlocal with local indices and the compactified size
-  // it doesn't compactify
-  PetscCall(MatCreateMPIAIJWithSeqAIJ(PETSC_COMM_WORLD, M, N, output_mat_local, output_mat_nonlocal, garray_h, &B));
-
-  PetscCall(MatEqual(A, B, &equal));
+  // 1) Test MatCreateMPIAIJWithSeqAIJ with compressed off-diag.
+  PetscCall(MatCreateMPIAIJWithSeqAIJ(PETSC_COMM_WORLD, M, N, A2, B2, garray_h, &mat2));
+  PetscCall(MatEqual(mat, mat2, &equal));                   // might directly compare value arrays
+  if (equal) PetscCall(MatMultEqual(mat, mat2, 2, &equal)); // compare via MatMult()
   PetscCheck(equal, PETSC_COMM_SELF, PETSC_ERR_PLIB, "Likely a bug in MatCreateMPIAIJWithSeqAIJ()");
-  PetscCall(MatDestroy(&B));
+  PetscCall(MatDestroy(&mat2)); // A2 and B2 are also destroyed
 
-  // ~~~~~~~~~~~~~~~~~
-  // Test MatCreateMPIAIJWithSeqAIJ with compactification
-  // This is just for testing - would be silly to do this in practice
-  // ~~~~~~~~~~~~~~~~~
-  garray_h = NULL;
-  PetscCall(MatGetRowIJ(AB, 0, PETSC_FALSE, PETSC_FALSE, &nd, (const PetscInt **)&oi, (const PetscInt **)&oj, &done));
-  PetscCall(MatSeqAIJGetArray(AB, &oa));
+  // 2) Test MatCreateMPIAIJWithSeqAIJ with non-compressed off-diag.
+  PetscCall(MatConvert(A, mat_type, MAT_INITIAL_MATRIX, &A2));
 
-  // Create a version of AB of size N with global indices
-  PetscCall(MatGetType(AB, &mat_type));
-  PetscCall(MatCreate(PETSC_COMM_SELF, &output_mat_nonlocal_copy));
-  PetscCall(MatSetSizes(output_mat_nonlocal_copy, m, N, m, N));
-  PetscCall(MatSetType(output_mat_nonlocal_copy, mat_type));
-  PetscCall(MatSeqAIJSetPreallocation(output_mat_nonlocal_copy, oi[5], NULL));
-
-  // Fill the matrix
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < oi[i + 1] - oi[i]; j++) { PetscCall(MatSetValue(output_mat_nonlocal_copy, i, garray[oj[oi[i] + j]], oa[oi[i] + j], INSERT_VALUES)); }
+  // Create a version of B2 of size N with global indices
+  PetscCall(MatGetRowIJ(B, 0, PETSC_FALSE, PETSC_FALSE, &nd, (const PetscInt **)&bi, (const PetscInt **)&bj, &done));
+  PetscCall(MatSeqAIJGetArrayRead(B, &ba));
+  PetscCall(MatCreate(PETSC_COMM_SELF, &B2));
+  PetscCall(MatSetSizes(B2, m, N, m, N));
+  PetscCall(MatSetType(B2, mat_type));
+  PetscCall(MatSeqAIJSetPreallocation(B2, o_nz, NULL));
+  for (int i = 0; i < m; i++) { // Fill B2 with values from B
+    for (int j = 0; j < bi[i + 1] - bi[i]; j++) { PetscCall(MatSetValue(B2, i, garray[bj[bi[i] + j]], ba[bi[i] + j], INSERT_VALUES)); }
   }
-  PetscCall(MatAssemblyBegin(output_mat_nonlocal_copy, MAT_FINAL_ASSEMBLY));
-  PetscCall(MatAssemblyEnd(output_mat_nonlocal_copy, MAT_FINAL_ASSEMBLY));
+  PetscCall(MatAssemblyBegin(B2, MAT_FINAL_ASSEMBLY));
+  PetscCall(MatAssemblyEnd(B2, MAT_FINAL_ASSEMBLY));
 
-  PetscCall(MatRestoreRowIJ(AB, 0, PETSC_FALSE, PETSC_FALSE, &nd, (const PetscInt **)&oi, (const PetscInt **)&oj, &done));
-  PetscCall(MatSeqAIJRestoreArray(AB, &oa));
+  PetscCall(MatRestoreRowIJ(B, 0, PETSC_FALSE, PETSC_FALSE, &nd, (const PetscInt **)&bi, (const PetscInt **)&bj, &done));
+  PetscCall(MatSeqAIJRestoreArrayRead(B, &ba));
 
-  // Build our MPI matrix
-  // If we don't provide garray and output_mat_local_copy with global indices and size N
-  // it will do compactification
-  PetscCall(MatCreateMPIAIJWithSeqAIJ(PETSC_COMM_WORLD, M, N, output_mat_local_copy, output_mat_nonlocal_copy, garray_h, &B));
-
-  PetscCall(MatEqual(A, B, &equal));
+  PetscCall(MatCreateMPIAIJWithSeqAIJ(PETSC_COMM_WORLD, M, N, A2, B2, NULL, &mat2));
+  PetscCall(MatEqual(mat, mat2, &equal));
+  if (equal) PetscCall(MatMultEqual(mat, mat2, 2, &equal));
   PetscCheck(equal, PETSC_COMM_SELF, PETSC_ERR_PLIB, "Likely a bug in MatCreateMPIAIJWithSeqAIJ()");
-  PetscCall(MatDestroy(&B));
 
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  /* Free spaces */
-  PetscCall(PetscRandomDestroy(&rctx));
-  PetscCall(MatDestroy(&A));
+  PetscCall(MatDestroy(&mat));
+  PetscCall(MatDestroy(&mat2));
   PetscCall(PetscFinalize());
   return 0;
 }
 
 /*TEST
 
-  test:
+  testset:
     nsize: 2
-    args: -mat_type aij
     output_file: output/empty.out
+
+    test:
+      suffix: 1
+      args: -mat_type aij
+
+    test:
+      suffix: 2
+      args: -mat_type aijkokkos
+      requires: kokkos_kernels
+
+    test:
+      suffix: 3
+      args: -mat_type aijcusparse
+      requires: cuda
+
+    test:
+      suffix: 4
+      args: -mat_type aijhipsparse
+      requires: hip
 
 TEST*/
