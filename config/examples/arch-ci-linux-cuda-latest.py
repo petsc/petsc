bab4f31fbc56a2391a69d444429935ae2c2add2d
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-09-10 01:28:06 +0100

    CI: update linux-cuda-latest to use cuda-13
    - linux-cuda-latest: update to use compatible hypre snapshot from hypre-3.0
    - SuperLU_DIST: update to cuda-13 compatible snapshot from master
    - SuperLU_DIST: remove unused CMake variables
    - hypre: Remove unneeded brokengnu23 flag

--- config/examples/arch-ci-linux-cuda-latest.py
+++ config/examples/arch-ci-linux-cuda-latest.py
@@ -16,9 +16,13 @@ if __name__ == '__main__':
     'CXXOPTFLAGS=-g -O',
     '--with-precision=double',
     '--with-clanguage=c',
-    '--with-mpi-dir=/software/mpich-430p2-cuda129',
-    '--with-cuda-dir=/usr/local/cuda-12.9',
+    '--with-mpi-dir=/software/mpich-43-main-0476502690-cuda130',
+    '--with-cuda-dir=/usr/local/cuda-13.0',
     '--download-hypre=1',
+    '--download-hypre-commit=b950aec946b3b8b85f2a66ce26f1cb13438ec903', # hypre-3.0 snapshot from Sep 12, 2025
+    '--download-hypre-configure-arguments=--without-umpire',
+    '--download-superlu_dist',
+    '--with-cxx-dialect=17',
     '--with-strict-petscerrorcode',
   ]
 
