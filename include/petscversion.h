95f7d040108ce4934b071f1d6e3322ab842fd355
Satish Balay <balay@mcs.anl.gov>
2025-09-29 13:21:51 -0500

    revert sources to dev   - reset PETSC_VERSION_RELEASE   - reset doc/changes/dev.md

--- include/petscversion.h
+++ include/petscversion.h
@@ -4,7 +4,7 @@
 
 /* MANSEC = Sys */
 
-#define PETSC_VERSION_RELEASE    1
+#define PETSC_VERSION_RELEASE    0
 #define PETSC_VERSION_MAJOR      3
 #define PETSC_VERSION_MINOR      24
 #define PETSC_VERSION_SUBMINOR   0
