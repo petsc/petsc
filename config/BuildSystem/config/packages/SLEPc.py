78236e8f2eb8dd1f2d91c1e80df293f289a899f8
Satish Balay <balay@mcs.anl.gov>
2025-09-29 13:17:05 -0500

    SLEPc: update to latest  (pre-3.24.0) snapshot

--- config/BuildSystem/config/packages/SLEPc.py
+++ config/BuildSystem/config/packages/SLEPc.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit              = '705a8d354db0e00f6e8f00244a812a4f66c4b715' # jose/ftn-object-init Sep-23-2025
+    self.gitcommit              = '0781cbf813ba3e39cd03b7f735dbbaa9fc8e0756' # jose/new-release-3.24
     #self.gitcommit             = 'v'+self.version
     self.download               = ['git://https://gitlab.com/slepc/slepc.git','https://gitlab.com/slepc/slepc/-/archive/'+self.gitcommit+'/slepc-'+self.gitcommit+'.tar.gz']
     self.functions              = []
