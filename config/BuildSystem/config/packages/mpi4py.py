69dbca04830e4855b3788e253726b99a77396ffb
Pierre Jolivet <pierre@joliv.et>
2025-10-11 17:11:31 +0200

    configure: mpi4py 4.1.1

--- config/BuildSystem/config/packages/mpi4py.py
+++ config/BuildSystem/config/packages/mpi4py.py
@@ -5,7 +5,9 @@ import script
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.download          = ['https://github.com/mpi4py/mpi4py/releases/download/4.1.0/mpi4py-4.1.0.tar.gz']
+    self.version           = '4.1.1'
+    self.download          = ['https://github.com/mpi4py/mpi4py/releases/download/' + self.version + '/mpi4py-' + self.version + '.tar.gz',
+                              'https://web.cels.anl.gov/projects/petsc/download/externalpackages/mpi4py-'+self.version+'.tar.gz']
     self.functions         = []
     self.includes          = []
     self.useddirectly      = 0
