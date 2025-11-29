6220ef827cfb29f143b71dc8d0e16ee8b5f4575d
Satish Balay <balay@mcs.anl.gov>
2025-11-29 16:32:33 -0600

    Increase patchlevel to 3.24.2

--- include/petscversion.h
+++ include/petscversion.h
@@ -7,7 +7,7 @@
 #define PETSC_VERSION_RELEASE    1
 #define PETSC_VERSION_MAJOR      3
 #define PETSC_VERSION_MINOR      24
-#define PETSC_VERSION_SUBMINOR   1
+#define PETSC_VERSION_SUBMINOR   2
 #define PETSC_RELEASE_DATE       "Sep 29, 2025"
 #define PETSC_VERSION_DATE       "unknown"
 
