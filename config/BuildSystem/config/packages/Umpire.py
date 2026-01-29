5c2d5736af4bb3c6f6a876c209e20b0bc9d41e18
Junchao Zhang <jczhang@anl.gov>
2026-01-29 16:32:22 -0600

    Umpire: update to 2025.12.0

--- config/BuildSystem/config/packages/Umpire.py
+++ config/BuildSystem/config/packages/Umpire.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.version          = '2025.09.0'
+    self.version          = '2025.12.0'
     self.gitcommit        = 'v'+self.version
     self.download         = ['git://https://github.com/LLNL/Umpire.git',
                              'https://github.com/LLNL/Umpire/archive/'+self.gitcommit+'.tar.gz']
