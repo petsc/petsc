6bdf0ee5c44837385a1dca9204e053aa3cafb981
Pierre Jolivet <pierre@joliv.et>
2026-03-28 17:05:41 +0100

    configure: update CMake/Htool/OpenBLAS/ScaLAPACK/Netlib LAPACK

--- config/examples/arch-ci-freebsd-pkgs-opt.py
+++ config/examples/arch-ci-freebsd-pkgs-opt.py
@@ -10,7 +10,7 @@ configure_options = [
   '--useThreads=0', # for some reason cmake hangs when invoked from configure on bsd?
   '--download-netlib-lapack=1',
   '--with-netlib-lapack-c-bindings=1',
-  '--download-pastix=1',
+  #'--download-pastix=1', disabled as it does not work with Netlib LAPACK 3.12.1, can use with --download-netlib-lapack-commit=v3.12.0
   '--download-hwloc=1',
   '--with-mpi-dir=/home/svcpetsc/soft/mpich-4.2.2',
   '--download-metis=1',
