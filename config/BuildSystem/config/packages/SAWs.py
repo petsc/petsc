5affe32f9d22c19cbd14053bc91197b52fecf188
Barry Smith <bsmith@mcs.anl.gov>
2026-05-03 21:04:43 -0400

    SAWs: Update to new snapshot (and URL) that provides an MCP server
    - Add tech report number

--- config/BuildSystem/config/packages/SAWs.py
+++ config/BuildSystem/config/packages/SAWs.py
@@ -5,8 +5,8 @@ import os
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.gitcommit   = 'c483e26' # jan 23, 2019
-    self.download    = ['git://https://bitbucket.org/saws/saws.git','https://bitbucket.org/saws/saws/get/'+self.gitcommit+'.tar.gz']
+    self.gitcommit   = '028a32f0118f227c9264bebdc282ab71f8eb88db' # May 3, 2026
+    self.download    = ['git://https://gitlab.com/petsc/saws.git','https://gitlab.com/petsc/saws/get/'+self.gitcommit+'.tar.gz']
     self.functions   = ['SAWs_Register']
     self.includes    = ['SAWs.h']
     self.liblist     = [['libSAWs.a']]
