d4da8db68aa82dae56f3a1627fea98e9ab6b6811
Jose E. Roman <jroman@dsic.upv.es>
2026-06-07 13:26:07 +0200

    No need to have a separate header file

--- src/ksp/ksp/impls/idr/idr.c
+++ src/ksp/ksp/impls/idr/idr.c
@@ -1,4 +1,21 @@
-#include <../src/ksp/ksp/impls/idr/idrimpl.h> /*I  "petscksp.h"  I*/
+
+#include <petsc/private/kspimpl.h> /*I "petscksp.h" I*/
+
+typedef struct {
+  PetscInt     s;     /* shadow space dimension; default 4 */
+  Vec         *GG;    /* s direction vectors G[0..s-1] */
+  Vec         *UU;    /* s update vectors   U[0..s-1] */
+  Vec         *PP;    /* s shadow vectors   P[0..s-1] (fixed, random orthonormal) */
+  Vec          r;     /* current residual */
+  Vec          v;     /* work vector */
+  Vec          t;     /* work vector (preconditioned operator result) */
+  Vec          guess; /* saved initial guess, used with right preconditioning */
+  PetscScalar *M;     /* s*s matrix M[j,k] = <G[k],P[j]>, column-major */
+  PetscScalar *f;     /* length s: P^T r */
+  PetscScalar *c;     /* length s: solution of M c = f */
+  PetscReal    cth;   /* omega stabilization threshold (0 = off, default 0.7) */
+  PetscRandom  rand;  /* random context to initialize shadow vectors */
+} KSP_IDR;
 
 /*
    KSPIDRInitShadowSpace_IDR - Fill shadow space P[0..s-1] with random
