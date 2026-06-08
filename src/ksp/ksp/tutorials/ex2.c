498c6c6e584d44cd2a6b13a854a9c6bac6c94933
Jose E. Roman <jroman@dsic.upv.es>
2026-06-08 06:45:50 +0200

    CI: merge all IDR tests into a testset

--- src/ksp/ksp/tutorials/ex2.c
+++ src/ksp/ksp/tutorials/ex2.c
@@ -408,23 +408,22 @@ int main(int argc, char **args)
       nsize: 2
       args: -ksp_view -pc_type redundant -redundant_pc_type redundant -redundant_redundant_pc_type redundant -redundant_redundant_redundant_pc_type redundant -redundant_redundant_redundant_redundant_pc_type redundant
 
-   test:
-      suffix: idr
-      args: -ksp_type idr -pc_type ilu
-
-   test:
-      suffix: idr_1
-      args: -ksp_type idr -ksp_idr_s 1 -pc_type ilu
-
-   test:
-      suffix: idr_2
-      nsize: 2
-      args: -ksp_type idr -pc_type jacobi
-
-   test:
-      suffix: idr_3
-      requires: !complex
-      nsize: 4
-      args: -ksp_type idr -pc_type bjacobi
+   testset:
+      args: -ksp_type idr
+      test:
+         suffix: idr
+         args: -pc_type ilu
+      test:
+         suffix: idr_1
+         args: -ksp_idr_s 1 -pc_type ilu
+      test:
+         suffix: idr_2
+         nsize: 2
+         args: -pc_type jacobi
+      test:
+         suffix: idr_3
+         requires: !complex
+         nsize: 4
+         args: -pc_type bjacobi
 
  TEST*/
