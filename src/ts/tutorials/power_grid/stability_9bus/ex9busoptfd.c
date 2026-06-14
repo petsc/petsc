4f8a009a160042394d2f5fbf328b451b3a738444
Barry Smith <bsmith@mcs.anl.gov>
2026-06-14 13:54:24 -0400

    Fix unrelated error in requires:

--- src/ts/tutorials/power_grid/stability_9bus/ex9busoptfd.c
+++ src/ts/tutorials/power_grid/stability_9bus/ex9busoptfd.c
@@ -1140,7 +1140,7 @@ PetscErrorCode FormFunction(Tao tao, Vec P, PetscReal *f, PetscCtx ctx0)
 /*TEST
 
   build:
-      requires: double !complex !defined(USE_64BIT_INDICES)
+      requires: double !complex !defined(PETSC_USE_64BIT_INDICES)
 
    test:
       args: -viewer_binary_skip_info -tao_monitor -tao_gttol .2
