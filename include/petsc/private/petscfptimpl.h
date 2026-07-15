4d8d7e8de8e11abeddcf1cecf109eef45537768d
Pierre Jolivet <pierre@joliv.et>
2026-07-15 20:33:14 +0200

    include/petsc/private/petscfptimpl.h: switch to PetscCount

--- include/petsc/private/petscfptimpl.h
+++ include/petsc/private/petscfptimpl.h
@@ -14,17 +14,17 @@
 
 typedef struct _n_PetscFPT *PetscFPT;
 struct _n_PetscFPT {
-  void        **functionpointer;
-  char        **functionname;
-  PetscInt      count;
-  unsigned long tablesize;
+  void     **functionpointer;
+  char     **functionname;
+  PetscCount count;
+  PetscCount tablesize;
 };
 PETSC_INTERN PetscFPT PetscFPTData;
 
-static inline PetscErrorCode PetscFPTView(PetscViewer viewer)
+static inline PetscErrorCode PetscFPTView(PETSC_UNUSED PetscViewer viewer)
 {
   if (PetscFPTData) {
-    for (unsigned long i = 0; i < PetscFPTData->tablesize; ++i) {
+    for (PetscCount i = 0; i < PetscFPTData->tablesize; ++i) {
       if (PetscFPTData->functionpointer[i]) printf("%s()\n", PetscFPTData->functionname[i]);
     }
   }
@@ -50,33 +50,33 @@ static inline PetscErrorCode PetscFPTDestroy(void)
 .     n - expected number of keys
 
 */
-static inline PetscErrorCode PetscFPTCreate(PetscInt n)
+static inline PetscErrorCode PetscFPTCreate(PetscCount n)
 {
   PetscFPT _PetscFPTData;
 
   PetscCheck(n >= 0, PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "n < 0");
   /* Cannot use PetscNew() here because it is not yet defined in the include file chain */
   PetscCallQ(PetscMalloc(sizeof(struct _n_PetscFPT), &_PetscFPTData));
-  _PetscFPTData->tablesize = (3 * n) / 2 + 17;
-  if (_PetscFPTData->tablesize < (unsigned long)n) _PetscFPTData->tablesize = PETSC_INT_MAX / 4; /* overflow */
-  PetscCallQ(PetscCalloc(sizeof(void *) * _PetscFPTData->tablesize, &_PetscFPTData->functionpointer));
-  PetscCallQ(PetscMalloc(sizeof(char **) * _PetscFPTData->tablesize, &_PetscFPTData->functionname));
+  if (n > PETSC_COUNT_MAX - n / 2 - 17) _PetscFPTData->tablesize = PETSC_COUNT_MAX / 4;
+  else _PetscFPTData->tablesize = n + n / 2 + 17;
+  PetscCallQ(PetscCalloc1(_PetscFPTData->tablesize, &_PetscFPTData->functionpointer));
+  PetscCallQ(PetscMalloc1(_PetscFPTData->tablesize, &_PetscFPTData->functionname));
   _PetscFPTData->count = 0;
   PetscFPTData         = _PetscFPTData;
   return PETSC_SUCCESS;
 }
 
-static inline unsigned long PetscFPTHashPointer(void *ptr)
+static inline PetscCount PetscFPTHashPointer(void *ptr)
 {
 #define PETSC_FPT_HASH_FACT 79943
-  return (PETSC_FPT_HASH_FACT * ((size_t)ptr)) % PetscFPTData->tablesize;
+  return (PetscCount)((PETSC_FPT_HASH_FACT * ((size_t)ptr)) % (size_t)PetscFPTData->tablesize);
 }
 
 static inline PetscErrorCode PetscFPTAdd(void *key, const char *data)
 {
   PetscCheck(data, PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "Null function name");
   if (!PetscFPTData) return PETSC_SUCCESS;
-  for (unsigned long i = 0, hash = PetscFPTHashPointer(key); i < PetscFPTData->tablesize; ++i) {
+  for (PetscCount i = 0, hash = PetscFPTHashPointer(key); i < PetscFPTData->tablesize; ++i) {
     if (PetscFPTData->functionpointer[hash] == key) {
       PetscFPTData->functionname[hash] = (char *)data;
       return PETSC_SUCCESS;
@@ -99,7 +99,7 @@ static inline PetscErrorCode PetscFPTAdd(void *key, const char *data)
 */
 static inline PetscErrorCode PetscFPTFind(void *key, char const **data)
 {
-  unsigned long hash, ii = 0;
+  PetscCount hash, ii = 0;
 
   *data = NULL;
   if (!PetscFPTData) return PETSC_SUCCESS;
