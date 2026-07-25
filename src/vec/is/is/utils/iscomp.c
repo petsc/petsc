603eb521b7ed1236ae2cdd2509d34b54a40db57b
Pierre Jolivet <pierre@joliv.et>
2026-07-25 16:02:48 +0200

    ISEqual: style fix

--- src/vec/is/is/utils/iscomp.c
+++ src/vec/is/is/utils/iscomp.c
@@ -36,7 +36,6 @@ PetscErrorCode ISEqual(IS is1, IS is2, PetscBool *flg)
 {
   PetscInt        sz1, sz2, *a1, *a2;
   const PetscInt *ptr1, *ptr2;
-  PetscBool       flag;
   MPI_Comm        comm;
   PetscMPIInt     mflg;
 
@@ -63,7 +62,7 @@ PetscErrorCode ISEqual(IS is1, IS is2, PetscBool *flg)
     PetscCall(ISGetLocalSize(is1, &sz1));
     PetscCall(ISGetLocalSize(is2, &sz2));
 
-    if (sz1 != sz2) flag = PETSC_FALSE;
+    if (sz1 != sz2) *flg = PETSC_FALSE;
     else {
       PetscCall(ISGetIndices(is1, &ptr1));
       PetscCall(ISGetIndices(is2, &ptr2));
@@ -76,7 +75,7 @@ PetscErrorCode ISEqual(IS is1, IS is2, PetscBool *flg)
 
       PetscCall(PetscIntSortSemiOrdered(sz1, a1));
       PetscCall(PetscIntSortSemiOrdered(sz2, a2));
-      PetscCall(PetscArraycmp(a1, a2, sz1, &flag));
+      PetscCall(PetscArraycmp(a1, a2, sz1, flg));
 
       PetscCall(ISRestoreIndices(is1, &ptr1));
       PetscCall(ISRestoreIndices(is2, &ptr2));
@@ -85,7 +84,7 @@ PetscErrorCode ISEqual(IS is1, IS is2, PetscBool *flg)
       PetscCall(PetscFree(a2));
     }
     PetscCall(PetscObjectGetComm((PetscObject)is1, &comm));
-    PetscCallMPI(MPIU_Allreduce(&flag, flg, 1, MPI_C_BOOL, MPI_LAND, comm));
+    PetscCallMPI(MPIU_Allreduce(MPI_IN_PLACE, flg, 1, MPI_C_BOOL, MPI_LAND, comm));
   }
   PetscFunctionReturn(PETSC_SUCCESS);
 }
